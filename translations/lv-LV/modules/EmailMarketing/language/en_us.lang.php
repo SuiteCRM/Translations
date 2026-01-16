fb0a22db845e323b0e5a7dea8669a7ef68c10dc9
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-16 21:12:18 +0000

    New translations en_us.lang.php (Latvian)

---
+++ translations/lv-LV/modules/EmailMarketing/language/en_us.lang.php
@@ -0,0 +1,108 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_REPLY_ADDR' => '"Atbildēt-kam" adrese:',
+    'LBL_REPLY_NAME' => '"Atbildēt-kam" vārds:',
+
+    'LBL_MODULE_NAME' => 'E-pasta mārketings',
+    'LBL_MODULE_TITLE' => 'E-pasta mārketings',
+    'LBL_LIST_FORM_TITLE' => 'E-pasta marketinga kampaņas',
+    'LBL_NAME' => 'Nosaukums:',
+    'LBL_LIST_NAME' => 'Vārds:',
+    'LBL_LIST_FROM_ADDR' => 'Sūtītāja e-pasts',
+    'LBL_LIST_DATE_START' => 'Sākuma datums:',
+    'LBL_LIST_TEMPLATE_NAME' => 'E-pasta veidne',
+    'LBL_LIST_STATUS' => 'Statuss',
+    'LBL_STATUS' => 'Statuss',
+    'LBL_STATUS_TEXT' => 'Statuss:',
+    'LBL_TEMPLATE_NAME' => 'Veidnes nosaukums',
+    'LBL_DATE_ENTERED' => 'Izveidots',
+    'LBL_DATE_MODIFIED' => 'Modificēts',
+    'LBL_MODIFIED' => 'Modificēja:',
+    'LBL_CREATED' => 'Izveidoja:',
+    'LBL_MESSAGE_FOR' => 'Sūtīt šo ziņu uz:',
+
+    'LBL_FROM_NAME' => 'Sūtītāja vārds:',
+    'LBL_FROM_ADDR' => 'Sūtītāja e-pasts:',
+    'LBL_DATE_START' => 'Sākuma datums:',
+    'LBL_TIME_START' => 'Sākuma laiks',
+    'LBL_START_DATE_TIME' => 'Sākuma datums un laiks:',
+    'LBL_TEMPLATE' => 'E-pasta veidne:',
+
+    'LBL_MODIFIED_BY' => 'Modificēja:',
+    'LBL_CREATED_BY' => 'Izveidoja:',
+
+    'LNK_NEW_CAMPAIGN' => 'Izveidot kampaņu (Vednis)',
+    'LNK_CAMPAIGN_LIST' => 'Mārketings',
+    'LNK_NEW_PROSPECT_LIST' => 'Izveidot mērķu sarakstu',
+    'LNK_PROSPECT_LIST_LIST' => 'Mērķu saraksti',
+    'LNK_NEW_PROSPECT' => 'Izveidot mērķi',
+    'LNK_PROSPECT_LIST' => 'Mērķi',
+    'LBL_DEFAULT_SUBPANEL_TITLE' => 'E-pasta mārketings',
+    'LBL_CREATE_EMAIL_TEMPLATE' => 'Jauns',
+    'LBL_EDIT_EMAIL_TEMPLATE' => 'Rediģēt',
+    'LBL_FROM_MAILBOX' => 'No pastkastes',
+    'LBL_FROM_MAILBOX_NAME' => 'Lietot pastkasti:',
+    'LBL_OUTBOUND_EMAIL_ACCOUNT_NAME' => 'Outbound Email Account:',
+    'LBL_PROSPECT_LIST_SUBPANEL_TITLE' => 'Mērķu saraksti',
+    'LBL_ALL_PROSPECT_LISTS' => 'Atzīmēt lai kampaņā izmantotu visus mērķu sarakstus.',
+    'LBL_RELATED_PROSPECT_LISTS' => 'Visi ar šo ziņu saistītie mērķu saraksti.',
+    'LBL_PROSPECT_LIST_NAME' => 'Mērķu saraksta nosaukums',
+
+    'LBL_LIST_PROSPECT_LIST_NAME' => 'Targeted Lists',
+    'LBL_MODULE_SEND_TEST' => 'Campaign: Send Test',
+    'LBL_MODULE_SEND_EMAILS' => 'Campaign: Send Emails',
+    'LBL_SCHEDULE_MESSAGE_TEST' => 'Please select the campaign messages that you would like to test:',
+    'LBL_SCHEDULE_MESSAGE_EMAILS' => 'Please select the campaign messages that you would like to schedule for distribution on the specified start date and time:',
+    'LBL_SCHEDULE_BUTTON_TITLE' => 'Sūtīt',
+    'LBL_SCHEDULE_BUTTON_LABEL' => 'Sūtīt',
+    'LBL_ERROR_ON_MARKETING' => 'Iztrūkst obligātie lauki',
+
+    'LBL_CAMPAIGN_ID' => 'Kampaņas ID',
+    'LBL_OUTBOUND_EMAIL_ACOUNT_ID' => 'Outbound Email Account ID',
+    'LBL_EMAIL_TEMPLATE' => 'E-pasta veidne',
+    'LBL_PROSPECT_LISTS' => 'Prospect Lists',
+
+);
