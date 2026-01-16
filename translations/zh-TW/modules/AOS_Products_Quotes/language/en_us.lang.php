d81eea49b147565d242b3945a6d90eba195d1b71
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-16 20:24:21 +0000

    New translations en_us.lang.php (Chinese Traditional)

---
+++ translations/zh-TW/modules/AOS_Products_Quotes/language/en_us.lang.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID' => '負責人Id',
+    'LBL_ASSIGNED_TO_NAME' => '負責人',
+    'LBL_ID' => '編號',
+    'LBL_DATE_ENTERED' => '建立日期',
+    'LBL_DATE_MODIFIED' => '修改日期',
+    'LBL_MODIFIED' => '修改人',
+    'LBL_MODIFIED_NAME' => '修改人姓名',
+    'LBL_CREATED' => '建立人',
+    'LBL_DESCRIPTION' => '備註',
+    'LBL_DELETED' => '已刪除',
+    'LBL_NAME' => '名稱',
+    'LBL_CREATED_USER' => '創建者',
+    'LBL_MODIFIED_USER' => '修改的使用者',
+    'LBL_LIST_FORM_TITLE' => '產品報價清單',
+    'LBL_MODULE_NAME' => '行項目',
+    'LBL_MODULE_TITLE' => '產品報價: 首頁',
+    'LBL_HOMEPAGE_TITLE' => '我的產品報價',
+    'LNK_NEW_RECORD' => '創建產品報價',
+    'LNK_LIST' => '產品報價',
+    'LBL_SEARCH_FORM_TITLE' => '搜尋產品報價',
+    'LBL_HISTORY_SUBPANEL_TITLE' => '查看歷史記錄',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => '銷售活動',
+    'LBL_NEW_FORM_TITLE' => '新產品報價',
+    'LBL_PRODUCT_NAME' => '名稱',
+    'LBL_PRODUCT_QTY' => '數量',
+    'LBL_PRODUCT_COST_PRICE' => '成本',
+    'LBL_PRODUCT_LIST_PRICE' => '列出單價',
+    'LBL_PRODUCT_UNIT_PRICE' => '實際單價',
+    'LBL_PRODUCT_DISCOUNT' => '折扣',
+    'LBL_PRODUCT_DISCOUNT_AMOUNT' => '折扣金額',
+    'LBL_PART_NUMBER' => '部件編號',
+    'LBL_PRODUCT_DESCRIPTION' => '描述',
+    'LBL_DISCOUNT' => '折扣類型',
+    'LBL_VAT_AMT' => '稅額',
+    'LBL_VAT' => '稅金',
+    'LBL_PRODUCT_TOTAL_PRICE' => '總價',
+    'LBL_PRODUCT_NOTE' => '備註',
+    'Quote' => '',
+    'LBL_FLEX_RELATE' => '關聯到',
+    'LBL_PRODUCT' => '產品',
+
+    'LBL_SERVICE_MODULE_NAME' => '服務',
+    'LBL_LIST_NUM' => '數量',
+    'LBL_PARENT_ID' => '父記錄ID',
+    'LBL_GROUP_NAME' => '群組',
+    'LBL_PRODUCT_COST_PRICE_USDOLLAR' => '成本 (預設貨幣)',
+    'LBL_PRODUCT_LIST_PRICE_USDOLLAR' => '列出單價 (預設貨幣)',
+    'LBL_PRODUCT_UNIT_PRICE_USDOLLAR' => '實際單價 (預設貨幣)',
+    'LBL_PRODUCT_TOTAL_PRICE_USDOLLAR' => '總價 (預設貨幣)',
+    'LBL_PRODUCT_DISCOUNT_USDOLLAR' => '折扣(預設貨幣)',
+    'LBL_PRODUCT_DISCOUNT_AMOUNT_USDOLLAR' => '折扣金額 (預設貨幣)',
+    'LBL_VAT_AMT_USDOLLAR' => '稅金 (預設貨幣)',
+    'LBL_PRODUCTS_SERVICES' => '產品 / 服務',
+    'LBL_PRODUCT_ID' => '產品 ID',
+
+    'LBL_AOS_CONTRACTS' => '合約',
+    'LBL_AOS_INVOICES' => '發票',
+    'LBL_AOS_PRODUCTS' => '產品',
+    'LBL_AOS_QUOTES' => '報價單',
+);
