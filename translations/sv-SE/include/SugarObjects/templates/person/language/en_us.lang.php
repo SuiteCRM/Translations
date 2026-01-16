f7b4bd3078ea16330b303735cdfecc2ad829638a
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-16 19:50:54 +0000

    New translations en_us.lang.php (Swedish)

---
+++ translations/sv-SE/include/SugarObjects/templates/person/language/en_us.lang.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_SALUTATION' => 'Hälsningsfras',
+    'LBL_NAME' => 'Namn',
+    'LBL_FIRST_NAME' => 'Förnamn',
+    'LBL_LAST_NAME' => 'Efternamn',
+    'LBL_TITLE' => 'Rubrik',
+    'LBL_DEPARTMENT' => 'Avdelning',
+    'LBL_DO_NOT_CALL' => 'Ring inte',
+    'LBL_HOME_PHONE' => 'Hem Telefon',
+    'LBL_MOBILE_PHONE' => 'Mobiltelefon',
+    'LBL_OFFICE_PHONE' => 'Jobb Telefon',
+    'LBL_OTHER_PHONE' => 'Annan Telefon',
+    'LBL_FAX_PHONE' => 'Fax',
+    'LBL_EMAIL_ADDRESS' => 'E-postadress(er)',
+    'LBL_PRIMARY_ADDRESS' => 'Primär Adress',
+    'LBL_PRIMARY_ADDRESS_STREET' => 'Primär Adress',
+    'LBL_PRIMARY_ADDRESS_STREET_2' => 'Primär adress Gatuvägen 2:',
+    'LBL_PRIMARY_ADDRESS_STREET_3' => 'Primär adress Gatuvägen 3:',
+    'LBL_PRIMARY_ADDRESS_CITY' => 'Stad',
+    'LBL_PRIMARY_ADDRESS_STATE' => 'Landskap',
+    'LBL_PRIMARY_ADDRESS_POSTALCODE' => 'Postnummer',
+    'LBL_PRIMARY_ADDRESS_COUNTRY' => 'Primär Landsadress:',
+    'LBL_ALT_ADDRESS' => 'Annan Adress',
+    'LBL_ALT_ADDRESS_STREET' => 'Annan Adress',
+    'LBL_ALT_ADDRESS_STREET_2' => 'Alternativ adress Gata/Väg 2:',
+    'LBL_ALT_ADDRESS_STREET_3' => 'Alternativ adress Gatuvägen 3:',
+    'LBL_ALT_ADDRESS_CITY' => 'Stad',
+    'LBL_ALT_ADDRESS_STATE' => 'Landskap',
+    'LBL_ALT_ADDRESS_POSTALCODE' => 'Postnummer',
+    'LBL_ALT_ADDRESS_COUNTRY' => 'Land',
+    'LBL_PRIMARY_STREET' => 'Adress:',
+    'LBL_ALT_STREET' => 'Annan Adress',
+    'LBL_STREET' => 'Annan Adress',
+    'LBL_CITY' => 'Stad',
+    'LBL_STATE' => 'Landskap',
+    'LBL_POSTAL_CODE' => 'Postnummer',
+    'LBL_COUNTRY' => 'Land',
+    'LBL_CONTACT_INFORMATION' => 'Kontakt Information',
+    'LBL_ADDRESS_INFORMATION' => 'Adress(er)',
+    'LBL_ASSIGNED_TO_NAME' => 'Användare',
+    'LBL_OTHER_EMAIL_ADDRESS' => 'Annan E-post:',
+    'LBL_ASSISTANT' => 'Assistent',
+    'LBL_ASSISTANT_PHONE' => 'Assistent Telefon',
+    'LBL_WORK_PHONE' => 'Jobb Telefon',
+    'LNK_IMPORT_VCARD' => 'Skapa från vCard',
+    'LBL_ANY_EMAIL' => 'Någon E-post:',
+    'LBL_EMAIL_NON_PRIMARY' => 'Inga primära mail',
+    'LBL_PHOTO' => 'Foto',
+    'LBL_EDIT_BUTTON' => 'Redigera',
+    'LBL_REMOVE' => 'Ta bort',
+
+    //Lawful Basis labels
+    'LBL_LAWFUL_BASIS' => 'Laglig grund',
+    'LBL_DATE_REVIEWED' => 'Laglig grund datum ses över',
+    'LBL_LAWFUL_BASIS_SOURCE' => 'Laglig källa',
+    'LBL_CONSENT' => 'Samtycke',
+    //End Lawful Basis labels
+);
