4094572287fbf323e34bb8864f4ec69f8dc91c60
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-16 20:13:46 +0000

    New translations en_us.lang.php (Latvian)

---
+++ translations/lv-LV/modules/AOR_Fields/language/en_us.lang.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ID' => 'ID',
+    'LBL_DATE_ENTERED' => 'Izveidošanas datums',
+    'LBL_DATE_MODIFIED' => 'Modificēts',
+    'LBL_MODIFIED' => 'Rediģēja',
+    'LBL_MODIFIED_NAME' => 'Modificēja vārds',
+    'LBL_CREATED' => 'Autors',
+    'LBL_DESCRIPTION' => 'Apraksts',
+    'LBL_DELETED' => 'Izdzēsts',
+    'LBL_NAME' => 'Vārds:',
+    'LBL_CREATED_USER' => 'Izveidoja lietotājs',
+    'LBL_MODIFIED_USER' => 'Modificēja',
+    'LBL_LIST_NAME' => 'Vārds:',
+    'LBL_EDIT_BUTTON' => 'Rediģēt',
+    'LBL_REMOVE' => 'Izņemt',
+    'LBL_ASSIGNED_TO_ID' => 'Piešķirts lietotājam ar ID',
+    'LBL_ASSIGNED_TO_NAME' => 'Piešķirts lietotājam',
+    'LBL_LIST_FORM_TITLE' => 'Fields List',
+    'LBL_MODULE_NAME' => 'Lauki',
+    'LBL_MODULE_TITLE' => 'Lauki',
+    'LBL_HOMEPAGE_TITLE' => 'My Fields',
+    'LNK_NEW_RECORD' => 'Create Fields',
+    'LNK_LIST' => 'Skatīt laukus',
+    'LBL_SEARCH_FORM_TITLE' => 'Search Fields',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Aplūkot vēsturi',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Darbības',
+    'LBL_NEW_FORM_TITLE' => 'New Fields',
+    'LBL_ORDER' => 'Secība',
+    'LBL_MODULE_PATH' => 'Modulis',
+    'LBL_FIELD' => 'Lauks',
+    'LBL_DISPLAY' => 'Display',
+    'LBL_LINK' => 'Saite',
+    'LBL_LABEL' => 'Etiķete',
+    'LBL_FUNCTION' => 'Function',
+    'LBL_SORT' => 'Kārtot',
+    'LBL_SORT_ORDER' => 'Sort Order',
+    'LBL_GROUP' => 'Grupa',
+    'LBL_GROUP_ORDER' => 'Group Order',
+    'LBL_AUDIT_TABLE' => 'Audit table',
+    'LBL_TOTAL' => 'Kopā',
+    'LBL_FORMAT' => 'Format Options',
+);
