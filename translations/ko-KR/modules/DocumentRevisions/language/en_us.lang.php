07b2e9dbc17924f9a3b176bf4c0636073a9c73bc
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-16 21:04:52 +0000

    New translations en_us.lang.php (Korean)

---
+++ translations/ko-KR/modules/DocumentRevisions/language/en_us.lang.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    //module
+    'LBL_MODULE_NAME' => '문서 버전',
+
+    'LNK_NEW_DOCUMENT' => '자료 작성',
+    'LNK_DOCUMENT_LIST' => '자료 리스트',
+
+    //vardef labels
+    'LBL_REVISION_NAME' => '개정번호',
+    'LBL_FILENAME' => '파일:',
+    'LBL_MIME' => 'Mime 형식',
+    'LBL_REVISION' => '개정판',
+    'LBL_DOCUMENT' => '관련 자료',
+    'LBL_LATEST_REVISION' => '최종개정판',
+    'LBL_CHANGE_LOG' => '변경로그',
+    'LBL_ACTIVE_DATE' => '개정일',
+    'LBL_EXPIRATION_DATE' => '만료일',
+    'LBL_FILE_EXTENSION' => '파일확장자',
+
+    'LBL_DOC_NAME' => '문서명:',
+    'LBL_DOC_VERSION' => '개정번호:',
+
+    //document revisions.
+    'LBL_REV_LIST_REVISION' => '개정판',
+    'LBL_REV_LIST_ENTERED' => '생성날짜',
+    'LBL_REV_LIST_CREATED' => '작성자',
+    'LBL_REV_LIST_LOG' => '변경로그',
+    'LBL_REV_LIST_FILENAME' => '파일',
+
+    'LBL_CURRENT_DOC_VERSION' => 'Latest Revision:',
+    'LBL_SEARCH_FORM_TITLE' => '자료 검색',
+    'LBL_REVISIONS' => '수정 사항',
+
+    //error messages
+    'ERR_FILENAME' => '파일명',
+    'ERR_DOC_VERSION' => '자료버젼',
+    'ERR_DELETE_CONFIRM' => '이 문서 수정본을 삭제하시겠습니까?',
+    'ERR_DELETE_LATEST_VERSION' => '문서의 최신 수정본을 삭제할 수 없습니다.',
+    'LNK_NEW_MAIL_MERGE' => '메일 병합',
+    'LBL_DOC_ID' => '문서 소스 ID',
+    'LBL_DOC_TYPE' => '소스',
+    'LBL_DOC_URL' => '문서 소스 URL',
+
+    'LBL_CREATED_BY_NAME' => 'Created by Name',
+);
