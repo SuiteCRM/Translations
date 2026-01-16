231916b670d1a6cf9badddcb28f424b529e0f843
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-16 21:07:22 +0000

    New translations en_us.lang.php (Albanian)

---
+++ translations/sq-AL/modules/DynamicFields/language/en_us.lang.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LNK_NEW_CALL' => 'Thirje identifikuese',
+    'LNK_NEW_MEETING' => 'Cakto mbledhje',
+    'LNK_NEW_TASK' => 'Krijo detyrë',
+    'LNK_NEW_NOTE' => 'Krijo shënim ose bashkëngjitje',
+    'LNK_NEW_EMAIL' => 'Emaili i arkivuar',
+    'LNK_CALL_LIST' => 'Thirjet',
+    'LNK_MEETING_LIST' => 'Mbledhjet',
+    'LNK_TASK_LIST' => 'Detyrat',
+    'LNK_NOTE_LIST' => 'Shënimet',
+    'LBL_ADD_FIELD' => 'fusha e shtuar',
+    'LBL_SEARCH_FORM_TITLE' => 'Kërkim i modulës',
+    'COLUMN_TITLE_NAME' => 'Emri i fushës',
+    'COLUMN_TITLE_DISPLAY_LABEL' => 'formati i shfaqur',
+    'COLUMN_TITLE_LABEL_VALUE' => 'vlera e klasifikimit',
+    'COLUMN_TITLE_LABEL' => 'systemi i klasifikimit',
+    'COLUMN_TITLE_DATA_TYPE' => 'lloji i të dhënave',
+    'COLUMN_TITLE_MAX_SIZE' => 'madhësia maksimum',
+    'COLUMN_TITLE_HELP_TEXT' => 'teksti ndihmës',
+    'COLUMN_TITLE_COMMENT_TEXT' => 'teksti komentues',
+    'COLUMN_TITLE_REQUIRED_OPTION' => 'fusha e kërkuar',
+    'COLUMN_TITLE_DEFAULT_VALUE' => 'Vlerë e gabuar',
+    'COLUMN_TITLE_FRAME_HEIGHT' => 'lartësia e kornizës',
+    'COLUMN_TITLE_HTML_CONTENT' => 'HTML',
+    'COLUMN_TITLE_URL' => 'URL e gabuar',
+    'COLUMN_TITLE_AUDIT' => 'Audito',
+    'COLUMN_TITLE_MIN_VALUE' => 'Vlera minimale',
+    'COLUMN_TITLE_MAX_VALUE' => 'Vlera maksimale',
+    'COLUMN_TITLE_LABEL_ROWS' => 'Reshtat',
+    'COLUMN_TITLE_LABEL_COLS' => 'Kolonat',
+    'COLUMN_TITLE_DISPLAYED_ITEM_COUNT' => '# artikujt e shfaqur',
+    'COLUMN_TITLE_AUTOINC_NEXT' => 'Rritje automatike e vlerës së ardhëshme',
+    'COLUMN_DISABLE_NUMBER_FORMAT' => 'format i çaktivizuar',
+    'COLUMN_TITLE_ENABLE_RANGE_SEARCH' => 'Mundëso kërkim vargu',
+    'LBL_DROP_DOWN_LIST' => 'Lista e mëposhtme',
+    'LBL_RADIO_FIELDS' => 'fushat e radios',
+    'LBL_MULTI_SELECT_LIST' => 'Lista e multi selektuar',
+    'COLUMN_TITLE_PRECISION' => 'Saktësia',
+    'LBL_MODULE' => 'Modul',
+    'COLUMN_TITLE_MASS_UPDATE' => 'Masa e rinovuar',
+    'COLUMN_TITLE_IMPORTABLE' => 'E transferuar',
+    'COLUMN_TITLE_DUPLICATE_MERGE' => 'dupliko bashkimin',
+    'LBL_LABEL' => 'Label',
+    'LBL_DATA_TYPE' => 'lloji i të dhënave',
+    'LBL_DEFAULT_VALUE' => 'Vlerë e gabuar',
+    'ERR_RESERVED_FIELD_NAME' => "Fjala kyçe e rezervuar",
+    'ERR_SELECT_FIELD_TYPE' => 'Ju lutemi selektoni llojin e fushës',
+    'ERR_FIELD_NAME_ALREADY_EXISTS' => 'Emri i fushës tashmë ekziston',
+    'LBL_BTN_ADD' => 'shto',
+    'LBL_BTN_EDIT' => 'Ndrysho',
+    'LBL_GENERATE_URL' => 'Gjenero URL',
+    'LBL_CALCULATED' => 'vlera e kalkuluar',
+    'LBL_LINK_TARGET' => 'lidhje e hapur në',
+    'LBL_IMAGE_WIDTH' => 'gjerësia',
+    'LBL_IMAGE_HEIGHT' => 'lartësia',
+    'LBL_IMAGE_BORDER' => 'kufiri',
+    'LBL_HELP' => 'ndihmë' /*for 508 compliance fix*/,
+    'COLUMN_TITLE_INLINE_EDIT_TEXT' => 'Inline Edit',
+    'COLUMN_TITLE_PARENT_ENUM' => 'Parent DropDown',
+    'LBL_INSERT_FIELD' => 'Insert Field',
+);
