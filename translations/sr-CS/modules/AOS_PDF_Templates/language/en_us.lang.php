20a024380ccd3cebc0ce8d87cfc532a141e8d42e
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-16 20:21:20 +0000

    New translations en_us.lang.php (Serbian (Latin))

---
+++ translations/sr-CS/modules/AOS_PDF_Templates/language/en_us.lang.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID' => 'ID broj dodeljenog korisnika',
+    'LBL_ASSIGNED_TO_NAME' => 'Dodeljen',
+    'LBL_ID' => 'ID broj',
+    'LBL_DATE_ENTERED' => 'Datum kreiranja',
+    'LBL_DATE_MODIFIED' => 'Datum izmene',
+    'LBL_MODIFIED' => 'Izmenio',
+    'LBL_MODIFIED_NAME' => 'Promenio',
+    'LBL_CREATED' => 'Autor',
+    'LBL_DESCRIPTION' => 'Sadržaj',
+    'LBL_HEADER' => 'Zaglavlje',
+    'LBL_FOOTER' => 'Podnožje',
+    'LBL_DELETED' => 'Obrisan',
+    'LBL_NAME' => 'Ime',
+    'LBL_CREATED_USER' => 'Autor je korisnik',
+    'LBL_MODIFIED_USER' => 'Promenio korisnik',
+    'LBL_LIST_FORM_TITLE' => 'Lista PDF šablona',
+    'LBL_MODULE_NAME' => 'PDF šabloni',
+    'LBL_MODULE_TITLE' => 'PDF šabloni: Početna',
+    'LBL_HOMEPAGE_TITLE' => 'Moji PDF šabloni',
+    'LNK_NEW_RECORD' => 'Kreiraj PDF šablone',
+    'LNK_LIST' => 'Prikaži PDF šablone',
+    'LBL_SEARCH_FORM_TITLE' => 'Pretraži PDF šablone',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Pregled istorije',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Aktivnosti',
+    'LBL_NEW_FORM_TITLE' => 'Novi PDF šablon',
+    'LBL_TYPE' => 'Tip',
+    'LBL_ACTIVE' => 'Aktivan',
+    'LBL_BUTTON_INSERT' => 'Ubaci',
+    'LBL_WARNING_OVERWRITE' => 'Upozorenje: Ovo će prepisati preko vašeg postojećeg rada',
+    'LBL_INSERT_FIELDS' => 'Ubaci Polja',
+
+    'LBL_SAMPLE' => 'Učitaj primer',
+    'LBL_PAGE' => 'Stranica',
+    'LBL_PREPARED_FOR' => 'Priremljen Za',
+    'LBL_PREPARED_BY' => 'Pripremio',
+    'LBL_QUOTE_SAMPLE' => 'Quote Sample',
+    'LBL_INVOICE_SAMPLE' => 'Invoice Sample',
+    'LBL_ACCOUNT_SAMPLE' => 'Account Sample',
+    'LBL_CONTACT_SAMPLE' => 'Contact Sample',
+    'LBL_LEAD_SAMPLE' => 'Lead Sample',
+    'LBL_ANY_STREET' => 'Bilo koja Ulica',
+    'LBL_ANY_TOWN' => 'Bilo koji Grad',
+    'LBL_ANY_WHERE' => 'Bilo Gde',
+
+    'LBL_QUOTE_GROUP_SAMPLE' => 'Quote Group Sample',
+    'LBL_INVOICE_GROUP_SAMPLE' => 'Invoice Group Sample',
+    'LBL_MARGIN_LEFT' => 'Leva Margina',
+    'LBL_MARGIN_RIGHT' => 'Desna Margina',
+    'LBL_MARGIN_TOP' => 'Gornja Margina',
+    'LBL_MARGIN_BOTTOM' => 'Donja Margina',
+    'LBL_MARGIN_HEADER' => 'Zaglavlje Margina',
+    'LBL_MARGIN_FOOTER' => 'Podnožje Margina',
+    'LBL_EDITVIEW_PANEL1' => 'Margine',
+    'LBL_DETAILVIEW_PANEL1' => 'Margine',
+    'LBL_PAGE_SIZE' => 'Veličina stranice',
+    'LBL_ORIENTATION' => 'Orjentacija',
+);
