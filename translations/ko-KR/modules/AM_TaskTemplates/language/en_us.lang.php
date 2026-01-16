7e4d63c208a1fe680e3660dbcf732b698d773afe
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-16 20:04:08 +0000

    New translations en_us.lang.php (Korean)

---
+++ translations/ko-KR/modules/AM_TaskTemplates/language/en_us.lang.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID' => '할당된 사용자 Id',
+    'LBL_ASSIGNED_TO_NAME' => '담당자',
+    'LBL_ID' => 'ID',
+    'LBL_DATE_ENTERED' => '생성날짜',
+    'LBL_DATE_MODIFIED' => '수정일',
+    'LBL_MODIFIED' => '변경됨',
+    'LBL_MODIFIED_ID' => 'Id로 변경됨',
+    'LBL_MODIFIED_NAME' => '이름으로 변경됨',
+    'LBL_CREATED' => '작성자',
+    'LBL_CREATED_ID' => 'Id로 생성됨',
+    'LBL_DESCRIPTION' => '상세',
+    'LBL_DELETED' => '삭제됨',
+    'LBL_NAME' => '업무 이름',
+    'LBL_CREATED_USER' => '사용자로 생성됨',
+    'LBL_MODIFIED_USER' => '변경유저',
+    'LBL_LIST_NAME' => '이름',
+    'LBL_EDIT_BUTTON' => '편집',
+    'LBL_REMOVE' => '삭제',
+    'LBL_LIST_FORM_TITLE' => '프로젝트 업무 서식 목록',
+    'LBL_MODULE_NAME' => '프로젝트 업무 서식',
+    'LBL_MODULE_TITLE' => '프로젝트 업무 서식',
+    'LBL_HOMEPAGE_TITLE' => '내 프로젝트 업무 서식',
+    'LNK_NEW_RECORD' => '프로젝트 업무 서식 작성',
+    'LNK_LIST' => '프로젝트 업무 서식 보기',
+    'LNK_IMPORT_AM_TASKTEMPLATES' => '프로젝트 업무 서식 가져오기',
+    'LBL_SEARCH_FORM_TITLE' => '프로젝트 업무 서식 검색',
+    'LBL_HISTORY_SUBPANEL_TITLE' => '이력 표시',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => '영업활동',
+    'LBL_AM_TASKTEMPLATES_SUBPANEL_TITLE' => '프로젝트 업무 서식',
+    'LBL_NEW_FORM_TITLE' => '신규 프로젝트 업무 서식',
+    'LBL_STATUS' => '상태',
+    'LBL_PRIORITY' => '우선순위',
+    'LBL_PERCENT_COMPLETE' => '완료율',
+    'LBL_PREDECESSORS' => '선행 작업',
+    'LBL_MILESTONE_FLAG' => '이정표 플래그',
+    'LBL_RELATIONSHIP_TYPE' => '관계 유형',
+    'LBL_TASK_NUMBER' => '업무 번호',
+    'LBL_ORDER_NUMBER' => '주문 ',
+    'LBL_ESTIMATED_EFFORT' => '예상된 작업량 (시간)',
+    'LBL_UTILIZATION' => '사용률 (%)',
+    'LBL_DURATION' => '기간 (일)',
+    'LBL_AM_TASKTEMPLATES_AM_PROJECTTEMPLATES_FROM_AM_PROJECTTEMPLATES_TITLE' => '프로젝트 서식',
+
+    'LBL_AM_TASKTEMPLATES_AM_PROJECTTEMPLATES_FROM_AM_TASKTEMPLATES_TITLE' => 'Task Templates: Project Templates from Task Templates Title',
+);
