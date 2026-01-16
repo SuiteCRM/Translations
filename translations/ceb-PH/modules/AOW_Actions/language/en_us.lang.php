50bbf1e92f8528d58d142806f0cd673f371c72a1
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-16 20:27:05 +0000

    New translations en_us.lang.php (Cebuano)

---
+++ translations/ceb-PH/modules/AOW_Actions/language/en_us.lang.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ID' => 'ID',
+    'LBL_DATE_ENTERED' => 'Petsa nga gihimo',
+    'LBL_DATE_MODIFIED' => 'Petsa nga gi-usab',
+    'LBL_MODIFIED' => 'Giusab Ni',
+    'LBL_MODIFIED_ID' => 'Ge bag-o ni Pangalan',
+    'LBL_MODIFIED_NAME' => 'Giusab pinaagi sa Pangan',
+    'LBL_CREATED_USER' => 'Gihimo ni User',
+    'LBL_MODIFIED_USER' => 'Giusab ni User',
+    'LBL_CREATED' => 'Gihimo ni',
+    'LBL_DESCRIPTION' => 'Deskripsyon',
+    'LBL_CREATED_ID' => 'Gebuhat ni',
+    'LBL_DELETED' => 'Gitangtang',
+    'LBL_NAME' => 'Pangalan',
+    'LBL_MODULE_NAME' => 'Mga Aksyon sa WorkFlow',
+    'LBL_MODULE_TITLE' => 'Mga Aksyon sa WorkFlow',
+    'LBL_AOW_WORKFLOW_ID' => 'AOW_Trabhopaubos Id',
+    'LBL_ACTION' => 'Aksyon',
+    'LBL_PARAMETERS' => 'Mga parameter',
+    'LBL_SELECT_ACTION' => 'Pagpili ug Akawnt',
+    'LBL_CREATE_EMAIL_TEMPLATE' => 'Paghimo',
+    'LBL_RECORD_TYPE' => 'Rekord na sulat',
+    'LBL_SENDEMAIL' => 'Magpadala ug Email',
+    'LBL_CREATERECORD' => 'Mga Nabuhat na mga Rekord',
+    'LBL_MODIFYRECORD' => 'Gi-usab nga mga rekord',
+    'LBL_ADD_FIELD' => 'Dugangan ug Lugar',
+    'LBL_ADD_RELATIONSHIP' => 'Dugangan sa mga relasyon',
+    'LBL_EDIT_EMAIL_TEMPLATE' => 'Usba',
+    'LBL_EMAIL' => 'Email',
+    'LBL_EMAIL_TEMPLATE' => 'Email plaka',
+    'LBL_SETAPPROVAL' => 'I-set. Mapadayun',
+    'LBL_RELATE_WORKFLOW' => 'Relate. sa. Wrokpaubos Module',
+    'LBL_INDIVIDUAL_EMAILS' => 'Nag send og isa isa Emails',
+    'LBL_COMPUTEFIELD' => 'Kalkyulit ang mga field',
+    'LBL_COMPUTEFIELD_PARAMETERS' => 'Mga parameter',
+    'LBL_COMPUTEFIELD_FIELD_NAME' => 'Ngalan sa Lugar',
+    'LBL_COMPUTEFIELD_IDENTIFIER' => 'Unsaymga ngalan',
+    'LBL_COMPUTEFIELD_ADD_PARAMETER' => 'Dugangan ang sukod',
+    'LBL_COMPUTEFIELD_RELATION_PARAMETERS' => 'Relasyon sukdanan',
+    'LBL_COMPUTEFIELD_RELATION' => 'Relasyon',
+    'LBL_COMPUTEFIELD_ADD_RELATION_PARAMETER' => 'Dugangan og sukdanan',
+    'LBL_COMPUTEFIELD_FORMULA' => 'Formula',
+    'LBL_COMPUTEFIELD_FORMULAS' => 'Mga formulas',
+    'LBL_COMPUTEFIELD_ADD_FORMULA' => 'Dugangan na Formula',
+    'LBL_COMPUTEFIELD_VALUE_TYPE' => 'Balyo sulat',
+    'LBL_COMPUTEFIELD_RAW_VALUE' => 'Pinakadako nga halaga',
+    'LBL_COMPUTEFIELD_FORMATTED_VALUE' => 'Formattend balyo',
+    'LBL_COPY_EMAIL_ADDRESSES_WORKFLOW' => 'Copy emails from WorkFlow Module'
+);
