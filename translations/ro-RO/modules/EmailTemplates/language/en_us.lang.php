e5fdb4b5a5f8ed52ed2dea7ced2487d0ddcb31c8
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-16 21:12:41 +0000

    New translations en_us.lang.php (Romanian)

---
+++ translations/ro-RO/modules/EmailTemplates/language/en_us.lang.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ADD_DOCUMENT' => 'Adaugă un document',
+    'LBL_ADD_FILE' => 'Adăugă un fișier',
+    'LBL_ATTACHMENTS' => 'Atașamente',
+    'LBL_BODY' => 'Corp:',
+    'LBL_CLOSE' => 'Închide:',
+    'LBL_DESCRIPTION' => 'Descriere:',
+    'LBL_EDIT_ALT_TEXT' => 'Editați în simplu text',
+    'LBL_EMAIL_ATTACHMENT' => 'Atașare e-mail',
+    'LBL_HIDE_ALT_TEXT' => 'Ascunde text simplu',
+    'LBL_HTML_BODY' => 'Corp HTML',
+    'LBL_INSERT_VARIABLE' => 'Inserare variabilă:',
+    'LBL_INSERT_URL_REF' => 'Inserează referință URL',
+    'LBL_INSERT_TRACKER_URL' => 'Inserează urmăritor URL:',
+    'LBL_INSERT' => 'Inserează',
+    'LBL_LIST_DATE_MODIFIED' => 'Data modificării',
+    'LBL_LIST_DESCRIPTION' => 'Descriere',
+    'LBL_LIST_FORM_TITLE' => 'Lista de șabloane de e-mail',
+    'LBL_LIST_NAME' => 'Nume',
+    'LBL_MODULE_NAME' => 'Șabloane de e-mail',
+    'LBL_MODULE_TITLE' => 'Șabloane de email: Start',
+    'LBL_NAME' => 'Nume:',
+    'LBL_NEW_FORM_TITLE' => 'Creează șabloane de email',
+    'LBL_PUBLISH' => 'Publică:',
+    'LBL_RELATED_TO' => 'Legat De:',
+    'LBL_SEARCH_FORM_TITLE' => 'Căutare șabloane de e-mail',
+    'LBL_SHOW_ALT_TEXT' => 'Arată în mod text',
+    'LBL_SUBJECT' => 'Subiect:',
+    'LBL_SUITE_DOCUMENT' => 'Document',
+    'LBL_UPLOAD_FILE' => 'Încarcă fișier',
+    'LBL_TEXT_BODY' => 'Corp text',
+    'LBL_USERS' => 'Utilizatori',
+
+    'LNK_EMAIL_TEMPLATE_LIST' => 'Vezi șabloane de email',
+    'LNK_IMPORT_NOTES' => 'Importă note',
+    'LNK_NEW_EMAIL_TEMPLATE' => 'Creează șabloane de email',
+    'LNK_NEW_EMAIL' => 'Arhivează email',
+    'LNK_NEW_SEND_EMAIL' => 'Compuneți Email',
+    'LNK_SENT_EMAIL_LIST' => 'E-mailuri trimise',
+    'LNK_VIEW_CALENDAR' => 'Astazi',
+    // for Inbox
+    'LBL_NEW' => 'Nou',
+    'LNK_MY_DRAFTS' => 'Schițele mele',
+    'LNK_MY_INBOX' => 'E-mailul meu',
+    'LBL_TEXT_ONLY' => 'Doar text',
+    'LBL_SEND_AS_TEXT' => 'Trimite doar textul',
+    'LBL_ACCOUNT' => 'Cont',
+    'LBL_FROM_NAME' => 'De la',
+    'LBL_PLAIN_TEXT' => 'Text simplu',
+    'LBL_CREATED_BY' => 'Creat De',
+    'LBL_PUBLISHED' => 'Publicat',
+    'LNK_VIEW_MY_INBOX' => 'Vezi e-mailul meu',
+    'LBL_ASSIGNED_TO_ID' => 'Atribuit lui',
+    'LBL_EDIT_LAYOUT' => 'Editare aranjament' /*for 508 compliance fix*/,
+    'LBL_SELECT' => 'Selectaţi' /*for 508 compliance fix*/,
+    'LBL_ID_FF_CLEAR' => 'Curăță' /*for 508 compliance fix*/,
+    'LBL_TYPE' => 'Tip',
+    'LBL_WIDTH' => 'Lățime implicită',
+    'LNK_IMPORT_CAMPAIGNS' => 'Importați campanie',
+);
