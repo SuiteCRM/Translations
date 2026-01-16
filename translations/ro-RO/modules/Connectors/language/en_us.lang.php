bb271e86eb8cd1dcf94e4b8ffdcaeb7f70ecf9a4
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-16 20:59:44 +0000

    New translations en_us.lang.php (Romanian)

---
+++ translations/ro-RO/modules/Connectors/language/en_us.lang.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+
+$mod_strings = array(
+
+    'LBL_ADMINISTRATION_MAIN' => 'Setări conector',
+    'LBL_AVAILABLE' => 'Disponibil',
+    'LBL_BACK' => '< Înapoi',
+    'LBL_CONFIRM_CONTINUE_SAVE' => 'Câteva câmpuri necesare au fost lăsate goale. Continuăm să salvăm schimbările?',
+    'LBL_CONNECTOR_FIELDS' => 'Câmpurile conectorului',
+    'LBL_DATA' => 'Date',
+    'LBL_DEFAULT' => 'Implicit',
+    'LBL_DISABLED' => 'Dezactivat',
+    'LBL_ENABLED' => 'Accesibil',
+    'LBL_EXTERNAL' => 'Permite utilizatorilor să creeze înregistrări de cont extern la acest conector.',
+    'LBL_EXTERNAL_SET_PROPERTIES' => ' Pentru a utiliza acest conector, proprietățile ar trebui să fie, de asemenea, setate în pagina de setări a proprietăților de conector.',
+    'LBL_MERGE' => 'Suprascrie duplicate',
+    'LBL_MODIFY_DISPLAY_TITLE' => 'Activează conectori',
+    'LBL_MODIFY_DISPLAY_DESC' => 'Selectați care module sunt activate pentru fiecare conector.',
+    'LBL_MODULE_FIELDS' => 'Câmpurile modulului',
+    'LBL_MODIFY_MAPPING_TITLE' => 'Harta campuri modul',
+    'LBL_MODIFY_MAPPING_DESC' => 'Harta câmpurilor conectorului la câmpurile modulului pentru a determina ce date de conector pot fi vizualizate și fuzionate în înregistrările modulului.',
+    'LBL_MODIFY_PROPERTIES_TITLE' => 'Setează proprietățile conectorului',
+    'LBL_MODIFY_PROPERTIES_DESC' => 'Configurați proprietățile pentru fiecare conector, inclusiv cheile URL și API.',
+    'LBL_MODIFY_SEARCH_TITLE' => 'Gestionare căutare conector',
+    'LBL_MODIFY_SEARCH' => 'Caută',
+    'LBL_MODIFY_SEARCH_DESC' => 'Selectați câmpurile de conector pentru a utiliza pentru a căuta date pentru fiecare modul.',
+    'LBL_MODULE_NAME' => 'Conectori',
+    'LBL_NO_PROPERTIES' => 'Nu există proprietăți configurabile pentru acest conector.',
+    'LBL_SAVE' => 'Salvați',
+    'LBL_SUMMARY' => 'Sumar',
+    'LBL_STEP1' => 'Căutare și vizualizare date',
+    'LBL_STEP2' => 'Îmbinați înregistrările cu',
+    'LBL_TEST_SOURCE' => 'Testează conector',
+    'LBL_TEST_SOURCE_FAILED' => 'Test eșuat',
+    'LBL_TEST_SOURCE_SUCCESS' => 'Testat cu succes',
+    'LBL_TITLE' => 'Îmbinare date',
+
+    'ERROR_NO_ADDITIONAL_DETAIL' => 'Eroare: Nu au fost găsite detalii suplimentare pentru înregistrare.',
+    'ERROR_NO_SEARCHDEFS_DEFINED' => 'Niciun modul nu a fost activat pentru acest conector. Selectați un modul pentru acest conector din pagina Activați Conectori.',
+    'ERROR_NO_SEARCHDEFS_MAPPED' => 'Eroare: Nu sunt conectori activați care să aibă câmpuri de căutare definite.',
+    'ERROR_NO_SEARCHDEFS_MAPPING' => 'Eroare: Nu sunt câmpuri de căutare definite pentru modul si conector. Va rugam contactați administratorul de sistem.',
+    'ERROR_NO_DISPLAYABLE_MAPPED_FIELDS' => 'Eroare: Nu exista câmpuri de module care sa fie asociate pentru afișarea rezultatelor. Vă rugăm contactați administratorul de sistem.',
+    'LBL_INFO_INLINE' => 'Informații' /*for 508 compliance fix*/,
+    'LBL_CLOSE' => 'Închide' /*for 508 compliance fix*/,
+
+);
