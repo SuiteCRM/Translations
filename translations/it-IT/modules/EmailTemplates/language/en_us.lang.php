bb66adb60474f1caae961f82d909b09dfde125b8
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-16 21:12:58 +0000

    New translations en_us.lang.php (Italian)

---
+++ translations/it-IT/modules/EmailTemplates/language/en_us.lang.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ADD_DOCUMENT' => 'Aggiungi un Documento',
+    'LBL_ADD_FILE' => 'Aggiungi un File',
+    'LBL_ATTACHMENTS' => 'Allegati',
+    'LBL_BODY' => 'Corpo:',
+    'LBL_CLOSE' => 'Chiudi:',
+    'LBL_DESCRIPTION' => 'Descrizione:',
+    'LBL_EDIT_ALT_TEXT' => 'Modifica Testo Alternativo',
+    'LBL_EMAIL_ATTACHMENT' => 'Allegato Email',
+    'LBL_HIDE_ALT_TEXT' => 'Nascondi Formato Testo',
+    'LBL_HTML_BODY' => 'Corpo HTML',
+    'LBL_INSERT_VARIABLE' => 'Inserisci Variabile:',
+    'LBL_INSERT_URL_REF' => 'Inserisci riferimento ad URL',
+    'LBL_INSERT_TRACKER_URL' => 'Inserisci Tracker URL:',
+    'LBL_INSERT' => 'Inserisci',
+    'LBL_LIST_DATE_MODIFIED' => 'Ultima Modifica',
+    'LBL_LIST_DESCRIPTION' => 'Descrizione',
+    'LBL_LIST_FORM_TITLE' => 'Elenco Modelli Email',
+    'LBL_LIST_NAME' => 'Nome',
+    'LBL_MODULE_NAME' => 'Modelli Email',
+    'LBL_MODULE_TITLE' => 'Modelli Email: Home',
+    'LBL_NAME' => 'Nome:',
+    'LBL_NEW_FORM_TITLE' => 'Nuovo Modello Email',
+    'LBL_PUBLISH' => 'Pubblicazione:',
+    'LBL_RELATED_TO' => 'Riferito A:',
+    'LBL_SEARCH_FORM_TITLE' => 'Cerca Modelli Email',
+    'LBL_SHOW_ALT_TEXT' => 'Mostra Testo Alternativo',
+    'LBL_SUBJECT' => 'Oggetto:',
+    'LBL_SUITE_DOCUMENT' => 'Documento',
+    'LBL_UPLOAD_FILE' => 'Carica file',
+    'LBL_TEXT_BODY' => 'Corpo Testo',
+    'LBL_USERS' => 'Utenti',
+
+    'LNK_EMAIL_TEMPLATE_LIST' => 'Visualizza Modelli Email',
+    'LNK_IMPORT_NOTES' => 'Importa Note',
+    'LNK_NEW_EMAIL_TEMPLATE' => 'Nuovo Modello Email',
+    'LNK_NEW_EMAIL' => 'Archivia Email',
+    'LNK_NEW_SEND_EMAIL' => 'Scrivi Email',
+    'LNK_SENT_EMAIL_LIST' => 'Email Inviate',
+    'LNK_VIEW_CALENDAR' => 'Oggi',
+    // for Inbox
+    'LBL_NEW' => 'Nuovo',
+    'LNK_MY_DRAFTS' => 'Le mie Bozze',
+    'LNK_MY_INBOX' => 'La mia Email',
+    'LBL_TEXT_ONLY' => 'Solo Testo',
+    'LBL_SEND_AS_TEXT' => 'Invia Solo Testo',
+    'LBL_ACCOUNT' => 'Azienda',
+    'LBL_FROM_NAME' => 'Nome mittente',
+    'LBL_PLAIN_TEXT' => 'Testo Semplice',
+    'LBL_CREATED_BY' => 'Creato da',
+    'LBL_PUBLISHED' => 'In pubblicazione',
+    'LNK_VIEW_MY_INBOX' => 'Visualizza le mie Email',
+    'LBL_ASSIGNED_TO_ID' => 'Assegnato a',
+    'LBL_EDIT_LAYOUT' => 'Modifica Maschera' /*for 508 compliance fix*/,
+    'LBL_SELECT' => 'Selezionare' /*for 508 compliance fix*/,
+    'LBL_ID_FF_CLEAR' => 'Svuota' /*for 508 compliance fix*/,
+    'LBL_TYPE' => 'Tipo',
+    'LBL_WIDTH' => 'Larghezza predefinita',
+    'LNK_IMPORT_CAMPAIGNS' => 'Importa Campagna',
+);
