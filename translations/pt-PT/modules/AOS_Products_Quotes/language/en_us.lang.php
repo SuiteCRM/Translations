a16a3551b057d07359bdb42a26d9937453dd4611
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-16 20:24:12 +0000

    New translations en_us.lang.php (Portuguese)

---
+++ translations/pt-PT/modules/AOS_Products_Quotes/language/en_us.lang.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID' => 'ID do utilizador atribuído',
+    'LBL_ASSIGNED_TO_NAME' => 'Atribuído a',
+    'LBL_ID' => 'ID',
+    'LBL_DATE_ENTERED' => 'Data de criação',
+    'LBL_DATE_MODIFIED' => 'Data de alteração',
+    'LBL_MODIFIED' => 'Alterado por',
+    'LBL_MODIFIED_NAME' => 'Alterado por (nome)',
+    'LBL_CREATED' => 'Criado por',
+    'LBL_DESCRIPTION' => 'Nota',
+    'LBL_DELETED' => 'Eliminado',
+    'LBL_NAME' => 'Nome',
+    'LBL_CREATED_USER' => 'Criado pelo utilizador',
+    'LBL_MODIFIED_USER' => 'Alterado pelo utilizador',
+    'LBL_LIST_FORM_TITLE' => 'Lista de propostas de produtos',
+    'LBL_MODULE_NAME' => 'Itens de linha',
+    'LBL_MODULE_TITLE' => 'Propostas de produtos: entrada',
+    'LBL_HOMEPAGE_TITLE' => 'Minhas propostas de produtos',
+    'LNK_NEW_RECORD' => 'Criar proposta de produtos',
+    'LNK_LIST' => 'Propostas para produtos',
+    'LBL_SEARCH_FORM_TITLE' => 'Pesquisar propostas de produtos',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Ver histórico',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Atividades',
+    'LBL_NEW_FORM_TITLE' => 'Nova proposta de produto',
+    'LBL_PRODUCT_NAME' => 'Nome',
+    'LBL_PRODUCT_QTY' => 'Quantidade',
+    'LBL_PRODUCT_COST_PRICE' => 'Preço de custo',
+    'LBL_PRODUCT_LIST_PRICE' => 'Preço listado',
+    'LBL_PRODUCT_UNIT_PRICE' => 'Preço unitário',
+    'LBL_PRODUCT_DISCOUNT' => 'Desconto',
+    'LBL_PRODUCT_DISCOUNT_AMOUNT' => 'Desconto total',
+    'LBL_PART_NUMBER' => 'Número da peça',
+    'LBL_PRODUCT_DESCRIPTION' => 'Descrição',
+    'LBL_DISCOUNT' => 'Tipo de desconto',
+    'LBL_VAT_AMT' => 'Total de IVA',
+    'LBL_VAT' => 'IVA',
+    'LBL_PRODUCT_TOTAL_PRICE' => 'Preço total',
+    'LBL_PRODUCT_NOTE' => 'Nota',
+    'Quote' => '',
+    'LBL_FLEX_RELATE' => 'Relacionado com',
+    'LBL_PRODUCT' => 'Produto',
+
+    'LBL_SERVICE_MODULE_NAME' => 'Serviços',
+    'LBL_LIST_NUM' => 'Número',
+    'LBL_PARENT_ID' => 'ID hospedeiro:',
+    'LBL_GROUP_NAME' => 'Grupo',
+    'LBL_PRODUCT_COST_PRICE_USDOLLAR' => 'Preço de custo (moeda predefinida)',
+    'LBL_PRODUCT_LIST_PRICE_USDOLLAR' => 'Preço listado (moeda predefinida)',
+    'LBL_PRODUCT_UNIT_PRICE_USDOLLAR' => 'Preço unitário (moeda predefinida)',
+    'LBL_PRODUCT_TOTAL_PRICE_USDOLLAR' => 'Preço final (moeda predefinida)',
+    'LBL_PRODUCT_DISCOUNT_USDOLLAR' => 'Desconto (moeda predefinida)',
+    'LBL_PRODUCT_DISCOUNT_AMOUNT_USDOLLAR' => 'Desconto total (moeda predefinida)',
+    'LBL_VAT_AMT_USDOLLAR' => 'Total de IVA (moeda predefinida)',
+    'LBL_PRODUCTS_SERVICES' => 'Produto / Serviço',
+    'LBL_PRODUCT_ID' => 'ID do produto',
+
+    'LBL_AOS_CONTRACTS' => 'Contratos',
+    'LBL_AOS_INVOICES' => 'Faturas',
+    'LBL_AOS_PRODUCTS' => 'Produtos',
+    'LBL_AOS_QUOTES' => 'Propostas',
+);
