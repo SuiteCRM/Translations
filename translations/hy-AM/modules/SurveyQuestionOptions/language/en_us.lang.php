257e06343ccd2fb8b3551a29a6ef9ae38b0e8378
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-16 21:38:02 +0000

    New translations en_us.lang.php (Armenian)

---
+++ translations/hy-AM/modules/SurveyQuestionOptions/language/en_us.lang.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID'                                                                   => 'Պատասխանատու- ID',
+    'LBL_ASSIGNED_TO_NAME'                                                                 => 'Կատարող',
+    'LBL_SECURITYGROUPS'                                                                   => 'Օգտագործողների խմբեր',
+    'LBL_SECURITYGROUPS_SUBPANEL_TITLE'                                                    => 'Օգտագործողների խմբեր',
+    'LBL_ID'                                                                               => 'ID',
+    'LBL_DATE_ENTERED'                                                                     => 'Ստեղծման ամսաթիվ',
+    'LBL_DATE_MODIFIED'                                                                    => 'Փոփոխման ամսաթիվ',
+    'LBL_MODIFIED'                                                                         => 'Փոփոխված է',
+    'LBL_MODIFIED_ID'                                                                      => 'Փոփոխված է ID-ի կողմից',
+    'LBL_MODIFIED_NAME'                                                                    => 'Փոփոխված է ըստ անվանման',
+    'LBL_CREATED'                                                                          => 'Ստեղծված է',
+    'LBL_CREATED_ID'                                                                       => 'Ստեղծված է ID',
+    'LBL_DESCRIPTION'                                                                      => 'Նկարագրություն',
+    'LBL_SORT_ORDER'                                                                       => 'Պատվեր',
+    'LBL_DELETED'                                                                          => 'Ջնջված է',
+    'LBL_NAME'                                                                             => 'Անվանում',
+    'LBL_CREATED_USER'                                                                     => 'Փոփոխված է օգտագործողի կողմից ',
+    'LBL_MODIFIED_USER'                                                                    => 'Փոփոխված է օգտագործողի կողմից',
+    'LBL_LIST_NAME'                                                                        => 'Անվանում',
+    'LBL_EDIT_BUTTON'                                                                      => 'Խմբագրել',
+    'LBL_REMOVE'                                                                           => 'Հեռացնել',
+    'LBL_LIST_FORM_TITLE'                                                                  => 'Survey Question Options List',
+    'LBL_MODULE_NAME'                                                                      => 'Survey Question Options',
+    'LBL_MODULE_TITLE'                                                                     => 'Survey Question Options',
+    'LBL_HOMEPAGE_TITLE'                                                                   => 'My Survey Question Options',
+    'LNK_NEW_RECORD'                                                                       => 'Create Survey Question Options',
+    'LNK_LIST'                                                                             => 'View Survey Question Options',
+    'LNK_IMPORT_SURVEYQUESTIONOPTIONS'                                                     => 'Import Survey Question Options',
+    'LBL_SEARCH_FORM_TITLE'                                                                => 'Search Survey Question Options',
+    'LBL_HISTORY_SUBPANEL_TITLE'                                                           => 'Դիտել պատմությունը',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE'                                                        => 'Գործունեություն',
+    'LBL_SURVEYQUESTIONOPTIONS_SUBPANEL_TITLE'                                             => 'Survey Question Options',
+    'LBL_NEW_FORM_TITLE'                                                                   => 'New Survey Question Options',
+    'LBL_SURVEYQUESTIONOPTIONS_SURVEYQUESTIONRESPONSES_FROM_SURVEYQUESTIONRESPONSES_TITLE' => 'Survey Question Responses',
+);
