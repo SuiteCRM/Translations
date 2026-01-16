1a9848a9024d1ce1e9c6918ddabcac29f8b3a93f
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-16 21:07:20 +0000

    New translations en_us.lang.php (Slovak)

---
+++ translations/sk-SK/modules/DynamicFields/language/en_us.lang.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LNK_NEW_CALL' => 'Naplánovať hovor',
+    'LNK_NEW_MEETING' => 'Harmonogram rokovaní',
+    'LNK_NEW_TASK' => 'Vytvoriť úlohu',
+    'LNK_NEW_NOTE' => 'Vytvoriť poznámku alebo prílohu',
+    'LNK_NEW_EMAIL' => 'Archivovať správu',
+    'LNK_CALL_LIST' => 'Hovory',
+    'LNK_MEETING_LIST' => 'Schôdzky',
+    'LNK_TASK_LIST' => 'Úlohy',
+    'LNK_NOTE_LIST' => 'Poznámky',
+    'LBL_ADD_FIELD' => 'Pridať pole:',
+    'LBL_SEARCH_FORM_TITLE' => 'Modul vyhľadávania',
+    'COLUMN_TITLE_NAME' => 'Názov poľa',
+    'COLUMN_TITLE_DISPLAY_LABEL' => 'Zobrazenie etikiet',
+    'COLUMN_TITLE_LABEL_VALUE' => 'Hodnota etikety',
+    'COLUMN_TITLE_LABEL' => 'Systémová etiketa',
+    'COLUMN_TITLE_DATA_TYPE' => 'Typ údajov',
+    'COLUMN_TITLE_MAX_SIZE' => 'Max. veľkosť',
+    'COLUMN_TITLE_HELP_TEXT' => 'Nápoveda',
+    'COLUMN_TITLE_COMMENT_TEXT' => 'Komentár',
+    'COLUMN_TITLE_REQUIRED_OPTION' => 'Vyžadované pole',
+    'COLUMN_TITLE_DEFAULT_VALUE' => 'Prednastavená hodnota',
+    'COLUMN_TITLE_FRAME_HEIGHT' => 'IFrame výška',
+    'COLUMN_TITLE_HTML_CONTENT' => 'HTML',
+    'COLUMN_TITLE_URL' => 'Prednastavené URL',
+    'COLUMN_TITLE_AUDIT' => 'Audit',
+    'COLUMN_TITLE_MIN_VALUE' => 'Minimálna hodnota',
+    'COLUMN_TITLE_MAX_VALUE' => 'Max. hodnota',
+    'COLUMN_TITLE_LABEL_ROWS' => 'Riadky',
+    'COLUMN_TITLE_LABEL_COLS' => 'Stĺpce',
+    'COLUMN_TITLE_DISPLAYED_ITEM_COUNT' => '# zobrazených položiek',
+    'COLUMN_TITLE_AUTOINC_NEXT' => 'Automatický prírastok ďalšej hodnoty',
+    'COLUMN_DISABLE_NUMBER_FORMAT' => 'Nedovolený formát',
+    'COLUMN_TITLE_ENABLE_RANGE_SEARCH' => 'Povoliť rozsah vyhľadávania',
+    'LBL_DROP_DOWN_LIST' => 'Rozbaľovací zoznam',
+    'LBL_RADIO_FIELDS' => 'Radio pole',
+    'LBL_MULTI_SELECT_LIST' => 'Viacnásobný zoznam',
+    'COLUMN_TITLE_PRECISION' => 'Presnosť',
+    'LBL_MODULE' => 'Modul',
+    'COLUMN_TITLE_MASS_UPDATE' => 'Hromadná aktualizácia',
+    'COLUMN_TITLE_IMPORTABLE' => 'Importovateľný',
+    'COLUMN_TITLE_DUPLICATE_MERGE' => 'Duplicita zlučovania',
+    'LBL_LABEL' => 'Štítok',
+    'LBL_DATA_TYPE' => 'Typ údajov',
+    'LBL_DEFAULT_VALUE' => 'Prednastavená hodnota',
+    'ERR_RESERVED_FIELD_NAME' => "Rezervované kľúčové slovo",
+    'ERR_SELECT_FIELD_TYPE' => 'Prosím, vyberte typ poľa',
+    'ERR_FIELD_NAME_ALREADY_EXISTS' => 'Názov poľa už existuje',
+    'LBL_BTN_ADD' => 'Pridať',
+    'LBL_BTN_EDIT' => 'Upraviť',
+    'LBL_GENERATE_URL' => 'Generovať URL',
+    'LBL_CALCULATED' => 'Kalkulovaná hodnota',
+    'LBL_LINK_TARGET' => 'Otvoriť link v',
+    'LBL_IMAGE_WIDTH' => 'Šírka',
+    'LBL_IMAGE_HEIGHT' => 'Výška',
+    'LBL_IMAGE_BORDER' => 'Okraj',
+    'LBL_HELP' => 'Pomoc' /*for 508 compliance fix*/,
+    'COLUMN_TITLE_INLINE_EDIT_TEXT' => 'Úprava v riadku',
+    'COLUMN_TITLE_PARENT_ENUM' => 'Nadradené rozbalovacie menu',
+    'LBL_INSERT_FIELD' => 'Insert Field',
+);
