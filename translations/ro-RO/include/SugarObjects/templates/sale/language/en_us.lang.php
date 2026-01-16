d446edc26696290e2aef2d2e8067a5a79733fd58
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-16 19:52:42 +0000

    New translations en_us.lang.php (Romanian)

---
+++ translations/ro-RO/include/SugarObjects/templates/sale/language/en_us.lang.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_MODULE_NAME' => 'Vânzare',
+    'LBL_MODULE_TITLE' => 'Vânzare: Acasă',
+    'LBL_SEARCH_FORM_TITLE' => 'Căutare vânzări',
+    'LBL_LIST_FORM_TITLE' => 'Listă vânzări',
+    'LBL_NAME' => 'Nume',
+    'LBL_LIST_SALE_NAME' => 'Nume',
+    'LBL_LIST_ACCOUNT_NAME' => 'Nume Cont',
+    'LBL_LIST_AMOUNT' => 'Suma, valoare',
+    'LBL_LIST_DATE_CLOSED' => 'Închide',
+    'LBL_LIST_SALE_STAGE' => 'Stadiul',
+    'LBL_ACCOUNT_ID' => 'ID Cont',
+    //DON'T CONVERT THESE THEY ARE MAPPINGS
+    'db_name' => 'LBL_NAME',
+    //END DON'T CONVERT
+    'LBL_ACCOUNT_NAME' => 'Nume Cont:',
+    'LBL_AMOUNT' => 'Suma:',
+    'LBL_AMOUNT_USDOLLAR' => 'Suma USD:',
+    'LBL_CURRENCY' => 'Monedă:',
+    'LBL_DATE_CLOSED' => 'Dată Așteaptă Închidere:',
+    'LBL_TYPE' => 'Tip:',
+    'LBL_CAMPAIGN' => 'Campanie:',
+    'LBL_LEADS_SUBPANEL_TITLE' => 'Piste',
+    'LBL_PROJECTS_SUBPANEL_TITLE' => 'Proiecte',
+    'LBL_NEXT_STEP' => 'Următorul pas:',
+    'LBL_LEAD_SOURCE' => 'Sursă conversie:',
+    'LBL_SALES_STAGE' => 'Stadiu:',
+    'LBL_PROBABILITY' => 'Probabilitate (%):',
+    'LBL_DESCRIPTION' => 'Descriere:',
+    'LBL_DUPLICATE' => 'Posibile vânzări duplicate',
+    'MSG_DUPLICATE' => 'Înregistrarea de vânzări pe care urmează să o creați ar putea fi un duplicat al unei alte înregistrări de vânzare existente. Înregistrările de vânzări care conțin nume similare sunt listate mai jos.<br>Apasă pe Salvează pentru a continua crearea acestei noi vânzări, sau apasă Anulează pentru a reveni la modul fără a crea vânzarea.',
+    'LBL_NEW_FORM_TITLE' => 'Creează vânzare',
+    'ERR_DELETE_RECORD' => 'Un număr de înregistrare trebuie specificat pentru a șterge această vânzare.',
+    'LBL_DEFAULT_SUBPANEL_TITLE' => 'Vânzare',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Activităţi',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Istoric',
+
+    'LBL_CONTACTS_SUBPANEL_TITLE' => 'Contacte',
+    'LBL_ASSIGNED_TO_NAME' => 'Utilizator:',
+    'LBL_LIST_ASSIGNED_TO_NAME' => 'Atribuit utilizatorului',
+    'LBL_ASSIGNED_TO_ID' => 'Atribuit ID-lui',
+    'LBL_MODIFIED_NAME' => 'Modificat de Nume de Utilizator',
+    'LBL_SALE_INFORMATION' => 'Informații vânzare',
+    'LBL_CURRENCY_NAME' => 'Nume Monedă',
+    'LBL_CURRENCY_SYMBOL' => 'Simbol Monedă',
+    'LBL_EDIT_BUTTON' => 'Editați',
+    'LBL_REMOVE' => 'Înlăturați',
+
+);
