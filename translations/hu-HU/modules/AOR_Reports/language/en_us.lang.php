19dc224810d41e0a061cac0bacfb2e14d6e64b74
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-16 20:14:25 +0000

    New translations en_us.lang.php (Hungarian)

---
+++ translations/hu-HU/modules/AOR_Reports/language/en_us.lang.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID' => 'Felelős felhasználó ID',
+    'LBL_ASSIGNED_TO_NAME' => 'Felelős',
+    'LBL_ID' => 'ID',
+    'LBL_DATE_ENTERED' => 'Létrehozás dátuma',
+    'LBL_DATE_MODIFIED' => 'Módosítás dátuma',
+    'LBL_MODIFIED' => 'Módosító',
+    'LBL_MODIFIED_NAME' => 'Módosító név',
+    'LBL_CREATED' => 'Létrehozta',
+    'LBL_DESCRIPTION' => 'Leírás',
+    'LBL_DELETED' => 'Törölve',
+    'LBL_NAME' => 'Név',
+    'LBL_CREATED_USER' => 'Felhasználó által létrehozva',
+    'LBL_MODIFIED_USER' => 'Felhasználó által módosítva',
+    'LBL_LIST_NAME' => 'Név',
+    'LBL_EDIT_BUTTON' => 'Szerkesztés',
+    'LBL_REMOVE' => 'Eltávolítás',
+    'LBL_LIST_FORM_TITLE' => 'Jelentések listája',
+    'LBL_MODULE_NAME' => 'Jelentések',
+    'LBL_MODULE_TITLE' => 'Jelentések',
+    'LBL_HOMEPAGE_TITLE' => 'Jelentéseim',
+    'LNK_NEW_RECORD' => 'Jelentés létrehozása',
+    'LNK_LIST' => 'Jelentések',
+    'LBL_SEARCH_FORM_TITLE' => 'Jelentés keresése',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Előzmény Megtekintése',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Tevékenységek',
+    'LBL_NEW_FORM_TITLE' => 'Új jelentések',
+    'LBL_REPORT_MODULE' => 'Jelentés modul',
+    'LBL_GRAPHS_PER_ROW' => 'Diagramok soronként',
+    'LBL_FIELD_LINES' => 'Mezők megjelenítése',
+    'LBL_ADD_FIELD' => 'Mező hozzáadása',
+    'LBL_CONDITION_LINES' => 'Feltételek',
+    'LBL_ADD_CONDITION' => 'Feltétel hozzáadása',
+    'LBL_EXPORT' => 'Exportálás',
+    'LBL_DOWNLOAD_PDF' => 'PDF letöltése',
+    'LBL_ADD_TO_PROSPECT_LIST' => 'Hozzáadás a Cél-listához',
+    'LBL_AOR_MODULETREE_SUBPANEL_TITLE' => 'Modul fa',
+    'LBL_AOR_FIELDS_SUBPANEL_TITLE' => 'Mezők',
+    'LBL_AOR_CONDITIONS_SUBPANEL_TITLE' => 'Feltételek',
+    'LBL_TOTAL' => 'Összesen',
+    'LBL_AOR_CHARTS_SUBPANEL_TITLE' => 'Diagramok',
+    'LBL_ADD_CHART' => 'Diagram hozzáadása',
+    'LBL_ADD_PARENTHESIS' => 'Zárójelek elhelyezése',
+    'LBL_CHART_TITLE' => 'Megnevezés',
+    'LBL_CHART_TYPE' => 'Típus',
+    'LBL_CHART_X_FIELD' => 'X tengely',
+    'LBL_CHART_Y_FIELD' => 'X tengely',
+    'LBL_AOR_REPORTS_DASHLET' => 'Jelentések',
+    'LBL_DASHLET_TITLE' => 'Megnevezés',
+    'LBL_DASHLET_REPORT' => 'Jelentés',
+    'LBL_DASHLET_CHOOSE_REPORT' => 'Kérjük válassza ki a kívánt jelentést',
+    'LBL_DASHLET_SAVE' => 'Mentés',
+    'LBL_DASHLET_CHARTS' => 'Diagramok',
+    'LBL_DASHLET_ONLY_CHARTS' => 'Csak diagramokat mutasson',
+    'LBL_UPDATE_PARAMETERS' => 'Frissítés',
+    'LBL_PARAMETERS' => 'Paraméterek',
+    'LBL_TOOLTIP_DRAG_DROP_ELEMS' => 'A mező vagy feltételek területre húzzon elemeket',
+    'LBL_MAIN_GROUPS' => 'Fő csoport:',
+    'LBL_CHAR_UNNAMED_DEFAULT_TITLE' => 'Névtelen diagram',
+    'LBL_REPORT' => 'Jelentés',
+);
