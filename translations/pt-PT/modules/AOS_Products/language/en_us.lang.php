d8be57a6bebb260ded955478c8802e82e9119e90
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-16 20:23:03 +0000

    New translations en_us.lang.php (Portuguese)

---
+++ translations/pt-PT/modules/AOS_Products/language/en_us.lang.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID' => 'ID do utilizador atribuído',
+    'LBL_ASSIGNED_TO_NAME' => 'Atribuído a',
+    'LBL_ID' => 'ID',
+    'LBL_DATE_ENTERED' => 'Data de criação',
+    'LBL_DATE_MODIFIED' => 'Data de alteração',
+    'LBL_MODIFIED' => 'Alterado por',
+    'LBL_MODIFIED_NAME' => 'Alterado por (nome)',
+    'LBL_CREATED' => 'Criado por',
+    'LBL_DESCRIPTION' => 'Descrição',
+    'LBL_DELETED' => 'Eliminado',
+    'LBL_NAME' => 'Nome do produto',
+    'LBL_CREATED_USER' => 'Criado pelo utilizador',
+    'LBL_MODIFIED_USER' => 'Alterado pelo utilizador',
+    'LBL_LIST_FORM_TITLE' => 'Lista de produtos',
+    'LBL_MODULE_NAME' => 'Produtos',
+    'LBL_MODULE_TITLE' => 'Produtos: entrada',
+    'LBL_HOMEPAGE_TITLE' => 'Meus produtos',
+    'LNK_NEW_RECORD' => 'Criar produto',
+    'LNK_LIST' => 'Ver produtos',
+    'LBL_SEARCH_FORM_TITLE' => 'Procurar produtos',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Ver histórico',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Atividades',
+    'LBL_NEW_FORM_TITLE' => 'Novo produto',
+    'LBL_MAINCODE' => 'Código do produto',
+    'VALUE' => 'Nome do produto',
+    'LBL_PART_NUMBER' => 'Número da peça',
+    'LBL_CATEGORY' => 'Categoria',
+    'LBL_TYPE' => 'Tipo de produto',
+    'LBL_COST' => 'Custo',
+    'LBL_PRICE' => 'Preço',
+    'LBL_URL' => 'URL:',
+    'LBL_CONTACT' => 'Contacto',
+    'LBL_PRODUCT_IMAGE' => 'Imagem do produto',
+    'LBL_IMAGE_UPLOAD_FAIL' => 'Erro: O ficheiro carregador excede o tamanho máximo: max filesize:',
+    'LBL_AOS_PRODUCT_CATEGORYS_NAME' => 'Categoria do produto',
+    'LBL_AOS_PRODUCT_CATEGORY' => 'ID da categoria do produto',
+    'LBL_AOS_PRODUCT_CATEGORIES' => 'Produtos — Categorias',
+    'LBL_COST_USDOLLAR' => 'Custo (moeda predefinida)',
+    'LBL_PRICE_USDOLLAR' => 'Preço (moeda predefinida)',
+    'LBL_FILE_URL' => 'URL do Ficheiro',
+    'LBL_CUSTOMERS_PURCHASED_PRODUCTS_SUBPANEL_TITLE' => 'Compras',
+    'LBL_PRODUCTS_PURCHASES' => 'Compras',
+    'LBL_AOS_QUOTE_NAME' => 'Proposta',
+    'LBL_ACCOUNT_NAME' => 'Conta',
+);
