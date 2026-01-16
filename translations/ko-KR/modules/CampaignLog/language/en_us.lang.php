853055b387a46de7e89ec9958baba37060b97638
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-16 20:42:31 +0000

    New translations en_us.lang.php (Korean)

---
+++ translations/ko-KR/modules/CampaignLog/language/en_us.lang.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_LIST_ID' => '잠재고객 리스트 ID',
+    'LBL_ID' => 'ID',
+    'LBL_TARGET_TRACKER_KEY' => '타겟 트래커 Key',
+    'LBL_TARGET_ID' => '타겟 ID',
+    'LBL_TARGET_TYPE' => '대상 유형',
+    'LBL_ACTIVITY_TYPE' => '활동 유형',
+    'LBL_ACTIVITY_DATE' => '활동일',
+    'LBL_RELATED_ID' => '관련된 Id',
+    'LBL_RELATED_TYPE' => '관련 유형',
+    'LBL_DELETED' => '삭제됨',
+    'LBL_MODULE_NAME' => '캠페인 로그',
+    'LBL_LIST_RECIPIENT_EMAIL' => 'Recipient Email',
+    'LBL_LIST_RECIPIENT_NAME' => 'Recipient Name',
+    'LBL_ARCHIVED' => '저장',
+    'LBL_HITS' => 'Hits',
+
+    'LBL_CAMPAIGN_NAME' => '이름:',
+    'LBL_CAMPAIGN' => '캠페인:',
+    'LBL_NAME' => '이름: ',
+    'LBL_INVITEE' => '연락처',
+    'LBL_LIST_CAMPAIGN_NAME' => '캠페인',
+    'LBL_LIST_STATUS' => '상태',
+    'LBL_LIST_TYPE' => '종류',
+    'LBL_LIST_END_DATE' => '종료일',
+    'LBL_DATE_ENTERED' => '입력일',
+    'LBL_DATE_MODIFIED' => '수정일',
+    'LBL_MODIFIED' => 'Modified By: ',
+    'LBL_CREATED' => '작성자: ',
+    'LBL_TEAM' => '팀: ',
+    'LBL_ASSIGNED_TO' => 'Assigned To: ',
+    'LBL_CAMPAIGN_START_DATE' => '시작일: ',
+    'LBL_CAMPAIGN_END_DATE' => '종료일: ',
+    'LBL_CAMPAIGN_STATUS' => '상태: ',
+    'LBL_CAMPAIGN_BUDGET' => '예산: ',
+    'LBL_CAMPAIGN_EXPECTED_COST' => '예상경비: ',
+    'LBL_CAMPAIGN_ACTUAL_COST' => '실질경비: ',
+    'LBL_CAMPAIGN_EXPECTED_REVENUE' => '예상매출: ',
+    'LBL_CAMPAIGN_TYPE' => '종류: ',
+    'LBL_CAMPAIGN_OBJECTIVE' => '목적: ',
+    'LBL_CAMPAIGN_CONTENT' => '상세: ',
+    'LBL_CREATED_LEAD' => 'Created Lead',
+    'LBL_CREATED_CONTACT' => '기존 거래처 담당자',
+    'LBL_CREATED_OPPORTUNITY' => '생성된 영업기회',
+    'LBL_TARGETED_USER' => 'Targeted User',
+    'LBL_SENT_EMAIL' => '보낸 이메일',
+    'LBL_LIST_FORM_TITLE' => '목표 캠페인',
+    'LBL_LIST_ACTIVITY_DATE' => '활동일',
+    'LBL_RELATED' => '관련됨',
+    'LBL_MORE_INFO' => '추가정보',
+
+    'LBL_CAMPAIGNS' => '캠페인',
+    'LBL_LIST_MARKETING_NAME' => '마케팅 Id',
+
+    'LBL_MARKETING_ID' => '마케팅 Id',
+    'LBL_RELATED_NAME' => 'Related Name',
+);
