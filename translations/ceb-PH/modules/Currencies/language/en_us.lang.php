eec5f1f0cbfe1675526998dbda72141268d8253b
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-16 21:03:18 +0000

    New translations en_us.lang.php (Cebuano)

---
+++ translations/ceb-PH/modules/Currencies/language/en_us.lang.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_MODULE_NAME' => 'Kwarta',
+    'LBL_LIST_FORM_TITLE' => 'Kwarta',
+    'LBL_CURRENCY' => 'Kasalukuyan',
+    'LBL_ADD' => 'Dungagi',
+    'LBL_MERGE' => 'Isagol',
+    'LBL_MERGE_TXT' => 'Palihug pagpili sa mga kuwarta na imong ganahan mo map sa mga napili na kuwarta. Kani mopapas sa tanan nga kuwarta nga adunay checkmark og mag gahin og balik sa bisag unsa na balyo kuyog ani ngadto sa napilian nga kuwarta.',
+    'LBL_US_DOLLAR' => 'U.S, na Dolyar',
+    'LBL_DELETE' => 'Tangtangon',
+    'LBL_LIST_SYMBOL' => 'Simbolo sa kwarta',
+    'LBL_LIST_NAME' => 'Pangan sa kwarta',
+    'LBL_LIST_ISO4217' => 'ISO 4217 na Code',
+    'LBL_LIST_ISO4217_HELP' => 'Pag-entra og tulo ka letra nga ISO 4217 na code nga magsaysay sa pangalan sa kuwarta og simbolo sa kuwarta.',
+    'LBL_UPDATE' => 'Update',
+    'LBL_LIST_RATE' => 'Konbirsiyun Rate',
+    'LBL_LIST_RATE_HELP' => 'Ang Konbirsiyun Rate nga 0.5 para sa Euro nagpasabot nga ang 10 USD = 5 Euro.',
+    'LBL_LIST_STATUS' => 'Stado',
+    'LNK_NEW_CONTACT' => 'Bag-ong Kontak',
+    'LNK_NEW_ACCOUNT' => 'Bag-ong Account',
+    'LNK_NEW_OPPORTUNITY' => 'Bag-o nga Oportunidad',
+    'LNK_NEW_CASE' => 'Bag-ong Kaso',
+    'LNK_NEW_NOTE' => 'Paghimo og Note o Sumpay',
+    'LNK_NEW_CALL' => 'Bag-o na Tawag',
+    'LNK_NEW_EMAIL' => 'Bag-o na Email',
+    'LNK_NEW_MEETING' => 'Bag-o na Pulong-pulong',
+    'LNK_NEW_TASK' => 'Paghimo og buhaton',
+    'NTC_DELETE_CONFIRMATION' => 'Sigurado ba ka na papason ang kini nga rekord? Bisag unsa na rekord na gagamit ani nga kuwarta kay ma kombertir ngadto sa default nga sistema sa kuwarta kung sila i-akses. Mas maayo kung i-set ang estado nga dili aktibo,.',
+    'LBL_BELOW_MIN' => 'Ang rate sa konbirsiyun dapat sobra sa 0',
+    'currency_status_dom' =>
+        array(
+            'Active' => 'Aktib',
+            'Inactive' => 'Dili aktib',
+        ),
+    'LBL_CREATED_BY' => 'Gihimo ni',
+    'LBL_EDIT_LAYOUT' => 'I-edit ang Plano' /*for 508 compliance fix*/,
+);
