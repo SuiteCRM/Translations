7011b6c6b87b48f2aab3f8e0e8a3bb55736b7b85
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-16 21:13:16 +0000

    New translations en_us.lang.php (Chinese Traditional)

---
+++ translations/zh-TW/modules/EmailTemplates/language/en_us.lang.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ADD_DOCUMENT' => '增加文件',
+    'LBL_ADD_FILE' => '增加一個檔案',
+    'LBL_ATTACHMENTS' => '附件',
+    'LBL_BODY' => '正文: ',
+    'LBL_CLOSE' => '關閉:',
+    'LBL_DESCRIPTION' => '描述:',
+    'LBL_EDIT_ALT_TEXT' => '編輯純文字:',
+    'LBL_EMAIL_ATTACHMENT' => '電子郵件附件',
+    'LBL_HIDE_ALT_TEXT' => '隱藏提示文字',
+    'LBL_HTML_BODY' => 'HTML正文',
+    'LBL_INSERT_VARIABLE' => '插入變數:',
+    'LBL_INSERT_URL_REF' => '插入網址引用',
+    'LBL_INSERT_TRACKER_URL' => '插入追蹤網址:',
+    'LBL_INSERT' => '插入',
+    'LBL_LIST_DATE_MODIFIED' => '最新修改',
+    'LBL_LIST_DESCRIPTION' => '描述',
+    'LBL_LIST_FORM_TITLE' => '電子郵件範本清單',
+    'LBL_LIST_NAME' => '名稱',
+    'LBL_MODULE_NAME' => '電子郵件範本',
+    'LBL_MODULE_TITLE' => '電子郵件範本: 首頁',
+    'LBL_NAME' => '名稱:',
+    'LBL_NEW_FORM_TITLE' => '創建電子郵件模版',
+    'LBL_PUBLISH' => '公佈:',
+    'LBL_RELATED_TO' => '相關:',
+    'LBL_SEARCH_FORM_TITLE' => '搜尋電子郵件範本',
+    'LBL_SHOW_ALT_TEXT' => '顯示純文字',
+    'LBL_SUBJECT' => '主旨:',
+    'LBL_SUITE_DOCUMENT' => '文件',
+    'LBL_UPLOAD_FILE' => '上傳檔案',
+    'LBL_TEXT_BODY' => '文本正文',
+    'LBL_USERS' => '使用者',
+
+    'LNK_EMAIL_TEMPLATE_LIST' => '電子郵件模版',
+    'LNK_IMPORT_NOTES' => '導入備忘錄',
+    'LNK_NEW_EMAIL_TEMPLATE' => '創建電子郵件模版',
+    'LNK_NEW_EMAIL' => '存檔電子郵件',
+    'LNK_NEW_SEND_EMAIL' => '撰寫電子郵件',
+    'LNK_SENT_EMAIL_LIST' => '發件箱',
+    'LNK_VIEW_CALENDAR' => '今天',
+    // for Inbox
+    'LBL_NEW' => '新建',
+    'LNK_MY_DRAFTS' => '我的草稿箱',
+    'LNK_MY_INBOX' => '我的收件箱',
+    'LBL_TEXT_ONLY' => '只有文本',
+    'LBL_SEND_AS_TEXT' => '只發送文本',
+    'LBL_ACCOUNT' => '客戶',
+    'LBL_FROM_NAME' => '發信人名稱',
+    'LBL_PLAIN_TEXT' => '純文本',
+    'LBL_CREATED_BY' => '建立人',
+    'LBL_PUBLISHED' => '已公佈',
+    'LNK_VIEW_MY_INBOX' => '查看我的電子郵件',
+    'LBL_ASSIGNED_TO_ID' => '負責人',
+    'LBL_EDIT_LAYOUT' => '編輯佈局' /*for 508 compliance fix*/,
+    'LBL_SELECT' => '選擇' /*for 508 compliance fix*/,
+    'LBL_ID_FF_CLEAR' => '清除' /*for 508 compliance fix*/,
+    'LBL_TYPE' => '類型',
+    'LBL_WIDTH' => '預設寬度',
+    'LNK_IMPORT_CAMPAIGNS' => '匯入市場活動',
+);
