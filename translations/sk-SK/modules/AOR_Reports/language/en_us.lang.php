6b432615980695c37cc4904fd3b6b943246319b5
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-16 20:14:38 +0000

    New translations en_us.lang.php (Slovak)

---
+++ translations/sk-SK/modules/AOR_Reports/language/en_us.lang.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID' => 'Priradené ID používateľa',
+    'LBL_ASSIGNED_TO_NAME' => 'Priradené ku',
+    'LBL_ID' => 'ID',
+    'LBL_DATE_ENTERED' => 'Dátum vytvorenia',
+    'LBL_DATE_MODIFIED' => 'Dátum úpravy',
+    'LBL_MODIFIED' => 'Upravil',
+    'LBL_MODIFIED_NAME' => 'Upravil',
+    'LBL_CREATED' => 'Vytvoril',
+    'LBL_DESCRIPTION' => 'Popis',
+    'LBL_DELETED' => 'Vymazané',
+    'LBL_NAME' => 'Meno',
+    'LBL_CREATED_USER' => 'Vytvoril',
+    'LBL_MODIFIED_USER' => 'Upravil',
+    'LBL_LIST_NAME' => 'Meno',
+    'LBL_EDIT_BUTTON' => 'Upraviť',
+    'LBL_REMOVE' => 'Odstrániť',
+    'LBL_LIST_FORM_TITLE' => 'Zoznam správ',
+    'LBL_MODULE_NAME' => 'Reporty',
+    'LBL_MODULE_TITLE' => 'Reporty',
+    'LBL_HOMEPAGE_TITLE' => 'Moje správy',
+    'LNK_NEW_RECORD' => 'Vytvoriť správu',
+    'LNK_LIST' => 'Reporty',
+    'LBL_SEARCH_FORM_TITLE' => 'Vyhľadanie správ',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Zobraziť Históriu',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Aktivity',
+    'LBL_NEW_FORM_TITLE' => 'Nová správa',
+    'LBL_REPORT_MODULE' => 'Modul správ',
+    'LBL_GRAPHS_PER_ROW' => 'Grafy na riadok',
+    'LBL_FIELD_LINES' => 'Zobraziť polia',
+    'LBL_ADD_FIELD' => 'Pridať pole:',
+    'LBL_CONDITION_LINES' => 'Podmienky',
+    'LBL_ADD_CONDITION' => 'Pridať podmienku',
+    'LBL_EXPORT' => 'Exportovať',
+    'LBL_DOWNLOAD_PDF' => 'Stiahnuť PDF',
+    'LBL_ADD_TO_PROSPECT_LIST' => 'Pridať k zoznamu cieľov',
+    'LBL_AOR_MODULETREE_SUBPANEL_TITLE' => 'Strom modulov',
+    'LBL_AOR_FIELDS_SUBPANEL_TITLE' => 'Polia',
+    'LBL_AOR_CONDITIONS_SUBPANEL_TITLE' => 'Podmienky',
+    'LBL_TOTAL' => 'Spolu',
+    'LBL_AOR_CHARTS_SUBPANEL_TITLE' => 'Grafy',
+    'LBL_ADD_CHART' => 'Pridanie grafu',
+    'LBL_ADD_PARENTHESIS' => 'Vložiť zátvorky',
+    'LBL_CHART_TITLE' => 'Titul',
+    'LBL_CHART_TYPE' => 'Typ',
+    'LBL_CHART_X_FIELD' => 'Os X',
+    'LBL_CHART_Y_FIELD' => 'Os Y',
+    'LBL_AOR_REPORTS_DASHLET' => 'Reporty',
+    'LBL_DASHLET_TITLE' => 'Titul',
+    'LBL_DASHLET_REPORT' => 'Správa',
+    'LBL_DASHLET_CHOOSE_REPORT' => 'Vyberte správu, prosím',
+    'LBL_DASHLET_SAVE' => 'Uložiť',
+    'LBL_DASHLET_CHARTS' => 'Grafy',
+    'LBL_DASHLET_ONLY_CHARTS' => 'Zobraziť iba grafy',
+    'LBL_UPDATE_PARAMETERS' => 'Aktualizovať',
+    'LBL_PARAMETERS' => 'Parametre',
+    'LBL_TOOLTIP_DRAG_DROP_ELEMS' => 'Pretiahnuť prvky do oblasti poľa alebo oblasti podmienok',
+    'LBL_MAIN_GROUPS' => 'Hlavná skupina:',
+    'LBL_CHAR_UNNAMED_DEFAULT_TITLE' => 'Nepomenovaný graf',
+    'LBL_REPORT' => 'Správa',
+);
