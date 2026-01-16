eca619c933b25b046b9f9e8c14d130c9fb173f9d
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-16 21:13:20 +0000

    New translations en_us.lang.php (Indonesian)

---
+++ translations/id-ID/modules/EmailTemplates/language/en_us.lang.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ADD_DOCUMENT' => 'Tambahkan sebuah dokumen',
+    'LBL_ADD_FILE' => 'Tambah file',
+    'LBL_ATTACHMENTS' => 'Lampiran',
+    'LBL_BODY' => 'Tubuh: ',
+    'LBL_CLOSE' => 'Tutup:',
+    'LBL_DESCRIPTION' => 'Deskripsi:',
+    'LBL_EDIT_ALT_TEXT' => 'Edit Teks Biasa',
+    'LBL_EMAIL_ATTACHMENT' => 'Lampiran Email',
+    'LBL_HIDE_ALT_TEXT' => 'Sembunyikan Teks Biasa',
+    'LBL_HTML_BODY' => 'Badan HTML',
+    'LBL_INSERT_VARIABLE' => 'Sisipkan Variabel:',
+    'LBL_INSERT_URL_REF' => 'Masukkan URL Reference',
+    'LBL_INSERT_TRACKER_URL' => 'Masukkan Tracker URL:',
+    'LBL_INSERT' => 'Sisipkan',
+    'LBL_LIST_DATE_MODIFIED' => 'Modifikasi Terakhir',
+    'LBL_LIST_DESCRIPTION' => 'Deskripsi',
+    'LBL_LIST_FORM_TITLE' => 'Daftar Template Email',
+    'LBL_LIST_NAME' => 'Nama',
+    'LBL_MODULE_NAME' => 'Template Email',
+    'LBL_MODULE_TITLE' => 'Template Email: Home',
+    'LBL_NAME' => 'Nama:',
+    'LBL_NEW_FORM_TITLE' => 'Buat Template Email',
+    'LBL_PUBLISH' => 'Terbitkan:',
+    'LBL_RELATED_TO' => 'Terkait dengan',
+    'LBL_SEARCH_FORM_TITLE' => 'Cari template email',
+    'LBL_SHOW_ALT_TEXT' => 'Tampilkan Plain Text',
+    'LBL_SUBJECT' => 'Subjek:',
+    'LBL_SUITE_DOCUMENT' => 'Dokumen',
+    'LBL_UPLOAD_FILE' => 'Upload File',
+    'LBL_TEXT_BODY' => 'Teks Badan',
+    'LBL_USERS' => 'Pemakai',
+
+    'LNK_EMAIL_TEMPLATE_LIST' => 'Lihat Template Email',
+    'LNK_IMPORT_NOTES' => 'Impor Catatan',
+    'LNK_NEW_EMAIL_TEMPLATE' => 'Buat Template Email',
+    'LNK_NEW_EMAIL' => 'Arsip Email',
+    'LNK_NEW_SEND_EMAIL' => 'Tulis Surel',
+    'LNK_SENT_EMAIL_LIST' => 'Email Terkirim',
+    'LNK_VIEW_CALENDAR' => 'Hari ini',
+    // for Inbox
+    'LBL_NEW' => 'Baru',
+    'LNK_MY_DRAFTS' => 'Draft saya',
+    'LNK_MY_INBOX' => 'Email saya',
+    'LBL_TEXT_ONLY' => 'Teks saja',
+    'LBL_SEND_AS_TEXT' => 'Kirim Teks Saja',
+    'LBL_ACCOUNT' => 'Akun',
+    'LBL_FROM_NAME' => 'Dari Nama',
+    'LBL_PLAIN_TEXT' => 'Teks Polos',
+    'LBL_CREATED_BY' => 'Dibuat Oleh',
+    'LBL_PUBLISHED' => 'Ditampilkan',
+    'LNK_VIEW_MY_INBOX' => 'Lihat Email Saya',
+    'LBL_ASSIGNED_TO_ID' => 'ditugaskan kepada',
+    'LBL_EDIT_LAYOUT' => 'Sunting Tata Letak' /*for 508 compliance fix*/,
+    'LBL_SELECT' => 'Pilih [Alt+T]' /*for 508 compliance fix*/,
+    'LBL_ID_FF_CLEAR' => 'Bersihkan [Alt+C]' /*for 508 compliance fix*/,
+    'LBL_TYPE' => 'Tipe',
+    'LBL_WIDTH' => 'Lebar Baku',
+    'LNK_IMPORT_CAMPAIGNS' => 'Ekspedisi Impor',
+);
