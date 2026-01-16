08054365a2a7bde62a6aa9fbe1d9bf5311d0e905
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-16 21:12:46 +0000

    New translations en_us.lang.php (Bulgarian)

---
+++ translations/bg-BG/modules/EmailTemplates/language/en_us.lang.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ADD_DOCUMENT' => 'Добавяне на документ',
+    'LBL_ADD_FILE' => 'Прикачване на файл',
+    'LBL_ATTACHMENTS' => 'Приложения',
+    'LBL_BODY' => 'Body:',
+    'LBL_CLOSE' => 'Затвори:',
+    'LBL_DESCRIPTION' => 'Описание:',
+    'LBL_EDIT_ALT_TEXT' => 'Редактиране на обикновен текст',
+    'LBL_EMAIL_ATTACHMENT' => 'Приложения',
+    'LBL_HIDE_ALT_TEXT' => 'Скриване на обикновен текст',
+    'LBL_HTML_BODY' => 'HTML',
+    'LBL_INSERT_VARIABLE' => 'Вмъкване на променлива:',
+    'LBL_INSERT_URL_REF' => 'Вмъкване на URL',
+    'LBL_INSERT_TRACKER_URL' => 'Вмъкване на маркер:',
+    'LBL_INSERT' => 'Вмъкни',
+    'LBL_LIST_DATE_MODIFIED' => 'Последно модифициран',
+    'LBL_LIST_DESCRIPTION' => 'Описание',
+    'LBL_LIST_FORM_TITLE' => 'Списък с шаблони',
+    'LBL_LIST_NAME' => 'Име',
+    'LBL_MODULE_NAME' => 'Шаблони за електронна поща',
+    'LBL_MODULE_TITLE' => 'Шаблони на писма:',
+    'LBL_NAME' => 'Име:',
+    'LBL_NEW_FORM_TITLE' => 'Създаване на шаблон',
+    'LBL_PUBLISH' => 'Публикувай:',
+    'LBL_RELATED_TO' => 'Свързано с:',
+    'LBL_SEARCH_FORM_TITLE' => 'Търсене в модул "Шаблони на писма"',
+    'LBL_SHOW_ALT_TEXT' => 'Показване на обикновен текст',
+    'LBL_SUBJECT' => 'Относно:',
+    'LBL_SUITE_DOCUMENT' => 'Документ',
+    'LBL_UPLOAD_FILE' => 'Upload File',
+    'LBL_TEXT_BODY' => 'Текст',
+    'LBL_USERS' => 'Потребители',
+
+    'LNK_EMAIL_TEMPLATE_LIST' => 'Списък с шаблони',
+    'LNK_IMPORT_NOTES' => 'Импортиране на бележки',
+    'LNK_NEW_EMAIL_TEMPLATE' => 'Създаване на шаблон',
+    'LNK_NEW_EMAIL' => 'Създаване на запис за изпратена поща',
+    'LNK_NEW_SEND_EMAIL' => 'Ново писмо',
+    'LNK_SENT_EMAIL_LIST' => 'Изпратени',
+    'LNK_VIEW_CALENDAR' => 'Днес',
+    // for Inbox
+    'LBL_NEW' => 'Нов',
+    'LNK_MY_DRAFTS' => 'Чернови',
+    'LNK_MY_INBOX' => 'Входящи',
+    'LBL_TEXT_ONLY' => 'Само текст',
+    'LBL_SEND_AS_TEXT' => 'Изпрати само текст',
+    'LBL_ACCOUNT' => 'Организация',
+    'LBL_FROM_NAME' => 'Подател',
+    'LBL_PLAIN_TEXT' => 'обикновен текст',
+    'LBL_CREATED_BY' => 'Създадено от',
+    'LBL_PUBLISHED' => 'Стартирал',
+    'LNK_VIEW_MY_INBOX' => 'Преглед на входящи съобщения',
+    'LBL_ASSIGNED_TO_ID' => 'Отговорник:',
+    'LBL_EDIT_LAYOUT' => 'Редактиране на подредби' /*for 508 compliance fix*/,
+    'LBL_SELECT' => 'Избери' /*for 508 compliance fix*/,
+    'LBL_ID_FF_CLEAR' => 'Изчисти' /*for 508 compliance fix*/,
+    'LBL_TYPE' => 'Тип',
+    'LBL_WIDTH' => 'Width Default',
+    'LNK_IMPORT_CAMPAIGNS' => 'Import Campaign',
+);
