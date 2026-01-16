c82870fc049d6c45dcccac3110f6bbdf4a355e0b
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-16 21:47:32 +0000

    New translations en_us.lang.php (Slovak)

---
+++ translations/sk-SK/modules/Surveys/language/en_us.lang.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID'                                     => 'Priradené ID používateľa',
+    'LBL_ADD_OPTION'                                         => 'Add new option',
+    'LBL_ASSIGNED_TO_NAME'                                   => 'Priradené ku',
+    'LBL_SECURITYGROUPS'                                     => 'CRM Skupiny',
+    'LBL_SECURITYGROUPS_SUBPANEL_TITLE'                      => 'CRM Skupiny',
+    'LBL_ID'                                                 => 'ID',
+    'LBL_DATE_ENTERED'                                       => 'Dátum vytvorenia',
+    'LBL_DATE_MODIFIED'                                      => 'Dátum úpravy',
+    'LBL_MODIFIED'                                           => 'Upravil',
+    'LBL_MODIFIED_ID'                                        => 'Upravené cez ID',
+    'LBL_MODIFIED_NAME'                                      => 'Upravil',
+    'LBL_CREATED'                                            => 'Vytvoril',
+    'LBL_CREATED_ID'                                         => 'Vytvorené cez ID',
+    'LBL_DESCRIPTION'                                        => 'Popis',
+    'LBL_DELETED'                                            => 'Vymazané',
+    'LBL_NAME'                                               => 'Meno',
+    'LBL_CREATED_USER'                                       => 'Vytvoril',
+    'LBL_MODIFIED_USER'                                      => 'Upravil',
+    'LBL_LIST_NAME'                                          => 'Meno',
+    'LBL_EDIT_BUTTON'                                        => 'Upraviť',
+    'LBL_REMOVE'                                             => 'Odstrániť',
+    'LBL_LIST_FORM_TITLE'                                    => 'Zoznam prieskumov',
+    'LBL_MODULE_NAME'                                        => 'Priezkumy',
+    'LBL_MODULE_TITLE'                                       => 'Priezkumy',
+    'LBL_HOMEPAGE_TITLE'                                     => 'Moje prieskumy',
+    'LNK_NEW_RECORD'                                         => 'Vytvorenie prieskumu',
+    'LNK_LIST'                                               => 'Zobrazenie prieskumov',
+    'LNK_IMPORT_SURVEYS'                                     => 'Importovať prieskumy',
+    'LBL_SEARCH_FORM_TITLE'                                  => 'Hľadať prieskumy',
+    'LBL_HISTORY_SUBPANEL_TITLE'                             => 'Zobraziť Históriu',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE'                          => 'Aktivity',
+    'LBL_SURVEYS_SUBPANEL_TITLE'                             => 'Priezkumy',
+    'LBL_NEW_FORM_TITLE'                                     => 'Nové prieskumy',
+    'LBL_STATUS'                                             => 'Stav',
+    'LBL_SURVEY_QUESTIONS_DISPLAY'                           => 'Otázky',
+    'LBL_SURVEY_URL_DISPLAY'                                 => 'URL',
+    'LBL_HAPPINESS_QUESTION'                                 => 'Otázka šťastia',
+    'LBL_CANT_EDIT_RESPONDED'                                => 'Otázky prieskumu s odpoveďami nie je možné upravovať',
+    'LBL_VIEW_SURVEY_REPORTS'                                => 'Zobraziť správy o prieskume',
+    'LBL_CHECKED'                                            => 'Označené',
+    'LBL_UNCHECKED'                                          => 'Odznačené',
+    'LBL_RESPONSE_ANSWER'                                    => 'Odpoveď',
+    'LBL_RESPONSE_CONTACT'                                   => 'Kontakt',
+    'LBL_RESPONSE_TIME'                                      => 'Dátum',
+    'LBL_UNKNOWN_CONTACT'                                    => 'Neznáme',
+    'LBL_RESPONSE_COUNT'                                     => 'Počet',
+    'LBL_SUBMIT_TEXT'                                        => 'Odoslať text',
+    'LBL_SATISFIED_TEXT'                                     => 'Spokojný Text',
+    'LBL_NEITHER_TEXT'                                       => 'Neutrálny text',
+    'LBL_DISSATISFIED_TEXT'                                  => 'Nespokojný Text',
+    'LBL_SURVEYS_SURVEYQUESTIONS_FROM_SURVEYQUESTIONS_TITLE' => 'Otázky prieskumu',
+    'LBL_SURVEYS_SURVEYRESPONSES_FROM_SURVEYRESPONSES_TITLE' => 'Odpovede na prieskum',
+    'LBL_SHOW_RESPONSES' => 'Zobraziť odpovede',
+    'LBL_HIDE_RESPONSES' => 'Hide responses',
+    'LNK_LIST_SURVEY_RESPONSES' => 'Show Survey Responses',
+    'LBL_SURVEY_QUESTION' => 'Question',
+    'LBL_SURVEY_TEXT' => 'Text',
+    'LBL_SURVEY_TYPE' => 'Typ',
+    'LBL_SURVEY_ACTIONS' => 'Akcie',
+    'LBL_SURVEY_NEW_QUESTION' => 'New Question',
+    'LBL_SURVEY_RESPONSES' => 'New Question',
+    'LBL_SURVEYS_SENT' => 'Surveys Sent:',
+    'LBL_SURVEY_DISTINCT' => 'Distinct Surveys Sent:',
+    'LBL_DRAFT' => 'Koncept',
+    'LBL_SUBMIT' => 'Poslať',
+    'LBL_STARS' => 'Stars',
+    'LBL_SURVEY_CLOSE_RESPONSE' => 'Thanks for your interest but this survey is now closed.',
+);
