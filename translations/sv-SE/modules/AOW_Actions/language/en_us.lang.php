da6f04fb772ce771077af7aa643d21600997763b
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-16 20:26:33 +0000

    New translations en_us.lang.php (Swedish)

---
+++ translations/sv-SE/modules/AOW_Actions/language/en_us.lang.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ID' => 'ID',
+    'LBL_DATE_ENTERED' => 'Datum Skapad',
+    'LBL_DATE_MODIFIED' => 'Datum Ändrad',
+    'LBL_MODIFIED' => 'Ändrad av',
+    'LBL_MODIFIED_ID' => 'Ändrad Av Id',
+    'LBL_MODIFIED_NAME' => 'Ändrad av Namn',
+    'LBL_CREATED_USER' => 'Skapad av Användare',
+    'LBL_MODIFIED_USER' => 'Ändrad av Användare',
+    'LBL_CREATED' => 'Skapad av',
+    'LBL_DESCRIPTION' => 'Beskrivning',
+    'LBL_CREATED_ID' => 'Skapad Av Id',
+    'LBL_DELETED' => 'Raderad',
+    'LBL_NAME' => 'Namn',
+    'LBL_MODULE_NAME' => 'Arbetsflödesåtgärder',
+    'LBL_MODULE_TITLE' => 'Arbetsflödesåtgärder',
+    'LBL_AOW_WORKFLOW_ID' => 'AOW_WorkFlow Id',
+    'LBL_ACTION' => 'Handling',
+    'LBL_PARAMETERS' => 'Parametrar',
+    'LBL_SELECT_ACTION' => 'Välj åtgärd',
+    'LBL_CREATE_EMAIL_TEMPLATE' => 'Skapa',
+    'LBL_RECORD_TYPE' => 'Posttyp',
+    'LBL_SENDEMAIL' => 'Skicka E-post',
+    'LBL_CREATERECORD' => 'Create Record',
+    'LBL_MODIFYRECORD' => 'Modify Record',
+    'LBL_ADD_FIELD' => 'Lägg till Fält',
+    'LBL_ADD_RELATIONSHIP' => 'Lägg till relation',
+    'LBL_EDIT_EMAIL_TEMPLATE' => 'Redigera',
+    'LBL_EMAIL' => 'E-post',
+    'LBL_EMAIL_TEMPLATE' => 'E-postmall',
+    'LBL_SETAPPROVAL' => 'Set Approval',
+    'LBL_RELATE_WORKFLOW' => 'Relate to WorkFlow Module',
+    'LBL_INDIVIDUAL_EMAILS' => 'Send Individual Emails',
+    'LBL_COMPUTEFIELD' => 'Beräkna fält',
+    'LBL_COMPUTEFIELD_PARAMETERS' => 'Parametrar',
+    'LBL_COMPUTEFIELD_FIELD_NAME' => 'Fältnamn',
+    'LBL_COMPUTEFIELD_IDENTIFIER' => 'Identifierare',
+    'LBL_COMPUTEFIELD_ADD_PARAMETER' => 'Lägg till parameter',
+    'LBL_COMPUTEFIELD_RELATION_PARAMETERS' => 'Relation parameters',
+    'LBL_COMPUTEFIELD_RELATION' => 'Relation',
+    'LBL_COMPUTEFIELD_ADD_RELATION_PARAMETER' => 'Add relation parameter',
+    'LBL_COMPUTEFIELD_FORMULA' => 'Formel',
+    'LBL_COMPUTEFIELD_FORMULAS' => 'Formler',
+    'LBL_COMPUTEFIELD_ADD_FORMULA' => 'Lägg till formel',
+    'LBL_COMPUTEFIELD_VALUE_TYPE' => 'Värdetyp',
+    'LBL_COMPUTEFIELD_RAW_VALUE' => 'Råa värdet',
+    'LBL_COMPUTEFIELD_FORMATTED_VALUE' => 'Formatted value',
+    'LBL_COPY_EMAIL_ADDRESSES_WORKFLOW' => 'Copy emails from WorkFlow Module'
+);
