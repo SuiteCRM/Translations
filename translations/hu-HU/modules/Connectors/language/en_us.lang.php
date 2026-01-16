17ed6c3700c3eccb48fb04db2616e9c3c63af09c
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-16 20:59:58 +0000

    New translations en_us.lang.php (Hungarian)

---
+++ translations/hu-HU/modules/Connectors/language/en_us.lang.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+
+$mod_strings = array(
+
+    'LBL_ADMINISTRATION_MAIN' => 'Csatlakozó beállítások',
+    'LBL_AVAILABLE' => 'Elérhető',
+    'LBL_BACK' => '<Vissza',
+    'LBL_CONFIRM_CONTINUE_SAVE' => 'Néhány kötelezően kitöltendő mező üresen maradt. Folytatja a mentést?',
+    'LBL_CONNECTOR_FIELDS' => 'Kapcsoló mezők',
+    'LBL_DATA' => 'Adat',
+    'LBL_DEFAULT' => 'Alapértelmezett',
+    'LBL_DISABLED' => 'Letiltva',
+    'LBL_ENABLED' => 'Engedélyezve',
+    'LBL_EXTERNAL' => 'Felhasználók engedélyezése új külső fiók rekordok létrehozásához ehhez a kapcsolóhoz. Ennek használatához a megfelelő tulajdonságokat be kell állítani a Kapcsoló Tulajdonságok Beállítása oldalon.',
+    'LBL_EXTERNAL_SET_PROPERTIES' => 'A csatlakozó megfelelő használatához testre kell szabni a "kapcsoló beállításai" oldalon.',
+    'LBL_MERGE' => 'Egyesítés',
+    'LBL_MODIFY_DISPLAY_TITLE' => 'Kapcsolatok engedélyezése',
+    'LBL_MODIFY_DISPLAY_DESC' => 'Válassza ki azokat a modulokat, amelyeket engedélyez a kapcsolóhoz.',
+    'LBL_MODULE_FIELDS' => 'Modul mezők',
+    'LBL_MODIFY_MAPPING_TITLE' => 'Modul mezők térképe',
+    'LBL_MODIFY_MAPPING_DESC' => 'Kapcsoló mezők térképe a modul mezőkhöz annak érdekében, hagy a kapcsoló mezők megtekinthetők és beleolvaszthatók legyenek a modul rekordjaiba.',
+    'LBL_MODIFY_PROPERTIES_TITLE' => 'Kapcsoló beállítása',
+    'LBL_MODIFY_PROPERTIES_DESC' => 'Állítsa be a csatlakozó beállításait, beleértve az URL-eket és az API kulcsokat.',
+    'LBL_MODIFY_SEARCH_TITLE' => 'Csatlakozó keresés kezelés',
+    'LBL_MODIFY_SEARCH' => 'Keresés',
+    'LBL_MODIFY_SEARCH_DESC' => 'Válassza ki a kapcsoló mezőket az adatok kereséséhez az egyes modulokban.',
+    'LBL_MODULE_NAME' => 'Csatlakozók',
+    'LBL_NO_PROPERTIES' => 'Nincsenek beállítva ennek a kapcsolónak a tulajdonságai.',
+    'LBL_SAVE' => 'Mentés',
+    'LBL_SUMMARY' => 'Összefoglaló',
+    'LBL_STEP1' => 'Adat keresés és nézet',
+    'LBL_STEP2' => 'Sorok egyesítése (-val)',
+    'LBL_TEST_SOURCE' => 'Kapcsoló teszt',
+    'LBL_TEST_SOURCE_FAILED' => 'Hibás teszt',
+    'LBL_TEST_SOURCE_SUCCESS' => 'Sikeres teszt',
+    'LBL_TITLE' => 'Adategyesítés',
+
+    'ERROR_NO_ADDITIONAL_DETAIL' => 'Hiba: A sorhoz nem találhatók további részletek.',
+    'ERROR_NO_SEARCHDEFS_DEFINED' => 'Nincsenek modulok engedélyezve ehhez a kapcsolóhoz. Válasszon hozzá a kapcsoló engedélyezése oldalon.',
+    'ERROR_NO_SEARCHDEFS_MAPPED' => 'Hiba: Nincsenek kapcsolók engedélyezve ahhoz, hogy kereső mezőket határozzon meg.',
+    'ERROR_NO_SEARCHDEFS_MAPPING' => 'Hiba: nincsenek kereső mezők rendelve a modulhoz és a kapcsolóhoz. Vegye fel a kapcsolatot a rendszer adminisztrátorral.',
+    'ERROR_NO_DISPLAYABLE_MAPPED_FIELDS' => 'Hiba: Nincsenek leképezett modul mezők a megjelenített eredményekben. Vegye fel a kapcsolatot a rendszer adminisztrátorral.',
+    'LBL_INFO_INLINE' => 'Infó' /*for 508 compliance fix*/,
+    'LBL_CLOSE' => 'Bezárás' /*for 508 compliance fix*/,
+
+);
