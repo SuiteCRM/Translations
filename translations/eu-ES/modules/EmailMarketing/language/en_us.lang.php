48fea3e0257e1ab1d92db65452c99a6745ef8c83
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-16 21:11:41 +0000

    New translations en_us.lang.php (Basque)

---
+++ translations/eu-ES/modules/EmailMarketing/language/en_us.lang.php
@@ -0,0 +1,108 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_REPLY_ADDR' => '"Reply-to" Address: ',
+    'LBL_REPLY_NAME' => '"Reply-to" Name: ',
+
+    'LBL_MODULE_NAME' => 'Posta elektroniko bidezko marketing-a',
+    'LBL_MODULE_TITLE' => 'Posta elektroniko bidezko marketing-a: hasiera',
+    'LBL_LIST_FORM_TITLE' => 'Posta elektroniko bidezko marketing-kanpainak',
+    'LBL_NAME' => 'Izena ',
+    'LBL_LIST_NAME' => 'Izena',
+    'LBL_LIST_FROM_ADDR' => 'Posta elektronikoaren igorlea',
+    'LBL_LIST_DATE_START' => 'Hasiera-data',
+    'LBL_LIST_TEMPLATE_NAME' => 'Posta elektroniko txantiloia',
+    'LBL_LIST_STATUS' => 'Egoera',
+    'LBL_STATUS' => 'Egoera',
+    'LBL_STATUS_TEXT' => 'Egoera',
+    'LBL_TEMPLATE_NAME' => 'Template Name',
+    'LBL_DATE_ENTERED' => 'Alta-data',
+    'LBL_DATE_MODIFIED' => 'Aldaketa-data',
+    'LBL_MODIFIED' => 'Aldatzailea ',
+    'LBL_CREATED' => 'Sortzailea: ',
+    'LBL_MESSAGE_FOR' => 'Mezu hau nori bidali:',
+
+    'LBL_FROM_NAME' => 'Izen-igorlea: ',
+    'LBL_FROM_ADDR' => 'Posta elektroniko igorlearen helbidea: ',
+    'LBL_DATE_START' => 'Hasiera-data',
+    'LBL_TIME_START' => 'Start Time',
+    'LBL_START_DATE_TIME' => 'Hasiera-data eta ordua: ',
+    'LBL_TEMPLATE' => 'Posta elektroniko txantiloia: ',
+
+    'LBL_MODIFIED_BY' => 'Aldatzailea ',
+    'LBL_CREATED_BY' => 'Sortzailea: ',
+
+    'LNK_NEW_CAMPAIGN' => 'Kanpaina sortu',
+    'LNK_CAMPAIGN_LIST' => 'Kanpainak',
+    'LNK_NEW_PROSPECT_LIST' => 'Helburu den publikoaren zerrenda sortu',
+    'LNK_PROSPECT_LIST_LIST' => 'Helburu den publikoaren zerrendak',
+    'LNK_NEW_PROSPECT' => 'Helburu den publikoa sortu',
+    'LNK_PROSPECT_LIST' => 'Helburu den publikoa ',
+    'LBL_DEFAULT_SUBPANEL_TITLE' => 'Posta elektroniko bidezko marketing-a',
+    'LBL_CREATE_EMAIL_TEMPLATE' => 'Berria',
+    'LBL_EDIT_EMAIL_TEMPLATE' => 'Editatu',
+    'LBL_FROM_MAILBOX' => 'Postontzitik',
+    'LBL_FROM_MAILBOX_NAME' => 'Postontzia erabili:',
+    'LBL_OUTBOUND_EMAIL_ACCOUNT_NAME' => 'Outbound Email Account:',
+    'LBL_PROSPECT_LIST_SUBPANEL_TITLE' => 'Helburu den publikoaren zerrendak',
+    'LBL_ALL_PROSPECT_LISTS' => 'Kanpainaren helburu den jende-zerrendak  ',
+    'LBL_RELATED_PROSPECT_LISTS' => 'Mezu honekin lotutako kanpainaren helburu den jende-zerrendak  ',
+    'LBL_PROSPECT_LIST_NAME' => 'Izena',
+
+    'LBL_LIST_PROSPECT_LIST_NAME' => 'Targeted Lists',
+    'LBL_MODULE_SEND_TEST' => 'Campaign: Send Test',
+    'LBL_MODULE_SEND_EMAILS' => 'Campaign: Send Emails',
+    'LBL_SCHEDULE_MESSAGE_TEST' => 'Please select the campaign messages that you would like to test:',
+    'LBL_SCHEDULE_MESSAGE_EMAILS' => 'Please select the campaign messages that you would like to schedule for distribution on the specified start date and time:',
+    'LBL_SCHEDULE_BUTTON_TITLE' => 'Bidali',
+    'LBL_SCHEDULE_BUTTON_LABEL' => 'Bidali',
+    'LBL_ERROR_ON_MARKETING' => 'Missing required field(s)',
+
+    'LBL_CAMPAIGN_ID' => 'Campaign ID',
+    'LBL_OUTBOUND_EMAIL_ACOUNT_ID' => 'Outbound Email Account ID',
+    'LBL_EMAIL_TEMPLATE' => 'Posta elektroniko txantiloia',
+    'LBL_PROSPECT_LISTS' => 'Prospect Lists',
+
+);
