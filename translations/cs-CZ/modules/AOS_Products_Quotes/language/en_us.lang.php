72365ac98185c5b07f4beabfd352e65224ed6089
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-16 20:23:54 +0000

    New translations en_us.lang.php (Czech)

---
+++ translations/cs-CZ/modules/AOS_Products_Quotes/language/en_us.lang.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID' => 'ID přiřazeného uživatele',
+    'LBL_ASSIGNED_TO_NAME' => 'Přiřazeno k',
+    'LBL_ID' => 'ID',
+    'LBL_DATE_ENTERED' => 'Datum vytvoření',
+    'LBL_DATE_MODIFIED' => 'Datum změny',
+    'LBL_MODIFIED' => 'Změnil',
+    'LBL_MODIFIED_NAME' => 'Změněno dle Jména',
+    'LBL_CREATED' => 'Vytvořeno',
+    'LBL_DESCRIPTION' => 'Poznámka',
+    'LBL_DELETED' => 'Vymazáno',
+    'LBL_NAME' => 'Název',
+    'LBL_CREATED_USER' => 'Vytvořeno dle Uživatele',
+    'LBL_MODIFIED_USER' => 'Změněno dle Uživatele',
+    'LBL_LIST_FORM_TITLE' => 'Seznam nabídek produktů',
+    'LBL_MODULE_NAME' => 'Položky řádku',
+    'LBL_MODULE_TITLE' => 'Nabídky produktů: Domů',
+    'LBL_HOMEPAGE_TITLE' => 'Moje nabídky produktů',
+    'LNK_NEW_RECORD' => 'Vytvořit nabídku produktu',
+    'LNK_LIST' => 'Nabídky produktů',
+    'LBL_SEARCH_FORM_TITLE' => 'Vyhledat nabídky produktů',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Historie',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Aktivity',
+    'LBL_NEW_FORM_TITLE' => 'Nová nabídka produktu',
+    'LBL_PRODUCT_NAME' => 'Název',
+    'LBL_PRODUCT_QTY' => 'Množství',
+    'LBL_PRODUCT_COST_PRICE' => 'Nákladová cena',
+    'LBL_PRODUCT_LIST_PRICE' => 'Ceníková cena',
+    'LBL_PRODUCT_UNIT_PRICE' => 'Jednotková cena',
+    'LBL_PRODUCT_DISCOUNT' => 'Sleva',
+    'LBL_PRODUCT_DISCOUNT_AMOUNT' => 'Výše slevy',
+    'LBL_PART_NUMBER' => 'Číslo položky',
+    'LBL_PRODUCT_DESCRIPTION' => 'Popis',
+    'LBL_DISCOUNT' => 'Typ slevy',
+    'LBL_VAT_AMT' => 'Výše daně',
+    'LBL_VAT' => 'Daň',
+    'LBL_PRODUCT_TOTAL_PRICE' => 'Celková cena',
+    'LBL_PRODUCT_NOTE' => 'Poznámka',
+    'Quote' => '',
+    'LBL_FLEX_RELATE' => 'Související s',
+    'LBL_PRODUCT' => 'Produkt',
+
+    'LBL_SERVICE_MODULE_NAME' => 'Služby',
+    'LBL_LIST_NUM' => 'Číslo',
+    'LBL_PARENT_ID' => 'Rodičovské ID ',
+    'LBL_GROUP_NAME' => 'Skupina',
+    'LBL_PRODUCT_COST_PRICE_USDOLLAR' => 'Nákladová cena (výchozí měna)',
+    'LBL_PRODUCT_LIST_PRICE_USDOLLAR' => 'Ceníková cena (výchozí měna)',
+    'LBL_PRODUCT_UNIT_PRICE_USDOLLAR' => 'Jednotková cena (výchozí měna)',
+    'LBL_PRODUCT_TOTAL_PRICE_USDOLLAR' => 'Celková cena (výchozí měna)',
+    'LBL_PRODUCT_DISCOUNT_USDOLLAR' => 'Sleva (výchozí měna)',
+    'LBL_PRODUCT_DISCOUNT_AMOUNT_USDOLLAR' => 'Výše slevy (výchozí měna)',
+    'LBL_VAT_AMT_USDOLLAR' => 'Výše daně (výchozí měna)',
+    'LBL_PRODUCTS_SERVICES' => 'Produkt / služba',
+    'LBL_PRODUCT_ID' => 'ID produktu',
+
+    'LBL_AOS_CONTRACTS' => 'Smlouvy',
+    'LBL_AOS_INVOICES' => 'Faktury',
+    'LBL_AOS_PRODUCTS' => 'Produkty',
+    'LBL_AOS_QUOTES' => 'Nabídky',
+);
