24c00ae70af6aa6d17e03f526e1915a5d7401026
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-16 21:07:52 +0000

    New translations en_us.lang.php (Serbian (Latin))

---
+++ translations/sr-CS/modules/DynamicFields/language/en_us.lang.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LNK_NEW_CALL' => 'Evidentiraj poziv',
+    'LNK_NEW_MEETING' => 'Zakaži sastanak',
+    'LNK_NEW_TASK' => 'Kreiraj zadatak',
+    'LNK_NEW_NOTE' => 'Kreiraj belešku ili prilog',
+    'LNK_NEW_EMAIL' => 'Arhiviraj Email',
+    'LNK_CALL_LIST' => 'Pozivi',
+    'LNK_MEETING_LIST' => 'Sastanci',
+    'LNK_TASK_LIST' => 'Zadaci',
+    'LNK_NOTE_LIST' => 'Beleške',
+    'LBL_ADD_FIELD' => 'Dodaj Polje:',
+    'LBL_SEARCH_FORM_TITLE' => 'Pretraga Modula',
+    'COLUMN_TITLE_NAME' => 'Ime Polja',
+    'COLUMN_TITLE_DISPLAY_LABEL' => 'Prikazana Labela ',
+    'COLUMN_TITLE_LABEL_VALUE' => 'Vrednost Labele',
+    'COLUMN_TITLE_LABEL' => 'Sistemska Labela',
+    'COLUMN_TITLE_DATA_TYPE' => 'Tip Podatka',
+    'COLUMN_TITLE_MAX_SIZE' => 'Maksimalna Veličina',
+    'COLUMN_TITLE_HELP_TEXT' => 'Tekst za Pomoć',
+    'COLUMN_TITLE_COMMENT_TEXT' => 'Tekst za Komentar',
+    'COLUMN_TITLE_REQUIRED_OPTION' => 'Zahtevano Polje',
+    'COLUMN_TITLE_DEFAULT_VALUE' => 'Podrazumevana Vrednost',
+    'COLUMN_TITLE_FRAME_HEIGHT' => 'Visina IFrame-a',
+    'COLUMN_TITLE_HTML_CONTENT' => 'HTML',
+    'COLUMN_TITLE_URL' => 'Podrazumevani URL',
+    'COLUMN_TITLE_AUDIT' => 'Praćenje promena',
+    'COLUMN_TITLE_MIN_VALUE' => 'Minimalna Vrednost',
+    'COLUMN_TITLE_MAX_VALUE' => 'Maksimalna Vrednost',
+    'COLUMN_TITLE_LABEL_ROWS' => 'Redovi',
+    'COLUMN_TITLE_LABEL_COLS' => 'Kolone',
+    'COLUMN_TITLE_DISPLAYED_ITEM_COUNT' => 'Broj Prikazanih predmeta',
+    'COLUMN_TITLE_AUTOINC_NEXT' => 'Sledeća Auto-Inkrementirana Vrednost',
+    'COLUMN_DISABLE_NUMBER_FORMAT' => 'Onemogući Formatiranje',
+    'COLUMN_TITLE_ENABLE_RANGE_SEARCH' => 'Omogući okvir pretrage',
+    'LBL_DROP_DOWN_LIST' => 'Padajuća Lista',
+    'LBL_RADIO_FIELDS' => 'Radio Dugmići',
+    'LBL_MULTI_SELECT_LIST' => 'Lista sa više opcija',
+    'COLUMN_TITLE_PRECISION' => 'Preciznost',
+    'LBL_MODULE' => 'Modul',
+    'COLUMN_TITLE_MASS_UPDATE' => 'Masovno ažuriranje',
+    'COLUMN_TITLE_IMPORTABLE' => 'Uvoz',
+    'COLUMN_TITLE_DUPLICATE_MERGE' => 'Dupliraj Spajanje',
+    'LBL_LABEL' => 'Oznaka',
+    'LBL_DATA_TYPE' => 'Tip Podatka',
+    'LBL_DEFAULT_VALUE' => 'Podrazumevana Vrednost',
+    'ERR_RESERVED_FIELD_NAME' => "Ključna reč",
+    'ERR_SELECT_FIELD_TYPE' => 'Izaberite Tip Polja',
+    'ERR_FIELD_NAME_ALREADY_EXISTS' => 'Naziv polja već postoji',
+    'LBL_BTN_ADD' => 'Dodaj',
+    'LBL_BTN_EDIT' => 'Izmeni',
+    'LBL_GENERATE_URL' => 'Generiši URL',
+    'LBL_CALCULATED' => 'Izračunata Vrednost',
+    'LBL_LINK_TARGET' => 'Otvori Link U',
+    'LBL_IMAGE_WIDTH' => 'Širina',
+    'LBL_IMAGE_HEIGHT' => 'Visina',
+    'LBL_IMAGE_BORDER' => 'Graničnik',
+    'LBL_HELP' => 'Pomoć' /*for 508 compliance fix*/,
+    'COLUMN_TITLE_INLINE_EDIT_TEXT' => 'Izmena u polju',
+    'COLUMN_TITLE_PARENT_ENUM' => 'Matični padajući meni',
+    'LBL_INSERT_FIELD' => 'Insert Field',
+);
