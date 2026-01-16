10ba2448725debe1c33fc691331e5ce557b01e73
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-16 20:04:47 +0000

    New translations en_us.lang.php (Serbian (Latin))

---
+++ translations/sr-CS/modules/AM_TaskTemplates/language/en_us.lang.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID' => 'ID broj dodeljenog korisnika',
+    'LBL_ASSIGNED_TO_NAME' => 'Dodeljen',
+    'LBL_ID' => 'ID broj',
+    'LBL_DATE_ENTERED' => 'Datum kreiranja',
+    'LBL_DATE_MODIFIED' => 'Datum izmene',
+    'LBL_MODIFIED' => 'Izmenio',
+    'LBL_MODIFIED_ID' => 'Promenio korisnik sa ID brojem',
+    'LBL_MODIFIED_NAME' => 'Promenio',
+    'LBL_CREATED' => 'Autor',
+    'LBL_CREATED_ID' => 'ID broj autora',
+    'LBL_DESCRIPTION' => 'Opis',
+    'LBL_DELETED' => 'Obrisan',
+    'LBL_NAME' => 'Ime zadatka',
+    'LBL_CREATED_USER' => 'Autor je korisnik',
+    'LBL_MODIFIED_USER' => 'Promenio korisnik',
+    'LBL_LIST_NAME' => 'Ime',
+    'LBL_EDIT_BUTTON' => 'Izmeni',
+    'LBL_REMOVE' => 'Ukloni',
+    'LBL_LIST_FORM_TITLE' => 'Lista šablona za zadatke u projektima',
+    'LBL_MODULE_NAME' => 'Šabloni za zadatke u projektima',
+    'LBL_MODULE_TITLE' => 'Šabloni za zadatke u projektima',
+    'LBL_HOMEPAGE_TITLE' => 'Moji šabloni za zadatke u projektima',
+    'LNK_NEW_RECORD' => 'Napravi šablon za zadatke u projektima',
+    'LNK_LIST' => 'Prikaži šablone za zadatke u projektima',
+    'LNK_IMPORT_AM_TASKTEMPLATES' => 'Uvezi šablon za zadatke u projektima',
+    'LBL_SEARCH_FORM_TITLE' => 'Pretraži šablone za zadatke u projektima',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Pregled istorije',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Aktivnosti',
+    'LBL_AM_TASKTEMPLATES_SUBPANEL_TITLE' => 'Šabloni za zadatke u projektima',
+    'LBL_NEW_FORM_TITLE' => 'Novi šablon za zadatke u projektima',
+    'LBL_STATUS' => 'Status',
+    'LBL_PRIORITY' => 'Prioritet',
+    'LBL_PERCENT_COMPLETE' => 'Procentualno završeno',
+    'LBL_PREDECESSORS' => 'Prethodnici',
+    'LBL_MILESTONE_FLAG' => 'Oznaka prekretnice',
+    'LBL_RELATIONSHIP_TYPE' => 'Tip veze',
+    'LBL_TASK_NUMBER' => 'Broj zadatka',
+    'LBL_ORDER_NUMBER' => 'Redosled ',
+    'LBL_ESTIMATED_EFFORT' => 'Procena broja radnih sati',
+    'LBL_UTILIZATION' => 'Iskorišćenost (%)',
+    'LBL_DURATION' => 'Trajanje (dani)',
+    'LBL_AM_TASKTEMPLATES_AM_PROJECTTEMPLATES_FROM_AM_PROJECTTEMPLATES_TITLE' => 'Šabloni za projekte',
+
+    'LBL_AM_TASKTEMPLATES_AM_PROJECTTEMPLATES_FROM_AM_TASKTEMPLATES_TITLE' => 'Task Templates: Project Templates from Task Templates Title',
+);
