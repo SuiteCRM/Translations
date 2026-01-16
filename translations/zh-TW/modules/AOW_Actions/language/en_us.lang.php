47760f1a34eb0f9cbab6f85e8b04217af0455f42
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-16 20:26:37 +0000

    New translations en_us.lang.php (Chinese Traditional)

---
+++ translations/zh-TW/modules/AOW_Actions/language/en_us.lang.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ID' => '編號',
+    'LBL_DATE_ENTERED' => '建立日期',
+    'LBL_DATE_MODIFIED' => '修改日期',
+    'LBL_MODIFIED' => '修改人',
+    'LBL_MODIFIED_ID' => '修改人編號',
+    'LBL_MODIFIED_NAME' => '修改人姓名',
+    'LBL_CREATED_USER' => '創建者',
+    'LBL_MODIFIED_USER' => '修改的使用者',
+    'LBL_CREATED' => '建立人',
+    'LBL_DESCRIPTION' => '描述',
+    'LBL_CREATED_ID' => '創建人編號',
+    'LBL_DELETED' => '已刪除',
+    'LBL_NAME' => '名稱',
+    'LBL_MODULE_NAME' => '行為',
+    'LBL_MODULE_TITLE' => '行為',
+    'LBL_AOW_WORKFLOW_ID' => '工作流程編號',
+    'LBL_ACTION' => '動作',
+    'LBL_PARAMETERS' => '參數',
+    'LBL_SELECT_ACTION' => '選擇動作',
+    'LBL_CREATE_EMAIL_TEMPLATE' => '建立',
+    'LBL_RECORD_TYPE' => '記錄類型',
+    'LBL_SENDEMAIL' => '存檔電子郵件',
+    'LBL_CREATERECORD' => '創建記錄',
+    'LBL_MODIFYRECORD' => '修改記錄',
+    'LBL_ADD_FIELD' => '添加欄位',
+    'LBL_ADD_RELATIONSHIP' => '添加關係項',
+    'LBL_EDIT_EMAIL_TEMPLATE' => '編輯',
+    'LBL_EMAIL' => '電子郵件',
+    'LBL_EMAIL_TEMPLATE' => '電子郵件範本',
+    'LBL_SETAPPROVAL' => '設置審批',
+    'LBL_RELATE_WORKFLOW' => '關聯到工作流程模組',
+    'LBL_INDIVIDUAL_EMAILS' => '發送個別電子郵件',
+    'LBL_COMPUTEFIELD' => '計算欄位',
+    'LBL_COMPUTEFIELD_PARAMETERS' => '參數',
+    'LBL_COMPUTEFIELD_FIELD_NAME' => '欄位名稱',
+    'LBL_COMPUTEFIELD_IDENTIFIER' => '辨識碼',
+    'LBL_COMPUTEFIELD_ADD_PARAMETER' => '添加參數',
+    'LBL_COMPUTEFIELD_RELATION_PARAMETERS' => '關聯參數',
+    'LBL_COMPUTEFIELD_RELATION' => '關聯',
+    'LBL_COMPUTEFIELD_ADD_RELATION_PARAMETER' => '添加關聯參數',
+    'LBL_COMPUTEFIELD_FORMULA' => '公式',
+    'LBL_COMPUTEFIELD_FORMULAS' => '公式',
+    'LBL_COMPUTEFIELD_ADD_FORMULA' => '添加公式',
+    'LBL_COMPUTEFIELD_VALUE_TYPE' => '數值類型',
+    'LBL_COMPUTEFIELD_RAW_VALUE' => '原始數值',
+    'LBL_COMPUTEFIELD_FORMATTED_VALUE' => '格式化的數值',
+    'LBL_COPY_EMAIL_ADDRESSES_WORKFLOW' => '從 WorkFolw 模組拷貝信箱位址'
+);
