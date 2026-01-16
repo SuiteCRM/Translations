b8a3a483af534f7f76e74c522a03a2ee62de0796
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-16 21:12:53 +0000

    New translations en_us.lang.php (Finnish)

---
+++ translations/fi-FI/modules/EmailTemplates/language/en_us.lang.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ADD_DOCUMENT' => 'Lisää dokumentti',
+    'LBL_ADD_FILE' => 'Lisää tiedosto',
+    'LBL_ATTACHMENTS' => 'Liitteet',
+    'LBL_BODY' => 'Viesti: ',
+    'LBL_CLOSE' => 'Sulje:',
+    'LBL_DESCRIPTION' => 'Kuvaus:',
+    'LBL_EDIT_ALT_TEXT' => 'Muokkaa vain tekstinä',
+    'LBL_EMAIL_ATTACHMENT' => 'Viestin liite',
+    'LBL_HIDE_ALT_TEXT' => 'Piilota pelkkä teksti',
+    'LBL_HTML_BODY' => 'HTML viesti',
+    'LBL_INSERT_VARIABLE' => 'Lisää muuttuja:',
+    'LBL_INSERT_URL_REF' => 'Lisää URL viittaus',
+    'LBL_INSERT_TRACKER_URL' => 'Lisää seurantakoodin URL:',
+    'LBL_INSERT' => 'Lisää',
+    'LBL_LIST_DATE_MODIFIED' => 'Muokattu viimeksi',
+    'LBL_LIST_DESCRIPTION' => 'Kuvaus',
+    'LBL_LIST_FORM_TITLE' => 'Viestipohjat',
+    'LBL_LIST_NAME' => 'Nimi',
+    'LBL_MODULE_NAME' => 'Sähköpostipohjat',
+    'LBL_MODULE_TITLE' => 'Viestipohjat: Etusivu',
+    'LBL_NAME' => 'Nimi:',
+    'LBL_NEW_FORM_TITLE' => 'Luo viestipohja',
+    'LBL_PUBLISH' => 'Julkaise:',
+    'LBL_RELATED_TO' => 'Liittyen:',
+    'LBL_SEARCH_FORM_TITLE' => 'Hae viestipohjista',
+    'LBL_SHOW_ALT_TEXT' => 'Pelkkä teksti',
+    'LBL_SUBJECT' => 'Aihe:',
+    'LBL_SUITE_DOCUMENT' => 'Dokumentti',
+    'LBL_UPLOAD_FILE' => 'Upload File',
+    'LBL_TEXT_BODY' => 'Tekstirunko',
+    'LBL_USERS' => 'Käyttäjät',
+
+    'LNK_EMAIL_TEMPLATE_LIST' => 'Viestipohjat',
+    'LNK_IMPORT_NOTES' => 'Tuo muistioita',
+    'LNK_NEW_EMAIL_TEMPLATE' => 'Luo viestipohja',
+    'LNK_NEW_EMAIL' => 'Arkistoi sähköposti',
+    'LNK_NEW_SEND_EMAIL' => 'Luo sähköposti',
+    'LNK_SENT_EMAIL_LIST' => 'Lähetetyt viestit',
+    'LNK_VIEW_CALENDAR' => 'Tänään',
+    // for Inbox
+    'LBL_NEW' => 'Uusi',
+    'LNK_MY_DRAFTS' => 'Luonnokset',
+    'LNK_MY_INBOX' => 'Saapuneet',
+    'LBL_TEXT_ONLY' => 'Pelkkä teksti',
+    'LBL_SEND_AS_TEXT' => 'Lähetä pelkkänä tekstinä',
+    'LBL_ACCOUNT' => 'Asiakas',
+    'LBL_FROM_NAME' => 'Lähettäjä',
+    'LBL_PLAIN_TEXT' => 'Pelkkä teksti',
+    'LBL_CREATED_BY' => 'Tekijä',
+    'LBL_PUBLISHED' => 'Julkaistu',
+    'LNK_VIEW_MY_INBOX' => 'Saapuneet viestit',
+    'LBL_ASSIGNED_TO_ID' => 'Vastuuhenkilö',
+    'LBL_EDIT_LAYOUT' => 'Muuta layoutia' /*for 508 compliance fix*/,
+    'LBL_SELECT' => 'Valitse' /*for 508 compliance fix*/,
+    'LBL_ID_FF_CLEAR' => 'Tyhjennä' /*for 508 compliance fix*/,
+    'LBL_TYPE' => 'Tyyppi',
+    'LBL_WIDTH' => 'Leveyden oletusarvo',
+    'LNK_IMPORT_CAMPAIGNS' => 'Tuo kampanja',
+);
