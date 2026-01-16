3e5cff7dcfa4eddbd8820b932c460abf45e382ce
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-16 20:20:36 +0000

    New translations en_us.lang.php (Italian)

---
+++ translations/it-IT/modules/AOS_PDF_Templates/language/en_us.lang.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID' => 'Id utente assegnato',
+    'LBL_ASSIGNED_TO_NAME' => 'Assegnato a',
+    'LBL_ID' => 'ID',
+    'LBL_DATE_ENTERED' => 'Data di Creazione',
+    'LBL_DATE_MODIFIED' => 'Data Modifica',
+    'LBL_MODIFIED' => 'Modificato da',
+    'LBL_MODIFIED_NAME' => 'Modificato da Nome',
+    'LBL_CREATED' => 'Creato da',
+    'LBL_DESCRIPTION' => 'Corpo',
+    'LBL_HEADER' => 'Testata',
+    'LBL_FOOTER' => 'Piè di pagina',
+    'LBL_DELETED' => 'Eliminato',
+    'LBL_NAME' => 'Nome',
+    'LBL_CREATED_USER' => 'Creato dall\'utente',
+    'LBL_MODIFIED_USER' => 'Modificato dall\'utente',
+    'LBL_LIST_FORM_TITLE' => 'Elenco modelli PDF',
+    'LBL_MODULE_NAME' => 'Modelli PDF',
+    'LBL_MODULE_TITLE' => 'Modelli PDF: Home',
+    'LBL_HOMEPAGE_TITLE' => 'I miei modelli PDF',
+    'LNK_NEW_RECORD' => 'Nuovo modello PDF',
+    'LNK_LIST' => 'Modelli PDF',
+    'LBL_SEARCH_FORM_TITLE' => 'Cerca modelli PDF',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Visualizza Cronologia',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Attività',
+    'LBL_NEW_FORM_TITLE' => 'Nuovo modello PDF',
+    'LBL_TYPE' => 'Tipo',
+    'LBL_ACTIVE' => 'Attivo',
+    'LBL_BUTTON_INSERT' => 'Inserisci',
+    'LBL_WARNING_OVERWRITE' => 'Attenzione questa azione sovrascriverà il lavoro attuale',
+    'LBL_INSERT_FIELDS' => 'Inserisci campi',
+
+    'LBL_SAMPLE' => 'Carica esempio',
+    'LBL_PAGE' => 'Pagina',
+    'LBL_PREPARED_FOR' => 'Preparato Per',
+    'LBL_PREPARED_BY' => 'Preparato Da',
+    'LBL_QUOTE_SAMPLE' => 'Esempio di Offerta',
+    'LBL_INVOICE_SAMPLE' => 'Esempio di Fattura',
+    'LBL_ACCOUNT_SAMPLE' => 'Esempio di Account',
+    'LBL_CONTACT_SAMPLE' => 'Esempio di Contatto',
+    'LBL_LEAD_SAMPLE' => 'Esempio di Opportunità',
+    'LBL_ANY_STREET' => 'Qualsiasi indirizzo',
+    'LBL_ANY_TOWN' => 'Qualsiasi Città',
+    'LBL_ANY_WHERE' => 'Ovunque',
+
+    'LBL_QUOTE_GROUP_SAMPLE' => 'Esempio di gruppo di Offerta',
+    'LBL_INVOICE_GROUP_SAMPLE' => 'Esempio di gruppo Fattura',
+    'LBL_MARGIN_LEFT' => 'Margine Sinistro',
+    'LBL_MARGIN_RIGHT' => 'Margine Destro',
+    'LBL_MARGIN_TOP' => 'Margine Alto',
+    'LBL_MARGIN_BOTTOM' => 'Margine Basso',
+    'LBL_MARGIN_HEADER' => 'Margine Intestazione',
+    'LBL_MARGIN_FOOTER' => 'Margine Piè di Pagina',
+    'LBL_EDITVIEW_PANEL1' => 'Margini',
+    'LBL_DETAILVIEW_PANEL1' => 'Margini',
+    'LBL_PAGE_SIZE' => 'Dimensioni pagina',
+    'LBL_ORIENTATION' => 'Orientamento',
+);
