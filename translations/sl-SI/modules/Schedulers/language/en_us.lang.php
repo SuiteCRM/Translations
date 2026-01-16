3b59ab54a87c51843ee7596a00d619c2cd1230b7
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-16 21:48:34 +0000

    New translations en_us.lang.php (Slovenian)

---
+++ translations/sl-SI/modules/Schedulers/language/en_us.lang.php
@@ -0,0 +1,164 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+// OOTB Scheduler Job Names:
+    'LBL_OOTB_WORKFLOW' => 'Procesiraj Naloge delovnega toka',
+    'LBL_OOTB_REPORTS' => 'Poženi generiranje poročila o načrtovanih nalogah',
+    'LBL_OOTB_IE' => 'Preveri prihajujoče E-nabiralnike',
+    'LBL_OOTB_BOUNCE' => 'Poženi nočni proces odbitih (bounced) kampanjskih mailov',
+    'LBL_OOTB_CAMPAIGN' => 'Poženi nočne masovne Email kampanje',
+    'LBL_OOTB_PRUNE' => 'Očisti bazo podatkov 1. v mesecu',
+    'LBL_OOTB_TRACKER' => 'Prune tracker tabelo',
+    'LBL_OOTB_SUITEFEEDS' => 'Prune SuiteCRM Feed Tables',
+    'LBL_OOTB_SEND_EMAIL_REMINDERS' => 'Run Email Reminder Notifications',
+    'LBL_OOTB_CLEANUP_QUEUE' => 'Clean Jobs Queue',
+    'LBL_OOTB_REMOVE_DOCUMENTS_FROM_FS' => 'Removal of documents from filesystem',
+    'LBL_OOTB_GOOGLE_CAL_SYNC' => 'Google Calendar Sync',
+    'LBL_OOTB_ELASTIC_INDEX' => 'Perform Elasticsearch Index',
+
+// List Labels
+    'LBL_LIST_JOB_INTERVAL' => 'Interval:',
+    'LBL_LIST_LIST_ORDER' => 'Urniki:',
+    'LBL_LIST_NAME' => 'Urnik:',
+    'LBL_LIST_RANGE' => 'Obseg:',
+    'LBL_LIST_STATUS' => 'Stanje:',
+    'LBL_LIST_TITLE' => 'Seznam urnika:',
+// human readable:
+    'LBL_SUN' => 'Nedelja',
+    'LBL_MON' => 'Ponedeljek',
+    'LBL_TUE' => 'Torek',
+    'LBL_WED' => 'Sreda',
+    'LBL_THU' => 'Četrtek',
+    'LBL_FRI' => 'Petek',
+    'LBL_SAT' => 'Sobota',
+    'LBL_ALL' => 'Vsak dan',
+    'LBL_EVERY' => 'Vsak',
+    'LBL_FROM' => 'Od',
+    'LBL_ON_THE' => 'na ',
+    'LBL_RANGE' => 'naslovnik',
+    'LBL_AND' => 'in',
+    'LBL_MINUTES' => 'minute ',
+    'LBL_HOUR' => 'ure',
+    'LBL_HOUR_SING' => 'hour',
+    'LBL_OFTEN' => 'Čim pogosteje.',
+    'LBL_MIN_MARK' => 'minutna oznaka',
+
+
+// crontabs
+    'LBL_MINS' => 'min',
+    'LBL_HOURS' => 'ure',
+    'LBL_DAY_OF_MONTH' => 'datum',
+    'LBL_MONTHS' => 'mes.',
+    'LBL_DAY_OF_WEEK' => 'dan',
+    'LBL_CRONTAB_EXAMPLES' => 'The above uses standard crontab notation.',
+// Labels
+    'LBL_ALWAYS' => 'Vedno',
+    'LBL_CATCH_UP' => 'Execute If Missed',
+    'LBL_CATCH_UP_WARNING' => 'Uncheck if this job may take more than a moment to run.',
+    'LBL_DATE_TIME_END' => 'Date & Time End',
+    'LBL_DATE_TIME_START' => 'Date & Time Start',
+    'LBL_INTERVAL' => 'Interval',
+    'LBL_JOB' => 'Job',
+    'LBL_JOB_URL' => 'Job URL',
+    'LBL_LAST_RUN' => 'Last Successful Run',
+    'LBL_MODULE_NAME' => 'SuiteCRM Scheduler',
+    'LBL_MODULE_TITLE' => 'Urniki',
+    'LBL_NAME' => 'Ime opravila',
+    'LBL_NEVER' => 'Nikoli',
+    'LBL_NEW_FORM_TITLE' => 'New Schedule',
+    'LBL_PERENNIAL' => 'perpetual',
+    'LBL_SEARCH_FORM_TITLE' => 'Scheduler Search',
+    'LBL_SCHEDULER' => 'Urnik:',
+    'LBL_STATUS' => 'Stanje',
+    'LBL_TIME_FROM' => 'Active From',
+    'LBL_TIME_TO' => 'Active To',
+    'LBL_WARN_CURL_TITLE' => 'cURL opozorilo:',
+    'LBL_WARN_CURL' => 'Opozorilo:',
+    'LBL_WARN_NO_CURL' => 'This system does not have the cURL libraries enabled/compiled into the PHP module (--with-curl=/path/to/curl_library). Please contact your administrator to resolve this issue. Without the cURL functionality, the Scheduler cannot thread its jobs.',
+    'LBL_BASIC_OPTIONS' => 'Osnovne nastavitve',
+    'LBL_ADV_OPTIONS' => 'Advanced Options',
+    'LBL_TOGGLE_ADV' => 'Show Advanced Options',
+    'LBL_TOGGLE_BASIC' => 'Show Basic Options',
+// Links
+    'LNK_LIST_SCHEDULER' => 'Urniki',
+    'LNK_NEW_SCHEDULER' => 'Create Scheduler',
+// Messages
+    'ERR_CRON_SYNTAX' => 'Napačna Cron sintaksa',
+    'NTC_LIST_ORDER' => 'Set the order this schedule will appear in the Scheduler dropdown lists',
+    'LBL_CRON_INSTRUCTIONS_WINDOWS' => 'To Setup Windows Scheduler',
+    'LBL_CRON_INSTRUCTIONS_LINUX' => 'To Setup Crontab',
+    'LBL_CRON_LINUX_DESC1' => 'In order to run SuiteCRM Schedulers, edit your web server user\'s crontab file with this command:',
+    'LBL_CRON_LINUX_DESC2' => '... and add the following line to the crontab file:',
+    'LBL_CRON_LINUX_DESC3' => 'You should do this only after the installation is concluded.',
+    'LBL_CRON_WINDOWS_DESC' => 'In order to run the SuiteCRM schedulers, create a batch file to run using Windows Scheduled Tasks. The batch file should include the following commands:',
+// Subpanels
+    'LBL_JOBS_SUBPANEL_TITLE' => 'Job Log',
+    'LBL_EXECUTE_TIME' => 'Čas izvajanja',
+
+//jobstrings
+    'LBL_REFRESHJOBS' => 'Refresh Jobs',
+    'LBL_POLLMONITOREDINBOXES' => 'Check Inbound Mail Accounts',
+    'LBL_PERFORMFULLFTSINDEX' => 'Full-text Search Index System',
+
+    'LBL_RUNMASSEMAILCAMPAIGN' => 'Poženi nočne masovne Email kampanje',
+    'LBL_POLLMONITOREDINBOXESFORBOUNCEDCAMPAIGNEMAILS' => 'Poženi nočni proces odbitih (bounced) kampanjskih mailov',
+    'LBL_PRUNEDATABASE' => 'Očisti bazo podatkov 1. v mesecu',
+    'LBL_TRIMTRACKER' => 'Prune tracker tabelo',
+    'LBL_TRIMSUGARFEEDS' => 'Prune SuiteCRM Feed Tables',
+    'LBL_SENDEMAILREMINDERS' => 'Run Email Reminders Sending',
+    'LBL_CLEANJOBQUEUE' => 'Cleanup Job Queue',
+    'LBL_REMOVEDOCUMENTSFROMFS' => 'Removal of documents from filesystem',
+
+    'LBL_POLLMONITOREDINBOXESAOP' => 'AOP Poll Monitored Inboxes',
+    'LBL_AORRUNSCHEDULEDREPORTS' => 'Run scheduled reports',
+    'LBL_PROCESSAOW_WORKFLOW' => 'Process AOW Workflow',
+
+    'LBL_RUNELASTICSEARCHINDEXERSCHEDULER' => 'Elasticsearch indexer',
+
+    'LBL_SCHEDULER_TIMES' => 'Scheduler Times',
+    'LBL_SYNCGOOGLECALENDAR' => 'Sync Google Calendars',
+);
+
+global $sugar_config;
