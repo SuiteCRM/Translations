2316adba5ee43bd842786d0bdf038a7d4ef23834
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-16 20:26:17 +0000

    New translations en_us.lang.php (Hungarian)

---
+++ translations/hu-HU/modules/AOW_Actions/language/en_us.lang.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ID' => 'ID',
+    'LBL_DATE_ENTERED' => 'Létrehozás dátuma',
+    'LBL_DATE_MODIFIED' => 'Módosítás dátuma',
+    'LBL_MODIFIED' => 'Módosító',
+    'LBL_MODIFIED_ID' => 'Módosító Id',
+    'LBL_MODIFIED_NAME' => 'Módosító név',
+    'LBL_CREATED_USER' => 'Felhasználó által létrehozva',
+    'LBL_MODIFIED_USER' => 'Felhasználó által módosítva',
+    'LBL_CREATED' => 'Létrehozta',
+    'LBL_DESCRIPTION' => 'Leírás',
+    'LBL_CREATED_ID' => 'Készítette: Id',
+    'LBL_DELETED' => 'Törölve',
+    'LBL_NAME' => 'Név',
+    'LBL_MODULE_NAME' => 'Munkafolyamat események',
+    'LBL_MODULE_TITLE' => 'Munkafolyamat események',
+    'LBL_AOW_WORKFLOW_ID' => 'AOW_WorkFlow azonosítója',
+    'LBL_ACTION' => 'Művelet',
+    'LBL_PARAMETERS' => 'Paraméterek',
+    'LBL_SELECT_ACTION' => 'Válasszon műveletet',
+    'LBL_CREATE_EMAIL_TEMPLATE' => 'Létrehozás',
+    'LBL_RECORD_TYPE' => 'Rekord típusa',
+    'LBL_SENDEMAIL' => 'Email küldése',
+    'LBL_CREATERECORD' => 'Rekord létrehozása',
+    'LBL_MODIFYRECORD' => 'Rekord módosítása',
+    'LBL_ADD_FIELD' => 'Mező hozzáadása',
+    'LBL_ADD_RELATIONSHIP' => 'Kapcsolat hozzáadása',
+    'LBL_EDIT_EMAIL_TEMPLATE' => 'Szerkesztés',
+    'LBL_EMAIL' => 'Email',
+    'LBL_EMAIL_TEMPLATE' => 'Email sablom',
+    'LBL_SETAPPROVAL' => 'Jóváhagyás beállítása',
+    'LBL_RELATE_WORKFLOW' => 'Munkafolyamat modul kapcsolat',
+    'LBL_INDIVIDUAL_EMAILS' => 'Egyéni emailek kiküldése',
+    'LBL_COMPUTEFIELD' => 'Mezők kiszámítása',
+    'LBL_COMPUTEFIELD_PARAMETERS' => 'Paraméterek',
+    'LBL_COMPUTEFIELD_FIELD_NAME' => 'Mező neve',
+    'LBL_COMPUTEFIELD_IDENTIFIER' => 'Azonosító',
+    'LBL_COMPUTEFIELD_ADD_PARAMETER' => 'Paraméter hozzáadása',
+    'LBL_COMPUTEFIELD_RELATION_PARAMETERS' => 'Relációs paraméterek',
+    'LBL_COMPUTEFIELD_RELATION' => 'Kapcsolat',
+    'LBL_COMPUTEFIELD_ADD_RELATION_PARAMETER' => 'Kapcsolat paraméter hozzárendelése',
+    'LBL_COMPUTEFIELD_FORMULA' => 'Szabály',
+    'LBL_COMPUTEFIELD_FORMULAS' => 'Képletek',
+    'LBL_COMPUTEFIELD_ADD_FORMULA' => 'Képlet hozzáadása',
+    'LBL_COMPUTEFIELD_VALUE_TYPE' => 'Érték típusa',
+    'LBL_COMPUTEFIELD_RAW_VALUE' => 'Nyers érték',
+    'LBL_COMPUTEFIELD_FORMATTED_VALUE' => 'Formázott érték',
+    'LBL_COPY_EMAIL_ADDRESSES_WORKFLOW' => 'E-mailek átmásolása a WorkFlow modulból'
+);
