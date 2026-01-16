56dabca9e112cf8a45881048de45c0615b9f406a
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-16 21:25:43 +0000

    New translations en_us.lang.php (Romanian)

---
+++ translations/ro-RO/modules/ProspectLists/language/en_us.lang.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_MODULE_NAME' => 'Liste de ținte',
+    'LBL_MODULE_ID' => 'Liste de ținte',
+    'LBL_MODULE_TITLE' => 'Asocieri: Start',
+    'LBL_SEARCH_FORM_TITLE' => 'Caută asocieri',
+    'LBL_LIST_FORM_TITLE' => 'Liste de ținte',
+    'LBL_PROSPECT_LIST_NAME' => 'Nume:',
+    'LBL_NAME' => 'Nume',
+    'LBL_ENTRIES' => 'Total Intrări',
+    'LBL_LIST_PROSPECT_LIST_NAME' => 'Listă ținte',
+    'LBL_LIST_ENTRIES' => 'Număr de intrări',
+    'LBL_LIST_DESCRIPTION' => 'Descriere',
+    'LBL_LIST_TYPE_NO' => 'Tip',
+    'LBL_LIST_END_DATE' => 'Dată Sfârșit',
+    'LBL_DATE_ENTERED' => 'Data creării',
+    'LBL_MARKETING_ID' => 'Id marketing',
+    'LBL_DATE_MODIFIED' => 'Data modificării',
+    'LBL_MODIFIED' => 'Modificat de',
+    'LBL_CREATED' => 'Creat de',
+    'LBL_ASSIGNED_TO' => 'Atribuit lui',
+    'LBL_DESCRIPTION' => 'Descriere',
+    'LNK_NEW_CAMPAIGN' => 'Creare campanie',
+    'LNK_CAMPAIGN_LIST' => 'Campanii',
+    'LNK_NEW_PROSPECT_LIST' => 'Creează listă de ținte',
+    'LNK_PROSPECT_LIST_LIST' => 'Vezi lista ținte',
+    'LBL_MODIFIED_BY' => 'Modificat de',
+    'LBL_CREATED_BY' => 'Creat de',
+    'LNK_NEW_PROSPECT' => 'Creare țintă',
+    'LNK_PROSPECT_LIST' => 'Ținte',
+
+    'LBL_CONTACTS_SUBPANEL_TITLE' => 'Contacte',
+    'LBL_LEADS_SUBPANEL_TITLE' => 'Piste',
+    'LBL_PROSPECTS_SUBPANEL_TITLE' => 'Ținte',
+    'LBL_ACCOUNTS_SUBPANEL_TITLE' => 'Conturi',
+    'LBL_COPY_PREFIX' => 'Copie a',
+    'LBL_USERS_SUBPANEL_TITLE' => 'Utilizatori',
+    'LBL_TYPE' => 'Tip',
+    'LBL_LIST_TYPE' => 'Tip',
+    'LBL_LIST_TYPE_LIST_NAME' => 'Tip',
+    'LBL_NEW_FORM_TITLE' => 'Nouă asociere',
+    'LBL_MARKETING_NAME' => 'Nume Marketing',
+    'LBL_DOMAIN_NAME' => 'Nume domeniu',
+    'LBL_DOMAIN' => 'Fără e-mailuri către domeniu',
+    'LBL_LIST_PROSPECTLIST_NAME' => 'Nume',
+
+    'LBL_EMAIL_MARKETING' => 'Marketing prin Email',
+
+    'LBL_ASCENDING' => 'Ascendent',
+    'LBL_DESCENDING' => 'Descendent',
+);
