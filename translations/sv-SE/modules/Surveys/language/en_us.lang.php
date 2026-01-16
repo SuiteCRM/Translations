5d251f4ab26b10892c68d763c4ddb45b590cf21d
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-16 21:49:47 +0000

    New translations en_us.lang.php (Swedish)

---
+++ translations/sv-SE/modules/Surveys/language/en_us.lang.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID'                                     => 'Tilldelat användar-id',
+    'LBL_ADD_OPTION'                                         => 'Add new option',
+    'LBL_ASSIGNED_TO_NAME'                                   => 'Tilldelad till',
+    'LBL_SECURITYGROUPS'                                     => 'Säkerhetsgrupper',
+    'LBL_SECURITYGROUPS_SUBPANEL_TITLE'                      => 'Säkerhetsgrupper',
+    'LBL_ID'                                                 => 'ID',
+    'LBL_DATE_ENTERED'                                       => 'Datum Skapad',
+    'LBL_DATE_MODIFIED'                                      => 'Datum Ändrad',
+    'LBL_MODIFIED'                                           => 'Ändrad av',
+    'LBL_MODIFIED_ID'                                        => 'Ändrad Av Id',
+    'LBL_MODIFIED_NAME'                                      => 'Ändrad av Namn',
+    'LBL_CREATED'                                            => 'Skapad av',
+    'LBL_CREATED_ID'                                         => 'Skapad Av Id',
+    'LBL_DESCRIPTION'                                        => 'Beskrivning',
+    'LBL_DELETED'                                            => 'Raderad',
+    'LBL_NAME'                                               => 'Namn',
+    'LBL_CREATED_USER'                                       => 'Skapad av Användare',
+    'LBL_MODIFIED_USER'                                      => 'Ändrad av Användare',
+    'LBL_LIST_NAME'                                          => 'Namn',
+    'LBL_EDIT_BUTTON'                                        => 'Redigera',
+    'LBL_REMOVE'                                             => 'Ta bort',
+    'LBL_LIST_FORM_TITLE'                                    => 'Enkäter',
+    'LBL_MODULE_NAME'                                        => 'Enkäter',
+    'LBL_MODULE_TITLE'                                       => 'Enkäter',
+    'LBL_HOMEPAGE_TITLE'                                     => 'Mina enkäter',
+    'LNK_NEW_RECORD'                                         => 'Skapa enkät',
+    'LNK_LIST'                                               => 'Visa enkäter',
+    'LNK_IMPORT_SURVEYS'                                     => 'Importera enkäter',
+    'LBL_SEARCH_FORM_TITLE'                                  => 'Sök enkäter',
+    'LBL_HISTORY_SUBPANEL_TITLE'                             => 'Visa historik',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE'                          => 'Aktiviteter',
+    'LBL_SURVEYS_SUBPANEL_TITLE'                             => 'Enkäter',
+    'LBL_NEW_FORM_TITLE'                                     => 'Nya enkäter',
+    'LBL_STATUS'                                             => 'Status',
+    'LBL_SURVEY_QUESTIONS_DISPLAY'                           => 'Frågor',
+    'LBL_SURVEY_URL_DISPLAY'                                 => 'URL:',
+    'LBL_HAPPINESS_QUESTION'                                 => 'Happiness Question',
+    'LBL_CANT_EDIT_RESPONDED'                                => 'Survey questions with responses cannot be edited',
+    'LBL_VIEW_SURVEY_REPORTS'                                => 'View Survey Reports',
+    'LBL_CHECKED'                                            => 'Markerad',
+    'LBL_UNCHECKED'                                          => 'Avmarkerad',
+    'LBL_RESPONSE_ANSWER'                                    => 'Svar',
+    'LBL_RESPONSE_CONTACT'                                   => 'Kontakter',
+    'LBL_RESPONSE_TIME'                                      => 'Datum',
+    'LBL_UNKNOWN_CONTACT'                                    => 'Okänd',
+    'LBL_RESPONSE_COUNT'                                     => 'Räkna',
+    'LBL_SUBMIT_TEXT'                                        => 'Skicka inlägg',
+    'LBL_SATISFIED_TEXT'                                     => 'Satisfied Text',
+    'LBL_NEITHER_TEXT'                                       => 'Neither Text',
+    'LBL_DISSATISFIED_TEXT'                                  => 'Dissatisfied Text',
+    'LBL_SURVEYS_SURVEYQUESTIONS_FROM_SURVEYQUESTIONS_TITLE' => 'Undersökningsfrågor',
+    'LBL_SURVEYS_SURVEYRESPONSES_FROM_SURVEYRESPONSES_TITLE' => 'Svar från undersökning',
+    'LBL_SHOW_RESPONSES' => 'Show responses',
+    'LBL_HIDE_RESPONSES' => 'Hide responses',
+    'LNK_LIST_SURVEY_RESPONSES' => 'Show Survey Responses',
+    'LBL_SURVEY_QUESTION' => 'Question',
+    'LBL_SURVEY_TEXT' => 'Text',
+    'LBL_SURVEY_TYPE' => 'Typ',
+    'LBL_SURVEY_ACTIONS' => 'Händelser',
+    'LBL_SURVEY_NEW_QUESTION' => 'Ny fråga',
+    'LBL_SURVEY_RESPONSES' => 'Ny fråga',
+    'LBL_SURVEYS_SENT' => 'Surveys Sent:',
+    'LBL_SURVEY_DISTINCT' => 'Distinct Surveys Sent:',
+    'LBL_DRAFT' => 'Utkast',
+    'LBL_SUBMIT' => 'Lägg till',
+    'LBL_STARS' => 'Stjärnor',
+    'LBL_SURVEY_CLOSE_RESPONSE' => 'Thanks for your interest but this survey is now closed.',
+);
