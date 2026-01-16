81345d989d085396fbba4c3ef0f302ec51f15216
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-16 20:04:13 +0000

    New translations en_us.lang.php (Polish)

---
+++ translations/pl-PL/modules/AM_TaskTemplates/language/en_us.lang.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID' => 'ID przydzielonego użytkownika',
+    'LBL_ASSIGNED_TO_NAME' => 'Przypisano do',
+    'LBL_ID' => 'ID',
+    'LBL_DATE_ENTERED' => 'Data utworzenia',
+    'LBL_DATE_MODIFIED' => 'Data Modyfikacji',
+    'LBL_MODIFIED' => 'Zmodyfikowane przez',
+    'LBL_MODIFIED_ID' => 'Zmodyfikowane przez ID',
+    'LBL_MODIFIED_NAME' => 'Nazwa użytkownika modyfikującego',
+    'LBL_CREATED' => 'Utworzone przez',
+    'LBL_CREATED_ID' => 'ID tworzącego',
+    'LBL_DESCRIPTION' => 'Opis',
+    'LBL_DELETED' => 'Usunięte',
+    'LBL_NAME' => 'Nazwa Etapu',
+    'LBL_CREATED_USER' => 'Utworzony przez Użytkownika',
+    'LBL_MODIFIED_USER' => 'Zmodyfikowany przez Użytkownika',
+    'LBL_LIST_NAME' => 'Nazwa',
+    'LBL_EDIT_BUTTON' => 'Edytuj',
+    'LBL_REMOVE' => 'Usuń',
+    'LBL_LIST_FORM_TITLE' => 'Lista szablonów Zadań Projektu',
+    'LBL_MODULE_NAME' => 'Szablony Zadań Projektu',
+    'LBL_MODULE_TITLE' => 'Szablony Zadań Projektu',
+    'LBL_HOMEPAGE_TITLE' => 'Moje szablony zadań projektu',
+    'LNK_NEW_RECORD' => 'Utwórz szablony Zadań Projektu',
+    'LNK_LIST' => 'Przeglądaj Szablony Zadań Projektu',
+    'LNK_IMPORT_AM_TASKTEMPLATES' => 'Importuj Szablony Zadań Projektu',
+    'LBL_SEARCH_FORM_TITLE' => 'Szukaj szablonów Zadań Projektu',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Przeglądaj historię',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Działania',
+    'LBL_AM_TASKTEMPLATES_SUBPANEL_TITLE' => 'Szablony Zadań Projektu',
+    'LBL_NEW_FORM_TITLE' => 'Nowe szablony Zadań Projektu',
+    'LBL_STATUS' => 'Status',
+    'LBL_PRIORITY' => 'Priorytet',
+    'LBL_PERCENT_COMPLETE' => 'Procent wykonania',
+    'LBL_PREDECESSORS' => 'Poprzednie',
+    'LBL_MILESTONE_FLAG' => 'Flaga kamienia milowego',
+    'LBL_RELATIONSHIP_TYPE' => 'Typ zależności',
+    'LBL_TASK_NUMBER' => 'Numer zadania',
+    'LBL_ORDER_NUMBER' => 'Zamówienie ',
+    'LBL_ESTIMATED_EFFORT' => 'Szacunkowy nakład (godz.)',
+    'LBL_UTILIZATION' => 'Wykorzystanie (%)',
+    'LBL_DURATION' => 'Czas trwania (dni)',
+    'LBL_AM_TASKTEMPLATES_AM_PROJECTTEMPLATES_FROM_AM_PROJECTTEMPLATES_TITLE' => 'Szablony Projektu',
+
+    'LBL_AM_TASKTEMPLATES_AM_PROJECTTEMPLATES_FROM_AM_TASKTEMPLATES_TITLE' => 'Szablony zadań: Szablony projektu z Tytułu szablonu zadań',
+);
