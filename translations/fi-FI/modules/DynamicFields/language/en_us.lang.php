94d0f0a2fb94ea4e0d6ad43e00c4c739cd106c51
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-16 21:07:04 +0000

    New translations en_us.lang.php (Finnish)

---
+++ translations/fi-FI/modules/DynamicFields/language/en_us.lang.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LNK_NEW_CALL' => 'Kirjaa puhelu',
+    'LNK_NEW_MEETING' => 'Ajoita tapaaminen',
+    'LNK_NEW_TASK' => 'Luo tehtävä',
+    'LNK_NEW_NOTE' => 'Luo muistio tai liite',
+    'LNK_NEW_EMAIL' => 'Arkistoi sähköposti',
+    'LNK_CALL_LIST' => 'Puhelut',
+    'LNK_MEETING_LIST' => 'Tapaamiset',
+    'LNK_TASK_LIST' => 'Tehtävät',
+    'LNK_NOTE_LIST' => 'Muistiot',
+    'LBL_ADD_FIELD' => 'Lisää kenttä:',
+    'LBL_SEARCH_FORM_TITLE' => 'Moduulihaku',
+    'COLUMN_TITLE_NAME' => 'Kentän nimi',
+    'COLUMN_TITLE_DISPLAY_LABEL' => 'Näytä otsikko',
+    'COLUMN_TITLE_LABEL_VALUE' => 'Otsikon arvo',
+    'COLUMN_TITLE_LABEL' => 'System Label',
+    'COLUMN_TITLE_DATA_TYPE' => 'Tietotyyppi',
+    'COLUMN_TITLE_MAX_SIZE' => 'Maksimi koko',
+    'COLUMN_TITLE_HELP_TEXT' => 'Help Text',
+    'COLUMN_TITLE_COMMENT_TEXT' => 'Comment Text',
+    'COLUMN_TITLE_REQUIRED_OPTION' => 'Required Field',
+    'COLUMN_TITLE_DEFAULT_VALUE' => 'Oletusarvo',
+    'COLUMN_TITLE_FRAME_HEIGHT' => 'IFrame korkeus',
+    'COLUMN_TITLE_HTML_CONTENT' => 'HTML',
+    'COLUMN_TITLE_URL' => 'Oletus URL',
+    'COLUMN_TITLE_AUDIT' => 'Audit',
+    'COLUMN_TITLE_MIN_VALUE' => 'Min Value',
+    'COLUMN_TITLE_MAX_VALUE' => 'Max Value',
+    'COLUMN_TITLE_LABEL_ROWS' => 'Rivit',
+    'COLUMN_TITLE_LABEL_COLS' => 'Sarakkeet',
+    'COLUMN_TITLE_DISPLAYED_ITEM_COUNT' => '# Items displayed',
+    'COLUMN_TITLE_AUTOINC_NEXT' => 'Auto Increment Next Value',
+    'COLUMN_DISABLE_NUMBER_FORMAT' => 'Disable Format',
+    'COLUMN_TITLE_ENABLE_RANGE_SEARCH' => 'Enable Range Search',
+    'LBL_DROP_DOWN_LIST' => 'Drop Down List',
+    'LBL_RADIO_FIELDS' => 'Radio Fields',
+    'LBL_MULTI_SELECT_LIST' => 'Multi Select List',
+    'COLUMN_TITLE_PRECISION' => 'Precision',
+    'LBL_MODULE' => 'Moduuli',
+    'COLUMN_TITLE_MASS_UPDATE' => 'Massapäivitys',
+    'COLUMN_TITLE_IMPORTABLE' => 'Importable',
+    'COLUMN_TITLE_DUPLICATE_MERGE' => 'Duplicate Merge',
+    'LBL_LABEL' => 'Nimike',
+    'LBL_DATA_TYPE' => 'Tietotyyppi',
+    'LBL_DEFAULT_VALUE' => 'Oletusarvo',
+    'ERR_RESERVED_FIELD_NAME' => "Varattu Avainsana",
+    'ERR_SELECT_FIELD_TYPE' => 'Valitse kentän tyyppi',
+    'ERR_FIELD_NAME_ALREADY_EXISTS' => 'Kentän nimi on jo olemassa',
+    'LBL_BTN_ADD' => 'Lisää',
+    'LBL_BTN_EDIT' => 'Muokkaa',
+    'LBL_GENERATE_URL' => 'Luo URL-osoite',
+    'LBL_CALCULATED' => 'Laskettu Arvo',
+    'LBL_LINK_TARGET' => 'Avaa linkki',
+    'LBL_IMAGE_WIDTH' => 'Leveys',
+    'LBL_IMAGE_HEIGHT' => 'Korkeus',
+    'LBL_IMAGE_BORDER' => 'Reuna',
+    'LBL_HELP' => 'Apua' /*for 508 compliance fix*/,
+    'COLUMN_TITLE_INLINE_EDIT_TEXT' => 'Inline Edit',
+    'COLUMN_TITLE_PARENT_ENUM' => 'Ylempi pudotusvalikko',
+    'LBL_INSERT_FIELD' => 'Insert Field',
+);
