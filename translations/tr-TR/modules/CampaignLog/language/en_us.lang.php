5de77aee6e46381bfb45a1bb12f40ff404058ea3
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-16 20:42:44 +0000

    New translations en_us.lang.php (Turkish)

---
+++ translations/tr-TR/modules/CampaignLog/language/en_us.lang.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_LIST_ID' => 'Aday listesi kodu',
+    'LBL_ID' => 'Kod',
+    'LBL_TARGET_TRACKER_KEY' => 'Hedef izleyici anahtarı',
+    'LBL_TARGET_ID' => 'Hedef kodu',
+    'LBL_TARGET_TYPE' => 'Hedef türü',
+    'LBL_ACTIVITY_TYPE' => 'İşlem türü',
+    'LBL_ACTIVITY_DATE' => 'İşlem tarihi',
+    'LBL_RELATED_ID' => 'İlişkili öge kodu',
+    'LBL_RELATED_TYPE' => 'İlişkili öge türü',
+    'LBL_DELETED' => 'Silindi',
+    'LBL_MODULE_NAME' => 'Kampanya günlüğü',
+    'LBL_LIST_RECIPIENT_EMAIL' => 'Alıcı e-postası',
+    'LBL_LIST_RECIPIENT_NAME' => 'Alıcı adı',
+    'LBL_ARCHIVED' => 'Arşivlenmiş',
+    'LBL_HITS' => 'Tıklanma',
+
+    'LBL_CAMPAIGN_NAME' => 'Ad:',
+    'LBL_CAMPAIGN' => 'Kampanya:',
+    'LBL_NAME' => 'Ad: ',
+    'LBL_INVITEE' => 'Kişiler',
+    'LBL_LIST_CAMPAIGN_NAME' => 'Kampanya',
+    'LBL_LIST_STATUS' => 'Durum',
+    'LBL_LIST_TYPE' => 'Tür',
+    'LBL_LIST_END_DATE' => 'Bitiş tarihi',
+    'LBL_DATE_ENTERED' => 'Yazılma tarihi',
+    'LBL_DATE_MODIFIED' => 'Değiştirilme tarihi',
+    'LBL_MODIFIED' => 'Değiştiren: ',
+    'LBL_CREATED' => 'Ekleyen: ',
+    'LBL_TEAM' => 'Takım: ',
+    'LBL_ASSIGNED_TO' => 'Atanmış kişi: ',
+    'LBL_CAMPAIGN_START_DATE' => 'Başlangıç tarihi: ',
+    'LBL_CAMPAIGN_END_DATE' => 'Bitiş tarihi: ',
+    'LBL_CAMPAIGN_STATUS' => 'Durum: ',
+    'LBL_CAMPAIGN_BUDGET' => 'Bütçe: ',
+    'LBL_CAMPAIGN_EXPECTED_COST' => 'Öngörülen maliyet: ',
+    'LBL_CAMPAIGN_ACTUAL_COST' => 'Gerçek maliyet: ',
+    'LBL_CAMPAIGN_EXPECTED_REVENUE' => 'Öngörülen gelir: ',
+    'LBL_CAMPAIGN_TYPE' => 'Tür: ',
+    'LBL_CAMPAIGN_OBJECTIVE' => 'Amaç: ',
+    'LBL_CAMPAIGN_CONTENT' => 'Açıklama: ',
+    'LBL_CREATED_LEAD' => 'Potansiyel eklendi',
+    'LBL_CREATED_CONTACT' => 'Kişi eklendi',
+    'LBL_CREATED_OPPORTUNITY' => 'Fırsat eklendi',
+    'LBL_TARGETED_USER' => 'Hedeflenen kullanıcı',
+    'LBL_SENT_EMAIL' => 'E-posta gönderildi',
+    'LBL_LIST_FORM_TITLE' => 'Hedef kampanyalar',
+    'LBL_LIST_ACTIVITY_DATE' => 'İşlem tarihi',
+    'LBL_RELATED' => 'İlişkili öge',
+    'LBL_MORE_INFO' => 'Ayrıntılı bilgiler',
+
+    'LBL_CAMPAIGNS' => 'Kampanyalar',
+    'LBL_LIST_MARKETING_NAME' => 'Pazarlama kodu',
+
+    'LBL_MARKETING_ID' => 'Pazarlama kodu',
+    'LBL_RELATED_NAME' => 'İlişkili öge adı',
+);
