94525c3d8ed9048d0412f8de8d71a14157340f70
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-16 21:28:46 +0000

    New translations en_us.lang.php (Arabic)

---
+++ translations/ar-SA/modules/SurveyResponses/language/en_us.lang.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID'                                                             => 'معرف المستخدم المكلف',
+    'LBL_ASSIGNED_TO_NAME'                                                           => 'أُسند إلى',
+    'LBL_SECURITYGROUPS'                                                             => 'المجموعات الأمنية',
+    'LBL_SECURITYGROUPS_SUBPANEL_TITLE'                                              => 'المجموعات الأمنية',
+    'LBL_ID'                                                                         => 'المعرف',
+    'LBL_DATE_ENTERED'                                                               => 'تاريخ الإنشاء',
+    'LBL_DATE_MODIFIED'                                                              => 'تاريخ التعديل',
+    'LBL_MODIFIED'                                                                   => 'عُدل بواسطة',
+    'LBL_MODIFIED_ID'                                                                => 'عدل بواسطة المعرف',
+    'LBL_MODIFIED_NAME'                                                              => 'اسم من قام بالتعديل',
+    'LBL_CREATED'                                                                    => 'أنشئ بواسطة',
+    'LBL_CREATED_ID'                                                                 => 'أنشئ بواسطة المعرف',
+    'LBL_DESCRIPTION'                                                                => 'الوصف',
+    'LBL_DELETED'                                                                    => 'محذوف',
+    'LBL_NAME'                                                                       => 'الاسم',
+    'LBL_CREATED_USER'                                                               => 'أنشئ بواسطة المستخدم',
+    'LBL_MODIFIED_USER'                                                              => 'عدل بواسطة المستخدم',
+    'LBL_LIST_NAME'                                                                  => 'الاسم',
+    'LBL_EDIT_BUTTON'                                                                => 'تحرير',
+    'LBL_REMOVE'                                                                     => 'حذف',
+    'LBL_LIST_FORM_TITLE'                                                            => 'قائمة الردود على الاستطلاع',
+    'LBL_MODULE_NAME'                                                                => 'الردود على الاستطلاع',
+    'LBL_MODULE_TITLE'                                                               => 'الردود على الاستطلاع',
+    'LBL_HOMEPAGE_TITLE'                                                             => 'الردود الخاصة بي على الاستطلاع',
+    'LNK_NEW_RECORD'                                                                 => 'إنشاء الردود الاستطلاع',
+    'LNK_LIST'                                                                       => 'عرض الردود على الاستطلاع',
+    'LNK_IMPORT_SURVEYRESPONSES'                                                     => 'استيراد أجوبة الاستطلاع',
+    'LBL_SEARCH_FORM_TITLE'                                                          => 'بحث ردود الاستطلاع',
+    'LBL_HISTORY_SUBPANEL_TITLE'                                                     => 'عرض التاريخ',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE'                                                  => 'الأنشطة',
+    'LBL_SURVEYRESPONSES_SUBPANEL_TITLE'                                             => 'الردود على الاستطلاع',
+    'LBL_NEW_FORM_TITLE'                                                             => 'أجوبة استطلاع جديدة',
+    'LBL_HAPPINESS'                                                                  => 'السعادة',
+    'LBL_EMAIL_RESPONSE_SENT'                                                        => 'تم إرسال رد البريد الإلكتروني',
+    'LBL_SURVEYRESPONSES_SURVEYQUESTIONRESPONSES_FROM_SURVEYQUESTIONRESPONSES_TITLE' => 'ردود أسئلة الاستطلاع',
+    'LBL_SURVEYRESPONSES_CONTACTS_FROM_CONTACTS_TITLE'                               => 'جهة الإتصال',
+    'LBL_SURVEYRESPONSES_ACCOUNTS_FROM_ACCOUNTS_TITLE'                               => 'الحساب',
+    'LBL_SURVEYS_SURVEYRESPONSES_FROM_SURVEYS_TITLE'                                 => 'الاستطلاع',
+    'LBL_QUESTION_RESPONSES_DISPLAY'                                                 => 'الردود على السؤال',
+    'LBL_SURVEYRESPONSES_CAMPAIGNS_FROM_CAMPAIGNS_TITLE'                             => 'الحملة',
+    'LBL_QUESTION' => 'سؤال',
+    'LBL_RESPONSE' => 'الإستجابة'
+
+);
