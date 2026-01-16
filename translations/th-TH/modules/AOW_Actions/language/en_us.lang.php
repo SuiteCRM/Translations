5563a525861425a9141032121357ac85fef8e416
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-16 20:26:46 +0000

    New translations en_us.lang.php (Thai)

---
+++ translations/th-TH/modules/AOW_Actions/language/en_us.lang.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ID' => 'ไอดี',
+    'LBL_DATE_ENTERED' => 'วันที่สร้าง',
+    'LBL_DATE_MODIFIED' => 'วันที่แก้ไข',
+    'LBL_MODIFIED' => 'แก้ไขโดย',
+    'LBL_MODIFIED_ID' => 'แก้ไขโดยไอดี',
+    'LBL_MODIFIED_NAME' => 'แก้ไขโดยชื่อ',
+    'LBL_CREATED_USER' => 'สร้างโดยผู้ใช้งาน',
+    'LBL_MODIFIED_USER' => 'แก้ไขโดยผู้ใช้',
+    'LBL_CREATED' => 'สร้างโดย',
+    'LBL_DESCRIPTION' => 'รายละเอียด',
+    'LBL_CREATED_ID' => 'สร้างโดยไอดี',
+    'LBL_DELETED' => 'ลบ',
+    'LBL_NAME' => 'ชื่อ',
+    'LBL_MODULE_NAME' => 'การดำเนินการลำดับงาน',
+    'LBL_MODULE_TITLE' => 'การดำเนินการลำดับงาน',
+    'LBL_AOW_WORKFLOW_ID' => 'AOW_WorkFlow Id',
+    'LBL_ACTION' => 'คำสั่ง',
+    'LBL_PARAMETERS' => 'พารามิเตอร์',
+    'LBL_SELECT_ACTION' => 'เลือกการดำเนินการ',
+    'LBL_CREATE_EMAIL_TEMPLATE' => 'สร้าง',
+    'LBL_RECORD_TYPE' => 'ชนิดของเรกคอร์ด',
+    'LBL_SENDEMAIL' => 'ส่งอีเมล',
+    'LBL_CREATERECORD' => 'Create Record',
+    'LBL_MODIFYRECORD' => 'Modify Record',
+    'LBL_ADD_FIELD' => 'เพิ่มเขตข้อมูล',
+    'LBL_ADD_RELATIONSHIP' => 'เพิ่มความสัมพันธ์',
+    'LBL_EDIT_EMAIL_TEMPLATE' => 'แก้ไข',
+    'LBL_EMAIL' => 'อีเมล',
+    'LBL_EMAIL_TEMPLATE' => 'แม่แบบอีเมล',
+    'LBL_SETAPPROVAL' => 'Set Approval',
+    'LBL_RELATE_WORKFLOW' => 'เกี่ยวข้องกับลำดับงานโมดูล',
+    'LBL_INDIVIDUAL_EMAILS' => 'ส่งอีเมลส่วนบุคคล',
+    'LBL_COMPUTEFIELD' => 'คำนวณฟิลด์',
+    'LBL_COMPUTEFIELD_PARAMETERS' => 'พารามิเตอร์',
+    'LBL_COMPUTEFIELD_FIELD_NAME' => 'ชื่อฟิลด์',
+    'LBL_COMPUTEFIELD_IDENTIFIER' => 'แยกแยะ',
+    'LBL_COMPUTEFIELD_ADD_PARAMETER' => 'เพิ่มพารามิเตอร์',
+    'LBL_COMPUTEFIELD_RELATION_PARAMETERS' => 'พารามิเตอร์ความสัมพันธ์',
+    'LBL_COMPUTEFIELD_RELATION' => 'ความสัมพันธ์',
+    'LBL_COMPUTEFIELD_ADD_RELATION_PARAMETER' => 'เพิ่มพารามิเตอร์ความสัมพันธ์',
+    'LBL_COMPUTEFIELD_FORMULA' => 'สูตร',
+    'LBL_COMPUTEFIELD_FORMULAS' => 'สูตร',
+    'LBL_COMPUTEFIELD_ADD_FORMULA' => 'เพิ่มสูตร',
+    'LBL_COMPUTEFIELD_VALUE_TYPE' => 'ค่า ประเภท',
+    'LBL_COMPUTEFIELD_RAW_VALUE' => 'Raw ค่า',
+    'LBL_COMPUTEFIELD_FORMATTED_VALUE' => 'ที่จัดรูปแบบ ค่า',
+    'LBL_COPY_EMAIL_ADDRESSES_WORKFLOW' => 'คัดลอกอีเมลจาก WorkFlow Module'
+);
