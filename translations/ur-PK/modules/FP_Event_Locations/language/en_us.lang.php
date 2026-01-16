6d0f7d956da2cd04c0eee5da827f99f0a6bd1c72
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-16 21:19:08 +0000

    New translations en_us.lang.php (Urdu (Pakistan))

---
+++ translations/ur-PK/modules/FP_Event_Locations/language/en_us.lang.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID' => 'مقرر کردہ صارف کی شناخت',
+    'LBL_ASSIGNED_TO_NAME' => 'کے لئے تفویض',
+    'LBL_ID' => 'شناخت',
+    'LBL_DATE_ENTERED' => 'تاریخ بنائی گئی ہے',
+    'LBL_DATE_MODIFIED' => 'تاریخ میں ترمیم',
+    'LBL_MODIFIED' => 'کی طرف سے ترمیم',
+    'LBL_MODIFIED_NAME' => 'نام کی طرف سے ترمیم',
+    'LBL_CREATED' => 'کی طرف سے بنایا گیا',
+    'LBL_DESCRIPTION' => 'تفصیل',
+    'LBL_DELETED' => 'ختم کر دیا گیا',
+    'LBL_NAME' => 'نام',
+    'LBL_CREATED_USER' => 'صارف کی طرف سے پیدا شدہ',
+    'LBL_MODIFIED_USER' => 'صارف کی طرف سے ترمیم',
+    'LBL_LIST_NAME' => 'نام',
+    'LBL_EDIT_BUTTON' => 'ردوبدل',
+    'LBL_REMOVE' => 'ہٹا دیں',
+    'LBL_LIST_FORM_TITLE' => 'مقامات کی فہرست',
+    'LBL_MODULE_NAME' => 'مقامات',
+    'LBL_MODULE_TITLE' => 'مقامات',
+    'LBL_HOMEPAGE_TITLE' => 'میرے مقامات',
+    'LNK_NEW_RECORD' => 'مقامات بنائیں',
+    'LNK_LIST' => 'مقامات ملاحظہ کریں',
+    'LBL_SEARCH_FORM_TITLE' => 'مقامات تلاش کریں',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'تاریخ دیکھیں',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'سرگرمیاں',
+    'LBL_NEW_FORM_TITLE' => 'نئے مقامات',
+    'LBL_ADDRESS' => 'پتہ',
+    'LBL_ADDRESS_CITY' => 'شہر',
+    'LBL_ADDRESS_COUNTRY' => 'ملک',
+    'LBL_ADDRESS_POSTALCODE' => 'پوسٹ کوڈ',
+    'LBL_ADDRESS_STATE' => 'کاؤنٹی',
+    'LBL_CAPACITY' => 'صلاحیت',
+    'LBL_EDITVIEW_PANEL1' => 'پتہ',
+    'LBL_FP_EVENT_LOCATIONS_FP_EVENTS_1_FROM_FP_EVENTS_TITLE' => 'تقریبات',
+);
