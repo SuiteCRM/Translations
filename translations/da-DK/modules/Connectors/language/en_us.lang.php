7cc26488e296968887143b084f119707fdaa0567
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-16 20:59:52 +0000

    New translations en_us.lang.php (Danish)

---
+++ translations/da-DK/modules/Connectors/language/en_us.lang.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+
+$mod_strings = array(
+
+    'LBL_ADMINISTRATION_MAIN' => 'Forbindelsesindstillinger',
+    'LBL_AVAILABLE' => 'Tilgængelige',
+    'LBL_BACK' => '< Tilbage',
+    'LBL_CONFIRM_CONTINUE_SAVE' => 'Nogle obligatoriske felter er tomme. Vil du fortsætte for at gemme ændringer?',
+    'LBL_CONNECTOR_FIELDS' => 'Forbindelsesfelter',
+    'LBL_DATA' => 'Data',
+    'LBL_DEFAULT' => 'Standard',
+    'LBL_DISABLED' => 'Slukket',
+    'LBL_ENABLED' => 'Tændt',
+    'LBL_EXTERNAL' => 'Giv brugerne mulighed for at oprette eksterne konto-poster til denne connector. For at bruge denne connector skal egenskaber også sættes i "Set Connector egenskaber".',
+    'LBL_EXTERNAL_SET_PROPERTIES' => ' For at bruge denne forbindelse, bør egenskaberne også angives på siden angiv forbindelse egenskaber indstillinger.',
+    'LBL_MERGE' => 'Flet dupletter',
+    'LBL_MODIFY_DISPLAY_TITLE' => 'Aktivér forbindelser',
+    'LBL_MODIFY_DISPLAY_DESC' => 'Vælg, hvilke moduler der skal aktiveres for hver forbindelse.',
+    'LBL_MODULE_FIELDS' => 'Modulfelter',
+    'LBL_MODIFY_MAPPING_TITLE' => 'Tilknyt forbindelsesfelter',
+    'LBL_MODIFY_MAPPING_DESC' => 'Knyt forbindelsesfelter til modulfelter for at bestemme, hvilke forbindelsesdata der kan vises og flettes ind i modulposterne.',
+    'LBL_MODIFY_PROPERTIES_TITLE' => 'Angiv forbindelsesegenskaber',
+    'LBL_MODIFY_PROPERTIES_DESC' => 'Konfigurer egenskaberne for hver forbindelse, herunder URL\'er og API-nøgler.',
+    'LBL_MODIFY_SEARCH_TITLE' => 'Administrer forbindelsessøgning',
+    'LBL_MODIFY_SEARCH' => 'Søg',
+    'LBL_MODIFY_SEARCH_DESC' => 'Vælg, hvilke forbindelsesfelter der skal bruges til at søge efter data for hvert modul.',
+    'LBL_MODULE_NAME' => 'Forbindelser',
+    'LBL_NO_PROPERTIES' => 'Der er ingen konfigurerbare egenskaber til denne forbindelse.',
+    'LBL_SAVE' => 'Gem',
+    'LBL_SUMMARY' => 'Oppsummering',
+    'LBL_STEP1' => 'Søg efter og vis data',
+    'LBL_STEP2' => 'Flet poster med',
+    'LBL_TEST_SOURCE' => 'Test forbindelse',
+    'LBL_TEST_SOURCE_FAILED' => 'Testen mislykkedes',
+    'LBL_TEST_SOURCE_SUCCESS' => 'Testen lykkedes',
+    'LBL_TITLE' => 'Datafletning',
+
+    'ERROR_NO_ADDITIONAL_DETAIL' => 'Fejl: Ingen yderligere detaljer blev fundet til posten.',
+    'ERROR_NO_SEARCHDEFS_DEFINED' => 'Ingen moduler er aktiveret for denne forbindelse. Vælg et modul til denne forbindelse på siden Aktivér forbindelser.',
+    'ERROR_NO_SEARCHDEFS_MAPPED' => 'Fejl: Der er ingen forbindelser aktiveret som kan have søgefelter defineret.',
+    'ERROR_NO_SEARCHDEFS_MAPPING' => 'Fejl: Der er ingen søgefelter defineret for modulet og forbindelsen. Kontakt systemadministratoren.',
+    'ERROR_NO_DISPLAYABLE_MAPPED_FIELDS' => 'Fejl: Der er ingen modulfelter tilknyttet, der kan vises i resultaterne. Kontakt systemadministratoren.',
+    'LBL_INFO_INLINE' => 'Info' /*for 508 compliance fix*/,
+    'LBL_CLOSE' => 'Luk' /*for 508 compliance fix*/,
+
+);
