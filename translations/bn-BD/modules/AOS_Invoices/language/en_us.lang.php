5041c112b7f94276fb8dfd1306f08f0e6d199dc0
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-16 20:18:39 +0000

    New translations en_us.lang.php (Bengali)

---
+++ translations/bn-BD/modules/AOS_Invoices/language/en_us.lang.php
@@ -0,0 +1,203 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID' => 'সীমাবদ্ধ ইউজার আইডি',
+    'LBL_ASSIGNED_TO_NAME' => 'নির্ধারিত',
+    'LBL_ID' => 'আইডি',
+    'LBL_DATE_ENTERED' => 'তারিখ তৈরী',
+    'LBL_DATE_MODIFIED' => 'পরিবর্তিত তারিখ',
+    'LBL_MODIFIED' => 'দ্বারা পরিবর্তিত',
+    'LBL_MODIFIED_NAME' => 'নাম দ্বারা সংশোধিত',
+    'LBL_CREATED' => 'দ্বারা সৃষ্টি',
+    'LBL_DESCRIPTION' => 'বিবরণ',
+    'LBL_DELETED' => 'মোছা',
+    'LBL_NAME' => 'Title',
+    'LBL_CREATED_USER' => 'ব্যবহারকারী দ্বারা তৈরি',
+    'LBL_MODIFIED_USER' => 'ব্যবহারকারী দ্বারা সংশোধিত',
+    'ERR_DELETE_RECORD' => 'অ্যাকাউন্টটি মুছে ফেলতে একটি রেকর্ড নম্বর নির্দিষ্ট করা আবশ্যক।.',
+    'LBL_ACCOUNT_NAME' => 'Title',
+    'LBL_ACCOUNT' => 'কোম্পানি:',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'কার্যকলাপসমূহ',
+    'LBL_ADDRESS_INFORMATION' => 'ঠিকানার তথ্য',
+    'LBL_ANNUAL_REVENUE' => 'বার্ষিক আয়:',
+    'LBL_ANY_ADDRESS' => 'কোন ঠিকানা:',
+    'LBL_ANY_EMAIL' => 'কোনও ই-মেল:',
+    'LBL_ANY_PHONE' => 'কোন ফোন:',
+    'LBL_RATING' => 'নির্ধারণ',
+    'LBL_ASSIGNED_USER' => 'ব্যবহারকারী',
+    'LBL_BILLING_ADDRESS_CITY' => 'বিলিং শহর:',
+    'LBL_BILLING_ADDRESS_COUNTRY' => 'বিলিং শহর:',
+    'LBL_BILLING_ADDRESS_POSTALCODE' => 'বিলিং ডাক কোড:',
+    'LBL_BILLING_ADDRESS_STATE' => 'বিলিং স্টেট:',
+    'LBL_BILLING_ADDRESS_STREET_2' => 'বিলিং স্টেট ২ঃ',
+    'LBL_BILLING_ADDRESS_STREET_3' => 'বিলিং স্টেট ৩ঃ',
+    'LBL_BILLING_ADDRESS_STREET_4' => 'বিলিং স্টেট ৪ঃ',
+    'LBL_BILLING_ADDRESS_STREET' => 'বিলিং স্টেট:',
+    'LBL_BILLING_ADDRESS' => 'বিলিং ঠিকানা:',
+    'LBL_ACCOUNT_INFORMATION' => 'Overview',
+    'LBL_CITY' => 'শহর:',
+    'LBL_CONTACTS_SUBPANEL_TITLE' => 'যোগাযোগ',
+    'LBL_COUNTRY' => 'দেশ:',
+    'LBL_DEFAULT_SUBPANEL_TITLE' => 'অ্যাকাউন্ট',
+    'LBL_DUPLICATE' => 'সম্ভাব্য ডুপ্লিকেট অ্যাকাউন্ট',
+    'LBL_EMAIL' => 'Email:',
+    'LBL_EMPLOYEES' => 'এমপ্লয়িজ:',
+    'LBL_FAX' => 'ফ্যাক্স:',
+    'LBL_INDUSTRY' => 'শিল্প:',
+    'LBL_LIST_ACCOUNT_NAME' => 'হিসাবের নাম',
+    'LBL_LIST_CITY' => 'শহর',
+    'LBL_LIST_EMAIL_ADDRESS' => 'ইমেল ঠিকানা',
+    'LBL_LIST_PHONE' => 'ফোন',
+    'LBL_LIST_STATE' => 'রাষ্ট্র',
+    'LBL_MEMBER_OF' => 'এর সদস্য:',
+    'LBL_MEMBER_ORG_SUBPANEL_TITLE' => 'সদস্য সংগঠন',
+    'LBL_OTHER_EMAIL_ADDRESS' => 'অন্যান্য ইমেল:',
+    'LBL_OTHER_PHONE' => 'অন্যান্য ইমেল:',
+    'LBL_OWNERSHIP' => 'মালিকানা:',
+    'LBL_PARENT_ACCOUNT_ID' => 'মূল অ্যাকাউন্ট আইডি',
+    'LBL_PHONE_ALT' => 'বিকল্প ফোন:',
+    'LBL_PHONE_FAX' => 'ফোন ফ্যাক্স:',
+    'LBL_PHONE_OFFICE' => 'Phone Office:',
+    'LBL_PHONE' => 'Phone:',
+    'LBL_POSTAL_CODE' => 'Postal Code:',
+    'LBL_SAVE_ACCOUNT' => 'Save Account',
+    'LBL_SHIPPING_ADDRESS_CITY' => 'Shipping City:',
+    'LBL_SHIPPING_ADDRESS_COUNTRY' => 'Shipping Country:',
+    'LBL_SHIPPING_ADDRESS_POSTALCODE' => 'Shipping Postal Code:',
+    'LBL_SHIPPING_ADDRESS_STATE' => 'Shipping State:',
+    'LBL_SHIPPING_ADDRESS_STREET_2' => 'Shipping Street 2',
+    'LBL_SHIPPING_ADDRESS_STREET_3' => 'Shipping Street 3',
+    'LBL_SHIPPING_ADDRESS_STREET_4' => 'Shipping Street 4',
+    'LBL_SHIPPING_ADDRESS_STREET' => 'Shipping Street:',
+    'LBL_SHIPPING_ADDRESS' => 'Shipping Address:',
+    'LBL_STATE' => 'State/Region:',
+    'LBL_TICKER_SYMBOL' => 'Ticker Symbol:',
+    'LBL_TYPE' => 'Type:',
+    'LBL_WEBSITE' => 'Website:',
+    'LNK_ACCOUNT_LIST' => 'অ্যাকাউন্ট',
+    'LNK_NEW_ACCOUNT' => 'Create Account',
+    'MSG_DUPLICATE' => 'Creating this account may potentially create a duplicate account. You may either select an account from the list below or you may click on Save to continue creating a new account with the previously entered data.',
+    'MSG_SHOW_DUPLICATES' => 'Creating this account may potentially create a duplicate account. You may either click on Save to continue creating this new account with the previously entered data or you may click Cancel.',
+    'NTC_DELETE_CONFIRMATION' => 'Are you sure you want to delete this record?',
+    'LBL_LIST_FORM_TITLE' => 'Invoices List',
+    'LBL_MODULE_NAME' => 'চালান',
+    'LBL_MODULE_TITLE' => 'Invoices: Home',
+    'LBL_HOMEPAGE_TITLE' => 'My Invoices',
+    'LNK_NEW_RECORD' => 'Create Invoice',
+    'LNK_LIST' => 'View Invoices',
+    'LBL_SEARCH_FORM_TITLE' => 'Search Invoices',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'View History',
+    'LBL_NEW_FORM_TITLE' => 'New Invoice',
+    'LBL_TERMS_C' => 'Terms',
+    'LBL_APPROVAL_ISSUE' => 'Approval Issues',
+    'LBL_APPROVAL_STATUS' => 'Approval Status',
+    'LBL_BILLING_ACCOUNT' => 'হিসাব',
+    'LBL_BILLING_CONTACT' => 'যোগাযোগ',
+    'LBL_EXPIRATION' => 'Valid Until',
+    'LBL_INVOICE_NUMBER' => 'Invoice Number',
+    'LBL_OPPORTUNITY' => 'Opportunity Name',
+    'LBL_TEMPLATE_DDOWN_C' => 'Invoice Templates',
+    'LBL_STAGE' => 'Quote Stage',
+    'LBL_TERM' => 'Payment Terms',
+    'LBL_SUBTOTAL_AMOUNT' => 'Subtotal',
+    'LBL_DISCOUNT_AMOUNT' => 'Discount',
+    'LBL_TAX_AMOUNT' => 'Tax',
+    'LBL_SHIPPING_AMOUNT' => 'পাঠানো',
+    'LBL_TOTAL_AMT' => 'Total',
+    'VALUE' => 'Title',
+    'LBL_EMAIL_ADDRESSES' => 'Email Addresses',
+    'LBL_LINE_ITEMS' => 'Line Items',
+    'LBL_GRAND_TOTAL' => 'Grand Total',
+    'LBL_QUOTE_NUMBER' => 'Quote Number',
+    'LBL_QUOTE_DATE' => 'Quote Date',
+    'LBL_INVOICE_DATE' => 'Invoice Date',
+    'LBL_DUE_DATE' => 'Due Date',
+    'LBL_STATUS' => 'অবস্থা',
+    'LBL_INVOICE_STATUS' => 'Invoice Status',
+    'LBL_PRODUCT_QUANITY' => 'Quantity',
+    'LBL_PRODUCT_NAME' => 'প্রোডাক্ট',
+    'LBL_PART_NUMBER' => 'Part Number',
+    'LBL_PRODUCT_NOTE' => 'নোট',
+    'LBL_PRODUCT_DESCRIPTION' => 'বিবরণ',
+    'LBL_LIST_PRICE' => 'List',
+    'LBL_DISCOUNT_AMT' => 'Discount',
+    'LBL_UNIT_PRICE' => 'Sale Price',
+    'LBL_TOTAL_PRICE' => 'Total',
+    'LBL_VAT' => 'Tax', //VAT
+    'LBL_VAT_AMT' => 'Tax Amount', //VAT
+    'LBL_ADD_PRODUCT_LINE' => 'Add Product Line',
+    'LBL_SERVICE_NAME' => 'Service',
+    'LBL_SERVICE_LIST_PRICE' => 'List',
+    'LBL_SERVICE_PRICE' => 'Sale Price',
+    'LBL_SERVICE_DISCOUNT' => 'Discount',
+    'LBL_ADD_SERVICE_LINE' => 'Add Service Line ',
+    'LBL_REMOVE_PRODUCT_LINE' => 'অপসারণ',
+    'LBL_PRINT_AS_PDF' => 'Print as PDF',
+    'LBL_EMAIL_INVOICE' => 'Email Invoice',
+    'LBL_LIST_NUM' => 'Num',
+    'LBL_PDF_NAME' => 'Invoice',
+    'LBL_EMAIL_NAME' => 'Invoice for',
+    'LBL_NO_TEMPLATE' => 'ERROR\nNo templates found. If you have not created an Invoice template, go to the PDF templates module and create one',
+    'LBL_SUBTOTAL_TAX_AMOUNT' => 'Subtotal + Tax',//pre shipping
+    'LBL_EMAIL_PDF' => 'Email PDF',
+    'LBL_ADD_GROUP' => 'Add Group',
+    'LBL_DELETE_GROUP' => 'Delete Group',
+    'LBL_GROUP_NAME' => 'Group Name',
+    'LBL_GROUP_TOTAL' => 'Group Total',
+    'LBL_SHIPPING_TAX' => 'Shipping Tax',
+    'LBL_SHIPPING_TAX_AMT' => 'Shipping Tax',
+    'LBL_IMPORT_LINE_ITEMS' => 'Import Line Items',
+    'LBL_SUBTOTAL_AMOUNT_USDOLLAR' => 'Subtotal (Default Currency)',
+    'LBL_DISCOUNT_AMOUNT_USDOLLAR' => 'Discount (Default Currency)',
+    'LBL_TAX_AMOUNT_USDOLLAR' => 'Tax (Default Currency)',
+    'LBL_SHIPPING_AMOUNT_USDOLLAR' => 'Shipping (Default Currency)',
+    'LBL_TOTAL_AMT_USDOLLAR' => 'Total (Default Currency)',
+    'LBL_SHIPPING_TAX_AMT_USDOLLAR' => 'Shipping Tax (Default Currency)',
+    'LBL_GRAND_TOTAL_USDOLLAR' => 'Grand Total (Default Currency)',
+    'LBL_INVOICE_TO' => 'Invoice To',
+    'LBL_AOS_LINE_ITEM_GROUPS' => 'Line Item Groups',
+    'LBL_AOS_PRODUCT_QUOTES' => 'Product Quotes',
+    'LBL_AOS_QUOTES_AOS_INVOICES' => 'Quotes: Invoices',
+);
