f8594fcd162fccb9569921b5526e9434828554fb
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 03:29:25 +0000

    New translations en_us.lang.php (Ukrainian)

---
+++ translations/uk_UA/modules/AOS_Line_Item_Groups/language/uk_UA.lang.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ID' => 'ID',
+    'LBL_MODULE_NAME' => 'Групи',
+    'LBL_DATE_ENTERED' => 'Дата створення',
+    'LBL_DATE_MODIFIED' => 'Дата зміни',
+    'LBL_DESCRIPTION' => 'Опис',
+    'LBL_DELETED' => 'Видалено',
+    'LBL_NAME' => 'Назва групи',
+    'LBL_TOTAL_AMT' => 'Всього',
+    'LBL_DISCOUNT_AMOUNT' => 'Знижка',
+    'LBL_SUBTOTAL_AMOUNT' => 'Проміжний підсумок',
+    'LBL_TAX_AMOUNT' => 'Податок',
+    'LBL_SUBTOTAL_TAX_AMOUNT' => 'Проміжний підсум + податки',
+    'LBL_GROUP_TOTAL' => 'Підсумки по групі',
+    'LBL_LIST_NUM' => 'Число',
+    'LBL_SUBTOTAL_AMOUNT_USDOLLAR' => 'Проміжний підсумок (валюта за замовчуванням)',
+    'LBL_DISCOUNT_AMOUNT_USDOLLAR' => 'Знижка (валюта за замовчуванням)',
+    'LBL_TAX_AMOUNT_USDOLLAR' => 'Податки (валюта за замовчуванням)',
+    'LBL_TOTAL_AMT_USDOLLAR' => 'Всього (валюта за замовчуванням)',
+    'LBL_GROUP_TOTAL_USDOLLAR' => 'Загалом по групі (валюта за замовчуванням)',
+    'LBL_SUBTOTAL_TAX_AMOUNT_USDOLLAR' => 'Проміжний підсумок та податок (валюта за замовчуванням)',
+    'LBL_PARENT_ID' => 'ID початкового запису',
+
+    'AOS_PRODUCT_QUOTES' => 'Пропозиції за товаром',
+);
