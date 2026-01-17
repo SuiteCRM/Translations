bf9ef3a3a30a17d486f00e6a1ca5ff21910e7a91
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 02:29:53 +0000

    New translations en_us.lang.php (Arabic)

---
+++ translations/ar/modules/Surveys/language/ar.lang.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID'                                     => 'معرف المستخدم المكلف',
+    'LBL_ADD_OPTION'                                         => 'إضافة خيار جديد',
+    'LBL_ASSIGNED_TO_NAME'                                   => 'أُسند إلى',
+    'LBL_SECURITYGROUPS'                                     => 'المجموعات الأمنية',
+    'LBL_SECURITYGROUPS_SUBPANEL_TITLE'                      => 'المجموعات الأمنية',
+    'LBL_ID'                                                 => 'المعرف',
+    'LBL_DATE_ENTERED'                                       => 'تاريخ الإنشاء',
+    'LBL_DATE_MODIFIED'                                      => 'تاريخ التعديل',
+    'LBL_MODIFIED'                                           => 'عُدل بواسطة',
+    'LBL_MODIFIED_ID'                                        => 'عدل بواسطة المعرف',
+    'LBL_MODIFIED_NAME'                                      => 'اسم من قام بالتعديل',
+    'LBL_CREATED'                                            => 'أنشئ بواسطة',
+    'LBL_CREATED_ID'                                         => 'أنشئ بواسطة المعرف',
+    'LBL_DESCRIPTION'                                        => 'الوصف',
+    'LBL_DELETED'                                            => 'محذوف',
+    'LBL_NAME'                                               => 'الاسم',
+    'LBL_CREATED_USER'                                       => 'أنشئ بواسطة المستخدم',
+    'LBL_MODIFIED_USER'                                      => 'عدل بواسطة المستخدم',
+    'LBL_LIST_NAME'                                          => 'الاسم',
+    'LBL_EDIT_BUTTON'                                        => 'تحرير',
+    'LBL_REMOVE'                                             => 'حذف',
+    'LBL_LIST_FORM_TITLE'                                    => 'قائمة الاستطلاعات',
+    'LBL_MODULE_NAME'                                        => 'الاستطلاعات',
+    'LBL_MODULE_TITLE'                                       => 'الاستطلاعات',
+    'LBL_HOMEPAGE_TITLE'                                     => 'الاستطلاعات الخاصة بي',
+    'LNK_NEW_RECORD'                                         => 'إنشاء استطلاعات',
+    'LNK_LIST'                                               => 'عرض الاستطلاعات',
+    'LNK_IMPORT_SURVEYS'                                     => 'إستيراد الاستطلاعات',
+    'LBL_SEARCH_FORM_TITLE'                                  => 'بحث الاستطلاعات',
+    'LBL_HISTORY_SUBPANEL_TITLE'                             => 'عرض التاريخ',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE'                          => 'الأنشطة',
+    'LBL_SURVEYS_SUBPANEL_TITLE'                             => 'الاستطلاعات',
+    'LBL_NEW_FORM_TITLE'                                     => 'استطلاعات جديدة',
+    'LBL_STATUS'                                             => 'الحالة',
+    'LBL_SURVEY_QUESTIONS_DISPLAY'                           => 'الأسئلة',
+    'LBL_SURVEY_URL_DISPLAY'                                 => 'الرابط (URL)',
+    'LBL_HAPPINESS_QUESTION'                                 => 'سؤال السعادة',
+    'LBL_CANT_EDIT_RESPONDED'                                => 'أسئلة الاستطلاع التي لها ردود لا يمكن تعديلها',
+    'LBL_VIEW_SURVEY_REPORTS'                                => 'عرض تقارير الاستطلاع',
+    'LBL_CHECKED'                                            => 'مختار',
+    'LBL_UNCHECKED'                                          => 'غير مختار',
+    'LBL_RESPONSE_ANSWER'                                    => 'الإجابة',
+    'LBL_RESPONSE_CONTACT'                                   => 'جهة الإتصال',
+    'LBL_RESPONSE_TIME'                                      => 'التاريخ',
+    'LBL_UNKNOWN_CONTACT'                                    => 'مجهول',
+    'LBL_RESPONSE_COUNT'                                     => 'العد',
+    'LBL_SUBMIT_TEXT'                                        => 'إرسال النص',
+    'LBL_SATISFIED_TEXT'                                     => 'نص مُرضٍ',
+    'LBL_NEITHER_TEXT'                                       => 'لا يوجد أي نص',
+    'LBL_DISSATISFIED_TEXT'                                  => 'نص غير مُرضٍ',
+    'LBL_SURVEYS_SURVEYQUESTIONS_FROM_SURVEYQUESTIONS_TITLE' => 'أسئلة الاستطلاع',
+    'LBL_SURVEYS_SURVEYRESPONSES_FROM_SURVEYRESPONSES_TITLE' => 'الردود على الاستطلاع',
+    'LBL_SHOW_RESPONSES' => 'إظهار الردود',
+    'LBL_HIDE_RESPONSES' => 'إخفاء الردود',
+    'LBL_SURVEY_QUESTION' => 'سؤال',
+    'LBL_SURVEY_TEXT' => 'نص',
+    'LBL_SURVEY_TYPE' => 'النوع',
+    'LBL_SURVEY_ACTIONS' => 'الإجراءات',
+    'LBL_SURVEY_NEW_QUESTION' => 'سؤال جديد',
+    'LBL_SURVEY_RESPONSES' => 'سؤال جديد',
+    'LBL_SURVEYS_SENT' => 'تم إرسال الإستبيانات:',
+    'LBL_SURVEY_DISTINCT' => 'الإستبيانات المميزة التي أرسلت:',
+    'LBL_DRAFT' => 'مسودة',
+    'LBL_SUBMIT' => 'موافق',
+    'LBL_STARS' => 'نجوم',
+    'LBL_SURVEY_CLOSE_RESPONSE' => 'شكرا لاهتمامك ولكن هذا الاستبيان أغلق الآن.',
+    'LBL_SURVEY_THANKS' => 'Thanks for completing this survey.',
+);
