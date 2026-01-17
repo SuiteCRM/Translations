1ee6e1e11797f96601480e4dee7125d7bec5bc18
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 04:46:13 +0000

    New translations en_us.lang.php (Korean)

---
+++ translations/ko_KR/modules/EmailMarketing/language/ko_KR.lang.php
@@ -0,0 +1,108 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_REPLY_ADDR' => '"Reply-to" 주소: ',
+    'LBL_REPLY_NAME' => '"Reply-to" 이름: ',
+
+    'LBL_MODULE_NAME' => '이메일 마켓팅',
+    'LBL_MODULE_TITLE' => '이메일 마케팅',
+    'LBL_LIST_FORM_TITLE' => '이메일 마케팅 캠페인',
+    'LBL_NAME' => '이름: ',
+    'LBL_LIST_NAME' => '이름',
+    'LBL_LIST_FROM_ADDR' => '이메일에서',
+    'LBL_LIST_DATE_START' => '시작일',
+    'LBL_LIST_TEMPLATE_NAME' => '이메일 서식',
+    'LBL_LIST_STATUS' => '상태',
+    'LBL_STATUS' => '상태',
+    'LBL_STATUS_TEXT' => '상태',
+    'LBL_TEMPLATE_NAME' => '서식명',
+    'LBL_DATE_ENTERED' => '입력일',
+    'LBL_DATE_MODIFIED' => '수정일',
+    'LBL_MODIFIED' => '편집자:',
+    'LBL_CREATED' => '등록자: ',
+    'LBL_MESSAGE_FOR' => '이 메시지를 보낼 대상:',
+
+    'LBL_FROM_NAME' => '발신자 명: ',
+    'LBL_FROM_ADDR' => '발신 이메일 주소: ',
+    'LBL_DATE_START' => '시작일',
+    'LBL_TIME_START' => '시작시간',
+    'LBL_START_DATE_TIME' => '시작 일자 & 시간: ',
+    'LBL_TEMPLATE' => '이메일 서식: ',
+
+    'LBL_MODIFIED_BY' => '편집자:',
+    'LBL_CREATED_BY' => '등록자: ',
+
+    'LNK_NEW_CAMPAIGN' => '캠페인 작성',
+    'LNK_CAMPAIGN_LIST' => '캠페인',
+    'LNK_NEW_PROSPECT_LIST' => '타겟 목록 작성',
+    'LNK_PROSPECT_LIST_LIST' => '타겟 목록',
+    'LNK_NEW_PROSPECT' => '타겟 작성',
+    'LNK_PROSPECT_LIST' => '대상',
+    'LBL_DEFAULT_SUBPANEL_TITLE' => '이메일 마켓팅',
+    'LBL_CREATE_EMAIL_TEMPLATE' => '작성',
+    'LBL_EDIT_EMAIL_TEMPLATE' => '편집',
+    'LBL_FROM_MAILBOX' => '발신 메일박스',
+    'LBL_FROM_MAILBOX_NAME' => '사용할 메일박스:',
+    'LBL_OUTBOUND_EMAIL_ACCOUNT_NAME' => '아웃바운드 이메일 계정:',
+    'LBL_PROSPECT_LIST_SUBPANEL_TITLE' => '타겟 목록',
+    'LBL_ALL_PROSPECT_LISTS' => '캠페인의 모든 타겟 목록을 선택하세요.',
+    'LBL_RELATED_PROSPECT_LISTS' => '이 메세지와 관련된 모든 타겟 목록',
+    'LBL_PROSPECT_LIST_NAME' => '명칭:',
+
+    'LBL_LIST_PROSPECT_LIST_NAME' => '타겟 목록',
+    'LBL_MODULE_SEND_TEST' => '캠페인: 테스트 전송',
+    'LBL_MODULE_SEND_EMAILS' => '캠페인: 이메일 전송',
+    'LBL_SCHEDULE_MESSAGE_TEST' => '테스트할 캠페인 메시지를 선택하세요:',
+    'LBL_SCHEDULE_MESSAGE_EMAILS' => '지정된 시작 날짜와 시간에 배포할 예정인 캠페인 메시지를 선택하십시오:',
+    'LBL_SCHEDULE_BUTTON_TITLE' => '보내기',
+    'LBL_SCHEDULE_BUTTON_LABEL' => '보내기',
+    'LBL_ERROR_ON_MARKETING' => '필수 입력 필드가 비어있습니다',
+
+    'LBL_CAMPAIGN_ID' => '캠페인 ID',
+    'LBL_OUTBOUND_EMAIL_ACOUNT_ID' => '아웃바운드 이메일 계정 Id',
+    'LBL_EMAIL_TEMPLATE' => '이메일 서식',
+    'LBL_PROSPECT_LISTS' => '잠재고객 목록',
+
+);
