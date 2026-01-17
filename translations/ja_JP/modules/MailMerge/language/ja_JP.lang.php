0404b3a96007865a89b494ff11c09253490fa733
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 02:58:50 +0000

    New translations en_us.lang.php (Japanese)

---
+++ translations/ja_JP/modules/MailMerge/language/ja_JP.lang.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_STEP_1' => 'ステップ1: モジュールとテンプレートを選択',
+    'LBL_MAILMERGE_MODULE' => 'モジュール選択: ',
+    'LBL_MAILMERGE_SELECTED_MODULE' => '選択されたモジュール: ',
+    'LBL_MAILMERGE_TEMPLATES' => 'テンプレート選択: ',
+    'LBL_STEP_4' => 'レビューの後に完了',
+    'LBL_SELECTED_MODULE' => '選択されたモジュール: ',
+    'LBL_SELECTED_TEMPLATE' => '選択されたテンプレート: ',
+    'LBL_SELECTED_ITEMS' => '選択されたアイテム: ',
+    'LBL_STEP_5' => 'メールマージ完了',
+    'LBL_MERGED_FILE' => 'マージされたファイル: ',
+    'LNK_NEW_MAILMERGE' => 'メールマージ開始',
+    'LNK_UPLOAD_TEMPLATE' => 'テンプレートのアップロード',
+    'LBL_DOC_NAME' => 'ドキュメント名:',
+    'LBL_FILENAME' => 'ファイル名:',
+    'LBL_DOC_VERSION' => '版数:',
+    'LBL_DOC_DESCRIPTION' => '詳細:',
+    'LBL_LIST_NAME' => '名前',
+    'LBL_FINISH' => 'メールマージ開始',
+    'LBL_NEXT' => '次へ >',
+    'LBL_BACK' => '< 戻る',
+    'LBL_START' => '再開始',
+    'LBL_CONTAINS_CONTACT_INFO' => '選択されたテンプレートは取引先担当者情報を含んでいます。',
+    'LBL_ADDIN_NOTICE' => 'これにはMicrosoft WordへのSuiteCRM Mail Mergeアドインのインストールが必要です。',
+    'LBL_REMOVE' => 'アイテムを削除' /*for 508 compliance fix*/,
+    'LBL_ADD' => 'アイテムを追加' /*for 508 compliance fix*/,
+    'LBL_BROWSER_REQUIREMENT' => 'Windowsに対してIE 6.0以上が要求されます。',
+    'LBL_FORM_SELECT_TEMPLATE' => 'テンプレートを選択してください。',
+    'LBL_SELECT_ONE' => '少なくとも1つのアイテムを選択してください。',
+    'LBL_AVAILABLE' => '利用可能',
+    'LBL_SELECTED' => '選択済',
+    'LBL_RELATED_INFO_SELECTION' => '以下の選択肢を使用して、差し込み印刷文書に表示する関連情報を指定します。テンプレート文書に関連情報が含まれていない場合は、この手順をスキップしてください。',
+
+);
