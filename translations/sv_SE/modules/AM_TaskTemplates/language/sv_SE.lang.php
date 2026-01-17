6b62a65ff829617a190a8c62b0879a371031751f
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 03:25:01 +0000

    New translations en_us.lang.php (Swedish)

---
+++ translations/sv_SE/modules/AM_TaskTemplates/language/sv_SE.lang.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID' => 'Tilldelat användar-id',
+    'LBL_ASSIGNED_TO_NAME' => 'Tilldelad till',
+    'LBL_ID' => 'ID',
+    'LBL_DATE_ENTERED' => 'Datum Skapad',
+    'LBL_DATE_MODIFIED' => 'Datum Ändrad',
+    'LBL_MODIFIED' => 'Ändrad av',
+    'LBL_MODIFIED_ID' => 'Ändrad Av Id',
+    'LBL_MODIFIED_NAME' => 'Ändrad av Namn',
+    'LBL_CREATED' => 'Skapad av',
+    'LBL_CREATED_ID' => 'Skapad Av Id',
+    'LBL_DESCRIPTION' => 'Beskrivning',
+    'LBL_DELETED' => 'Raderad',
+    'LBL_NAME' => 'Uppgiftsnamn',
+    'LBL_CREATED_USER' => 'Skapad av Användare',
+    'LBL_MODIFIED_USER' => 'Ändrad av Användare',
+    'LBL_LIST_NAME' => 'Namn',
+    'LBL_EDIT_BUTTON' => 'Redigera',
+    'LBL_REMOVE' => 'Ta bort',
+    'LBL_LIST_FORM_TITLE' => 'Projektuppgiftsmallar',
+    'LBL_MODULE_NAME' => 'Projektuppgiftsmallar',
+    'LBL_MODULE_TITLE' => 'Projektuppgiftsmallar',
+    'LBL_HOMEPAGE_TITLE' => 'Mina projektuppgiftsmallar',
+    'LNK_NEW_RECORD' => 'Skapa projektuppgiftsmallar',
+    'LNK_LIST' => 'Visa projektuppgiftsmallar',
+    'LNK_IMPORT_AM_TASKTEMPLATES' => 'Importera projektuppgiftsmallar',
+    'LBL_SEARCH_FORM_TITLE' => 'Sök projektuppgiftsmallar',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Visa historik',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Aktiviteter',
+    'LBL_AM_TASKTEMPLATES_SUBPANEL_TITLE' => 'Projektuppgiftsmallar',
+    'LBL_NEW_FORM_TITLE' => 'Nya projektuppgiftsmallar',
+    'LBL_STATUS' => 'Status',
+    'LBL_PRIORITY' => 'Prioritet',
+    'LBL_PERCENT_COMPLETE' => 'Procent klart',
+    'LBL_PREDECESSORS' => 'Förgångare',
+    'LBL_MILESTONE_FLAG' => 'Milstolpe',
+    'LBL_RELATIONSHIP_TYPE' => 'Relationstyp',
+    'LBL_TASK_NUMBER' => 'Uppgiftsnummer',
+    'LBL_ORDER_NUMBER' => 'Beställning ',
+    'LBL_ESTIMATED_EFFORT' => 'Beräknad insats (tim)',
+    'LBL_UTILIZATION' => 'Utnyttjande (%)',
+    'LBL_DURATION' => 'Varaktighet (dagar)',
+    'LBL_AM_TASKTEMPLATES_AM_PROJECTTEMPLATES_FROM_AM_PROJECTTEMPLATES_TITLE' => 'Projektmallar',
+
+    'LBL_AM_TASKTEMPLATES_AM_PROJECTTEMPLATES_FROM_AM_TASKTEMPLATES_TITLE' => 'Uppgiftsmall: Projektmall från uppgiftsmallens titel',
+);
