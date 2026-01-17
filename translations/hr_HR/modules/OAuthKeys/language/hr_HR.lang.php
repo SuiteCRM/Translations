922e2171a71302cb5ee67b53800259640f980939
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 03:55:53 +0000

    New translations en_us.lang.php (Croatian)

---
+++ translations/hr_HR/modules/OAuthKeys/language/hr_HR.lang.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_CONSKEY' => 'Potrošački ključ',
+    'LBL_CONSSECRET' => 'Potrošačka tajna',
+    'LBL_ASSIGNED_TO_ID' => 'Id dodijeljenog korisnika',
+    'LBL_ASSIGNED_TO_NAME' => 'korisnik',
+    'LBL_ID' => 'ID',
+    'LBL_DATE_ENTERED' => 'Kreirano:',
+    'LBL_DATE_MODIFIED' => 'Izmijenjeno:',
+    'LBL_MODIFIED' => 'Izmijenio:',
+    'LBL_MODIFIED_ID' => 'Izmjenjeno od',
+    'LBL_MODIFIED_NAME' => 'Izmijenjeno prema Nazivu',
+    'LBL_CREATED' => 'Kreirano od',
+    'LBL_CREATED_ID' => 'Kreirano od Id',
+    'LBL_DESCRIPTION' => 'Opis',
+    'LBL_DELETED' => 'Izbrisano',
+    'LBL_NAME' => 'Ime potrošačkog ključa',
+    'LBL_CREATED_USER' => 'Kreirano od korisnika',
+    'LBL_MODIFIED_USER' => 'Izmijenjeno od korisnika',
+    'LBL_LIST_NAME' => 'Ime ključa',
+    'LBL_LIST_FORM_TITLE' => 'OAuth ključevi',
+    'LBL_MODULE_NAME' => 'OAuth ključevi',
+    'LBL_MODULE_TITLE' => 'OAuth ključevi',
+    'LNK_NEW_RECORD' => 'Kreiraj OAuth ključ',
+    'LNK_LIST' => 'Pregledaj OAuth ključeve',
+    'LBL_TOKENS' => 'Tokeni',
+);
