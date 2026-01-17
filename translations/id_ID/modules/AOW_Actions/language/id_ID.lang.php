abeb690121947ff9a61189f68cbde7968c011ad4
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 03:42:08 +0000

    New translations en_us.lang.php (Indonesian)

---
+++ translations/id_ID/modules/AOW_Actions/language/id_ID.lang.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ID' => 'ID',
+    'LBL_DATE_ENTERED' => 'Tanggal dibuat',
+    'LBL_DATE_MODIFIED' => 'Tanggal dibuat',
+    'LBL_MODIFIED' => 'Diubah Oleh',
+    'LBL_MODIFIED_ID' => 'Modifikasi berdasarkan ID',
+    'LBL_MODIFIED_NAME' => 'Diubah oleh Nama',
+    'LBL_CREATED_USER' => 'Dibuat oleh Pengguna',
+    'LBL_MODIFIED_USER' => 'Diubah oleh Pengguna',
+    'LBL_CREATED' => 'Dibuat Oleh',
+    'LBL_DESCRIPTION' => 'Deskripsi',
+    'LBL_CREATED_ID' => 'Dibuat oleh Id',
+    'LBL_DELETED' => 'Dihapus',
+    'LBL_NAME' => 'Nama',
+    'LBL_MODULE_NAME' => 'Tindakan Alur Kerja',
+    'LBL_MODULE_TITLE' => 'Tindakan Alur Kerja',
+    'LBL_AOW_WORKFLOW_ID' => 'AOW_WorkFlow Id',
+    'LBL_ACTION' => 'Aksi',
+    'LBL_PARAMETERS' => 'Parameter',
+    'LBL_SELECT_ACTION' => 'Pilih aksi',
+    'LBL_CREATE_EMAIL_TEMPLATE' => 'Buat',
+    'LBL_RECORD_TYPE' => 'Jenis Catatan',
+    'LBL_SENDEMAIL' => 'Kirim Email',
+    'LBL_CREATERECORD' => 'Membuat Catatan',
+    'LBL_MODIFYRECORD' => 'Memodifikasi Record',
+    'LBL_ADD_FIELD' => 'Tambah Field',
+    'LBL_ADD_RELATIONSHIP' => 'Menambahkan Hubungan',
+    'LBL_EDIT_EMAIL_TEMPLATE' => 'Sunting',
+    'LBL_EMAIL' => 'Surel',
+    'LBL_EMAIL_TEMPLATE' => 'Template Email',
+    'LBL_SETAPPROVAL' => 'Menetapkan Persetujuan',
+    'LBL_RELATE_WORKFLOW' => 'Berhubungan dengan Alur kerja Modul',
+    'LBL_INDIVIDUAL_EMAILS' => 'Mengirim Email Pribadi',
+    'LBL_COMPUTEFIELD' => 'Menghitung Bidang',
+    'LBL_COMPUTEFIELD_PARAMETERS' => 'Parameter',
+    'LBL_COMPUTEFIELD_FIELD_NAME' => 'Nama Field',
+    'LBL_COMPUTEFIELD_IDENTIFIER' => 'Pengenal',
+    'LBL_COMPUTEFIELD_ADD_PARAMETER' => 'Tambahkan parameter',
+    'LBL_COMPUTEFIELD_RELATION_PARAMETERS' => 'Hubungan parameter',
+    'LBL_COMPUTEFIELD_RELATION' => 'Hubungan',
+    'LBL_COMPUTEFIELD_ADD_RELATION_PARAMETER' => 'Menambah relasi parameter',
+    'LBL_COMPUTEFIELD_FORMULA' => 'Formula',
+    'LBL_COMPUTEFIELD_FORMULAS' => 'Formula',
+    'LBL_COMPUTEFIELD_ADD_FORMULA' => 'Tambahkan formula',
+    'LBL_COMPUTEFIELD_VALUE_TYPE' => 'Jenis nilai',
+    'LBL_COMPUTEFIELD_RAW_VALUE' => 'Nilai mentah',
+    'LBL_COMPUTEFIELD_FORMATTED_VALUE' => 'Diformat nilai',
+    'LBL_COPY_EMAIL_ADDRESSES_WORKFLOW' => 'Copy emails from WorkFlow Module'
+);
