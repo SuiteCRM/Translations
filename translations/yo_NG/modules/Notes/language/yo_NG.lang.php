98c079b0b1312522c2f10900fd8e463f7c7d1088
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 04:35:31 +0000

    New translations en_us.lang.php (Yoruba)

---
+++ translations/yo_NG/modules/Notes/language/yo_NG.lang.php
@@ -0,0 +1,110 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'ERR_DELETE_RECORD' => 'You must specify a record number to delete the account.',
+    'LBL_ACCOUNT_ID' => 'Account ID:',
+    'LBL_CASE_ID' => 'Case ID:',
+    'LBL_CLOSE' => 'Close:',
+    'LBL_CONTACT_ID' => 'Contact ID:',
+    'LBL_CONTACT_NAME' => 'Kontati:',
+    'LBL_DEFAULT_SUBPANEL_TITLE' => 'Akosile',
+    'LBL_DESCRIPTION' => 'Akosile',
+    'LBL_EMAIL_ADDRESS' => 'Adiresi emaili:',
+    'LBL_EMAIL_ATTACHMENT' => 'Asomo Email',
+    'LBL_FILE_MIME_TYPE' => 'Mime Type',
+    'LBL_FILE_URL' => 'File URL',
+    'LBL_FILENAME' => 'Attachment:',
+    'LBL_LEAD_ID' => 'Lead ID:',
+    'LBL_LIST_CONTACT_NAME' => 'Kontati',
+    'LBL_LIST_DATE_MODIFIED' => 'Atunse ti o kehin',
+    'LBL_LIST_FILENAME' => 'Attachment',
+    'LBL_LIST_FORM_TITLE' => 'Note List',
+    'LBL_LIST_RELATED_TO' => 'Related To',
+    'LBL_LIST_SUBJECT' => 'Koko-oro',
+    'LBL_LIST_STATUS' => 'Ipo',
+    'LBL_LIST_CONTACT' => 'Kontati',
+    'LBL_MODULE_NAME' => 'Akosile',
+    'LBL_MODULE_TITLE' => 'Notes: Home',
+    'LBL_NEW_FORM_TITLE' => 'Create Note or Add Attachment',
+    'LBL_NOTE_STATUS' => 'Akosile',
+    'LBL_NOTE_SUBJECT' => 'Koko-oro:',
+    'LBL_NOTES_SUBPANEL_TITLE' => 'Attachments',
+    'LBL_NOTE' => 'Note:',
+    'LBL_OPPORTUNITY_ID' => 'Opportunity ID:',
+    'LBL_PARENT_ID' => 'Ami idanimo obi:',
+    'LBL_PARENT_TYPE' => 'Parent Type',
+    'LBL_PHONE' => 'Foonu:',
+    'LBL_PORTAL_FLAG' => 'Display in Portal?',
+    'LBL_EMBED_FLAG' => 'Embed in email?',
+    'LBL_PRODUCT_ID' => 'Product ID:',
+    'LBL_QUOTE_ID' => 'Quote ID:',
+    'LBL_RELATED_TO' => 'Related To:',
+    'LBL_SEARCH_FORM_TITLE' => 'Note Search',
+    'LBL_STATUS' => 'Ipo',
+    'LBL_SUBJECT' => 'Koko-oro:',
+    'LNK_IMPORT_NOTES' => 'Import Notes',
+    'LNK_NEW_NOTE' => 'Seda Akosile abi Asomo',
+    'LNK_NOTE_LIST' => 'View Notes',
+    'LBL_MEMBER_OF' => 'Egbe ti:',
+    'LBL_LIST_ASSIGNED_TO_NAME' => 'Onilo ti ayan',
+    'LBL_REMOVING_ATTACHMENT' => 'Removing attachment...',
+    'ERR_REMOVING_ATTACHMENT' => 'Failed to remove attachment...',
+    'LBL_CREATED_BY' => 'Siseda nipa',
+    'LBL_MODIFIED_BY' => 'Se iyipada nipasẹ',
+    'LBL_SEND_ANYWAYS' => 'This email has no subject. Send/save anyway?',
+    'LBL_NOTE_INFORMATION' => 'Isonisoki', //No need to be translated in all caps. Translation used just in menu action items when using the SuiteP template
+    'LBL_MY_NOTES_DASHLETNAME' => 'My Notes',
+    'LBL_EDITLAYOUT' => 'Ṣatunkọ Aṣayan' /*for 508 compliance fix*/,
+    //For export labels
+    'LBL_FIRST_NAME' => 'Oruko akoko',
+    'LBL_LAST_NAME' => 'Oruko ikehin',
+    'LBL_DATE_ENTERED' => 'Ojo ti a seda',
+    'LBL_DATE_MODIFIED' => 'Ojo ti a se Atunse',
+    'LBL_DELETED' => 'Paare',
+    'LBL_FILE_CONTENTS' => 'File Contents',
+
+    'LBL_ASCENDING' => 'Lati isale lo s\'oke',
+    'LBL_DESCENDING' => 'Lati oke wa sile',
+);
