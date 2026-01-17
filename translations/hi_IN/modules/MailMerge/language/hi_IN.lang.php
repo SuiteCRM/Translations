aebf959b00256fe185e430c9e7c80968c150c764
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 04:04:35 +0000

    New translations en_us.lang.php (Hindi)

---
+++ translations/hi_IN/modules/MailMerge/language/hi_IN.lang.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_STEP_1' => 'चरण 1: मॉड्यूल और टेम्पलेट का चयन करें',
+    'LBL_MAILMERGE_MODULE' => 'मॉड्यूल चुनें: ',
+    'LBL_MAILMERGE_SELECTED_MODULE' => 'चयनित मॉड्यूल: ',
+    'LBL_MAILMERGE_TEMPLATES' => 'टेम्पलेट चुनें: ',
+    'LBL_STEP_4' => 'समीक्षा करें और पूरा करें',
+    'LBL_SELECTED_MODULE' => 'चयनित मॉड्यूल: ',
+    'LBL_SELECTED_TEMPLATE' => 'चयनित टेम्पलेट: ',
+    'LBL_SELECTED_ITEMS' => 'चयनित आइटम: ',
+    'LBL_STEP_5' => 'मेल मर्ज पूर्ण',
+    'LBL_MERGED_FILE' => 'मर्ज की गई फ़ाइल: ',
+    'LNK_NEW_MAILMERGE' => 'मेल मर्ज आरंभ करें',
+    'LNK_UPLOAD_TEMPLATE' => 'टेम्पलेट अपलोड करें',
+    'LBL_DOC_NAME' => 'दस्तावेज़ का नाम:',
+    'LBL_FILENAME' => 'फ़ाइल का नाम:',
+    'LBL_DOC_VERSION' => 'दोहराव:',
+    'LBL_DOC_DESCRIPTION' => 'विवरण:',
+    'LBL_LIST_NAME' => 'नाम',
+    'LBL_FINISH' => 'मर्ज शुरू करें',
+    'LBL_NEXT' => 'अगला >',
+    'LBL_BACK' => '< पीछे',
+    'LBL_START' => 'जारी रखने के लिए यहां क्लिक करें',
+    'LBL_CONTAINS_CONTACT_INFO' => 'चयनित टेम्पलेट में संबंधित शामिल हैं ',
+    'LBL_ADDIN_NOTICE' => 'इसके लिए Microsoft Word में SuiteCRM मेल मर्ज ऐड-इन की स्थापना की आवश्यकता होती है।',
+    'LBL_REMOVE' => 'आइटम हटाएँ' /*for 508 compliance fix*/,
+    'LBL_ADD' => 'सामगंरियां जोड़ें' /*for 508 compliance fix*/,
+    'LBL_BROWSER_REQUIREMENT' => 'इसके लिए विंडोज़ के लिए IE 6.0 या उच्चतर की आवश्यकता है।',
+    'LBL_FORM_SELECT_TEMPLATE' => 'कृपया एक टेम्पलेट चुनें.',
+    'LBL_SELECT_ONE' => 'कृपया कम से कम एक आइटम का चयन करें.',
+    'LBL_AVAILABLE' => 'उपलब्ध',
+    'LBL_SELECTED' => 'चयनित',
+    'LBL_RELATED_INFO_SELECTION' => 'आप अपने मर्ज किए गए दस्तावेज़ में जो संबंधित जानकारी प्रदर्शित करना चाहते हैं, उसकी पहचान करने के लिए नीचे दिए गए चयनों का उपयोग करें। यदि आपके टेम्पलेट दस्तावेज़ में कोई संबंधित जानकारी नहीं है तो इस चरण को छोड़ दें।',
+
+);
