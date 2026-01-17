2b077e2f8046a58129c352d08257ee3f23a9a37c
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 03:25:18 +0000

    New translations en_us.lang.php (Swedish)

---
+++ translations/sv_SE/modules/AOS_Products_Quotes/language/sv_SE.lang.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID' => 'Tilldelat användar-id',
+    'LBL_ASSIGNED_TO_NAME' => 'Tilldelad till',
+    'LBL_ID' => 'ID',
+    'LBL_DATE_ENTERED' => 'Datum Skapad',
+    'LBL_DATE_MODIFIED' => 'Datum Ändrad',
+    'LBL_MODIFIED' => 'Ändrad av',
+    'LBL_MODIFIED_NAME' => 'Ändrad av Namn',
+    'LBL_CREATED' => 'Skapad av',
+    'LBL_DESCRIPTION' => 'Anteckningar',
+    'LBL_DELETED' => 'Raderad',
+    'LBL_NAME' => 'Namn',
+    'LBL_CREATED_USER' => 'Skapad av Användare',
+    'LBL_MODIFIED_USER' => 'Ändrad av Användare',
+    'LBL_LIST_FORM_TITLE' => 'Products Quotes List',
+    'LBL_MODULE_NAME' => 'Line Items',
+    'LBL_MODULE_TITLE' => 'Products Quotes: Home',
+    'LBL_HOMEPAGE_TITLE' => 'My Products Quotes',
+    'LNK_NEW_RECORD' => 'Create Product Quote',
+    'LNK_LIST' => 'Products Quotes',
+    'LBL_SEARCH_FORM_TITLE' => 'Search Products Quotes',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Visa historik',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Aktiviteter',
+    'LBL_NEW_FORM_TITLE' => 'New Product Quote',
+    'LBL_PRODUCT_NAME' => 'Namn',
+    'LBL_PRODUCT_QTY' => 'Kvantitet',
+    'LBL_PRODUCT_COST_PRICE' => 'Cost Price',
+    'LBL_PRODUCT_LIST_PRICE' => 'Listpris',
+    'LBL_PRODUCT_UNIT_PRICE' => 'Styckpris',
+    'LBL_PRODUCT_DISCOUNT' => 'Rabatt',
+    'LBL_PRODUCT_DISCOUNT_AMOUNT' => 'Rabattbelopp',
+    'LBL_PART_NUMBER' => 'Delnummer',
+    'LBL_PRODUCT_DESCRIPTION' => 'Beskrivning',
+    'LBL_DISCOUNT' => 'Rabatt-typ',
+    'LBL_VAT_AMT' => 'Skattebelopp',
+    'LBL_VAT' => 'Skatt',
+    'LBL_PRODUCT_TOTAL_PRICE' => 'Total Price',
+    'LBL_PRODUCT_NOTE' => 'Anteckningar',
+    'Quote' => '',
+    'LBL_FLEX_RELATE' => 'Relaterad till',
+    'LBL_PRODUCT' => 'Produkt',
+
+    'LBL_SERVICE_MODULE_NAME' => 'Tjänster',
+    'LBL_LIST_NUM' => 'Nummer',
+    'LBL_PARENT_ID' => 'Överordnat ID',
+    'LBL_GROUP_NAME' => 'Grupp',
+    'LBL_PRODUCT_COST_PRICE_USDOLLAR' => 'Cost Price (Default Currency)',
+    'LBL_PRODUCT_LIST_PRICE_USDOLLAR' => 'List Price (Default Currency)',
+    'LBL_PRODUCT_UNIT_PRICE_USDOLLAR' => 'Unit Price (Default Currency)',
+    'LBL_PRODUCT_TOTAL_PRICE_USDOLLAR' => 'Total Price (Default Currency)',
+    'LBL_PRODUCT_DISCOUNT_USDOLLAR' => 'Rabatt (standardvaluta)',
+    'LBL_PRODUCT_DISCOUNT_AMOUNT_USDOLLAR' => 'Discount Amount (Default Currency)',
+    'LBL_VAT_AMT_USDOLLAR' => 'Skattebelopp (standardvaluta)',
+    'LBL_PRODUCTS_SERVICES' => 'Produkt / tjänst',
+    'LBL_PRODUCT_ID' => 'Produkt-ID',
+
+    'LBL_AOS_CONTRACTS' => 'Kontrakt',
+    'LBL_AOS_INVOICES' => 'Fakturor',
+    'LBL_AOS_PRODUCTS' => 'Produkter',
+    'LBL_AOS_QUOTES' => 'Offert',
+);
