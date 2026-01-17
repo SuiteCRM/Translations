3a4f80d4d35c98f2b7f437421b79122e7f8c1bcd
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 03:24:52 +0000

    New translations en_us.lang.php (Swedish)

---
+++ translations/sv_SE/include/SugarObjects/templates/sale/language/sv_SE.lang.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_MODULE_NAME' => 'Försäljning',
+    'LBL_MODULE_TITLE' => 'Försäljning: Hem',
+    'LBL_SEARCH_FORM_TITLE' => 'Sök försäljning',
+    'LBL_LIST_FORM_TITLE' => 'Försäljningslista',
+    'LBL_NAME' => 'Försäljningsnamn',
+    'LBL_LIST_SALE_NAME' => 'Namn',
+    'LBL_LIST_ACCOUNT_NAME' => 'Kontonamn',
+    'LBL_LIST_AMOUNT' => 'Summa',
+    'LBL_LIST_DATE_CLOSED' => 'Stäng',
+    'LBL_LIST_SALE_STAGE' => 'Säljläge',
+    'LBL_ACCOUNT_ID' => 'Företags-ID',
+    //DON'T CONVERT THESE THEY ARE MAPPINGS
+    'db_name' => 'LBL_NAME',
+    //END DON'T CONVERT
+    'LBL_ACCOUNT_NAME' => 'Företagsnamn:',
+    'LBL_AMOUNT' => 'Summa:',
+    'LBL_AMOUNT_USDOLLAR' => 'Summa USD:',
+    'LBL_CURRENCY' => 'Valuta',
+    'LBL_DATE_CLOSED' => 'Förväntat avslutsdatum:',
+    'LBL_TYPE' => 'Typ:',
+    'LBL_CAMPAIGN' => 'Kampanj:',
+    'LBL_LEADS_SUBPANEL_TITLE' => 'Kundämnen',
+    'LBL_PROJECTS_SUBPANEL_TITLE' => 'Projekt',
+    'LBL_NEXT_STEP' => 'Nästa steg:',
+    'LBL_LEAD_SOURCE' => 'Lead-källa:',
+    'LBL_SALES_STAGE' => 'Säljläge:',
+    'LBL_PROBABILITY' => 'Sannolikhet (%):',
+    'LBL_DESCRIPTION' => 'Beskrivning:',
+    'LBL_DUPLICATE' => 'Möjlig duplicerad försäljning',
+    'MSG_DUPLICATE' => 'Försäljningsposten du håller på att skapa kan vara en dublett av en redan existerande post. Försäljningar med liknande namn är listade nedan.<br>Klicka Spara för att fortsätta att skapa denna nya försäljning, eller klicka Avbryt för att återgå till modulen utan att skapa försäljningen.',
+    'LBL_NEW_FORM_TITLE' => 'Skapa försäljning',
+    'ERR_DELETE_RECORD' => 'Ett postnummer måste vara specifierat för att ta bort denna försäljning.',
+    'LBL_DEFAULT_SUBPANEL_TITLE' => 'Försäljning',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Aktiviteter',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Historia',
+
+    'LBL_CONTACTS_SUBPANEL_TITLE' => 'Kontakter',
+    'LBL_ASSIGNED_TO_NAME' => 'Användare:',
+    'LBL_LIST_ASSIGNED_TO_NAME' => 'Tilldelad användare',
+    'LBL_ASSIGNED_TO_ID' => 'Tilldelad till ID',
+    'LBL_MODIFIED_NAME' => 'Ändrad av Användar Namn',
+    'LBL_SALE_INFORMATION' => 'Försäljningsinformation',
+    'LBL_CURRENCY_NAME' => 'Valuta namn',
+    'LBL_CURRENCY_SYMBOL' => 'Valutasymbol',
+    'LBL_EDIT_BUTTON' => 'Redigera',
+    'LBL_REMOVE' => 'Ta bort',
+
+);
