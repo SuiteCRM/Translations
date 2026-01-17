a08bbc877d5cea5ec2c9a1790866de7b485f1497
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 04:18:56 +0000

    New translations en_us.lang.php (Sinhala)

---
+++ translations/si_LK/modules/Bugs/language/si_LK.lang.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_MODULE_NAME' => ' බග්ස්',
+    'LBL_MODULE_TITLE' => 'බග් ට්රැකර්: මුල් පිටුව',
+    'LBL_MODULE_ID' => ' බග්ස්',
+    'LBL_SEARCH_FORM_TITLE' => 'බග් සොයන්න',
+    'LBL_LIST_FORM_TITLE' => 'බග් ලැයිස්තුව',
+    'LBL_NEW_FORM_TITLE' => 'නව බග්',
+    'LBL_SUBJECT' => 'විෂය:',
+    'LBL_NUMBER' => 'අංකය:',
+    'LBL_STATUS' => 'තත්වය:',
+    'LBL_PRIORITY' => 'ප්රමුඛතාව:',
+    'LBL_DESCRIPTION' => 'විස්තරය:',
+    'LBL_CONTACT_NAME' => 'සබඳතාවේ නම:',
+    'LBL_CONTACT_ROLE' => 'කාර්යභාරය:',
+    'LBL_LIST_NUMBER' => 'ගණන්.',
+    'LBL_LIST_SUBJECT' => ' විෂය',
+    'LBL_LIST_STATUS' => 'තත්වය',
+    'LBL_LIST_PRIORITY' => 'ප්රමුඛ',
+    'LBL_LIST_RESOLUTION' => 'යෝජනාව',
+    'LBL_LIST_LAST_MODIFIED' => 'අවසන් වරට සංස්කරණය',
+    'LBL_INVITEE' => 'සබඳතා',
+    'LBL_TYPE' => 'වර්ගය:',
+    'LBL_LIST_TYPE' => 'වර්ගය',
+    'LBL_RESOLUTION' => 'යෝජනාව:',
+    'LBL_RELEASE' => 'නිකුතු:',
+    'LNK_NEW_BUG' => 'වාර්තාව බග්',
+    'LNK_BUG_LIST' => 'දැක්ම බග්ස්',
+    'ERR_DELETE_RECORD' => 'ඔබ දෝෂයක් මකා ගැනීම සඳහා වාර්තාගත අංකය සඳහන් කළ යුතුය.',
+    'LBL_LIST_MY_BUGS' => 'මගේ යොදවා බග්ස්',
+    'LNK_IMPORT_BUGS' => 'ආනයන බග්ස්',
+    'LBL_FOUND_IN_RELEASE' => 'නිකුතු සොයා ගනී:',
+    'LBL_FIXED_IN_RELEASE' => 'නිකුතු දී ස්ථාවර:',
+    'LBL_LIST_FIXED_IN_RELEASE' => 'නිකුතු දී ස්ථාවර',
+    'LBL_WORK_LOG' => 'වැඩ ලොග්:',
+    'LBL_SOURCE' => 'මූලාශ්රය:',
+    'LBL_PRODUCT_CATEGORY' => 'වර්ගය:',
+
+    'LBL_CREATED_BY' => 'සෑදුවේ:',
+    'LBL_MODIFIED_BY' => 'පසුගිය විසින් නවීකරණය කරන ලද:',
+
+    'LBL_LIST_EMAIL_ADDRESS' => 'විද්යුත් තැපැල් ලිපිනය',
+    'LBL_LIST_CONTACT_NAME' => 'සබඳතාවේ නම',
+    'LBL_LIST_ACCOUNT_NAME' => 'ගිණුමේ නම',
+    'LBL_LIST_PHONE' => 'දුරකතන',
+    'NTC_DELETE_CONFIRMATION' => 'ඔබ දෝෂයක් සිට මෙම සබඳතා ඉවත් කිරීමට අවශ්ය බව ඔබට විශ්වාසද?',
+
+    'LBL_DEFAULT_SUBPANEL_TITLE' => 'බග් ට්රැකර්',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'ක්‍රියාකාරකම්',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'ඉතිහාසය',
+    'LBL_CONTACTS_SUBPANEL_TITLE' => 'සබඳතා',
+    'LBL_ACCOUNTS_SUBPANEL_TITLE' => 'ගිණුම්',
+    'LBL_CASES_SUBPANEL_TITLE' => ' නඩු',
+    'LBL_PROJECTS_SUBPANEL_TITLE' => 'ව්යාපෘති',
+    'LBL_DOCUMENTS_SUBPANEL_TITLE' => 'ලිපි ලේඛන',
+    'LBL_LIST_ASSIGNED_TO_NAME' => 'පවරා පරිශීලක',
+    'LBL_ASSIGNED_TO_NAME' => 'පවරා',
+
+    'LBL_BUG_INFORMATION' => 'දළ විශ්ලේෂණය', //No need to be translated in all caps. Translation used just in menu action items when using the SuiteP template
+
+);
