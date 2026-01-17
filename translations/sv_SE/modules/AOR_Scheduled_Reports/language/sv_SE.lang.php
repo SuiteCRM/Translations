d7f211de0b97bb1435f57efc51eaff96bbfc2bf5
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 03:25:11 +0000

    New translations en_us.lang.php (Swedish)

---
+++ translations/sv_SE/modules/AOR_Scheduled_Reports/language/sv_SE.lang.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID' => 'Tilldelat användar-id',
+    'LBL_ASSIGNED_TO_NAME' => 'Tilldelad till',
+    'LBL_ID' => 'ID',
+    'LBL_DATE_ENTERED' => 'Datum Skapad',
+    'LBL_DATE_MODIFIED' => 'Datum Ändrad',
+    'LBL_MODIFIED' => 'Ändrad av',
+    'LBL_MODIFIED_NAME' => 'Ändrad av Namn',
+    'LBL_CREATED' => 'Skapad av',
+    'LBL_DESCRIPTION' => 'Beskrivning',
+    'LBL_DELETED' => 'Raderad',
+    'LBL_NAME' => 'Namn',
+    'LBL_CREATED_USER' => 'Skapad av Användare',
+    'LBL_MODIFIED_USER' => 'Ändrad av Användare',
+    'LBL_LIST_NAME' => 'Namn',
+    'LBL_EDIT_BUTTON' => 'Redigera',
+    'LBL_REMOVE' => 'Ta bort',
+    'LBL_ANY_EMAIL' => 'E-post',
+    'LBL_EMAIL_NON_PRIMARY' => 'Inga primära mail',
+    'LBL_ASSIGNED_TO' => 'Tilldelad till:',
+    'LBL_ASSIGNED_USER' => 'Tilldelad till:',
+    'LBL_EMAIL_RECIPIENTS' => 'E-postmottagare:',
+    'NTC_DELETE_CONFIRMATION' => 'Är du säker på att du vill ta bort denna posten?',
+    'LBL_LIST_FORM_TITLE' => 'Scheduled Reports List',
+    'LBL_MODULE_NAME' => 'Schemalagda rapporter',
+    'LBL_MODULE_TITLE' => 'Schemalagda rapporter',
+    'LBL_HOMEPAGE_TITLE' => 'Mina schemalagda rapporter',
+    'LNK_NEW_RECORD' => 'Skapa schemalagda rapporter',
+    'LNK_LIST' => 'Visa schemalagda rapporter',
+    'LBL_SEARCH_FORM_TITLE' => 'Sök schemalagda rapporter',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Visa historik',
+    'LBL_NEW_FORM_TITLE' => 'Nya schemalagda rapporter',
+    'LBL_SCHEDULE' => 'Schema',
+    'LBL_AOR_REPORT_NAME' => 'Rapporter',
+    'LBL_SCHEDULED_REPORTS_INFORMATION' => 'Schemalagda rapporter',
+    'LBL_LAST_RUN' => 'Senast körd',
+    'LBL_STATUS' => 'Status',
+);
