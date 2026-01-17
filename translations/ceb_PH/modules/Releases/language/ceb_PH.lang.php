b02a9079c63888b37c797ed4212a38faf4a85cfa
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 04:37:45 +0000

    New translations en_us.lang.php (Cebuano)

---
+++ translations/ceb_PH/modules/Releases/language/ceb_PH.lang.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_MODULE_NAME' => 'Makuha na',
+    'LBL_MODULE_TITLE' => 'Mga Pagbuhi: Panimalay',
+    'LBL_SEARCH_FORM_TITLE' => 'Pagbuhi sa Pagpangita',
+    'LBL_LIST_FORM_TITLE' => 'Lista sa Igawas',
+    'LBL_NEW_FORM_TITLE' => 'Bag-o nga Gawas',
+    'LBL_RELEASE' => 'Igawas:',
+    'LBL_LIST_NAME' => 'Buhian',
+    'LBL_NAME' => 'Bersyon sa Gigawas:',
+    'LBL_LIST_LIST_ORDER' => 'Sugo',
+    'LBL_LIST_ORDER' => 'Sugo:',
+    'LBL_LIST_STATUS' => 'Stado',
+    'LBL_STATUS' => 'Estado:',
+    'LNK_NEW_RELEASE' => 'Lista sa Igawas',
+    'NTC_DELETE_CONFIRMATION' => 'Sigurado ka ba sa pagtangtang niining rekord?',
+    'ERR_DELETE_RECORD' => 'Dapat hingalan nimo ang numero sa rekord para mapapas ang gigawas.',
+    'NTC_STATUS' => 'I-set ang estado ngadto sa dili aktibo para matangtang ang kini na gigawas gikan sa Gigawas na dropdown na mga lista',
+    'NTC_LIST_ORDER' => 'I-set ang order sa kini na gigawas nga motungha sa Gigawas na dropdown na mga lista',
+    'release_status_dom' =>
+        array(
+            'Active' => 'Aktib',
+            'Inactive' => 'Dili aktib',
+        ),
+    'LBL_EDITLAYOUT' => 'I-edit ang Plano' /*for 508 compliance fix*/,
+);
