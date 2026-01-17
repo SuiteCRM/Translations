be89f05d180c055c793e6009000f0dcdb802f205
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 03:02:22 +0000

    New translations en_us.lang.php (Korean)

---
+++ translations/ko_KR/modules/AOS_Line_Item_Groups/language/ko_KR.lang.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ID' => 'ID',
+    'LBL_MODULE_NAME' => '그룹',
+    'LBL_DATE_ENTERED' => '생성날짜',
+    'LBL_DATE_MODIFIED' => '수정일',
+    'LBL_DESCRIPTION' => '상세',
+    'LBL_DELETED' => '삭제됨',
+    'LBL_NAME' => '그룹 이름',
+    'LBL_TOTAL_AMT' => '합계',
+    'LBL_DISCOUNT_AMOUNT' => '할인',
+    'LBL_SUBTOTAL_AMOUNT' => '부분합',
+    'LBL_TAX_AMOUNT' => '세금',
+    'LBL_SUBTOTAL_TAX_AMOUNT' => '소계 + 세금',
+    'LBL_GROUP_TOTAL' => '그룹 합계',
+    'LBL_LIST_NUM' => '번호',
+    'LBL_SUBTOTAL_AMOUNT_USDOLLAR' => '부분합 (기본 통화)',
+    'LBL_DISCOUNT_AMOUNT_USDOLLAR' => '할인 (기본 통화)',
+    'LBL_TAX_AMOUNT_USDOLLAR' => '세금 (기본 통화)',
+    'LBL_TOTAL_AMT_USDOLLAR' => '총 (기본 통화)',
+    'LBL_GROUP_TOTAL_USDOLLAR' => '그룹 합계 (기본 통화)',
+    'LBL_SUBTOTAL_TAX_AMOUNT_USDOLLAR' => '소계 + 세금 (기본 통화)',
+    'LBL_PARENT_ID' => '패런트ID:',
+
+    'AOS_PRODUCT_QUOTES' => 'Product Quotes',
+);
