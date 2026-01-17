70e906add62822e89276a1b0cf442a440234a666
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 03:20:33 +0000

    New translations en_us.lang.php (Slovenian)

---
+++ translations/sl_SI/include/SugarObjects/templates/file/language/sl_SI.lang.php
@@ -0,0 +1,117 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    //module
+    'LBL_MODULE_NAME' => 'Dokumenti',
+    'LBL_MODULE_TITLE' => 'Dokumenti: Domov',
+    'LNK_NEW_DOCUMENT' => 'Ustvari dokument',
+    'LNK_DOCUMENT_LIST' => 'Seznam dokumentov',
+    'LBL_SEARCH_FORM_TITLE' => 'Iskanje dokumentov',
+    //vardef labels
+    'LBL_NAME' => 'Ime dokumenta',
+    'LBL_DESCRIPTION' => 'Opis',
+    'LBL_ASSIGNED_TO' => 'Dodeljeno:',
+    'LBL_CATEGORY' => 'Kategorija',
+    'LBL_SUBCATEGORY' => 'Pod-kategorija',
+    'LBL_STATUS' => 'Stanje',
+    'LBL_IS_TEMPLATE' => 'Je predloga',
+    'LBL_TEMPLATE_TYPE' => 'Tip dokumenta',
+    'LBL_REVISION_NAME' => 'Število različice',
+    'LBL_MIME' => 'MIME tip',
+    'LBL_REVISION' => 'Različica',
+    'LBL_DOCUMENT' => 'Povezan dokument',
+    'LBL_LATEST_REVISION' => 'Zadnja različica',
+    'LBL_CHANGE_LOG' => 'Zgodovina sprememb',
+    'LBL_ACTIVE_DATE' => 'Datum objave',
+    'LBL_EXPIRATION_DATE' => 'Datum veljavnosti',
+    'LBL_FILE_EXTENSION' => 'Končnica datoteke',
+
+    'LBL_CAT_OR_SUBCAT_UNSPEC' => 'Nedefiniran',
+    //quick search
+    'LBL_NEW_FORM_TITLE' => 'Nov dokument',
+    //document edit and detail view
+    'LBL_DOC_NAME' => 'Ime dokumenta:',
+    'LBL_FILENAME' => 'Ime datoteke:',
+    'LBL_FILE_UPLOAD' => 'Ime datoteke',
+    'LBL_DOC_VERSION' => 'Različica:',
+    'LBL_CATEGORY_VALUE' => 'Kategorija:',
+    'LBL_SUBCATEGORY_VALUE' => 'Pod-kategorija:',
+    'LBL_DOC_STATUS' => 'Stanje:',
+    'LBL_DET_TEMPLATE_TYPE' => 'Tip dokumenta:',
+    'LBL_DOC_DESCRIPTION' => 'Opis:',
+    'LBL_DOC_ACTIVE_DATE' => 'Datum objave:',
+    'LBL_DOC_EXP_DATE' => 'Datum veljavnosti:',
+
+    //document list view.
+    'LBL_LIST_FORM_TITLE' => 'Seznam dokumentov:',
+    'LBL_LIST_DOCUMENT' => 'Dokument',
+    'LBL_LIST_CATEGORY' => 'Kategorija',
+    'LBL_LIST_SUBCATEGORY' => 'Pod-kategorija',
+    'LBL_LIST_REVISION' => 'Različica',
+    'LBL_LIST_LAST_REV_CREATOR' => 'Objavil',
+    'LBL_LIST_LAST_REV_DATE' => 'Datum različice',
+    'LBL_LIST_VIEW_DOCUMENT' => 'Poglej',
+    'LBL_LIST_ACTIVE_DATE' => 'Datum objave',
+    'LBL_LIST_EXP_DATE' => 'Datum veljavnosti',
+    'LBL_LIST_STATUS' => 'Stanje',
+
+    //document search form.
+    'LBL_SF_CATEGORY' => 'Kategorija:',
+    'LBL_SF_SUBCATEGORY' => 'Pod-kategorija:',
+
+    'DEF_CREATE_LOG' => 'Dokument ustvarjen',
+
+    //error messages
+    'ERR_DOC_NAME' => 'Ime dokumenta',
+    'ERR_DOC_ACTIVE_DATE' => 'Datum objave',
+    'ERR_FILENAME' => 'Ime datoteke',
+
+    //sub-panel vardefs.
+    'LBL_LIST_DOCUMENT_NAME' => 'Ime dokumenta',
+
+    'LBL_EDIT_BUTTON' => 'Uredi',
+    'LBL_REMOVE' => 'Odstrani',
+
+);
