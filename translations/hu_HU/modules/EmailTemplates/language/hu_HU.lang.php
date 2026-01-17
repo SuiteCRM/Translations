8c7f99d1a2bf7d547cc7e22855fe2c631455f449
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 04:47:02 +0000

    New translations en_us.lang.php (Hungarian)

---
+++ translations/hu_HU/modules/EmailTemplates/language/hu_HU.lang.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ADD_DOCUMENT' => 'Dokumentum hozzáadása',
+    'LBL_ADD_FILE' => 'File hozzáadása',
+    'LBL_ATTACHMENTS' => 'Mellékletek',
+    'LBL_BODY' => 'Tartalom: ',
+    'LBL_CLOSE' => 'Bezárás:',
+    'LBL_DESCRIPTION' => 'Leírás:',
+    'LBL_EDIT_ALT_TEXT' => 'Alt szöveg módosítása',
+    'LBL_EMAIL_ATTACHMENT' => 'Email csatolt állományok',
+    'LBL_HIDE_ALT_TEXT' => 'Szöveg elrejtése',
+    'LBL_HTML_BODY' => 'HTML Törzs',
+    'LBL_INSERT_VARIABLE' => 'Változó beszúrása:',
+    'LBL_INSERT_URL_REF' => 'URL hivatkozás beszúrás',
+    'LBL_INSERT_TRACKER_URL' => 'Tracker URL beszúrása:',
+    'LBL_INSERT' => 'Beszúr',
+    'LBL_LIST_DATE_MODIFIED' => 'Utolsó módosítás',
+    'LBL_LIST_DESCRIPTION' => 'Leírás',
+    'LBL_LIST_FORM_TITLE' => 'Email Sablonok Listája',
+    'LBL_LIST_NAME' => 'Név',
+    'LBL_MODULE_NAME' => 'Email sablonok',
+    'LBL_MODULE_TITLE' => 'Email sablonok: Kezdőlap',
+    'LBL_NAME' => 'Név:',
+    'LBL_NEW_FORM_TITLE' => 'Email sablon létrehozása',
+    'LBL_PUBLISH' => 'Közzétesz:',
+    'LBL_RELATED_TO' => 'Kapcsolódó:',
+    'LBL_SEARCH_FORM_TITLE' => 'Email sablonok Keresése',
+    'LBL_SHOW_ALT_TEXT' => 'Egyszerű szöveg megjelenítése',
+    'LBL_SUBJECT' => 'Tárgy:',
+    'LBL_SUITE_DOCUMENT' => 'Dokumentum',
+    'LBL_UPLOAD_FILE' => 'Fájl feltöltése',
+    'LBL_TEXT_BODY' => 'Szövegestörzs',
+    'LBL_USERS' => 'Felhasználók',
+
+    'LNK_EMAIL_TEMPLATE_LIST' => 'Email sablonok megtekintése',
+    'LNK_IMPORT_NOTES' => 'Jegyzetek importálása',
+    'LNK_NEW_EMAIL_TEMPLATE' => 'Email sablon létrehozása',
+    'LNK_NEW_EMAIL' => 'Email archiválása',
+    'LNK_NEW_SEND_EMAIL' => 'Levélírás',
+    'LNK_SENT_EMAIL_LIST' => 'Email küldése',
+    'LNK_VIEW_CALENDAR' => 'Ma',
+    // for Inbox
+    'LBL_NEW' => 'Új',
+    'LNK_MY_DRAFTS' => 'Piszkozataim',
+    'LNK_MY_INBOX' => 'Leveleim',
+    'LBL_TEXT_ONLY' => 'Csak Szöveg',
+    'LBL_SEND_AS_TEXT' => 'Csak Szöveget Küld',
+    'LBL_ACCOUNT' => 'Ügyfél',
+    'LBL_FROM_NAME' => 'Feladó neve',
+    'LBL_PLAIN_TEXT' => 'Egyszerű szöveg',
+    'LBL_CREATED_BY' => 'Létrehozta',
+    'LBL_PUBLISHED' => 'Megjelent',
+    'LNK_VIEW_MY_INBOX' => 'Emailjeim megtekintése',
+    'LBL_ASSIGNED_TO_ID' => 'Felelős',
+    'LBL_EDIT_LAYOUT' => 'Elrendezés szerkesztése' /*for 508 compliance fix*/,
+    'LBL_SELECT' => 'Kiválasztás' /*for 508 compliance fix*/,
+    'LBL_ID_FF_CLEAR' => 'Alaphelyzet' /*for 508 compliance fix*/,
+    'LBL_TYPE' => 'Típus',
+    'LBL_WIDTH' => 'Alapértelmezett szélesség',
+    'LNK_IMPORT_CAMPAIGNS' => 'Kampány Importálása',
+);
