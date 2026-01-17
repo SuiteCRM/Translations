6b6b66384e7e9c342a6462208edc7e0fa10d1bbf
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 03:17:02 +0000

    New translations en_us.lang.php (Portuguese)

---
+++ translations/pt_PT/modules/jjwg_Areas/language/pt_PT.lang.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings['LBL_ASSIGNED_TO_ID'] = 'ID do utilizador atribuído';
+$mod_strings['LBL_ASSIGNED_TO_NAME'] = 'Utilizador';
+$mod_strings['LBL_ID'] = 'ID';
+$mod_strings['LBL_DATE_ENTERED'] = 'Data de criação';
+$mod_strings['LBL_DATE_MODIFIED'] = 'Data de alteração';
+$mod_strings['LBL_MODIFIED'] = 'Alterado por';
+$mod_strings['LBL_MODIFIED_NAME'] = 'Alterado por (nome)';
+$mod_strings['LBL_CREATED'] = 'Criado por';
+$mod_strings['LBL_DESCRIPTION'] = 'Descrição';
+$mod_strings['LBL_DELETED'] = 'Eliminado';
+$mod_strings['LBL_NAME'] = 'Nome';
+$mod_strings['LBL_CREATED_USER'] = 'Criado pelo utilizador';
+$mod_strings['LBL_MODIFIED_USER'] = 'Alterado pelo utilizador';
+$mod_strings['LBL_LIST_NAME'] = 'Nome';
+$mod_strings['LBL_LIST_FORM_TITLE'] = 'Lista das áreas';
+$mod_strings['LBL_MODULE_NAME'] = 'Áreas';
+$mod_strings['LBL_MODULE_TITLE'] = 'Áreas';
+$mod_strings['LBL_HOMEPAGE_TITLE'] = 'Minhas áreas';
+$mod_strings['LNK_NEW_RECORD'] = 'Criar áreas';
+$mod_strings['LNK_LIST'] = 'Ver áreas';
+$mod_strings['LBL_SEARCH_FORM_TITLE'] = 'Pesquisar áreas';
+$mod_strings['LBL_HISTORY_SUBPANEL_TITLE'] = 'Ver histórico';
+$mod_strings['LBL_ACTIVITIES_SUBPANEL_TITLE'] = 'Atividades';
+$mod_strings['LBL_NEW_FORM_TITLE'] = 'Novas áreas';
+$mod_strings['LBL_CITY'] = 'Cidade';
+$mod_strings['LBL_STATE'] = 'Concelho';
+$mod_strings['LBL_COUNTRY'] = 'País';
+$mod_strings['LBL_COORDINATES'] = 'Coordenadas';
+$mod_strings['LBL_LIST_ASSIGNED_USER'] = 'Utilizador';
+$mod_strings['LBL_AREA_MAP'] = 'Mapa da área';
+
+$mod_strings['LBL_AREA_EDIT_TITLE'] = 'Instruções para a criação de áreas:';
+$mod_strings['LBL_AREA_EDIT_DESC_1'] = 'Clicar com o botão esquerdo do rato, no sentido dos ponteiros do relógio, para criar delimitadores para a área.';
+$mod_strings['LBL_AREA_EDIT_DESC_2'] = 'Clique no primeiro ponto do marcador para fechar a área do polígono.';
+$mod_strings['LBL_AREA_EDIT_RESET'] = 'Repor';
+$mod_strings['LBL_AREA_EDIT_USE_AREA_COORDINATES'] = 'Utilize as coordenadas da área';
+$mod_strings['LBL_AREA_EDIT_COORDINATE_RESULTS'] = 'Coordenar resultados (long,lat,elev):';
+$mod_strings['LBL_JJWG_MAPS_JJWG_AREAS_FROM_JJWG_MAPS_TITLE'] = 'Mapas';
