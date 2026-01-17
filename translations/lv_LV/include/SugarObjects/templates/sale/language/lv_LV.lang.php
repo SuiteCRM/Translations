545a38211ca67535e27d563ff1436bf2560dffa3
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 03:58:57 +0000

    New translations en_us.lang.php (Latvian)

---
+++ translations/lv_LV/include/SugarObjects/templates/sale/language/lv_LV.lang.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_MODULE_NAME' => 'Pārdošana',
+    'LBL_MODULE_TITLE' => 'Pārdošana: Sākums',
+    'LBL_SEARCH_FORM_TITLE' => 'Meklēšana',
+    'LBL_LIST_FORM_TITLE' => 'Pārdošanas saraksts',
+    'LBL_NAME' => 'Virsraksts',
+    'LBL_LIST_SALE_NAME' => 'Vārds:',
+    'LBL_LIST_ACCOUNT_NAME' => 'Konta nosaukums',
+    'LBL_LIST_AMOUNT' => 'Summa',
+    'LBL_LIST_DATE_CLOSED' => 'Slēgt',
+    'LBL_LIST_SALE_STAGE' => 'Pārdošanas posms',
+    'LBL_ACCOUNT_ID' => 'Uzņēmuma ID',
+    //DON'T CONVERT THESE THEY ARE MAPPINGS
+    'db_name' => 'LBL_NAME',
+    //END DON'T CONVERT
+    'LBL_ACCOUNT_NAME' => 'Uzņēmuma nosaukums:',
+    'LBL_AMOUNT' => 'Summa:',
+    'LBL_AMOUNT_USDOLLAR' => 'Summa pamatvalūtā:',
+    'LBL_CURRENCY' => 'Valūta:',
+    'LBL_DATE_CLOSED' => 'Paredzēts slēgšanas datums:',
+    'LBL_TYPE' => 'Veids:',
+    'LBL_CAMPAIGN' => 'Kampaņa:',
+    'LBL_LEADS_SUBPANEL_TITLE' => 'Interesenti',
+    'LBL_PROJECTS_SUBPANEL_TITLE' => 'Projekti',
+    'LBL_NEXT_STEP' => 'Nākamais solis:',
+    'LBL_LEAD_SOURCE' => 'Interesenta avots:',
+    'LBL_SALES_STAGE' => 'Pārdošanas posms:',
+    'LBL_PROBABILITY' => 'Varbūtība (%):',
+    'LBL_DESCRIPTION' => 'Apraksts:',
+    'LBL_DUPLICATE' => 'Iespējams pārdošanas dublikāts',
+    'MSG_DUPLICATE' => 'Pārdošanas ieraksts kuru Jūs veidojat iespējams ir dublikāts jau esošam pārdošanas ierakstam.  Pārdošanas ieraksti, kuri satur līdzīgus nosaukumus ir izkārtoti zemāk<br>Klikšķiniet Saglabāt, lai turpinātu veidot jauno pārdošanu, vai klikšķiniet Atcelt, lai atgrieztos modulī neveidojot pārdošanu.',
+    'LBL_NEW_FORM_TITLE' => 'Izveidot pārdošanu',
+    'ERR_DELETE_RECORD' => 'Jānorāda ieraksta numurs, lai dzēstu pārdošanu.',
+    'LBL_DEFAULT_SUBPANEL_TITLE' => 'Pārdošana',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Darbības',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Vēsture',
+
+    'LBL_CONTACTS_SUBPANEL_TITLE' => 'Ielūgtie',
+    'LBL_ASSIGNED_TO_NAME' => 'Piešķirts lietotājam:',
+    'LBL_LIST_ASSIGNED_TO_NAME' => 'Piešķirts lietotājam',
+    'LBL_ASSIGNED_TO_ID' => 'Piešķirts lietotājam ID:',
+    'LBL_MODIFIED_NAME' => 'Modificēja lietotājs',
+    'LBL_SALE_INFORMATION' => 'Pārdošanas informācija',
+    'LBL_CURRENCY_NAME' => 'Valūtas nosaukums',
+    'LBL_CURRENCY_SYMBOL' => 'Valūtas simbols:',
+    'LBL_EDIT_BUTTON' => 'Rediģēt',
+    'LBL_REMOVE' => 'Izņemt',
+
+);
