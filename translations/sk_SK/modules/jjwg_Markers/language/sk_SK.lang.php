4ff25319303561498f947578f8b4f97b5aa702f0
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 03:21:14 +0000

    New translations en_us.lang.php (Slovak)

---
+++ translations/sk_SK/modules/jjwg_Markers/language/sk_SK.lang.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings['LBL_ASSIGNED_TO_ID'] = 'Priradené ID používateľa';
+$mod_strings['LBL_ASSIGNED_TO_NAME'] = 'Užívateľ';
+$mod_strings['LBL_ID'] = 'ID';
+$mod_strings['LBL_DATE_ENTERED'] = 'Dátum vytvorenia';
+$mod_strings['LBL_DATE_MODIFIED'] = 'Dátum úpravy';
+$mod_strings['LBL_MODIFIED'] = 'Upravil';
+$mod_strings['LBL_MODIFIED_NAME'] = 'Upravil';
+$mod_strings['LBL_CREATED'] = 'Vytvoril';
+$mod_strings['LBL_DESCRIPTION'] = 'Popis';
+$mod_strings['LBL_DELETED'] = 'Vymazané';
+$mod_strings['LBL_NAME'] = 'Meno';
+$mod_strings['LBL_CREATED_USER'] = 'Vytvoril';
+$mod_strings['LBL_MODIFIED_USER'] = 'Upravil';
+$mod_strings['LBL_LIST_NAME'] = 'Meno';
+$mod_strings['LBL_LIST_FORM_TITLE'] = 'Zoznam značiek';
+$mod_strings['LBL_MODULE_NAME'] = 'Značky';
+$mod_strings['LBL_MODULE_TITLE'] = 'Značky';
+$mod_strings['LBL_HOMEPAGE_TITLE'] = 'Moje značky';
+$mod_strings['LNK_NEW_RECORD'] = 'Vytvoriť značky';
+$mod_strings['LNK_LIST'] = 'Zobraziť značky';
+$mod_strings['LNK_IMPORT_JJWG_MARKERS'] = 'Import značiek';
+$mod_strings['LBL_SEARCH_FORM_TITLE'] = 'Vyhľadávanie značky';
+$mod_strings['LBL_HISTORY_SUBPANEL_TITLE'] = 'Zobraziť Históriu';
+$mod_strings['LBL_ACTIVITIES_SUBPANEL_TITLE'] = 'Aktivity';
+$mod_strings['LBL_NEW_FORM_TITLE'] = 'Nové značky';
+$mod_strings['LBL_CITY'] = 'Mesto';
+$mod_strings['LBL_STATE'] = 'Štát';
+$mod_strings['LBL_COUNTRY'] = 'Krajina';
+$mod_strings['LBL_JJWG_MAPS_LAT'] = 'Zemepisná šírka';
+$mod_strings['LBL_JJWG_MAPS_LNG'] = 'Zemepisná dĺžka';
+$mod_strings['LBL_MARKER_IMAGE'] = 'Typ obrázka značky';
+$mod_strings['LBL_LIST_ASSIGNED_USER'] = 'Priradený k';
+
+$mod_strings['LBL_MARKER_MARKER_POSITION'] = 'Poloha značky (zemepisná šírka, zemepisná dĺžka):';
+$mod_strings['LBL_MARKER_CLOSEST_MATCHING_ADDRESS'] = 'Najbližšia zodpovedajúca adresa:';
+$mod_strings['LBL_MARKER_MARKER_STATUS'] = 'Stav značky';
+$mod_strings['LBL_MARKER_EDIT_DESCRIPTION'] = 'Kliknite a presuňte značku.';
+$mod_strings['LBL_JJWG_MAPS_JJWG_MARKERS_FROM_JJWG_MAPS_TITLE'] = 'Mapy';
