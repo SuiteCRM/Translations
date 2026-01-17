9f93187bcd698d4a9920d77add797f4d1495f1f2
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 02:20:33 +0000

    New translations en_us.lang.php (Romanian)

---
+++ translations/ro_RO/modules/AOS_PDF_Templates/language/ro_RO.lang.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID' => 'Id atribuit utilizatorului',
+    'LBL_ASSIGNED_TO_NAME' => 'Atribuit lui',
+    'LBL_ID' => 'ID-ul',
+    'LBL_DATE_ENTERED' => 'Data creării',
+    'LBL_DATE_MODIFIED' => 'Data modificării',
+    'LBL_MODIFIED' => 'Modificat de',
+    'LBL_MODIFIED_NAME' => 'Nume modificant',
+    'LBL_CREATED' => 'Creat De',
+    'LBL_DESCRIPTION' => 'Corp',
+    'LBL_HEADER' => 'Antet',
+    'LBL_FOOTER' => 'Subsol',
+    'LBL_DELETED' => 'Şters',
+    'LBL_NAME' => 'Nume',
+    'LBL_CREATED_USER' => 'Creat de Utilizator',
+    'LBL_MODIFIED_USER' => 'Modificat de utilizator',
+    'LBL_LIST_FORM_TITLE' => 'Lista de șabloane PDF',
+    'LBL_MODULE_NAME' => 'Șabloane PDF',
+    'LBL_MODULE_TITLE' => 'Șabloane PDF: Acasă',
+    'LBL_HOMEPAGE_TITLE' => 'Șabloanele mele PDF',
+    'LNK_NEW_RECORD' => 'Creați Șablon PDF',
+    'LNK_LIST' => 'Vizualizați Șabloane PDF',
+    'LBL_SEARCH_FORM_TITLE' => 'Căutați Șabloane PDF',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Vizualizare Istoric',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Activităţi',
+    'LBL_NEW_FORM_TITLE' => 'Nou Șablon PDF',
+    'LBL_TYPE' => 'Tip',
+    'LBL_ACTIVE' => 'Activ',
+    'LBL_BUTTON_INSERT' => 'Inserează',
+    'LBL_WARNING_OVERWRITE' => 'Atenție, aceasta opțiune va suprascrie activitatea curent',
+    'LBL_INSERT_FIELDS' => 'Inserați Câmpuri',
+
+    'LBL_SAMPLE' => 'Încarcă Eșantion',
+    'LBL_PAGE' => 'Pagina',
+    'LBL_PREPARED_FOR' => 'Pregătită pentru',
+    'LBL_PREPARED_BY' => 'Pregătită de',
+    'LBL_QUOTE_SAMPLE' => 'Mostră de Ofertă',
+    'LBL_INVOICE_SAMPLE' => 'Mostră factură',
+    'LBL_ACCOUNT_SAMPLE' => 'Exemplu de Cont',
+    'LBL_CONTACT_SAMPLE' => 'Mostră Contact',
+    'LBL_LEAD_SAMPLE' => 'Nume conversiei',
+    'LBL_ANY_STREET' => 'Orice stradă',
+    'LBL_ANY_TOWN' => 'Orice oraș',
+    'LBL_ANY_WHERE' => 'Oriunde',
+
+    'LBL_QUOTE_GROUP_SAMPLE' => 'Exemplu grup de oferte',
+    'LBL_INVOICE_GROUP_SAMPLE' => 'Exemplu de grup de facturi',
+    'LBL_MARGIN_LEFT' => 'Margine stânga',
+    'LBL_MARGIN_RIGHT' => 'Margine dreapta',
+    'LBL_MARGIN_TOP' => 'Margine sus',
+    'LBL_MARGIN_BOTTOM' => 'Margine jos',
+    'LBL_MARGIN_HEADER' => 'Margine antet',
+    'LBL_MARGIN_FOOTER' => 'Margine subsol',
+    'LBL_EDITVIEW_PANEL1' => 'Margini',
+    'LBL_DETAILVIEW_PANEL1' => 'Margini',
+    'LBL_PAGE_SIZE' => 'Dimensiunea pagină',
+    'LBL_ORIENTATION' => 'Orientare',
+);
