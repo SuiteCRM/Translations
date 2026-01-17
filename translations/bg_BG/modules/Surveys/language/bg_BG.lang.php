ba9d9c3ce66913ef06cd6e77224b51343218f443
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 02:32:29 +0000

    New translations en_us.lang.php (Bulgarian)

---
+++ translations/bg_BG/modules/Surveys/language/bg_BG.lang.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID'                                     => 'Възложен на оператор с идентификационен номер',
+    'LBL_ADD_OPTION'                                         => 'Add new option',
+    'LBL_ASSIGNED_TO_NAME'                                   => 'Потребител',
+    'LBL_SECURITYGROUPS'                                     => 'Security Groups',
+    'LBL_SECURITYGROUPS_SUBPANEL_TITLE'                      => 'Security Groups',
+    'LBL_ID'                                                 => 'Идентификационен номер',
+    'LBL_DATE_ENTERED'                                       => 'Създадено на',
+    'LBL_DATE_MODIFIED'                                      => 'Модифицирано на',
+    'LBL_MODIFIED'                                           => 'Модифицирано от',
+    'LBL_MODIFIED_ID'                                        => 'Модифицирано от',
+    'LBL_MODIFIED_NAME'                                      => 'Модифицирано от',
+    'LBL_CREATED'                                            => 'Създадено от',
+    'LBL_CREATED_ID'                                         => 'Създадено от',
+    'LBL_DESCRIPTION'                                        => 'Описание',
+    'LBL_DELETED'                                            => 'Изтрити',
+    'LBL_NAME'                                               => 'Име',
+    'LBL_CREATED_USER'                                       => 'Създадено от потребител',
+    'LBL_MODIFIED_USER'                                      => 'Модифицирано от потребител',
+    'LBL_LIST_NAME'                                          => 'Име',
+    'LBL_EDIT_BUTTON'                                        => 'Редактиране',
+    'LBL_REMOVE'                                             => 'Изтрий',
+    'LBL_LIST_FORM_TITLE'                                    => 'Surveys List',
+    'LBL_MODULE_NAME'                                        => 'Surveys',
+    'LBL_MODULE_TITLE'                                       => 'Surveys',
+    'LBL_HOMEPAGE_TITLE'                                     => 'My Surveys',
+    'LNK_NEW_RECORD'                                         => 'Create Surveys',
+    'LNK_LIST'                                               => 'View Surveys',
+    'LNK_IMPORT_SURVEYS'                                     => 'Import Surveys',
+    'LBL_SEARCH_FORM_TITLE'                                  => 'Search Surveys',
+    'LBL_HISTORY_SUBPANEL_TITLE'                             => 'История',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE'                          => 'Дейности',
+    'LBL_SURVEYS_SUBPANEL_TITLE'                             => 'Surveys',
+    'LBL_NEW_FORM_TITLE'                                     => 'New Surveys',
+    'LBL_STATUS'                                             => 'Статус',
+    'LBL_SURVEY_QUESTIONS_DISPLAY'                           => 'Questions',
+    'LBL_SURVEY_URL_DISPLAY'                                 => 'Адрес',
+    'LBL_HAPPINESS_QUESTION'                                 => 'Happiness Question',
+    'LBL_CANT_EDIT_RESPONDED'                                => 'Survey questions with responses cannot be edited',
+    'LBL_VIEW_SURVEY_REPORTS'                                => 'View Survey Reports',
+    'LBL_CHECKED'                                            => 'Checked',
+    'LBL_UNCHECKED'                                          => 'Unchecked',
+    'LBL_RESPONSE_ANSWER'                                    => 'Answer',
+    'LBL_RESPONSE_CONTACT'                                   => 'Контакт',
+    'LBL_RESPONSE_TIME'                                      => 'Дата:',
+    'LBL_UNKNOWN_CONTACT'                                    => 'Unknown',
+    'LBL_RESPONSE_COUNT'                                     => 'Брой',
+    'LBL_SUBMIT_TEXT'                                        => 'Submit Text',
+    'LBL_SATISFIED_TEXT'                                     => 'Satisfied Text',
+    'LBL_NEITHER_TEXT'                                       => 'Neither Text',
+    'LBL_DISSATISFIED_TEXT'                                  => 'Dissatisfied Text',
+    'LBL_SURVEYS_SURVEYQUESTIONS_FROM_SURVEYQUESTIONS_TITLE' => 'Survey Questions',
+    'LBL_SURVEYS_SURVEYRESPONSES_FROM_SURVEYRESPONSES_TITLE' => 'Survey Responses',
+    'LBL_SHOW_RESPONSES' => 'Show responses',
+    'LBL_HIDE_RESPONSES' => 'Hide responses',
+    'LBL_SURVEY_QUESTION' => 'Question',
+    'LBL_SURVEY_TEXT' => 'Text',
+    'LBL_SURVEY_TYPE' => 'Тип',
+    'LBL_SURVEY_ACTIONS' => 'Меню',
+    'LBL_SURVEY_NEW_QUESTION' => 'New Question',
+    'LBL_SURVEY_RESPONSES' => 'New Question',
+    'LBL_SURVEYS_SENT' => 'Surveys Sent:',
+    'LBL_SURVEY_DISTINCT' => 'Distinct Surveys Sent:',
+    'LBL_DRAFT' => 'Работен вариант',
+    'LBL_SUBMIT' => 'Изпрати',
+    'LBL_STARS' => 'Stars',
+    'LBL_SURVEY_CLOSE_RESPONSE' => 'Thanks for your interest but this survey is now closed.',
+    'LBL_SURVEY_THANKS' => 'Thanks for completing this survey.',
+);
