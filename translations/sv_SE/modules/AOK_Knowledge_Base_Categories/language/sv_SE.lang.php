12cba977cae38fbfb4466b865bc44931dc0321fc
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 03:25:04 +0000

    New translations en_us.lang.php (Swedish)

---
+++ translations/sv_SE/modules/AOK_Knowledge_Base_Categories/language/sv_SE.lang.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID' => 'Tilldelat användar-id',
+    'LBL_ASSIGNED_TO_NAME' => 'Tilldelad till',
+    'LBL_ID' => 'ID',
+    'LBL_DATE_ENTERED' => 'Datum Skapad',
+    'LBL_DATE_MODIFIED' => 'Datum Ändrad',
+    'LBL_MODIFIED' => 'Ändrad av',
+    'LBL_MODIFIED_ID' => 'Ändrad Av Id',
+    'LBL_MODIFIED_NAME' => 'Ändrad av Namn',
+    'LBL_CREATED' => 'Skapad av',
+    'LBL_CREATED_ID' => 'Skapad Av Id',
+    'LBL_DESCRIPTION' => 'Beskrivning',
+    'LBL_DELETED' => 'Raderad',
+    'LBL_NAME' => 'Namn',
+    'LBL_CREATED_USER' => 'Skapad av Användare',
+    'LBL_MODIFIED_USER' => 'Ändrad av Användare',
+    'LBL_LIST_NAME' => 'Namn',
+    'LBL_EDIT_BUTTON' => 'Redigera',
+    'LBL_REMOVE' => 'Ta bort',
+    'LBL_LIST_FORM_TITLE' => 'KB-kategorilista',
+    'LBL_MODULE_NAME' => 'KB Kategorier',
+    'LBL_MODULE_TITLE' => 'KB Kategorier',
+    'LBL_HOMEPAGE_TITLE' => 'Mina KB-kategorier',
+    'LNK_NEW_RECORD' => 'Skapa KB-kategorier',
+    'LNK_LIST' => 'Visa KB-kategorier',
+    'LNK_IMPORT_AOK_KB_CATEGORIES' => 'Importera KB-kategorier',
+    'LBL_SEARCH_FORM_TITLE' => 'Sök KB-kategorier',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Visa historik',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Aktiviteter',
+    'LBL_AOK_KB_CATEGORIES_SUBPANEL_TITLE' => 'KB Kategorier',
+    'LBL_NEW_FORM_TITLE' => 'Nya KB-kategorier',
+);
+
+$mod_strings['LBL_AOK_KB_TITLE'] = 'Kunskapsbas';
