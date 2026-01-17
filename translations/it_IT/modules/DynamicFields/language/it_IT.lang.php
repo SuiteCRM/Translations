9d0e2b75256b8ace9df7a5b02b44235943b10d66
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 02:56:31 +0000

    New translations en_us.lang.php (Italian)

---
+++ translations/it_IT/modules/DynamicFields/language/it_IT.lang.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LNK_NEW_CALL' => 'Schedula chiamata',
+    'LNK_NEW_MEETING' => 'Schedula appuntamento',
+    'LNK_NEW_TASK' => 'Nuovo Compito',
+    'LNK_NEW_NOTE' => 'Nuova Nota o Allegato',
+    'LNK_NEW_EMAIL' => 'Archivia Email',
+    'LNK_CALL_LIST' => 'Chiamate',
+    'LNK_MEETING_LIST' => 'Appuntamenti',
+    'LNK_TASK_LIST' => 'Compiti',
+    'LNK_NOTE_LIST' => 'Note',
+    'LBL_ADD_FIELD' => 'Aggiungi Campo:',
+    'LBL_SEARCH_FORM_TITLE' => 'Cerca Modulo',
+    'COLUMN_TITLE_NAME' => 'Nome Campo',
+    'COLUMN_TITLE_DISPLAY_LABEL' => 'Mostra Etichetta',
+    'COLUMN_TITLE_LABEL_VALUE' => 'Valore Etichetta',
+    'COLUMN_TITLE_LABEL' => 'Sistema Etichette',
+    'COLUMN_TITLE_DATA_TYPE' => 'Tipo Dato',
+    'COLUMN_TITLE_MAX_SIZE' => 'Dimensione Massima',
+    'COLUMN_TITLE_HELP_TEXT' => 'Testo di Aiuto',
+    'COLUMN_TITLE_COMMENT_TEXT' => 'Commento',
+    'COLUMN_TITLE_REQUIRED_OPTION' => 'Campo Richiesto',
+    'COLUMN_TITLE_DEFAULT_VALUE' => 'Valore predefinito',
+    'COLUMN_TITLE_FRAME_HEIGHT' => 'Altezza IFrame',
+    'COLUMN_TITLE_HTML_CONTENT' => 'HTML',
+    'COLUMN_TITLE_URL' => 'URL Predefinito',
+    'COLUMN_TITLE_AUDIT' => 'Audit',
+    'COLUMN_TITLE_MIN_VALUE' => 'Valore Minimo',
+    'COLUMN_TITLE_MAX_VALUE' => 'Valore Massimo',
+    'COLUMN_TITLE_LABEL_ROWS' => 'Righe',
+    'COLUMN_TITLE_LABEL_COLS' => 'Colonne',
+    'COLUMN_TITLE_DISPLAYED_ITEM_COUNT' => '# Elementi visualizzati',
+    'COLUMN_TITLE_AUTOINC_NEXT' => 'Prossimo Valore Auto Incrementato',
+    'COLUMN_DISABLE_NUMBER_FORMAT' => 'Disattiva Formato',
+    'COLUMN_TITLE_ENABLE_RANGE_SEARCH' => 'Attiva ricerca ad intervallo',
+    'LBL_DROP_DOWN_LIST' => 'Lista Drop Down',
+    'LBL_RADIO_FIELDS' => 'Campi Radio',
+    'LBL_MULTI_SELECT_LIST' => 'Lista Multiselezione',
+    'COLUMN_TITLE_PRECISION' => 'Precisione',
+    'LBL_MODULE' => 'Modulo',
+    'COLUMN_TITLE_MASS_UPDATE' => 'Aggiornamento di Massa',
+    'COLUMN_TITLE_IMPORTABLE' => 'Importabile',
+    'COLUMN_TITLE_DUPLICATE_MERGE' => 'Unisci Duplicati',
+    'LBL_LABEL' => 'Etichetta',
+    'LBL_DATA_TYPE' => 'Tipo Dato',
+    'LBL_DEFAULT_VALUE' => 'Valore predefinito',
+    'ERR_RESERVED_FIELD_NAME' => "Parola chiave riservata",
+    'ERR_SELECT_FIELD_TYPE' => 'Si prega di selezionare un Tipo Campo',
+    'ERR_FIELD_NAME_ALREADY_EXISTS' => 'Nome Campo già esistente',
+    'LBL_BTN_ADD' => 'Aggiungi',
+    'LBL_BTN_EDIT' => 'Modifica',
+    'LBL_GENERATE_URL' => 'Crea URL',
+    'LBL_CALCULATED' => 'Valore Calcolato',
+    'LBL_LINK_TARGET' => 'Apri il collegamento in',
+    'LBL_IMAGE_WIDTH' => 'Larghezza',
+    'LBL_IMAGE_HEIGHT' => 'Altezza',
+    'LBL_IMAGE_BORDER' => 'Bordi',
+    'LBL_HELP' => 'Aiuto' /*for 508 compliance fix*/,
+    'COLUMN_TITLE_INLINE_EDIT_TEXT' => 'Editor di linea',
+    'COLUMN_TITLE_PARENT_ENUM' => 'DropDown Padre',
+    'LBL_INSERT_FIELD' => 'Inserisci Campo',
+);
