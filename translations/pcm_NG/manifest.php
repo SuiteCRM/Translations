5ea9f8847ab7b87d94281f88c77060cd0dd88853
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 04:39:54 +0000

    New translations manifest.php (Nigerian Pidgin)

---
+++ translations/pcm_NG/manifest.php
@@ -0,0 +1,25 @@
+<?php
+/******************************************
+ * SuiteCRM Translations
+ * @URL: https://crowdin.com/project/suitecrmtrans
+ * @author SuiteCRM Community via Crowdin
+ ******************************************/
+
+$manifest = array(
+    'name' => 'Nigerian Pidgin (Nigeria)',
+    'description' => 'Translation: crowdin.com/project/suitecrmtranslations',
+    'type' => 'langpack',
+    'is_uninstallable' => 'Yes',
+    'author' => 'SuiteCRM Community',
+    'version' => '7.15-1',
+    'published_date' => '2026-01-16',
+);
+$installdefs = array(
+    'id' => 'pcm_NG',
+    'image_dir' => '<basepath>/images',
+    'copy' => array(
+        array('from' => '<basepath>/include', 'to' => 'include'),
+        array('from' => '<basepath>/modules', 'to' => 'modules'),
+        array('from' => '<basepath>/install', 'to' => 'install'),
+    )
+);
