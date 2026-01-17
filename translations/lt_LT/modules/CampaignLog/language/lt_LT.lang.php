dc691082951216c672d4fc7076a69f0dddea5f99
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 03:04:42 +0000

    New translations en_us.lang.php (Lithuanian)

---
+++ translations/lt_LT/modules/CampaignLog/language/lt_LT.lang.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_LIST_ID' => 'Adresatų sąrašo ID',
+    'LBL_ID' => 'Id',
+    'LBL_TARGET_TRACKER_KEY' => 'Adresato sekimo raktas',
+    'LBL_TARGET_ID' => 'Adresato ID',
+    'LBL_TARGET_TYPE' => 'Adresato tipas',
+    'LBL_ACTIVITY_TYPE' => 'Veiklos tipas',
+    'LBL_ACTIVITY_DATE' => 'Veiklos data',
+    'LBL_RELATED_ID' => 'Susijęs Id',
+    'LBL_RELATED_TYPE' => 'Susijęs tipas',
+    'LBL_DELETED' => 'Ištrintas',
+    'LBL_MODULE_NAME' => 'Kampanijos istorija',
+    'LBL_LIST_RECIPIENT_EMAIL' => 'Gavėjo adresas:',
+    'LBL_LIST_RECIPIENT_NAME' => 'Gavėjo vardas',
+    'LBL_ARCHIVED' => 'Archyvuotas',
+    'LBL_HITS' => 'Paspaudimai',
+
+    'LBL_CAMPAIGN_NAME' => 'Vardas:',
+    'LBL_CAMPAIGN' => 'Kampanija:',
+    'LBL_NAME' => 'Vardas:',
+    'LBL_INVITEE' => 'Kontaktas',
+    'LBL_LIST_CAMPAIGN_NAME' => 'Kampanija',
+    'LBL_LIST_STATUS' => 'Būsena',
+    'LBL_LIST_TYPE' => 'Tipas:',
+    'LBL_LIST_END_DATE' => 'Pabaigos laikas',
+    'LBL_DATE_ENTERED' => 'Įvedimo data',
+    'LBL_DATE_MODIFIED' => 'Redagavimo data',
+    'LBL_MODIFIED' => 'Redagavo:',
+    'LBL_CREATED' => 'Sukūrė:',
+    'LBL_TEAM' => 'Komanda:',
+    'LBL_ASSIGNED_TO' => 'Priskirtas: ',
+    'LBL_CAMPAIGN_START_DATE' => 'Pradžios laikas:',
+    'LBL_CAMPAIGN_END_DATE' => 'Pabaigos data:',
+    'LBL_CAMPAIGN_STATUS' => 'Statusas:',
+    'LBL_CAMPAIGN_BUDGET' => 'Biudžetas:',
+    'LBL_CAMPAIGN_EXPECTED_COST' => 'Tikėtosios išlaidos:',
+    'LBL_CAMPAIGN_ACTUAL_COST' => 'Faktinės išlaidos:',
+    'LBL_CAMPAIGN_EXPECTED_REVENUE' => 'Tikėtosios pajamos:',
+    'LBL_CAMPAIGN_TYPE' => 'Tipas:',
+    'LBL_CAMPAIGN_OBJECTIVE' => 'Tikslas:',
+    'LBL_CAMPAIGN_CONTENT' => 'Aprašymas:',
+    'LBL_CREATED_LEAD' => 'Sukurtas potencialus kontaktas',
+    'LBL_CREATED_CONTACT' => 'Sukurtas kontaktas',
+    'LBL_CREATED_OPPORTUNITY' => 'Sukurti Pardavimai',
+    'LBL_TARGETED_USER' => 'Tikslinės Vartotojas',
+    'LBL_SENT_EMAIL' => 'Siųsti elektroninį laišką',
+    'LBL_LIST_FORM_TITLE' => 'Kampanijos',
+    'LBL_LIST_ACTIVITY_DATE' => 'Veiklos data',
+    'LBL_RELATED' => 'Susijęs',
+    'LBL_MORE_INFO' => 'Daugiau informacijos',
+
+    'LBL_CAMPAIGNS' => 'Kampanijos',
+    'LBL_LIST_MARKETING_NAME' => 'Marketingo id',
+
+    'LBL_MARKETING_ID' => 'Marketingo id',
+    'LBL_RELATED_NAME' => 'Related Name',
+);
