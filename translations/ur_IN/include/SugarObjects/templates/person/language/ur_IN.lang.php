a5f69d0a629c9d32f4da860d0074bd8d31d77410
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 04:34:42 +0000

    New translations en_us.lang.php (Urdu (India))

---
+++ translations/ur_IN/include/SugarObjects/templates/person/language/ur_IN.lang.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_SALUTATION' => 'سلامتی',
+    'LBL_NAME' => 'نام',
+    'LBL_FIRST_NAME' => 'پہلا نام',
+    'LBL_LAST_NAME' => 'آخری نام',
+    'LBL_TITLE' => 'عنوان، موضع',
+    'LBL_DEPARTMENT' => 'محکمہ',
+    'LBL_DO_NOT_CALL' => 'کال نا کریں',
+    'LBL_HOME_PHONE' => 'ہوم فون',
+    'LBL_MOBILE_PHONE' => 'موبائل فون',
+    'LBL_OFFICE_PHONE' => 'آفس کا فون',
+    'LBL_OTHER_PHONE' => 'دیگر فون',
+    'LBL_FAX_PHONE' => 'فیکس',
+    'LBL_EMAIL_ADDRESS' => 'ای میل ایڈریس(ز)',
+    'LBL_PRIMARY_ADDRESS' => 'ابتدائی پتہ',
+    'LBL_PRIMARY_ADDRESS_STREET' => 'ابتدائی پتہ',
+    'LBL_PRIMARY_ADDRESS_STREET_2' => 'ابتدائی پتہ سڈک 2:',
+    'LBL_PRIMARY_ADDRESS_STREET_3' => 'ابتدائی پتہ سڈک 3:',
+    'LBL_PRIMARY_ADDRESS_CITY' => 'ابتدائی شہر',
+    'LBL_PRIMARY_ADDRESS_STATE' => 'ابتدائی ریاست',
+    'LBL_PRIMARY_ADDRESS_POSTALCODE' => 'پرائمری پوسٹل کوڈ',
+    'LBL_PRIMARY_ADDRESS_COUNTRY' => 'ابتدائی ملک کا پتہ:',
+    'LBL_ALT_ADDRESS' => 'متبادل پتہ',
+    'LBL_ALT_ADDRESS_STREET' => 'متبادل پتہ',
+    'LBL_ALT_ADDRESS_STREET_2' => 'متبادل پتہ سڈک 2:',
+    'LBL_ALT_ADDRESS_STREET_3' => 'متبادل پتہ سڈک 3:',
+    'LBL_ALT_ADDRESS_CITY' => 'متبادل شہر',
+    'LBL_ALT_ADDRESS_STATE' => 'متبادل ریاست',
+    'LBL_ALT_ADDRESS_POSTALCODE' => 'متبادل پوسٹل کوڈ',
+    'LBL_ALT_ADDRESS_COUNTRY' => 'متبادل ملک',
+    'LBL_PRIMARY_STREET' => 'پتہ',
+    'LBL_ALT_STREET' => 'دیگر پتہ',
+    'LBL_STREET' => 'دیگر پتہ',
+    'LBL_CITY' => 'شہر',
+    'LBL_STATE' => 'ریاست',
+    'LBL_POSTAL_CODE' => 'پوسٹل کوڈ',
+    'LBL_COUNTRY' => 'ملک',
+    'LBL_CONTACT_INFORMATION' => 'رابطے کی معلومات',
+    'LBL_ADDRESS_INFORMATION' => 'پتہ(ے)',
+    'LBL_ASSIGNED_TO_NAME' => 'صارف',
+    'LBL_OTHER_EMAIL_ADDRESS' => 'دوسرے ای میل:',
+    'LBL_ASSISTANT' => 'مددگار',
+    'LBL_ASSISTANT_PHONE' => 'مددگار فون',
+    'LBL_WORK_PHONE' => 'کام کا فون',
+    'LNK_IMPORT_VCARD' => 'وی کارڈ سے تخلیق کیجئے',
+    'LBL_ANY_EMAIL' => 'کوئی بھی ای میل',
+    'LBL_EMAIL_NON_PRIMARY' => 'غیر پرائمری ای میل',
+    'LBL_PHOTO' => 'تصویر',
+    'LBL_EDIT_BUTTON' => 'ردوبدل',
+    'LBL_REMOVE' => 'نکال دیں',
+
+    //Lawful Basis labels
+    'LBL_LAWFUL_BASIS' => 'Lawful Basis',
+    'LBL_DATE_REVIEWED' => 'Lawful Basis Date Reviewed',
+    'LBL_LAWFUL_BASIS_SOURCE' => 'Lawful Basis Source',
+    'LBL_CONSENT' => 'Consent',
+    //End Lawful Basis labels
+);
