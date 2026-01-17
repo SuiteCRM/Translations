e31de64016ae266ebe23d2fbb4d829e30e84a0aa
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 04:11:06 +0000

    New translations en_us.lang.php (Tagalog)

---
+++ translations/tl_PH/modules/OAuth2Tokens/language/tl_PH.lang.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_MODULE_NAME' => 'Ang mga OAuth2 Token',
+    'LBL_MODULE_TITLE' => 'Ang mga OAuth2 Token',
+    'LBL_MODULE_ID'=> 'Ang mga OAuth2 Token',
+    'LBL_TOKEN_ID'=> 'Ang token ID',
+    'LBL_CLIENT'=> 'Ang Kliyente',
+    'LBL_TOKEN_IS_REVOKED'=> 'Ang Bawiin',
+    'LBL_ACCESS_TOKEN_TYPE'=> 'Ang uti ng Token',
+    'LBL_ACCESS_TOKEN_EXPIRES'=> 'Ma-expire ang Access sa roken',
+    'LBL_REFRESH_TOKEN_EXPIRES'=> 'Palamigin ang mga na-expire na token',
+    'LBL_GRANT_TYPE'=> 'Magbigay ng uri',
+    'LBL_SCOPES'=> 'Ang mga Saklaw',
+    'LBL_REVOKE_TOKEN'=> 'Bawiin ang token',
+    'LBL_REVOKE_TOKENS'=> 'Revoke Tokens',
+    'LBL_CLIENT_NAME'=> 'Ang pangalan ng kliyente',
+    'LBL_USER' => 'Nauugnay na user',
+    'LBL_IS_ACTIVE' => 'Mga aktibong mga token lamang',
+
+    'LNK_OAUTH2_TOKEN_LIST'=> 'Listahan ng mga OAuth2 Token',
+    'LNK_OAUTH2_CLIENT_LIST' => 'Listahan ng OAuth2 na mga Kliyente',
+    'LNK_OAUTH2_AUTHCODE_LIST' => 'List OAuth2 Authorization Codes',
+    
+    'LNK_NEW_OAUTH2_CLIENT' => 'Gumawa ng OAuth2 na Kliyente',
+    'LNK_NEW_OAUTH2_PASSWORD_CLIENT' => 'Bagong Password na Kliyente',
+    'LNK_NEW_OAUTH2_CREDENTIALS_CLIENT' => 'Bagong Kliyente ng Kredensyal na Kliyente',
+    'LNK_NEW_OAUTH2_IMPLICIT_CLIENT' => 'Bagong Pahiwatig na Kliyente',
+    'LNK_NEW_OAUTH2_AUTHORIZATION_CLIENT' => 'Bagong Awtorisasyon na Kliyente',
+
+    'LBL_ACCESS_TOKEN' => 'Access token',
+    'LBL_REFRESH_TOKEN' => 'Refresh token',
+    'LBL_DELETE_CONFIRMATION' => 'Are you sure you want to delete this record? The client will not be granted an authorization code any more.',
+    'LBL_REVOKE_AND_DELETE_BUTTON_LABEL' => 'Revoke and Delete',
+);
