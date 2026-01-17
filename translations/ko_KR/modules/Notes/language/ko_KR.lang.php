5a98392c0dbf849b2586a3fc6776ff13e8cef483
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 03:03:24 +0000

    New translations en_us.lang.php (Korean)

---
+++ translations/ko_KR/modules/Notes/language/ko_KR.lang.php
@@ -0,0 +1,110 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'ERR_DELETE_RECORD' => '거래처를 삭제하려면 레코드 번호를 지정해야 합니다.',
+    'LBL_ACCOUNT_ID' => '거래처 ID:',
+    'LBL_CASE_ID' => '사례ID:',
+    'LBL_CLOSE' => '종료:',
+    'LBL_CONTACT_ID' => '거래처 담당자 ID:',
+    'LBL_CONTACT_NAME' => '연락처:',
+    'LBL_DEFAULT_SUBPANEL_TITLE' => '노트',
+    'LBL_DESCRIPTION' => '노트',
+    'LBL_EMAIL_ADDRESS' => '이메일 주소:',
+    'LBL_EMAIL_ATTACHMENT' => '이메일 첨부 파일',
+    'LBL_FILE_MIME_TYPE' => 'Mime 형식',
+    'LBL_FILE_URL' => '파일 URL',
+    'LBL_FILENAME' => '첨부파일:',
+    'LBL_LEAD_ID' => '리드ID:',
+    'LBL_LIST_CONTACT_NAME' => '연락처',
+    'LBL_LIST_DATE_MODIFIED' => '최종변경일',
+    'LBL_LIST_FILENAME' => '첨부',
+    'LBL_LIST_FORM_TITLE' => '노트 리스트',
+    'LBL_LIST_RELATED_TO' => '관련',
+    'LBL_LIST_SUBJECT' => '제목',
+    'LBL_LIST_STATUS' => '상태',
+    'LBL_LIST_CONTACT' => '연락처',
+    'LBL_MODULE_NAME' => '노트',
+    'LBL_MODULE_TITLE' => '노트: 홈',
+    'LBL_NEW_FORM_TITLE' => '노트 작성',
+    'LBL_NOTE_STATUS' => '노트',
+    'LBL_NOTE_SUBJECT' => '제목:',
+    'LBL_NOTES_SUBPANEL_TITLE' => '첨부파일',
+    'LBL_NOTE' => '노트:',
+    'LBL_OPPORTUNITY_ID' => '영업기회 ID:',
+    'LBL_PARENT_ID' => '패런트ID:',
+    'LBL_PARENT_TYPE' => '부모 유형',
+    'LBL_PHONE' => '전화:',
+    'LBL_PORTAL_FLAG' => '포탈에표시?',
+    'LBL_EMBED_FLAG' => '이메일 내 포함?',
+    'LBL_PRODUCT_ID' => '제품 ID:',
+    'LBL_QUOTE_ID' => '견적 ID:',
+    'LBL_RELATED_TO' => '관련처:',
+    'LBL_SEARCH_FORM_TITLE' => '노트 검색',
+    'LBL_STATUS' => '상태',
+    'LBL_SUBJECT' => '제목:',
+    'LNK_IMPORT_NOTES' => '노트읽어들이기',
+    'LNK_NEW_NOTE' => '노트작성',
+    'LNK_NOTE_LIST' => '노트',
+    'LBL_MEMBER_OF' => '멤버:',
+    'LBL_LIST_ASSIGNED_TO_NAME' => '담당자',
+    'LBL_REMOVING_ATTACHMENT' => '첨부파일 제거...',
+    'ERR_REMOVING_ATTACHMENT' => '첨부 파일 삭제 실패...',
+    'LBL_CREATED_BY' => '작성자',
+    'LBL_MODIFIED_BY' => '변경됨',
+    'LBL_SEND_ANYWAYS' => '이메일의 제목이 없습니다. 그래도 보내시겠습니까/저장하시겠습니까?',
+    'LBL_NOTE_INFORMATION' => '개요', //No need to be translated in all caps. Translation used just in menu action items when using the SuiteP template
+    'LBL_MY_NOTES_DASHLETNAME' => '내 노트',
+    'LBL_EDITLAYOUT' => '레이아웃 편집' /*for 508 compliance fix*/,
+    //For export labels
+    'LBL_FIRST_NAME' => '이름',
+    'LBL_LAST_NAME' => '성',
+    'LBL_DATE_ENTERED' => '생성날짜',
+    'LBL_DATE_MODIFIED' => '수정일',
+    'LBL_DELETED' => '삭제됨',
+    'LBL_FILE_CONTENTS' => 'File Contents',
+
+    'LBL_ASCENDING' => '오름차순',
+    'LBL_DESCENDING' => '내림차순',
+);
