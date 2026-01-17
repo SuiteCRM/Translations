d575fe398b2e9264a9dd77b342b5c68b828d4799
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 03:25:53 +0000

    New translations en_us.lang.php (Swedish)

---
+++ translations/sv_SE/modules/History/language/sv_SE.lang.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_MODULE_NAME' => 'Historia',
+    'LBL_MODULE_TITLE' => 'Historik: Hem',
+    'LBL_SEARCH_FORM_TITLE' => 'Sök historik',
+    'LBL_LIST_FORM_TITLE' => 'Historia',
+    'LBL_LIST_SUBJECT' => 'Ämne',
+    'LBL_LIST_CONTACT' => 'Kontakter',
+    'LBL_LIST_RELATED_TO' => 'Relaterad till',
+    'LBL_LIST_DATE' => 'Datum',
+    'LBL_LIST_CLOSE' => 'Stäng',
+    'LBL_SUBJECT' => 'Ämne:',
+    'LBL_STATUS' => 'Status:',
+    'LBL_LOCATION' => 'Plats:',
+    'LBL_DATE_TIME' => 'Startdatum & tid:',
+    'LBL_DATE' => 'Startdatum:',
+    'LBL_TIME' => 'Starttid:',
+    'LBL_DURATION' => 'Varaktighet:',
+    'LBL_HOURS_MINS' => '(timmar/minuter)',
+    'LBL_CONTACT_NAME' => 'Kontakt namn:',
+    'LBL_DESCRIPTION' => 'Beskrivning:',
+    'LNK_NEW_CALL' => 'Schemalägg samtal',
+    'LNK_NEW_MEETING' => 'Schemalägg möte',
+    'LNK_NEW_TASK' => 'Skapa uppgift',
+    'LNK_NEW_NOTE' => 'Skapa anteckning eller bilaga',
+    'LNK_NEW_EMAIL' => 'Arkivera E-post',
+    'LNK_CALL_LIST' => 'Samtal',
+    'LNK_MEETING_LIST' => 'Möten',
+    'LNK_TASK_LIST' => 'Uppgifter',
+    'LNK_NOTE_LIST' => 'Anteckningar:',
+    'ERR_DELETE_RECORD' => 'Ett nummer på posten måste specifiseras för att ta bort kontot.',
+    'LBL_INVITEE' => 'Inbjudningar',
+    'LBL_LIST_DIRECTION' => 'Inriktning',
+    'LBL_DIRECTION' => 'Inriktning',
+    'LNK_NEW_APPOINTMENT' => 'Ny aktivitet',
+    'LNK_VIEW_CALENDAR' => 'Idag',
+    'LBL_OPEN_ACTIVITIES' => 'Öppna aktiviteter',
+    'LBL_HISTORY' => 'Historia',
+    'LBL_NEW_TASK_BUTTON_TITLE' => 'Skapa uppgift',
+    'LBL_NEW_TASK_BUTTON_LABEL' => 'Skapa uppgift',
+    'LBL_SCHEDULE_MEETING_BUTTON_TITLE' => 'Schemalägg möte',
+    'LBL_SCHEDULE_MEETING_BUTTON_LABEL' => 'Schemalägg möte',
+    'LBL_SCHEDULE_CALL_BUTTON_LABEL' => 'Schemalägg samtal',
+    'LBL_NEW_NOTE_BUTTON_TITLE' => 'Skapa anteckning eller bilaga',
+    'LBL_NEW_NOTE_BUTTON_LABEL' => 'Skapa anteckning eller bilaga',
+    'LBL_TRACK_EMAIL_BUTTON_TITLE' => 'Arkivera E-post',
+    'LBL_TRACK_EMAIL_BUTTON_LABEL' => 'Arkivera E-post',
+    'LBL_LIST_STATUS' => 'Status',
+    'LBL_LIST_DUE_DATE' => 'Förfallodag',
+    'LBL_LIST_LAST_MODIFIED' => 'Senast ändrad',
+    'LNK_IMPORT_NOTES' => 'Importera anteckningar',
+    'LBL_ACCEPT_THIS' => 'Accepteras?',
+    'LBL_DEFAULT_SUBPANEL_TITLE' => 'Historia',
+
+    'LBL_CATEGORY' => 'Kategori',
+    'LBL_LIST_CATEGORY' => 'Kategori',
+);
