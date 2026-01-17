97538c7a9f9e0ca6803dc9450043c8c64754fcd4
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 04:38:20 +0000

    New translations en_us.lang.php (English (upside down))

---
+++ translations/en_UD/modules/AM_TaskTemplates/language/en_UD.lang.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID' => 'pI ɹǝs∩ pǝuᵷᴉssⱯ',
+    'LBL_ASSIGNED_TO_NAME' => 'oʇ pǝuᵷᴉssⱯ',
+    'LBL_ID' => 'ᗡI',
+    'LBL_DATE_ENTERED' => 'pǝʇɐǝɹƆ ǝʇɐᗡ',
+    'LBL_DATE_MODIFIED' => 'pǝᴉɟᴉpoW ǝʇɐᗡ',
+    'LBL_MODIFIED' => 'ʎᗺ pǝᴉɟᴉpoW',
+    'LBL_MODIFIED_ID' => 'Modified By Id',
+    'LBL_MODIFIED_NAME' => 'ǝɯɐN ʎᗺ pǝᴉɟᴉpoW',
+    'LBL_CREATED' => 'ʎᗺ pǝʇɐǝɹƆ',
+    'LBL_CREATED_ID' => 'Created By Id',
+    'LBL_DESCRIPTION' => 'uoᴉʇdᴉɹɔsǝᗡ',
+    'LBL_DELETED' => 'pǝʇǝꞁǝᗡ',
+    'LBL_NAME' => 'Task Name',
+    'LBL_CREATED_USER' => 'ɹǝs∩ ʎq pǝʇɐǝɹƆ',
+    'LBL_MODIFIED_USER' => 'ɹǝs∩ ʎq pǝᴉɟᴉpoW',
+    'LBL_LIST_NAME' => 'ǝɯɐN',
+    'LBL_EDIT_BUTTON' => 'ʇᴉpƎ',
+    'LBL_REMOVE' => 'ǝʌoɯǝᴚ',
+    'LBL_LIST_FORM_TITLE' => 'Project Task Templates List',
+    'LBL_MODULE_NAME' => 'Project Task Templates',
+    'LBL_MODULE_TITLE' => 'Project Task Templates',
+    'LBL_HOMEPAGE_TITLE' => 'My Project Task Templates',
+    'LNK_NEW_RECORD' => 'Create Project Task Templates',
+    'LNK_LIST' => 'View Project Task Templates',
+    'LNK_IMPORT_AM_TASKTEMPLATES' => 'Import Project Task Templates',
+    'LBL_SEARCH_FORM_TITLE' => 'Search Project Task Templates',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'View History',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Activities',
+    'LBL_AM_TASKTEMPLATES_SUBPANEL_TITLE' => 'Project Task Templates',
+    'LBL_NEW_FORM_TITLE' => 'New Project Task Templates',
+    'LBL_STATUS' => 'Status',
+    'LBL_PRIORITY' => 'Priority',
+    'LBL_PERCENT_COMPLETE' => 'Percent Complete',
+    'LBL_PREDECESSORS' => 'Predecessors',
+    'LBL_MILESTONE_FLAG' => 'Milestone Flag',
+    'LBL_RELATIONSHIP_TYPE' => 'Relationship Type',
+    'LBL_TASK_NUMBER' => 'Task Number',
+    'LBL_ORDER_NUMBER' => 'Order ',
+    'LBL_ESTIMATED_EFFORT' => 'Estimated Effort (hrs)',
+    'LBL_UTILIZATION' => 'Utilization (%)',
+    'LBL_DURATION' => 'Duration (days)',
+    'LBL_AM_TASKTEMPLATES_AM_PROJECTTEMPLATES_FROM_AM_PROJECTTEMPLATES_TITLE' => 'Project Templates',
+
+    'LBL_AM_TASKTEMPLATES_AM_PROJECTTEMPLATES_FROM_AM_TASKTEMPLATES_TITLE' => 'Task Templates: Project Templates from Task Templates Title',
+);
