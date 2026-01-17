16e5e8198de9247a068e448d7973ab192d6da246
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 03:04:23 +0000

    New translations en_us.lang.php (Lithuanian)

---
+++ translations/lt_LT/modules/AOS_PDF_Templates/language/lt_LT.lang.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID' => 'Atsakingo Vartotojo Id',
+    'LBL_ASSIGNED_TO_NAME' => 'Priskirtas',
+    'LBL_ID' => 'Id',
+    'LBL_DATE_ENTERED' => 'Sukurta',
+    'LBL_DATE_MODIFIED' => 'Redagavimo data',
+    'LBL_MODIFIED' => 'Redagavo',
+    'LBL_MODIFIED_NAME' => 'Redaguotojo vardas',
+    'LBL_CREATED' => 'Sukūrė',
+    'LBL_DESCRIPTION' => 'Kūnas',
+    'LBL_HEADER' => 'Antraštė',
+    'LBL_FOOTER' => 'Apačia',
+    'LBL_DELETED' => 'Ištrintas',
+    'LBL_NAME' => 'Vardas',
+    'LBL_CREATED_USER' => 'Sukūrė vartotojas',
+    'LBL_MODIFIED_USER' => 'Redagavo vartotojas',
+    'LBL_LIST_FORM_TITLE' => 'PDF Šablonų Sąrašas',
+    'LBL_MODULE_NAME' => 'PDF šablonai',
+    'LBL_MODULE_TITLE' => 'PDF Šablonai: Pradžia',
+    'LBL_HOMEPAGE_TITLE' => 'Mano PDF Šablonai',
+    'LNK_NEW_RECORD' => 'Sukurkite PDF Šabloną',
+    'LNK_LIST' => 'Rodyti PDF Šablonus',
+    'LBL_SEARCH_FORM_TITLE' => 'Paieška PDF Šablonus',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Peržiūros istorija',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Įvykiai',
+    'LBL_NEW_FORM_TITLE' => 'Naują PDF Šabloną',
+    'LBL_TYPE' => 'Tipas:',
+    'LBL_ACTIVE' => 'Aktyvu',
+    'LBL_BUTTON_INSERT' => 'Įdėti',
+    'LBL_WARNING_OVERWRITE' => 'Įspėjimas tai užrašys ant jūsų dabartino Darbo',
+    'LBL_INSERT_FIELDS' => 'Įterpti Laukus',
+
+    'LBL_SAMPLE' => 'Užkrauti Bandomąjį',
+    'LBL_PAGE' => 'Puslapis',
+    'LBL_PREPARED_FOR' => 'Paruoštas dėl',
+    'LBL_PREPARED_BY' => 'Parengė',
+    'LBL_QUOTE_SAMPLE' => 'Quote Sample',
+    'LBL_INVOICE_SAMPLE' => 'Invoice Sample',
+    'LBL_ACCOUNT_SAMPLE' => 'Account Sample',
+    'LBL_CONTACT_SAMPLE' => 'Contact Sample',
+    'LBL_LEAD_SAMPLE' => 'Lead Sample',
+    'LBL_ANY_STREET' => 'Kita Gatvė',
+    'LBL_ANY_TOWN' => 'Kitas Miestas',
+    'LBL_ANY_WHERE' => 'Bet Kur',
+
+    'LBL_QUOTE_GROUP_SAMPLE' => 'Quote Group Sample',
+    'LBL_INVOICE_GROUP_SAMPLE' => 'Invoice Group Sample',
+    'LBL_MARGIN_LEFT' => 'Paraštės Kairėje',
+    'LBL_MARGIN_RIGHT' => 'Paraštės Dešinėje',
+    'LBL_MARGIN_TOP' => 'Paraštės Viršus',
+    'LBL_MARGIN_BOTTOM' => 'Paraštės Apačioje',
+    'LBL_MARGIN_HEADER' => 'Paraštė Viršuje Puslapio',
+    'LBL_MARGIN_FOOTER' => 'Paraštė Apatinė puslapio',
+    'LBL_EDITVIEW_PANEL1' => 'Paraštės',
+    'LBL_DETAILVIEW_PANEL1' => 'Paraštės',
+    'LBL_PAGE_SIZE' => 'Page Size',
+    'LBL_ORIENTATION' => 'Orientation',
+);
