b463a1f127e4e0cdb981f71165454578fbb2d682
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 04:46:44 +0000

    New translations en_us.lang.php (Nigerian Pidgin)

---
+++ translations/pcm_NG/modules/EmailMarketing/language/pcm_NG.lang.php
@@ -0,0 +1,108 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_REPLY_ADDR' => '"Reply-to" Address: ',
+    'LBL_REPLY_NAME' => '"Reply-to" Name: ',
+
+    'LBL_MODULE_NAME' => 'Were dem dey market email',
+    'LBL_MODULE_TITLE' => 'Email Marketing: Home',
+    'LBL_LIST_FORM_TITLE' => 'Email Marketing Campaigns',
+    'LBL_NAME' => 'Name: ',
+    'LBL_LIST_NAME' => 'Wetin be your name',
+    'LBL_LIST_FROM_ADDR' => 'From Email',
+    'LBL_LIST_DATE_START' => 'Date wey e begin',
+    'LBL_LIST_TEMPLATE_NAME' => 'Make you Email Template',
+    'LBL_LIST_STATUS' => 'Status',
+    'LBL_STATUS' => 'Status',
+    'LBL_STATUS_TEXT' => 'Status:',
+    'LBL_TEMPLATE_NAME' => 'Template Name',
+    'LBL_DATE_ENTERED' => 'Date wey e Enter',
+    'LBL_DATE_MODIFIED' => 'De day wey dem modify am',
+    'LBL_MODIFIED' => 'Modified by: ',
+    'LBL_CREATED' => 'Who create am:',
+    'LBL_MESSAGE_FOR' => 'Make you send this message give:',
+
+    'LBL_FROM_NAME' => 'From Name: ',
+    'LBL_FROM_ADDR' => 'From Email Address: ',
+    'LBL_DATE_START' => 'Date wey e begin',
+    'LBL_TIME_START' => 'Time wey e start',
+    'LBL_START_DATE_TIME' => 'Date & timme wey e start: ',
+    'LBL_TEMPLATE' => 'Email Template: ',
+
+    'LBL_MODIFIED_BY' => 'Modified by: ',
+    'LBL_CREATED_BY' => 'Who create am:',
+
+    'LNK_NEW_CAMPAIGN' => 'Make you Create Campaign',
+    'LNK_CAMPAIGN_LIST' => 'Campaigns dem',
+    'LNK_NEW_PROSPECT_LIST' => 'Make you Create Target List',
+    'LNK_PROSPECT_LIST_LIST' => 'Target Lists dem',
+    'LNK_NEW_PROSPECT' => 'Make you Create Target',
+    'LNK_PROSPECT_LIST' => 'Targets dem',
+    'LBL_DEFAULT_SUBPANEL_TITLE' => 'Were dem dey market email',
+    'LBL_CREATE_EMAIL_TEMPLATE' => 'Create',
+    'LBL_EDIT_EMAIL_TEMPLATE' => 'Edit',
+    'LBL_FROM_MAILBOX' => 'From mailbox',
+    'LBL_FROM_MAILBOX_NAME' => 'Make you use Mailbox:',
+    'LBL_OUTBOUND_EMAIL_ACCOUNT_NAME' => 'Outbound Email Account:',
+    'LBL_PROSPECT_LIST_SUBPANEL_TITLE' => 'Target Lists dem',
+    'LBL_ALL_PROSPECT_LISTS' => 'Make you Select to choose all Target List(s) in the Campaign.',
+    'LBL_RELATED_PROSPECT_LISTS' => 'All Target List(s) wey dey related to this message.',
+    'LBL_PROSPECT_LIST_NAME' => 'Target List Name',
+
+    'LBL_LIST_PROSPECT_LIST_NAME' => 'Targeted Lists dem',
+    'LBL_MODULE_SEND_TEST' => 'Campaign: Send Test',
+    'LBL_MODULE_SEND_EMAILS' => 'Campaign: Make you Send Emails',
+    'LBL_SCHEDULE_MESSAGE_TEST' => 'Abeg make you select the cammpaign messages wey you go like test:',
+    'LBL_SCHEDULE_MESSAGE_EMAILS' => 'Abeg make you select the campaign messages wey you go like schedule for distribution for the date and time wey e go start wey dey don specify:',
+    'LBL_SCHEDULE_BUTTON_TITLE' => 'Make you Send',
+    'LBL_SCHEDULE_BUTTON_LABEL' => 'Make you Send',
+    'LBL_ERROR_ON_MARKETING' => 'Required field(s) wey no dey',
+
+    'LBL_CAMPAIGN_ID' => 'Campaign ID',
+    'LBL_OUTBOUND_EMAIL_ACOUNT_ID' => 'Outbound Email Account ID',
+    'LBL_EMAIL_TEMPLATE' => 'Make you Email Template',
+    'LBL_PROSPECT_LISTS' => 'Prospect Lists dem',
+
+);
