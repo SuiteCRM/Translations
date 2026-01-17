d7d732c8570ec2e5c8deb1661f96c989c28209ac
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 04:11:48 +0000

    New translations en_us.lang.php (Tagalog)

---
+++ translations/tl_PH/modules/SurveyQuestionResponses/language/tl_PH.lang.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID'                                                                 => 'Ang nagagamit sa Id ay nagtalaga',
+    'LBL_ASSIGNED_TO_NAME'                                                               => 'Magtalaga sa',
+    'LBL_SECURITYGROUPS'                                                                 => 'Mga grupong seguridad',
+    'LBL_SECURITYGROUPS_SUBPANEL_TITLE'                                                  => 'Mga grupong seguridad',
+    'LBL_ID'                                                                             => 'Ang ID',
+    'LBL_DATE_ENTERED'                                                                   => 'Ang petsa ay lumikha',
+    'LBL_DATE_MODIFIED'                                                                  => 'Ang petsa ay binago',
+    'LBL_MODIFIED'                                                                       => 'Binago sa pamamagitan ng',
+    'LBL_MODIFIED_ID'                                                                    => 'Binago sa pamamagitan ng Id',
+    'LBL_MODIFIED_NAME'                                                                  => 'Sa pamamagitan ng pangalan ay binago',
+    'LBL_CREATED'                                                                        => 'Nilikha ni',
+    'LBL_CREATED_ID'                                                                     => 'Nilikha sa pamamagitan ng Id',
+    'LBL_DESCRIPTION'                                                                    => 'Ang Paglalarawan',
+    'LBL_ANSWER'                                                                         => 'Ang sagot',
+    'LBL_ANSWER_BOOL'                                                                    => 'Boolean answer',
+    'LBL_ANSWER_DATETIME'                                                                => 'Datetime answer',
+    'LBL_DELETED'                                                                        => 'Ang Nabura',
+    'LBL_NAME'                                                                           => 'Ang Pangalan',
+    'LBL_CREATED_USER'                                                                   => 'Nililikha sa pamamagitan ng Gumagamit',
+    'LBL_MODIFIED_USER'                                                                  => 'Ang Binago sa pamamagitan ng gumagamit',
+    'LBL_LIST_NAME'                                                                      => 'Ang Pangalan',
+    'LBL_EDIT_BUTTON'                                                                    => 'I-edit',
+    'LBL_REMOVE'                                                                         => 'Alisin',
+    'LBL_LIST_FORM_TITLE'                                                                => 'Listahan sa mga tugon ng tanong sa pagsisiyasat',
+    'LBL_MODULE_NAME'                                                                    => 'Ang Pagsusuri sa mga Pagtugon sa Katanungan',
+    'LBL_MODULE_TITLE'                                                                   => 'Ang Pagsusuri sa mga Pagtugon sa Katanungan',
+    'LBL_HOMEPAGE_TITLE'                                                                 => 'Ang aking mga tugon ng tanong ng pagsisiyasat',
+    'LNK_NEW_RECORD'                                                                     => 'Lumikha ng mga tugon ng tanong ng pagsisiyasat',
+    'LNK_LIST'                                                                           => 'Tingnan ang mga tugon sa tanong ng pagsisiyasat',
+    'LNK_IMPORT_SURVEYQUESTIONRESPONSES'                                                 => 'I-import ang mga Pagtugon ng Tanong sa Pagsusuri',
+    'LBL_SEARCH_FORM_TITLE'                                                              => 'Maghanap ng mga tugon sa tanong ng pagsisiyasat',
+    'LBL_HISTORY_SUBPANEL_TITLE'                                                         => 'Tanawin ang kasaysayan',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE'                                                      => 'Ang mga aktibidad',
+    'LBL_SURVEYQUESTIONRESPONSES_SUBPANEL_TITLE'                                         => 'Ang Pagsusuri sa mga Pagtugon sa Katanungan',
+    'LBL_NEW_FORM_TITLE'                                                                 => 'Bagong mga tugon sa tanong ng pagsisiyasat',
+    'LBL_SURVEYRESPONSES_SURVEYQUESTIONRESPONSES_FROM_SURVEYRESPONSES_TITLE'             => 'Ang pag Susuri sa mga Pagtugon',
+    'LBL_SURVEYQUESTIONS_SURVEYQUESTIONRESPONSES_FROM_SURVEYQUESTIONS_TITLE'             => 'Ang mga Pagsusuri sa Katanungan',
+    'LBL_SURVEYQUESTIONOPTIONS_SURVEYQUESTIONRESPONSES_FROM_SURVEYQUESTIONOPTIONS_TITLE' => 'Ang Pagsusuri sa mga Pagppipilian ng Katanungan',
+);
