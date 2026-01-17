b5b85c1935eca5a5c9faf87510fc6f2de23f9f52
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 04:50:59 +0000

    New translations en_us.lang.php (Chinese Traditional)

---
+++ translations/zh_TW/modules/ExternalOAuthConnection/language/zh_TW.lang.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2022 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = [
+
+    'LBL_ASSIGNED_TO_ID' => '負責人Id',
+    'LBL_ASSIGNED_TO_NAME' => '負責人',
+    'LBL_ID' => '編號',
+    'LBL_DATE_ENTERED' => '建立日期',
+    'LBL_DATE_MODIFIED' => '修改日期',
+    'LBL_MODIFIED' => '修改人',
+    'LBL_MODIFIED_NAME' => '修改人姓名',
+    'LBL_CREATED' => '建立人',
+    'LBL_DESCRIPTION' => '描述',
+    'LBL_DELETED' => '已刪除',
+    'LBL_NAME' => '名稱',
+    'LBL_CREATED_USER' => '創建者',
+    'LBL_MODIFIED_USER' => '修改的使用者',
+    'LBL_LIST_NAME' => '名稱',
+    'LBL_EDIT_BUTTON' => '編輯',
+    'LBL_REMOVE' => '刪除',
+    'LBL_LIST_FORM_TITLE' => 'External OAuth Connection List',
+    'LBL_MODULE_NAME' => '外部 Oauth 連結',
+    'LBL_MODULE_TITLE' => '外部 Oauth 連結',
+    'LBL_HOMEPAGE_TITLE' => 'My External OAuth Connections',
+    'LNK_NEW_RECORD' => 'Create External OAuth Connection',
+
+    'LNK_LIST' => '外部 Oauth 連結',
+    'LBL_SEARCH_FORM_TITLE' => 'Search External OAuth Connections',
+    'LBL_HISTORY_SUBPANEL_TITLE' => '查看歷史記錄',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => '銷售活動',
+    'LBL_NEW_FORM_TITLE' => 'New External OAuth Connection',
+
+    'LBL_LIST_DELETE' => '刪除',
+    'LBL_TYPE' => '類型',
+
+    'LBL_CLIENT_ID' => 'Client Id',
+    'LBL_CLIENT_SECRET' => 'Client Secret',
+    'LBL_TOKEN_TYPE' => '令牌類別',
+    'LBL_EXPIRES_IN' => 'Expires In',
+    'LBL_ACCESS_TOKEN' => 'Access Token',
+    'LBL_REFRESH_TOKEN' => 'Refresh Token',
+    'LBL_PROVIDER' => 'Provider',
+
+    'LBL_AUTHENTICATE' => 'Authenticate',
+
+
+    'LBL_EXTERNAL_OAUTH_PROVIDER' => 'Provider',
+    'LBL_EXTERNAL_OAUTH_PROVIDER_NAME' => 'Provider',
+    'LBL_EXTERNAL_OAUTH_PROVIDER_ID' => 'Provider Id',
+
+    'LNK_LIST_CREATE_NEW_PERSONAL' => 'New Personal OAuth Connection',
+    'LNK_LIST_CREATE_NEW_GROUP' => 'New Group OAuth Connection',
+    'LNK_LIST_INBOUND_EMAILS' => '入站電子郵件帳戶',
+    'LNK_LIST_OUTBOUND_EMAILS' => '出站電子郵件帳號',
+    'LNK_LIST_EXTERNAL_OAUTH_PROVIDERS' => '外部 OAuth 服務供應商',
+
+    'LBL_OWNER' => '負責人',
+];
