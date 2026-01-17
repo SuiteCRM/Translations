8097dfc6e904884b4d46ef22f0d6fdbb561f6df6
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 04:19:37 +0000

    New translations en_us.lang.php (Sinhala)

---
+++ translations/si_LK/modules/InboundEmail/language/si_LK.lang.php
@@ -0,0 +1,229 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+
+
+    'LBL_RE' => 'RE:',
+
+    'ERR_BAD_LOGIN_PASSWORD' => 'සම්පූර්ණ ඊ-අල්ලා ගැනීමට බොහෝ කාලයක් \ සිරුර පෙළ. පිළිවෙල.',
+    'ERR_INI_ZLIB' => 'තාවකාලිකව හි Zlib සම්පීඩන අක්රිය කිරීමට නොහැකි විය. "ටෙස්ට් සැකසීම්" අසාර්ථක විය හැක.',
+    'ERR_NO_IMAP' => 'මෙයට IMAP පුස්තකාල සොයාගෙන ඇත. රට තුළට පැමිණෙන සංචාරකයින්ට විද්යුත් සමග තවදුරටත් පවත්වාගෙන යාමට පෙර කරුණාකර මෙම විසඳීම',
+    'ERR_NO_OPTS_SAVED' => 'මෙයට optimus ඔබේ රට තුළට පැමිණෙන සංචාරකයින්ට විද්යුත් ගිණුම සමඟ බේරා ගන්නා ලදී. සැකසුම් සමාලෝචනය කරන්න',
+    'ERR_TEST_MAILBOX' => 'ඔබගේ සැකසුම් පරීක්ෂා කර නැවත උත්සාහ කරන්න.',
+    'ERR_INVALID_PORT' => 'Invalid Port',
+
+    'LBL_ASSIGN_TO_USER' => 'පරිශීලක කිරීම සදනන',
+    'LBL_AUTOREPLY' => 'වාහන-ඊ-මේල් මගින් පිලිතුරු දෙන්න සැකිල්ල',
+    'LBL_AUTOREPLY_HELP' => 'ඔවුන්ගේ ප්රතිචාර ලැබී ඇති බව විද්යුත් තැපැල් senders දැනුම් කිරීම සඳහා ස්වයංක්රීය ප්රතිචාර තෝරන්න.',
+    'LBL_BASIC' => 'තැපැල් ගිණුම් තොරතුරු',
+    'LBL_CASE_MACRO' => 'නඩු සාර්ව',
+    'LBL_CASE_MACRO_DESC' => 'කියවූ සහ නඩු ආනයනය ඊ-තැපැල් සම්බන්ධ කිරීමට භාවිතා කරනු ලබන සාර්ව සකසන්න.',
+    'LBL_CASE_MACRO_DESC2' => 'යම් අගය කිරීමට මෙම සකසන්න, නමුත් මෙම <b> "% 1" </ b> ආරක්ෂා.',
+    'LBL_CLOSE_POPUP' => 'කවුළුව වසන්න',
+    'LBL_CREATE_TEMPLATE' => 'සාදන්න',
+    'LBL_DELETE_SEEN' => 'ආනයන පසු කියවන්න විද්යුත් තැපැල් පණිවුඩ Delete',
+    'LBL_EDIT_TEMPLATE' => 'සංස්කරණය',
+    'LBL_EMAIL_OPTIONS' => 'විද්යුත් පරිහරණය විකල්ප',
+    'LBL_EMAIL_BOUNCE_OPTIONS' => 'විකල්ප හැසිරවීම සම්බන්ධ ළඟාවීමටත් අපේක්ෂා',
+    'LBL_FILTER_DOMAIN_DESC' => 'කිසිදු ස්වයංක්රීය-පිළිතුරු යවා කරනු ලබන කිරීමට ඩොමේන් සඳහන් කරන්න.',
+    'LBL_ASSIGN_TO_GROUP_FOLDER_DESC' => 'ස්වයංක්රීයව ලැබෙන සියලුම විද්යුත් ලිපි සඳහා SuiteCRM විද්යුත් තැපැල් වාර්තා නිර්මාණය කිරීමට තෝරන්න.',
+    'LBL_FILTER_DOMAIN' => 'මෙම වසම් වාහන-ඊ-මේල් මගින් පිලිතුරු දෙන්න නොමැත',
+    'LBL_FIND_SSL_WARN' => '<br> පරීක්ෂා SSL දීර්ඝ කාලයක් ගත විය හැකිය. කරුණාකර ඉවසන්න. <br>',
+    'LBL_FROM_ADDR' => 'ලිපිනය "සිට"',
+    'LBL_FROM_ADDR_DESC' => 'The email address provided here might not appear in the &quot;From&quot; address section of the email sent due to restrictions imposed by the mail service provider. In these circumstances, the email address defined in the outgoing mail server will be used.', // as long as XTemplate doesn't support output escaping, transform quotes to html-entities right here (bug #48913)
+    'LBL_FROM_NAME' => '"වෙතින්" නම',
+    'LBL_GROUP_QUEUE' => 'සමූහ කිරීම සදනන',
+    'LBL_HOME' => 'මුල',
+    'LBL_LIST_MAILBOX_TYPE' => 'තැපැල් ගිණුම භාවිතය',
+    'LBL_LIST_NAME' => 'නම:',
+    'LBL_LIST_GLOBAL_PERSONAL' => 'වර්ගය',
+    'LBL_LIST_SERVER_URL' => 'තැපැල් සේවාදායකය',
+    'LBL_SERVER_ADDRESS' => 'Server Address',
+    'LBL_LIST_STATUS' => 'තත්වය',
+    'LBL_LOGIN' => ' පරිශීලක නාමය',
+    'LBL_USERNAME' => ' පරිශීලක නාමය',
+    'LBL_MAILBOX_DEFAULT' => 'එන ලිපි',
+    'LBL_MAILBOX_SSL' => 'SSL භාවිතා කරන්න',
+    'LBL_MAILBOX_TYPE' => 'හැකි ක්රියාමාර්ග',
+    'LBL_DISTRIBUTION_METHOD' => 'බෙදාහැරීම් ක්රමය',
+    'LBL_CREATE_CASE_REPLY_TEMPLATE' => 'නව නඩු වාහන-ඊ-මේල් මගින් පිලිතුරු දෙන්න සැකිල්ල',
+    'LBL_CREATE_CASE_REPLY_TEMPLATE_HELP' => 'නඩුවක් නිර්මාණය කර ඇති බව විද්යුත් තැපැල් senders දැනුම් කිරීම සඳහා ස්වයංක්රීය ප්රතිචාර තෝරන්න. ඊ-තැපැල් මෙම නඩුව සාර්ව සැකසුම ගැලී සිටින විෂය සමගාමී නඩු අංකය අඩංගු වේ. පළමු විද්යුත් තැපැල් ලබන්නා ලැබුණු විට මෙම ප්රතිචාරය පමණක් යවනු ලැබේ.',
+    'LBL_MAILBOX' => 'අධීක්ෂණය ෆෝල්ඩර',
+    'LBL_TRASH_FOLDER' => 'කුණු කූඩයට ෆෝල්ඩරය',
+    'LBL_SENT_FOLDER' => 'එවා ෆෝල්ඩරය',
+    'LBL_SELECT' => ' තෝරන්න',
+    'LBL_MARK_READ_NO' => 'විද්යුත් ආනයන පසුව මකාදැමනු සලකුණු',
+    'LBL_MARK_READ_YES' => 'විද්යුත් ආනයන පසු සේවාදායකය මත ඉතිරි',
+    'LBL_MARK_READ' => 'සර්වර් මත පණිවිඩ අවසරය,',
+    'LBL_MAX_AUTO_REPLIES' => 'වාහන-ප්රතිචාර සංඛ්යාව',
+    'LBL_MAX_AUTO_REPLIES_DESC' => 'පැය 24 ක කාලය තුළ අද්විතීය ඊ-මේල් ලිපිනය වෙත යැවීමට ස්වයංක්රීය-ප්රතිචාර උපරිම සංඛ්යාව සකසන්න.',
+    'LBL_PERSONAL_MODULE_NAME' => 'පුද්ගලික තැපැල් ගිණුම',
+    'LBL_CREATE_CASE' => 'විද්යුත් සිට නඩු සාදන්න',
+    'LBL_CREATE_CASE_HELP' => 'ස්වයංක්රීයව ලැබෙන ඊ-තැපැල් මඟින් SuiteCRM නඩුවක් වාර්තා නිර්මාණය කිරීමට තෝරන්න.',
+    'LBL_MODULE_NAME' => 'Group Mail Account',
+    'LBL_BOUNCE_MODULE_NAME' => 'තැපැල් පෙට්ටිය හැසිරවීම සම්බන්ධ ළඟාවීමටත් අපේක්ෂා',
+    'LBL_MODULE_TITLE' => 'රට තුළට පැමිණෙන සංචාරකයින්ට විද්යුත්',
+    'LBL_NAME' => 'නම',
+    'LBL_NONE' => 'කිසිවක් නැත',
+    'LBL_ONLY_SINCE_NO' => 'අංක තැපැල් සේවාදායකය මත තිබෙන සියළුම විද්යුත් තැපැල් එරෙහිව පරීක්ෂා කර බලන්න.',
+    'LBL_ONLY_SINCE_YES' => 'ඔව්.',
+    'LBL_PASSWORD' => 'මුරපදය',
+    'LBL_EMAIL_PASSWORD' => 'මුරපදය',
+    'LBL_POP3_SUCCESS' => 'ඔබේ නොකරන ලද POP3 ටෙස්ට් සම්බන්ධයෙන් සාර්ථක විය.',
+    'LBL_POPUP_TITLE' => 'සැකසුම් පරීක්ෂා',
+    'LBL_SELECT_SUBSCRIBED_FOLDERS' => 'දායක වූ ෆෝල්ඩරය (ව) තෝරන්න',
+    'LBL_SELECT_TRASH_FOLDERS' => 'ඉවත ලූ ලිපි ෆෝල්ඩරය තෝරන්න',
+    'LBL_SELECT_SENT_FOLDERS' => 'යැවූ ෆෝල්ඩරය තෝරන්න',
+    'LBL_DELETED_FOLDERS_LIST' => 'පහත දැක්වෙන ෆෝල්ඩරය (ව)% s, නොපවතියි හෝ සේවාදායකය වෙතින් මකා දමා ඇත එක්කෝ',
+    'LBL_PORT' => 'තැපැල් සේවාදායකය වරාය',
+    'LBL_REPLY_TO_NAME' => '"පිලිතුරු දෙන්න-කිරීමට" නම',
+    'LBL_REPLY_TO_ADDR' => '"පිලිතුරු දෙන්න-කිරීමට" ලිපිනය',
+    'LBL_SAME_AS_ABOVE' => 'නම / ලිපිනය භාවිතා කරමින් සිට',
+    'LBL_SERVER_OPTIONS' => 'උසස් කරන සැකසුම',
+    'LBL_SERVER_TYPE' => 'තැපැල් සේවාදායකය ප්රොටොකෝලය',
+    'LBL_SERVER_PORT' => 'තැපැල් සේවාදායකය වරාය',
+    'LBL_SERVER_URL' => 'තැපැල් සේවාදායකය ලිපිනය',
+    'LBL_SSL_DESC' => 'ඔබගේ තැපැල් සේවාදායකය මෙය සක්රීය ආරක්ෂිත Socket සම්බන්ධතා සහය නොදක්වයි නම් විද්යුත් තැපැල් ආනයනය විට SSL සම්බන්ධතා බල කරනු ඇත.',
+    'LBL_ASSIGN_TO_TEAM_DESC' => 'තෝරාගත් කණ්ඩායම තැපැල් ගිණුම ප්රවේශය ඇත.',
+    'LBL_SSL' => 'SSL භාවිතා කරන්න',
+    'LBL_STATUS' => 'තත්වය',
+    'LBL_EMAIL_BODY_FILTERING' => 'Email Body Filter Type',
+    'LBL_SYSTEM_DEFAULT' => 'පද්ධතිය පෙරනිමි',
+    'LBL_TEST_BUTTON_TITLE' => 'ටෙස්ට්',
+    'LBL_TEST_SETTINGS' => 'සැකසුම් පරීක්ෂා',
+    'LBL_TEST_CONNECTION_SETTINGS' => 'Test Connection Settings',
+    'LBL_TEST_SUCCESSFUL' => 'සම්බන්ධතා සාර්ථකව නිම.',
+    'LBL_TEST_WAIT_MESSAGE' => 'එක් මොහොතක අනේ ...',
+    'LBL_WARN_IMAP_TITLE' => 'රට තුළට පැමිණෙන සංචාරකයින්ට විද්යුත් ආබාධිත',
+    'LBL_WARN_IMAP' => 'අනතුරු ඇඟවීම්:',
+    'LBL_WARN_NO_IMAP' => 'රට තුළට පැමිණෙන සංචාරකයින්ට විද්යුත් <b> හැකි නොවේ </ b> කාර්යය එම PHP මොඩියුලය සමඟ සකස් / සක්රීය වූ IMAP ඇ-සේවාදායකයා පුස්තකාල නොමැති. මෙම ගැටළුව විසඳීම සඳහා ඔබගේ පරිපාලක අමතන්න.',
+
+    'LNK_LIST_CREATE_NEW_PERSONAL' => 'New Personal Inbound Email Account',
+    'LNK_LIST_CREATE_NEW_GROUP' => 'New Group Inbound Email Account',
+    'LNK_LIST_CREATE_NEW_CASES_TYPE' => 'New Case Handling Email Account',
+    'LNK_LIST_CREATE_NEW_BOUNCE' => 'New Bounce Handling Email Account',
+    'LNK_LIST_MAILBOXES' => 'Inbound Email Accounts',
+    'LNK_LIST_OUTBOUND_EMAILS' => 'Outbound Email Accounts',
+    'LNK_LIST_SCHEDULER' => 'සැලසුම් කරණයන් සඳහා',
+    'LNK_SEED_QUEUES' => 'කණ්ඩායම් සිට බීජ පෝලිම්',
+    'LBL_GROUPFOLDER_ID' => 'සමූහ ෆෝල්ඩරය අංකය',
+
+    'LBL_ALLOW_OUTBOUND_GROUP_USAGE' => 'පරිශීලකයන් ලිපිනය වෙත ඊ-මේල් මගින් පිලිතුරු දෙන්න ලෙස "වෙතින්" නම සහ ලිපිනය භාවිතා ඊ-තැපැල් යැවීමට ඉඩ දෙන්න',
+    'LBL_ALLOW_OUTBOUND_GROUP_USAGE_DESC' => 'මෙම විකල්පය තෝරාගෙන තිබෙන විට කණ්ඩායමක් තැපැල් ගිණුම වෙත ප්රවේශ කරන්නන් සඳහා විද්යුත් තැපැල් ලියන විට, නම සිට හා මෙම කණ්ඩායම තැපැල් ගිණුම සමග සම්බන්ධ විද්යුත් තැපැල් ලිපිනය සිට මෙම සිට ක්ෂේත්රයේ සඳහා විකල්ප ලෙස දිස් වනු ඇත. ',
+    'LBL_STATUS_ACTIVE' => 'ක්රියාකාරී',
+    'LBL_STATUS_INACTIVE' => 'අක්රීය',
+    'LBL_IS_PERSONAL' => 'පුද්ගලික',
+    'LBL_IS_GROUP' => 'සමූහය',
+    'LBL_ENABLE_AUTO_IMPORT' => 'ස්වයංක්රීයව ආනයන විද්යුත් තැපැල් පණිවුඩ',
+    'LBL_WARNING_CHANGING_AUTO_IMPORT' => 'අවවාදයයි:. ඔබ දත්ත අහිමි විය හැක ඔබේ ස්වයංක්රීය ආනයන සැකසුම වෙනස් වේ',
+    'LBL_WARNING_CHANGING_AUTO_IMPORT_WITH_CREATE_CASE' => 'අවවාදයයි: ස්වයංක්රීයව නඩු නිර්මාණය කිරීමේදී වාහන ආනයනය කිරීම සක්රීයව තිබිය යුතුය ',
+    'LBL_LIST_TITLE_MY_DRAFTS' => 'Drafts',
+    'LBL_LIST_TITLE_MY_INBOX' => 'Inbox',
+    'LBL_LIST_TITLE_MY_SENT' => 'විද්යුත් යවන්න',
+    'LBL_LIST_TITLE_MY_ARCHIVES' => 'සංරක්ෂිත විද්යුත් තැපැල් පණිවුඩ',
+    'LNK_MY_DRAFTS' => 'Drafts',
+    'LNK_MY_INBOX' => 'විද්යුත් තැපෑල',
+    'LNK_VIEW_MY_INBOX' => 'View Email',
+    'LNK_QUICK_REPLY' => 'පිලිතුරු',
+    'LNK_SENT_EMAIL_LIST' => 'විද්යුත් තැපැල් පණිවුඩ යැවූ',
+    'LBL_EDIT_LAYOUT' => 'සංස්කරණය කරන්න පිරිසැලසුම' /*for 508 compliance fix*/,
+    'LBL_TYPE_DIFFERENT' => 'External OAuth Connection type must be the SAME as the Inbound Email Account type',
+
+    'LBL_MODIFIED_BY' => ' වන විට නවීකරණය කරන ලද',
+    'LBL_SERVICE' => 'සේවය',
+    'LBL_STORED_OPTIONS' => 'Stored Options',
+    'LBL_GROUP_ID' => 'Group ID',
+
+    'LBL_OUTBOUND_CONFIGURATION' => 'Outbound Configuration',
+    'LBL_CONNECTION_CONFIGURATION' => 'Server Configuration',
+    'LBL_AUTO_REPLY_CONFIGURATION' => 'Auto Reply Configuration',
+    'LBL_CASE_CONFIGURATION' => 'Case Configuration',
+    'LBL_GROUP_CONFIGURATION' => 'Group Configuration',
+
+    'LBL_SECURITYGROUPS_SUBPANEL_TITLE' => 'ආරක්ෂක සමූහ',
+
+
+    'LBL_OUTBOUND_EMAIL_ACCOUNT' => 'Outbound Email Account',
+    'LBL_OUTBOUND_EMAIL_ACCOUNT_ID' => 'Outbound Email Account id',
+    'LBL_OUTBOUND_EMAIL_ACCOUNT_NAME' => 'Outbound Email Account',
+
+    'LBL_AUTOREPLY_EMAIL_TEMPLATE' => 'Auto Reply Email Template',
+    'LBL_AUTOREPLY_EMAIL_TEMPLATE_ID' => 'Auto Reply Email Template id',
+    'LBL_AUTOREPLY_EMAIL_TEMPLATE_NAME' => 'Auto Reply Email Template',
+
+    'LBL_CASE_EMAIL_TEMPLATE' => 'Case Email Template',
+    'LBL_CASE_EMAIL_TEMPLATE_ID' => 'Case Email Template id',
+    'LBL_CASE_EMAIL_TEMPLATE_NAME' => 'Case Email Template',
+
+    'LBL_PROTOCOL' => 'Protocol',
+    'LBL_CONNECTION_STRING' => 'Connection String',
+    'LBL_DISTRIB_METHOD' => 'බෙදාහැරීම් ක්රමය',
+    'LBL_DISTRIB_OPTIONS' => 'Distribution Options',
+
+    'LBL_DISTRIBUTION_USER' => 'Distribution User',
+    'LBL_DISTRIBUTION_USER_ID' => 'Distribution User id',
+    'LBL_DISTRIBUTION_USER_NAME' => 'Distribution User',
+
+    'LBL_EXTERNAL_OAUTH_CONNECTION' => 'External OAuth Connection',
+    'LBL_EXTERNAL_OAUTH_CONNECTION_ID' => 'External OAuth Connection id',
+    'LBL_EXTERNAL_OAUTH_CONNECTION_NAME' => 'External OAuth Connection',
+    'LNK_EXTERNAL_OAUTH_CONNECTIONS' => 'External OAuth Connections',
+
+    'LBL_TYPE' => 'වර්ගය',
+    'LBL_AUTH_TYPE' => 'Auth Type',
+    'LBL_IS_DEFAULT' => 'පෙරනිමි',
+    'LBL_SIGNATURE' => '	අත්සන',
+
+    'LBL_OWNER_NAME' => 'හිමිකරු',
+
+    'LBL_SET_AS_DEFAULT_BUTTON' => 'Set as default',
+
+    'LBL_MOVE_MESSAGES_TO_TRASH_AFTER_IMPORT' => 'Move Messages To Trash After Import?',
+    'LBL_LAST_IMPORT_RUN_DATETIME' => 'Last Import Run Date/Time',
+    'LBL_MAILBOX_LAST_IMPORTED_DAYS' => 'Last Imported Date Per Mailbox',
+    'LBL_EMAIL_IMPORT_PER_RUN_THRESHOLD' => 'Emails to Import Per Run',
+    'LBL_EMAIL_IMPORT_TIMEFRAME_START' => 'Import Timeframe Start',
+    'LBL_EMAIL_IMPORT_UNREAD_ONLY' => 'Import Unread Emails Only'
+);
