b33eaba026e8dcf7ebc2d7911e6f56072a339405
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 02:34:50 +0000

    New translations en_us.lang.php (Czech)

---
+++ translations/cs_CZ/modules/AOR_Scheduled_Reports/language/cs_CZ.lang.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID' => 'ID přiřazeného uživatele',
+    'LBL_ASSIGNED_TO_NAME' => 'Přiřazeno k',
+    'LBL_ID' => 'ID',
+    'LBL_DATE_ENTERED' => 'Datum vytvoření',
+    'LBL_DATE_MODIFIED' => 'Datum změny',
+    'LBL_MODIFIED' => 'Změnil',
+    'LBL_MODIFIED_NAME' => 'Změněno dle Jména',
+    'LBL_CREATED' => 'Vytvořeno',
+    'LBL_DESCRIPTION' => 'Popis',
+    'LBL_DELETED' => 'Vymazáno',
+    'LBL_NAME' => 'Název',
+    'LBL_CREATED_USER' => 'Vytvořeno dle Uživatele',
+    'LBL_MODIFIED_USER' => 'Změněno dle Uživatele',
+    'LBL_LIST_NAME' => 'Název',
+    'LBL_EDIT_BUTTON' => 'Upravit',
+    'LBL_REMOVE' => 'Odebrat',
+    'LBL_ANY_EMAIL' => 'Jakýkoliv Email:',
+    'LBL_EMAIL_NON_PRIMARY' => 'Ne primární e-maily',
+    'LBL_ASSIGNED_TO' => 'Přiřazeno k:',
+    'LBL_ASSIGNED_USER' => 'Přiřazeno k:',
+    'LBL_EMAIL_RECIPIENTS' => 'Příjemci e-mailu:',
+    'NTC_DELETE_CONFIRMATION' => 'Opravdu chcete smazat tento záznam?',
+    'LBL_LIST_FORM_TITLE' => 'Seznam naplánovaných reportů',
+    'LBL_MODULE_NAME' => 'Naplánované reporty',
+    'LBL_MODULE_TITLE' => 'Naplánované reporty',
+    'LBL_HOMEPAGE_TITLE' => 'Moje naplánované reporty',
+    'LNK_NEW_RECORD' => 'Vytvořit naplánované reporty',
+    'LNK_LIST' => 'Zobrazit naplánované reporty',
+    'LBL_SEARCH_FORM_TITLE' => 'Hledat naplánované reporty',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Historie',
+    'LBL_NEW_FORM_TITLE' => 'Nové naplánované reporty',
+    'LBL_SCHEDULE' => 'Plán',
+    'LBL_AOR_REPORT_NAME' => 'Reporty',
+    'LBL_SCHEDULED_REPORTS_INFORMATION' => 'Naplánované reporty',
+    'LBL_LAST_RUN' => 'Poslední spuštění',
+    'LBL_STATUS' => 'Stav',
+);
