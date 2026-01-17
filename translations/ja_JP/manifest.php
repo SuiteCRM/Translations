ab9ce022751b8689a86b8dcb9f9531f556c38499
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 02:57:18 +0000

    New translations manifest.php (Japanese)

---
+++ translations/ja_JP/manifest.php
@@ -0,0 +1,25 @@
+<?php
+/******************************************
+ * SuiteCRM Translations
+ * @URL: https://crowdin.com/project/suitecrmtrans
+ * @author SuiteCRM Community via Crowdin
+ ******************************************/
+
+$manifest = array(
+    'name' => 'Japanese (Japan)',
+    'description' => 'Translation: crowdin.com/project/suitecrmtranslations',
+    'type' => 'langpack',
+    'is_uninstallable' => 'はい',
+    'author' => 'SuiteCRM コミュニティ',
+    'version' => '7.15-1',
+    'published_date' => '2026-01-16',
+);
+$installdefs = array(
+    'id' => 'ja_JP',
+    'image_dir' => '<basepath>/images',
+    'copy' => array(
+        array('from' => '<basepath>/include', 'to' => 'include'),
+        array('from' => '<basepath>/modules', 'to' => 'modules'),
+        array('from' => '<basepath>/install', 'to' => 'install'),
+    )
+);
