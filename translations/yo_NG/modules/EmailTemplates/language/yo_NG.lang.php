85e0c6e18e62c6939e1796ba9ed01f01dd18c7d5
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 04:47:50 +0000

    New translations en_us.lang.php (Yoruba)

---
+++ translations/yo_NG/modules/EmailTemplates/language/yo_NG.lang.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ADD_DOCUMENT' => 'Add a Document',
+    'LBL_ADD_FILE' => 'Add a file',
+    'LBL_ATTACHMENTS' => 'Attachments',
+    'LBL_BODY' => 'Body:',
+    'LBL_CLOSE' => 'Close:',
+    'LBL_DESCRIPTION' => 'Apejuwe:',
+    'LBL_EDIT_ALT_TEXT' => 'Edit Plain Text',
+    'LBL_EMAIL_ATTACHMENT' => 'Asomo Email',
+    'LBL_HIDE_ALT_TEXT' => 'Hide Plain Text',
+    'LBL_HTML_BODY' => 'HTML Body',
+    'LBL_INSERT_VARIABLE' => 'Insert Variable:',
+    'LBL_INSERT_URL_REF' => 'Insert URL Reference',
+    'LBL_INSERT_TRACKER_URL' => 'Insert Tracker URL:',
+    'LBL_INSERT' => 'Insert',
+    'LBL_LIST_DATE_MODIFIED' => 'Atunse ti o kehin',
+    'LBL_LIST_DESCRIPTION' => 'Apejuwe',
+    'LBL_LIST_FORM_TITLE' => 'Email Templates List',
+    'LBL_LIST_NAME' => 'Oruko',
+    'LBL_MODULE_NAME' => 'Email Templates',
+    'LBL_MODULE_TITLE' => 'Email Templates: Home',
+    'LBL_NAME' => 'Oruko:',
+    'LBL_NEW_FORM_TITLE' => 'Create Email Template',
+    'LBL_PUBLISH' => 'Publish:',
+    'LBL_RELATED_TO' => 'Related To:',
+    'LBL_SEARCH_FORM_TITLE' => 'Email Templates Search',
+    'LBL_SHOW_ALT_TEXT' => 'Show Plain Text',
+    'LBL_SUBJECT' => 'Koko-oro:',
+    'LBL_SUITE_DOCUMENT' => 'Iwe ase',
+    'LBL_UPLOAD_FILE' => 'Upload File',
+    'LBL_TEXT_BODY' => 'Text Body',
+    'LBL_USERS' => 'Onilo',
+
+    'LNK_EMAIL_TEMPLATE_LIST' => 'View Email Templates',
+    'LNK_IMPORT_NOTES' => 'Import Notes',
+    'LNK_NEW_EMAIL_TEMPLATE' => 'Create Email Template',
+    'LNK_NEW_EMAIL' => 'Archive Email',
+    'LNK_NEW_SEND_EMAIL' => 'Ko email',
+    'LNK_SENT_EMAIL_LIST' => 'Sent Emails',
+    'LNK_VIEW_CALENDAR' => 'Oni',
+    // for Inbox
+    'LBL_NEW' => 'Titun',
+    'LNK_MY_DRAFTS' => 'My Drafts',
+    'LNK_MY_INBOX' => 'My Email',
+    'LBL_TEXT_ONLY' => 'Text Only',
+    'LBL_SEND_AS_TEXT' => 'Send Text Only',
+    'LBL_ACCOUNT' => 'Akanti',
+    'LBL_FROM_NAME' => 'Lati Oruko',
+    'LBL_PLAIN_TEXT' => 'Plain Text',
+    'LBL_CREATED_BY' => 'Siseda nipa',
+    'LBL_PUBLISHED' => 'Published',
+    'LNK_VIEW_MY_INBOX' => 'View My Email',
+    'LBL_ASSIGNED_TO_ID' => 'Assigned To',
+    'LBL_EDIT_LAYOUT' => 'Ṣatunkọ Aṣayan' /*for 508 compliance fix*/,
+    'LBL_SELECT' => 'Yan' /*for 508 compliance fix*/,
+    'LBL_ID_FF_CLEAR' => 'Parẹ' /*for 508 compliance fix*/,
+    'LBL_TYPE' => 'Iru',
+    'LBL_WIDTH' => 'Width Default',
+    'LNK_IMPORT_CAMPAIGNS' => 'Import Campaign',
+);
