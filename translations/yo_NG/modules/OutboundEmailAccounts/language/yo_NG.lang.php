b71472610f4ac722556913c9d4365d2ca4b75668
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 04:35:56 +0000

    New translations en_us.lang.php (Yoruba)

---
+++ translations/yo_NG/modules/OutboundEmailAccounts/language/yo_NG.lang.php
@@ -0,0 +1,131 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID' => 'Onilo ti ayan',
+    'LBL_ASSIGNED_TO_NAME' => 'Ayan si',
+    'LBL_ID' => 'Idanimọ',
+    'LBL_DATE_ENTERED' => 'Ojo ti a seda',
+    'LBL_DATE_MODIFIED' => 'Ojo ti a se Atunse',
+    'LBL_MODIFIED' => 'Se iyipada nipasẹ',
+    'LBL_MODIFIED_NAME' => 'Atunse nipa Oruko',
+    'LBL_CREATED' => 'Siseda nipa',
+    'LBL_DESCRIPTION' => 'Apejuwe',
+    'LBL_DELETED' => 'Paare',
+    'LBL_NAME' => 'Oruko',
+    'LBL_CREATED_USER' => 'Seda nipase Onilo',
+    'LBL_MODIFIED_USER' => 'Atunse nipa se onilo',
+    'LBL_LIST_NAME' => 'Oruko',
+    'LBL_EDIT_BUTTON' => 'Satunko',
+    'LBL_REMOVE' => 'Yo kuro',
+    'LBL_LIST_FORM_TITLE' => 'Outbound Email Accounts List',
+    'LBL_MODULE_NAME' => 'Outbound Email Accounts',
+    'LBL_MODULE_TITLE' => 'Outbound Email Accounts',
+    'LBL_HOMEPAGE_TITLE' => 'My Outbound Email Accounts',
+    'LNK_NEW_RECORD' => 'Create Outbound Email Accounts',
+
+    'LNK_LIST_CREATE_NEW_PERSONAL' => 'New Personal Outbound Email Account',
+    'LNK_LIST_CREATE_NEW_GROUP' => 'New Group Outbound Email Account',
+    'LNK_LIST_CREATE_SYSTEM_OVERRIDE_GROUP' => 'New System Override Outbound Email Account',
+
+    'LNK_LIST' => 'Outbound Email Accounts',
+    'LNK_LIST_INBOUND_EMAIL_ACCOUNTS' => 'Inbound Email Accounts',
+    'LBL_SEARCH_FORM_TITLE' => 'Search Outbound Email Accounts',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Wo Itan to ti koja',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Akitiyan',
+    'LBL_NEW_FORM_TITLE' => 'New Outbound Email Accounts',
+    'LBL_USERNAME' => 'Oruko onilo',
+    'LBL_PASSWORD' => 'Pasiwodu',
+    'LBL_SMTP_SERVERNAME' => 'SMTP Server Name',
+    'LBL_SMTP_AUTH' => 'SMTP Auth',
+    'LBL_SMTP_PORT' => 'SMTP Port',
+    'LBL_SMTP_PROTOCOL' => 'SMTP Protocol',
+    'LBL_EDITVIEW_PANEL1' => 'Account settings',
+    'LBL_OUTBOUND_CONFIGURATION' => 'Outbound Configuration',
+    'LBL_CONNECTION_CONFIGURATION' => 'Server Configuration',
+    'LBL_CHANGE_PASSWORD' => 'Paaro pasiwodu',
+    'LBL_SEND_TEST_EMAIL' => 'Fi email idanwo ranse',
+
+    // for outbound email dialog
+    'LBL_MISSING_DEFAULT_OUTBOUND_SMTP_SETTINGS' => 'The administrator has not yet configured the default outbound account. Unable to send test email.',
+    'LBL_MAIL_SMTPAUTH_REQ' => 'Lo ijeri ti SMTP?',
+    'LBL_MAIL_SMTPPASS' => 'Pasiwodu',
+    'LBL_MAIL_SMTPPORT' => 'Port',
+    'LBL_MAIL_SMTPSERVER' => 'Server',
+    'LBL_MAIL_SMTPUSER' => 'Oruko onilo',
+    'LBL_MAIL_SMTP_SETTINGS' => 'SMTP Server Specification',
+    'LBL_CHOOSE_EMAIL_PROVIDER' => 'Yan Eleto email re:',
+    'LBL_YAHOOMAIL_SMTPPASS' => 'Yahoo! Mail Password:',
+    'LBL_YAHOOMAIL_SMTPUSER' => 'Yahoo! Mail ID:',
+    'LBL_GMAIL_SMTPPASS' => 'Gmail Password:',
+    'LBL_GMAIL_SMTPUSER' => 'Gmail Email Address:',
+    'LBL_EXCHANGE_SMTPPASS' => 'Pasipaaro Pasiwodu:',
+    'LBL_EXCHANGE_SMTPUSER' => 'Pasipaaro Onilo:',
+    'LBL_EXCHANGE_SMTPPORT' => 'Exchange Server Port:',
+    'LBL_EXCHANGE_SMTPSERVER' => 'Exchange Server:',
+
+    'LBL_TYPE' => 'Iru',
+    'LBL_MAIL_SENDTYPE' => 'Mail Send Type',
+    'LBL_MAIL_SMTPSSL' => 'Mail SMTP/SSL',
+    'LBL_SMTP_FROM_NAME' => '"From" name',
+    'LBL_SMTP_FROM_ADDR' => '"From" address',
+    'LBL_REPLY_TO_NAME' => '"Reply-to" name',
+    'LBL_REPLY_TO_ADDR' => '"Reply-to" address',
+
+    'LBL_SECURITYGROUPS_SUBPANEL_TITLE' => 'Awọn ẹgbẹ Aabo',
+
+    'LBL_OWNER' => 'Onikan',
+    'LBL_OWNER_NAME' => 'Onikan',
+
+    'LBL_AUTH_TYPE' => 'Auth Type',
+
+    'LBL_EXTERNAL_OAUTH_CONNECTION' => 'External OAuth Connection',
+    'LBL_EXTERNAL_OAUTH_CONNECTION_ID' => 'External OAuth Connection id',
+    'LBL_EXTERNAL_OAUTH_CONNECTION_NAME' => 'External OAuth Connection',
+    'LNK_EXTERNAL_OAUTH_CONNECTIONS' => 'External OAuth Connections',
+
+    'LBL_ERROR_OUTBOUND_EMAIL_SYSTEM_EXISTS' => 'System Outbound Email Account already exists. Please remove it before creating a new one.',
+    'LBL_ERROR_OUTBOUND_EMAIL_SYSTEM_IS_NOT_GROUP' => 'When configuring the System Outbound account using OAuth, you must select a Group-Type External Oauth Connection',
+    'LBL_ERROR_OUTBOUND_EMAIL_CONNECTION_TYPE_MISMATCH' => 'When configuring the Outbound account using OAuth, you must select a External Oauth Connection the same type as the Outbound Email Account (Group or Personal)',
+);
