2f117d69804c5e519f2c225bcac10fe66195ef1b
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 03:07:09 +0000

    New translations en_us.lang.php (Macedonian)

---
+++ translations/mk_MK/modules/DocumentRevisions/language/mk_MK.lang.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    //module
+    'LBL_MODULE_NAME' => 'Document Revision',
+
+    'LNK_NEW_DOCUMENT' => 'Креирај документ',
+    'LNK_DOCUMENT_LIST' => 'View Documents',
+
+    //vardef labels
+    'LBL_REVISION_NAME' => 'Број на ревизија',
+    'LBL_FILENAME' => 'Фајл:',
+    'LBL_MIME' => 'MIME тип',
+    'LBL_REVISION' => 'Ревизија',
+    'LBL_DOCUMENT' => 'Поврзани документи',
+    'LBL_LATEST_REVISION' => 'Последна ревизија',
+    'LBL_CHANGE_LOG' => 'Дневник на промени',
+    'LBL_ACTIVE_DATE' => 'Датум на објава',
+    'LBL_EXPIRATION_DATE' => 'Датум на истекување',
+    'LBL_FILE_EXTENSION' => 'Екстензија на фајл',
+
+    'LBL_DOC_NAME' => 'Назив на документ:',
+    'LBL_DOC_VERSION' => 'Ревизија:',
+
+    //document revisions.
+    'LBL_REV_LIST_REVISION' => 'Ревизија',
+    'LBL_REV_LIST_ENTERED' => 'Дата на креирање',
+    'LBL_REV_LIST_CREATED' => 'Направено од',
+    'LBL_REV_LIST_LOG' => 'Дневник на промени',
+    'LBL_REV_LIST_FILENAME' => 'Фајл',
+
+    'LBL_CURRENT_DOC_VERSION' => 'Latest Revision:',
+    'LBL_SEARCH_FORM_TITLE' => 'Пребарувај документи',
+    'LBL_REVISIONS' => 'Revisions',
+
+    //error messages
+    'ERR_FILENAME' => 'Име на фајл',
+    'ERR_DOC_VERSION' => 'Document Version',
+    'ERR_DELETE_CONFIRM' => 'Do you want to delete this document revision?',
+    'ERR_DELETE_LATEST_VERSION' => 'You are not allowed to delete the latest revision of a document.',
+    'LNK_NEW_MAIL_MERGE' => 'Спојување Email-ови',
+    'LBL_DOC_ID' => 'Document Source ID',
+    'LBL_DOC_TYPE' => 'Source',
+    'LBL_DOC_URL' => 'Document Source URL',
+
+    'LBL_CREATED_BY_NAME' => 'Created by Name',
+);
