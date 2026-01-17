92c5c5256bc3ebb76a3aaf4476f800d44b18b9ae
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 02:40:41 +0000

    New translations en_us.lang.php (Greek)

---
+++ translations/el_GR/modules/AM_TaskTemplates/language/el_GR.lang.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID' => 'Υπεύθυνος Χρήστης Α/Α ',
+    'LBL_ASSIGNED_TO_NAME' => 'Υπεύθυνος',
+    'LBL_ID' => 'Α/Α',
+    'LBL_DATE_ENTERED' => 'Ημ/νία καταχώρησης',
+    'LBL_DATE_MODIFIED' => 'Ημερομηνία Τροποποίησης',
+    'LBL_MODIFIED' => 'Τροποποιήθηκε από',
+    'LBL_MODIFIED_ID' => 'Τροποποιήθηκε από Id:',
+    'LBL_MODIFIED_NAME' => 'Τροποποίηση ανά Όνομα',
+    'LBL_CREATED' => 'Δημιουργήθηκε από',
+    'LBL_CREATED_ID' => 'Δημιουργήθηκε από Id',
+    'LBL_DESCRIPTION' => 'Περιγραφή',
+    'LBL_DELETED' => 'Διαγράφηκε',
+    'LBL_NAME' => 'Όνομα εργασίας',
+    'LBL_CREATED_USER' => 'Δημιουργήθηκε από',
+    'LBL_MODIFIED_USER' => 'τροποποίηση από',
+    'LBL_LIST_NAME' => 'Όνομα',
+    'LBL_EDIT_BUTTON' => 'Επεξεργασία',
+    'LBL_REMOVE' => 'Αφαίρεση',
+    'LBL_LIST_FORM_TITLE' => 'Λίστα Προτύπων Καθηκόντων Έργου',
+    'LBL_MODULE_NAME' => 'Πρότυπα  Προγράμματος Εργασίας',
+    'LBL_MODULE_TITLE' => 'Πρότυπα  Προγράμματος Εργασίας',
+    'LBL_HOMEPAGE_TITLE' => 'Τα Πρότυπα Καθηκόντων Έργου μου',
+    'LNK_NEW_RECORD' => 'Δημιουργία Προτύπων Καθηκόντων Έργου',
+    'LNK_LIST' => 'Εμφάνιση Προτύπων Καθηκόντων Έργου',
+    'LNK_IMPORT_AM_TASKTEMPLATES' => 'Εισαγωγή Προτύπων Καθηκόντων Έργου',
+    'LBL_SEARCH_FORM_TITLE' => 'Αναζήτηση Προτύπων Καθηκόντων Έργου',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Προβολή Ιστορικού',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Δραστηριότητες',
+    'LBL_AM_TASKTEMPLATES_SUBPANEL_TITLE' => 'Πρότυπα  Προγράμματος Εργασίας',
+    'LBL_NEW_FORM_TITLE' => 'Νέα Πρότυπα Καθηκόντων Έργου',
+    'LBL_STATUS' => 'Κατάσταση',
+    'LBL_PRIORITY' => 'Προτεραιότητα',
+    'LBL_PERCENT_COMPLETE' => 'Ποσοστό Ολοκλήρωσης',
+    'LBL_PREDECESSORS' => 'Προαπαιτούμενα',
+    'LBL_MILESTONE_FLAG' => 'Σημαία ορόσημου',
+    'LBL_RELATIONSHIP_TYPE' => 'Τύπος σχέσης',
+    'LBL_TASK_NUMBER' => 'Αριθμός Καθήκοντος',
+    'LBL_ORDER_NUMBER' => 'Σειρά',
+    'LBL_ESTIMATED_EFFORT' => 'Αναμενόμενη ενασχόληση (ώρες)',
+    'LBL_UTILIZATION' => 'Αξιοποίηση (%)',
+    'LBL_DURATION' => 'Διάρκεια (μέρες)',
+    'LBL_AM_TASKTEMPLATES_AM_PROJECTTEMPLATES_FROM_AM_PROJECTTEMPLATES_TITLE' => 'Πρότυπα Εργασίας ',
+
+    'LBL_AM_TASKTEMPLATES_AM_PROJECTTEMPLATES_FROM_AM_TASKTEMPLATES_TITLE' => 'Πρότυπα Καθηκόντων: Πρότυπα Έργου από Τίτλο Προτύπων Καθηκόντων',
+);
