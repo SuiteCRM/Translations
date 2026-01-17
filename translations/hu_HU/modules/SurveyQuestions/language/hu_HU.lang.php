64ce37783f68e61efc02375a4a41d89adbb260e3
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 02:53:18 +0000

    New translations en_us.lang.php (Hungarian)

---
+++ translations/hu_HU/modules/SurveyQuestions/language/hu_HU.lang.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID'                                                               => 'Felelős felhasználó ID',
+    'LBL_ASSIGNED_TO_NAME'                                                             => 'Felelős',
+    'LBL_SECURITYGROUPS'                                                               => 'Biztonsági Csoportok',
+    'LBL_SECURITYGROUPS_SUBPANEL_TITLE'                                                => 'Biztonsági Csoportok',
+    'LBL_ID'                                                                           => 'ID',
+    'LBL_DATE_ENTERED'                                                                 => 'Létrehozás dátuma',
+    'LBL_DATE_MODIFIED'                                                                => 'Módosítás dátuma',
+    'LBL_MODIFIED'                                                                     => 'Módosító',
+    'LBL_MODIFIED_ID'                                                                  => 'Módosító Id',
+    'LBL_MODIFIED_NAME'                                                                => 'Módosító név',
+    'LBL_CREATED'                                                                      => 'Létrehozta',
+    'LBL_CREATED_ID'                                                                   => 'Készítette: Id',
+    'LBL_DESCRIPTION'                                                                  => 'Leírás',
+    'LBL_DELETED'                                                                      => 'Törölve',
+    'LBL_NAME'                                                                         => 'Név',
+    'LBL_CREATED_USER'                                                                 => 'Felhasználó által létrehozva',
+    'LBL_MODIFIED_USER'                                                                => 'Felhasználó által módosítva',
+    'LBL_LIST_NAME'                                                                    => 'Név',
+    'LBL_EDIT_BUTTON'                                                                  => 'Szerkesztés',
+    'LBL_REMOVE'                                                                       => 'Eltávolítás',
+    'LBL_LIST_FORM_TITLE'                                                              => 'Survey Questions List',
+    'LBL_MODULE_NAME'                                                                  => 'Kérdőív Kérdések',
+    'LBL_MODULE_TITLE'                                                                 => 'Kérdőív Kérdések',
+    'LBL_HOMEPAGE_TITLE'                                                               => 'My Survey Questions',
+    'LNK_NEW_RECORD'                                                                   => 'Create Survey Questions',
+    'LNK_LIST'                                                                         => 'View Survey Questions',
+    'LNK_IMPORT_SURVEYQUESTIONS'                                                       => 'Import Survey Questions',
+    'LBL_SEARCH_FORM_TITLE'                                                            => 'Search Survey Questions',
+    'LBL_HISTORY_SUBPANEL_TITLE'                                                       => 'Előzmény Megtekintése',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE'                                                    => 'Tevékenységek',
+    'LBL_SURVEYQUESTIONS_SUBPANEL_TITLE'                                               => 'Kérdőív Kérdések',
+    'LBL_NEW_FORM_TITLE'                                                               => 'New Survey Questions',
+    'LBL_SORT_ORDER'                                                                   => 'Sorrend',
+    'LBL_TYPE'                                                                         => 'Típus',
+    'LBL_HAPPINESS_QUESTION'                                                           => 'Happiness Question',
+    'LBL_SURVEYS_SURVEYQUESTIONS_FROM_SURVEYS_TITLE'                                   => 'Kérdőív',
+    'LBL_SURVEYQUESTIONS_SURVEYQUESTIONRESPONSES_FROM_SURVEYQUESTIONRESPONSES_TITLE'   => 'Kérdőív kérdéseinek válaszai',
+    'LBL_SURVEYQUESTIONS_SURVEYQUESTIONRESPONSES_1_FROM_SURVEYQUESTIONRESPONSES_TITLE' => 'Kérdőív kérdéseinek válaszai',
+    'LBL_SURVEYQUESTIONS_SURVEYQUESTIONOPTIONS_FROM_SURVEYQUESTIONOPTIONS_TITLE'       => 'Kérdőív Kérdés Beállítások',
+);
