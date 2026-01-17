fa26ad8265d8b4c14db1cef0fcb55cad6bc01952
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 04:16:32 +0000

    New translations en_us.lang.php (Bosnian)

---
+++ translations/bs_BA/modules/AOR_Reports/language/bs_BA.lang.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID' => 'Id dodijeljenog korisnika',
+    'LBL_ASSIGNED_TO_NAME' => 'Dodijeljeno korisniku',
+    'LBL_ID' => 'ID',
+    'LBL_DATE_ENTERED' => 'Datum kreiranja',
+    'LBL_DATE_MODIFIED' => 'Datum izmjene',
+    'LBL_MODIFIED' => 'Modifikovao-la',
+    'LBL_MODIFIED_NAME' => 'Izmjenjeno od strane korisnika',
+    'LBL_CREATED' => 'Kreirao',
+    'LBL_DESCRIPTION' => 'Opis',
+    'LBL_DELETED' => 'Izbrisano',
+    'LBL_NAME' => 'Ime',
+    'LBL_CREATED_USER' => 'Kreirao Korisnik',
+    'LBL_MODIFIED_USER' => 'Izmijenio Korisnik',
+    'LBL_LIST_NAME' => 'Ime',
+    'LBL_EDIT_BUTTON' => 'Uredi',
+    'LBL_REMOVE' => 'Ukloni',
+    'LBL_LIST_FORM_TITLE' => 'Lista Izvještaja',
+    'LBL_MODULE_NAME' => 'Izvještaji',
+    'LBL_MODULE_TITLE' => 'Izvještaji',
+    'LBL_HOMEPAGE_TITLE' => 'Moji Izvještaji',
+    'LNK_NEW_RECORD' => 'Kreirajte izvještaj',
+    'LNK_LIST' => 'Pregledajte izvještaje',
+    'LBL_SEARCH_FORM_TITLE' => 'Pretražite Izvještaje',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Pregled istorije',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Aktivnosti',
+    'LBL_NEW_FORM_TITLE' => 'Novi Izvještaji',
+    'LBL_REPORT_MODULE' => 'Modul Izvještaja',
+    'LBL_GRAPHS_PER_ROW' => 'Grafova u redu',
+    'LBL_FIELD_LINES' => 'Prikaži Polja',
+    'LBL_ADD_FIELD' => 'Dodaj Polje',
+    'LBL_CONDITION_LINES' => 'Uslovi',
+    'LBL_ADD_CONDITION' => 'Dodaj uslov',
+    'LBL_EXPORT' => 'Izvoz',
+    'LBL_DOWNLOAD_PDF' => 'Spremi PDF',
+    'LBL_ADD_TO_PROSPECT_LIST' => 'Dodaj na Listu Ciljeva',
+    'LBL_AOR_MODULETREE_SUBPANEL_TITLE' => 'Drvo modula',
+    'LBL_AOR_FIELDS_SUBPANEL_TITLE' => 'Polja',
+    'LBL_AOR_CONDITIONS_SUBPANEL_TITLE' => 'Uslovi',
+    'LBL_TOTAL' => 'Ukupno',
+    'LBL_AOR_CHARTS_SUBPANEL_TITLE' => 'Grafikoni',
+    'LBL_ADD_CHART' => 'Dodaj grafikon',
+    'LBL_ADD_PARENTHESIS' => 'Drop parenthesis',
+    'LBL_CHART_TITLE' => 'Naslov',
+    'LBL_CHART_TYPE' => 'Tip',
+    'LBL_CHART_X_FIELD' => 'X osa',
+    'LBL_CHART_Y_FIELD' => 'Y osa',
+    'LBL_AOR_REPORTS_DASHLET' => 'Izvještaji',
+    'LBL_DASHLET_TITLE' => 'Naslov',
+    'LBL_DASHLET_REPORT' => 'Izvještaj',
+    'LBL_DASHLET_CHOOSE_REPORT' => 'Molimo odaberite izvještaj',
+    'LBL_DASHLET_SAVE' => 'Spremi',
+    'LBL_DASHLET_CHARTS' => 'Grafikoni',
+    'LBL_DASHLET_ONLY_CHARTS' => 'Prikaži samo grafikone',
+    'LBL_UPDATE_PARAMETERS' => 'Ažuriraj',
+    'LBL_PARAMETERS' => 'Parametri',
+    'LBL_TOOLTIP_DRAG_DROP_ELEMS' => 'Prenesi elemente u polje ili uslove',
+    'LBL_MAIN_GROUPS' => 'Glavna grupa:',
+    'LBL_CHAR_UNNAMED_DEFAULT_TITLE' => 'Neimenovani grafik',
+    'LBL_REPORT' => 'Izvještaj',
+);
