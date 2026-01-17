75d18e328104083f9edb461d14c03adf62096a28
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 05:03:37 +0000

    New translations en_us.lang.php (Italian)

---
+++ translations/it_IT/modules/Spots/language/it_IT.lang.php
@@ -0,0 +1,184 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID' => 'Id utente assegnato',
+    'LBL_ASSIGNED_TO_NAME' => 'Assegnato a',
+    'LBL_SECURITYGROUPS' => 'Gruppi di Sicurezza',
+    'LBL_SECURITYGROUPS_SUBPANEL_TITLE' => 'Gruppi di Sicurezza',
+    'LBL_ID' => 'ID',
+    'LBL_DATE_ENTERED' => 'Data di Creazione',
+    'LBL_DATE_MODIFIED' => 'Data Modifica',
+    'LBL_MODIFIED' => 'Modificato da',
+    'LBL_MODIFIED_NAME' => 'Modificato da Nome',
+    'LBL_CREATED' => 'Creato da',
+    'LBL_DESCRIPTION' => 'Descrizione',
+    'LBL_DELETED' => 'Eliminato',
+    'LBL_NAME' => 'Nome',
+    'LBL_CREATED_USER' => 'Creato dall\'utente',
+    'LBL_MODIFIED_USER' => 'Modificato dall\'utente',
+    'LBL_LIST_NAME' => 'Nome',
+    'LBL_EDIT_BUTTON' => 'Modifica',
+    'LBL_REMOVE' => 'Cancella',
+    'LBL_LIST_FORM_TITLE' => 'Lista Pivot',
+    'LBL_MODULE_NAME' => 'Pivot',
+    'LBL_MODULE_TITLE' => 'Pivot',
+    'LBL_HOMEPAGE_TITLE' => 'Il mio Pivot',
+    'LNK_NEW_RECORD' => 'Crea Pivot',
+    'LNK_LIST' => 'Vedi Pivot',
+    'LBL_SEARCH_FORM_TITLE' => 'Cerca Pivot',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Visualizza Cronologia',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Attività',
+    'LBL_NEW_FORM_TITLE' => 'Nuovo Pivot',
+    'LBL_CONFIG' => 'Configura',
+    'LBL_TYPE' => 'Area per l\'analisi',
+    'LNK_SPOT_LIST' => 'Visualizza Spots',
+    'LNK_SPOT_CREATE' => 'Crea Spot',
+
+    //Analytics
+    'LBL_AN_CONFIGURATION' => 'Preferenze',
+
+    'LBL_AN_UNSUPPORTED_DB' => 'Spiacenti, le Suite Spots sono attualmente configurate solo per MySQL e MS SQL',
+
+    //Analytics labels for accounts pivot
+    'LBL_AN_ACCOUNTS_ACCOUNT_NAME' => 'Nome',
+    'LBL_AN_ACCOUNTS_ACCOUNT_TYPE' => 'Tipo Azienda',
+    'LBL_AN_ACCOUNTS_ACCOUNT_INDUSTRY' => 'Settore',
+    'LBL_AN_ACCOUNTS_ACCOUNT_BILLING_COUNTRY' => 'Paese di fatturazione',
+
+    //Analytics labels for leads pivot
+    'LBL_AN_LEADS_ASSIGNED_USER' => 'Utente Assegnato',
+    'LBL_AN_LEADS_STATUS' => 'Stato',
+    'LBL_AN_LEADS_LEAD_SOURCE' => 'Fonte del lead',
+    'LBL_AN_LEADS_CAMPAIGN_NAME' => 'Nome Campagna',
+    'LBL_AN_LEADS_YEAR' => 'Anno',
+    'LBL_AN_LEADS_QUARTER' => 'Trimestre',
+    'LBL_AN_LEADS_MONTH' => 'Mese',
+    'LBL_AN_LEADS_WEEK' => 'Settimana',
+    'LBL_AN_LEADS_DAY' => 'Giorno',
+
+    //Analytics labels for sales pivot
+    'LBL_AN_SALES_ACCOUNT_NAME' => 'Nome Azienda',
+    'LBL_AN_SALES_OPPORTUNITY_NAME' => 'Nome Opportunità',
+    'LBL_AN_SALES_ASSIGNED_USER' => 'Utente Assegnato',
+    'LBL_AN_SALES_OPPORTUNITY_TYPE' => 'Tipo di Opportunità',
+    'LBL_AN_SALES_LEAD_SOURCE' => 'Fonte del lead',
+    'LBL_AN_SALES_AMOUNT' => 'Importo',
+    'LBL_AN_SALES_STAGE' => 'Fase di Vendita',
+    'LBL_AN_SALES_PROBABILITY' => 'Probabilità',
+    'LBL_AN_SALES_DATE' => 'Data di vendita',
+    'LBL_AN_SALES_QUARTER' => 'Vendite del trimestre',
+    'LBL_AN_SALES_MONTH' => 'Vendite del mese',
+    'LBL_AN_SALES_WEEK' => 'Vendite della settimana',
+    'LBL_AN_SALES_DAY' => 'Vendite giornaliere',
+    'LBL_AN_SALES_YEAR' => 'Vendite annuali',
+    'LBL_AN_SALES_CAMPAIGN' => 'Campagna',
+
+    //Analytics labels for service pivot
+    'LBL_AN_SERVICE_ACCOUNT_NAME' => 'Nome Azienda',
+    'LBL_AN_SERVICE_STATE' => 'Provincia',
+    'LBL_AN_SERVICE_STATUS' => 'Stato',
+    'LBL_AN_SERVICE_PRIORITY' => 'Priorità',
+    'LBL_AN_SERVICE_CREATED_DAY' => 'Giorno creazione',
+    'LBL_AN_SERVICE_CREATED_WEEK' => 'Settimana creazione',
+    'LBL_AN_SERVICE_CREATED_MONTH' => 'Mese creazione',
+    'LBL_AN_SERVICE_CREATED_QUARTER' => 'Trimestre creazione',
+    'LBL_AN_SERVICE_CREATED_YEAR' => 'Anno creazione',
+    'LBL_AN_SERVICE_CONTACT_NAME' => 'Contatto',
+    'LBL_AN_SERVICE_ASSIGNED_TO' => 'Utente Assegnato',
+
+    //Analytics labels for the activities pivot
+    'LBL_AN_ACTIVITIES_TYPE' => 'Tipo',
+    'LBL_AN_ACTIVITIES_NAME' => 'Nome',
+    'LBL_AN_ACTIVITIES_STATUS' => 'Stato',
+    'LBL_AN_ACTIVITIES_ASSIGNED_TO' => 'Utente Assegnato',
+
+    //Analytics labels for the marketing pivot
+    'LBL_AN_MARKETING_STATUS' => 'Stato',
+    'LBL_AN_MARKETING_TYPE' => 'Tipo',
+    'LBL_AN_MARKETING_BUDGET' => 'Budget',
+    'LBL_AN_MARKETING_EXPECTED_COST' => 'Costo previsto',
+    'LBL_AN_MARKETING_EXPECTED_REVENUE' => 'Entrate Previste',
+    'LBL_AN_MARKETING_OPPORTUNITY_NAME' => 'Nome Opportunità',
+    'LBL_AN_MARKETING_OPPORTUNITY_AMOUNT' => 'Importo Opportunità',
+    'LBL_AN_MARKETING_OPPORTUNITY_SALES_STAGE' => 'Fase di opportunità di vendita',
+    'LBL_AN_MARKETING_OPPORTUNITY_ASSIGNED_TO' => 'Opportunità assegnata a',
+    'LBL_AN_MARKETING_ACCOUNT_NAME' => 'Nome Azienda',
+
+    //Analytics labels for the marketing activities pivot
+    'LBL_AN_MARKETINGACTIVITY_CAMPAIGN_NAME' => 'Nome Campagna',
+    'LBL_AN_MARKETINGACTIVITY_ACTIVITY_DATE' => 'Data Attività',
+    'LBL_AN_MARKETINGACTIVITY_ACTIVITY_TYPE' => 'Tipo Attività',
+    'LBL_AN_MARKETINGACTIVITY_RELATED_TYPE' => 'Tipo Relazione',
+    'LBL_AN_MARKETINGACTIVITY_RELATED_ID' => 'ID correlato',
+
+    //Analytics labels for the quotes pivot
+    'LBL_AN_QUOTES_OPPORTUNITY_NAME' => 'Nome Opportunità',
+    'LBL_AN_QUOTES_OPPORTUNITY_TYPE' => 'Tipo Opportunità',
+    'LBL_AN_QUOTES_OPPORTUNITY_LEAD_SOURCE' => 'Fonte principale di Opportunità',
+    'LBL_AN_QUOTES_OPPORTUNITY_SALES_STAGE' => 'Fase di opportunità di vendita',
+    'LBL_AN_QUOTES_ACCOUNT_NAME' => 'Nome Azienda',
+    'LBL_AN_QUOTES_CONTACT_NAME' => 'Contatto',
+    'LBL_AN_QUOTES_ITEM_NAME' => 'Nome dell\'elemento',
+    'LBL_AN_QUOTES_ITEM_TYPE' => 'Tipo elemento',
+    'LBL_AN_QUOTES_ITEM_CATEGORY' => 'Categoria elemento',
+    'LBL_AN_QUOTES_ITEM_QTY' => 'Quantità',
+    'LBL_AN_QUOTES_ITEM_LIST_PRICE' => 'Prezzo listino',
+    'LBL_AN_QUOTES_ITEM_SALE_PRICE' => 'Prezzo vendita',
+    'LBL_AN_QUOTES_ITEM_COST_PRICE' => 'Costo',
+    'LBL_AN_QUOTES_ITEM_DISCOUNT_PRICE' => 'Prezzo scontato',
+    'LBL_AN_QUOTES_ITEM_DISCOUNT_AMOUNT' => 'Importo sconto',
+    'LBL_AN_QUOTES_ITEM_TOTAL' => 'Subtotale',
+    'LBL_AN_QUOTES_GRAND_TOTAL' => 'Totale',
+    'LBL_AN_QUOTES_ASSIGNED_TO' => 'Utente Assegnato',
+    'LBL_AN_QUOTES_DATE_CREATED' => 'Data di Creazione',
+    'LBL_AN_QUOTES_DAY_CREATED' => 'Giorno creazione',
+    'LBL_AN_QUOTES_WEEK_CREATED' => 'Settimana creazione',
+    'LBL_AN_QUOTES_MONTH_CREATED' => 'Mese creazione',
+    'LBL_AN_QUOTES_QUARTER_CREATED' => 'Trimestre creazione',
+    'LBL_AN_QUOTES_YEAR_CREATED' => 'Anno creazione',
+
+    //Error message when there are multiple values for the label
+    'LBL_AN_DUPLICATE_LABEL_FOR_SUBAREA' => 'Errore nell\'identificazione dell\'etichetta per la sottozona pivot',
+);
