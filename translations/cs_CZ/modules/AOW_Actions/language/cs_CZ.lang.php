074b4b9a062674f66225c00be7cac90f3f3f0a6c
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 02:34:58 +0000

    New translations en_us.lang.php (Czech)

---
+++ translations/cs_CZ/modules/AOW_Actions/language/cs_CZ.lang.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ID' => 'ID',
+    'LBL_DATE_ENTERED' => 'Datum vytvoření',
+    'LBL_DATE_MODIFIED' => 'Datum změny',
+    'LBL_MODIFIED' => 'Změnil',
+    'LBL_MODIFIED_ID' => 'Změnil ID',
+    'LBL_MODIFIED_NAME' => 'Změněno dle Jména',
+    'LBL_CREATED_USER' => 'Vytvořeno dle Uživatele',
+    'LBL_MODIFIED_USER' => 'Změněno dle Uživatele',
+    'LBL_CREATED' => 'Vytvořeno',
+    'LBL_DESCRIPTION' => 'Popis',
+    'LBL_CREATED_ID' => 'Vytvořil ID',
+    'LBL_DELETED' => 'Vymazáno',
+    'LBL_NAME' => 'Název',
+    'LBL_MODULE_NAME' => 'Akce workflow',
+    'LBL_MODULE_TITLE' => 'Akce workflow',
+    'LBL_AOW_WORKFLOW_ID' => 'AOW_WorkFlow ID',
+    'LBL_ACTION' => 'Akce',
+    'LBL_PARAMETERS' => 'Parametry',
+    'LBL_SELECT_ACTION' => 'Vybrat akci',
+    'LBL_CREATE_EMAIL_TEMPLATE' => 'Přidat',
+    'LBL_RECORD_TYPE' => 'Typ záznamu',
+    'LBL_SENDEMAIL' => 'Odeslat email',
+    'LBL_CREATERECORD' => 'Vytvořit záznam',
+    'LBL_MODIFYRECORD' => 'Změnit záznam',
+    'LBL_ADD_FIELD' => 'Přidat pole',
+    'LBL_ADD_RELATIONSHIP' => 'Přidat vztah',
+    'LBL_EDIT_EMAIL_TEMPLATE' => 'Upravit',
+    'LBL_EMAIL' => 'E-mail',
+    'LBL_EMAIL_TEMPLATE' => 'Vzor zprávy',
+    'LBL_SETAPPROVAL' => 'Nastavit schválení',
+    'LBL_RELATE_WORKFLOW' => 'Vztah k modulu WorkFlow',
+    'LBL_INDIVIDUAL_EMAILS' => 'Odeslat jednotlivé E-maily',
+    'LBL_COMPUTEFIELD' => 'Vypočítat pole',
+    'LBL_COMPUTEFIELD_PARAMETERS' => 'Parametry',
+    'LBL_COMPUTEFIELD_FIELD_NAME' => 'Název pole',
+    'LBL_COMPUTEFIELD_IDENTIFIER' => 'Identifikátor',
+    'LBL_COMPUTEFIELD_ADD_PARAMETER' => 'Přidat parametr',
+    'LBL_COMPUTEFIELD_RELATION_PARAMETERS' => 'Parametry vztahu',
+    'LBL_COMPUTEFIELD_RELATION' => 'Vztah',
+    'LBL_COMPUTEFIELD_ADD_RELATION_PARAMETER' => 'Přidat parametr vztahu',
+    'LBL_COMPUTEFIELD_FORMULA' => 'Vzorec',
+    'LBL_COMPUTEFIELD_FORMULAS' => 'Formulace',
+    'LBL_COMPUTEFIELD_ADD_FORMULA' => 'Přidat vzorec',
+    'LBL_COMPUTEFIELD_VALUE_TYPE' => 'Typ hodnoty',
+    'LBL_COMPUTEFIELD_RAW_VALUE' => 'Nezpracovaná hodnota',
+    'LBL_COMPUTEFIELD_FORMATTED_VALUE' => 'Formátovaná hodnota',
+    'LBL_COPY_EMAIL_ADDRESSES_WORKFLOW' => 'Kopírovat e-maily z modulu WorkFlow'
+);
