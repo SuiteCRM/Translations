7954c053e0c0531719a7687287e9d206b34adc18
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 03:13:47 +0000

    New translations en_us.lang.php (Polish)

---
+++ translations/pl_PL/modules/MailMerge/language/pl_PL.lang.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_STEP_1' => 'Krok 1: Wybierz moduł i szablon',
+    'LBL_MAILMERGE_MODULE' => 'Wybierz Modul:',
+    'LBL_MAILMERGE_SELECTED_MODULE' => 'Wybrany Modul:',
+    'LBL_MAILMERGE_TEMPLATES' => 'Wybrany Szablon:',
+    'LBL_STEP_4' => 'Przegląd i Zakończony',
+    'LBL_SELECTED_MODULE' => 'Wybrany Modul:',
+    'LBL_SELECTED_TEMPLATE' => 'Wybrany Szablon:',
+    'LBL_SELECTED_ITEMS' => 'Wybrane Pozycje:',
+    'LBL_STEP_5' => 'Integracja Poczty Ukończona',
+    'LBL_MERGED_FILE' => 'Polączone Pola:',
+    'LNK_NEW_MAILMERGE' => 'Rozpocznij Korespondencję Seryjną',
+    'LNK_UPLOAD_TEMPLATE' => 'Załaduj Szablon',
+    'LBL_DOC_NAME' => 'Nazwa dokumentu:',
+    'LBL_FILENAME' => 'Nazwa Pliku:',
+    'LBL_DOC_VERSION' => 'Wersja:',
+    'LBL_DOC_DESCRIPTION' => 'Opis:',
+    'LBL_LIST_NAME' => 'Nazwa',
+    'LBL_FINISH' => 'Rozpocznij Łączenie',
+    'LBL_NEXT' => 'Dalej >',
+    'LBL_BACK' => '< Wstecz',
+    'LBL_START' => 'Rozpocznij Ponownie',
+    'LBL_CONTAINS_CONTACT_INFO' => 'Wybrane Szablony zawieraja informacje o kontaktach. ',
+    'LBL_ADDIN_NOTICE' => 'To wymaga instalacji dodatku do korespondencji seryjnej SuiteCRM w programie Microsoft Word (Mail Merge add-in).',
+    'LBL_REMOVE' => 'Usuń element(y)' /*for 508 compliance fix*/,
+    'LBL_ADD' => 'Dodaj element(y)' /*for 508 compliance fix*/,
+    'LBL_BROWSER_REQUIREMENT' => 'To wymaga IE 6.0 lub nowszego dla systemu Windows.',
+    'LBL_FORM_SELECT_TEMPLATE' => 'Proszę wybrać szablon.',
+    'LBL_SELECT_ONE' => 'Wybierz co najmniej jeden element.',
+    'LBL_AVAILABLE' => 'Dostępne',
+    'LBL_SELECTED' => 'Wybrane',
+    'LBL_RELATED_INFO_SELECTION' => 'Wykorzystaj poniższe opcje aby zidentyfikować powiązane informacje, które chcesz wyświetlić w scalanym dokumencie. Pomiń ten krok, jeśli szablon dokumentu nie zawiera powiązanych informacji.',
+
+);
