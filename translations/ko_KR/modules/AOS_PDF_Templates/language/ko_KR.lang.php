dd208370be066df1bfcce7fb562627b041d93377
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 03:02:23 +0000

    New translations en_us.lang.php (Korean)

---
+++ translations/ko_KR/modules/AOS_PDF_Templates/language/ko_KR.lang.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID' => '할당된 사용자 Id',
+    'LBL_ASSIGNED_TO_NAME' => '담당자',
+    'LBL_ID' => 'ID',
+    'LBL_DATE_ENTERED' => '생성날짜',
+    'LBL_DATE_MODIFIED' => '수정일',
+    'LBL_MODIFIED' => '변경됨',
+    'LBL_MODIFIED_NAME' => '이름으로 변경됨',
+    'LBL_CREATED' => '작성자',
+    'LBL_DESCRIPTION' => '본문',
+    'LBL_HEADER' => '머리글',
+    'LBL_FOOTER' => '바닥글',
+    'LBL_DELETED' => '삭제됨',
+    'LBL_NAME' => '이름',
+    'LBL_CREATED_USER' => '사용자로 생성됨',
+    'LBL_MODIFIED_USER' => '변경유저',
+    'LBL_LIST_FORM_TITLE' => 'PDF 서식 목록',
+    'LBL_MODULE_NAME' => 'PDF 서식',
+    'LBL_MODULE_TITLE' => 'PDF 서식: 홈',
+    'LBL_HOMEPAGE_TITLE' => '내 PDF 서식',
+    'LNK_NEW_RECORD' => 'PDF 서식 작성',
+    'LNK_LIST' => 'PDF 서식 보기',
+    'LBL_SEARCH_FORM_TITLE' => 'PDF 서식 검색',
+    'LBL_HISTORY_SUBPANEL_TITLE' => '이력 표시',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => '영업활동',
+    'LBL_NEW_FORM_TITLE' => '신규 PDF 서식',
+    'LBL_TYPE' => '종류',
+    'LBL_ACTIVE' => '활성',
+    'LBL_BUTTON_INSERT' => '삽입',
+    'LBL_WARNING_OVERWRITE' => '경고 지금 당신이 하고 있는 작업을 덮어씁니다',
+    'LBL_INSERT_FIELDS' => '필드 삽입',
+
+    'LBL_SAMPLE' => '샘플 가져오기',
+    'LBL_PAGE' => '페이지',
+    'LBL_PREPARED_FOR' => '에 대한 준비',
+    'LBL_PREPARED_BY' => '에 의한 준비',
+    'LBL_QUOTE_SAMPLE' => 'Quote Sample',
+    'LBL_INVOICE_SAMPLE' => 'Invoice Sample',
+    'LBL_ACCOUNT_SAMPLE' => 'Account Sample',
+    'LBL_CONTACT_SAMPLE' => 'Contact Sample',
+    'LBL_LEAD_SAMPLE' => 'Lead Sample',
+    'LBL_ANY_STREET' => '어떤 거리',
+    'LBL_ANY_TOWN' => '어떤 마을',
+    'LBL_ANY_WHERE' => '어디든지',
+
+    'LBL_QUOTE_GROUP_SAMPLE' => 'Quote Group Sample',
+    'LBL_INVOICE_GROUP_SAMPLE' => 'Invoice Group Sample',
+    'LBL_MARGIN_LEFT' => '왼쪽 여백',
+    'LBL_MARGIN_RIGHT' => '여백 오른쪽',
+    'LBL_MARGIN_TOP' => '상단 여백',
+    'LBL_MARGIN_BOTTOM' => '하단 여백',
+    'LBL_MARGIN_HEADER' => '여백 머리글',
+    'LBL_MARGIN_FOOTER' => '여백 바닥글',
+    'LBL_EDITVIEW_PANEL1' => '여백',
+    'LBL_DETAILVIEW_PANEL1' => '여백',
+    'LBL_PAGE_SIZE' => 'Page Size',
+    'LBL_ORIENTATION' => 'Orientation',
+);
