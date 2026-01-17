1dbe599eae86a8326c73391fbca76d02bb5f148f
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 04:46:04 +0000

    New translations en_us.lang.php (Finnish)

---
+++ translations/fi_FI/modules/EmailMarketing/language/fi_FI.lang.php
@@ -0,0 +1,108 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_REPLY_ADDR' => '"Vastaa" osoite: ',
+    'LBL_REPLY_NAME' => '"Vastaa" Nimi: ',
+
+    'LBL_MODULE_NAME' => 'Sähköpostimarkkinointi',
+    'LBL_MODULE_TITLE' => 'Sähköpostimarkkinointi: Etusivu',
+    'LBL_LIST_FORM_TITLE' => 'Sähköpostikampanjat',
+    'LBL_NAME' => 'Nimi: ',
+    'LBL_LIST_NAME' => 'Nimi',
+    'LBL_LIST_FROM_ADDR' => 'Lähettäjä',
+    'LBL_LIST_DATE_START' => 'Aloitus pvm',
+    'LBL_LIST_TEMPLATE_NAME' => 'Viestipohja',
+    'LBL_LIST_STATUS' => 'Tila',
+    'LBL_STATUS' => 'Tila',
+    'LBL_STATUS_TEXT' => 'Tila:',
+    'LBL_TEMPLATE_NAME' => 'Mallipohjan nimi',
+    'LBL_DATE_ENTERED' => 'Kirjauspvm',
+    'LBL_DATE_MODIFIED' => 'Muokkaus pvm',
+    'LBL_MODIFIED' => 'Muokkaaja: ',
+    'LBL_CREATED' => 'Tekijä: ',
+    'LBL_MESSAGE_FOR' => 'Lähetä viesti vastaanottajille:',
+
+    'LBL_FROM_NAME' => 'Lähettäjän nimi: ',
+    'LBL_FROM_ADDR' => 'Lähettäjän sähköpostiosoite: ',
+    'LBL_DATE_START' => 'Aloitus pvm',
+    'LBL_TIME_START' => 'Alkamisaika ',
+    'LBL_START_DATE_TIME' => 'Aloitus pvm & aika: ',
+    'LBL_TEMPLATE' => 'Viestipohja: ',
+
+    'LBL_MODIFIED_BY' => 'Muokkaaja: ',
+    'LBL_CREATED_BY' => 'Tekijä: ',
+
+    'LNK_NEW_CAMPAIGN' => 'Luo kampanja',
+    'LNK_CAMPAIGN_LIST' => 'Kampanjat',
+    'LNK_NEW_PROSPECT_LIST' => 'Luo prospektilista',
+    'LNK_PROSPECT_LIST_LIST' => 'Prospektilistat',
+    'LNK_NEW_PROSPECT' => 'Luo prospekti',
+    'LNK_PROSPECT_LIST' => 'Prospektit',
+    'LBL_DEFAULT_SUBPANEL_TITLE' => 'Sähköpostimarkkinointi',
+    'LBL_CREATE_EMAIL_TEMPLATE' => 'Luo',
+    'LBL_EDIT_EMAIL_TEMPLATE' => 'Muokkaa',
+    'LBL_FROM_MAILBOX' => 'Postitililtä',
+    'LBL_FROM_MAILBOX_NAME' => 'Käytä postitiliä:',
+    'LBL_OUTBOUND_EMAIL_ACCOUNT_NAME' => 'Outbound Email Account:',
+    'LBL_PROSPECT_LIST_SUBPANEL_TITLE' => 'Prospektilistat',
+    'LBL_ALL_PROSPECT_LISTS' => 'Tee valinta valitaksesi kaikki kampanjan prospektilistat.',
+    'LBL_RELATED_PROSPECT_LISTS' => 'Viestiin liittyvät kaikki prospektilistat.',
+    'LBL_PROSPECT_LIST_NAME' => 'Prospektilistan nimi',
+
+    'LBL_LIST_PROSPECT_LIST_NAME' => 'Targeted Lists',
+    'LBL_MODULE_SEND_TEST' => 'Campaign: Send Test',
+    'LBL_MODULE_SEND_EMAILS' => 'Campaign: Send Emails',
+    'LBL_SCHEDULE_MESSAGE_TEST' => 'Please select the campaign messages that you would like to test:',
+    'LBL_SCHEDULE_MESSAGE_EMAILS' => 'Please select the campaign messages that you would like to schedule for distribution on the specified start date and time:',
+    'LBL_SCHEDULE_BUTTON_TITLE' => 'Lähetä',
+    'LBL_SCHEDULE_BUTTON_LABEL' => 'Lähetä',
+    'LBL_ERROR_ON_MARKETING' => 'Pakollinen kenttä puuttuu',
+
+    'LBL_CAMPAIGN_ID' => 'Kampanja ID',
+    'LBL_OUTBOUND_EMAIL_ACOUNT_ID' => 'Outbound Email Account ID',
+    'LBL_EMAIL_TEMPLATE' => 'Viestipohja',
+    'LBL_PROSPECT_LISTS' => 'Prospektilistat',
+
+);
