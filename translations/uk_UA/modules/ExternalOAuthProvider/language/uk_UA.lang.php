af63e965d976c65227c449c3bdd6ac268fb82c0c
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 04:52:08 +0000

    New translations en_us.lang.php (Ukrainian)

---
+++ translations/uk_UA/modules/ExternalOAuthProvider/language/uk_UA.lang.php
@@ -0,0 +1,110 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2022 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = [
+
+    'LBL_ASSIGNED_TO_ID' => 'Код користувача відповідальної особи',
+    'LBL_ASSIGNED_TO_NAME' => 'Відповідальна особа',
+    'LBL_ID' => 'ID',
+    'LBL_DATE_ENTERED' => 'Дата створення',
+    'LBL_DATE_MODIFIED' => 'Дата зміни',
+    'LBL_MODIFIED' => 'Змінено',
+    'LBL_MODIFIED_NAME' => 'Змінено користувачем',
+    'LBL_CREATED' => 'Ким створено',
+    'LBL_DESCRIPTION' => 'Опис',
+    'LBL_DELETED' => 'Видалено',
+    'LBL_NAME' => 'Назва',
+    'LBL_CREATED_USER' => 'Створено користувачем',
+    'LBL_MODIFIED_USER' => 'Змінено користувачем',
+    'LBL_LIST_NAME' => 'Назва',
+    'LBL_EDIT_BUTTON' => 'Змінити',
+    'LBL_REMOVE' => 'Видалити',
+    'LBL_LIST_FORM_TITLE' => 'Сторонні постачальники OAuth',
+    'LBL_MODULE_NAME' => 'Сторонні постачальники OAuth',
+    'LBL_MODULE_TITLE' => 'Сторонні постачальники OAuth',
+    'LBL_HOMEPAGE_TITLE' => 'Мої сторонні постачальники OAuth',
+    'LNK_NEW_RECORD' => 'Додати постачальника OAuth',
+
+    'LNK_LIST' => 'Сторонні постачальники OAuth',
+    'LBL_SEARCH_FORM_TITLE' => 'Шукати постачальника OAuth',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Історія',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Заходи',
+    'LBL_NEW_FORM_TITLE' => 'Новий сторонній постачальник OAuth',
+
+    'LBL_LIST_DELETE' => 'Видалити',
+    'LBL_TYPE' => 'Тип',
+    'LBL_CONNECTOR' => 'Конектор',
+    'LBL_REDIRECT_URI' => 'URI перенаправлення',
+    'LBL_REDIRECT_URI_TYPE' => 'Redirect URI Type',
+
+    'LBL_CLIENT_ID' => 'Ідентифікатор клієнта',
+    'LBL_CLIENT_SECRET' => 'Ключ клієнта',
+    'LBL_SCOPE' => 'Область застосування',
+    'LBL_URL_AUTHORIZE' => 'Url авторизації',
+    'LBL_AUTHORIZE_URL_OPTIONS' => 'Параметри авторизації Url',
+    'LBL_URL_ACCESS_TOKEN' => 'Url-адреса токена доступу',
+    'LBL_EXTRA_PROVIDER_PARAMS' => 'Додаткові параметри постачальника',
+    'LBL_GET_TOKEN_REQUEST_GRANT' => 'Отримати тип запиту дозволу токена',
+    'LBL_GET_TOKEN_REQUEST_OPTIONS' => 'Отримати параметри запиту токена',
+    'LBL_REFRESH_TOKEN_REQUEST_GRANT' => 'Оновити тип запиту дозволу жетона',
+    'LBL_REFRESH_TOKEN_REQUEST_OPTIONS' => 'Оновити параметри запиту жетона',
+
+    'LBL_ACCESS_TOKEN_MAPPING' => 'Відображення жетону доступу',
+    'LBL_EXPIRES_IN_MAPPING' => 'Термін дії закінчується у відображенні',
+    'LBL_REFRESH_TOKEN_MAPPING' => 'Оновити відображення жетонів',
+    'LBL_TOKEN_TYPE_MAPPING' => 'Відображення типу жетонів',
+
+    'LBL_EXTRA' => 'Додаткові налаштування',
+    'LBL_MAPPING' => 'Відображення налаштувань',
+    'LBL_OTHER' => 'Інший',
+
+
+    'LNK_LIST_CREATE_NEW_PERSONAL' => 'New Personal OAuth Provider',
+    'LNK_LIST_CREATE_NEW_GROUP' => 'New Group OAuth Provider',
+    'LNK_LIST_INBOUND_EMAILS' => 'Облікові записи вхідної пошти',
+    'LNK_LIST_OUTBOUND_EMAILS' => 'Облікові записи вихідної електронної пошти',
+    'LNK_LIST_EXTERNAL_OAUTH_CONNECTION' => 'Зовнішні OAuth підключення',
+
+    'LBL_OWNER' => 'Власник',
+];
