e671cd2587663b8ebd5b66b687b3347eca8fb016
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 02:21:22 +0000

    New translations en_us.lang.php (Romanian)

---
+++ translations/ro_RO/modules/OAuth2Clients/language/ro_RO.lang.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = [
+    'LBL_MODULE_NAME' => 'Clienți OAuth',
+    'LBL_MODULE_TITLE' => 'Clienți OAuth',
+    'LBL_MODULE_ID' => 'Clienți OAuth',
+    'LBL_IS_CONFIDENTIAL' => 'Este confidențial',
+    'LBL_SECRET' => 'Secret',
+    'LBL_SECRET_HASHED' => 'Schimbare secret',
+    'LBL_LEAVE_BLANK' => 'Lasă necompletat dacă nu se schimbă',
+    'LBL_REMEMBER_SECRET' => 'Vă rugăm să rețineți secretul deoarece acesta nu va fi disponibil după ce salvați.',
+    'LBL_REDIRECT_URL' => 'URL de redirecționare',
+    'LBL_ALLOWED_GRANT_TYPE' => 'Tip de acordare permisiune',
+    'LBL_DURATION_AMOUNT' => 'Valoare durată',
+    'LBL_DURATION_UNIT' => 'Unitate de durată',
+    'LBL_DURATION_VALUE' => 'Valoare durată',
+    'LBL_USER' => 'Utilizator asociat',
+
+    'LBL_OAUTHTOKENS_SUBPANEL_TITLE' => 'Token-uri OAuth2 active',
+    'LBL_TOKEN_ID' => 'Token',
+    'LBL_DATE_ENTERED' => 'Data creării',
+    'LBL_ACCESS_TOKEN_EXPIRES' => 'Token-ul de acces expiră',
+    'LBL_REFRESH_TOKEN_EXPIRES' => 'Token-ul de reîmprospătare expiră',
+
+    'LNK_OAUTH2_TOKEN_LIST'=> 'Listă Token-uri OAuth2',
+    'LNK_OAUTH2_CLIENT_LIST' => 'Lista de clienți OAuth2',
+    'LNK_OAUTH2_AUTHCODE_LIST' => 'List OAuth2 Authorization Codes',
+
+    'LNK_NEW_OAUTH2_CLIENT' => 'Creare client OAuth2',
+    'LNK_NEW_OAUTH2_PASSWORD_CLIENT' => 'Client parolă nouă',
+    'LNK_NEW_OAUTH2_CREDENTIALS_CLIENT' => 'Client nou cu autentificare proprie',
+    'LNK_NEW_OAUTH2_IMPLICIT_CLIENT' => 'Client implicit nou',
+    'LNK_NEW_OAUTH2_AUTHORIZATION_CLIENT' => 'Client de autorizare nou',
+
+];
