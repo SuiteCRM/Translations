306760c5ece9b5abf6eb96d6f04df08d806e3ed7
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 03:56:18 +0000

    New translations manifest.php (Estonian)

---
+++ translations/et_EE/manifest.php
@@ -0,0 +1,25 @@
+<?php
+/******************************************
+ * SuiteCRM Translations
+ * @URL: https://crowdin.com/project/suitecrmtrans
+ * @author SuiteCRM Community via Crowdin
+ ******************************************/
+
+$manifest = array(
+    'name' => 'Estonian (Estonia)',
+    'description' => 'Translation: crowdin.com/project/suitecrmtranslations',
+    'type' => 'langpack',
+    'is_uninstallable' => 'Jah',
+    'author' => 'SuiteCRM kogukonna versioon',
+    'version' => '7.15-1',
+    'published_date' => '2026-01-16',
+);
+$installdefs = array(
+    'id' => 'et_EE',
+    'image_dir' => '<basepath>/images',
+    'copy' => array(
+        array('from' => '<basepath>/include', 'to' => 'include'),
+        array('from' => '<basepath>/modules', 'to' => 'modules'),
+        array('from' => '<basepath>/install', 'to' => 'install'),
+    )
+);
