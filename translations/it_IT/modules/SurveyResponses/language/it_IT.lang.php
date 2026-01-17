fe99dde100ce39985daa8330fbb7574c447564a0
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 02:57:39 +0000

    New translations en_us.lang.php (Italian)

---
+++ translations/it_IT/modules/SurveyResponses/language/it_IT.lang.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID'                                                             => 'Id utente assegnato',
+    'LBL_ASSIGNED_TO_NAME'                                                           => 'Assegnato a',
+    'LBL_SECURITYGROUPS'                                                             => 'Gruppi di Sicurezza',
+    'LBL_SECURITYGROUPS_SUBPANEL_TITLE'                                              => 'Gruppi di Sicurezza',
+    'LBL_ID'                                                                         => 'ID',
+    'LBL_DATE_ENTERED'                                                               => 'Data di Creazione',
+    'LBL_DATE_MODIFIED'                                                              => 'Data Modifica',
+    'LBL_MODIFIED'                                                                   => 'Modificato da',
+    'LBL_MODIFIED_ID'                                                                => 'Modificato da ID',
+    'LBL_MODIFIED_NAME'                                                              => 'Modificato da Nome',
+    'LBL_CREATED'                                                                    => 'Creato da',
+    'LBL_CREATED_ID'                                                                 => 'Creato Da Id',
+    'LBL_DESCRIPTION'                                                                => 'Descrizione',
+    'LBL_DELETED'                                                                    => 'Eliminato',
+    'LBL_NAME'                                                                       => 'Nome',
+    'LBL_CREATED_USER'                                                               => 'Creato dall\'utente',
+    'LBL_MODIFIED_USER'                                                              => 'Modificato dall\'utente',
+    'LBL_LIST_NAME'                                                                  => 'Nome',
+    'LBL_EDIT_BUTTON'                                                                => 'Modifica',
+    'LBL_REMOVE'                                                                     => 'Cancella',
+    'LBL_LIST_FORM_TITLE'                                                            => 'Elenco Risposte al Sondaggio',
+    'LBL_MODULE_NAME'                                                                => 'Risposte al sondaggio',
+    'LBL_MODULE_TITLE'                                                               => 'Risposte al sondaggio',
+    'LBL_HOMEPAGE_TITLE'                                                             => 'Le mie risposte al sondaggio',
+    'LNK_NEW_RECORD'                                                                 => 'Crea le risposte al Sondaggio',
+    'LNK_LIST'                                                                       => 'Vedi le risposte al sondaggio',
+    'LNK_IMPORT_SURVEYRESPONSES'                                                     => 'Importa le risposte al sondaggio',
+    'LBL_SEARCH_FORM_TITLE'                                                          => 'Cerca le risposte al sondaggio',
+    'LBL_HISTORY_SUBPANEL_TITLE'                                                     => 'Visualizza Cronologia',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE'                                                  => 'Attività',
+    'LBL_SURVEYRESPONSES_SUBPANEL_TITLE'                                             => 'Risposte al sondaggio',
+    'LBL_NEW_FORM_TITLE'                                                             => 'Nuove risposte al sondaggio',
+    'LBL_HAPPINESS'                                                                  => 'Felicità',
+    'LBL_EMAIL_RESPONSE_SENT'                                                        => 'Risposta all\'email inviata',
+    'LBL_SURVEYRESPONSES_SURVEYQUESTIONRESPONSES_FROM_SURVEYQUESTIONRESPONSES_TITLE' => 'Risposte alla domanda del sondaggio',
+    'LBL_SURVEYRESPONSES_CONTACTS_FROM_CONTACTS_TITLE'                               => 'Contatto',
+    'LBL_SURVEYRESPONSES_ACCOUNTS_FROM_ACCOUNTS_TITLE'                               => 'Azienda',
+    'LBL_SURVEYS_SURVEYRESPONSES_FROM_SURVEYS_TITLE'                                 => 'Sondaggio',
+    'LBL_QUESTION_RESPONSES_DISPLAY'                                                 => 'Risposte alle domande',
+    'LBL_SURVEYRESPONSES_CAMPAIGNS_FROM_CAMPAIGNS_TITLE'                             => 'Campagna',
+    'LBL_QUESTION' => 'Domanda',
+    'LBL_RESPONSE' => 'Risposta'
+
+);
