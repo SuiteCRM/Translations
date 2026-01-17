ce47e584d0c4ba0d609f3c5ff4ed8dc14e2489c1
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 02:53:55 +0000

    New translations en_us.lang.php (Armenian)

---
+++ translations/hy_AM/modules/AOS_Products/language/hy_AM.lang.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID' => 'Պատասխանատու- ID',
+    'LBL_ASSIGNED_TO_NAME' => 'Կատարող',
+    'LBL_ID' => 'ID',
+    'LBL_DATE_ENTERED' => 'Ստեղծման ամսաթիվ',
+    'LBL_DATE_MODIFIED' => 'Փոփոխման ամսաթիվ',
+    'LBL_MODIFIED' => 'Փոփոխված է',
+    'LBL_MODIFIED_NAME' => 'Փոփոխված է ըստ անվանման',
+    'LBL_CREATED' => 'Ստեղծված է',
+    'LBL_DESCRIPTION' => 'Նկարագրություն',
+    'LBL_DELETED' => 'Ջնջված է',
+    'LBL_NAME' => 'Ապրանքի անվանումը',
+    'LBL_CREATED_USER' => 'Փոփոխված է օգտագործողի կողմից ',
+    'LBL_MODIFIED_USER' => 'Փոփոխված է օգտագործողի կողմից',
+    'LBL_LIST_FORM_TITLE' => 'Ապրանքների ցանկ',
+    'LBL_MODULE_NAME' => 'Արտադրանք',
+    'LBL_MODULE_TITLE' => 'Արտադրանք․ Գլխավոր',
+    'LBL_HOMEPAGE_TITLE' => 'Իմ ապրանքները',
+    'LNK_NEW_RECORD' => 'Ստեղծել ապրանք',
+    'LNK_LIST' => 'Դիտել ապրանքները',
+    'LBL_SEARCH_FORM_TITLE' => 'Որոնել ապրանքները',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Դիտել պատմությունը',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Գործունեություն',
+    'LBL_NEW_FORM_TITLE' => 'Նոր ապրանք',
+    'LBL_MAINCODE' => 'Ապրանքի կոդը',
+    'VALUE' => 'Ապրանքի անվանումը',
+    'LBL_PART_NUMBER' => 'Կոդի համարը',
+    'LBL_CATEGORY' => 'Կատեգորիա',
+    'LBL_TYPE' => 'Ապրանքի տեսակը',
+    'LBL_COST' => 'Արժեքը',
+    'LBL_PRICE' => 'Գինը',
+    'LBL_URL' => 'URL հասցե',
+    'LBL_CONTACT' => 'Կոնտակտ',
+    'LBL_PRODUCT_IMAGE' => 'Ապրանքի նկարը',
+    'LBL_IMAGE_UPLOAD_FAIL' => 'Սխալ․ բեռնված ֆայլի չափը գերազանցում է առավելագույն թույլատրելի շեմը․ առավելագույն շեմը․',
+    'LBL_AOS_PRODUCT_CATEGORYS_NAME' => 'Ապրանքի կատեգորիան',
+    'LBL_AOS_PRODUCT_CATEGORY' => 'Ապրանքի կատեգորիա ID',
+    'LBL_AOS_PRODUCT_CATEGORIES' => 'Ապրանքի կատեգորիաներ',
+    'LBL_COST_USDOLLAR' => 'Արժեքը (ընդունված արժույթը)',
+    'LBL_PRICE_USDOLLAR' => 'Գինը (ընդունված արժույթը)',
+    'LBL_FILE_URL' => 'URL ֆայլ',
+    'LBL_CUSTOMERS_PURCHASED_PRODUCTS_SUBPANEL_TITLE' => 'Գնումները',
+    'LBL_PRODUCTS_PURCHASES' => 'Գնումները',
+    'LBL_AOS_QUOTE_NAME' => 'Առաջարկ',
+    'LBL_ACCOUNT_NAME' => 'Հաշիվ',
+);
