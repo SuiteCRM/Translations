e66ad8fc10db7f388a167fce586249866dfba16e
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 02:54:52 +0000

    New translations en_us.lang.php (Armenian)

---
+++ translations/hy_AM/modules/ProspectLists/language/hy_AM.lang.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_MODULE_NAME' => 'Նպատակակետերի ցուցակ',
+    'LBL_MODULE_ID' => 'Նպատակակետերի ցուցակ',
+    'LBL_MODULE_TITLE' => 'Հասցեատերեր․ Գլխավոր',
+    'LBL_SEARCH_FORM_TITLE' => 'Թիրախների ցուցակի որոնում',
+    'LBL_LIST_FORM_TITLE' => 'Նպատակակետերի ցուցակ',
+    'LBL_PROSPECT_LIST_NAME' => 'Թիրախների ցուցակ․',
+    'LBL_NAME' => 'Անվանում',
+    'LBL_ENTRIES' => 'Ընդամենը գրառումներ',
+    'LBL_LIST_PROSPECT_LIST_NAME' => 'Թիրախների ցուցակ',
+    'LBL_LIST_ENTRIES' => 'Թիրախներ ցուցակից',
+    'LBL_LIST_DESCRIPTION' => 'Նկարագրություն',
+    'LBL_LIST_TYPE_NO' => 'Տեսակ',
+    'LBL_LIST_END_DATE' => 'Ավարտման ամսաթիվը',
+    'LBL_DATE_ENTERED' => 'Ստեղծման ամսաթիվ',
+    'LBL_MARKETING_ID' => 'Մարքեթինգային արշավի ID',
+    'LBL_DATE_MODIFIED' => 'Փոփոխման ամսաթիվ',
+    'LBL_MODIFIED' => 'Փոփոխված է',
+    'LBL_CREATED' => 'Ստեղծված է․',
+    'LBL_ASSIGNED_TO' => 'Կատարող',
+    'LBL_DESCRIPTION' => 'Նկարագրություն',
+    'LNK_NEW_CAMPAIGN' => 'Ստեղծել մարքեթինգային արշավ',
+    'LNK_CAMPAIGN_LIST' => 'Կամպանիաներ',
+    'LNK_NEW_PROSPECT_LIST' => 'Ստեղծել թիրախների ցուցակ',
+    'LNK_PROSPECT_LIST_LIST' => 'Ցույց տալ թիրախների ցուցակը',
+    'LBL_MODIFIED_BY' => 'Փոփոխված է',
+    'LBL_CREATED_BY' => 'Ստեղծված է․',
+    'LNK_NEW_PROSPECT' => 'Ստեղծել թիրախ',
+    'LNK_PROSPECT_LIST' => 'Նպատակակետեր',
+
+    'LBL_CONTACTS_SUBPANEL_TITLE' => 'Կոնտակտներ',
+    'LBL_LEADS_SUBPANEL_TITLE' => 'Պոտենցիալ կլիենտներ (առաջնորդներ)',
+    'LBL_PROSPECTS_SUBPANEL_TITLE' => 'Նպատակակետեր',
+    'LBL_ACCOUNTS_SUBPANEL_TITLE' => 'Հաշիվներ',
+    'LBL_COPY_PREFIX' => 'Պատճեն',
+    'LBL_USERS_SUBPANEL_TITLE' => 'Օգտվողներ',
+    'LBL_TYPE' => 'Տեսակ',
+    'LBL_LIST_TYPE' => 'Տեսակ',
+    'LBL_LIST_TYPE_LIST_NAME' => 'Տեսակ',
+    'LBL_NEW_FORM_TITLE' => 'Նոր թիրախների ցուցակ',
+    'LBL_MARKETING_NAME' => 'Մարքեթինգային անվանում',
+    'LBL_DOMAIN_NAME' => 'Դոմենի անունը',
+    'LBL_DOMAIN' => 'Չկան էլ-նամակներ դոմենում',
+    'LBL_LIST_PROSPECTLIST_NAME' => 'Անվանում',
+
+    'LBL_EMAIL_MARKETING' => 'Էլ-փոստի մարքեթինգ',
+
+    'LBL_ASCENDING' => 'Բարձրացող',
+    'LBL_DESCENDING' => 'Վայրընթաց',
+);
