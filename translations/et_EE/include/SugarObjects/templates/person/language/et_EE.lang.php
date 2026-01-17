380e812e85be93dd168f4e270853269a547f0369
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 03:56:11 +0000

    New translations en_us.lang.php (Estonian)

---
+++ translations/et_EE/include/SugarObjects/templates/person/language/et_EE.lang.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_SALUTATION' => 'Tiitel',
+    'LBL_NAME' => 'Nimi',
+    'LBL_FIRST_NAME' => 'Eesnimi',
+    'LBL_LAST_NAME' => 'Perekonnanimi',
+    'LBL_TITLE' => 'Tiitel',
+    'LBL_DEPARTMENT' => 'Osakond',
+    'LBL_DO_NOT_CALL' => 'Mitte helistada',
+    'LBL_HOME_PHONE' => 'Telefon kodus',
+    'LBL_MOBILE_PHONE' => 'Mobiil',
+    'LBL_OFFICE_PHONE' => 'Töötelefon',
+    'LBL_OTHER_PHONE' => 'Teine telefon',
+    'LBL_FAX_PHONE' => 'Faks',
+    'LBL_EMAIL_ADDRESS' => 'E-posti aadress(d)',
+    'LBL_PRIMARY_ADDRESS' => 'Esmane aadress',
+    'LBL_PRIMARY_ADDRESS_STREET' => 'Esmane aadress',
+    'LBL_PRIMARY_ADDRESS_STREET_2' => 'Esmane aadress Tänav 2:',
+    'LBL_PRIMARY_ADDRESS_STREET_3' => 'Esmane aadress Tänav 3:',
+    'LBL_PRIMARY_ADDRESS_CITY' => 'Esmane linn',
+    'LBL_PRIMARY_ADDRESS_STATE' => 'Esmane riik',
+    'LBL_PRIMARY_ADDRESS_POSTALCODE' => 'Esmane postiindeks',
+    'LBL_PRIMARY_ADDRESS_COUNTRY' => 'Esmane aadress maakond:',
+    'LBL_ALT_ADDRESS' => 'Alternatiivne aadress',
+    'LBL_ALT_ADDRESS_STREET' => 'Alternatiivne aadress',
+    'LBL_ALT_ADDRESS_STREET_2' => 'Alternatiivne aadress tänav 2:',
+    'LBL_ALT_ADDRESS_STREET_3' => 'Alternatiivne aadress tänav 3:',
+    'LBL_ALT_ADDRESS_CITY' => 'Alternatiivne linn:',
+    'LBL_ALT_ADDRESS_STATE' => 'Alternatiivne riik:',
+    'LBL_ALT_ADDRESS_POSTALCODE' => 'Alternatiivne postiindeks:',
+    'LBL_ALT_ADDRESS_COUNTRY' => 'Alternatiivne maakond:',
+    'LBL_PRIMARY_STREET' => 'Aadress',
+    'LBL_ALT_STREET' => 'Teine aadress',
+    'LBL_STREET' => 'Teine aadress',
+    'LBL_CITY' => 'Linn:',
+    'LBL_STATE' => 'Riik',
+    'LBL_POSTAL_CODE' => 'Postiindeks',
+    'LBL_COUNTRY' => 'Maakond',
+    'LBL_CONTACT_INFORMATION' => 'Kontaktinfo',
+    'LBL_ADDRESS_INFORMATION' => 'Aadress(id)',
+    'LBL_ASSIGNED_TO_NAME' => 'Kasutaja',
+    'LBL_OTHER_EMAIL_ADDRESS' => 'Teine e-post:',
+    'LBL_ASSISTANT' => 'Assistent',
+    'LBL_ASSISTANT_PHONE' => 'Assistendi telefon:',
+    'LBL_WORK_PHONE' => 'Telefon tööl:',
+    'LNK_IMPORT_VCARD' => 'Loo vCard´ist',
+    'LBL_ANY_EMAIL' => 'Muu E-post:',
+    'LBL_EMAIL_NON_PRIMARY' => 'Mitte esmased e-postid',
+    'LBL_PHOTO' => 'Foto',
+    'LBL_EDIT_BUTTON' => 'Redigeeri',
+    'LBL_REMOVE' => 'Eemalda',
+
+    //Lawful Basis labels
+    'LBL_LAWFUL_BASIS' => 'Juriidiline alus',
+    'LBL_DATE_REVIEWED' => 'Juriidilise aluse ülevaatamise kuupäev',
+    'LBL_LAWFUL_BASIS_SOURCE' => 'Juriidilise aluse allikas',
+    'LBL_CONSENT' => 'Nõusolek',
+    //End Lawful Basis labels
+);
