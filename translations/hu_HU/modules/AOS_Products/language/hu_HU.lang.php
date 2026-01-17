5702ec4c8c74d7be5f915b9ac3fa9b067ec29a67
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 02:51:44 +0000

    New translations en_us.lang.php (Hungarian)

---
+++ translations/hu_HU/modules/AOS_Products/language/hu_HU.lang.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID' => 'Felelős felhasználó ID',
+    'LBL_ASSIGNED_TO_NAME' => 'Felelős',
+    'LBL_ID' => 'ID',
+    'LBL_DATE_ENTERED' => 'Létrehozás dátuma',
+    'LBL_DATE_MODIFIED' => 'Módosítás dátuma',
+    'LBL_MODIFIED' => 'Módosító',
+    'LBL_MODIFIED_NAME' => 'Módosító név',
+    'LBL_CREATED' => 'Létrehozta',
+    'LBL_DESCRIPTION' => 'Leírás',
+    'LBL_DELETED' => 'Törölve',
+    'LBL_NAME' => 'Termék neve',
+    'LBL_CREATED_USER' => 'Felhasználó által létrehozva',
+    'LBL_MODIFIED_USER' => 'Felhasználó által módosítva',
+    'LBL_LIST_FORM_TITLE' => 'Termékek listája',
+    'LBL_MODULE_NAME' => 'Termékek',
+    'LBL_MODULE_TITLE' => 'Termékek: Főoldal',
+    'LBL_HOMEPAGE_TITLE' => 'Termékeim',
+    'LNK_NEW_RECORD' => 'Új termék létrehozása',
+    'LNK_LIST' => 'Termékek megtekintése',
+    'LBL_SEARCH_FORM_TITLE' => 'Termékek keresése',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Előzmény Megtekintése',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Tevékenységek',
+    'LBL_NEW_FORM_TITLE' => 'Új termék',
+    'LBL_MAINCODE' => 'Termék kód',
+    'VALUE' => 'Termék neve',
+    'LBL_PART_NUMBER' => 'Termékszám',
+    'LBL_CATEGORY' => 'Kategória',
+    'LBL_TYPE' => 'Termék típus',
+    'LBL_COST' => 'Költség',
+    'LBL_PRICE' => 'Ár',
+    'LBL_URL' => 'URL:',
+    'LBL_CONTACT' => 'Kapcsolattartó',
+    'LBL_PRODUCT_IMAGE' => 'Termék kép',
+    'LBL_IMAGE_UPLOAD_FAIL' => 'Hiba: feltöltött fájl túllépte a maximális fájlméret: max méret: ',
+    'LBL_AOS_PRODUCT_CATEGORYS_NAME' => 'Termékkategória',
+    'LBL_AOS_PRODUCT_CATEGORY' => 'Termékkategória azonosító',
+    'LBL_AOS_PRODUCT_CATEGORIES' => 'Termékkategóriák',
+    'LBL_COST_USDOLLAR' => 'Költség (alapértelmezett pénznem)',
+    'LBL_PRICE_USDOLLAR' => 'Ár (alapértelmezett pénznem)',
+    'LBL_FILE_URL' => 'Fájl URL',
+    'LBL_CUSTOMERS_PURCHASED_PRODUCTS_SUBPANEL_TITLE' => 'Vásárlások',
+    'LBL_PRODUCTS_PURCHASES' => 'Vásárlások',
+    'LBL_AOS_QUOTE_NAME' => 'Árajánlat',
+    'LBL_ACCOUNT_NAME' => 'Ügyfél',
+);
