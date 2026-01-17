5c57adecf5e2381a5f0c4eb05c09d33d6fc48098
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 04:20:19 +0000

    New translations en_us.lang.php (Sinhala)

---
+++ translations/si_LK/modules/ProspectLists/language/si_LK.lang.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_MODULE_NAME' => 'ඉලක්ක ලැයිස්තු',
+    'LBL_MODULE_ID' => 'ඉලක්ක ලැයිස්තු',
+    'LBL_MODULE_TITLE' => 'ඉලක්ක ලැයිස්තු: මුල් පිටුව',
+    'LBL_SEARCH_FORM_TITLE' => 'ඉලක්ක සොයන්න ලැයිස්තු',
+    'LBL_LIST_FORM_TITLE' => 'ඉලක්ක ලැයිස්තු',
+    'LBL_PROSPECT_LIST_NAME' => 'ඉලක්ක ලැයිස්තුව:',
+    'LBL_NAME' => 'නම',
+    'LBL_ENTRIES' => 'මුළු අයැදුම්පත්',
+    'LBL_LIST_PROSPECT_LIST_NAME' => 'ඉලක්ක ලැයිස්තුව',
+    'LBL_LIST_ENTRIES' => 'ලැයිස්තුවේ ඉලක්ක',
+    'LBL_LIST_DESCRIPTION' => 'විස්තරය',
+    'LBL_LIST_TYPE_NO' => 'වර්ගය',
+    'LBL_LIST_END_DATE' => 'අවසාන දිනය',
+    'LBL_DATE_ENTERED' => 'සෑදූ දිනය',
+    'LBL_MARKETING_ID' => 'අලෙවි Id',
+    'LBL_DATE_MODIFIED' => 'නවීකරණය කරන ලද දිනය',
+    'LBL_MODIFIED' => 'ෙවනස්කර',
+    'LBL_CREATED' => 'සෑදුවේ',
+    'LBL_ASSIGNED_TO' => 'පවරා',
+    'LBL_DESCRIPTION' => 'විස්තරය',
+    'LNK_NEW_CAMPAIGN' => 'ව්යාපාරය නිර්මාණය කරන්න',
+    'LNK_CAMPAIGN_LIST' => 'ප්රචාරණ ව්යාපාර',
+    'LNK_NEW_PROSPECT_LIST' => 'ඉලක්ක ලැයිස්තුවක් නිර්මාණය',
+    'LNK_PROSPECT_LIST_LIST' => 'දැක්ම ඉලක්ක ලැයිස්තු',
+    'LBL_MODIFIED_BY' => 'ෙවනස්කර',
+    'LBL_CREATED_BY' => 'සෑදුවේ',
+    'LNK_NEW_PROSPECT' => 'ඉලක්ක නිර්මාණය',
+    'LNK_PROSPECT_LIST' => 'ඉලක්ක',
+
+    'LBL_CONTACTS_SUBPANEL_TITLE' => 'සබඳතා',
+    'LBL_LEADS_SUBPANEL_TITLE' => 'ඇද්ද',
+    'LBL_PROSPECTS_SUBPANEL_TITLE' => 'ඉලක්ක',
+    'LBL_ACCOUNTS_SUBPANEL_TITLE' => 'ගිණුම්',
+    'LBL_COPY_PREFIX' => 'පිටපතක්',
+    'LBL_USERS_SUBPANEL_TITLE' => 'පරිශිලකයින්',
+    'LBL_TYPE' => 'වර්ගය',
+    'LBL_LIST_TYPE' => 'වර්ගය',
+    'LBL_LIST_TYPE_LIST_NAME' => 'වර්ගය',
+    'LBL_NEW_FORM_TITLE' => 'නව ඉලක්ක ලැයිස්තුව',
+    'LBL_MARKETING_NAME' => 'අලෙවි නම',
+    'LBL_DOMAIN_NAME' => 'වසම් නම',
+    'LBL_DOMAIN' => 'වසම් නෑ විද්යුත් තැපැල්',
+    'LBL_LIST_PROSPECTLIST_NAME' => 'නම',
+
+    'LBL_EMAIL_MARKETING' => 'වි-තැපැල් අලෙවිකරණය',
+
+    'LBL_ASCENDING' => 'යද්දි',
+    'LBL_DESCENDING' => 'බැස',
+);
