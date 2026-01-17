ebca8a5032e5b2cb852acf9b912424ae66e2f8f7
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 02:31:06 +0000

    New translations en_us.lang.php (Bulgarian)

---
+++ translations/bg_BG/modules/CampaignLog/language/bg_BG.lang.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_LIST_ID' => 'Идентификационен номер на целевата група',
+    'LBL_ID' => 'Идентификационен номер',
+    'LBL_TARGET_TRACKER_KEY' => 'Target Tracker Key',
+    'LBL_TARGET_ID' => 'Целеви клиент',
+    'LBL_TARGET_TYPE' => 'Тип',
+    'LBL_ACTIVITY_TYPE' => 'Действие',
+    'LBL_ACTIVITY_DATE' => 'Дата',
+    'LBL_RELATED_ID' => 'Свързан идентификатор',
+    'LBL_RELATED_TYPE' => 'Свързан тип',
+    'LBL_DELETED' => 'Изтрити',
+    'LBL_MODULE_NAME' => 'Дневник на събития на кампанията',
+    'LBL_LIST_RECIPIENT_EMAIL' => 'Електронен адрес',
+    'LBL_LIST_RECIPIENT_NAME' => 'Получател',
+    'LBL_ARCHIVED' => 'Архивни',
+    'LBL_HITS' => 'Повторения',
+
+    'LBL_CAMPAIGN_NAME' => 'Име:',
+    'LBL_CAMPAIGN' => 'Кампания:',
+    'LBL_NAME' => 'Име:',
+    'LBL_INVITEE' => 'Контакти',
+    'LBL_LIST_CAMPAIGN_NAME' => 'Кампании',
+    'LBL_LIST_STATUS' => 'Статус',
+    'LBL_LIST_TYPE' => 'Тип',
+    'LBL_LIST_END_DATE' => 'Крайна дата',
+    'LBL_DATE_ENTERED' => 'Въведено на',
+    'LBL_DATE_MODIFIED' => 'Модифицирано на',
+    'LBL_MODIFIED' => 'Модифицирано от:',
+    'LBL_CREATED' => 'Създадено от:',
+    'LBL_TEAM' => 'Екип:',
+    'LBL_ASSIGNED_TO' => 'Отговорник:',
+    'LBL_CAMPAIGN_START_DATE' => 'Начална дата:',
+    'LBL_CAMPAIGN_END_DATE' => 'Крайна дата:',
+    'LBL_CAMPAIGN_STATUS' => 'Статус: ',
+    'LBL_CAMPAIGN_BUDGET' => 'Бюджет:',
+    'LBL_CAMPAIGN_EXPECTED_COST' => 'Планирани разходи:',
+    'LBL_CAMPAIGN_ACTUAL_COST' => 'Действителни разходи:',
+    'LBL_CAMPAIGN_EXPECTED_REVENUE' => 'Планирани приходи:',
+    'LBL_CAMPAIGN_TYPE' => 'Вид:',
+    'LBL_CAMPAIGN_OBJECTIVE' => 'Цел:',
+    'LBL_CAMPAIGN_CONTENT' => 'Описание:',
+    'LBL_CREATED_LEAD' => 'създаден потенциален клиент',
+    'LBL_CREATED_CONTACT' => 'създаден контакт',
+    'LBL_CREATED_OPPORTUNITY' => 'Created Opportunity',
+    'LBL_TARGETED_USER' => 'Targeted User',
+    'LBL_SENT_EMAIL' => 'Sent Email',
+    'LBL_LIST_FORM_TITLE' => 'Targeted Campaigns',
+    'LBL_LIST_ACTIVITY_DATE' => 'Дата',
+    'LBL_RELATED' => 'Относно',
+    'LBL_MORE_INFO' => 'Допълнителна информация',
+
+    'LBL_CAMPAIGNS' => 'Кампании',
+    'LBL_LIST_MARKETING_NAME' => 'Marketing Id',
+
+    'LBL_MARKETING_ID' => 'Marketing Id',
+    'LBL_RELATED_NAME' => 'Related Name',
+);
