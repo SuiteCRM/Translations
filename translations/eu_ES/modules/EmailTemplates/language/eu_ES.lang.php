a2f26549d88946e979a1ffe724d0d7362afe93a6
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 04:46:57 +0000

    New translations en_us.lang.php (Basque)

---
+++ translations/eu_ES/modules/EmailTemplates/language/eu_ES.lang.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ADD_DOCUMENT' => 'Add a Document',
+    'LBL_ADD_FILE' => 'Artxiboa erantsi',
+    'LBL_ATTACHMENTS' => 'Gehituak',
+    'LBL_BODY' => 'Gorputza:',
+    'LBL_CLOSE' => 'Itxita',
+    'LBL_DESCRIPTION' => 'Deskribapena',
+    'LBL_EDIT_ALT_TEXT' => 'Testu laua editatu',
+    'LBL_EMAIL_ATTACHMENT' => 'Posta elektronikoaren erantsia',
+    'LBL_HIDE_ALT_TEXT' => 'Hide Plain Text',
+    'LBL_HTML_BODY' => 'HTML gorputza',
+    'LBL_INSERT_VARIABLE' => 'Aldagaia txertatu',
+    'LBL_INSERT_URL_REF' => 'URL-ari erreferentzia txertatu',
+    'LBL_INSERT_TRACKER_URL' => 'Jarraipen URL-a txertatu:',
+    'LBL_INSERT' => 'Txertatu',
+    'LBL_LIST_DATE_MODIFIED' => 'Aldatua',
+    'LBL_LIST_DESCRIPTION' => 'Deskribapena',
+    'LBL_LIST_FORM_TITLE' => 'Posta elektroniko txantiloien zerrenda',
+    'LBL_LIST_NAME' => 'Izena',
+    'LBL_MODULE_NAME' => 'Posta elektronikorako txantiloiak',
+    'LBL_MODULE_TITLE' => 'Posta elektronikoen txantiloiak: hasiera',
+    'LBL_NAME' => 'Izena',
+    'LBL_NEW_FORM_TITLE' => 'Posta elektronikoaren txantiloi berria',
+    'LBL_PUBLISH' => 'Argitaratzea',
+    'LBL_RELATED_TO' => 'Zeri buruzkoa',
+    'LBL_SEARCH_FORM_TITLE' => 'Posta elektroniko txantiloien bilaketa',
+    'LBL_SHOW_ALT_TEXT' => 'Testu laua erakutsi',
+    'LBL_SUBJECT' => 'Gaia',
+    'LBL_SUITE_DOCUMENT' => 'Dokumentuak',
+    'LBL_UPLOAD_FILE' => 'Upload File',
+    'LBL_TEXT_BODY' => 'Testuaren gorputza',
+    'LBL_USERS' => 'Erabiltzaileak',
+
+    'LNK_EMAIL_TEMPLATE_LIST' => 'Posta elektronikoaren txantiloiak',
+    'LNK_IMPORT_NOTES' => 'Oharrak inportatu',
+    'LNK_NEW_EMAIL_TEMPLATE' => 'Posta elektronikoaren txantiloi berria',
+    'LNK_NEW_EMAIL' => 'Posta elektronikoa artxibatu',
+    'LNK_NEW_SEND_EMAIL' => 'Posta elektronikoa idatzi',
+    'LNK_SENT_EMAIL_LIST' => 'Bidalitako posta elektronikoak',
+    'LNK_VIEW_CALENDAR' => 'Gaur',
+    // for Inbox
+    'LBL_NEW' => 'Berria',
+    'LNK_MY_DRAFTS' => 'Zirriborroak',
+    'LNK_MY_INBOX' => 'Sarrera-postontzia',
+    'LBL_TEXT_ONLY' => 'Text Only',
+    'LBL_SEND_AS_TEXT' => 'Send Text Only',
+    'LBL_ACCOUNT' => 'Kontuak',
+    'LBL_FROM_NAME' => 'Izen-igorlea',
+    'LBL_PLAIN_TEXT' => 'Plain Text',
+    'LBL_CREATED_BY' => 'Sortzailea',
+    'LBL_PUBLISHED' => 'Published',
+    'LNK_VIEW_MY_INBOX' => 'View My Email',
+    'LBL_ASSIGNED_TO_ID' => 'Esleitua izan da',
+    'LBL_EDIT_LAYOUT' => 'Edit Layout' /*for 508 compliance fix*/,
+    'LBL_SELECT' => 'Hautatu' /*for 508 compliance fix*/,
+    'LBL_ID_FF_CLEAR' => 'Garbitu' /*for 508 compliance fix*/,
+    'LBL_TYPE' => 'Mota',
+    'LBL_WIDTH' => 'Width Default',
+    'LNK_IMPORT_CAMPAIGNS' => 'Import Campaign',
+);
