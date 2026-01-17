0d520a81d3af53b544897789c216c451658844de
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 03:09:58 +0000

    New translations en_us.lang.php (Mongolian)

---
+++ translations/mn_MN/modules/Surveys/language/mn_MN.lang.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID'                                     => 'Хариуцагчийн ID',
+    'LBL_ADD_OPTION'                                         => 'Add new option',
+    'LBL_ASSIGNED_TO_NAME'                                   => 'Хариуцагч',
+    'LBL_SECURITYGROUPS'                                     => 'Нууцлалын Групп',
+    'LBL_SECURITYGROUPS_SUBPANEL_TITLE'                      => 'Нууцлалын Групп',
+    'LBL_ID'                                                 => 'ID',
+    'LBL_DATE_ENTERED'                                       => 'Үүссэн Огноо',
+    'LBL_DATE_MODIFIED'                                      => 'Өөрчилсөн',
+    'LBL_MODIFIED'                                           => 'Өөрчилсөн',
+    'LBL_MODIFIED_ID'                                        => 'Өөрчилсөн хүн',
+    'LBL_MODIFIED_NAME'                                      => 'Өөрчилсөн',
+    'LBL_CREATED'                                            => 'Үүсгэсэн',
+    'LBL_CREATED_ID'                                         => 'Үүсгэсэн хүн',
+    'LBL_DESCRIPTION'                                        => 'Тайлбар',
+    'LBL_DELETED'                                            => 'Устгасан',
+    'LBL_NAME'                                               => 'Нэр',
+    'LBL_CREATED_USER'                                       => 'Үүсгэсэн',
+    'LBL_MODIFIED_USER'                                      => 'Өөрчилсөн',
+    'LBL_LIST_NAME'                                          => 'Нэр',
+    'LBL_EDIT_BUTTON'                                        => 'Засах',
+    'LBL_REMOVE'                                             => 'Устгах',
+    'LBL_LIST_FORM_TITLE'                                    => 'Судалгааны жагсаалт',
+    'LBL_MODULE_NAME'                                        => 'Судалгаа',
+    'LBL_MODULE_TITLE'                                       => 'Судалгаа',
+    'LBL_HOMEPAGE_TITLE'                                     => 'Миний судалгаа',
+    'LNK_NEW_RECORD'                                         => 'Судалгаа үүсгэх',
+    'LNK_LIST'                                               => 'Судалгаа харах',
+    'LNK_IMPORT_SURVEYS'                                     => 'Судалгаа импортлох',
+    'LBL_SEARCH_FORM_TITLE'                                  => 'Судалгаа хайх',
+    'LBL_HISTORY_SUBPANEL_TITLE'                             => 'Түүхийг харах',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE'                          => 'Үйл ажиллагаа',
+    'LBL_SURVEYS_SUBPANEL_TITLE'                             => 'Судалгаа',
+    'LBL_NEW_FORM_TITLE'                                     => 'Шинэ судалгаа',
+    'LBL_STATUS'                                             => 'Төлөв',
+    'LBL_SURVEY_QUESTIONS_DISPLAY'                           => 'Асуултууд',
+    'LBL_SURVEY_URL_DISPLAY'                                 => 'URL',
+    'LBL_HAPPINESS_QUESTION'                                 => 'Аз жаргалын асуулт',
+    'LBL_CANT_EDIT_RESPONDED'                                => 'Survey questions with responses cannot be edited',
+    'LBL_VIEW_SURVEY_REPORTS'                                => 'Судалгааны тайлан харах',
+    'LBL_CHECKED'                                            => 'Сонгосон',
+    'LBL_UNCHECKED'                                          => 'Сонгоогүй',
+    'LBL_RESPONSE_ANSWER'                                    => 'Хариулт',
+    'LBL_RESPONSE_CONTACT'                                   => 'Харилцагч',
+    'LBL_RESPONSE_TIME'                                      => 'Огноо',
+    'LBL_UNKNOWN_CONTACT'                                    => 'Тодорхойгүй',
+    'LBL_RESPONSE_COUNT'                                     => 'Тоо',
+    'LBL_SUBMIT_TEXT'                                        => 'Submit Text',
+    'LBL_SATISFIED_TEXT'                                     => 'Satisfied Text',
+    'LBL_NEITHER_TEXT'                                       => 'Neither Text',
+    'LBL_DISSATISFIED_TEXT'                                  => 'Dissatisfied Text',
+    'LBL_SURVEYS_SURVEYQUESTIONS_FROM_SURVEYQUESTIONS_TITLE' => 'Судалгааны асуулт',
+    'LBL_SURVEYS_SURVEYRESPONSES_FROM_SURVEYRESPONSES_TITLE' => 'Судалгааны хариулт',
+    'LBL_SHOW_RESPONSES' => 'Show responses',
+    'LBL_HIDE_RESPONSES' => 'Hide responses',
+    'LBL_SURVEY_QUESTION' => 'Question',
+    'LBL_SURVEY_TEXT' => 'Текст',
+    'LBL_SURVEY_TYPE' => 'Төрөл',
+    'LBL_SURVEY_ACTIONS' => 'Үйлдэл',
+    'LBL_SURVEY_NEW_QUESTION' => 'New Question',
+    'LBL_SURVEY_RESPONSES' => 'New Question',
+    'LBL_SURVEYS_SENT' => 'Surveys Sent:',
+    'LBL_SURVEY_DISTINCT' => 'Distinct Surveys Sent:',
+    'LBL_DRAFT' => 'Ноорог',
+    'LBL_SUBMIT' => 'Илгээх',
+    'LBL_STARS' => 'Stars',
+    'LBL_SURVEY_CLOSE_RESPONSE' => 'Thanks for your interest but this survey is now closed.',
+    'LBL_SURVEY_THANKS' => 'Thanks for completing this survey.',
+);
