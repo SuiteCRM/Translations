757e0c0988beada2fc95faa4565c0c08d6de3165
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 04:35:59 +0000

    New translations en_us.lang.php (Yoruba)

---
+++ translations/yo_NG/modules/ProspectLists/language/yo_NG.lang.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_MODULE_NAME' => 'Target Lists',
+    'LBL_MODULE_ID' => 'Target Lists',
+    'LBL_MODULE_TITLE' => 'Target Lists: Home',
+    'LBL_SEARCH_FORM_TITLE' => 'Target Lists Search',
+    'LBL_LIST_FORM_TITLE' => 'Target Lists',
+    'LBL_PROSPECT_LIST_NAME' => 'Target List:',
+    'LBL_NAME' => 'Oruko',
+    'LBL_ENTRIES' => 'Total Entries',
+    'LBL_LIST_PROSPECT_LIST_NAME' => 'Target List',
+    'LBL_LIST_ENTRIES' => 'Targets in List',
+    'LBL_LIST_DESCRIPTION' => 'Apejuwe',
+    'LBL_LIST_TYPE_NO' => 'Iru',
+    'LBL_LIST_END_DATE' => 'Ojo to gbehin',
+    'LBL_DATE_ENTERED' => 'Ojo ti a seda',
+    'LBL_MARKETING_ID' => 'Marketing Id',
+    'LBL_DATE_MODIFIED' => 'Ojo ti a se Atunse',
+    'LBL_MODIFIED' => 'Se iyipada nipasẹ',
+    'LBL_CREATED' => 'Siseda nipa',
+    'LBL_ASSIGNED_TO' => 'Ayan si',
+    'LBL_DESCRIPTION' => 'Apejuwe',
+    'LNK_NEW_CAMPAIGN' => 'Create Campaign',
+    'LNK_CAMPAIGN_LIST' => 'Ipolongo',
+    'LNK_NEW_PROSPECT_LIST' => 'Create Target List',
+    'LNK_PROSPECT_LIST_LIST' => 'View Target Lists',
+    'LBL_MODIFIED_BY' => 'Se iyipada nipasẹ',
+    'LBL_CREATED_BY' => 'Siseda nipa',
+    'LNK_NEW_PROSPECT' => 'Create Target',
+    'LNK_PROSPECT_LIST' => 'Awon ifojusi',
+
+    'LBL_CONTACTS_SUBPANEL_TITLE' => 'Kontati',
+    'LBL_LEADS_SUBPANEL_TITLE' => 'Yorisi',
+    'LBL_PROSPECTS_SUBPANEL_TITLE' => 'Awon ifojusi',
+    'LBL_ACCOUNTS_SUBPANEL_TITLE' => 'Akanti',
+    'LBL_COPY_PREFIX' => 'Copy of',
+    'LBL_USERS_SUBPANEL_TITLE' => 'Onilo',
+    'LBL_TYPE' => 'Iru',
+    'LBL_LIST_TYPE' => 'Iru',
+    'LBL_LIST_TYPE_LIST_NAME' => 'Iru',
+    'LBL_NEW_FORM_TITLE' => 'New Target List',
+    'LBL_MARKETING_NAME' => 'Marketing Name',
+    'LBL_DOMAIN_NAME' => 'Domain Name',
+    'LBL_DOMAIN' => 'No emails to Domain',
+    'LBL_LIST_PROSPECTLIST_NAME' => 'Oruko',
+
+    'LBL_EMAIL_MARKETING' => 'Itaja lori imeeli',
+
+    'LBL_ASCENDING' => 'Lati isale lo s\'oke',
+    'LBL_DESCENDING' => 'Lati oke wa sile',
+);
