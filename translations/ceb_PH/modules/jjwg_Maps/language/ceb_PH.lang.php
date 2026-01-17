bcc6c50f7523b75b7cd46495ce2acdb3d06075b6
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 04:38:06 +0000

    New translations en_us.lang.php (Cebuano)

---
+++ translations/ceb_PH/modules/jjwg_Maps/language/ceb_PH.lang.php
@@ -0,0 +1,191 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings['LBL_MAP'] = 'Mapa';
+$mod_strings['LBL_MODULE_NAME'] = 'Mga Mapa';
+$mod_strings['LBL_MODULE_TITLE'] = 'Mga Mapa: Balay';
+$mod_strings['LBL_MODULE_ID'] = 'Mga Mapa';
+$mod_strings['LBL_LIST_FORM_TITLE'] = 'Naglista sa mga Mapa';
+$mod_strings['LBL_MAP_CUSTOM_MARKER'] = 'Kustom nga Tigmarka';
+$mod_strings['LBL_MAP_CUSTOM_AREA'] = 'Kustom nga Lugar';
+$mod_strings['LBL_HOMEPAGE_TITLE'] = 'Akoy Naglista sa mga Mapa';
+
+$mod_strings['LBL_FLEX_RELATE'] = 'May Kalabutan sa (Sentro):';
+$mod_strings['LBL_MODULE_TYPE'] = 'Klase sa Modyul nga Ipakita:';
+$mod_strings['LBL_DISTANCE'] = 'Gilay-on (Radyos):';
+$mod_strings['LBL_UNIT_TYPE'] = 'Klase sa Yunit:';
+
+$mod_strings['LBL_MAP_DISPLAY'] = 'Ipakita nga Mapa';
+$mod_strings['LBL_MAP_LEGEND'] = 'Sugilanon:';
+$mod_strings['LBL_MAP_USER_GROUPS'] = 'Mga Grupo:';
+$mod_strings['LBL_MAP_GROUP'] = 'Grupo';
+$mod_strings['LBL_MAP_TYPE'] = 'Isulat';
+$mod_strings['LBL_MAP_ASSIGNED_TO'] = 'Gi-assign sa:';
+$mod_strings['LBL_MAP_GET_DIRECTIONS'] = 'Pagkuha ug mga Direksyon';
+$mod_strings['LBL_MAP_GOOGLE_MAPS_VIEW'] = 'Pagtan-aw sa mga Mapa sa Google';
+
+$mod_strings['LNK_NEW_MAP'] = 'Pagdugang ug Bag-o nga Mapa';
+$mod_strings['LNK_NEW_RECORD'] = 'Pagdugang ug Bag-o nga Mapa';
+$mod_strings['LNK_MAP_LIST'] = 'Paglista ug mga Mapa';
+
+$mod_strings['LBL_MAP_ADDRESS_TEST'] = 'Pagsulay sa Geocoding';
+$mod_strings['LBL_MAP_QUICK_RADIUS'] = 'Dali nga Mapa sa Radyos';
+$mod_strings['LBL_MAP_NULL_GROUP_NAME'] = 'Wala';
+$mod_strings['LBL_MAP_ADDRESS'] = 'Adres';
+$mod_strings['LBL_MAP_PROCESS'] = 'Iproseso Kini!';
+
+$mod_strings['LBL_MAP_LAST_STATUS'] = 'Katapusang Istado sa Geocode';
+$mod_strings['LBL_GEOCODED_COUNTS'] = 'Mga inihap nga Modyul nga Geocoded';
+$mod_strings['LBL_CRON_URL'] = 'Cron URL:';
+$mod_strings['LBL_MODULE_HEADING'] = 'Module';
+
+$mod_strings['LBL_N/A'] = 'N/A';
+$mod_strings['LBL_ZERO_RESULTS'] = 'Zero Results';
+$mod_strings['LBL_OK'] = 'Ok';
+$mod_strings['LBL_INVALID_REQUEST'] = 'Invalid Request';
+$mod_strings['LBL_APPROXIMATE'] = 'Approximate';
+$mod_strings['LBL_EMPTY'] = 'Walay sulod';
+
+$mod_strings['LBL_MODULE_TOTAL_HEADING'] = 'Kinatibuk-an';
+$mod_strings['LBL_MODULE_RESET_HEADING'] = 'Usbon';
+$mod_strings['LBL_GEOCODED_COUNTS_DESCRIPTION'] = 'Ang table nga gipakita sa ubos nagpakita sa numero sa mga modyul nga butang nga na-geocode, gipundok sa geocoding nga tubag. Ibutang sa hunahuna nga ang sukdanan sa limitadong paggamit sa mga Mapa sa Google kay 2500 ra ka mga hangyo kada adlaw. Kini nga modyul mo-cache sa mga impormasyon sa puloy-anan nga geocoding sa panahon sa pagproseso aron sa pagmenos sa kinatibuk-ang numero sa mga hangyo nga gikinahanglan.';
+
+$mod_strings['LBL_CRON_INSTRUCTIONS'] = 'Aron sa pagproseso sa mga hangyo sa geocoding girekomendar nga i-setup ug kada-gabii nga Cron-Trabaho. Ang kustom nga pagsulod sa punto nabuhat para niini nga katuyoan ug mahimo nga ma-access nga walay kamatuoran. Ang URL nga gipakita sa ubos gituyong gamiton uban sa usa ka Pagdumala sa mga Tahas sa Iskedyul. Palehug sa pagtan-aw sa dokumentasyon para sa dugang nga impormasyon.';
+$mod_strings['LBL_EXPORT_ADDRESS_URL'] = 'Pag-export ug mga URL';
+$mod_strings['LBL_EXPORT_INSTRUCTIONS'] = 'Gamita ang mga link sa ubos aron sa pag-export ug kumpleto nga mga puloy-anan nga nanginahanlan ug impormasyon sa geocoding. Kung nahuman naka sa imong pag-geocode, i-import ang mga puloy-anan sa Cache sa Puloy-anan nga modyul aron magamit sa imong mga mapa. Pahimangno, ang Cache sa Puloy-anan nga modyul opsyonal. Tanan nga impormasyon sa geocoding gitapigan sa representante sa modyul.';
+$mod_strings['LBL_ADDRESS_CACHE'] = 'Cache sa Puloy-anan';
+$mod_strings['LBL_ADD_TO_TARGET_LIST'] = 'Idugang sa Listahan sa Target';
+$mod_strings['LBL_ADD_TO_TARGET_LIST_PROCESSING'] = 'Nagproseso...';
+
+
+$mod_strings['LBL_CONFIG_TITLE'] = 'Mga Pagpahimutang sa Pagsumpo';
+$mod_strings['LBL_CONFIG_SAVED'] = 'Ang mga Pagpahimutang Naluwas nga Magmalampuson!';
+$mod_strings['LBL_BILLING_ADDRESS'] = 'Address kung asa ipadala ang bill';
+$mod_strings['LBL_SHIPPING_ADDRESS'] = 'Address kung asa ipadala';
+$mod_strings['LBL_PRIMARY_ADDRESS'] = 'Panguna nga Address';
+$mod_strings['LBL_ALTERNATIVE_ADDRESS'] = 'Alternatibo ng Puloy-anan';
+$mod_strings['LBL_ADDRESS_FLEX_RELATE'] = 'Paglambigit';
+$mod_strings['LBL_ADDRESS_ADDRESS'] = 'Puloy-anan (Simple, Mga Tiggamit)';
+$mod_strings['LBL_ADDRESS_CUSTOM'] = 'Kustom (Kustom nga Kontroler sa Lohika)';
+$mod_strings['LBL_ENABLED'] = 'Gipagana';
+$mod_strings['LBL_DISABLED'] = 'Wala paganaha';
+$mod_strings['LBL_DEFAULT'] = 'Sinugdanan:';
+$mod_strings['LBL_CONFIG_DEFAULT'] = 'Sinugdanan:';
+
+$mod_strings['LBL_CONFIG_VALID_GEOCODE_MODULES'] = 'Balido nga mga Modyul sa Geocode:';
+$mod_strings['LBL_CONFIG_VALID_GEOCODE_TABLES'] = 'Balido nga mga Table sa Geocode:';
+$mod_strings['LBL_CONFIG_ADDRESS_TYPE_SETTINGS_TITLE'] = "Mga Pagpahimutang sa Klase sa Puloy-anan: Gihulagway niini ang mga klase sa puloy-anan sa modyul nga gigamit sa panahon sa pag-geocode sa mga poluy-anan. Madawat nga mga Bili: 'billing'; 'shipping'; 'primary'; 'alt'; 'flex_relate'";
+$mod_strings['LBL_CONFIG_ADDRESS_TYPE_FOR'] = 'Klase sa Puloy-anan para ';
+$mod_strings['LBL_CONFIG_ADDRESS_TYPE_FOR_ACCOUNTS'] = 'Klase sa Puloy-anan para sa mga Account:';
+$mod_strings['LBL_CONFIG_ADDRESS_TYPE_FOR_CONTACTS'] = 'Klase sa Puloy-anan para sa mga Kontak:';
+$mod_strings['LBL_CONFIG_ADDRESS_TYPE_FOR_LEADS'] = 'Klase sa Puloy-anan para sa mga Nagpauna:';
+$mod_strings['LBL_CONFIG_ADDRESS_TYPE_FOR_OPPORTUNITIES'] = 'Klase sa Puloy-anan para sa mga Oportunidad:';
+$mod_strings['LBL_CONFIG_OF_RELATED_ACCOUNT'] = '(adunay Kalabutan sa Account)';
+$mod_strings['LBL_CONFIG_ADDRESS_TYPE_FOR_CASES'] = 'Klase sa Puloy-anan para sa mga Kaso:';
+$mod_strings['LBL_CONFIG_ADDRESS_TYPE_FOR_PROJECTS'] = 'Klase sa Puloy-anan para sa mga Proyekto:';
+$mod_strings['LBL_CONFIG_OF_RELATED_ACCOUNT_OPPORTUNITY'] = '(adunay Kalabutan sa Account/Oportunidad)';
+$mod_strings['LBL_CONFIG_ADDRESS_TYPE_FOR_MEETINGS'] = 'Klase sa Puloy-anan para sa mga Panagtagbo:';
+$mod_strings['LBL_CONFIG_ADDRESS_TYPE_FOR_PROSPECTS'] = 'Klase sa Puloy-anan para sa mga Gipaabot/Target:';
+$mod_strings['LBL_CONFIG_RELATED_OBJECT_THRU_FLEX_RELATE'] = 'Butang nga may Kalabutan pinaagi sa Lugar sa Paglambigit';
+
+$mod_strings['LBL_CONFIG_MARKER_GROUP_FIELD_SETTINGS_TITLE'] = "Mga pagpahimutang sa Lugar sa Grupo sa Tigmarka: Gihulagway niini ang 'field' aron gamiton isip parameter sa grupo sa panahon sa pag-display ug mga tigmarka sa mapa. Mga pananglitan: ssigned_user_name, industry, status, sales_stage, priority";
+$mod_strings['LBL_CONFIG_GROUP_FIELD_FOR'] = 'Lugar sa Grupo para sa ';
+$mod_strings['LBL_CONFIG_GROUP_FIELD_FOR_ACCOUNTS'] = 'Lugar sa Grupo para sa mga Account:';
+$mod_strings['LBL_CONFIG_GROUP_FIELD_FOR_CONTACTS'] = 'Lugar sa Grupo para sa mga Kontak:';
+$mod_strings['LBL_CONFIG_GROUP_FIELD_FOR_LEADS'] = 'Lugar sa Grupo para sa mga Nagpauna:';
+$mod_strings['LBL_CONFIG_GROUP_FIELD_FOR_OPPORTUNITIES'] = 'Lugar sa Grupo para sa mga Oportunidad:';
+$mod_strings['LBL_CONFIG_GROUP_FIELD_FOR_CASES'] = 'Lugar sa Grupo para sa mga Kaso:';
+$mod_strings['LBL_CONFIG_GROUP_FIELD_FOR_PROJECTS'] = 'Lugar sa Grupo para sa mga Proyekto:';
+$mod_strings['LBL_CONFIG_GROUP_FIELD_FOR_MEETINGS'] = 'Lugar sa Grupo para sa mga Panagtagbo:';
+$mod_strings['LBL_CONFIG_GROUP_FIELD_FOR_PROSPECTS'] = 'Lugar sa Grupo para sa mga Gipaabot/Target:';
+
+$mod_strings['LBL_CONFIG_GEOCODING_SETTINGS_TITLE'] = 'Mga Pagpahimutang sa Geocoding/Google:';
+$mod_strings['LBL_CONFIG_GEOCODING_API_URL_TITLE'] = 'API URL sa Geocoding:';
+$mod_strings['LBL_CONFIG_GEOCODING_API_URL_DESC'] = 'Ang URL sa mga Mapa sa Google Geocoding API o Proxy';
+$mod_strings['LBL_CONFIG_GEOCODING_API_SECRET_TITLE'] = 'Sekretong Hugpong sa mga Pulong Para sa Proxy:';
+$mod_strings['LBL_CONFIG_GEOCODING_API_SECRET_DESC'] = 'Sekretong Hugpong sa mga Pulong nga gamiton uban sa Proxy MD5 nga pagtandi.';
+$mod_strings['LBL_CONFIG_GEOCODING_LIMIT_TITLE'] = 'Utlanan sa Geocoding:';
+$mod_strings['LBL_CONFIG_GEOCODING_LIMIT_DESC'] = "ang 'geocoding_limit' magtakda sa utlanan sa pangutana sa panahon sa pagpili sa mga rekord para sa geocode.";
+$mod_strings['LBL_CONFIG_GOOGLE_GEOCODING_LIMIT_TITLE'] = 'Utlanan sa Geocoding sa Google:';
+$mod_strings['LBL_CONFIG_GOOGLE_GEOCODING_LIMIT_DESC'] = "ang 'google_geocoding_limit' magtakda sa utlanan sa hangyo sa panahon sa pag-geocode gamit ang mga Mapa nga API sa Google.";
+$mod_strings['LBL_CONFIG_EXPORT_ADDRESSES_LIMIT_TITLE'] = 'Utlanan sa I-export nga mga Puloy-anan:';
+$mod_strings['LBL_CONFIG_EXPORT_ADDRESSES_LIMIT_DESC'] = "ang 'export_addresses_limit' magtakda sa utlanan sa pangutana sa panahon sa pagpili sa mga rekord nga i-export.";
+$mod_strings['LBL_CONFIG_ALLOW_APPROXIMATE_LOCATION_TYPE_TITLE'] = "Pagtugot sa mga Klase sa 'APPROXIMATE' nga Lokasyon:";
+$mod_strings['LBL_CONFIG_ALLOW_APPROXIMATE_LOCATION_TYPE_DESC'] = "'allow_approximate_location_type' - nagtugot sa mga klase sa lokasyon sa 'APPROXIMATE' nga isipon isip 'OK' sa mga resulta sa geocoding.";
+
+$mod_strings['LBL_CONFIG_ADDRESS_CACHE_SETTINGS_TITLE'] = 'Mga Pagpahimutang sa Cache sa Puloy-anan:';
+$mod_strings['LBL_CONFIG_ADDRESS_CACHE_GET_ENABLED_TITLE'] = 'Paganaha ang Cache sa Puloy-anan (Pagkuha):';
+$mod_strings['LBL_CONFIG_ADDRESS_CACHE_GET_ENABLED_DESC'] = "ang 'address_cache_get_enabled' nagtugot sa cache modyul sa puloy-anan aron sa pagkuha ug usab sa data gikan sa table sa cache.";
+$mod_strings['LBL_CONFIG_ADDRESS_CACHE_SAVE_ENABLED_TITLE'] = 'Paganaha ang Cache sa Pagluwas sa Puloy-anan (Luwasa):';
+$mod_strings['LBL_CONFIG_ADDRESS_CACHE_SAVE_ENABLED_DESC'] = "ang 'address_cache_get_enabled' nagtugot sa cache modyul sa puloy-anan aron sa pagluwas sa data gikan sa table sa cache.";
+
+$mod_strings['LBL_CONFIG_LOGIC_HOOKS_SETTINGS_TITLE'] = 'Pagpahimutang sa mga Kaw-it sa Lohika:';
+$mod_strings['LBL_CONFIG_LOGIC_HOOKS_ENABLED_TITLE'] = 'Paganaha ang Tanan nga Kaw-it sa Lohika: ';
+$mod_strings['LBL_CONFIG_LOGIC_HOOKS_ENABLED_DESC'] = "ang 'logic_hooks_enabled' nagtugot sa mga kaw-it sa lohika para sa awtomatiko nga pag-update base sa mga butang nga may kalabutan. Girekomendar kini aron dili mogana sa panahon sa pag-upgrade sa imong SuiteCRM.";
+
+$mod_strings['LBL_CONFIG_MARKER_MAPPING_SETTINGS_TITLE'] = 'Mga Pagpahimutang sa Tigmarka/Mapping:';
+$mod_strings['LBL_CONFIG_MAP_MARKERS_LIMIT_TITLE'] = "Utlanan sa mga Tigmarka sa Mapa:";
+$mod_strings['LBL_CONFIG_MAP_MARKERS_LIMIT_DESC'] = "ang 'map_markers_limit' nagtakda sa utlanan sa pangutana sa panahon sa pagpili sa mga rekord aron makita sa mapa.";
+$mod_strings['LBL_CONFIG_MAP_DEFAULT_CENTER_LATITUDE_TITLE'] = "Giluagon sa Sentro sa Default sa Mapa:";
+$mod_strings['LBL_CONFIG_MAP_DEFAULT_CENTER_LATITUDE_DESC'] = "ang 'map_default_center_latitude' nagtakda sa default nga sentro sa posisyon sa giluagon para sa mga mapa.";
+$mod_strings['LBL_CONFIG_MAP_DEFAULT_CENTER_LONGITUDE_TITLE'] = "Longhitud sa Sentro sa Default sa Mapa:";
+$mod_strings['LBL_CONFIG_MAP_DEFAULT_CENTER_LONGITUDE_DESC'] = "ang 'map_default_center_longitude' nagtakda sa default nga sentro sa posisyon sa longhitud para sa mga mapa.";
+$mod_strings['LBL_CONFIG_MAP_DEFAULT_UNIT_TYPE_TITLE'] = "Default nga Klase sa Yunit sa Mapa:";
+$mod_strings['LBL_CONFIG_MAP_DEFAULT_UNIT_TYPE_DESC'] = "ang 'map_default_unit_type' nagtakda sa default nga klase sa yunit sa pagsukod para sa mga pagkalkyula sa gilay-on. Mga bili: 'mi' (milya) or 'km' (kilometro).";
+$mod_strings['LBL_CONFIG_MAP_DEFAULT_DISTANCE_TITLE'] = "Default nga Gilay-on sa Mapa:";
+$mod_strings['LBL_CONFIG_MAP_DEFAULT_DISTANCE_DESC'] = "ang 'map_default_distance' nagtakda sa default nga gilay-on nga gigamit para sa gilay-on sa mga mapa sa gi-base sa gilay-on.";
+$mod_strings['LBL_CONFIG_MAP_DUPLICATE_MARKER_ADJUSTMENT_TITLE'] = "Kausaban sa Kapareho nga Tigmarka sa Mapa:";
+$mod_strings['LBL_CONFIG_MAP_DUPLICATE_MARKER_ADJUSTMENT_DESC'] = "ang 'map_duplicate_marker_adjustment' nagtakda ug kabag-ohan sa usa ka offset nga idungag sa longhitud ug giloagun sa kaso sa pagkapareho sa posisyon sa marker.";
+$mod_strings['LBL_CONFIG_MAP_CLUSTER_GRID_SIZE_TITLE'] = "Gidak-on sa Clusterer Grid sa mga Tigmarka sa Map:";
+$mod_strings['LBL_CONFIG_MAP_CLUSTER_GRID_SIZE_DESC'] = "ang 'map_clusterer_grid_size' gigamit aron itakda ang gidak-on sa grid alang sa pagkalkyula sa mga clusterer sa mapa.";
+$mod_strings['LBL_CONFIG_MAP_MARKERS_CLUSTERER_MAX_ZOOM_TITLE'] = "Kinadak-ang Pag-zoom sa Clusterer Grid sa mga Tigmarka sa Map:";
+$mod_strings['LBL_CONFIG_MAP_MARKERS_CLUSTERER_MAX_ZOOM_DESC'] = "ang 'map_clusterer_max_zoom' gigamit aron sa pagtakda sa kinadak-ang lebel as pag-zoom kung diin ang dili ma-aplay ang clustering.";
+$mod_strings['LBL_CONFIG_CUSTOM_CONTROLLER_DESC'] = "Importanteng Pahimangno: Tanang naluwas nga mga pagpahimutang makita sa table sa 'config' nga anaa sa ilalum sa kategorya sa 'jjwg'. Pahimangno, ang kustom controller.php nga file dili na kinahanglan nga gamiton aron sa mga pagpahimutang sa pagsalikway.";
+$mod_strings['LBL_JJWG_MAPS_JJWG_AREAS_FROM_JJWG_AREAS_TITLE'] = 'Mga Lugar';
+$mod_strings['LBL_JJWG_MAPS_JJWG_MARKERS_FROM_JJWG_MARKERS_TITLE'] = 'Mga Markahan';
+$mod_strings['LBL_PARENT_ID'] = 'Ginikanan ID';
+$mod_strings['LBL_JJWP_PARTNERS'] = 'Mga Partner sa JJWP';
+$mod_strings['LBL_GET_GOOGLE_API_KEY'] = 'Pagkuha ug Yawi';
+$mod_strings['LBL_GOOGLE_API_KEY'] = 'Yawi sa Api sa Google';
+$mod_strings['LBL_ERROR_NO_GOOGLE_API_KEY'] = 'Palehug sa pagtakda sa Yawi sa Google Api diha sa mga Mapa sa Panel sa Pagdumala sa Google.';
