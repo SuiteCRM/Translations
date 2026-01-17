ea38431bb4d5d1ca3a8d0227684f9295c4e397d5
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 04:11:52 +0000

    New translations en_us.lang.php (Tagalog)

---
+++ translations/tl_PH/modules/Tasks/language/tl_PH.lang.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_MODULE_NAME' => 'Ang mga gawain',
+    'LBL_MODULE_TITLE' => ' Ang mga gawain: tirahan',
+    'LBL_SEARCH_FORM_TITLE' => ' Naghahanap ng gawain',
+    'LBL_LIST_FORM_TITLE' => ' Ang listahan ng gawain',
+    'LBL_NEW_FORM_TITLE' => ' Ang lumikha ng gawain',
+    'LBL_LIST_CLOSE' => 'Sarado',
+    'LBL_LIST_SUBJECT' => 'Paksa',
+    'LBL_LIST_CONTACT' => 'Kontak',
+    'LBL_LIST_PRIORITY' => 'Prayoridad',
+    'LBL_LIST_RELATED_TO' => 'Para sa kaugnayan',
+    'LBL_LIST_DUE_DATE' => 'Takdang petsa',
+    'LBL_LIST_DUE_TIME' => 'Ang nakatadang oras',
+    'LBL_SUBJECT' => 'Ang Paksa:',
+    'LBL_STATUS' => 'Ang Kalagayan:',
+    'LBL_DUE_DATE' => 'Ang nakatakdang petsa:',
+    'LBL_DUE_TIME' => 'Ang Nakatakdang Oras:',
+    'LBL_PRIORITY' => 'Prayoridad:',
+    'LBL_DUE_DATE_AND_TIME' => 'Ang nakatakdang petsa at oras:',
+    'LBL_START_DATE_AND_TIME' => 'Magsimula sa petsa at oras:',
+    'LBL_START_DATE' => 'Magsimula sa petsa:',
+    'LBL_LIST_START_DATE' => 'Magsimula sa petsa',
+    'LBL_START_TIME' => 'Oras ng simula:',
+    'DATE_FORMAT' => '(yyyy-mm-dd)',
+    'LBL_NONE' => 'Wala',
+    'LBL_CONTACT' => 'Kontak:',
+    'LBL_EMAIL_ADDRESS' => 'Ang Email Address:',
+    'LBL_PHONE' => 'Telepono:',
+    'LBL_EMAIL' => 'Ang Email Address:',
+    'LBL_DESCRIPTION' => 'Ang Paglalarawan:',
+    'LBL_NAME' => 'Ang Pangalan:',
+    'LBL_CONTACT_NAME' => 'Ang Pangalan ng kontak ',
+    'LBL_LIST_STATUS' => 'Kalagayan',
+    'LBL_DATE_DUE_FLAG' => 'Walang nakatakdang petsa',
+    'LBL_DATE_START_FLAG' => 'Hindi nagsimula sa petsa',
+    'LBL_LIST_MY_TASKS' => 'Ang aking mga gawain bukas',
+    'LNK_NEW_TASK' => 'Gumawa ng Gawain',
+    'LNK_TASK_LIST' => 'Tingnan ang mga gawain',
+    'LNK_IMPORT_TASKS' => 'Ipasok ang mga gawain',
+    'LBL_LIST_ASSIGNED_TO_NAME' => 'Magtalaga ng gumagamit',
+    'LBL_ASSIGNED_TO_NAME' => 'Ang Itinalaga kay:',
+    'LBL_LIST_DATE_MODIFIED' => 'Ang petsa ay binago',
+    'LBL_CONTACT_ID' => 'Ang kontak ID:',
+    'LBL_PARENT_ID' => 'Ang parent ID:',
+    'LBL_CONTACT_PHONE' => 'Ang kontak ng phone:',
+    'LBL_PARENT_TYPE' => 'Ang uring parent:',
+    'LBL_TASK_INFORMATION' => 'Ang gawaing Pangkalahatang-ideya', //No need to be translated in all caps. Translation used just in menu action items when using the SuiteP template
+    'LBL_EDITLAYOUT' => 'I-edit ang layout' /*for 508 compliance fix*/,
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Ang mga tala',
+    //For export labels
+    'LBL_DATE_DUE' => 'Ang petsa ng tinakda',
+    'LBL_RELATED_TO' => 'Kaugnay sa:',
+);
