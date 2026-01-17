e0fc43eb6f2e8c4f76e9371b56f9a6bbf69a7a10
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 03:42:36 +0000

    New translations en_us.lang.php (Indonesian)

---
+++ translations/id_ID/modules/DynamicFields/language/id_ID.lang.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LNK_NEW_CALL' => 'Jadwal Panggilan',
+    'LNK_NEW_MEETING' => 'Jadwal Rapat',
+    'LNK_NEW_TASK' => 'Buat Tugas',
+    'LNK_NEW_NOTE' => 'Membuat Catatan atau Lampiran',
+    'LNK_NEW_EMAIL' => 'Arsip Email',
+    'LNK_CALL_LIST' => 'Panggilan',
+    'LNK_MEETING_LIST' => 'Rapat',
+    'LNK_TASK_LIST' => 'Tugas',
+    'LNK_NOTE_LIST' => 'Catatan',
+    'LBL_ADD_FIELD' => 'Tambahkan Field:',
+    'LBL_SEARCH_FORM_TITLE' => 'Pencarian Modul',
+    'COLUMN_TITLE_NAME' => 'Nama field',
+    'COLUMN_TITLE_DISPLAY_LABEL' => 'Label Display',
+    'COLUMN_TITLE_LABEL_VALUE' => 'Nilai label',
+    'COLUMN_TITLE_LABEL' => 'Label Sistem',
+    'COLUMN_TITLE_DATA_TYPE' => 'Tipe data',
+    'COLUMN_TITLE_MAX_SIZE' => 'Ukuran Maks',
+    'COLUMN_TITLE_HELP_TEXT' => 'Teks bantuan',
+    'COLUMN_TITLE_COMMENT_TEXT' => 'Teks komentar',
+    'COLUMN_TITLE_REQUIRED_OPTION' => 'Kolom yang harus diisi',
+    'COLUMN_TITLE_DEFAULT_VALUE' => 'Nilai Default',
+    'COLUMN_TITLE_FRAME_HEIGHT' => 'Tinggi IFrame',
+    'COLUMN_TITLE_HTML_CONTENT' => 'HTML',
+    'COLUMN_TITLE_URL' => 'URL default',
+    'COLUMN_TITLE_AUDIT' => 'Audit',
+    'COLUMN_TITLE_MIN_VALUE' => 'Nilai Min',
+    'COLUMN_TITLE_MAX_VALUE' => 'Nilai maks',
+    'COLUMN_TITLE_LABEL_ROWS' => 'Baris',
+    'COLUMN_TITLE_LABEL_COLS' => 'Kolom',
+    'COLUMN_TITLE_DISPLAYED_ITEM_COUNT' => '# Item ditampilkan',
+    'COLUMN_TITLE_AUTOINC_NEXT' => 'Nilai Selisih Otomatis Berikutnya',
+    'COLUMN_DISABLE_NUMBER_FORMAT' => 'Nonaktifkan Format',
+    'COLUMN_TITLE_ENABLE_RANGE_SEARCH' => 'Aktifkan Pencarian Rentang',
+    'LBL_DROP_DOWN_LIST' => 'Daftar drop down',
+    'LBL_RADIO_FIELDS' => 'Bidang Radio',
+    'LBL_MULTI_SELECT_LIST' => 'Multi Pilih Daftar',
+    'COLUMN_TITLE_PRECISION' => 'Presisi',
+    'LBL_MODULE' => 'Modul',
+    'COLUMN_TITLE_MASS_UPDATE' => 'Update Masal',
+    'COLUMN_TITLE_IMPORTABLE' => 'Yg diimpor',
+    'COLUMN_TITLE_DUPLICATE_MERGE' => 'Gabung Duplikat',
+    'LBL_LABEL' => 'Label',
+    'LBL_DATA_TYPE' => 'Tipe data',
+    'LBL_DEFAULT_VALUE' => 'Nilai Default',
+    'ERR_RESERVED_FIELD_NAME' => "Kata kunci yang dilindungi",
+    'ERR_SELECT_FIELD_TYPE' => 'Silahkan Pilih Field Type',
+    'ERR_FIELD_NAME_ALREADY_EXISTS' => 'Nama Field sudah ada',
+    'LBL_BTN_ADD' => 'Tambah',
+    'LBL_BTN_EDIT' => 'Sunting',
+    'LBL_GENERATE_URL' => 'Buat URL',
+    'LBL_CALCULATED' => 'Nilai yang Dihitung',
+    'LBL_LINK_TARGET' => 'Buka Tautan di',
+    'LBL_IMAGE_WIDTH' => 'Lebar',
+    'LBL_IMAGE_HEIGHT' => 'Tinggi',
+    'LBL_IMAGE_BORDER' => 'Berbatasan',
+    'LBL_HELP' => 'Bantuan' /*for 508 compliance fix*/,
+    'COLUMN_TITLE_INLINE_EDIT_TEXT' => 'Inline Edit',
+    'COLUMN_TITLE_PARENT_ENUM' => 'Orang tua DropDown',
+    'LBL_INSERT_FIELD' => 'Insert Field',
+);
