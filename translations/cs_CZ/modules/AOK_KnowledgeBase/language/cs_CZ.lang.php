a31dd5f4f550e3ea2ec06a0406cdf26899da26ab
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 02:34:42 +0000

    New translations en_us.lang.php (Czech)

---
+++ translations/cs_CZ/modules/AOK_KnowledgeBase/language/cs_CZ.lang.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID' => 'ID přiřazeného uživatele',
+    'LBL_ASSIGNED_TO_NAME' => 'Přiřazeno k',
+    'LBL_ID' => 'ID',
+    'LBL_DATE_ENTERED' => 'Datum vytvoření',
+    'LBL_DATE_MODIFIED' => 'Datum změny',
+    'LBL_MODIFIED' => 'Změnil',
+    'LBL_MODIFIED_NAME' => 'Změněno dle Jména',
+    'LBL_CREATED' => 'Vytvořeno',
+    'LBL_DESCRIPTION' => 'Tělo',
+    'LBL_ADDITIONAL_INFO' => 'Rozlišení',
+    'LBL_DELETED' => 'Vymazáno',
+    'LBL_NAME' => 'Nadpis',
+    'LBL_CREATED_USER' => 'Vytvořeno dle Uživatele',
+    'LBL_MODIFIED_USER' => 'Změněno dle Uživatele',
+    'LBL_LIST_NAME' => 'Název',
+    'LBL_EDIT_BUTTON' => 'Upravit',
+    'LBL_REMOVE' => 'Odebrat',
+    'LBL_LIST_FORM_TITLE' => 'Seznam článků znalostní databáze',
+    'LBL_MODULE_NAME' => 'Základní znalost',
+    'LBL_MODULE_TITLE' => 'Základní znalost',
+    'LBL_HOMEPAGE_TITLE' => 'Moje znalostní databáze',
+    'LNK_NEW_RECORD' => 'Vytvoření znalostní databáze',
+    'LNK_LIST' => 'Zobrazit znalostní databázi',
+    'LBL_SEARCH_FORM_TITLE' => 'Prohledat znalostní databázi',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Historie',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Aktivity',
+    'LBL_NEW_FORM_TITLE' => 'Nová znalostní databáze',
+    'LBL_STATUS' => 'Stav',
+    'LBL_REVISION' => 'Revize',
+    'LBL_AUTHOR_USER_ID' => 'Autor (související uživatelské ID)',
+    'LBL_AUTHOR' => 'Autor',
+    'LBL_APPROVER_USER_ID' => 'Schvalovatel (související ID uživatele)',
+    'LBL_APPROVER' => 'Schvalovatel',
+);
+
+$mod_strings['LBL_AOK_KB_CATEGORIES_TITLE'] = 'Kategorie';
