a3678aab8594872b68b21fb0f326ac989b353d65
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 04:10:26 +0000

    New translations en_us.lang.php (Tagalog)

---
+++ translations/tl_PH/modules/Bugs/language/tl_PH.lang.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_MODULE_NAME' => 'Ang mga mali',
+    'LBL_MODULE_TITLE' => 'Tracker ng bug: Bahay',
+    'LBL_MODULE_ID' => 'Ang mga mali',
+    'LBL_SEARCH_FORM_TITLE' => 'Maghanap ng bug',
+    'LBL_LIST_FORM_TITLE' => 'Listahan ng bug',
+    'LBL_NEW_FORM_TITLE' => 'Bagong bug',
+    'LBL_SUBJECT' => 'Ang Paksa:',
+    'LBL_NUMBER' => 'Numero:',
+    'LBL_STATUS' => 'Ang Kalagayan:',
+    'LBL_PRIORITY' => 'Prayoridad:',
+    'LBL_DESCRIPTION' => 'Ang Paglalarawan:',
+    'LBL_CONTACT_NAME' => 'Pangalan ng contact:',
+    'LBL_CONTACT_ROLE' => 'Tungkulin:',
+    'LBL_LIST_NUMBER' => 'Num.',
+    'LBL_LIST_SUBJECT' => 'Paksa',
+    'LBL_LIST_STATUS' => 'Kalagayan',
+    'LBL_LIST_PRIORITY' => 'Prayoridad',
+    'LBL_LIST_RESOLUTION' => 'Ang Resolusyon',
+    'LBL_LIST_LAST_MODIFIED' => 'Ang Huling Nabago',
+    'LBL_INVITEE' => 'Ang mga koneksyon',
+    'LBL_TYPE' => 'Uri:',
+    'LBL_LIST_TYPE' => 'Klase',
+    'LBL_RESOLUTION' => 'Paglutas:',
+    'LBL_RELEASE' => 'Palayain:',
+    'LNK_NEW_BUG' => 'I-ulat ang bug',
+    'LNK_BUG_LIST' => 'Tingnan ang mga bug',
+    'ERR_DELETE_RECORD' => 'Dapat mong tukuyin ang bilang ng talaan nang sa gayon ay mabura ang bug.',
+    'LBL_LIST_MY_BUGS' => 'Ang aking itinalagang mga bug',
+    'LNK_IMPORT_BUGS' => 'Ipasok ang nga bug',
+    'LBL_FOUND_IN_RELEASE' => 'Natagpuan sa paglabas:',
+    'LBL_FIXED_IN_RELEASE' => 'Naayos sa paglabas:',
+    'LBL_LIST_FIXED_IN_RELEASE' => 'Naayos sa paglabas',
+    'LBL_WORK_LOG' => 'Ang Trabaho sa Log:',
+    'LBL_SOURCE' => 'Pinagmulan:',
+    'LBL_PRODUCT_CATEGORY' => 'Ang kategorya:',
+
+    'LBL_CREATED_BY' => 'Nilikha ni:',
+    'LBL_MODIFIED_BY' => 'Ang Huling Nabago ni:',
+
+    'LBL_LIST_EMAIL_ADDRESS' => 'Ang email na padalahan',
+    'LBL_LIST_CONTACT_NAME' => 'Kontak na Pangalan',
+    'LBL_LIST_ACCOUNT_NAME' => 'Ang Pangalan ng akawnt',
+    'LBL_LIST_PHONE' => 'Telepono',
+    'NTC_DELETE_CONFIRMATION' => 'Sigurado ka bang gusto mong alisin ang contact na ito mula sa bug na ito?',
+
+    'LBL_DEFAULT_SUBPANEL_TITLE' => 'Tracker ng bug',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Ang mga aktibidad',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Kasaysayan',
+    'LBL_CONTACTS_SUBPANEL_TITLE' => 'Ang mga koneksyon',
+    'LBL_ACCOUNTS_SUBPANEL_TITLE' => 'Ang mga Akawnt',
+    'LBL_CASES_SUBPANEL_TITLE' => 'Ang mga kaso',
+    'LBL_PROJECTS_SUBPANEL_TITLE' => 'Ang mga proyekto',
+    'LBL_DOCUMENTS_SUBPANEL_TITLE' => 'Ang mga dokumento',
+    'LBL_LIST_ASSIGNED_TO_NAME' => 'Magtalaga ng gumagamit',
+    'LBL_ASSIGNED_TO_NAME' => 'Magtalaga sa',
+
+    'LBL_BUG_INFORMATION' => 'Sa taas ng tanawin', //No need to be translated in all caps. Translation used just in menu action items when using the SuiteP template
+
+);
