28ba177ef23c31cec95a246e035c8400fcf856c7
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 04:38:05 +0000

    New translations en_us.lang.php (Cebuano)

---
+++ translations/ceb_PH/modules/jjwg_Areas/language/ceb_PH.lang.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings['LBL_ASSIGNED_TO_ID'] = 'Mag assign ug user Id';
+$mod_strings['LBL_ASSIGNED_TO_NAME'] = 'Tig gamit';
+$mod_strings['LBL_ID'] = 'ID';
+$mod_strings['LBL_DATE_ENTERED'] = 'Petsa nga gihimo';
+$mod_strings['LBL_DATE_MODIFIED'] = 'Petsa nga gi-usab';
+$mod_strings['LBL_MODIFIED'] = 'Giusab Ni';
+$mod_strings['LBL_MODIFIED_NAME'] = 'Giusab pinaagi sa Pangan';
+$mod_strings['LBL_CREATED'] = 'Gihimo ni';
+$mod_strings['LBL_DESCRIPTION'] = 'Deskripsyon';
+$mod_strings['LBL_DELETED'] = 'Gitangtang';
+$mod_strings['LBL_NAME'] = 'Pangalan';
+$mod_strings['LBL_CREATED_USER'] = 'Gihimo ni User';
+$mod_strings['LBL_MODIFIED_USER'] = 'Giusab ni User';
+$mod_strings['LBL_LIST_NAME'] = 'Pangalan';
+$mod_strings['LBL_LIST_FORM_TITLE'] = 'Listahan sa mga lugar';
+$mod_strings['LBL_MODULE_NAME'] = 'Mga Lugar';
+$mod_strings['LBL_MODULE_TITLE'] = 'Mga Lugar';
+$mod_strings['LBL_HOMEPAGE_TITLE'] = 'Akong mga lugar';
+$mod_strings['LNK_NEW_RECORD'] = 'Himo ug mga lugar';
+$mod_strings['LNK_LIST'] = 'Tan-aw ug mga lugar';
+$mod_strings['LBL_SEARCH_FORM_TITLE'] = 'Mangita ug mga lugar';
+$mod_strings['LBL_HISTORY_SUBPANEL_TITLE'] = 'Tan-awon ang Kasaysayan';
+$mod_strings['LBL_ACTIVITIES_SUBPANEL_TITLE'] = 'Kalihukan';
+$mod_strings['LBL_NEW_FORM_TITLE'] = 'Bag-ong mga lugar';
+$mod_strings['LBL_CITY'] = 'Siyudad';
+$mod_strings['LBL_STATE'] = 'Estado';
+$mod_strings['LBL_COUNTRY'] = 'Nasud';
+$mod_strings['LBL_COORDINATES'] = 'Mga coordinate';
+$mod_strings['LBL_LIST_ASSIGNED_USER'] = 'Tig gamit';
+$mod_strings['LBL_AREA_MAP'] = 'Mapa sa lugar';
+
+$mod_strings['LBL_AREA_EDIT_TITLE'] = 'Mga instruksyon sa paghimo og lugar:';
+$mod_strings['LBL_AREA_EDIT_DESC_1'] = 'I-left click sa mapa, sa clockwise nga paglihok, para makahimo og mga marker point alang sa lugar.';
+$mod_strings['LBL_AREA_EDIT_DESC_2'] = 'Pislit sa una nga marker point para i-close ang lugar sa polygon.';
+$mod_strings['LBL_AREA_EDIT_RESET'] = 'Usbon';
+$mod_strings['LBL_AREA_EDIT_USE_AREA_COORDINATES'] = 'Gamit sa mga coordinate sa lugar';
+$mod_strings['LBL_AREA_EDIT_COORDINATE_RESULTS'] = 'Mga resulta sa coordinate (lng,lat,elv):';
+$mod_strings['LBL_JJWG_MAPS_JJWG_AREAS_FROM_JJWG_MAPS_TITLE'] = 'Mga Mapa';
