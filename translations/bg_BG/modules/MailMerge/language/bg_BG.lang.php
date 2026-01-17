9dc1c815b5940e223450c04394f5204e8406496b
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 02:31:31 +0000

    New translations en_us.lang.php (Bulgarian)

---
+++ translations/bg_BG/modules/MailMerge/language/bg_BG.lang.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_STEP_1' => 'Стъпка 1: Избиране на модул и шаблон',
+    'LBL_MAILMERGE_MODULE' => 'Избиране на модул: ',
+    'LBL_MAILMERGE_SELECTED_MODULE' => 'Избрани модули: ',
+    'LBL_MAILMERGE_TEMPLATES' => 'Избиране на шаблон: ',
+    'LBL_STEP_4' => 'Преглед и финализиране',
+    'LBL_SELECTED_MODULE' => 'Избрани модули: ',
+    'LBL_SELECTED_TEMPLATE' => 'Избрани шаблони: ',
+    'LBL_SELECTED_ITEMS' => 'Избрани записи: ',
+    'LBL_STEP_5' => 'Сливането на писма е завършено',
+    'LBL_MERGED_FILE' => 'Файл за сливане: ',
+    'LNK_NEW_MAILMERGE' => 'Сливане на писма:  начало',
+    'LNK_UPLOAD_TEMPLATE' => 'Зареждане на шаблон',
+    'LBL_DOC_NAME' => 'Име на документа:',
+    'LBL_FILENAME' => 'Име на файла:',
+    'LBL_DOC_VERSION' => 'Версия:',
+    'LBL_DOC_DESCRIPTION' => 'Описание:',
+    'LBL_LIST_NAME' => 'Име',
+    'LBL_FINISH' => 'Стартиране на сливането',
+    'LBL_NEXT' => 'Продължи >',
+    'LBL_BACK' => '< Върни',
+    'LBL_START' => 'Натиснете за да продължите',
+    'LBL_CONTAINS_CONTACT_INFO' => 'Избраните шаблони съдържат контактна информация ',
+    'LBL_ADDIN_NOTICE' => 'This requires the installation of SuiteCRM Mail Merge add-in to Microsoft Word.',
+    'LBL_REMOVE' => 'Remove Item(s)' /*for 508 compliance fix*/,
+    'LBL_ADD' => 'Add Item(s)' /*for 508 compliance fix*/,
+    'LBL_BROWSER_REQUIREMENT' => 'This requires IE 6.0 or greater for Windows.',
+    'LBL_FORM_SELECT_TEMPLATE' => 'Please select a template.',
+    'LBL_SELECT_ONE' => 'Please select at least one item.',
+    'LBL_AVAILABLE' => 'Забранени полета',
+    'LBL_SELECTED' => 'Selected',
+    'LBL_RELATED_INFO_SELECTION' => 'Use the selections below to identify the related information you would like to display in your merged document. Skip this step if your template document contains no related information.',
+
+);
