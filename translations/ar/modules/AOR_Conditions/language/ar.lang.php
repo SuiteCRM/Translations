d2b6c7dc2534b1b58a87c5c51f1e1910a9b634ae
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 02:28:33 +0000

    New translations en_us.lang.php (Arabic)

---
+++ translations/ar/modules/AOR_Conditions/language/ar.lang.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ID' => 'المعرف',
+    'LBL_DATE_ENTERED' => 'تاريخ الإنشاء',
+    'LBL_DATE_MODIFIED' => 'تاريخ التعديل',
+    'LBL_MODIFIED' => 'عُدل بواسطة',
+    'LBL_MODIFIED_NAME' => 'اسم من قام بالتعديل',
+    'LBL_CREATED' => 'أنشئ بواسطة',
+    'LBL_DESCRIPTION' => 'الوصف',
+    'LBL_DELETED' => 'محذوف',
+    'LBL_NAME' => 'الاسم',
+    'LBL_CREATED_USER' => 'أنشئ بواسطة المستخدم',
+    'LBL_MODIFIED_USER' => 'عدل بواسطة المستخدم',
+    'LBL_LIST_NAME' => 'الاسم',
+    'LBL_EDIT_BUTTON' => 'تحرير',
+    'LBL_REMOVE' => 'حذف',
+    'LBL_ASSIGNED_TO_ID' => 'معرف المستخدم المكلف',
+    'LBL_ASSIGNED_TO_NAME' => 'أُسند إلى',
+    'LBL_LIST_FORM_TITLE' => 'قائمة الشروط',
+    'LBL_MODULE_NAME' => 'الشروط',
+    'LBL_MODULE_TITLE' => 'الشروط',
+    'LBL_HOMEPAGE_TITLE' => 'الشروط الخاصة بي',
+    'LNK_NEW_RECORD' => 'إنشاء شروط',
+    'LNK_LIST' => 'عرض الشروط',
+    'LBL_SEARCH_FORM_TITLE' => 'بحث الشروط',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'عرض التاريخ',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'الأنشطة',
+    'LBL_NEW_FORM_TITLE' => 'شروط جديدة',
+    'LBL_LOGIC_OP' => 'المنطق',
+    'LBL_MODULE_PATH' => 'الوحدة',
+    'LBL_FIELD' => 'الحقل',
+    'LBL_OPERATOR' => 'العامل الرياضي',
+    'LBL_VALUE_TYPE' => 'النوع',
+    'LBL_VALUE' => 'القيمة',
+    'LBL_ORDER' => 'ترتيب',
+    'LBL_CONDITION_OPERATOR' => 'عامل الشرط',
+    'LBL_AOR_REPORT_ID' => 'معرف التقرير',
+    'LBL_PARAMETER' => 'المعطى',
+    'LBL_CONDITION_AND' => 'و',
+    'LBL_CONDITION_OR' => 'أو',
+);
