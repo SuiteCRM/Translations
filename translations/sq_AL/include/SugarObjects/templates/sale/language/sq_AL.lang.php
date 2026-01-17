400275b504923bdfd011ce53b84f178bc2ea2822
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 03:22:21 +0000

    New translations en_us.lang.php (Albanian)

---
+++ translations/sq_AL/include/SugarObjects/templates/sale/language/sq_AL.lang.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_MODULE_NAME' => 'Shitje',
+    'LBL_MODULE_TITLE' => 'Shitje: Ballina',
+    'LBL_SEARCH_FORM_TITLE' => 'Kërkimi i shitjeve',
+    'LBL_LIST_FORM_TITLE' => 'Lista e shitjeve',
+    'LBL_NAME' => 'Emri i shitjes',
+    'LBL_LIST_SALE_NAME' => 'Emri',
+    'LBL_LIST_ACCOUNT_NAME' => 'Emri i llogarisë:',
+    'LBL_LIST_AMOUNT' => 'Vlerë',
+    'LBL_LIST_DATE_CLOSED' => 'Mbyll:',
+    'LBL_LIST_SALE_STAGE' => 'Faza e shitjes',
+    'LBL_ACCOUNT_ID' => 'ID e Llogarisë',
+    //DON'T CONVERT THESE THEY ARE MAPPINGS
+    'db_name' => 'LBL_NAME',
+    //END DON'T CONVERT
+    'LBL_ACCOUNT_NAME' => 'Emri i llogarisë:',
+    'LBL_AMOUNT' => 'Sasia:',
+    'LBL_AMOUNT_USDOLLAR' => 'Sasia USD:',
+    'LBL_CURRENCY' => 'monedha:',
+    'LBL_DATE_CLOSED' => 'Data e përfundimit e pritur',
+    'LBL_TYPE' => 'Lloji',
+    'LBL_CAMPAIGN' => 'Fushata',
+    'LBL_LEADS_SUBPANEL_TITLE' => 'udhëheqjet',
+    'LBL_PROJECTS_SUBPANEL_TITLE' => 'Projektet',
+    'LBL_NEXT_STEP' => 'Hapi vijues',
+    'LBL_LEAD_SOURCE' => 'Burimi i udhëheqjes',
+    'LBL_SALES_STAGE' => 'Faza e shitjes',
+    'LBL_PROBABILITY' => 'Probabiliteti (%)',
+    'LBL_DESCRIPTION' => 'Përshkrim',
+    'LBL_DUPLICATE' => 'Shitje e mundshme e dubluar',
+    'MSG_DUPLICATE' => 'Regjistrimi i shitjes që kryeni mund të jetë dublim i shitjes së regjistruar që tashmë ekziston. Shitjet e regjistruara që përmbajnë emra të ngjashëm janë të rënditura posht. Shtyp Ruaj për të vazhduar me krijimin e shitjes së re, ose shtyp Anulim pr tu kthyer në modulë duke mos krijuar shitjen.',
+    'LBL_NEW_FORM_TITLE' => 'Krijo shitje',
+    'ERR_DELETE_RECORD' => 'Duhet përcaktuar numrin e regjistrimit për të fshirë shitjen',
+    'LBL_DEFAULT_SUBPANEL_TITLE' => 'Shitje',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Aktivitetet',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Historia',
+
+    'LBL_CONTACTS_SUBPANEL_TITLE' => 'Kontaktet',
+    'LBL_ASSIGNED_TO_NAME' => 'Përdorues',
+    'LBL_LIST_ASSIGNED_TO_NAME' => 'Përdorues i caktuar',
+    'LBL_ASSIGNED_TO_ID' => 'Id e përdoruesve të caktuar',
+    'LBL_MODIFIED_NAME' => 'Modifikuar nga emri',
+    'LBL_SALE_INFORMATION' => 'Informacioni i shitjeve',
+    'LBL_CURRENCY_NAME' => 'Emri i Monedhës',
+    'LBL_CURRENCY_SYMBOL' => 'Simboli i monedhës',
+    'LBL_EDIT_BUTTON' => 'Ndrysho',
+    'LBL_REMOVE' => 'Fshijë',
+
+);
