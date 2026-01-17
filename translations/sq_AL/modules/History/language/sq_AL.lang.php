7608cdbea44d3421e648bc27327289ba9b8c0986
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 03:23:50 +0000

    New translations en_us.lang.php (Albanian)

---
+++ translations/sq_AL/modules/History/language/sq_AL.lang.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_MODULE_NAME' => 'Historia',
+    'LBL_MODULE_TITLE' => 'Historia: Ballina',
+    'LBL_SEARCH_FORM_TITLE' => 'Kërkim i historisë',
+    'LBL_LIST_FORM_TITLE' => 'Historia',
+    'LBL_LIST_SUBJECT' => 'Subjekti',
+    'LBL_LIST_CONTACT' => 'Kontaktet',
+    'LBL_LIST_RELATED_TO' => 'Në lidhje me',
+    'LBL_LIST_DATE' => 'minutData',
+    'LBL_LIST_CLOSE' => 'Mbyll:',
+    'LBL_SUBJECT' => 'Subjekti',
+    'LBL_STATUS' => 'statusi',
+    'LBL_LOCATION' => 'Lokacioni',
+    'LBL_DATE_TIME' => 'Data dhe ora e nisjes',
+    'LBL_DATE' => 'Data e nisjes',
+    'LBL_TIME' => 'Ora e nisjes',
+    'LBL_DURATION' => 'Kohëzgjatja',
+    'LBL_HOURS_MINS' => '(orët/minutat)',
+    'LBL_CONTACT_NAME' => 'Emri i kontaktit',
+    'LBL_DESCRIPTION' => 'Përshkrim',
+    'LNK_NEW_CALL' => 'Thirje identifikuese',
+    'LNK_NEW_MEETING' => 'Cakto mbledhje',
+    'LNK_NEW_TASK' => 'Krijo detyrë',
+    'LNK_NEW_NOTE' => 'Krijo shënim ose bashkëngjitje',
+    'LNK_NEW_EMAIL' => 'Emaili i arkivuar',
+    'LNK_CALL_LIST' => 'Thirjet',
+    'LNK_MEETING_LIST' => 'Mbledhjet',
+    'LNK_TASK_LIST' => 'Detyrat',
+    'LNK_NOTE_LIST' => 'Shënimet',
+    'ERR_DELETE_RECORD' => 'Duhet përcaktuar numrin e regjistrimit për të fshirë llogarinë',
+    'LBL_INVITEE' => 'Të ftuarit',
+    'LBL_LIST_DIRECTION' => 'Drejtimi',
+    'LBL_DIRECTION' => 'Drejtimi',
+    'LNK_NEW_APPOINTMENT' => 'Takim i ri',
+    'LNK_VIEW_CALENDAR' => 'Sot',
+    'LBL_OPEN_ACTIVITIES' => 'Hap aktivitetet',
+    'LBL_HISTORY' => 'Historia',
+    'LBL_NEW_TASK_BUTTON_TITLE' => 'Krijo detyrë',
+    'LBL_NEW_TASK_BUTTON_LABEL' => 'Krijo detyrë',
+    'LBL_SCHEDULE_MEETING_BUTTON_TITLE' => 'Cakto mbledhje',
+    'LBL_SCHEDULE_MEETING_BUTTON_LABEL' => 'Cakto mbledhje',
+    'LBL_SCHEDULE_CALL_BUTTON_LABEL' => 'Thirje identifikuese',
+    'LBL_NEW_NOTE_BUTTON_TITLE' => 'Krijo shënim ose bashkëngjitje',
+    'LBL_NEW_NOTE_BUTTON_LABEL' => 'Krijo shënim ose bashkëngjitje',
+    'LBL_TRACK_EMAIL_BUTTON_TITLE' => 'Emaili i arkivuar',
+    'LBL_TRACK_EMAIL_BUTTON_LABEL' => 'Emaili i arkivuar',
+    'LBL_LIST_STATUS' => 'Statusi',
+    'LBL_LIST_DUE_DATE' => 'Due Date',
+    'LBL_LIST_LAST_MODIFIED' => 'Ndryshimi i fundit',
+    'LNK_IMPORT_NOTES' => 'Shënime të rëndësishme',
+    'LBL_ACCEPT_THIS' => 'Prano?',
+    'LBL_DEFAULT_SUBPANEL_TITLE' => 'Historia',
+
+    'LBL_CATEGORY' => 'Kategoria',
+    'LBL_LIST_CATEGORY' => 'Kategoria',
+);
