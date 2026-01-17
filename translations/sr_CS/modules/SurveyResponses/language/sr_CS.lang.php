fc421d7cd26cb39b3517ca61b940e50af6c46c63
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 04:30:00 +0000

    New translations en_us.lang.php (Serbian (Latin))

---
+++ translations/sr_CS/modules/SurveyResponses/language/sr_CS.lang.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID'                                                             => 'ID broj dodeljenog korisnika',
+    'LBL_ASSIGNED_TO_NAME'                                                           => 'Dodeljen',
+    'LBL_SECURITYGROUPS'                                                             => 'Sigurnosne grupe',
+    'LBL_SECURITYGROUPS_SUBPANEL_TITLE'                                              => 'Sigurnosne grupe',
+    'LBL_ID'                                                                         => 'ID broj',
+    'LBL_DATE_ENTERED'                                                               => 'Datum kreiranja',
+    'LBL_DATE_MODIFIED'                                                              => 'Datum izmene',
+    'LBL_MODIFIED'                                                                   => 'Izmenio',
+    'LBL_MODIFIED_ID'                                                                => 'Promenio korisnik sa ID brojem',
+    'LBL_MODIFIED_NAME'                                                              => 'Promenio',
+    'LBL_CREATED'                                                                    => 'Autor',
+    'LBL_CREATED_ID'                                                                 => 'ID broj autora',
+    'LBL_DESCRIPTION'                                                                => 'Opis',
+    'LBL_DELETED'                                                                    => 'Obrisan',
+    'LBL_NAME'                                                                       => 'Ime',
+    'LBL_CREATED_USER'                                                               => 'Autor je korisnik',
+    'LBL_MODIFIED_USER'                                                              => 'Promenio korisnik',
+    'LBL_LIST_NAME'                                                                  => 'Ime',
+    'LBL_EDIT_BUTTON'                                                                => 'Izmeni',
+    'LBL_REMOVE'                                                                     => 'Ukloni',
+    'LBL_LIST_FORM_TITLE'                                                            => 'Lista anketnih odgovora',
+    'LBL_MODULE_NAME'                                                                => 'Anketni odgovori',
+    'LBL_MODULE_TITLE'                                                               => 'Anketni odgovori',
+    'LBL_HOMEPAGE_TITLE'                                                             => 'Moji anketni odgovori',
+    'LNK_NEW_RECORD'                                                                 => 'Kreiraj anketne odgovore',
+    'LNK_LIST'                                                                       => 'Pregledaj anketne odgovore',
+    'LNK_IMPORT_SURVEYRESPONSES'                                                     => 'Uvezi anketne odgovore',
+    'LBL_SEARCH_FORM_TITLE'                                                          => 'Pretraži anketne odgovore',
+    'LBL_HISTORY_SUBPANEL_TITLE'                                                     => 'Pregled istorije',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE'                                                  => 'Aktivnosti',
+    'LBL_SURVEYRESPONSES_SUBPANEL_TITLE'                                             => 'Anketni odgovori',
+    'LBL_NEW_FORM_TITLE'                                                             => 'Dodaj anketne odgovore',
+    'LBL_HAPPINESS'                                                                  => 'Happiness',
+    'LBL_EMAIL_RESPONSE_SENT'                                                        => 'Email Response Sent',
+    'LBL_SURVEYRESPONSES_SURVEYQUESTIONRESPONSES_FROM_SURVEYQUESTIONRESPONSES_TITLE' => 'Anketna pitanja i odgovori',
+    'LBL_SURVEYRESPONSES_CONTACTS_FROM_CONTACTS_TITLE'                               => 'Kontakt',
+    'LBL_SURVEYRESPONSES_ACCOUNTS_FROM_ACCOUNTS_TITLE'                               => 'Kompanija',
+    'LBL_SURVEYS_SURVEYRESPONSES_FROM_SURVEYS_TITLE'                                 => 'Anketa',
+    'LBL_QUESTION_RESPONSES_DISPLAY'                                                 => 'Anketna pitanja i odgovori',
+    'LBL_SURVEYRESPONSES_CAMPAIGNS_FROM_CAMPAIGNS_TITLE'                             => 'Kampanja',
+    'LBL_QUESTION' => 'Question',
+    'LBL_RESPONSE' => 'Response'
+
+);
