a7d831b08ed710da928391c99d43f56787ac7180
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 02:34:49 +0000

    New translations en_us.lang.php (Czech)

---
+++ translations/cs_CZ/modules/AOR_Reports/language/cs_CZ.lang.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID' => 'ID přiřazeného uživatele',
+    'LBL_ASSIGNED_TO_NAME' => 'Přiřazeno k',
+    'LBL_ID' => 'ID',
+    'LBL_DATE_ENTERED' => 'Datum vytvoření',
+    'LBL_DATE_MODIFIED' => 'Datum změny',
+    'LBL_MODIFIED' => 'Změnil',
+    'LBL_MODIFIED_NAME' => 'Změněno dle Jména',
+    'LBL_CREATED' => 'Vytvořeno',
+    'LBL_DESCRIPTION' => 'Popis',
+    'LBL_DELETED' => 'Vymazáno',
+    'LBL_NAME' => 'Název',
+    'LBL_CREATED_USER' => 'Vytvořeno dle Uživatele',
+    'LBL_MODIFIED_USER' => 'Změněno dle Uživatele',
+    'LBL_LIST_NAME' => 'Název',
+    'LBL_EDIT_BUTTON' => 'Upravit',
+    'LBL_REMOVE' => 'Odebrat',
+    'LBL_LIST_FORM_TITLE' => 'Seznam reportů',
+    'LBL_MODULE_NAME' => 'Reporty',
+    'LBL_MODULE_TITLE' => 'Reporty',
+    'LBL_HOMEPAGE_TITLE' => 'Moje reporty',
+    'LNK_NEW_RECORD' => 'Vytvořit report',
+    'LNK_LIST' => 'Zobrazit report',
+    'LBL_SEARCH_FORM_TITLE' => 'Vyhledat report',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Historie',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Aktivity',
+    'LBL_NEW_FORM_TITLE' => 'Nový report',
+    'LBL_REPORT_MODULE' => 'Modul reportů',
+    'LBL_GRAPHS_PER_ROW' => 'Grafy na řádek',
+    'LBL_FIELD_LINES' => 'Zobrazit pole',
+    'LBL_ADD_FIELD' => 'Přidat pole',
+    'LBL_CONDITION_LINES' => 'Podmínky',
+    'LBL_ADD_CONDITION' => 'Přidat podmínku',
+    'LBL_EXPORT' => 'Exportovat',
+    'LBL_DOWNLOAD_PDF' => 'Stáhnout ve formátu PDF',
+    'LBL_ADD_TO_PROSPECT_LIST' => 'Přidat do seznamu cílů',
+    'LBL_AOR_MODULETREE_SUBPANEL_TITLE' => 'Strom modulů',
+    'LBL_AOR_FIELDS_SUBPANEL_TITLE' => 'Pole',
+    'LBL_AOR_CONDITIONS_SUBPANEL_TITLE' => 'Podmínky',
+    'LBL_TOTAL' => 'Celkem',
+    'LBL_AOR_CHARTS_SUBPANEL_TITLE' => 'Grafy',
+    'LBL_ADD_CHART' => 'Přidat graf',
+    'LBL_ADD_PARENTHESIS' => 'Vložit závorky',
+    'LBL_CHART_TITLE' => 'Nadpis',
+    'LBL_CHART_TYPE' => 'Typ',
+    'LBL_CHART_X_FIELD' => 'Osa x',
+    'LBL_CHART_Y_FIELD' => 'Osa Y',
+    'LBL_AOR_REPORTS_DASHLET' => 'Reporty',
+    'LBL_DASHLET_TITLE' => 'Nadpis',
+    'LBL_DASHLET_REPORT' => 'Report',
+    'LBL_DASHLET_CHOOSE_REPORT' => 'Vyberte, prosím, report',
+    'LBL_DASHLET_SAVE' => 'Uložit',
+    'LBL_DASHLET_CHARTS' => 'Grafy',
+    'LBL_DASHLET_ONLY_CHARTS' => 'Zobrazit pouze grafy',
+    'LBL_UPDATE_PARAMETERS' => 'Aktualizovat',
+    'LBL_PARAMETERS' => 'Parametry',
+    'LBL_TOOLTIP_DRAG_DROP_ELEMS' => 'Přetáhnout prvky do oblasti pole nebo oblasti podmínek',
+    'LBL_MAIN_GROUPS' => 'Hlavní skupina:',
+    'LBL_CHAR_UNNAMED_DEFAULT_TITLE' => 'Nepojmenovaný graf',
+    'LBL_REPORT' => 'Report',
+);
