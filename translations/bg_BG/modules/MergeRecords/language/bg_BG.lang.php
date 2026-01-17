898310e5e644f7c4f67bd218bf352d1d2a174e36
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 02:31:33 +0000

    New translations en_us.lang.php (Bulgarian)

---
+++ translations/bg_BG/modules/MergeRecords/language/bg_BG.lang.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_MODULE_NAME' => 'Сливане на записи',
+    'LBL_MODULE_TITLE' => 'Сливане на записи:',
+    'LBL_SEARCH_FORM_TITLE' => 'Търсене на записи за сливане',
+    'LBL_LIST_FORM_TITLE' => 'Списък със записи за сливане',
+
+    'LBL_LBL_MERGE_RECORDS_STEP_1' => 'Стъпка 1: Търсене на записи за сливане с',
+    'LBL_AVAIL_FIELDS' => 'Налични полета',
+    'LBL_FILTER_COND' => 'Условия за филтриране',
+    'LBL_MERGE_VALUE_OVER' => 'Записване със стойност',
+
+    'LBL_NEXT_STEP_TITLE' => 'Следваща стъпка',
+    'LBL_NEXT_STEP_BUTTON_LABEL' => 'Следваща стъпка >',
+
+    'LBL_PERFORM_MERGE_BUTTON_TITLE' => 'Изпълни',
+    'LBL_PERFORM_MERGE_BUTTON_KEY' => 'P',
+    'LBL_PERFORM_MERGE_BUTTON_LABEL' => 'Изпълни',
+
+    'LBL_SAVE_MERGED_RECORD_BUTTON_TITLE' => 'Съхрани',
+    'LBL_SAVE_MERGED_RECORD_BUTTON_LABEL' => 'Съхрани',
+
+    'LBL_STEP2_FORM_TITLE' => 'Намерени записи за сливане с:',
+    'LBL_SELECT_ERROR' => 'Необходимо е да изберете записи, преди да продължите.',
+    'LBL_CHANGE_PARENT' => 'Маркирай като основен',
+    'LBL_REMOVE_FROM_MERGE' => 'Изтрий',
+    'LBL_DIFF_COL_VALUES' => 'Колони със стойности различни в основния и допълнителните записи:',
+    'LBL_SAME_COL_VALUES' => 'Колони със стойности еднакви в маркираните записи:',
+    'LBL_DELETE_MESSAGE' => 'Ще бъдат изтрити следните записи:',
+    'LBL_PROCEED' => 'Продължи ?',
+    'LBL_STEP1_DIRECTIONS' => 'Търсене на възможни дублирани записи. Ако такива са намерени, можете да изберете кои от тях да бъдат обединени с текущикя запис.',
+);
