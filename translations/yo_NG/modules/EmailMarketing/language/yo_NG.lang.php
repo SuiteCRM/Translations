ff1d9fcce66bfea6427b785f6cb5822ffd1d4177
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 04:35:13 +0000

    New translations en_us.lang.php (Yoruba)

---
+++ translations/yo_NG/modules/EmailMarketing/language/yo_NG.lang.php
@@ -0,0 +1,108 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_REPLY_ADDR' => 'Fesi-si Adiresi: ',
+    'LBL_REPLY_NAME' => 'Fesi-si Oruko: ',
+
+    'LBL_MODULE_NAME' => 'Itaja lori imeeli',
+    'LBL_MODULE_TITLE' => 'Email Marketing: Home',
+    'LBL_LIST_FORM_TITLE' => 'Email Marketing Campaigns',
+    'LBL_NAME' => 'Oruko: ',
+    'LBL_LIST_NAME' => 'Oruko',
+    'LBL_LIST_FROM_ADDR' => 'From Email',
+    'LBL_LIST_DATE_START' => 'Ojo ibeere',
+    'LBL_LIST_TEMPLATE_NAME' => 'Imeeli - Awon awose',
+    'LBL_LIST_STATUS' => 'Ipo',
+    'LBL_STATUS' => 'Ipo',
+    'LBL_STATUS_TEXT' => 'Ipo:',
+    'LBL_TEMPLATE_NAME' => 'Template Name',
+    'LBL_DATE_ENTERED' => 'Date Entered',
+    'LBL_DATE_MODIFIED' => 'Ojo ti a se Atunse',
+    'LBL_MODIFIED' => 'Se iyipada nipasẹ: ',
+    'LBL_CREATED' => 'Siseda nipa: ',
+    'LBL_MESSAGE_FOR' => 'Firanse Ifiranse yii si:',
+
+    'LBL_FROM_NAME' => 'Lati Oruko: ',
+    'LBL_FROM_ADDR' => 'From Email Address: ',
+    'LBL_DATE_START' => 'Ojo ibeere',
+    'LBL_TIME_START' => 'Asiko ibeere ',
+    'LBL_START_DATE_TIME' => 'Start Date & Time: ',
+    'LBL_TEMPLATE' => 'Email Template: ',
+
+    'LBL_MODIFIED_BY' => 'Se iyipada nipasẹ: ',
+    'LBL_CREATED_BY' => 'Siseda nipa: ',
+
+    'LNK_NEW_CAMPAIGN' => 'Create Campaign',
+    'LNK_CAMPAIGN_LIST' => 'Ipolongo',
+    'LNK_NEW_PROSPECT_LIST' => 'Create Target List',
+    'LNK_PROSPECT_LIST_LIST' => 'Target Lists',
+    'LNK_NEW_PROSPECT' => 'Create Target',
+    'LNK_PROSPECT_LIST' => 'Awon ifojusi',
+    'LBL_DEFAULT_SUBPANEL_TITLE' => 'Itaja lori imeeli',
+    'LBL_CREATE_EMAIL_TEMPLATE' => 'Seda',
+    'LBL_EDIT_EMAIL_TEMPLATE' => 'Satunko',
+    'LBL_FROM_MAILBOX' => 'From Mailbox',
+    'LBL_FROM_MAILBOX_NAME' => 'Use Mailbox:',
+    'LBL_OUTBOUND_EMAIL_ACCOUNT_NAME' => 'Outbound Email Account:',
+    'LBL_PROSPECT_LIST_SUBPANEL_TITLE' => 'Target Lists',
+    'LBL_ALL_PROSPECT_LISTS' => 'Select to choose all Target List(s) in the Campaign.',
+    'LBL_RELATED_PROSPECT_LISTS' => 'All Target List(s) related to this message.',
+    'LBL_PROSPECT_LIST_NAME' => 'Target List Name',
+
+    'LBL_LIST_PROSPECT_LIST_NAME' => 'Targeted Lists',
+    'LBL_MODULE_SEND_TEST' => 'Campaign: Send Test',
+    'LBL_MODULE_SEND_EMAILS' => 'Campaign: Send Emails',
+    'LBL_SCHEDULE_MESSAGE_TEST' => 'Please select the campaign messages that you would like to test:',
+    'LBL_SCHEDULE_MESSAGE_EMAILS' => 'Please select the campaign messages that you would like to schedule for distribution on the specified start date and time:',
+    'LBL_SCHEDULE_BUTTON_TITLE' => 'Firanṣẹ',
+    'LBL_SCHEDULE_BUTTON_LABEL' => 'Firanṣẹ',
+    'LBL_ERROR_ON_MARKETING' => 'Missing required field(s)',
+
+    'LBL_CAMPAIGN_ID' => 'Campaign ID',
+    'LBL_OUTBOUND_EMAIL_ACOUNT_ID' => 'Outbound Email Account ID',
+    'LBL_EMAIL_TEMPLATE' => 'Imeeli - Awon awose',
+    'LBL_PROSPECT_LISTS' => 'Prospect Lists',
+
+);
