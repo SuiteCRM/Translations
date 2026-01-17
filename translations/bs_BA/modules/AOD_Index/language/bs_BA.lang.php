160164ca0ffa625898d141d7ba8a8307251446ce
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 04:59:21 +0000

    New translations en_us.lang.php (Bosnian)

---
+++ translations/bs_BA/modules/AOD_Index/language/bs_BA.lang.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID' => 'Id dodijeljenog korisnika',
+    'LBL_ASSIGNED_TO_NAME' => 'Dodijeljeno korisniku',
+    'LBL_ID' => 'ID',
+    'LBL_DATE_ENTERED' => 'Datum kreiranja',
+    'LBL_DATE_MODIFIED' => 'Datum izmjene',
+    'LBL_MODIFIED' => 'Modifikovao-la',
+    'LBL_MODIFIED_NAME' => 'Izmjenjeno od strane korisnika',
+    'LBL_CREATED' => 'Kreirao',
+    'LBL_DESCRIPTION' => 'Opis',
+    'LBL_DELETED' => 'Izbrisano',
+    'LBL_NAME' => 'Ime',
+    'LBL_CREATED_USER' => 'Kreirao Korisnik',
+    'LBL_MODIFIED_USER' => 'Izmijenio Korisnik',
+    'LBL_LIST_NAME' => 'Ime',
+    'LBL_EDIT_BUTTON' => 'Uredi',
+    'LBL_REMOVE' => 'Ukloni',
+    'LBL_LIST_FORM_TITLE' => 'Lista indeksa',
+    'LBL_MODULE_NAME' => 'Indeks',
+    'LBL_MODULE_TITLE' => 'Indeks',
+    'LBL_HOMEPAGE_TITLE' => 'Moj indeks',
+    'LNK_NEW_RECORD' => 'Kreiraj indeks',
+    'LNK_LIST' => 'Pregledaj indeks',
+    'LBL_SEARCH_FORM_TITLE' => 'Pretraživanje indeksa',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Pregled istorije',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Aktivnosti',
+    'LBL_NEW_FORM_TITLE' => 'Novi indeks',
+    'LBL_LAST_OPTIMISED' => 'Zadnje optimizirano',
+    'LBL_LOCATION' => 'Lokacija',
+    'LBL_SEARCH_BUTTON' => 'Pretraga',
+    'LBL_SEARCH_QUERY_PLACEHOLDER' => 'Unesite pretragu...',
+    'LBL_INDEX_STATS' => 'Status indeksa',
+    'LBL_OPTIMISE_NOW' => "Optimiziraj sada",
+    'LBL_TOTAL_RECORDS' => 'Ukupni zapisi',
+    'LBL_INDEXED_RECORDS' => 'Indeksirani zapisi',
+    'LBL_UNINDEXED_RECORDS' => 'Neindeksirani zapisi',
+    'LBL_FAILED_RECORDS' => 'Neuspjeli zapisi',
+    'LBL_INDEX_FILES' => 'Broj indeks datoteka',
+    'LBL_SEARCH_RESULT_SCORE' => 'Rezultat pretraživanja',
+    'LBL_SEARCH_RESULT_MODULE' => 'Modul',
+    'LBL_SEARCH_RESULT_NAME' => 'Ime',
+    'LBL_SEARCH_RESULT_DATE_CREATED' => 'Datum kreiranja',
+    'LBL_SEARCH_RESULT_DATE_MODIFIED' => 'Datum izmjene',
+    'LBL_SEARCH_RESULT_EMPTY' => 'Nema rezultata',
+    'LBL_SEARCH_RESULT_SUMMARY' => 'Sažetak',
+    'LBL_NEVER_OPTIMISED' => 'Nikad',
+    'LBL_USE_AOD_SEARCH' => 'Koristite napredno pretraživanje',
+    'LBL_USE_VANILLA_SEARCH' => 'Koristite osnovno pretraživanje',
+);
