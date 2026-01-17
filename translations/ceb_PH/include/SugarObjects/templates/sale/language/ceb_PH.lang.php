a753d771d0129d994ff1905c367429b83549e347
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 04:35:35 +0000

    New translations en_us.lang.php (Cebuano)

---
+++ translations/ceb_PH/include/SugarObjects/templates/sale/language/ceb_PH.lang.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_MODULE_NAME' => 'Baligya',
+    'LBL_MODULE_TITLE' => 'Baligya: Balay',
+    'LBL_SEARCH_FORM_TITLE' => 'Pagpangita sa Baligya',
+    'LBL_LIST_FORM_TITLE' => 'Listahan sa Baligya',
+    'LBL_NAME' => 'Pangan sa Baligya',
+    'LBL_LIST_SALE_NAME' => 'Pangalan',
+    'LBL_LIST_ACCOUNT_NAME' => 'Ngalan sa Account',
+    'LBL_LIST_AMOUNT' => 'Kantidad',
+    'LBL_LIST_DATE_CLOSED' => 'Sirado',
+    'LBL_LIST_SALE_STAGE' => 'Baligya na Stage',
+    'LBL_ACCOUNT_ID' => 'ID sa Account',
+    //DON'T CONVERT THESE THEY ARE MAPPINGS
+    'db_name' => 'LBL_NAME',
+    //END DON'T CONVERT
+    'LBL_ACCOUNT_NAME' => 'Pangalan sa account:',
+    'LBL_AMOUNT' => 'Kantidad:',
+    'LBL_AMOUNT_USDOLLAR' => 'Kantidad sa USD:',
+    'LBL_CURRENCY' => 'Salapi:',
+    'LBL_DATE_CLOSED' => 'Gidahom nga Petsang Masirado:',
+    'LBL_TYPE' => 'Matang:',
+    'LBL_CAMPAIGN' => 'Kampanya:',
+    'LBL_LEADS_SUBPANEL_TITLE' => 'Nag-una',
+    'LBL_PROJECTS_SUBPANEL_TITLE' => 'Mga Proyekto',
+    'LBL_NEXT_STEP' => 'Sunod nga lakang:',
+    'LBL_LEAD_SOURCE' => 'Unang Source:',
+    'LBL_SALES_STAGE' => 'Bantawan sa mga baligya:',
+    'LBL_PROBABILITY' => 'Kalagmitan (%):',
+    'LBL_DESCRIPTION' => 'Deskripsyon:',
+    'LBL_DUPLICATE' => 'Posible nga Kopya sa Baligya',
+    'MSG_DUPLICATE' => 'Ang baligya nga rekord nga imong buhaton kay tingalig pareho sa baligya nga rekord nga naa na. Ang mga rekords nga adunay kapareho og pangan kay gilista sa ubos.<br>Pislita ang Save para magpadayon sa paghimo og bag-o nga baligya, o pislita ang Cancel para mabalik sa modyul og dili magpadayon sa pagbuhat sa baligya.',
+    'LBL_NEW_FORM_TITLE' => 'Pagbuhat og Baligya',
+    'ERR_DELETE_RECORD' => 'Kinahanglang nga i-specify ang numero sa rekord para matangtang ang baligya.',
+    'LBL_DEFAULT_SUBPANEL_TITLE' => 'Baligya',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Kalihukan',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Kasaysayan',
+
+    'LBL_CONTACTS_SUBPANEL_TITLE' => 'Kontak',
+    'LBL_ASSIGNED_TO_NAME' => 'Tiggamitan:',
+    'LBL_LIST_ASSIGNED_TO_NAME' => 'Na assign nga manggagamit',
+    'LBL_ASSIGNED_TO_ID' => 'Gigahin didto sa ID',
+    'LBL_MODIFIED_NAME' => 'Giusab pinaagi sa Pangan sa User',
+    'LBL_SALE_INFORMATION' => 'Impormasyon sa pagbaligya',
+    'LBL_CURRENCY_NAME' => 'Pangan sa kwarta',
+    'LBL_CURRENCY_SYMBOL' => 'Simbolo sa kwarta',
+    'LBL_EDIT_BUTTON' => 'Usba',
+    'LBL_REMOVE' => 'Tangtangon',
+
+);
