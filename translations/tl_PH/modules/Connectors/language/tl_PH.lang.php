c4ce6c132afe231990b9a5d63004f81a1263d48f
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 04:10:40 +0000

    New translations en_us.lang.php (Tagalog)

---
+++ translations/tl_PH/modules/Connectors/language/tl_PH.lang.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+
+$mod_strings = array(
+
+    'LBL_ADMINISTRATION_MAIN' => 'Ang konektor ng mga setting',
+    'LBL_AVAILABLE' => 'Magagamit',
+    'LBL_BACK' => '< Bumalik',
+    'LBL_CONFIRM_CONTINUE_SAVE' => 'Ang ilang kinakailangang mga field ay naiwang blangko. Magpatuloy para ma-save ang mga pagbabago?',
+    'LBL_CONNECTOR_FIELDS' => 'Mga filed ng konektor',
+    'LBL_DATA' => 'Datos',
+    'LBL_DEFAULT' => 'Naka-default',
+    'LBL_DISABLED' => 'Huwag Paganahin',
+    'LBL_ENABLED' => 'Paganahin',
+    'LBL_EXTERNAL' => 'Payagan ang mga gumagamit na lumikha ng panglabas na mga talaan ng account sa konektor na ito.',
+    'LBL_EXTERNAL_SET_PROPERTIES' => ' Nang sa gayon ay magamit ang konektor na ito, ang mga ati-arian ay dapat rin na naitakda sa pahina ng settings ng pagtatakda ng mga ari-arian ng konektor.',
+    'LBL_MERGE' => 'Pagsamahin',
+    'LBL_MODIFY_DISPLAY_TITLE' => 'Paganahin ang mga konektor',
+    'LBL_MODIFY_DISPLAY_DESC' => 'Pumili kung aling mga modyul ang pinagana sa bawat konektor.',
+    'LBL_MODULE_FIELDS' => 'Mga field ng modyul',
+    'LBL_MODIFY_MAPPING_TITLE' => 'Mapa ng mga field ng konektor',
+    'LBL_MODIFY_MAPPING_DESC' => 'Mapa ng mga field ng konektor sa mga field ng modyul nang sa gayon ay matukoy kung anong datos ng konektor ang maaaring matingnan at maisama sa mga talaan ng modyul.',
+    'LBL_MODIFY_PROPERTIES_TITLE' => 'Itakda ang mga ari-arian ng konektor',
+    'LBL_MODIFY_PROPERTIES_DESC' => 'Isaayos ang mga ari-arian para sa bawat konektor, kasama ang URLs at mga susi ng API.',
+    'LBL_MODIFY_SEARCH_TITLE' => 'Pamahalaan ang paghanap ng konektor',
+    'LBL_MODIFY_SEARCH' => 'Maghanap',
+    'LBL_MODIFY_SEARCH_DESC' => 'Piliin ang mga field ng konektor na gagamitin para mahanap ang datos para sa bawat modyul.',
+    'LBL_MODULE_NAME' => 'Ang mga Konekonekta',
+    'LBL_NO_PROPERTIES' => 'Walang pwedeng isaayos na mga ari-arian oara sa konektor na ito.',
+    'LBL_SAVE' => 'I-seyb',
+    'LBL_SUMMARY' => 'Buod',
+    'LBL_STEP1' => 'Hanapin at tingnan ang datos',
+    'LBL_STEP2' => 'Pagsamahin ang mga talaan kasama ng',
+    'LBL_TEST_SOURCE' => 'Pagsusulit ng konektor',
+    'LBL_TEST_SOURCE_FAILED' => 'Nabigo ang pagsusulit',
+    'LBL_TEST_SOURCE_SUCCESS' => 'Tagumpay ang pagsusulit',
+    'LBL_TITLE' => 'Pinagsamang datos',
+
+    'ERROR_NO_ADDITIONAL_DETAIL' => 'Kamalian: Walang dagdag na mga detalye ang nahanap para sa talaan.',
+    'ERROR_NO_SEARCHDEFS_DEFINED' => 'Walang mga modyul ang napagana para sa konektor na ito. Pumili ng modyul lara sa konektor na ito sa pahina ng pagpapagana ng mga konektor.',
+    'ERROR_NO_SEARCHDEFS_MAPPED' => 'Error: Walang mga konektor ang na-enable na may search fields na tinukoy.',
+    'ERROR_NO_SEARCHDEFS_MAPPING' => 'Error: Walang search field na tinukoy para sa modyul at konektor. Pakiusap kontakin ang system administrador.',
+    'ERROR_NO_DISPLAYABLE_MAPPED_FIELDS' => 'Error: Walang modyul fields na na-mapped para ipakita sa mga resulta. Pakiusap kontakin ang system administrator.',
+    'LBL_INFO_INLINE' => 'Impormasyon' /*for 508 compliance fix*/,
+    'LBL_CLOSE' => 'Sarado' /*for 508 compliance fix*/,
+
+);
