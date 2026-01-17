12a36ec2c1fcfe1c6041f86a5fdf948ffc0c6cb7
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 02:27:42 +0000

    New translations en_us.lang.php (Arabic)

---
+++ translations/ar/include/SugarObjects/templates/sale/language/ar.lang.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_MODULE_NAME' => 'بيع',
+    'LBL_MODULE_TITLE' => 'البيع: الصفحة الرئيسية',
+    'LBL_SEARCH_FORM_TITLE' => 'البحث عن بيع',
+    'LBL_LIST_FORM_TITLE' => 'قائمة البيع',
+    'LBL_NAME' => 'اسم البيع',
+    'LBL_LIST_SALE_NAME' => 'الاسم',
+    'LBL_LIST_ACCOUNT_NAME' => 'اسم الحساب',
+    'LBL_LIST_AMOUNT' => 'المبلغ',
+    'LBL_LIST_DATE_CLOSED' => 'إغلاق',
+    'LBL_LIST_SALE_STAGE' => 'مرحلة المبيعات',
+    'LBL_ACCOUNT_ID' => 'معرف الحساب',
+    //DON'T CONVERT THESE THEY ARE MAPPINGS
+    'db_name' => 'LBL_NAME',
+    //END DON'T CONVERT
+    'LBL_ACCOUNT_NAME' => 'اسم الحساب:',
+    'LBL_AMOUNT' => 'المبلغ:',
+    'LBL_AMOUNT_USDOLLAR' => 'مبلغ بالدولار الأمريكي:',
+    'LBL_CURRENCY' => 'العملة:',
+    'LBL_DATE_CLOSED' => 'تاريخ الإغلاق المتوقع:',
+    'LBL_TYPE' => 'النوع:',
+    'LBL_CAMPAIGN' => 'الحملة:',
+    'LBL_LEADS_SUBPANEL_TITLE' => 'العملاء المحتملون',
+    'LBL_PROJECTS_SUBPANEL_TITLE' => 'المشاريع',
+    'LBL_NEXT_STEP' => 'الخطوة التالية:',
+    'LBL_LEAD_SOURCE' => 'مصدر العميل المحتمل:',
+    'LBL_SALES_STAGE' => 'مرحلة المبيعات:',
+    'LBL_PROBABILITY' => 'نسبة الاحتمال (%):',
+    'LBL_DESCRIPTION' => 'الوصف:',
+    'LBL_DUPLICATE' => 'إحتمالية تكرار البيع',
+    'MSG_DUPLICATE' => 'سجل البيع الذي ستنشئه قد يكون تكرار لسجل بيع موجود مُسبقاً. إن سجلات البيع التي تحتوي أسماء مُشابهة مذكورة بالأسفل.<br>انقر على حفظ للإستمرار في إنشاء سجل البيع الجديد أو أنقر على إلغاء حتى تعود للنظام بدون إنشاء سجل البيع.',
+    'LBL_NEW_FORM_TITLE' => 'إنشاء بيع',
+    'ERR_DELETE_RECORD' => 'رقم السجل يجب أن يكون مُحدد حتى يتم حذف سجل البيع.',
+    'LBL_DEFAULT_SUBPANEL_TITLE' => 'بيع',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'الأنشطة',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'التاريخ',
+
+    'LBL_CONTACTS_SUBPANEL_TITLE' => 'جهات الإتصال',
+    'LBL_ASSIGNED_TO_NAME' => 'المستخدم:',
+    'LBL_LIST_ASSIGNED_TO_NAME' => 'المستخدم المكلف',
+    'LBL_ASSIGNED_TO_ID' => 'أُسندت لهذا المُعرف',
+    'LBL_MODIFIED_NAME' => 'عُدلت بواسطة اسم المُستخدم',
+    'LBL_SALE_INFORMATION' => 'معلومات البيع',
+    'LBL_CURRENCY_NAME' => 'اسم العملة',
+    'LBL_CURRENCY_SYMBOL' => 'رمز العملة',
+    'LBL_EDIT_BUTTON' => 'تحرير',
+    'LBL_REMOVE' => 'حذف',
+
+);
