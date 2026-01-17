10372413cf1409a89e4b90a6945ba5066ac96ac9
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 03:18:07 +0000

    New translations en_us.lang.php (Slovak)

---
+++ translations/sk_SK/include/SugarObjects/templates/person/language/sk_SK.lang.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_SALUTATION' => 'Oslovenie',
+    'LBL_NAME' => 'Meno',
+    'LBL_FIRST_NAME' => 'Krstné meno',
+    'LBL_LAST_NAME' => 'Priezvisko',
+    'LBL_TITLE' => 'Titul',
+    'LBL_DEPARTMENT' => 'Oddelenie',
+    'LBL_DO_NOT_CALL' => 'Nevolať',
+    'LBL_HOME_PHONE' => 'Telefón domov',
+    'LBL_MOBILE_PHONE' => 'Mobilný telefón',
+    'LBL_OFFICE_PHONE' => 'Telefón do práce',
+    'LBL_OTHER_PHONE' => 'Ďalší telefón',
+    'LBL_FAX_PHONE' => 'Fax',
+    'LBL_EMAIL_ADDRESS' => 'E-malové adresy',
+    'LBL_PRIMARY_ADDRESS' => 'Primárna adresa',
+    'LBL_PRIMARY_ADDRESS_STREET' => 'Primárna adresa',
+    'LBL_PRIMARY_ADDRESS_STREET_2' => 'Primárna adresa, Ulica 2:',
+    'LBL_PRIMARY_ADDRESS_STREET_3' => 'Primárna adresa, Ulica 3:',
+    'LBL_PRIMARY_ADDRESS_CITY' => 'Primárna adresa: Mesto',
+    'LBL_PRIMARY_ADDRESS_STATE' => 'Primárna adresa: Štát',
+    'LBL_PRIMARY_ADDRESS_POSTALCODE' => 'Primárne adresa: PSČ',
+    'LBL_PRIMARY_ADDRESS_COUNTRY' => 'Primárna adresa: Krajina:',
+    'LBL_ALT_ADDRESS' => 'Alternatívna adresa',
+    'LBL_ALT_ADDRESS_STREET' => 'Alternatívna adresa',
+    'LBL_ALT_ADDRESS_STREET_2' => 'Alternatívna adresa ulica 2:',
+    'LBL_ALT_ADDRESS_STREET_3' => 'Alternatívna adresa ulica 3:',
+    'LBL_ALT_ADDRESS_CITY' => 'Alternatívna adresa: Mesto',
+    'LBL_ALT_ADDRESS_STATE' => 'Alternatívna adresa: Štát',
+    'LBL_ALT_ADDRESS_POSTALCODE' => 'Alternatívna adresa: PSČ',
+    'LBL_ALT_ADDRESS_COUNTRY' => 'Alternatívna adresa: Krajina',
+    'LBL_PRIMARY_STREET' => 'Adresa',
+    'LBL_ALT_STREET' => 'Iná adresa',
+    'LBL_STREET' => 'Iná adresa',
+    'LBL_CITY' => 'Mesto',
+    'LBL_STATE' => 'Štát',
+    'LBL_POSTAL_CODE' => 'PSČ',
+    'LBL_COUNTRY' => 'Krajina',
+    'LBL_CONTACT_INFORMATION' => 'Kontaktné informácie',
+    'LBL_ADDRESS_INFORMATION' => 'Adresa(y)',
+    'LBL_ASSIGNED_TO_NAME' => 'Užívateľ',
+    'LBL_OTHER_EMAIL_ADDRESS' => 'Iný email:',
+    'LBL_ASSISTANT' => 'Asistent',
+    'LBL_ASSISTANT_PHONE' => 'Telefón asistenta',
+    'LBL_WORK_PHONE' => 'Pracovný telefón',
+    'LNK_IMPORT_VCARD' => 'Vytvoriť z vCard',
+    'LBL_ANY_EMAIL' => 'Akýkoľvek email',
+    'LBL_EMAIL_NON_PRIMARY' => 'Nie-Primárne E-maily',
+    'LBL_PHOTO' => 'Fotka',
+    'LBL_EDIT_BUTTON' => 'Upraviť',
+    'LBL_REMOVE' => 'Odstrániť',
+
+    //Lawful Basis labels
+    'LBL_LAWFUL_BASIS' => 'Právny základ',
+    'LBL_DATE_REVIEWED' => 'Dátum kontroly právneho základu',
+    'LBL_LAWFUL_BASIS_SOURCE' => 'Zdroj právneho základu',
+    'LBL_CONSENT' => 'Súhlas',
+    //End Lawful Basis labels
+);
