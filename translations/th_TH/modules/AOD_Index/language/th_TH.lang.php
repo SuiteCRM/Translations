f011908527500142c1b693870e01695e16396907
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 04:59:10 +0000

    New translations en_us.lang.php (Thai)

---
+++ translations/th_TH/modules/AOD_Index/language/th_TH.lang.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID' => 'ชื่อไอดีผู้รับผิดชอบ',
+    'LBL_ASSIGNED_TO_NAME' => 'ผู้รับผิดชอบ',
+    'LBL_ID' => 'ไอดี',
+    'LBL_DATE_ENTERED' => 'วันที่สร้าง',
+    'LBL_DATE_MODIFIED' => 'วันที่แก้ไข',
+    'LBL_MODIFIED' => 'แก้ไขโดย',
+    'LBL_MODIFIED_NAME' => 'แก้ไขโดยชื่อ',
+    'LBL_CREATED' => 'สร้างโดย',
+    'LBL_DESCRIPTION' => 'รายละเอียด',
+    'LBL_DELETED' => 'ลบ',
+    'LBL_NAME' => 'ชื่อ',
+    'LBL_CREATED_USER' => 'สร้างโดยผู้ใช้งาน',
+    'LBL_MODIFIED_USER' => 'แก้ไขโดยผู้ใช้',
+    'LBL_LIST_NAME' => 'ชื่อ',
+    'LBL_EDIT_BUTTON' => 'แก้ไข',
+    'LBL_REMOVE' => 'ลบออก',
+    'LBL_LIST_FORM_TITLE' => 'รายการ Index',
+    'LBL_MODULE_NAME' => 'Index',
+    'LBL_MODULE_TITLE' => 'Index',
+    'LBL_HOMEPAGE_TITLE' => 'Index ของฉัน',
+    'LNK_NEW_RECORD' => 'สร้าง Index',
+    'LNK_LIST' => 'ดู Index',
+    'LBL_SEARCH_FORM_TITLE' => 'ค้นหา Index',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'ดูประวัติ',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'กิจกรรม',
+    'LBL_NEW_FORM_TITLE' => 'Index ใหม่',
+    'LBL_LAST_OPTIMISED' => 'Last Optimised',
+    'LBL_LOCATION' => 'สถานที่',
+    'LBL_SEARCH_BUTTON' => 'ค้นหา',
+    'LBL_SEARCH_QUERY_PLACEHOLDER' => 'Enter search...',
+    'LBL_INDEX_STATS' => 'Index สถิติ',
+    'LBL_OPTIMISE_NOW' => "Optimise now",
+    'LBL_TOTAL_RECORDS' => 'Total records',
+    'LBL_INDEXED_RECORDS' => 'บันทึกการจัดทำ Index',
+    'LBL_UNINDEXED_RECORDS' => 'Unindexed บันทึก',
+    'LBL_FAILED_RECORDS' => 'Failed records',
+    'LBL_INDEX_FILES' => 'การนับไฟล์ Index',
+    'LBL_SEARCH_RESULT_SCORE' => 'Search Score',
+    'LBL_SEARCH_RESULT_MODULE' => 'โมดูล',
+    'LBL_SEARCH_RESULT_NAME' => 'ชื่อ',
+    'LBL_SEARCH_RESULT_DATE_CREATED' => 'วันที่สร้าง',
+    'LBL_SEARCH_RESULT_DATE_MODIFIED' => 'วันที่แก้ไข',
+    'LBL_SEARCH_RESULT_EMPTY' => 'No results',
+    'LBL_SEARCH_RESULT_SUMMARY' => 'สรุป',
+    'LBL_NEVER_OPTIMISED' => 'ไม่เป็นอันขาด',
+    'LBL_USE_AOD_SEARCH' => 'Use Advanced Search',
+    'LBL_USE_VANILLA_SEARCH' => 'Use Basic Search',
+);
