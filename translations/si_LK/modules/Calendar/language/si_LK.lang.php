7222ae914fbae234349b10eb2a4d743345f18a57
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 04:18:57 +0000

    New translations en_us.lang.php (Sinhala)

---
+++ translations/si_LK/modules/Calendar/language/si_LK.lang.php
@@ -0,0 +1,225 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+
+    'LBL_SHAREDWEEK' => 'Shared Week',
+    'LBL_SHAREDMONTH' => 'Shared Month',
+
+    'LBL_MODULE_NAME' => 'දින දසුන',
+    'LBL_MODULE_TITLE' => 'දින දසුන',
+    'LNK_NEW_CALL' => 'උපෙල්ඛනෙය් ඇමතුම්',
+    'LNK_NEW_MEETING' => 'උපෙල්ඛනෙය් රැස්වීම',
+    'LNK_NEW_TASK' => ' කාර්ය සාධක නිර්මාණය',
+    'LNK_CALL_LIST' => 'ඇමතුම්',
+    'LNK_MEETING_LIST' => 'රැස්වීම්',
+    'LNK_TASK_LIST' => ' කාර්යයන්',
+    'LNK_TASK' => 'කාර්ය',
+    'LNK_TASK_VIEW' => 'View Task',
+    'LNK_EVENT' => 'අවස්ථාවට',
+    'LNK_EVENT_VIEW' => 'View Event',
+    'LNK_VIEW_CALENDAR' => 'අද',
+    'LNK_IMPORT_CALLS' => 'ආනයන ඇමතුම්',
+    'LNK_IMPORT_MEETINGS' => 'ආනයන රැස්වීම්',
+    'LNK_IMPORT_TASKS' => 'ආනයන කර්තව්යයෝ',
+    'LBL_MONTH' => 'මාසික',
+    'LBL_AGENDADAY' => 'දවස',
+    'LBL_YEAR' => 'අවුරුදු',
+
+    'LBL_AGENDAWEEK' => 'සතිය',
+    'LBL_PREVIOUS_MONTH' => 'පසුගිය මාසික',
+    'LBL_PREVIOUS_DAY' => 'පෙර දින',
+    'LBL_PREVIOUS_YEAR' => 'ෙපර වර්ෂය',
+    'LBL_PREVIOUS_WEEK' => 'පසුගිය සතිය',
+    'LBL_NEXT_MONTH' => 'ඉදිරි මාසය',
+    'LBL_NEXT_DAY' => 'ඉදිරි දින',
+    'LBL_NEXT_YEAR' => 'ලබන වසර',
+    'LBL_NEXT_WEEK' => 'ලබන සතිය',
+    'LBL_AM' => 'පෙ.ව.',
+    'LBL_PM' => 'ප.ව.',
+    'LBL_SCHEDULED' => 'නියමිත',
+    'LBL_BUSY' => 'කාර්යබහුලයි',
+    'LBL_CONFLICT' => 'ගැටුම',
+    'LBL_USER_CALENDARS' => 'පරිශීලක දින දර්ශන',
+    'LBL_SHARED' => 'හවුල්',
+    'LBL_PREVIOUS_SHARED' => ' කලින්',
+    'LBL_NEXT_SHARED' => 'ඊලඟ',
+    'LBL_SHARED_CAL_TITLE' => 'හවුල් දින දසුන',
+    'LBL_USERS' => 'පරිශිලකයින්',
+    'LBL_REFRESH' => 'නැවුම් කරන්න',
+    'LBL_EDIT_USERLIST' => ' පරිශීලක ලැයිස්තුව',
+    'LBL_SELECT_USERS' => 'දින දර්ශනය ප්රදර්ශනය සඳහා භාවිතා කරන්නන් තෝරන්න',
+    'LBL_FILTER_BY_TEAM' => 'කණ්ඩායමක් විසින් පරිශීලක ලැයිස්තුව පෙරහන් කරන්න:',
+    'LBL_ASSIGNED_TO_NAME' => 'පවරා',
+    'LBL_DATE' => 'දිනය සහ වේලාව ආරම්භ කරන්න',
+    'LBL_CREATE_MEETING' => 'උපෙල්ඛනෙය් රැස්වීම',
+    'LBL_CREATE_CALL' => ' ලඝු-සටහන ඇමතුම්',
+    'LBL_HOURS_ABBREV' => 'ඌ',
+    'LBL_MINS_ABBREV' => 'එම්',
+
+
+    'LBL_YES' => 'ඔව්',
+    'LBL_NO' => 'නැහැ',
+    'LBL_SETTINGS' => 'සැකසුම්',
+    'LBL_CREATE_NEW_RECORD' => 'ක්රියාකාරකම් නිර්මාණය',
+    'LBL_LOADING' => 'පූරණය වෙමින්...',
+    'LBL_SAVING' => 'ඉතිරිය ...',
+    'LBL_SENDING_INVITES' => 'සුරකිමින් සහ ආරාධනා යැවීම .....',
+    'LBL_CONFIRM_REMOVE' => 'ඔබ වාර්තාව ඉවත් කිරීමට අවශ්ය බව ඔබට විශ්වාසද?',
+    'LBL_CONFIRM_REMOVE_ALL_RECURRING' => 'ඔබ සියලු නැවත නැවත වාර්තා ඉවත් කිරීමට අවශ්ය බව ඔබට විශ්වාසද?',
+    'LBL_EDIT_RECORD' => 'සංස්කරණය කරන්න ක්රියාකාරකම්',
+    'LBL_ERROR_SAVING' => 'දෝෂ ඉතිරිකර ගනිමින්',
+    'LBL_ERROR_LOADING' => 'පැටවීම දෝශයක්',
+    'LBL_GOTO_DATE' => 'Goto දිනය',
+    'NOTICE_DURATION_TIME' => 'කාලය කාලය 0 ට වඩා වැඩි විය යුතුය',
+    'LBL_STYLE_BASIC' => 'මූලික', //No need to be translated in all caps. Translation used just in menu action items when using the SuiteP template
+    'LBL_STYLE_ADVANCED' => ' උසස්', //No need to be translated in all caps. Translation used just in menu action items when using the SuiteP template
+
+    'LBL_NO_USER' => 'ක්ෂේත්රයේ තරගය අංකය: කිරීමට යොදවා',
+    'LBL_SUBJECT' => ' විෂය',
+    'LBL_DURATION' => 'කාල සීමාව',
+    'LBL_STATUS' => 'තත්වය',
+    'LBL_PRIORITY' => 'ප්රමුඛ',
+
+    'LBL_SETTINGS_TITLE' => 'සැකසුම්',
+    'LBL_SETTINGS_DISPLAY_TIMESLOTS' => 'දිනය සහ සතිය අදහස් සංදර්ශනය කාලය මඳබව:',
+    'LBL_SETTINGS_TIME_STARTS' => 'කාලය ආරම්භ කරන්න:',
+    'LBL_SETTINGS_TIME_ENDS' => 'අවසන් වන වේලාව:',
+    'LBL_SETTINGS_CALLS_SHOW' => 'පෙන්වන්න ඇමතුම්:',
+    'LBL_SETTINGS_TASKS_SHOW' => 'පෙන්වන්න කර්තව්යයෝ:',
+    'LBL_SETTINGS_COMPLETED_SHOW' => 'පෙන්වන්න රැස්වීම්, ඇමතුම් හා කර්තව්යයෝ සම්පූර්ණ:',
+    'LBL_SETTINGS_DISPLAY_SHARED_CALENDAR_SEPARATE' => 'Shared Calendar Separate:',
+
+    'LBL_SAVE_BUTTON' => 'සුරකින්න',
+    'LBL_DELETE_BUTTON' => 'මකා',
+    'LBL_APPLY_BUTTON' => 'අයදුම් කරන්න',
+    'LBL_SEND_INVITES' => 'ආරාධනා සුරකින්න & යවන්න',
+    'LBL_CANCEL_BUTTON' => 'අවලංගු කරන්න',
+    'LBL_CLOSE_BUTTON' => 'වසන්න / * 508 අනුකූල විසඳුමක් සඳහා * /',
+
+    'LBL_GENERAL_TAB' => 'විස්තර',
+    'LBL_PARTICIPANTS_TAB' => 'ආරාධිත අමුත්තන්',
+    'LBL_REPEAT_TAB' => 'පුනරාගමනය',
+
+    'LBL_REPEAT_TYPE' => 'නැවත',
+    'LBL_REPEAT_INTERVAL' => 'සෑම',
+    'LBL_REPEAT_END' => ' අවසානය',
+    'LBL_REPEAT_END_AFTER' => 'පසු',
+    'LBL_REPEAT_OCCURRENCES' => 'නිතර මෙම',
+    'LBL_REPEAT_END_BY' => 'විසින්',
+    'LBL_REPEAT_DOW' => '	මත',
+    'LBL_REPEAT_UNTIL' => 'තුරු නැවත නැවත',
+    'LBL_REPEAT_COUNT' => 'සිදුවීම් සංඛ්යාව',
+    'LBL_REPEAT_LIMIT_ERROR' => 'ඔබගේ ඉල්ලීම $ සීමාව රැස්වීම් වඩා නිර්මාණය කිරීමට යන්නේ.',
+
+    'LBL_EDIT_ALL_RECURRENCES' => 'සංස්කරණය කරන්න සියලුම සිදුවීම්',
+    'LBL_REMOVE_ALL_RECURRENCES' => 'සියලුම සිදුවීම් Delete',
+
+    'LBL_DATE_END_ERROR' => 'අවසාන දිනය ආරම්භක දිනය පෙර ය',
+    'ERR_YEAR_BETWEEN' => 'සමාවන්න, දින අවුරුදු <br> ඔබ විසින් ඉල්ලා වසර හැසිරවිය නොහැකි 1970 හා 2037 අතර විය යුතු',
+    'ERR_NEIGHBOR_DATE' => 'get_neighbor_date_str: මෙම දැක්ම සඳහා අර්ථ දක්වා නැත',
+    'LBL_NO_ITEMS_MOBILE' => 'ඔබේ දින දර්ශනය වූ සතිය සඳහා පැහැදිලි ය.',
+    'LBL_GENERAL_SETTINGS' => 'General Settings',
+    'LBL_COLOR_SETTINGS' => 'Colour Settings',
+    'LBL_MODULE' => 'Module',
+    'LBL_BODY' => 'ඛඳ',
+    'LBL_BORDER' => 'දේශසීමා',
+    'LBL_TEXT' => 'පෙළ',
+);
+
+
+$mod_list_strings = array(
+    'dom_cal_weekdays' =>
+        array(
+            '0' => "සන්",
+            '1' => "සඳු",
+            '2' => "අග",
+            '3' => "බදාදා",
+            '4' => "බිඳ වැටීම",
+            '5' => "සිකු",
+            '6' => "සෙන",
+        ),
+    'dom_cal_weekdays_long' =>
+        array(
+            '0' => "ඉරිදා",
+            '1' => "සඳුදා",
+            '2' => "අගහරුවදා",
+            '3' => "බදාදා",
+            '4' => "බ්රහස්පතින්දා",
+            '5' => "සිකුරාදා",
+            '6' => "සෙනසුරාදා",
+        ),
+    'dom_cal_month' =>
+        array(
+            '0' => "",
+            '1' => "දුරුතු",
+            '2' => "නවම්",
+            '3' => "මැදින්",
+            '4' => "බක්",
+            '5' => "වෙසක්",
+            '6' => "පොසොන්",
+            '7' => "ඇසළ",
+            '8' => "නිකිණි",
+            '9' => "බිනර",
+            '10' => "වප්",
+            '11' => "ඉල්",
+            '12' => "උඳු",
+        ),
+    'dom_cal_month_long' =>
+        array(
+            '0' => "",
+            '1' => "දුරුතු",
+            '2' => "නවම්",
+            '3' => "මැදින්",
+            '4' => "බක්",
+            '5' => "වෙසක්",
+            '6' => "පොසොන්",
+            '7' => "ඇසළ",
+            '8' => "නිකිණි",
+            '9' => "බිනර",
+            '10' => "වප්",
+            '11' => "ඉල්",
+            '12' => "උඳුවප්",
+        ),
+);
