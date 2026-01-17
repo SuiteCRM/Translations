abf6808d65692850f0c23f3797b38b0c7e42207c
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 03:03:40 +0000

    New translations en_us.lang.php (Korean)

---
+++ translations/ko_KR/modules/Schedulers/language/ko_KR.lang.php
@@ -0,0 +1,170 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+// OOTB Scheduler Job Names:
+    'LBL_OOTB_WORKFLOW' => '워크플로우 업무 실행',
+    'LBL_OOTB_REPORTS' => '보고서 생성 예약 업무 실행',
+    'LBL_OOTB_IE' => '인바운드 사서함 확인',
+    'LBL_OOTB_BOUNCE' => '바운스(반송)된 캠페인 메일 처리를 야간에 실행',
+    'LBL_OOTB_CAMPAIGN' => '캠페인 이메일 대량 송신을 야간에 실행',
+    'LBL_OOTB_PRUNE' => '매달 1일의 Prune 데이타베이스',
+    'LBL_OOTB_TRACKER' => 'Prune Tracker Tables',
+    'LBL_OOTB_SUITEFEEDS' => 'SuiteCRM 피드 테이블 정리',
+    'LBL_OOTB_LUCENE_INDEX' => 'Perform Lucene Index',
+    'LBL_OOTB_OPTIMISE_INDEX' => 'Optimise AOD Index',
+    'LBL_OOTB_SEND_EMAIL_REMINDERS' => '이메일 미리 알림 실행',
+    'LBL_OOTB_CLEANUP_QUEUE' => 'Jobs Queue 지우기',
+    'LBL_OOTB_REMOVE_DOCUMENTS_FROM_FS' => '파일시스템의 파일 삭제',
+    'LBL_OOTB_GOOGLE_CAL_SYNC' => '구글 캘린더 동기화',
+    'LBL_OOTB_ELASTIC_INDEX' => 'Perform Elasticsearch Index',
+    'LBL_OOTB_CAL_ACC_SYNC' => 'Calendar Accounts Sync',
+
+// List Labels
+    'LBL_LIST_JOB_INTERVAL' => '간격:',
+    'LBL_LIST_LIST_ORDER' => '일정:',
+    'LBL_LIST_NAME' => '스케줄러',
+    'LBL_LIST_RANGE' => '범위:',
+    'LBL_LIST_STATUS' => '상태',
+    'LBL_LIST_TITLE' => '일정 목록:',
+// human readable:
+    'LBL_SUN' => '일요일',
+    'LBL_MON' => '월요일',
+    'LBL_TUE' => '화요일',
+    'LBL_WED' => '수요일',
+    'LBL_THU' => '목요일',
+    'LBL_FRI' => '금요일',
+    'LBL_SAT' => '토요일',
+    'LBL_ALL' => '매일',
+    'LBL_EVERY' => '모든',
+    'LBL_FROM' => '보낸 사람',
+    'LBL_ON_THE' => 'On the',
+    'LBL_RANGE' => '받는 사람',
+    'LBL_AND' => '그리고',
+    'LBL_MINUTES' => '분',
+    'LBL_HOUR' => '시간',
+    'LBL_HOUR_SING' => '시간',
+    'LBL_OFTEN' => '가능한 한 자주',
+    'LBL_MIN_MARK' => '분 표시',
+
+
+// crontabs
+    'LBL_MINS' => 'min',
+    'LBL_HOURS' => '시간',
+    'LBL_DAY_OF_MONTH' => '날짜',
+    'LBL_MONTHS' => 'mo',
+    'LBL_DAY_OF_WEEK' => '일',
+    'LBL_CRONTAB_EXAMPLES' => '위의 내용은 표준 crontab 표기법을 사용합니다.',
+// Labels
+    'LBL_ALWAYS' => '항상',
+    'LBL_CATCH_UP' => '놓친 경우 실행',
+    'LBL_CATCH_UP_WARNING' => '이 작업을 실행하는 데 1분 이상 걸릴 수 있으면 선택을 취소하세요.',
+    'LBL_DATE_TIME_END' => '날짜 및 시간 종료',
+    'LBL_DATE_TIME_START' => '날짜 및 시간 시작',
+    'LBL_INTERVAL' => '간격',
+    'LBL_JOB' => '작업',
+    'LBL_JOB_URL' => '작업 경로',
+    'LBL_LAST_RUN' => '최근 성공한 실행',
+    'LBL_MODULE_NAME' => 'SuiteCRM 스케줄러',
+    'LBL_MODULE_TITLE' => '일정',
+    'LBL_NAME' => '작업 이름',
+    'LBL_NEVER' => '절대아님',
+    'LBL_NEW_FORM_TITLE' => '새로운 스케쥴',
+    'LBL_PERENNIAL' => '빈번한',
+    'LBL_SEARCH_FORM_TITLE' => '스케쥴러 검색',
+    'LBL_SCHEDULER' => '스케줄러',
+    'LBL_STATUS' => '상태',
+    'LBL_TIME_FROM' => 'Active From',
+    'LBL_TIME_TO' => 'Active To',
+    'LBL_WARN_CURL_TITLE' => 'cURL Warning:',
+    'LBL_WARN_CURL' => '경고:',
+    'LBL_WARN_NO_CURL' => '이 시스템에는 PHP 모듈로 활성화/컴파일된 cURL 라이브러리가 없습니다(--with-curl=/path/to/curl_library). 이 문제를 해결하려면 관리자에게 문의하세요. cURL 기능이 없으면 스케줄러가 해당 작업을 스레드할 수 없습니다.',
+    'LBL_BASIC_OPTIONS' => '기본 설정',
+    'LBL_ADV_OPTIONS' => '고급 설정',
+    'LBL_TOGGLE_ADV' => '고급 선택사항 보이기',
+    'LBL_TOGGLE_BASIC' => '기본 설정 표시',
+// Links
+    'LNK_LIST_SCHEDULER' => '일정',
+    'LNK_NEW_SCHEDULER' => '스케줄러 생성',
+// Messages
+    'ERR_CRON_SYNTAX' => '잘못 된 Cron 구문',
+    'NTC_LIST_ORDER' => '스케줄러 드롭다운 목록에서 보여질 순서를 설정합니다.',
+    'LBL_CRON_INSTRUCTIONS_WINDOWS' => 'Windows 스케줄러 설정',
+    'LBL_CRON_INSTRUCTIONS_LINUX' => 'Crontab 설정',
+    'LBL_CRON_LINUX_DESC1' => 'SuiteCRM 스케줄러를 실행하려면 다음 명령을 사용하여 웹 서버 사용자의 crontab 파일을 편집하십시오.',
+    'LBL_CRON_LINUX_DESC2' => '... and add the following line to the crontab file:',
+    'LBL_CRON_LINUX_DESC3' => 'You should do this only after the installation is concluded.',
+    'LBL_CRON_WINDOWS_DESC' => '참고: SuiteCRM 스케줄러를 실행 하려면 Windows 예약 된 작업을 사용하는 배치 파일을 만듭니다. 배치 파일에는 다음 명령을 포함 해야 합니다. ',
+// Subpanels
+    'LBL_JOBS_SUBPANEL_TITLE' => 'Job Log',
+    'LBL_EXECUTE_TIME' => '실행 시간',
+
+//jobstrings
+    'LBL_REFRESHJOBS' => 'Refresh Jobs',
+    'LBL_POLLMONITOREDINBOXES' => 'Check Inbound Mail Accounts',
+    'LBL_PERFORMFULLFTSINDEX' => 'Full-text Search Index System',
+
+    'LBL_RUNMASSEMAILCAMPAIGN' => '캠페인 이메일 대량 송신을 야간에 실행',
+    'LBL_POLLMONITOREDINBOXESFORBOUNCEDCAMPAIGNEMAILS' => '바운스(반송)된 캠페인 메일 처리를 야간에 실행',
+    'LBL_PRUNEDATABASE' => '매달 1일의 Prune 데이타베이스',
+    'LBL_TRIMTRACKER' => 'Prune Tracker Tables',
+    'LBL_TRIMSUGARFEEDS' => 'SuiteCRM 피드 테이블 정리',
+    'LBL_SENDEMAILREMINDERS' => 'Run Email Reminders Sending',
+    'LBL_CLEANJOBQUEUE' => 'Cleanup Job Queue',
+    'LBL_REMOVEDOCUMENTSFROMFS' => '파일시스템의 파일 삭제',
+
+    'LBL_AODOPTIMISEINDEX' => 'Optimise Advanced OpenDiscovery Index',
+    'LBL_AODINDEXUNINDEXED' => 'Index unindexed documents',
+    'LBL_POLLMONITOREDINBOXESAOP' => 'AOP Poll Monitored Inboxes',
+    'LBL_AORRUNSCHEDULEDREPORTS' => 'Run scheduled reports',
+    'LBL_PROCESSAOW_WORKFLOW' => 'Process AOW Workflow',
+
+    'LBL_RUNELASTICSEARCHINDEXERSCHEDULER' => 'Elasticsearch indexer',
+    'LBL_CALENDARSYNCJOB' => 'Sync Calendar Accounts',
+
+    'LBL_SCHEDULER_TIMES' => 'Scheduler Times',
+    'LBL_SYNCGOOGLECALENDAR' => 'Sync Google Calendars',
+);
+
+global $sugar_config;
