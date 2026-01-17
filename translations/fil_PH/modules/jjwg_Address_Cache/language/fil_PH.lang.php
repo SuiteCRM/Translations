ebe4c812c00b8bfe60dfe00dbf41f541726c65c3
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 04:16:24 +0000

    New translations en_us.lang.php (Filipino)

---
+++ translations/fil_PH/modules/jjwg_Address_Cache/language/fil_PH.lang.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings['LBL_ASSIGNED_TO_ID'] = 'Magtalaga ng Id para sa gumagamit';
+$mod_strings['LBL_ASSIGNED_TO_NAME'] = 'Itinatalaga sa';
+$mod_strings['LBL_ID'] = 'Ang ID';
+$mod_strings['LBL_DATE_ENTERED'] = 'Ang petsa ng pagkakalikha';
+$mod_strings['LBL_DATE_MODIFIED'] = 'Ang petsa ng pagkabago';
+$mod_strings['LBL_MODIFIED'] = 'Binago ni';
+$mod_strings['LBL_MODIFIED_NAME'] = 'Ito ay ibinago sa pamamagitan ng pangalan';
+$mod_strings['LBL_CREATED'] = 'Ito ay nalikha sa pamamagitan ng';
+$mod_strings['LBL_DESCRIPTION'] = 'Ang deskripsyon';
+$mod_strings['LBL_DELETED'] = 'Ang pagkabura';
+$mod_strings['LBL_NAME'] = 'Kinaroroonang pook';
+$mod_strings['LBL_CREATED_USER'] = 'Inilikha ng tagagamit';
+$mod_strings['LBL_MODIFIED_USER'] = 'Binago sa pamamagitan ng tagagamit';
+$mod_strings['LBL_LIST_NAME'] = 'Kinaroroonang pook';
+$mod_strings['LBL_LIST_FORM_TITLE'] = 'Listahan ng cache ng address';
+$mod_strings['LBL_MODULE_NAME'] = 'Ang address ng Cache';
+$mod_strings['LBL_MODULE_TITLE'] = 'Ang address ng Cache';
+$mod_strings['LBL_HOMEPAGE_TITLE'] = 'Ang aking address Cache';
+$mod_strings['LNK_NEW_RECORD'] = 'Lumikha ng address Cache';
+$mod_strings['LNK_LIST'] = 'I-view ang address Cache';
+$mod_strings['LBL_SEARCH_FORM_TITLE'] = 'I-search ang Address Cache';
+$mod_strings['LBL_HISTORY_SUBPANEL_TITLE'] = 'Tingnan ang kasaysayan';
+$mod_strings['LBL_ACTIVITIES_SUBPANEL_TITLE'] = 'Mga ginagawa';
+$mod_strings['LBL_NEW_FORM_TITLE'] = 'Bagong address Cache';
+$mod_strings['LBL_LAT'] = 'Ang latitud';
+$mod_strings['LBL_LNG'] = 'Ang longhitud';
+$mod_strings['LBL_CURRENT_USER_FILTER'] = 'Aking mga item';
+$mod_strings['LBL_ASSIGNED_TO'] = 'Itinalaga sa gumagamit';
