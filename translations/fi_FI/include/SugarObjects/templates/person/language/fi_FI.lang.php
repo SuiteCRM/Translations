04f6ae541e19617347f0b40bc2008191ecec8140
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 02:44:41 +0000

    New translations en_us.lang.php (Finnish)

---
+++ translations/fi_FI/include/SugarObjects/templates/person/language/fi_FI.lang.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_SALUTATION' => 'Tervehdys',
+    'LBL_NAME' => 'Nimi',
+    'LBL_FIRST_NAME' => 'Etunimi',
+    'LBL_LAST_NAME' => 'Sukunimi',
+    'LBL_TITLE' => 'Asema',
+    'LBL_DEPARTMENT' => 'Osasto',
+    'LBL_DO_NOT_CALL' => 'Älä soita',
+    'LBL_HOME_PHONE' => 'Kotipuhelin',
+    'LBL_MOBILE_PHONE' => 'GSM',
+    'LBL_OFFICE_PHONE' => 'Toimiston puhelin',
+    'LBL_OTHER_PHONE' => 'Muu puhelin',
+    'LBL_FAX_PHONE' => 'Faksi',
+    'LBL_EMAIL_ADDRESS' => 'Sähköpostiosoite/osoitteet',
+    'LBL_PRIMARY_ADDRESS' => 'Ensisijainen osoite',
+    'LBL_PRIMARY_ADDRESS_STREET' => 'Ensisijainen osoite',
+    'LBL_PRIMARY_ADDRESS_STREET_2' => 'Ensisijainen osoite Katuosoite 2:',
+    'LBL_PRIMARY_ADDRESS_STREET_3' => 'Ensisijainen osoite Katuosoite 3:',
+    'LBL_PRIMARY_ADDRESS_CITY' => 'Ensisijainen osoite Kaupunki/kunta',
+    'LBL_PRIMARY_ADDRESS_STATE' => 'Ensisijainen osoite Maakunta/lääni',
+    'LBL_PRIMARY_ADDRESS_POSTALCODE' => 'Ensisijainen osoite postinumero',
+    'LBL_PRIMARY_ADDRESS_COUNTRY' => 'Ensisijainen osoite Maa:',
+    'LBL_ALT_ADDRESS' => 'Muu osoite Katuosoite',
+    'LBL_ALT_ADDRESS_STREET' => 'Muu osoite Katuosoite',
+    'LBL_ALT_ADDRESS_STREET_2' => 'Muu osoite Katuosoite 2:',
+    'LBL_ALT_ADDRESS_STREET_3' => 'Muu osoite Katuosoite 3:',
+    'LBL_ALT_ADDRESS_CITY' => 'Muu osoite Kaupunki/kunta',
+    'LBL_ALT_ADDRESS_STATE' => 'Muu osoite Maakunta/lääni',
+    'LBL_ALT_ADDRESS_POSTALCODE' => 'Muu osoite Postinumero',
+    'LBL_ALT_ADDRESS_COUNTRY' => 'Muu osoite Maa',
+    'LBL_PRIMARY_STREET' => 'Osoite',
+    'LBL_ALT_STREET' => 'Muu osoite',
+    'LBL_STREET' => 'Muu osoite',
+    'LBL_CITY' => 'Kaupunki/kunta',
+    'LBL_STATE' => 'Maakunta/lääni',
+    'LBL_POSTAL_CODE' => 'Postinumero',
+    'LBL_COUNTRY' => 'Maa',
+    'LBL_CONTACT_INFORMATION' => 'Yhteystiedot',
+    'LBL_ADDRESS_INFORMATION' => 'Osoite (osoitteet)',
+    'LBL_ASSIGNED_TO_NAME' => 'Vastuuhenkilö',
+    'LBL_OTHER_EMAIL_ADDRESS' => 'Toinen sähköpostiosoite:',
+    'LBL_ASSISTANT' => 'Assistentti',
+    'LBL_ASSISTANT_PHONE' => 'Assistentin puhelin',
+    'LBL_WORK_PHONE' => 'Työpuhelin',
+    'LNK_IMPORT_VCARD' => 'Luo käyntikortista',
+    'LBL_ANY_EMAIL' => 'Mikä tahansa sähköpostiosoite:',
+    'LBL_EMAIL_NON_PRIMARY' => 'Toissijaiset sähköpostit',
+    'LBL_PHOTO' => 'Kuva',
+    'LBL_EDIT_BUTTON' => 'Muokkaa',
+    'LBL_REMOVE' => 'Poista',
+
+    //Lawful Basis labels
+    'LBL_LAWFUL_BASIS' => 'Oikeutettu peruste',
+    'LBL_DATE_REVIEWED' => 'Oikeutetun perusteen tarkistuspvm',
+    'LBL_LAWFUL_BASIS_SOURCE' => 'Oikeutetun perusteen lähde',
+    'LBL_CONSENT' => 'Suostumus',
+    //End Lawful Basis labels
+);
