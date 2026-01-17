f64924be967c7eb81f7324e91634bd5f2fb0bf0c
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 04:31:11 +0000

    New translations en_us.lang.php (Cebuano)

---
+++ translations/ceb_PH/include/SugarObjects/templates/file/language/ceb_PH.lang.php
@@ -0,0 +1,117 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    //module
+    'LBL_MODULE_NAME' => 'Mga dokumento',
+    'LBL_MODULE_TITLE' => 'Mga Dokumento: Home',
+    'LNK_NEW_DOCUMENT' => 'Maghimo og Dokumento',
+    'LNK_DOCUMENT_LIST' => 'Listahan sa mga Dokumento',
+    'LBL_SEARCH_FORM_TITLE' => 'Pagpangita sa Dokumento',
+    //vardef labels
+    'LBL_NAME' => 'Pangan sa Dokumento',
+    'LBL_DESCRIPTION' => 'Deskripsyon',
+    'LBL_ASSIGNED_TO' => 'Gi-assign sa:',
+    'LBL_CATEGORY' => 'Kategorya',
+    'LBL_SUBCATEGORY' => 'Sub nga Kategorya',
+    'LBL_STATUS' => 'Stado',
+    'LBL_IS_TEMPLATE' => 'Kay usa ka Template',
+    'LBL_TEMPLATE_TYPE' => 'Matang sa Dokumento',
+    'LBL_REVISION_NAME' => 'Numero sa pag-usab',
+    'LBL_MIME' => 'Matang sa Mime',
+    'LBL_REVISION' => 'Pag-usab',
+    'LBL_DOCUMENT' => 'Naay kalabotan nga Dokumento',
+    'LBL_LATEST_REVISION' => 'Kinaulahiang pag-usab',
+    'LBL_CHANGE_LOG' => 'Usbon ang log',
+    'LBL_ACTIVE_DATE' => 'Petsa sa pagpublikar',
+    'LBL_EXPIRATION_DATE' => 'Petsa nga ma-ekspire',
+    'LBL_FILE_EXTENSION' => 'Ekstensiyon nga file',
+
+    'LBL_CAT_OR_SUBCAT_UNSPEC' => 'Wala hingali',
+    //quick search
+    'LBL_NEW_FORM_TITLE' => 'Bag-ong Dokumento',
+    //document edit and detail view
+    'LBL_DOC_NAME' => 'Pangan sa Dokumento:',
+    'LBL_FILENAME' => 'Pangalan sa File:',
+    'LBL_FILE_UPLOAD' => 'File:',
+    'LBL_DOC_VERSION' => 'Pag-usab:',
+    'LBL_CATEGORY_VALUE' => 'Kategorya:',
+    'LBL_SUBCATEGORY_VALUE' => 'Sub nga Kategorya:',
+    'LBL_DOC_STATUS' => 'Estado:',
+    'LBL_DET_TEMPLATE_TYPE' => 'Matang sa Dokumento:',
+    'LBL_DOC_DESCRIPTION' => 'Deskripsyon:',
+    'LBL_DOC_ACTIVE_DATE' => 'Petsa sa Pagpublikar:',
+    'LBL_DOC_EXP_DATE' => 'Petsa sa kataposan:',
+
+    //document list view.
+    'LBL_LIST_FORM_TITLE' => 'Listahan sa Dokumento',
+    'LBL_LIST_DOCUMENT' => 'Dokumento',
+    'LBL_LIST_CATEGORY' => 'Kategorya',
+    'LBL_LIST_SUBCATEGORY' => 'Sub nga Kategorya',
+    'LBL_LIST_REVISION' => 'Pag-usab',
+    'LBL_LIST_LAST_REV_CREATOR' => 'Gipublikar ni',
+    'LBL_LIST_LAST_REV_DATE' => 'Petsa sa pag-usab',
+    'LBL_LIST_VIEW_DOCUMENT' => 'Tan-awa',
+    'LBL_LIST_ACTIVE_DATE' => 'Petsa sa pagpublikar',
+    'LBL_LIST_EXP_DATE' => 'Petsa nga ma-ekspire',
+    'LBL_LIST_STATUS' => 'Stado',
+
+    //document search form.
+    'LBL_SF_CATEGORY' => 'Kategorya:',
+    'LBL_SF_SUBCATEGORY' => 'Sub nga Kategorya:',
+
+    'DEF_CREATE_LOG' => 'Nabuhat na ang Dokumento',
+
+    //error messages
+    'ERR_DOC_NAME' => 'Pangan sa Dokumento',
+    'ERR_DOC_ACTIVE_DATE' => 'Petsa sa pagpublikar',
+    'ERR_FILENAME' => 'Pangan sa File',
+
+    //sub-panel vardefs.
+    'LBL_LIST_DOCUMENT_NAME' => 'Pangan sa Dokumento',
+
+    'LBL_EDIT_BUTTON' => 'Usbon ',
+    'LBL_REMOVE' => 'Tangtangon',
+
+);
