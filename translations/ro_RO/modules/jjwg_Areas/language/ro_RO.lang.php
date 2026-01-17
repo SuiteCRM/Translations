3cc2ce67da4a8c3fb2d595b4de30f98a3ff049b4
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 02:22:21 +0000

    New translations en_us.lang.php (Romanian)

---
+++ translations/ro_RO/modules/jjwg_Areas/language/ro_RO.lang.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings['LBL_ASSIGNED_TO_ID'] = 'Id atribuit utilizatorului';
+$mod_strings['LBL_ASSIGNED_TO_NAME'] = 'Utilizator';
+$mod_strings['LBL_ID'] = 'ID-ul';
+$mod_strings['LBL_DATE_ENTERED'] = 'Data creării';
+$mod_strings['LBL_DATE_MODIFIED'] = 'Data modificării';
+$mod_strings['LBL_MODIFIED'] = 'Modificat de';
+$mod_strings['LBL_MODIFIED_NAME'] = 'Nume modificant';
+$mod_strings['LBL_CREATED'] = 'Creat De';
+$mod_strings['LBL_DESCRIPTION'] = 'Descriere';
+$mod_strings['LBL_DELETED'] = 'Şters';
+$mod_strings['LBL_NAME'] = 'Nume';
+$mod_strings['LBL_CREATED_USER'] = 'Creat de Utilizator';
+$mod_strings['LBL_MODIFIED_USER'] = 'Modificat de utilizator';
+$mod_strings['LBL_LIST_NAME'] = 'Nume';
+$mod_strings['LBL_LIST_FORM_TITLE'] = 'Listă de zone';
+$mod_strings['LBL_MODULE_NAME'] = 'Zone';
+$mod_strings['LBL_MODULE_TITLE'] = 'Zone';
+$mod_strings['LBL_HOMEPAGE_TITLE'] = 'Zonele Mele';
+$mod_strings['LNK_NEW_RECORD'] = 'Creare zone';
+$mod_strings['LNK_LIST'] = 'Vezi zonele';
+$mod_strings['LBL_SEARCH_FORM_TITLE'] = 'Zone de Căutare';
+$mod_strings['LBL_HISTORY_SUBPANEL_TITLE'] = 'Vizualizare Istoric';
+$mod_strings['LBL_ACTIVITIES_SUBPANEL_TITLE'] = 'Activităţi';
+$mod_strings['LBL_NEW_FORM_TITLE'] = 'Zone noi';
+$mod_strings['LBL_CITY'] = 'Oraș';
+$mod_strings['LBL_STATE'] = 'Stat';
+$mod_strings['LBL_COUNTRY'] = 'Ţară';
+$mod_strings['LBL_COORDINATES'] = 'Coordonate';
+$mod_strings['LBL_LIST_ASSIGNED_USER'] = 'Utilizator';
+$mod_strings['LBL_AREA_MAP'] = 'Harta zonei';
+
+$mod_strings['LBL_AREA_EDIT_TITLE'] = 'Instrucțiuni de creare a zonei:';
+$mod_strings['LBL_AREA_EDIT_DESC_1'] = 'Faceți clic stânga pe hartă, într-o mișcare în sensul acelor de ceasornic, pentru a crea puncte de marcaj pentru zonă.';
+$mod_strings['LBL_AREA_EDIT_DESC_2'] = 'Faceți clic pe primul punct de marcaj pentru a închide zona poligonului.';
+$mod_strings['LBL_AREA_EDIT_RESET'] = 'Resetează';
+$mod_strings['LBL_AREA_EDIT_USE_AREA_COORDINATES'] = 'Folosește coordonatele zonei';
+$mod_strings['LBL_AREA_EDIT_COORDINATE_RESULTS'] = 'Rezultate coordonate (lng,lat,elv):';
+$mod_strings['LBL_JJWG_MAPS_JJWG_AREAS_FROM_JJWG_MAPS_TITLE'] = 'Hărți';
