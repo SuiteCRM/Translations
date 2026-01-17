ccc272149193f8ba589b7d9ac85912a5ff49febe
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 02:59:19 +0000

    New translations en_us.lang.php (Japanese)

---
+++ translations/ja_JP/modules/SurveyQuestionResponses/language/ja_JP.lang.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID'                                                                 => 'アサインされたユーザーID',
+    'LBL_ASSIGNED_TO_NAME'                                                               => '割り当て先',
+    'LBL_SECURITYGROUPS'                                                                 => 'セキュリティグループ',
+    'LBL_SECURITYGROUPS_SUBPANEL_TITLE'                                                  => 'セキュリティグループ',
+    'LBL_ID'                                                                             => 'ID',
+    'LBL_DATE_ENTERED'                                                                   => '作成日',
+    'LBL_DATE_MODIFIED'                                                                  => '更新日',
+    'LBL_MODIFIED'                                                                       => '更新者',
+    'LBL_MODIFIED_ID'                                                                    => '更新ID',
+    'LBL_MODIFIED_NAME'                                                                  => '更新者',
+    'LBL_CREATED'                                                                        => '作成者',
+    'LBL_CREATED_ID'                                                                     => '生成ID',
+    'LBL_DESCRIPTION'                                                                    => '詳細',
+    'LBL_ANSWER'                                                                         => '回答',
+    'LBL_ANSWER_BOOL'                                                                    => 'ブール型の回答',
+    'LBL_ANSWER_DATETIME'                                                                => '日時型の回答',
+    'LBL_DELETED'                                                                        => '削除済み',
+    'LBL_NAME'                                                                           => '名前',
+    'LBL_CREATED_USER'                                                                   => 'ユーザーが作成',
+    'LBL_MODIFIED_USER'                                                                  => 'ユーザーが更新',
+    'LBL_LIST_NAME'                                                                      => '名前',
+    'LBL_EDIT_BUTTON'                                                                    => '編集',
+    'LBL_REMOVE'                                                                         => '削除',
+    'LBL_LIST_FORM_TITLE'                                                                => '調査質問の回答一覧',
+    'LBL_MODULE_NAME'                                                                    => '調査質問の回答',
+    'LBL_MODULE_TITLE'                                                                   => '調査質問の回答',
+    'LBL_HOMEPAGE_TITLE'                                                                 => '私の調査質問の回答',
+    'LNK_NEW_RECORD'                                                                     => '調査質問の回答の作成',
+    'LNK_LIST'                                                                           => '調査質問の回答の表示',
+    'LNK_IMPORT_SURVEYQUESTIONRESPONSES'                                                 => '調査質問の回答のインポート',
+    'LBL_SEARCH_FORM_TITLE'                                                              => '調査質問の回答の検索',
+    'LBL_HISTORY_SUBPANEL_TITLE'                                                         => '履歴',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE'                                                      => '活動',
+    'LBL_SURVEYQUESTIONRESPONSES_SUBPANEL_TITLE'                                         => '調査質問の回答',
+    'LBL_NEW_FORM_TITLE'                                                                 => '新しい調査質問',
+    'LBL_SURVEYRESPONSES_SURVEYQUESTIONRESPONSES_FROM_SURVEYRESPONSES_TITLE'             => '調査の回答',
+    'LBL_SURVEYQUESTIONS_SURVEYQUESTIONRESPONSES_FROM_SURVEYQUESTIONS_TITLE'             => '調査質問',
+    'LBL_SURVEYQUESTIONOPTIONS_SURVEYQUESTIONRESPONSES_FROM_SURVEYQUESTIONOPTIONS_TITLE' => '調査質問の選択肢',
+);
