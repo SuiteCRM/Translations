0f35869b62baad1b3924dddcc355f5a108f914c8
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 04:16:40 +0000

    New translations en_us.lang.php (Bosnian)

---
+++ translations/bs_BA/modules/AOS_Products_Quotes/language/bs_BA.lang.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID' => 'Id dodijeljenog korisnika',
+    'LBL_ASSIGNED_TO_NAME' => 'Dodijeljeno korisniku',
+    'LBL_ID' => 'ID',
+    'LBL_DATE_ENTERED' => 'Datum kreiranja',
+    'LBL_DATE_MODIFIED' => 'Datum izmjene',
+    'LBL_MODIFIED' => 'Modifikovao-la',
+    'LBL_MODIFIED_NAME' => 'Izmjenjeno od strane korisnika',
+    'LBL_CREATED' => 'Kreirao',
+    'LBL_DESCRIPTION' => 'Bilješka',
+    'LBL_DELETED' => 'Izbrisano',
+    'LBL_NAME' => 'Ime',
+    'LBL_CREATED_USER' => 'Kreirao Korisnik',
+    'LBL_MODIFIED_USER' => 'Izmijenio Korisnik',
+    'LBL_LIST_FORM_TITLE' => 'Lista ponuda proizvoda',
+    'LBL_MODULE_NAME' => 'Stavke',
+    'LBL_MODULE_TITLE' => 'Ponude proizvoda: Početno',
+    'LBL_HOMEPAGE_TITLE' => 'Moje ponude proizvoda',
+    'LNK_NEW_RECORD' => 'Kreiraj ponudu proizvoda',
+    'LNK_LIST' => 'Ponude proizvoda',
+    'LBL_SEARCH_FORM_TITLE' => 'Pretražite ponude proizvoda',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Pregled istorije',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Aktivnosti',
+    'LBL_NEW_FORM_TITLE' => 'Nova ponuda proizvoda',
+    'LBL_PRODUCT_NAME' => 'Ime',
+    'LBL_PRODUCT_QTY' => 'Količina',
+    'LBL_PRODUCT_COST_PRICE' => 'Cijena troška',
+    'LBL_PRODUCT_LIST_PRICE' => 'Cijena',
+    'LBL_PRODUCT_UNIT_PRICE' => 'Pojedinačna cijena',
+    'LBL_PRODUCT_DISCOUNT' => 'Popust',
+    'LBL_PRODUCT_DISCOUNT_AMOUNT' => 'Iznos popusta',
+    'LBL_PART_NUMBER' => 'Broj dijela',
+    'LBL_PRODUCT_DESCRIPTION' => 'Opis',
+    'LBL_DISCOUNT' => 'Vrsta popusta',
+    'LBL_VAT_AMT' => 'Iznos PDV',
+    'LBL_VAT' => 'PDV',
+    'LBL_PRODUCT_TOTAL_PRICE' => 'Ukupna cijena',
+    'LBL_PRODUCT_NOTE' => 'Bilješka',
+    'Quote' => '',
+    'LBL_FLEX_RELATE' => 'Povezano sa',
+    'LBL_PRODUCT' => 'Proizvod',
+
+    'LBL_SERVICE_MODULE_NAME' => 'Usluge',
+    'LBL_LIST_NUM' => 'Broj',
+    'LBL_PARENT_ID' => 'Matični ID',
+    'LBL_GROUP_NAME' => 'Grupa',
+    'LBL_PRODUCT_COST_PRICE_USDOLLAR' => 'Trošak (zadana valuta)',
+    'LBL_PRODUCT_LIST_PRICE_USDOLLAR' => 'Cijena (zadana valuta)',
+    'LBL_PRODUCT_UNIT_PRICE_USDOLLAR' => 'Jedinična cijena (zadana valuta)',
+    'LBL_PRODUCT_TOTAL_PRICE_USDOLLAR' => 'Ukupna cijena (zadana valuta)',
+    'LBL_PRODUCT_DISCOUNT_USDOLLAR' => 'Popust (Zadana valuta)',
+    'LBL_PRODUCT_DISCOUNT_AMOUNT_USDOLLAR' => 'Iznos popusta (zadana valuta)',
+    'LBL_VAT_AMT_USDOLLAR' => 'Iznos poreza (zadana valuta)',
+    'LBL_PRODUCTS_SERVICES' => 'Proizvod / Usluga',
+    'LBL_PRODUCT_ID' => 'ID broj proizvoda',
+
+    'LBL_AOS_CONTRACTS' => 'Ugovori',
+    'LBL_AOS_INVOICES' => 'Fakture',
+    'LBL_AOS_PRODUCTS' => 'Proizvodi',
+    'LBL_AOS_QUOTES' => 'Ponude',
+);
