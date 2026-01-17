9a08973f35571302f15849e772c61a80c4ca4855
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-17 03:45:10 +0000

    New translations en_us.lang.php (Persian)

---
+++ translations/fa_IR/modules/ProspectLists/language/fa_IR.lang.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_MODULE_NAME' => 'لیست‌های هدف',
+    'LBL_MODULE_ID' => 'لیست‌های هدف',
+    'LBL_MODULE_TITLE' => 'لیست‌های اهداف: صفحه اصلی',
+    'LBL_SEARCH_FORM_TITLE' => 'جستجوی لیست‌های اهداف',
+    'LBL_LIST_FORM_TITLE' => 'لیست‌های هدف',
+    'LBL_PROSPECT_LIST_NAME' => 'لیست اهداف',
+    'LBL_NAME' => 'نام',
+    'LBL_ENTRIES' => 'Total Entries',
+    'LBL_LIST_PROSPECT_LIST_NAME' => 'لیست هدف',
+    'LBL_LIST_ENTRIES' => 'اهداف در لیست',
+    'LBL_LIST_DESCRIPTION' => 'توضیحات',
+    'LBL_LIST_TYPE_NO' => 'نوع',
+    'LBL_LIST_END_DATE' => 'تاریخ پایان',
+    'LBL_DATE_ENTERED' => 'تاریخ ایجاد',
+    'LBL_MARKETING_ID' => 'شناسه بازاریابی',
+    'LBL_DATE_MODIFIED' => 'تاریخ ویرایش',
+    'LBL_MODIFIED' => 'ویرایش کننده',
+    'LBL_CREATED' => 'ایجاد کننده',
+    'LBL_ASSIGNED_TO' => 'اختصاص داده شده به',
+    'LBL_DESCRIPTION' => 'توضیحات',
+    'LNK_NEW_CAMPAIGN' => 'ایجاد کمپین',
+    'LNK_CAMPAIGN_LIST' => 'کمپین‌ها',
+    'LNK_NEW_PROSPECT_LIST' => 'ایجاد لیست هدف',
+    'LNK_PROSPECT_LIST_LIST' => 'مشاهده لیست هدف',
+    'LBL_MODIFIED_BY' => 'ویرایش کننده',
+    'LBL_CREATED_BY' => 'ایجاد کننده',
+    'LNK_NEW_PROSPECT' => 'ایجاد هدف',
+    'LNK_PROSPECT_LIST' => 'اهداف',
+
+    'LBL_CONTACTS_SUBPANEL_TITLE' => 'مخاطب‌ها',
+    'LBL_LEADS_SUBPANEL_TITLE' => 'سرنخ‌ها',
+    'LBL_PROSPECTS_SUBPANEL_TITLE' => 'اهداف',
+    'LBL_ACCOUNTS_SUBPANEL_TITLE' => 'حساب ها',
+    'LBL_COPY_PREFIX' => 'کپی از ',
+    'LBL_USERS_SUBPANEL_TITLE' => 'کاربرها',
+    'LBL_TYPE' => 'نوع',
+    'LBL_LIST_TYPE' => 'نوع',
+    'LBL_LIST_TYPE_LIST_NAME' => 'نوع',
+    'LBL_NEW_FORM_TITLE' => 'لیست هدف جدید',
+    'LBL_MARKETING_NAME' => 'نام بازاریابی',
+    'LBL_DOMAIN_NAME' => 'نام دامنه',
+    'LBL_DOMAIN' => 'No emails to Domain',
+    'LBL_LIST_PROSPECTLIST_NAME' => 'نام',
+
+    'LBL_EMAIL_MARKETING' => 'بازاریابی ایمیلی',
+
+    'LBL_ASCENDING' => 'صعودی',
+    'LBL_DESCENDING' => 'نزولی',
+);
