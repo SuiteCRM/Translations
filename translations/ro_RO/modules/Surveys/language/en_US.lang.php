8fef42d6be145bfd7a572389db072343cb9da9d1
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-26 20:49:30 +0000

    New translations en_us.lang.php (Romanian)

---
+++ translations/ro_RO/modules/Surveys/language/en_US.lang.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID'                                     => 'Id atribuit utilizatorului',
+    'LBL_ADD_OPTION'                                         => 'Adaugă opțiune nouă',
+    'LBL_ASSIGNED_TO_NAME'                                   => 'Atribuit lui',
+    'LBL_SECURITYGROUPS'                                     => 'Grupuri Securitate',
+    'LBL_SECURITYGROUPS_SUBPANEL_TITLE'                      => 'Grupuri Securitate',
+    'LBL_ID'                                                 => 'ID-ul',
+    'LBL_DATE_ENTERED'                                       => 'Data creării',
+    'LBL_DATE_MODIFIED'                                      => 'Data modificării',
+    'LBL_MODIFIED'                                           => 'Modificat de',
+    'LBL_MODIFIED_ID'                                        => 'Id modificant',
+    'LBL_MODIFIED_NAME'                                      => 'Nume modificant',
+    'LBL_CREATED'                                            => 'Creat De',
+    'LBL_CREATED_ID'                                         => 'Id creatorului',
+    'LBL_DESCRIPTION'                                        => 'Descriere',
+    'LBL_DELETED'                                            => 'Şters',
+    'LBL_NAME'                                               => 'Nume',
+    'LBL_CREATED_USER'                                       => 'Creat de Utilizator',
+    'LBL_MODIFIED_USER'                                      => 'Modificat de utilizator',
+    'LBL_LIST_NAME'                                          => 'Nume',
+    'LBL_EDIT_BUTTON'                                        => 'Editați',
+    'LBL_REMOVE'                                             => 'Înlăturați',
+    'LBL_LIST_FORM_TITLE'                                    => 'Lista de chestionare',
+    'LBL_MODULE_NAME'                                        => 'Sondaje',
+    'LBL_MODULE_TITLE'                                       => 'Sondaje',
+    'LBL_HOMEPAGE_TITLE'                                     => 'Chestionarele mele',
+    'LNK_NEW_RECORD'                                         => 'Creare chestionare',
+    'LNK_LIST'                                               => 'Vezi chestionare',
+    'LNK_IMPORT_SURVEYS'                                     => 'Importă chestionare',
+    'LBL_SEARCH_FORM_TITLE'                                  => 'Căutare chestionare',
+    'LBL_HISTORY_SUBPANEL_TITLE'                             => 'Vizualizare Istoric',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE'                          => 'Activităţi',
+    'LBL_SURVEYS_SUBPANEL_TITLE'                             => 'Sondaje',
+    'LBL_NEW_FORM_TITLE'                                     => 'Chestionare noi',
+    'LBL_STATUS'                                             => 'Status',
+    'LBL_SURVEY_QUESTIONS_DISPLAY'                           => 'Întrebări',
+    'LBL_SURVEY_URL_DISPLAY'                                 => 'URL',
+    'LBL_HAPPINESS_QUESTION'                                 => 'Întrebare despre fericire',
+    'LBL_CANT_EDIT_RESPONDED'                                => 'Întrebări chestionar cu răspunsuri nu pot fi editate',
+    'LBL_VIEW_SURVEY_REPORTS'                                => 'Vezi rapoarte sondaj',
+    'LBL_CHECKED'                                            => 'Verificat',
+    'LBL_UNCHECKED'                                          => 'Nebifat',
+    'LBL_RESPONSE_ANSWER'                                    => 'Răspuns',
+    'LBL_RESPONSE_CONTACT'                                   => 'Contact',
+    'LBL_RESPONSE_TIME'                                      => 'Dată',
+    'LBL_UNKNOWN_CONTACT'                                    => 'Necunoscut',
+    'LBL_RESPONSE_COUNT'                                     => 'Număra',
+    'LBL_SUBMIT_TEXT'                                        => 'Trimite text',
+    'LBL_SATISFIED_TEXT'                                     => 'Text despre satisfacție',
+    'LBL_NEITHER_TEXT'                                       => 'Nici un text',
+    'LBL_DISSATISFIED_TEXT'                                  => 'Text nemulțumit',
+    'LBL_SURVEYS_SURVEYQUESTIONS_FROM_SURVEYQUESTIONS_TITLE' => 'Întrebări la sondaj',
+    'LBL_SURVEYS_SURVEYRESPONSES_FROM_SURVEYRESPONSES_TITLE' => 'Răspunsuri la sondaj',
+    'LBL_SHOW_RESPONSES' => 'Arată răspunsurile',
+    'LBL_HIDE_RESPONSES' => 'Ascunde răspunsurile',
+    'LBL_SURVEY_QUESTION' => 'Întrebare',
+    'LBL_SURVEY_TEXT' => 'Text',
+    'LBL_SURVEY_TYPE' => 'Tip',
+    'LBL_SURVEY_ACTIONS' => 'Acțiuni',
+    'LBL_SURVEY_NEW_QUESTION' => 'Întrebare nouă',
+    'LBL_SURVEY_RESPONSES' => 'Întrebare nouă',
+    'LBL_SURVEYS_SENT' => 'Sondaje trimise:',
+    'LBL_SURVEY_DISTINCT' => 'Sondaje distincte trimise:',
+    'LBL_DRAFT' => 'Schiță',
+    'LBL_SUBMIT' => 'Trimite',
+    'LBL_STARS' => 'Stele',
+    'LBL_SURVEY_CLOSE_RESPONSE' => 'Vă mulțumim pentru interes, dar acest sondaj a fost închis.',
+    'LBL_SURVEY_THANKS' => 'Thanks for completing this survey.',
+);
