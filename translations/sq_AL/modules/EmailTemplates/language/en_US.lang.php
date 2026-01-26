8e046e557ae71503699e37fe3f05b3c511d14be4
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-26 19:49:22 +0000

    New translations en_us.lang.php (Albanian)

---
+++ translations/sq_AL/modules/EmailTemplates/language/en_US.lang.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ADD_DOCUMENT' => 'Shto SuiteCRM dokumentacion',
+    'LBL_ADD_FILE' => 'Shto dosje',
+    'LBL_ATTACHMENTS' => 'Bashkëngjitjet',
+    'LBL_BODY' => 'Body:',
+    'LBL_CLOSE' => 'Mbyll:',
+    'LBL_DESCRIPTION' => 'Përshkrim',
+    'LBL_EDIT_ALT_TEXT' => 'Ndrysho tekst të qartë',
+    'LBL_EMAIL_ATTACHMENT' => 'bashkëngjitje të mailit',
+    'LBL_HIDE_ALT_TEXT' => 'Fsheh Tekst të plotë',
+    'LBL_HTML_BODY' => 'HTML trupi',
+    'LBL_INSERT_VARIABLE' => 'Shto një ndryshore:',
+    'LBL_INSERT_URL_REF' => 'Shto URL Referencë',
+    'LBL_INSERT_TRACKER_URL' => 'Shto ULR gjurmimi',
+    'LBL_INSERT' => 'Insert',
+    'LBL_LIST_DATE_MODIFIED' => 'Ndryshimi i fundit',
+    'LBL_LIST_DESCRIPTION' => 'Përshkrim',
+    'LBL_LIST_FORM_TITLE' => 'Lista e shabllonave të emailit',
+    'LBL_LIST_NAME' => 'Emri',
+    'LBL_MODULE_NAME' => 'Shabllona të emailit',
+    'LBL_MODULE_TITLE' => 'Shabllona të emailit: ballina',
+    'LBL_NAME' => 'Emri',
+    'LBL_NEW_FORM_TITLE' => 'Krijo shabllona të emailit',
+    'LBL_PUBLISH' => 'Publiko',
+    'LBL_RELATED_TO' => 'Në lidhje me:',
+    'LBL_SEARCH_FORM_TITLE' => 'Kërkim i shabllona të emailit',
+    'LBL_SHOW_ALT_TEXT' => 'Trego tekst të qartë',
+    'LBL_SUBJECT' => 'Subjekti',
+    'LBL_SUITE_DOCUMENT' => 'Dokumentacion',
+    'LBL_UPLOAD_FILE' => 'Upload File',
+    'LBL_TEXT_BODY' => 'Trupi tekstual',
+    'LBL_USERS' => 'përdoruesit',
+
+    'LNK_EMAIL_TEMPLATE_LIST' => 'Shiko shabllonat e emailit',
+    'LNK_IMPORT_NOTES' => 'Shënime të rëndësishme',
+    'LNK_NEW_EMAIL_TEMPLATE' => 'Krijo shabllona të emailit',
+    'LNK_NEW_EMAIL' => 'Emaili i arkivuar',
+    'LNK_NEW_SEND_EMAIL' => 'Ndërto email',
+    'LNK_SENT_EMAIL_LIST' => 'Dërgo email',
+    'LNK_VIEW_CALENDAR' => 'Sot',
+    // for Inbox
+    'LBL_NEW' => 'I ri',
+    'LNK_MY_DRAFTS' => 'Skicat e mia',
+    'LNK_MY_INBOX' => 'Emailet e mia',
+    'LBL_TEXT_ONLY' => 'Vetëm tekst',
+    'LBL_SEND_AS_TEXT' => 'Dërgo vetëm tekstin',
+    'LBL_ACCOUNT' => 'llogaritë',
+    'LBL_FROM_NAME' => 'Nga emri',
+    'LBL_PLAIN_TEXT' => 'Tekst i qartë',
+    'LBL_CREATED_BY' => 'Krijuar nga',
+    'LBL_PUBLISHED' => 'publikuar',
+    'LNK_VIEW_MY_INBOX' => 'Shiko emailet e mia',
+    'LBL_ASSIGNED_TO_ID' => 'Drejtuar deri te',
+    'LBL_EDIT_LAYOUT' => 'Ndrysho formatin' /*for 508 compliance fix*/,
+    'LBL_SELECT' => 'Selekto' /*for 508 compliance fix*/,
+    'LBL_ID_FF_CLEAR' => 'Fshi' /*for 508 compliance fix*/,
+    'LBL_TYPE' => 'Lloji',
+    'LBL_WIDTH' => 'Width Default',
+    'LNK_IMPORT_CAMPAIGNS' => 'Import Campaign',
+);
