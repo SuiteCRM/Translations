999a4b28711d918da55f36e8e324b6ccfc745b3a
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-26 19:49:40 +0000

    New translations en_us.lang.php (Estonian)

---
+++ translations/et_EE/modules/EmailTemplates/language/en_US.lang.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ADD_DOCUMENT' => 'Lisa Dokument',
+    'LBL_ADD_FILE' => 'Lisa fail',
+    'LBL_ATTACHMENTS' => 'Manused',
+    'LBL_BODY' => 'Sisu: ',
+    'LBL_CLOSE' => 'Sulge:',
+    'LBL_DESCRIPTION' => 'Kirjeldus',
+    'LBL_EDIT_ALT_TEXT' => 'Redigeeri lihttekst',
+    'LBL_EMAIL_ATTACHMENT' => 'E-kirja manus:',
+    'LBL_HIDE_ALT_TEXT' => 'Peida lihttekst',
+    'LBL_HTML_BODY' => 'HTML kehatekst:',
+    'LBL_INSERT_VARIABLE' => 'Sisesta muutuja:',
+    'LBL_INSERT_URL_REF' => 'Sisesta URL viide',
+    'LBL_INSERT_TRACKER_URL' => 'Sisesta trackeri URL:',
+    'LBL_INSERT' => 'Sisesta',
+    'LBL_LIST_DATE_MODIFIED' => 'Viimati muudetud:',
+    'LBL_LIST_DESCRIPTION' => 'Kirjeldus',
+    'LBL_LIST_FORM_TITLE' => 'E-posti mallide loend',
+    'LBL_LIST_NAME' => 'Nimi',
+    'LBL_MODULE_NAME' => 'E-kirja mallid',
+    'LBL_MODULE_TITLE' => 'E-kirja mallid: Avaleht',
+    'LBL_NAME' => 'Nimi:',
+    'LBL_NEW_FORM_TITLE' => 'Loo e-kirja mall',
+    'LBL_PUBLISH' => 'Avalda:',
+    'LBL_RELATED_TO' => 'Seotud:',
+    'LBL_SEARCH_FORM_TITLE' => 'E-kirja mallide otsing',
+    'LBL_SHOW_ALT_TEXT' => 'Näita lihtteksti',
+    'LBL_SUBJECT' => 'Teema:',
+    'LBL_SUITE_DOCUMENT' => 'Dokument',
+    'LBL_UPLOAD_FILE' => 'Lae fail üles',
+    'LBL_TEXT_BODY' => 'Kehatekst:',
+    'LBL_USERS' => 'Kasutajad',
+
+    'LNK_EMAIL_TEMPLATE_LIST' => 'Vaata e-kirja malle',
+    'LNK_IMPORT_NOTES' => 'Impordi märkused',
+    'LNK_NEW_EMAIL_TEMPLATE' => 'Loo e-kirja mall',
+    'LNK_NEW_EMAIL' => 'Arhiveeri e-kiri',
+    'LNK_NEW_SEND_EMAIL' => 'Koosta e-kiri',
+    'LNK_SENT_EMAIL_LIST' => 'Saada e-kirjad',
+    'LNK_VIEW_CALENDAR' => 'Täna',
+    // for Inbox
+    'LBL_NEW' => 'Uus',
+    'LNK_MY_DRAFTS' => 'Minu mustandid',
+    'LNK_MY_INBOX' => 'Minu e-kiri',
+    'LBL_TEXT_ONLY' => 'Ainult tekst',
+    'LBL_SEND_AS_TEXT' => 'Saada vaid tekst',
+    'LBL_ACCOUNT' => 'Ettevõtted',
+    'LBL_FROM_NAME' => 'Nime alt:',
+    'LBL_PLAIN_TEXT' => 'Lihttekst',
+    'LBL_CREATED_BY' => 'Loodud',
+    'LBL_PUBLISHED' => 'Avaldatud',
+    'LNK_VIEW_MY_INBOX' => 'Vaata Minu e-kirju',
+    'LBL_ASSIGNED_TO_ID' => 'Vastutaja',
+    'LBL_EDIT_LAYOUT' => 'Muuda paigutust' /*for 508 compliance fix*/,
+    'LBL_SELECT' => 'Vali' /*for 508 compliance fix*/,
+    'LBL_ID_FF_CLEAR' => 'Tühjenda' /*for 508 compliance fix*/,
+    'LBL_TYPE' => 'Tüüp:',
+    'LBL_WIDTH' => 'Width Default',
+    'LNK_IMPORT_CAMPAIGNS' => 'Impordi kampaania',
+);
