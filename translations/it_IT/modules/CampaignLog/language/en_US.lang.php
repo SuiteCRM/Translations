8bb369edf00eb1b532b94ec32199fc97a5e2d0aa
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-26 19:22:30 +0000

    New translations en_us.lang.php (Italian)

---
+++ translations/it_IT/modules/CampaignLog/language/en_US.lang.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_LIST_ID' => 'ID Lista Obiettivo',
+    'LBL_ID' => 'ID',
+    'LBL_TARGET_TRACKER_KEY' => 'Key Tracker Obiettivo',
+    'LBL_TARGET_ID' => 'ID Obiettivo',
+    'LBL_TARGET_TYPE' => 'Tipo Obiettivo',
+    'LBL_ACTIVITY_TYPE' => 'Tipo Attività',
+    'LBL_ACTIVITY_DATE' => 'Data Attività',
+    'LBL_RELATED_ID' => 'ID Correlato',
+    'LBL_RELATED_TYPE' => 'Tipo Relazione',
+    'LBL_DELETED' => 'Eliminato',
+    'LBL_MODULE_NAME' => 'Log Campagna',
+    'LBL_LIST_RECIPIENT_EMAIL' => 'Email Destinatario',
+    'LBL_LIST_RECIPIENT_NAME' => 'Nome Destinatario',
+    'LBL_ARCHIVED' => 'Archiviato',
+    'LBL_HITS' => 'Visite',
+
+    'LBL_CAMPAIGN_NAME' => 'Nome:',
+    'LBL_CAMPAIGN' => 'Campagna:',
+    'LBL_NAME' => 'Nome:',
+    'LBL_INVITEE' => 'Contatti',
+    'LBL_LIST_CAMPAIGN_NAME' => 'Campagna',
+    'LBL_LIST_STATUS' => 'Stato',
+    'LBL_LIST_TYPE' => 'Tipo',
+    'LBL_LIST_END_DATE' => 'Data di Fine',
+    'LBL_DATE_ENTERED' => 'Data di inserimento',
+    'LBL_DATE_MODIFIED' => 'Data Modifica',
+    'LBL_MODIFIED' => 'Modificato da:',
+    'LBL_CREATED' => 'Creato da:',
+    'LBL_TEAM' => 'Gruppo:',
+    'LBL_ASSIGNED_TO' => 'Assegnato a:',
+    'LBL_CAMPAIGN_START_DATE' => 'Data di Inizio: ',
+    'LBL_CAMPAIGN_END_DATE' => 'Data di Fine: ',
+    'LBL_CAMPAIGN_STATUS' => 'Stato:',
+    'LBL_CAMPAIGN_BUDGET' => 'Budget:',
+    'LBL_CAMPAIGN_EXPECTED_COST' => 'Costo Previsto:',
+    'LBL_CAMPAIGN_ACTUAL_COST' => 'Costo Effettivo:',
+    'LBL_CAMPAIGN_EXPECTED_REVENUE' => 'Ritorno Atteso:',
+    'LBL_CAMPAIGN_TYPE' => 'Tipo:',
+    'LBL_CAMPAIGN_OBJECTIVE' => 'Obiettivo:',
+    'LBL_CAMPAIGN_CONTENT' => 'Descrizione:',
+    'LBL_CREATED_LEAD' => 'Lead creato',
+    'LBL_CREATED_CONTACT' => 'Contatto creato',
+    'LBL_CREATED_OPPORTUNITY' => 'Opportunità creata',
+    'LBL_TARGETED_USER' => 'Utente Designato',
+    'LBL_SENT_EMAIL' => 'Email Inviata',
+    'LBL_LIST_FORM_TITLE' => 'Campagne Obiettivo',
+    'LBL_LIST_ACTIVITY_DATE' => 'Data Attività',
+    'LBL_RELATED' => 'Correlato',
+    'LBL_MORE_INFO' => 'Ulteriori informazioni',
+
+    'LBL_CAMPAIGNS' => 'Campagne',
+    'LBL_LIST_MARKETING_NAME' => 'Marketing',
+
+    'LBL_MARKETING_ID' => 'Marketing',
+    'LBL_RELATED_NAME' => 'Nome associato',
+);
