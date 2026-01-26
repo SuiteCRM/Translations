24752ffe7c331f599b4c6e843ffb397c4064ae12
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-26 18:59:43 +0000

    New translations en_us.lang.php (Hungarian)

---
+++ translations/hu_HU/modules/AOS_PDF_Templates/language/en_US.lang.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID' => 'Felelős felhasználó ID',
+    'LBL_ASSIGNED_TO_NAME' => 'Felelős',
+    'LBL_ID' => 'ID',
+    'LBL_DATE_ENTERED' => 'Létrehozás dátuma',
+    'LBL_DATE_MODIFIED' => 'Módosítás dátuma',
+    'LBL_MODIFIED' => 'Módosító',
+    'LBL_MODIFIED_NAME' => 'Módosító név',
+    'LBL_CREATED' => 'Létrehozta',
+    'LBL_DESCRIPTION' => 'Tartalom',
+    'LBL_HEADER' => 'Fejléc',
+    'LBL_FOOTER' => 'Lábléc',
+    'LBL_DELETED' => 'Törölve',
+    'LBL_NAME' => 'Név',
+    'LBL_CREATED_USER' => 'Felhasználó által létrehozva',
+    'LBL_MODIFIED_USER' => 'Felhasználó által módosítva',
+    'LBL_LIST_FORM_TITLE' => 'PDF sablonok listája',
+    'LBL_MODULE_NAME' => 'PDF sablonok',
+    'LBL_MODULE_TITLE' => 'PDF sablonok: Alaphelyzet',
+    'LBL_HOMEPAGE_TITLE' => 'PDF sablonjaim',
+    'LNK_NEW_RECORD' => 'PDF sablon létrehozás',
+    'LNK_LIST' => 'PDF sablon megtekintés',
+    'LBL_SEARCH_FORM_TITLE' => 'PDF sablon keresés',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Előzmény Megtekintése',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Tevékenységek',
+    'LBL_NEW_FORM_TITLE' => 'Új PDF sablon',
+    'LBL_TYPE' => 'Típus',
+    'LBL_ACTIVE' => 'Aktív',
+    'LBL_BUTTON_INSERT' => 'Beszúr',
+    'LBL_WARNING_OVERWRITE' => 'Figyelem, ezzel felülírja jelenlegi munkáját',
+    'LBL_INSERT_FIELDS' => 'Mező beszúrás',
+
+    'LBL_SAMPLE' => 'Minta betöltés',
+    'LBL_PAGE' => 'Oldal',
+    'LBL_PREPARED_FOR' => 'Előkészítve',
+    'LBL_PREPARED_BY' => 'Készítette',
+    'LBL_QUOTE_SAMPLE' => 'Árajánlat minta',
+    'LBL_INVOICE_SAMPLE' => 'Számla minta',
+    'LBL_ACCOUNT_SAMPLE' => 'Felhasználó minta',
+    'LBL_CONTACT_SAMPLE' => 'Szerződés minta',
+    'LBL_LEAD_SAMPLE' => 'Lead minta',
+    'LBL_ANY_STREET' => 'Valamilyen utca',
+    'LBL_ANY_TOWN' => 'Valamilyen város',
+    'LBL_ANY_WHERE' => 'Valamilyen irányítószám',
+
+    'LBL_QUOTE_GROUP_SAMPLE' => 'Ajánlatcsomag minta',
+    'LBL_INVOICE_GROUP_SAMPLE' => 'Számlacsomag minta',
+    'LBL_MARGIN_LEFT' => 'Bal margó',
+    'LBL_MARGIN_RIGHT' => 'Jobb margó',
+    'LBL_MARGIN_TOP' => 'Felső margó',
+    'LBL_MARGIN_BOTTOM' => 'Alsó margó',
+    'LBL_MARGIN_HEADER' => 'Fejléc margó',
+    'LBL_MARGIN_FOOTER' => 'Lábléc margó',
+    'LBL_EDITVIEW_PANEL1' => 'Margó',
+    'LBL_DETAILVIEW_PANEL1' => 'Margó',
+    'LBL_PAGE_SIZE' => 'Oldal mérete',
+    'LBL_ORIENTATION' => 'Tájolás',
+);
