f5f6e6dabe50bd5f6679331060f5098c18d31e5d
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-26 18:54:27 +0000

    New translations en_us.lang.php (Norwegian Bokmal)

---
+++ translations/nb_NO/modules/AOR_Reports/language/en_US.lang.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID' => 'Tilordnet brukerID',
+    'LBL_ASSIGNED_TO_NAME' => 'Tildelt til',
+    'LBL_ID' => 'ID',
+    'LBL_DATE_ENTERED' => 'Dato opprettet',
+    'LBL_DATE_MODIFIED' => 'Dato endret',
+    'LBL_MODIFIED' => 'Endret av',
+    'LBL_MODIFIED_NAME' => 'Endret av navn',
+    'LBL_CREATED' => 'Opprettet av',
+    'LBL_DESCRIPTION' => 'Beskrivelse',
+    'LBL_DELETED' => 'Slettet',
+    'LBL_NAME' => 'Navn',
+    'LBL_CREATED_USER' => 'Opprettet av bruker',
+    'LBL_MODIFIED_USER' => 'Modifisert av bruker',
+    'LBL_LIST_NAME' => 'Navn',
+    'LBL_EDIT_BUTTON' => 'Rediger',
+    'LBL_REMOVE' => 'Fjern',
+    'LBL_LIST_FORM_TITLE' => 'Liste over Rapporter',
+    'LBL_MODULE_NAME' => 'Rapporter',
+    'LBL_MODULE_TITLE' => 'Rapporter',
+    'LBL_HOMEPAGE_TITLE' => 'Mine Rapporter',
+    'LNK_NEW_RECORD' => 'Opprett Rapporter',
+    'LNK_LIST' => 'Rapporter',
+    'LBL_SEARCH_FORM_TITLE' => 'Søk i Rapporter',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Se på historikk',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Aktiviteter',
+    'LBL_NEW_FORM_TITLE' => 'Nye Rapporter',
+    'LBL_REPORT_MODULE' => 'Rapportmodul',
+    'LBL_GRAPHS_PER_ROW' => 'Diagrammer per rad',
+    'LBL_FIELD_LINES' => 'Visningsfelter',
+    'LBL_ADD_FIELD' => 'Legg til felt',
+    'LBL_CONDITION_LINES' => 'Betingelser',
+    'LBL_ADD_CONDITION' => 'Legg til betingelse',
+    'LBL_EXPORT' => 'Eksporter',
+    'LBL_DOWNLOAD_PDF' => 'Last ned PDF',
+    'LBL_ADD_TO_PROSPECT_LIST' => 'Legg til liste over mål',
+    'LBL_AOR_MODULETREE_SUBPANEL_TITLE' => 'Modultre',
+    'LBL_AOR_FIELDS_SUBPANEL_TITLE' => 'Felt',
+    'LBL_AOR_CONDITIONS_SUBPANEL_TITLE' => 'Betingelser',
+    'LBL_TOTAL' => 'Totalt',
+    'LBL_AOR_CHARTS_SUBPANEL_TITLE' => 'Grafer',
+    'LBL_ADD_CHART' => 'Legg til diagram',
+    'LBL_ADD_PARENTHESIS' => 'Drop parenthesis',
+    'LBL_CHART_TITLE' => 'Tittel:',
+    'LBL_CHART_TYPE' => 'Type',
+    'LBL_CHART_X_FIELD' => 'X-akse',
+    'LBL_CHART_Y_FIELD' => 'Y-akse',
+    'LBL_AOR_REPORTS_DASHLET' => 'Rapporter',
+    'LBL_DASHLET_TITLE' => 'Tittel:',
+    'LBL_DASHLET_REPORT' => 'Rapport',
+    'LBL_DASHLET_CHOOSE_REPORT' => 'Velg en rapport',
+    'LBL_DASHLET_SAVE' => 'Lagre',
+    'LBL_DASHLET_CHARTS' => 'Grafer',
+    'LBL_DASHLET_ONLY_CHARTS' => 'Vis bare diagrammer',
+    'LBL_UPDATE_PARAMETERS' => 'Oppdater',
+    'LBL_PARAMETERS' => 'Parametre',
+    'LBL_TOOLTIP_DRAG_DROP_ELEMS' => 'Dra og slipp elementer i feltet eller området',
+    'LBL_MAIN_GROUPS' => 'Hovedgruppe:',
+    'LBL_CHAR_UNNAMED_DEFAULT_TITLE' => 'Navnløst diagram',
+    'LBL_REPORT' => 'Rapport',
+);
