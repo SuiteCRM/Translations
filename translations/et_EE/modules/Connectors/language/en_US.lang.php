59a2f755fd1ba73dd71441f1c80d3775bdece83d
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-26 19:35:56 +0000

    New translations en_us.lang.php (Estonian)

---
+++ translations/et_EE/modules/Connectors/language/en_US.lang.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+
+$mod_strings = array(
+
+    'LBL_ADMINISTRATION_MAIN' => 'Connector sätted',
+    'LBL_AVAILABLE' => 'Saadaval',
+    'LBL_BACK' => '< Tagasi',
+    'LBL_CONFIRM_CONTINUE_SAVE' => 'Mõned nõutud väljad on jäetud tühjaks. Jätkata muudatuste salvestamist?',
+    'LBL_CONNECTOR_FIELDS' => 'Connector väljadq',
+    'LBL_DATA' => 'Andmed',
+    'LBL_DEFAULT' => 'Vaikimisi',
+    'LBL_DISABLED' => 'Keelatud',
+    'LBL_ENABLED' => 'Lubatud',
+    'LBL_EXTERNAL' => 'Enable users to create external account records to this connector.',
+    'LBL_EXTERNAL_SET_PROPERTIES' => ' In order to use this connector, the properties should also be set in the Set Connector Properties settings page.',
+    'LBL_MERGE' => 'Mesti',
+    'LBL_MODIFY_DISPLAY_TITLE' => 'Luba Connectorid',
+    'LBL_MODIFY_DISPLAY_DESC' => 'Vali, millised mooduli on lubatud iga korrektori jaoks.',
+    'LBL_MODULE_FIELDS' => 'Mooduliväljad',
+    'LBL_MODIFY_MAPPING_TITLE' => 'Kaardista Connectori väljad',
+    'LBL_MODIFY_MAPPING_DESC' => 'Kaardista connectori väljad mooduli väljadeks, kui soovid kindlaks määrata, millist connectori infot saab vaadata ja mestida mooduli kirjetega.',
+    'LBL_MODIFY_PROPERTIES_TITLE' => 'Määra Connectori omadused',
+    'LBL_MODIFY_PROPERTIES_DESC' => 'Konfigureeri iga connectori omadused, kaasaarvatud URL-id ja API key-d.',
+    'LBL_MODIFY_SEARCH_TITLE' => 'Halda Connectori otsingut',
+    'LBL_MODIFY_SEARCH' => 'Otsi',
+    'LBL_MODIFY_SEARCH_DESC' => 'Vali connectorväli, mida kasutada iga mooduli andmete otsimiseks.',
+    'LBL_MODULE_NAME' => 'Connectorid',
+    'LBL_NO_PROPERTIES' => 'Selle connectori jaoks pole konfigureeritavaid omadusi.',
+    'LBL_SAVE' => 'Salvesta',
+    'LBL_SUMMARY' => 'Kokkuvõte',
+    'LBL_STEP1' => 'Otsi ja vaata andmeid',
+    'LBL_STEP2' => 'Mesti kirjed',
+    'LBL_TEST_SOURCE' => 'Test Connector',
+    'LBL_TEST_SOURCE_FAILED' => 'Test ebaõnnestus',
+    'LBL_TEST_SOURCE_SUCCESS' => 'Test oli edukas',
+    'LBL_TITLE' => 'Andme mestimine',
+
+    'ERROR_NO_ADDITIONAL_DETAIL' => 'Viga: Lisainfot kirje jaoks ei leitud.',
+    'ERROR_NO_SEARCHDEFS_DEFINED' => 'Selle connectori jaoks pole mooduleid lubatud. Vali selle connectori jaoks moodul lehel Luba Connectorid.',
+    'ERROR_NO_SEARCHDEFS_MAPPED' => 'Viga: Puuduvad lubatud connectorid, millel on ettenähtud otsinguväljad.',
+    'ERROR_NO_SEARCHDEFS_MAPPING' => 'Viga: Mooduli ja connectori jaoks pole otsinguvälju määratletud. Palun kontakteeru süsteemiadministraatoriga.',
+    'ERROR_NO_DISPLAYABLE_MAPPED_FIELDS' => 'Viga: Tulemuste kuvamise jaoks pole kaardistatud mooduli välju. Palun kontakteeru süsteemiadministraatoriga.',
+    'LBL_INFO_INLINE' => 'Info' /*for 508 compliance fix*/,
+    'LBL_CLOSE' => 'Sulge' /*for 508 compliance fix*/,
+
+);
