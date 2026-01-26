920aa320b876d2b40ef92a17268f9aafbcfd9cca
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-26 20:49:55 +0000

    New translations en_us.lang.php (Dutch)

---
+++ translations/nl_NL/modules/Surveys/language/en_US.lang.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID'                                     => 'Toegewezen gebruiker-Id',
+    'LBL_ADD_OPTION'                                         => 'Nieuwe optie toevoegen',
+    'LBL_ASSIGNED_TO_NAME'                                   => 'Toegewezen aan',
+    'LBL_SECURITYGROUPS'                                     => 'Security Groepen',
+    'LBL_SECURITYGROUPS_SUBPANEL_TITLE'                      => 'Security Groepen',
+    'LBL_ID'                                                 => 'ID',
+    'LBL_DATE_ENTERED'                                       => 'Aanmaakdatum',
+    'LBL_DATE_MODIFIED'                                      => 'Datum gewijzigd',
+    'LBL_MODIFIED'                                           => 'Gewijzigd door',
+    'LBL_MODIFIED_ID'                                        => 'Gewijzigd door ID',
+    'LBL_MODIFIED_NAME'                                      => 'Gewijzigd door naam',
+    'LBL_CREATED'                                            => 'Aangemaakt door',
+    'LBL_CREATED_ID'                                         => 'Gemaakt door ID',
+    'LBL_DESCRIPTION'                                        => 'Omschrijving',
+    'LBL_DELETED'                                            => 'Verwijderd',
+    'LBL_NAME'                                               => 'Naam',
+    'LBL_CREATED_USER'                                       => 'Aangemaakt door gebruiker',
+    'LBL_MODIFIED_USER'                                      => 'Gewijzigd door gebruiker',
+    'LBL_LIST_NAME'                                          => 'Naam',
+    'LBL_EDIT_BUTTON'                                        => 'Bewerken',
+    'LBL_REMOVE'                                             => 'Verwijderen',
+    'LBL_LIST_FORM_TITLE'                                    => 'Enquêtes lijst',
+    'LBL_MODULE_NAME'                                        => 'Enquêtes',
+    'LBL_MODULE_TITLE'                                       => 'Enquêtes',
+    'LBL_HOMEPAGE_TITLE'                                     => 'Mijn enquêtes',
+    'LNK_NEW_RECORD'                                         => 'Enquêtes maken',
+    'LNK_LIST'                                               => 'Enquêtes bekijken',
+    'LNK_IMPORT_SURVEYS'                                     => 'Enquêtes importeren',
+    'LBL_SEARCH_FORM_TITLE'                                  => 'Zoek enquêtes',
+    'LBL_HISTORY_SUBPANEL_TITLE'                             => 'Bekijk geschiedenis',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE'                          => 'Activiteiten',
+    'LBL_SURVEYS_SUBPANEL_TITLE'                             => 'Enquêtes',
+    'LBL_NEW_FORM_TITLE'                                     => 'Nieuwe enquêtes',
+    'LBL_STATUS'                                             => 'Status',
+    'LBL_SURVEY_QUESTIONS_DISPLAY'                           => 'Vragen',
+    'LBL_SURVEY_URL_DISPLAY'                                 => 'URL',
+    'LBL_HAPPINESS_QUESTION'                                 => 'Geluk vraag',
+    'LBL_CANT_EDIT_RESPONDED'                                => 'Enquêtevragen met antwoorden kunnen niet worden bewerkt',
+    'LBL_VIEW_SURVEY_REPORTS'                                => 'Enquête rapporten weergeven',
+    'LBL_CHECKED'                                            => 'Gecontroleerd',
+    'LBL_UNCHECKED'                                          => 'Niet gecontroleerd',
+    'LBL_RESPONSE_ANSWER'                                    => 'Antwoord',
+    'LBL_RESPONSE_CONTACT'                                   => 'Contactpersonen',
+    'LBL_RESPONSE_TIME'                                      => 'Datum',
+    'LBL_UNKNOWN_CONTACT'                                    => 'Onbekend',
+    'LBL_RESPONSE_COUNT'                                     => 'Tellen',
+    'LBL_SUBMIT_TEXT'                                        => 'Tekst insturen',
+    'LBL_SATISFIED_TEXT'                                     => 'Tevreden tekst',
+    'LBL_NEITHER_TEXT'                                       => 'Onverschillig tekst',
+    'LBL_DISSATISFIED_TEXT'                                  => 'Ontevreden tekst',
+    'LBL_SURVEYS_SURVEYQUESTIONS_FROM_SURVEYQUESTIONS_TITLE' => 'Enquête vragen',
+    'LBL_SURVEYS_SURVEYRESPONSES_FROM_SURVEYRESPONSES_TITLE' => 'Enquête antwoorden',
+    'LBL_SHOW_RESPONSES' => 'Toon antwoorden',
+    'LBL_HIDE_RESPONSES' => 'Reacties verbergen',
+    'LBL_SURVEY_QUESTION' => 'Vraag',
+    'LBL_SURVEY_TEXT' => 'Tekst',
+    'LBL_SURVEY_TYPE' => 'Type',
+    'LBL_SURVEY_ACTIONS' => 'Acties',
+    'LBL_SURVEY_NEW_QUESTION' => 'Nieuwe vraag',
+    'LBL_SURVEY_RESPONSES' => 'Nieuwe vraag',
+    'LBL_SURVEYS_SENT' => 'Enquêtes verzonden:',
+    'LBL_SURVEY_DISTINCT' => 'Verschillende enquêtes verzonden:',
+    'LBL_DRAFT' => 'Concept',
+    'LBL_SUBMIT' => 'OK',
+    'LBL_STARS' => 'Sterren',
+    'LBL_SURVEY_CLOSE_RESPONSE' => 'Bedankt voor uw belangstelling, maar dit onderzoek is nu gesloten.',
+    'LBL_SURVEY_THANKS' => 'Thanks for completing this survey.',
+);
