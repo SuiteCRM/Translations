ff851f28b4abc9db816e286de494a80869ac7446
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-26 19:04:06 +0000

    New translations en_us.lang.php (Indonesian)

---
+++ translations/id_ID/modules/AOS_Products_Quotes/language/en_US.lang.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID' => 'ID Pengguna yang di-assign :',
+    'LBL_ASSIGNED_TO_NAME' => 'Ditugaskan kepada',
+    'LBL_ID' => 'ID',
+    'LBL_DATE_ENTERED' => 'Tanggal dibuat',
+    'LBL_DATE_MODIFIED' => 'Tanggal dibuat',
+    'LBL_MODIFIED' => 'Diubah Oleh',
+    'LBL_MODIFIED_NAME' => 'Diubah oleh Nama',
+    'LBL_CREATED' => 'Dibuat Oleh',
+    'LBL_DESCRIPTION' => 'Catatan',
+    'LBL_DELETED' => 'Dihapus',
+    'LBL_NAME' => 'Nama',
+    'LBL_CREATED_USER' => 'Dibuat oleh Pengguna',
+    'LBL_MODIFIED_USER' => 'Diubah oleh Pengguna',
+    'LBL_LIST_FORM_TITLE' => 'Daftar harga produk',
+    'LBL_MODULE_NAME' => 'Item baris',
+    'LBL_MODULE_TITLE' => 'Harga Produk: Beranda',
+    'LBL_HOMEPAGE_TITLE' => 'Penawaran Produk Saya',
+    'LNK_NEW_RECORD' => 'Buat Penawaran Produk',
+    'LNK_LIST' => 'Penawaran Produk',
+    'LBL_SEARCH_FORM_TITLE' => 'Cari Produk Kutipan',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Lihat Histori',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Aktifitas',
+    'LBL_NEW_FORM_TITLE' => 'Kutipan Produk Baru',
+    'LBL_PRODUCT_NAME' => 'Nama',
+    'LBL_PRODUCT_QTY' => 'Jumlah',
+    'LBL_PRODUCT_COST_PRICE' => 'Biaya',
+    'LBL_PRODUCT_LIST_PRICE' => 'Harga Terdaftar',
+    'LBL_PRODUCT_UNIT_PRICE' => 'Harga Satuan',
+    'LBL_PRODUCT_DISCOUNT' => 'Diskon',
+    'LBL_PRODUCT_DISCOUNT_AMOUNT' => 'Total Diskon',
+    'LBL_PART_NUMBER' => 'Nomor Barang',
+    'LBL_PRODUCT_DESCRIPTION' => 'Deskripsi',
+    'LBL_DISCOUNT' => 'Tipe Diskon',
+    'LBL_VAT_AMT' => 'Total Pajak',
+    'LBL_VAT' => 'Pajak',
+    'LBL_PRODUCT_TOTAL_PRICE' => 'Total Harga',
+    'LBL_PRODUCT_NOTE' => 'Catatan',
+    'Quote' => '',
+    'LBL_FLEX_RELATE' => 'Terkait dengan',
+    'LBL_PRODUCT' => 'Produk',
+
+    'LBL_SERVICE_MODULE_NAME' => 'Layanan',
+    'LBL_LIST_NUM' => 'Nomor',
+    'LBL_PARENT_ID' => 'ID Induk',
+    'LBL_GROUP_NAME' => 'Grup',
+    'LBL_PRODUCT_COST_PRICE_USDOLLAR' => 'Harga Harga ( Mata uang Kelalaian)',
+    'LBL_PRODUCT_LIST_PRICE_USDOLLAR' => 'Daftar Harga (Mata uang Kelalaian)',
+    'LBL_PRODUCT_UNIT_PRICE_USDOLLAR' => 'Harga Satuan (Mata uang Kelalaian)',
+    'LBL_PRODUCT_TOTAL_PRICE_USDOLLAR' => 'Total Harga (Mata uang Kelalaian)',
+    'LBL_PRODUCT_DISCOUNT_USDOLLAR' => 'Diskon (Mata Uang Default)',
+    'LBL_PRODUCT_DISCOUNT_AMOUNT_USDOLLAR' => 'Jumlah Potongan (Mata uang Kelalaian)',
+    'LBL_VAT_AMT_USDOLLAR' => 'Jumlah Pajak (Mata uang Kelalaian)',
+    'LBL_PRODUCTS_SERVICES' => 'Produk / Layanan',
+    'LBL_PRODUCT_ID' => 'ID Produk',
+
+    'LBL_AOS_CONTRACTS' => 'Kontrak',
+    'LBL_AOS_INVOICES' => 'Faktur',
+    'LBL_AOS_PRODUCTS' => 'Produk',
+    'LBL_AOS_QUOTES' => 'Penawaran',
+);
