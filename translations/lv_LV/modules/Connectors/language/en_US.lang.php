21a55fcc68914d4be11c0000b5faf0ceca42b54a
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-26 19:35:57 +0000

    New translations en_us.lang.php (Latvian)

---
+++ translations/lv_LV/modules/Connectors/language/en_US.lang.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+
+$mod_strings = array(
+
+    'LBL_ADMINISTRATION_MAIN' => 'Konektoru uzstādījumi',
+    'LBL_AVAILABLE' => 'Pieejams',
+    'LBL_BACK' => '< Atpakaļ',
+    'LBL_CONFIRM_CONTINUE_SAVE' => 'Daži obligātie lauki nav aizpildīti. Vai turpināt saglabāšanu?',
+    'LBL_CONNECTOR_FIELDS' => 'Konektora lauki',
+    'LBL_DATA' => 'Dati',
+    'LBL_DEFAULT' => 'Noklusētā',
+    'LBL_DISABLED' => 'Izslēgta',
+    'LBL_ENABLED' => 'Ieslēgta',
+    'LBL_EXTERNAL' => 'Atļaut lietotājiem veidot ārējā konta ierakstus šim konektoram',
+    'LBL_EXTERNAL_SET_PROPERTIES' => 'Lai lietotu šo konektoru, ir jābūt uzstādītiem parametriem konektoru parametru uzstādījumu lapā.',
+    'LBL_MERGE' => 'Sapludināt',
+    'LBL_MODIFY_DISPLAY_TITLE' => 'Aktivēt konektorus',
+    'LBL_MODIFY_DISPLAY_DESC' => 'Norādīt, kuri moduļi ir pieejami katram konektoram.',
+    'LBL_MODULE_FIELDS' => 'Moduļu lauki',
+    'LBL_MODIFY_MAPPING_TITLE' => 'Kartēt konektora laukus',
+    'LBL_MODIFY_MAPPING_DESC' => 'Kartēt konektoru laukus pret moduļu laukiem, lai noteiktu, kuri konektoru dati var tikt parādīti un ievietoti  moduļu ierakstos.',
+    'LBL_MODIFY_PROPERTIES_TITLE' => 'Konektoru parametri',
+    'LBL_MODIFY_PROPERTIES_DESC' => 'Konektoru parametru konfigurēšana, ieskaitot URL un API atslēgas.',
+    'LBL_MODIFY_SEARCH_TITLE' => 'Konektoru meklēšanas pārvaldība',
+    'LBL_MODIFY_SEARCH' => 'Meklēšana',
+    'LBL_MODIFY_SEARCH_DESC' => 'Atzīmējiet konektora laukus, kurus lietot, meklējot datus katrā modulī.',
+    'LBL_MODULE_NAME' => 'Konektori',
+    'LBL_NO_PROPERTIES' => 'Konektoram nav konfigurējamu parametru.',
+    'LBL_SAVE' => 'Pabeigt',
+    'LBL_SUMMARY' => 'Kopsavilkums',
+    'LBL_STEP1' => 'Meklēt un aplūkot datus',
+    'LBL_STEP2' => 'Sapludināt ierakstus ar',
+    'LBL_TEST_SOURCE' => 'Konektora tests',
+    'LBL_TEST_SOURCE_FAILED' => 'Tests neizdevās',
+    'LBL_TEST_SOURCE_SUCCESS' => 'Tests veiksmīgs',
+    'LBL_TITLE' => 'Datu sapludināšana',
+
+    'ERROR_NO_ADDITIONAL_DETAIL' => 'Kļūda: Šim ierakstam nav papildus datu',
+    'ERROR_NO_SEARCHDEFS_DEFINED' => 'Konektoram nav aktivēts nevien modulis. Norādiet šim konektoram moduli konektoru aktivēšanas lapā.',
+    'ERROR_NO_SEARCHDEFS_MAPPED' => 'Kļūda: Nav neviena konektora, kuram būtu nodefinēti meklēšanas lauki.',
+    'ERROR_NO_SEARCHDEFS_MAPPING' => 'Kļūda: Modulim un konektoram nav nodefinēti meklēšanas lauki. Sazinieties ar sistēmas administratoru.',
+    'ERROR_NO_DISPLAYABLE_MAPPED_FIELDS' => 'Kļūda: Nav norādīts neviens moduļa lauks, ko rādīt rezultātos. Sazinieties ar sistēmas administratoru.',
+    'LBL_INFO_INLINE' => 'Informācija' /*for 508 compliance fix*/,
+    'LBL_CLOSE' => 'Slēgt' /*for 508 compliance fix*/,
+
+);
