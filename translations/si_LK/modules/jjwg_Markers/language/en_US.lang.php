6607e676a56c02452705ecf0d6764d1d4195a1f4
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-26 21:03:16 +0000

    New translations en_us.lang.php (Sinhala)

---
+++ translations/si_LK/modules/jjwg_Markers/language/en_US.lang.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings['LBL_ASSIGNED_TO_ID'] = 'පවරා පරිශීලක අංකය';
+$mod_strings['LBL_ASSIGNED_TO_NAME'] = 'පරිශීලක';
+$mod_strings['LBL_ID'] = 'හැඳු.';
+$mod_strings['LBL_DATE_ENTERED'] = 'සෑදූ දිනය';
+$mod_strings['LBL_DATE_MODIFIED'] = 'නවීකරණය කරන ලද දිනය';
+$mod_strings['LBL_MODIFIED'] = ' වන විට නවීකරණය කරන ලද';
+$mod_strings['LBL_MODIFIED_NAME'] = ' නම වන විට නවීකරණය කරන ලද';
+$mod_strings['LBL_CREATED'] = 'සෑදුවේ';
+$mod_strings['LBL_DESCRIPTION'] = 'විස්තරය';
+$mod_strings['LBL_DELETED'] = ' මකා දමන';
+$mod_strings['LBL_NAME'] = 'නම';
+$mod_strings['LBL_CREATED_USER'] = ' පරිශීලක විසින් නිර්මාණය';
+$mod_strings['LBL_MODIFIED_USER'] = ' පරිශීලක විසින් නවීකරණය කරන ලද';
+$mod_strings['LBL_LIST_NAME'] = 'නම';
+$mod_strings['LBL_LIST_FORM_TITLE'] = 'සලකුණු ලැයිස්තුව';
+$mod_strings['LBL_MODULE_NAME'] = 'සලකුණු';
+$mod_strings['LBL_MODULE_TITLE'] = 'සලකුණු';
+$mod_strings['LBL_HOMEPAGE_TITLE'] = 'මගේ පෑන';
+$mod_strings['LNK_NEW_RECORD'] = 'සලකුණු සාදන්න';
+$mod_strings['LNK_LIST'] = 'දැක්ම සලකුණු';
+$mod_strings['LNK_IMPORT_JJWG_MARKERS'] = 'ආනයන සලකුණු';
+$mod_strings['LBL_SEARCH_FORM_TITLE'] = 'සොයන්න සලකුණු';
+$mod_strings['LBL_HISTORY_SUBPANEL_TITLE'] = ' දැක්ම ඉතිහාසය';
+$mod_strings['LBL_ACTIVITIES_SUBPANEL_TITLE'] = 'ක්‍රියාකාරකම්';
+$mod_strings['LBL_NEW_FORM_TITLE'] = 'නව සලකුණු';
+$mod_strings['LBL_CITY'] = 'නගරය';
+$mod_strings['LBL_STATE'] = 'රජයේ';
+$mod_strings['LBL_COUNTRY'] = 'රට';
+$mod_strings['LBL_JJWG_MAPS_LAT'] = 'අක්ෂාංශ';
+$mod_strings['LBL_JJWG_MAPS_LNG'] = 'දේශාංශ';
+$mod_strings['LBL_MARKER_IMAGE'] = 'මාකර් පිංතූර වර්ගය';
+$mod_strings['LBL_LIST_ASSIGNED_USER'] = 'කිරීම සඳහා පවරා';
+
+$mod_strings['LBL_MARKER_MARKER_POSITION'] = 'මාකර් තත්ත්වය (අක්ෂාංශ, දේශාංශ):';
+$mod_strings['LBL_MARKER_CLOSEST_MATCHING_ADDRESS'] = 'කිට්ටුම ගැලපීම් ෙමරට ලිපිනය:';
+$mod_strings['LBL_MARKER_MARKER_STATUS'] = 'වෙළඳ පොළ තත්ත්වය';
+$mod_strings['LBL_MARKER_EDIT_DESCRIPTION'] = 'ක්ලික් කරන්න සහ මාකර් ඇදගෙන යන්න.';
+$mod_strings['LBL_JJWG_MAPS_JJWG_MARKERS_FROM_JJWG_MAPS_TITLE'] = 'සිතියම්';
