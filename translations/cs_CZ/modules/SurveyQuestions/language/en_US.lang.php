6788cc3e73ce8188d1bd0c7e601cdb3e313e7bb5
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-26 20:47:08 +0000

    New translations en_us.lang.php (Czech)

---
+++ translations/cs_CZ/modules/SurveyQuestions/language/en_US.lang.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID'                                                               => 'ID přiřazeného uživatele',
+    'LBL_ASSIGNED_TO_NAME'                                                             => 'Přiřazeno k',
+    'LBL_SECURITYGROUPS'                                                               => 'Skupiny zabezpečení',
+    'LBL_SECURITYGROUPS_SUBPANEL_TITLE'                                                => 'Skupiny zabezpečení',
+    'LBL_ID'                                                                           => 'ID',
+    'LBL_DATE_ENTERED'                                                                 => 'Datum vytvoření',
+    'LBL_DATE_MODIFIED'                                                                => 'Datum změny',
+    'LBL_MODIFIED'                                                                     => 'Změnil',
+    'LBL_MODIFIED_ID'                                                                  => 'Změnil ID',
+    'LBL_MODIFIED_NAME'                                                                => 'Změněno dle Jména',
+    'LBL_CREATED'                                                                      => 'Vytvořeno',
+    'LBL_CREATED_ID'                                                                   => 'Vytvořil ID',
+    'LBL_DESCRIPTION'                                                                  => 'Popis',
+    'LBL_DELETED'                                                                      => 'Vymazáno',
+    'LBL_NAME'                                                                         => 'Název',
+    'LBL_CREATED_USER'                                                                 => 'Vytvořeno dle Uživatele',
+    'LBL_MODIFIED_USER'                                                                => 'Změněno dle Uživatele',
+    'LBL_LIST_NAME'                                                                    => 'Název',
+    'LBL_EDIT_BUTTON'                                                                  => 'Upravit',
+    'LBL_REMOVE'                                                                       => 'Odebrat',
+    'LBL_LIST_FORM_TITLE'                                                              => 'Seznam otázek dotazníku',
+    'LBL_MODULE_NAME'                                                                  => 'Otázky průzkumů',
+    'LBL_MODULE_TITLE'                                                                 => 'Otázky průzkumů',
+    'LBL_HOMEPAGE_TITLE'                                                               => 'Moje otázky dotazníku',
+    'LNK_NEW_RECORD'                                                                   => 'Vytvořit otázky dotazníku',
+    'LNK_LIST'                                                                         => 'Zobrazit otázky dotazníku',
+    'LNK_IMPORT_SURVEYQUESTIONS'                                                       => 'Importovat otázky dotazníku',
+    'LBL_SEARCH_FORM_TITLE'                                                            => 'Vyhledat otázky dotazníku',
+    'LBL_HISTORY_SUBPANEL_TITLE'                                                       => 'Historie',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE'                                                    => 'Aktivity',
+    'LBL_SURVEYQUESTIONS_SUBPANEL_TITLE'                                               => 'Otázky průzkumů',
+    'LBL_NEW_FORM_TITLE'                                                               => 'Nové otázky dotazníku',
+    'LBL_SORT_ORDER'                                                                   => 'Jiné',
+    'LBL_TYPE'                                                                         => 'Typ',
+    'LBL_HAPPINESS_QUESTION'                                                           => 'Otázka spokojenosti',
+    'LBL_SURVEYS_SURVEYQUESTIONS_FROM_SURVEYS_TITLE'                                   => 'Průzkumy',
+    'LBL_SURVEYQUESTIONS_SURVEYQUESTIONRESPONSES_FROM_SURVEYQUESTIONRESPONSES_TITLE'   => 'Odpovědi na otázky průzkumů',
+    'LBL_SURVEYQUESTIONS_SURVEYQUESTIONRESPONSES_1_FROM_SURVEYQUESTIONRESPONSES_TITLE' => 'Odpovědi na otázky průzkumů',
+    'LBL_SURVEYQUESTIONS_SURVEYQUESTIONOPTIONS_FROM_SURVEYQUESTIONOPTIONS_TITLE'       => 'Volby otázky dotazníku',
+);
