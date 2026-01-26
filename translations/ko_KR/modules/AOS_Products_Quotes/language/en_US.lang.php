644a7212062e76e3115e268a2de4c32edd32aa8d
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-26 19:03:45 +0000

    New translations en_us.lang.php (Korean)

---
+++ translations/ko_KR/modules/AOS_Products_Quotes/language/en_US.lang.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID' => '할당된 사용자 Id',
+    'LBL_ASSIGNED_TO_NAME' => '담당자',
+    'LBL_ID' => 'ID',
+    'LBL_DATE_ENTERED' => '생성날짜',
+    'LBL_DATE_MODIFIED' => '수정일',
+    'LBL_MODIFIED' => '변경됨',
+    'LBL_MODIFIED_NAME' => '이름으로 변경됨',
+    'LBL_CREATED' => '작성자',
+    'LBL_DESCRIPTION' => '노트',
+    'LBL_DELETED' => '삭제됨',
+    'LBL_NAME' => '이름',
+    'LBL_CREATED_USER' => '사용자로 생성됨',
+    'LBL_MODIFIED_USER' => '변경유저',
+    'LBL_LIST_FORM_TITLE' => '제품 견적 목록',
+    'LBL_MODULE_NAME' => '행 항목',
+    'LBL_MODULE_TITLE' => '제품 견적: 홈',
+    'LBL_HOMEPAGE_TITLE' => '내 제품 견적',
+    'LNK_NEW_RECORD' => '제품 견적 만들기',
+    'LNK_LIST' => '제품 견적',
+    'LBL_SEARCH_FORM_TITLE' => '제품 견적 검색',
+    'LBL_HISTORY_SUBPANEL_TITLE' => '이력 표시',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => '영업활동',
+    'LBL_NEW_FORM_TITLE' => '새 제품 견적',
+    'LBL_PRODUCT_NAME' => '이름',
+    'LBL_PRODUCT_QTY' => '수량',
+    'LBL_PRODUCT_COST_PRICE' => '원가',
+    'LBL_PRODUCT_LIST_PRICE' => '정찰 가격',
+    'LBL_PRODUCT_UNIT_PRICE' => '단위 가격',
+    'LBL_PRODUCT_DISCOUNT' => '할인',
+    'LBL_PRODUCT_DISCOUNT_AMOUNT' => '할인 금액',
+    'LBL_PART_NUMBER' => '부품 번호',
+    'LBL_PRODUCT_DESCRIPTION' => '상세',
+    'LBL_DISCOUNT' => '할인 유형',
+    'LBL_VAT_AMT' => '세 액',
+    'LBL_VAT' => '세금',
+    'LBL_PRODUCT_TOTAL_PRICE' => '총 가격',
+    'LBL_PRODUCT_NOTE' => '노트',
+    'Quote' => '',
+    'LBL_FLEX_RELATE' => '관련처',
+    'LBL_PRODUCT' => '제품',
+
+    'LBL_SERVICE_MODULE_NAME' => '서비스',
+    'LBL_LIST_NUM' => '번호',
+    'LBL_PARENT_ID' => '패런트ID:',
+    'LBL_GROUP_NAME' => '그룹',
+    'LBL_PRODUCT_COST_PRICE_USDOLLAR' => '원가 (기본 통화)',
+    'LBL_PRODUCT_LIST_PRICE_USDOLLAR' => '정찰가ㅣ격(기본 통화)',
+    'LBL_PRODUCT_UNIT_PRICE_USDOLLAR' => '단가(기본 통화)',
+    'LBL_PRODUCT_TOTAL_PRICE_USDOLLAR' => '총 가격 (기본 통화)',
+    'LBL_PRODUCT_DISCOUNT_USDOLLAR' => '할인 (기본 통화)',
+    'LBL_PRODUCT_DISCOUNT_AMOUNT_USDOLLAR' => '할인 금액 (기본 통화)',
+    'LBL_VAT_AMT_USDOLLAR' => '세금 금액 (기본 통화)',
+    'LBL_PRODUCTS_SERVICES' => '제품 / 서비스',
+    'LBL_PRODUCT_ID' => '제품 ID',
+
+    'LBL_AOS_CONTRACTS' => '계약',
+    'LBL_AOS_INVOICES' => '송장',
+    'LBL_AOS_PRODUCTS' => '제품',
+    'LBL_AOS_QUOTES' => '견적',
+);
