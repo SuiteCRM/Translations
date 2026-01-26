fc702eceb216dc20f9b22ac256d449ff2d8f0bd5
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-26 19:02:42 +0000

    New translations en_us.lang.php (Swedish)

---
+++ translations/sv_SE/modules/AOS_Products/language/en_US.lang.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID' => 'Tilldelat användar-id',
+    'LBL_ASSIGNED_TO_NAME' => 'Tilldelad till',
+    'LBL_ID' => 'ID',
+    'LBL_DATE_ENTERED' => 'Datum Skapad',
+    'LBL_DATE_MODIFIED' => 'Datum Ändrad',
+    'LBL_MODIFIED' => 'Ändrad av',
+    'LBL_MODIFIED_NAME' => 'Ändrad av Namn',
+    'LBL_CREATED' => 'Skapad av',
+    'LBL_DESCRIPTION' => 'Beskrivning',
+    'LBL_DELETED' => 'Raderad',
+    'LBL_NAME' => 'Produktnamn',
+    'LBL_CREATED_USER' => 'Skapad av Användare',
+    'LBL_MODIFIED_USER' => 'Ändrad av Användare',
+    'LBL_LIST_FORM_TITLE' => 'Produktlista',
+    'LBL_MODULE_NAME' => 'Produkter',
+    'LBL_MODULE_TITLE' => 'Produkter: hem',
+    'LBL_HOMEPAGE_TITLE' => 'Mina produkter',
+    'LNK_NEW_RECORD' => 'Skapa Produkt',
+    'LNK_LIST' => 'Visa produkter',
+    'LBL_SEARCH_FORM_TITLE' => 'Sök produkter',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Visa historik',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Aktiviteter',
+    'LBL_NEW_FORM_TITLE' => 'Ny produkt',
+    'LBL_MAINCODE' => 'Produktkod',
+    'VALUE' => 'Produktnamn',
+    'LBL_PART_NUMBER' => 'Delnummer',
+    'LBL_CATEGORY' => 'Kategori',
+    'LBL_TYPE' => 'Produkttyp',
+    'LBL_COST' => 'Kostnad',
+    'LBL_PRICE' => 'Pris',
+    'LBL_URL' => 'URL:',
+    'LBL_CONTACT' => 'Kontakter',
+    'LBL_PRODUCT_IMAGE' => 'Produktbild',
+    'LBL_IMAGE_UPLOAD_FAIL' => 'ERROR: uploaded file exceeded the max filesize: max filesize: ',
+    'LBL_AOS_PRODUCT_CATEGORYS_NAME' => 'Produktkategori',
+    'LBL_AOS_PRODUCT_CATEGORY' => 'Produktkategori-ID',
+    'LBL_AOS_PRODUCT_CATEGORIES' => 'Produktkategorier',
+    'LBL_COST_USDOLLAR' => 'Cost (Default Currency)',
+    'LBL_PRICE_USDOLLAR' => 'Pris (standardvaluta)',
+    'LBL_FILE_URL' => 'Fil-URL',
+    'LBL_CUSTOMERS_PURCHASED_PRODUCTS_SUBPANEL_TITLE' => 'Inköp',
+    'LBL_PRODUCTS_PURCHASES' => 'Inköp',
+    'LBL_AOS_QUOTE_NAME' => 'Offert',
+    'LBL_ACCOUNT_NAME' => 'Företag',
+);
