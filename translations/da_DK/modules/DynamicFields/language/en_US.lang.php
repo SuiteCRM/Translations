62e680dc3a5e2866c09079033f0c8a67b861878c
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-26 19:42:48 +0000

    New translations en_us.lang.php (Danish)

---
+++ translations/da_DK/modules/DynamicFields/language/en_US.lang.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LNK_NEW_CALL' => 'Planlæg opkald',
+    'LNK_NEW_MEETING' => 'Planlæg møde',
+    'LNK_NEW_TASK' => 'Opret opgave',
+    'LNK_NEW_NOTE' => 'Opret note eller vedhæft',
+    'LNK_NEW_EMAIL' => 'Arkiver Email',
+    'LNK_CALL_LIST' => 'Opkald',
+    'LNK_MEETING_LIST' => 'Møder',
+    'LNK_TASK_LIST' => 'Opgaver',
+    'LNK_NOTE_LIST' => 'Noter',
+    'LBL_ADD_FIELD' => 'Tilføj felt:',
+    'LBL_SEARCH_FORM_TITLE' => 'Modulsøgning',
+    'COLUMN_TITLE_NAME' => 'Feltnavn',
+    'COLUMN_TITLE_DISPLAY_LABEL' => 'Vist etiket',
+    'COLUMN_TITLE_LABEL_VALUE' => 'Etiketværdi',
+    'COLUMN_TITLE_LABEL' => 'Systemediket',
+    'COLUMN_TITLE_DATA_TYPE' => 'Datatype',
+    'COLUMN_TITLE_MAX_SIZE' => 'Maksimum størrelse',
+    'COLUMN_TITLE_HELP_TEXT' => 'Hjælpetekst',
+    'COLUMN_TITLE_COMMENT_TEXT' => 'Kommentar tekst',
+    'COLUMN_TITLE_REQUIRED_OPTION' => 'Påkrævet felt',
+    'COLUMN_TITLE_DEFAULT_VALUE' => 'Standardværdi',
+    'COLUMN_TITLE_FRAME_HEIGHT' => 'IFrame højde',
+    'COLUMN_TITLE_HTML_CONTENT' => 'HTML',
+    'COLUMN_TITLE_URL' => 'Standard URL',
+    'COLUMN_TITLE_AUDIT' => 'Revision',
+    'COLUMN_TITLE_MIN_VALUE' => 'Minimumsværdi',
+    'COLUMN_TITLE_MAX_VALUE' => 'Maksimumsværdi',
+    'COLUMN_TITLE_LABEL_ROWS' => 'Rækker',
+    'COLUMN_TITLE_LABEL_COLS' => 'Kolonner',
+    'COLUMN_TITLE_DISPLAYED_ITEM_COUNT' => '# elementer der vises',
+    'COLUMN_TITLE_AUTOINC_NEXT' => 'Auto forøg næste værdi',
+    'COLUMN_DISABLE_NUMBER_FORMAT' => 'Vis format',
+    'COLUMN_TITLE_ENABLE_RANGE_SEARCH' => 'Aktiver områdesøgning',
+    'LBL_DROP_DOWN_LIST' => 'Drop Down liste',
+    'LBL_RADIO_FIELDS' => 'Radio felter',
+    'LBL_MULTI_SELECT_LIST' => 'Multi valgliste',
+    'COLUMN_TITLE_PRECISION' => 'Præcision',
+    'LBL_MODULE' => 'Modul',
+    'COLUMN_TITLE_MASS_UPDATE' => 'Masse opdatering',
+    'COLUMN_TITLE_IMPORTABLE' => 'Importerbar',
+    'COLUMN_TITLE_DUPLICATE_MERGE' => 'Kopier flet',
+    'LBL_LABEL' => 'Label',
+    'LBL_DATA_TYPE' => 'Datatype',
+    'LBL_DEFAULT_VALUE' => 'Standardværdi',
+    'ERR_RESERVED_FIELD_NAME' => "Reserveret nøgleord",
+    'ERR_SELECT_FIELD_TYPE' => 'Venligst vælg en felt type',
+    'ERR_FIELD_NAME_ALREADY_EXISTS' => 'Feltnavnet findes allerede',
+    'LBL_BTN_ADD' => 'Tilføj',
+    'LBL_BTN_EDIT' => 'Rediger',
+    'LBL_GENERATE_URL' => 'Generer URL',
+    'LBL_CALCULATED' => 'Udregnet værdi',
+    'LBL_LINK_TARGET' => 'Åben link i',
+    'LBL_IMAGE_WIDTH' => 'Bredde',
+    'LBL_IMAGE_HEIGHT' => 'Højde',
+    'LBL_IMAGE_BORDER' => 'Kant',
+    'LBL_HELP' => 'Hjælp' /*for 508 compliance fix*/,
+    'COLUMN_TITLE_INLINE_EDIT_TEXT' => 'Rediger Indlejret',
+    'COLUMN_TITLE_PARENT_ENUM' => 'Overordnet Dropdown',
+    'LBL_INSERT_FIELD' => 'Insert Field',
+);
