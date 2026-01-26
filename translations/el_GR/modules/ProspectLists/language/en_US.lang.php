42c2dfc1f8b6039d81324c4a68ca3f576746171f
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-26 20:28:24 +0000

    New translations en_us.lang.php (Greek)

---
+++ translations/el_GR/modules/ProspectLists/language/en_US.lang.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_MODULE_NAME' => 'Λίστες Στόχων',
+    'LBL_MODULE_ID' => 'Λίστες Στόχων',
+    'LBL_MODULE_TITLE' => 'Λίστες Στόχων: Άρχικη',
+    'LBL_SEARCH_FORM_TITLE' => 'Αναζήτηση Λίστας Στόχων',
+    'LBL_LIST_FORM_TITLE' => 'Λίστες Στόχων',
+    'LBL_PROSPECT_LIST_NAME' => 'Λίστα Στόχων',
+    'LBL_NAME' => 'Όνομα',
+    'LBL_ENTRIES' => 'Συνολικές Εγγραφές',
+    'LBL_LIST_PROSPECT_LIST_NAME' => 'Λίστα Στόχων',
+    'LBL_LIST_ENTRIES' => 'Στόχοι Στην Λίστα',
+    'LBL_LIST_DESCRIPTION' => 'Περιγραφή',
+    'LBL_LIST_TYPE_NO' => 'Τύπος',
+    'LBL_LIST_END_DATE' => 'Ημερομηνία Λήξης',
+    'LBL_DATE_ENTERED' => 'Ημ/νία καταχώρησης',
+    'LBL_MARKETING_ID' => 'Μάρκετινγκ Id',
+    'LBL_DATE_MODIFIED' => 'Ημερομηνία Τροποποίησης',
+    'LBL_MODIFIED' => 'Τροποποιήθηκε από',
+    'LBL_CREATED' => 'Δημιουργήθηκε από',
+    'LBL_ASSIGNED_TO' => 'Υπεύθυνος',
+    'LBL_DESCRIPTION' => 'Περιγραφή',
+    'LNK_NEW_CAMPAIGN' => 'Δημιουργία καμπάνιας',
+    'LNK_CAMPAIGN_LIST' => 'Εκστρατείες',
+    'LNK_NEW_PROSPECT_LIST' => 'Δημιουργία Λίστας Στόχων',
+    'LNK_PROSPECT_LIST_LIST' => 'Προβολή Λίστες Στόχων',
+    'LBL_MODIFIED_BY' => 'Τροποποιήθηκε από',
+    'LBL_CREATED_BY' => 'Δημιουργήθηκε από',
+    'LNK_NEW_PROSPECT' => 'Δημιουργία Στόχων',
+    'LNK_PROSPECT_LIST' => 'Στόχοι',
+
+    'LBL_CONTACTS_SUBPANEL_TITLE' => 'Επαφές',
+    'LBL_LEADS_SUBPANEL_TITLE' => 'Δυνητικοί Πελάτες',
+    'LBL_PROSPECTS_SUBPANEL_TITLE' => 'Στόχοι',
+    'LBL_ACCOUNTS_SUBPANEL_TITLE' => 'Λογαριασμοί',
+    'LBL_COPY_PREFIX' => 'Αντιγραφή του',
+    'LBL_USERS_SUBPANEL_TITLE' => 'Χειριστές',
+    'LBL_TYPE' => 'Τύπος',
+    'LBL_LIST_TYPE' => 'Τύπος',
+    'LBL_LIST_TYPE_LIST_NAME' => 'Τύπος',
+    'LBL_NEW_FORM_TITLE' => 'Νέα Λίστα Στόχων',
+    'LBL_MARKETING_NAME' => 'Όνομα Marketing',
+    'LBL_DOMAIN_NAME' => 'Όνομα Domain',
+    'LBL_DOMAIN' => 'No emails to Domain:',
+    'LBL_LIST_PROSPECTLIST_NAME' => 'Όνομα',
+
+    'LBL_EMAIL_MARKETING' => 'Email Μάρκετινγκ',
+
+    'LBL_ASCENDING' => 'Αύξουσα',
+    'LBL_DESCENDING' => 'Φθίνουσα',
+);
