28d3231f017136d4db53930f14034ae0981e8365
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-26 21:02:50 +0000

    New translations en_us.lang.php (Turkish)

---
+++ translations/tr_TR/modules/jjwg_Markers/language/en_US.lang.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings['LBL_ASSIGNED_TO_ID'] = 'Atanmış kullanıcı kodu';
+$mod_strings['LBL_ASSIGNED_TO_NAME'] = 'Kullanıcı';
+$mod_strings['LBL_ID'] = 'Kod';
+$mod_strings['LBL_DATE_ENTERED'] = 'Eklenme tarihi';
+$mod_strings['LBL_DATE_MODIFIED'] = 'Değiştirilme tarihi';
+$mod_strings['LBL_MODIFIED'] = 'Değiştiren';
+$mod_strings['LBL_MODIFIED_NAME'] = 'Değiştiren adı';
+$mod_strings['LBL_CREATED'] = 'Ekleyen';
+$mod_strings['LBL_DESCRIPTION'] = 'Açıklama';
+$mod_strings['LBL_DELETED'] = 'Silindi';
+$mod_strings['LBL_NAME'] = 'Ad';
+$mod_strings['LBL_CREATED_USER'] = 'Ekleyen kullanıcı';
+$mod_strings['LBL_MODIFIED_USER'] = 'Değiştiren kullanıcı';
+$mod_strings['LBL_LIST_NAME'] = 'Ad';
+$mod_strings['LBL_LIST_FORM_TITLE'] = 'İşaretleyici listesi';
+$mod_strings['LBL_MODULE_NAME'] = 'İşaretleyiciler';
+$mod_strings['LBL_MODULE_TITLE'] = 'İşaretleyiciler';
+$mod_strings['LBL_HOMEPAGE_TITLE'] = 'İşaretleyicilerim';
+$mod_strings['LNK_NEW_RECORD'] = 'İşaretleyici ekle';
+$mod_strings['LNK_LIST'] = 'İşaretleyicileri görüntüle';
+$mod_strings['LNK_IMPORT_JJWG_MARKERS'] = 'İşaretleyicileri içe aktar';
+$mod_strings['LBL_SEARCH_FORM_TITLE'] = 'İşaretleyici arama';
+$mod_strings['LBL_HISTORY_SUBPANEL_TITLE'] = 'Geçmişi görüntüle';
+$mod_strings['LBL_ACTIVITIES_SUBPANEL_TITLE'] = 'İşlemler';
+$mod_strings['LBL_NEW_FORM_TITLE'] = 'Yeni işaretleyiciler';
+$mod_strings['LBL_CITY'] = 'İlçe';
+$mod_strings['LBL_STATE'] = 'İl';
+$mod_strings['LBL_COUNTRY'] = 'Ülke';
+$mod_strings['LBL_JJWG_MAPS_LAT'] = 'Enlem';
+$mod_strings['LBL_JJWG_MAPS_LNG'] = 'Boylam';
+$mod_strings['LBL_MARKER_IMAGE'] = 'İşaretleyici görseli türü';
+$mod_strings['LBL_LIST_ASSIGNED_USER'] = 'Atanmış kişi';
+
+$mod_strings['LBL_MARKER_MARKER_POSITION'] = 'İşaretleyici konumu (enlem, boylam):';
+$mod_strings['LBL_MARKER_CLOSEST_MATCHING_ADDRESS'] = 'Bulunan en yakın adres:';
+$mod_strings['LBL_MARKER_MARKER_STATUS'] = 'İşaretleyici durumu';
+$mod_strings['LBL_MARKER_EDIT_DESCRIPTION'] = 'İşaretleyiciyi sürüklemek için tıklayın.';
+$mod_strings['LBL_JJWG_MAPS_JJWG_MARKERS_FROM_JJWG_MAPS_TITLE'] = 'Haritalar';
