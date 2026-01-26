2db9924792db12967f1f1fe67ab7a375501d5e43
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-26 19:06:06 +0000

    New translations en_us.lang.php (Greek)

---
+++ translations/el_GR/modules/AOW_Actions/language/en_US.lang.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ID' => 'Α/Α',
+    'LBL_DATE_ENTERED' => 'Ημ/νία καταχώρησης',
+    'LBL_DATE_MODIFIED' => 'Ημερομηνία Τροποποίησης',
+    'LBL_MODIFIED' => 'Τροποποιήθηκε από',
+    'LBL_MODIFIED_ID' => 'Τροποποιήθηκε από Id:',
+    'LBL_MODIFIED_NAME' => 'Τροποποίηση ανά Όνομα',
+    'LBL_CREATED_USER' => 'Δημιουργήθηκε από',
+    'LBL_MODIFIED_USER' => 'τροποποίηση από',
+    'LBL_CREATED' => 'Δημιουργήθηκε από',
+    'LBL_DESCRIPTION' => 'Περιγραφή',
+    'LBL_CREATED_ID' => 'Δημιουργήθηκε από Id',
+    'LBL_DELETED' => 'Διαγράφηκε',
+    'LBL_NAME' => 'Όνομα',
+    'LBL_MODULE_NAME' => 'Ενέργειες Ροής Εργασίας',
+    'LBL_MODULE_TITLE' => 'Ενέργειες Ροής Εργασίας',
+    'LBL_AOW_WORKFLOW_ID' => 'AOW_WorkFlow Id',
+    'LBL_ACTION' => 'Δράση',
+    'LBL_PARAMETERS' => 'Παράμετροι',
+    'LBL_SELECT_ACTION' => 'Επιλογή Ενέργειας',
+    'LBL_CREATE_EMAIL_TEMPLATE' => 'Δημιουργία',
+    'LBL_RECORD_TYPE' => 'Τύπος Εγγραφής',
+    'LBL_SENDEMAIL' => 'Αποστολή Email',
+    'LBL_CREATERECORD' => 'Δημιουργία Εγγραφής',
+    'LBL_MODIFYRECORD' => 'Επεξεργασία Εγγραφής',
+    'LBL_ADD_FIELD' => 'Προσθήκη Πεδίου',
+    'LBL_ADD_RELATIONSHIP' => 'Προσθήκη Σχέσης',
+    'LBL_EDIT_EMAIL_TEMPLATE' => 'Επεξεργασία',
+    'LBL_EMAIL' => 'Email',
+    'LBL_EMAIL_TEMPLATE' => 'Πρότυπο email',
+    'LBL_SETAPPROVAL' => 'Ορισμός Έγκρισης',
+    'LBL_RELATE_WORKFLOW' => 'Συσχετισμός με την Ενότητα Ροή Εργασίας',
+    'LBL_INDIVIDUAL_EMAILS' => 'Αποστολή Μεμονωμένων Emails',
+    'LBL_COMPUTEFIELD' => 'Υπολογισμός πεδίων',
+    'LBL_COMPUTEFIELD_PARAMETERS' => 'Παράμετροι',
+    'LBL_COMPUTEFIELD_FIELD_NAME' => 'Όνομα πεδίου',
+    'LBL_COMPUTEFIELD_IDENTIFIER' => 'Αναγνωριστικό',
+    'LBL_COMPUTEFIELD_ADD_PARAMETER' => 'Προσθήκη παραμέτρου',
+    'LBL_COMPUTEFIELD_RELATION_PARAMETERS' => 'Παράμετροι σχέσης',
+    'LBL_COMPUTEFIELD_RELATION' => 'Σχέση',
+    'LBL_COMPUTEFIELD_ADD_RELATION_PARAMETER' => 'Προσθήκη παραμέτρου σχέσης',
+    'LBL_COMPUTEFIELD_FORMULA' => 'Τύπος',
+    'LBL_COMPUTEFIELD_FORMULAS' => 'Μαθηματικοί τύποι',
+    'LBL_COMPUTEFIELD_ADD_FORMULA' => 'Προσθήκη μαθηματικού τύπου',
+    'LBL_COMPUTEFIELD_VALUE_TYPE' => 'Τύπος τιμής',
+    'LBL_COMPUTEFIELD_RAW_VALUE' => 'Ακατέργαστη αξία',
+    'LBL_COMPUTEFIELD_FORMATTED_VALUE' => 'Μορφοποιημένη αξία',
+    'LBL_COPY_EMAIL_ADDRESSES_WORKFLOW' => 'Αντιγραφή emails από την ενότητα Ροή Εργασίας'
+);
