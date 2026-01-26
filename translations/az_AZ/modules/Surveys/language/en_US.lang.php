ea6c684c1b305dcec1a2e811bc02bb9662382ecf
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-26 20:50:21 +0000

    New translations en_us.lang.php (Azerbaijani)

---
+++ translations/az_AZ/modules/Surveys/language/en_US.lang.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID'                                     => 'Təyin Edilmiş İstifadəçi kimliyi',
+    'LBL_ADD_OPTION'                                         => 'Yeni seçim əlavə edin',
+    'LBL_ASSIGNED_TO_NAME'                                   => 'Təyin Edilsin',
+    'LBL_SECURITYGROUPS'                                     => 'Təhlükəsizlik Qrupları',
+    'LBL_SECURITYGROUPS_SUBPANEL_TITLE'                      => 'Təhlükəsizlik Qrupları',
+    'LBL_ID'                                                 => 'Kimlik',
+    'LBL_DATE_ENTERED'                                       => 'Yaradılma Tarixi',
+    'LBL_DATE_MODIFIED'                                      => 'Dəyişdirilmə Tarixi',
+    'LBL_MODIFIED'                                           => 'Redaktə etdi',
+    'LBL_MODIFIED_ID'                                        => 'Modified By Id',
+    'LBL_MODIFIED_NAME'                                      => 'Ad tərəfindən dəyişdirildi',
+    'LBL_CREATED'                                            => 'Müəllif',
+    'LBL_CREATED_ID'                                         => 'İd ilə yaradılıb',
+    'LBL_DESCRIPTION'                                        => 'Açıqlama',
+    'LBL_DELETED'                                            => 'Silinib',
+    'LBL_NAME'                                               => 'Ad',
+    'LBL_CREATED_USER'                                       => 'İstifadəçi tərəfindən yaradılıb',
+    'LBL_MODIFIED_USER'                                      => 'İstifadəçi tərəfindən dəyişdirildi',
+    'LBL_LIST_NAME'                                          => 'Ad',
+    'LBL_EDIT_BUTTON'                                        => 'Redaktə et',
+    'LBL_REMOVE'                                             => 'Sil',
+    'LBL_LIST_FORM_TITLE'                                    => 'Sorğular Siyahısı',
+    'LBL_MODULE_NAME'                                        => 'Sorğular',
+    'LBL_MODULE_TITLE'                                       => 'Sorğular',
+    'LBL_HOMEPAGE_TITLE'                                     => 'Mənim sorğularım',
+    'LNK_NEW_RECORD'                                         => 'Sorğular yaradın',
+    'LNK_LIST'                                               => 'Sorğulara baxın',
+    'LNK_IMPORT_SURVEYS'                                     => 'Sorğuları İdxal edin',
+    'LBL_SEARCH_FORM_TITLE'                                  => 'Axtarış sorğuları',
+    'LBL_HISTORY_SUBPANEL_TITLE'                             => 'Tarixçəyə bax',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE'                          => 'Fəaliyyətlər',
+    'LBL_SURVEYS_SUBPANEL_TITLE'                             => 'Sorğular',
+    'LBL_NEW_FORM_TITLE'                                     => 'Yeni Anketlər',
+    'LBL_STATUS'                                             => 'Vəziyyət',
+    'LBL_SURVEY_QUESTIONS_DISPLAY'                           => 'Suallar',
+    'LBL_SURVEY_URL_DISPLAY'                                 => 'URL',
+    'LBL_HAPPINESS_QUESTION'                                 => 'Məmnuniyyət sorğusu',
+    'LBL_CANT_EDIT_RESPONDED'                                => 'Cavabları olan sorğu suallarına düzəliş edilə bilməz',
+    'LBL_VIEW_SURVEY_REPORTS'                                => 'Anket Hesabatlarına Baxın',
+    'LBL_CHECKED'                                            => 'Yoxlanılıb',
+    'LBL_UNCHECKED'                                          => 'Yoxlanılmayıb',
+    'LBL_RESPONSE_ANSWER'                                    => 'Cavab',
+    'LBL_RESPONSE_CONTACT'                                   => 'Əlaqə',
+    'LBL_RESPONSE_TIME'                                      => 'Tarix',
+    'LBL_UNKNOWN_CONTACT'                                    => 'Naməlum',
+    'LBL_RESPONSE_COUNT'                                     => 'Sayı',
+    'LBL_SUBMIT_TEXT'                                        => 'Mətni Göndər',
+    'LBL_SATISFIED_TEXT'                                     => 'Məmnun Mətni',
+    'LBL_NEITHER_TEXT'                                       => 'Neither Text',
+    'LBL_DISSATISFIED_TEXT'                                  => 'Narazılıq Mətni',
+    'LBL_SURVEYS_SURVEYQUESTIONS_FROM_SURVEYQUESTIONS_TITLE' => 'Anket sualları',
+    'LBL_SURVEYS_SURVEYRESPONSES_FROM_SURVEYRESPONSES_TITLE' => 'Anket Cavabları',
+    'LBL_SHOW_RESPONSES' => 'Cavabları göstərin',
+    'LBL_HIDE_RESPONSES' => 'Cavabları gizlədin',
+    'LBL_SURVEY_QUESTION' => 'Sual',
+    'LBL_SURVEY_TEXT' => 'Mətn',
+    'LBL_SURVEY_TYPE' => 'Növ',
+    'LBL_SURVEY_ACTIONS' => 'Fəaliyyətlər',
+    'LBL_SURVEY_NEW_QUESTION' => 'Yeni Sual',
+    'LBL_SURVEY_RESPONSES' => 'Yeni Sual',
+    'LBL_SURVEYS_SENT' => 'Göndərilən anketlər:',
+    'LBL_SURVEY_DISTINCT' => 'Göndərilən fərqli anketlər:',
+    'LBL_DRAFT' => 'Qaralama',
+    'LBL_SUBMIT' => 'Təqdim et',
+    'LBL_STARS' => 'Ulduzlar',
+    'LBL_SURVEY_CLOSE_RESPONSE' => 'Maraq üçün təşəkkür edirik, lakin bu anket indi bağlıdır.',
+    'LBL_SURVEY_THANKS' => 'Thanks for completing this survey.',
+);
