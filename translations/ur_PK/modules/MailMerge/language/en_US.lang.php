e823efceba34bbe29999f6018bef405d7571802d
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-26 20:09:55 +0000

    New translations en_us.lang.php (Urdu (Pakistan))

---
+++ translations/ur_PK/modules/MailMerge/language/en_US.lang.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_STEP_1' => 'مرحلہ 1: ماڈیول اور سانچہ منتخب کریں',
+    'LBL_MAILMERGE_MODULE' => 'ماڈیول منتخب کریں: ',
+    'LBL_MAILMERGE_SELECTED_MODULE' => 'منتخب ماڈیول: ',
+    'LBL_MAILMERGE_TEMPLATES' => 'سانچہ منتخب کریں: ',
+    'LBL_STEP_4' => 'جائزہ لیں اور مکمل کریں',
+    'LBL_SELECTED_MODULE' => 'منتخب ماڈیول: ',
+    'LBL_SELECTED_TEMPLATE' => 'منتخب کردہ سانچہ: ',
+    'LBL_SELECTED_ITEMS' => 'منتخب اشیاء: ',
+    'LBL_STEP_5' => 'میل ضم مکمل',
+    'LBL_MERGED_FILE' => 'ضم کیا فائل: ',
+    'LNK_NEW_MAILMERGE' => 'میل ضم شروع کریں',
+    'LNK_UPLOAD_TEMPLATE' => 'سانچہ اپ لوڈ کریں',
+    'LBL_DOC_NAME' => 'دستاویز کا نام:',
+    'LBL_FILENAME' => 'فائل کا نام:',
+    'LBL_DOC_VERSION' => 'نظر ثانی:',
+    'LBL_DOC_DESCRIPTION' => 'تفصیل:',
+    'LBL_LIST_NAME' => 'نام',
+    'LBL_FINISH' => 'شروع کریں',
+    'LBL_NEXT' => 'اگے',
+    'LBL_BACK' => 'پیچھے جانا',
+    'LBL_START' => 'جاری رکھنے کیلئے یہاں کلک کریں',
+    'LBL_CONTAINS_CONTACT_INFO' => 'منتخب ٹیمپلیٹ پر مشتمل ہے ',
+    'LBL_ADDIN_NOTICE' => 'یہ مائیکروسافٹ ورڈ میں سویٹ آر آر ایم میل ضم شامل کریں کی تنصیب کی ضرورت ہے.',
+    'LBL_REMOVE' => 'آئٹم (س) کو ہٹا دیں' /*for 508 compliance fix*/,
+    'LBL_ADD' => 'آئٹم (س) شامل کریں' /*for 508 compliance fix*/,
+    'LBL_BROWSER_REQUIREMENT' => 'یہ IE 6.0 چاہئے یا ونڈوز کے لئے زیادہ سے زیادہ ہے.',
+    'LBL_FORM_SELECT_TEMPLATE' => 'ایک ٹیمپلیٹ منتخب کریں.',
+    'LBL_SELECT_ONE' => 'براہ کرم کم از کم ایک آئٹم منتخب کریں.',
+    'LBL_AVAILABLE' => 'دستیاب',
+    'LBL_SELECTED' => 'چنہ',
+    'LBL_RELATED_INFO_SELECTION' => 'آپ کی ضم شدہ دستاویز میں ظاہر ہونے والی متعلق معلومات کی شناخت کے لئے نیچے کے انتخاب کا استعمال کریں. اس قدم کو چھوڑ دو اگر آپ کے سانچے میں کوئی متعلق معلومات نہیں ہے.',
+
+);
