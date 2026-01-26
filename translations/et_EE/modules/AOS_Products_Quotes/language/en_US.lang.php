7898e5a48fa312892aa5a24fac4eec8e13737b6e
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-26 19:04:13 +0000

    New translations en_us.lang.php (Estonian)

---
+++ translations/et_EE/modules/AOS_Products_Quotes/language/en_US.lang.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID' => 'Määratud kasutaja Id',
+    'LBL_ASSIGNED_TO_NAME' => 'Vastutaja',
+    'LBL_ID' => 'ID',
+    'LBL_DATE_ENTERED' => 'Loomiskuupäev',
+    'LBL_DATE_MODIFIED' => 'Muutmiskuupäev',
+    'LBL_MODIFIED' => 'Muutja',
+    'LBL_MODIFIED_NAME' => 'Muutja nime järgi',
+    'LBL_CREATED' => 'Loodud',
+    'LBL_DESCRIPTION' => 'Märkused',
+    'LBL_DELETED' => 'Kustutatud',
+    'LBL_NAME' => 'Nimi',
+    'LBL_CREATED_USER' => 'Looja',
+    'LBL_MODIFIED_USER' => 'Muutja',
+    'LBL_LIST_FORM_TITLE' => 'Toodete Hinnapakkumised',
+    'LBL_MODULE_NAME' => 'Read',
+    'LBL_MODULE_TITLE' => 'Toodete hinnapakkumised: Avaleht',
+    'LBL_HOMEPAGE_TITLE' => 'Minu Toodete Hinnapakkumised',
+    'LNK_NEW_RECORD' => 'Loo toote hinnapakkumine',
+    'LNK_LIST' => 'Toodete hinnapakkumised',
+    'LBL_SEARCH_FORM_TITLE' => 'Otsi Toodete Hinnapakkumistest',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Vaata ajalugu',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Tegevused',
+    'LBL_NEW_FORM_TITLE' => 'Uus Toodete Hinnapakkumine',
+    'LBL_PRODUCT_NAME' => 'Nimi',
+    'LBL_PRODUCT_QTY' => 'Kogus',
+    'LBL_PRODUCT_COST_PRICE' => 'Sisseostu Hind',
+    'LBL_PRODUCT_LIST_PRICE' => 'Hinnakirja Hind',
+    'LBL_PRODUCT_UNIT_PRICE' => 'Ühiku Hind',
+    'LBL_PRODUCT_DISCOUNT' => 'Allahindlus',
+    'LBL_PRODUCT_DISCOUNT_AMOUNT' => 'Allahindluse määr',
+    'LBL_PART_NUMBER' => 'Osa number',
+    'LBL_PRODUCT_DESCRIPTION' => 'Kirjeldus',
+    'LBL_DISCOUNT' => 'Allahndluse liik',
+    'LBL_VAT_AMT' => 'Maksud kokku',
+    'LBL_VAT' => 'Maks',
+    'LBL_PRODUCT_TOTAL_PRICE' => 'Hind Kokku',
+    'LBL_PRODUCT_NOTE' => 'Märkused',
+    'Quote' => '',
+    'LBL_FLEX_RELATE' => 'Seotud',
+    'LBL_PRODUCT' => 'Artikkel',
+
+    'LBL_SERVICE_MODULE_NAME' => 'Teenused',
+    'LBL_LIST_NUM' => 'Number',
+    'LBL_PARENT_ID' => 'Peamine ID',
+    'LBL_GROUP_NAME' => 'Grupp',
+    'LBL_PRODUCT_COST_PRICE_USDOLLAR' => 'Sisseostu Hind (Põhivaluutas)',
+    'LBL_PRODUCT_LIST_PRICE_USDOLLAR' => 'Hinnakirja Hind (Põhivaluutas)',
+    'LBL_PRODUCT_UNIT_PRICE_USDOLLAR' => 'Ühiku Hind (Põhivaluutas)',
+    'LBL_PRODUCT_TOTAL_PRICE_USDOLLAR' => 'Hind Kokku (Põhivaluutas)',
+    'LBL_PRODUCT_DISCOUNT_USDOLLAR' => 'Allahindlus (Põhivaluutas)',
+    'LBL_PRODUCT_DISCOUNT_AMOUNT_USDOLLAR' => 'Allahindluse Summa (Põhivaluutas)',
+    'LBL_VAT_AMT_USDOLLAR' => 'Maksusumma (Põhivaluutas)',
+    'LBL_PRODUCTS_SERVICES' => 'Toode / Teenus',
+    'LBL_PRODUCT_ID' => 'Toote ID',
+
+    'LBL_AOS_CONTRACTS' => 'Lepingud',
+    'LBL_AOS_INVOICES' => 'Arved',
+    'LBL_AOS_PRODUCTS' => 'Artiklid',
+    'LBL_AOS_QUOTES' => 'Pakkumised',
+);
