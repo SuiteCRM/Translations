305fb63b10fbe93bd72aed1ace31589d32fadf23
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-26 20:28:43 +0000

    New translations en_us.lang.php (Slovak)

---
+++ translations/sk_SK/modules/ProspectLists/language/en_US.lang.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_MODULE_NAME' => 'Cieľové skupiny',
+    'LBL_MODULE_ID' => 'Cieľové skupiny',
+    'LBL_MODULE_TITLE' => 'Zoznam cieľov: Hlavná stránka',
+    'LBL_SEARCH_FORM_TITLE' => 'Prehľadávanie zoznamu cieľov',
+    'LBL_LIST_FORM_TITLE' => 'Cieľové skupiny',
+    'LBL_PROSPECT_LIST_NAME' => 'Zoznam cieľov:',
+    'LBL_NAME' => 'Meno',
+    'LBL_ENTRIES' => 'Položiek celkom',
+    'LBL_LIST_PROSPECT_LIST_NAME' => 'Cieľové skupiny',
+    'LBL_LIST_ENTRIES' => 'Ciele v zozname',
+    'LBL_LIST_DESCRIPTION' => 'Popis',
+    'LBL_LIST_TYPE_NO' => 'Typ',
+    'LBL_LIST_END_DATE' => 'Dátum ukončenia',
+    'LBL_DATE_ENTERED' => 'Dátum vytvorenia',
+    'LBL_MARKETING_ID' => 'Marketingové ID',
+    'LBL_DATE_MODIFIED' => 'Dátum úpravy',
+    'LBL_MODIFIED' => 'Zmenené podľa',
+    'LBL_CREATED' => 'Vytvorené podľa',
+    'LBL_ASSIGNED_TO' => 'Priradené ku',
+    'LBL_DESCRIPTION' => 'Popis',
+    'LNK_NEW_CAMPAIGN' => 'Vytvoriť kampaň',
+    'LNK_CAMPAIGN_LIST' => 'Kampane',
+    'LNK_NEW_PROSPECT_LIST' => 'Vytvoriť Cieľovú skupinu',
+    'LNK_PROSPECT_LIST_LIST' => 'Zobraziť Cieľové skupiny',
+    'LBL_MODIFIED_BY' => 'Zmenené podľa',
+    'LBL_CREATED_BY' => 'Vytvorené podľa',
+    'LNK_NEW_PROSPECT' => 'Vytvoriť cieľ',
+    'LNK_PROSPECT_LIST' => 'Ciele',
+
+    'LBL_CONTACTS_SUBPANEL_TITLE' => 'Kontakty',
+    'LBL_LEADS_SUBPANEL_TITLE' => 'Iniciatívy',
+    'LBL_PROSPECTS_SUBPANEL_TITLE' => 'Ciele',
+    'LBL_ACCOUNTS_SUBPANEL_TITLE' => 'Firmy',
+    'LBL_COPY_PREFIX' => 'Kópia',
+    'LBL_USERS_SUBPANEL_TITLE' => 'Užívatelia',
+    'LBL_TYPE' => 'Typ',
+    'LBL_LIST_TYPE' => 'Typ',
+    'LBL_LIST_TYPE_LIST_NAME' => 'Typ',
+    'LBL_NEW_FORM_TITLE' => 'Nový zoznam cieľov',
+    'LBL_MARKETING_NAME' => 'Názov marketingu',
+    'LBL_DOMAIN_NAME' => 'Názov domény',
+    'LBL_DOMAIN' => 'Žiadne emaily pre doménu',
+    'LBL_LIST_PROSPECTLIST_NAME' => 'Meno',
+
+    'LBL_EMAIL_MARKETING' => 'E-mailový marketing',
+
+    'LBL_ASCENDING' => 'Vzostupne (A-Z)',
+    'LBL_DESCENDING' => 'Zostupne (Z-A)',
+);
