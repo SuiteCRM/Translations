7a4778299b8e974bbcef2e8f14655e8e48f5e075
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-26 19:54:13 +0000

    New translations en_us.lang.php (Japanese)

---
+++ translations/ja_JP/modules/ExternalOAuthProvider/language/en_US.lang.php
@@ -0,0 +1,110 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2022 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = [
+
+    'LBL_ASSIGNED_TO_ID' => 'アサインされたユーザーID',
+    'LBL_ASSIGNED_TO_NAME' => '割り当て先',
+    'LBL_ID' => 'ID',
+    'LBL_DATE_ENTERED' => '作成日',
+    'LBL_DATE_MODIFIED' => '更新日',
+    'LBL_MODIFIED' => '更新者',
+    'LBL_MODIFIED_NAME' => '更新者',
+    'LBL_CREATED' => '作成者',
+    'LBL_DESCRIPTION' => '詳細',
+    'LBL_DELETED' => '削除済み',
+    'LBL_NAME' => '名前',
+    'LBL_CREATED_USER' => 'ユーザーが作成',
+    'LBL_MODIFIED_USER' => 'ユーザーが更新',
+    'LBL_LIST_NAME' => '名前',
+    'LBL_EDIT_BUTTON' => '編集',
+    'LBL_REMOVE' => '削除',
+    'LBL_LIST_FORM_TITLE' => 'External OAuth Provider List',
+    'LBL_MODULE_NAME' => '外部OAuthプロバイダ',
+    'LBL_MODULE_TITLE' => '外部OAuthプロバイダ',
+    'LBL_HOMEPAGE_TITLE' => 'My External OAuth Providers',
+    'LNK_NEW_RECORD' => 'Create External OAuth Provider',
+
+    'LNK_LIST' => '外部OAuthプロバイダ',
+    'LBL_SEARCH_FORM_TITLE' => 'Search External OAuth Providers',
+    'LBL_HISTORY_SUBPANEL_TITLE' => '履歴',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => '活動',
+    'LBL_NEW_FORM_TITLE' => 'New External OAuth Provider',
+
+    'LBL_LIST_DELETE' => '削除',
+    'LBL_TYPE' => 'タイプ',
+    'LBL_CONNECTOR' => 'Connector',
+    'LBL_REDIRECT_URI' => 'Redirect URI',
+    'LBL_REDIRECT_URI_TYPE' => 'Redirect URI Type',
+
+    'LBL_CLIENT_ID' => 'Client Id',
+    'LBL_CLIENT_SECRET' => 'Client Secret',
+    'LBL_SCOPE' => 'Scope',
+    'LBL_URL_AUTHORIZE' => 'Authorize Url',
+    'LBL_AUTHORIZE_URL_OPTIONS' => 'Authorize Url Options',
+    'LBL_URL_ACCESS_TOKEN' => 'Access Token Url',
+    'LBL_EXTRA_PROVIDER_PARAMS' => 'Extra Provider Params',
+    'LBL_GET_TOKEN_REQUEST_GRANT' => 'Get Token Request grant type',
+    'LBL_GET_TOKEN_REQUEST_OPTIONS' => 'Get Token Request options',
+    'LBL_REFRESH_TOKEN_REQUEST_GRANT' => 'Refresh Token Request Grant Type',
+    'LBL_REFRESH_TOKEN_REQUEST_OPTIONS' => 'トークン要求オプションのリフレッシュ',
+
+    'LBL_ACCESS_TOKEN_MAPPING' => 'アクセストークンマッピング',
+    'LBL_EXPIRES_IN_MAPPING' => 'マッピングで期限切れ',
+    'LBL_REFRESH_TOKEN_MAPPING' => 'トークンマッピングのリフレッシュ',
+    'LBL_TOKEN_TYPE_MAPPING' => 'トークンタイプのマッピング',
+
+    'LBL_EXTRA' => '追加構成',
+    'LBL_MAPPING' => 'マッピングの構成',
+    'LBL_OTHER' => 'その他',
+
+
+    'LNK_LIST_CREATE_NEW_PERSONAL' => '新しいOAuthプロバイダ',
+    'LNK_LIST_CREATE_NEW_GROUP' => '新しいグループOAuthプロバイダ',
+    'LNK_LIST_INBOUND_EMAILS' => '受信メールアカウント',
+    'LNK_LIST_OUTBOUND_EMAILS' => '送信メール アカウント',
+    'LNK_LIST_EXTERNAL_OAUTH_CONNECTION' => '外部OAuth接続',
+
+    'LBL_OWNER' => 'オーナー',
+];
