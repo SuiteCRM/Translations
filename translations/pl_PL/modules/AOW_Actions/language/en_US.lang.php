ce52ccc978c69a37737543a15dbef546f8a3cd64
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-26 19:06:22 +0000

    New translations en_us.lang.php (Polish)

---
+++ translations/pl_PL/modules/AOW_Actions/language/en_US.lang.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ID' => 'ID',
+    'LBL_DATE_ENTERED' => 'Data utworzenia',
+    'LBL_DATE_MODIFIED' => 'Data Modyfikacji',
+    'LBL_MODIFIED' => 'Zmodyfikowane przez',
+    'LBL_MODIFIED_ID' => 'Zmodyfikowane przez ID',
+    'LBL_MODIFIED_NAME' => 'Nazwa użytkownika modyfikującego',
+    'LBL_CREATED_USER' => 'Utworzony przez Użytkownika',
+    'LBL_MODIFIED_USER' => 'Zmodyfikowany przez Użytkownika',
+    'LBL_CREATED' => 'Utworzone przez',
+    'LBL_DESCRIPTION' => 'Opis',
+    'LBL_CREATED_ID' => 'ID tworzącego',
+    'LBL_DELETED' => 'Usunięte',
+    'LBL_NAME' => 'Nazwa',
+    'LBL_MODULE_NAME' => 'Akcje WorkFlow',
+    'LBL_MODULE_TITLE' => 'Akcje WorkFlow',
+    'LBL_AOW_WORKFLOW_ID' => 'Identyfikator AOW_WorkFlow',
+    'LBL_ACTION' => 'Działanie',
+    'LBL_PARAMETERS' => 'Parametry',
+    'LBL_SELECT_ACTION' => 'Wybierz działanie',
+    'LBL_CREATE_EMAIL_TEMPLATE' => 'Utwórz',
+    'LBL_RECORD_TYPE' => 'Typ wpisu',
+    'LBL_SENDEMAIL' => 'Wyślij wiadomość',
+    'LBL_CREATERECORD' => 'Utwórz wpis',
+    'LBL_MODIFYRECORD' => 'Modyfikuj wpis',
+    'LBL_ADD_FIELD' => 'Dodaj Pole',
+    'LBL_ADD_RELATIONSHIP' => 'Dodaj zależności',
+    'LBL_EDIT_EMAIL_TEMPLATE' => 'Edytuj',
+    'LBL_EMAIL' => 'E-mail',
+    'LBL_EMAIL_TEMPLATE' => 'Szablon e-mail',
+    'LBL_SETAPPROVAL' => 'Ustaw akceptację',
+    'LBL_RELATE_WORKFLOW' => 'Powiąż z modułem WorkFlow',
+    'LBL_INDIVIDUAL_EMAILS' => 'Wyślij indywidualne e-maile',
+    'LBL_COMPUTEFIELD' => 'Pola obliczane',
+    'LBL_COMPUTEFIELD_PARAMETERS' => 'Parametry',
+    'LBL_COMPUTEFIELD_FIELD_NAME' => 'Nazwa pola',
+    'LBL_COMPUTEFIELD_IDENTIFIER' => 'Identyfikator',
+    'LBL_COMPUTEFIELD_ADD_PARAMETER' => 'Dodaj parametr',
+    'LBL_COMPUTEFIELD_RELATION_PARAMETERS' => 'Parametry relacji',
+    'LBL_COMPUTEFIELD_RELATION' => 'Relacja',
+    'LBL_COMPUTEFIELD_ADD_RELATION_PARAMETER' => 'Dodaj parametr relacji',
+    'LBL_COMPUTEFIELD_FORMULA' => 'Formuła',
+    'LBL_COMPUTEFIELD_FORMULAS' => 'Formuły',
+    'LBL_COMPUTEFIELD_ADD_FORMULA' => 'Dodaj formułę',
+    'LBL_COMPUTEFIELD_VALUE_TYPE' => 'Typ wartości',
+    'LBL_COMPUTEFIELD_RAW_VALUE' => 'Wartość surowa (niestandaryzowana)',
+    'LBL_COMPUTEFIELD_FORMATTED_VALUE' => 'Wartość standaryzowana (formatowana)',
+    'LBL_COPY_EMAIL_ADDRESSES_WORKFLOW' => 'Kopiuj e-maile z modułu WorkFlow'
+);
