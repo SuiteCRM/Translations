3d05bcef2aea23f83a38b4fdd0fb1695e76f7ed6
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-26 18:22:06 +0000

    New translations en_us.lang.php (Czech)

---
+++ translations/cs_CZ/include/SugarObjects/templates/file/language/en_US.lang.php
@@ -0,0 +1,117 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    //module
+    'LBL_MODULE_NAME' => 'Dokumenty',
+    'LBL_MODULE_TITLE' => 'Dokumenty: Domů',
+    'LNK_NEW_DOCUMENT' => 'Vytvořit dokument',
+    'LNK_DOCUMENT_LIST' => 'Seznam dokumentů',
+    'LBL_SEARCH_FORM_TITLE' => 'Vyhledat dokument',
+    //vardef labels
+    'LBL_NAME' => 'Název dokumentu',
+    'LBL_DESCRIPTION' => 'Popis',
+    'LBL_ASSIGNED_TO' => 'Přiřazeno k:',
+    'LBL_CATEGORY' => 'Kategorie',
+    'LBL_SUBCATEGORY' => 'Podkategorie',
+    'LBL_STATUS' => 'Stav',
+    'LBL_IS_TEMPLATE' => 'Je šablona',
+    'LBL_TEMPLATE_TYPE' => 'Typ dokumentu',
+    'LBL_REVISION_NAME' => 'Číslo revize',
+    'LBL_MIME' => 'MIME typ',
+    'LBL_REVISION' => 'Revize',
+    'LBL_DOCUMENT' => 'Související dokument',
+    'LBL_LATEST_REVISION' => 'Poslední revize',
+    'LBL_CHANGE_LOG' => 'Protokol změn',
+    'LBL_ACTIVE_DATE' => 'Datum zveřejnění',
+    'LBL_EXPIRATION_DATE' => 'Datum vypršení',
+    'LBL_FILE_EXTENSION' => 'Přípona souboru',
+
+    'LBL_CAT_OR_SUBCAT_UNSPEC' => 'Neurčeno',
+    //quick search
+    'LBL_NEW_FORM_TITLE' => 'Nový dokument',
+    //document edit and detail view
+    'LBL_DOC_NAME' => 'Název dokumentu:',
+    'LBL_FILENAME' => 'Název souboru:',
+    'LBL_FILE_UPLOAD' => 'Soubor:',
+    'LBL_DOC_VERSION' => 'Revize:',
+    'LBL_CATEGORY_VALUE' => 'Kategorie:',
+    'LBL_SUBCATEGORY_VALUE' => 'Podkategorie:',
+    'LBL_DOC_STATUS' => 'Stav:',
+    'LBL_DET_TEMPLATE_TYPE' => 'Typ dokumentu:',
+    'LBL_DOC_DESCRIPTION' => 'Popis:',
+    'LBL_DOC_ACTIVE_DATE' => 'Datum zveřejnění:',
+    'LBL_DOC_EXP_DATE' => 'Datum vypršení:',
+
+    //document list view.
+    'LBL_LIST_FORM_TITLE' => 'Seznam dokumentů',
+    'LBL_LIST_DOCUMENT' => 'Dokumenty',
+    'LBL_LIST_CATEGORY' => 'Kategorie',
+    'LBL_LIST_SUBCATEGORY' => 'Podkategorie',
+    'LBL_LIST_REVISION' => 'Revize',
+    'LBL_LIST_LAST_REV_CREATOR' => 'Publikoval',
+    'LBL_LIST_LAST_REV_DATE' => 'Datum revize',
+    'LBL_LIST_VIEW_DOCUMENT' => 'Zobrazit',
+    'LBL_LIST_ACTIVE_DATE' => 'Datum zveřejnění',
+    'LBL_LIST_EXP_DATE' => 'Datum vypršení',
+    'LBL_LIST_STATUS' => 'Stav',
+
+    //document search form.
+    'LBL_SF_CATEGORY' => 'Kategorie:',
+    'LBL_SF_SUBCATEGORY' => 'Podkategorie:',
+
+    'DEF_CREATE_LOG' => 'Vytvořen dokument',
+
+    //error messages
+    'ERR_DOC_NAME' => 'Název dokumentu',
+    'ERR_DOC_ACTIVE_DATE' => 'Datum zveřejnění',
+    'ERR_FILENAME' => 'Název souboru',
+
+    //sub-panel vardefs.
+    'LBL_LIST_DOCUMENT_NAME' => 'Název dokumentu',
+
+    'LBL_EDIT_BUTTON' => 'Upravit ',
+    'LBL_REMOVE' => 'Odebrat',
+
+);
