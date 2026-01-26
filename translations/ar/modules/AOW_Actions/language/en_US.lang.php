75899e3cb37c9f1c012f143ce7655f39f969a989
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-26 19:05:59 +0000

    New translations en_us.lang.php (Arabic)

---
+++ translations/ar/modules/AOW_Actions/language/en_US.lang.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ID' => 'المعرف',
+    'LBL_DATE_ENTERED' => 'تاريخ الإنشاء',
+    'LBL_DATE_MODIFIED' => 'تاريخ التعديل',
+    'LBL_MODIFIED' => 'عُدل بواسطة',
+    'LBL_MODIFIED_ID' => 'عدل بواسطة المعرف',
+    'LBL_MODIFIED_NAME' => 'اسم من قام بالتعديل',
+    'LBL_CREATED_USER' => 'أنشئ بواسطة المستخدم',
+    'LBL_MODIFIED_USER' => 'عدل بواسطة المستخدم',
+    'LBL_CREATED' => 'أنشئ بواسطة',
+    'LBL_DESCRIPTION' => 'الوصف',
+    'LBL_CREATED_ID' => 'أنشئ بواسطة المعرف',
+    'LBL_DELETED' => 'محذوف',
+    'LBL_NAME' => 'الاسم',
+    'LBL_MODULE_NAME' => 'إجراءات سير العمل',
+    'LBL_MODULE_TITLE' => 'إجراءات سير العمل',
+    'LBL_AOW_WORKFLOW_ID' => 'معرف سير عمل AOW_WorkFlow',
+    'LBL_ACTION' => 'الإجراء',
+    'LBL_PARAMETERS' => 'المعطيات',
+    'LBL_SELECT_ACTION' => 'حدد الإجراء',
+    'LBL_CREATE_EMAIL_TEMPLATE' => 'إنشاء',
+    'LBL_RECORD_TYPE' => 'نوع السجل',
+    'LBL_SENDEMAIL' => 'إرسال البريد',
+    'LBL_CREATERECORD' => 'إنشاء سجل',
+    'LBL_MODIFYRECORD' => 'تعديل السجل',
+    'LBL_ADD_FIELD' => 'إضافة حقل',
+    'LBL_ADD_RELATIONSHIP' => 'إضافة علاقة',
+    'LBL_EDIT_EMAIL_TEMPLATE' => 'تحرير',
+    'LBL_EMAIL' => 'البريد الالكتروني',
+    'LBL_EMAIL_TEMPLATE' => 'قالب البريد الإلكتروني',
+    'LBL_SETAPPROVAL' => 'تعيين الموافقة',
+    'LBL_RELATE_WORKFLOW' => 'ربط بوحدة سير العمل',
+    'LBL_INDIVIDUAL_EMAILS' => 'إرسال رسائل بريد إلكتروني فردية',
+    'LBL_COMPUTEFIELD' => 'حساب الحقول',
+    'LBL_COMPUTEFIELD_PARAMETERS' => 'المعطيات',
+    'LBL_COMPUTEFIELD_FIELD_NAME' => 'اسم الحقل',
+    'LBL_COMPUTEFIELD_IDENTIFIER' => 'معرف',
+    'LBL_COMPUTEFIELD_ADD_PARAMETER' => 'إضافة معطى',
+    'LBL_COMPUTEFIELD_RELATION_PARAMETERS' => 'معطيات العلاقة',
+    'LBL_COMPUTEFIELD_RELATION' => 'العلاقة',
+    'LBL_COMPUTEFIELD_ADD_RELATION_PARAMETER' => 'إضافة معطى علاقة',
+    'LBL_COMPUTEFIELD_FORMULA' => 'معادلة',
+    'LBL_COMPUTEFIELD_FORMULAS' => 'معادلات',
+    'LBL_COMPUTEFIELD_ADD_FORMULA' => 'إضافة معادلة',
+    'LBL_COMPUTEFIELD_VALUE_TYPE' => 'نوع القيمة',
+    'LBL_COMPUTEFIELD_RAW_VALUE' => 'القيمة الخام',
+    'LBL_COMPUTEFIELD_FORMATTED_VALUE' => 'القيمة المنسقة',
+    'LBL_COPY_EMAIL_ADDRESSES_WORKFLOW' => 'نسخ رسائل البريد الإلكتروني من وحدة سير العمل'
+);
