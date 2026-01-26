6757299b1883de42b47130da379f4723f8dff0f9
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-26 19:35:45 +0000

    New translations en_us.lang.php (Urdu (Pakistan))

---
+++ translations/ur_PK/modules/Connectors/language/en_US.lang.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+
+$mod_strings = array(
+
+    'LBL_ADMINISTRATION_MAIN' => 'معاہدے کی ترتیب کرنے والا',
+    'LBL_AVAILABLE' => 'دستیاب',
+    'LBL_BACK' => 'پیچھے جانا',
+    'LBL_CONFIRM_CONTINUE_SAVE' => 'کچھ شعبوں کی ضرورت ہے جو خالی ہوچکے ہیں ان کو تبدیلی کے عمل محفوظ کرنا?',
+    'LBL_CONNECTOR_FIELDS' => 'شعبوں سے رابطہ کرنے والا',
+    'LBL_DATA' => 'کوائف معلومات',
+    'LBL_DEFAULT' => 'طے شدہ',
+    'LBL_DISABLED' => 'غیر فعال',
+    'LBL_ENABLED' => 'فعال کریں',
+    'LBL_EXTERNAL' => 'صارف کو قابل بنائیں بیرونی اکاؤنٹ بنانے کے لیے اور اس ریکارڈ کو رابطہ بنانے کے لیے.',
+    'LBL_EXTERNAL_SET_PROPERTIES' => ' جو اس اگر میرا پتہ کو استعمال کرے گا اس کی جے داد رابطے کے ساتھ ٹھیک ہو جائے گی اور جگہ کی ترتیب کا پیج.',
+    'LBL_MERGE' => 'کٹھہ کرنا',
+    'LBL_MODIFY_DISPLAY_TITLE' => 'رابطے کو اہل بنائیں',
+    'LBL_MODIFY_DISPLAY_DESC' => 'ہار اس ماڈل کا انتخاب کریں جو رابطے کے ساتھ کنکٹ ہے.',
+    'LBL_MODULE_FIELDS' => 'ماڈل کا شعبہ',
+    'LBL_MODIFY_MAPPING_TITLE' => 'نقشہ سے رابطہ کرنے والا شعبہ',
+    'LBL_MODIFY_MAPPING_DESC' => 'نقشہ سے رابطہ کرنے والا شعبہ ماڈل کے شعبہ کے آرڈر میں را پر لگتا ہے کون سے رابطے کر ڈیٹا ہم دیکھ سکتے ہیں اور ماڈل ریکارڈ کو ایک دوسرے کے ساتھ ملا سکتا ہے.',
+    'LBL_MODIFY_PROPERTIES_TITLE' => 'رابطے کی جائیداد کو ٹھیک کریں',
+    'LBL_MODIFY_PROPERTIES_DESC' => 'ہار جائیداد کی ترتیب کے لیے رابطے کے ساتھ جمع کریں یو آر ایل اور آئی پی آئی کی.',
+    'LBL_MODIFY_SEARCH_TITLE' => 'تلاش رابطے کا انتظام کریں',
+    'LBL_MODIFY_SEARCH' => 'تلاش کریں',
+    'LBL_MODIFY_SEARCH_DESC' => 'رابطہ کرنے والے شعبوں کا انتخاب کریں تلاش کرنے کے لئے ڈیٹا کو ہر ماڈل کے لیے.',
+    'LBL_MODULE_NAME' => 'جوڑنے والا',
+    'LBL_NO_PROPERTIES' => 'اس جیداد کی کوئی مطابقت نہیں ہے رابطہ کرنے کے لئے.',
+    'LBL_SAVE' => 'محفوظ کریں',
+    'LBL_SUMMARY' => 'خلاصہ',
+    'LBL_STEP1' => 'ڈیٹا کو تلاش کرے اور دیکھیں',
+    'LBL_STEP2' => 'ریکارڈ کے ساتھ ملائیں',
+    'LBL_TEST_SOURCE' => 'رابطے کی جانچ پڑتال کرنا',
+    'LBL_TEST_SOURCE_FAILED' => 'غلطی کی جانچ پڑتال کرنا',
+    'LBL_TEST_SOURCE_SUCCESS' => 'جانچ پڑتال کامیاب ہو گئی ہے',
+    'LBL_TITLE' => 'ڈیٹا کو ملانا',
+
+    'ERROR_NO_ADDITIONAL_DETAIL' => 'غلطی کی کوئی اضافی تفصیل موجود نہیں ہے جہاں سے یہ ریکارڈ ملا ہے.',
+    'ERROR_NO_SEARCHDEFS_DEFINED' => 'کوئی ماڈل ابھی تک کنیکٹر کے ساتھ رابطے کے لیے تیار نہیں ہوا رابطی کے لیے کسی ماڈل کا انتخاب کریں قابل کنٹر پیج میں.',
+    'ERROR_NO_SEARCHDEFS_MAPPED' => 'غلطی کوئی رابطے کے قابل نہیں ہے جس شعبے کو اپنے تلاش کیا اور تعریف کی.',
+    'ERROR_NO_SEARCHDEFS_MAPPING' => 'غلطی کوئی شعبہ تلاش میں نہیں ہے جو ماڈل کے رابطوں کی تعریف کرے رابطے کے لیے برائے مہربانی ایڈمنسٹریٹر سسٹم کے ساتھ رابطہ کریں.',
+    'ERROR_NO_DISPLAYABLE_MAPPED_FIELDS' => 'غلطی یہاں پر کسی ماڈل کا شعبہ نہیں ہے جس کا آپ نقشہ بنانا چاہتے ہیں نتائج میں دکھانے کے لئے برائے مہربانی سسٹم ایڈمنسٹریٹر کے ساتھ رابطہ کریں.',
+    'LBL_INFO_INLINE' => 'معلومات' /*for 508 compliance fix*/,
+    'LBL_CLOSE' => 'بند کریں' /*for 508 compliance fix*/,
+
+);
