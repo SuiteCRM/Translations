ebc88b27abe351a3db49dd5709a3a4b726a05ae4
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-26 20:48:23 +0000

    New translations en_us.lang.php (Czech)

---
+++ translations/cs_CZ/modules/SurveyResponses/language/en_US.lang.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID'                                                             => 'ID přiřazeného uživatele',
+    'LBL_ASSIGNED_TO_NAME'                                                           => 'Přiřazeno k',
+    'LBL_SECURITYGROUPS'                                                             => 'Skupiny zabezpečení',
+    'LBL_SECURITYGROUPS_SUBPANEL_TITLE'                                              => 'Skupiny zabezpečení',
+    'LBL_ID'                                                                         => 'ID',
+    'LBL_DATE_ENTERED'                                                               => 'Datum vytvoření',
+    'LBL_DATE_MODIFIED'                                                              => 'Datum změny',
+    'LBL_MODIFIED'                                                                   => 'Změnil',
+    'LBL_MODIFIED_ID'                                                                => 'Změnil ID',
+    'LBL_MODIFIED_NAME'                                                              => 'Změněno dle Jména',
+    'LBL_CREATED'                                                                    => 'Vytvořeno',
+    'LBL_CREATED_ID'                                                                 => 'Vytvořil ID',
+    'LBL_DESCRIPTION'                                                                => 'Popis',
+    'LBL_DELETED'                                                                    => 'Vymazáno',
+    'LBL_NAME'                                                                       => 'Název',
+    'LBL_CREATED_USER'                                                               => 'Vytvořeno dle Uživatele',
+    'LBL_MODIFIED_USER'                                                              => 'Změněno dle Uživatele',
+    'LBL_LIST_NAME'                                                                  => 'Název',
+    'LBL_EDIT_BUTTON'                                                                => 'Upravit',
+    'LBL_REMOVE'                                                                     => 'Odebrat',
+    'LBL_LIST_FORM_TITLE'                                                            => 'Seznam odpovědí na dotazník',
+    'LBL_MODULE_NAME'                                                                => 'Odpovědi na průzkum',
+    'LBL_MODULE_TITLE'                                                               => 'Odpovědi na průzkum',
+    'LBL_HOMEPAGE_TITLE'                                                             => 'Moje odpovědi na dotazník',
+    'LNK_NEW_RECORD'                                                                 => 'Vytvořit odpovědi dotazníku',
+    'LNK_LIST'                                                                       => 'Zobrazit odpovědi dotazníku',
+    'LNK_IMPORT_SURVEYRESPONSES'                                                     => 'Import odpovědi dotazníku',
+    'LBL_SEARCH_FORM_TITLE'                                                          => 'Vyhledat odpovědi na dotazník',
+    'LBL_HISTORY_SUBPANEL_TITLE'                                                     => 'Historie',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE'                                                  => 'Aktivity',
+    'LBL_SURVEYRESPONSES_SUBPANEL_TITLE'                                             => 'Odpovědi na průzkum',
+    'LBL_NEW_FORM_TITLE'                                                             => 'Nové odpovědi na dotazník',
+    'LBL_HAPPINESS'                                                                  => 'Spokojenost',
+    'LBL_EMAIL_RESPONSE_SENT'                                                        => 'Odpověď zaslaná emailem',
+    'LBL_SURVEYRESPONSES_SURVEYQUESTIONRESPONSES_FROM_SURVEYQUESTIONRESPONSES_TITLE' => 'Odpovědi na otázky průzkumů',
+    'LBL_SURVEYRESPONSES_CONTACTS_FROM_CONTACTS_TITLE'                               => 'Kontakt',
+    'LBL_SURVEYRESPONSES_ACCOUNTS_FROM_ACCOUNTS_TITLE'                               => 'Firma',
+    'LBL_SURVEYS_SURVEYRESPONSES_FROM_SURVEYS_TITLE'                                 => 'Dotazník',
+    'LBL_QUESTION_RESPONSES_DISPLAY'                                                 => 'Odpovědi na otázku',
+    'LBL_SURVEYRESPONSES_CAMPAIGNS_FROM_CAMPAIGNS_TITLE'                             => 'Kampaň',
+    'LBL_QUESTION' => 'Question',
+    'LBL_RESPONSE' => 'Response'
+
+);
