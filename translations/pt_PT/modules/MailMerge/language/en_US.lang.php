7c0c78333e7aa348a518dfdeda07077e707a02d1
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-26 20:09:45 +0000

    New translations en_us.lang.php (Portuguese)

---
+++ translations/pt_PT/modules/MailMerge/language/en_US.lang.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_STEP_1' => 'Passo 1: Selecione o módulo e o modelo',
+    'LBL_MAILMERGE_MODULE' => 'Selecionar módulo: ',
+    'LBL_MAILMERGE_SELECTED_MODULE' => 'Módulo selecionado: ',
+    'LBL_MAILMERGE_TEMPLATES' => 'Selecionar modelo: ',
+    'LBL_STEP_4' => 'Faça uma revisão e complete a operação',
+    'LBL_SELECTED_MODULE' => 'Módulo selecionado: ',
+    'LBL_SELECTED_TEMPLATE' => 'Modelo selecionado: ',
+    'LBL_SELECTED_ITEMS' => 'Itens selecionados: ',
+    'LBL_STEP_5' => 'Impressão em série concluída',
+    'LBL_MERGED_FILE' => 'Ficheiro fundido: ',
+    'LNK_NEW_MAILMERGE' => 'Iniciar impressão em série',
+    'LNK_UPLOAD_TEMPLATE' => 'Carregar modelo',
+    'LBL_DOC_NAME' => 'Nome do documento:',
+    'LBL_FILENAME' => 'Nome do Ficheiro:',
+    'LBL_DOC_VERSION' => 'Revisão:',
+    'LBL_DOC_DESCRIPTION' => 'Descrição:',
+    'LBL_LIST_NAME' => 'Nome',
+    'LBL_FINISH' => 'Iniciar fusão',
+    'LBL_NEXT' => 'Seguinte >',
+    'LBL_BACK' => '< Voltar',
+    'LBL_START' => 'Carregar aqui para continuar',
+    'LBL_CONTAINS_CONTACT_INFO' => 'O modelo selecionado contém relacionados',
+    'LBL_ADDIN_NOTICE' => 'Esta função requer a instalação do módulo adicional de Mail Merge do SuiteCRM para Microsoft Word.',
+    'LBL_REMOVE' => 'Remover item(ns)' /*for 508 compliance fix*/,
+    'LBL_ADD' => 'Adicionar Item(ns)' /*for 508 compliance fix*/,
+    'LBL_BROWSER_REQUIREMENT' => 'Isto requer IE 6.0 ou superior para Windows.',
+    'LBL_FORM_SELECT_TEMPLATE' => 'Por favor, selecione um modelo.',
+    'LBL_SELECT_ONE' => 'Por favor, selecione pelo menos um item.',
+    'LBL_AVAILABLE' => 'Disponível',
+    'LBL_SELECTED' => 'Selecionado',
+    'LBL_RELATED_INFO_SELECTION' => 'Use as seleções abaixo para identificar a informação relacionada, que gostaria de exibir no documento unido. Ignore esta etapa se o seu documento modelo não contém nenhuma informação relacionada.',
+
+);
