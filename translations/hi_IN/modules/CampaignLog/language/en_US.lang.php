f39db6c5b59d6a88c021fcd7263423921827fd80
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-26 19:23:04 +0000

    New translations en_us.lang.php (Hindi)

---
+++ translations/hi_IN/modules/CampaignLog/language/en_US.lang.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_LIST_ID' => 'संभावना सूची आईडी',
+    'LBL_ID' => 'आईडी',
+    'LBL_TARGET_TRACKER_KEY' => 'लक्ष्य ट्रैकर कुंजी',
+    'LBL_TARGET_ID' => 'लक्ष्य आईडी',
+    'LBL_TARGET_TYPE' => 'लक्ष्य प्रकार',
+    'LBL_ACTIVITY_TYPE' => 'गतिविधि प्रकार',
+    'LBL_ACTIVITY_DATE' => 'गतिविधि दिनांक',
+    'LBL_RELATED_ID' => 'संबंधित आईडी',
+    'LBL_RELATED_TYPE' => 'संबंधित प्रकार',
+    'LBL_DELETED' => 'हटाए गए',
+    'LBL_MODULE_NAME' => 'अभियान लॉग ',
+    'LBL_LIST_RECIPIENT_EMAIL' => 'प्राप्तकर्ता ईमेल',
+    'LBL_LIST_RECIPIENT_NAME' => 'प्राप्तकर्ता का नाम',
+    'LBL_ARCHIVED' => 'संग्रहीत',
+    'LBL_HITS' => 'प्राप्ति',
+
+    'LBL_CAMPAIGN_NAME' => 'नाम:',
+    'LBL_CAMPAIGN' => 'अभियान:',
+    'LBL_NAME' => 'नाम: ',
+    'LBL_INVITEE' => 'संपर्कों',
+    'LBL_LIST_CAMPAIGN_NAME' => 'अभियान',
+    'LBL_LIST_STATUS' => 'स्थिति',
+    'LBL_LIST_TYPE' => 'प्रकार',
+    'LBL_LIST_END_DATE' => 'समाप्ति दिनांक',
+    'LBL_DATE_ENTERED' => 'दर्ज किया गया दिनांक',
+    'LBL_DATE_MODIFIED' => 'तिथि संशोधित',
+    'LBL_MODIFIED' => 'द्वारा संशोधित: ',
+    'LBL_CREATED' => 'Created By: ',
+    'LBL_TEAM' => 'समूह: ',
+    'LBL_ASSIGNED_TO' => 'को सौंपना: ',
+    'LBL_CAMPAIGN_START_DATE' => 'प्रारंभ दिनांक: ',
+    'LBL_CAMPAIGN_END_DATE' => 'समाप्ति दिनांक: ',
+    'LBL_CAMPAIGN_STATUS' => 'स्थिति: ',
+    'LBL_CAMPAIGN_BUDGET' => 'बजट: ',
+    'LBL_CAMPAIGN_EXPECTED_COST' => 'अपेक्षित लागत: ',
+    'LBL_CAMPAIGN_ACTUAL_COST' => 'वास्तविक कीमत: ',
+    'LBL_CAMPAIGN_EXPECTED_REVENUE' => 'अपेक्षीत आमदनी: ',
+    'LBL_CAMPAIGN_TYPE' => 'प्रकार: ',
+    'LBL_CAMPAIGN_OBJECTIVE' => 'उद्देश्य: ',
+    'LBL_CAMPAIGN_CONTENT' => 'विवरण: ',
+    'LBL_CREATED_LEAD' => 'लीड बनाया गया',
+    'LBL_CREATED_CONTACT' => 'संपर्क बनाया गया',
+    'LBL_CREATED_OPPORTUNITY' => 'अवसर निर्मित किया गया',
+    'LBL_TARGETED_USER' => 'लक्षित उपयोगकर्ता',
+    'LBL_SENT_EMAIL' => 'ईमेल भेजी',
+    'LBL_LIST_FORM_TITLE' => 'लक्षित अभियान',
+    'LBL_LIST_ACTIVITY_DATE' => 'गतिविधि दिनांक',
+    'LBL_RELATED' => 'इसके बारे में',
+    'LBL_MORE_INFO' => 'अधिक जानकारी',
+
+    'LBL_CAMPAIGNS' => 'अभीयाँन ',
+    'LBL_LIST_MARKETING_NAME' => 'मार्केटिंग आईडी',
+
+    'LBL_MARKETING_ID' => 'मार्केटिंग आईडी',
+    'LBL_RELATED_NAME' => 'संबंधित नाम',
+);
