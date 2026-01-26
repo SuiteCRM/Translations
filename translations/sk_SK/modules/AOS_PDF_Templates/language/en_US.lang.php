a0b87da5baaf2edf12c6217f48c74d36aa48ee23
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-26 18:59:57 +0000

    New translations en_us.lang.php (Slovak)

---
+++ translations/sk_SK/modules/AOS_PDF_Templates/language/en_US.lang.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID' => 'Priradené ID používateľa',
+    'LBL_ASSIGNED_TO_NAME' => 'Priradené ku',
+    'LBL_ID' => 'ID',
+    'LBL_DATE_ENTERED' => 'Dátum vytvorenia',
+    'LBL_DATE_MODIFIED' => 'Dátum úpravy',
+    'LBL_MODIFIED' => 'Upravil',
+    'LBL_MODIFIED_NAME' => 'Upravil',
+    'LBL_CREATED' => 'Vytvoril',
+    'LBL_DESCRIPTION' => 'Telo',
+    'LBL_HEADER' => 'Hlavička',
+    'LBL_FOOTER' => 'Päta',
+    'LBL_DELETED' => 'Vymazané',
+    'LBL_NAME' => 'Meno',
+    'LBL_CREATED_USER' => 'Vytvoril',
+    'LBL_MODIFIED_USER' => 'Upravil',
+    'LBL_LIST_FORM_TITLE' => 'Zoznam PDF šablón',
+    'LBL_MODULE_NAME' => 'PDF Šablóny',
+    'LBL_MODULE_TITLE' => 'PDF šablóny: Domov',
+    'LBL_HOMEPAGE_TITLE' => 'Moje PDF šablóny',
+    'LNK_NEW_RECORD' => 'Vytvoriť PDF šablónu',
+    'LNK_LIST' => 'Zobraziť PDF šablóny',
+    'LBL_SEARCH_FORM_TITLE' => 'Hľadať PDF šablónu',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Zobraziť Históriu',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Aktivity',
+    'LBL_NEW_FORM_TITLE' => 'Nová PDF šablóna',
+    'LBL_TYPE' => 'Typ',
+    'LBL_ACTIVE' => 'Aktívny',
+    'LBL_BUTTON_INSERT' => 'Vložiť',
+    'LBL_WARNING_OVERWRITE' => 'Upozornenie: Toto prepíše súčasnú prácu',
+    'LBL_INSERT_FIELDS' => 'Vložiť polia',
+
+    'LBL_SAMPLE' => 'Načítať vzor',
+    'LBL_PAGE' => 'Strana',
+    'LBL_PREPARED_FOR' => 'Pripravené pre',
+    'LBL_PREPARED_BY' => 'Pripravil(a)',
+    'LBL_QUOTE_SAMPLE' => 'Vzor ponuky',
+    'LBL_INVOICE_SAMPLE' => 'Vzor faktúry',
+    'LBL_ACCOUNT_SAMPLE' => 'Account Sample',
+    'LBL_CONTACT_SAMPLE' => 'Vzor kontaktu',
+    'LBL_LEAD_SAMPLE' => 'Vzor možného klienta',
+    'LBL_ANY_STREET' => 'Ulica',
+    'LBL_ANY_TOWN' => 'Mesto',
+    'LBL_ANY_WHERE' => 'Kde',
+
+    'LBL_QUOTE_GROUP_SAMPLE' => 'Vzor skupinovej ponuky',
+    'LBL_INVOICE_GROUP_SAMPLE' => 'Vzor skupinovej faktúry',
+    'LBL_MARGIN_LEFT' => 'Ľavý okraj',
+    'LBL_MARGIN_RIGHT' => 'Pravý okraj',
+    'LBL_MARGIN_TOP' => 'Horný okraj',
+    'LBL_MARGIN_BOTTOM' => 'Dolný okraj',
+    'LBL_MARGIN_HEADER' => 'Okraj hlavičky',
+    'LBL_MARGIN_FOOTER' => 'Okraj päty',
+    'LBL_EDITVIEW_PANEL1' => 'Okraje',
+    'LBL_DETAILVIEW_PANEL1' => 'Okraje',
+    'LBL_PAGE_SIZE' => 'Veľkosť papiera',
+    'LBL_ORIENTATION' => 'Orientácia',
+);
