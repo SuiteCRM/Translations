7db0fbbb76320d03bb902bb341644261083612c6
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-26 20:50:39 +0000

    New translations en_us.lang.php (Cebuano)

---
+++ translations/ceb_PH/modules/Surveys/language/en_US.lang.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID'                                     => 'Mag assign ug user Id',
+    'LBL_ADD_OPTION'                                         => 'Add new option',
+    'LBL_ASSIGNED_TO_NAME'                                   => 'Gigahin kang',
+    'LBL_SECURITYGROUPS'                                     => 'Mga Grupo para sa seguridad',
+    'LBL_SECURITYGROUPS_SUBPANEL_TITLE'                      => 'Mga Grupo para sa seguridad',
+    'LBL_ID'                                                 => 'ID',
+    'LBL_DATE_ENTERED'                                       => 'Petsa nga gihimo',
+    'LBL_DATE_MODIFIED'                                      => 'Petsa nga gi-usab',
+    'LBL_MODIFIED'                                           => 'Giusab Ni',
+    'LBL_MODIFIED_ID'                                        => 'Ge bag-o ni Pangalan',
+    'LBL_MODIFIED_NAME'                                      => 'Giusab pinaagi sa Pangan',
+    'LBL_CREATED'                                            => 'Gihimo ni',
+    'LBL_CREATED_ID'                                         => 'Gebuhat ni',
+    'LBL_DESCRIPTION'                                        => 'Deskripsyon',
+    'LBL_DELETED'                                            => 'Gitangtang',
+    'LBL_NAME'                                               => 'Pangalan',
+    'LBL_CREATED_USER'                                       => 'Gihimo ni User',
+    'LBL_MODIFIED_USER'                                      => 'Giusab ni User',
+    'LBL_LIST_NAME'                                          => 'Pangalan',
+    'LBL_EDIT_BUTTON'                                        => 'Usba',
+    'LBL_REMOVE'                                             => 'Tangtangon',
+    'LBL_LIST_FORM_TITLE'                                    => 'Listahan sa mga survey',
+    'LBL_MODULE_NAME'                                        => 'Mga survey',
+    'LBL_MODULE_TITLE'                                       => 'Mga survey',
+    'LBL_HOMEPAGE_TITLE'                                     => 'Akong mga survey',
+    'LNK_NEW_RECORD'                                         => 'Himo ug mga survey',
+    'LNK_LIST'                                               => 'Tan-aw ug mga survey',
+    'LNK_IMPORT_SURVEYS'                                     => 'Import ug mga survey',
+    'LBL_SEARCH_FORM_TITLE'                                  => 'Mangita ug mga survey',
+    'LBL_HISTORY_SUBPANEL_TITLE'                             => 'Tan-awon ang Kasaysayan',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE'                          => 'Kalihukan',
+    'LBL_SURVEYS_SUBPANEL_TITLE'                             => 'Mga survey',
+    'LBL_NEW_FORM_TITLE'                                     => 'Bag-ong mga survey',
+    'LBL_STATUS'                                             => 'Stado',
+    'LBL_SURVEY_QUESTIONS_DISPLAY'                           => 'Mga pangutana',
+    'LBL_SURVEY_URL_DISPLAY'                                 => 'URL',
+    'LBL_HAPPINESS_QUESTION'                                 => 'Nalipay na Pangutana',
+    'LBL_CANT_EDIT_RESPONDED'                                => 'Mga pangutana sa survey nga naay mga tubag nga dili mailisan',
+    'LBL_VIEW_SURVEY_REPORTS'                                => 'Tan-aw sa mga report sa survey',
+    'LBL_CHECKED'                                            => 'Na-tsek',
+    'LBL_UNCHECKED'                                          => 'Dili tsekan',
+    'LBL_RESPONSE_ANSWER'                                    => 'Tubag',
+    'LBL_RESPONSE_CONTACT'                                   => 'Kontak',
+    'LBL_RESPONSE_TIME'                                      => 'Petsa',
+    'LBL_UNKNOWN_CONTACT'                                    => 'Wala Mailhi',
+    'LBL_RESPONSE_COUNT'                                     => 'Ihap',
+    'LBL_SUBMIT_TEXT'                                        => 'I-submit ang teksto',
+    'LBL_SATISFIED_TEXT'                                     => 'Natagbaw nga teksto',
+    'LBL_NEITHER_TEXT'                                       => 'Walay teksto',
+    'LBL_DISSATISFIED_TEXT'                                  => 'Wala matagbaw nga teksto',
+    'LBL_SURVEYS_SURVEYQUESTIONS_FROM_SURVEYQUESTIONS_TITLE' => 'Mga pangutana sa pagsurvey',
+    'LBL_SURVEYS_SURVEYRESPONSES_FROM_SURVEYRESPONSES_TITLE' => 'Mga pag tubag sa survey',
+    'LBL_SHOW_RESPONSES' => 'Ipakita ang mga tubag',
+    'LBL_HIDE_RESPONSES' => 'Hide responses',
+    'LBL_SURVEY_QUESTION' => 'Question',
+    'LBL_SURVEY_TEXT' => 'Teksto',
+    'LBL_SURVEY_TYPE' => 'Isulat',
+    'LBL_SURVEY_ACTIONS' => 'Mga Aksyon',
+    'LBL_SURVEY_NEW_QUESTION' => 'New Question',
+    'LBL_SURVEY_RESPONSES' => 'New Question',
+    'LBL_SURVEYS_SENT' => 'Surveys Sent:',
+    'LBL_SURVEY_DISTINCT' => 'Distinct Surveys Sent:',
+    'LBL_DRAFT' => 'Tig drap',
+    'LBL_SUBMIT' => 'I-summiter',
+    'LBL_STARS' => 'Stars',
+    'LBL_SURVEY_CLOSE_RESPONSE' => 'Thanks for your interest but this survey is now closed.',
+    'LBL_SURVEY_THANKS' => 'Thanks for completing this survey.',
+);
