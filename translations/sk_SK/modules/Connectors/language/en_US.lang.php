0e15cc21e67958a61a6ae75fb43eb6421c6677e2
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-26 19:35:37 +0000

    New translations en_us.lang.php (Slovak)

---
+++ translations/sk_SK/modules/Connectors/language/en_US.lang.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+
+$mod_strings = array(
+
+    'LBL_ADMINISTRATION_MAIN' => 'Nastavenie konektorov',
+    'LBL_AVAILABLE' => 'Dostupné',
+    'LBL_BACK' => '< Späť',
+    'LBL_CONFIRM_CONTINUE_SAVE' => 'Niektoré požadované údaje neboli vyplnené. Pokračovať v uložení zmien?',
+    'LBL_CONNECTOR_FIELDS' => 'Polia konektora',
+    'LBL_DATA' => 'Údaje',
+    'LBL_DEFAULT' => 'Prednastavený',
+    'LBL_DISABLED' => 'Blokovaný',
+    'LBL_ENABLED' => 'povolené',
+    'LBL_EXTERNAL' => 'Povoliť užívateľom vytvárať záznamy externých účtov k tomuto konektoru. Za účelom uplatňovania konektora, vlastnosti by mali byť určené na stránke nastavení Nastavenie vlastností konektora',
+    'LBL_EXTERNAL_SET_PROPERTIES' => 'Za účelom uplatňovania tohto konektora, vlastnosti by mali byť nastavené v Nastavenie konektora, na stránke nastavení',
+    'LBL_MERGE' => 'Zlúčiť',
+    'LBL_MODIFY_DISPLAY_TITLE' => 'Povoliť konektory',
+    'LBL_MODIFY_DISPLAY_DESC' => 'Vybrať, ktoré moduly budú povolené pre každý konektor.',
+    'LBL_MODULE_FIELDS' => 'Polia modulu',
+    'LBL_MODIFY_MAPPING_TITLE' => 'Mapa polí konektora',
+    'LBL_MODIFY_MAPPING_DESC' => 'Mapa polí konektora do polí modulu, za účelom vymedzenia dát, ktoré majú byť zobrazené a zlužované do záznamov modulu.',
+    'LBL_MODIFY_PROPERTIES_TITLE' => 'Nastavenie vlastností konektora',
+    'LBL_MODIFY_PROPERTIES_DESC' => 'Configurovať vlastnosti pre každý konektor, vrátane URL adries a API kľúčov.',
+    'LBL_MODIFY_SEARCH_TITLE' => 'Správa prehľadávania konektorov',
+    'LBL_MODIFY_SEARCH' => 'Vyhľadať',
+    'LBL_MODIFY_SEARCH_DESC' => 'Výber polí konektora k vyhľadávaniu pre každý modul.',
+    'LBL_MODULE_NAME' => 'Prípojky',
+    'LBL_NO_PROPERTIES' => 'Tu nie sú konfigurovateľné vlastnosti pre tento konektor.',
+    'LBL_SAVE' => 'Uložiť',
+    'LBL_SUMMARY' => 'Sumár',
+    'LBL_STEP1' => 'Údaje vyhľadať a zobraziť',
+    'LBL_STEP2' => 'Zlúčiť záznam s',
+    'LBL_TEST_SOURCE' => 'Test konektora',
+    'LBL_TEST_SOURCE_FAILED' => 'Test zlyhal',
+    'LBL_TEST_SOURCE_SUCCESS' => 'Test bol úspešný',
+    'LBL_TITLE' => 'Zlúčenie údajov',
+
+    'ERROR_NO_ADDITIONAL_DETAIL' => 'Chyba:  Nebolí nájdené žiadne rozširuhúce informácie pre tento záznam.',
+    'ERROR_NO_SEARCHDEFS_DEFINED' => 'Žiadne moduly neboli povolené pre tento konektor. Vyberte modul pre tento konektor na stránke Povolenie konektorov.',
+    'ERROR_NO_SEARCHDEFS_MAPPED' => 'Chyba: Žiadne konektory, povolené pre vyhľadávanie definovaných polí.',
+    'ERROR_NO_SEARCHDEFS_MAPPING' => 'Chyba: Žiadne polia definované pre vyhľadávanie pre tento modul a konektor. Prosím, kontaktujte správcu systému/Administrátora.',
+    'ERROR_NO_DISPLAYABLE_MAPPED_FIELDS' => 'Chyba: Žiadne zmapované polia modulu, pre zobrazenie vo výsledkoch. Prosím, kontaktujte správcu systému/Administrátora.',
+    'LBL_INFO_INLINE' => 'Info' /*for 508 compliance fix*/,
+    'LBL_CLOSE' => 'Uzavrieť' /*for 508 compliance fix*/,
+
+);
