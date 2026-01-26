6399613188bd52ed5d5606d3d3f492bad88bd7b2
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-26 20:02:28 +0000

    New translations en_us.lang.php (Tagalog)

---
+++ translations/tl_PH/modules/History/language/en_US.lang.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_MODULE_NAME' => 'Kasaysayan',
+    'LBL_MODULE_TITLE' => 'Kasaysayan: Bahay',
+    'LBL_SEARCH_FORM_TITLE' => 'Maghanap ng kasaysayan',
+    'LBL_LIST_FORM_TITLE' => 'Kasaysayan',
+    'LBL_LIST_SUBJECT' => 'Paksa',
+    'LBL_LIST_CONTACT' => 'Kontak',
+    'LBL_LIST_RELATED_TO' => 'Para sa kaugnayan',
+    'LBL_LIST_DATE' => 'Petsa',
+    'LBL_LIST_CLOSE' => 'Sarado',
+    'LBL_SUBJECT' => 'Ang Paksa:',
+    'LBL_STATUS' => 'Ang Kalagayan:',
+    'LBL_LOCATION' => 'Lokasyon:',
+    'LBL_DATE_TIME' => 'Magsimula sa petsa at oras:',
+    'LBL_DATE' => 'Magsimula sa petsa:',
+    'LBL_TIME' => 'Oras ng simula:',
+    'LBL_DURATION' => 'Tagal:',
+    'LBL_HOURS_MINS' => '(Mga oras o minuto)',
+    'LBL_CONTACT_NAME' => 'Pangalan ng contact: ',
+    'LBL_DESCRIPTION' => 'Ang Paglalarawan:',
+    'LNK_NEW_CALL' => 'I-log ang tawag',
+    'LNK_NEW_MEETING' => 'Naka-iskedyul na ang pagpupulong',
+    'LNK_NEW_TASK' => 'Gumawa ng Gawain',
+    'LNK_NEW_NOTE' => 'Gumawa ng Nota o Ilalakip',
+    'LNK_NEW_EMAIL' => 'Email na Nasinup',
+    'LNK_CALL_LIST' => 'Ang mga tawag',
+    'LNK_MEETING_LIST' => 'Ang mga pagpupulong',
+    'LNK_TASK_LIST' => 'Ang mga gawain',
+    'LNK_NOTE_LIST' => 'Ang mga tala',
+    'ERR_DELETE_RECORD' => 'Ang talaan ng numero ay dapat na tukuyin upang mabura ang akawnt.',
+    'LBL_INVITEE' => 'Mga imbitado',
+    'LBL_LIST_DIRECTION' => 'Direksyon',
+    'LBL_DIRECTION' => 'Direksyon',
+    'LNK_NEW_APPOINTMENT' => 'Bagong usapan',
+    'LNK_VIEW_CALENDAR' => 'Ngayon',
+    'LBL_OPEN_ACTIVITIES' => 'Bukas na mga aktibidad',
+    'LBL_HISTORY' => 'Kasaysayan',
+    'LBL_NEW_TASK_BUTTON_TITLE' => 'Gumawa ng Gawain',
+    'LBL_NEW_TASK_BUTTON_LABEL' => 'Gumawa ng Gawain',
+    'LBL_SCHEDULE_MEETING_BUTTON_TITLE' => 'Naka-iskedyul na ang pagpupulong',
+    'LBL_SCHEDULE_MEETING_BUTTON_LABEL' => 'Naka-iskedyul na ang pagpupulong',
+    'LBL_SCHEDULE_CALL_BUTTON_LABEL' => 'I-log ang tawag',
+    'LBL_NEW_NOTE_BUTTON_TITLE' => 'Gumawa ng Nota o Ilalakip',
+    'LBL_NEW_NOTE_BUTTON_LABEL' => 'Gumawa ng Nota o Ilalakip',
+    'LBL_TRACK_EMAIL_BUTTON_TITLE' => 'Email na Nasinup',
+    'LBL_TRACK_EMAIL_BUTTON_LABEL' => 'Email na Nasinup',
+    'LBL_LIST_STATUS' => 'Kalagayan',
+    'LBL_LIST_DUE_DATE' => 'Takdang petsa',
+    'LBL_LIST_LAST_MODIFIED' => 'Ang Huling Nabago',
+    'LNK_IMPORT_NOTES' => 'Ipasok ang mga tala',
+    'LBL_ACCEPT_THIS' => 'Tanggapin?',
+    'LBL_DEFAULT_SUBPANEL_TITLE' => 'Kasaysayan',
+
+    'LBL_CATEGORY' => 'Kategorya',
+    'LBL_LIST_CATEGORY' => 'Kategorya',
+);
