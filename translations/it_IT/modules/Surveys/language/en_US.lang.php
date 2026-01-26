6c133f9d8eab60366ed5d562219fcf53c0ac502f
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-26 20:49:48 +0000

    New translations en_us.lang.php (Italian)

---
+++ translations/it_IT/modules/Surveys/language/en_US.lang.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID'                                     => 'Id utente assegnato',
+    'LBL_ADD_OPTION'                                         => 'Aggiungi nuova opzione',
+    'LBL_ASSIGNED_TO_NAME'                                   => 'Assegnato a',
+    'LBL_SECURITYGROUPS'                                     => 'Gruppi di Sicurezza',
+    'LBL_SECURITYGROUPS_SUBPANEL_TITLE'                      => 'Gruppi di Sicurezza',
+    'LBL_ID'                                                 => 'ID',
+    'LBL_DATE_ENTERED'                                       => 'Data di Creazione',
+    'LBL_DATE_MODIFIED'                                      => 'Data Modifica',
+    'LBL_MODIFIED'                                           => 'Modificato da',
+    'LBL_MODIFIED_ID'                                        => 'Modificato da ID',
+    'LBL_MODIFIED_NAME'                                      => 'Modificato da Nome',
+    'LBL_CREATED'                                            => 'Creato da',
+    'LBL_CREATED_ID'                                         => 'Creato Da Id',
+    'LBL_DESCRIPTION'                                        => 'Descrizione',
+    'LBL_DELETED'                                            => 'Eliminato',
+    'LBL_NAME'                                               => 'Nome',
+    'LBL_CREATED_USER'                                       => 'Creato dall\'utente',
+    'LBL_MODIFIED_USER'                                      => 'Modificato dall\'utente',
+    'LBL_LIST_NAME'                                          => 'Nome',
+    'LBL_EDIT_BUTTON'                                        => 'Modifica',
+    'LBL_REMOVE'                                             => 'Cancella',
+    'LBL_LIST_FORM_TITLE'                                    => 'Elenco Sondaggi',
+    'LBL_MODULE_NAME'                                        => 'Sondaggi',
+    'LBL_MODULE_TITLE'                                       => 'Sondaggi',
+    'LBL_HOMEPAGE_TITLE'                                     => 'I Miei Sondaggi',
+    'LNK_NEW_RECORD'                                         => 'Crea Sondaggi',
+    'LNK_LIST'                                               => 'Visualizza Sondaggi',
+    'LNK_IMPORT_SURVEYS'                                     => 'Importa Sondaggi',
+    'LBL_SEARCH_FORM_TITLE'                                  => 'Cerca Sondaggi',
+    'LBL_HISTORY_SUBPANEL_TITLE'                             => 'Visualizza Cronologia',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE'                          => 'Attività',
+    'LBL_SURVEYS_SUBPANEL_TITLE'                             => 'Sondaggi',
+    'LBL_NEW_FORM_TITLE'                                     => 'Nuovi Sondaggi',
+    'LBL_STATUS'                                             => 'Stato',
+    'LBL_SURVEY_QUESTIONS_DISPLAY'                           => 'Domande',
+    'LBL_SURVEY_URL_DISPLAY'                                 => 'URL',
+    'LBL_HAPPINESS_QUESTION'                                 => 'Domanda di felicità',
+    'LBL_CANT_EDIT_RESPONDED'                                => 'Domande del sondaggio con risposte che non possono essere modificate',
+    'LBL_VIEW_SURVEY_REPORTS'                                => 'Vedi il report del sondaggio',
+    'LBL_CHECKED'                                            => 'Selezionato',
+    'LBL_UNCHECKED'                                          => 'Deselezionato',
+    'LBL_RESPONSE_ANSWER'                                    => 'Risposta',
+    'LBL_RESPONSE_CONTACT'                                   => 'Contatto',
+    'LBL_RESPONSE_TIME'                                      => 'Data',
+    'LBL_UNKNOWN_CONTACT'                                    => 'Sconosciuto',
+    'LBL_RESPONSE_COUNT'                                     => 'Conta',
+    'LBL_SUBMIT_TEXT'                                        => 'Invia Testo',
+    'LBL_SATISFIED_TEXT'                                     => 'Soddisfatto del Testo',
+    'LBL_NEITHER_TEXT'                                       => 'Nessun Testo',
+    'LBL_DISSATISFIED_TEXT'                                  => 'Insoddisfatto del Testo',
+    'LBL_SURVEYS_SURVEYQUESTIONS_FROM_SURVEYQUESTIONS_TITLE' => 'Domande del sondaggio',
+    'LBL_SURVEYS_SURVEYRESPONSES_FROM_SURVEYRESPONSES_TITLE' => 'Risposte al sondaggio',
+    'LBL_SHOW_RESPONSES' => 'Mostra Risposte',
+    'LBL_HIDE_RESPONSES' => 'Nascondi Risposte',
+    'LBL_SURVEY_QUESTION' => 'Domanda',
+    'LBL_SURVEY_TEXT' => 'Testo',
+    'LBL_SURVEY_TYPE' => 'Tipo',
+    'LBL_SURVEY_ACTIONS' => 'Azioni',
+    'LBL_SURVEY_NEW_QUESTION' => 'Nuova Domanda',
+    'LBL_SURVEY_RESPONSES' => 'Nuova Domanda',
+    'LBL_SURVEYS_SENT' => 'Sondaggi Inviati:',
+    'LBL_SURVEY_DISTINCT' => 'Sondaggi Distinti Inviati:',
+    'LBL_DRAFT' => 'Bozza',
+    'LBL_SUBMIT' => 'Invio',
+    'LBL_STARS' => 'Valutazione',
+    'LBL_SURVEY_CLOSE_RESPONSE' => 'Grazie per il vostro interesse, ma questo sondaggio è chiuso.',
+    'LBL_SURVEY_THANKS' => 'Thanks for completing this survey.',
+);
