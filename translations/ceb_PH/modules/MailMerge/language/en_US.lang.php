0b0764d9044f14acd152c3255f15fee8764ba8f6
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-26 20:10:27 +0000

    New translations en_us.lang.php (Cebuano)

---
+++ translations/ceb_PH/modules/MailMerge/language/en_US.lang.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_STEP_1' => 'Ang-ang isa: Pilia ang Module og Plaka',
+    'LBL_MAILMERGE_MODULE' => 'Pilia ang Module: ',
+    'LBL_MAILMERGE_SELECTED_MODULE' => 'Gepili ang Module: ',
+    'LBL_MAILMERGE_TEMPLATES' => 'Pilia ang Plaka: ',
+    'LBL_STEP_4' => 'Ge rebyo og Kumpleto',
+    'LBL_SELECTED_MODULE' => 'Gepili ang Module: ',
+    'LBL_SELECTED_TEMPLATE' => 'Pilia ang Plaka: ',
+    'LBL_SELECTED_ITEMS' => 'Pilia ang mga Item: ',
+    'LBL_STEP_5' => 'Suwata og Tipo aron ma Kumpleto',
+    'LBL_MERGED_FILE' => 'Tipoun ang File: ',
+    'LNK_NEW_MAILMERGE' => 'Sugdon ang Suwat og Tipoun',
+    'LNK_UPLOAD_TEMPLATE' => 'Upload ang Plaka',
+    'LBL_DOC_NAME' => 'Pangan sa Dokumento:',
+    'LBL_FILENAME' => 'Pangalan sa File:',
+    'LBL_DOC_VERSION' => 'Pag-usab:',
+    'LBL_DOC_DESCRIPTION' => 'Deskripsyon:',
+    'LBL_LIST_NAME' => 'Pangalan',
+    'LBL_FINISH' => 'Sugdan og Tipo',
+    'LBL_NEXT' => 'Sunod >',
+    'LBL_BACK' => '< Balik',
+    'LBL_START' => 'Pinduta Diria og Padayun',
+    'LBL_CONTAINS_CONTACT_INFO' => 'Pilia ang plaka mga sulod iglabot ',
+    'LBL_ADDIN_NOTICE' => 'Kini ge geapil ang installation sa SuiteCRM suwat tapo og dugangan sa Microsoft Word.',
+    'LBL_REMOVE' => 'Tangtanga ang mga item' /*for 508 compliance fix*/,
+    'LBL_ADD' => 'Dugangan og (mga) item' /*for 508 compliance fix*/,
+    'LBL_BROWSER_REQUIREMENT' => 'Kini nga mga apilon IE 6.0 o dako pa sa Windows.',
+    'LBL_FORM_SELECT_TEMPLATE' => 'Palihug ko pilia ang plaka.',
+    'LBL_SELECT_ONE' => 'Palihug kog apila maskin isa nga item.',
+    'LBL_AVAILABLE' => 'Naa',
+    'LBL_SELECTED' => 'Pili',
+    'LBL_RELATED_INFO_SELECTION' => 'Gamita ang pilianan sa ubos para ma hibaw-an nga apil raka ani nga impormasyon og gusto ka na eh display saimong tipo dokumento. Lab-ang aning ang-ang kung imong plaka dokumento nag sulod og mga walay relasyong dokumento.',
+
+);
