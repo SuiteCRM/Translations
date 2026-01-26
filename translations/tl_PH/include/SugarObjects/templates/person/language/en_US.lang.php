4460f905f4402b3df166c3fa4eb4d226ec2ac6bd
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-26 18:27:02 +0000

    New translations en_us.lang.php (Tagalog)

---
+++ translations/tl_PH/include/SugarObjects/templates/person/language/en_US.lang.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_SALUTATION' => 'Salutasyon',
+    'LBL_NAME' => 'Ang Pangalan',
+    'LBL_FIRST_NAME' => 'Unang pangalan',
+    'LBL_LAST_NAME' => 'Huling pangalan',
+    'LBL_TITLE' => 'Ang Pamagat',
+    'LBL_DEPARTMENT' => 'Departamento',
+    'LBL_DO_NOT_CALL' => 'Huwag Tumawag',
+    'LBL_HOME_PHONE' => 'Ang Telepono sa Bahay',
+    'LBL_MOBILE_PHONE' => 'Ang Mobile Phone',
+    'LBL_OFFICE_PHONE' => 'Ang Telepono ng opisina',
+    'LBL_OTHER_PHONE' => 'Ibang Telepono',
+    'LBL_FAX_PHONE' => 'Fax',
+    'LBL_EMAIL_ADDRESS' => 'Ang mga email address',
+    'LBL_PRIMARY_ADDRESS' => 'Ang Primerong Address',
+    'LBL_PRIMARY_ADDRESS_STREET' => 'Ang Primerong Address',
+    'LBL_PRIMARY_ADDRESS_STREET_2' => 'Ang Dalawang Kalye ng Pangunahing Tirahan:',
+    'LBL_PRIMARY_ADDRESS_STREET_3' => 'Ang Pangunahing Address Kalye 3:',
+    'LBL_PRIMARY_ADDRESS_CITY' => 'Ang Pangunahin na Siyudad',
+    'LBL_PRIMARY_ADDRESS_STATE' => 'Ang Pangunahin na Bansa',
+    'LBL_PRIMARY_ADDRESS_POSTALCODE' => 'Ang Pangunahin na Postal Code',
+    'LBL_PRIMARY_ADDRESS_COUNTRY' => 'Ang Pangunahing Address sa Bansa:',
+    'LBL_ALT_ADDRESS' => 'Ang Alternatibong Address',
+    'LBL_ALT_ADDRESS_STREET' => 'Ang Alternatibong Address',
+    'LBL_ALT_ADDRESS_STREET_2' => 'Ang Alternatibong Address kalye 2:',
+    'LBL_ALT_ADDRESS_STREET_3' => 'Ang Alternatibong address Kalye 3:',
+    'LBL_ALT_ADDRESS_CITY' => 'Ang Alternatibo na Siyudad',
+    'LBL_ALT_ADDRESS_STATE' => 'Ang Alternatibo na Bansa',
+    'LBL_ALT_ADDRESS_POSTALCODE' => 'Ang Alternatibo na Postal kowd',
+    'LBL_ALT_ADDRESS_COUNTRY' => 'Ang Alternatibo na Bansa',
+    'LBL_PRIMARY_STREET' => 'Tirahan',
+    'LBL_ALT_STREET' => 'Ibang Address',
+    'LBL_STREET' => 'Ibang Address',
+    'LBL_CITY' => 'Ang Syudad',
+    'LBL_STATE' => 'Ang Estado',
+    'LBL_POSTAL_CODE' => 'Kowd ng Postal',
+    'LBL_COUNTRY' => 'Bansa',
+    'LBL_CONTACT_INFORMATION' => 'Ang Kontak sa Impormasyon',
+    'LBL_ADDRESS_INFORMATION' => '(mga) address',
+    'LBL_ASSIGNED_TO_NAME' => 'Ang Tagagamit',
+    'LBL_OTHER_EMAIL_ADDRESS' => 'Ibang email:',
+    'LBL_ASSISTANT' => 'Assistant',
+    'LBL_ASSISTANT_PHONE' => 'Assistant ns telepono',
+    'LBL_WORK_PHONE' => 'Telepono sa Trabaho',
+    'LNK_IMPORT_VCARD' => 'Lumikha mula sa vCArd',
+    'LBL_ANY_EMAIL' => 'Kahit anong Email',
+    'LBL_EMAIL_NON_PRIMARY' => 'Ang hindi pangunahing mga email',
+    'LBL_PHOTO' => 'Litrato',
+    'LBL_EDIT_BUTTON' => 'I-edit',
+    'LBL_REMOVE' => 'Alisin',
+
+    //Lawful Basis labels
+    'LBL_LAWFUL_BASIS' => 'Lawful Basis',
+    'LBL_DATE_REVIEWED' => 'Lawful Basis Date Reviewed',
+    'LBL_LAWFUL_BASIS_SOURCE' => 'Lawful Basis Source',
+    'LBL_CONSENT' => 'Consent',
+    //End Lawful Basis labels
+);
