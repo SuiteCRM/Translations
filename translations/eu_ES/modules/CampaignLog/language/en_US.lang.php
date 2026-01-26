e1d3b53023fdbc2d9f10c65cdf1639f7d0a11df9
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-26 19:22:24 +0000

    New translations en_us.lang.php (Basque)

---
+++ translations/eu_ES/modules/CampaignLog/language/en_US.lang.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_LIST_ID' => 'Prospektu zerrenda ID',
+    'LBL_ID' => 'ID',
+    'LBL_TARGET_TRACKER_KEY' => 'Objektuaren jarraipen-gakoa',
+    'LBL_TARGET_ID' => 'Objektuaren ID',
+    'LBL_TARGET_TYPE' => 'Objektu-mota',
+    'LBL_ACTIVITY_TYPE' => 'Ekintza-mota',
+    'LBL_ACTIVITY_DATE' => 'Ekintza-data',
+    'LBL_RELATED_ID' => 'Erlazionatutako ID',
+    'LBL_RELATED_TYPE' => 'Erlazionatu mota',
+    'LBL_DELETED' => 'Ezabatua',
+    'LBL_MODULE_NAME' => 'Kanpainen erregistroa',
+    'LBL_LIST_RECIPIENT_EMAIL' => 'Hartzailearen helbide elektronikoa',
+    'LBL_LIST_RECIPIENT_NAME' => 'Hartzaile-izena',
+    'LBL_ARCHIVED' => 'Artxibatua',
+    'LBL_HITS' => 'Asmatzeak',
+
+    'LBL_CAMPAIGN_NAME' => 'Izena',
+    'LBL_CAMPAIGN' => 'Kanpaina',
+    'LBL_NAME' => 'Izena ',
+    'LBL_INVITEE' => 'Kontaktuak',
+    'LBL_LIST_CAMPAIGN_NAME' => 'Kanpainak',
+    'LBL_LIST_STATUS' => 'Egoera',
+    'LBL_LIST_TYPE' => 'Mota',
+    'LBL_LIST_END_DATE' => 'Amaiera-data',
+    'LBL_DATE_ENTERED' => 'Alta-data',
+    'LBL_DATE_MODIFIED' => 'Aldaketa-data',
+    'LBL_MODIFIED' => 'Aldatzailea ',
+    'LBL_CREATED' => 'Sortzailea: ',
+    'LBL_TEAM' => 'Ekipoa ',
+    'LBL_ASSIGNED_TO' => 'Esleitua izan da ',
+    'LBL_CAMPAIGN_START_DATE' => 'Hasiera-data ',
+    'LBL_CAMPAIGN_END_DATE' => 'Amaiera-data ',
+    'LBL_CAMPAIGN_STATUS' => 'Egoera ',
+    'LBL_CAMPAIGN_BUDGET' => 'Aurrekontua ',
+    'LBL_CAMPAIGN_EXPECTED_COST' => 'Zenbatetsitako kostua ',
+    'LBL_CAMPAIGN_ACTUAL_COST' => 'Benetako kostua ',
+    'LBL_CAMPAIGN_EXPECTED_REVENUE' => 'Zenbatetsitako sarrerak ',
+    'LBL_CAMPAIGN_TYPE' => 'Mota ',
+    'LBL_CAMPAIGN_OBJECTIVE' => 'Helburua ',
+    'LBL_CAMPAIGN_CONTENT' => 'Deskribapena ',
+    'LBL_CREATED_LEAD' => 'Created Lead',
+    'LBL_CREATED_CONTACT' => 'Created Contact',
+    'LBL_CREATED_OPPORTUNITY' => 'Created Opportunity',
+    'LBL_TARGETED_USER' => 'Targeted User',
+    'LBL_SENT_EMAIL' => 'Sent Email',
+    'LBL_LIST_FORM_TITLE' => 'Helburu diren kanpainak',
+    'LBL_LIST_ACTIVITY_DATE' => 'Ekintza-data',
+    'LBL_RELATED' => 'Erlazionatua',
+    'LBL_MORE_INFO' => 'Informazio gehiago',
+
+    'LBL_CAMPAIGNS' => 'Kanpainak',
+    'LBL_LIST_MARKETING_NAME' => 'Marketing Id-a',
+
+    'LBL_MARKETING_ID' => 'Marketing Id-a',
+    'LBL_RELATED_NAME' => 'Related Name',
+);
