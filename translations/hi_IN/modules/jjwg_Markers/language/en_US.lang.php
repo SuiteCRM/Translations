83a10e3b01a49bf10fbbbbbc35ec1c1054f1fcd1
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-26 21:03:08 +0000

    New translations en_us.lang.php (Hindi)

---
+++ translations/hi_IN/modules/jjwg_Markers/language/en_US.lang.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings['LBL_ASSIGNED_TO_ID'] = 'पहचानकर्ता कुंजी:
+$मॉड_स्ट्रिंग्स [लेबल_असाइन_टू_आईडी\']
+$मॉड_स्ट्रिंग्स [लेबल_असाइन_टू_आईडी\']';
+$mod_strings['LBL_ASSIGNED_TO_NAME'] = 'उपयोगकर्ता';
+$mod_strings['LBL_ID'] = 'आईडी';
+$mod_strings['LBL_DATE_ENTERED'] = 'बनाया गया दिनांक';
+$mod_strings['LBL_DATE_MODIFIED'] = 'तिथि संशोधित';
+$mod_strings['LBL_MODIFIED'] = 'द्वारा संशोधित';
+$mod_strings['LBL_MODIFIED_NAME'] = 'नाम से संशोधित';
+$mod_strings['LBL_CREATED'] = 'के द्वारा बनाई गई';
+$mod_strings['LBL_DESCRIPTION'] = 'विवरण';
+$mod_strings['LBL_DELETED'] = 'हटाए गए';
+$mod_strings['LBL_NAME'] = 'नाम';
+$mod_strings['LBL_CREATED_USER'] = 'उपयोगकर्ता द्वारा बनाया गया';
+$mod_strings['LBL_MODIFIED_USER'] = 'उपयोगकर्ता द्वारा संशोधित';
+$mod_strings['LBL_LIST_NAME'] = 'नाम';
+$mod_strings['LBL_LIST_FORM_TITLE'] = 'मार्कर सूची';
+$mod_strings['LBL_MODULE_NAME'] = 'मार्करों';
+$mod_strings['LBL_MODULE_TITLE'] = 'मार्करों';
+$mod_strings['LBL_HOMEPAGE_TITLE'] = 'मेरे मार्कर';
+$mod_strings['LNK_NEW_RECORD'] = 'मार्कर बनाएं';
+$mod_strings['LNK_LIST'] = 'मार्कर देखें';
+$mod_strings['LNK_IMPORT_JJWG_MARKERS'] = 'मार्कर आयात करें';
+$mod_strings['LBL_SEARCH_FORM_TITLE'] = 'खोज मार्कर';
+$mod_strings['LBL_HISTORY_SUBPANEL_TITLE'] = 'इतिहास देखें';
+$mod_strings['LBL_ACTIVITIES_SUBPANEL_TITLE'] = 'गतिविधियाँ';
+$mod_strings['LBL_NEW_FORM_TITLE'] = 'नए मार्कर';
+$mod_strings['LBL_CITY'] = 'शहर';
+$mod_strings['LBL_STATE'] = 'राज्य';
+$mod_strings['LBL_COUNTRY'] = 'देश';
+$mod_strings['LBL_JJWG_MAPS_LAT'] = 'अक्षांश';
+$mod_strings['LBL_JJWG_MAPS_LNG'] = 'देशांतर';
+$mod_strings['LBL_MARKER_IMAGE'] = 'मार्कर छवि प्रकार';
+$mod_strings['LBL_LIST_ASSIGNED_USER'] = 'को सौंपना';
+
+$mod_strings['LBL_MARKER_MARKER_POSITION'] = 'मार्कर स्थिति (अक्षांश, देशांतर):';
+$mod_strings['LBL_MARKER_CLOSEST_MATCHING_ADDRESS'] = 'निकटतम मिलान पता:';
+$mod_strings['LBL_MARKER_MARKER_STATUS'] = 'मार्कर स्थिति';
+$mod_strings['LBL_MARKER_EDIT_DESCRIPTION'] = 'मार्कर पर क्लिक करें और खींचें.';
+$mod_strings['LBL_JJWG_MAPS_JJWG_MARKERS_FROM_JJWG_MAPS_TITLE'] = 'मानचित्र';
