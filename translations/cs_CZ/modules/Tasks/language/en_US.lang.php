87a50f6436d3c2c8d93656885d2e5ba10806888a
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-26 20:50:51 +0000

    New translations en_us.lang.php (Czech)

---
+++ translations/cs_CZ/modules/Tasks/language/en_US.lang.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_MODULE_NAME' => 'Úkoly',
+    'LBL_MODULE_TITLE' => ' Úkoly: Domů',
+    'LBL_SEARCH_FORM_TITLE' => ' Vyhledat úkol',
+    'LBL_LIST_FORM_TITLE' => ' Seznam úkolů',
+    'LBL_NEW_FORM_TITLE' => ' Vytvořit úkol',
+    'LBL_LIST_CLOSE' => 'Zavřít',
+    'LBL_LIST_SUBJECT' => 'Předmět',
+    'LBL_LIST_CONTACT' => 'Kontakt',
+    'LBL_LIST_PRIORITY' => 'Priorita',
+    'LBL_LIST_RELATED_TO' => 'Související s',
+    'LBL_LIST_DUE_DATE' => 'Do data',
+    'LBL_LIST_DUE_TIME' => 'Termín (čas)',
+    'LBL_SUBJECT' => 'Předmět:',
+    'LBL_STATUS' => 'Stav:',
+    'LBL_DUE_DATE' => 'Do data:',
+    'LBL_DUE_TIME' => 'Udělat do (čas):',
+    'LBL_PRIORITY' => 'Priorita:',
+    'LBL_DUE_DATE_AND_TIME' => 'Do data a času:',
+    'LBL_START_DATE_AND_TIME' => 'Datum a čas začátku:',
+    'LBL_START_DATE' => 'Datum začátku:',
+    'LBL_LIST_START_DATE' => 'Datum začátku',
+    'LBL_START_TIME' => 'Čas začátku:',
+    'DATE_FORMAT' => '(yyyy-mm-dd)',
+    'LBL_NONE' => 'Žádný',
+    'LBL_CONTACT' => 'Kontakt:',
+    'LBL_EMAIL_ADDRESS' => 'Emailová adresa:',
+    'LBL_PHONE' => 'Telefon:',
+    'LBL_EMAIL' => 'Emailová adresa:',
+    'LBL_DESCRIPTION' => 'Popis:',
+    'LBL_NAME' => 'Název:',
+    'LBL_CONTACT_NAME' => 'Název kontaktu ',
+    'LBL_LIST_STATUS' => 'Stav',
+    'LBL_DATE_DUE_FLAG' => 'Bez termínu',
+    'LBL_DATE_START_FLAG' => 'Nezahájené datum',
+    'LBL_LIST_MY_TASKS' => 'Moje otevřené úkoly',
+    'LNK_NEW_TASK' => 'Vytvořit úkol',
+    'LNK_TASK_LIST' => 'Zobrazit úkoly',
+    'LNK_IMPORT_TASKS' => 'Import úkolů',
+    'LBL_LIST_ASSIGNED_TO_NAME' => 'Přiřazený uživatel',
+    'LBL_ASSIGNED_TO_NAME' => 'Přiřazeno k:',
+    'LBL_LIST_DATE_MODIFIED' => 'Datum změny',
+    'LBL_CONTACT_ID' => 'ID kontaktu:',
+    'LBL_PARENT_ID' => 'Rodičovské ID:',
+    'LBL_CONTACT_PHONE' => 'kontaktní telefon:',
+    'LBL_PARENT_TYPE' => 'Typ zdroje:',
+    'LBL_TASK_INFORMATION' => 'Přehled úkolu', //No need to be translated in all caps. Translation used just in menu action items when using the SuiteP template
+    'LBL_EDITLAYOUT' => 'Úprava rozvržení' /*for 508 compliance fix*/,
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Poznámky',
+    //For export labels
+    'LBL_DATE_DUE' => 'Do data',
+    'LBL_RELATED_TO' => 'Souvisí s:',
+);
