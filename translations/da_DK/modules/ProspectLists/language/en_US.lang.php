54abd89f0e13b33f787516b94c6bc929d0467a56
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-26 20:28:23 +0000

    New translations en_us.lang.php (Danish)

---
+++ translations/da_DK/modules/ProspectLists/language/en_US.lang.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_MODULE_NAME' => 'Mållister',
+    'LBL_MODULE_ID' => 'Mållister',
+    'LBL_MODULE_TITLE' => 'Målgruppe Liste: Hjem',
+    'LBL_SEARCH_FORM_TITLE' => 'Målgruppe Liste Søgning',
+    'LBL_LIST_FORM_TITLE' => 'Mållister',
+    'LBL_PROSPECT_LIST_NAME' => 'Navn',
+    'LBL_NAME' => 'Navn',
+    'LBL_ENTRIES' => 'Antal elementer',
+    'LBL_LIST_PROSPECT_LIST_NAME' => 'Målliste',
+    'LBL_LIST_ENTRIES' => 'Personer i Listen',
+    'LBL_LIST_DESCRIPTION' => 'Beskrivelse',
+    'LBL_LIST_TYPE_NO' => 'Type',
+    'LBL_LIST_END_DATE' => 'Slutdato',
+    'LBL_DATE_ENTERED' => 'Oprettelsesdato',
+    'LBL_MARKETING_ID' => 'Markedsførings id',
+    'LBL_DATE_MODIFIED' => 'Ændret dato',
+    'LBL_MODIFIED' => 'Ændret af',
+    'LBL_CREATED' => 'Oprettet af',
+    'LBL_ASSIGNED_TO' => 'Tildelt til',
+    'LBL_DESCRIPTION' => 'Beskrivelse',
+    'LNK_NEW_CAMPAIGN' => 'Opret kampagne',
+    'LNK_CAMPAIGN_LIST' => 'Kampagner',
+    'LNK_NEW_PROSPECT_LIST' => 'Opret målgruppe liste',
+    'LNK_PROSPECT_LIST_LIST' => 'Mållister',
+    'LBL_MODIFIED_BY' => 'Ændret af',
+    'LBL_CREATED_BY' => 'Oprettet af',
+    'LNK_NEW_PROSPECT' => 'Opret målgruppe',
+    'LNK_PROSPECT_LIST' => 'Målgrupper',
+
+    'LBL_CONTACTS_SUBPANEL_TITLE' => 'Kontakter',
+    'LBL_LEADS_SUBPANEL_TITLE' => 'Emner',
+    'LBL_PROSPECTS_SUBPANEL_TITLE' => 'Målgrupper',
+    'LBL_ACCOUNTS_SUBPANEL_TITLE' => 'Kunder',
+    'LBL_COPY_PREFIX' => 'Kopi af',
+    'LBL_USERS_SUBPANEL_TITLE' => 'Brugere',
+    'LBL_TYPE' => 'Type',
+    'LBL_LIST_TYPE' => 'Type',
+    'LBL_LIST_TYPE_LIST_NAME' => 'Type',
+    'LBL_NEW_FORM_TITLE' => 'Opret Mål Liste',
+    'LBL_MARKETING_NAME' => 'Marketing navn',
+    'LBL_DOMAIN_NAME' => 'Domæne Navn',
+    'LBL_DOMAIN' => 'Ingen emails til Domæne',
+    'LBL_LIST_PROSPECTLIST_NAME' => 'Navn',
+
+    'LBL_EMAIL_MARKETING' => 'Email markedsføring',
+
+    'LBL_ASCENDING' => 'Stigende',
+    'LBL_DESCENDING' => 'Faldende',
+);
