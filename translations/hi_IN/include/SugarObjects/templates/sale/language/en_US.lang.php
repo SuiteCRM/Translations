777583dc84473c0d2f274bd76c13443526d73bf3
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-26 18:29:47 +0000

    New translations en_us.lang.php (Hindi)

---
+++ translations/hi_IN/include/SugarObjects/templates/sale/language/en_US.lang.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_MODULE_NAME' => 'बिक्री',
+    'LBL_MODULE_TITLE' => 'बिक्री: होम',
+    'LBL_SEARCH_FORM_TITLE' => 'बिक्री खोज',
+    'LBL_LIST_FORM_TITLE' => 'बिक्री सूची',
+    'LBL_NAME' => 'बिक्री का नाम',
+    'LBL_LIST_SALE_NAME' => 'नाम',
+    'LBL_LIST_ACCOUNT_NAME' => 'खाता नाम',
+    'LBL_LIST_AMOUNT' => 'रकम',
+    'LBL_LIST_DATE_CLOSED' => 'बंद करें',
+    'LBL_LIST_SALE_STAGE' => 'बिक्री चरण',
+    'LBL_ACCOUNT_ID' => 'खाता आईडी',
+    //DON'T CONVERT THESE THEY ARE MAPPINGS
+    'db_name' => 'LBL_NAME',
+    //END DON'T CONVERT
+    'LBL_ACCOUNT_NAME' => 'खाते का नाम:',
+    'LBL_AMOUNT' => 'रकम:',
+    'LBL_AMOUNT_USDOLLAR' => 'राशि डोलर:',
+    'LBL_CURRENCY' => 'मुद्रा:',
+    'LBL_DATE_CLOSED' => 'अपेक्षित समापन तिथि:',
+    'LBL_TYPE' => 'प्रकार:',
+    'LBL_CAMPAIGN' => 'अभियान:',
+    'LBL_LEADS_SUBPANEL_TITLE' => 'नेतृत्व',
+    'LBL_PROJECTS_SUBPANEL_TITLE' => 'परियोजना ',
+    'LBL_NEXT_STEP' => 'अगला कदम:',
+    'LBL_LEAD_SOURCE' => 'लीड स्रोत:',
+    'LBL_SALES_STAGE' => 'बिक्री चरण:',
+    'LBL_PROBABILITY' => 'संभावना (%):',
+    'LBL_DESCRIPTION' => 'विवरण:',
+    'LBL_DUPLICATE' => 'संभावित डुप्लिकेट बिक्री',
+    'MSG_DUPLICATE' => 'आप जो बिक्री रिकॉर्ड बनाने जा रहे हैं, वह पहले से मौजूद किसी बिक्री रिकॉर्ड का डुप्लिकेट हो सकता है। समान नाम वाले बिक्री रिकॉर्ड नीचे सूचीबद्ध हैं।<br>इस नई बिक्री को बनाना जारी रखने के लिए सहेजें पर क्लिक करें, या बिक्री बनाए बिना मॉड्यूल पर वापस लौटने के लिए रद्द करें पर क्लिक करें।',
+    'LBL_NEW_FORM_TITLE' => 'बिक्री बनाएं',
+    'ERR_DELETE_RECORD' => 'बिक्री को हटाने के लिए रिकॉर्ड संख्या निर्दिष्ट की जानी चाहिए.',
+    'LBL_DEFAULT_SUBPANEL_TITLE' => 'बिक्री',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'गतिविधियाँ',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'इतिहास',
+
+    'LBL_CONTACTS_SUBPANEL_TITLE' => 'संपर्कों',
+    'LBL_ASSIGNED_TO_NAME' => 'उपयोगकर्ता:',
+    'LBL_LIST_ASSIGNED_TO_NAME' => 'निर्दिष्ट उपयोगकर्ता',
+    'LBL_ASSIGNED_TO_ID' => 'आईडी को सौंपा गया',
+    'LBL_MODIFIED_NAME' => 'उपयोगकर्ता नाम द्वारा संशोधित',
+    'LBL_SALE_INFORMATION' => 'बिक्री की जानकारी',
+    'LBL_CURRENCY_NAME' => 'मुद्रा का नाम',
+    'LBL_CURRENCY_SYMBOL' => 'मुद्रा चिह्न',
+    'LBL_EDIT_BUTTON' => 'संशोधन करें',
+    'LBL_REMOVE' => 'हटाऐं',
+
+);
