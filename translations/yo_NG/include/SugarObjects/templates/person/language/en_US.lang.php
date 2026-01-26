f1872baf3ea8c4aaafb9e7cad2a657e49fe3e249
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-26 18:27:14 +0000

    New translations en_us.lang.php (Yoruba)

---
+++ translations/yo_NG/include/SugarObjects/templates/person/language/en_US.lang.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_SALUTATION' => 'Kiki',
+    'LBL_NAME' => 'Oruko',
+    'LBL_FIRST_NAME' => 'Oruko akoko',
+    'LBL_LAST_NAME' => 'Oruko ikehin',
+    'LBL_TITLE' => 'Oye',
+    'LBL_DEPARTMENT' => 'Eka',
+    'LBL_DO_NOT_CALL' => 'Ma se pe',
+    'LBL_HOME_PHONE' => 'Foonu tile',
+    'LBL_MOBILE_PHONE' => 'Foonu alagbeka',
+    'LBL_OFFICE_PHONE' => 'Foonu Ofiisi',
+    'LBL_OTHER_PHONE' => 'Foonu miiran',
+    'LBL_FAX_PHONE' => 'Faksi',
+    'LBL_EMAIL_ADDRESS' => 'Imeeli adiresi (awon)',
+    'LBL_PRIMARY_ADDRESS' => 'Adiresi akoko',
+    'LBL_PRIMARY_ADDRESS_STREET' => 'Adiresi akoko',
+    'LBL_PRIMARY_ADDRESS_STREET_2' => 'Adiresi opopona ikeji:',
+    'LBL_PRIMARY_ADDRESS_STREET_3' => 'Adiresi opopona keta:',
+    'LBL_PRIMARY_ADDRESS_CITY' => 'Ilu akoko',
+    'LBL_PRIMARY_ADDRESS_STATE' => 'Ipinle akoko',
+    'LBL_PRIMARY_ADDRESS_POSTALCODE' => 'Primary Postal Code',
+    'LBL_PRIMARY_ADDRESS_COUNTRY' => 'Adiresi Orile-ede akoko:',
+    'LBL_ALT_ADDRESS' => 'Adiresi miiran',
+    'LBL_ALT_ADDRESS_STREET' => 'Adiresi miiran',
+    'LBL_ALT_ADDRESS_STREET_2' => 'Alternate Address Street 2:',
+    'LBL_ALT_ADDRESS_STREET_3' => 'Alternate Address Street 3:',
+    'LBL_ALT_ADDRESS_CITY' => 'Ilu miiran',
+    'LBL_ALT_ADDRESS_STATE' => 'Alternate State',
+    'LBL_ALT_ADDRESS_POSTALCODE' => 'Alternate Postal Code',
+    'LBL_ALT_ADDRESS_COUNTRY' => 'Alternate Country',
+    'LBL_PRIMARY_STREET' => 'Adiresi',
+    'LBL_ALT_STREET' => 'Adiresi miran',
+    'LBL_STREET' => 'Adiresi miran',
+    'LBL_CITY' => 'Ilu',
+    'LBL_STATE' => 'Ipo',
+    'LBL_POSTAL_CODE' => 'Koodu ifiweranse',
+    'LBL_COUNTRY' => 'Country',
+    'LBL_CONTACT_INFORMATION' => 'Contact Information',
+    'LBL_ADDRESS_INFORMATION' => 'Address(es)',
+    'LBL_ASSIGNED_TO_NAME' => 'Olumulo',
+    'LBL_OTHER_EMAIL_ADDRESS' => 'Emaili awon omiran:',
+    'LBL_ASSISTANT' => 'Iranlowo',
+    'LBL_ASSISTANT_PHONE' => 'Iranlowo Foonu',
+    'LBL_WORK_PHONE' => 'Work Phone',
+    'LNK_IMPORT_VCARD' => 'Create From vCard',
+    'LBL_ANY_EMAIL' => 'Any Email',
+    'LBL_EMAIL_NON_PRIMARY' => 'Atejishe ti kii se akoko',
+    'LBL_PHOTO' => 'Aworan',
+    'LBL_EDIT_BUTTON' => 'Satunko',
+    'LBL_REMOVE' => 'Yo kuro',
+
+    //Lawful Basis labels
+    'LBL_LAWFUL_BASIS' => 'Lawful Basis',
+    'LBL_DATE_REVIEWED' => 'Lawful Basis Date Reviewed',
+    'LBL_LAWFUL_BASIS_SOURCE' => 'Lawful Basis Source',
+    'LBL_CONSENT' => 'Consent',
+    //End Lawful Basis labels
+);
