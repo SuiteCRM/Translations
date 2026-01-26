ce3e1750b3dbc05c2110e8fd87458ec964cb7512
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-26 19:48:58 +0000

    New translations en_us.lang.php (Czech)

---
+++ translations/cs_CZ/modules/EmailTemplates/language/en_US.lang.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ADD_DOCUMENT' => 'Přidat dokument',
+    'LBL_ADD_FILE' => 'Přidat soubor',
+    'LBL_ATTACHMENTS' => 'Přílohy',
+    'LBL_BODY' => 'Tělo:',
+    'LBL_CLOSE' => 'Uzavřeno:',
+    'LBL_DESCRIPTION' => 'Popis:',
+    'LBL_EDIT_ALT_TEXT' => 'Upravit prostý text',
+    'LBL_EMAIL_ATTACHMENT' => 'Příloha zprávy',
+    'LBL_HIDE_ALT_TEXT' => 'Upravit prostý text',
+    'LBL_HTML_BODY' => 'Tělo HTML',
+    'LBL_INSERT_VARIABLE' => 'Vložit parametr:',
+    'LBL_INSERT_URL_REF' => 'Vložit referenční URL',
+    'LBL_INSERT_TRACKER_URL' => 'Vložit URL Sledování:',
+    'LBL_INSERT' => 'Vložit',
+    'LBL_LIST_DATE_MODIFIED' => 'Poslední změna',
+    'LBL_LIST_DESCRIPTION' => 'Popis',
+    'LBL_LIST_FORM_TITLE' => 'Šablony zpráv',
+    'LBL_LIST_NAME' => 'Název',
+    'LBL_MODULE_NAME' => 'Šablony emailů',
+    'LBL_MODULE_TITLE' => 'Šablony zpráv',
+    'LBL_NAME' => 'Název:',
+    'LBL_NEW_FORM_TITLE' => 'Přidat šablonu zprávy',
+    'LBL_PUBLISH' => 'Zveřejnit:',
+    'LBL_RELATED_TO' => 'Vztahuje se k:',
+    'LBL_SEARCH_FORM_TITLE' => 'Vyhledat šablony zpráv',
+    'LBL_SHOW_ALT_TEXT' => 'Zobrazit prostý text',
+    'LBL_SUBJECT' => 'Předmět:',
+    'LBL_SUITE_DOCUMENT' => 'Dokumenty',
+    'LBL_UPLOAD_FILE' => 'Upload File',
+    'LBL_TEXT_BODY' => 'Tělo textu',
+    'LBL_USERS' => 'Uživatelé',
+
+    'LNK_EMAIL_TEMPLATE_LIST' => 'Šablony emailů',
+    'LNK_IMPORT_NOTES' => 'Importovat poznámky',
+    'LNK_NEW_EMAIL_TEMPLATE' => 'Přidat šablonu zprávy',
+    'LNK_NEW_EMAIL' => 'Archivovat zprávu',
+    'LNK_NEW_SEND_EMAIL' => 'Nová zpráva',
+    'LNK_SENT_EMAIL_LIST' => 'Odeslané zprávy',
+    'LNK_VIEW_CALENDAR' => 'Dnes',
+    // for Inbox
+    'LBL_NEW' => 'Nový',
+    'LNK_MY_DRAFTS' => 'Koncepty',
+    'LNK_MY_INBOX' => 'Email',
+    'LBL_TEXT_ONLY' => 'Pouze text',
+    'LBL_SEND_AS_TEXT' => 'Odeslat pouze text',
+    'LBL_ACCOUNT' => 'Firma',
+    'LBL_FROM_NAME' => 'Jméno odesílatele',
+    'LBL_PLAIN_TEXT' => 'Čistý text',
+    'LBL_CREATED_BY' => 'Vytvořeno',
+    'LBL_PUBLISHED' => 'Zveřejněno',
+    'LNK_VIEW_MY_INBOX' => 'Zobrazit mé e-maily',
+    'LBL_ASSIGNED_TO_ID' => 'Přiřazeno k',
+    'LBL_EDIT_LAYOUT' => 'Úprava rozvržení' /*for 508 compliance fix*/,
+    'LBL_SELECT' => 'Vybrat' /*for 508 compliance fix*/,
+    'LBL_ID_FF_CLEAR' => 'Vyprázdnit' /*for 508 compliance fix*/,
+    'LBL_TYPE' => 'Typ',
+    'LBL_WIDTH' => 'Výchozí šířka',
+    'LNK_IMPORT_CAMPAIGNS' => 'Importovat kampaň',
+);
