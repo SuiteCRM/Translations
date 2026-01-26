40e5bd376680000a339b26c90535c35dc0b0935b
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-26 18:55:42 +0000

    New translations en_us.lang.php (Yoruba)

---
+++ translations/yo_NG/modules/AOR_Scheduled_Reports/language/en_US.lang.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID' => 'Onilo ti ayan',
+    'LBL_ASSIGNED_TO_NAME' => 'Ayan si',
+    'LBL_ID' => 'Idanimọ',
+    'LBL_DATE_ENTERED' => 'Ojo ti a seda',
+    'LBL_DATE_MODIFIED' => 'Ojo ti a se Atunse',
+    'LBL_MODIFIED' => 'Se iyipada nipasẹ',
+    'LBL_MODIFIED_NAME' => 'Atunse nipa Oruko',
+    'LBL_CREATED' => 'Siseda nipa',
+    'LBL_DESCRIPTION' => 'Apejuwe',
+    'LBL_DELETED' => 'Paare',
+    'LBL_NAME' => 'Oruko',
+    'LBL_CREATED_USER' => 'Seda nipase Onilo',
+    'LBL_MODIFIED_USER' => 'Atunse nipa se onilo',
+    'LBL_LIST_NAME' => 'Oruko',
+    'LBL_EDIT_BUTTON' => 'Satunko',
+    'LBL_REMOVE' => 'Yo kuro',
+    'LBL_ANY_EMAIL' => 'Eyikeyi emaili:',
+    'LBL_EMAIL_NON_PRIMARY' => 'Atejishe ti kii se akoko',
+    'LBL_ASSIGNED_TO' => 'Ayan si:',
+    'LBL_ASSIGNED_USER' => 'Ayan si:',
+    'LBL_EMAIL_RECIPIENTS' => 'Email Recipients:',
+    'NTC_DELETE_CONFIRMATION' => 'Se o da e loju pe o fe pa igbasile yii re?',
+    'LBL_LIST_FORM_TITLE' => 'Scheduled Reports List',
+    'LBL_MODULE_NAME' => 'Scheduled Reports',
+    'LBL_MODULE_TITLE' => 'Scheduled Reports',
+    'LBL_HOMEPAGE_TITLE' => 'My Scheduled Reports',
+    'LNK_NEW_RECORD' => 'Create Scheduled Reports',
+    'LNK_LIST' => 'View Scheduled Reports',
+    'LBL_SEARCH_FORM_TITLE' => 'Search Scheduled Reports',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Wo Itan to ti koja',
+    'LBL_NEW_FORM_TITLE' => 'New Scheduled Reports',
+    'LBL_SCHEDULE' => 'Schedule',
+    'LBL_AOR_REPORT_NAME' => 'Awon iroyin',
+    'LBL_SCHEDULED_REPORTS_INFORMATION' => 'Scheduled Reports',
+    'LBL_LAST_RUN' => 'Last run',
+    'LBL_STATUS' => 'Ipo',
+);
