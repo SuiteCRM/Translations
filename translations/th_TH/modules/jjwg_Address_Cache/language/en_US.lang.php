62f56aa2eb81a7a3b661027e2db0ffbf6e04d44b
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-26 20:57:48 +0000

    New translations en_us.lang.php (Thai)

---
+++ translations/th_TH/modules/jjwg_Address_Cache/language/en_US.lang.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings['LBL_ASSIGNED_TO_ID'] = 'ชื่อไอดีผู้รับผิดชอบ';
+$mod_strings['LBL_ASSIGNED_TO_NAME'] = 'ผู้รับผิดชอบ';
+$mod_strings['LBL_ID'] = 'ไอดี';
+$mod_strings['LBL_DATE_ENTERED'] = 'วันที่สร้าง';
+$mod_strings['LBL_DATE_MODIFIED'] = 'วันที่แก้ไข';
+$mod_strings['LBL_MODIFIED'] = 'แก้ไขโดย';
+$mod_strings['LBL_MODIFIED_NAME'] = 'แก้ไขโดยชื่อ';
+$mod_strings['LBL_CREATED'] = 'สร้างโดย';
+$mod_strings['LBL_DESCRIPTION'] = 'รายละเอียด';
+$mod_strings['LBL_DELETED'] = 'ลบ';
+$mod_strings['LBL_NAME'] = 'ที่อยู่';
+$mod_strings['LBL_CREATED_USER'] = 'สร้างโดยผู้ใช้งาน';
+$mod_strings['LBL_MODIFIED_USER'] = 'แก้ไขโดยผู้ใช้';
+$mod_strings['LBL_LIST_NAME'] = 'ที่อยู่';
+$mod_strings['LBL_LIST_FORM_TITLE'] = 'รายการที่อยู่แดช';
+$mod_strings['LBL_MODULE_NAME'] = 'ที่อยู่แคช';
+$mod_strings['LBL_MODULE_TITLE'] = 'ที่อยู่แคช';
+$mod_strings['LBL_HOMEPAGE_TITLE'] = 'ที่อยู่แคชของฉัน';
+$mod_strings['LNK_NEW_RECORD'] = 'สร้างที่อยู่แคช';
+$mod_strings['LNK_LIST'] = 'ดูที่อยู่แคช';
+$mod_strings['LBL_SEARCH_FORM_TITLE'] = 'ค้นหาที่อยู่แคช';
+$mod_strings['LBL_HISTORY_SUBPANEL_TITLE'] = 'ดูประวัติ';
+$mod_strings['LBL_ACTIVITIES_SUBPANEL_TITLE'] = 'กิจกรรม';
+$mod_strings['LBL_NEW_FORM_TITLE'] = 'ที่อยู่แคชใหม่';
+$mod_strings['LBL_LAT'] = 'ละติจูด';
+$mod_strings['LBL_LNG'] = 'ลองจิจูด';
+$mod_strings['LBL_CURRENT_USER_FILTER'] = 'รายการของฉัน';
+$mod_strings['LBL_ASSIGNED_TO'] = 'ผู้รับผิดชอบ';
