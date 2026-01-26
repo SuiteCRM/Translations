5cd722bd2651eba4e5543cee65ec97f6ded150cb
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-26 18:58:53 +0000

    New translations en_us.lang.php (Urdu (Pakistan))

---
+++ translations/ur_PK/modules/AOS_Line_Item_Groups/language/en_US.lang.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ID' => 'شناخت',
+    'LBL_MODULE_NAME' => 'انجمن',
+    'LBL_DATE_ENTERED' => 'تاریخ بنائی گئی ہے',
+    'LBL_DATE_MODIFIED' => 'تاریخ میں ترمیم',
+    'LBL_DESCRIPTION' => 'تفصیل',
+    'LBL_DELETED' => 'ختم کر دیا گیا',
+    'LBL_NAME' => 'گروپ کا نام',
+    'LBL_TOTAL_AMT' => 'حاصل جمع',
+    'LBL_DISCOUNT_AMOUNT' => 'ڈسکاؤنٹ',
+    'LBL_SUBTOTAL_AMOUNT' => 'ذیلی کل',
+    'LBL_TAX_AMOUNT' => 'ٹیکس',
+    'LBL_SUBTOTAL_TAX_AMOUNT' => 'ذیلی کل + ٹیکس',
+    'LBL_GROUP_TOTAL' => 'مکمل گروپ',
+    'LBL_LIST_NUM' => 'نمبر',
+    'LBL_SUBTOTAL_AMOUNT_USDOLLAR' => 'کرنسی کی غفلت کا گوشوارہ',
+    'LBL_DISCOUNT_AMOUNT_USDOLLAR' => 'کرنسی کی غفلت کی کٹوتی',
+    'LBL_TAX_AMOUNT_USDOLLAR' => 'کرنسی کی غفلت کا لگان مقرر کرنا',
+    'LBL_TOTAL_AMT_USDOLLAR' => 'مکمل کرنسی کی غفلت',
+    'LBL_GROUP_TOTAL_USDOLLAR' => 'گروپ کل (پہلے سے طے شدہ کرنسی)',
+    'LBL_SUBTOTAL_TAX_AMOUNT_USDOLLAR' => 'ذیلی کل + ٹیکس (ڈیفالٹ کرنسی)',
+    'LBL_PARENT_ID' => 'والدین کی شناخت',
+
+    'AOS_PRODUCT_QUOTES' => 'مصنوعات کی قطار',
+);
