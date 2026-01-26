eb51894cb1fdbe973eef406e4310fd679107dda8
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-26 19:53:53 +0000

    New translations en_us.lang.php (Romanian)

---
+++ translations/ro_RO/modules/ExternalOAuthProvider/language/en_US.lang.php
@@ -0,0 +1,110 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2022 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = [
+
+    'LBL_ASSIGNED_TO_ID' => 'Id atribuit utilizatorului',
+    'LBL_ASSIGNED_TO_NAME' => 'Atribuit lui',
+    'LBL_ID' => 'ID-ul',
+    'LBL_DATE_ENTERED' => 'Data creării',
+    'LBL_DATE_MODIFIED' => 'Data modificării',
+    'LBL_MODIFIED' => 'Modificat de',
+    'LBL_MODIFIED_NAME' => 'Nume modificant',
+    'LBL_CREATED' => 'Creat De',
+    'LBL_DESCRIPTION' => 'Descriere',
+    'LBL_DELETED' => 'Şters',
+    'LBL_NAME' => 'Nume',
+    'LBL_CREATED_USER' => 'Creat de Utilizator',
+    'LBL_MODIFIED_USER' => 'Modificat de utilizator',
+    'LBL_LIST_NAME' => 'Nume',
+    'LBL_EDIT_BUTTON' => 'Editați',
+    'LBL_REMOVE' => 'Înlăturați',
+    'LBL_LIST_FORM_TITLE' => 'Listă de furnizori externi OAuth',
+    'LBL_MODULE_NAME' => 'Furnizori externi OAuth',
+    'LBL_MODULE_TITLE' => 'Furnizori externi OAuth',
+    'LBL_HOMEPAGE_TITLE' => 'Furnizorii mei externi OAuth',
+    'LNK_NEW_RECORD' => 'Creați un furnizor extern OAuth',
+
+    'LNK_LIST' => 'Furnizori externi OAuth',
+    'LBL_SEARCH_FORM_TITLE' => 'Caută furnizori externi de OAuth',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Vizualizare Istoric',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Activităţi',
+    'LBL_NEW_FORM_TITLE' => 'Noul furnizor extern de OAuth',
+
+    'LBL_LIST_DELETE' => 'Ștergere',
+    'LBL_TYPE' => 'Tip',
+    'LBL_CONNECTOR' => 'Conector',
+    'LBL_REDIRECT_URI' => 'URL-ul de redirecționare',
+    'LBL_REDIRECT_URI_TYPE' => 'Redirect URI Type',
+
+    'LBL_CLIENT_ID' => 'Id client',
+    'LBL_CLIENT_SECRET' => 'Secret client',
+    'LBL_SCOPE' => 'Domeniu',
+    'LBL_URL_AUTHORIZE' => 'Autorizare Url',
+    'LBL_AUTHORIZE_URL_OPTIONS' => 'Autorizează opțiuni Url',
+    'LBL_URL_ACCESS_TOKEN' => 'Url Token de acces',
+    'LBL_EXTRA_PROVIDER_PARAMS' => 'Parametri suplimentari ai furnizorului',
+    'LBL_GET_TOKEN_REQUEST_GRANT' => 'Obține tipul de subvenție al cererii de Token',
+    'LBL_GET_TOKEN_REQUEST_OPTIONS' => 'Obțineți opțiuni de solicitare a token',
+    'LBL_REFRESH_TOKEN_REQUEST_GRANT' => 'Solicitare Token de permisiune',
+    'LBL_REFRESH_TOKEN_REQUEST_OPTIONS' => 'Solicitare Token de Reîmprospătare',
+
+    'LBL_ACCESS_TOKEN_MAPPING' => 'Mapare Token de acces',
+    'LBL_EXPIRES_IN_MAPPING' => 'Expiră în Mapare',
+    'LBL_REFRESH_TOKEN_MAPPING' => 'Actualizare mapări Token',
+    'LBL_TOKEN_TYPE_MAPPING' => 'Mapări tip Token',
+
+    'LBL_EXTRA' => 'Configurații suplimentare',
+    'LBL_MAPPING' => 'Configurații de mapare',
+    'LBL_OTHER' => 'Altele',
+
+
+    'LNK_LIST_CREATE_NEW_PERSONAL' => 'New Personal OAuth Provider',
+    'LNK_LIST_CREATE_NEW_GROUP' => 'New Group OAuth Provider',
+    'LNK_LIST_INBOUND_EMAILS' => 'Intrare conturi de email',
+    'LNK_LIST_OUTBOUND_EMAILS' => 'Conturi de e-mail de ieșire',
+    'LNK_LIST_EXTERNAL_OAUTH_CONNECTION' => 'Conexiuni OAuth externe',
+
+    'LBL_OWNER' => 'Proprietar',
+];
