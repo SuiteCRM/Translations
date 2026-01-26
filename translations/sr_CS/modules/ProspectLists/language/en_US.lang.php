dc13a0933b58fef5f6cc15b35ae2d50fecf37eee
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-26 20:29:16 +0000

    New translations en_us.lang.php (Serbian (Latin))

---
+++ translations/sr_CS/modules/ProspectLists/language/en_US.lang.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_MODULE_NAME' => 'Liste ciljeva',
+    'LBL_MODULE_ID' => 'Liste ciljeva',
+    'LBL_MODULE_TITLE' => 'Lista ciljeva: Početna strana',
+    'LBL_SEARCH_FORM_TITLE' => 'Pretraga lista ciljeva',
+    'LBL_LIST_FORM_TITLE' => 'Liste ciljeva',
+    'LBL_PROSPECT_LIST_NAME' => 'Lista ciljeva:',
+    'LBL_NAME' => 'Ime',
+    'LBL_ENTRIES' => 'Ukupno unosa',
+    'LBL_LIST_PROSPECT_LIST_NAME' => 'Lista ciljeva',
+    'LBL_LIST_ENTRIES' => 'Ciljevi u listi',
+    'LBL_LIST_DESCRIPTION' => 'Opis',
+    'LBL_LIST_TYPE_NO' => 'Tip',
+    'LBL_LIST_END_DATE' => 'Krajnji datum',
+    'LBL_DATE_ENTERED' => 'Datum kreiranja',
+    'LBL_MARKETING_ID' => 'Redni broj Marketinga',
+    'LBL_DATE_MODIFIED' => 'Datum izmene',
+    'LBL_MODIFIED' => 'Promenio',
+    'LBL_CREATED' => 'Autor',
+    'LBL_ASSIGNED_TO' => 'Dodeljen',
+    'LBL_DESCRIPTION' => 'Opis',
+    'LNK_NEW_CAMPAIGN' => 'Kreiraj kampanju',
+    'LNK_CAMPAIGN_LIST' => 'Kampanje',
+    'LNK_NEW_PROSPECT_LIST' => 'Kreiranje liste ciljeva',
+    'LNK_PROSPECT_LIST_LIST' => 'Pregled liste ciljnih grupa',
+    'LBL_MODIFIED_BY' => 'Promenio',
+    'LBL_CREATED_BY' => 'Autor',
+    'LNK_NEW_PROSPECT' => 'Kreiranje cilja',
+    'LNK_PROSPECT_LIST' => 'Ciljevi',
+
+    'LBL_CONTACTS_SUBPANEL_TITLE' => 'Kontakti',
+    'LBL_LEADS_SUBPANEL_TITLE' => 'Potencijalni klijenti',
+    'LBL_PROSPECTS_SUBPANEL_TITLE' => 'Ciljevi',
+    'LBL_ACCOUNTS_SUBPANEL_TITLE' => 'Kompanije',
+    'LBL_COPY_PREFIX' => 'Kopija',
+    'LBL_USERS_SUBPANEL_TITLE' => 'Korisnici',
+    'LBL_TYPE' => 'Tip',
+    'LBL_LIST_TYPE' => 'Tip',
+    'LBL_LIST_TYPE_LIST_NAME' => 'Tip',
+    'LBL_NEW_FORM_TITLE' => 'Nova lista ciljeva',
+    'LBL_MARKETING_NAME' => 'Ime marketinga',
+    'LBL_DOMAIN_NAME' => 'Ime domena',
+    'LBL_DOMAIN' => 'Nema elektronskih poruka na domenu',
+    'LBL_LIST_PROSPECTLIST_NAME' => 'Ime',
+
+    'LBL_EMAIL_MARKETING' => 'Email Marketing',
+
+    'LBL_ASCENDING' => 'Rastući',
+    'LBL_DESCENDING' => 'Opadajući',
+);
