5c9e75c79a82c81570492aabb3143bd2e04defdd
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-27 18:54:11 +0000

    New translations en_us.lang.php (Chinese Traditional)

---
+++ translations/zh_TW/modules/AOS_PDF_Templates/language/zh_TW.lang.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID' => '負責人Id',
+    'LBL_ASSIGNED_TO_NAME' => '負責人',
+    'LBL_ID' => '編號',
+    'LBL_DATE_ENTERED' => '建立日期',
+    'LBL_DATE_MODIFIED' => '修改日期',
+    'LBL_MODIFIED' => '修改人',
+    'LBL_MODIFIED_NAME' => '修改人姓名',
+    'LBL_CREATED' => '建立人',
+    'LBL_DESCRIPTION' => '正文',
+    'LBL_HEADER' => '頁首',
+    'LBL_FOOTER' => '頁尾',
+    'LBL_DELETED' => '已刪除',
+    'LBL_NAME' => '名稱',
+    'LBL_CREATED_USER' => '創建者',
+    'LBL_MODIFIED_USER' => '修改的使用者',
+    'LBL_LIST_FORM_TITLE' => 'PDF 範本清單',
+    'LBL_MODULE_NAME' => 'PDF 範本',
+    'LBL_MODULE_TITLE' => 'PDF 範本: 首頁',
+    'LBL_HOMEPAGE_TITLE' => '我的 PDF 範本',
+    'LNK_NEW_RECORD' => '創建 PDF 範本',
+    'LNK_LIST' => '查看 PDF 範本',
+    'LBL_SEARCH_FORM_TITLE' => '搜尋 PDF 範本',
+    'LBL_HISTORY_SUBPANEL_TITLE' => '查看歷史記錄',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => '銷售活動',
+    'LBL_NEW_FORM_TITLE' => '新建 PDF 範本',
+    'LBL_TYPE' => '類型',
+    'LBL_ACTIVE' => '啟用',
+    'LBL_BUTTON_INSERT' => '插入',
+    'LBL_WARNING_OVERWRITE' => '警告這會覆蓋你目前的工作',
+    'LBL_INSERT_FIELDS' => '插入欄位',
+
+    'LBL_SAMPLE' => '載入範例',
+    'LBL_PAGE' => '頁',
+    'LBL_PREPARED_FOR' => '準備給',
+    'LBL_PREPARED_BY' => '準備人',
+    'LBL_QUOTE_SAMPLE' => '報價範例',
+    'LBL_INVOICE_SAMPLE' => '發票範例',
+    'LBL_ACCOUNT_SAMPLE' => '帳戶範例',
+    'LBL_CONTACT_SAMPLE' => '聯絡人範例',
+    'LBL_LEAD_SAMPLE' => '潛在客戶範例',
+    'LBL_ANY_STREET' => '任何街道',
+    'LBL_ANY_TOWN' => '任何城鎮',
+    'LBL_ANY_WHERE' => '任何地方',
+
+    'LBL_QUOTE_GROUP_SAMPLE' => '報價組範例',
+    'LBL_INVOICE_GROUP_SAMPLE' => '發票組範例',
+    'LBL_MARGIN_LEFT' => '左邊距',
+    'LBL_MARGIN_RIGHT' => '右邊距',
+    'LBL_MARGIN_TOP' => '上邊距',
+    'LBL_MARGIN_BOTTOM' => '下邊距',
+    'LBL_MARGIN_HEADER' => '頁首邊距',
+    'LBL_MARGIN_FOOTER' => '頁尾邊距',
+    'LBL_EDITVIEW_PANEL1' => '邊距',
+    'LBL_DETAILVIEW_PANEL1' => '邊距',
+    'LBL_PAGE_SIZE' => '頁面大小',
+    'LBL_ORIENTATION' => '方向',
+);
