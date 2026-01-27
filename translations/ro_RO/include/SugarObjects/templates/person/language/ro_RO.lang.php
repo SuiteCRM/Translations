5001cd941e7dbd3a5475e59ff6c9640d21e9835c
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-27 18:17:48 +0000

    New translations en_us.lang.php (Romanian)

---
+++ translations/ro_RO/include/SugarObjects/templates/person/language/ro_RO.lang.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_SALUTATION' => 'Apelativ',
+    'LBL_NAME' => 'Nume',
+    'LBL_FIRST_NAME' => 'Prenume',
+    'LBL_LAST_NAME' => 'Nume de Familie',
+    'LBL_TITLE' => 'Titlu',
+    'LBL_DEPARTMENT' => 'Departament',
+    'LBL_DO_NOT_CALL' => 'Nu suna',
+    'LBL_HOME_PHONE' => 'Telefon Acasă',
+    'LBL_MOBILE_PHONE' => 'Telefon Mobil',
+    'LBL_OFFICE_PHONE' => 'Telefon Birou',
+    'LBL_OTHER_PHONE' => 'Alt Telefon',
+    'LBL_FAX_PHONE' => 'Fax',
+    'LBL_EMAIL_ADDRESS' => 'Adresă(e) Email',
+    'LBL_PRIMARY_ADDRESS' => 'Adresă Principală',
+    'LBL_PRIMARY_ADDRESS_STREET' => 'Adresă Principală',
+    'LBL_PRIMARY_ADDRESS_STREET_2' => 'Adresă Principală Stradă 2:',
+    'LBL_PRIMARY_ADDRESS_STREET_3' => 'Adresă Principală Stradă 3:',
+    'LBL_PRIMARY_ADDRESS_CITY' => 'Oraș principal',
+    'LBL_PRIMARY_ADDRESS_STATE' => 'Statul Principal',
+    'LBL_PRIMARY_ADDRESS_POSTALCODE' => 'Cod postal principal',
+    'LBL_PRIMARY_ADDRESS_COUNTRY' => 'Adresă principală - Țară:',
+    'LBL_ALT_ADDRESS' => 'Alta Adresa',
+    'LBL_ALT_ADDRESS_STREET' => 'Alta Adresa',
+    'LBL_ALT_ADDRESS_STREET_2' => 'Adresă Alternativă Stradă 2:',
+    'LBL_ALT_ADDRESS_STREET_3' => 'Adresa Supleant Strada 3:',
+    'LBL_ALT_ADDRESS_CITY' => 'Oraș alternativ',
+    'LBL_ALT_ADDRESS_STATE' => 'Statul Supleant',
+    'LBL_ALT_ADDRESS_POSTALCODE' => 'Cod Postal Supleant',
+    'LBL_ALT_ADDRESS_COUNTRY' => 'Țară alternativă',
+    'LBL_PRIMARY_STREET' => 'Adresă',
+    'LBL_ALT_STREET' => 'Alta adresa',
+    'LBL_STREET' => 'Alta adresa',
+    'LBL_CITY' => 'Oraș',
+    'LBL_STATE' => 'Stat',
+    'LBL_POSTAL_CODE' => 'Cod Poștal',
+    'LBL_COUNTRY' => 'Ţară',
+    'LBL_CONTACT_INFORMATION' => 'Informații Contact',
+    'LBL_ADDRESS_INFORMATION' => 'Adresă(e)',
+    'LBL_ASSIGNED_TO_NAME' => 'Utilizator',
+    'LBL_OTHER_EMAIL_ADDRESS' => 'Alt email:',
+    'LBL_ASSISTANT' => 'Asistent',
+    'LBL_ASSISTANT_PHONE' => 'Telefon asistent',
+    'LBL_WORK_PHONE' => 'Telefon Serviciu',
+    'LNK_IMPORT_VCARD' => 'Creează din vCard',
+    'LBL_ANY_EMAIL' => 'Orice Email',
+    'LBL_EMAIL_NON_PRIMARY' => 'E-mail alternativ',
+    'LBL_PHOTO' => 'Fotografie',
+    'LBL_EDIT_BUTTON' => 'Editați',
+    'LBL_REMOVE' => 'Înlăturați',
+
+    //Lawful Basis labels
+    'LBL_LAWFUL_BASIS' => 'Bază Legală',
+    'LBL_DATE_REVIEWED' => 'Dată Revizuire Bază Legală',
+    'LBL_LAWFUL_BASIS_SOURCE' => 'Sursă Bază Legală',
+    'LBL_CONSENT' => 'Consimțământ',
+    //End Lawful Basis labels
+);
