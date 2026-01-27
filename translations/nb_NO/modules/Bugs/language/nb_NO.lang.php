3418f7419199ea2f56419989f8951fd75425a8a3
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-27 19:14:14 +0000

    New translations en_us.lang.php (Norwegian Bokmal)

---
+++ translations/nb_NO/modules/Bugs/language/nb_NO.lang.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_MODULE_NAME' => 'Feil',
+    'LBL_MODULE_TITLE' => 'Feil: Hjem',
+    'LBL_MODULE_ID' => 'Feil',
+    'LBL_SEARCH_FORM_TITLE' => 'Søk etter feil',
+    'LBL_LIST_FORM_TITLE' => 'Liste over feil',
+    'LBL_NEW_FORM_TITLE' => 'Ny bug',
+    'LBL_SUBJECT' => 'Emne:',
+    'LBL_NUMBER' => 'Nummer:',
+    'LBL_STATUS' => 'Status',
+    'LBL_PRIORITY' => 'Prioritet:',
+    'LBL_DESCRIPTION' => 'Beskrivelse:',
+    'LBL_CONTACT_NAME' => 'Kontaktnavn:',
+    'LBL_CONTACT_ROLE' => 'Rolle:',
+    'LBL_LIST_NUMBER' => 'Nr.',
+    'LBL_LIST_SUBJECT' => 'Emne',
+    'LBL_LIST_STATUS' => 'Status',
+    'LBL_LIST_PRIORITY' => 'Prioritet',
+    'LBL_LIST_RESOLUTION' => 'Løsning',
+    'LBL_LIST_LAST_MODIFIED' => 'Sist endret',
+    'LBL_INVITEE' => 'Kontakter',
+    'LBL_TYPE' => 'Type:',
+    'LBL_LIST_TYPE' => 'Type',
+    'LBL_RESOLUTION' => 'Løsning:',
+    'LBL_RELEASE' => 'Utgave:',
+    'LNK_NEW_BUG' => 'Ny bug',
+    'LNK_BUG_LIST' => 'Se feiler',
+    'ERR_DELETE_RECORD' => 'Oppgi referansenr for å slette bugen.',
+    'LBL_LIST_MY_BUGS' => 'Mine tildelte feil',
+    'LNK_IMPORT_BUGS' => 'Importer feil',
+    'LBL_FOUND_IN_RELEASE' => 'Funnet i utgave:',
+    'LBL_FIXED_IN_RELEASE' => 'Reparert i utgave:',
+    'LBL_LIST_FIXED_IN_RELEASE' => 'Fikset i versjon',
+    'LBL_WORK_LOG' => 'Arbeidslogg:',
+    'LBL_SOURCE' => 'Kilde:',
+    'LBL_PRODUCT_CATEGORY' => 'Kategori:',
+
+    'LBL_CREATED_BY' => 'Opprettet av:',
+    'LBL_MODIFIED_BY' => 'Sist endret av:',
+
+    'LBL_LIST_EMAIL_ADDRESS' => 'E-postadresse:',
+    'LBL_LIST_CONTACT_NAME' => 'Kontaktens navn',
+    'LBL_LIST_ACCOUNT_NAME' => 'Navn på kunden',
+    'LBL_LIST_PHONE' => 'Telefonnr:',
+    'NTC_DELETE_CONFIRMATION' => 'Er du sikker på at du vil slette denne kontakten fra bugen?',
+
+    'LBL_DEFAULT_SUBPANEL_TITLE' => 'Feil tracker',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Aktiviteter',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Historie',
+    'LBL_CONTACTS_SUBPANEL_TITLE' => 'Kontakter',
+    'LBL_ACCOUNTS_SUBPANEL_TITLE' => 'Kunder',
+    'LBL_CASES_SUBPANEL_TITLE' => 'Kundestøtte',
+    'LBL_PROJECTS_SUBPANEL_TITLE' => 'Prosjekter',
+    'LBL_DOCUMENTS_SUBPANEL_TITLE' => 'Dokumenter',
+    'LBL_LIST_ASSIGNED_TO_NAME' => 'Tildelt bruker',
+    'LBL_ASSIGNED_TO_NAME' => 'Tildelt til',
+
+    'LBL_BUG_INFORMATION' => 'Info om kunden', //No need to be translated in all caps. Translation used just in menu action items when using the SuiteP template
+
+);
