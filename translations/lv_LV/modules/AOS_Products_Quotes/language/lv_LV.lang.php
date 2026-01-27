d981a2655bfaf9884ae2774821093210dade4f58
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-27 18:58:20 +0000

    New translations en_us.lang.php (Latvian)

---
+++ translations/lv_LV/modules/AOS_Products_Quotes/language/lv_LV.lang.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID' => 'Piešķirts lietotājam ar ID',
+    'LBL_ASSIGNED_TO_NAME' => 'Piešķirts lietotājam',
+    'LBL_ID' => 'ID',
+    'LBL_DATE_ENTERED' => 'Izveidošanas datums',
+    'LBL_DATE_MODIFIED' => 'Modificēts',
+    'LBL_MODIFIED' => 'Rediģēja',
+    'LBL_MODIFIED_NAME' => 'Modificēja vārds',
+    'LBL_CREATED' => 'Autors',
+    'LBL_DESCRIPTION' => 'Piezīme',
+    'LBL_DELETED' => 'Izdzēsts',
+    'LBL_NAME' => 'Vārds:',
+    'LBL_CREATED_USER' => 'Izveidoja lietotājs',
+    'LBL_MODIFIED_USER' => 'Modificēja',
+    'LBL_LIST_FORM_TITLE' => 'Produktu piedāvājumu saraksts',
+    'LBL_MODULE_NAME' => 'Line Items',
+    'LBL_MODULE_TITLE' => 'Produktu Piedāvājumi: sākumlapa',
+    'LBL_HOMEPAGE_TITLE' => 'My Products Quotes',
+    'LNK_NEW_RECORD' => 'Create Product Quote',
+    'LNK_LIST' => 'Products Quotes',
+    'LBL_SEARCH_FORM_TITLE' => 'Search Products Quotes',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Aplūkot vēsturi',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Darbības',
+    'LBL_NEW_FORM_TITLE' => 'New Product Quote',
+    'LBL_PRODUCT_NAME' => 'Vārds:',
+    'LBL_PRODUCT_QTY' => 'Daudzums',
+    'LBL_PRODUCT_COST_PRICE' => 'Pašizmaksa',
+    'LBL_PRODUCT_LIST_PRICE' => 'List Price',
+    'LBL_PRODUCT_UNIT_PRICE' => 'Vienības cena',
+    'LBL_PRODUCT_DISCOUNT' => 'Atlaide',
+    'LBL_PRODUCT_DISCOUNT_AMOUNT' => 'Atlaides apjoms',
+    'LBL_PART_NUMBER' => 'Daļas numurs',
+    'LBL_PRODUCT_DESCRIPTION' => 'Apraksts',
+    'LBL_DISCOUNT' => 'Atlaides tips',
+    'LBL_VAT_AMT' => 'Nodokļa summa',
+    'LBL_VAT' => 'Nodoklis',
+    'LBL_PRODUCT_TOTAL_PRICE' => 'Kopējā cena',
+    'LBL_PRODUCT_NOTE' => 'Piezīme',
+    'Quote' => '',
+    'LBL_FLEX_RELATE' => 'Attiecas uz',
+    'LBL_PRODUCT' => 'Produkts',
+
+    'LBL_SERVICE_MODULE_NAME' => 'Pakalpojumi',
+    'LBL_LIST_NUM' => 'Numurs',
+    'LBL_PARENT_ID' => 'Vecāka ID',
+    'LBL_GROUP_NAME' => 'Grupa',
+    'LBL_PRODUCT_COST_PRICE_USDOLLAR' => 'Izmaksu cena (noklusētajā valūtā)',
+    'LBL_PRODUCT_LIST_PRICE_USDOLLAR' => 'List Price (Default Currency)',
+    'LBL_PRODUCT_UNIT_PRICE_USDOLLAR' => 'Vienības cena (noklusētajā valūtā)',
+    'LBL_PRODUCT_TOTAL_PRICE_USDOLLAR' => 'Kopējā cena (noklusētajā valūtā)',
+    'LBL_PRODUCT_DISCOUNT_USDOLLAR' => 'Atlaide (noklusētajā valūtā)',
+    'LBL_PRODUCT_DISCOUNT_AMOUNT_USDOLLAR' => 'Atlaides summa (noklusētajā valūtā)',
+    'LBL_VAT_AMT_USDOLLAR' => 'Nodokļa summa (noklusētajā valūtā)',
+    'LBL_PRODUCTS_SERVICES' => 'Produkts / pakalpojums',
+    'LBL_PRODUCT_ID' => 'Product ID',
+
+    'LBL_AOS_CONTRACTS' => 'Līgumi',
+    'LBL_AOS_INVOICES' => 'Rēķini',
+    'LBL_AOS_PRODUCTS' => 'Produkti',
+    'LBL_AOS_QUOTES' => 'Piedāvājumi',
+);
