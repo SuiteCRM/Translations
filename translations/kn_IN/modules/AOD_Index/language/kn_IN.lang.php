46f056f6dcaa3c332cd7438ca51943ac447db8e7
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-27 18:36:12 +0000

    New translations en_us.lang.php (Kannada)

---
+++ translations/kn_IN/modules/AOD_Index/language/kn_IN.lang.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID' => 'ನಿಯೋಜಿಸಲಾದ ಬಳಕೆದಾರ ಗುರುತು',
+    'LBL_ASSIGNED_TO_NAME' => 'ಇವರಿಗೆ ನಿಯೋಜಿಸಲಾಗಿದೆ',
+    'LBL_ID' => 'ಗುರುತು',
+    'LBL_DATE_ENTERED' => 'ರಚಿಸಲಾದ ದಿನಾಂಕ',
+    'LBL_DATE_MODIFIED' => 'ತಿದ್ದಲಾದ ದಿನಾಂಕ',
+    'LBL_MODIFIED' => 'ಇವರಿಂದ ಮಾರ್ಪಡಿಸಲಾಗಿದೆ',
+    'LBL_MODIFIED_NAME' => 'ಮಾರ್ಪಡಿಸಿದವರ ಹೆಸರು',
+    'LBL_CREATED' => 'ರಚಿಸಿದವರು',
+    'LBL_DESCRIPTION' => 'ವಿವರಣೆ',
+    'LBL_DELETED' => 'ಅಳಿಸಲಾಗಿದೆ',
+    'LBL_NAME' => 'ಹೆಸರು',
+    'LBL_CREATED_USER' => 'ರಚಿಸಿದ ಬಳಕೆದಾರ',
+    'LBL_MODIFIED_USER' => 'ಮಾರ್ಪಡಿಸಿದ ಬಳಕೆದಾರ',
+    'LBL_LIST_NAME' => 'ಹೆಸರು',
+    'LBL_EDIT_BUTTON' => 'ತಿದ್ದು',
+    'LBL_REMOVE' => 'ತೆಗೆಯಿರಿ',
+    'LBL_LIST_FORM_TITLE' => 'Index List',
+    'LBL_MODULE_NAME' => 'Index',
+    'LBL_MODULE_TITLE' => 'Index',
+    'LBL_HOMEPAGE_TITLE' => 'My Index',
+    'LNK_NEW_RECORD' => 'Create Index',
+    'LNK_LIST' => 'View Index',
+    'LBL_SEARCH_FORM_TITLE' => 'Search Index',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'View History',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'ಚಟುವಟಿಕೆ',
+    'LBL_NEW_FORM_TITLE' => 'New Index',
+    'LBL_LAST_OPTIMISED' => 'Last Optimised',
+    'LBL_LOCATION' => 'Location',
+    'LBL_SEARCH_BUTTON' => 'Search',
+    'LBL_SEARCH_QUERY_PLACEHOLDER' => 'Enter search...',
+    'LBL_INDEX_STATS' => 'Index stats',
+    'LBL_OPTIMISE_NOW' => "Optimise now",
+    'LBL_TOTAL_RECORDS' => 'Total records',
+    'LBL_INDEXED_RECORDS' => 'Indexed records',
+    'LBL_UNINDEXED_RECORDS' => 'Unindexed records',
+    'LBL_FAILED_RECORDS' => 'Failed records',
+    'LBL_INDEX_FILES' => 'Index file count',
+    'LBL_SEARCH_RESULT_SCORE' => 'Search Score',
+    'LBL_SEARCH_RESULT_MODULE' => 'Module',
+    'LBL_SEARCH_RESULT_NAME' => 'ಹೆಸರು',
+    'LBL_SEARCH_RESULT_DATE_CREATED' => 'ರಚಿಸಲಾದ ದಿನಾಂಕ',
+    'LBL_SEARCH_RESULT_DATE_MODIFIED' => 'ತಿದ್ದಲಾದ ದಿನಾಂಕ',
+    'LBL_SEARCH_RESULT_EMPTY' => 'No results',
+    'LBL_SEARCH_RESULT_SUMMARY' => 'Summary',
+    'LBL_NEVER_OPTIMISED' => 'Never',
+    'LBL_USE_AOD_SEARCH' => 'Use Advanced Search',
+    'LBL_USE_VANILLA_SEARCH' => 'Use Basic Search',
+);
