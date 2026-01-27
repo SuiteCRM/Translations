a26bb9c839ab2dc68ed8ade217036498b678a916
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-27 18:31:16 +0000

    New translations en_us.lang.php (Finnish)

---
+++ translations/fi_FI/modules/AM_ProjectTemplates/language/fi_FI.lang.php
@@ -0,0 +1,145 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID' => 'Vastuuhenkilön tunnus',
+    'LBL_ASSIGNED_TO_NAME' => 'Projekti päällikkö',
+    'LBL_ID' => 'Tunnus',
+    'LBL_DATE_ENTERED' => 'Luonti pvm',
+    'LBL_DATE_MODIFIED' => 'Muokkaus pvm',
+    'LBL_MODIFIED' => 'Muokkaaja',
+    'LBL_MODIFIED_NAME' => 'Muokkaaja',
+    'LBL_CREATED' => 'Tekijä',
+    'LBL_DELETED' => 'Poistettu',
+    'LBL_NAME' => 'Mallipohjan nimi',
+    'LBL_CREATED_USER' => 'Tekijä',
+    'LBL_MODIFIED_USER' => 'Muokkaaja',
+    'LBL_LIST_NAME' => 'Nimi',
+    'LBL_EDIT_BUTTON' => 'Muokkaa',
+    'LBL_REMOVE' => 'Poista',
+    'LBL_LIST_FORM_TITLE' => 'Project Templates List',
+    'LBL_MODULE_NAME' => 'Projektimallit',
+    'LBL_MODULE_TITLE' => 'Projektimallit',
+    'LBL_HOMEPAGE_TITLE' => 'My Project Templates',
+    'LNK_NEW_RECORD' => 'Create Project Templates',
+    'LNK_LIST' => 'View Project Templates',
+    'LNK_IMPORT_AM_PROJECTTEMPLATES' => 'Tuo projektimallit',
+    'LBL_SEARCH_FORM_TITLE' => 'Etsi projektimallit',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Historia',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Aktiviteetit',
+    'LBL_NEW_FORM_TITLE' => 'Uusi projektimalli',
+    'LBL_STATUS' => 'Tila',
+    'LBL_PRIORITY' => 'Prioriteetti',
+    'LBL_PROJECT_NAME' => 'Projektin nimi',
+    'LBL_START_DATE' => 'Aloitus pvm',
+    'LBL_CREATE_PROJECT_TITLE' => 'Create a new project from this template?',
+    'LBL_AM_TASKTEMPLATES_AM_PROJECTTEMPLATES_FROM_AM_TASKTEMPLATES_TITLE' => 'Task Templates',
+    'LBL_AM_PROJECTTEMPLATES_USERS_1_TITLE' => 'Käyttäjät',
+    'LBL_AM_PROJECTTEMPLATES_CONTACTS_1_TITLE' => 'Kontaktit',
+    'LBL_AM_PROJECTTEMPLATES_RESOURCES_TITLE' => 'Select Resources',
+    'LBL_NEW_PROJECT_CREATED' => 'New Project Created',
+    'LBL_NEW_PROJECT' => 'Luo projekti',
+    'LBL_CANCEL_PROJECT' => 'Peruuta',
+
+    'LBL_SUBTASK' => 'Tehtävä',
+    'LBL_MILESTONE_FLAG' => 'Välietappi',
+    'LBL_RELATIONSHIP_TYPE' => 'Relaation tyyppi',
+    'LBL_LAG' => 'Viive',
+    'LBL_DAYS' => 'Päivää',
+    'LBL_HOURS' => 'Tuntia',
+    'LBL_MONTHS' => 'Kuukautta',
+
+    'LBL_PROJECT_TASKS_SUBPANEL_TITLE' => 'Projektitehtävät',
+    'LBL_VIEW_GANTT_TITLE' => 'Katso Gantt-kaavio',
+    'LBL_VIEW_GANTT_DURATION' => 'Kesto',
+    'LBL_TASK_TITLE' => 'Muokkaa tehtävää',
+    'LBL_DURATION_TITLE' => 'Muokkaa kestoa',
+    'LBL_DESCRIPTION' => 'Muistiot',
+    'LBL_ASSIGNED_USER_ID' => 'Vastuuhenkilö:',
+
+    'LBL_LIST_ASSIGNED_USER' => 'Projekti päällikkö',
+    'LBL_UNASSIGNED' => 'Ei kohdistettu',
+    'LBL_PROJECT_USERS_1_FROM_USERS_TITLE' => 'Resurssit',
+    'LBL_DELETE_TASK' => 'Poista tehtävä',
+    'LBL_VIEW_DETAIL' => 'Näytä tiedot',
+    'LBL_ADD_NEW_TASK' => 'Lisää uusi tehtävä',
+    'LBL_ASSIGNED_USER_NAME' => 'Projektipäällikkö:',
+
+    'LBL_TASK_ID' => 'Tunnus',
+    'LBL_TASK_NAME' => 'Tehtävän nimi',
+    'LBL_DURATION' => 'Kesto',
+    'LBL_ACTUAL_DURATION' => 'Todellinen kesto',
+    'LBL_START' => 'Aloita',
+    'LBL_FINISH' => 'Lopeta',
+    'LBL_PREDECESSORS' => 'Edeltäjät',
+    'LBL_PERCENT_COMPLETE' => '% valmiina',
+    'LBL_EDIT_TASK_PROPERTIES' => 'Muokkaa tehtävän ominaisuuksia.',
+
+    'LBL_OVERRIDE_BUSINESS_HOURS' => 'Consider Working days',
+    'LBL_COPY_ALL_TASKS' => 'Copy All Tasks With Resources',
+    'LBL_COPY_SEL_TASKS' => 'Copy Selected Tasks With Resources',
+    'LBL_TOOLTIP_TITLE' => 'Hint',
+    'LBL_TOOLTIP_TEXT' => 'Copies All tasks with assigned users',
+
+    'LBL_EMAIL' => 'Sähköposti',
+    'LBL_PHONE' => 'Toimiston puhelin:',
+    'LBL_ADD_BUTTON' => 'Lisää',
+    'LBL_ADD_INVITEE' => 'Add Resource',
+    'LBL_FIRST_NAME' => 'Etunimi',
+    'LBL_LAST_NAME' => 'Sukunimi',
+    'LBL_SEARCH_BUTTON' => 'Etsi',
+    'LBL_EMPTY_SEARCH_RESULT' => 'Ei hakutuloksia, luo kutsuttava alapuolella.',
+    'LBL_CREATE_INVITEE' => 'Create a Resource',
+    'LBL_CREATE_CONTACT' => 'Kontaktina',
+    'LBL_CREATE_AND_ADD' => 'Luo & lisää',
+    'LBL_CANCEL_CREATE_INVITEE' => 'Peruuta',
+    'LBL_NO_ACCESS' => 'Sinulla ei ole oikeuksia luoda $module',
+    'LBL_SCHEDULING_FORM_TITLE' => 'Resource List',
+    'LBL_NONE' => 'Ei mitään',
+    'LBL_TASKS_NOT_FOUND' => 'There are no Tasks to build the Gantt Table',
+
+    'LBL_AM_PROJECTTEMPLATES_PROJECT_1_FROM_PROJECT_TITLE' => 'Project Templates: Project from Project Title',
+
+
+);
