73345d6ab4436620adaea18b7f7cf17c52cc2b17
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-27 18:49:00 +0000

    New translations en_us.lang.php (Thai)

---
+++ translations/th_TH/modules/AOR_Scheduled_Reports/language/th_TH.lang.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID' => 'ชื่อไอดีผู้รับผิดชอบ',
+    'LBL_ASSIGNED_TO_NAME' => 'ผู้รับผิดชอบ',
+    'LBL_ID' => 'ไอดี',
+    'LBL_DATE_ENTERED' => 'วันที่สร้าง',
+    'LBL_DATE_MODIFIED' => 'วันที่แก้ไข',
+    'LBL_MODIFIED' => 'แก้ไขโดย',
+    'LBL_MODIFIED_NAME' => 'แก้ไขโดยชื่อ',
+    'LBL_CREATED' => 'สร้างโดย',
+    'LBL_DESCRIPTION' => 'รายละเอียด',
+    'LBL_DELETED' => 'ลบ',
+    'LBL_NAME' => 'ชื่อ',
+    'LBL_CREATED_USER' => 'สร้างโดยผู้ใช้งาน',
+    'LBL_MODIFIED_USER' => 'แก้ไขโดยผู้ใช้',
+    'LBL_LIST_NAME' => 'ชื่อ',
+    'LBL_EDIT_BUTTON' => 'แก้ไข',
+    'LBL_REMOVE' => 'ลบออก',
+    'LBL_ANY_EMAIL' => 'อีเมลใด ๆ:',
+    'LBL_EMAIL_NON_PRIMARY' => 'ไม่ใช่อีเมลหลัก',
+    'LBL_ASSIGNED_TO' => 'กำหนดให้:',
+    'LBL_ASSIGNED_USER' => 'กำหนดให้:',
+    'LBL_EMAIL_RECIPIENTS' => 'อีเมล์ผู้รับ:',
+    'NTC_DELETE_CONFIRMATION' => 'คุณแน่ใจไหมที่ต้องการจะลบบันทึกนี้?',
+    'LBL_LIST_FORM_TITLE' => 'รายการกำหนดเวลาการรายงาน',
+    'LBL_MODULE_NAME' => 'กำหนดเวลาการรายงาน',
+    'LBL_MODULE_TITLE' => 'กำหนดเวลาการรายงาน',
+    'LBL_HOMEPAGE_TITLE' => 'กำหนดเวลาการรายงานของฉัน',
+    'LNK_NEW_RECORD' => 'สร้างกำหนดเวลาการรายงาน',
+    'LNK_LIST' => 'ดูกำหนดเวลาการรายงาน',
+    'LBL_SEARCH_FORM_TITLE' => 'ค้นหากำหนดเวลาการรายงาน',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'ดูประวัติ',
+    'LBL_NEW_FORM_TITLE' => 'กำหนดเวลาการรายงานใหม่',
+    'LBL_SCHEDULE' => 'กำหนดการ',
+    'LBL_AOR_REPORT_NAME' => 'รายงาน',
+    'LBL_SCHEDULED_REPORTS_INFORMATION' => 'กำหนดเวลาการรายงาน',
+    'LBL_LAST_RUN' => 'เรียกใช้ล่าสุด',
+    'LBL_STATUS' => 'สถานะ',
+);
