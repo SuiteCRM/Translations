695042cd752d1f9cdd72e1c8a96fee03ef737109
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-27 18:48:34 +0000

    New translations en_us.lang.php (Lithuanian)

---
+++ translations/lt_LT/modules/AOR_Scheduled_Reports/language/lt_LT.lang.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID' => 'Atsakingo Vartotojo Id',
+    'LBL_ASSIGNED_TO_NAME' => 'Priskirtas',
+    'LBL_ID' => 'Id',
+    'LBL_DATE_ENTERED' => 'Sukurta',
+    'LBL_DATE_MODIFIED' => 'Redagavimo data',
+    'LBL_MODIFIED' => 'Redagavo',
+    'LBL_MODIFIED_NAME' => 'Redaguotojo vardas',
+    'LBL_CREATED' => 'Sukūrė',
+    'LBL_DESCRIPTION' => 'Aprašymas',
+    'LBL_DELETED' => 'Ištrintas',
+    'LBL_NAME' => 'Vardas',
+    'LBL_CREATED_USER' => 'Sukūrė vartotojas',
+    'LBL_MODIFIED_USER' => 'Redagavo vartotojas',
+    'LBL_LIST_NAME' => 'Vardas',
+    'LBL_EDIT_BUTTON' => 'Redaguoti',
+    'LBL_REMOVE' => 'Išmesti',
+    'LBL_ANY_EMAIL' => 'El.paštas:',
+    'LBL_EMAIL_NON_PRIMARY' => 'Ne Pirminia El. paštas',
+    'LBL_ASSIGNED_TO' => 'Priskirtas:',
+    'LBL_ASSIGNED_USER' => 'Priskirtas:',
+    'LBL_EMAIL_RECIPIENTS' => 'Elektroninio pašto gavėjams:',
+    'NTC_DELETE_CONFIRMATION' => 'Ar tikrai norite ištrinti šį įrašą ?',
+    'LBL_LIST_FORM_TITLE' => 'Planuojams Ataskaitų Sarašas',
+    'LBL_MODULE_NAME' => 'Suplanuotos ataskaitos',
+    'LBL_MODULE_TITLE' => 'Suplanuotos ataskaitos',
+    'LBL_HOMEPAGE_TITLE' => 'Mano Planavimo Ataskaitos',
+    'LNK_NEW_RECORD' => 'Sukurti Planavimo Ataskaitas',
+    'LNK_LIST' => 'Rodyti Planavimo Ataskaitas',
+    'LBL_SEARCH_FORM_TITLE' => 'Paieška Planavimo Ataskaitose',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Peržiūros istorija',
+    'LBL_NEW_FORM_TITLE' => 'Nauja Planavimo Ataskaita',
+    'LBL_SCHEDULE' => 'Planuotojas',
+    'LBL_AOR_REPORT_NAME' => 'Ataskaitos',
+    'LBL_SCHEDULED_REPORTS_INFORMATION' => 'Suplanuotos ataskaitos',
+    'LBL_LAST_RUN' => 'Paskutinį kartą vykdyta',
+    'LBL_STATUS' => 'Būsena',
+);
