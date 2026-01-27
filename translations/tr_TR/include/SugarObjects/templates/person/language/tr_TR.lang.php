ee272ab10a3eaddf44a67a8477cdb1477bfd20eb
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-27 18:18:27 +0000

    New translations en_us.lang.php (Turkish)

---
+++ translations/tr_TR/include/SugarObjects/templates/person/language/tr_TR.lang.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_SALUTATION' => 'Hitap',
+    'LBL_NAME' => 'Ad',
+    'LBL_FIRST_NAME' => 'Ad',
+    'LBL_LAST_NAME' => 'Soyad',
+    'LBL_TITLE' => 'Başlık',
+    'LBL_DEPARTMENT' => 'Bölüm',
+    'LBL_DO_NOT_CALL' => 'Telefon ile aranmayacak',
+    'LBL_HOME_PHONE' => 'Ev telefonu',
+    'LBL_MOBILE_PHONE' => 'Cep telefonu',
+    'LBL_OFFICE_PHONE' => 'İş telefonu',
+    'LBL_OTHER_PHONE' => 'Diğer telefon',
+    'LBL_FAX_PHONE' => 'Faks',
+    'LBL_EMAIL_ADDRESS' => 'E-posta adresleri',
+    'LBL_PRIMARY_ADDRESS' => 'Birincil adres',
+    'LBL_PRIMARY_ADDRESS_STREET' => 'Birincil adres',
+    'LBL_PRIMARY_ADDRESS_STREET_2' => '2. birincil adres satırı:',
+    'LBL_PRIMARY_ADDRESS_STREET_3' => '3. birincil adres satırı:',
+    'LBL_PRIMARY_ADDRESS_CITY' => 'Birincil ilçe',
+    'LBL_PRIMARY_ADDRESS_STATE' => 'Birincil il',
+    'LBL_PRIMARY_ADDRESS_POSTALCODE' => 'Birincil posta kodu',
+    'LBL_PRIMARY_ADDRESS_COUNTRY' => 'Birincil adres ülke:',
+    'LBL_ALT_ADDRESS' => 'Diğer adres',
+    'LBL_ALT_ADDRESS_STREET' => 'Diğer adres',
+    'LBL_ALT_ADDRESS_STREET_2' => '2. diğer adres satırı:',
+    'LBL_ALT_ADDRESS_STREET_3' => '3. diğer adres satırı:',
+    'LBL_ALT_ADDRESS_CITY' => 'Diğer ilçe',
+    'LBL_ALT_ADDRESS_STATE' => 'Diğer il',
+    'LBL_ALT_ADDRESS_POSTALCODE' => 'Diğer posta kodu',
+    'LBL_ALT_ADDRESS_COUNTRY' => 'Diğer ülke',
+    'LBL_PRIMARY_STREET' => 'Adres',
+    'LBL_ALT_STREET' => 'Diğer adres',
+    'LBL_STREET' => 'Diğer adres',
+    'LBL_CITY' => 'İlçe',
+    'LBL_STATE' => 'İl',
+    'LBL_POSTAL_CODE' => 'Posta kodu',
+    'LBL_COUNTRY' => 'Ülke',
+    'LBL_CONTACT_INFORMATION' => 'Kişi bilgileri',
+    'LBL_ADDRESS_INFORMATION' => 'Adres(ler)',
+    'LBL_ASSIGNED_TO_NAME' => 'Kullanıcı',
+    'LBL_OTHER_EMAIL_ADDRESS' => 'Diğer e-posta:',
+    'LBL_ASSISTANT' => 'Sekreter',
+    'LBL_ASSISTANT_PHONE' => 'Sekreter telefonu',
+    'LBL_WORK_PHONE' => 'İş telefonu',
+    'LNK_IMPORT_VCARD' => 'vCard ile ekle',
+    'LBL_ANY_EMAIL' => 'Herhangi bir e-posta',
+    'LBL_EMAIL_NON_PRIMARY' => 'İkincil e-posta adresleri',
+    'LBL_PHOTO' => 'Fotoğraf',
+    'LBL_EDIT_BUTTON' => 'Düzenle',
+    'LBL_REMOVE' => 'Sil',
+
+    //Lawful Basis labels
+    'LBL_LAWFUL_BASIS' => 'Yasal hükümler',
+    'LBL_DATE_REVIEWED' => 'Yasal hükümleri onaylama tarihi',
+    'LBL_LAWFUL_BASIS_SOURCE' => 'Yasal hükümler kaynağı',
+    'LBL_CONSENT' => 'Kabul',
+    //End Lawful Basis labels
+);
