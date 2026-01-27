282f9903b9ebdd9b63c19f0f8dd92157d2e5200b
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-27 19:05:32 +0000

    New translations en_us.lang.php (Filipino)

---
+++ translations/fil_PH/modules/AOW_WorkFlow/language/fil_PH.lang.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID' => 'Magtalaga ng Id para sa gumagamit',
+    'LBL_ASSIGNED_TO_NAME' => 'Itinatalaga sa',
+    'LBL_ID' => 'Ang ID',
+    'LBL_DATE_ENTERED' => 'Ang petsa ng pagkakalikha',
+    'LBL_DATE_MODIFIED' => 'Ang petsa ng pagkabago',
+    'LBL_MODIFIED' => 'Binago ni',
+    'LBL_MODIFIED_NAME' => 'Ito ay ibinago sa pamamagitan ng pangalan',
+    'LBL_CREATED' => 'Ito ay nalikha sa pamamagitan ng',
+    'LBL_DESCRIPTION' => 'Ang deskripsyon',
+    'LBL_DELETED' => 'Ang pagkabura',
+    'LBL_NAME' => 'Ang pangalan',
+    'LBL_CREATED_USER' => 'Inilikha ng tagagamit',
+    'LBL_MODIFIED_USER' => 'Binago sa pamamagitan ng tagagamit',
+    'LBL_LIST_NAME' => 'Ang pangalan',
+    'LBL_LIST_FORM_TITLE' => 'Listahan ng workFlow',
+    'LBL_MODULE_NAME' => 'Ang daloy ng trabaho',
+    'LBL_MODULE_TITLE' => 'Ang daloy ng trabaho',
+    'LBL_HOMEPAGE_TITLE' => 'Aking workflow',
+    'LNK_NEW_RECORD' => 'Lumikha ng WorkFlow',
+    'LNK_LIST' => 'I-view ang WorkFlow',
+    'LNK_PROCESSED_LIST' => 'Tumingin ng Audit ng proseso',
+    'LBL_SEARCH_FORM_TITLE' => 'Maghanap ng WorkFlow',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Tingnan ang kasaysayan',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Mga ginagawa',
+    'LBL_NEW_FORM_TITLE' => 'Bagong WorkFlow',
+    'LBL_FLOW_MODULE' => 'Modyul ng workFlow',
+    'LBL_STATUS' => 'Katayuan',
+    'LBL_FLOW_RUN_ON' => 'Patakbuhin',
+    'LBL_CONDITION_LINES' => 'Mga kondisyon',
+    'LBL_ADD_CONDITION' => 'Magdagdag ng kondisyon',
+    'LBL_ACTION_LINES' => 'Mga Aksyon',
+    'LBL_ADD_ACTION' => 'Magdagdag ng aksyon',
+    'LBL_MULTIPLE_RUNS' => 'Naulit na mga pagtakbo',
+    'LBL_RUN_WHEN' => 'I-run',
+    'LBL_RUN_ON_IMPORT' => 'Run on Import'
+);
