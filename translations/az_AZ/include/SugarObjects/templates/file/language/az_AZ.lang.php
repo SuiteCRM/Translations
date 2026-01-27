bec61748ce1963f2f8aa9d74ca95cc64ccf0d13f
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-27 18:14:35 +0000

    New translations en_us.lang.php (Azerbaijani)

---
+++ translations/az_AZ/include/SugarObjects/templates/file/language/az_AZ.lang.php
@@ -0,0 +1,117 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    //module
+    'LBL_MODULE_NAME' => 'Sənədlər',
+    'LBL_MODULE_TITLE' => 'Sənədlər: Giriş',
+    'LNK_NEW_DOCUMENT' => 'Sənəd yaradın',
+    'LNK_DOCUMENT_LIST' => 'Sənədlər siyahısı',
+    'LBL_SEARCH_FORM_TITLE' => 'Sənəd axtarışı',
+    //vardef labels
+    'LBL_NAME' => 'Sənədin adı',
+    'LBL_DESCRIPTION' => 'Açıqlama',
+    'LBL_ASSIGNED_TO' => 'Təyin edilsin:',
+    'LBL_CATEGORY' => 'Kateqoriya',
+    'LBL_SUBCATEGORY' => 'Alt kateqoriya',
+    'LBL_STATUS' => 'Vəziyyət',
+    'LBL_IS_TEMPLATE' => 'Şablondur',
+    'LBL_TEMPLATE_TYPE' => 'Sənəd növü',
+    'LBL_REVISION_NAME' => 'Revizyon nömrəsi',
+    'LBL_MIME' => 'Mime növü',
+    'LBL_REVISION' => 'Yenidən baxma',
+    'LBL_DOCUMENT' => 'Əlaqəli sənəd',
+    'LBL_LATEST_REVISION' => 'Son revizyon',
+    'LBL_CHANGE_LOG' => 'Dəyişiklik jurnalı',
+    'LBL_ACTIVE_DATE' => 'Nəşr olunma tarixi',
+    'LBL_EXPIRATION_DATE' => 'Bitmə tarixi',
+    'LBL_FILE_EXTENSION' => 'Fayl uzantısı',
+
+    'LBL_CAT_OR_SUBCAT_UNSPEC' => 'Dəqiqləşdirilməmiş',
+    //quick search
+    'LBL_NEW_FORM_TITLE' => 'Yeni sənəd',
+    //document edit and detail view
+    'LBL_DOC_NAME' => 'Sənəd Adı:',
+    'LBL_FILENAME' => 'Fayl Adı:',
+    'LBL_FILE_UPLOAD' => 'Fayl:',
+    'LBL_DOC_VERSION' => 'Revizyon:',
+    'LBL_CATEGORY_VALUE' => 'Kateqoriya:',
+    'LBL_SUBCATEGORY_VALUE' => 'Alt Kateqoriya:',
+    'LBL_DOC_STATUS' => 'Status:',
+    'LBL_DET_TEMPLATE_TYPE' => 'Sənəd növü:',
+    'LBL_DOC_DESCRIPTION' => 'Açıqlama:',
+    'LBL_DOC_ACTIVE_DATE' => 'Nəşt olunma tarixi:',
+    'LBL_DOC_EXP_DATE' => 'İstifadə müddəti:',
+
+    //document list view.
+    'LBL_LIST_FORM_TITLE' => 'Sənəd siyahısı',
+    'LBL_LIST_DOCUMENT' => 'Sənəd',
+    'LBL_LIST_CATEGORY' => 'Kateqoriya',
+    'LBL_LIST_SUBCATEGORY' => 'Alt kateqoriya',
+    'LBL_LIST_REVISION' => 'Yenidən baxma',
+    'LBL_LIST_LAST_REV_CREATOR' => 'Nəşr edən',
+    'LBL_LIST_LAST_REV_DATE' => 'Yoxlanma tarixi',
+    'LBL_LIST_VIEW_DOCUMENT' => 'Bax',
+    'LBL_LIST_ACTIVE_DATE' => 'Nəşr olunma tarixi',
+    'LBL_LIST_EXP_DATE' => 'Bitmə tarixi',
+    'LBL_LIST_STATUS' => 'Vəziyyət',
+
+    //document search form.
+    'LBL_SF_CATEGORY' => 'Kateqoriya:',
+    'LBL_SF_SUBCATEGORY' => 'Alt Kateqoriya:',
+
+    'DEF_CREATE_LOG' => 'Sənəd yaradılıdı',
+
+    //error messages
+    'ERR_DOC_NAME' => 'Sənədin adı',
+    'ERR_DOC_ACTIVE_DATE' => 'Nəşr olunma tarixi',
+    'ERR_FILENAME' => 'Faylın adı',
+
+    //sub-panel vardefs.
+    'LBL_LIST_DOCUMENT_NAME' => 'Sənədin adı',
+
+    'LBL_EDIT_BUTTON' => 'Redaktə etmək ',
+    'LBL_REMOVE' => 'Sil',
+
+);
