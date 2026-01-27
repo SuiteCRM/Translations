13cf9d50704b5025c1f8493e2d46201548c66420
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-27 19:19:56 +0000

    New translations en_us.lang.php (Romanian)

---
+++ translations/ro_RO/modules/CampaignLog/language/ro_RO.lang.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_LIST_ID' => 'ID-ul asocierii',
+    'LBL_ID' => 'ID-ul',
+    'LBL_TARGET_TRACKER_KEY' => 'Cheie de găsire obiectiv',
+    'LBL_TARGET_ID' => 'ID-ul obiectivului',
+    'LBL_TARGET_TYPE' => 'Tipul obiectivului',
+    'LBL_ACTIVITY_TYPE' => 'Tip de activității',
+    'LBL_ACTIVITY_DATE' => 'Data activității',
+    'LBL_RELATED_ID' => 'Legat de Id-ul',
+    'LBL_RELATED_TYPE' => 'Legat de tipul',
+    'LBL_DELETED' => 'Şters',
+    'LBL_MODULE_NAME' => 'Jurnal campanii',
+    'LBL_LIST_RECIPIENT_EMAIL' => 'Emailuri destinatari',
+    'LBL_LIST_RECIPIENT_NAME' => 'Nume destinatari',
+    'LBL_ARCHIVED' => 'Arhivat',
+    'LBL_HITS' => 'Accesuri',
+
+    'LBL_CAMPAIGN_NAME' => 'Nume:',
+    'LBL_CAMPAIGN' => 'Campanie:',
+    'LBL_NAME' => 'Nume: ',
+    'LBL_INVITEE' => 'Contacte',
+    'LBL_LIST_CAMPAIGN_NAME' => 'Campanie',
+    'LBL_LIST_STATUS' => 'Status',
+    'LBL_LIST_TYPE' => 'Tip',
+    'LBL_LIST_END_DATE' => 'Dată Sfârșit',
+    'LBL_DATE_ENTERED' => 'Data de începere',
+    'LBL_DATE_MODIFIED' => 'Data modificării',
+    'LBL_MODIFIED' => 'Modificat de: ',
+    'LBL_CREATED' => 'Creat de: ',
+    'LBL_TEAM' => 'Echipă: ',
+    'LBL_ASSIGNED_TO' => 'Atribuit lui: ',
+    'LBL_CAMPAIGN_START_DATE' => 'Data început: ',
+    'LBL_CAMPAIGN_END_DATE' => 'Data sfârșit: ',
+    'LBL_CAMPAIGN_STATUS' => 'Status: ',
+    'LBL_CAMPAIGN_BUDGET' => 'Buget: ',
+    'LBL_CAMPAIGN_EXPECTED_COST' => 'Cost prevăzut: ',
+    'LBL_CAMPAIGN_ACTUAL_COST' => 'Cost actual: ',
+    'LBL_CAMPAIGN_EXPECTED_REVENUE' => 'Profit prevăzut: ',
+    'LBL_CAMPAIGN_TYPE' => 'Tip: ',
+    'LBL_CAMPAIGN_OBJECTIVE' => 'Obiectiv: ',
+    'LBL_CAMPAIGN_CONTENT' => 'Descriere: ',
+    'LBL_CREATED_LEAD' => 'Conversie creată',
+    'LBL_CREATED_CONTACT' => 'Contact Creeat',
+    'LBL_CREATED_OPPORTUNITY' => 'Oportunitate creată',
+    'LBL_TARGETED_USER' => 'Utilizator vizat',
+    'LBL_SENT_EMAIL' => 'Email-uri Trimise',
+    'LBL_LIST_FORM_TITLE' => 'Campanii prevăzute',
+    'LBL_LIST_ACTIVITY_DATE' => 'Data activității',
+    'LBL_RELATED' => 'Legat de',
+    'LBL_MORE_INFO' => 'Detalii',
+
+    'LBL_CAMPAIGNS' => 'Campanii',
+    'LBL_LIST_MARKETING_NAME' => 'Id marketing',
+
+    'LBL_MARKETING_ID' => 'Id marketing',
+    'LBL_RELATED_NAME' => 'Nume asociat',
+);
