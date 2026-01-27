5ca7e9c465d67457f4e8442fdc6cbacae46fea61
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-27 18:33:29 +0000

    New translations en_us.lang.php (Tagalog)

---
+++ translations/tl_PH/modules/AM_TaskTemplates/language/tl_PH.lang.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID' => 'Ang nagagamit sa Id ay nagtalaga',
+    'LBL_ASSIGNED_TO_NAME' => 'Magtalaga sa',
+    'LBL_ID' => 'Ang ID',
+    'LBL_DATE_ENTERED' => 'Ang petsa ay lumikha',
+    'LBL_DATE_MODIFIED' => 'Ang petsa ay binago',
+    'LBL_MODIFIED' => 'Binago sa pamamagitan ng',
+    'LBL_MODIFIED_ID' => 'Binago sa pamamagitan ng Id',
+    'LBL_MODIFIED_NAME' => 'Sa pamamagitan ng pangalan ay binago',
+    'LBL_CREATED' => 'Nilikha ni',
+    'LBL_CREATED_ID' => 'Nilikha sa pamamagitan ng Id',
+    'LBL_DESCRIPTION' => 'Ang Paglalarawan',
+    'LBL_DELETED' => 'Ang Nabura',
+    'LBL_NAME' => 'Ang pangalan ng gawain',
+    'LBL_CREATED_USER' => 'Nililikha sa pamamagitan ng Gumagamit',
+    'LBL_MODIFIED_USER' => 'Ang Binago sa pamamagitan ng gumagamit',
+    'LBL_LIST_NAME' => 'Ang Pangalan',
+    'LBL_EDIT_BUTTON' => 'I-edit',
+    'LBL_REMOVE' => 'Alisin',
+    'LBL_LIST_FORM_TITLE' => 'Ang proyektong gawain sa mga template ay nakalista',
+    'LBL_MODULE_NAME' => 'Ang proyektong gawain sa mga template',
+    'LBL_MODULE_TITLE' => 'Ang proyektong gawain sa mga template',
+    'LBL_HOMEPAGE_TITLE' => 'Ang aking proyektong gawain sa mga template',
+    'LNK_NEW_RECORD' => 'Lumikha ng proyektong gawain sa mga template',
+    'LNK_LIST' => 'Tingnan ang mga template ng proyekto ng gawain',
+    'LNK_IMPORT_AM_TASKTEMPLATES' => 'Ipasok ang mga template ng proyekto ng gawain',
+    'LBL_SEARCH_FORM_TITLE' => 'Maghanap ng mga template ng proyekto ng gawain',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Tanawin ang kasaysayan',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Ang mga aktibidad',
+    'LBL_AM_TASKTEMPLATES_SUBPANEL_TITLE' => 'Ang proyektong gawain sa mga template',
+    'LBL_NEW_FORM_TITLE' => 'Bagong mga template ng proyekto ng gawain',
+    'LBL_STATUS' => 'Kalagayan',
+    'LBL_PRIORITY' => 'Prayoridad',
+    'LBL_PERCENT_COMPLETE' => 'Kumpleto ang porsiyento',
+    'LBL_PREDECESSORS' => 'Mga hinalinhan',
+    'LBL_MILESTONE_FLAG' => 'Bandila ng milyahe',
+    'LBL_RELATIONSHIP_TYPE' => 'Uri ng relasyon',
+    'LBL_TASK_NUMBER' => 'Numero ng gawain',
+    'LBL_ORDER_NUMBER' => 'Utos ',
+    'LBL_ESTIMATED_EFFORT' => 'Tinantyang pagsisikap (mga oras)',
+    'LBL_UTILIZATION' => 'Paggamit (%)',
+    'LBL_DURATION' => 'Tinatagal (mga araw)',
+    'LBL_AM_TASKTEMPLATES_AM_PROJECTTEMPLATES_FROM_AM_PROJECTTEMPLATES_TITLE' => 'Ang proyekto ng mga template',
+
+    'LBL_AM_TASKTEMPLATES_AM_PROJECTTEMPLATES_FROM_AM_TASKTEMPLATES_TITLE' => 'Mga template ng gawain: Mga template ng proyekto mula sa mga titulo ng template ng gawain',
+);
