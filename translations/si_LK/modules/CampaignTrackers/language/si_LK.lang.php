17fc7d7be8e9f0a0fea7a9d73eeb91dee965ca53
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-27 19:22:18 +0000

    New translations en_us.lang.php (Sinhala)

---
+++ translations/si_LK/modules/CampaignTrackers/language/si_LK.lang.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ID' => 'Id',
+    'LBL_TRACKER_KEY' => 'ට්රැකර් කී',
+    'LBL_TRACKER_URL' => 'ට්රැකර් URL එක',
+    'LBL_TRACKER_NAME' => 'ට්රැකර් නම',
+    'LBL_CAMPAIGN_ID' => 'ව්යාපාරය Id',
+    'LBL_DATE_ENTERED' => 'දිනය තේරී පත්විය',
+    'LBL_DATE_MODIFIED' => 'නවීකරණය කරන ලද දිනය',
+    'LBL_MODIFIED_USER_ID' => 'නවීකරණය කරන ලද පරිශීලක Id',
+    'LBL_CREATED_BY' => 'සෑදුවේ',
+    'LBL_DELETED' => ' මකා දමන',
+    'LBL_CAMPAIGN' => 'ව්යාපාරය',
+    'LBL_OPTOUT' => 'හැර යන්න',
+
+    'LBL_MODULE_NAME' => 'ව්යාපාරය ට්රැකර්ස්',
+    'LBL_EDIT_CAMPAIGN_NAME' => 'ව්යාපාරය නම:',
+    'LBL_EDIT_TRACKER_NAME' => 'ට්රැකර් නම:',
+    'LBL_EDIT_TRACKER_URL' => 'ට්රැකර් URL එක:',
+
+    'LBL_SUBPANEL_TRACKER_NAME' => 'නම',
+    'LBL_SUBPANEL_TRACKER_URL' => 'ඒ.ස.නි.',
+    'LBL_SUBPANEL_TRACKER_KEY' => 'යතුර',
+    'LBL_EDIT_MESSAGE_URL' => 'ව්යාපාරය පණිවුඩය සඳහා URL:',
+    'LBL_EDIT_TRACKER_KEY' => 'ට්රැකර් කී:',
+    'LBL_EDIT_OPT_OUT' => 'වෙතින් ඉවත් ලින්ක්?',
+    'LNK_CAMPAIGN_LIST' => 'ප්රචාරණ ව්යාපාර',
+    'LBL_EDIT_LAYOUT' => 'සංස්කරණය කරන්න පිරිසැලසුම' /*for 508 compliance fix*/,
+);
