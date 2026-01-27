bbba851ed2ba581f5ed9141c249a14e636111108
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-27 18:35:26 +0000

    New translations en_us.lang.php (Italian)

---
+++ translations/it_IT/modules/AOD_Index/language/it_IT.lang.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID' => 'Id utente assegnato',
+    'LBL_ASSIGNED_TO_NAME' => 'Assegnato a',
+    'LBL_ID' => 'ID',
+    'LBL_DATE_ENTERED' => 'Data di Creazione',
+    'LBL_DATE_MODIFIED' => 'Data Modifica',
+    'LBL_MODIFIED' => 'Modificato da',
+    'LBL_MODIFIED_NAME' => 'Modificato da Nome',
+    'LBL_CREATED' => 'Creato da',
+    'LBL_DESCRIPTION' => 'Descrizione',
+    'LBL_DELETED' => 'Eliminato',
+    'LBL_NAME' => 'Nome',
+    'LBL_CREATED_USER' => 'Creato dall\'utente',
+    'LBL_MODIFIED_USER' => 'Modificato dall\'utente',
+    'LBL_LIST_NAME' => 'Nome',
+    'LBL_EDIT_BUTTON' => 'Modifica',
+    'LBL_REMOVE' => 'Cancella',
+    'LBL_LIST_FORM_TITLE' => 'Elenco Indici',
+    'LBL_MODULE_NAME' => 'Indice',
+    'LBL_MODULE_TITLE' => 'Indice',
+    'LBL_HOMEPAGE_TITLE' => 'Miei Indici',
+    'LNK_NEW_RECORD' => 'Nuovo Indice',
+    'LNK_LIST' => 'Indici',
+    'LBL_SEARCH_FORM_TITLE' => 'Cerca Indici',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Visualizza Cronologia',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Attività',
+    'LBL_NEW_FORM_TITLE' => 'Nuovo Indice',
+    'LBL_LAST_OPTIMISED' => 'Ultimi Ottimizzati',
+    'LBL_LOCATION' => 'Posizione',
+    'LBL_SEARCH_BUTTON' => 'Ricerca',
+    'LBL_SEARCH_QUERY_PLACEHOLDER' => 'Ricerca...',
+    'LBL_INDEX_STATS' => 'Statistiche Indici',
+    'LBL_OPTIMISE_NOW' => "Ottimizza ora",
+    'LBL_TOTAL_RECORDS' => 'Records Totali',
+    'LBL_INDEXED_RECORDS' => 'Records Indicizzati',
+    'LBL_UNINDEXED_RECORDS' => 'Records Non Indicizzati',
+    'LBL_FAILED_RECORDS' => 'Records Falliti',
+    'LBL_INDEX_FILES' => 'Contatore Files Indici',
+    'LBL_SEARCH_RESULT_SCORE' => 'Punteggio Ricerca',
+    'LBL_SEARCH_RESULT_MODULE' => 'Modulo',
+    'LBL_SEARCH_RESULT_NAME' => 'Nome',
+    'LBL_SEARCH_RESULT_DATE_CREATED' => 'Data di Creazione',
+    'LBL_SEARCH_RESULT_DATE_MODIFIED' => 'Data Modifica',
+    'LBL_SEARCH_RESULT_EMPTY' => 'Nessun risultato',
+    'LBL_SEARCH_RESULT_SUMMARY' => 'Riassunto',
+    'LBL_NEVER_OPTIMISED' => 'Mai',
+    'LBL_USE_AOD_SEARCH' => 'Usa la Ricerca Avanzata',
+    'LBL_USE_VANILLA_SEARCH' => 'Usa la Ricerca di Base',
+);
