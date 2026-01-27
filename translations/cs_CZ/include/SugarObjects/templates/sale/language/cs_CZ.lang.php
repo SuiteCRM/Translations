5ed53d33f84328e7904f84bb61f516f4528f6a1f
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-27 18:20:47 +0000

    New translations en_us.lang.php (Czech)

---
+++ translations/cs_CZ/include/SugarObjects/templates/sale/language/cs_CZ.lang.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_MODULE_NAME' => 'Prodej',
+    'LBL_MODULE_TITLE' => 'Prodej: Domů',
+    'LBL_SEARCH_FORM_TITLE' => 'Vyhledat Prodej',
+    'LBL_LIST_FORM_TITLE' => 'Seznam Prodejů',
+    'LBL_NAME' => 'Název prodeje',
+    'LBL_LIST_SALE_NAME' => 'Název',
+    'LBL_LIST_ACCOUNT_NAME' => 'Název Firmy',
+    'LBL_LIST_AMOUNT' => 'Objem',
+    'LBL_LIST_DATE_CLOSED' => 'Zavřít',
+    'LBL_LIST_SALE_STAGE' => 'Fáze prodeje',
+    'LBL_ACCOUNT_ID' => 'ID Firmy',
+    //DON'T CONVERT THESE THEY ARE MAPPINGS
+    'db_name' => 'LBL_NAME',
+    //END DON'T CONVERT
+    'LBL_ACCOUNT_NAME' => 'Název Firmy:',
+    'LBL_AMOUNT' => 'Objem:',
+    'LBL_AMOUNT_USDOLLAR' => 'Objem USD:',
+    'LBL_CURRENCY' => 'Měna:',
+    'LBL_DATE_CLOSED' => 'Očekávané datum uzavření:',
+    'LBL_TYPE' => 'Typ:',
+    'LBL_CAMPAIGN' => 'Kampaň:',
+    'LBL_LEADS_SUBPANEL_TITLE' => 'Iniciativy',
+    'LBL_PROJECTS_SUBPANEL_TITLE' => 'Projekty',
+    'LBL_NEXT_STEP' => 'Další krok:',
+    'LBL_LEAD_SOURCE' => 'Zdroj Iniciativy:',
+    'LBL_SALES_STAGE' => 'Fáze prodeje:',
+    'LBL_PROBABILITY' => 'Pravděpodobnost (%):',
+    'LBL_DESCRIPTION' => 'Popis:',
+    'LBL_DUPLICATE' => 'Možný duplikát prodeje',
+    'MSG_DUPLICATE' => 'Vytvořením tohoto záznamu Prodeje můžete vytvořit duplicitní záznam, který již existuje. Záznamy Prodeje obsahující podobné názvy jsou vypsány níže. <br>Klikněte na Uložit pro vytvoření tohoto nového Prodeje nebo na Zrušit k návratu na modul bez vytvoření Prodeje.',
+    'LBL_NEW_FORM_TITLE' => 'Vytvořit prodej',
+    'ERR_DELETE_RECORD' => 'Pro smazání Prodeje musíte zadat číslo záznamu.',
+    'LBL_DEFAULT_SUBPANEL_TITLE' => 'Prodej',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Aktivity',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Historie',
+
+    'LBL_CONTACTS_SUBPANEL_TITLE' => 'Kontakty',
+    'LBL_ASSIGNED_TO_NAME' => 'Uživatel:',
+    'LBL_LIST_ASSIGNED_TO_NAME' => 'Přiřazený uživatel',
+    'LBL_ASSIGNED_TO_ID' => 'Přiřazeno k ID',
+    'LBL_MODIFIED_NAME' => 'Změněno Uživatelem',
+    'LBL_SALE_INFORMATION' => 'Prodejní informace',
+    'LBL_CURRENCY_NAME' => 'Název měny',
+    'LBL_CURRENCY_SYMBOL' => 'Symbol měny',
+    'LBL_EDIT_BUTTON' => 'Upravit',
+    'LBL_REMOVE' => 'Odebrat',
+
+);
