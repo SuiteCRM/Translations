97f2f7231048215b5041bc8c463eeda12f28326b
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-27 18:56:32 +0000

    New translations en_us.lang.php (Macedonian)

---
+++ translations/mk_MK/modules/AOS_Products/language/mk_MK.lang.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID' => 'Доделено на Корисник ID',
+    'LBL_ASSIGNED_TO_NAME' => 'Доделено на',
+    'LBL_ID' => 'ID',
+    'LBL_DATE_ENTERED' => 'Дата на креирање',
+    'LBL_DATE_MODIFIED' => 'Дата на промена',
+    'LBL_MODIFIED' => 'Променето од',
+    'LBL_MODIFIED_NAME' => 'Променето од - Име',
+    'LBL_CREATED' => 'Креирано од',
+    'LBL_DESCRIPTION' => 'Опис',
+    'LBL_DELETED' => 'Избришан',
+    'LBL_NAME' => 'Име на производ',
+    'LBL_CREATED_USER' => 'Крирано од корисник',
+    'LBL_MODIFIED_USER' => 'Променето од корисник',
+    'LBL_LIST_FORM_TITLE' => 'Листа на производи',
+    'LBL_MODULE_NAME' => 'Производи',
+    'LBL_MODULE_TITLE' => 'Производи: Домашна страна',
+    'LBL_HOMEPAGE_TITLE' => 'Мои Производи',
+    'LNK_NEW_RECORD' => 'Креирај Производ',
+    'LNK_LIST' => 'Прикажи Производи',
+    'LBL_SEARCH_FORM_TITLE' => 'Пребарај Производи',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Види историја',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Активности',
+    'LBL_NEW_FORM_TITLE' => 'Нов Производ',
+    'LBL_MAINCODE' => 'Код на Производот',
+    'VALUE' => 'Име на производ',
+    'LBL_PART_NUMBER' => 'Број на Дел',
+    'LBL_CATEGORY' => 'Категорија',
+    'LBL_TYPE' => 'Тип на Производ',
+    'LBL_COST' => 'Трошок',
+    'LBL_PRICE' => 'Цена',
+    'LBL_URL' => 'URL',
+    'LBL_CONTACT' => 'Контакт',
+    'LBL_PRODUCT_IMAGE' => 'Слика на Производот',
+    'LBL_IMAGE_UPLOAD_FAIL' => 'Грешка: вчитаниот фајл ја преминува максималната големина од: max filesize: ',
+    'LBL_AOS_PRODUCT_CATEGORYS_NAME' => 'Категорија на Производ',
+    'LBL_AOS_PRODUCT_CATEGORY' => 'Број на Категорија на Производ',
+    'LBL_AOS_PRODUCT_CATEGORIES' => 'Категорија на Производ',
+    'LBL_COST_USDOLLAR' => 'Трошок (Зададена валута)',
+    'LBL_PRICE_USDOLLAR' => 'Цена (зададена валута)',
+    'LBL_FILE_URL' => 'URL на фајлот',
+    'LBL_CUSTOMERS_PURCHASED_PRODUCTS_SUBPANEL_TITLE' => 'Купувања',
+    'LBL_PRODUCTS_PURCHASES' => 'Купувања',
+    'LBL_AOS_QUOTE_NAME' => 'Понуда',
+    'LBL_ACCOUNT_NAME' => 'Сметка',
+);
