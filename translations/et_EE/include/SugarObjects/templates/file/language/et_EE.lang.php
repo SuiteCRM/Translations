8cfbe61eb3042405ec8cd069c41db2586b233001
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-27 18:14:33 +0000

    New translations en_us.lang.php (Estonian)

---
+++ translations/et_EE/include/SugarObjects/templates/file/language/et_EE.lang.php
@@ -0,0 +1,117 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    //module
+    'LBL_MODULE_NAME' => 'Dokumendid',
+    'LBL_MODULE_TITLE' => 'Dokumendid: Avaleht',
+    'LNK_NEW_DOCUMENT' => 'Loo dokument',
+    'LNK_DOCUMENT_LIST' => 'Dokumentide loend',
+    'LBL_SEARCH_FORM_TITLE' => 'Dokumendi otsing',
+    //vardef labels
+    'LBL_NAME' => 'Dokumendi nimi',
+    'LBL_DESCRIPTION' => 'Kirjeldus',
+    'LBL_ASSIGNED_TO' => 'Vastutaja:',
+    'LBL_CATEGORY' => 'Kategooria',
+    'LBL_SUBCATEGORY' => 'Alamkategooria',
+    'LBL_STATUS' => 'Olek',
+    'LBL_IS_TEMPLATE' => 'On mall',
+    'LBL_TEMPLATE_TYPE' => 'Dokumendi tüüp',
+    'LBL_REVISION_NAME' => 'Läbivaatuse number',
+    'LBL_MIME' => 'MIME tüüp',
+    'LBL_REVISION' => 'Läbivaatus',
+    'LBL_DOCUMENT' => 'Seotud dokument',
+    'LBL_LATEST_REVISION' => 'Viimane ülevaatus',
+    'LBL_CHANGE_LOG' => 'Muudatuste logi',
+    'LBL_ACTIVE_DATE' => 'Avaldamiskuupäev',
+    'LBL_EXPIRATION_DATE' => 'Aegumiskuupäev',
+    'LBL_FILE_EXTENSION' => 'Faili laiendus',
+
+    'LBL_CAT_OR_SUBCAT_UNSPEC' => 'Määramata',
+    //quick search
+    'LBL_NEW_FORM_TITLE' => 'Uus dokument',
+    //document edit and detail view
+    'LBL_DOC_NAME' => 'Dokumendi nimi:',
+    'LBL_FILENAME' => 'Faili nimi:',
+    'LBL_FILE_UPLOAD' => 'Faili nimi',
+    'LBL_DOC_VERSION' => 'Parandus:',
+    'LBL_CATEGORY_VALUE' => 'Kategooria:',
+    'LBL_SUBCATEGORY_VALUE' => 'Alamkategooria:',
+    'LBL_DOC_STATUS' => 'Olek:',
+    'LBL_DET_TEMPLATE_TYPE' => 'Dokumendi tüüp:',
+    'LBL_DOC_DESCRIPTION' => 'Kirjeldus',
+    'LBL_DOC_ACTIVE_DATE' => 'Avaldamiskuupäev:',
+    'LBL_DOC_EXP_DATE' => 'Aegumiskuupäev:',
+
+    //document list view.
+    'LBL_LIST_FORM_TITLE' => 'Dokumendi loend',
+    'LBL_LIST_DOCUMENT' => 'Dokument',
+    'LBL_LIST_CATEGORY' => 'Kategooria',
+    'LBL_LIST_SUBCATEGORY' => 'Alamkategooria',
+    'LBL_LIST_REVISION' => 'Läbivaatus',
+    'LBL_LIST_LAST_REV_CREATOR' => 'Avaldatud',
+    'LBL_LIST_LAST_REV_DATE' => 'Ülevaatamiskuupäev',
+    'LBL_LIST_VIEW_DOCUMENT' => 'Vaade',
+    'LBL_LIST_ACTIVE_DATE' => 'Avaldamiskuupäev',
+    'LBL_LIST_EXP_DATE' => 'Aegumiskuupäev',
+    'LBL_LIST_STATUS' => 'Olek',
+
+    //document search form.
+    'LBL_SF_CATEGORY' => 'Kategooria:',
+    'LBL_SF_SUBCATEGORY' => 'Alamkategooria:',
+
+    'DEF_CREATE_LOG' => 'Dokument on loodud',
+
+    //error messages
+    'ERR_DOC_NAME' => 'Dokumendi nimi',
+    'ERR_DOC_ACTIVE_DATE' => 'Avaldamiskuupäev',
+    'ERR_FILENAME' => 'Faili nimi',
+
+    //sub-panel vardefs.
+    'LBL_LIST_DOCUMENT_NAME' => 'Dokumendi nimi',
+
+    'LBL_EDIT_BUTTON' => 'Muuda ',
+    'LBL_REMOVE' => 'Eemalda',
+
+);
