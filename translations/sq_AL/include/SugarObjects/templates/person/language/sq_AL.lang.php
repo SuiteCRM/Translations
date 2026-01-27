5d8c4bb77058f189f9593bc31299feda65c932d8
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-27 18:18:24 +0000

    New translations en_us.lang.php (Albanian)

---
+++ translations/sq_AL/include/SugarObjects/templates/person/language/sq_AL.lang.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_SALUTATION' => 'Përshëndetje',
+    'LBL_NAME' => 'Emri',
+    'LBL_FIRST_NAME' => 'Emri',
+    'LBL_LAST_NAME' => 'Mbiemri',
+    'LBL_TITLE' => 'Titulli',
+    'LBL_DEPARTMENT' => 'Departamentet',
+    'LBL_DO_NOT_CALL' => 'Mos lajmëroheni',
+    'LBL_HOME_PHONE' => 'telefoni i shtëpisë',
+    'LBL_MOBILE_PHONE' => 'Celulari',
+    'LBL_OFFICE_PHONE' => 'telefoni i zyrës',
+    'LBL_OTHER_PHONE' => 'Telefon tjetër',
+    'LBL_FAX_PHONE' => 'faks',
+    'LBL_EMAIL_ADDRESS' => 'Email Adresa',
+    'LBL_PRIMARY_ADDRESS' => 'Adresa primare',
+    'LBL_PRIMARY_ADDRESS_STREET' => 'Adresa primare',
+    'LBL_PRIMARY_ADDRESS_STREET_2' => 'Adresa e rrugës primare 2',
+    'LBL_PRIMARY_ADDRESS_STREET_3' => 'Adresa e rrugës primare 3',
+    'LBL_PRIMARY_ADDRESS_CITY' => 'Qyteti primar',
+    'LBL_PRIMARY_ADDRESS_STATE' => 'Shteti primar',
+    'LBL_PRIMARY_ADDRESS_POSTALCODE' => 'Kodi postal primar',
+    'LBL_PRIMARY_ADDRESS_COUNTRY' => 'Adresë primare e shtetit',
+    'LBL_ALT_ADDRESS' => 'Adresë tjetër',
+    'LBL_ALT_ADDRESS_STREET' => 'Adresë tjetër',
+    'LBL_ALT_ADDRESS_STREET_2' => 'Adresa alternative e rrugës 2',
+    'LBL_ALT_ADDRESS_STREET_3' => 'Adresa alternative e rrugës 3',
+    'LBL_ALT_ADDRESS_CITY' => 'Qyteti alternativ',
+    'LBL_ALT_ADDRESS_STATE' => 'Shteti alternativ',
+    'LBL_ALT_ADDRESS_POSTALCODE' => 'Kodi postal alternativ',
+    'LBL_ALT_ADDRESS_COUNTRY' => 'Shteti alternativ',
+    'LBL_PRIMARY_STREET' => 'adresat',
+    'LBL_ALT_STREET' => 'Adresa të tjera',
+    'LBL_STREET' => 'Adresa të tjera',
+    'LBL_CITY' => 'Qyteti',
+    'LBL_STATE' => 'Shteti',
+    'LBL_POSTAL_CODE' => 'Kodi postal',
+    'LBL_COUNTRY' => 'Shteti',
+    'LBL_CONTACT_INFORMATION' => 'Informacione për kontakt',
+    'LBL_ADDRESS_INFORMATION' => 'Adresë (at)',
+    'LBL_ASSIGNED_TO_NAME' => 'Përdorues',
+    'LBL_OTHER_EMAIL_ADDRESS' => 'Email tjetër',
+    'LBL_ASSISTANT' => 'Ndihmë',
+    'LBL_ASSISTANT_PHONE' => 'Telefon i ndihmësit',
+    'LBL_WORK_PHONE' => 'Telefoni i punës',
+    'LNK_IMPORT_VCARD' => 'Krijo nga vKarta',
+    'LBL_ANY_EMAIL' => 'çdo email',
+    'LBL_EMAIL_NON_PRIMARY' => 'Non Primary E-mails',
+    'LBL_PHOTO' => 'Photo',
+    'LBL_EDIT_BUTTON' => 'Ndrysho',
+    'LBL_REMOVE' => 'Fshijë',
+
+    //Lawful Basis labels
+    'LBL_LAWFUL_BASIS' => 'Lawful Basis',
+    'LBL_DATE_REVIEWED' => 'Lawful Basis Date Reviewed',
+    'LBL_LAWFUL_BASIS_SOURCE' => 'Lawful Basis Source',
+    'LBL_CONSENT' => 'Consent',
+    //End Lawful Basis labels
+);
