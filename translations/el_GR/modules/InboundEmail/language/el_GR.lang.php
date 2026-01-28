5b4e30305340ad041fa89eefbca3f925e49abdcc
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-28 18:47:52 +0000

    New translations en_us.lang.php (Greek)

---
+++ translations/el_GR/modules/InboundEmail/language/el_GR.lang.php
@@ -0,0 +1,229 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+
+
+    'LBL_RE' => 'Απάντηση:',
+
+    'ERR_BAD_LOGIN_PASSWORD' => 'Η σύνδεση ή ο Κωδικός Πρόσβασης είναι Λάθος',
+    'ERR_INI_ZLIB' => 'Δεν θα μπορούσε να κλείσει την συμπίεση Zlib προσωρινά. "Δοκιμή Ρυθμίσεων" μπορεί να απέτυχαν.',
+    'ERR_NO_IMAP' => 'Δεν βρέθηκε βιβλιοθήκη IMAP. Παρακαλώ επιλύστε αυτό πρίν συνεχίσετε με το Εισερχόμενο Email',
+    'ERR_NO_OPTS_SAVED' => 'Κανένα βέλτιστο δεν σώθηκε με τον Εσερχόμενο Λογαριασμό Email σας. Παρακαλώ αναθεωρήστε τις ρυθμίσεις',
+    'ERR_TEST_MAILBOX' => 'Παρακαλώ ελέγξτε τις ρυθμίσεις σας και προσπαθήστε πάλι.',
+    'ERR_INVALID_PORT' => 'Invalid Port',
+
+    'LBL_ASSIGN_TO_USER' => 'Ανάθεση σε Χειριστή',
+    'LBL_AUTOREPLY' => 'Πρότυπο Αυτόματης-Απάντησης',
+    'LBL_AUTOREPLY_HELP' => 'Επιλέξτε μια αυτοματοποιημένη απάντηση για να ειδοποιήσετε τους αποστολείς ηλεκτρονικού ταχυδρομείου email, ότι η απάντησή τους έχει παραληφθεί.',
+    'LBL_BASIC' => 'Πληροφορίες Λογαριασμού Ταχυδρομείου',
+    'LBL_CASE_MACRO' => 'Μακροεντολή Υπόθεσης',
+    'LBL_CASE_MACRO_DESC' => 'Καθορίστε την μακροεντολή που θα αναλυθεί και θα χρησιμοποιηθεί για να συνδέσει το εισαγόμενο ηλεκτρονικό ταχυδρομείο με μία Υπόθεση.',
+    'LBL_CASE_MACRO_DESC2' => 'Θέστε αυτό σε οποιαδήποτε αξία, αλλά συντηρήστε το "%1".',
+    'LBL_CLOSE_POPUP' => 'Κλείσιμο Παραθύρου',
+    'LBL_CREATE_TEMPLATE' => 'Δημιουργία',
+    'LBL_DELETE_SEEN' => 'Διαγραφή Αναγνωσμένων Emails Μετά την Εισαγωγή',
+    'LBL_EDIT_TEMPLATE' => 'Επεξεργασία',
+    'LBL_EMAIL_OPTIONS' => 'Επιλογές Χειρισμού Email',
+    'LBL_EMAIL_BOUNCE_OPTIONS' => 'Αναπήδηση Επιλογών Χειρισμού',
+    'LBL_FILTER_DOMAIN_DESC' => 'Διευκρινίστε μια περιοχή στην οποία καμία αυτόματη-απάντηση δεν θα σταλεί.',
+    'LBL_ASSIGN_TO_GROUP_FOLDER_DESC' => 'Επιλογή για δημιουργία αυτόματων email εγγραφών στην SuiteCRM για όλα τα εισερχόμενα μηνύματα..',
+    'LBL_FILTER_DOMAIN' => 'Όχι Αυτόματη-Απάντηση σε αυτήν την Περιοχή',
+    'LBL_FIND_SSL_WARN' => 'Η δοκιμή SSL μπορεί να πάρει πολύ χρόνο. Παρακαλώ να είστε υπομονετικοί.',
+    'LBL_FROM_ADDR' => '"Aπό" Διεύθυνση',
+    'LBL_FROM_ADDR_DESC' => 'The email address provided here might not appear in the &quot;From&quot; address section of the email sent due to restrictions imposed by the mail service provider. In these circumstances, the email address defined in the outgoing mail server will be used.', // as long as XTemplate doesn't support output escaping, transform quotes to html-entities right here (bug #48913)
+    'LBL_FROM_NAME' => '"Από" Όνομα',
+    'LBL_GROUP_QUEUE' => 'Ανατέθηκε σε Γκρουπ',
+    'LBL_HOME' => 'Αρχική Σελίδα',
+    'LBL_LIST_MAILBOX_TYPE' => 'Χρήση Λογαριασμού Ταχυδρομείου',
+    'LBL_LIST_NAME' => 'Όνομα:',
+    'LBL_LIST_GLOBAL_PERSONAL' => 'Τύπος',
+    'LBL_LIST_SERVER_URL' => 'Ταχυδρομείο Server',
+    'LBL_SERVER_ADDRESS' => 'Server Address',
+    'LBL_LIST_STATUS' => 'Κατάσταση',
+    'LBL_LOGIN' => 'Όνομα Χειριστή',
+    'LBL_USERNAME' => 'Όνομα Χειριστή',
+    'LBL_MAILBOX_DEFAULT' => 'ΕΙΣΕΡΧΟΜΕΝΑ',
+    'LBL_MAILBOX_SSL' => 'Χρήση SSL',
+    'LBL_MAILBOX_TYPE' => 'Πιθανές Ενέργειες',
+    'LBL_DISTRIBUTION_METHOD' => 'Μέθοδος Διανομής',
+    'LBL_CREATE_CASE_REPLY_TEMPLATE' => 'Νέα Υπόθεση Αυτόματη-Απάντηση Προτύπου',
+    'LBL_CREATE_CASE_REPLY_TEMPLATE_HELP' => 'Επιλέξτε μια αυτοματοποιημένη απάντηση για να ειδοποιήσετε τους αποστολείς email ότι μια περίπτωση έχει δημιουργηθεί. Το μήνυμα ηλεκτρονικού ταχυδρομείου περιέχει τον αριθμό της υπόθεσης στη γραμμή Θέματος που προσκολλάται στην ρύθμιση "Υπόθεση Μακροεντολών". Αυτή η απάντηση αποστέλλεται μόνο όταν το πρώτο μήνυμα έχει ληφθεί από τον παραλήπτη.',
+    'LBL_MAILBOX' => 'Ελεγχόμενος Φάκελος',
+    'LBL_TRASH_FOLDER' => 'Φάκελος Απορριμμάτων',
+    'LBL_SENT_FOLDER' => 'Αποστολή Φακέλου',
+    'LBL_SELECT' => 'Επιλογή',
+    'LBL_MARK_READ_NO' => 'Email που διαγράφηκε μετά από την εισαγωγή',
+    'LBL_MARK_READ_YES' => 'Email έφυγε από τον server μετά την εισαγωγή',
+    'LBL_MARK_READ' => 'Αφήστε τα Μηνύματα στον Server',
+    'LBL_MAX_AUTO_REPLIES' => 'Αριθμός Αυτόματων-Απαντήσεων',
+    'LBL_MAX_AUTO_REPLIES_DESC' => 'Καθορίστε τον μέγιστο αριθμό αυτόματων-απαντήσεων να στέλνουν σε μια μοναδική διεύθυνση emai κατά τη διάρκεια μιας περιόδου 24 ωρών.',
+    'LBL_PERSONAL_MODULE_NAME' => 'Προσωπικός Λογαριασμός Ταχυδρομείου',
+    'LBL_CREATE_CASE' => 'Δημιουργία Υπόθεσης από Email',
+    'LBL_CREATE_CASE_HELP' => 'Επιλέξτε να δημιουργήσετε αυτόματα τα αρχεία υπόθεσης στη SuiteCRM από τα εισερχόμενα emails.',
+    'LBL_MODULE_NAME' => 'Λογαριασμός Γκρουπ Ταχυδρομείου',
+    'LBL_BOUNCE_MODULE_NAME' => 'Αναπήδηση Χειρισμού Ταχυδρομικής Θυρίδας',
+    'LBL_MODULE_TITLE' => 'Εισερχόμενα Email',
+    'LBL_NAME' => 'Όνομα',
+    'LBL_NONE' => 'Καμία',
+    'LBL_ONLY_SINCE_NO' => 'Όχι. Έλεγχος σε σχέση με όλα τα μηνύματα emails στο διακομιστή ταχυδρομείου.',
+    'LBL_ONLY_SINCE_YES' => 'Ναι.',
+    'LBL_PASSWORD' => 'Κωδικός Πρόσβασης',
+    'LBL_EMAIL_PASSWORD' => 'Κωδικός Πρόσβασης',
+    'LBL_POP3_SUCCESS' => 'POP3 η σύνδεση δοκιμής σας ήταν επιτυχής.',
+    'LBL_POPUP_TITLE' => 'Δοκιμή Ρυθμίσεων',
+    'LBL_SELECT_SUBSCRIBED_FOLDERS' => 'Επιλέξτε τον Εγγεγραμμένο Φάκελο(ους)',
+    'LBL_SELECT_TRASH_FOLDERS' => 'Επιλέξτε τον Φάκελο Απορριμμάτων',
+    'LBL_SELECT_SENT_FOLDERS' => 'Επιλέξτε τον Απεσταλμένο Φάκελο',
+    'LBL_DELETED_FOLDERS_LIST' => 'Ο ακόλουθος φάκελος(οι) %ς είτε δεν υπάρχει είτε έχει διαγραφεί από τον διακομιστή',
+    'LBL_PORT' => 'Θύρα Διακομιστή Ταχυδρομείου',
+    'LBL_REPLY_TO_NAME' => '"Απάντηση σε" Όνομα',
+    'LBL_REPLY_TO_ADDR' => '"Απάντηση σε" Διεύθυνση',
+    'LBL_SAME_AS_ABOVE' => 'Χρησιμοποιώντας από Όνομα/Διεύθυνση',
+    'LBL_SERVER_OPTIONS' => 'Ρύθμιση για Προχωρημένους',
+    'LBL_SERVER_TYPE' => 'Πρωτόκολλο Διακομιστή Ταχυδρομείου',
+    'LBL_SERVER_PORT' => 'Θύρα Διακομιστή Ταχυδρομείου',
+    'LBL_SERVER_URL' => 'Διεύθυνση Διακομιστή Ταχυδρομείου',
+    'LBL_SSL_DESC' => 'Εάν ο διακομιστής ταχυδρομείου σας υποστηρίζει ασφαλείς συνδέσεις υποδοχών, η διευκόλυνση αυτή θα αναγκάσει τις συνδέσεις SSL κατά την εισαγωγή email.',
+    'LBL_ASSIGN_TO_TEAM_DESC' => 'Η επιλεγμένη ομάδα έχει πρόσβαση στο λογαριασμό ηλεκτρονικού ταχυδρομείου.',
+    'LBL_SSL' => 'Χρήση SSL',
+    'LBL_STATUS' => 'Κατάσταση',
+    'LBL_EMAIL_BODY_FILTERING' => 'Email Body Filter Type',
+    'LBL_SYSTEM_DEFAULT' => 'Προεπιλογή Συστήματος',
+    'LBL_TEST_BUTTON_TITLE' => 'Δοκιμή',
+    'LBL_TEST_SETTINGS' => 'Δοκιμή Ρυθμίσεων',
+    'LBL_TEST_CONNECTION_SETTINGS' => 'Test Connection Settings',
+    'LBL_TEST_SUCCESSFUL' => 'Η Σύνδεση ολοκληρώθηκε επιτυχώς.',
+    'LBL_TEST_WAIT_MESSAGE' => 'Μια στιγμή παρακαλώ...',
+    'LBL_WARN_IMAP_TITLE' => 'Απενεργοποιημένα Εισερχόμενα Email',
+    'LBL_WARN_IMAP' => 'Προειδοποιήσεις:',
+    'LBL_WARN_NO_IMAP' => 'Το εισερχόμενο Email δεν μπορεί να λειτουργήσει χωρίς τις βιβλιοθήκες IMAP-πελατών που επιτρέπονται/που συντάσσονται με την PHP ενότητα. Παρακαλώ επικοινωνήστε με τον διαχειριστή σας για να επιλύσετε αυτό το ζήτημα.',
+
+    'LNK_LIST_CREATE_NEW_PERSONAL' => 'New Personal Inbound Email Account',
+    'LNK_LIST_CREATE_NEW_GROUP' => 'New Group Inbound Email Account',
+    'LNK_LIST_CREATE_NEW_CASES_TYPE' => 'New Case Handling Email Account',
+    'LNK_LIST_CREATE_NEW_BOUNCE' => 'New Bounce Handling Email Account',
+    'LNK_LIST_MAILBOXES' => 'Λογαριασμοί Εισερχομένων Email',
+    'LNK_LIST_OUTBOUND_EMAILS' => 'Λογαριασμοί Εξερχομένων Email',
+    'LNK_LIST_SCHEDULER' => 'Προγραμματισμένες Εργασίες',
+    'LNK_SEED_QUEUES' => 'Σειρές Αναμονής Από Ομάδες',
+    'LBL_GROUPFOLDER_ID' => 'Ταυτότητα Φακέλου Γκρουπ',
+
+    'LBL_ALLOW_OUTBOUND_GROUP_USAGE' => 'Επιτρέψτε στους χειριστές να στείλουν emails χρησιμοποιώντας "Από"  Όνομα και Διεύθυνση ως απάντηση στη διεύθυνση',
+    'LBL_ALLOW_OUTBOUND_GROUP_USAGE_DESC' => 'Όταν αυτή η επιλογή επιλέγεται, Από  Όνομα και Από Διεύθυνση Email που συνδέεται ο λογαριασμός με αυτό το γκρουπ ταχυδρομείου, θα εμφανιστεί ως επιλογή Από το πεδίο, κατά την σύνταξη των μηνυμάτων emails για τους χειριστές που έχουν πρόσβαση στο λογαριασμό ηλεκτρονικού ταχυδρομείου του γκρουπ.',
+    'LBL_STATUS_ACTIVE' => 'Ενεργός',
+    'LBL_STATUS_INACTIVE' => 'Ανενεργός',
+    'LBL_IS_PERSONAL' => 'Προσωπικά',
+    'LBL_IS_GROUP' => 'γκρουπ',
+    'LBL_ENABLE_AUTO_IMPORT' => 'Αυτόματη Εισαγωγή Emails',
+    'LBL_WARNING_CHANGING_AUTO_IMPORT' => 'Προειδοποίηση: Τροποποιείτε την αυτόματη εισαγωγή ρυθμίσεων που μπορεί να οδηγήσει στην απώλεια στοιχείων.',
+    'LBL_WARNING_CHANGING_AUTO_IMPORT_WITH_CREATE_CASE' => 'Προειδοποίηση: Η αυτόματη εισαγωγή πρέπει να επιτραπεί όταν αυτόματα δημιουργούνται  υποθέσεις.',
+    'LBL_LIST_TITLE_MY_DRAFTS' => 'Drafts',
+    'LBL_LIST_TITLE_MY_INBOX' => 'Inbox',
+    'LBL_LIST_TITLE_MY_SENT' => 'Στάλθηκε Email',
+    'LBL_LIST_TITLE_MY_ARCHIVES' => 'Αρχειοθετημένα email',
+    'LNK_MY_DRAFTS' => 'Drafts',
+    'LNK_MY_INBOX' => 'Email',
+    'LNK_VIEW_MY_INBOX' => 'View Email',
+    'LNK_QUICK_REPLY' => 'Απάντηση',
+    'LNK_SENT_EMAIL_LIST' => 'Αποστολή email',
+    'LBL_EDIT_LAYOUT' => 'Επεξεργασία Διάταξης' /*for 508 compliance fix*/,
+    'LBL_TYPE_DIFFERENT' => 'External OAuth Connection type must be the SAME as the Inbound Email Account type',
+
+    'LBL_MODIFIED_BY' => 'Τροποποιήθηκε από',
+    'LBL_SERVICE' => 'Υπηρεσία',
+    'LBL_STORED_OPTIONS' => 'Stored Options',
+    'LBL_GROUP_ID' => 'Group ID',
+
+    'LBL_OUTBOUND_CONFIGURATION' => 'Outbound Configuration',
+    'LBL_CONNECTION_CONFIGURATION' => 'Server Configuration',
+    'LBL_AUTO_REPLY_CONFIGURATION' => 'Auto Reply Configuration',
+    'LBL_CASE_CONFIGURATION' => 'Case Configuration',
+    'LBL_GROUP_CONFIGURATION' => 'Group Configuration',
+
+    'LBL_SECURITYGROUPS_SUBPANEL_TITLE' => 'Ομάδες Ασφάλειας',
+
+
+    'LBL_OUTBOUND_EMAIL_ACCOUNT' => 'Outbound Email Account',
+    'LBL_OUTBOUND_EMAIL_ACCOUNT_ID' => 'Outbound Email Account id',
+    'LBL_OUTBOUND_EMAIL_ACCOUNT_NAME' => 'Outbound Email Account',
+
+    'LBL_AUTOREPLY_EMAIL_TEMPLATE' => 'Auto Reply Email Template',
+    'LBL_AUTOREPLY_EMAIL_TEMPLATE_ID' => 'Auto Reply Email Template id',
+    'LBL_AUTOREPLY_EMAIL_TEMPLATE_NAME' => 'Auto Reply Email Template',
+
+    'LBL_CASE_EMAIL_TEMPLATE' => 'Case Email Template',
+    'LBL_CASE_EMAIL_TEMPLATE_ID' => 'Case Email Template id',
+    'LBL_CASE_EMAIL_TEMPLATE_NAME' => 'Case Email Template',
+
+    'LBL_PROTOCOL' => 'Protocol',
+    'LBL_CONNECTION_STRING' => 'Connection String',
+    'LBL_DISTRIB_METHOD' => 'Μέθοδος Διανομής',
+    'LBL_DISTRIB_OPTIONS' => 'Distribution Options',
+
+    'LBL_DISTRIBUTION_USER' => 'Distribution User',
+    'LBL_DISTRIBUTION_USER_ID' => 'Distribution User id',
+    'LBL_DISTRIBUTION_USER_NAME' => 'Distribution User',
+
+    'LBL_EXTERNAL_OAUTH_CONNECTION' => 'External OAuth Connection',
+    'LBL_EXTERNAL_OAUTH_CONNECTION_ID' => 'External OAuth Connection id',
+    'LBL_EXTERNAL_OAUTH_CONNECTION_NAME' => 'External OAuth Connection',
+    'LNK_EXTERNAL_OAUTH_CONNECTIONS' => 'External OAuth Connections',
+
+    'LBL_TYPE' => 'Τύπος',
+    'LBL_AUTH_TYPE' => 'Auth Type',
+    'LBL_IS_DEFAULT' => 'Προεπιλογή',
+    'LBL_SIGNATURE' => 'Υπογραφή',
+
+    'LBL_OWNER_NAME' => 'Ιδιοκτήτης',
+
+    'LBL_SET_AS_DEFAULT_BUTTON' => 'Set as default',
+
+    'LBL_MOVE_MESSAGES_TO_TRASH_AFTER_IMPORT' => 'Move Messages To Trash After Import?',
+    'LBL_LAST_IMPORT_RUN_DATETIME' => 'Last Import Run Date/Time',
+    'LBL_MAILBOX_LAST_IMPORTED_DAYS' => 'Last Imported Date Per Mailbox',
+    'LBL_EMAIL_IMPORT_PER_RUN_THRESHOLD' => 'Emails to Import Per Run',
+    'LBL_EMAIL_IMPORT_TIMEFRAME_START' => 'Import Timeframe Start',
+    'LBL_EMAIL_IMPORT_UNREAD_ONLY' => 'Import Unread Emails Only'
+);
