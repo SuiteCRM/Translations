9413bd6c8779529c275d0d0e702ad46f21a5dfae
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-28 18:20:12 +0000

    New translations en_us.lang.php (Tagalog)

---
+++ translations/tl_PH/modules/Currencies/language/tl_PH.lang.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_MODULE_NAME' => 'Ang mga halaga',
+    'LBL_LIST_FORM_TITLE' => 'Ang mga halaga',
+    'LBL_CURRENCY' => 'Ang Pera',
+    'LBL_ADD' => 'Magdagdag',
+    'LBL_MERGE' => 'Pagsamahin',
+    'LBL_MERGE_TXT' => 'Mangyari lamang na pumili ng mga kurensiya na gusto mong i-mapa sa napiling kurensiya. Ito ay magbubura sa lahat ng mga kurensiya gamit ang isang tsekmark at magtatalaga muli sa kahit anong mga balyu na kaugnay sa kanila sa napiling kurensiya.',
+    'LBL_US_DOLLAR' => 'U.S. na Dolyar',
+    'LBL_DELETE' => 'Burahin',
+    'LBL_LIST_SYMBOL' => 'Simbolo ng pera',
+    'LBL_LIST_NAME' => 'Pangalan ng pera',
+    'LBL_LIST_ISO4217' => 'ISO 4217 Code',
+    'LBL_LIST_ISO4217_HELP' => 'Maglagay ng tatlong-letra ng ISO 4217 code na magbibigay kahulugan sa pangalan ng kurensiya at ng simbolo ng kurensiya.',
+    'LBL_UPDATE' => 'I-upadate',
+    'LBL_LIST_RATE' => 'Palitan',
+    'LBL_LIST_RATE_HELP' => 'Ang Palitan ng 0.5 para sa Euro ay nangangahulugan na 10 USD = 5 Euro.',
+    'LBL_LIST_STATUS' => 'Kalagayan',
+    'LNK_NEW_CONTACT' => 'Bagong Kontak',
+    'LNK_NEW_ACCOUNT' => 'Bagong account',
+    'LNK_NEW_OPPORTUNITY' => 'Bagong Oportunidad',
+    'LNK_NEW_CASE' => 'Bagong Kaso',
+    'LNK_NEW_NOTE' => 'Gumawa ng Nota o Ilalakip',
+    'LNK_NEW_CALL' => 'Bagong Tawag',
+    'LNK_NEW_EMAIL' => 'Bagong Email',
+    'LNK_NEW_MEETING' => 'Bagong Pagpupulong',
+    'LNK_NEW_TASK' => 'Gumawa ng Gawain',
+    'NTC_DELETE_CONFIRMATION' => 'Sigurado ka bang gusto mong burahin ang rekord na ito? Ang kahit anong rekord na gamit ang kurensiyang ito ay ma-papalitan sa default na kurensiya ng sistema na kung saan sila ay mapapasok. Mas mabuting itakda ang estado bilang hindi aktibo.',
+    'LBL_BELOW_MIN' => 'Ang palitan ay kinakailangang maging mas mataas sa 0',
+    'currency_status_dom' =>
+        array(
+            'Active' => 'Gawing aktibo',
+            'Inactive' => 'Huwag gawing aktibo',
+        ),
+    'LBL_CREATED_BY' => 'Nilikha ni',
+    'LBL_EDIT_LAYOUT' => 'I-edit ang layout' /*for 508 compliance fix*/,
+);
