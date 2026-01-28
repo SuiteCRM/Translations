8df48d121f63e37c264d40589c688ffaaf0cf369
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-28 19:48:14 +0000

    New translations en_us.lang.php (Norwegian Bokmal)

---
+++ translations/nb_NO/modules/jjwg_Areas/language/nb_NO.lang.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings['LBL_ASSIGNED_TO_ID'] = 'Tilordnet brukerID';
+$mod_strings['LBL_ASSIGNED_TO_NAME'] = 'Bruker';
+$mod_strings['LBL_ID'] = 'ID';
+$mod_strings['LBL_DATE_ENTERED'] = 'Dato opprettet';
+$mod_strings['LBL_DATE_MODIFIED'] = 'Dato endret';
+$mod_strings['LBL_MODIFIED'] = 'Endret av';
+$mod_strings['LBL_MODIFIED_NAME'] = 'Endret av navn';
+$mod_strings['LBL_CREATED'] = 'Opprettet av';
+$mod_strings['LBL_DESCRIPTION'] = 'Beskrivelse';
+$mod_strings['LBL_DELETED'] = 'Slettet';
+$mod_strings['LBL_NAME'] = 'Navn';
+$mod_strings['LBL_CREATED_USER'] = 'Opprettet av bruker';
+$mod_strings['LBL_MODIFIED_USER'] = 'Modifisert av bruker';
+$mod_strings['LBL_LIST_NAME'] = 'Navn';
+$mod_strings['LBL_LIST_FORM_TITLE'] = 'Liste over områder';
+$mod_strings['LBL_MODULE_NAME'] = 'Områder';
+$mod_strings['LBL_MODULE_TITLE'] = 'Områder';
+$mod_strings['LBL_HOMEPAGE_TITLE'] = 'Mine områder';
+$mod_strings['LNK_NEW_RECORD'] = 'Opprett områder';
+$mod_strings['LNK_LIST'] = 'Vis områder';
+$mod_strings['LBL_SEARCH_FORM_TITLE'] = 'Søk i områder';
+$mod_strings['LBL_HISTORY_SUBPANEL_TITLE'] = 'Se på historikk';
+$mod_strings['LBL_ACTIVITIES_SUBPANEL_TITLE'] = 'Aktiviteter';
+$mod_strings['LBL_NEW_FORM_TITLE'] = 'Nye områder';
+$mod_strings['LBL_CITY'] = 'By:';
+$mod_strings['LBL_STATE'] = 'Fylke:';
+$mod_strings['LBL_COUNTRY'] = 'Land:';
+$mod_strings['LBL_COORDINATES'] = 'Koordinater';
+$mod_strings['LBL_LIST_ASSIGNED_USER'] = 'Bruker';
+$mod_strings['LBL_AREA_MAP'] = 'Områdekart';
+
+$mod_strings['LBL_AREA_EDIT_TITLE'] = 'Områdeopprettingsinstruksjoner:';
+$mod_strings['LBL_AREA_EDIT_DESC_1'] = 'Venstreklikk på kartet, og beveg musepekeren med klokka, for å opprette markørpunkter for området.';
+$mod_strings['LBL_AREA_EDIT_DESC_2'] = 'Klikk på det første markørpunktet for å lukke mangekantområdet.';
+$mod_strings['LBL_AREA_EDIT_RESET'] = 'Tilbakestill';
+$mod_strings['LBL_AREA_EDIT_USE_AREA_COORDINATES'] = 'Bruk områdekoordinater';
+$mod_strings['LBL_AREA_EDIT_COORDINATE_RESULTS'] = 'Koordinatresultater (lengdegrad, breddegrad, høyde)';
+$mod_strings['LBL_JJWG_MAPS_JJWG_AREAS_FROM_JJWG_MAPS_TITLE'] = 'Kart';
