3b9f5254e1309e0c5533257f4012d38cc24f1856
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-28 19:37:10 +0000

    New translations en_us.lang.php (Korean)

---
+++ translations/ko_KR/modules/Surveys/language/ko_KR.lang.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID'                                     => '할당된 사용자 Id',
+    'LBL_ADD_OPTION'                                         => 'Add new option',
+    'LBL_ASSIGNED_TO_NAME'                                   => '담당자',
+    'LBL_SECURITYGROUPS'                                     => '보안 그룹',
+    'LBL_SECURITYGROUPS_SUBPANEL_TITLE'                      => '보안 그룹',
+    'LBL_ID'                                                 => 'ID',
+    'LBL_DATE_ENTERED'                                       => '생성날짜',
+    'LBL_DATE_MODIFIED'                                      => '수정일',
+    'LBL_MODIFIED'                                           => '변경됨',
+    'LBL_MODIFIED_ID'                                        => 'Id로 변경됨',
+    'LBL_MODIFIED_NAME'                                      => '이름으로 변경됨',
+    'LBL_CREATED'                                            => '작성자',
+    'LBL_CREATED_ID'                                         => 'Id로 생성됨',
+    'LBL_DESCRIPTION'                                        => '상세',
+    'LBL_DELETED'                                            => '삭제됨',
+    'LBL_NAME'                                               => '이름',
+    'LBL_CREATED_USER'                                       => '사용자로 생성됨',
+    'LBL_MODIFIED_USER'                                      => '변경유저',
+    'LBL_LIST_NAME'                                          => '이름',
+    'LBL_EDIT_BUTTON'                                        => '편집',
+    'LBL_REMOVE'                                             => '삭제',
+    'LBL_LIST_FORM_TITLE'                                    => '설문조사 목록',
+    'LBL_MODULE_NAME'                                        => '설문조사',
+    'LBL_MODULE_TITLE'                                       => '설문조사',
+    'LBL_HOMEPAGE_TITLE'                                     => '내 설문조사',
+    'LNK_NEW_RECORD'                                         => '설문조사 생성',
+    'LNK_LIST'                                               => '설문조사 보기',
+    'LNK_IMPORT_SURVEYS'                                     => '설문조사 가져오기',
+    'LBL_SEARCH_FORM_TITLE'                                  => '설문조사 검색',
+    'LBL_HISTORY_SUBPANEL_TITLE'                             => '이력 표시',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE'                          => '영업활동',
+    'LBL_SURVEYS_SUBPANEL_TITLE'                             => '설문조사',
+    'LBL_NEW_FORM_TITLE'                                     => '신규 설문조사',
+    'LBL_STATUS'                                             => '상태',
+    'LBL_SURVEY_QUESTIONS_DISPLAY'                           => '질문',
+    'LBL_SURVEY_URL_DISPLAY'                                 => 'URL:',
+    'LBL_HAPPINESS_QUESTION'                                 => '행복한 질문',
+    'LBL_CANT_EDIT_RESPONDED'                                => '답변한 설문조사 질문은 수정할 수 없습니다.',
+    'LBL_VIEW_SURVEY_REPORTS'                                => '설문조사 보고서 보기',
+    'LBL_CHECKED'                                            => '선택',
+    'LBL_UNCHECKED'                                          => '선택해제',
+    'LBL_RESPONSE_ANSWER'                                    => '답변',
+    'LBL_RESPONSE_CONTACT'                                   => '연락처',
+    'LBL_RESPONSE_TIME'                                      => '날짜',
+    'LBL_UNKNOWN_CONTACT'                                    => '알수 없음',
+    'LBL_RESPONSE_COUNT'                                     => '개수',
+    'LBL_SUBMIT_TEXT'                                        => '제출',
+    'LBL_SATISFIED_TEXT'                                     => '만족 텍스트',
+    'LBL_NEITHER_TEXT'                                       => '부정 텍스트',
+    'LBL_DISSATISFIED_TEXT'                                  => '불만족 텍스트',
+    'LBL_SURVEYS_SURVEYQUESTIONS_FROM_SURVEYQUESTIONS_TITLE' => '설문조사 질문',
+    'LBL_SURVEYS_SURVEYRESPONSES_FROM_SURVEYRESPONSES_TITLE' => '설문조사 답변',
+    'LBL_SHOW_RESPONSES' => '답변 보기',
+    'LBL_HIDE_RESPONSES' => 'Hide responses',
+    'LBL_SURVEY_QUESTION' => 'Question',
+    'LBL_SURVEY_TEXT' => '텍스트',
+    'LBL_SURVEY_TYPE' => '종류',
+    'LBL_SURVEY_ACTIONS' => '작업',
+    'LBL_SURVEY_NEW_QUESTION' => 'New Question',
+    'LBL_SURVEY_RESPONSES' => 'New Question',
+    'LBL_SURVEYS_SENT' => 'Surveys Sent:',
+    'LBL_SURVEY_DISTINCT' => 'Distinct Surveys Sent:',
+    'LBL_DRAFT' => '초안',
+    'LBL_SUBMIT' => '송신',
+    'LBL_STARS' => 'Stars',
+    'LBL_SURVEY_CLOSE_RESPONSE' => 'Thanks for your interest but this survey is now closed.',
+    'LBL_SURVEY_THANKS' => 'Thanks for completing this survey.',
+);
