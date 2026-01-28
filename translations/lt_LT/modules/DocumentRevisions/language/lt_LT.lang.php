731cdd9bdafaee1b6e2b1df7adbadb64965fc771
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-28 18:21:57 +0000

    New translations en_us.lang.php (Lithuanian)

---
+++ translations/lt_LT/modules/DocumentRevisions/language/lt_LT.lang.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    //module
+    'LBL_MODULE_NAME' => 'Dokumento versijos',
+
+    'LNK_NEW_DOCUMENT' => 'Sukurti dokumentą',
+    'LNK_DOCUMENT_LIST' => 'Dokumentų sąrašas',
+
+    //vardef labels
+    'LBL_REVISION_NAME' => 'Versijos Nr.',
+    'LBL_FILENAME' => 'Filo:',
+    'LBL_MIME' => 'Mime tipas',
+    'LBL_REVISION' => 'Versija',
+    'LBL_DOCUMENT' => 'Susiję dokumentai',
+    'LBL_LATEST_REVISION' => 'Naujausia versija',
+    'LBL_CHANGE_LOG' => 'Pakeitimų istorija',
+    'LBL_ACTIVE_DATE' => 'Publikavimo data',
+    'LBL_EXPIRATION_DATE' => 'Pasibaigimo datą',
+    'LBL_FILE_EXTENSION' => 'Failo plėtinys',
+
+    'LBL_DOC_NAME' => 'Dokumento pavadinimas:',
+    'LBL_DOC_VERSION' => 'Versija:',
+
+    //document revisions.
+    'LBL_REV_LIST_REVISION' => 'Versija',
+    'LBL_REV_LIST_ENTERED' => 'Sukurta',
+    'LBL_REV_LIST_CREATED' => 'Sukūrė:',
+    'LBL_REV_LIST_LOG' => 'Pakeitimų istorija',
+    'LBL_REV_LIST_FILENAME' => 'Failo pavadinimas',
+
+    'LBL_CURRENT_DOC_VERSION' => 'Naujausia versija:',
+    'LBL_SEARCH_FORM_TITLE' => 'Dokumentų paieška',
+    'LBL_REVISIONS' => 'Versijos',
+
+    //error messages
+    'ERR_FILENAME' => 'Failo pavadinimas',
+    'ERR_DOC_VERSION' => 'Dokumento versija',
+    'ERR_DELETE_CONFIRM' => 'Ar Jūs norite ištrinti šią dokumento versiją?',
+    'ERR_DELETE_LATEST_VERSION' => 'Jums neleidžiama trinti naujausios dokumento versijos.',
+    'LNK_NEW_MAIL_MERGE' => 'El. paštas apjungiamas',
+    'LBL_DOC_ID' => 'Dokumento šaltinio ID',
+    'LBL_DOC_TYPE' => 'Šaltinis',
+    'LBL_DOC_URL' => 'Dokumento šaltinio nuoroda',
+
+    'LBL_CREATED_BY_NAME' => 'Created by Name',
+);
