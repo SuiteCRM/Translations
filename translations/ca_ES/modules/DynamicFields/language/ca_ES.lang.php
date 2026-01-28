58988c70114f795a14f3566fb7099aa48804c091
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-28 18:23:38 +0000

    New translations en_us.lang.php (Catalan)

---
+++ translations/ca_ES/modules/DynamicFields/language/ca_ES.lang.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LNK_NEW_CALL' => 'Programar Trucada',
+    'LNK_NEW_MEETING' => 'Programar Reunió',
+    'LNK_NEW_TASK' => 'Nova Tasca',
+    'LNK_NEW_NOTE' => 'Nova Nota o Arxiu Adjunt',
+    'LNK_NEW_EMAIL' => 'Arxivar correu electrònic',
+    'LNK_CALL_LIST' => 'Trucades',
+    'LNK_MEETING_LIST' => 'Reunions',
+    'LNK_TASK_LIST' => 'Tasques',
+    'LNK_NOTE_LIST' => 'Notes',
+    'LBL_ADD_FIELD' => 'Afegir camp:',
+    'LBL_SEARCH_FORM_TITLE' => 'Cerca de mòdul',
+    'COLUMN_TITLE_NAME' => 'Nom de Camp',
+    'COLUMN_TITLE_DISPLAY_LABEL' => 'Mostrar etiqueta',
+    'COLUMN_TITLE_LABEL_VALUE' => 'Valor de la etiqueta',
+    'COLUMN_TITLE_LABEL' => 'Etiqueta del sistema',
+    'COLUMN_TITLE_DATA_TYPE' => 'Tipus de dada',
+    'COLUMN_TITLE_MAX_SIZE' => 'Mida màxima',
+    'COLUMN_TITLE_HELP_TEXT' => 'Text d\'ajuda',
+    'COLUMN_TITLE_COMMENT_TEXT' => 'Text de comentari',
+    'COLUMN_TITLE_REQUIRED_OPTION' => 'Camp requerit',
+    'COLUMN_TITLE_DEFAULT_VALUE' => 'Valor per Defecte',
+    'COLUMN_TITLE_FRAME_HEIGHT' => 'Altura del IFrame',
+    'COLUMN_TITLE_HTML_CONTENT' => 'HTML',
+    'COLUMN_TITLE_URL' => 'URL per defecte',
+    'COLUMN_TITLE_AUDIT' => 'Auditat',
+    'COLUMN_TITLE_MIN_VALUE' => 'Valor mínim',
+    'COLUMN_TITLE_MAX_VALUE' => 'Valor màxim',
+    'COLUMN_TITLE_LABEL_ROWS' => 'Files',
+    'COLUMN_TITLE_LABEL_COLS' => 'Columnes',
+    'COLUMN_TITLE_DISPLAYED_ITEM_COUNT' => 'Nº d\'elements mostrats',
+    'COLUMN_TITLE_AUTOINC_NEXT' => 'Autoincrementar el següent valor',
+    'COLUMN_DISABLE_NUMBER_FORMAT' => 'Deshabilitar format',
+    'COLUMN_TITLE_ENABLE_RANGE_SEARCH' => 'Habilitar la cerca per ranc',
+    'LBL_DROP_DOWN_LIST' => 'Llista desplegable',
+    'LBL_RADIO_FIELDS' => 'Opcions radio',
+    'LBL_MULTI_SELECT_LIST' => 'Selecció múltiple',
+    'COLUMN_TITLE_PRECISION' => 'Precisió',
+    'LBL_MODULE' => 'Mòdul',
+    'COLUMN_TITLE_MASS_UPDATE' => 'Actualització Massiva',
+    'COLUMN_TITLE_IMPORTABLE' => 'Es pot importar',
+    'COLUMN_TITLE_DUPLICATE_MERGE' => 'Combinar duplicats',
+    'LBL_LABEL' => 'Etiqueta',
+    'LBL_DATA_TYPE' => 'Tipus de dada',
+    'LBL_DEFAULT_VALUE' => 'Valor per Defecte',
+    'ERR_RESERVED_FIELD_NAME' => "Paraula reservada",
+    'ERR_SELECT_FIELD_TYPE' => 'Si us plau, seleccioni un tipus de camp',
+    'ERR_FIELD_NAME_ALREADY_EXISTS' => 'El nom del camp ja existeix',
+    'LBL_BTN_ADD' => 'Afegir',
+    'LBL_BTN_EDIT' => 'Editar',
+    'LBL_GENERATE_URL' => 'Generar URL',
+    'LBL_CALCULATED' => 'Valor calculat',
+    'LBL_LINK_TARGET' => 'Obre enllaç en',
+    'LBL_IMAGE_WIDTH' => 'Amplada',
+    'LBL_IMAGE_HEIGHT' => 'Altura',
+    'LBL_IMAGE_BORDER' => 'Frontera',
+    'LBL_HELP' => 'Ajuda' /*for 508 compliance fix*/,
+    'COLUMN_TITLE_INLINE_EDIT_TEXT' => 'edició de línia ',
+    'COLUMN_TITLE_PARENT_ENUM' => 'Llista desplegable pare',
+    'LBL_INSERT_FIELD' => 'Insert Field',
+);
