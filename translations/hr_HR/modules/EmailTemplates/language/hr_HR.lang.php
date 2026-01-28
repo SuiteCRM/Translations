c6eb92c3b9cd7430d15dec4fe0de7506cf0bdace
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-28 18:31:53 +0000

    New translations en_us.lang.php (Croatian)

---
+++ translations/hr_HR/modules/EmailTemplates/language/hr_HR.lang.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ADD_DOCUMENT' => 'Add a Document',
+    'LBL_ADD_FILE' => 'Dodaj datoteku',
+    'LBL_ATTACHMENTS' => 'Privitci',
+    'LBL_BODY' => 'Body:',
+    'LBL_CLOSE' => 'Zatvori:',
+    'LBL_DESCRIPTION' => 'Opis',
+    'LBL_EDIT_ALT_TEXT' => 'Uredi običan tekst',
+    'LBL_EMAIL_ATTACHMENT' => 'Email privitak',
+    'LBL_HIDE_ALT_TEXT' => 'Sakrij običan tekst',
+    'LBL_HTML_BODY' => 'HTML tijelo',
+    'LBL_INSERT_VARIABLE' => 'Umetni varijablu',
+    'LBL_INSERT_URL_REF' => 'Umetni URL referencu',
+    'LBL_INSERT_TRACKER_URL' => 'Umetni URL trackera:',
+    'LBL_INSERT' => 'Ubaci',
+    'LBL_LIST_DATE_MODIFIED' => 'Posljednje modificirano',
+    'LBL_LIST_DESCRIPTION' => 'Opis',
+    'LBL_LIST_FORM_TITLE' => 'Lista predložaka e-pošte',
+    'LBL_LIST_NAME' => 'Ime',
+    'LBL_MODULE_NAME' => 'Predlošci e-pošte',
+    'LBL_MODULE_TITLE' => 'Predložak e-pošte: Početno',
+    'LBL_NAME' => 'Naziv:',
+    'LBL_NEW_FORM_TITLE' => 'Kreiraj predložak e-pošte',
+    'LBL_PUBLISH' => 'Objavi:',
+    'LBL_RELATED_TO' => 'Povezano sa:',
+    'LBL_SEARCH_FORM_TITLE' => 'Pretraga predložaka e-pošte',
+    'LBL_SHOW_ALT_TEXT' => 'Prikaži običan tekst',
+    'LBL_SUBJECT' => 'Subjekt:',
+    'LBL_SUITE_DOCUMENT' => 'Dokument',
+    'LBL_UPLOAD_FILE' => 'Upload File',
+    'LBL_TEXT_BODY' => 'Tijelo teksta:',
+    'LBL_USERS' => 'korisnik',
+
+    'LNK_EMAIL_TEMPLATE_LIST' => 'Pregledaj predloške e-pošte',
+    'LNK_IMPORT_NOTES' => 'Uvoz bilješki',
+    'LNK_NEW_EMAIL_TEMPLATE' => 'Kreiraj predložak e-pošte',
+    'LNK_NEW_EMAIL' => 'Arhiviraj e-poštu',
+    'LNK_NEW_SEND_EMAIL' => 'Sastavi e-poštu',
+    'LNK_SENT_EMAIL_LIST' => 'Poslane e-pošte',
+    'LNK_VIEW_CALENDAR' => 'Danas',
+    // for Inbox
+    'LBL_NEW' => 'Novi',
+    'LNK_MY_DRAFTS' => 'Moje skice',
+    'LNK_MY_INBOX' => 'Moj e-pošta',
+    'LBL_TEXT_ONLY' => 'Samo tekst',
+    'LBL_SEND_AS_TEXT' => 'Pošalji samo tekst',
+    'LBL_ACCOUNT' => 'Tvrtka',
+    'LBL_FROM_NAME' => 'Od imena',
+    'LBL_PLAIN_TEXT' => 'Običan tekst',
+    'LBL_CREATED_BY' => 'Kreirano od',
+    'LBL_PUBLISHED' => 'Objavljeno',
+    'LNK_VIEW_MY_INBOX' => 'Pregledaj moju e-poštu',
+    'LBL_ASSIGNED_TO_ID' => 'Dodijeljeno',
+    'LBL_EDIT_LAYOUT' => 'Uredi izgled' /*for 508 compliance fix*/,
+    'LBL_SELECT' => 'Odaberi' /*for 508 compliance fix*/,
+    'LBL_ID_FF_CLEAR' => 'Očisti' /*for 508 compliance fix*/,
+    'LBL_TYPE' => 'Tip',
+    'LBL_WIDTH' => 'Width Default',
+    'LNK_IMPORT_CAMPAIGNS' => 'Import Campaign',
+);
