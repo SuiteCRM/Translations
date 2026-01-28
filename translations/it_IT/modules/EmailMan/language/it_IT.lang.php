df08b18180798505d1b538d6ab17952580ff2031
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-28 18:28:36 +0000

    New translations en_us.lang.php (Italian)

---
+++ translations/it_IT/modules/EmailMan/language/it_IT.lang.php
@@ -0,0 +1,151 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_SEND_DATE_TIME' => 'Data Invio',
+    'LBL_IN_QUEUE' => 'In lavorazione',
+    'LBL_IN_QUEUE_DATE' => 'Data in Coda',
+
+    'ERR_INT_ONLY_EMAIL_PER_RUN' => 'Per specificare il numero di email inviate per batch usare solamente valori interi',
+
+    'LBL_ATTACHMENT_AUDIT' => 'è stato inviato. Non è stato duplicato localmente per risparmiare spazio su disco.',
+    'LBL_CONFIGURE_SETTINGS' => 'Configura Impostazioni Email',
+    'LBL_CUSTOM_LOCATION' => 'Specificato dall\'utente',
+    'LBL_DEFAULT_LOCATION' => 'Predefinita',
+
+    'LBL_EMAIL_DEFAULT_DELETE_ATTACHMENTS' => 'Elimina note correlate & files allegati con le email eliminate',
+    'LBL_EMAIL_WARNING_NOTIFICATIONS' => 'Notifiche di avviso via email',
+    'LBL_EMAIL_ENABLE_CONFIRM_OPT_IN' => 'Opt in Impostazioni',
+    'LBL_EMAIL_ENABLE_SEND_OPT_IN' => 'Invia automaticament Opt In Email',
+    'LBL_EMAIL_CONFIRM_OPT_IN_TEMPLATE_ID' => 'Conferma Opt In Email Template',
+    'LBL_LEGACY_EMAIL_COMPOSE_BEHAVIOR' => 'Abilita Comportamento Email Legacy',
+    'LBL_EMAIL_OUTBOUND_CONFIGURATION' => 'Configurazione Email in uscita',
+    'LBL_EMAILS_PER_RUN' => 'Numero di email per batch:',
+    'LBL_ID' => 'ID',
+    'LBL_LIST_CAMPAIGN' => 'Campagna',
+    'LBL_LIST_FORM_TITLE' => 'Coda',
+    'LBL_LIST_FROM_NAME' => 'Nome mittente',
+    'LBL_LIST_IN_QUEUE' => 'In lavorazione',
+    'LBL_LIST_MESSAGE_NAME' => 'Messaggio Marketing',
+    'LBL_LIST_RECIPIENT_EMAIL' => 'Email Destinatario',
+    'LBL_LIST_RECIPIENT_NAME' => 'Nome Destinatario',
+    'LBL_LIST_SEND_ATTEMPTS' => 'Invia Tentativi',
+    'LBL_LIST_SEND_DATE_TIME' => 'Spedire',
+    'LBL_LIST_USER_NAME' => 'Utente per Autenticazione',
+    'LBL_LOCATION_ONLY' => 'Posizione',
+    'LBL_LOCATION_TRACK' => 'Posizione dei file di monitoraggio della campagna (ad es. campaing_tracker.php)',
+    'LBL_CAMP_MESSAGE_COPY' => 'Mantieni una copia dei messaggi delle campagne:',
+    'LBL_CAMP_MESSAGE_COPY_DESC' => 'Vuole conservare una copia di <bold>OGNI</bold> messaggio email inviato durante tutte le campagne?  <bold>Noi consigliamo e impostiamo no.</bold>. Scegliendo no sarà conservato solamente il template che è stato usato e che necessita di eventuali variabili per essere riutilizzato come messaggio individuale.',
+    'LBL_MAIL_SENDTYPE' => 'Mail Transfer Agent (MTA):',
+    'LBL_MAIL_SMTPAUTH_REQ' => 'Utilizzare Autenticazione SMTP ?',
+    'LBL_MAIL_SMTPPASS' => 'Password:',
+    'LBL_MAIL_SMTPPORT' => 'Porta SMTP:',
+    'LBL_MAIL_SMTPSERVER' => 'Server Mail SMTP:',
+    'LBL_MAIL_SMTPUSER' => 'Utente per Autenticazione:',
+    'LBL_CHOOSE_EMAIL_PROVIDER' => 'Scegli il tuo provider Email',
+    'LBL_YAHOOMAIL_SMTPPASS' => 'Password Mail Yahoo!',
+    'LBL_YAHOOMAIL_SMTPUSER' => 'ID Mail Yahoo!',
+    'LBL_GMAIL_SMTPPASS' => 'Password Gmail',
+    'LBL_GMAIL_SMTPUSER' => 'Indirizzo Email di Gmail',
+    'LBL_EXCHANGE_SMTPPASS' => 'Password di Exchange',
+    'LBL_EXCHANGE_SMTPUSER' => 'Nome Utente di Exchange',
+    'LBL_EXCHANGE_SMTPPORT' => 'Exchange Server Porta',
+    'LBL_EXCHANGE_SMTPSERVER' => 'Cambia Server',
+    'LBL_EMAIL_LINK_TYPE' => 'Programma di Posta',
+    'LBL_MARKETING_ID' => 'Marketing',
+    'LBL_MODULE_ID' => 'EmailMan',
+    'LBL_MODULE_NAME' => 'Impostazioni Email',
+    'LBL_MODULE_TITLE' => 'Gestione Coda Email in uscita',
+    'LBL_NOTIFICATION_ON_DESC' => 'Se abilitato, le email vengono inviate agli utenti quando i record vengono assegnati a loro.',
+    'LBL_NOTIFY_FROMADDRESS' => 'Indirizzo Mittente:',
+    'LBL_NOTIFY_FROMNAME' => '"Da" Nome:',
+    'LBL_NOTIFY_ON' => 'Assegnazione Notifiche',
+    'LBL_NOTIFY_TITLE' => 'Opzioni Email',
+    'LBL_OUTBOUND_EMAIL_TITLE' => 'Opzioni Posta in Uscita',
+    'LBL_RELATED_ID' => 'ID Correlato',
+    'LBL_RELATED_TYPE' => 'Tipo Relazione',
+    'LBL_SEARCH_FORM_TITLE' => 'Cerca Coda',
+    'TRACKING_ENTRIES_LOCATION_DEFAULT_VALUE' => 'Valore delle impostazioni site_url di config.php',
+    'TXT_REMOVE_ME_ALT' => 'Per rimuoverti da questa lista vai in',
+    'TXT_REMOVE_ME_CLICK' => 'clicca qui',
+    'TXT_REMOVE_ME' => 'Per rimuoverti da questa lista',
+    'LBL_NOTIFY_SEND_FROM_ASSIGNING_USER' => 'Invia una notifica dall\'indirizzo email dell\'utente assegnato',
+
+    'LBL_SECURITY_TITLE' => 'Impostazioni Sicurezza Email',
+    'LBL_SECURITY_DESC' => 'Seleziona i tag che non devono essere permessi nelle mail in entrata o mostrati nel modulo Email.',
+    'LBL_SECURITY_APPLET' => 'Applet tag',
+    'LBL_SECURITY_BASE' => 'Base tag',
+    'LBL_SECURITY_EMBED' => 'Embed tag',
+    'LBL_SECURITY_FORM' => 'Form tag',
+    'LBL_SECURITY_FRAME' => 'Frame tag',
+    'LBL_SECURITY_FRAMESET' => 'Frameset tag',
+    'LBL_SECURITY_IFRAME' => 'iFrame tag',
+    'LBL_SECURITY_IMPORT' => 'Importa tag',
+    'LBL_SECURITY_LAYER' => 'Layer tag',
+    'LBL_SECURITY_LINK' => 'Link tag',
+    'LBL_SECURITY_OBJECT' => 'Object tag',
+    'LBL_SECURITY_OUTLOOK_DEFAULTS' => 'Selezione il livello minimo di sicurezza per Outlook (errori nella visualizzazione).',
+    'LBL_SECURITY_STYLE' => 'Style tag',
+    'LBL_SECURITY_TOGGLE_ALL' => 'Attiva tutte le opzioni',
+    'LBL_SECURITY_XMP' => 'Xmp tag',
+    'LBL_YES' => 'Sì',
+    'LBL_NO' => 'No',
+    'LBL_PREPEND_TEST' => '[Test]:',
+    'LBL_SEND_ATTEMPTS' => 'Invia Tentativi',
+    'LBL_OUTGOING_SECTION_HELP' => 'Configura il server di posta in uscita predefinito per inviare notifiche via email, inclusi avvisi del flusso di lavoro.',
+    'LBL_ALLOW_DEFAULT_SELECTION' => "Gli utenti possono inviare come identità di questo account:",
+    'LBL_ALLOW_DEFAULT_SELECTION_HELP' => 'Quando questa opzione è selezionata, tutti gli utenti saranno in grado di inviare email utilizzando lo stesso<br> account di posta usato dal sistema per inviare notifiche e avvertimenti. Se questa opzione non viene selezionata,<br> gli utenti potranno comunque usare il server di posta in uscita dopo aver fornito i dati del loro account.',
+    'LBL_FROM_ADDRESS_HELP' => 'Se attivato, il nome e l´indirizzo email dell´utente che assegna saranno inclusi nel campo Da dell´email. Questa funzione potrebbe non funzionare con servers SMTP che non permettono l´invio da un account di email diverso dal server accunt.',
+    'LBL_HELP' => 'Aiuto' /*for 508 compliance fix*/,
+    'LBL_OUTBOUND_EMAIL_ACCOUNT_VIEW' => 'Visualizza gli account di posta elettronica in uscita',
+    'LBL_SYSTEM_OUTBOUND_EMAIL_ACCOUNT' => 'System Outbound Email Account',
+    'LBL_ALLOW_SEND_AS_USER' => 'Gli utenti possono inviare come se stessi:',
+    'LBL_ALLOW_SEND_AS_USER_DESC' => 'Quando abilitato, <b>tutti</b> gli utenti possono inviare posta elettronica utilizzando il server di posta in uscita, utilizzando il proprio indirizzo email principale come il &quot;da:&quot; Indirizzo.<br>questa funzionalità potrebbe non funzionare con i server SMTP che non consentono l\'invio da un account di posta elettronica diverso rispetto all\'account di server.',
+    'LBL_EMAIL_IMPORT_CONFIGURATION' => 'Email Import Configuration',
+    'LBL_EMAIL_IMPORT_PER_RUN_THRESHOLD' => 'Threshold for Number of Emails Imported Per Run (per Mailbox)',
+    'LBL_EMAIL_IMPORT_PER_RUN_THRESHOLD_DESC' => 'Threshold of emails to import per run. Default is 25. Each run will go through each day in the mailbox within the set timeframe, from oldest to most recent. It will import all emails on each day until the total emails imported on this run exceeds the threshold. Once met, the job will stop and mark the last day imported, starting there on the next run.',
+    'LBL_EMAIL_IMPORT_TIMEFRAME_START' => 'Starting Point for New Mailbox Imports',
+    'LBL_EMAIL_IMPORT_TIMEFRAME_START_DESC' => 'Period of time from which emails will start being imported. For example, selecting 30 Days will start with emails from 30 days ago and move forward from there.',
+    'LBL_EMAIL_IMPORT_FETCH_UNREAD_ONLY' => 'Only fetch unread emails when importing',
+);
