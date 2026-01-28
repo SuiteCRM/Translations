995b83014f36086721ae493c8a46a5ec66d3bbd9
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-28 19:38:50 +0000

    New translations en_us.lang.php (Korean)

---
+++ translations/ko_KR/modules/Tasks/language/ko_KR.lang.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_MODULE_NAME' => '작업',
+    'LBL_MODULE_TITLE' => ' 업무: 홈',
+    'LBL_SEARCH_FORM_TITLE' => ' 업무 검색',
+    'LBL_LIST_FORM_TITLE' => ' 업무 목록',
+    'LBL_NEW_FORM_TITLE' => ' 업무 작성',
+    'LBL_LIST_CLOSE' => '닫기',
+    'LBL_LIST_SUBJECT' => '제목',
+    'LBL_LIST_CONTACT' => '연락처',
+    'LBL_LIST_PRIORITY' => '우선순위',
+    'LBL_LIST_RELATED_TO' => '관련처',
+    'LBL_LIST_DUE_DATE' => '종료일',
+    'LBL_LIST_DUE_TIME' => '종료시간',
+    'LBL_SUBJECT' => '제목:',
+    'LBL_STATUS' => '상태',
+    'LBL_DUE_DATE' => '만료일:',
+    'LBL_DUE_TIME' => '만료시간:',
+    'LBL_PRIORITY' => '우선순위:',
+    'LBL_DUE_DATE_AND_TIME' => '종료일시:',
+    'LBL_START_DATE_AND_TIME' => '시작일시:',
+    'LBL_START_DATE' => '시작일:',
+    'LBL_LIST_START_DATE' => '시작일',
+    'LBL_START_TIME' => '시작시간:',
+    'DATE_FORMAT' => '(년-월-일)',
+    'LBL_NONE' => '없음',
+    'LBL_CONTACT' => '연락처:',
+    'LBL_EMAIL_ADDRESS' => '이메일 주소:',
+    'LBL_PHONE' => '전화:',
+    'LBL_EMAIL' => '이메일 주소:',
+    'LBL_DESCRIPTION' => '상세:',
+    'LBL_NAME' => '이름:',
+    'LBL_CONTACT_NAME' => '거래처 담당자명 ',
+    'LBL_LIST_STATUS' => '상태',
+    'LBL_DATE_DUE_FLAG' => '종료일',
+    'LBL_DATE_START_FLAG' => '시작일',
+    'LBL_LIST_MY_TASKS' => '내 진행중 업무',
+    'LNK_NEW_TASK' => '업무 작성',
+    'LNK_TASK_LIST' => '업무',
+    'LNK_IMPORT_TASKS' => '업무 가져오기',
+    'LBL_LIST_ASSIGNED_TO_NAME' => '담당자',
+    'LBL_ASSIGNED_TO_NAME' => '담당자:',
+    'LBL_LIST_DATE_MODIFIED' => '수정일',
+    'LBL_CONTACT_ID' => '거래처 담당자 ID:',
+    'LBL_PARENT_ID' => '패런트ID:',
+    'LBL_CONTACT_PHONE' => '거래처 담당자 전화번호:',
+    'LBL_PARENT_TYPE' => '상위 유형:',
+    'LBL_TASK_INFORMATION' => '업무 개요', //No need to be translated in all caps. Translation used just in menu action items when using the SuiteP template
+    'LBL_EDITLAYOUT' => '레이아웃 편집' /*for 508 compliance fix*/,
+    'LBL_HISTORY_SUBPANEL_TITLE' => '노트',
+    //For export labels
+    'LBL_DATE_DUE' => '만료일',
+    'LBL_RELATED_TO' => '관련:',
+);
