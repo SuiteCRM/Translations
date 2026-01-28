b6bd583e976b0dffdac476572720e02b8dd1ae2b
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-28 19:37:24 +0000

    New translations en_us.lang.php (Turkish)

---
+++ translations/tr_TR/modules/Surveys/language/tr_TR.lang.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID'                                     => 'Atanmış kullanıcı kodu',
+    'LBL_ADD_OPTION'                                         => 'Yeni seçenek ekle',
+    'LBL_ASSIGNED_TO_NAME'                                   => 'Atanmış kişi',
+    'LBL_SECURITYGROUPS'                                     => 'Güvenlik grupları',
+    'LBL_SECURITYGROUPS_SUBPANEL_TITLE'                      => 'Güvenlik grupları',
+    'LBL_ID'                                                 => 'Kod',
+    'LBL_DATE_ENTERED'                                       => 'Eklenme tarihi',
+    'LBL_DATE_MODIFIED'                                      => 'Değiştirilme tarihi',
+    'LBL_MODIFIED'                                           => 'Değiştiren',
+    'LBL_MODIFIED_ID'                                        => 'Değiştiren kodu',
+    'LBL_MODIFIED_NAME'                                      => 'Değiştiren adı',
+    'LBL_CREATED'                                            => 'Ekleyen',
+    'LBL_CREATED_ID'                                         => 'Ekleyen kodu',
+    'LBL_DESCRIPTION'                                        => 'Açıklama',
+    'LBL_DELETED'                                            => 'Silindi',
+    'LBL_NAME'                                               => 'Ad',
+    'LBL_CREATED_USER'                                       => 'Ekleyen kullanıcı',
+    'LBL_MODIFIED_USER'                                      => 'Değiştiren kullanıcı',
+    'LBL_LIST_NAME'                                          => 'Ad',
+    'LBL_EDIT_BUTTON'                                        => 'Düzenle',
+    'LBL_REMOVE'                                             => 'Sil',
+    'LBL_LIST_FORM_TITLE'                                    => 'Anketler listesi',
+    'LBL_MODULE_NAME'                                        => 'Anketler',
+    'LBL_MODULE_TITLE'                                       => 'Anketler',
+    'LBL_HOMEPAGE_TITLE'                                     => 'Anketlerim',
+    'LNK_NEW_RECORD'                                         => 'Anket ekle',
+    'LNK_LIST'                                               => 'Anketleri görüntüle',
+    'LNK_IMPORT_SURVEYS'                                     => 'Anketleri içe aktar',
+    'LBL_SEARCH_FORM_TITLE'                                  => 'Anket arama',
+    'LBL_HISTORY_SUBPANEL_TITLE'                             => 'Geçmişi görüntüle',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE'                          => 'İşlemler',
+    'LBL_SURVEYS_SUBPANEL_TITLE'                             => 'Anketler',
+    'LBL_NEW_FORM_TITLE'                                     => 'Yeni anketler',
+    'LBL_STATUS'                                             => 'Durum',
+    'LBL_SURVEY_QUESTIONS_DISPLAY'                           => 'Sorular',
+    'LBL_SURVEY_URL_DISPLAY'                                 => 'Adres',
+    'LBL_HAPPINESS_QUESTION'                                 => 'Memnuniyet sorusu',
+    'LBL_CANT_EDIT_RESPONDED'                                => 'Yanıtlanmış anket soruları düzeltilemez',
+    'LBL_VIEW_SURVEY_REPORTS'                                => 'Anket raporlarını görüntüle',
+    'LBL_CHECKED'                                            => 'İşaretli',
+    'LBL_UNCHECKED'                                          => 'İşaretsiz',
+    'LBL_RESPONSE_ANSWER'                                    => 'Yanıt',
+    'LBL_RESPONSE_CONTACT'                                   => 'İlgili kişi',
+    'LBL_RESPONSE_TIME'                                      => 'Tarih',
+    'LBL_UNKNOWN_CONTACT'                                    => 'Bilinmiyor',
+    'LBL_RESPONSE_COUNT'                                     => 'Sayı',
+    'LBL_SUBMIT_TEXT'                                        => 'Metni Gönder',
+    'LBL_SATISFIED_TEXT'                                     => 'Memnun metni',
+    'LBL_NEITHER_TEXT'                                       => 'Ne memnun ne hoşnutsuz metni',
+    'LBL_DISSATISFIED_TEXT'                                  => 'Memnuniyetsiz metni',
+    'LBL_SURVEYS_SURVEYQUESTIONS_FROM_SURVEYQUESTIONS_TITLE' => 'Anket soruları',
+    'LBL_SURVEYS_SURVEYRESPONSES_FROM_SURVEYRESPONSES_TITLE' => 'Anket yanıtları',
+    'LBL_SHOW_RESPONSES' => 'Yanıtları görüntüle',
+    'LBL_HIDE_RESPONSES' => 'Yanıtları gizle',
+    'LBL_SURVEY_QUESTION' => 'Soru',
+    'LBL_SURVEY_TEXT' => 'Metin',
+    'LBL_SURVEY_TYPE' => 'Tür',
+    'LBL_SURVEY_ACTIONS' => 'İşlemler',
+    'LBL_SURVEY_NEW_QUESTION' => 'Yeni soru',
+    'LBL_SURVEY_RESPONSES' => 'Yeni soru',
+    'LBL_SURVEYS_SENT' => 'Gönderilen anket:',
+    'LBL_SURVEY_DISTINCT' => 'Gönderilen farklı anket:',
+    'LBL_DRAFT' => 'Taslak',
+    'LBL_SUBMIT' => 'Gönder',
+    'LBL_STARS' => 'Yıldız',
+    'LBL_SURVEY_CLOSE_RESPONSE' => 'İlgilendiğiniz için teşekkür ederiz ancak bu anket tamamlanmış.',
+    'LBL_SURVEY_THANKS' => 'Thanks for completing this survey.',
+);
