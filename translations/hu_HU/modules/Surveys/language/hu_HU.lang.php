ae64698aa2ce9db369cc74a8e67c0fcf373394d4
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-28 19:37:04 +0000

    New translations en_us.lang.php (Hungarian)

---
+++ translations/hu_HU/modules/Surveys/language/hu_HU.lang.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID'                                     => 'Felelős felhasználó ID',
+    'LBL_ADD_OPTION'                                         => 'Új opció felvétele',
+    'LBL_ASSIGNED_TO_NAME'                                   => 'Felelős',
+    'LBL_SECURITYGROUPS'                                     => 'Biztonsági Csoportok',
+    'LBL_SECURITYGROUPS_SUBPANEL_TITLE'                      => 'Biztonsági Csoportok',
+    'LBL_ID'                                                 => 'ID',
+    'LBL_DATE_ENTERED'                                       => 'Létrehozás dátuma',
+    'LBL_DATE_MODIFIED'                                      => 'Módosítás dátuma',
+    'LBL_MODIFIED'                                           => 'Módosító',
+    'LBL_MODIFIED_ID'                                        => 'Módosító Id',
+    'LBL_MODIFIED_NAME'                                      => 'Módosító név',
+    'LBL_CREATED'                                            => 'Létrehozta',
+    'LBL_CREATED_ID'                                         => 'Készítette: Id',
+    'LBL_DESCRIPTION'                                        => 'Leírás',
+    'LBL_DELETED'                                            => 'Törölve',
+    'LBL_NAME'                                               => 'Név',
+    'LBL_CREATED_USER'                                       => 'Felhasználó által létrehozva',
+    'LBL_MODIFIED_USER'                                      => 'Felhasználó által módosítva',
+    'LBL_LIST_NAME'                                          => 'Név',
+    'LBL_EDIT_BUTTON'                                        => 'Szerkesztés',
+    'LBL_REMOVE'                                             => 'Eltávolítás',
+    'LBL_LIST_FORM_TITLE'                                    => 'Surveys List',
+    'LBL_MODULE_NAME'                                        => 'Kérdőív',
+    'LBL_MODULE_TITLE'                                       => 'Kérdőív',
+    'LBL_HOMEPAGE_TITLE'                                     => 'My Surveys',
+    'LNK_NEW_RECORD'                                         => 'Create Surveys',
+    'LNK_LIST'                                               => 'View Surveys',
+    'LNK_IMPORT_SURVEYS'                                     => 'Import Surveys',
+    'LBL_SEARCH_FORM_TITLE'                                  => 'Search Surveys',
+    'LBL_HISTORY_SUBPANEL_TITLE'                             => 'Előzmény Megtekintése',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE'                          => 'Tevékenységek',
+    'LBL_SURVEYS_SUBPANEL_TITLE'                             => 'Kérdőív',
+    'LBL_NEW_FORM_TITLE'                                     => 'New Surveys',
+    'LBL_STATUS'                                             => 'Állapot',
+    'LBL_SURVEY_QUESTIONS_DISPLAY'                           => 'Questions',
+    'LBL_SURVEY_URL_DISPLAY'                                 => 'URL:',
+    'LBL_HAPPINESS_QUESTION'                                 => 'Happiness Question',
+    'LBL_CANT_EDIT_RESPONDED'                                => 'Survey questions with responses cannot be edited',
+    'LBL_VIEW_SURVEY_REPORTS'                                => 'View Survey Reports',
+    'LBL_CHECKED'                                            => 'Checked',
+    'LBL_UNCHECKED'                                          => 'Unchecked',
+    'LBL_RESPONSE_ANSWER'                                    => 'Válasz',
+    'LBL_RESPONSE_CONTACT'                                   => 'Kapcsolattartó',
+    'LBL_RESPONSE_TIME'                                      => 'Dátum',
+    'LBL_UNKNOWN_CONTACT'                                    => 'Ismeretlen',
+    'LBL_RESPONSE_COUNT'                                     => 'Számlál',
+    'LBL_SUBMIT_TEXT'                                        => 'Submit Text',
+    'LBL_SATISFIED_TEXT'                                     => 'Satisfied Text',
+    'LBL_NEITHER_TEXT'                                       => 'Neither Text',
+    'LBL_DISSATISFIED_TEXT'                                  => 'Dissatisfied Text',
+    'LBL_SURVEYS_SURVEYQUESTIONS_FROM_SURVEYQUESTIONS_TITLE' => 'Kérdőív Kérdések',
+    'LBL_SURVEYS_SURVEYRESPONSES_FROM_SURVEYRESPONSES_TITLE' => 'Válaszok',
+    'LBL_SHOW_RESPONSES' => 'Show responses',
+    'LBL_HIDE_RESPONSES' => 'Válaszok elrejtése',
+    'LBL_SURVEY_QUESTION' => 'Question',
+    'LBL_SURVEY_TEXT' => 'Szöveg',
+    'LBL_SURVEY_TYPE' => 'Típus',
+    'LBL_SURVEY_ACTIONS' => 'Műveletek',
+    'LBL_SURVEY_NEW_QUESTION' => 'New Question',
+    'LBL_SURVEY_RESPONSES' => 'New Question',
+    'LBL_SURVEYS_SENT' => 'Surveys Sent:',
+    'LBL_SURVEY_DISTINCT' => 'Distinct Surveys Sent:',
+    'LBL_DRAFT' => 'Vázlat',
+    'LBL_SUBMIT' => 'Küldés',
+    'LBL_STARS' => 'Stars',
+    'LBL_SURVEY_CLOSE_RESPONSE' => 'Thanks for your interest but this survey is now closed.',
+    'LBL_SURVEY_THANKS' => 'Thanks for completing this survey.',
+);
