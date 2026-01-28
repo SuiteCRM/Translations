6f2ca38bb13120f4224d259aa203b5f6817ebae7
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-28 18:35:07 +0000

    New translations en_us.lang.php (Yoruba)

---
+++ translations/yo_NG/modules/Employees/language/yo_NG.lang.php
@@ -0,0 +1,157 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_MODULE_NAME' => 'Awon Osise',
+    'LBL_MODULE_TITLE' => 'Employees: Home',
+    'LBL_SEARCH_FORM_TITLE' => 'Employee Search',
+    'LBL_LIST_FORM_TITLE' => 'Awon Osise',
+    'LBL_NEW_FORM_TITLE' => 'New Employee',
+    'LBL_LOGIN' => 'Login',
+    'LBL_RESET_PREFERENCES' => 'Reset To Default Preferences',
+    'LBL_TIME_FORMAT' => 'Time Format:',
+    'LBL_DATE_FORMAT' => 'Date Format:',
+    'LBL_TIMEZONE' => 'Current Time:',
+    'LBL_CURRENCY' => 'Owo:',
+    'LBL_LIST_NAME' => 'Oruko',
+    'LBL_LIST_LAST_NAME' => 'Oruko ikehin',
+    'LBL_LIST_EMPLOYEE_NAME' => 'Employee Name',
+    'LBL_LIST_DEPARTMENT' => 'Eka',
+    'LBL_LIST_REPORTS_TO_NAME' => 'Royin fun',
+    'LBL_LIST_EMAIL' => 'Imeeli',
+    'LBL_LIST_USER_NAME' => 'User Name',
+    'LBL_ERROR' => 'Asise:',
+    'LBL_PASSWORD' => 'Password:',
+    'LBL_USER_NAME' => 'User Name:',
+    'LBL_USER_TYPE' => 'User Type',
+    'LBL_FIRST_NAME' => 'Oruko akoko:',
+    'LBL_LAST_NAME' => 'Oruko ikehin:',
+    'LBL_THEME' => 'Akori:',
+    'LBL_LANGUAGE' => 'Language:',
+    'LBL_ADMIN' => 'Administrator:',
+    'LBL_EMPLOYEE_INFORMATION' => 'Employee Information',
+    'LBL_OFFICE_PHONE' => 'Foonu Ofiisi:',
+    'LBL_REPORTS_TO' => 'Reports to Id:',
+    'LBL_REPORTS_TO_NAME' => 'Reports to',
+    'LBL_OTHER_PHONE' => 'Foonu miiran:',
+    'LBL_NOTES' => 'Notes:',
+    'LBL_DEPARTMENT' => 'Eka:',
+    'LBL_TITLE' => 'Job Title:',
+    'LBL_ANY_ADDRESS' => 'Adiresi eyikeyi:',
+    'LBL_ANY_PHONE' => 'Foonu eyikeyi:',
+    'LBL_ANY_EMAIL' => 'Eyikeyi emaili:',
+    'LBL_ADDRESS' => 'Address:',
+    'LBL_CITY' => 'Ilu:',
+    'LBL_STATE' => 'Ipinle tabi Agbegbe:',
+    'LBL_POSTAL_CODE' => 'Koodu ifiweranse:',
+    'LBL_COUNTRY' => 'Orile-ede:',
+    'LBL_NAME' => 'Oruko:',
+    'LBL_MOBILE_PHONE' => 'Mobile:',
+    'LBL_FAX' => 'Faksi:',
+    'LBL_EMAIL' => 'Adiresi emaili:',
+    'LBL_EMAIL_LINK_TYPE' => 'Email Client',
+    'LBL_EMAIL_LINK_TYPE_HELP' => '<b>SuiteCRM Mail Client:</b> Send emails using the email client in the SuiteCRM application.<br><b>External Mail Client:</b> Send email using an email client outside of the SuiteCRM application, such as Microsoft Outlook.',
+    'LBL_HOME_PHONE' => 'Foonu tile:',
+    'LBL_WORK_PHONE' => 'Work Phone:',
+    'LBL_EMPLOYEE_STATUS' => 'Employee Status:',
+    'LBL_PRIMARY_ADDRESS' => 'Adiresi akoko:',
+    'LBL_SAVED_SEARCH' => 'Layout Options',
+    'LBL_MESSENGER_ID' => 'IM Name:',
+    'LBL_MESSENGER_TYPE' => 'IM Type:',
+    'ERR_LAST_ADMIN_1' => 'The employee name "',
+    'ERR_LAST_ADMIN_2' => '" is the last employee with administrator access. At least one employee must be an administrator.',
+    'LNK_NEW_EMPLOYEE' => 'Create Employee',
+    'LNK_EMPLOYEE_LIST' => 'View Employees',
+    'ERR_DELETE_RECORD' => 'You must specify a record number to delete the account.',
+    'LBL_LIST_EMPLOYEE_STATUS' => 'Employee Status',
+
+    'LBL_SUITE_LOGIN' => 'Is User',
+    'LBL_RECEIVE_NOTIFICATIONS' => 'Notify on Assignment',
+    'LBL_IS_ADMIN' => 'Is Administrator',
+    'LBL_GROUP' => 'Group User',
+    'LBL_PHOTO' => 'Aworan',
+    'LBL_DELETE_USER_CONFIRM' => 'This Employee is also a User. Deleting the Employee record will also delete the User record, and the User will no longer be able to access the application. Do you want to proceed with deleting this record?',
+    'LBL_DELETE_EMPLOYEE_CONFIRM' => 'Are you sure you want to delete this employee?',
+    'LBL_ONLY_ACTIVE' => 'Active Employees',
+    'LBL_SELECT' => 'Yan' /*for 508 compliance fix*/,
+    'LBL_AUTHENTICATE_ID' => 'Authentication Id',
+    'LBL_EXT_AUTHENTICATE' => 'External Authentication',
+    'LBL_GROUP_USER' => 'Group User',
+    'LBL_LIST_ACCEPT_STATUS' => 'Gba Ipo',
+    'LBL_MODIFIED_BY' => 'Se iyipada nipasẹ',
+    'LBL_MODIFIED_BY_ID' => 'Se iyipada nipase id',
+    'LBL_CREATED_BY_NAME' => 'Siseda nipa', //bug48978
+    'LBL_PORTAL_ONLY_USER' => 'Portal API User',
+    'LBL_PSW_MODIFIED' => 'Password Last Changed',
+    'LBL_SHOW_ON_EMPLOYEES' => 'Display Employee Record',
+    'LBL_USER_HASH' => 'Pasiwodu',
+    'LBL_SYSTEM_GENERATED_PASSWORD' => 'Pasiwodu ti sistem pilese e',
+    'LBL_DESCRIPTION' => 'Apejuwe',
+    'LBL_FAX_PHONE' => 'Faksi',
+    'LBL_STATUS' => 'Ipo',
+    'LBL_ADDRESS_CITY' => 'Adiresi Ilu',
+    'LBL_ADDRESS_COUNTRY' => 'Adiresi Orile ede',
+    'LBL_ADDRESS_INFORMATION' => 'Alaye Adiresi',
+    'LBL_ADDRESS_POSTALCODE' => 'Address Postal Code',
+    'LBL_ADDRESS_STATE' => 'Adiresi Ipinle',
+    'LBL_ADDRESS_STREET' => 'Adiresi Opopona',
+
+    'LBL_DATE_MODIFIED' => 'Ojo ti a se Atunse',
+    'LBL_DATE_ENTERED' => 'Date Entered',
+    'LBL_DELETED' => 'Paare',
+
+    'LBL_BUTTON_SELECT' => 'Yan',
+    'LBL_BUTTON_CLEAR' => 'Parẹ',
+
+    'LBL_CONTACTS_SYNC' => 'Contact Sync',
+    'LBL_OAUTH_TOKENS' => 'Awon ami OAuth',
+    'LBL_PROJECT_USERS_1_FROM_PROJECT_TITLE' => 'Project Users from Project Title',
+    'LBL_PROJECT_CONTACTS_1_FROM_CONTACTS_TITLE' => 'Project Contacts from Contacts Title',
+    'LBL_ROLES' => 'Ipa',
+    'LBL_SECURITYGROUPS' => 'Awọn ẹgbẹ Aabo',
+    'LBL_PROSPECT_LIST' => 'Prospect List',
+
+    'LBL_FACTOR_AUTH_INTERFACE' => 'Two Factor Authentication Interface:',
+    'LBL_EDITOR_TYPE' => 'Editor',
+);
