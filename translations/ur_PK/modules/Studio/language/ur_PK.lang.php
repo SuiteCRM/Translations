a83d98f64e243a12ac16f45107f49a2ac76049eb
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-28 19:29:09 +0000

    New translations en_us.lang.php (Urdu (Pakistan))

---
+++ translations/ur_PK/modules/Studio/language/ur_PK.lang.php
@@ -0,0 +1,150 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_EDIT_LAYOUT' => 'لے آؤٹ میں ترمیم کریں',
+    'LBL_EDIT_FIELDS' => 'Edit Custom Fields',
+    'LBL_SELECT_FILE' => 'Select File',
+    'LBL_MODULE_TITLE' => 'وہ جگہ جہاں سے ٹی وی اور ریڈیو کے پروگرام نشر ھوتے ہیں',
+    'LBL_TOOLBOX' => 'Toolbox',
+    'LBL_SUITE_FIELDS_STAGE' => 'سویٹ کروم کی فیڈ تمثیل علاقے میں درج کرنے کے لئے کلک کریں',
+    'LBL_VIEW_SUITE_FIELDS' => 'View SuiteCRM Fields',
+    'LBL_FAILED_TO_SAVE' => 'Failed To Save',
+    'LBL_CONFIRM_UNSAVE' => 'کسی بھی تبدیلی کے لیے نہ محفوظ اوشن جائے کیا ویقینا لاس کو جاری رکھنا چاہتے?',
+    'LBL_PUBLISHING' => 'Publishing...',
+    'LBL_PUBLISHED' => 'اشاعت',
+    'LBL_FAILED_PUBLISHED' => 'Failed to Publish',
+    'LBL_DROP_HERE' => '[یہاں رکھیں]',
+
+//CUSTOM FIELDS
+    'LBL_NAME' => 'نام',
+    'LBL_LABEL' => 'عنوان',
+    'LBL_MASS_UPDATE' => 'بڑے پیمانے پر تازہ کاری',
+    'LBL_DEFAULT_VALUE' => 'کوتاہي قیمت',
+    'LBL_REQUIRED' => 'ضرورت یا طالب',
+    'LBL_DATA_TYPE' => 'قسم',
+
+
+    'LBL_HISTORY' => 'تاریخ',
+
+//WIZARDS
+
+//STUDIO WIZARD
+    'LBL_SW_WELCOME' => '<h2>اسٹوڈیو میں خوش آمدید</h2><br>آج آپ کیا پسند کرنا چاہتے ہیں <br><b> براہ مہربانی نیچے آپشن کی مدد سے منتخب کریں',
+    'LBL_SW_EDIT_MODULE' => 'Edit a Module',
+    'LBL_SW_EDIT_DROPDOWNS' => 'Edit Drop Downs',
+    'LBL_SW_EDIT_TABS' => 'Configure Tabs',
+    'LBL_SW_RENAME_TABS' => 'Rename Tabs',
+    'LBL_SW_EDIT_GROUPTABS' => 'Configure Group Tabs',
+    'LBL_SW_EDIT_PORTAL' => 'Edit Portal',
+    'LBL_SW_REPAIR_CUSTOMFIELDS' => 'Repair Custom Fields',
+    'LBL_SW_MIGRATE_CUSTOMFIELDS' => 'اپنی مرضی کے فیلڈز کو منتقل کریں',
+
+// JS LABELS
+    'LBL_REBUILD_JAVASCRIPT_LANG_DESC_SHORT' => 'سکریپ کر لینگویج دوبارہ تبدیل کیا جا سکتا',
+    'LBL_REBUILD_JAVASCRIPT_LANG_DESC' => 'جاوا اسکریپت زبان تبدیل ہو رہی ہےضرورت کے مطابق اس کو پھر بنایا جاتا ہے.',
+
+
+//Manager Backups History
+    'LBL_MB_DELETE' => 'ختم کریں',
+
+//EDIT DROP DOWNS
+    'LBL_ED_CREATE_DROPDOWN' => 'Create a Drop Down',
+    'LBL_DROPDOWN_NAME' => 'Dropdown Name:',
+    'LBL_DROPDOWN_LANGUAGE' => 'Dropdown Language:',
+    'LBL_TABGROUP_LANGUAGE' => 'زبان:',
+
+//END WIZARDS
+
+//DROP DOWN EDITOR
+    'LBL_DD_DISPALYVALUE' => 'Display Value',
+    'LBL_DD_DATABASEVALUE' => 'Database Value',
+    'LBL_DD_ALL' => 'سب',
+
+//BUTTONS
+    'LBL_BTN_SAVE' => 'محفوظ کریں',
+    'LBL_BTN_CANCEL' => 'منسوخ کریں',
+    'LBL_BTN_SAVEPUBLISH' => 'Save & Deploy',
+    'LBL_BTN_HISTORY' => 'تاریخ',
+    'LBL_BTN_ADDROWS' => 'Add Rows',
+    'LBL_BTN_UNDO' => 'Undo',
+    'LBL_BTN_REDO' => 'Redo',
+    'LBL_BTN_ADDCUSTOMFIELD' => 'Add Custom Field',
+    'LBL_BTN_TABINDEX' => 'Edit Tabbing Order',
+
+//TABS
+    'LBL_MODULES' => 'ماڈیولز',
+    'LBL_MODULE_NAME' => 'انتظامیہ',
+    'LBL_CONFIGURE_GROUP_TABS' => 'ماڈیول مینو فلٹرز کو تشکیل دیں',
+    'LBL_GROUP_TAB_WELCOME' => 'نیچے فلٹر دکھائی دیا جائے گا اور زیادہ فہرست ماڈل میں صارف کے یوزر کے لیے استعمال کریں اور دیکھیں گروپ کے ماڈل مینیو میں ماڈل کو کھیچے اور ڈالے فلٹر میں درج کرلی کے خالی فلٹر مینیو میں دکھائی نہیں دیئے جائیں گے.',
+    'LBL_RENAME_TAB_WELCOME' => 'دکھائی دینے والے ٹیپ کی قیمت پر کلک کریں اور نیچے اسٹیپ کا نام تبدیل کریں.',
+    'LBL_DELETE_MODULE' => 'مٹانا&nbsp;ماڈل<br /> سے&nbsp;فلٹر',
+    'LBL_TAB_GROUP_LANGUAGE_HELP' => 'دستیاب زبان کا انتخاب کریں گروپ لیبل کو تبدیل کریں اور دل کو دکھانے کے لئے منتخب کریں.',
+    'LBL_ADD_GROUP' => 'Add Filter',
+    'LBL_NEW_GROUP' => 'New Group',
+    'LBL_RENAME_TABS' => 'ماڈلز کا نام تبدیل کرنا',
+
+//ERRORS
+    'ERROR_INVALID_KEY_VALUE' => "Error: Invalid Key Value: [']",
+
+//SUGAR PORTAL
+    'LBL_SAVE' => 'محفوظ کریں' /*for 508 compliance fix*/,
+    'LBL_UNDO' => 'Undo' /*for 508 compliance fix*/,
+    'LBL_REDO' => 'Redo' /*for 508 compliance fix*/,
+    'LBL_INLINE' => 'Inline' /*for 508 compliance fix*/,
+    'LBL_DELETE' => 'ختم کریں' /*for 508 compliance fix*/,
+    'LBL_ADD_FIELD' => 'فیلڈ شامل کریں' /*for 508 compliance fix*/,
+    'LBL_MAXIMIZE' => 'Maximize' /*for 508 compliance fix*/,
+    'LBL_MINIMIZE' => 'Minimize' /*for 508 compliance fix*/,
+    'LBL_PUBLISH' => 'شائع کریں' /*for 508 compliance fix*/,
+    'LBL_ADDROWS' => 'Add Rows' /*for 508 compliance fix*/,
+    'LBL_ADDFIELD' => 'فیلڈ شامل کریں' /*for 508 compliance fix*/,
+    'LBL_EDIT' => 'ردوبدل' /*for 508 compliance fix*/,
+
+    'LBL_LANGUAGE_TOOLTIP' => 'Select the language to edit.',
+    'LBL_SINGULAR' => 'Singular Label',
+    'LBL_PLURAL' => 'Plural Label',
+    'LBL_RENAME_MOD_SAVE_HELP' => 'کلک<b> محفوظ</b> تبدیل کرنے کے لئے.'
+
+);
