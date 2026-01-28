72d76da81f007e7f3cde41a2ae0dbf34c2eee3a6
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-28 19:16:31 +0000

    New translations en_us.lang.php (Latvian)

---
+++ translations/lv_LV/modules/Prospects/language/lv_LV.lang.php
@@ -0,0 +1,157 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_MODULE_NAME' => 'Mērķi',
+    'LBL_MODULE_ID' => 'Mērķi',
+    'LBL_INVITEE' => 'Padotie (?) / Tieši atskaitās',
+    'LBL_MODULE_TITLE' => 'Mērķi: sākums',
+    'LBL_SEARCH_FORM_TITLE' => 'Meklēt Mērķi',
+    'LBL_LIST_FORM_TITLE' => 'Mērķu saraksts',
+    'LBL_NEW_FORM_TITLE' => 'Jauns Mērķis',
+    'LBL_LIST_NAME' => 'Vārds:',
+    'LBL_LIST_LAST_NAME' => 'Uzvārds',
+    'LBL_LIST_TITLE' => 'Amats',
+    'LBL_LIST_EMAIL_ADDRESS' => 'E-pasts',
+    'LBL_LIST_PHONE' => 'Phone',
+    'LBL_LIST_FIRST_NAME' => 'Vārds',
+    'LBL_ASSIGNED_TO_NAME' => 'Piešķirts lietotājam',
+    'LBL_ASSIGNED_TO_ID' => 'Piešķirts lietotājam:',
+    'LBL_CAMPAIGN_ID' => 'Kampaņas ID',
+    'LBL_EXISTING_ACCOUNT' => 'Lietots esošs uzņēmums',
+    'LBL_CREATED_ACCOUNT' => 'Izveidots jauns uzņēmums',
+    'LBL_CREATED_CALL' => 'Izveidots jauns zvans',
+    'LBL_CREATED_MEETING' => 'Izveidota jauna tikšanās',
+    'LBL_NAME' => 'Vārds:',
+    'LBL_PROSPECT_INFORMATION' => 'Apskats', //No need to be translated in all caps. Translation used just in menu action items when using the SuiteP template
+    'LBL_MORE_INFORMATION' => 'Vairāk informācijas',
+    'LBL_FIRST_NAME' => 'Vārds:',
+    'LBL_OFFICE_PHONE' => 'Darba tālrunis:',
+    'LBL_ANY_PHONE' => 'Jebkurš tālrunis:',
+    'LBL_PHONE' => 'Tālrunis:',
+    'LBL_LAST_NAME' => 'Uzvārds:',
+    'LBL_MOBILE_PHONE' => 'Mobilais tālrunis:',
+    'LBL_HOME_PHONE' => 'Sākums:',
+    'LBL_OTHER_PHONE' => 'Cits tālrunis:',
+    'LBL_FAX_PHONE' => 'Fakss:',
+    'LBL_PRIMARY_ADDRESS_STREET' => 'Pamatadrese - Iela:',
+    'LBL_PRIMARY_ADDRESS_CITY' => 'Pamatadrese - Pilsēta:',
+    'LBL_PRIMARY_ADDRESS_COUNTRY' => 'Pamata adreses valsts:',
+    'LBL_PRIMARY_ADDRESS_STATE' => 'Pamatadrese - Novads:',
+    'LBL_PRIMARY_ADDRESS_POSTALCODE' => 'Pamatadrese - Pasta indekss:',
+    'LBL_ALT_ADDRESS_STREET' => 'Cita adrese - Iela:',
+    'LBL_ALT_ADDRESS_CITY' => 'Cita adrese - Pilsēta:',
+    'LBL_ALT_ADDRESS_COUNTRY' => 'Cita adrese - Valsts:',
+    'LBL_ALT_ADDRESS_STATE' => 'Cita adrese - Novads:',
+    'LBL_ALT_ADDRESS_POSTALCODE' => 'Cita adrese - Pasta indekss:',
+    'LBL_TITLE' => 'Job Title:',
+    'LBL_DEPARTMENT' => 'Nodaļa:',
+    'LBL_BIRTHDATE' => 'Dzimšanas diena:',
+    'LBL_EMAIL_ADDRESS' => 'E-pasts:',
+    'LBL_OTHER_EMAIL_ADDRESS' => 'Cits E-pasts:',
+    'LBL_ANY_EMAIL' => 'Jebkurš E-pasts:',
+    'LBL_ASSISTANT' => 'Asistents:',
+    'LBL_ASSISTANT_PHONE' => 'Asistenta tālrunis:',
+    'LBL_DO_NOT_CALL' => 'Nezvanīt!:',
+    'LBL_EMAIL_OPT_OUT' => 'Atteikties no e-pastu saņemšanas:',
+    'LBL_PRIMARY_ADDRESS' => 'Primārā adrese:',
+    'LBL_ALTERNATE_ADDRESS' => 'Cita adrese:',
+    'LBL_ANY_ADDRESS' => 'Jebkāda adrese:',
+    'LBL_CITY' => 'Pilsēta:',
+    'LBL_STATE' => 'State/Region:',
+    'LBL_POSTAL_CODE' => 'Pasta indekss:',
+    'LBL_COUNTRY' => 'Valsts:',
+    'LBL_ADDRESS_INFORMATION' => 'Adreses informācija',
+    'LBL_DESCRIPTION' => 'Apraksts:',
+    'LBL_OPP_NAME' => 'Iespējas nosaukums:',
+    'LBL_IMPORT_VCARD' => 'Importēt vCard',
+    'LBL_IMPORT_VCARDTEXT' => 'Automātiski veidot jaunu kontaktpersonu, importējot vCard.',
+    'LBL_DUPLICATE' => 'Iespējams dublikāts',
+    'MSG_SHOW_DUPLICATES' => 'The target record you are about to create might be a duplicate of a target record that already exists. Target records containing similar names and/or email addresses are listed below.<br>Click Create Target to continue creating this new target, or select an existing target listed below.',
+    'MSG_DUPLICATE' => 'The target record you are about to create might be a duplicate of a target record that already exists. Target records containing similar names and/or email addresses are listed below.<br>Click Save to continue creating this new target, or click Cancel to return to the module without creating the target.',
+    'LNK_IMPORT_VCARD' => 'Izveidot no vCard',
+    'LNK_NEW_ACCOUNT' => 'Izveidot uzņēmumu',
+    'LNK_NEW_OPPORTUNITY' => 'Jauna iespēja',
+    'LNK_NEW_CASE' => 'Izveidot pieteikumu',
+    'LNK_NEW_NOTE' => 'Izveidot piezīmi vai pielikumu',
+    'LNK_NEW_CALL' => 'Ieplānot zvanu [Alt+C]',
+    'LNK_NEW_EMAIL' => 'Arhivēt E-pastu [Alt+K]',
+    'LNK_NEW_MEETING' => 'Ieplānot tikšanos [Alt+M]',
+    'LNK_NEW_TASK' => 'Izveidot uzdevumu [Alt+N]',
+    'LNK_NEW_APPOINTMENT' => 'Izveidot tikšanos:',
+    'LNK_IMPORT_PROSPECTS' => 'Importēt mērķus',
+    'NTC_DELETE_CONFIRMATION' => 'Vai jūs tiešām vēlaties dzēst šo ierakstu?',
+    'NTC_REMOVE_CONFIRMATION' => 'Vai tiešām vēlaties noņemt šo kontaktu noņemt no pieteikuma?',
+    'ERR_DELETE_RECORD' => 'Lai dzēstu kontaktpersonu, jānorāda ieraksta numurs.',
+    'LBL_SALUTATION' => 'Uzruna',
+    'LBL_CREATED_OPPORTUNITY' => 'Izveidota jauna iespēja',
+    'LNK_SELECT_ACCOUNT' => "Izvēlēties uzņēmumu [Alt+A]",
+    'LNK_NEW_PROSPECT' => 'Izveidot mērķi',
+    'LNK_PROSPECT_LIST' => 'Aplūkot mērķus',
+    'LNK_NEW_CAMPAIGN' => 'Izveidot kampaņu (Vednis)',
+    'LNK_CAMPAIGN_LIST' => 'Mārketings',
+    'LNK_NEW_PROSPECT_LIST' => 'Izveidot mērķu sarakstu',
+    'LNK_PROSPECT_LIST_LIST' => 'Mērķu saraksti',
+    'LBL_SELECT_CHECKED_BUTTON_LABEL' => 'Izvēlēties Atzīmētos Mēķus',
+    'LBL_SELECT_CHECKED_BUTTON_TITLE' => 'Izvēlēties Atzīmētos Mēķus',
+    'LBL_INVALID_EMAIL' => 'Nederīgs e-pasts:',
+    'LBL_DEFAULT_SUBPANEL_TITLE' => 'Mērķi',
+    'LBL_PROSPECT_LIST' => 'Prospektu saraksts',
+    'LBL_CONVERT_BUTTON_TITLE' => 'Konvertēt Mērķi',
+    'LBL_CONVERT_BUTTON_LABEL' => 'Konvertēt Mērķi',
+    'LNK_NEW_CONTACT' => 'Jauna kontaktpersona',
+    'LBL_CREATED_CONTACT' => "Izveidota jauna kontaktpersona",
+    'LBL_CAMPAIGNS' => 'Mārketings',
+    'LBL_CAMPAIGN_LIST_SUBPANEL_TITLE' => 'Kampaņas žurnāls',
+    'LBL_TRACKER_KEY' => 'Sekotāja atslēga',
+    'LBL_LEAD_ID' => 'Interesenta ID',
+    'LBL_CONVERTED_LEAD' => 'Konvertēts Interesents',
+    'LBL_ACCOUNT_NAME' => 'Konta nosaukums',
+    'LBL_EDIT_ACCOUNT_NAME' => 'Uzņēmuma nosaukums:',
+    'LBL_CREATED_USER' => 'Izveidoja lietotājs',
+    'LBL_MODIFIED_USER' => 'Modificēja lietotājs',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Vēsture',
+    //For export labels
+    'LBL_FP_EVENTS_PROSPECTS_1_FROM_FP_EVENTS_TITLE' => 'Notikumi',
+);
