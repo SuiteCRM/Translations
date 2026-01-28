f2a44db73d4892455a27bc28ef30c6265e1aa4b0
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-28 19:37:28 +0000

    New translations en_us.lang.php (Chinese Traditional)

---
+++ translations/zh_TW/modules/Surveys/language/zh_TW.lang.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID'                                     => '負責人Id',
+    'LBL_ADD_OPTION'                                         => 'Add new option',
+    'LBL_ASSIGNED_TO_NAME'                                   => '負責人',
+    'LBL_SECURITYGROUPS'                                     => '安全群組',
+    'LBL_SECURITYGROUPS_SUBPANEL_TITLE'                      => '安全群組',
+    'LBL_ID'                                                 => '編號',
+    'LBL_DATE_ENTERED'                                       => '建立日期',
+    'LBL_DATE_MODIFIED'                                      => '修改日期',
+    'LBL_MODIFIED'                                           => '修改人',
+    'LBL_MODIFIED_ID'                                        => '修改人編號',
+    'LBL_MODIFIED_NAME'                                      => '修改人姓名',
+    'LBL_CREATED'                                            => '建立人',
+    'LBL_CREATED_ID'                                         => '創建人編號',
+    'LBL_DESCRIPTION'                                        => '描述',
+    'LBL_DELETED'                                            => '已刪除',
+    'LBL_NAME'                                               => '名稱',
+    'LBL_CREATED_USER'                                       => '創建者',
+    'LBL_MODIFIED_USER'                                      => '修改的使用者',
+    'LBL_LIST_NAME'                                          => '名稱',
+    'LBL_EDIT_BUTTON'                                        => '編輯',
+    'LBL_REMOVE'                                             => '刪除',
+    'LBL_LIST_FORM_TITLE'                                    => '問卷清單',
+    'LBL_MODULE_NAME'                                        => '調查問卷',
+    'LBL_MODULE_TITLE'                                       => '調查問卷',
+    'LBL_HOMEPAGE_TITLE'                                     => '我的問卷',
+    'LNK_NEW_RECORD'                                         => '建立問卷',
+    'LNK_LIST'                                               => '檢視問卷',
+    'LNK_IMPORT_SURVEYS'                                     => '匯入問卷',
+    'LBL_SEARCH_FORM_TITLE'                                  => '搜尋問卷',
+    'LBL_HISTORY_SUBPANEL_TITLE'                             => '查看歷史記錄',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE'                          => '銷售活動',
+    'LBL_SURVEYS_SUBPANEL_TITLE'                             => '調查問卷',
+    'LBL_NEW_FORM_TITLE'                                     => '新的問卷',
+    'LBL_STATUS'                                             => '狀態',
+    'LBL_SURVEY_QUESTIONS_DISPLAY'                           => '問題',
+    'LBL_SURVEY_URL_DISPLAY'                                 => '超鏈接',
+    'LBL_HAPPINESS_QUESTION'                                 => '滿意度問題',
+    'LBL_CANT_EDIT_RESPONDED'                                => '有回應的問卷問題不可編輯',
+    'LBL_VIEW_SURVEY_REPORTS'                                => '檢視問卷報告',
+    'LBL_CHECKED'                                            => '已勾選',
+    'LBL_UNCHECKED'                                          => '未勾選',
+    'LBL_RESPONSE_ANSWER'                                    => '回答',
+    'LBL_RESPONSE_CONTACT'                                   => '聯絡人',
+    'LBL_RESPONSE_TIME'                                      => '日期型',
+    'LBL_UNKNOWN_CONTACT'                                    => '未知',
+    'LBL_RESPONSE_COUNT'                                     => '合計',
+    'LBL_SUBMIT_TEXT'                                        => '上傳文字意見',
+    'LBL_SATISFIED_TEXT'                                     => '滿意的意見',
+    'LBL_NEITHER_TEXT'                                       => '普通的意見',
+    'LBL_DISSATISFIED_TEXT'                                  => '不滿意的意見',
+    'LBL_SURVEYS_SURVEYQUESTIONS_FROM_SURVEYQUESTIONS_TITLE' => '問卷問題',
+    'LBL_SURVEYS_SURVEYRESPONSES_FROM_SURVEYRESPONSES_TITLE' => '問卷回應',
+    'LBL_SHOW_RESPONSES' => '顯示回傳訊息',
+    'LBL_HIDE_RESPONSES' => 'Hide responses',
+    'LBL_SURVEY_QUESTION' => '問題',
+    'LBL_SURVEY_TEXT' => '文字',
+    'LBL_SURVEY_TYPE' => '類型',
+    'LBL_SURVEY_ACTIONS' => '動作',
+    'LBL_SURVEY_NEW_QUESTION' => '新的問題',
+    'LBL_SURVEY_RESPONSES' => '新的問題',
+    'LBL_SURVEYS_SENT' => '問件寄送:',
+    'LBL_SURVEY_DISTINCT' => '不同的問卷寄送:',
+    'LBL_DRAFT' => '草稿',
+    'LBL_SUBMIT' => '提交',
+    'LBL_STARS' => '星星數',
+    'LBL_SURVEY_CLOSE_RESPONSE' => 'Thanks for your interest but this survey is now closed.',
+    'LBL_SURVEY_THANKS' => 'Thanks for completing this survey.',
+);
