c2b99867e659489b65d650bfae79082735d93e14
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-28 19:47:05 +0000

    New translations en_us.lang.php (Turkish)

---
+++ translations/tr_TR/modules/jjwg_Areas/language/tr_TR.lang.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings['LBL_ASSIGNED_TO_ID'] = 'Atanmış kullanıcı kodu';
+$mod_strings['LBL_ASSIGNED_TO_NAME'] = 'Kullanıcı';
+$mod_strings['LBL_ID'] = 'Kod';
+$mod_strings['LBL_DATE_ENTERED'] = 'Eklenme tarihi';
+$mod_strings['LBL_DATE_MODIFIED'] = 'Değiştirilme tarihi';
+$mod_strings['LBL_MODIFIED'] = 'Değiştiren';
+$mod_strings['LBL_MODIFIED_NAME'] = 'Değiştiren adı';
+$mod_strings['LBL_CREATED'] = 'Ekleyen';
+$mod_strings['LBL_DESCRIPTION'] = 'Açıklama';
+$mod_strings['LBL_DELETED'] = 'Silindi';
+$mod_strings['LBL_NAME'] = 'Ad';
+$mod_strings['LBL_CREATED_USER'] = 'Ekleyen kullanıcı';
+$mod_strings['LBL_MODIFIED_USER'] = 'Değiştiren kullanıcı';
+$mod_strings['LBL_LIST_NAME'] = 'Ad';
+$mod_strings['LBL_LIST_FORM_TITLE'] = 'Bölgeler listesi';
+$mod_strings['LBL_MODULE_NAME'] = 'Bölgeler';
+$mod_strings['LBL_MODULE_TITLE'] = 'Bölgeler';
+$mod_strings['LBL_HOMEPAGE_TITLE'] = 'Bölgelerim';
+$mod_strings['LNK_NEW_RECORD'] = 'Bölge ekle';
+$mod_strings['LNK_LIST'] = 'Bölgeleri görüntüle';
+$mod_strings['LBL_SEARCH_FORM_TITLE'] = 'Bölge arama';
+$mod_strings['LBL_HISTORY_SUBPANEL_TITLE'] = 'Geçmişi görüntüle';
+$mod_strings['LBL_ACTIVITIES_SUBPANEL_TITLE'] = 'İşlemler';
+$mod_strings['LBL_NEW_FORM_TITLE'] = 'Yeni bölgeler';
+$mod_strings['LBL_CITY'] = 'İlçe';
+$mod_strings['LBL_STATE'] = 'İl';
+$mod_strings['LBL_COUNTRY'] = 'Ülke';
+$mod_strings['LBL_COORDINATES'] = 'Koordinatlar';
+$mod_strings['LBL_LIST_ASSIGNED_USER'] = 'Kullanıcı';
+$mod_strings['LBL_AREA_MAP'] = 'Bölge haritası';
+
+$mod_strings['LBL_AREA_EDIT_TITLE'] = 'Bölge ekleme yönergeleri:';
+$mod_strings['LBL_AREA_EDIT_DESC_1'] = 'Harita üzerine sol tıklayın ve saat yönüne doğru hareket edip tıklayarak bölgeyi kapsayacak noktaları ekleyin.';
+$mod_strings['LBL_AREA_EDIT_DESC_2'] = 'Bölgeyi kapsayacak çokgeni tamamlamak için ilk işaret noktasına tıklayın.';
+$mod_strings['LBL_AREA_EDIT_RESET'] = 'Sıfırla';
+$mod_strings['LBL_AREA_EDIT_USE_AREA_COORDINATES'] = 'Bölge koordinatları kullanılsın';
+$mod_strings['LBL_AREA_EDIT_COORDINATE_RESULTS'] = 'Koordinat sonuçları (enlem,boylam,yükseklik):';
+$mod_strings['LBL_JJWG_MAPS_JJWG_AREAS_FROM_JJWG_MAPS_TITLE'] = 'Haritalar';
