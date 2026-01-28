b667da0927e10fe8bab26b4224d7005024da40b6
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-28 19:50:53 +0000

    New translations manifest.php (Chinese Traditional)

---
+++ translations/zh_TW/manifest.php
@@ -0,0 +1,25 @@
+<?php
+/******************************************
+ * SuiteCRM Translations
+ * @URL: https://crowdin.com/project/suitecrmtrans
+ * @author SuiteCRM Community via Crowdin
+ ******************************************/
+
+$manifest = array(
+    'name' => 'Chinese (Taiwan)',
+    'description' => 'Translation: crowdin.com/project/suitecrmtranslations',
+    'type' => 'langpack',
+    'is_uninstallable' => 'Yes',
+    'author' => 'SuiteCRM 社群',
+    'version' => '7.15-1',
+    'published_date' => '2026-01-16',
+);
+$installdefs = array(
+    'id' => 'zh_TW',
+    'image_dir' => '<basepath>/images',
+    'copy' => array(
+        array('from' => '<basepath>/include', 'to' => 'include'),
+        array('from' => '<basepath>/modules', 'to' => 'modules'),
+        array('from' => '<basepath>/install', 'to' => 'install'),
+    )
+);
