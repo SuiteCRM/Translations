66aa791f70d49991d94d046cac8eef891c64b1c0
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-28 18:29:10 +0000

    New translations en_us.lang.php (Estonian)

---
+++ translations/et_EE/modules/EmailMan/language/et_EE.lang.php
@@ -0,0 +1,151 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_SEND_DATE_TIME' => 'Saatmiskuupäev:',
+    'LBL_IN_QUEUE' => 'Töös',
+    'LBL_IN_QUEUE_DATE' => 'Ootelepaneku kuupäev',
+
+    'ERR_INT_ONLY_EMAIL_PER_RUN' => 'Saadetavate e-kirjade arvu täpsustamisel kasutage ainult täisarve',
+
+    'LBL_ATTACHMENT_AUDIT' => 'saadetud. Seda ei dubleeritud lokaalselt, et säilitada kettamahtu.',
+    'LBL_CONFIGURE_SETTINGS' => 'Konfigureeri e-posti sätteid',
+    'LBL_CUSTOM_LOCATION' => 'Kasutaja on määratletud',
+    'LBL_DEFAULT_LOCATION' => 'Vaikimisi',
+
+    'LBL_EMAIL_DEFAULT_DELETE_ATTACHMENTS' => 'Koos kustutatud e-kirjadega kustuta ka seotud märkused ja manused',
+    'LBL_EMAIL_WARNING_NOTIFICATIONS' => 'Email warning notifications',
+    'LBL_EMAIL_ENABLE_CONFIRM_OPT_IN' => 'Opt In Settings',
+    'LBL_EMAIL_ENABLE_SEND_OPT_IN' => 'Automatically Send Opt In Email',
+    'LBL_EMAIL_CONFIRM_OPT_IN_TEMPLATE_ID' => 'Confirm Opt In Email Template',
+    'LBL_LEGACY_EMAIL_COMPOSE_BEHAVIOR' => 'Enable Legacy Email Compose Behaviour',
+    'LBL_EMAIL_OUTBOUND_CONFIGURATION' => 'Väljaminevate e-kirjade konfiguratsioon',
+    'LBL_EMAILS_PER_RUN' => 'Saadetud e-kirju partii kohta:',
+    'LBL_ID' => 'Id',
+    'LBL_LIST_CAMPAIGN' => 'Kampaaniad',
+    'LBL_LIST_FORM_TITLE' => 'Ootejärjekord',
+    'LBL_LIST_FROM_NAME' => 'Nime alt:',
+    'LBL_LIST_IN_QUEUE' => 'Töös',
+    'LBL_LIST_MESSAGE_NAME' => 'Turundussõnum',
+    'LBL_LIST_RECIPIENT_EMAIL' => 'Saaja e-post',
+    'LBL_LIST_RECIPIENT_NAME' => '\'Saaja nimi',
+    'LBL_LIST_SEND_ATTEMPTS' => 'Saada katsed',
+    'LBL_LIST_SEND_DATE_TIME' => 'Saadetud',
+    'LBL_LIST_USER_NAME' => 'Kasutajanimi',
+    'LBL_LOCATION_ONLY' => 'Asukoht',
+    'LBL_LOCATION_TRACK' => 'Kampaania jälgimise failide asukoht (nagu campaign_tracker.php)',
+    'LBL_CAMP_MESSAGE_COPY' => 'Kampaania sõnumite koopiad hoia:',
+    'LBL_CAMP_MESSAGE_COPY_DESC' => 'Kas soovite hoiustada täielikud koopiad kõigi e-kirjade kohta, mis on kampaaniate käigus edastatud? Soovitame mitte ja see on ka vaikimisi seadistus. Valides Ei hoiustatakse vaid saadetud sõnumi mall, mis on vajalik uue individuaalse sõnumi taasloomiseks.',
+    'LBL_MAIL_SENDTYPE' => 'Mail Transfer Agent:',
+    'LBL_MAIL_SMTPAUTH_REQ' => 'Kasuta SMTP autentimist:',
+    'LBL_MAIL_SMTPPASS' => 'Salasõna:',
+    'LBL_MAIL_SMTPPORT' => 'SMTP port:',
+    'LBL_MAIL_SMTPSERVER' => 'SMTP Mail Server:',
+    'LBL_MAIL_SMTPUSER' => 'Kasutajanimi:',
+    'LBL_CHOOSE_EMAIL_PROVIDER' => 'Vali oma e-posti teenusepakkuja',
+    'LBL_YAHOOMAIL_SMTPPASS' => 'Yahoo! Mail Password',
+    'LBL_YAHOOMAIL_SMTPUSER' => 'Yahoo! Mail ID',
+    'LBL_GMAIL_SMTPPASS' => 'Gmaili salasõna',
+    'LBL_GMAIL_SMTPUSER' => 'Gmaili e-posti aadress',
+    'LBL_EXCHANGE_SMTPPASS' => 'Vaheta salasõna',
+    'LBL_EXCHANGE_SMTPUSER' => 'Vaheta kasutajanime',
+    'LBL_EXCHANGE_SMTPPORT' => 'Vaheta Server Port',
+    'LBL_EXCHANGE_SMTPSERVER' => 'Vaheta server',
+    'LBL_EMAIL_LINK_TYPE' => 'E-posti klient',
+    'LBL_MARKETING_ID' => 'Turundus Id',
+    'LBL_MODULE_ID' => 'EmailMan',
+    'LBL_MODULE_NAME' => 'E-posti sätted',
+    'LBL_MODULE_TITLE' => 'Väljaminevate e-kirjade ootejärjekorra juhtimine',
+    'LBL_NOTIFICATION_ON_DESC' => 'Kui lubatud, siis e-kirjad saadetakse kasutajatele kui kirjed on neile määratud',
+    'LBL_NOTIFY_FROMADDRESS' => '"Kellelt" aadress',
+    'LBL_NOTIFY_FROMNAME' => '"Kellelt" nimi:',
+    'LBL_NOTIFY_ON' => 'Vastutamisteavitused',
+    'LBL_NOTIFY_TITLE' => 'E-posti suvandid',
+    'LBL_OUTBOUND_EMAIL_TITLE' => 'Väljaminevate e-kirjade suvandid',
+    'LBL_RELATED_ID' => 'Seotud Id',
+    'LBL_RELATED_TYPE' => 'Seotud tüüp',
+    'LBL_SEARCH_FORM_TITLE' => 'Ootejärjekorra otsing',
+    'TRACKING_ENTRIES_LOCATION_DEFAULT_VALUE' => 'Value of Config.php setting site_url',
+    'TXT_REMOVE_ME_ALT' => 'Enda eemaldamiseks sellest e-posti listist mine',
+    'TXT_REMOVE_ME_CLICK' => 'kliki siia',
+    'TXT_REMOVE_ME' => 'Enda eemaldamiseks sellest e-posti listist',
+    'LBL_NOTIFY_SEND_FROM_ASSIGNING_USER' => 'Send notification from the email address of the assigning user',
+
+    'LBL_SECURITY_TITLE' => 'E-posti turvasätted',
+    'LBL_SECURITY_DESC' => 'Kontrolli järgnevalt, mis pole lubatud sissetuleva e-kirjas või kuvatud e-kirjade moodulis.',
+    'LBL_SECURITY_APPLET' => 'Applet tag',
+    'LBL_SECURITY_BASE' => 'Base tag',
+    'LBL_SECURITY_EMBED' => 'Embed tag',
+    'LBL_SECURITY_FORM' => 'Form tag',
+    'LBL_SECURITY_FRAME' => 'Frame tag',
+    'LBL_SECURITY_FRAMESET' => 'Frameset tag',
+    'LBL_SECURITY_IFRAME' => 'iFrame tag',
+    'LBL_SECURITY_IMPORT' => 'Import tag',
+    'LBL_SECURITY_LAYER' => 'Kihi silt',
+    'LBL_SECURITY_LINK' => 'Lingi silt',
+    'LBL_SECURITY_OBJECT' => 'Object tag',
+    'LBL_SECURITY_OUTLOOK_DEFAULTS' => 'Select Outlook default minimum security settings (errs on the side of correct display).',
+    'LBL_SECURITY_STYLE' => 'Style tag',
+    'LBL_SECURITY_TOGGLE_ALL' => 'Toggle All Options',
+    'LBL_SECURITY_XMP' => 'Xmp tag',
+    'LBL_YES' => 'Jah',
+    'LBL_NO' => 'Ei',
+    'LBL_PREPEND_TEST' => '[Test]:',
+    'LBL_SEND_ATTEMPTS' => 'Saada katsed',
+    'LBL_OUTGOING_SECTION_HELP' => 'Configure the default outgoing mail server for sending email notifications, including workflow alerts.',
+    'LBL_ALLOW_DEFAULT_SELECTION' => "Users may send as this account's identity:",
+    'LBL_ALLOW_DEFAULT_SELECTION_HELP' => 'When this option selected, all users will be able to send emails using the same outgoing mail account used to send system notifications and alerts. If the option is not selected,<br>users can still use the outgoing mail server after providing their own account information.',
+    'LBL_FROM_ADDRESS_HELP' => 'When enabled, the assigning user\\\'s name and email address will be included in the From field of the email. This feature might not work with SMTP servers that do not allow sending from a different email account than the server account.',
+    'LBL_HELP' => 'Abi' /*for 508 compliance fix*/,
+    'LBL_OUTBOUND_EMAIL_ACCOUNT_VIEW' => 'View Outbound Email Accounts',
+    'LBL_SYSTEM_OUTBOUND_EMAIL_ACCOUNT' => 'System Outbound Email Account',
+    'LBL_ALLOW_SEND_AS_USER' => 'Users may send as themselves:',
+    'LBL_ALLOW_SEND_AS_USER_DESC' => 'When enabled, <b>all</b> users can send email using the outgoing mail server, using their own primary email address as the &quot;From:&quot; address.<br>This feature might not work with SMTP servers that do not allow sending from a different email account than the server account.',
+    'LBL_EMAIL_IMPORT_CONFIGURATION' => 'Email Import Configuration',
+    'LBL_EMAIL_IMPORT_PER_RUN_THRESHOLD' => 'Threshold for Number of Emails Imported Per Run (per Mailbox)',
+    'LBL_EMAIL_IMPORT_PER_RUN_THRESHOLD_DESC' => 'Threshold of emails to import per run. Default is 25. Each run will go through each day in the mailbox within the set timeframe, from oldest to most recent. It will import all emails on each day until the total emails imported on this run exceeds the threshold. Once met, the job will stop and mark the last day imported, starting there on the next run.',
+    'LBL_EMAIL_IMPORT_TIMEFRAME_START' => 'Starting Point for New Mailbox Imports',
+    'LBL_EMAIL_IMPORT_TIMEFRAME_START_DESC' => 'Period of time from which emails will start being imported. For example, selecting 30 Days will start with emails from 30 days ago and move forward from there.',
+    'LBL_EMAIL_IMPORT_FETCH_UNREAD_ONLY' => 'Only fetch unread emails when importing',
+);
