83785a10abd05f3ef69cafa11cfef2457f5e7658
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-28 19:34:44 +0000

    New translations en_us.lang.php (Nigerian Pidgin)

---
+++ translations/pcm_NG/modules/SurveyQuestionResponses/language/pcm_NG.lang.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID'                                                                 => 'User id wey dem give',
+    'LBL_ASSIGNED_TO_NAME'                                                               => 'Dem give am',
+    'LBL_SECURITYGROUPS'                                                                 => 'Security Groups',
+    'LBL_SECURITYGROUPS_SUBPANEL_TITLE'                                                  => 'Security Groups',
+    'LBL_ID'                                                                             => 'ID',
+    'LBL_DATE_ENTERED'                                                                   => 'Day wey dem create am',
+    'LBL_DATE_MODIFIED'                                                                  => 'De day wey dem modify am',
+    'LBL_MODIFIED'                                                                       => 'Person wey modify am',
+    'LBL_MODIFIED_ID'                                                                    => 'Modified By Id',
+    'LBL_MODIFIED_NAME'                                                                  => 'Na name dem use modify am',
+    'LBL_CREATED'                                                                        => 'Person wey create am',
+    'LBL_CREATED_ID'                                                                     => 'Created By Id',
+    'LBL_DESCRIPTION'                                                                    => 'Na for here you go sabi as the thing be',
+    'LBL_ANSWER'                                                                         => 'Answer',
+    'LBL_ANSWER_BOOL'                                                                    => 'Boolean answer',
+    'LBL_ANSWER_DATETIME'                                                                => 'Datetime answer',
+    'LBL_DELETED'                                                                        => 'Comot am',
+    'LBL_NAME'                                                                           => 'Wetin be your name',
+    'LBL_CREATED_USER'                                                                   => 'Na person wey dey use am create am',
+    'LBL_MODIFIED_USER'                                                                  => 'Na person wey dey use am modify am',
+    'LBL_LIST_NAME'                                                                      => 'Wetin be your name',
+    'LBL_EDIT_BUTTON'                                                                    => 'Edit',
+    'LBL_REMOVE'                                                                         => 'Comot am',
+    'LBL_LIST_FORM_TITLE'                                                                => 'Survey Question Responses List',
+    'LBL_MODULE_NAME'                                                                    => 'Survey Question Responses',
+    'LBL_MODULE_TITLE'                                                                   => 'Survey Question Responses',
+    'LBL_HOMEPAGE_TITLE'                                                                 => 'My Survey Question Responses',
+    'LNK_NEW_RECORD'                                                                     => 'Create Survey Question Responses',
+    'LNK_LIST'                                                                           => 'View Survey Question Responses',
+    'LNK_IMPORT_SURVEYQUESTIONRESPONSES'                                                 => 'Import Survey Question Responses',
+    'LBL_SEARCH_FORM_TITLE'                                                              => 'Search Survey Question Responses',
+    'LBL_HISTORY_SUBPANEL_TITLE'                                                         => 'Make you view History',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE'                                                      => 'Activities',
+    'LBL_SURVEYQUESTIONRESPONSES_SUBPANEL_TITLE'                                         => 'Survey Question Responses',
+    'LBL_NEW_FORM_TITLE'                                                                 => 'New Survey Question Responses',
+    'LBL_SURVEYRESPONSES_SURVEYQUESTIONRESPONSES_FROM_SURVEYRESPONSES_TITLE'             => 'Survey Responses',
+    'LBL_SURVEYQUESTIONS_SURVEYQUESTIONRESPONSES_FROM_SURVEYQUESTIONS_TITLE'             => 'Survey Questions',
+    'LBL_SURVEYQUESTIONOPTIONS_SURVEYQUESTIONRESPONSES_FROM_SURVEYQUESTIONOPTIONS_TITLE' => 'Survey Question Options',
+);
