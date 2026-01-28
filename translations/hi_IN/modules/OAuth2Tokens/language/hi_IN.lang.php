fc8933830c09339ab7d54929d51d0ac7c5dd6d17
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-28 19:04:00 +0000

    New translations en_us.lang.php (Hindi)

---
+++ translations/hi_IN/modules/OAuth2Tokens/language/hi_IN.lang.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_MODULE_NAME' => 'OAuth2 टोकन',
+    'LBL_MODULE_TITLE' => 'OAuth2 टोकन',
+    'LBL_MODULE_ID'=> 'OAuth2 टोकन',
+    'LBL_TOKEN_ID'=> 'टोकन आईडी',
+    'LBL_CLIENT'=> 'ग्राहक',
+    'LBL_TOKEN_IS_REVOKED'=> 'वापस लेना',
+    'LBL_ACCESS_TOKEN_TYPE'=> 'टोकन प्रकार',
+    'LBL_ACCESS_TOKEN_EXPIRES'=> 'एक्सेस टोकन की समयसीमा समाप्त हो गई',
+    'LBL_REFRESH_TOKEN_EXPIRES'=> 'रिफ्रेश टोकन की समयसीमा समाप्त हो गई',
+    'LBL_GRANT_TYPE'=> 'अनुदान का प्रकार',
+    'LBL_SCOPES'=> 'कार्यक्षेत्र',
+    'LBL_REVOKE_TOKEN'=> 'टोकन रद्द करें',
+    'LBL_REVOKE_TOKENS'=> 'Revoke Tokens',
+    'LBL_CLIENT_NAME'=> 'ग्राहक नाम',
+    'LBL_USER' => 'संबद्ध उपयोगकर्ता',
+    'LBL_IS_ACTIVE' => 'केवल सक्रिय टोकन',
+
+    'LNK_OAUTH2_TOKEN_LIST'=> 'OAuth2 टोकन की सूची बनाएं',
+    'LNK_OAUTH2_CLIENT_LIST' => 'OAuth2 क्लाइंट की सूची बनाएं',
+    'LNK_OAUTH2_AUTHCODE_LIST' => 'List OAuth2 Authorization Codes',
+    
+    'LNK_NEW_OAUTH2_CLIENT' => 'OAuth2 क्लाइंट बनाएँ',
+    'LNK_NEW_OAUTH2_PASSWORD_CLIENT' => 'नया पासवर्ड क्लाइंट',
+    'LNK_NEW_OAUTH2_CREDENTIALS_CLIENT' => 'नए ग्राहक क्रेडेंशियल्स क्लाइंट',
+    'LNK_NEW_OAUTH2_IMPLICIT_CLIENT' => 'नया इंप्लिसिट क्लाइंट',
+    'LNK_NEW_OAUTH2_AUTHORIZATION_CLIENT' => 'नया प्राधिकरण क्लाइंट',
+
+    'LBL_ACCESS_TOKEN' => 'एक्सेस टोकन',
+    'LBL_REFRESH_TOKEN' => 'टोकन ताज़ा करें',
+    'LBL_DELETE_CONFIRMATION' => 'Are you sure you want to delete this record? The client will not be granted an authorization code any more.',
+    'LBL_REVOKE_AND_DELETE_BUTTON_LABEL' => 'Revoke and Delete',
+);
