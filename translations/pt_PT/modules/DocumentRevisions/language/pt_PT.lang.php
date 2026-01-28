f4985ce77c4061ff75e35609fa90f71a5d327bce
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-28 18:22:03 +0000

    New translations en_us.lang.php (Portuguese)

---
+++ translations/pt_PT/modules/DocumentRevisions/language/pt_PT.lang.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    //module
+    'LBL_MODULE_NAME' => 'Revisão do documento',
+
+    'LNK_NEW_DOCUMENT' => 'Criar documento',
+    'LNK_DOCUMENT_LIST' => 'Ver documentos',
+
+    //vardef labels
+    'LBL_REVISION_NAME' => 'Número da revisão',
+    'LBL_FILENAME' => 'Ficheiro:',
+    'LBL_MIME' => 'Tipo de Mime',
+    'LBL_REVISION' => 'Revisão',
+    'LBL_DOCUMENT' => 'Documento relacionado',
+    'LBL_LATEST_REVISION' => 'Última Revisão',
+    'LBL_CHANGE_LOG' => 'Historial de alterações',
+    'LBL_ACTIVE_DATE' => 'Data de publicação',
+    'LBL_EXPIRATION_DATE' => 'Data de validade',
+    'LBL_FILE_EXTENSION' => 'Extensão de ficheiro',
+
+    'LBL_DOC_NAME' => 'Nome do documento:',
+    'LBL_DOC_VERSION' => 'Revisão:',
+
+    //document revisions.
+    'LBL_REV_LIST_REVISION' => 'Revisão',
+    'LBL_REV_LIST_ENTERED' => 'Data de criação',
+    'LBL_REV_LIST_CREATED' => 'Criado por',
+    'LBL_REV_LIST_LOG' => 'Historial de alterações',
+    'LBL_REV_LIST_FILENAME' => 'Ficheiro',
+
+    'LBL_CURRENT_DOC_VERSION' => 'Última revisão:',
+    'LBL_SEARCH_FORM_TITLE' => 'Pesquisar documentos',
+    'LBL_REVISIONS' => 'Revisões',
+
+    //error messages
+    'ERR_FILENAME' => 'Nome do ficheiro',
+    'ERR_DOC_VERSION' => 'Versão do documento',
+    'ERR_DELETE_CONFIRM' => 'Deseja eliminar esta revisão do documento?',
+    'ERR_DELETE_LATEST_VERSION' => 'Não tem permissão para eliminar a revisão mais recente do documento.',
+    'LNK_NEW_MAIL_MERGE' => 'Impressão em série',
+    'LBL_DOC_ID' => 'ID da origem do documento',
+    'LBL_DOC_TYPE' => 'Origem',
+    'LBL_DOC_URL' => 'URL de origem do documento',
+
+    'LBL_CREATED_BY_NAME' => 'Criado por nome',
+);
