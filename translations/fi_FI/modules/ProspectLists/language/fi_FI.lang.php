5717e15430f1e3ca2e56afb947c86cec6ce9eb21
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-28 19:14:05 +0000

    New translations en_us.lang.php (Finnish)

---
+++ translations/fi_FI/modules/ProspectLists/language/fi_FI.lang.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_MODULE_NAME' => 'Prospektilistat',
+    'LBL_MODULE_ID' => 'Prospektilistat',
+    'LBL_MODULE_TITLE' => 'Prospektilistat: Etusivu',
+    'LBL_SEARCH_FORM_TITLE' => 'Prospektilistojen haku',
+    'LBL_LIST_FORM_TITLE' => 'Prospektilistat',
+    'LBL_PROSPECT_LIST_NAME' => 'Prospektilistat:',
+    'LBL_NAME' => 'Nimi',
+    'LBL_ENTRIES' => 'Prospekteja yhteensä',
+    'LBL_LIST_PROSPECT_LIST_NAME' => 'Prospektilista',
+    'LBL_LIST_ENTRIES' => 'Prospekteja listalla',
+    'LBL_LIST_DESCRIPTION' => 'Kuvaus',
+    'LBL_LIST_TYPE_NO' => 'Tyyppi',
+    'LBL_LIST_END_DATE' => 'Vanhentumispäivä',
+    'LBL_DATE_ENTERED' => 'Luonti pvm',
+    'LBL_MARKETING_ID' => 'Markkinointitunnus',
+    'LBL_DATE_MODIFIED' => 'Muokkaus pvm',
+    'LBL_MODIFIED' => 'Muokkaaja',
+    'LBL_CREATED' => 'Tekijä',
+    'LBL_ASSIGNED_TO' => 'Vastuuhenkilö',
+    'LBL_DESCRIPTION' => 'Kuvaus',
+    'LNK_NEW_CAMPAIGN' => 'Luo kampanja',
+    'LNK_CAMPAIGN_LIST' => 'Kampanjat',
+    'LNK_NEW_PROSPECT_LIST' => 'Luo prospektilista',
+    'LNK_PROSPECT_LIST_LIST' => 'Prospektilistat',
+    'LBL_MODIFIED_BY' => 'Muokkaaja',
+    'LBL_CREATED_BY' => 'Tekijä',
+    'LNK_NEW_PROSPECT' => 'Luo prospekti',
+    'LNK_PROSPECT_LIST' => 'Prospektit',
+
+    'LBL_CONTACTS_SUBPANEL_TITLE' => 'Kontaktit',
+    'LBL_LEADS_SUBPANEL_TITLE' => 'Liidit',
+    'LBL_PROSPECTS_SUBPANEL_TITLE' => 'Prospektit',
+    'LBL_ACCOUNTS_SUBPANEL_TITLE' => 'Asiakkaat',
+    'LBL_COPY_PREFIX' => 'Kopio',
+    'LBL_USERS_SUBPANEL_TITLE' => 'Käyttäjät',
+    'LBL_TYPE' => 'Tyyppi',
+    'LBL_LIST_TYPE' => 'Tyyppi',
+    'LBL_LIST_TYPE_LIST_NAME' => 'Tyyppi',
+    'LBL_NEW_FORM_TITLE' => 'Uusi prospektilista',
+    'LBL_MARKETING_NAME' => 'Marketing Name',
+    'LBL_DOMAIN_NAME' => 'Domain nimi',
+    'LBL_DOMAIN' => 'Ei viestejä domainiin',
+    'LBL_LIST_PROSPECTLIST_NAME' => 'Nimi',
+
+    'LBL_EMAIL_MARKETING' => 'Sähköpostimarkkinointi',
+
+    'LBL_ASCENDING' => 'Nouseva',
+    'LBL_DESCENDING' => 'Laskeva',
+);
