c50f04d0fc35e48738081a91f1ff7d707982fa80
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-28 18:48:40 +0000

    New translations en_us.lang.php (Lithuanian)

---
+++ translations/lt_LT/modules/InboundEmail/language/lt_LT.lang.php
@@ -0,0 +1,229 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+
+
+    'LBL_RE' => 'RE:',
+
+    'ERR_BAD_LOGIN_PASSWORD' => 'Vartotojo vardas arba slaptažodis neteisingas',
+    'ERR_INI_ZLIB' => 'Nepavyko trumpam išjungti Zlib kompresijos.  "Bandyti nustatymai" gali nepavykti.',
+    'ERR_NO_IMAP' => 'Nerastos IMAP bibliotekos. Prašome išspręsti tai, prieš pradedant dirbti su įeinančiu paštu',
+    'ERR_NO_OPTS_SAVED' => 'Nebuvo išsaugota su Jūsų įeinančiu pašto dėžute. Prašome peržiūrėti nustatymus',
+    'ERR_TEST_MAILBOX' => 'Prašome patikrinti nustatymus ir bandyti dar kartą',
+    'ERR_INVALID_PORT' => 'Invalid Port',
+
+    'LBL_ASSIGN_TO_USER' => 'Atsakingas vartotojas',
+    'LBL_AUTOREPLY' => 'Auto-atsakymo šablonas',
+    'LBL_AUTOREPLY_HELP' => 'Pasirinkite laiško šabloną, kurį norėsite automatiškai išsiųsti, kiekvieną gausite laišką iš siuntėjo.',
+    'LBL_BASIC' => 'Pašto dėžutės informacija',
+    'LBL_CASE_MACRO' => 'Aptarnavimo makro komanda',
+    'LBL_CASE_MACRO_DESC' => 'Nustatykite makro komandą, kuri bus naudojama susieti importuotus laiškus su aptarnavimu.',
+    'LBL_CASE_MACRO_DESC2' => 'Nustatyti tai į bet kokią reikšmę, tik išlaikykite šią frazę  <b>"%1"</b>.',
+    'LBL_CLOSE_POPUP' => 'Uždaryti langą',
+    'LBL_CREATE_TEMPLATE' => 'Sukurti',
+    'LBL_DELETE_SEEN' => 'Ištrinti perskaitytus laiškus po importavimo',
+    'LBL_EDIT_TEMPLATE' => 'Redaguoti',
+    'LBL_EMAIL_OPTIONS' => 'Pašto tvarkymo pasirinkimas',
+    'LBL_EMAIL_BOUNCE_OPTIONS' => 'Nepasiekusių laiškų valdymas',
+    'LBL_FILTER_DOMAIN_DESC' => 'Nurodyti domeną, kuriam nebus siunčiami auto-atsakymai.',
+    'LBL_ASSIGN_TO_GROUP_FOLDER_DESC' => 'Select to automatically create email records in SuiteCRM for all incoming emails.',
+    'LBL_FILTER_DOMAIN' => 'Nesiųsti auto-atsakymo į šį domeną',
+    'LBL_FIND_SSL_WARN' => '<br>Testuojant SSL gali ilgai užtrukti. Prašome būti kantriems.<br>',
+    'LBL_FROM_ADDR' => 'Siuntėjo adresas',
+    'LBL_FROM_ADDR_DESC' => 'The email address provided here might not appear in the &quot;From&quot; address section of the email sent due to restrictions imposed by the mail service provider. In these circumstances, the email address defined in the outgoing mail server will be used.', // as long as XTemplate doesn't support output escaping, transform quotes to html-entities right here (bug #48913)
+    'LBL_FROM_NAME' => 'Siuntėjo vardas',
+    'LBL_GROUP_QUEUE' => 'Priskirti grupei',
+    'LBL_HOME' => 'Pradžia',
+    'LBL_LIST_MAILBOX_TYPE' => 'Pašo dėžutės naudojimas',
+    'LBL_LIST_NAME' => 'Vardas:',
+    'LBL_LIST_GLOBAL_PERSONAL' => 'Tipas:',
+    'LBL_LIST_SERVER_URL' => 'Pašto serveris:',
+    'LBL_SERVER_ADDRESS' => 'Server Address',
+    'LBL_LIST_STATUS' => 'Būsena',
+    'LBL_LOGIN' => 'Vartotojas vardas',
+    'LBL_USERNAME' => 'Vartotojas vardas',
+    'LBL_MAILBOX_DEFAULT' => 'Gauti laiškai',
+    'LBL_MAILBOX_SSL' => 'Naudoti SSL',
+    'LBL_MAILBOX_TYPE' => 'Galimi veiksmai',
+    'LBL_DISTRIBUTION_METHOD' => 'Paskirstymo metodas',
+    'LBL_CREATE_CASE_REPLY_TEMPLATE' => 'Naujas auto-aptarnavimo šablonas',
+    'LBL_CREATE_CASE_REPLY_TEMPLATE_HELP' => 'Pasirinkite laiško šabloną, kurį norėsite automatiškai išsiųsti, kiekvieną kartą kai naujas aptarnavimas bus sukurtas.',
+    'LBL_MAILBOX' => 'Stebimi aplankai',
+    'LBL_TRASH_FOLDER' => 'Šiukšlių aplankas',
+    'LBL_SENT_FOLDER' => 'Siuntimo aplankas',
+    'LBL_SELECT' => 'Pasirinkite',
+    'LBL_MARK_READ_NO' => 'El. paštas pažymėtas ištrynimui po importavimo',
+    'LBL_MARK_READ_YES' => 'El. paštas liko serveryje po importavimo',
+    'LBL_MARK_READ' => 'Palikti laišką serveryje',
+    'LBL_MAX_AUTO_REPLIES' => 'Auto-atsakymų skaičius',
+    'LBL_MAX_AUTO_REPLIES_DESC' => 'Nustatyti maksimalų auto-atsakymų skaičių per 24 valandų periodą.',
+    'LBL_PERSONAL_MODULE_NAME' => 'Asmeninė pašto dėžutė',
+    'LBL_CREATE_CASE' => 'Sukurti aptarnavimą iš laiško',
+    'LBL_CREATE_CASE_HELP' => 'Select to automatically create case records in SuiteCRM from incoming emails.',
+    'LBL_MODULE_NAME' => 'Group Mail Account',
+    'LBL_BOUNCE_MODULE_NAME' => 'Nepasiekusių laiškų pašto dėžutė',
+    'LBL_MODULE_TITLE' => 'Įeinantis paštas',
+    'LBL_NAME' => 'Vardas',
+    'LBL_NONE' => 'Nėra',
+    'LBL_ONLY_SINCE_NO' => 'Ne. Patikrinti paštą serveryje.',
+    'LBL_ONLY_SINCE_YES' => 'Taip.',
+    'LBL_PASSWORD' => 'SMTP slaptažodis:',
+    'LBL_EMAIL_PASSWORD' => 'SMTP slaptažodis:',
+    'LBL_POP3_SUCCESS' => 'Jūsų POP3 prisijungimo testas sėkmingas.',
+    'LBL_POPUP_TITLE' => 'Testavimo nustatymai',
+    'LBL_SELECT_SUBSCRIBED_FOLDERS' => 'Pasirinkite prenumeruotus aplankus',
+    'LBL_SELECT_TRASH_FOLDERS' => 'Pasirinkite šiukšlių katalogą',
+    'LBL_SELECT_SENT_FOLDERS' => 'Pasirinkite siuntimo katalogą',
+    'LBL_DELETED_FOLDERS_LIST' => 'Šis katalogas neegzistuoja arba buvo ištrintas iš serverio',
+    'LBL_PORT' => 'Pašto serverio portas',
+    'LBL_REPLY_TO_NAME' => '"Atsakyti-kam" vardas',
+    'LBL_REPLY_TO_ADDR' => '"Atsakyti-kam" adresas',
+    'LBL_SAME_AS_ABOVE' => 'Naudojant vardas/adresas',
+    'LBL_SERVER_OPTIONS' => 'Detalus nustatymas',
+    'LBL_SERVER_TYPE' => 'Pašto serverio protokolas',
+    'LBL_SERVER_PORT' => 'Pašto serverio portas',
+    'LBL_SERVER_URL' => 'Pašto Serverio adresas',
+    'LBL_SSL_DESC' => 'Jeigu jūsų pašto serveris palaiko saugius prisijungimus, tai bus prisijungta per SSL prisijungimą laiškų importavimo metu',
+    'LBL_ASSIGN_TO_TEAM_DESC' => 'Pažymėta komanda turi priėjimą prie pašto dėžutės.',
+    'LBL_SSL' => 'Naudoti SSL',
+    'LBL_STATUS' => 'Būsena',
+    'LBL_EMAIL_BODY_FILTERING' => 'Email Body Filter Type',
+    'LBL_SYSTEM_DEFAULT' => 'Sistemos numatytas',
+    'LBL_TEST_BUTTON_TITLE' => 'Testas',
+    'LBL_TEST_SETTINGS' => 'Testavimo nustatymai',
+    'LBL_TEST_CONNECTION_SETTINGS' => 'Test Connection Settings',
+    'LBL_TEST_SUCCESSFUL' => 'Prisijungimas sėkmingas.',
+    'LBL_TEST_WAIT_MESSAGE' => 'Prašome palaukti...',
+    'LBL_WARN_IMAP_TITLE' => 'Įeinantis paštas išjungtas',
+    'LBL_WARN_IMAP' => 'Perspėjimai:',
+    'LBL_WARN_NO_IMAP' => 'Įeinantis paštas <b>negali</b> funkcionuoti be IMAP c-kliento bibliotekų įjungtų/sukompiliuotų su PHP moduliu. Prašome susisiekti su administratoriumi, kad išspręsti šią problemą.',
+
+    'LNK_LIST_CREATE_NEW_PERSONAL' => 'New Personal Inbound Email Account',
+    'LNK_LIST_CREATE_NEW_GROUP' => 'New Group Inbound Email Account',
+    'LNK_LIST_CREATE_NEW_CASES_TYPE' => 'New Case Handling Email Account',
+    'LNK_LIST_CREATE_NEW_BOUNCE' => 'New Bounce Handling Email Account',
+    'LNK_LIST_MAILBOXES' => 'Inbound Email Accounts',
+    'LNK_LIST_OUTBOUND_EMAILS' => 'Išeinančio pašto paskyros',
+    'LNK_LIST_SCHEDULER' => 'Planuotojai',
+    'LNK_SEED_QUEUES' => 'Platinti eiles nuo komandų',
+    'LBL_GROUPFOLDER_ID' => 'Grupės aplanko Id',
+
+    'LBL_ALLOW_OUTBOUND_GROUP_USAGE' => 'Leisti vartotojams siunčiant naudoti šį siuntėjo vardą ir adresą, kaip atsakymo adresą',
+    'LBL_ALLOW_OUTBOUND_GROUP_USAGE_DESC' => 'Kai pasirinkta ši nuostata, Siuntėjo vardas ir Siuntėjo adresas matysis vartotojams kuriant laiškus (Tik tiems vartotojams, kurie turi priėjimą prie grupinės pašto dėžutės).',
+    'LBL_STATUS_ACTIVE' => 'Aktyvu',
+    'LBL_STATUS_INACTIVE' => 'Neaktyvu',
+    'LBL_IS_PERSONAL' => 'Asmeninis',
+    'LBL_IS_GROUP' => 'grupės',
+    'LBL_ENABLE_AUTO_IMPORT' => 'Importuoti laiškus automatiškai',
+    'LBL_WARNING_CHANGING_AUTO_IMPORT' => 'Perspėjimas: Jūs modifikuojate auto importavimo nustatymus, o tai gali sukelti duomenų praradimą.',
+    'LBL_WARNING_CHANGING_AUTO_IMPORT_WITH_CREATE_CASE' => 'Perspėjimas: Auto importavimas turi būti įjungtas, jei norite automatiškai kurti aptarnavimus.',
+    'LBL_LIST_TITLE_MY_DRAFTS' => 'Drafts',
+    'LBL_LIST_TITLE_MY_INBOX' => 'Inbox',
+    'LBL_LIST_TITLE_MY_SENT' => 'Siųsti elektroninį laišką',
+    'LBL_LIST_TITLE_MY_ARCHIVES' => 'Archyvuoti laiškai',
+    'LNK_MY_DRAFTS' => 'Drafts',
+    'LNK_MY_INBOX' => 'el.paštas',
+    'LNK_VIEW_MY_INBOX' => 'View Email',
+    'LNK_QUICK_REPLY' => 'Atsakyta',
+    'LNK_SENT_EMAIL_LIST' => 'Išsiųsti laiškai',
+    'LBL_EDIT_LAYOUT' => 'Redaguoti išdėstymą' /*for 508 compliance fix*/,
+    'LBL_TYPE_DIFFERENT' => 'External OAuth Connection type must be the SAME as the Inbound Email Account type',
+
+    'LBL_MODIFIED_BY' => 'Redagavo',
+    'LBL_SERVICE' => 'Paslauga',
+    'LBL_STORED_OPTIONS' => 'Stored Options',
+    'LBL_GROUP_ID' => 'Group ID',
+
+    'LBL_OUTBOUND_CONFIGURATION' => 'Outbound Configuration',
+    'LBL_CONNECTION_CONFIGURATION' => 'Server Configuration',
+    'LBL_AUTO_REPLY_CONFIGURATION' => 'Auto Reply Configuration',
+    'LBL_CASE_CONFIGURATION' => 'Case Configuration',
+    'LBL_GROUP_CONFIGURATION' => 'Group Configuration',
+
+    'LBL_SECURITYGROUPS_SUBPANEL_TITLE' => 'Apsaugotos grupės',
+
+
+    'LBL_OUTBOUND_EMAIL_ACCOUNT' => 'Outbound Email Account',
+    'LBL_OUTBOUND_EMAIL_ACCOUNT_ID' => 'Outbound Email Account id',
+    'LBL_OUTBOUND_EMAIL_ACCOUNT_NAME' => 'Outbound Email Account',
+
+    'LBL_AUTOREPLY_EMAIL_TEMPLATE' => 'Auto Reply Email Template',
+    'LBL_AUTOREPLY_EMAIL_TEMPLATE_ID' => 'Auto Reply Email Template id',
+    'LBL_AUTOREPLY_EMAIL_TEMPLATE_NAME' => 'Auto Reply Email Template',
+
+    'LBL_CASE_EMAIL_TEMPLATE' => 'Case Email Template',
+    'LBL_CASE_EMAIL_TEMPLATE_ID' => 'Case Email Template id',
+    'LBL_CASE_EMAIL_TEMPLATE_NAME' => 'Case Email Template',
+
+    'LBL_PROTOCOL' => 'Protocol',
+    'LBL_CONNECTION_STRING' => 'Connection String',
+    'LBL_DISTRIB_METHOD' => 'Paskirstymo metodas',
+    'LBL_DISTRIB_OPTIONS' => 'Distribution Options',
+
+    'LBL_DISTRIBUTION_USER' => 'Distribution User',
+    'LBL_DISTRIBUTION_USER_ID' => 'Distribution User id',
+    'LBL_DISTRIBUTION_USER_NAME' => 'Distribution User',
+
+    'LBL_EXTERNAL_OAUTH_CONNECTION' => 'External OAuth Connection',
+    'LBL_EXTERNAL_OAUTH_CONNECTION_ID' => 'External OAuth Connection id',
+    'LBL_EXTERNAL_OAUTH_CONNECTION_NAME' => 'External OAuth Connection',
+    'LNK_EXTERNAL_OAUTH_CONNECTIONS' => 'External OAuth Connections',
+
+    'LBL_TYPE' => 'Tipas:',
+    'LBL_AUTH_TYPE' => 'Auth Type',
+    'LBL_IS_DEFAULT' => 'Numatytasis',
+    'LBL_SIGNATURE' => 'Parašas',
+
+    'LBL_OWNER_NAME' => 'Savininkas',
+
+    'LBL_SET_AS_DEFAULT_BUTTON' => 'Set as default',
+
+    'LBL_MOVE_MESSAGES_TO_TRASH_AFTER_IMPORT' => 'Move Messages To Trash After Import?',
+    'LBL_LAST_IMPORT_RUN_DATETIME' => 'Last Import Run Date/Time',
+    'LBL_MAILBOX_LAST_IMPORTED_DAYS' => 'Last Imported Date Per Mailbox',
+    'LBL_EMAIL_IMPORT_PER_RUN_THRESHOLD' => 'Emails to Import Per Run',
+    'LBL_EMAIL_IMPORT_TIMEFRAME_START' => 'Import Timeframe Start',
+    'LBL_EMAIL_IMPORT_UNREAD_ONLY' => 'Import Unread Emails Only'
+);
