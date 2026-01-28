224a5c0b7ff660eb4382edd689a94d4c22ce1b8c
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-28 19:13:23 +0000

    New translations en_us.lang.php (Chinese Traditional)

---
+++ translations/zh_TW/modules/ProjectTask/language/zh_TW.lang.php
@@ -0,0 +1,122 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_MODULE_NAME' => '專案工作',
+    'LBL_MODULE_TITLE' => '專案任務: 首頁',
+
+    'LBL_ID' => 'ID:',
+    'LBL_PROJECT_TASK_ID' => '專案工作編號:',
+    'LBL_PROJECT_ID' => '專案編號:',
+    'LBL_DATE_ENTERED' => '創建日期:',
+    'LBL_DATE_MODIFIED' => '修改日期:',
+    'LBL_ASSIGNED_USER_ID' => '負責人:',
+    'LBL_MODIFIED_USER_ID' => '修改人編號:',
+    'LBL_CREATED_BY' => '建立人:',
+    'LBL_NAME' => '名稱:',
+    'LBL_STATUS' => '狀態:',
+    'LBL_DATE_DUE' => '完成日期:',
+    'LBL_TIME_DUE' => '開始時間:',
+    'LBL_PREDECESSORS' => '前任',
+    'LBL_DATE_START' => '開始日期:',
+    'LBL_DATE_FINISH' => '完成日期:',
+    'LBL_TIME_START' => '開始時間:',
+    'LBL_TIME_FINISH' => '完成時間:',
+    'LBL_DURATION' => '期間:',
+    'LBL_DURATION_UNIT' => '期間單元:',
+    'LBL_ACTUAL_DURATION' => '實際期間:',
+    'LBL_PARENT_ID' => '專案:',
+    'LBL_PARENT_TASK_ID' => '父任務編號:',
+    'LBL_PERCENT_COMPLETE' => '%完成',
+    'LBL_PRIORITY' => '優先順序:',
+    'LBL_DESCRIPTION' => '備忘:',
+    'LBL_ORDER_NUMBER' => '排序:',
+    'LBL_TASK_NUMBER' => '任務編號:',
+    'LBL_TASK_ID' => '任務編號:',
+    'LBL_MILESTONE_FLAG' => '里程碑:',
+    'LBL_ESTIMATED_EFFORT' => '估計時間(小時):',
+    'LBL_ACTUAL_EFFORT' => '實際時間(小時):',
+    'LBL_UTILIZATION' => '使用率(%):',
+    'LBL_DELETED' => '已刪除:',
+    'LBL_IMPORT_PROJECT_TASKS' => '匯入專案',
+
+    'LBL_LIST_NAME' => '名稱',
+    'LBL_LIST_PARENT_NAME' => '專案',
+    'LBL_SEARCH_FORM_TITLE' => 'Project Task Search',
+    'LBL_LIST_PERCENT_COMPLETE' => '% 完成',
+    'LBL_LIST_STATUS' => '狀態',
+    'LBL_LIST_ASSIGNED_USER_ID' => '負責人',
+    'LBL_LIST_DATE_DUE' => '到期日期',
+    'LBL_LIST_PRIORITY' => '優先順序',
+    'LBL_LIST_CLOSE' => '結束',
+    'LBL_PROJECT_NAME' => '專案名稱',
+    'LBL_LIST_FORM_TITLE' => 'Project Task List',
+
+    'LNK_NEW_PROJECT' => '創建專案',
+    'LNK_PROJECT_LIST' => '專案清單',
+    'LNK_NEW_PROJECT_TASK' => '新增專案任務',
+    'LNK_PROJECT_TASK_LIST' => '專案工作',
+
+    'LBL_LIST_MY_PROJECT_TASKS' => '我的專案工作',
+    'LBL_DEFAULT_SUBPANEL_TITLE' => '專案工作',
+    'LBL_NEW_FORM_TITLE' => '新專案工作',
+
+    'LBL_HISTORY_TITLE' => '歷史',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => '銷售活動',
+    'LBL_HISTORY_SUBPANEL_TITLE' => '歷史',
+
+    'LBL_ASSIGNED_USER_NAME' => '負責人',
+    'LBL_PARENT_NAME' => '專案名稱',
+    'LBL_EDITLAYOUT' => '編輯佈局' /*for 508 compliance fix*/,
+    'LBL_PANEL_TIMELINE' => '時間軸',
+
+    'LBL_SUBTASK' => '子任務',
+    'LBL_LAG' => '延遲',
+    'LBL_DAYS' => '天',
+    'LBL_HOURS' => '時',
+    'LBL_RELATIONSHIP_TYPE' => '關係類型',
+
+    'LBL_ASCENDING' => '升序',
+    'LBL_DESCENDING' => '降序',
+);
