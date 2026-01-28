5801e48a3ccc30d40ed3ef56264c8411b57a1f3f
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-28 18:25:54 +0000

    New translations en_us.lang.php (Lithuanian)

---
+++ translations/lt_LT/modules/EAPM/language/lt_LT.lang.php
@@ -0,0 +1,103 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID' => 'Atsakingo Vartotojo Id',
+    'LBL_ASSIGNED_TO_NAME' => 'Sistemos vartotojas',
+    'LBL_ID' => 'Id',
+    'LBL_DATE_ENTERED' => 'Sukurta',
+    'LBL_DATE_MODIFIED' => 'Redagavimo data',
+    'LBL_MODIFIED' => 'Redagavo',
+    'LBL_MODIFIED_NAME' => 'Redaguotojo vardas',
+    'LBL_CREATED' => 'Sukūrė',
+    'LBL_DESCRIPTION' => 'Aprašymas',
+    'LBL_DELETED' => 'Ištrintas',
+    'LBL_NAME' => 'Pavadinimas',
+    'LBL_CREATED_USER' => 'Sukūrė',
+    'LBL_MODIFIED_USER' => 'Redagavo',
+    'LBL_LIST_NAME' => 'Vardas',
+    'LBL_LIST_FORM_TITLE' => 'Išorinių prisijungimų sąrašas',
+    'LBL_MODULE_NAME' => 'Išorinis prisijungimas',
+    'LBL_MODULE_TITLE' => 'Išoriniai prisijungimai',
+    'LBL_HOMEPAGE_TITLE' => 'Mano išoriniai prisijungimai',
+    'LNK_NEW_RECORD' => 'Sukurti išorinį prisijungimą',
+    'LNK_LIST' => 'Išoriniai prisijungimai',
+    'LBL_SEARCH_FORM_TITLE' => 'Ieškoti išorinių šaltinių',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Peržiūros istorija',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Įvykiai',
+    'LBL_NEW_FORM_TITLE' => 'Naujas išorinis prisijungimas',
+    'LBL_PASSWORD' => 'Slaptažodis',
+    'LBL_USER_NAME' => 'Pavadinimas',
+    'LBL_URL' => 'Tinklapio adresas',
+    'LBL_APPLICATION' => 'Aplikacija',
+    'LBL_API_DATA' => 'API duomenys',
+    'LBL_API_CONSKEY' => 'Vartotojo raktas',
+    'LBL_API_CONSSECRET' => 'Vartotojo slaptas raktas',
+    'LBL_API_OAUTHTOKEN' => 'OAuth Token',
+    'LBL_AUTH_UNSUPPORTED' => "Šis autentifikacijos metodas nepalaikomas",
+    'LBL_AUTH_ERROR' => 'Klaida prisijungimo metu: %s',
+    'LBL_VALIDATED' => 'Prisijungimas patvirtintas',
+    'LBL_ACTIVE' => 'Aktyvu',
+    'LBL_OAUTH_NAME' => '%s',
+    'LBL_CONNECT_BUTTON_TITLE' => 'Prisijungti',
+    'LBL_NOTE' => 'Atkreipkite dėmesį',
+    'LBL_CONNECTED' => 'Prisijungimas patvirtintas',
+
+    'LBL_ERR_NO_AUTHINFO' => 'Nėra autentifikavimo informacijos šiam prisijungimui.',
+    'LBL_ERR_NO_TOKEN' => 'There are no valid login tokens for this account.',
+
+    'LBL_ERR_FAILED_QUICKCHECK' => 'Jūs nesate prisijungęs {0} . Paspauskite "Gerai" norėdami dar kartą prisijungti ir aktyvuoti išorinį prisijungimą.',
+
+    'LBL_CLICK_TO_EDIT' => 'Click to Edit',
+
+    // Various strings used throughout the external account modules
+    'LBL_REAUTHENTICATE_LABEL' => 'Pakartotinis patikrinimas',
+    'LBL_APPLICATION_FOUND_NOTICE' => 'Šiai aplikacijai prisijungimas buvo atkurtas.',
+    'LBL_OMIT_URL' => '(Omit http:// or https://)',
+    'LBL_OAUTH_SAVE_NOTICE' => 'Click <b>Connect</b> to be directed to a page to provide your account information and to authorize access to the account by SuiteCRM. After connecting, you will be directed back to SuiteCRM.',
+    'LBL_BASIC_SAVE_NOTICE' => 'Click <b>Connect</b> to connect this account to SuiteCRM.',
+    'LBL_ERR_POPUPS_DISABLED' => 'Please enable browser popup windows or add an exception for website "{0}" to the exceptions list in order to connect.',
+
+    'LBL_API_OAUTHSECRET' => 'API OAuth Secret',
+);
