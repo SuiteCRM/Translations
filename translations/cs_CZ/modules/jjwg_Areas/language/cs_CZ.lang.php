9991a4cef720cf3419c161c461e5c68d6fae7191
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-28 19:46:37 +0000

    New translations en_us.lang.php (Czech)

---
+++ translations/cs_CZ/modules/jjwg_Areas/language/cs_CZ.lang.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings['LBL_ASSIGNED_TO_ID'] = 'ID přiřazeného uživatele';
+$mod_strings['LBL_ASSIGNED_TO_NAME'] = 'Uživatel';
+$mod_strings['LBL_ID'] = 'ID';
+$mod_strings['LBL_DATE_ENTERED'] = 'Datum vytvoření';
+$mod_strings['LBL_DATE_MODIFIED'] = 'Datum změny';
+$mod_strings['LBL_MODIFIED'] = 'Změnil';
+$mod_strings['LBL_MODIFIED_NAME'] = 'Změněno dle Jména';
+$mod_strings['LBL_CREATED'] = 'Vytvořeno';
+$mod_strings['LBL_DESCRIPTION'] = 'Popis';
+$mod_strings['LBL_DELETED'] = 'Vymazáno';
+$mod_strings['LBL_NAME'] = 'Název';
+$mod_strings['LBL_CREATED_USER'] = 'Vytvořeno dle Uživatele';
+$mod_strings['LBL_MODIFIED_USER'] = 'Změněno dle Uživatele';
+$mod_strings['LBL_LIST_NAME'] = 'Název';
+$mod_strings['LBL_LIST_FORM_TITLE'] = 'Seznam oblastí';
+$mod_strings['LBL_MODULE_NAME'] = 'Oblasti';
+$mod_strings['LBL_MODULE_TITLE'] = 'Oblasti';
+$mod_strings['LBL_HOMEPAGE_TITLE'] = 'Mé oblasti';
+$mod_strings['LNK_NEW_RECORD'] = 'Vytvořit oblasti';
+$mod_strings['LNK_LIST'] = 'Zobrazit oblasti';
+$mod_strings['LBL_SEARCH_FORM_TITLE'] = 'Vyhledat oblasti';
+$mod_strings['LBL_HISTORY_SUBPANEL_TITLE'] = 'Historie';
+$mod_strings['LBL_ACTIVITIES_SUBPANEL_TITLE'] = 'Aktivity';
+$mod_strings['LBL_NEW_FORM_TITLE'] = 'Nové oblasti';
+$mod_strings['LBL_CITY'] = 'Město';
+$mod_strings['LBL_STATE'] = 'Kraj';
+$mod_strings['LBL_COUNTRY'] = 'Kraj';
+$mod_strings['LBL_COORDINATES'] = 'Souřadnice';
+$mod_strings['LBL_LIST_ASSIGNED_USER'] = 'Uživatel';
+$mod_strings['LBL_AREA_MAP'] = 'Mapa oblasti';
+
+$mod_strings['LBL_AREA_EDIT_TITLE'] = 'Pokyny pro vytvoření oblasti:';
+$mod_strings['LBL_AREA_EDIT_DESC_1'] = 'Klepněte levým tlačítkem myši na mapu a po směru hodinových ručiček vytvořte požadovanou oblast.';
+$mod_strings['LBL_AREA_EDIT_DESC_2'] = 'Klepněte na první značku bodu pro uzavření mnohoúhelníkové oblasti.';
+$mod_strings['LBL_AREA_EDIT_RESET'] = 'Obnovit výchozí';
+$mod_strings['LBL_AREA_EDIT_USE_AREA_COORDINATES'] = 'Použít souřadnice oblasti';
+$mod_strings['LBL_AREA_EDIT_COORDINATE_RESULTS'] = 'Výsledky souřadnic (dél, šíř, elv):';
+$mod_strings['LBL_JJWG_MAPS_JJWG_AREAS_FROM_JJWG_MAPS_TITLE'] = 'Mapy';
