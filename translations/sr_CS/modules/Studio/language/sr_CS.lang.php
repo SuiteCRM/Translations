9690a5e5d8dc80aca00950e371500322c6d16739
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-28 19:30:19 +0000

    New translations en_us.lang.php (Serbian (Latin))

---
+++ translations/sr_CS/modules/Studio/language/sr_CS.lang.php
@@ -0,0 +1,150 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_EDIT_LAYOUT' => 'Izmeni izgled',
+    'LBL_EDIT_FIELDS' => 'Izmeni proizvoljna polja',
+    'LBL_SELECT_FILE' => 'Izaberite fajl',
+    'LBL_MODULE_TITLE' => 'Studio',
+    'LBL_TOOLBOX' => 'Set alata',
+    'LBL_SUITE_FIELDS_STAGE' => 'SuiteCRM polja (kliknite na elemente da bi ga dodali u oblast za postavljanje)',
+    'LBL_VIEW_SUITE_FIELDS' => 'Pregled SuiteCRM polja',
+    'LBL_FAILED_TO_SAVE' => 'Čuvanje nije uspelo',
+    'LBL_CONFIRM_UNSAVE' => 'Promene neće biti sačuvane. Da li želite da nastavite?',
+    'LBL_PUBLISHING' => 'Objavljivanje...',
+    'LBL_PUBLISHED' => 'Objavljen',
+    'LBL_FAILED_PUBLISHED' => 'Objavljivanje nije uspelo',
+    'LBL_DROP_HERE' => '[Spusti ovde]',
+
+//CUSTOM FIELDS
+    'LBL_NAME' => 'Ime',
+    'LBL_LABEL' => 'Oznaka',
+    'LBL_MASS_UPDATE' => 'Masovno ažuriranje',
+    'LBL_DEFAULT_VALUE' => 'Podrazumevana Vrednost',
+    'LBL_REQUIRED' => 'Zahtevano',
+    'LBL_DATA_TYPE' => 'Tip',
+
+
+    'LBL_HISTORY' => 'Istorija',
+
+//WIZARDS
+
+//STUDIO WIZARD
+    'LBL_SW_WELCOME' => '<h2>Dobrodošli u Studio!</h2><br> Šta želite danas da radite?<br><b> Molim, izaberite iz opcija ispod.</b>',
+    'LBL_SW_EDIT_MODULE' => 'Izmeni modul',
+    'LBL_SW_EDIT_DROPDOWNS' => 'Izmeni padajuće liste',
+    'LBL_SW_EDIT_TABS' => 'Konfiguriši jezičke',
+    'LBL_SW_RENAME_TABS' => 'Promeni ime jezičaka',
+    'LBL_SW_EDIT_GROUPTABS' => 'Konfiguriši jezičke grupe',
+    'LBL_SW_EDIT_PORTAL' => 'Izmeni portal',
+    'LBL_SW_REPAIR_CUSTOMFIELDS' => 'Popravi proizvoljna polja',
+    'LBL_SW_MIGRATE_CUSTOMFIELDS' => 'Premesti prilagođena polja',
+
+// JS LABELS
+    'LBL_REBUILD_JAVASCRIPT_LANG_DESC_SHORT' => 'Rekonstruiše javascript verzije fajlova jezika',
+    'LBL_REBUILD_JAVASCRIPT_LANG_DESC' => 'Uklanjanje javascript verzija jezičkih fajlova, rekonstruisaće se kada bude potrebno.',
+
+
+//Manager Backups History
+    'LBL_MB_DELETE' => 'Obriši',
+
+//EDIT DROP DOWNS
+    'LBL_ED_CREATE_DROPDOWN' => 'Napravi padajući meni',
+    'LBL_DROPDOWN_NAME' => 'Ime padajućeg menija:',
+    'LBL_DROPDOWN_LANGUAGE' => 'Jezik padajućeg menija:',
+    'LBL_TABGROUP_LANGUAGE' => 'Jezik:',
+
+//END WIZARDS
+
+//DROP DOWN EDITOR
+    'LBL_DD_DISPALYVALUE' => 'Prikaži vrednost',
+    'LBL_DD_DATABASEVALUE' => 'Vrednost baze',
+    'LBL_DD_ALL' => 'Svi',
+
+//BUTTONS
+    'LBL_BTN_SAVE' => 'Sačuvaj',
+    'LBL_BTN_CANCEL' => 'Otkaži',
+    'LBL_BTN_SAVEPUBLISH' => 'Sačuvaj i rasporedi',
+    'LBL_BTN_HISTORY' => 'Istorija',
+    'LBL_BTN_ADDROWS' => 'Dodaj redove',
+    'LBL_BTN_UNDO' => 'Poništi',
+    'LBL_BTN_REDO' => 'Vrati poništeno',
+    'LBL_BTN_ADDCUSTOMFIELD' => 'Dodaj prilagođeno polje',
+    'LBL_BTN_TABINDEX' => 'Izmeni redosled obeležavanja',
+
+//TABS
+    'LBL_MODULES' => 'Moduli',
+    'LBL_MODULE_NAME' => 'Administracija',
+    'LBL_CONFIGURE_GROUP_TABS' => 'Konfiguriši grupne module',
+    'LBL_GROUP_TAB_WELCOME' => 'Izgled grupnog jezička ispod koristiće se svaki put kada korisnik odabere da koristi Grupne jezičke umesto uobičajenog modula Jezičci u Moj nalog>Opcije izgleda.',
+    'LBL_RENAME_TAB_WELCOME' => 'U tabeli ispod kliknite na bilo koju prikazanu vrednost jezička da bi  promenili ime jezička.',
+    'LBL_DELETE_MODULE' => 'Remove&nbsp;module<br />from&nbsp;filter',
+    'LBL_TAB_GROUP_LANGUAGE_HELP' => 'Za podešavanje natpisa grupa jezičaka na drugim dostupnim jezicima, odaberite jezik, ispravite natpis i kliknite na opciju \"Save and Deploy\" da napravite promene za taj jezik.',
+    'LBL_ADD_GROUP' => 'Dodaj filtеr',
+    'LBL_NEW_GROUP' => 'Nova grupa',
+    'LBL_RENAME_TABS' => 'Promeni ime kartice',
+
+//ERRORS
+    'ERROR_INVALID_KEY_VALUE' => "Greška: Neispravna vrednost ključa: [&#39;]",
+
+//SUGAR PORTAL
+    'LBL_SAVE' => 'Sačuvaj' /*for 508 compliance fix*/,
+    'LBL_UNDO' => 'Poništi' /*for 508 compliance fix*/,
+    'LBL_REDO' => 'Vrati poništeno' /*for 508 compliance fix*/,
+    'LBL_INLINE' => 'tokomunosa' /*for 508 compliance fix*/,
+    'LBL_DELETE' => 'Obriši' /*for 508 compliance fix*/,
+    'LBL_ADD_FIELD' => 'Dodaj polje' /*for 508 compliance fix*/,
+    'LBL_MAXIMIZE' => 'Maksimizuj' /*for 508 compliance fix*/,
+    'LBL_MINIMIZE' => 'Minimizuj' /*for 508 compliance fix*/,
+    'LBL_PUBLISH' => 'Objavi' /*for 508 compliance fix*/,
+    'LBL_ADDROWS' => 'Dodaj redove' /*for 508 compliance fix*/,
+    'LBL_ADDFIELD' => 'Dodaj polje' /*for 508 compliance fix*/,
+    'LBL_EDIT' => 'Izmeni' /*for 508 compliance fix*/,
+
+    'LBL_LANGUAGE_TOOLTIP' => 'Izaberite jezik za izmenu',
+    'LBL_SINGULAR' => 'Naziv u jednini',
+    'LBL_PLURAL' => 'Naziv u množini',
+    'LBL_RENAME_MOD_SAVE_HELP' => 'Pritisni <b>Sačuvaj</b> da primenite promene.'
+
+);
