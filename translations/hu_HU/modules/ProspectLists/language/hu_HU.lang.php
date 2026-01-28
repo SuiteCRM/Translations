e591c47225b7e3b80eb6abbd6fd573eadd3a6e78
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-28 19:14:10 +0000

    New translations en_us.lang.php (Hungarian)

---
+++ translations/hu_HU/modules/ProspectLists/language/hu_HU.lang.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_MODULE_NAME' => 'Céllista',
+    'LBL_MODULE_ID' => 'Céllista',
+    'LBL_MODULE_TITLE' => 'Prospectlisták: Home',
+    'LBL_SEARCH_FORM_TITLE' => 'Prospectlisták keresése',
+    'LBL_LIST_FORM_TITLE' => 'Céllista',
+    'LBL_PROSPECT_LIST_NAME' => 'Név:',
+    'LBL_NAME' => 'Név',
+    'LBL_ENTRIES' => 'Összes bejegyzések: ',
+    'LBL_LIST_PROSPECT_LIST_NAME' => 'Prospect lista',
+    'LBL_LIST_ENTRIES' => 'Prospectek a listában',
+    'LBL_LIST_DESCRIPTION' => 'Leírás',
+    'LBL_LIST_TYPE_NO' => 'Típus',
+    'LBL_LIST_END_DATE' => 'Végdátum',
+    'LBL_DATE_ENTERED' => 'Létrehozás dátuma',
+    'LBL_MARKETING_ID' => 'Marketing ID',
+    'LBL_DATE_MODIFIED' => 'Módosítás dátuma',
+    'LBL_MODIFIED' => 'Módosította:',
+    'LBL_CREATED' => 'Létrehozta:',
+    'LBL_ASSIGNED_TO' => 'Felelős',
+    'LBL_DESCRIPTION' => 'Leírás',
+    'LNK_NEW_CAMPAIGN' => 'Kampány létrehozása',
+    'LNK_CAMPAIGN_LIST' => 'Kampányok',
+    'LNK_NEW_PROSPECT_LIST' => 'Céllista létrehozása',
+    'LNK_PROSPECT_LIST_LIST' => 'Céllista megtekintése',
+    'LBL_MODIFIED_BY' => 'Módosította:',
+    'LBL_CREATED_BY' => 'Létrehozta:',
+    'LNK_NEW_PROSPECT' => 'Cél létrehozása',
+    'LNK_PROSPECT_LIST' => 'Célcsoport',
+
+    'LBL_CONTACTS_SUBPANEL_TITLE' => 'Kapcsolattartók',
+    'LBL_LEADS_SUBPANEL_TITLE' => 'Érdeklődők',
+    'LBL_PROSPECTS_SUBPANEL_TITLE' => 'Célcsoport',
+    'LBL_ACCOUNTS_SUBPANEL_TITLE' => 'Ügyfelek',
+    'LBL_COPY_PREFIX' => 'Ennek a másolata: ',
+    'LBL_USERS_SUBPANEL_TITLE' => 'Felhasználók',
+    'LBL_TYPE' => 'Típus',
+    'LBL_LIST_TYPE' => 'Típus',
+    'LBL_LIST_TYPE_LIST_NAME' => 'Típus',
+    'LBL_NEW_FORM_TITLE' => 'Új Prospect lista',
+    'LBL_MARKETING_NAME' => 'Marketingnév',
+    'LBL_DOMAIN_NAME' => 'Domain név',
+    'LBL_DOMAIN' => 'Ne küld emailt a domain-re:',
+    'LBL_LIST_PROSPECTLIST_NAME' => 'Név',
+
+    'LBL_EMAIL_MARKETING' => 'E-mail Marketing',
+
+    'LBL_ASCENDING' => 'Növekvő',
+    'LBL_DESCENDING' => 'Csökkenő',
+);
