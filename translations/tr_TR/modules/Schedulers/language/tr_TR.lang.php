ac100807a7ca57f14d0fe2f6e7b113f412a7822f
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-28 19:23:43 +0000

    New translations en_us.lang.php (Turkish)

---
+++ translations/tr_TR/modules/Schedulers/language/tr_TR.lang.php
@@ -0,0 +1,170 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+// OOTB Scheduler Job Names:
+    'LBL_OOTB_WORKFLOW' => 'İş akışı görevlerini işle',
+    'LBL_OOTB_REPORTS' => 'Zamanlanmış rapor üretme görevlerini yürüt',
+    'LBL_OOTB_IE' => 'Gelen e-posta kutularını denetle',
+    'LBL_OOTB_BOUNCE' => 'Gecelik geri dönen kampanya e-postası işleme görevini çalıştır',
+    'LBL_OOTB_CAMPAIGN' => 'Gecelik toplu e-posta kampanyaları görevini çalıştır',
+    'LBL_OOTB_PRUNE' => 'Ayın 1. gününde veri tabanını temizle',
+    'LBL_OOTB_TRACKER' => 'İzleyici tablolarını temizle',
+    'LBL_OOTB_SUITEFEEDS' => 'SuiteCRM akış tablolarını temizle',
+    'LBL_OOTB_LUCENE_INDEX' => 'Lucene Dizinini Oluştur',
+    'LBL_OOTB_OPTIMISE_INDEX' => 'AOD Dizinini İyileştir',
+    'LBL_OOTB_SEND_EMAIL_REMINDERS' => 'E-posta anımsatıcı bildirimlerini çalıştır',
+    'LBL_OOTB_CLEANUP_QUEUE' => 'Görev kuyruğunu temizle',
+    'LBL_OOTB_REMOVE_DOCUMENTS_FROM_FS' => 'Belgelerin dosya sisteminden kaldırılması',
+    'LBL_OOTB_GOOGLE_CAL_SYNC' => 'Google Takvim eşitleme',
+    'LBL_OOTB_ELASTIC_INDEX' => 'Elasticsearch dizini oluştur',
+    'LBL_OOTB_CAL_ACC_SYNC' => 'Calendar Accounts Sync',
+
+// List Labels
+    'LBL_LIST_JOB_INTERVAL' => 'Aralık:',
+    'LBL_LIST_LIST_ORDER' => 'Zamanlanmış görevler:',
+    'LBL_LIST_NAME' => 'Zamanlanmış görev:',
+    'LBL_LIST_RANGE' => 'Aralık:',
+    'LBL_LIST_STATUS' => 'Durum:',
+    'LBL_LIST_TITLE' => 'Zamanlanmış görev listesi:',
+// human readable:
+    'LBL_SUN' => 'Pazar',
+    'LBL_MON' => 'Pazartesi',
+    'LBL_TUE' => 'Salı',
+    'LBL_WED' => 'Çarşamba',
+    'LBL_THU' => 'Perşembe',
+    'LBL_FRI' => 'Cuma',
+    'LBL_SAT' => 'Cumartesi',
+    'LBL_ALL' => 'Her gün',
+    'LBL_EVERY' => 'Her',
+    'LBL_FROM' => 'Kimden',
+    'LBL_ON_THE' => 'Şu tarihte',
+    'LBL_RANGE' => 'kime',
+    'LBL_AND' => 've',
+    'LBL_MINUTES' => 'dakika',
+    'LBL_HOUR' => 'saat',
+    'LBL_HOUR_SING' => 'saat',
+    'LBL_OFTEN' => 'Olabildiğince sık.',
+    'LBL_MIN_MARK' => 'dakika işareti',
+
+
+// crontabs
+    'LBL_MINS' => 'dak',
+    'LBL_HOURS' => 'sa',
+    'LBL_DAY_OF_MONTH' => 'tarih',
+    'LBL_MONTHS' => 'ay',
+    'LBL_DAY_OF_WEEK' => 'gün',
+    'LBL_CRONTAB_EXAMPLES' => 'Yukarıda standart zamanlanmış görev gösterimi kullanılır.',
+// Labels
+    'LBL_ALWAYS' => 'Her zaman',
+    'LBL_CATCH_UP' => 'Atlanmış ise çalıştır',
+    'LBL_CATCH_UP_WARNING' => 'Bu görev bir saniyeden daha uzun sürecek ise işareti kaldırın.',
+    'LBL_DATE_TIME_END' => 'Bitiş tarihi ve saati',
+    'LBL_DATE_TIME_START' => 'Başlangıç tarihi ve saati',
+    'LBL_INTERVAL' => 'Aralık',
+    'LBL_JOB' => 'Görev',
+    'LBL_JOB_URL' => 'Görev adresi',
+    'LBL_LAST_RUN' => 'Son başarılı çalışma',
+    'LBL_MODULE_NAME' => 'SuiteCRM zamanlanmış görev',
+    'LBL_MODULE_TITLE' => 'Zamanlanmış görevler',
+    'LBL_NAME' => 'Görev adı',
+    'LBL_NEVER' => 'Asla',
+    'LBL_NEW_FORM_TITLE' => 'Yeni zamanlama',
+    'LBL_PERENNIAL' => 'sürekli',
+    'LBL_SEARCH_FORM_TITLE' => 'Zamanlanmış görev arama',
+    'LBL_SCHEDULER' => 'Zamanlanmış görev:',
+    'LBL_STATUS' => 'Durum',
+    'LBL_TIME_FROM' => 'Etkinleştirme başlangıcı',
+    'LBL_TIME_TO' => 'Etkinleştirme bitişi',
+    'LBL_WARN_CURL_TITLE' => 'cURL uyarısı:',
+    'LBL_WARN_CURL' => 'Uyarı:',
+    'LBL_WARN_NO_CURL' => 'Bu sistemdeki PHP modülünde cURL kitaplıkları etkinleştirilmemiş ya da derlenmemiş (--with-curl=/path/to/curl_library). Bu sorunun çözümü için sistem yöneticisi ile görüşün. Zamanlanmış görevler cURL işlevi olmadan çalıştırılamaz.',
+    'LBL_BASIC_OPTIONS' => 'Temel kurulum',
+    'LBL_ADV_OPTIONS' => 'Gelişmiş ayarlar',
+    'LBL_TOGGLE_ADV' => 'Gelişmiş ayarları görüntüle',
+    'LBL_TOGGLE_BASIC' => 'Temel ayarları görüntüle',
+// Links
+    'LNK_LIST_SCHEDULER' => 'Zamanlanmış görevler',
+    'LNK_NEW_SCHEDULER' => 'Zamanlanmış görev ekle',
+// Messages
+    'ERR_CRON_SYNTAX' => 'Zamanlanmış görev söz dizimi geçersiz',
+    'NTC_LIST_ORDER' => 'Bu zamanlanmış görevin, zamanlanmış görevler açılan kutusu listesinde görüntülenmesini istediğiniz sırayı belirtin',
+    'LBL_CRON_INSTRUCTIONS_WINDOWS' => 'Windows zamanlanmış görevi kurmak için',
+    'LBL_CRON_INSTRUCTIONS_LINUX' => 'Zamanlanmış görev kurulumu için',
+    'LBL_CRON_LINUX_DESC1' => 'SuiteCRM zamanlanmış görevlerini çalıştırmak için, şu komutu kullanarak site sunucunuzun kullanıcı crontab dosyasını düzenlemek üzere açın:',
+    'LBL_CRON_LINUX_DESC2' => '... ve crontab dosyasına şu satırı ekleyin:',
+    'LBL_CRON_LINUX_DESC3' => 'Bu işlemi yalnızca kurulum tamamlandıktan sonra yapmalısınız.',
+    'LBL_CRON_WINDOWS_DESC' => 'SuiteCRM zamanlanmış görevlerini çalıştırmak için Windows zamanlanmış görevlerini kullanarak çalıştırılacak bir toplu işlem dosyası oluşturun. Toplu işlem dosyasında şu komutlar bulunmalıdır:',
+// Subpanels
+    'LBL_JOBS_SUBPANEL_TITLE' => 'Görev günlüğü',
+    'LBL_EXECUTE_TIME' => 'Çalıştırılma zamanı',
+
+//jobstrings
+    'LBL_REFRESHJOBS' => 'Görevleri yenile',
+    'LBL_POLLMONITOREDINBOXES' => 'Gelen e-posta hesaplarını denetle',
+    'LBL_PERFORMFULLFTSINDEX' => 'Tam metin arama dizin sistemi',
+
+    'LBL_RUNMASSEMAILCAMPAIGN' => 'Gecelik toplu e-posta kampanyaları görevini çalıştır',
+    'LBL_POLLMONITOREDINBOXESFORBOUNCEDCAMPAIGNEMAILS' => 'Gecelik geri dönen kampanya e-postası işleme görevini çalıştır',
+    'LBL_PRUNEDATABASE' => 'Ayın 1. gününde veri tabanını temizle',
+    'LBL_TRIMTRACKER' => 'İzleyici tablolarını temizle',
+    'LBL_TRIMSUGARFEEDS' => 'SuiteCRM akış tablolarını temizle',
+    'LBL_SENDEMAILREMINDERS' => 'E-posta anımsatıcılarını gönder',
+    'LBL_CLEANJOBQUEUE' => 'Görev kuyruğunu temizle',
+    'LBL_REMOVEDOCUMENTSFROMFS' => 'Belgelerin dosya sisteminden kaldırılması',
+
+    'LBL_AODOPTIMISEINDEX' => 'Advanced OpenDiscovery Dizinini İyileştir',
+    'LBL_AODINDEXUNINDEXED' => 'Dizine eklenmemiş belgeleri dizine ekle',
+    'LBL_POLLMONITOREDINBOXESAOP' => 'AOP anket izleme gelen kutuları',
+    'LBL_AORRUNSCHEDULEDREPORTS' => 'Zamanlanmış raporları yürüt',
+    'LBL_PROCESSAOW_WORKFLOW' => 'AOW iş akışını işle',
+
+    'LBL_RUNELASTICSEARCHINDEXERSCHEDULER' => 'Elasticsearch dizin oluşturucu',
+    'LBL_CALENDARSYNCJOB' => 'Sync Calendar Accounts',
+
+    'LBL_SCHEDULER_TIMES' => 'Zamanlanmış görev zamanları',
+    'LBL_SYNCGOOGLECALENDAR' => 'Google takvimlerini eşitle',
+);
+
+global $sugar_config;
