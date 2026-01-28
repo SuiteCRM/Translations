6d73e72fa0368eececf86f3affa2ff103582acf1
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-28 18:31:47 +0000

    New translations en_us.lang.php (Persian)

---
+++ translations/fa_IR/modules/EmailTemplates/language/fa_IR.lang.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ADD_DOCUMENT' => 'افزودن سند',
+    'LBL_ADD_FILE' => 'افزودن یک فایل',
+    'LBL_ATTACHMENTS' => 'پیوست‌ها',
+    'LBL_BODY' => 'متن اصلی: ',
+    'LBL_CLOSE' => 'بستن:',
+    'LBL_DESCRIPTION' => 'توضیحات:',
+    'LBL_EDIT_ALT_TEXT' => 'ویرایش متن ساده',
+    'LBL_EMAIL_ATTACHMENT' => 'پیوست ایمیل',
+    'LBL_HIDE_ALT_TEXT' => 'پنهان کردن متن ساده',
+    'LBL_HTML_BODY' => 'متن Html',
+    'LBL_INSERT_VARIABLE' => 'متغیر را وارد کنید:',
+    'LBL_INSERT_URL_REF' => 'درج آدرس مرجع',
+    'LBL_INSERT_TRACKER_URL' => 'Insert Tracker URL:',
+    'LBL_INSERT' => 'درج',
+    'LBL_LIST_DATE_MODIFIED' => 'آخرین ویرایش',
+    'LBL_LIST_DESCRIPTION' => 'توضیحات',
+    'LBL_LIST_FORM_TITLE' => 'لیست قالب های ایمیل',
+    'LBL_LIST_NAME' => 'نام',
+    'LBL_MODULE_NAME' => 'قالب‌های ایمیل',
+    'LBL_MODULE_TITLE' => 'قالب های ایمیل: صفحه اصلی',
+    'LBL_NAME' => 'نام:',
+    'LBL_NEW_FORM_TITLE' => 'ایجاد قالب ایمیل',
+    'LBL_PUBLISH' => 'انتشار:',
+    'LBL_RELATED_TO' => 'مربوط به:',
+    'LBL_SEARCH_FORM_TITLE' => 'Email Templates Search',
+    'LBL_SHOW_ALT_TEXT' => 'نمایش متن ساده',
+    'LBL_SUBJECT' => 'موضوع:',
+    'LBL_SUITE_DOCUMENT' => 'سند',
+    'LBL_UPLOAD_FILE' => 'Upload File',
+    'LBL_TEXT_BODY' => 'بدنه متن',
+    'LBL_USERS' => 'کاربرها',
+
+    'LNK_EMAIL_TEMPLATE_LIST' => 'مشاهده قالب ایمیل',
+    'LNK_IMPORT_NOTES' => 'درون‌ریزی یادداشت‌ها',
+    'LNK_NEW_EMAIL_TEMPLATE' => 'ایجاد قالب ایمیل',
+    'LNK_NEW_EMAIL' => 'آرشیو ایمیل',
+    'LNK_NEW_SEND_EMAIL' => 'نوشتن ایمیل',
+    'LNK_SENT_EMAIL_LIST' => 'ایمیل های ارسال شده',
+    'LNK_VIEW_CALENDAR' => 'امروز',
+    // for Inbox
+    'LBL_NEW' => 'جدید',
+    'LNK_MY_DRAFTS' => 'پیش نویس های من',
+    'LNK_MY_INBOX' => 'ایمیل من',
+    'LBL_TEXT_ONLY' => 'فقط متن',
+    'LBL_SEND_AS_TEXT' => 'فرستادن فقط متن',
+    'LBL_ACCOUNT' => 'مرکز',
+    'LBL_FROM_NAME' => 'نام فرستنده',
+    'LBL_PLAIN_TEXT' => 'متن ساده',
+    'LBL_CREATED_BY' => 'ایجاد کننده',
+    'LBL_PUBLISHED' => 'منتشرشده',
+    'LNK_VIEW_MY_INBOX' => 'مشاهده ایمیل من',
+    'LBL_ASSIGNED_TO_ID' => 'اختصاص داده شده به',
+    'LBL_EDIT_LAYOUT' => 'ویرایش چیدمان' /*for 508 compliance fix*/,
+    'LBL_SELECT' => 'انتخاب' /*for 508 compliance fix*/,
+    'LBL_ID_FF_CLEAR' => 'پاکسازی' /*for 508 compliance fix*/,
+    'LBL_TYPE' => 'نوع',
+    'LBL_WIDTH' => 'Width Default',
+    'LNK_IMPORT_CAMPAIGNS' => 'Import Campaign',
+);
