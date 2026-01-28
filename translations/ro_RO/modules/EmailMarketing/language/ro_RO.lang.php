6b9624ee6e48fae8b5a913d5cdaa4f668cfd037a
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-28 18:29:20 +0000

    New translations en_us.lang.php (Romanian)

---
+++ translations/ro_RO/modules/EmailMarketing/language/ro_RO.lang.php
@@ -0,0 +1,108 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_REPLY_ADDR' => 'Adresa de răspuns: ',
+    'LBL_REPLY_NAME' => 'Nume expeditor: ',
+
+    'LBL_MODULE_NAME' => 'Marketing prin Email',
+    'LBL_MODULE_TITLE' => 'Marketing prin email: Start',
+    'LBL_LIST_FORM_TITLE' => 'Campanii de marketing prin email',
+    'LBL_NAME' => 'Nume: ',
+    'LBL_LIST_NAME' => 'Nume',
+    'LBL_LIST_FROM_ADDR' => 'De la adresa',
+    'LBL_LIST_DATE_START' => 'Dată Început',
+    'LBL_LIST_TEMPLATE_NAME' => 'Șablon e-mail',
+    'LBL_LIST_STATUS' => 'Status',
+    'LBL_STATUS' => 'Status',
+    'LBL_STATUS_TEXT' => 'Status:',
+    'LBL_TEMPLATE_NAME' => 'Nume Șablon',
+    'LBL_DATE_ENTERED' => 'Data de începere',
+    'LBL_DATE_MODIFIED' => 'Data modificării',
+    'LBL_MODIFIED' => 'Modificat de: ',
+    'LBL_CREATED' => 'Creat de: ',
+    'LBL_MESSAGE_FOR' => 'Trimite acest mesaj către:',
+
+    'LBL_FROM_NAME' => 'De la nume: ',
+    'LBL_FROM_ADDR' => 'De la adresa de e-mail: ',
+    'LBL_DATE_START' => 'Dată Început',
+    'LBL_TIME_START' => 'Momentul începerii',
+    'LBL_START_DATE_TIME' => 'Data și ora de început: ',
+    'LBL_TEMPLATE' => 'Șablon de e-mail: ',
+
+    'LBL_MODIFIED_BY' => 'Modificat de: ',
+    'LBL_CREATED_BY' => 'Creat de: ',
+
+    'LNK_NEW_CAMPAIGN' => 'Creare campanie',
+    'LNK_CAMPAIGN_LIST' => 'Campanii',
+    'LNK_NEW_PROSPECT_LIST' => 'Creează listă de ținte',
+    'LNK_PROSPECT_LIST_LIST' => 'Liste de ținte',
+    'LNK_NEW_PROSPECT' => 'Creare țintă',
+    'LNK_PROSPECT_LIST' => 'Ținte',
+    'LBL_DEFAULT_SUBPANEL_TITLE' => 'Marketing prin Email',
+    'LBL_CREATE_EMAIL_TEMPLATE' => 'Creați',
+    'LBL_EDIT_EMAIL_TEMPLATE' => 'Editați',
+    'LBL_FROM_MAILBOX' => 'Din căsuța poștală',
+    'LBL_FROM_MAILBOX_NAME' => 'Folosește cutia poștală:',
+    'LBL_OUTBOUND_EMAIL_ACCOUNT_NAME' => 'Cont de e-mail de ieșire:',
+    'LBL_PROSPECT_LIST_SUBPANEL_TITLE' => 'Liste de ținte',
+    'LBL_ALL_PROSPECT_LISTS' => 'Selectați pentru a alege lista (listele) țintă din campanie.',
+    'LBL_RELATED_PROSPECT_LISTS' => 'Lista (listele) țintă legate de acest mesaj.',
+    'LBL_PROSPECT_LIST_NAME' => 'Numele listei țintă',
+
+    'LBL_LIST_PROSPECT_LIST_NAME' => 'Liste vizate',
+    'LBL_MODULE_SEND_TEST' => 'Campanie: Trimite test',
+    'LBL_MODULE_SEND_EMAILS' => 'Campanie: Trimite e-mailuri',
+    'LBL_SCHEDULE_MESSAGE_TEST' => 'Vă rugăm să selectați mesajele campaniei pe care doriți să le testați:',
+    'LBL_SCHEDULE_MESSAGE_EMAILS' => 'Vă rugăm să selectați mesajele din campanie pe care doriți să le programați pentru distribuție la data și ora specificate:',
+    'LBL_SCHEDULE_BUTTON_TITLE' => 'Trimite',
+    'LBL_SCHEDULE_BUTTON_LABEL' => 'Trimite',
+    'LBL_ERROR_ON_MARKETING' => 'Lipsă câmp (câmpuri) necesare',
+
+    'LBL_CAMPAIGN_ID' => 'ID campanie',
+    'LBL_OUTBOUND_EMAIL_ACOUNT_ID' => 'ID cont de email de ieșire',
+    'LBL_EMAIL_TEMPLATE' => 'Șablon e-mail',
+    'LBL_PROSPECT_LISTS' => 'Liste de prospecte',
+
+);
