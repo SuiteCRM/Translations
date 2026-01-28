324d51948ca1b9c98d50caf7ce0962f4ca8a63cf
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-28 19:36:12 +0000

    New translations en_us.lang.php (Portuguese)

---
+++ translations/pt_PT/modules/SurveyResponses/language/pt_PT.lang.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID'                                                             => 'ID do utilizador atribuído',
+    'LBL_ASSIGNED_TO_NAME'                                                           => 'Atribuído a',
+    'LBL_SECURITYGROUPS'                                                             => 'Grupos de segurança',
+    'LBL_SECURITYGROUPS_SUBPANEL_TITLE'                                              => 'Grupos de segurança',
+    'LBL_ID'                                                                         => 'ID',
+    'LBL_DATE_ENTERED'                                                               => 'Data de criação',
+    'LBL_DATE_MODIFIED'                                                              => 'Data de alteração',
+    'LBL_MODIFIED'                                                                   => 'Alterado por',
+    'LBL_MODIFIED_ID'                                                                => 'Alterado por (ID)',
+    'LBL_MODIFIED_NAME'                                                              => 'Alterado por (nome)',
+    'LBL_CREATED'                                                                    => 'Criado por',
+    'LBL_CREATED_ID'                                                                 => 'ID do autor',
+    'LBL_DESCRIPTION'                                                                => 'Descrição',
+    'LBL_DELETED'                                                                    => 'Eliminado',
+    'LBL_NAME'                                                                       => 'Nome',
+    'LBL_CREATED_USER'                                                               => 'Criado pelo utilizador',
+    'LBL_MODIFIED_USER'                                                              => 'Alterado pelo utilizador',
+    'LBL_LIST_NAME'                                                                  => 'Nome',
+    'LBL_EDIT_BUTTON'                                                                => 'Editar',
+    'LBL_REMOVE'                                                                     => 'Remover',
+    'LBL_LIST_FORM_TITLE'                                                            => 'Lista de respostas de inquérito',
+    'LBL_MODULE_NAME'                                                                => 'Respostas de inquérito',
+    'LBL_MODULE_TITLE'                                                               => 'Respostas de inquérito',
+    'LBL_HOMEPAGE_TITLE'                                                             => 'Minhas respostas de inquérito',
+    'LNK_NEW_RECORD'                                                                 => 'Criar respostas para inquérito',
+    'LNK_LIST'                                                                       => 'Ver respostas para inquérito',
+    'LNK_IMPORT_SURVEYRESPONSES'                                                     => 'Importar respostas para inquérito',
+    'LBL_SEARCH_FORM_TITLE'                                                          => 'Pesquisar respostas para inquérito',
+    'LBL_HISTORY_SUBPANEL_TITLE'                                                     => 'Ver histórico',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE'                                                  => 'Atividades',
+    'LBL_SURVEYRESPONSES_SUBPANEL_TITLE'                                             => 'Respostas de inquérito',
+    'LBL_NEW_FORM_TITLE'                                                             => 'Novas respostas para inquérito',
+    'LBL_HAPPINESS'                                                                  => 'Satisfação',
+    'LBL_EMAIL_RESPONSE_SENT'                                                        => 'Email de resposta enviado',
+    'LBL_SURVEYRESPONSES_SURVEYQUESTIONRESPONSES_FROM_SURVEYQUESTIONRESPONSES_TITLE' => 'Respostas a pergunta de inquérito',
+    'LBL_SURVEYRESPONSES_CONTACTS_FROM_CONTACTS_TITLE'                               => 'Contacto',
+    'LBL_SURVEYRESPONSES_ACCOUNTS_FROM_ACCOUNTS_TITLE'                               => 'Conta',
+    'LBL_SURVEYS_SURVEYRESPONSES_FROM_SURVEYS_TITLE'                                 => 'Inquérito',
+    'LBL_QUESTION_RESPONSES_DISPLAY'                                                 => 'Respostas a pergunta',
+    'LBL_SURVEYRESPONSES_CAMPAIGNS_FROM_CAMPAIGNS_TITLE'                             => 'Campanha',
+    'LBL_QUESTION' => 'Pergunta',
+    'LBL_RESPONSE' => 'Resposta'
+
+);
