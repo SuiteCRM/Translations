db8f5c5dcda086b19190fdc8ca268b7b03260b88
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-28 19:49:54 +0000

    New translations en_us.lang.php (Estonian)

---
+++ translations/et_EE/modules/jjwg_Markers/language/et_EE.lang.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings['LBL_ASSIGNED_TO_ID'] = 'Määratud kasutaja Id';
+$mod_strings['LBL_ASSIGNED_TO_NAME'] = 'Kasutaja';
+$mod_strings['LBL_ID'] = 'ID';
+$mod_strings['LBL_DATE_ENTERED'] = 'Loomiskuupäev';
+$mod_strings['LBL_DATE_MODIFIED'] = 'Muutmiskuupäev';
+$mod_strings['LBL_MODIFIED'] = 'Muutja';
+$mod_strings['LBL_MODIFIED_NAME'] = 'Muutja nime järgi';
+$mod_strings['LBL_CREATED'] = 'Loodud';
+$mod_strings['LBL_DESCRIPTION'] = 'Kirjeldus';
+$mod_strings['LBL_DELETED'] = 'Kustutatud';
+$mod_strings['LBL_NAME'] = 'Nimi';
+$mod_strings['LBL_CREATED_USER'] = 'Looja';
+$mod_strings['LBL_MODIFIED_USER'] = 'Muutja';
+$mod_strings['LBL_LIST_NAME'] = 'Nimi';
+$mod_strings['LBL_LIST_FORM_TITLE'] = 'Markerite List';
+$mod_strings['LBL_MODULE_NAME'] = 'Markerid';
+$mod_strings['LBL_MODULE_TITLE'] = 'Markerid';
+$mod_strings['LBL_HOMEPAGE_TITLE'] = 'Minu markerid';
+$mod_strings['LNK_NEW_RECORD'] = 'Loo Markerid';
+$mod_strings['LNK_LIST'] = 'Vaata markereid';
+$mod_strings['LNK_IMPORT_JJWG_MARKERS'] = 'Impordi Markerid';
+$mod_strings['LBL_SEARCH_FORM_TITLE'] = 'Otsi Markereid';
+$mod_strings['LBL_HISTORY_SUBPANEL_TITLE'] = 'Vaata ajalugu';
+$mod_strings['LBL_ACTIVITIES_SUBPANEL_TITLE'] = 'Tegevused';
+$mod_strings['LBL_NEW_FORM_TITLE'] = 'Uued Markerid';
+$mod_strings['LBL_CITY'] = 'Linn:';
+$mod_strings['LBL_STATE'] = 'Riik';
+$mod_strings['LBL_COUNTRY'] = 'Maakond';
+$mod_strings['LBL_JJWG_MAPS_LAT'] = 'Laiuskraad';
+$mod_strings['LBL_JJWG_MAPS_LNG'] = 'Pikkuskraad';
+$mod_strings['LBL_MARKER_IMAGE'] = 'Marker Image Type';
+$mod_strings['LBL_LIST_ASSIGNED_USER'] = 'Vastutaja';
+
+$mod_strings['LBL_MARKER_MARKER_POSITION'] = 'Markeri Positsioon (Laiuskraad, Pikkuskraad):';
+$mod_strings['LBL_MARKER_CLOSEST_MATCHING_ADDRESS'] = 'Closest Matching Address:';
+$mod_strings['LBL_MARKER_MARKER_STATUS'] = 'Markeri Staatus';
+$mod_strings['LBL_MARKER_EDIT_DESCRIPTION'] = 'Vajuta ja lohista Markerit.';
+$mod_strings['LBL_JJWG_MAPS_JJWG_MARKERS_FROM_JJWG_MAPS_TITLE'] = 'Kaardid';
