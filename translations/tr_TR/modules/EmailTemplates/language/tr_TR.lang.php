bd4325c4f3b5c96e5bcbf481285dbf43c946302a
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-28 18:31:39 +0000

    New translations en_us.lang.php (Turkish)

---
+++ translations/tr_TR/modules/EmailTemplates/language/tr_TR.lang.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ADD_DOCUMENT' => 'Bir belge ekle',
+    'LBL_ADD_FILE' => 'Bir dosya ekle',
+    'LBL_ATTACHMENTS' => 'Ek dosyalar',
+    'LBL_BODY' => 'İçerik metni:',
+    'LBL_CLOSE' => 'Kapat:',
+    'LBL_DESCRIPTION' => 'Açıklama:',
+    'LBL_EDIT_ALT_TEXT' => 'Düz metin biçimini düzenle',
+    'LBL_EMAIL_ATTACHMENT' => 'E-posta ek dosyası',
+    'LBL_HIDE_ALT_TEXT' => 'Düz metni gizle',
+    'LBL_HTML_BODY' => 'HTML içerik metni',
+    'LBL_INSERT_VARIABLE' => 'Değişken ekle:',
+    'LBL_INSERT_URL_REF' => 'Adres referansı ekle',
+    'LBL_INSERT_TRACKER_URL' => 'İzleyici adresi ekle:',
+    'LBL_INSERT' => 'Ekle',
+    'LBL_LIST_DATE_MODIFIED' => 'Son değişiklik',
+    'LBL_LIST_DESCRIPTION' => 'Açıklama',
+    'LBL_LIST_FORM_TITLE' => 'E-posta kalıpları listesi',
+    'LBL_LIST_NAME' => 'Ad',
+    'LBL_MODULE_NAME' => 'E-posta kalıpları',
+    'LBL_MODULE_TITLE' => 'E-posta kalıpları: Giriş',
+    'LBL_NAME' => 'Ad:',
+    'LBL_NEW_FORM_TITLE' => 'E-posta kalıbı ekle',
+    'LBL_PUBLISH' => 'Yayınla:',
+    'LBL_RELATED_TO' => 'İlişkili öge:',
+    'LBL_SEARCH_FORM_TITLE' => 'E-posta kalıbı arama',
+    'LBL_SHOW_ALT_TEXT' => 'Düz metin olarak görüntüle',
+    'LBL_SUBJECT' => 'Konu:',
+    'LBL_SUITE_DOCUMENT' => 'Belge',
+    'LBL_UPLOAD_FILE' => 'Dosya yükle',
+    'LBL_TEXT_BODY' => 'İçerik metni',
+    'LBL_USERS' => 'Kullanıcılar',
+
+    'LNK_EMAIL_TEMPLATE_LIST' => 'E-posta kalıplarını görüntüle',
+    'LNK_IMPORT_NOTES' => 'Notları içe aktar',
+    'LNK_NEW_EMAIL_TEMPLATE' => 'E-posta kalıbı ekle',
+    'LNK_NEW_EMAIL' => 'E-postayı arşivle',
+    'LNK_NEW_SEND_EMAIL' => 'Yeni e-posta',
+    'LNK_SENT_EMAIL_LIST' => 'Gönderilmiş e-postalar',
+    'LNK_VIEW_CALENDAR' => 'Bugün',
+    // for Inbox
+    'LBL_NEW' => 'Yeni',
+    'LNK_MY_DRAFTS' => 'Taslaklarım',
+    'LNK_MY_INBOX' => 'E-postam',
+    'LBL_TEXT_ONLY' => 'Yalnızca metin',
+    'LBL_SEND_AS_TEXT' => 'Yalnızca metin gönderilsin',
+    'LBL_ACCOUNT' => 'Hesap',
+    'LBL_FROM_NAME' => 'Kimden adı',
+    'LBL_PLAIN_TEXT' => 'Düz metin',
+    'LBL_CREATED_BY' => 'Ekleyen',
+    'LBL_PUBLISHED' => 'Yayınlandı',
+    'LNK_VIEW_MY_INBOX' => 'E-postamı görüntüle',
+    'LBL_ASSIGNED_TO_ID' => 'Atanmış kişi',
+    'LBL_EDIT_LAYOUT' => 'Görünümü düzenle' /*for 508 compliance fix*/,
+    'LBL_SELECT' => 'Seçin' /*for 508 compliance fix*/,
+    'LBL_ID_FF_CLEAR' => 'Temizle' /*for 508 compliance fix*/,
+    'LBL_TYPE' => 'Tür',
+    'LBL_WIDTH' => 'Varsayılan Genişlik',
+    'LNK_IMPORT_CAMPAIGNS' => 'Kampanyayı içe aktar',
+);
