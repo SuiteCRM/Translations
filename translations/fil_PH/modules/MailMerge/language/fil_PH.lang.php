eaeea62071c16b23e48949b197dbf33bcac05f02
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-28 18:53:15 +0000

    New translations en_us.lang.php (Filipino)

---
+++ translations/fil_PH/modules/MailMerge/language/fil_PH.lang.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_STEP_1' => 'Hakbang 1: Piliin ang module at template',
+    'LBL_MAILMERGE_MODULE' => 'Piliin ang module: ',
+    'LBL_MAILMERGE_SELECTED_MODULE' => 'Piniling module: ',
+    'LBL_MAILMERGE_TEMPLATES' => 'Pumili ng template: ',
+    'LBL_STEP_4' => 'Suriin at kumpletuhin',
+    'LBL_SELECTED_MODULE' => 'Piniling module: ',
+    'LBL_SELECTED_TEMPLATE' => 'Piniling template: ',
+    'LBL_SELECTED_ITEMS' => 'Napiling mga aytem: ',
+    'LBL_STEP_5' => 'Nakompletong pagsamahin ang mail',
+    'LBL_MERGED_FILE' => 'Pinagsamang file: ',
+    'LNK_NEW_MAILMERGE' => 'Umpisahan ang pinagsamang mail',
+    'LNK_UPLOAD_TEMPLATE' => 'I-upload ang template',
+    'LBL_DOC_NAME' => 'Pangalan ng dokumento:',
+    'LBL_FILENAME' => 'Pangalan ng file:',
+    'LBL_DOC_VERSION' => 'Rebisyon:',
+    'LBL_DOC_DESCRIPTION' => 'Ang deskripsyon:',
+    'LBL_LIST_NAME' => 'Ang pangalan',
+    'LBL_FINISH' => 'Simulan na pagsamahin',
+    'LBL_NEXT' => 'Susunod >',
+    'LBL_BACK' => '< Bumalik',
+    'LBL_START' => 'Pindutin ito upang magpatuloy',
+    'LBL_CONTAINS_CONTACT_INFO' => 'Ang napiling template ay naglalaman ng mga na-uugnay ',
+    'LBL_ADDIN_NOTICE' => 'Ito ay nangangailangan ng pag-install sa SuiteCRM na pinagsamang mail na add-in sa Microsoft Word.',
+    'LBL_REMOVE' => 'Tanggalin ang mga aytem(s)' /*for 508 compliance fix*/,
+    'LBL_ADD' => 'Magdagdag ng aytem(s)' /*for 508 compliance fix*/,
+    'LBL_BROWSER_REQUIREMENT' => 'Ito ay nangangailan ng IE 6.0 o mas higit pa para sa Windows.',
+    'LBL_FORM_SELECT_TEMPLATE' => 'Paki-piliin ang template.',
+    'LBL_SELECT_ONE' => 'Mangyaring pumili ng kahit isang aytem.',
+    'LBL_AVAILABLE' => 'Mayroon',
+    'LBL_SELECTED' => 'Napili',
+    'LBL_RELATED_INFO_SELECTION' => 'Gamitin ang mga seleksyon sa ibaba para makilala ang kaugnay na impormasyo na gusto mong i-display sa iyong pinagsamang dokumento. Laktawan ang hakbang na ito kapag ang iyong template na dokumento ay naglalaman ng di ugnay na impormasyon.',
+
+);
