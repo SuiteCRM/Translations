e94d15d90ee851c7ae00ed1982cecf0f90df936c
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-28 19:46:55 +0000

    New translations en_us.lang.php (Mongolian)

---
+++ translations/mn_MN/modules/jjwg_Areas/language/mn_MN.lang.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings['LBL_ASSIGNED_TO_ID'] = 'Хариуцагчийн ID';
+$mod_strings['LBL_ASSIGNED_TO_NAME'] = 'Хариуцагч';
+$mod_strings['LBL_ID'] = 'ID';
+$mod_strings['LBL_DATE_ENTERED'] = 'Үүссэн Огноо';
+$mod_strings['LBL_DATE_MODIFIED'] = 'Өөрчилсөн';
+$mod_strings['LBL_MODIFIED'] = 'Өөрчилсөн';
+$mod_strings['LBL_MODIFIED_NAME'] = 'Өөрчилсөн';
+$mod_strings['LBL_CREATED'] = 'Үүсгэсэн';
+$mod_strings['LBL_DESCRIPTION'] = 'Тайлбар';
+$mod_strings['LBL_DELETED'] = 'Устгасан';
+$mod_strings['LBL_NAME'] = 'Нэр';
+$mod_strings['LBL_CREATED_USER'] = 'Үүсгэсэн';
+$mod_strings['LBL_MODIFIED_USER'] = 'Өөрчилсөн';
+$mod_strings['LBL_LIST_NAME'] = 'Нэр';
+$mod_strings['LBL_LIST_FORM_TITLE'] = 'Хүрээ';
+$mod_strings['LBL_MODULE_NAME'] = 'Хүрээнүүд';
+$mod_strings['LBL_MODULE_TITLE'] = 'Хүрээнүүд';
+$mod_strings['LBL_HOMEPAGE_TITLE'] = 'Миний хүрээ';
+$mod_strings['LNK_NEW_RECORD'] = 'Хүрээ үүсгэх';
+$mod_strings['LNK_LIST'] = 'Хүрээ харах';
+$mod_strings['LBL_SEARCH_FORM_TITLE'] = 'Хүрээ хайх';
+$mod_strings['LBL_HISTORY_SUBPANEL_TITLE'] = 'Түүхийг харах';
+$mod_strings['LBL_ACTIVITIES_SUBPANEL_TITLE'] = 'Үйл ажиллагаа';
+$mod_strings['LBL_NEW_FORM_TITLE'] = 'Шинэ хүрээ';
+$mod_strings['LBL_CITY'] = 'Хот';
+$mod_strings['LBL_STATE'] = 'Муж';
+$mod_strings['LBL_COUNTRY'] = 'Улс';
+$mod_strings['LBL_COORDINATES'] = 'Координатууд';
+$mod_strings['LBL_LIST_ASSIGNED_USER'] = 'Хариуцагч';
+$mod_strings['LBL_AREA_MAP'] = 'Хүрээний газрын зураг';
+
+$mod_strings['LBL_AREA_EDIT_TITLE'] = 'Хүрээ үүсгэх заавар:';
+$mod_strings['LBL_AREA_EDIT_DESC_1'] = 'Left click on the map, in a clockwise motion, to create marker points for the area.';
+$mod_strings['LBL_AREA_EDIT_DESC_2'] = 'Click on the first marker point to close the polygon area.';
+$mod_strings['LBL_AREA_EDIT_RESET'] = 'Цуцлах';
+$mod_strings['LBL_AREA_EDIT_USE_AREA_COORDINATES'] = 'Хүрээний координат ашиглах';
+$mod_strings['LBL_AREA_EDIT_COORDINATE_RESULTS'] = 'Coordinate Results (lng,lat,elv):';
+$mod_strings['LBL_JJWG_MAPS_JJWG_AREAS_FROM_JJWG_MAPS_TITLE'] = 'Газрын зургууд';
