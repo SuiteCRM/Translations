ff9979f5105fd92e893f648d4eab132bbcc4bf28
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-28 18:15:31 +0000

    New translations en_us.lang.php (Lithuanian)

---
+++ translations/lt_LT/modules/Connectors/language/lt_LT.lang.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+
+$mod_strings = array(
+
+    'LBL_ADMINISTRATION_MAIN' => 'Jungties nustatymai',
+    'LBL_AVAILABLE' => 'Galimas',
+    'LBL_BACK' => '< Atgal',
+    'LBL_CONFIRM_CONTINUE_SAVE' => 'Kai kurie privalomi laukai buvo palikti tušti. Ar norite tęsti toliau ir išsaugoti?',
+    'LBL_CONNECTOR_FIELDS' => 'Jungties laukai',
+    'LBL_DATA' => 'Duomenys',
+    'LBL_DEFAULT' => 'Numatytasis',
+    'LBL_DISABLED' => 'Išjungtas',
+    'LBL_ENABLED' => 'Įjungtas',
+    'LBL_EXTERNAL' => 'Leisti vartotojams sukurti išorinius prisijungimus šiai jungčiai. Taip pat šis nustatymas turi būti įjungtas Jungties nustatymų lange.',
+    'LBL_EXTERNAL_SET_PROPERTIES' => ' Siekiant naudoti šios jungties, savybes reikėtų nustatyti Set Connector Properties nustatymų puslapyje.',
+    'LBL_MERGE' => 'Apjungti',
+    'LBL_MODIFY_DISPLAY_TITLE' => 'Įjungti jungtis',
+    'LBL_MODIFY_DISPLAY_DESC' => 'Pasirinkti kuriems moduliams yra įjungtos jungtys.',
+    'LBL_MODULE_FIELDS' => 'Modulio laukai',
+    'LBL_MODIFY_MAPPING_TITLE' => 'Susieti jungties laukus',
+    'LBL_MODIFY_MAPPING_DESC' => 'Susieti jungties laukus su modulio laukais, kad būtų nustatyti kokie jungties duomenys gali patekti į modulį.',
+    'LBL_MODIFY_PROPERTIES_TITLE' => 'Įvardinti jungties savybes',
+    'LBL_MODIFY_PROPERTIES_DESC' => 'Nustatyti kiekvienos jungties savybes, kaip adresą ir API raktus.',
+    'LBL_MODIFY_SEARCH_TITLE' => 'Valdyti jungties paiešką',
+    'LBL_MODIFY_SEARCH' => 'Paieška',
+    'LBL_MODIFY_SEARCH_DESC' => 'Pasirinkite sujungimo laukus naudojant paiešką duomenų kiekvienam moduliui.',
+    'LBL_MODULE_NAME' => 'Jungties nustatymai',
+    'LBL_NO_PROPERTIES' => 'Šiai jungčiai nėra jokių nustatomų savybių.',
+    'LBL_SAVE' => 'Saugoti',
+    'LBL_SUMMARY' => 'Santrauka',
+    'LBL_STEP1' => 'Ieškoti ir žiūrėti duomenis',
+    'LBL_STEP2' => 'Apjungti įrašus su',
+    'LBL_TEST_SOURCE' => 'Testuoti jungtį',
+    'LBL_TEST_SOURCE_FAILED' => 'Nesėkmingas testas',
+    'LBL_TEST_SOURCE_SUCCESS' => 'Sėkmingas testas',
+    'LBL_TITLE' => 'Duomenų apjungimas',
+
+    'ERROR_NO_ADDITIONAL_DETAIL' => 'Klaida: Nerasta papildomų įrašo detalių.',
+    'ERROR_NO_SEARCHDEFS_DEFINED' => 'Jokie moduliai nebuvo įjungti šiai jungčiai. Pasirinkite modulį šiai jungčiai Jungčių įjungimo puslapyje.',
+    'ERROR_NO_SEARCHDEFS_MAPPED' => 'Klaida: nėra įjungtų jungčių, kurios turėtų apibrėžtų paieškos laukų.',
+    'ERROR_NO_SEARCHDEFS_MAPPING' => 'Klaida: Nėra paieškos laukų nurodytam moduliui ir jungčiai. Prašome susisiekti su administratoriumi.',
+    'ERROR_NO_DISPLAYABLE_MAPPED_FIELDS' => 'Klaida: Nėra modulio laukų, kurie būtų nurodyti rezultatų atvaizdavimui. Prašome susisiekti su administratoriumi.',
+    'LBL_INFO_INLINE' => 'Informacija' /*for 508 compliance fix*/,
+    'LBL_CLOSE' => 'Užbaigti' /*for 508 compliance fix*/,
+
+);
