43a8759517c26167cc80998fc3a3ad6ab58c5c8e
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-28 18:24:59 +0000

    New translations en_us.lang.php (Chinese Traditional)

---
+++ translations/zh_TW/modules/DynamicFields/language/zh_TW.lang.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LNK_NEW_CALL' => '安排通話',
+    'LNK_NEW_MEETING' => '安排會議',
+    'LNK_NEW_TASK' => '創建任務',
+    'LNK_NEW_NOTE' => '創建備忘或附件',
+    'LNK_NEW_EMAIL' => '存檔電子郵件',
+    'LNK_CALL_LIST' => '通話',
+    'LNK_MEETING_LIST' => '會議',
+    'LNK_TASK_LIST' => '任務',
+    'LNK_NOTE_LIST' => '備註',
+    'LBL_ADD_FIELD' => '添加欄位:',
+    'LBL_SEARCH_FORM_TITLE' => '模組搜尋',
+    'COLUMN_TITLE_NAME' => '欄位名稱',
+    'COLUMN_TITLE_DISPLAY_LABEL' => '顯示標簽',
+    'COLUMN_TITLE_LABEL_VALUE' => '標籤值',
+    'COLUMN_TITLE_LABEL' => '系統標籤',
+    'COLUMN_TITLE_DATA_TYPE' => '資料型態',
+    'COLUMN_TITLE_MAX_SIZE' => '最大尺寸',
+    'COLUMN_TITLE_HELP_TEXT' => '説明文字',
+    'COLUMN_TITLE_COMMENT_TEXT' => '註解文字',
+    'COLUMN_TITLE_REQUIRED_OPTION' => '必填的欄位',
+    'COLUMN_TITLE_DEFAULT_VALUE' => '默認值',
+    'COLUMN_TITLE_FRAME_HEIGHT' => 'IFrame 高度',
+    'COLUMN_TITLE_HTML_CONTENT' => 'HTML',
+    'COLUMN_TITLE_URL' => '預設 URL',
+    'COLUMN_TITLE_AUDIT' => '審計',
+    'COLUMN_TITLE_MIN_VALUE' => '最小值',
+    'COLUMN_TITLE_MAX_VALUE' => '最大值',
+    'COLUMN_TITLE_LABEL_ROWS' => '列',
+    'COLUMN_TITLE_LABEL_COLS' => '欄',
+    'COLUMN_TITLE_DISPLAYED_ITEM_COUNT' => '# 項目已顯示',
+    'COLUMN_TITLE_AUTOINC_NEXT' => '自動增加下一個值',
+    'COLUMN_DISABLE_NUMBER_FORMAT' => '禁用格式',
+    'COLUMN_TITLE_ENABLE_RANGE_SEARCH' => '啟用範圍搜尋',
+    'LBL_DROP_DOWN_LIST' => '下拉清單',
+    'LBL_RADIO_FIELDS' => '單選欄位',
+    'LBL_MULTI_SELECT_LIST' => '多選清單',
+    'COLUMN_TITLE_PRECISION' => '精確度',
+    'LBL_MODULE' => '模組',
+    'COLUMN_TITLE_MASS_UPDATE' => '批量更新',
+    'COLUMN_TITLE_IMPORTABLE' => '可導入',
+    'COLUMN_TITLE_DUPLICATE_MERGE' => '合併重複',
+    'LBL_LABEL' => '標簽',
+    'LBL_DATA_TYPE' => '資料型態',
+    'LBL_DEFAULT_VALUE' => '默認值',
+    'ERR_RESERVED_FIELD_NAME' => "保留的關鍵字",
+    'ERR_SELECT_FIELD_TYPE' => '請選擇一個欄位類型',
+    'ERR_FIELD_NAME_ALREADY_EXISTS' => '欄位名稱已存在',
+    'LBL_BTN_ADD' => '新增',
+    'LBL_BTN_EDIT' => '編輯',
+    'LBL_GENERATE_URL' => '產生 URL',
+    'LBL_CALCULATED' => '已計算的值',
+    'LBL_LINK_TARGET' => '開啟連結方式',
+    'LBL_IMAGE_WIDTH' => '寬',
+    'LBL_IMAGE_HEIGHT' => '高度',
+    'LBL_IMAGE_BORDER' => '邊框',
+    'LBL_HELP' => '幫助' /*for 508 compliance fix*/,
+    'COLUMN_TITLE_INLINE_EDIT_TEXT' => '行內編輯',
+    'COLUMN_TITLE_PARENT_ENUM' => '父下拉列表',
+    'LBL_INSERT_FIELD' => '插入欄位',
+);
