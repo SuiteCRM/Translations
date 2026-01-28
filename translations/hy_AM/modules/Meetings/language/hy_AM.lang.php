b7f41e89918ec7eb7629b74513e24ccfd2c75df4
Matt Lorimer <mattlorimer@users.noreply.github.com>
2026-01-28 18:53:44 +0000

    New translations en_us.lang.php (Armenian)

---
+++ translations/hy_AM/modules/Meetings/language/hy_AM.lang.php
@@ -0,0 +1,171 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ACCEPT_THIS' => 'Ընդունե՞լ',
+    'LBL_ADD_BUTTON' => 'Ավելացնել',
+    'LBL_ADD_INVITEE' => 'Ավելացնել հրավիրվածներին',
+    'LBL_CONTACT_NAME' => 'Կոնտակտ․',
+    'LBL_CONTACTS_SUBPANEL_TITLE' => 'Կոնտակտներ',
+    'LBL_CREATED_BY' => 'Ստեղծված է․',
+    'LBL_DATE_END' => 'Ավարտման ամսաթիվը',
+    'LBL_DATE_TIME' => 'Մեկնարկի ամսաթիվը և ժամանակը․',
+    'LBL_DATE' => 'Մեկնարկի ամսաթիվը',
+    'LBL_DEFAULT_SUBPANEL_TITLE' => 'Հանդիպումներ',
+    'LBL_DESCRIPTION' => 'Նկարագրություն.',
+    'LBL_DIRECTION' => 'Ուղղություն․',
+    'LBL_DURATION_HOURS' => 'Տևողությունը (ժամ)․',
+    'LBL_DURATION_MINUTES' => 'Տևողությունը (րոպե)․',
+    'LBL_DURATION' => 'Տևողություն․',
+    'LBL_EMAIL' => 'Էլ-փոստ',
+    'LBL_FIRST_NAME' => 'Անուն',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Նշումներ',
+    'LBL_HOURS_ABBREV' => 'հ',
+    'LBL_HOURS_MINS' => '(ժամ/րոպե)',
+    'LBL_INVITEE' => 'Հրավիրվածներ',
+    'LBL_LAST_NAME' => 'Ազգանուն',
+    'LBL_ASSIGNED_TO_NAME' => 'Կատարող',
+    'LBL_LIST_ASSIGNED_TO_NAME' => 'Պատասխանատու',
+    'LBL_LIST_CLOSE' => 'Փակել',
+    'LBL_LIST_CONTACT' => 'Կոնտակտ',
+    'LBL_LIST_DATE_MODIFIED' => 'Փոփոխման ամսաթիվ',
+    'LBL_LIST_DATE' => 'Մեկնարկի ամսաթիվը',
+    'LBL_LIST_DIRECTION' => 'Ուղղություն',
+    'LBL_LIST_DUE_DATE' => 'Կատարման ամսաթիվը',
+    'LBL_LIST_FORM_TITLE' => 'Հանդիպումների ցուցակ',
+    'LBL_LIST_MY_MEETINGS' => 'Իմ հանդիպումները',
+    'LBL_LIST_RELATED_TO' => 'Առնչվում է',
+    'LBL_LIST_STATUS' => 'Կարգավիճակ',
+    'LBL_LIST_SUBJECT' => 'Թեմա',
+    'LBL_LEADS_SUBPANEL_TITLE' => 'Պոտենցիալ կլիենտներ (առաջնորդներ)',
+    'LBL_LOCATION' => 'Տեղանք.',
+    'LBL_MINSS_ABBREV' => 'ր',
+    'LBL_MODIFIED_BY' => 'Փոփոխված է',
+    'LBL_MODULE_NAME' => 'Հանդիպումներ',
+    'LBL_MODULE_TITLE' => 'Հանդիպումներ․ Գլխավոր',
+    'LBL_NAME' => 'Անվանում',
+    'LBL_NEW_FORM_TITLE' => 'Նշանակել հանդիպում',
+    'LBL_OUTLOOK_ID' => 'Outlook ID',
+    'LBL_SEQUENCE' => 'Թարմացման հաջորդականությունը',
+    'LBL_PHONE' => 'Հեռախոս:',
+    'LBL_REMINDER_TIME' => 'Հիշեցման ժամանակ',
+    'LBL_EMAIL_REMINDER_SENT' => 'Հիշեցումը ուղարկված է էլ-փոստով',
+    'LBL_REMINDER' => 'Հիշեցումներ․',
+    'LBL_REMINDER_POPUP' => 'Դուրս ելնող պատուհան',
+    'LBL_REMINDER_EMAIL_ALL_INVITEES' => 'Ուղարկել էլ-նամակներ բոլոր հրավիրվածներին',
+    'LBL_EMAIL_REMINDER' => 'Հիշեցում էլ-փոստի միջոցով',
+    'LBL_EMAIL_REMINDER_TIME' => 'Հիշեցման ժամանակը էլ-փոստով',
+    'LBL_REMOVE' => 'Հեռացնել',
+    'LBL_SCHEDULING_FORM_TITLE' => 'Պլանավորում',
+    'LBL_SEARCH_BUTTON' => 'Որոնում',
+    'LBL_SEARCH_FORM_TITLE' => 'Հանդիպման որոնում',
+    'LBL_SEND_BUTTON_LABEL' => 'Պահպանել և ուղարկել հրավերներ',
+    'LBL_SEND_BUTTON_TITLE' => 'Պահպանել և ուղարկել հրավերներ',
+    'LBL_STATUS' => 'Կարգավիճակ',
+    'LBL_TYPE' => 'Հանդիպման տեսակը',
+    'LBL_PASSWORD' => 'Հանդիպման գաղտնաբառ',
+    'LBL_URL' => 'Սկսել/Միանալ հանդիպմանը',
+    'LBL_HOST_URL' => 'Բազմակի URL',
+    'LBL_DISPLAYED_URL' => 'Ցուցադրվող URL',
+    'LBL_CREATOR' => 'Հանդիպում ստեղծող',
+    'LBL_EXTERNALID' => 'Արտաքին ծրագրի ID',
+    'LBL_SUBJECT' => 'Թեմա',
+    'LBL_TIME' => 'Մեկնարկի ժամանակը․',
+    'LBL_USERS_SUBPANEL_TITLE' => 'Օգտվողներ',
+    'LBL_CALENDAR_ACCOUNTS' => 'Calendar Accounts',
+    'LBL_PARENT_TYPE' => 'Ծնողի տեսակը',
+    'LBL_PARENT_ID' => 'Ծնողի ID',
+    'LNK_MEETING_LIST' => 'Հանդիպումների դիտում',
+    'LNK_NEW_APPOINTMENT' => 'Նշանակել հանդիպում',
+    'LNK_NEW_MEETING' => 'Պլանավորել հանդիպում',
+    'LNK_IMPORT_MEETINGS' => 'Հանդիպումների ներմուծում',
+
+    'LBL_CREATED_USER' => 'Ստեղծել օգտագործող',
+    'LBL_MODIFIED_USER' => 'Փոփոխված օգտվող',
+    'NOTICE_DURATION_TIME' => 'Ժամանակի տևողությունը պետք է լինի ավելի մեծ քան 0',
+    'LBL_MEETING_INFORMATION' => 'Հիմնական ինֆորմացիա', //No need to be translated in all caps. Translation used just in menu action items when using the SuiteP template
+    'LBL_LIST_JOIN_MEETING' => 'Միանալ հանդիպմանը',
+    'LBL_ACCEPT_STATUS' => 'Ընդունել կարգավիճակ',
+    'LBL_ACCEPT_LINK' => 'Ընդունման հղում',
+    // You are not invited to the meeting messages
+    'LBL_EXTNOT_MAIN' => 'Դուք չեք կարող միանալ այս հանդիպմանը, քանի որ դուք հրավիրված չեք:',
+    'LBL_EXTNOT_RECORD_LINK' => 'Հանդիպման դիտում',
+
+    //cannot start messages
+    'LBL_EXTNOSTART_MAIN' => 'Դուք չեք կարող սկսել այս հանդիպումը, քանի որ դուք համակարգի ադմինիստրատորը չեք, կամ էլ հանդիպումը պլանավորված չէ ձեր կողմից:',
+
+    // create invitee functionallity
+    'LBL_CREATE_INVITEE' => 'Ստեղծել հրավերք',
+    'LBL_CREATE_CONTACT' => 'Որպես կոնտակտ',  // Create invitee functionallity
+    'LBL_CREATE_LEAD' => 'Նոր պոտենցիալ կլիենտ',  // Create invitee functionallity
+    'LBL_CREATE_AND_ADD' => 'Ստեղծել և ավելացնել',  // Create invitee functionallity
+    'LBL_CANCEL_CREATE_INVITEE' => 'Չեղարկել',
+    'LBL_EMPTY_SEARCH_RESULT' => 'Ներեցեք, չկան արդյունքներ: Խնդրում ենք ստեղծել հրավիրյալին ստորև։',
+    'LBL_NO_ACCESS' => 'Դուք ոչ մի մուտքի իրավունք չունեք $module-ում գրառում ստեղծելու համար',  // Create invitee functionallity
+
+    'LBL_REPEAT_TYPE' => 'Կրկնության տեսակ',
+    'LBL_REPEAT_INTERVAL' => 'Կրկնել ընդմիջումից',
+    'LBL_REPEAT_DOW' => 'Կրկնում',
+    'LBL_REPEAT_UNTIL' => 'Կրկնել մինչև',
+    'LBL_REPEAT_COUNT' => 'Կրկնությունների քանակը',
+    'LBL_REPEAT_PARENT_ID' => 'Կրկնել ծնողների ID',
+    'LBL_RECURRING_SOURCE' => 'Կրկնվող աղբյուր',
+
+    'LBL_SYNCED_RECURRING_MSG' => 'Այս հանդիպման տվյալները ստեղծվել են մեկ այլ ծրագրի շրջանակներում և սինքրոնացվել SuiteCRM-ի հետ։ Տվյալ գրառման մեջ փոփոխություններ կատարելու համար բացեք նախնական համակարգը, հանդիպման մեջ կատարեք անհրաժեշտ փոփոխություները և նորից սինքրոնացրեք տվյալները SuiteCRM-ի հետ։',
+    'LBL_RELATED_TO' => 'Առնչվում է',
+
+    // for reminders
+    'LBL_REMINDERS' => 'Հիշեցումներ',
+    'LBL_REMINDERS_ACTIONS' => 'Գործողություններ.',
+    'LBL_REMINDERS_POPUP' => 'Դուրս ելնող պատուհան',
+    'LBL_REMINDERS_EMAIL' => 'Էլ-փոստով հրավերներ',
+    'LBL_REMINDERS_WHEN' => 'Երբ.',
+    'LBL_REMINDERS_REMOVE_REMINDER' => 'Հեռացնել հիշեցումը',
+    'LBL_REMINDERS_ADD_ALL_INVITEES' => 'Ավելացնել բոլոր հրավիրվածներին',
+    'LBL_REMINDERS_ADD_REMINDER' => 'Ավելացնել հիշեցում',
+
+    // for google sync
+    'LBL_GSYNC_ID' => 'Google Event ID',
+    'LBL_GSYNC_LASTSYNC' => 'Last Google Sync Timestamp',
+);
