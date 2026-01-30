d7cf3a8f513c0af51bb3b1dca1f6f3665214b5d8
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-01-30 18:23:24 +0000

    New translations en_us.lang.php (Serbian (Latin))

---
+++ translations/sr_CS/modules/EmailMarketing/language/sr_CS.lang.php
@@ -0,0 +1,108 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_REPLY_ADDR' => '"Odgovori" na adresu: ',
+    'LBL_REPLY_NAME' => 'Odgovor pošalji na ime: ',
+
+    'LBL_MODULE_NAME' => 'Email Marketing',
+    'LBL_MODULE_TITLE' => 'Marketinška elektronska pošta: Početna Strana',
+    'LBL_LIST_FORM_TITLE' => 'Marketinška kampanja putem elektronskih poruka',
+    'LBL_NAME' => 'Ime: ',
+    'LBL_LIST_NAME' => 'Ime',
+    'LBL_LIST_FROM_ADDR' => 'Iz elektronske pošte',
+    'LBL_LIST_DATE_START' => 'Datum početka',
+    'LBL_LIST_TEMPLATE_NAME' => 'Email šablon',
+    'LBL_LIST_STATUS' => 'Status',
+    'LBL_STATUS' => 'Status',
+    'LBL_STATUS_TEXT' => 'Status',
+    'LBL_TEMPLATE_NAME' => 'Ime šablona',
+    'LBL_DATE_ENTERED' => 'Datum unosa',
+    'LBL_DATE_MODIFIED' => 'Datum izmene',
+    'LBL_MODIFIED' => 'Izmenio: ',
+    'LBL_CREATED' => 'Autor: ',
+    'LBL_MESSAGE_FOR' => 'Pošalji ovu poruku ka:',
+
+    'LBL_FROM_NAME' => 'Ime pošiljaoca: ',
+    'LBL_FROM_ADDR' => 'Od adrese elektronske pošte: ',
+    'LBL_DATE_START' => 'Datum početka',
+    'LBL_TIME_START' => 'Vreme početka ',
+    'LBL_START_DATE_TIME' => 'Datum i vreme početka: ',
+    'LBL_TEMPLATE' => 'Šablon elektronske pošte: ',
+
+    'LBL_MODIFIED_BY' => 'Izmenio: ',
+    'LBL_CREATED_BY' => 'Autor: ',
+
+    'LNK_NEW_CAMPAIGN' => 'Kreiraj kampanju',
+    'LNK_CAMPAIGN_LIST' => 'Kampanje',
+    'LNK_NEW_PROSPECT_LIST' => 'Kreiranje liste ciljeva',
+    'LNK_PROSPECT_LIST_LIST' => 'Liste ciljeva',
+    'LNK_NEW_PROSPECT' => 'Kreiranje cilja',
+    'LNK_PROSPECT_LIST' => 'Ciljevi',
+    'LBL_DEFAULT_SUBPANEL_TITLE' => 'Email Marketing',
+    'LBL_CREATE_EMAIL_TEMPLATE' => 'Kreiraj',
+    'LBL_EDIT_EMAIL_TEMPLATE' => 'Izmeni',
+    'LBL_FROM_MAILBOX' => 'Iz poštanskog sandučeta',
+    'LBL_FROM_MAILBOX_NAME' => 'Koristi poštansko sanduče:',
+    'LBL_OUTBOUND_EMAIL_ACCOUNT_NAME' => 'Odlazni email nalog:',
+    'LBL_PROSPECT_LIST_SUBPANEL_TITLE' => 'Liste ciljeva',
+    'LBL_ALL_PROSPECT_LISTS' => 'Označite da izaberete sve ciljne liste u kampanji',
+    'LBL_RELATED_PROSPECT_LISTS' => 'Sve ciljne liste koje su u vezi sa porukom.',
+    'LBL_PROSPECT_LIST_NAME' => 'Ime liste ciljeva',
+
+    'LBL_LIST_PROSPECT_LIST_NAME' => 'Targeted Lists',
+    'LBL_MODULE_SEND_TEST' => 'Campaign: Send Test',
+    'LBL_MODULE_SEND_EMAILS' => 'Campaign: Send Emails',
+    'LBL_SCHEDULE_MESSAGE_TEST' => 'Please select the campaign messages that you would like to test:',
+    'LBL_SCHEDULE_MESSAGE_EMAILS' => 'Please select the campaign messages that you would like to schedule for distribution on the specified start date and time:',
+    'LBL_SCHEDULE_BUTTON_TITLE' => 'Pošalji',
+    'LBL_SCHEDULE_BUTTON_LABEL' => 'Pošalji',
+    'LBL_ERROR_ON_MARKETING' => 'Nedostaju obavezna polja',
+
+    'LBL_CAMPAIGN_ID' => 'ID broj kampanje',
+    'LBL_OUTBOUND_EMAIL_ACOUNT_ID' => 'ID odlaznog email naloga',
+    'LBL_EMAIL_TEMPLATE' => 'Email šablon',
+    'LBL_PROSPECT_LISTS' => 'Prospect Lists',
+
+);
