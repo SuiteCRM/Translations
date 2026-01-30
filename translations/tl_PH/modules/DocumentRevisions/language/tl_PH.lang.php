1219c7d78ffa35ffdfcf93150b2e9cb290c6f205
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-01-30 18:15:32 +0000

    New translations en_us.lang.php (Tagalog)

---
+++ translations/tl_PH/modules/DocumentRevisions/language/tl_PH.lang.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    //module
+    'LBL_MODULE_NAME' => 'Ang pagbabago ng dokumento',
+
+    'LNK_NEW_DOCUMENT' => 'Gumawa ng Dokumento',
+    'LNK_DOCUMENT_LIST' => 'Tingnan ang mga dokumento',
+
+    //vardef labels
+    'LBL_REVISION_NAME' => 'Ang pagbabago ng numero',
+    'LBL_FILENAME' => 'Ang payl:',
+    'LBL_MIME' => 'Ang uri ng panggagaya',
+    'LBL_REVISION' => 'Ang pagbabago',
+    'LBL_DOCUMENT' => 'Ang kaugnay na dokumento',
+    'LBL_LATEST_REVISION' => 'Ang pinakabagong pagbabago',
+    'LBL_CHANGE_LOG' => 'Ang log ay baguhin',
+    'LBL_ACTIVE_DATE' => 'Ang petsa ay i-publish',
+    'LBL_EXPIRATION_DATE' => 'Petsa ng Pag-expire',
+    'LBL_FILE_EXTENSION' => 'Ang ekstensiyon ng payl',
+
+    'LBL_DOC_NAME' => 'Pangalan ng dokumento:',
+    'LBL_DOC_VERSION' => 'Ang Pagbabago:',
+
+    //document revisions.
+    'LBL_REV_LIST_REVISION' => 'Ang pagbabago',
+    'LBL_REV_LIST_ENTERED' => 'Ang petsa ay lumikha',
+    'LBL_REV_LIST_CREATED' => 'Nilikha sa pamamagitan ng',
+    'LBL_REV_LIST_LOG' => 'Ang log ay baguhin',
+    'LBL_REV_LIST_FILENAME' => 'Payl',
+
+    'LBL_CURRENT_DOC_VERSION' => 'Ang pinakabagong pagbabago:',
+    'LBL_SEARCH_FORM_TITLE' => 'Ang paghahanap ng dokumento',
+    'LBL_REVISIONS' => 'Ang mga pagbabago',
+
+    //error messages
+    'ERR_FILENAME' => 'Ang Pangalan ng File',
+    'ERR_DOC_VERSION' => 'Ang bersyon ng dokumento',
+    'ERR_DELETE_CONFIRM' => 'Gusto mo bang burahin ang pagbabago ng dokumentong ito?',
+    'ERR_DELETE_LATEST_VERSION' => 'Ikaw ay hindi pinapayagang burahin ang pinakabagong pagbabago ng isang dokumento.',
+    'LNK_NEW_MAIL_MERGE' => 'Ang pagsasama ng sulat',
+    'LBL_DOC_ID' => 'Ang dokumento na pinagmulan ng ID',
+    'LBL_DOC_TYPE' => 'Ang pinagmulan',
+    'LBL_DOC_URL' => 'Ang dokumentong pinagmulan ng URL',
+
+    'LBL_CREATED_BY_NAME' => 'Lumikha sa pamamagitan ng pangalan',
+);
