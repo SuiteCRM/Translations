f29d6adfba5328303c4d7643bdff622bbd484154
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-01-30 19:05:04 +0000

    New translations en_us.lang.php (Hindi)

---
+++ translations/hi_IN/modules/ProspectLists/language/hi_IN.lang.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_MODULE_NAME' => 'लक्ष्य सूचियाँ',
+    'LBL_MODULE_ID' => 'लक्ष्य सूचियाँ',
+    'LBL_MODULE_TITLE' => 'लक्ष्य सूचियाँ: होम',
+    'LBL_SEARCH_FORM_TITLE' => 'लक्ष्य सूची खोज',
+    'LBL_LIST_FORM_TITLE' => 'लक्ष्य सूचियाँ',
+    'LBL_PROSPECT_LIST_NAME' => 'लक्ष्य सूची:',
+    'LBL_NAME' => 'नाम',
+    'LBL_ENTRIES' => 'कुल प्रविष्टियां',
+    'LBL_LIST_PROSPECT_LIST_NAME' => 'लक्ष्य सूची',
+    'LBL_LIST_ENTRIES' => 'सूची में लक्ष्य',
+    'LBL_LIST_DESCRIPTION' => 'विवरण',
+    'LBL_LIST_TYPE_NO' => 'प्रकार',
+    'LBL_LIST_END_DATE' => 'समाप्ति दिनांक',
+    'LBL_DATE_ENTERED' => 'बनाया गया दिनांक',
+    'LBL_MARKETING_ID' => 'मार्केटिंग आईडी',
+    'LBL_DATE_MODIFIED' => 'तिथि संशोधित',
+    'LBL_MODIFIED' => 'के द्वारा संशोधित',
+    'LBL_CREATED' => 'के द्वारा बनाया गया',
+    'LBL_ASSIGNED_TO' => 'को सौंपना',
+    'LBL_DESCRIPTION' => 'विवरण',
+    'LNK_NEW_CAMPAIGN' => 'अभियान बनाएँ',
+    'LNK_CAMPAIGN_LIST' => 'अभीयाँन ',
+    'LNK_NEW_PROSPECT_LIST' => 'लक्ष्य सूची बनाएं',
+    'LNK_PROSPECT_LIST_LIST' => 'लक्ष्य सूचियाँ देखें',
+    'LBL_MODIFIED_BY' => 'के द्वारा संशोधित',
+    'LBL_CREATED_BY' => 'के द्वारा बनाया गया',
+    'LNK_NEW_PROSPECT' => 'लक्ष्य बनाएं',
+    'LNK_PROSPECT_LIST' => 'लक्ष्यों को ',
+
+    'LBL_CONTACTS_SUBPANEL_TITLE' => 'संपर्कों',
+    'LBL_LEADS_SUBPANEL_TITLE' => 'नेतृत्व',
+    'LBL_PROSPECTS_SUBPANEL_TITLE' => 'लक्ष्यों को ',
+    'LBL_ACCOUNTS_SUBPANEL_TITLE' => 'खाता',
+    'LBL_COPY_PREFIX' => 'की प्रति',
+    'LBL_USERS_SUBPANEL_TITLE' => 'उपयोगकर्ताओं ',
+    'LBL_TYPE' => 'प्रकार',
+    'LBL_LIST_TYPE' => 'प्रकार',
+    'LBL_LIST_TYPE_LIST_NAME' => 'प्रकार',
+    'LBL_NEW_FORM_TITLE' => 'नई लक्ष्य सूची',
+    'LBL_MARKETING_NAME' => 'मार्केटिंग नाम',
+    'LBL_DOMAIN_NAME' => 'डोमेन नाम',
+    'LBL_DOMAIN' => 'डोमेन पर कोई ईमेल नहीं',
+    'LBL_LIST_PROSPECTLIST_NAME' => 'नाम',
+
+    'LBL_EMAIL_MARKETING' => 'ईमेल मार्केटिंग ',
+
+    'LBL_ASCENDING' => 'आरोही',
+    'LBL_DESCENDING' => 'अवरोही',
+);
