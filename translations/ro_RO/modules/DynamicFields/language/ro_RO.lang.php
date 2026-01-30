28c0b402f58a72ffeda2a142e088ea1ddb2234a2
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-01-30 18:17:10 +0000

    New translations en_us.lang.php (Romanian)

---
+++ translations/ro_RO/modules/DynamicFields/language/ro_RO.lang.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LNK_NEW_CALL' => 'Nou apel',
+    'LNK_NEW_MEETING' => 'Programare Întâlnire',
+    'LNK_NEW_TASK' => 'Creați Sarcină',
+    'LNK_NEW_NOTE' => 'Creează notă sau atașament',
+    'LNK_NEW_EMAIL' => 'Arhivează email',
+    'LNK_CALL_LIST' => 'Apeluri',
+    'LNK_MEETING_LIST' => 'Întâlniri',
+    'LNK_TASK_LIST' => 'Sarcini',
+    'LNK_NOTE_LIST' => 'Note',
+    'LBL_ADD_FIELD' => 'Adăugați Câmp:',
+    'LBL_SEARCH_FORM_TITLE' => 'Cauta Angajat',
+    'COLUMN_TITLE_NAME' => 'Nume Câmp',
+    'COLUMN_TITLE_DISPLAY_LABEL' => 'Arata Eticheta',
+    'COLUMN_TITLE_LABEL_VALUE' => 'Valoare Eticheta',
+    'COLUMN_TITLE_LABEL' => 'Eticheta Sistem',
+    'COLUMN_TITLE_DATA_TYPE' => 'Tip de date',
+    'COLUMN_TITLE_MAX_SIZE' => 'Dimensiune maxima',
+    'COLUMN_TITLE_HELP_TEXT' => 'Ajutor',
+    'COLUMN_TITLE_COMMENT_TEXT' => 'Text comentariu',
+    'COLUMN_TITLE_REQUIRED_OPTION' => 'Câmp obligatoriu',
+    'COLUMN_TITLE_DEFAULT_VALUE' => 'Valoare implicită',
+    'COLUMN_TITLE_FRAME_HEIGHT' => 'Înălțime Iframe',
+    'COLUMN_TITLE_HTML_CONTENT' => 'HTML',
+    'COLUMN_TITLE_URL' => 'URL-ul prestabilit',
+    'COLUMN_TITLE_AUDIT' => 'Auditare',
+    'COLUMN_TITLE_MIN_VALUE' => 'Valoare minimă',
+    'COLUMN_TITLE_MAX_VALUE' => 'Valoare maximă',
+    'COLUMN_TITLE_LABEL_ROWS' => 'Rânduri',
+    'COLUMN_TITLE_LABEL_COLS' => 'Coloane',
+    'COLUMN_TITLE_DISPLAYED_ITEM_COUNT' => '# Elemente afișate',
+    'COLUMN_TITLE_AUTOINC_NEXT' => 'Creștere automată la următoarea valoare',
+    'COLUMN_DISABLE_NUMBER_FORMAT' => 'Dezactivare format',
+    'COLUMN_TITLE_ENABLE_RANGE_SEARCH' => 'Activează căutarea în Interval',
+    'LBL_DROP_DOWN_LIST' => 'Listă derulantă',
+    'LBL_RADIO_FIELDS' => 'Câmpuri radio',
+    'LBL_MULTI_SELECT_LIST' => 'Selectare multiplă în listă',
+    'COLUMN_TITLE_PRECISION' => 'Precizie',
+    'LBL_MODULE' => 'Modul',
+    'COLUMN_TITLE_MASS_UPDATE' => 'Actualizare selecție',
+    'COLUMN_TITLE_IMPORTABLE' => 'Importabil',
+    'COLUMN_TITLE_DUPLICATE_MERGE' => 'Contopire duplicate',
+    'LBL_LABEL' => 'Etichetă',
+    'LBL_DATA_TYPE' => 'Tip de date',
+    'LBL_DEFAULT_VALUE' => 'Valoare implicită',
+    'ERR_RESERVED_FIELD_NAME' => "Cuvânt cheie rezervat",
+    'ERR_SELECT_FIELD_TYPE' => 'Vă rugăm să selectați un tip de câmp',
+    'ERR_FIELD_NAME_ALREADY_EXISTS' => 'Numele câmpului exista deja',
+    'LBL_BTN_ADD' => 'Adăugați',
+    'LBL_BTN_EDIT' => 'Editați',
+    'LBL_GENERATE_URL' => 'Generare URL',
+    'LBL_CALCULATED' => 'Calculat',
+    'LBL_LINK_TARGET' => 'Link deschis in',
+    'LBL_IMAGE_WIDTH' => 'Lățime',
+    'LBL_IMAGE_HEIGHT' => 'Înălțime',
+    'LBL_IMAGE_BORDER' => 'Margine',
+    'LBL_HELP' => 'Ajutor' /*for 508 compliance fix*/,
+    'COLUMN_TITLE_INLINE_EDIT_TEXT' => 'Editare în linie',
+    'COLUMN_TITLE_PARENT_ENUM' => 'Listă derulantă Părinte',
+    'LBL_INSERT_FIELD' => 'Inserați câmp',
+);
