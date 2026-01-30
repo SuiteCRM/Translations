c6b437f3b91e2a9f91b35daf796807d111be386c
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-01-30 17:56:39 +0000

    New translations en_us.lang.php (Cebuano)

---
+++ translations/ceb_PH/modules/CampaignLog/language/ceb_PH.lang.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_LIST_ID' => 'Prospektong listahan sa. ID',
+    'LBL_ID' => 'ID',
+    'LBL_TARGET_TRACKER_KEY' => 'Target. Tagasunod. sa yabe',
+    'LBL_TARGET_ID' => 'Target ang ID',
+    'LBL_TARGET_TYPE' => 'Targetanan klase',
+    'LBL_ACTIVITY_TYPE' => 'Aktib sulat',
+    'LBL_ACTIVITY_DATE' => 'Aktibong Date',
+    'LBL_RELATED_ID' => 'Ig-labot Id',
+    'LBL_RELATED_TYPE' => 'Parehas lang og sulat',
+    'LBL_DELETED' => 'Gitangtang',
+    'LBL_MODULE_NAME' => 'Mangumpanya sa Log',
+    'LBL_LIST_RECIPIENT_EMAIL' => 'Resibo sa Email',
+    'LBL_LIST_RECIPIENT_NAME' => 'Resibo sa pangalan',
+    'LBL_ARCHIVED' => 'Maynakuha',
+    'LBL_HITS' => 'Naigo',
+
+    'LBL_CAMPAIGN_NAME' => 'Pangalan:',
+    'LBL_CAMPAIGN' => 'Kampanya:',
+    'LBL_NAME' => 'Ngalan: ',
+    'LBL_INVITEE' => 'Kontak',
+    'LBL_LIST_CAMPAIGN_NAME' => 'Mangompanya',
+    'LBL_LIST_STATUS' => 'Stado',
+    'LBL_LIST_TYPE' => 'Isulat',
+    'LBL_LIST_END_DATE' => 'Katapusang Date',
+    'LBL_DATE_ENTERED' => 'Date. ni sulod',
+    'LBL_DATE_MODIFIED' => 'Petsa nga gi-usab',
+    'LBL_MODIFIED' => 'Giusab ni: ',
+    'LBL_CREATED' => 'Gihimo ni: ',
+    'LBL_TEAM' => 'Grupo: ',
+    'LBL_ASSIGNED_TO' => 'Gigahin kang: ',
+    'LBL_CAMPAIGN_START_DATE' => 'Petsa sa Pagsugod: ',
+    'LBL_CAMPAIGN_END_DATE' => 'Petsa sa Pagtapos: ',
+    'LBL_CAMPAIGN_STATUS' => 'Istado: ',
+    'LBL_CAMPAIGN_BUDGET' => 'Badyet: ',
+    'LBL_CAMPAIGN_EXPECTED_COST' => 'Gilauman nga Gasto: ',
+    'LBL_CAMPAIGN_ACTUAL_COST' => 'Aktwal nga Gstyo: ',
+    'LBL_CAMPAIGN_EXPECTED_REVENUE' => 'Gilauman nga Kita: ',
+    'LBL_CAMPAIGN_TYPE' => 'Matang: ',
+    'LBL_CAMPAIGN_OBJECTIVE' => 'Tumong: ',
+    'LBL_CAMPAIGN_CONTENT' => 'Deskripsyon: ',
+    'LBL_CREATED_LEAD' => 'Buhat og Una',
+    'LBL_CREATED_CONTACT' => 'Paghimo og Kontak',
+    'LBL_CREATED_OPPORTUNITY' => 'Maghimo ug opurtunidad',
+    'LBL_TARGETED_USER' => 'Targertanan na manggagamit',
+    'LBL_SENT_EMAIL' => 'Napadala nga mga Email',
+    'LBL_LIST_FORM_TITLE' => 'Targetanan og Kampin',
+    'LBL_LIST_ACTIVITY_DATE' => 'Aktibong Date',
+    'LBL_RELATED' => 'May kalabutan',
+    'LBL_MORE_INFO' => 'Dugang pang impormasyon',
+
+    'LBL_CAMPAIGNS' => 'Mangompanya',
+    'LBL_LIST_MARKETING_NAME' => 'Magmamalengki',
+
+    'LBL_MARKETING_ID' => 'Magmamalengki',
+    'LBL_RELATED_NAME' => 'Parehas lang og sulat',
+);
