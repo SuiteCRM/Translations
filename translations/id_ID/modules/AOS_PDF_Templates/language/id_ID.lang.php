af0563d21bbec943490917249b5e970d4df8604c
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-01-30 17:33:11 +0000

    New translations en_us.lang.php (Indonesian)

---
+++ translations/id_ID/modules/AOS_PDF_Templates/language/id_ID.lang.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID' => 'ID Pengguna yang di-assign :',
+    'LBL_ASSIGNED_TO_NAME' => 'Ditugaskan kepada',
+    'LBL_ID' => 'ID',
+    'LBL_DATE_ENTERED' => 'Tanggal dibuat',
+    'LBL_DATE_MODIFIED' => 'Tanggal dibuat',
+    'LBL_MODIFIED' => 'Diubah Oleh',
+    'LBL_MODIFIED_NAME' => 'Diubah oleh Nama',
+    'LBL_CREATED' => 'Dibuat Oleh',
+    'LBL_DESCRIPTION' => 'Tubuh',
+    'LBL_HEADER' => 'Header',
+    'LBL_FOOTER' => 'Footer',
+    'LBL_DELETED' => 'Dihapus',
+    'LBL_NAME' => 'Nama',
+    'LBL_CREATED_USER' => 'Dibuat oleh Pengguna',
+    'LBL_MODIFIED_USER' => 'Diubah oleh Pengguna',
+    'LBL_LIST_FORM_TITLE' => 'Daftar Template PDF',
+    'LBL_MODULE_NAME' => 'Template PDF',
+    'LBL_MODULE_TITLE' => 'Template PDF: Beranda',
+    'LBL_HOMEPAGE_TITLE' => 'Template PDF saya',
+    'LNK_NEW_RECORD' => 'Buat Template PDF',
+    'LNK_LIST' => 'Lihat PDF Template',
+    'LBL_SEARCH_FORM_TITLE' => 'Cari PDF Template',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Lihat Histori',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Aktifitas',
+    'LBL_NEW_FORM_TITLE' => 'Template PDF baru',
+    'LBL_TYPE' => 'Tipe',
+    'LBL_ACTIVE' => 'Aktif',
+    'LBL_BUTTON_INSERT' => 'Sisipkan',
+    'LBL_WARNING_OVERWRITE' => 'Peringatan ini akan menimpa pekerjaan Anda saat ini',
+    'LBL_INSERT_FIELDS' => 'Menyisipkan Bidang',
+
+    'LBL_SAMPLE' => 'Muat Sampel',
+    'LBL_PAGE' => 'Halaman',
+    'LBL_PREPARED_FOR' => 'Bersiap-siap untuk',
+    'LBL_PREPARED_BY' => 'Disiapkan oleh',
+    'LBL_QUOTE_SAMPLE' => 'Contoh kutipan',
+    'LBL_INVOICE_SAMPLE' => 'Contoh faktur',
+    'LBL_ACCOUNT_SAMPLE' => 'Contoh akun',
+    'LBL_CONTACT_SAMPLE' => 'Contoh kontak',
+    'LBL_LEAD_SAMPLE' => 'Memimpin Sampel',
+    'LBL_ANY_STREET' => 'Setiap Jalan',
+    'LBL_ANY_TOWN' => 'Setiap Kota',
+    'LBL_ANY_WHERE' => 'Setiap Tempat',
+
+    'LBL_QUOTE_GROUP_SAMPLE' => 'Quote Kelompok Sampel',
+    'LBL_INVOICE_GROUP_SAMPLE' => 'Faktur Kelompok Sampel',
+    'LBL_MARGIN_LEFT' => 'Batas kiri',
+    'LBL_MARGIN_RIGHT' => 'Batas kanan',
+    'LBL_MARGIN_TOP' => 'Batas atas',
+    'LBL_MARGIN_BOTTOM' => 'Batas bawah',
+    'LBL_MARGIN_HEADER' => 'Batas header',
+    'LBL_MARGIN_FOOTER' => 'Batas footer',
+    'LBL_EDITVIEW_PANEL1' => 'Batas',
+    'LBL_DETAILVIEW_PANEL1' => 'Batas',
+    'LBL_PAGE_SIZE' => 'Ukuran Halaman',
+    'LBL_ORIENTATION' => 'Arah',
+);
