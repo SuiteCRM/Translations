185b5ca98817d61a9b4e78f79278edf637a7584a
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-01-30 18:45:53 +0000

    New translations en_us.lang.php (Serbian (Latin))

---
+++ translations/sr_CS/modules/MailMerge/language/sr_CS.lang.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_STEP_1' => 'Korak 1: Izaberite modul i šablon',
+    'LBL_MAILMERGE_MODULE' => 'Izaberite modul:',
+    'LBL_MAILMERGE_SELECTED_MODULE' => 'Izabran modul:',
+    'LBL_MAILMERGE_TEMPLATES' => 'Izaberite šablon:',
+    'LBL_STEP_4' => 'Pregled i kompletiranje',
+    'LBL_SELECTED_MODULE' => 'Izabran modul:',
+    'LBL_SELECTED_TEMPLATE' => 'Izabran šablon:',
+    'LBL_SELECTED_ITEMS' => 'Izabrane stavke:',
+    'LBL_STEP_5' => 'Kompletiranje spajanja pošte',
+    'LBL_MERGED_FILE' => 'Spojeni fajl:',
+    'LNK_NEW_MAILMERGE' => 'Počnite spajanje pošte',
+    'LNK_UPLOAD_TEMPLATE' => 'Pošalji šablon',
+    'LBL_DOC_NAME' => 'Ime dokumenta:',
+    'LBL_FILENAME' => 'Ime fajla:',
+    'LBL_DOC_VERSION' => 'Revizija:',
+    'LBL_DOC_DESCRIPTION' => 'Opis:',
+    'LBL_LIST_NAME' => 'Ime',
+    'LBL_FINISH' => 'Počnite spajanje',
+    'LBL_NEXT' => 'Sledeći >',
+    'LBL_BACK' => '< Nazad',
+    'LBL_START' => 'Kliknite ovde da nastavite',
+    'LBL_CONTAINS_CONTACT_INFO' => 'Izabrani šablon sadrži povezano',
+    'LBL_ADDIN_NOTICE' => 'Ovo zahteva instalaciju SuiteCRM Mail Merge dodatka za Microsoft Word.',
+    'LBL_REMOVE' => 'Ukloni' /*for 508 compliance fix*/,
+    'LBL_ADD' => 'Dodaj' /*for 508 compliance fix*/,
+    'LBL_BROWSER_REQUIREMENT' => 'Ovo zahteva IE 6.0 ili noviji za Windows.',
+    'LBL_FORM_SELECT_TEMPLATE' => 'Izaberite šablon:',
+    'LBL_SELECT_ONE' => 'Izaberite bar jednu stavku.',
+    'LBL_AVAILABLE' => 'Dostupno',
+    'LBL_SELECTED' => 'Izabran',
+    'LBL_RELATED_INFO_SELECTION' => 'Uz pomoć selekcije ispod izaberite povezane informacije koje želite da prikažete u vašem spojenom dokumentu. Preskočite ovaj korak ako dokument koji koristite kao šablon ne sadrži povezane informacije.',
+
+);
