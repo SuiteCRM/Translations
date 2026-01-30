09a999518a3eb8f6a72278211f0a9b3e643b7c22
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-01-30 19:05:10 +0000

    New translations en_us.lang.php (Filipino)

---
+++ translations/fil_PH/modules/ProspectLists/language/fil_PH.lang.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_MODULE_NAME' => 'Mga listahan ng target',
+    'LBL_MODULE_ID' => 'Mga listahan ng target',
+    'LBL_MODULE_TITLE' => 'Mga listahan ng target: Home',
+    'LBL_SEARCH_FORM_TITLE' => 'Mga target ng paghahanap',
+    'LBL_LIST_FORM_TITLE' => 'Mga listahan ng target',
+    'LBL_PROSPECT_LIST_NAME' => 'Mga listahan ng target:',
+    'LBL_NAME' => 'Ang pangalan',
+    'LBL_ENTRIES' => 'Mga kabuuang entry',
+    'LBL_LIST_PROSPECT_LIST_NAME' => 'Listahan ng. target',
+    'LBL_LIST_ENTRIES' => 'Mga targe ay nasa listahan',
+    'LBL_LIST_DESCRIPTION' => 'Ang deskripsyon',
+    'LBL_LIST_TYPE_NO' => 'Uri',
+    'LBL_LIST_END_DATE' => 'Katapusan ng petsa',
+    'LBL_DATE_ENTERED' => 'Ang petsa ng pagkakalikha',
+    'LBL_MARKETING_ID' => 'Id ng marketing',
+    'LBL_DATE_MODIFIED' => 'Ang petsa ng pagkabago',
+    'LBL_MODIFIED' => 'Binago ni',
+    'LBL_CREATED' => 'Inilikha sa pamamagitan ng',
+    'LBL_ASSIGNED_TO' => 'Itinatalaga sa',
+    'LBL_DESCRIPTION' => 'Ang deskripsyon',
+    'LNK_NEW_CAMPAIGN' => 'Lumikha ng kampanya',
+    'LNK_CAMPAIGN_LIST' => 'Ang mga Kampanya',
+    'LNK_NEW_PROSPECT_LIST' => 'Lumikha ng listahan na target',
+    'LNK_PROSPECT_LIST_LIST' => 'Tingnan ang mga listahan ng target',
+    'LBL_MODIFIED_BY' => 'Binago ni',
+    'LBL_CREATED_BY' => 'Inilikha sa pamamagitan ng',
+    'LNK_NEW_PROSPECT' => 'Lumikha ng target',
+    'LNK_PROSPECT_LIST' => 'Ang Mga target',
+
+    'LBL_CONTACTS_SUBPANEL_TITLE' => 'Mga kontak',
+    'LBL_LEADS_SUBPANEL_TITLE' => 'Mga naggagabay',
+    'LBL_PROSPECTS_SUBPANEL_TITLE' => 'Ang Mga target',
+    'LBL_ACCOUNTS_SUBPANEL_TITLE' => 'Mga account',
+    'LBL_COPY_PREFIX' => 'Kopya ng',
+    'LBL_USERS_SUBPANEL_TITLE' => 'Ang mga gumagamit',
+    'LBL_TYPE' => 'Uri',
+    'LBL_LIST_TYPE' => 'Uri',
+    'LBL_LIST_TYPE_LIST_NAME' => 'Uri',
+    'LBL_NEW_FORM_TITLE' => 'Bagong listahan ng target',
+    'LBL_MARKETING_NAME' => 'Pangalan ng marketing',
+    'LBL_DOMAIN_NAME' => 'Pangalan ng domain',
+    'LBL_DOMAIN' => 'Walang mga emails para sa domain',
+    'LBL_LIST_PROSPECTLIST_NAME' => 'Ang pangalan',
+
+    'LBL_EMAIL_MARKETING' => 'Ang Email Maketing',
+
+    'LBL_ASCENDING' => 'Pataas',
+    'LBL_DESCENDING' => 'Pababa',
+);
