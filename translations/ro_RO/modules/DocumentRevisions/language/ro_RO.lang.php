fed569f664511e4fa4b53faf184e73be867bd0d0
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-01-30 18:14:34 +0000

    New translations en_us.lang.php (Romanian)

---
+++ translations/ro_RO/modules/DocumentRevisions/language/ro_RO.lang.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    //module
+    'LBL_MODULE_NAME' => 'Revizii de documente',
+
+    'LNK_NEW_DOCUMENT' => 'Nou Document',
+    'LNK_DOCUMENT_LIST' => 'Documente',
+
+    //vardef labels
+    'LBL_REVISION_NAME' => 'Numărul reviziei',
+    'LBL_FILENAME' => 'Fișier:',
+    'LBL_MIME' => 'Tip Mime',
+    'LBL_REVISION' => 'Revizie',
+    'LBL_DOCUMENT' => 'Documente în legătură',
+    'LBL_LATEST_REVISION' => 'Ultima revizie',
+    'LBL_CHANGE_LOG' => 'Jurnalul de modificări',
+    'LBL_ACTIVE_DATE' => 'Dată Publicare',
+    'LBL_EXPIRATION_DATE' => 'Data expirării',
+    'LBL_FILE_EXTENSION' => 'Extensia fișierului',
+
+    'LBL_DOC_NAME' => 'Nume document:',
+    'LBL_DOC_VERSION' => 'Revizie:',
+
+    //document revisions.
+    'LBL_REV_LIST_REVISION' => 'Revizie',
+    'LBL_REV_LIST_ENTERED' => 'Data creării',
+    'LBL_REV_LIST_CREATED' => 'Creat de',
+    'LBL_REV_LIST_LOG' => 'Jurnalul de modificări',
+    'LBL_REV_LIST_FILENAME' => 'Fișier',
+
+    'LBL_CURRENT_DOC_VERSION' => 'Ultima revizie:',
+    'LBL_SEARCH_FORM_TITLE' => 'Caută documente',
+    'LBL_REVISIONS' => 'Revizii',
+
+    //error messages
+    'ERR_FILENAME' => 'Numele fișierului',
+    'ERR_DOC_VERSION' => 'Versiune document',
+    'ERR_DELETE_CONFIRM' => 'Doriți să ștergeți această revizuire a documentului?',
+    'ERR_DELETE_LATEST_VERSION' => 'Nu aveți permisiunea de a șterge ultima revizuire a unui document.',
+    'LNK_NEW_MAIL_MERGE' => 'Preluare email',
+    'LBL_DOC_ID' => 'Sursa document de identitate',
+    'LBL_DOC_TYPE' => 'Sursa',
+    'LBL_DOC_URL' => 'Document Sursa URL',
+
+    'LBL_CREATED_BY_NAME' => 'Creat de Nume',
+);
