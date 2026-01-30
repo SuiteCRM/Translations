c83442ba0310fb533e14d92188208fe1556709fa
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-01-30 17:02:43 +0000

    New translations en_us.lang.php (Indonesian)

---
+++ translations/id_ID/include/SugarObjects/templates/sale/language/id_ID.lang.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_MODULE_NAME' => 'Penjualan',
+    'LBL_MODULE_TITLE' => 'Penjualan: Home',
+    'LBL_SEARCH_FORM_TITLE' => 'Cari Penjualan',
+    'LBL_LIST_FORM_TITLE' => 'Daftar Penjualan',
+    'LBL_NAME' => 'Nama Penjualan',
+    'LBL_LIST_SALE_NAME' => 'Nama',
+    'LBL_LIST_ACCOUNT_NAME' => 'Nama Akun',
+    'LBL_LIST_AMOUNT' => 'Jumlah',
+    'LBL_LIST_DATE_CLOSED' => 'Tutup',
+    'LBL_LIST_SALE_STAGE' => 'Tahapan Penjualan',
+    'LBL_ACCOUNT_ID' => 'ID Akun',
+    //DON'T CONVERT THESE THEY ARE MAPPINGS
+    'db_name' => 'LBL_NAME',
+    //END DON'T CONVERT
+    'LBL_ACCOUNT_NAME' => 'Nama Akun:',
+    'LBL_AMOUNT' => 'Jumlah:',
+    'LBL_AMOUNT_USDOLLAR' => 'Jumlah USD:',
+    'LBL_CURRENCY' => 'Mata Uang:',
+    'LBL_DATE_CLOSED' => 'Close Date yang Diharapkan:',
+    'LBL_TYPE' => 'Jenis:',
+    'LBL_CAMPAIGN' => 'Kampanye:',
+    'LBL_LEADS_SUBPANEL_TITLE' => 'Lead',
+    'LBL_PROJECTS_SUBPANEL_TITLE' => 'Proyek',
+    'LBL_NEXT_STEP' => 'Tahap Selanjutnya:',
+    'LBL_LEAD_SOURCE' => 'Sumber Lead:',
+    'LBL_SALES_STAGE' => 'Tahap penjualan:',
+    'LBL_PROBABILITY' => 'Peluang (%):',
+    'LBL_DESCRIPTION' => 'Deskripsi:',
+    'LBL_DUPLICATE' => 'Duplikasi penjualan yang mungkin',
+    'MSG_DUPLICATE' => 'Record penjualan yang akan dibuat sepertinya memiliki duplikat, record penjualan yang serupa sudah ada.Record penjualan yang memiliki nama yang sama ditampilkan di bawah. <br>Klik Save untuk melanjutkan membuat penjualan yang baru atau klik Cancel untuk kembali ke module sebelumnya tanpa membuat penjualan. ',
+    'LBL_NEW_FORM_TITLE' => 'Buat Penjualan',
+    'ERR_DELETE_RECORD' => 'No Record harus dipilih untuk menghapus penjualan.',
+    'LBL_DEFAULT_SUBPANEL_TITLE' => 'Penjualan',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Aktifitas',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Riwayat',
+
+    'LBL_CONTACTS_SUBPANEL_TITLE' => 'Kontak',
+    'LBL_ASSIGNED_TO_NAME' => 'Pengguna:',
+    'LBL_LIST_ASSIGNED_TO_NAME' => 'Pengguna yang ditugaskan',
+    'LBL_ASSIGNED_TO_ID' => 'Tugaskan kepada ID',
+    'LBL_MODIFIED_NAME' => 'Nama yang memodifikasi',
+    'LBL_SALE_INFORMATION' => 'Informasi Penjualan',
+    'LBL_CURRENCY_NAME' => 'Nama Mata Uang',
+    'LBL_CURRENCY_SYMBOL' => 'Simbol Mata Uang',
+    'LBL_EDIT_BUTTON' => 'Sunting',
+    'LBL_REMOVE' => 'Menghapus',
+
+);
