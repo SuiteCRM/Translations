c983fa2524e279d77f7188f85929ec3b26919cbd
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-01-30 17:02:50 +0000

    New translations en_us.lang.php (Estonian)

---
+++ translations/et_EE/include/SugarObjects/templates/sale/language/et_EE.lang.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_MODULE_NAME' => 'Müük',
+    'LBL_MODULE_TITLE' => 'Müük: Avaleht',
+    'LBL_SEARCH_FORM_TITLE' => 'Müügi otsing',
+    'LBL_LIST_FORM_TITLE' => 'Müügi loend',
+    'LBL_NAME' => 'Müügi nimi',
+    'LBL_LIST_SALE_NAME' => 'Nimi',
+    'LBL_LIST_ACCOUNT_NAME' => 'Ettevõtte nimi:',
+    'LBL_LIST_AMOUNT' => 'Summa',
+    'LBL_LIST_DATE_CLOSED' => 'Sulge',
+    'LBL_LIST_SALE_STAGE' => 'Müügi staadium',
+    'LBL_ACCOUNT_ID' => 'Ettevõtte Id',
+    //DON'T CONVERT THESE THEY ARE MAPPINGS
+    'db_name' => 'LBL_NAME',
+    //END DON'T CONVERT
+    'LBL_ACCOUNT_NAME' => 'Ettevõtte nimi:',
+    'LBL_AMOUNT' => 'Summa:',
+    'LBL_AMOUNT_USDOLLAR' => 'Summa USD:',
+    'LBL_CURRENCY' => 'Valuuta:',
+    'LBL_DATE_CLOSED' => 'Oodatav sulgemiskuupäev:',
+    'LBL_TYPE' => 'Tüüp:',
+    'LBL_CAMPAIGN' => 'Kampaania:',
+    'LBL_LEADS_SUBPANEL_TITLE' => 'Müügivihjed',
+    'LBL_PROJECTS_SUBPANEL_TITLE' => 'Projektid',
+    'LBL_NEXT_STEP' => 'Järgmine samm:',
+    'LBL_LEAD_SOURCE' => 'Müügivihje allikas:',
+    'LBL_SALES_STAGE' => 'Müügistaadium:',
+    'LBL_PROBABILITY' => 'Tõenäosus (%)',
+    'LBL_DESCRIPTION' => 'Kirjeldus',
+    'LBL_DUPLICATE' => 'Võimalik topeltmüük',
+    'MSG_DUPLICATE' => 'Loodav müügikirje võib olla juba eksisteeriv duplikaat müügikirje. Müügikirjed, mis sisaldavad sarnaseid nimesid on väljatoodud allpool. Kliki Salvesta selle uue müügi loomiseks või kliki Tühista moodulisse tagasisiirdumiseks ilma müüki loomata.',
+    'LBL_NEW_FORM_TITLE' => 'Loo müük',
+    'ERR_DELETE_RECORD' => 'Müügi kustutamiseks täpsusta kirje numbrit.',
+    'LBL_DEFAULT_SUBPANEL_TITLE' => 'Müük',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Tegevused',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Ajalugu',
+
+    'LBL_CONTACTS_SUBPANEL_TITLE' => 'Kontaktid',
+    'LBL_ASSIGNED_TO_NAME' => 'Kasutaja:',
+    'LBL_LIST_ASSIGNED_TO_NAME' => 'Määratud kasutaja',
+    'LBL_ASSIGNED_TO_ID' => 'Vastutaja ID:',
+    'LBL_MODIFIED_NAME' => 'Muutja nime järgi',
+    'LBL_SALE_INFORMATION' => 'Müügiinfo',
+    'LBL_CURRENCY_NAME' => 'Valuuta nimi',
+    'LBL_CURRENCY_SYMBOL' => 'Valuuta sümbol',
+    'LBL_EDIT_BUTTON' => 'Redigeeri',
+    'LBL_REMOVE' => 'Eemalda',
+
+);
