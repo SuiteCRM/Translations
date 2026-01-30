f9078ab071b76c524b0f40d64208ead80d3106b6
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-01-30 19:07:10 +0000

    New translations en_us.lang.php (Korean)

---
+++ translations/ko_KR/modules/Relationships/language/ko_KR.lang.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ID' => 'Relationship Id',
+    'LBL_RELATIONSHIP_NAME' => 'Relationship Name',
+    'LBL_LHS_MODULE' => 'LHS 모듈명',
+    'LBL_LHS_TABLE' => 'LHS 테이블명',
+    'LBL_LHS_KEY' => 'LHS 키 이름',
+    'LBL_RHS_MODULE' => 'RHS 모듈 이름',
+    'LBL_RHS_TABLE' => 'RHS 테이블명',
+    'LBL_RHS_KEY' => 'RHS 키 이름',
+    'LBL_JOIN_TABLE' => '조인 테이블명',
+    'LBL_JOIN_KEY_LHS' => '조인 Key LHS',
+    'LBL_JOIN_KEY_RHS' => '조인 Key RHS',
+    'LBL_RELATIONSHIP_TYPE' => '관계 유형',
+    'LBL_RELATIONSHIP_ROLE_COLUMN' => 'Relationship Role Column Name',
+    'LBL_RELATIONSHIP_ROLE_COLUMN_VALUE' => 'Relationship Role Column Value',
+    'LBL_REVERSE' => 'Reverse',
+    'LBL_DELETED' => '삭제됨',
+);
