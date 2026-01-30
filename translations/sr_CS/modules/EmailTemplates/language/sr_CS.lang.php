5e162d26d1bcda40f43895c306a148a61230a910
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-01-30 18:24:42 +0000

    New translations en_us.lang.php (Serbian (Latin))

---
+++ translations/sr_CS/modules/EmailTemplates/language/sr_CS.lang.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ADD_DOCUMENT' => 'Dodaj dokument',
+    'LBL_ADD_FILE' => 'Dodaj fajl',
+    'LBL_ATTACHMENTS' => 'Dodaci',
+    'LBL_BODY' => 'Tekst: ',
+    'LBL_CLOSE' => 'Zatvori:',
+    'LBL_DESCRIPTION' => 'Opis:',
+    'LBL_EDIT_ALT_TEXT' => 'Izmeni običan tekst',
+    'LBL_EMAIL_ATTACHMENT' => 'Pošalji e-poštom dodatak',
+    'LBL_HIDE_ALT_TEXT' => 'Sakrij običan tekst',
+    'LBL_HTML_BODY' => 'Telo HTML-a',
+    'LBL_INSERT_VARIABLE' => 'Unesi promenljivu:',
+    'LBL_INSERT_URL_REF' => 'Unesi URL reference',
+    'LBL_INSERT_TRACKER_URL' => 'Unesite URL sistema za praćenje :',
+    'LBL_INSERT' => 'Ubaci',
+    'LBL_LIST_DATE_MODIFIED' => 'Poslednja izmena',
+    'LBL_LIST_DESCRIPTION' => 'Opis',
+    'LBL_LIST_FORM_TITLE' => 'Pošalji e-porukom listu šablona',
+    'LBL_LIST_NAME' => 'Ime',
+    'LBL_MODULE_NAME' => 'Email šabloni',
+    'LBL_MODULE_TITLE' => 'Šabloni elektronske pošte: Početna strana',
+    'LBL_NAME' => 'Ime:',
+    'LBL_NEW_FORM_TITLE' => 'Kreiraj šablon elektronske pošte',
+    'LBL_PUBLISH' => 'Objava:',
+    'LBL_RELATED_TO' => 'Povezano sa:',
+    'LBL_SEARCH_FORM_TITLE' => 'Pretraživanje predložaka e-poruka:',
+    'LBL_SHOW_ALT_TEXT' => 'Prikaži običan tekst',
+    'LBL_SUBJECT' => 'Naslov:',
+    'LBL_SUITE_DOCUMENT' => 'Dokumenti',
+    'LBL_UPLOAD_FILE' => 'Upload File',
+    'LBL_TEXT_BODY' => 'Telo teksta',
+    'LBL_USERS' => 'Korisnici',
+
+    'LNK_EMAIL_TEMPLATE_LIST' => 'Prikaži šablone elektronske pošte',
+    'LNK_IMPORT_NOTES' => 'Uvezi beleške',
+    'LNK_NEW_EMAIL_TEMPLATE' => 'Kreiraj šablon elektronske pošte',
+    'LNK_NEW_EMAIL' => 'Arhiviraj Email',
+    'LNK_NEW_SEND_EMAIL' => 'Sastavi Email',
+    'LNK_SENT_EMAIL_LIST' => 'Poslane elektronske poruke:',
+    'LNK_VIEW_CALENDAR' => 'Danas',
+    // for Inbox
+    'LBL_NEW' => 'Novo',
+    'LNK_MY_DRAFTS' => 'Moji nacrti',
+    'LNK_MY_INBOX' => 'Moja elektronska pošta',
+    'LBL_TEXT_ONLY' => 'Telo teksta',
+    'LBL_SEND_AS_TEXT' => 'Pošalji samo tekst',
+    'LBL_ACCOUNT' => 'Kompanija',
+    'LBL_FROM_NAME' => 'Ime pošiljaoca',
+    'LBL_PLAIN_TEXT' => 'Čist tekst',
+    'LBL_CREATED_BY' => 'Autor',
+    'LBL_PUBLISHED' => 'Objavljen',
+    'LNK_VIEW_MY_INBOX' => 'Pregledaj Moju Elektronsku Poštu ',
+    'LBL_ASSIGNED_TO_ID' => 'Dodeljeno',
+    'LBL_EDIT_LAYOUT' => 'Izmeni izgled' /*for 508 compliance fix*/,
+    'LBL_SELECT' => 'Izaberi' /*for 508 compliance fix*/,
+    'LBL_ID_FF_CLEAR' => 'Obriši' /*for 508 compliance fix*/,
+    'LBL_TYPE' => 'Tip',
+    'LBL_WIDTH' => 'Podrazumevana širina',
+    'LNK_IMPORT_CAMPAIGNS' => 'Import Campaign',
+);
