668561f76f1fe4905ffd02e4e4efd6b9e9e0ecf7
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-01-30 18:24:35 +0000

    New translations en_us.lang.php (Filipino)

---
+++ translations/fil_PH/modules/EmailTemplates/language/fil_PH.lang.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ADD_DOCUMENT' => 'Magdagdag ng isang Dokumento',
+    'LBL_ADD_FILE' => 'Magdagdag ng isang File',
+    'LBL_ATTACHMENTS' => 'Mga Attachment',
+    'LBL_BODY' => 'Katawan: ',
+    'LBL_CLOSE' => 'Isara:',
+    'LBL_DESCRIPTION' => 'Ang deskripsyon:',
+    'LBL_EDIT_ALT_TEXT' => 'I-edit ang Plain Text',
+    'LBL_EMAIL_ATTACHMENT' => 'Email ng paglalakip',
+    'LBL_HIDE_ALT_TEXT' => 'Itago ang teksto na Plain',
+    'LBL_HTML_BODY' => 'Katawan ng HTML',
+    'LBL_INSERT_VARIABLE' => 'Mag-insert ng Variable:',
+    'LBL_INSERT_URL_REF' => 'Magsama ng Sanggunian ng URL',
+    'LBL_INSERT_TRACKER_URL' => 'Magpasok ng Tagasubaybay na URL:',
+    'LBL_INSERT' => 'Isingit',
+    'LBL_LIST_DATE_MODIFIED' => 'Huling Binago',
+    'LBL_LIST_DESCRIPTION' => 'Ang deskripsyon',
+    'LBL_LIST_FORM_TITLE' => 'Listahan ng mga Template ng Email',
+    'LBL_LIST_NAME' => 'Ang pangalan',
+    'LBL_MODULE_NAME' => 'Mga template sa Email',
+    'LBL_MODULE_TITLE' => 'Mga template ng Email: Home',
+    'LBL_NAME' => 'Pangalan:',
+    'LBL_NEW_FORM_TITLE' => 'Lumikha ng Template ng Email',
+    'LBL_PUBLISH' => 'I-publish:',
+    'LBL_RELATED_TO' => 'Ini-ugnay sa:',
+    'LBL_SEARCH_FORM_TITLE' => 'Paghahanap ng mga Template ng Email',
+    'LBL_SHOW_ALT_TEXT' => 'Ipakita ang Plain Text',
+    'LBL_SUBJECT' => 'Paksa:',
+    'LBL_SUITE_DOCUMENT' => 'Dokumento',
+    'LBL_UPLOAD_FILE' => 'Upload File',
+    'LBL_TEXT_BODY' => 'Katawan ng teksto',
+    'LBL_USERS' => 'Ang mga gumagamit',
+
+    'LNK_EMAIL_TEMPLATE_LIST' => 'Tingnan ang Mga Template ng Email',
+    'LNK_IMPORT_NOTES' => 'I-import ang mga talaan',
+    'LNK_NEW_EMAIL_TEMPLATE' => 'Lumikha ng Template ng Email',
+    'LNK_NEW_EMAIL' => 'I-archive ang email',
+    'LNK_NEW_SEND_EMAIL' => 'Ang email ay binuo',
+    'LNK_SENT_EMAIL_LIST' => 'Napadala na Mga Email',
+    'LNK_VIEW_CALENDAR' => 'Sa araw na ito',
+    // for Inbox
+    'LBL_NEW' => 'Bago',
+    'LNK_MY_DRAFTS' => 'Aking mga draft',
+    'LNK_MY_INBOX' => 'Aking Email',
+    'LBL_TEXT_ONLY' => 'Teksto lamang',
+    'LBL_SEND_AS_TEXT' => 'Magpadala lamang ng teksto',
+    'LBL_ACCOUNT' => 'Ang akawnt',
+    'LBL_FROM_NAME' => 'Pangalan mula sa',
+    'LBL_PLAIN_TEXT' => 'Plain na Teksto',
+    'LBL_CREATED_BY' => 'Ito ay nalikha sa pamamagitan ng',
+    'LBL_PUBLISHED' => 'Napalabas',
+    'LNK_VIEW_MY_INBOX' => 'I-view ang aking Email',
+    'LBL_ASSIGNED_TO_ID' => 'Ini-assign sa',
+    'LBL_EDIT_LAYOUT' => 'Baguhin ang layout' /*for 508 compliance fix*/,
+    'LBL_SELECT' => 'Piliin' /*for 508 compliance fix*/,
+    'LBL_ID_FF_CLEAR' => 'Malinaw' /*for 508 compliance fix*/,
+    'LBL_TYPE' => 'Uri',
+    'LBL_WIDTH' => 'Default na Lapad',
+    'LNK_IMPORT_CAMPAIGNS' => 'Mag-import ng Kampanya',
+);
