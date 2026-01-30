97cea6c869152cde007bfb026bb207b1fb918f8f
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-01-30 17:10:34 +0000

    New translations en_us.lang.php (Cebuano)

---
+++ translations/ceb_PH/modules/ACLActions/language/ceb_PH.lang.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ACCESS_ALL' => 'Tanan',
+    'LBL_ACCESS_NONE' => 'Wala',
+    'LBL_ACCESS_OWNER' => 'Tag-iya',
+    'LBL_ACCESS_GROUP' => 'Grupo',
+    'LBL_ACCESS_NORMAL' => 'Normal',
+    'LBL_ACCESS_ADMIN' => 'Admin',
+    'LBL_ACCESS_ENABLED' => 'Gipagana',
+    'LBL_ACCESS_DISABLED' => 'Wala paganaha',
+    'LBL_ACCESS_DEV' => 'Tig-develop',
+    'LBL_ACCESS_ADMIN_DEV' => 'Admin & tig-develop',
+    'LBL_NAME' => 'Pangalan',
+    'LBL_DESCRIPTION' => 'Deskripsyon',
+    'LIST_ROLES' => 'Listahan sa mga papel',
+    'LBL_USERS_SUBPANEL_TITLE' => 'Tig gamit',
+    'LIST_ROLES_BY_USER' => 'Listahan sa mga papel base sa tiggamitan',
+    'LBL_ROLES_SUBPANEL_TITLE' => 'Mga papel sa tiggamitan',
+    'LBL_SEARCH_FORM_TITLE' => 'Pangitaa',
+    'LBL_ACTION_VIEW' => 'Tan-awa',
+    'LBL_ACTION_EDIT' => 'Usba',
+    'LBL_ACTION_DELETE' => 'Tangtangon',
+    'LBL_ACTION_IMPORT' => 'Import',
+    'LBL_ACTION_EXPORT' => 'I-export',
+    'LBL_ACTION_LIST' => 'Listahan',
+    'LBL_ACTION_MASSUPDATE' => 'Mass update',
+    'LBL_ACTION_ACCESS' => 'I-access',
+    'LBL_ACTION_ADMIN' => 'Matang sa pag-access',
+    'LBL_ACCESS_DEFAULT' => 'Wala ma set',
+);
