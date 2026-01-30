532272be7509a128c8e82238cc5248e40919de96
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-01-30 17:02:57 +0000

    New translations en_us.lang.php (Tagalog)

---
+++ translations/tl_PH/include/SugarObjects/templates/sale/language/tl_PH.lang.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_MODULE_NAME' => 'Ang Sale',
+    'LBL_MODULE_TITLE' => 'Sale: Bahay',
+    'LBL_SEARCH_FORM_TITLE' => 'Magsaliksik ng Sale',
+    'LBL_LIST_FORM_TITLE' => 'Listahan ng Sale',
+    'LBL_NAME' => 'Pangalan ng Sale',
+    'LBL_LIST_SALE_NAME' => 'Ang Pangalan',
+    'LBL_LIST_ACCOUNT_NAME' => 'Ang Pangalan ng akawnt',
+    'LBL_LIST_AMOUNT' => 'Halaga',
+    'LBL_LIST_DATE_CLOSED' => 'Sarado',
+    'LBL_LIST_SALE_STAGE' => 'Imbakan ng Sale',
+    'LBL_ACCOUNT_ID' => 'Ang ID ng account',
+    //DON'T CONVERT THESE THEY ARE MAPPINGS
+    'db_name' => 'LBL_NAME',
+    //END DON'T CONVERT
+    'LBL_ACCOUNT_NAME' => 'Ang pangalan ng account:',
+    'LBL_AMOUNT' => 'Halaga:',
+    'LBL_AMOUNT_USDOLLAR' => 'Halaga sa USD:',
+    'LBL_CURRENCY' => 'Ang Halaga:',
+    'LBL_DATE_CLOSED' => 'Ang inaasahan na sarado na petsa:',
+    'LBL_TYPE' => 'Uri:',
+    'LBL_CAMPAIGN' => 'Kampanya:',
+    'LBL_LEADS_SUBPANEL_TITLE' => 'Pamunuan',
+    'LBL_PROJECTS_SUBPANEL_TITLE' => 'Ang mga proyekto',
+    'LBL_NEXT_STEP' => 'Susunod na Hakabng:',
+    'LBL_LEAD_SOURCE' => 'Pinagmulan ng Lead:',
+    'LBL_SALES_STAGE' => 'Mga Antas ng Sale:',
+    'LBL_PROBABILITY' => 'Probabilidad (%):',
+    'LBL_DESCRIPTION' => 'Ang Paglalarawan:',
+    'LBL_DUPLICATE' => 'Ang Posible na Kopya ng Sale',
+    'MSG_DUPLICATE' => 'Ang Tala sa Sale na iyong nilikha ay magiging duplicate ng sale sa nakatala na umiiral na. Ang mga nakatala na Sale ay naglalaman ng mga magkatulad na pangalan sa listahan na nasa ibaba.<br> I-Click ang Save para magpatuloy sa paglikha ng bagong sale na ito, o kaya naman i-click ang kanselsa upang bumalik sa modyul na hindi kasama ang nilikha na sale.',
+    'LBL_NEW_FORM_TITLE' => 'Lumikha ng Sale',
+    'ERR_DELETE_RECORD' => 'Ang nakatala na numero kinakailangan na matukoy na burahin sa pagbebenta.',
+    'LBL_DEFAULT_SUBPANEL_TITLE' => 'Ang Sale',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Ang mga aktibidad',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Kasaysayan',
+
+    'LBL_CONTACTS_SUBPANEL_TITLE' => 'Ang mga koneksyon',
+    'LBL_ASSIGNED_TO_NAME' => 'Ang gumagamit:',
+    'LBL_LIST_ASSIGNED_TO_NAME' => 'Magtalaga ng gumagamit',
+    'LBL_ASSIGNED_TO_ID' => 'Nakatalaga para sa ID',
+    'LBL_MODIFIED_NAME' => 'Binagi sa pamamagitan ng Gumagamit ng Pangalan',
+    'LBL_SALE_INFORMATION' => 'Ang Impormasyon sa Sale',
+    'LBL_CURRENCY_NAME' => 'Pangalan ng pera',
+    'LBL_CURRENCY_SYMBOL' => 'Simbolo ng pera',
+    'LBL_EDIT_BUTTON' => 'I-edit',
+    'LBL_REMOVE' => 'Alisin',
+
+);
