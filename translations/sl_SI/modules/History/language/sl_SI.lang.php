fcfacea1d995b0fa9d6676415dcc01efcde501f5
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-01-30 18:37:38 +0000

    New translations en_us.lang.php (Slovenian)

---
+++ translations/sl_SI/modules/History/language/sl_SI.lang.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_MODULE_NAME' => 'Zgodovina',
+    'LBL_MODULE_TITLE' => 'Zgodovina: Domov',
+    'LBL_SEARCH_FORM_TITLE' => 'History Search',
+    'LBL_LIST_FORM_TITLE' => 'Zgodovina',
+    'LBL_LIST_SUBJECT' => 'Zadeva',
+    'LBL_LIST_CONTACT' => 'Kontakti',
+    'LBL_LIST_RELATED_TO' => 'Povezano z',
+    'LBL_LIST_DATE' => 'Datum',
+    'LBL_LIST_CLOSE' => 'Zapri',
+    'LBL_SUBJECT' => 'Zadeva:',
+    'LBL_STATUS' => 'Stanje:',
+    'LBL_LOCATION' => 'Lokacija:',
+    'LBL_DATE_TIME' => 'Datum in čas začetka:',
+    'LBL_DATE' => 'Datum začetka:',
+    'LBL_TIME' => 'Čas začetka:',
+    'LBL_DURATION' => 'Trajanje:',
+    'LBL_HOURS_MINS' => '(ure/minute)',
+    'LBL_CONTACT_NAME' => 'Ime kontakta: ',
+    'LBL_DESCRIPTION' => 'Opis:',
+    'LNK_NEW_CALL' => 'Načrtuj klic',
+    'LNK_NEW_MEETING' => 'Načrtuj sestanek',
+    'LNK_NEW_TASK' => 'Ustvari nalogo',
+    'LNK_NEW_NOTE' => 'Ustvari opombo ali priponko',
+    'LNK_NEW_EMAIL' => 'Arhiviraj e-pošto',
+    'LNK_CALL_LIST' => 'Klici',
+    'LNK_MEETING_LIST' => 'Sestanki',
+    'LNK_TASK_LIST' => 'Naloge',
+    'LNK_NOTE_LIST' => 'Beležke',
+    'ERR_DELETE_RECORD' => 'Števika vnosa mora biti definirana za izbris Partnerja.',
+    'LBL_INVITEE' => 'Povabljeni',
+    'LBL_LIST_DIRECTION' => 'Smer',
+    'LBL_DIRECTION' => 'Smer',
+    'LNK_NEW_APPOINTMENT' => 'Ustvari obveznost',
+    'LNK_VIEW_CALENDAR' => 'Danes',
+    'LBL_OPEN_ACTIVITIES' => 'Odprte aktivnosti',
+    'LBL_HISTORY' => 'Zgodovina',
+    'LBL_NEW_TASK_BUTTON_TITLE' => 'Ustvari nalogo',
+    'LBL_NEW_TASK_BUTTON_LABEL' => 'Ustvari nalogo',
+    'LBL_SCHEDULE_MEETING_BUTTON_TITLE' => 'Načrtuj sestanek',
+    'LBL_SCHEDULE_MEETING_BUTTON_LABEL' => 'Načrtuj sestanek',
+    'LBL_SCHEDULE_CALL_BUTTON_LABEL' => 'Načrtuj klic',
+    'LBL_NEW_NOTE_BUTTON_TITLE' => 'Ustvari opombo ali priponko',
+    'LBL_NEW_NOTE_BUTTON_LABEL' => 'Ustvari opombo ali priponko',
+    'LBL_TRACK_EMAIL_BUTTON_TITLE' => 'Arhiviraj e-pošto',
+    'LBL_TRACK_EMAIL_BUTTON_LABEL' => 'Arhiviraj e-pošto',
+    'LBL_LIST_STATUS' => 'Stanje',
+    'LBL_LIST_DUE_DATE' => 'Datum zapadlosti',
+    'LBL_LIST_LAST_MODIFIED' => 'Nazadnje spremenjeno',
+    'LNK_IMPORT_NOTES' => 'Uvozi opombe',
+    'LBL_ACCEPT_THIS' => 'Sprejmi?',
+    'LBL_DEFAULT_SUBPANEL_TITLE' => 'Zgodovina',
+
+    'LBL_CATEGORY' => 'Kategorija',
+    'LBL_LIST_CATEGORY' => 'Kategorija',
+);
