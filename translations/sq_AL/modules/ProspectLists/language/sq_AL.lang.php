61d18b14c5b08c0040436ed30cd13edd2b362efc
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-01-30 19:04:44 +0000

    New translations en_us.lang.php (Albanian)

---
+++ translations/sq_AL/modules/ProspectLists/language/sq_AL.lang.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_MODULE_NAME' => 'listat e synuara',
+    'LBL_MODULE_ID' => 'listat e synuara',
+    'LBL_MODULE_TITLE' => 'listat e synuara: Ballina',
+    'LBL_SEARCH_FORM_TITLE' => 'Kërkimi i listave të synuara',
+    'LBL_LIST_FORM_TITLE' => 'listat e synuara',
+    'LBL_PROSPECT_LIST_NAME' => 'Lista e synuar',
+    'LBL_NAME' => 'Emri',
+    'LBL_ENTRIES' => 'të hyrat e përgjithshme',
+    'LBL_LIST_PROSPECT_LIST_NAME' => 'Lista e synimeve',
+    'LBL_LIST_ENTRIES' => 'objektivat në list',
+    'LBL_LIST_DESCRIPTION' => 'Përshkrim',
+    'LBL_LIST_TYPE_NO' => 'Lloji',
+    'LBL_LIST_END_DATE' => 'Data e përfundimit',
+    'LBL_DATE_ENTERED' => 'Të dhënat e krijuara',
+    'LBL_MARKETING_ID' => 'ID e marketingut',
+    'LBL_DATE_MODIFIED' => 'Të dhënat e modifikuara',
+    'LBL_MODIFIED' => 'Modifikuar nga',
+    'LBL_CREATED' => 'Krijuar nga',
+    'LBL_ASSIGNED_TO' => 'Përdorues',
+    'LBL_DESCRIPTION' => 'Përshkrim',
+    'LNK_NEW_CAMPAIGN' => 'krijo fushatë',
+    'LNK_CAMPAIGN_LIST' => 'fushatat',
+    'LNK_NEW_PROSPECT_LIST' => 'krijo list të synimeve',
+    'LNK_PROSPECT_LIST_LIST' => 'shiko listat e synimeve',
+    'LBL_MODIFIED_BY' => 'Modifikuar nga',
+    'LBL_CREATED_BY' => 'Krijuar nga',
+    'LNK_NEW_PROSPECT' => 'krijo synim',
+    'LNK_PROSPECT_LIST' => 'synimet',
+
+    'LBL_CONTACTS_SUBPANEL_TITLE' => 'Kontaktet',
+    'LBL_LEADS_SUBPANEL_TITLE' => 'udhëheqjet',
+    'LBL_PROSPECTS_SUBPANEL_TITLE' => 'synimet',
+    'LBL_ACCOUNTS_SUBPANEL_TITLE' => 'Accounts',
+    'LBL_COPY_PREFIX' => 'kopje nga',
+    'LBL_USERS_SUBPANEL_TITLE' => 'përdoruesit',
+    'LBL_TYPE' => 'Lloji',
+    'LBL_LIST_TYPE' => 'Lloji',
+    'LBL_LIST_TYPE_LIST_NAME' => 'Lloji',
+    'LBL_NEW_FORM_TITLE' => 'Listë e synuar e re',
+    'LBL_MARKETING_NAME' => 'emri i marketingut',
+    'LBL_DOMAIN_NAME' => 'emri i domenit',
+    'LBL_DOMAIN' => 'jo meila në domen',
+    'LBL_LIST_PROSPECTLIST_NAME' => 'Emri',
+
+    'LBL_EMAIL_MARKETING' => 'Email marketingu',
+
+    'LBL_ASCENDING' => 'Ngritje',
+    'LBL_DESCENDING' => 'Zbritje',
+);
