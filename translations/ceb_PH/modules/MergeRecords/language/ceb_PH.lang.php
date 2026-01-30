deb786f1c097464d86743d3ba88b67dbbbb3c151
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-01-30 18:48:29 +0000

    New translations en_us.lang.php (Cebuano)

---
+++ translations/ceb_PH/modules/MergeRecords/language/ceb_PH.lang.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_MODULE_NAME' => 'I-hiusa ang mga rekord',
+    'LBL_MODULE_TITLE' => 'I-ihiusa ang mga rekord: Home',
+    'LBL_SEARCH_FORM_TITLE' => 'Mangita sa gi-ihiusa',
+    'LBL_LIST_FORM_TITLE' => 'Listahan sa gi-hiusa',
+
+    'LBL_LBL_MERGE_RECORDS_STEP_1' => 'Mangita ug mga rekord sa pangita para i-hiusa uban sa',
+    'LBL_AVAIL_FIELDS' => 'Mga Field nga naa',
+    'LBL_FILTER_COND' => 'Kondisyon sa filter',
+    'LBL_MERGE_VALUE_OVER' => 'I-hiusa ang balor over',
+
+    'LBL_NEXT_STEP_TITLE' => 'Lihok ngadto sa sunod nga step',
+    'LBL_NEXT_STEP_BUTTON_LABEL' => 'Sunod nga step >',
+
+    'LBL_PERFORM_MERGE_BUTTON_TITLE' => 'I-perform ang pag-hiusa',
+    'LBL_PERFORM_MERGE_BUTTON_KEY' => 'P',
+    'LBL_PERFORM_MERGE_BUTTON_LABEL' => 'I-perform ang pag-hiusa',
+
+    'LBL_SAVE_MERGED_RECORD_BUTTON_TITLE' => 'I-save ang pag-hiusa',
+    'LBL_SAVE_MERGED_RECORD_BUTTON_LABEL' => 'I-save ang pag-hiusa',
+
+    'LBL_STEP2_FORM_TITLE' => 'Nakit-an na mga rekord para i-paghiusa uban sa',
+    'LBL_SELECT_ERROR' => 'Kinahanglan nimong muhimo ug usa ka pagpili sa dili paka makapadayun.',
+    'LBL_CHANGE_PARENT' => 'I-set isip nag-una',
+    'LBL_REMOVE_FROM_MERGE' => 'Tangtangon',
+    'LBL_DIFF_COL_VALUES' => 'Mga kolum nga ang balor kay naa sa nag-una nga row kay naay kalahian gikan sa balor sa naghiusa nga mga row:',
+    'LBL_SAME_COL_VALUES' => 'Mga kolum nga ang balor kay pariha sa tanang mga row:',
+    'LBL_DELETE_MESSAGE' => 'Ang kani nga aksyon kay motang-tang sa mga mosunod nga (mga) rekord:',
+    'LBL_PROCEED' => 'Padayun?',
+    'LBL_STEP1_DIRECTIONS' => 'Mangita ug posibleng dobleng mga rekord. kung posibleng mga doble kay makit-an, pwede ka pumili kung unsa nga mga rekord ang ihiusa kuyog sa karon nga rekord.',
+);
