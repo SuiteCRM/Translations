4f0a4c0808e76186b3f655c12070e85529395bf2
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-01-30 17:34:50 +0000

    New translations en_us.lang.php (Serbian (Latin))

---
+++ translations/sr_CS/modules/AOS_Product_Categories/language/sr_CS.lang.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID' => 'ID broj dodeljenog korisnika',
+    'LBL_ASSIGNED_TO_NAME' => 'Dodeljen',
+    'LBL_ID' => 'ID broj',
+    'LBL_DATE_ENTERED' => 'Datum kreiranja',
+    'LBL_DATE_MODIFIED' => 'Datum izmene',
+    'LBL_MODIFIED' => 'Izmenio',
+    'LBL_MODIFIED_NAME' => 'Promenio',
+    'LBL_CREATED' => 'Autor',
+    'LBL_DESCRIPTION' => 'Opis',
+    'LBL_DELETED' => 'Obrisan',
+    'LBL_NAME' => 'Ime',
+    'LBL_CREATED_USER' => 'Autor je korisnik',
+    'LBL_MODIFIED_USER' => 'Promenio korisnik',
+    'LBL_LIST_NAME' => 'Ime',
+    'LBL_EDIT_BUTTON' => 'Izmeni',
+    'LBL_REMOVE' => 'Ukloni',
+    'LBL_LIST_FORM_TITLE' => 'Lista kategorija proizvoda',
+    'LBL_MODULE_NAME' => 'Kategorije proizvoda',
+    'LBL_MODULE_TITLE' => 'Kategorije proizvoda',
+    'LBL_HOMEPAGE_TITLE' => 'Moje Kategorije proizvoda',
+    'LNK_NEW_RECORD' => 'Kreiraj Kategoriju proizvoda',
+    'LNK_LIST' => 'Prikaži Kategorije proizvoda',
+    'LBL_SEARCH_FORM_TITLE' => 'Pretraži Kategorije proizvoda',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Pregled istorije',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Aktivnosti',
+    'LBL_NEW_FORM_TITLE' => 'Nova Kategorija proizvoda',
+    'LBL_PARENT_CATEGORY' => 'Matična kategorija',
+    'LBL_IS_PARENT' => "Matična je kategorija",
+    'LBL_SUB_CATEGORIES' => "Podkategorije",
+    'LBL_PRODUCT_CATEGORYS_NAME' => "Matična kategorija",
+    'LBL_PARENT_CATEGORY_ID' => "ID matične kategorije",
+
+    'LBL_AOS_PRODUCT_CATEGORIES_AOS_PRODUCTS_FROM_AOS_PRODUCTS_TITLE' => 'Product Categories: Product from Product Title',
+);
