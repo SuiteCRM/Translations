625a35b46b38d2a217a77423ed1cef4e418c8917
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-01-30 19:37:45 +0000

    New translations en_us.lang.php (Indonesian)

---
+++ translations/id_ID/modules/jjwg_Maps/language/id_ID.lang.php
@@ -0,0 +1,191 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings['LBL_MAP'] = 'Peta';
+$mod_strings['LBL_MODULE_NAME'] = 'Peta';
+$mod_strings['LBL_MODULE_TITLE'] = 'Peta: Beranda';
+$mod_strings['LBL_MODULE_ID'] = 'Peta';
+$mod_strings['LBL_LIST_FORM_TITLE'] = 'Daftar Peta';
+$mod_strings['LBL_MAP_CUSTOM_MARKER'] = 'Penanda Khusus';
+$mod_strings['LBL_MAP_CUSTOM_AREA'] = 'Area Khusus';
+$mod_strings['LBL_HOMEPAGE_TITLE'] = 'Daftar Peta Saya';
+
+$mod_strings['LBL_FLEX_RELATE'] = 'Terkait ke (Tengah):';
+$mod_strings['LBL_MODULE_TYPE'] = 'Jenis Modul ke Tampilan:';
+$mod_strings['LBL_DISTANCE'] = 'Jarak (Radius):';
+$mod_strings['LBL_UNIT_TYPE'] = 'Jenis Unit:';
+
+$mod_strings['LBL_MAP_DISPLAY'] = 'Tampilan Peta';
+$mod_strings['LBL_MAP_LEGEND'] = 'Legenda:';
+$mod_strings['LBL_MAP_USER_GROUPS'] = 'Grup:';
+$mod_strings['LBL_MAP_GROUP'] = 'Grup';
+$mod_strings['LBL_MAP_TYPE'] = 'Tipe';
+$mod_strings['LBL_MAP_ASSIGNED_TO'] = 'Ditugaskan Untuk:';
+$mod_strings['LBL_MAP_GET_DIRECTIONS'] = 'Tentukan Arah';
+$mod_strings['LBL_MAP_GOOGLE_MAPS_VIEW'] = 'Penampilan Peta Google';
+
+$mod_strings['LNK_NEW_MAP'] = 'Tambahkan Peta Baru';
+$mod_strings['LNK_NEW_RECORD'] = 'Tambahkan Peta Baru';
+$mod_strings['LNK_MAP_LIST'] = 'Daftar Peta';
+
+$mod_strings['LBL_MAP_ADDRESS_TEST'] = 'Uji Geokode';
+$mod_strings['LBL_MAP_QUICK_RADIUS'] = 'Peta Radius Cepat';
+$mod_strings['LBL_MAP_NULL_GROUP_NAME'] = 'Tidak ada';
+$mod_strings['LBL_MAP_ADDRESS'] = 'Alamat';
+$mod_strings['LBL_MAP_PROCESS'] = 'Proses itu!';
+
+$mod_strings['LBL_MAP_LAST_STATUS'] = 'Status Geokode Terakhir';
+$mod_strings['LBL_GEOCODED_COUNTS'] = 'Hitung Modul Geokode';
+$mod_strings['LBL_CRON_URL'] = 'Cron URL:';
+$mod_strings['LBL_MODULE_HEADING'] = 'Modul';
+
+$mod_strings['LBL_N/A'] = 'N/A';
+$mod_strings['LBL_ZERO_RESULTS'] = 'Zero Results';
+$mod_strings['LBL_OK'] = 'Baik';
+$mod_strings['LBL_INVALID_REQUEST'] = 'Invalid Request';
+$mod_strings['LBL_APPROXIMATE'] = 'Approximate';
+$mod_strings['LBL_EMPTY'] = 'Kosong';
+
+$mod_strings['LBL_MODULE_TOTAL_HEADING'] = 'Total';
+$mod_strings['LBL_MODULE_RESET_HEADING'] = 'Setel ulang';
+$mod_strings['LBL_GEOCODED_COUNTS_DESCRIPTION'] = 'Tabel yang ditunjukkan di bawah ini menunjukkan jumlah objek modul geocoded, dikelompokkan berdasarkan respon geokode. Ingatlah bahwa batas penggunaan Google Maps standar adalah 2500 permintaan per hari. Modul ini akan menyimpan informasi geokode alamat selama pemprosesan untuk mengurangi jumlah permintaan keseluruhan yang diperlukan.';
+
+$mod_strings['LBL_CRON_INSTRUCTIONS'] = 'Untuk memproses permintaan geokode, disarankan untuk menyiapkan Cron-Job setiap malam. Titik masuk khusus telah dibuat untuk tujuan ini dan dapat diakses tanpa otentikasi. URL yang ditunjukkan di bawah ini dimaksudkan untuk digunakan dengan Tugas Administratif Terjadwal. Silakan lihat dokumentasi untuk informasi lebih lanjut.';
+$mod_strings['LBL_EXPORT_ADDRESS_URL'] = 'Ekspor URL';
+$mod_strings['LBL_EXPORT_INSTRUCTIONS'] = 'Gunakan link di bawah ini untuk mengekspor alamat lengkap yang membutuhkan informasi geokode. Kemudian gunakan alat geokode batch online atau offline untuk geocode alamat. Setelah selesai melakukan geokode, impor alamat ke modul Address Cache yang akan digunakan dengan peta Anda. Perhatikan, modul Address Cache bersifat opsional. Semua informasi geokode disimpan dalam modul representatif.';
+$mod_strings['LBL_ADDRESS_CACHE'] = 'Alamat Cache';
+$mod_strings['LBL_ADD_TO_TARGET_LIST'] = 'Tambahkan ke Daftar Target';
+$mod_strings['LBL_ADD_TO_TARGET_LIST_PROCESSING'] = 'Sedang Proses..';
+
+
+$mod_strings['LBL_CONFIG_TITLE'] = 'Pengaturan Konfigurasi';
+$mod_strings['LBL_CONFIG_SAVED'] = 'Pengaturan Tersimpan berhasil!';
+$mod_strings['LBL_BILLING_ADDRESS'] = 'Alamat Tagihan';
+$mod_strings['LBL_SHIPPING_ADDRESS'] = 'Alamat Pengiriman';
+$mod_strings['LBL_PRIMARY_ADDRESS'] = 'Alamat Utama';
+$mod_strings['LBL_ALTERNATIVE_ADDRESS'] = 'Alamat Alternatif';
+$mod_strings['LBL_ADDRESS_FLEX_RELATE'] = 'Flex Terkait';
+$mod_strings['LBL_ADDRESS_ADDRESS'] = 'Alamat (Sederhana, Pengguna)';
+$mod_strings['LBL_ADDRESS_CUSTOM'] = 'Khusus (Custom Controller Logic)';
+$mod_strings['LBL_ENABLED'] = 'Diaktifkan';
+$mod_strings['LBL_DISABLED'] = 'Tidak Diaktifkan';
+$mod_strings['LBL_DEFAULT'] = 'Standar:';
+$mod_strings['LBL_CONFIG_DEFAULT'] = 'Standar:';
+
+$mod_strings['LBL_CONFIG_VALID_GEOCODE_MODULES'] = 'Modul Geokode Valid:';
+$mod_strings['LBL_CONFIG_VALID_GEOCODE_TABLES'] = 'Tabel Geokode Valid:';
+$mod_strings['LBL_CONFIG_ADDRESS_TYPE_SETTINGS_TITLE'] = "Pengaturan Jenis Alamat: Ini mendefinisikan jenis alamat modul yang digunakan saat alamat geokode. Nilai yang Dapat Diterima: 'billing'; 'shipping'; 'primary'; 'alt'; 'flex_relate'";
+$mod_strings['LBL_CONFIG_ADDRESS_TYPE_FOR'] = 'Jenis Alamat untuk ';
+$mod_strings['LBL_CONFIG_ADDRESS_TYPE_FOR_ACCOUNTS'] = 'Jenis Alamat untuk Akun:';
+$mod_strings['LBL_CONFIG_ADDRESS_TYPE_FOR_CONTACTS'] = 'Jenis Alamat untuk kontak:';
+$mod_strings['LBL_CONFIG_ADDRESS_TYPE_FOR_LEADS'] = 'Jenis Alamat untuk Memimpin:';
+$mod_strings['LBL_CONFIG_ADDRESS_TYPE_FOR_OPPORTUNITIES'] = 'Jenis Alamat untuk Kesempatan:';
+$mod_strings['LBL_CONFIG_OF_RELATED_ACCOUNT'] = '(dari Akun Terkait)';
+$mod_strings['LBL_CONFIG_ADDRESS_TYPE_FOR_CASES'] = 'Jenis Alamat untuk Kasus:';
+$mod_strings['LBL_CONFIG_ADDRESS_TYPE_FOR_PROJECTS'] = 'Jenis Alamat untuk Proyek:';
+$mod_strings['LBL_CONFIG_OF_RELATED_ACCOUNT_OPPORTUNITY'] = '(dari Akun Terkait/Kesempatan)';
+$mod_strings['LBL_CONFIG_ADDRESS_TYPE_FOR_MEETINGS'] = 'Jenis Alamat untuk Pertemuan:';
+$mod_strings['LBL_CONFIG_ADDRESS_TYPE_FOR_PROSPECTS'] = 'Jenis Alamat untuk Prospek/Target:';
+$mod_strings['LBL_CONFIG_RELATED_OBJECT_THRU_FLEX_RELATE'] = 'Terkait Objek melalui Flex Berhubungan dengan Bidang';
+
+$mod_strings['LBL_CONFIG_MARKER_GROUP_FIELD_SETTINGS_TITLE'] = "Penanda Kelompok Bidang Pengaturan: Ini mendefinisikan 'lapangan' yang akan digunakan sebagai kelompok parameter ketika menampilkan penanda di peta. Contoh: assigned_user_name, industri, status, sales_stage, prioritas";
+$mod_strings['LBL_CONFIG_GROUP_FIELD_FOR'] = 'Grup Bidang untuk ';
+$mod_strings['LBL_CONFIG_GROUP_FIELD_FOR_ACCOUNTS'] = 'Grup Bidang untuk Akun:';
+$mod_strings['LBL_CONFIG_GROUP_FIELD_FOR_CONTACTS'] = 'Grup Bidang untuk Kontak:';
+$mod_strings['LBL_CONFIG_GROUP_FIELD_FOR_LEADS'] = 'Grup Bidang untuk Membimbing:';
+$mod_strings['LBL_CONFIG_GROUP_FIELD_FOR_OPPORTUNITIES'] = 'Grup Bidang untuk Kesempatan:';
+$mod_strings['LBL_CONFIG_GROUP_FIELD_FOR_CASES'] = 'Grup Bidang untuk Kasus:';
+$mod_strings['LBL_CONFIG_GROUP_FIELD_FOR_PROJECTS'] = 'Grup Bidang untuk Proyek:';
+$mod_strings['LBL_CONFIG_GROUP_FIELD_FOR_MEETINGS'] = 'Grup Bidang untuk Pertemuan:';
+$mod_strings['LBL_CONFIG_GROUP_FIELD_FOR_PROSPECTS'] = 'Grup Bidang untuk Prospek/Target:';
+
+$mod_strings['LBL_CONFIG_GEOCODING_SETTINGS_TITLE'] = 'Geokode/Pengaturan Google:';
+$mod_strings['LBL_CONFIG_GEOCODING_API_URL_TITLE'] = 'URL Geokode API:';
+$mod_strings['LBL_CONFIG_GEOCODING_API_URL_DESC'] = 'URL dari Peta Google Geocoding API atau Proxy';
+$mod_strings['LBL_CONFIG_GEOCODING_API_SECRET_TITLE'] = 'Kalimat rahasia untuk Proxy:';
+$mod_strings['LBL_CONFIG_GEOCODING_API_SECRET_DESC'] = 'Kalimat Rahasia untuk digunakan dengan Proxy MD5 perbandingan.';
+$mod_strings['LBL_CONFIG_GEOCODING_LIMIT_TITLE'] = 'Batas Geokode:';
+$mod_strings['LBL_CONFIG_GEOCODING_LIMIT_DESC'] = "geocoding_limit' menetapkan batas kueri saat memilih catatan ke geokode.";
+$mod_strings['LBL_CONFIG_GOOGLE_GEOCODING_LIMIT_TITLE'] = 'Batas Google Geocoding:';
+$mod_strings['LBL_CONFIG_GOOGLE_GEOCODING_LIMIT_DESC'] = "'google_geocoding_limit' set permintaan batas ketika geocoding menggunakan Google Maps API.";
+$mod_strings['LBL_CONFIG_EXPORT_ADDRESSES_LIMIT_TITLE'] = 'Batas Alamat Ekspor:';
+$mod_strings['LBL_CONFIG_EXPORT_ADDRESSES_LIMIT_DESC'] = "'export_addresses_limit' menetapkan batas kueri saat memilih catatan untuk diekspor.";
+$mod_strings['LBL_CONFIG_ALLOW_APPROXIMATE_LOCATION_TYPE_TITLE'] = "Izinkan 'APPROXIMATE' Jenis Lokasi:";
+$mod_strings['LBL_CONFIG_ALLOW_APPROXIMATE_LOCATION_TYPE_DESC'] = "'allow_approximate_location_type' - mengizinkan tipe lokasi 'APPROXIMATE' untuk dianggap sebagai hasil geokode yang 'OK'.";
+
+$mod_strings['LBL_CONFIG_ADDRESS_CACHE_SETTINGS_TITLE'] = 'Pengaturan Alamat Cache:';
+$mod_strings['LBL_CONFIG_ADDRESS_CACHE_GET_ENABLED_TITLE'] = 'Aktifkan Alamat Cache (Dapatkan):';
+$mod_strings['LBL_CONFIG_ADDRESS_CACHE_GET_ENABLED_DESC'] = "'address_cache_get_enabled' memungkinkan alamat modul cache untuk mengambil data dari tabel cache.";
+$mod_strings['LBL_CONFIG_ADDRESS_CACHE_SAVE_ENABLED_TITLE'] = 'Aktifkan Alamat Simpan Cache (Simpan):';
+$mod_strings['LBL_CONFIG_ADDRESS_CACHE_SAVE_ENABLED_DESC'] = "'address_cache_save_enabled' memungkinkan alamat modul cache untuk menyimpan data ke tabel cache.";
+
+$mod_strings['LBL_CONFIG_LOGIC_HOOKS_SETTINGS_TITLE'] = 'Pengaturan Logic Hooks:';
+$mod_strings['LBL_CONFIG_LOGIC_HOOKS_ENABLED_TITLE'] = 'Aktifkan Semua Logic Hooks: ';
+$mod_strings['LBL_CONFIG_LOGIC_HOOKS_ENABLED_DESC'] = "'logic_hooks_enabled' memungkinkan logic hooks untuk pembaharuan otomatis berdasarkan objek terkait. Dianjurkan untuk menonaktifkan saat memperbaharui SuiteCRM Anda.";
+
+$mod_strings['LBL_CONFIG_MARKER_MAPPING_SETTINGS_TITLE'] = 'Penanda/Pengaturan Pemetaan:';
+$mod_strings['LBL_CONFIG_MAP_MARKERS_LIMIT_TITLE'] = "Batas Penanda Peta:";
+$mod_strings['LBL_CONFIG_MAP_MARKERS_LIMIT_DESC'] = "'map_markers_limit' menetapkan batas kueri saat memilih catatan untuk ditampilkan di peta.";
+$mod_strings['LBL_CONFIG_MAP_DEFAULT_CENTER_LATITUDE_TITLE'] = "Peta Standar Pusat Lintang:";
+$mod_strings['LBL_CONFIG_MAP_DEFAULT_CENTER_LATITUDE_DESC'] = "'map_default_center_latitude' menetapkan posisi garis lintang standar untuk peta.";
+$mod_strings['LBL_CONFIG_MAP_DEFAULT_CENTER_LONGITUDE_TITLE'] = "Peta Standar Pusat Lintang:";
+$mod_strings['LBL_CONFIG_MAP_DEFAULT_CENTER_LONGITUDE_DESC'] = "'map_default_center_longitude' menetapkan posisi bujur tengah standar untuk peta.";
+$mod_strings['LBL_CONFIG_MAP_DEFAULT_UNIT_TYPE_TITLE'] = "Peta Standar Jenis Unit:";
+$mod_strings['LBL_CONFIG_MAP_DEFAULT_UNIT_TYPE_DESC'] = "'map_default_unit_type' menetapkan tipe pengukuran unit standar untuk perhitungan jarak jauh. Nilai: 'mi' (mil) atau 'km' (kilometer).";
+$mod_strings['LBL_CONFIG_MAP_DEFAULT_DISTANCE_TITLE'] = "Peta Jarak Jauh Standar:";
+$mod_strings['LBL_CONFIG_MAP_DEFAULT_DISTANCE_DESC'] = "'map__distance' menetapkan jarak default yang digunakan untuk jarak berdasarkan peta.";
+$mod_strings['LBL_CONFIG_MAP_DUPLICATE_MARKER_ADJUSTMENT_TITLE'] = "Peta Duplikat Penyesuaian Penanda:";
+$mod_strings['LBL_CONFIG_MAP_DUPLICATE_MARKER_ADJUSTMENT_DESC'] = "'map_duplicate_marker_adjustment' menetapkan sebuah penyesuaian offset untuk ditambahkan ke bujur dan lintang dalam hal posisi penanda duplikat.";
+$mod_strings['LBL_CONFIG_MAP_CLUSTER_GRID_SIZE_TITLE'] = "Peta Penanda Clusterer Ukuran Kotak:";
+$mod_strings['LBL_CONFIG_MAP_CLUSTER_GRID_SIZE_DESC'] = "'map_clusterer_grid_size' digunakan untuk mengatur ukuran kotak untuk menghitung peta clusterers.";
+$mod_strings['LBL_CONFIG_MAP_MARKERS_CLUSTERER_MAX_ZOOM_TITLE'] = "Peta Penanda Clusterer Zoom Maksimal:";
+$mod_strings['LBL_CONFIG_MAP_MARKERS_CLUSTERER_MAX_ZOOM_DESC'] = "'map_clusterer_max_zoom' digunakan untuk mengatur tingkat zoom maksimum di mana clustering tidak akan diterapkan.";
+$mod_strings['LBL_CONFIG_CUSTOM_CONTROLLER_DESC'] = "Catatan Penting: Semua pengaturan tersimpan dapat ditemukan di 'config' tabel di bawah kategori 'jjwg'. Catatan, file controller.php khusus seharusnya tidak lagi digunakan untuk mengganti pengaturan.";
+$mod_strings['LBL_JJWG_MAPS_JJWG_AREAS_FROM_JJWG_AREAS_TITLE'] = 'Area';
+$mod_strings['LBL_JJWG_MAPS_JJWG_MARKERS_FROM_JJWG_MARKERS_TITLE'] = 'Penanda';
+$mod_strings['LBL_PARENT_ID'] = 'ID Induk';
+$mod_strings['LBL_JJWP_PARTNERS'] = 'Partners JJWP';
+$mod_strings['LBL_GET_GOOGLE_API_KEY'] = 'Mendapatkan Sebuah Kunci';
+$mod_strings['LBL_GOOGLE_API_KEY'] = 'Kunci Google Api';
+$mod_strings['LBL_ERROR_NO_GOOGLE_API_KEY'] = 'Silahkan mengatur Kunci Google Api Di Panel Administrasi Google Map.';
