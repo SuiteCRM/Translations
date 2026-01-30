4fbd38f36bac617d795f4c34ac182687593996b1
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-01-30 18:45:38 +0000

    New translations en_us.lang.php (Estonian)

---
+++ translations/et_EE/modules/MailMerge/language/et_EE.lang.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_STEP_1' => 'Samm 1: Vali moodul ja mall',
+    'LBL_MAILMERGE_MODULE' => 'Vali moodul:',
+    'LBL_MAILMERGE_SELECTED_MODULE' => 'Valitud moodul:',
+    'LBL_MAILMERGE_TEMPLATES' => 'Vali mall:',
+    'LBL_STEP_4' => 'Vaata üle ja lõpeta',
+    'LBL_SELECTED_MODULE' => 'Valitud moodul:',
+    'LBL_SELECTED_TEMPLATE' => 'Valitud mall:',
+    'LBL_SELECTED_ITEMS' => 'Valitud ühikud:',
+    'LBL_STEP_5' => 'E-posti mestimine lõpetatud',
+    'LBL_MERGED_FILE' => 'Mestitud fail:',
+    'LNK_NEW_MAILMERGE' => 'Alusta kirja mestimist:',
+    'LNK_UPLOAD_TEMPLATE' => 'Lae mall',
+    'LBL_DOC_NAME' => 'Dokumendi nimi:',
+    'LBL_FILENAME' => 'Faili nimi:',
+    'LBL_DOC_VERSION' => 'Parandus:',
+    'LBL_DOC_DESCRIPTION' => 'Kirjeldus',
+    'LBL_LIST_NAME' => 'Nimi',
+    'LBL_FINISH' => 'Alusta mestimist',
+    'LBL_NEXT' => 'Järgmine >',
+    'LBL_BACK' => '< Tagasi',
+    'LBL_START' => 'Alustamiseks kliki siia',
+    'LBL_CONTAINS_CONTACT_INFO' => 'Valitud mall sisaldab seoseid',
+    'LBL_ADDIN_NOTICE' => 'This requires the installation of SuiteCRM Mail Merge add-in to Microsoft Word.',
+    'LBL_REMOVE' => 'Eemalda ühik(ud)' /*for 508 compliance fix*/,
+    'LBL_ADD' => 'Lisa ühik(ud)' /*for 508 compliance fix*/,
+    'LBL_BROWSER_REQUIREMENT' => 'This requires IE 6.0 or greater for Windows.',
+    'LBL_FORM_SELECT_TEMPLATE' => 'Palun vali Mall.',
+    'LBL_SELECT_ONE' => 'Palun vali vähemalt üks ühik.',
+    'LBL_AVAILABLE' => 'Saadaval',
+    'LBL_SELECTED' => 'Valitud',
+    'LBL_RELATED_INFO_SELECTION' => 'Use the selections below to identify the related information you would like to display in your merged document. Skip this step if your template document contains no related information.',
+
+);
