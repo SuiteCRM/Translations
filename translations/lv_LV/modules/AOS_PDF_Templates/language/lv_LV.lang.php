aabc4d92405b08cce76bacd28cd3e1583336f39b
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-01-30 17:33:20 +0000

    New translations en_us.lang.php (Latvian)

---
+++ translations/lv_LV/modules/AOS_PDF_Templates/language/lv_LV.lang.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID' => 'Piešķirts lietotājam ar ID',
+    'LBL_ASSIGNED_TO_NAME' => 'Piešķirts lietotājam',
+    'LBL_ID' => 'ID',
+    'LBL_DATE_ENTERED' => 'Izveidošanas datums',
+    'LBL_DATE_MODIFIED' => 'Modificēts',
+    'LBL_MODIFIED' => 'Rediģēja',
+    'LBL_MODIFIED_NAME' => 'Modificēja vārds',
+    'LBL_CREATED' => 'Autors',
+    'LBL_DESCRIPTION' => 'Ķermenis',
+    'LBL_HEADER' => 'Galvene',
+    'LBL_FOOTER' => 'Lapas apakša',
+    'LBL_DELETED' => 'Izdzēsts',
+    'LBL_NAME' => 'Vārds:',
+    'LBL_CREATED_USER' => 'Izveidoja lietotājs',
+    'LBL_MODIFIED_USER' => 'Modificēja',
+    'LBL_LIST_FORM_TITLE' => 'PDF veidņu saraksts',
+    'LBL_MODULE_NAME' => 'PDF veidnes',
+    'LBL_MODULE_TITLE' => 'PDF veidnes: Sākums',
+    'LBL_HOMEPAGE_TITLE' => 'Manas PDF veidnes',
+    'LNK_NEW_RECORD' => 'Izveidot PDF veidni',
+    'LNK_LIST' => 'Skatīt PDF veidnes',
+    'LBL_SEARCH_FORM_TITLE' => 'Meklēt PDF veidnes',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Aplūkot vēsturi',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Darbības',
+    'LBL_NEW_FORM_TITLE' => 'Jauna PDF veidne',
+    'LBL_TYPE' => 'Tips:',
+    'LBL_ACTIVE' => 'Aktīvs',
+    'LBL_BUTTON_INSERT' => 'Ievietot',
+    'LBL_WARNING_OVERWRITE' => 'Brīdinājums tiks pārrakstīts pašreizējais darbs',
+    'LBL_INSERT_FIELDS' => 'Ievietot laukus',
+
+    'LBL_SAMPLE' => 'Ielādēt paraugu',
+    'LBL_PAGE' => 'lapa',
+    'LBL_PREPARED_FOR' => 'Sagatavots',
+    'LBL_PREPARED_BY' => 'Sagatavoja',
+    'LBL_QUOTE_SAMPLE' => 'Quote Sample',
+    'LBL_INVOICE_SAMPLE' => 'Invoice Sample',
+    'LBL_ACCOUNT_SAMPLE' => 'Account Sample',
+    'LBL_CONTACT_SAMPLE' => 'Contact Sample',
+    'LBL_LEAD_SAMPLE' => 'Lead Sample',
+    'LBL_ANY_STREET' => 'Jebkura iela',
+    'LBL_ANY_TOWN' => 'Jebkura pilsēta',
+    'LBL_ANY_WHERE' => 'Any Where',
+
+    'LBL_QUOTE_GROUP_SAMPLE' => 'Quote Group Sample',
+    'LBL_INVOICE_GROUP_SAMPLE' => 'Invoice Group Sample',
+    'LBL_MARGIN_LEFT' => 'Kreiso piemali',
+    'LBL_MARGIN_RIGHT' => 'Labo piemali',
+    'LBL_MARGIN_TOP' => 'Augšējo piemali',
+    'LBL_MARGIN_BOTTOM' => 'Apakšējo piemali',
+    'LBL_MARGIN_HEADER' => 'Galvenes piemale',
+    'LBL_MARGIN_FOOTER' => 'Kājenes piemale',
+    'LBL_EDITVIEW_PANEL1' => 'Robežas',
+    'LBL_DETAILVIEW_PANEL1' => 'Robežas',
+    'LBL_PAGE_SIZE' => 'Page Size',
+    'LBL_ORIENTATION' => 'Orientation',
+);
