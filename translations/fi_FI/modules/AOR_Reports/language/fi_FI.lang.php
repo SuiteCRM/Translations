2e017132bd4a7f6897be1ffed77f1bb194eeaab8
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-01-30 17:26:11 +0000

    New translations en_us.lang.php (Finnish)

---
+++ translations/fi_FI/modules/AOR_Reports/language/fi_FI.lang.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID' => 'Vastuuhenkilön tunnus',
+    'LBL_ASSIGNED_TO_NAME' => 'Vastuuhenkilö',
+    'LBL_ID' => 'Tunnus',
+    'LBL_DATE_ENTERED' => 'Luonti pvm',
+    'LBL_DATE_MODIFIED' => 'Muokkaus pvm',
+    'LBL_MODIFIED' => 'Muokkaaja',
+    'LBL_MODIFIED_NAME' => 'Muokkaaja',
+    'LBL_CREATED' => 'Tekijä',
+    'LBL_DESCRIPTION' => 'Kuvaus',
+    'LBL_DELETED' => 'Poistettu',
+    'LBL_NAME' => 'Nimi',
+    'LBL_CREATED_USER' => 'Tekijä',
+    'LBL_MODIFIED_USER' => 'Muokkaaja',
+    'LBL_LIST_NAME' => 'Nimi',
+    'LBL_EDIT_BUTTON' => 'Muokkaa',
+    'LBL_REMOVE' => 'Poista',
+    'LBL_LIST_FORM_TITLE' => 'Raportit-luettelo',
+    'LBL_MODULE_NAME' => 'Raportit',
+    'LBL_MODULE_TITLE' => 'Raportit',
+    'LBL_HOMEPAGE_TITLE' => 'Omat Raportit',
+    'LNK_NEW_RECORD' => 'Luo Raportti',
+    'LNK_LIST' => 'Raportit',
+    'LBL_SEARCH_FORM_TITLE' => 'Etsi raportit',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Historia',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Aktiviteetit',
+    'LBL_NEW_FORM_TITLE' => 'Uudet Raportit',
+    'LBL_REPORT_MODULE' => 'Raportti moduuli',
+    'LBL_GRAPHS_PER_ROW' => 'Kaavioita riviä kohti',
+    'LBL_FIELD_LINES' => 'Näytä Kentät',
+    'LBL_ADD_FIELD' => 'Lisää kenttä',
+    'LBL_CONDITION_LINES' => 'Ehdot',
+    'LBL_ADD_CONDITION' => 'Lisää ehto',
+    'LBL_EXPORT' => 'Vie',
+    'LBL_DOWNLOAD_PDF' => 'Lataa PDF',
+    'LBL_ADD_TO_PROSPECT_LIST' => 'Lisää kohdelistaan',
+    'LBL_AOR_MODULETREE_SUBPANEL_TITLE' => 'Module tree',
+    'LBL_AOR_FIELDS_SUBPANEL_TITLE' => 'Kentät',
+    'LBL_AOR_CONDITIONS_SUBPANEL_TITLE' => 'Ehdot',
+    'LBL_TOTAL' => 'Summa',
+    'LBL_AOR_CHARTS_SUBPANEL_TITLE' => 'Kuvaajat',
+    'LBL_ADD_CHART' => 'Lisää kaavio',
+    'LBL_ADD_PARENTHESIS' => 'Drop parenthesis',
+    'LBL_CHART_TITLE' => 'Asema',
+    'LBL_CHART_TYPE' => 'Tyyppi',
+    'LBL_CHART_X_FIELD' => 'X- Akseli',
+    'LBL_CHART_Y_FIELD' => 'Y- Akseli',
+    'LBL_AOR_REPORTS_DASHLET' => 'Raportit',
+    'LBL_DASHLET_TITLE' => 'Asema',
+    'LBL_DASHLET_REPORT' => 'Raportti',
+    'LBL_DASHLET_CHOOSE_REPORT' => 'Valitse raportti',
+    'LBL_DASHLET_SAVE' => 'Tallenna',
+    'LBL_DASHLET_CHARTS' => 'Kuvaajat',
+    'LBL_DASHLET_ONLY_CHARTS' => 'Näytä vain kaaviot',
+    'LBL_UPDATE_PARAMETERS' => 'Päivitä',
+    'LBL_PARAMETERS' => 'Parametrit',
+    'LBL_TOOLTIP_DRAG_DROP_ELEMS' => 'Drag and drop elements into field or condition area',
+    'LBL_MAIN_GROUPS' => 'Main Group:',
+    'LBL_CHAR_UNNAMED_DEFAULT_TITLE' => 'Nimetön Kaavio',
+    'LBL_REPORT' => 'Raportti',
+);
