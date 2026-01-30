5f1049c88091244b7a22ba8306fadb8f56f2a9a0
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-01-30 17:26:43 +0000

    New translations en_us.lang.php (Chinese Traditional)

---
+++ translations/zh_TW/modules/AOR_Reports/language/zh_TW.lang.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID' => '負責人Id',
+    'LBL_ASSIGNED_TO_NAME' => '負責人',
+    'LBL_ID' => '編號',
+    'LBL_DATE_ENTERED' => '建立日期',
+    'LBL_DATE_MODIFIED' => '修改日期',
+    'LBL_MODIFIED' => '修改人',
+    'LBL_MODIFIED_NAME' => '修改人姓名',
+    'LBL_CREATED' => '建立人',
+    'LBL_DESCRIPTION' => '描述',
+    'LBL_DELETED' => '已刪除',
+    'LBL_NAME' => '名稱',
+    'LBL_CREATED_USER' => '創建者',
+    'LBL_MODIFIED_USER' => '修改的使用者',
+    'LBL_LIST_NAME' => '名稱',
+    'LBL_EDIT_BUTTON' => '編輯',
+    'LBL_REMOVE' => '刪除',
+    'LBL_LIST_FORM_TITLE' => '報表清單',
+    'LBL_MODULE_NAME' => '報表',
+    'LBL_MODULE_TITLE' => '報表',
+    'LBL_HOMEPAGE_TITLE' => '我的報表',
+    'LNK_NEW_RECORD' => '創建報表',
+    'LNK_LIST' => '查看報表',
+    'LBL_SEARCH_FORM_TITLE' => '搜尋報表',
+    'LBL_HISTORY_SUBPANEL_TITLE' => '查看歷史記錄',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => '銷售活動',
+    'LBL_NEW_FORM_TITLE' => '新建報表',
+    'LBL_REPORT_MODULE' => '報表模組',
+    'LBL_GRAPHS_PER_ROW' => '每行圖表數',
+    'LBL_FIELD_LINES' => '顯示欄位',
+    'LBL_ADD_FIELD' => '添加欄位',
+    'LBL_CONDITION_LINES' => '條件',
+    'LBL_ADD_CONDITION' => '添加條件',
+    'LBL_EXPORT' => '導出',
+    'LBL_DOWNLOAD_PDF' => '下載 PDF',
+    'LBL_ADD_TO_PROSPECT_LIST' => '增加到目標列表',
+    'LBL_AOR_MODULETREE_SUBPANEL_TITLE' => '模組樹',
+    'LBL_AOR_FIELDS_SUBPANEL_TITLE' => '欄位',
+    'LBL_AOR_CONDITIONS_SUBPANEL_TITLE' => '條件',
+    'LBL_TOTAL' => '總計',
+    'LBL_AOR_CHARTS_SUBPANEL_TITLE' => '圖表',
+    'LBL_ADD_CHART' => '添加圖表',
+    'LBL_ADD_PARENTHESIS' => '插入括弧',
+    'LBL_CHART_TITLE' => '標題',
+    'LBL_CHART_TYPE' => '類型',
+    'LBL_CHART_X_FIELD' => 'X 軸',
+    'LBL_CHART_Y_FIELD' => 'Y 軸',
+    'LBL_AOR_REPORTS_DASHLET' => '報表',
+    'LBL_DASHLET_TITLE' => '標題',
+    'LBL_DASHLET_REPORT' => '報表',
+    'LBL_DASHLET_CHOOSE_REPORT' => '請選擇報表',
+    'LBL_DASHLET_SAVE' => '儲存',
+    'LBL_DASHLET_CHARTS' => '圖表',
+    'LBL_DASHLET_ONLY_CHARTS' => '僅顯示圖表',
+    'LBL_UPDATE_PARAMETERS' => '更新',
+    'LBL_PARAMETERS' => '參數',
+    'LBL_TOOLTIP_DRAG_DROP_ELEMS' => '將元素拖放入欄位或條件區域',
+    'LBL_MAIN_GROUPS' => '主群組:',
+    'LBL_CHAR_UNNAMED_DEFAULT_TITLE' => '未命名的圖表',
+    'LBL_REPORT' => '報表',
+);
