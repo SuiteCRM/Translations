d4825b95e663682955e0ca2972920de7e3e1ab95
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-01-30 17:02:32 +0000

    New translations en_us.lang.php (Slovenian)

---
+++ translations/sl_SI/include/SugarObjects/templates/sale/language/sl_SI.lang.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_MODULE_NAME' => 'Prodaja',
+    'LBL_MODULE_TITLE' => 'Prodaja: Domov',
+    'LBL_SEARCH_FORM_TITLE' => 'Iskanje prodaje',
+    'LBL_LIST_FORM_TITLE' => 'Seznam prodaje',
+    'LBL_NAME' => 'Ime prodaje:',
+    'LBL_LIST_SALE_NAME' => 'Ime',
+    'LBL_LIST_ACCOUNT_NAME' => 'Ime Partnerja',
+    'LBL_LIST_AMOUNT' => 'Količina',
+    'LBL_LIST_DATE_CLOSED' => 'Zapri',
+    'LBL_LIST_SALE_STAGE' => 'Stopnja prodaje',
+    'LBL_ACCOUNT_ID' => 'ID partnerja',
+    //DON'T CONVERT THESE THEY ARE MAPPINGS
+    'db_name' => 'LBL_NAME',
+    //END DON'T CONVERT
+    'LBL_ACCOUNT_NAME' => 'Ime Partnerja:',
+    'LBL_AMOUNT' => 'Znesek:',
+    'LBL_AMOUNT_USDOLLAR' => 'Znesek v USD:',
+    'LBL_CURRENCY' => 'Valuta:',
+    'LBL_DATE_CLOSED' => 'Pričakovan datum zaključka:',
+    'LBL_TYPE' => 'Tip:',
+    'LBL_CAMPAIGN' => 'Kampanja:',
+    'LBL_LEADS_SUBPANEL_TITLE' => 'Potenciali',
+    'LBL_PROJECTS_SUBPANEL_TITLE' => 'Projekti',
+    'LBL_NEXT_STEP' => 'Naslednji korak:',
+    'LBL_LEAD_SOURCE' => 'Vir potencialov:',
+    'LBL_SALES_STAGE' => 'Faza prodaje:',
+    'LBL_PROBABILITY' => 'Verjetnost (%):',
+    'LBL_DESCRIPTION' => 'Opis:',
+    'LBL_DUPLICATE' => 'Možna podvojitev prodaje',
+    'MSG_DUPLICATE' => 'Prodajni zapis, ki ga želite ustvariti je mogoče dvojnik prodajnega zapisa, ki že obstaja. Prodajni zapisi, ki vsebujejo podobna imena so našteti spodaj. &lt;br&gt;Kliknite Shrani za nadaljevanje ustvarjanja nove prodaje, ali pa kliknite Prekliči za vrnitev v modul, brez ustvarjanja nove prodaje.',
+    'LBL_NEW_FORM_TITLE' => 'Ustvari prodajo',
+    'ERR_DELETE_RECORD' => 'Če želite izbrisati to prodajo, morate definirati številko zapisa.',
+    'LBL_DEFAULT_SUBPANEL_TITLE' => 'Prodaja',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Aktivnosti',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Zgodovina',
+
+    'LBL_CONTACTS_SUBPANEL_TITLE' => 'Kontakti',
+    'LBL_ASSIGNED_TO_NAME' => 'Dodeljeno:',
+    'LBL_LIST_ASSIGNED_TO_NAME' => 'Dodeljen uporabnik',
+    'LBL_ASSIGNED_TO_ID' => 'Dodeljeno IDju',
+    'LBL_MODIFIED_NAME' => 'Spremenjeno s strani uporabnika',
+    'LBL_SALE_INFORMATION' => 'Prodajne informacije',
+    'LBL_CURRENCY_NAME' => 'Ime valute',
+    'LBL_CURRENCY_SYMBOL' => 'Simbol valute',
+    'LBL_EDIT_BUTTON' => 'Uredi',
+    'LBL_REMOVE' => 'Odstrani',
+
+);
