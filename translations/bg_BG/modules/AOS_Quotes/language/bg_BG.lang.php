2a4a71a9dc4ecc33e11f014f68d1f77cda1c9501
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-01-30 17:37:39 +0000

    New translations en_us.lang.php (Bulgarian)

---
+++ translations/bg_BG/modules/AOS_Quotes/language/bg_BG.lang.php
@@ -0,0 +1,206 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID' => 'Възложен на оператор с идентификационен номер',
+    'LBL_ASSIGNED_TO_NAME' => 'Потребител',
+    'LBL_ID' => 'Идентификационен номер',
+    'LBL_DATE_ENTERED' => 'Създадено на',
+    'LBL_DATE_MODIFIED' => 'Модифицирано на',
+    'LBL_MODIFIED' => 'Модифицирано от',
+    'LBL_MODIFIED_NAME' => 'Модифицирано от',
+    'LBL_CREATED' => 'Създадено от',
+    'LBL_DESCRIPTION' => 'Описание',
+    'LBL_DELETED' => 'Изтрити',
+    'LBL_NAME' => 'Титла',
+    'LBL_CREATED_USER' => 'Създадено от потребител',
+    'LBL_MODIFIED_USER' => 'Модифицирано от потребител',
+    'ERR_DELETE_RECORD' => 'Трябва да определите номер, за да изтриете този запис.',
+    'LBL_ACCOUNT_NAME' => 'Титла',
+    'LBL_ACCOUNT' => 'Организация:',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Дейности',
+    'LBL_ADDRESS_INFORMATION' => 'Адресна информация',
+    'LBL_ANNUAL_REVENUE' => 'Годишни приходи:',
+    'LBL_ANY_ADDRESS' => 'Адрес:',
+    'LBL_ANY_EMAIL' => 'Електронна поща:',
+    'LBL_ANY_PHONE' => 'Телефон:',
+    'LBL_RATING' => 'Оценка',
+    'LBL_ASSIGNED_USER' => 'Потребител',
+    'LBL_BILLING_ADDRESS_CITY' => 'Данъчен адрес: град:',
+    'LBL_BILLING_ADDRESS_COUNTRY' => 'Данъчен адрес: държава:',
+    'LBL_BILLING_ADDRESS_POSTALCODE' => 'Данъчен адрес: пощенски код:',
+    'LBL_BILLING_ADDRESS_STATE' => 'Данъчен адрес: област:',
+    'LBL_BILLING_ADDRESS_STREET_2' => 'Данъчен адрес: улица 2',
+    'LBL_BILLING_ADDRESS_STREET_3' => 'Данъчен адрес: улица 3',
+    'LBL_BILLING_ADDRESS_STREET_4' => 'Данъчен адрес: улица 4',
+    'LBL_BILLING_ADDRESS_STREET' => 'Данъчен адрес: улица:',
+    'LBL_BILLING_ADDRESS' => 'Данъчен адрес:',
+    'LBL_ACCOUNT_INFORMATION' => 'Казус',
+    'LBL_CITY' => 'Град:',
+    'LBL_CONTACTS_SUBPANEL_TITLE' => 'Контакти',
+    'LBL_COUNTRY' => 'Държава:',
+    'LBL_DEFAULT_SUBPANEL_TITLE' => 'Организации',
+    'LBL_DUPLICATE' => 'Възможно дублиране с въведена организация',
+    'LBL_EMAIL' => 'Email:',
+    'LBL_EMPLOYEES' => 'Служители:',
+    'LBL_FAX' => 'Факс:',
+    'LBL_INDUSTRY' => 'Отрасъл:',
+    'LBL_LIST_ACCOUNT_NAME' => 'Oрганизация',
+    'LBL_LIST_CITY' => 'Град',
+    'LBL_LIST_EMAIL_ADDRESS' => 'Адрес на ел. поща',
+    'LBL_LIST_PHONE' => 'Телефон',
+    'LBL_LIST_STATE' => 'Област',
+    'LBL_MEMBER_OF' => 'Член на:',
+    'LBL_MEMBER_ORG_SUBPANEL_TITLE' => 'Дъщерни дружества',
+    'LBL_OTHER_EMAIL_ADDRESS' => 'Алтернативна електронна поща:',
+    'LBL_OTHER_PHONE' => 'Алтернативен телефонен номер:',
+    'LBL_OWNERSHIP' => 'Вид на организацията:',
+    'LBL_PARENT_ACCOUNT_ID' => 'Компания майка',
+    'LBL_PHONE_ALT' => 'Алтернативен телефонен номер:',
+    'LBL_PHONE_FAX' => 'Телефон / Факс:',
+    'LBL_PHONE_OFFICE' => 'Служебен телефон:',
+    'LBL_PHONE' => 'Телефон:',
+    'LBL_POSTAL_CODE' => 'Пощенски код:',
+    'LBL_SAVE_ACCOUNT' => 'Запазване на организация',
+    'LBL_SHIPPING_ADDRESS_CITY' => 'Адрес за кореспонденция: град:',
+    'LBL_SHIPPING_ADDRESS_COUNTRY' => 'Адрес за кореспонденция: държава:',
+    'LBL_SHIPPING_ADDRESS_POSTALCODE' => 'Адрес за кореспонденция: пощенски код:',
+    'LBL_SHIPPING_ADDRESS_STATE' => 'Адрес за кореспонденция: област:',
+    'LBL_SHIPPING_ADDRESS_STREET_2' => 'Адрес за кореспонденция: улица 2',
+    'LBL_SHIPPING_ADDRESS_STREET_3' => 'Адрес за кореспонденция: улица 3',
+    'LBL_SHIPPING_ADDRESS_STREET_4' => 'Адрес за кореспонденция: улица 4',
+    'LBL_SHIPPING_ADDRESS_STREET' => 'Адрес за кореспонденция: улица:',
+    'LBL_SHIPPING_ADDRESS' => 'Адрес за кореспонденция:',
+    'LBL_STATE' => 'State/Region:',
+    'LBL_TICKER_SYMBOL' => 'Борсов код:',
+    'LBL_TYPE' => 'Категория:',
+    'LBL_WEBSITE' => 'Сайт:',
+    'LNK_ACCOUNT_LIST' => 'Организации',
+    'LNK_NEW_ACCOUNT' => 'Въвеждане на организация',
+    'MSG_DUPLICATE' => 'Creating this account may potentially create a duplicate account. You may either select an account from the list below or you may click on Save to continue creating a new account with the previously entered data.',
+    'MSG_SHOW_DUPLICATES' => 'Creating this account may potentially create a duplicate account. You may either click on Save to continue creating this new account with the previously entered data or you may click Cancel.',
+    'NTC_DELETE_CONFIRMATION' => 'Сигурни ли сте, че желаете да изтриете този запис?',
+    'LBL_LIST_FORM_TITLE' => 'Quotes List',
+    'LBL_MODULE_NAME' => 'Оферти',
+    'LBL_MODULE_TITLE' => 'Quotes: Home',
+    'LBL_HOMEPAGE_TITLE' => 'My Quotes',
+    'LNK_NEW_RECORD' => 'Създаване на оферта',
+    'LNK_LIST' => 'View Quotes',
+    'LBL_SEARCH_FORM_TITLE' => 'Search Quotes',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'История',
+    'LBL_NEW_FORM_TITLE' => 'New Quote',
+    'LBL_TERMS_C' => 'Terms',
+    'LBL_APPROVAL_ISSUE' => 'Approval Issues',
+    'LBL_APPROVAL_STATUS' => 'Approval Status',
+    'LBL_BILLING_ACCOUNT' => 'Организация',
+    'LBL_BILLING_CONTACT' => 'Контакт',
+    'LBL_EXPIRATION' => 'Valid Until',
+    'LBL_QUOTE_NUMBER' => 'Quote Number',
+    'LBL_OPPORTUNITY' => 'Възможност',
+    'LBL_TEMPLATE_DDOWN_C' => 'Quote Template',
+    'LBL_STAGE' => 'Quote Stage',
+    'LBL_TERM' => 'Payment Terms',
+    'LBL_SUBTOTAL_AMOUNT' => 'Subtotal',
+    'LBL_DISCOUNT_AMOUNT' => 'Discount',
+    'LBL_TAX_AMOUNT' => 'Tax',
+    'LBL_SHIPPING_AMOUNT' => 'Превоз',
+    'LBL_TOTAL_AMT' => 'Total',
+    'VALUE' => 'Титла',
+    'LBL_EMAIL_ADDRESSES' => 'Адреси',
+    'LBL_LINE_ITEMS' => 'Line Items',
+    'LBL_GRAND_TOTAL' => 'Grand Total',
+    'LBL_INVOICE_STATUS' => 'Invoice Status',
+    'LBL_PRODUCT_QUANITY' => 'Quantity',
+    'LBL_PRODUCT_NAME' => 'Продукт',
+    'LBL_PART_NUMBER' => 'Part Number',
+    'LBL_PRODUCT_NOTE' => 'Бележки',
+    'LBL_PRODUCT_DESCRIPTION' => 'Описание',
+    'LBL_LIST_PRICE' => 'Списък',
+    'LBL_DISCOUNT_AMT' => 'Discount',
+    'LBL_UNIT_PRICE' => 'Продажна цена',
+    'LBL_TOTAL_PRICE' => 'Total',
+    'LBL_VAT' => 'Tax', // VAT
+    'LBL_VAT_AMT' => 'Tax Amount', // VAT
+    'LBL_ADD_PRODUCT_LINE' => 'Add Product Line',
+    'LBL_SERVICE_NAME' => 'Услуга',
+    'LBL_SERVICE_LIST_PRICE' => 'Списък',
+    'LBL_SERVICE_PRICE' => 'Продажна цена',
+    'LBL_SERVICE_DISCOUNT' => 'Discount',
+    'LBL_ADD_SERVICE_LINE' => 'Add Service Line ',
+    'LBL_REMOVE_PRODUCT_LINE' => 'Изтрий',
+    'LBL_CONVERT_TO_INVOICE' => 'Convert to Invoice',
+    'LBL_PRINT_AS_PDF' => 'Принтирай в PDF',
+    'LBL_EMAIL_QUOTE' => 'Email Quotation',
+    'LBL_CREATE_CONTRACT' => 'Create Contract',
+    'LBL_LIST_NUM' => 'Num',
+    'LBL_PDF_NAME' => 'Оферта',
+    'LBL_EMAIL_NAME' => 'Quote for',
+    'LBL_QUOTE_DATE' => 'Quotation Date',
+    'LBL_NO_TEMPLATE' => 'ERROR\nNo templates found. If you have not created a Quote template, go to the PDF templates module and create one',
+    'LBL_SUBTOTAL_TAX_AMOUNT' => 'Subtotal + Tax',//pre shipping
+    'LBL_EMAIL_PDF' => 'Email PDF',
+    'LBL_ADD_GROUP' => 'Add Group',
+    'LBL_DELETE_GROUP' => 'Delete Group',
+    'LBL_GROUP_NAME' => 'Group Name',
+    'LBL_GROUP_TOTAL' => 'Group Total',
+    'LBL_SHIPPING_TAX' => 'Shipping Tax',
+    'LBL_SHIPPING_TAX_AMT' => 'Shipping Tax',
+    'LBL_IMPORT_LINE_ITEMS' => 'Import Line Items',
+    'LBL_CREATE_OPPORTUNITY' => 'Създаване на възможност',
+    'LBL_SUBTOTAL_AMOUNT_USDOLLAR' => 'Subtotal (Default Currency)',
+    'LBL_DISCOUNT_AMOUNT_USDOLLAR' => 'Discount (Default Currency)',
+    'LBL_TAX_AMOUNT_USDOLLAR' => 'Tax (Default Currency)',
+    'LBL_SHIPPING_AMOUNT_USDOLLAR' => 'Shipping (Default Currency)',
+    'LBL_TOTAL_AMT_USDOLLAR' => 'Total (Default Currency)',
+    'LBL_SHIPPING_TAX_AMT_USDOLLAR' => 'Shipping Tax (Default Currency)',
+    'LBL_GRAND_TOTAL_USDOLLAR' => 'Grand Total (Default Currency)',
+    'LBL_QUOTE_TO' => 'Quote To',
+
+    'LBL_SUBTOTAL_TAX_AMOUNT_USDOLLAR' => 'Subtotal + Tax (Default Currency)',
+    'LBL_AOS_QUOTES_AOS_CONTRACTS' => 'Quotes: Contracts',
+    'LBL_AOS_QUOTES_AOS_INVOICES' => 'Quotes: Invoices',
+    'LBL_AOS_LINE_ITEM_GROUPS' => 'Line Item Groups',
+    'LBL_AOS_PRODUCT_QUOTES' => 'Product Quotes',
+    'LBL_AOS_QUOTES_PROJECT' => 'Quotes: Project',
+);
