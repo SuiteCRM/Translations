5e7fbcd6539139cb366b8a300ddd89b9c28c4b6a
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-01-30 19:38:56 +0000

    New translations en_us.lang.php (Chinese Traditional)

---
+++ translations/zh_TW/modules/jjwg_Markers/language/zh_TW.lang.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings['LBL_ASSIGNED_TO_ID'] = '負責人Id';
+$mod_strings['LBL_ASSIGNED_TO_NAME'] = '使用者';
+$mod_strings['LBL_ID'] = '編號';
+$mod_strings['LBL_DATE_ENTERED'] = '建立日期';
+$mod_strings['LBL_DATE_MODIFIED'] = '修改日期';
+$mod_strings['LBL_MODIFIED'] = '修改人';
+$mod_strings['LBL_MODIFIED_NAME'] = '修改人姓名';
+$mod_strings['LBL_CREATED'] = '建立人';
+$mod_strings['LBL_DESCRIPTION'] = '描述';
+$mod_strings['LBL_DELETED'] = '已刪除';
+$mod_strings['LBL_NAME'] = '名稱';
+$mod_strings['LBL_CREATED_USER'] = '創建者';
+$mod_strings['LBL_MODIFIED_USER'] = '修改的使用者';
+$mod_strings['LBL_LIST_NAME'] = '名稱';
+$mod_strings['LBL_LIST_FORM_TITLE'] = '標記清單';
+$mod_strings['LBL_MODULE_NAME'] = '標記';
+$mod_strings['LBL_MODULE_TITLE'] = '標記';
+$mod_strings['LBL_HOMEPAGE_TITLE'] = '我的標記';
+$mod_strings['LNK_NEW_RECORD'] = '創建標記';
+$mod_strings['LNK_LIST'] = '查看標記';
+$mod_strings['LNK_IMPORT_JJWG_MARKERS'] = '導入標記';
+$mod_strings['LBL_SEARCH_FORM_TITLE'] = '搜尋標記';
+$mod_strings['LBL_HISTORY_SUBPANEL_TITLE'] = '查看歷史記錄';
+$mod_strings['LBL_ACTIVITIES_SUBPANEL_TITLE'] = '銷售活動';
+$mod_strings['LBL_NEW_FORM_TITLE'] = '新建標記';
+$mod_strings['LBL_CITY'] = '城市';
+$mod_strings['LBL_STATE'] = '州或省';
+$mod_strings['LBL_COUNTRY'] = '國家';
+$mod_strings['LBL_JJWG_MAPS_LAT'] = '緯度';
+$mod_strings['LBL_JJWG_MAPS_LNG'] = '經度';
+$mod_strings['LBL_MARKER_IMAGE'] = '標記圖片類型';
+$mod_strings['LBL_LIST_ASSIGNED_USER'] = '負責人';
+
+$mod_strings['LBL_MARKER_MARKER_POSITION'] = '標記位置 (緯度, 經度):';
+$mod_strings['LBL_MARKER_CLOSEST_MATCHING_ADDRESS'] = '最接近的匹配地址:';
+$mod_strings['LBL_MARKER_MARKER_STATUS'] = '標記狀態';
+$mod_strings['LBL_MARKER_EDIT_DESCRIPTION'] = '按一下並拖動標記。';
+$mod_strings['LBL_JJWG_MAPS_JJWG_MARKERS_FROM_JJWG_MAPS_TITLE'] = '地圖';
