b908fad836e257745f810c65cfe5ebe8c98c3e0f
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-01-30 18:04:51 +0000

    New translations en_us.lang.php (Tagalog)

---
+++ translations/tl_PH/modules/Configurator/language/tl_PH.lang.php
@@ -0,0 +1,230 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    /*'ADMIN_EXPORT_ONLY'=>'Admin export only',*/
+    'ADVANCED' => 'Naka-adbans',
+    'DEFAULT_CURRENCY_ISO4217' => 'Kodigo sa kurensiya na ISO 4217',
+    'DEFAULT_CURRENCY_NAME' => 'Pangalan ng kuwarta',
+    'DEFAULT_CURRENCY_SYMBOL' => 'Simbolo ng kuwarta',
+    'DEFAULT_DATE_FORMAT' => 'Naka-default na format ng petsa',
+    'DEFAULT_DECIMAL_SEP' => 'Simbolong desimal',
+    'DEFAULT_LANGUAGE' => 'Naka-default na lingguwahe',
+    'DEFAULT_SYSTEM_SETTINGS' => 'Interfeys ng Gumagamit',
+    'DEFAULT_THEME' => 'Naka-default na tema',
+    'DEFAULT_TIME_FORMAT' => 'Naka-default na format ng oras',
+
+    'DISPLAY_RESPONSE_TIME' => 'Nag-didispley sa mga oras ng pag-responde ng serber',
+
+    'IMAGES' => 'Mga logo',
+    'LBL_ALLOW_USER_TABS' => 'Pinapayagan ang mga gumagamit na itago ang mga tab',
+    'LBL_CONFIGURE_SETTINGS_TITLE' => 'Mga Setting ng Sistema',
+    'LBL_LOGVIEW' => 'I-byu ang Log',
+    'LBL_MAIL_SMTPAUTH_REQ' => 'Gamitin ang Awtentikasyon sa SMTP?',
+    'LBL_MAIL_SMTPPASS' => 'Ang Password sa SMTP:',
+    'LBL_MAIL_SMTPPORT' => 'Ang port ng SMTP:',
+    'LBL_MAIL_SMTPSERVER' => 'Ang Serber ng SMTP:',
+    'LBL_MAIL_SMTPUSER' => 'Ang username ng SMTP:',
+    'LBL_MAIL_SMTP_SETTINGS' => 'Espesipikasyon sa Serber ng SMTP',
+    'LBL_CHOOSE_EMAIL_PROVIDER' => 'Pumili ng tagapagpapabigay ng iyong Email:',
+    'LBL_YAHOOMAIL_SMTPPASS' => 'Password sa Yahoo! Mail:',
+    'LBL_YAHOOMAIL_SMTPUSER' => 'ID ng Yahoo! Mail:',
+    'LBL_GMAIL_SMTPPASS' => 'Password sa Gmail:',
+    'LBL_GMAIL_SMTPUSER' => 'Email Address sa Gmail:',
+    'LBL_EXCHANGE_SMTPPASS' => 'Pagpapalit ng Password:',
+    'LBL_EXCHANGE_SMTPUSER' => 'Pagpapalit ng Username:',
+    'LBL_EXCHANGE_SMTPPORT' => 'Pagpapalit sa Port ng Serber:',
+    'LBL_EXCHANGE_SMTPSERVER' => 'Pagpapalit ng Serber:',
+    'LBL_ALLOW_DEFAULT_SELECTION' => 'Pinapayagan ang mga gumagamit na makagamit sa account na ito para papalitang email:',
+    'LBL_ALLOW_DEFAULT_SELECTION_HELP' => 'Kung ang opsyon na ito ay napilian, ang lahat ng gumagamit ay maaaring makakapagpadala ng mga email gamit ang account sa mail na papalitan na ginamit para makapagpadala ng mga notipikasyon sa sistema at mga alerto. Kung ang opsyon na ito ay hindi pinilian, ang mga gumagamit ay maaaring pa ring makakapaggamit sa serber ng mail na papalitan matapos makapagbigay ng impormasyon sa kanilang sariling account.',
+    'LBL_MAILMERGE' => 'Ang pagsasama ng sulat',
+    'LBL_MIN_AUTO_REFRESH_INTERVAL' => 'Minimong Interbal sa Awto-Refresh ng Dashlet',
+    'LBL_MIN_AUTO_REFRESH_INTERVAL_HELP' => 'Ito ang minimong halaga na ang isa ay maaaring makapagpili para magkaroon ng awto-refresh sa mga dashlet. Itakda sa \'Hinding-hindi\' na hindi nagpapagana sa pag-awto-refresh ng buong mga dashlet.',
+    'LBL_MODULE_FAVICON' => 'Pagpapakita sa ikono ng modyul bilang isang favicon',
+    'LBL_MODULE_FAVICON_HELP' => 'Kung ikaw ay nasa modyul na may ikono, gamitin ang ikono ng modyul bilang isang favicon, sa halip ng tema ng favicon, sa tab ng browser.',
+    'LBL_MODULE_NAME' => 'Mga Setting ng Sistema',
+    'LBL_MODULE_ID' => 'Tagapag-konfigyura',
+    'LBL_MODULE_TITLE' => 'Interfeys ng Gumagamit',
+    'LBL_NOTIFY_FROMADDRESS' => '"Mula" sa Address:',
+    'LBL_NOTIFY_SUBJECT' => 'Subjek ng email:',
+
+    'LBL_PROXY_AUTH' => 'Awtentikasyon?',
+    'LBL_PROXY_HOST' => 'Host na Proksi',
+    'LBL_PROXY_ON_DESC' => 'I-konfigyura ang address sa serber ng proksi at mga setting sa awtentikasyon',
+    'LBL_PROXY_ON' => 'Gumamit sa proksing serber?',
+    'LBL_PROXY_PASSWORD' => 'Ang Password',
+    'LBL_PROXY_PORT' => 'Ang Port',
+    'LBL_PROXY_TITLE' => 'Mga Setting ng Proksi',
+    'LBL_PROXY_USERNAME' => 'Gamit na Pangalan',
+    'LBL_RESTORE_BUTTON_LABEL' => 'Panumbalikin',
+    'LBL_SYSTEM_SETTINGS' => 'Mga Setting ng Sistema',
+    'LBL_USE_REAL_NAMES' => 'Ipakita ang mga Buong Pangalan',
+    'LBL_USE_REAL_NAMES_DESC' => 'Nagdi-displey sa mga buong pangalan ng mga gumagamit sa halip ng kanilang Mga Pangalan ng Gumagamit sa mga patlang ng asaynment.',
+    'LBL_DISALBE_CONVERT_LEAD' => 'Hindi ipagana ang pag-konbert sa aksyong lead para sa mga lead ng kinombert',
+    'LBL_DISALBE_CONVERT_LEAD_DESC' => 'Kung ang isang lead ay nai-konbert na pala, ang pag-papagana sa opsyong ito na makapagtanggal sa aksyon ng pagkonbert sa lead.',
+    'LBL_ENABLE_ACTION_MENU' => 'I-displey ang mga aksyon sa loob ng mga menu',
+    'LBL_ENABLE_ACTION_MENU_DESC' => 'Piliin para makapag-displey ng DetalyeByu at aksyong subpanel sa loob ng isang menu na naka-dropdown. Kung ito ay hindi pinili, ang mga aksyon ay madidispley bilang mga pindutan na magkakahiwalay.',
+    'LBL_ENABLE_INLINE_EDITING_LIST' => 'Paganahin ang pag-eedit na nasa linya sa listahan ng view',
+    'LBL_ENABLE_INLINE_EDITING_LIST_DESC' => 'Piliin para makapagpagana sa Pag-eedit na NasaLinya para sa mga patlang na nasa byu ng listahan. Kung ang hindi napiliang Pag-eedit na NasaLinya ay magiging hindi papaganahin sa byu ng listahan.',
+    'LBL_ENABLE_INLINE_EDITING_DETAIL' => 'Pagpapagana sa pag-eedit na nasalinya sa byu ng detalye',
+    'LBL_ENABLE_INLINE_EDITING_DETAIL_DESC' => 'Piliin para makapagpagana sa Pag-eedit na NasaLinya para sa mga patlang sa detalye ng byu. Kung ang hindi napiliang Pag-eedit na NasaLinya ay magiging hindi pinapagana sa detalye ng byu.',
+    'LBL_HIDE_SUBPANELS' => 'Mga subpanel na gumuho',
+    'LIST_ENTRIES_PER_LISTVIEW' => 'ListahanByu sa mga aytem kada pahina',
+    'LIST_ENTRIES_PER_SUBPANEL' => 'Mga aytem na subpanel kada pahina',
+    'LOG_MEMORY_USAGE' => 'Log sa memorya ng pagkakagamit',
+    'LOG_SLOW_QUERIES' => 'Log sa mahihinang mga katanungan',
+    'CURRENT_LOGO' => 'Logo sa Pangkasalukuyan:',
+    'CURRENT_LOGO_HELP' => 'This logo is displayed in the centre of the login screen of the SuiteCRM application.',
+    'NEW_LOGO' => 'Pumili ng Logo:',
+    'NEW_LOGO_HELP' => 'Ang format sa payl ng imahe ay maaaring .png o .jog. Ang maksimong taas ay 170px, at ang maksimong lapad ay 450px. Anumang imahe na iiupload na mas malaki pa sa anumang direksyon ay i-iskala sa maks na mga dimensiyon na ito.',
+    'NEW_LOGO_HELP_NO_SPACE' => 'Ang format sa payl ng imahe ay maaaring .png o .jog. Ang maksimong taas ay 170px, at ang maksimong lapad ay 450px. Anumang imahe na iiupload na mas malaki pa sa anumang direksyon ay i-iskala sa maks na mga dimensiyon na ito.',
+    'SLOW_QUERY_TIME_MSEC' => 'Threshold sa oras ng mahihinang tanong (msec)',
+    'STACK_TRACE_ERRORS' => 'Mag-displey sa mga naka-istak na tandaan ng mga mali',
+    'UPLOAD_MAX_SIZE' => 'Maksimong laki ng pag-upload',
+    'VERIFY_CLIENT_IP' => 'Pagbalido sa IP address ng gumagamit',
+    'LOCK_HOMEPAGE' => 'Naghaharang sa lay-out na i-customize ang Homepage ng gumagamit',
+    'LOCK_SUBPANELS' => 'Naghaharang sa layout na subpanel na nakocostumize ng gumagamit',
+    'MAX_DASHLETS' => 'Maksimong numero ng Dashlet na SuiteCRM dito sa Homepage',
+    'SYSTEM_NAME' => 'Pangalan ng Sistema:',
+    'SYSTEM_NAME_WIZARD' => 'Ang Pangalan:',
+    'SYSTEM_NAME_HELP' => 'Ito ang pangalan na naka-displey sa bar ng titulo sa iyong browser.',
+    'LBL_LDAP_TITLE' => 'Suporta sa Awtentikasyon ng LDAP',
+    'LBL_LDAP_ENABLE' => 'Ipagana ang LDAP',
+    'LBL_LDAP_SERVER_HOSTNAME' => 'Serber:',
+    'LBL_LDAP_SERVER_PORT' => 'Numero ng Port:',
+    'LBL_LDAP_ADMIN_USER' => 'Pangalan ng Gumagamit:',
+    'LBL_LDAP_ADMIN_USER_DESC' => 'Gamitin sa paghahanap para sa LDAP gumagamit. Ito ay maaaring kailanganing ganap na kwalipikado.',
+    'LBL_LDAP_ADMIN_PASSWORD' => 'Ang Password:',
+    'LBL_LDAP_AUTHENTICATION' => 'Pagpapatunay:',
+    'LBL_LDAP_AUTHENTICATION_DESC' => 'Maagbigkissa LDAP server gamit ang natukoy na mga keredensiyal ng mga gumagamit. Ito ay magbibigkis ng hindi nagpapakilala kung hindi mapupunan.',
+    'LBL_LDAP_AUTO_CREATE_USERS' => 'Awtomatikong paglikha ng mga gumagamit:',
+    'LBL_LDAP_USER_DN' => 'Ang DN na gumagamit:',
+    'LBL_LDAP_GROUP_DN' => 'Grupo ng DN:',
+    'LBL_LDAP_GROUP_DN_DESC' => 'Halimbawa: <em>ou=mga pangkat,dc=halimbawa,dc=com</em>',
+    'LBL_LDAP_USER_FILTER' => 'Ang salaan ng gumagamit:',
+    'LBL_LDAP_GROUP_MEMBERSHIP' => 'Pagpapamiyembro ng grupo:',
+    'LBL_LDAP_GROUP_MEMBERSHIP_DESC' => 'Ang mga gumagamit ay dapat miyembro ng isang espesipikong grupo',
+    'LBL_LDAP_GROUP_USER_ATTR' => 'Katangian ng gumagamit:',
+    'LBL_LDAP_GROUP_USER_ATTR_DESC' => 'Ang tagapagtukoy ng kakaiba sa tao ay ginagamit sa pagsuri kung sila ba ay mga miyemro sa grupo Halimbawa: <em>uid</em>',
+    'LBL_LDAP_GROUP_ATTR_DESC' => 'Ang katangian ng grupo ay magagamit para isala ang mga tutol sa Halimbawa sa katangian ng gumagamit: <em>memberUid</em>',
+    'LBL_LDAP_GROUP_ATTR' => 'Katangian ng Grupo:',
+    'LBL_LDAP_USER_FILTER_DESC' => 'Anumang mga parametro sa adisyonal na salaan para mai-aplay kung ang mga gumagamit ay ino-awtentika hal.<em>is_suitecrm_user=1 o (is_suitecrm_user=1)(is_sales=1)</em>',
+    'LBL_LDAP_LOGIN_ATTRIBUTE' => 'Katangian sa Login:',
+    'LBL_LDAP_BIND_ATTRIBUTE' => 'Katangian ng Pagbigkis:',
+    'LBL_LDAP_BIND_ATTRIBUTE_DESC' => 'Para sa pagbuo ng mga Halimbawa na gumagamit ng LDAP:[<b>AD:</b>&nbsp;userPrincipalName] [<b>openLDAP:</b>&nbsp;dn] [<b>Mac&nbsp;OS&nbsp;X:</b>&nbsp;uid] ',
+    'LBL_LDAP_LOGIN_ATTRIBUTE_DESC' => 'Para sa paghanap ng mga Halimbawa na gumagamit ng LDAP:[<b>AD:</b>&nbsp;userPrincipalName] [<b>openLDAP:</b>&nbsp;cn] [<b>Mac&nbsp;OS&nbsp;X:</b>&nbsp;dn] ',
+    'LBL_LDAP_SERVER_HOSTNAME_DESC' => 'Halimbawa: Idap.example.com o Idaps://Idap.example.com para sa SSL',
+    'LBL_LDAP_SERVER_PORT_DESC' => 'Halimbawa: <em>389 o 636 para sa SSL</em>',
+    'LBL_LDAP_GROUP_NAME' => 'Pangalan ng Pangkat:',
+    'LBL_LDAP_GROUP_NAME_DESC' => 'Halimbawa <em>cn=suitecrm</em>',
+    'LBL_LDAP_USER_DN_DESC' => 'Halimbawa:<em>ou=mga tao,dc=halimbawa,dc=com</em>',
+    'LBL_LDAP_AUTO_CREATE_USERS_DESC' => 'Kung ang napatunayang gumagamit ay hindi umiiral, ang isa ay malilikha sa SuiteCRM.',
+    'LBL_LDAP_ENC_KEY' => 'Key sa Enkrypsyon:',
+    'DEVELOPER_MODE' => 'Moda sa Tagapagdevelop',
+
+    'SHOW_DOWNLOADS_TAB' => 'Tab ng Displey ng mga Download',
+    'SHOW_DOWNLOADS_TAB_HELP' => 'Kapag napili, ang Download tab ay lalabas sa mga setting ng gumagamit at bigyan ang gumagamit ng pahintulot para makapasok sa SuiteCRM plug-ins at iba pang bakanteng mga file',
+    'LBL_LDAP_ENC_KEY_DESC' => 'Para sa awtentikasyong SOAP kapag ginagamit ang LDAP.',
+    'LDAP_ENC_KEY_NO_FUNC_DESC' => 'Ang ekstensyong php_mcrypt ay maaaring mapagana sa iyong payl na php.ini.',
+    'LDAP_ENC_KEY_NO_FUNC_OPENSSL_DESC' => 'The openssl extension must be enabled in your php.ini file.',
+    'LBL_ALL' => 'Lahat',
+    'LBL_MARK_POINT' => 'Puntong Minarkahan',
+    'LBL_NEXT_' => 'Susunod>>',
+    'LBL_REFRESH_FROM_MARK' => 'I-refresh Mula sa Marka',
+    'LBL_SEARCH' => 'Maghanap:',
+    'LBL_REG_EXP' => 'Exp na Reg:',
+    'LBL_IGNORE_SELF' => 'Balewalain ang Sarili:',
+    'LBL_MARKING_WHERE_START_LOGGING' => 'Minarkahan Kung Saan Makakapagsimulang Mag-log Mula sa',
+    'LBL_DISPLAYING_LOG' => 'Nagdi-displey ng Log',
+    'LBL_YOUR_PROCESS_ID' => 'Ang iyong ID sa proseso',
+    'LBL_YOUR_IP_ADDRESS' => 'Ang iyong IP Address ay',
+    'LBL_IT_WILL_BE_IGNORED' => ' Ito ay babalewalain ',
+    'LBL_LOG_NOT_CHANGED' => 'Ang Log ay hindi pa binago',
+    'LBL_ALERT_JPG_IMAGE' => 'Ang format ng fayl sa imaheng ito ay dapat naka JPEG. Mag-upload ng isang fayl na bago na may fayl ekstensyon na .jpg.',
+    'LBL_ALERT_TYPE_IMAGE' => 'Ang pormat ng fayl sa imaheng ito ay dapat naka JOEG o PNG. Mag-upload ng isang fayl na bago may fayl ekstensyong .jpg o .png.',
+    'LBL_ALERT_SIZE_RATIO' => 'Ang panumbas sa aspekto para sa imahe ay nararapat na nasa pagitang 1:1 at 10:1. Ang imahen ay masusukat nang muli.',
+    'ERR_ALERT_FILE_UPLOAD' => 'May nangyaring kamalian sa pag-a-upload ng imahe.',
+    'LBL_LOGGER' => 'Mga Setting ng Tagapag-log',
+    'LBL_LOGGER_FILENAME' => 'Pangalan sa Fayl ng Log',
+    'LBL_LOGGER_FILE_EXTENSION' => 'Ekstensyon',
+    'LBL_LOGGER_MAX_LOG_SIZE' => 'Maksimong laki ng log',
+    'LBL_STACK_TRACE' => 'Enable stack trace',
+    'LBL_LOGGER_DEFAULT_DATE_FORMAT' => 'Naka-default na format ng petsa',
+    'LBL_LOGGER_LOG_LEVEL' => 'Antas ng Log',
+    'LBL_LEAD_CONV_OPTION' => 'Mga Opsyon sa Kumbersyon ng Nangunguna',
+    'LEAD_CONV_OPT_HELP' => "<b>Kopya</b> - Gumagawa at umuugnay sa mga kopya ng lahat ng mga aktibidad ng Namumuno sa gumagamit habang nagku-kumbersyon. Ang kopyang naipasagawa para sa bawat napagpiliang mga rekord.<br><br><b>Ilipat</b> - Naglilipat sa lahat ng mga aktibidad ng Namumuno patungo sa isang rekord na napakabago na napagpilian para sa gumagamit habang nagkukumbersyon. <br><br><b>Walang Ginawa</b> - Walang ginagawa sa mga aktibidad ng Namumuno habanag nagkukumbersyon. Ang mga natitirang mga aktibidad ay may kaugnayan sa Namumuno lamang.",
+    'LBL_CONFIG_AJAX' => 'Konfigyurahin ang Interface sa Gumagamit ng AJAX',
+    'LBL_CONFIG_AJAX_DESC' => 'Ipagana o hindi ipagana ang paggamit sa UI ng AJAX para sa mga modyul na espesipiko.',
+    'LBL_LOGGER_MAX_LOGS' => 'Pinakamaraming bilang ng mga log (bago ang pagpapaikot)',
+    'LBL_LOGGER_FILENAME_SUFFIX' => 'Apendohan pagkatapos ng faylname',
+    'LBL_VCAL_PERIOD' => 'haba ng Oras sa mga Updeyt pang-vCal:',
+    'LBL_IMPORT_MAX_RECORDS' => 'Mag-angkat - Pinakamaraming Bilang ng mga Hilera:',
+    'LBL_IMPORT_MAX_RECORDS_HELP' => 'Mag-espesepay kung gaano karami ang mga hilera na pinapayagan sa loob ng mga fayl na nai-angkat.<br>Kung ang bilang ng mga hilera sa payl na inangkat ay lumampas sa numerong ito, aalertohan ang gumagamit.<br>Kung wala namang numerong naipasok, isang walang hangganang bilang ng mga pinapayagang hilera.',
+    'vCAL_HELP' => 'Gamitin ang setting na ito para makapag-tukoy sa bilang ng mga buwan na adbans sa petsang pangkasalukuyan na naiilathala ang mga Libre/Busy na impormasyon sa mga tawag at sa mga pagtitipon.<BR>Para mai-turn off ang paglalathalang Libre/Busy, mangyaring magpasok ng “0”. Ang minimo ay 1 na buwan; ang maksimong bilang ng buwan ay 12.',
+
+// Wizard
+    //Wizard Scenarios
+    'LBL_WIZARD_SCENARIOS' => 'Iyong Mga Senaryo',
+    'LBL_WIZARD_SCENARIOS_EMPTY_LIST' => 'Walang mga senaryong nai-konfigyura na',
+    'LBL_WIZARD_SCENARIOS_DESC' => 'Piliin kung anong mga senaryo ang naaayon para sa iyong instolasyon. Ang mga opsyong ito ay maaaring mababago pagkatapos mag-instol.',
+
+    'LBL_WIZARD_TITLE' => 'Wizard ng Admin',
+    'LBL_WIZARD_WELCOME_TAB' => 'Walang anuman',
+    'LBL_WIZARD_WELCOME_TITLE' => 'Maligayang Pagdating sa SuiteCRM!',
+    'LBL_WIZARD_WELCOME' => 'I-klik ang <b>Susunod</b> para makapag-tatak, makapag-lokalisa at makapag-konfigyur sa SuiteCRM ngayon. Kung gusto mong mag-konfigyur sa SuiteCRM mamaya, i-klik ang <b>Paglaktawan</b>.',
+    'LBL_WIZARD_NEXT_BUTTON' => 'Susunod >',
+    'LBL_WIZARD_BACK_BUTTON' => '< Bumalik',
+    'LBL_WIZARD_SKIP_BUTTON' => 'Laktawan',
+    'LBL_WIZARD_CONTINUE_BUTTON' => 'Magpatuloy',
+    'LBL_WIZARD_FINISH_TITLE' => 'Ang pangunahing konfigyurasyon sa sistema ay naikumpleto na',
+    'LBL_WIZARD_SYSTEM_TITLE' => 'Pagpatatak',
+    'LBL_WIZARD_SYSTEM_DESC' => 'Magbigay ng pangalan sa iyong organisasyon at logo nang sa ganun ikaw ay magpa-tatak sa iyong SuiteCRM.',
+    'LBL_WIZARD_LOCALE_DESC' => 'Tukuyin kung papaano ang gusto mong maipa-displey na data sa SuiteCRM, na basi sa iyong heograpikal na lokasyon. Ang mga setting na iyong ibinigay rito ay magiging mga setting na naka-default. Ang mga gumagamit ay maaaring makapag-set ng kani-kanilang mga sariling preferensiya.',
+    'LBL_WIZARD_SMTP_DESC' => 'Magbigay ng account sa email na gagamitin sa pagpapadala ng mga email, kagaya ng mga notipikasyon sa asaynment at mga password ng gumagamit na baguhan. Ang mga gumagamit ay makakatanggap ng mga email mula sa SuiteCRM, bilang isang naipadala mula sa tukoy na account sa email.',
+    'LBL_LOADING' => 'Nag-lo-load...' /*for 508 compliance fix*/,
+    'LBL_DELETE' => 'Burahin' /*for 508 compliance fix*/,
+    'LBL_WELCOME' => 'Walang anuman' /*for 508 compliance fix*/,
+    'LBL_LOGO' => 'Logo' /*for 508 compliance fix*/,
+    'LBL_ENABLE_HISTORY_CONTACTS_EMAILS' => 'Ipakita ang mga email ng kaugnay ng kontak sa subpanel na Kasaysayan para sa mga modyul',
+);
