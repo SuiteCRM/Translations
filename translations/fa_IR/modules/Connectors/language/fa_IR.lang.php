d3c2b212a8b1b99fd2311ea38849768c271b79bb
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-01-30 18:10:12 +0000

    New translations en_us.lang.php (Persian)

---
+++ translations/fa_IR/modules/Connectors/language/fa_IR.lang.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+
+$mod_strings = array(
+
+    'LBL_ADMINISTRATION_MAIN' => 'تنظیمات اتصال دهنده',
+    'LBL_AVAILABLE' => 'در دسترس',
+    'LBL_BACK' => 'بازگشت',
+    'LBL_CONFIRM_CONTINUE_SAVE' => 'Some required fields have been left blank. Proceed to save changes?',
+    'LBL_CONNECTOR_FIELDS' => 'زمینه های اتصال',
+    'LBL_DATA' => 'داده ها',
+    'LBL_DEFAULT' => 'پیش‌فرض',
+    'LBL_DISABLED' => 'غیرفعال شده',
+    'LBL_ENABLED' => 'فعال شده',
+    'LBL_EXTERNAL' => 'Enable users to create external account records to this connector.',
+    'LBL_EXTERNAL_SET_PROPERTIES' => ' In order to use this connector, the properties should also be set in the Set Connector Properties settings page.',
+    'LBL_MERGE' => 'ادغام',
+    'LBL_MODIFY_DISPLAY_TITLE' => 'فعال کردن اتصالات',
+    'LBL_MODIFY_DISPLAY_DESC' => 'Select which modules are enabled for each connector.',
+    'LBL_MODULE_FIELDS' => 'Module Fields',
+    'LBL_MODIFY_MAPPING_TITLE' => 'طراحی قسمت متصل کننده',
+    'LBL_MODIFY_MAPPING_DESC' => 'اتصال به زمینه های ماژول به منظور تعیین اطلاعات اتصال می تواند مشاهده و ادغام پرونده ماژول فیلد.',
+    'LBL_MODIFY_PROPERTIES_TITLE' => 'تعیین تنظیمات رابط',
+    'LBL_MODIFY_PROPERTIES_DESC' => 'Configure the properties for each connector, including URLs and API keys.',
+    'LBL_MODIFY_SEARCH_TITLE' => 'مدیریت رابط جستجو',
+    'LBL_MODIFY_SEARCH' => 'جستجو',
+    'LBL_MODIFY_SEARCH_DESC' => 'زمینه های اتصال برای استفاده از داده ها برای هر ماژول جستجو را انتخاب کنید.',
+    'LBL_MODULE_NAME' => 'متصل‌کننده‌ها',
+    'LBL_NO_PROPERTIES' => 'ویژگی قابل تنظیم برای این رابط وجود دارد.',
+    'LBL_SAVE' => 'ذخيره',
+    'LBL_SUMMARY' => 'خلاصه',
+    'LBL_STEP1' => 'جستجو و مشاهده داده ها',
+    'LBL_STEP2' => 'ادغام اطلاعات با',
+    'LBL_TEST_SOURCE' => 'تست اتصال دهنده',
+    'LBL_TEST_SOURCE_FAILED' => 'تست ناموفق بود',
+    'LBL_TEST_SOURCE_SUCCESS' => 'تست موفقیت آمیز بود',
+    'LBL_TITLE' => 'ادغام داده ها',
+
+    'ERROR_NO_ADDITIONAL_DETAIL' => 'خطا: جزئیات اضافی برای رکورد یافت نشد.',
+    'ERROR_NO_SEARCHDEFS_DEFINED' => 'هیچ ماژولی برای این رابط فعال نیست. یک ماژول را برای این رابط در صفحه رابط های فعال انتخاب کنید.',
+    'ERROR_NO_SEARCHDEFS_MAPPED' => 'خطا: هیچ رابط فعالی وجود ندارد که دارای فیلدهای جستجوی تعریف شده باشد.',
+    'ERROR_NO_SEARCHDEFS_MAPPING' => 'خطا: هیچ فیلد جستجویی برای این ماژول و متصل کننده‌اش تعریف نشده است.',
+    'ERROR_NO_DISPLAYABLE_MAPPED_FIELDS' => 'خطا: هیچ فیلد ماژولی که برای نمایش در نتیجه نگاشت شده باشد، وجود ندارد. لطفاً با مدیر سیستم تماس بگیرید.',
+    'LBL_INFO_INLINE' => 'اطلاعات' /*for 508 compliance fix*/,
+    'LBL_CLOSE' => 'بستن' /*for 508 compliance fix*/,
+
+);
