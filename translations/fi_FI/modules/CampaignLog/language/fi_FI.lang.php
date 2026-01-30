11764d6cb3d6390c029f412c9404713e73afe2cf
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-01-30 17:55:40 +0000

    New translations en_us.lang.php (Finnish)

---
+++ translations/fi_FI/modules/CampaignLog/language/fi_FI.lang.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_LIST_ID' => 'Prospektilistan tunnus',
+    'LBL_ID' => 'Tunnus',
+    'LBL_TARGET_TRACKER_KEY' => 'Prospektin seuranta-avain',
+    'LBL_TARGET_ID' => 'Prospektin tunnus',
+    'LBL_TARGET_TYPE' => 'Prospektin tyyppi',
+    'LBL_ACTIVITY_TYPE' => 'Aktiviteettityyppi',
+    'LBL_ACTIVITY_DATE' => 'Aktiviteetin pvm',
+    'LBL_RELATED_ID' => 'Liittyvän tunnus',
+    'LBL_RELATED_TYPE' => 'Liittyvän tyypi',
+    'LBL_DELETED' => 'Poistettu',
+    'LBL_MODULE_NAME' => 'Kampanjaloki',
+    'LBL_LIST_RECIPIENT_EMAIL' => 'Vastaanottajan sähköpostiosoite',
+    'LBL_LIST_RECIPIENT_NAME' => 'Vastaanottajan nimi',
+    'LBL_ARCHIVED' => 'Arkistoitu',
+    'LBL_HITS' => 'Osumia',
+
+    'LBL_CAMPAIGN_NAME' => 'Nimi:',
+    'LBL_CAMPAIGN' => 'Kampanja:',
+    'LBL_NAME' => 'Nimi: ',
+    'LBL_INVITEE' => 'Kontaktit',
+    'LBL_LIST_CAMPAIGN_NAME' => 'Kampanjat',
+    'LBL_LIST_STATUS' => 'Tila',
+    'LBL_LIST_TYPE' => 'Tyyppi',
+    'LBL_LIST_END_DATE' => 'Vanhentumispäivä',
+    'LBL_DATE_ENTERED' => 'Kirjauspvm',
+    'LBL_DATE_MODIFIED' => 'Muokkaus pvm',
+    'LBL_MODIFIED' => 'Muokkaaja: ',
+    'LBL_CREATED' => 'Tekijä: ',
+    'LBL_TEAM' => 'Tiimi: ',
+    'LBL_ASSIGNED_TO' => 'Vastuuhenkilö: ',
+    'LBL_CAMPAIGN_START_DATE' => 'Aloituspvm: ',
+    'LBL_CAMPAIGN_END_DATE' => 'Lopetuspvm: ',
+    'LBL_CAMPAIGN_STATUS' => 'Tila: ',
+    'LBL_CAMPAIGN_BUDGET' => 'Budjetti: ',
+    'LBL_CAMPAIGN_EXPECTED_COST' => 'Odotettu kustannus: ',
+    'LBL_CAMPAIGN_ACTUAL_COST' => 'Todellinen kustannus: ',
+    'LBL_CAMPAIGN_EXPECTED_REVENUE' => 'Odotettu tuotto: ',
+    'LBL_CAMPAIGN_TYPE' => 'Tyyppi: ',
+    'LBL_CAMPAIGN_OBJECTIVE' => 'Tavoite: ',
+    'LBL_CAMPAIGN_CONTENT' => 'Kuvaus: ',
+    'LBL_CREATED_LEAD' => 'Liidi luotu',
+    'LBL_CREATED_CONTACT' => 'Kontakti luotu',
+    'LBL_CREATED_OPPORTUNITY' => 'Myyntimahdollisuus luotu',
+    'LBL_TARGETED_USER' => 'Kohdekäyttäjä',
+    'LBL_SENT_EMAIL' => 'Sähköposti lähetetty',
+    'LBL_LIST_FORM_TITLE' => 'Kohdistetut kampanjat',
+    'LBL_LIST_ACTIVITY_DATE' => 'Aktiviteetin pvm',
+    'LBL_RELATED' => 'Liittyen',
+    'LBL_MORE_INFO' => 'Lisätietoja',
+
+    'LBL_CAMPAIGNS' => 'Kampanjat',
+    'LBL_LIST_MARKETING_NAME' => 'Markkinointitunnus',
+
+    'LBL_MARKETING_ID' => 'Markkinointitunnus',
+    'LBL_RELATED_NAME' => 'Liittyvä nimi',
+);
