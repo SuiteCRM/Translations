86fd4c29141a74a12bd12f673586e8e0326f614f
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-01-30 17:27:19 +0000

    New translations en_us.lang.php (Romanian)

---
+++ translations/ro_RO/modules/AOR_Scheduled_Reports/language/ro_RO.lang.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID' => 'Id atribuit utilizatorului',
+    'LBL_ASSIGNED_TO_NAME' => 'Atribuit lui',
+    'LBL_ID' => 'ID-ul',
+    'LBL_DATE_ENTERED' => 'Data creării',
+    'LBL_DATE_MODIFIED' => 'Data modificării',
+    'LBL_MODIFIED' => 'Modificat de',
+    'LBL_MODIFIED_NAME' => 'Nume modificant',
+    'LBL_CREATED' => 'Creat De',
+    'LBL_DESCRIPTION' => 'Descriere',
+    'LBL_DELETED' => 'Şters',
+    'LBL_NAME' => 'Nume',
+    'LBL_CREATED_USER' => 'Creat de Utilizator',
+    'LBL_MODIFIED_USER' => 'Modificat de utilizator',
+    'LBL_LIST_NAME' => 'Nume',
+    'LBL_EDIT_BUTTON' => 'Editați',
+    'LBL_REMOVE' => 'Înlăturați',
+    'LBL_ANY_EMAIL' => 'Orice Email:',
+    'LBL_EMAIL_NON_PRIMARY' => 'E-mail alternativ',
+    'LBL_ASSIGNED_TO' => 'Atribuit lui:',
+    'LBL_ASSIGNED_USER' => 'Atribuit lui:',
+    'LBL_EMAIL_RECIPIENTS' => 'Destinatari de e-mail:',
+    'NTC_DELETE_CONFIRMATION' => 'Sunteți sigur că doriți să ștergeți această înregistrare?',
+    'LBL_LIST_FORM_TITLE' => 'Rapoarte Programate',
+    'LBL_MODULE_NAME' => 'Rapoarte Programate',
+    'LBL_MODULE_TITLE' => 'Rapoarte Programate',
+    'LBL_HOMEPAGE_TITLE' => 'Rapoartele mele Programate',
+    'LNK_NEW_RECORD' => 'Creează Rapoarte Programate',
+    'LNK_LIST' => 'Vezi Rapoarte Programate',
+    'LBL_SEARCH_FORM_TITLE' => 'Caută Rapoarte Programate',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Vizualizare Istoric',
+    'LBL_NEW_FORM_TITLE' => 'Noi Rapoarte Programate',
+    'LBL_SCHEDULE' => 'Program',
+    'LBL_AOR_REPORT_NAME' => 'Rapoarte',
+    'LBL_SCHEDULED_REPORTS_INFORMATION' => 'Rapoarte Programate',
+    'LBL_LAST_RUN' => 'Ultima execuție',
+    'LBL_STATUS' => 'Status',
+);
