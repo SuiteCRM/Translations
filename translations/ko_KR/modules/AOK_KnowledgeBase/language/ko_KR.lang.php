adbe21e9ab70fd2ab814e0789529bb4155435dc3
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-01-30 17:17:23 +0000

    New translations en_us.lang.php (Korean)

---
+++ translations/ko_KR/modules/AOK_KnowledgeBase/language/ko_KR.lang.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID' => '할당된 사용자 Id',
+    'LBL_ASSIGNED_TO_NAME' => '담당자',
+    'LBL_ID' => 'ID',
+    'LBL_DATE_ENTERED' => '생성날짜',
+    'LBL_DATE_MODIFIED' => '수정일',
+    'LBL_MODIFIED' => '변경됨',
+    'LBL_MODIFIED_NAME' => '이름으로 변경됨',
+    'LBL_CREATED' => '작성자',
+    'LBL_DESCRIPTION' => '본문',
+    'LBL_ADDITIONAL_INFO' => '해결',
+    'LBL_DELETED' => '삭제됨',
+    'LBL_NAME' => '제목',
+    'LBL_CREATED_USER' => '사용자로 생성됨',
+    'LBL_MODIFIED_USER' => '변경유저',
+    'LBL_LIST_NAME' => '이름',
+    'LBL_EDIT_BUTTON' => '편집',
+    'LBL_REMOVE' => '삭제',
+    'LBL_LIST_FORM_TITLE' => '지식 자료 목록',
+    'LBL_MODULE_NAME' => '지식 자료',
+    'LBL_MODULE_TITLE' => '지식 자료',
+    'LBL_HOMEPAGE_TITLE' => '내 지식 자료',
+    'LNK_NEW_RECORD' => '새 지식 자료 작성',
+    'LNK_LIST' => '지식 자료 보기',
+    'LBL_SEARCH_FORM_TITLE' => '지식 자료 검색',
+    'LBL_HISTORY_SUBPANEL_TITLE' => '이력 표시',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => '영업활동',
+    'LBL_NEW_FORM_TITLE' => '신규 지식 자료',
+    'LBL_STATUS' => '상태',
+    'LBL_REVISION' => '개정판',
+    'LBL_AUTHOR_USER_ID' => '저자 (관련된 사용자 ID)',
+    'LBL_AUTHOR' => '저자',
+    'LBL_APPROVER_USER_ID' => '승인자 (관련된 사용자 ID)',
+    'LBL_APPROVER' => '승인자',
+);
+
+$mod_strings['LBL_AOK_KB_CATEGORIES_TITLE'] = '카테고리';
