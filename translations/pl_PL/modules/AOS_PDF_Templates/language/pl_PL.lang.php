75b39e3bfce4f1eed6af5b373d8edad25da35edb
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-01-30 17:32:55 +0000

    New translations en_us.lang.php (Polish)

---
+++ translations/pl_PL/modules/AOS_PDF_Templates/language/pl_PL.lang.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID' => 'ID przydzielonego użytkownika',
+    'LBL_ASSIGNED_TO_NAME' => 'Przypisano do',
+    'LBL_ID' => 'ID',
+    'LBL_DATE_ENTERED' => 'Data utworzenia',
+    'LBL_DATE_MODIFIED' => 'Data Modyfikacji',
+    'LBL_MODIFIED' => 'Zmodyfikowane przez',
+    'LBL_MODIFIED_NAME' => 'Nazwa użytkownika modyfikującego',
+    'LBL_CREATED' => 'Utworzone przez',
+    'LBL_DESCRIPTION' => 'Treść',
+    'LBL_HEADER' => 'Nagłówek',
+    'LBL_FOOTER' => 'Stopka',
+    'LBL_DELETED' => 'Usunięte',
+    'LBL_NAME' => 'Nazwa',
+    'LBL_CREATED_USER' => 'Utworzony przez Użytkownika',
+    'LBL_MODIFIED_USER' => 'Zmodyfikowany przez Użytkownika',
+    'LBL_LIST_FORM_TITLE' => 'Lista szablonów PDF',
+    'LBL_MODULE_NAME' => 'Szablony PDF',
+    'LBL_MODULE_TITLE' => 'Szablony PDF: Strona główna',
+    'LBL_HOMEPAGE_TITLE' => 'Moje szablony PDF',
+    'LNK_NEW_RECORD' => 'Utwórz szablon PDF',
+    'LNK_LIST' => 'Przeglądaj szablony PDF',
+    'LBL_SEARCH_FORM_TITLE' => 'Szukaj Szablonu PDF',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Przeglądaj historię',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Działania',
+    'LBL_NEW_FORM_TITLE' => 'Nowy szablon PDF',
+    'LBL_TYPE' => 'Typ',
+    'LBL_ACTIVE' => 'Aktywny',
+    'LBL_BUTTON_INSERT' => 'Wprowadź',
+    'LBL_WARNING_OVERWRITE' => 'Uwaga, to spowoduje nadpisanie twojej bieżącej pracy',
+    'LBL_INSERT_FIELDS' => 'Wstaw Pola',
+
+    'LBL_SAMPLE' => 'Załaduj szablon przykładowy',
+    'LBL_PAGE' => 'Strona',
+    'LBL_PREPARED_FOR' => 'Przygotowane Dla',
+    'LBL_PREPARED_BY' => 'Przygotowany przez',
+    'LBL_QUOTE_SAMPLE' => 'Wzór oferty',
+    'LBL_INVOICE_SAMPLE' => 'Wzór faktury',
+    'LBL_ACCOUNT_SAMPLE' => 'Wzór kontrahenta',
+    'LBL_CONTACT_SAMPLE' => 'Wzór kontaktu',
+    'LBL_LEAD_SAMPLE' => 'Wzór Potencjalnego klienta',
+    'LBL_ANY_STREET' => 'Ulica',
+    'LBL_ANY_TOWN' => 'Miejscowość',
+    'LBL_ANY_WHERE' => 'Gdzie',
+
+    'LBL_QUOTE_GROUP_SAMPLE' => 'Wzór oferty - grupa',
+    'LBL_INVOICE_GROUP_SAMPLE' => 'Wzór faktury - grupa',
+    'LBL_MARGIN_LEFT' => 'Margines Lewy',
+    'LBL_MARGIN_RIGHT' => 'Margines Prawy',
+    'LBL_MARGIN_TOP' => 'Margines Górny',
+    'LBL_MARGIN_BOTTOM' => 'Margines Dolny',
+    'LBL_MARGIN_HEADER' => 'Margines Nagłówka',
+    'LBL_MARGIN_FOOTER' => 'Margines Stopki',
+    'LBL_EDITVIEW_PANEL1' => 'Marginesy',
+    'LBL_DETAILVIEW_PANEL1' => 'Marginesy',
+    'LBL_PAGE_SIZE' => 'Rozmiar Strony',
+    'LBL_ORIENTATION' => 'Orientacja',
+);
