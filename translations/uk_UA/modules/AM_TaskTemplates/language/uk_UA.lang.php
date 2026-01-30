964c5f129261103ba090adc45aa24032ba6a28b8
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-01-30 17:13:47 +0000

    New translations en_us.lang.php (Ukrainian)

---
+++ translations/uk_UA/modules/AM_TaskTemplates/language/uk_UA.lang.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID' => 'Код користувача відповідальної особи',
+    'LBL_ASSIGNED_TO_NAME' => 'Відповідальна особа',
+    'LBL_ID' => 'ID',
+    'LBL_DATE_ENTERED' => 'Дата створення',
+    'LBL_DATE_MODIFIED' => 'Дата зміни',
+    'LBL_MODIFIED' => 'Змінено',
+    'LBL_MODIFIED_ID' => 'Змінено користувачем',
+    'LBL_MODIFIED_NAME' => 'Змінено користувачем',
+    'LBL_CREATED' => 'Ким створено',
+    'LBL_CREATED_ID' => 'Створено користувачем',
+    'LBL_DESCRIPTION' => 'Опис',
+    'LBL_DELETED' => 'Видалено',
+    'LBL_NAME' => 'Назва задачі',
+    'LBL_CREATED_USER' => 'Створено користувачем',
+    'LBL_MODIFIED_USER' => 'Змінено користувачем',
+    'LBL_LIST_NAME' => 'Назва',
+    'LBL_EDIT_BUTTON' => 'Змінити',
+    'LBL_REMOVE' => 'Видалити',
+    'LBL_LIST_FORM_TITLE' => 'Список шаблонів проєктних завдань',
+    'LBL_MODULE_NAME' => 'Шаблони проєктних завдань',
+    'LBL_MODULE_TITLE' => 'Шаблони проєктних завдань',
+    'LBL_HOMEPAGE_TITLE' => 'Мої шаблони проєктних завдань',
+    'LNK_NEW_RECORD' => 'Створити шаблони проєктного завдання',
+    'LNK_LIST' => 'Показ шаблонів проєктних завдань',
+    'LNK_IMPORT_AM_TASKTEMPLATES' => 'Імпорт шаблонів проєктних завдань',
+    'LBL_SEARCH_FORM_TITLE' => 'Шукати шаблони проєктних завдань',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Історія',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Заходи',
+    'LBL_AM_TASKTEMPLATES_SUBPANEL_TITLE' => 'Шаблони проєктних завдань',
+    'LBL_NEW_FORM_TITLE' => 'Новий шаблон проєктних завдань',
+    'LBL_STATUS' => 'Статус',
+    'LBL_PRIORITY' => 'Пріоритет',
+    'LBL_PERCENT_COMPLETE' => 'Процентів завершено',
+    'LBL_PREDECESSORS' => 'Попередники',
+    'LBL_MILESTONE_FLAG' => 'Відмітка віхи проєкту',
+    'LBL_RELATIONSHIP_TYPE' => 'Тип зв&#039;язку',
+    'LBL_TASK_NUMBER' => 'Номер завдання',
+    'LBL_ORDER_NUMBER' => 'Порядок ',
+    'LBL_ESTIMATED_EFFORT' => 'Оцінка зусиль (години)',
+    'LBL_UTILIZATION' => 'Зайнятість ресурсів (%)',
+    'LBL_DURATION' => 'Тривалість (дні)',
+    'LBL_AM_TASKTEMPLATES_AM_PROJECTTEMPLATES_FROM_AM_PROJECTTEMPLATES_TITLE' => 'Шаблони проектів',
+
+    'LBL_AM_TASKTEMPLATES_AM_PROJECTTEMPLATES_FROM_AM_TASKTEMPLATES_TITLE' => 'Шаблони завдань: Шаблони проєктів з шаблонів задач',
+);
