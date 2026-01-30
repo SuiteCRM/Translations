833dfb225f63e1b87d029d8b5b04b81f08116cc4
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-01-30 19:24:41 +0000

    New translations en_us.lang.php (Estonian)

---
+++ translations/et_EE/modules/SurveyQuestions/language/et_EE.lang.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID'                                                               => 'Määratud kasutaja Id',
+    'LBL_ASSIGNED_TO_NAME'                                                             => 'Vastutaja',
+    'LBL_SECURITYGROUPS'                                                               => 'Turvagrupid',
+    'LBL_SECURITYGROUPS_SUBPANEL_TITLE'                                                => 'Turvagrupid',
+    'LBL_ID'                                                                           => 'ID',
+    'LBL_DATE_ENTERED'                                                                 => 'Loomiskuupäev',
+    'LBL_DATE_MODIFIED'                                                                => 'Muutmiskuupäev',
+    'LBL_MODIFIED'                                                                     => 'Muutja',
+    'LBL_MODIFIED_ID'                                                                  => 'Muutja Id',
+    'LBL_MODIFIED_NAME'                                                                => 'Muutja nime järgi',
+    'LBL_CREATED'                                                                      => 'Loodud',
+    'LBL_CREATED_ID'                                                                   => 'Looja Id',
+    'LBL_DESCRIPTION'                                                                  => 'Kirjeldus',
+    'LBL_DELETED'                                                                      => 'Kustutatud',
+    'LBL_NAME'                                                                         => 'Nimi',
+    'LBL_CREATED_USER'                                                                 => 'Looja',
+    'LBL_MODIFIED_USER'                                                                => 'Muutja',
+    'LBL_LIST_NAME'                                                                    => 'Nimi',
+    'LBL_EDIT_BUTTON'                                                                  => 'Redigeeri',
+    'LBL_REMOVE'                                                                       => 'Eemalda',
+    'LBL_LIST_FORM_TITLE'                                                              => 'Küsitluse küsimuste List',
+    'LBL_MODULE_NAME'                                                                  => 'Küsimused küsitluses',
+    'LBL_MODULE_TITLE'                                                                 => 'Küsimused küsitluses',
+    'LBL_HOMEPAGE_TITLE'                                                               => 'Minu Küsitluse Küsimused',
+    'LNK_NEW_RECORD'                                                                   => 'Loo Küsitluse Küsimusi',
+    'LNK_LIST'                                                                         => 'Vaata Küsitluse Küsimusi',
+    'LNK_IMPORT_SURVEYQUESTIONS'                                                       => 'Impordi Küsitluse Küsimusi',
+    'LBL_SEARCH_FORM_TITLE'                                                            => 'Otsi Küsitluse Küsimusi',
+    'LBL_HISTORY_SUBPANEL_TITLE'                                                       => 'Vaata ajalugu',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE'                                                    => 'Tegevused',
+    'LBL_SURVEYQUESTIONS_SUBPANEL_TITLE'                                               => 'Küsimused küsitluses',
+    'LBL_NEW_FORM_TITLE'                                                               => 'Uued Küsitluse Küsimused',
+    'LBL_SORT_ORDER'                                                                   => 'Tellimus',
+    'LBL_TYPE'                                                                         => 'Tüüp:',
+    'LBL_HAPPINESS_QUESTION'                                                           => 'Õnne Küsimus',
+    'LBL_SURVEYS_SURVEYQUESTIONS_FROM_SURVEYS_TITLE'                                   => 'Küsitlused',
+    'LBL_SURVEYQUESTIONS_SURVEYQUESTIONRESPONSES_FROM_SURVEYQUESTIONRESPONSES_TITLE'   => 'Küsitluse küsimuste vastused',
+    'LBL_SURVEYQUESTIONS_SURVEYQUESTIONRESPONSES_1_FROM_SURVEYQUESTIONRESPONSES_TITLE' => 'Küsitluse küsimuste vastused',
+    'LBL_SURVEYQUESTIONS_SURVEYQUESTIONOPTIONS_FROM_SURVEYQUESTIONOPTIONS_TITLE'       => 'Küsitluse küsimuste valikud',
+);
