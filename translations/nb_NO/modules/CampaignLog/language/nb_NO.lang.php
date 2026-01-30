ce81eb728fb97e20bd0ee7eb5f9c83ab037e1f5e
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-01-30 17:56:33 +0000

    New translations en_us.lang.php (Norwegian Bokmal)

---
+++ translations/nb_NO/modules/CampaignLog/language/nb_NO.lang.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_LIST_ID' => 'Prospektliste ID',
+    'LBL_ID' => 'ID',
+    'LBL_TARGET_TRACKER_KEY' => 'Sporingsnøkkel for mål',
+    'LBL_TARGET_ID' => 'Mål ID',
+    'LBL_TARGET_TYPE' => 'Type mål',
+    'LBL_ACTIVITY_TYPE' => 'Aktivitetstype',
+    'LBL_ACTIVITY_DATE' => 'Aktivitetsdato',
+    'LBL_RELATED_ID' => 'Beslektet Id',
+    'LBL_RELATED_TYPE' => 'Beslektet type',
+    'LBL_DELETED' => 'Slettet',
+    'LBL_MODULE_NAME' => 'Kampanje logg',
+    'LBL_LIST_RECIPIENT_EMAIL' => 'Mottakers e-post',
+    'LBL_LIST_RECIPIENT_NAME' => 'Mottakers navn',
+    'LBL_ARCHIVED' => 'Arkivert',
+    'LBL_HITS' => 'Treff',
+
+    'LBL_CAMPAIGN_NAME' => 'Navn:',
+    'LBL_CAMPAIGN' => 'Kampanje:',
+    'LBL_NAME' => 'Navn: ',
+    'LBL_INVITEE' => 'Kontakter',
+    'LBL_LIST_CAMPAIGN_NAME' => 'Kampanjer',
+    'LBL_LIST_STATUS' => 'Status',
+    'LBL_LIST_TYPE' => 'Type',
+    'LBL_LIST_END_DATE' => 'Avslutningsdato',
+    'LBL_DATE_ENTERED' => 'Dato opprettet',
+    'LBL_DATE_MODIFIED' => 'Dato endret',
+    'LBL_MODIFIED' => 'Endret av: ',
+    'LBL_CREATED' => 'Opprettet av:',
+    'LBL_TEAM' => 'Lag: ',
+    'LBL_ASSIGNED_TO' => 'Tildelt: ',
+    'LBL_CAMPAIGN_START_DATE' => 'Påbegynt dato: ',
+    'LBL_CAMPAIGN_END_DATE' => 'Avslutningsdato: ',
+    'LBL_CAMPAIGN_STATUS' => 'Status:',
+    'LBL_CAMPAIGN_BUDGET' => 'Budsjettramme: ',
+    'LBL_CAMPAIGN_EXPECTED_COST' => 'Forventet kostnad: ',
+    'LBL_CAMPAIGN_ACTUAL_COST' => 'Aktuell kostnad: ',
+    'LBL_CAMPAIGN_EXPECTED_REVENUE' => 'Forventet omsetning: ',
+    'LBL_CAMPAIGN_TYPE' => 'Type: ',
+    'LBL_CAMPAIGN_OBJECTIVE' => 'Mål: ',
+    'LBL_CAMPAIGN_CONTENT' => 'Beskrivelse:',
+    'LBL_CREATED_LEAD' => 'Prospekt skapt',
+    'LBL_CREATED_CONTACT' => 'Kontakt skapt',
+    'LBL_CREATED_OPPORTUNITY' => 'Mulighet skapt',
+    'LBL_TARGETED_USER' => 'Bruker i sikte',
+    'LBL_SENT_EMAIL' => 'Sendt e-post',
+    'LBL_LIST_FORM_TITLE' => 'Utvalgte kampanjer',
+    'LBL_LIST_ACTIVITY_DATE' => 'Aktivitetsdato',
+    'LBL_RELATED' => 'Beslektet',
+    'LBL_MORE_INFO' => 'Mer informasjon',
+
+    'LBL_CAMPAIGNS' => 'Kampanjer',
+    'LBL_LIST_MARKETING_NAME' => 'Markedsførings Id',
+
+    'LBL_MARKETING_ID' => 'Markedsførings Id',
+    'LBL_RELATED_NAME' => 'Relaterte navn',
+);
