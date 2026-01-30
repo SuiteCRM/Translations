cb3be265af9d17fa8cb8fc4787e5006ee7df1152
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-01-30 18:29:43 +0000

    New translations en_us.lang.php (Portuguese)

---
+++ translations/pt_PT/modules/ExternalOAuthProvider/language/pt_PT.lang.php
@@ -0,0 +1,110 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2022 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = [
+
+    'LBL_ASSIGNED_TO_ID' => 'ID do utilizador atribuído',
+    'LBL_ASSIGNED_TO_NAME' => 'Atribuído a',
+    'LBL_ID' => 'ID',
+    'LBL_DATE_ENTERED' => 'Data de criação',
+    'LBL_DATE_MODIFIED' => 'Data de alteração',
+    'LBL_MODIFIED' => 'Alterado por',
+    'LBL_MODIFIED_NAME' => 'Alterado por (nome)',
+    'LBL_CREATED' => 'Criado por',
+    'LBL_DESCRIPTION' => 'Descrição',
+    'LBL_DELETED' => 'Eliminado',
+    'LBL_NAME' => 'Nome',
+    'LBL_CREATED_USER' => 'Criado pelo utilizador',
+    'LBL_MODIFIED_USER' => 'Alterado pelo utilizador',
+    'LBL_LIST_NAME' => 'Nome',
+    'LBL_EDIT_BUTTON' => 'Editar',
+    'LBL_REMOVE' => 'Remover',
+    'LBL_LIST_FORM_TITLE' => 'Lista de fornecedores externos OAuth',
+    'LBL_MODULE_NAME' => 'Fornecedores externos OAuth',
+    'LBL_MODULE_TITLE' => 'Fornecedores externos OAuth',
+    'LBL_HOMEPAGE_TITLE' => 'Meus fornecedores externos OAuth',
+    'LNK_NEW_RECORD' => 'Criar fornecedor externo OAuth',
+
+    'LNK_LIST' => 'Fornecedores externos OAuth',
+    'LBL_SEARCH_FORM_TITLE' => 'Pesquisar fornecedores externos OAuth',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Ver histórico',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Atividades',
+    'LBL_NEW_FORM_TITLE' => 'Novo fornecedor externo OAuth',
+
+    'LBL_LIST_DELETE' => 'Eliminar',
+    'LBL_TYPE' => 'Tipo',
+    'LBL_CONNECTOR' => 'Conector',
+    'LBL_REDIRECT_URI' => 'URI de redirecionamento',
+    'LBL_REDIRECT_URI_TYPE' => 'Redirect URI Type',
+
+    'LBL_CLIENT_ID' => 'ID de cliente',
+    'LBL_CLIENT_SECRET' => 'Segredo do cliente',
+    'LBL_SCOPE' => 'Âmbito',
+    'LBL_URL_AUTHORIZE' => 'URL de autorização',
+    'LBL_AUTHORIZE_URL_OPTIONS' => 'Opções de URL de autorização',
+    'LBL_URL_ACCESS_TOKEN' => 'URL de chave de acesso',
+    'LBL_EXTRA_PROVIDER_PARAMS' => 'Parâmetros adicionais de fornecedor',
+    'LBL_GET_TOKEN_REQUEST_GRANT' => 'Obter tipo de concessão de solicitação de chave',
+    'LBL_GET_TOKEN_REQUEST_OPTIONS' => 'Obter opções de solicitação de chave',
+    'LBL_REFRESH_TOKEN_REQUEST_GRANT' => 'Tipo de concessão de solicitação da chave de renovação',
+    'LBL_REFRESH_TOKEN_REQUEST_OPTIONS' => 'Opções de solicitação da chave de renovação',
+
+    'LBL_ACCESS_TOKEN_MAPPING' => 'Mapeamento de chave de acesso',
+    'LBL_EXPIRES_IN_MAPPING' => 'Expira no mapeamento',
+    'LBL_REFRESH_TOKEN_MAPPING' => 'Mapeamento da chave de renovação',
+    'LBL_TOKEN_TYPE_MAPPING' => 'Mapeamento de tipo de chave',
+
+    'LBL_EXTRA' => 'Configurações adicionais',
+    'LBL_MAPPING' => 'Configurações do mapeamento',
+    'LBL_OTHER' => 'Outros',
+
+
+    'LNK_LIST_CREATE_NEW_PERSONAL' => 'Novo fornecedor OAuth pessoal',
+    'LNK_LIST_CREATE_NEW_GROUP' => 'Novo fornecedor OAuth de grupo',
+    'LNK_LIST_INBOUND_EMAILS' => 'Contas de entrada de correio',
+    'LNK_LIST_OUTBOUND_EMAILS' => 'Contas de saída de correio',
+    'LNK_LIST_EXTERNAL_OAUTH_CONNECTION' => 'Ligações OAuth externas',
+
+    'LBL_OWNER' => 'Proprietário',
+];
