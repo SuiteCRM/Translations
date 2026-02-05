1337d66d9a96160f62f896ed10ec47bed8ee5419
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-02-05 23:21:19 +0000

    New translations en_us.lang.php (Azerbaijani)

---
+++ translations/az_AZ/include/SugarObjects/templates/sale/language/az_AZ.lang.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_MODULE_NAME' => 'Satış',
+    'LBL_MODULE_TITLE' => 'Satış: Giriş',
+    'LBL_SEARCH_FORM_TITLE' => 'Satış axtarışlar',
+    'LBL_LIST_FORM_TITLE' => 'Satış listi',
+    'LBL_NAME' => 'Satış adı',
+    'LBL_LIST_SALE_NAME' => 'Ad',
+    'LBL_LIST_ACCOUNT_NAME' => 'İstifadəçinin adı',
+    'LBL_LIST_AMOUNT' => 'Miqdar',
+    'LBL_LIST_DATE_CLOSED' => 'Bağla',
+    'LBL_LIST_SALE_STAGE' => 'Satış mərhələsi',
+    'LBL_ACCOUNT_ID' => 'Hesab İD-si',
+    //DON'T CONVERT THESE THEY ARE MAPPINGS
+    'db_name' => 'LBL_NAME',
+    //END DON'T CONVERT
+    'LBL_ACCOUNT_NAME' => 'Hesab Adı:',
+    'LBL_AMOUNT' => 'Miqdar:',
+    'LBL_AMOUNT_USDOLLAR' => 'Miqdar USD:',
+    'LBL_CURRENCY' => 'Valyuta:',
+    'LBL_DATE_CLOSED' => 'Gözlənilən Bağlanma Tarixi:',
+    'LBL_TYPE' => 'Növ:',
+    'LBL_CAMPAIGN' => 'Kampaniya:',
+    'LBL_LEADS_SUBPANEL_TITLE' => 'Rəhbərlər',
+    'LBL_PROJECTS_SUBPANEL_TITLE' => 'Layihələr',
+    'LBL_NEXT_STEP' => 'Növbəti addım:',
+    'LBL_LEAD_SOURCE' => 'Aparıcı Mənbə:',
+    'LBL_SALES_STAGE' => 'Satış mərhələsi:',
+    'LBL_PROBABILITY' => 'Ehtimal (%):',
+    'LBL_DESCRIPTION' => 'Açıqlama:',
+    'LBL_DUPLICATE' => 'Dublikat satış mümkündür',
+    'MSG_DUPLICATE' => 'Yaratmaq üzrə olduğunuz Satış qeydi artıq mövcud olan satış qeydinin dublikatı ola bilər. Oxşar adları olan satış qeydləri aşağıda verilmişdir.<br>Bu yeni Satışı yaratmağa davam etmək üçün Saxla üzərinə klikləyin və ya satış yaratmadan modula qayıtmaq üçün Ləğv et üzərinə klikləyin.',
+    'LBL_NEW_FORM_TITLE' => 'Satış yaratmaq',
+    'ERR_DELETE_RECORD' => 'Satışı silmək üçün rekord nömrə göstərilməlidir.',
+    'LBL_DEFAULT_SUBPANEL_TITLE' => 'Satış',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Fəaliyyətlər',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Tarixçə',
+
+    'LBL_CONTACTS_SUBPANEL_TITLE' => 'Əlaqələr',
+    'LBL_ASSIGNED_TO_NAME' => 'İstifadəçi:',
+    'LBL_LIST_ASSIGNED_TO_NAME' => 'Təyin edilmiş İstifadəçi',
+    'LBL_ASSIGNED_TO_ID' => 'ID-yə təyin edildi',
+    'LBL_MODIFIED_NAME' => 'İstifadəçi adı ilə dəyişdirilib',
+    'LBL_SALE_INFORMATION' => 'Satış məlumatı',
+    'LBL_CURRENCY_NAME' => 'Valyuta Adı',
+    'LBL_CURRENCY_SYMBOL' => 'Valyuta simvolu',
+    'LBL_EDIT_BUTTON' => 'Redaktə et',
+    'LBL_REMOVE' => 'Sil',
+
+);
