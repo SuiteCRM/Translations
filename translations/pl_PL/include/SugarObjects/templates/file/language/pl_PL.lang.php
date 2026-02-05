2ec2dd40884eafb7c964be6e3e0adc356365dd4d
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-02-05 23:13:44 +0000

    New translations en_us.lang.php (Polish)

---
+++ translations/pl_PL/include/SugarObjects/templates/file/language/pl_PL.lang.php
@@ -0,0 +1,117 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    //module
+    'LBL_MODULE_NAME' => 'Dokumenty',
+    'LBL_MODULE_TITLE' => 'Dokumenty: Strona główna',
+    'LNK_NEW_DOCUMENT' => 'Utwórz dokument',
+    'LNK_DOCUMENT_LIST' => 'Dokumenty',
+    'LBL_SEARCH_FORM_TITLE' => 'Szukaj dokumentu',
+    //vardef labels
+    'LBL_NAME' => 'Nazwa dokumentu',
+    'LBL_DESCRIPTION' => 'Opis',
+    'LBL_ASSIGNED_TO' => 'Przypisane do:',
+    'LBL_CATEGORY' => 'Kategoria',
+    'LBL_SUBCATEGORY' => 'Podkategoria',
+    'LBL_STATUS' => 'Status',
+    'LBL_IS_TEMPLATE' => 'Jest szablonem',
+    'LBL_TEMPLATE_TYPE' => 'Typ dokumentu',
+    'LBL_REVISION_NAME' => 'Numer wersji dokumentu',
+    'LBL_MIME' => 'Typ [Mime]',
+    'LBL_REVISION' => 'Wersja',
+    'LBL_DOCUMENT' => 'Powiązany dokument',
+    'LBL_LATEST_REVISION' => 'Ostatnia wersja',
+    'LBL_CHANGE_LOG' => 'Historia zmian',
+    'LBL_ACTIVE_DATE' => 'Data Rozpoczęcia',
+    'LBL_EXPIRATION_DATE' => 'Data wygaśnięcia',
+    'LBL_FILE_EXTENSION' => 'Rozszerzenie pliku',
+
+    'LBL_CAT_OR_SUBCAT_UNSPEC' => 'Niesprecyzowany',
+    //quick search
+    'LBL_NEW_FORM_TITLE' => 'Nowy dokument',
+    //document edit and detail view
+    'LBL_DOC_NAME' => 'Nazwa dokumentu:',
+    'LBL_FILENAME' => 'Nazwa pliku:',
+    'LBL_FILE_UPLOAD' => 'Plik:',
+    'LBL_DOC_VERSION' => 'Wersja:',
+    'LBL_CATEGORY_VALUE' => 'Kategoria:',
+    'LBL_SUBCATEGORY_VALUE' => 'Podkategoria:',
+    'LBL_DOC_STATUS' => 'Status:',
+    'LBL_DET_TEMPLATE_TYPE' => 'Typ dokumentu:',
+    'LBL_DOC_DESCRIPTION' => 'Opis:',
+    'LBL_DOC_ACTIVE_DATE' => 'Data publikacji:',
+    'LBL_DOC_EXP_DATE' => 'Data wygaśnięcia:',
+
+    //document list view.
+    'LBL_LIST_FORM_TITLE' => 'Lista dokumentów',
+    'LBL_LIST_DOCUMENT' => 'Dokument',
+    'LBL_LIST_CATEGORY' => 'Kategoria',
+    'LBL_LIST_SUBCATEGORY' => 'Podkategoria',
+    'LBL_LIST_REVISION' => 'Wersja',
+    'LBL_LIST_LAST_REV_CREATOR' => 'Opublikowany przez',
+    'LBL_LIST_LAST_REV_DATE' => 'Data wersji',
+    'LBL_LIST_VIEW_DOCUMENT' => 'Przeglądaj',
+    'LBL_LIST_ACTIVE_DATE' => 'Data Rozpoczęcia',
+    'LBL_LIST_EXP_DATE' => 'Data wygaśnięcia',
+    'LBL_LIST_STATUS' => 'Status',
+
+    //document search form.
+    'LBL_SF_CATEGORY' => 'Kategoria:',
+    'LBL_SF_SUBCATEGORY' => 'Podkategoria:',
+
+    'DEF_CREATE_LOG' => 'Dokument utworzony przez',
+
+    //error messages
+    'ERR_DOC_NAME' => 'Nazwa dokumentu',
+    'ERR_DOC_ACTIVE_DATE' => 'Data Rozpoczęcia',
+    'ERR_FILENAME' => 'Nazwa pliku',
+
+    //sub-panel vardefs.
+    'LBL_LIST_DOCUMENT_NAME' => 'Nazwa dokumentu',
+
+    'LBL_EDIT_BUTTON' => 'Edytuj',
+    'LBL_REMOVE' => 'Usuń',
+
+);
