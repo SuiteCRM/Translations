28e0a196dcef5359974cc26630012532f86c4c9f
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-02-06 16:03:58 +0000

    New translations en_us.lang.php (Cebuano)

---
+++ translations/ceb_PH/modules/DocumentRevisions/language/ceb_PH.lang.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SuiteCRM Ltd.
+ * Copyright (C) 2011 - 2025 SuiteCRM Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    //module
+    'LBL_MODULE_NAME' => 'Rebisyon sa Dokumento',
+
+    'LNK_NEW_DOCUMENT' => 'Pagbuhat og Dokumento',
+    'LNK_DOCUMENT_LIST' => 'Tan-awon ang mga Dokumento',
+
+    //vardef labels
+    'LBL_REVISION_NAME' => 'Numero sa Rebisyon',
+    'LBL_FILENAME' => 'File:',
+    'LBL_MIME' => 'Tipo sa Mime',
+    'LBL_REVISION' => 'Pag-usab',
+    'LBL_DOCUMENT' => 'Iglabot na Dokumento',
+    'LBL_LATEST_REVISION' => 'Kinaulahian na Rebisyon',
+    'LBL_CHANGE_LOG' => 'Usba ang Log',
+    'LBL_ACTIVE_DATE' => 'Petsa sa pagpublikar',
+    'LBL_EXPIRATION_DATE' => 'Petsa sa pagikspayr',
+    'LBL_FILE_EXTENSION' => 'Ekstensiyon sa File',
+
+    'LBL_DOC_NAME' => 'Pangalan sa Dokumento:',
+    'LBL_DOC_VERSION' => 'Rebisyon:',
+
+    //document revisions.
+    'LBL_REV_LIST_REVISION' => 'Pag-usab',
+    'LBL_REV_LIST_ENTERED' => 'Petsa nga gihimo',
+    'LBL_REV_LIST_CREATED' => 'Gibuhat ni',
+    'LBL_REV_LIST_LOG' => 'Usba ang Log',
+    'LBL_REV_LIST_FILENAME' => 'File',
+
+    'LBL_CURRENT_DOC_VERSION' => 'Kinaulahian na Rebisyon:',
+    'LBL_SEARCH_FORM_TITLE' => 'Pagpangita sa Dokumento',
+    'LBL_REVISIONS' => 'Mga Rebisyon',
+
+    //error messages
+    'ERR_FILENAME' => 'Pangalan sa File',
+    'ERR_DOC_VERSION' => 'Bersiyon sa Dokumento',
+    'ERR_DELETE_CONFIRM' => 'Ganahan ba ka papason ang rebisyon sa kini nga dokumento?',
+    'ERR_DELETE_LATEST_VERSION' => 'Wala ka gitugotan nga papason ang kinaulahia nga rebisyon sa dokumento.',
+    'LNK_NEW_MAIL_MERGE' => 'Ang mail e merge',
+    'LBL_DOC_ID' => 'ID sa Kakuhaan sa Dokumento',
+    'LBL_DOC_TYPE' => 'Kakuhaan',
+    'LBL_DOC_URL' => 'URL sa Kakuhaan sa Dokumento',
+
+    'LBL_CREATED_BY_NAME' => 'Gihimo sa Pangalan',
+);
