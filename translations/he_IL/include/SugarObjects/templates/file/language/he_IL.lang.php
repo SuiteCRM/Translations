243a0e143d768c56022ce290f88dccc2b791d802
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-02-06 13:35:21 +0000

    New translations en_us.lang.php (Hebrew)

---
+++ translations/he_IL/include/SugarObjects/templates/file/language/he_IL.lang.php
@@ -0,0 +1,117 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SuiteCRM Ltd.
+ * Copyright (C) 2011 - 2025 SuiteCRM Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    //module
+    'LBL_MODULE_NAME' => 'מסמכים',
+    'LBL_MODULE_TITLE' => 'מסמכים: דף ראשי',
+    'LNK_NEW_DOCUMENT' => 'צור מסמך',
+    'LNK_DOCUMENT_LIST' => 'רשימת מסמכים',
+    'LBL_SEARCH_FORM_TITLE' => 'חיפוש מסמך',
+    //vardef labels
+    'LBL_NAME' => 'שם מסמך',
+    'LBL_DESCRIPTION' => 'תאור',
+    'LBL_ASSIGNED_TO' => 'מנה שם משתמש:',
+    'LBL_CATEGORY' => 'קטגוריה',
+    'LBL_SUBCATEGORY' => 'קטרורית משנה',
+    'LBL_STATUS' => 'סטטוס',
+    'LBL_IS_TEMPLATE' => 'מדובר בתבנית',
+    'LBL_TEMPLATE_TYPE' => 'סוג מסמך',
+    'LBL_REVISION_NAME' => 'בדיקת מספר מחודשת',
+    'LBL_MIME' => 'סוג חיקוי',
+    'LBL_REVISION' => 'בדיקה מחדש',
+    'LBL_DOCUMENT' => 'מסמכים קשורים',
+    'LBL_LATEST_REVISION' => 'ריויזיה אחרונה',
+    'LBL_CHANGE_LOG' => 'שנה יומן',
+    'LBL_ACTIVE_DATE' => 'תאריך פרסום',
+    'LBL_EXPIRATION_DATE' => 'תאריך תפוגה:',
+    'LBL_FILE_EXTENSION' => 'סיומת הקובץ',
+
+    'LBL_CAT_OR_SUBCAT_UNSPEC' => 'לא מוגדר',
+    //quick search
+    'LBL_NEW_FORM_TITLE' => 'המסמך שלי',
+    //document edit and detail view
+    'LBL_DOC_NAME' => 'שם מסמך:',
+    'LBL_FILENAME' => 'שם קובץ:',
+    'LBL_FILE_UPLOAD' => 'שם סרט',
+    'LBL_DOC_VERSION' => 'ריויזיה',
+    'LBL_CATEGORY_VALUE' => 'קטגוריה:',
+    'LBL_SUBCATEGORY_VALUE' => 'קטגורית משנה:',
+    'LBL_DOC_STATUS' => 'סטטוס:',
+    'LBL_DET_TEMPLATE_TYPE' => 'סוג מסמך:',
+    'LBL_DOC_DESCRIPTION' => 'תיאור:',
+    'LBL_DOC_ACTIVE_DATE' => 'פורסם בתאריך:',
+    'LBL_DOC_EXP_DATE' => 'תאריך תפוגה:',
+
+    //document list view.
+    'LBL_LIST_FORM_TITLE' => 'רשימת מסמכים',
+    'LBL_LIST_DOCUMENT' => 'מסמך',
+    'LBL_LIST_CATEGORY' => 'קטגוריה',
+    'LBL_LIST_SUBCATEGORY' => 'קטרורית משנה',
+    'LBL_LIST_REVISION' => 'בדיקה מחדש',
+    'LBL_LIST_LAST_REV_CREATOR' => 'פורסם על ידי',
+    'LBL_LIST_LAST_REV_DATE' => 'תאריך בדיקה מחדש',
+    'LBL_LIST_VIEW_DOCUMENT' => 'צפיה',
+    'LBL_LIST_ACTIVE_DATE' => 'תאריך פרסום',
+    'LBL_LIST_EXP_DATE' => 'תאריך תפוגה:',
+    'LBL_LIST_STATUS' => 'סטטוס',
+
+    //document search form.
+    'LBL_SF_CATEGORY' => 'קטגוריה:',
+    'LBL_SF_SUBCATEGORY' => 'קטגורית משנה:',
+
+    'DEF_CREATE_LOG' => 'מסמך נוצר',
+
+    //error messages
+    'ERR_DOC_NAME' => 'שם מסמך',
+    'ERR_DOC_ACTIVE_DATE' => 'תאריך פרסום',
+    'ERR_FILENAME' => 'שם קובץ',
+
+    //sub-panel vardefs.
+    'LBL_LIST_DOCUMENT_NAME' => 'שם מסמך',
+
+    'LBL_EDIT_BUTTON' => 'עריכה',
+    'LBL_REMOVE' => 'הסר',
+
+);
