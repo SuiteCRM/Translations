e7a5c993f1116eb3ae52a711cf3e412af5fbc296
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-02-06 10:36:50 +0000

    New translations en_us.lang.php (Kannada)

---
+++ translations/kn_IN/modules/AOS_Products/language/kn_IN.lang.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID' => 'ನಿಯೋಜಿಸಲಾದ ಬಳಕೆದಾರ ಗುರುತು',
+    'LBL_ASSIGNED_TO_NAME' => 'ಇವರಿಗೆ ನಿಯೋಜಿಸಲಾಗಿದೆ',
+    'LBL_ID' => 'ಗುರುತು',
+    'LBL_DATE_ENTERED' => 'ರಚಿಸಲಾದ ದಿನಾಂಕ',
+    'LBL_DATE_MODIFIED' => 'ತಿದ್ದಲಾದ ದಿನಾಂಕ',
+    'LBL_MODIFIED' => 'ಇವರಿಂದ ಮಾರ್ಪಡಿಸಲಾಗಿದೆ',
+    'LBL_MODIFIED_NAME' => 'ಮಾರ್ಪಡಿಸಿದವರ ಹೆಸರು',
+    'LBL_CREATED' => 'ರಚಿಸಿದವರು',
+    'LBL_DESCRIPTION' => 'ವಿವರಣೆ',
+    'LBL_DELETED' => 'ಅಳಿಸಲಾಗಿದೆ',
+    'LBL_NAME' => 'Product Name',
+    'LBL_CREATED_USER' => 'ರಚಿಸಿದ ಬಳಕೆದಾರ',
+    'LBL_MODIFIED_USER' => 'ಮಾರ್ಪಡಿಸಿದ ಬಳಕೆದಾರ',
+    'LBL_LIST_FORM_TITLE' => 'Products List',
+    'LBL_MODULE_NAME' => 'Products',
+    'LBL_MODULE_TITLE' => 'Products: Home',
+    'LBL_HOMEPAGE_TITLE' => 'My Products',
+    'LNK_NEW_RECORD' => 'Create Product',
+    'LNK_LIST' => 'View Products',
+    'LBL_SEARCH_FORM_TITLE' => 'Search Products',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'View History',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'ಚಟುವಟಿಕೆ',
+    'LBL_NEW_FORM_TITLE' => 'New Product',
+    'LBL_MAINCODE' => 'Product Code',
+    'VALUE' => 'Product Name',
+    'LBL_PART_NUMBER' => 'Part Number',
+    'LBL_CATEGORY' => 'ವರ್ಗ',
+    'LBL_TYPE' => 'Product Type',
+    'LBL_COST' => 'Cost',
+    'LBL_PRICE' => 'Price',
+    'LBL_URL' => 'URL',
+    'LBL_CONTACT' => 'ಸಂಪರ್ಕ',
+    'LBL_PRODUCT_IMAGE' => 'Product Image',
+    'LBL_IMAGE_UPLOAD_FAIL' => 'ERROR: uploaded file exceeded the max filesize: max filesize: ',
+    'LBL_AOS_PRODUCT_CATEGORYS_NAME' => 'Product Category',
+    'LBL_AOS_PRODUCT_CATEGORY' => 'Product Category ID',
+    'LBL_AOS_PRODUCT_CATEGORIES' => 'Product Categories',
+    'LBL_COST_USDOLLAR' => 'Cost (Default Currency)',
+    'LBL_PRICE_USDOLLAR' => 'Price (Default Currency)',
+    'LBL_FILE_URL' => 'File URL',
+    'LBL_CUSTOMERS_PURCHASED_PRODUCTS_SUBPANEL_TITLE' => 'Purchases',
+    'LBL_PRODUCTS_PURCHASES' => 'Purchases',
+    'LBL_AOS_QUOTE_NAME' => 'ಬೆಲೆ ಸೂಚಕ',
+    'LBL_ACCOUNT_NAME' => 'ಖಾತೆ',
+);
