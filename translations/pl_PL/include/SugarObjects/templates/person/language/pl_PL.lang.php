fb60c3e5df6b42bcc9d7d41a2148ca669b06ff22
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-02-06 14:05:38 +0000

    New translations en_us.lang.php (Polish)

---
+++ translations/pl_PL/include/SugarObjects/templates/person/language/pl_PL.lang.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SuiteCRM Ltd.
+ * Copyright (C) 2011 - 2025 SuiteCRM Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_SALUTATION' => 'Forma grzecznościowa',
+    'LBL_NAME' => 'Nazwa',
+    'LBL_FIRST_NAME' => 'Imię',
+    'LBL_LAST_NAME' => 'Nazwisko',
+    'LBL_TITLE' => 'Tytuł',
+    'LBL_DEPARTMENT' => 'Dział',
+    'LBL_DO_NOT_CALL' => 'Nie dzwonić',
+    'LBL_HOME_PHONE' => 'Telefon Domowy',
+    'LBL_MOBILE_PHONE' => 'Tel. kom.',
+    'LBL_OFFICE_PHONE' => 'Telefon',
+    'LBL_OTHER_PHONE' => 'Inny telefon',
+    'LBL_FAX_PHONE' => 'Faks',
+    'LBL_EMAIL_ADDRESS' => 'Adres(y) e-mail',
+    'LBL_PRIMARY_ADDRESS' => 'Główny Adres',
+    'LBL_PRIMARY_ADDRESS_STREET' => 'Główny Adres',
+    'LBL_PRIMARY_ADDRESS_STREET_2' => 'Adres główny:',
+    'LBL_PRIMARY_ADDRESS_STREET_3' => 'Adres główny:',
+    'LBL_PRIMARY_ADDRESS_CITY' => 'Miasto',
+    'LBL_PRIMARY_ADDRESS_STATE' => 'Wojewodztwo',
+    'LBL_PRIMARY_ADDRESS_POSTALCODE' => 'Kod pocztowy',
+    'LBL_PRIMARY_ADDRESS_COUNTRY' => 'Kraj główny:',
+    'LBL_ALT_ADDRESS' => 'Inny adres',
+    'LBL_ALT_ADDRESS_STREET' => 'Inny adres',
+    'LBL_ALT_ADDRESS_STREET_2' => 'Adres dodatkowy:',
+    'LBL_ALT_ADDRESS_STREET_3' => 'Adres dodatkowy:',
+    'LBL_ALT_ADDRESS_CITY' => 'Miasto',
+    'LBL_ALT_ADDRESS_STATE' => 'Woj.',
+    'LBL_ALT_ADDRESS_POSTALCODE' => 'Kod pocztowy',
+    'LBL_ALT_ADDRESS_COUNTRY' => 'Kraj',
+    'LBL_PRIMARY_STREET' => 'Adres',
+    'LBL_ALT_STREET' => 'Inny adres',
+    'LBL_STREET' => 'Inny adres',
+    'LBL_CITY' => 'Miasto',
+    'LBL_STATE' => 'Województwo',
+    'LBL_POSTAL_CODE' => 'Kod Pocztowy',
+    'LBL_COUNTRY' => 'Kraj',
+    'LBL_CONTACT_INFORMATION' => 'Informacje o kontakcie',
+    'LBL_ADDRESS_INFORMATION' => 'Adresy',
+    'LBL_ASSIGNED_TO_NAME' => 'Przydzielony do',
+    'LBL_OTHER_EMAIL_ADDRESS' => 'Dodatkowy adres e-mail:',
+    'LBL_ASSISTANT' => 'Asystent',
+    'LBL_ASSISTANT_PHONE' => 'Telefon asystenta',
+    'LBL_WORK_PHONE' => 'Telefon',
+    'LNK_IMPORT_VCARD' => 'Utwórz z vCard',
+    'LBL_ANY_EMAIL' => 'Dowolny e-mail',
+    'LBL_EMAIL_NON_PRIMARY' => 'E-maile dodatkowe',
+    'LBL_PHOTO' => 'Zdjęcie',
+    'LBL_EDIT_BUTTON' => 'Edytuj',
+    'LBL_REMOVE' => 'Usuń',
+
+    //Lawful Basis labels
+    'LBL_LAWFUL_BASIS' => 'Podstawa prawna',
+    'LBL_DATE_REVIEWED' => 'Data przeglądu podstawy prawnej',
+    'LBL_LAWFUL_BASIS_SOURCE' => 'Źródło podstawy prawnej',
+    'LBL_CONSENT' => 'Zgoda',
+    //End Lawful Basis labels
+);
