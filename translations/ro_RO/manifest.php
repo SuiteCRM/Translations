5ee4fcaf83e8c2d611361fb7976dee967210bdd4
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-02-06 13:01:02 +0000

    New translations manifest.php (Romanian)

---
+++ translations/ro_RO/manifest.php
@@ -0,0 +1,25 @@
+<?php
+/******************************************
+ * SuiteCRM Translations
+ * @URL: https://crowdin.com/project/suitecrmtrans
+ * @author SuiteCRM Community via Crowdin
+ ******************************************/
+
+$manifest = array(
+    'name' => 'Romanian (Romania)',
+    'description' => 'Translation: crowdin.com/project/suitecrmtranslations',
+    'type' => 'langpack',
+    'is_uninstallable' => 'Da',
+    'author' => 'Comunitatea SuiteCRM',
+    'version' => '7.15-1',
+    'published_date' => '2026-01-16',
+);
+$installdefs = array(
+    'id' => 'ro_RO',
+    'image_dir' => '<basepath>/images',
+    'copy' => array(
+        array('from' => '<basepath>/include', 'to' => 'include'),
+        array('from' => '<basepath>/modules', 'to' => 'modules'),
+        array('from' => '<basepath>/install', 'to' => 'install'),
+    )
+);
