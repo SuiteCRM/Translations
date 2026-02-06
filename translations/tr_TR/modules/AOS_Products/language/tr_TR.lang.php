6fe869451f6063c141b7c6a8d4c04e2da4558bf4
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-02-06 10:36:19 +0000

    New translations en_us.lang.php (Turkish)

---
+++ translations/tr_TR/modules/AOS_Products/language/tr_TR.lang.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID' => 'Atanmış kullanıcı kodu',
+    'LBL_ASSIGNED_TO_NAME' => 'Atanmış kişi',
+    'LBL_ID' => 'Kod',
+    'LBL_DATE_ENTERED' => 'Eklenme tarihi',
+    'LBL_DATE_MODIFIED' => 'Değiştirilme tarihi',
+    'LBL_MODIFIED' => 'Değiştiren',
+    'LBL_MODIFIED_NAME' => 'Değiştiren adı',
+    'LBL_CREATED' => 'Ekleyen',
+    'LBL_DESCRIPTION' => 'Açıklama',
+    'LBL_DELETED' => 'Silindi',
+    'LBL_NAME' => 'Ürün adı',
+    'LBL_CREATED_USER' => 'Ekleyen kullanıcı',
+    'LBL_MODIFIED_USER' => 'Değiştiren kullanıcı',
+    'LBL_LIST_FORM_TITLE' => 'Ürün listesi',
+    'LBL_MODULE_NAME' => 'Ürünler',
+    'LBL_MODULE_TITLE' => 'Ürünler: Giriş',
+    'LBL_HOMEPAGE_TITLE' => 'Ürünlerim',
+    'LNK_NEW_RECORD' => 'Ürün ekle',
+    'LNK_LIST' => 'Ürünleri görüntüle',
+    'LBL_SEARCH_FORM_TITLE' => 'Ürün arama',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Geçmişi görüntüle',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'İşlemler',
+    'LBL_NEW_FORM_TITLE' => 'Yeni ürün',
+    'LBL_MAINCODE' => 'Ürün kodu',
+    'VALUE' => 'Ürün adı',
+    'LBL_PART_NUMBER' => 'Parça numarası',
+    'LBL_CATEGORY' => 'Kategori',
+    'LBL_TYPE' => 'Ürün türü',
+    'LBL_COST' => 'Maliyet',
+    'LBL_PRICE' => 'Fiyat',
+    'LBL_URL' => 'Adres',
+    'LBL_CONTACT' => 'İlgili kişi',
+    'LBL_PRODUCT_IMAGE' => 'Ürün görseli',
+    'LBL_IMAGE_UPLOAD_FAIL' => 'HATA: Yüklenen dosya izin verilen en büyük dosya boyutunu aşıyor: ',
+    'LBL_AOS_PRODUCT_CATEGORYS_NAME' => 'Ürün kategorisi',
+    'LBL_AOS_PRODUCT_CATEGORY' => 'Ürün kategori kodu',
+    'LBL_AOS_PRODUCT_CATEGORIES' => 'Ürün kategorileri',
+    'LBL_COST_USDOLLAR' => 'Maliyet (varsayılan para birimi)',
+    'LBL_PRICE_USDOLLAR' => 'Fiyat (varsayılan para birimi)',
+    'LBL_FILE_URL' => 'Dosya adresi',
+    'LBL_CUSTOMERS_PURCHASED_PRODUCTS_SUBPANEL_TITLE' => 'Satın almalar',
+    'LBL_PRODUCTS_PURCHASES' => 'Satın almalar',
+    'LBL_AOS_QUOTE_NAME' => 'Teklif',
+    'LBL_ACCOUNT_NAME' => 'Hesap',
+);
