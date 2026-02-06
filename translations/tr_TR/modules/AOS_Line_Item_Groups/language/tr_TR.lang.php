6600893c0465755afaa64a93fcdb4fba949d5e93
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-02-06 10:31:51 +0000

    New translations en_us.lang.php (Turkish)

---
+++ translations/tr_TR/modules/AOS_Line_Item_Groups/language/tr_TR.lang.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ID' => 'Kod',
+    'LBL_MODULE_NAME' => 'Gruplar',
+    'LBL_DATE_ENTERED' => 'Eklenme tarihi',
+    'LBL_DATE_MODIFIED' => 'Değiştirilme tarihi',
+    'LBL_DESCRIPTION' => 'Açıklama',
+    'LBL_DELETED' => 'Silindi',
+    'LBL_NAME' => 'Grup adı',
+    'LBL_TOTAL_AMT' => 'Toplam',
+    'LBL_DISCOUNT_AMOUNT' => 'İndirim',
+    'LBL_SUBTOTAL_AMOUNT' => 'Ara toplam',
+    'LBL_TAX_AMOUNT' => 'Vergi',
+    'LBL_SUBTOTAL_TAX_AMOUNT' => 'Ara toplam + vergi',
+    'LBL_GROUP_TOTAL' => 'Grup toplamı',
+    'LBL_LIST_NUM' => 'Sayı',
+    'LBL_SUBTOTAL_AMOUNT_USDOLLAR' => 'Ara toplam (varsayılan para birimi)',
+    'LBL_DISCOUNT_AMOUNT_USDOLLAR' => 'İndirim (varsayılan para birimi)',
+    'LBL_TAX_AMOUNT_USDOLLAR' => 'Vergi (varsayılan para birimi)',
+    'LBL_TOTAL_AMT_USDOLLAR' => 'Toplam (varsayılan para birimi)',
+    'LBL_GROUP_TOTAL_USDOLLAR' => 'Grup toplamı (varsayılan para birimi)',
+    'LBL_SUBTOTAL_TAX_AMOUNT_USDOLLAR' => 'Ara toplam + vergi (varsayılan para birimi)',
+    'LBL_PARENT_ID' => 'Üst öge kodu',
+
+    'AOS_PRODUCT_QUOTES' => 'Ürün teklifleri',
+);
