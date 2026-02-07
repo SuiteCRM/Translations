2efcb191a9d08c0b7741be0f6e9488851382b715
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-02-07 10:08:06 +0000

    New translations en_us.lang.php (Turkish)

--- translations/tr_TR/modules/AOS_Product_Categories/language/tr_TR.lang.php
+++ translations/tr_TR/modules/AOS_Product_Categories/language/tr_TR.lang.php
@@ -43,9 +43,9 @@ if (!defined('sugarEntry') || !sugarEntry) {
 }
 
 $mod_strings = array(
-    'LBL_ASSIGNED_TO_ID' => 'Atanmış kullanıcı kodu',
+    'LBL_ASSIGNED_TO_ID' => 'Atanmış kullanıcı kimliği',
     'LBL_ASSIGNED_TO_NAME' => 'Atanmış kişi',
-    'LBL_ID' => 'Kod',
+    'LBL_ID' => 'Kimlik',
     'LBL_DATE_ENTERED' => 'Eklenme tarihi',
     'LBL_DATE_MODIFIED' => 'Değiştirilme tarihi',
     'LBL_MODIFIED' => 'Değiştiren',
@@ -73,7 +73,7 @@ $mod_strings = array(
     'LBL_IS_PARENT' => "Üst kategori",
     'LBL_SUB_CATEGORIES' => "Alt kategoriler",
     'LBL_PRODUCT_CATEGORYS_NAME' => "Üst kategori",
-    'LBL_PARENT_CATEGORY_ID' => "Üst kategori kodu",
+    'LBL_PARENT_CATEGORY_ID' => "Üst kategori kimliği",
 
     'LBL_AOS_PRODUCT_CATEGORIES_AOS_PRODUCTS_FROM_AOS_PRODUCTS_TITLE' => 'Ürün kategorileri: Ürün başlığından ürün',
 );
