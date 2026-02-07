0346126866c09167cb56d029eb536ee8da83de83
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-02-07 10:10:34 +0000

    New translations en_us.lang.php (Portuguese)

---
+++ translations/pt_PT/modules/EmailMarketing/language/pt_PT.lang.php
@@ -0,0 +1,151 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_REPLY_ADDR' => 'Endereço de "Responder a": ',
+    'LBL_REPLY_NAME' => 'Nome de "Responder a": ',
+
+    'LBL_MODULE_NAME' => 'Marketing por email',
+    'LBL_MODULE_TITLE' => 'Marketing por email: entrada',
+    'LBL_LIST_FORM_TITLE' => 'Campanhas de Marketing por email',
+    'LBL_NAME' => 'Nome',
+    'LBL_LIST_NAME' => 'Nome',
+    'LBL_LIST_FROM_ADDR' => 'Email do remetente',
+    'LBL_LIST_DATE_START' => 'Data de início',
+    'LBL_LIST_TEMPLATE_NAME' => 'Modelo de email',
+    'LBL_LIST_STATUS' => 'Situação',
+    'LBL_STATUS' => 'Situação',
+    'LBL_QUEUEING_STATUS' => 'Queueing Status',
+    'LBL_STATUS_TEXT' => 'Situação:',
+    'LBL_TEMPLATE_NAME' => 'Nome de modelo',
+    'LBL_DATE_ENTERED' => 'Data de entrada',
+    'LBL_DATE_MODIFIED' => 'Data de alteração',
+    'LBL_MODIFIED' => 'Alterado por: ',
+    'LBL_CREATED' => 'Criado por: ',
+    'LBL_MESSAGE_FOR' => 'Enviar mensagem para:',
+
+    'LBL_FROM_NAME' => 'Nome do remetente: ',
+    'LBL_FROM_ADDR' => 'Endereço de email do remetente: ',
+    'LBL_DATE_START' => 'Data de início',
+    'LBL_SEND_DATE' => 'Data de envio',
+    'LBL_SCHEDULED_START_DATE' => 'Scheduled Run Date',
+    'LBL_CHECK_DUPLICATE' => 'Duplicate Handling',
+    'LBL_TIME_START' => 'Hora de início',
+    'LBL_START_DATE_TIME' => 'Data e hora de início: ',
+    'LBL_TEMPLATE' => 'Modelo de email: ',
+    'LBL_TEMPLATE_SELECTED' => 'Template',
+
+    'LBL_MODIFIED_BY' => 'Alterado por: ',
+    'LBL_CREATED_BY' => 'Criado por: ',
+
+    'LNK_NEW_CAMPAIGN' => 'Criar campanha',
+    'LNK_CAMPAIGN_LIST' => 'Campanhas',
+    'LBL_RELATED_CAMPAIGN' => 'Related Campaign',
+    'LNK_NEW_PROSPECT_LIST' => 'Criar lista de alvos',
+    'LNK_PROSPECT_LIST_LIST' => 'Listas de alvos',
+    'LNK_NEW_PROSPECT' => 'Novo alvo',
+    'LNK_PROSPECT_LIST' => 'Alvos',
+    'LBL_DEFAULT_SUBPANEL_TITLE' => 'Marketing por email',
+    'LBL_CREATE_EMAIL_TEMPLATE' => 'Criar',
+    'LBL_EDIT_EMAIL_TEMPLATE' => 'Editar',
+    'LBL_FROM_MAILBOX' => 'Caixa de correio de',
+    'LBL_FROM_MAILBOX_NAME' => 'Usar caixa de correio:',
+    'LBL_OUTBOUND_EMAIL_ACCOUNT_NAME' => 'Conta de saída de correio:',
+    'LBL_FROM' => 'De',
+    'LBL_PROSPECT_LIST_SUBPANEL_TITLE' => 'Listas de alvos',
+    'LBL_ALL_PROSPECT_LISTS' => 'Selecionar para escolher todas as listas de alvos na campanha.',
+    'LBL_RELATED_PROSPECT_LISTS' => 'Todas as listas de alvos relacionadas com esta mensagem.',
+    'LBL_PROSPECT_LIST_NAME' => 'Nome da lista de alvos',
+    'LBL_TARGET_LISTS' => 'Target List(s)',
+
+    'LBL_LIST_PROSPECT_LIST_NAME' => 'Listas de alvos',
+    'LBL_MODULE_SEND_TEST' => 'Campanha: enviar teste',
+    'LBL_MODULE_SEND_EMAILS' => 'Campanha: Enviar email',
+    'LBL_SCHEDULE_MESSAGE_TEST' => 'Por favor, selecione as mensagens de campanha que gostaria de enviar como teste:',
+    'LBL_SCHEDULE_MESSAGE_EMAILS' => 'Por favor, selecione as mensagens de campanha para as quais gostaria de agendar o envio em determinada data e hora:',
+    'LBL_SCHEDULE_BUTTON_TITLE' => 'Enviar',
+    'LBL_SEND_BUTTON_LABEL' => 'Enviar',
+    'LBL_SCHEDULE_BUTTON_LABEL' => 'Agendar',
+    'LBL_ERROR_ON_MARKETING' => 'Campo(s) obrigatório(s) em falta',
+
+    'LBL_CAMPAIGN_ID' => 'ID Campanha',
+    'LBL_OUTBOUND_EMAIL_ACOUNT_ID' => 'ID da conta de saída de correio',
+    'LBL_EMAIL_TEMPLATE' => 'Modelo de email',
+    'LBL_PROSPECT_LISTS' => 'Listas de clientes potenciais',
+    'LBL_OVERVIEW' => 'Básica',
+    'LBL_EMAIL_CONFIG' => 'Definições de email',
+    'LBL_CONFIGS' => 'CONFIGS',
+    'LBL_EMAIL' => 'Email',
+    'LBL_BODY' => 'Corpo',
+    'LBL_MARKETING_TYPE' => 'Marketing Type',
+    'LBL_SURVEY' => 'Inquérito',
+    'LBL_SCHEDULE' => 'Agendar',
+    'LBL_DELETE_TEST_ENTRIES' => 'Eliminar entradas de teste',
+    'LBL_UNSCHEDULE' => 'Unschedule',
+    'LBL_ABORT' => 'Abort',
+    'LBL_USERS' => 'Utilizadores',
+    'LBL_SEND_TEST_EMAIL' => 'Enviar email de teste',
+    'LBL_SELECT_RECORDS_WITH_EMAILS' => 'Please Select Records with Emails',
+    'LBL_MESSAGE_QUEUE_TITLE' => 'Mensagens em espera',
+    'LBL_SCHEDULED_START_DATE_HELP' => 'Sending will be processed in batches. This may take minutes/hours.',
+    'LBL_TARGET_LISTS_HELP' => 'Marketing recipients. Recipients on the campaign\'s suppression lists will be blocked.',
+    'LBL_STATUS_DRAFT_NOT_SEND_HELP' => 'Marketing process will only start when scheduled.',
+    'LBL_TYPE_MARKETING_HELP' => 'Recipients are subject to opt-out validation.',
+    'LBL_TYPE_LEGACY_HELP' => 'This is a Legacy Email Marketing Record and exists for historical data purposes.',
+    'LBL_TYPE_TRANSACTIONAL_HELP' => 'Recipients are NOT subject to opt-out validation.',
+
+    'LBL_SCHEDULER_WIDGET' => 'Email Marketing Diagnostics',
+    'LBL_OOTB_SEND_EMAIL_TO_QUEUE' => 'Queue Campaign Emails',
+    'LBL_OOTB_BOUNCE' => 'Executar à noite o processamento de emails de campanhas devolvidos',
+    'LBL_OOTB_SEND_EMAIL_FROM_QUEUE' => 'Send Campaign Emails',
+    'LBL_SCHEDULERS' => 'Agendadores',
+    'LBL_INBOUND_EMAIL' => 'Entrada de correio',
+
+    'LBL_TRACKERS_ENABLED' => 'Trackers Enabled',
+    'LBL_TRACKER_LINKS_ENABLED' => 'Tracker Links Enabled',
+    'LBL_TRACKERS_ENABLED_FOOTNOTE' => 'When enabled, links will be replaced with tracker links that monitor clicks.',
+    'LBL_TRACKERS_DISABLED_FOOTNOTE' => 'When disabled, links will NOT be replaced with tracker links that monitor clicks.',
+    'LBL_TRACKERS_DISABLED_GLOBALLY_FOOTNOTE' => 'Trackers disabled globally. Email views and clicks will not be tracked.',
+    'LBL_HAS_TEST_DATA' => 'Has test data',
+);
