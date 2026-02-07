ea2333f0e0a94fce7e63ce4ca325e2638b025050
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-02-07 10:07:34 +0000

    New translations en_us.lang.php (Turkish)

--- translations/tr_TR/modules/AOK_Knowledge_Base_Categories/language/tr_TR.lang.php
+++ translations/tr_TR/modules/AOK_Knowledge_Base_Categories/language/tr_TR.lang.php
@@ -43,16 +43,16 @@ if (!defined('sugarEntry') || !sugarEntry) {
 }
 
 $mod_strings = array(
-    'LBL_ASSIGNED_TO_ID' => 'Atanmış kullanıcı kodu',
+    'LBL_ASSIGNED_TO_ID' => 'Atanmış kullanıcı kimliği',
     'LBL_ASSIGNED_TO_NAME' => 'Atanmış kişi',
-    'LBL_ID' => 'Kod',
+    'LBL_ID' => 'Kimlik',
     'LBL_DATE_ENTERED' => 'Eklenme tarihi',
     'LBL_DATE_MODIFIED' => 'Değiştirilme tarihi',
     'LBL_MODIFIED' => 'Değiştiren',
-    'LBL_MODIFIED_ID' => 'Değiştiren kodu',
+    'LBL_MODIFIED_ID' => 'Değiştiren kimliği',
     'LBL_MODIFIED_NAME' => 'Değiştiren adı',
     'LBL_CREATED' => 'Ekleyen',
-    'LBL_CREATED_ID' => 'Ekleyen kodu',
+    'LBL_CREATED_ID' => 'Ekleyen kimliği',
     'LBL_DESCRIPTION' => 'Açıklama',
     'LBL_DELETED' => 'Silindi',
     'LBL_NAME' => 'Ad',
