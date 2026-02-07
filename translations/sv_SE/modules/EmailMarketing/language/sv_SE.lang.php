f294243f79dd7807d306c884b47c4a35dfc01926
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-02-07 10:10:39 +0000

    New translations en_us.lang.php (Swedish)

---
+++ translations/sv_SE/modules/EmailMarketing/language/sv_SE.lang.php
@@ -0,0 +1,151 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_REPLY_ADDR' => '"Svara-till" Adress: ',
+    'LBL_REPLY_NAME' => '"Svara-till" Namn: ',
+
+    'LBL_MODULE_NAME' => 'Marknadsföring via E-post',
+    'LBL_MODULE_TITLE' => 'Marknadsföring via E-post: Hem',
+    'LBL_LIST_FORM_TITLE' => 'Marknadsföring via E-post, kampanjer',
+    'LBL_NAME' => 'Namn',
+    'LBL_LIST_NAME' => 'Namn',
+    'LBL_LIST_FROM_ADDR' => 'Från E-post',
+    'LBL_LIST_DATE_START' => 'Startdatum',
+    'LBL_LIST_TEMPLATE_NAME' => 'E-postmall',
+    'LBL_LIST_STATUS' => 'Status',
+    'LBL_STATUS' => 'Status',
+    'LBL_QUEUEING_STATUS' => 'Queueing Status',
+    'LBL_STATUS_TEXT' => 'Status:',
+    'LBL_TEMPLATE_NAME' => 'Mall Namn',
+    'LBL_DATE_ENTERED' => 'Inmatningsdatum',
+    'LBL_DATE_MODIFIED' => 'Modifierad datum',
+    'LBL_MODIFIED' => 'Modifierat av: ',
+    'LBL_CREATED' => 'Skapat av: ',
+    'LBL_MESSAGE_FOR' => 'Sänd detta meddelande till:',
+
+    'LBL_FROM_NAME' => 'Från Namn: ',
+    'LBL_FROM_ADDR' => 'Från E-postadress: ',
+    'LBL_DATE_START' => 'Startdatum',
+    'LBL_SEND_DATE' => 'Skickat Datum',
+    'LBL_SCHEDULED_START_DATE' => 'Scheduled Run Date',
+    'LBL_CHECK_DUPLICATE' => 'Duplicate Handling',
+    'LBL_TIME_START' => 'Starttid',
+    'LBL_START_DATE_TIME' => 'Startdatum & tid: ',
+    'LBL_TEMPLATE' => 'E-postmall: ',
+    'LBL_TEMPLATE_SELECTED' => 'Template',
+
+    'LBL_MODIFIED_BY' => 'Modifierat av: ',
+    'LBL_CREATED_BY' => 'Skapat av: ',
+
+    'LNK_NEW_CAMPAIGN' => 'Skapa kampanj',
+    'LNK_CAMPAIGN_LIST' => 'Kampanjer',
+    'LBL_RELATED_CAMPAIGN' => 'Related Campaign',
+    'LNK_NEW_PROSPECT_LIST' => 'Skapa mållista',
+    'LNK_PROSPECT_LIST_LIST' => 'Mål listor',
+    'LNK_NEW_PROSPECT' => 'Skapa mål',
+    'LNK_PROSPECT_LIST' => 'Objekt',
+    'LBL_DEFAULT_SUBPANEL_TITLE' => 'Marknadsföring via E-post',
+    'LBL_CREATE_EMAIL_TEMPLATE' => 'Skapa',
+    'LBL_EDIT_EMAIL_TEMPLATE' => 'Redigera',
+    'LBL_FROM_MAILBOX' => 'Från postlåda',
+    'LBL_FROM_MAILBOX_NAME' => 'Använd postlåda:',
+    'LBL_OUTBOUND_EMAIL_ACCOUNT_NAME' => 'Utgående e-postkonto:',
+    'LBL_FROM' => 'Från',
+    'LBL_PROSPECT_LIST_SUBPANEL_TITLE' => 'Mål listor',
+    'LBL_ALL_PROSPECT_LISTS' => 'Alla objektlistor i kampanjen.',
+    'LBL_RELATED_PROSPECT_LISTS' => 'Alla objektlistor relaterade till detta meddelande.',
+    'LBL_PROSPECT_LIST_NAME' => 'Namn på Mållista',
+    'LBL_TARGET_LISTS' => 'Target List(s)',
+
+    'LBL_LIST_PROSPECT_LIST_NAME' => 'Targeted Lists',
+    'LBL_MODULE_SEND_TEST' => 'Campaign: Send Test',
+    'LBL_MODULE_SEND_EMAILS' => 'Campaign: Send Emails',
+    'LBL_SCHEDULE_MESSAGE_TEST' => 'Please select the campaign messages that you would like to test:',
+    'LBL_SCHEDULE_MESSAGE_EMAILS' => 'Please select the campaign messages that you would like to schedule for distribution on the specified start date and time:',
+    'LBL_SCHEDULE_BUTTON_TITLE' => 'Skicka',
+    'LBL_SEND_BUTTON_LABEL' => 'Skicka',
+    'LBL_SCHEDULE_BUTTON_LABEL' => 'Schema',
+    'LBL_ERROR_ON_MARKETING' => 'Missing required field(s)',
+
+    'LBL_CAMPAIGN_ID' => 'Campanj ID',
+    'LBL_OUTBOUND_EMAIL_ACOUNT_ID' => 'Outbound Email Account ID',
+    'LBL_EMAIL_TEMPLATE' => 'E-postmall',
+    'LBL_PROSPECT_LISTS' => 'Prospect Lists',
+    'LBL_OVERVIEW' => 'Grundläggande',
+    'LBL_EMAIL_CONFIG' => 'E-post inställningar',
+    'LBL_CONFIGS' => 'CONFIGS',
+    'LBL_EMAIL' => 'E-POST',
+    'LBL_BODY' => 'Meddelande',
+    'LBL_MARKETING_TYPE' => 'Marketing Type',
+    'LBL_SURVEY' => 'Enkät',
+    'LBL_SCHEDULE' => 'Schema',
+    'LBL_DELETE_TEST_ENTRIES' => 'Ta bort tester',
+    'LBL_UNSCHEDULE' => 'Unschedule',
+    'LBL_ABORT' => 'Abort',
+    'LBL_USERS' => 'Användare',
+    'LBL_SEND_TEST_EMAIL' => 'Skicka ett test meddelande',
+    'LBL_SELECT_RECORDS_WITH_EMAILS' => 'Please Select Records with Emails',
+    'LBL_MESSAGE_QUEUE_TITLE' => 'Meddelandekö',
+    'LBL_SCHEDULED_START_DATE_HELP' => 'Sending will be processed in batches. This may take minutes/hours.',
+    'LBL_TARGET_LISTS_HELP' => 'Marketing recipients. Recipients on the campaign\'s suppression lists will be blocked.',
+    'LBL_STATUS_DRAFT_NOT_SEND_HELP' => 'Marketing process will only start when scheduled.',
+    'LBL_TYPE_MARKETING_HELP' => 'Recipients are subject to opt-out validation.',
+    'LBL_TYPE_LEGACY_HELP' => 'This is a Legacy Email Marketing Record and exists for historical data purposes.',
+    'LBL_TYPE_TRANSACTIONAL_HELP' => 'Recipients are NOT subject to opt-out validation.',
+
+    'LBL_SCHEDULER_WIDGET' => 'Email Marketing Diagnostics',
+    'LBL_OOTB_SEND_EMAIL_TO_QUEUE' => 'Queue Campaign Emails',
+    'LBL_OOTB_BOUNCE' => 'Kör nattlig process för studsad kampanj epost',
+    'LBL_OOTB_SEND_EMAIL_FROM_QUEUE' => 'Send Campaign Emails',
+    'LBL_SCHEDULERS' => 'Schemaläggare',
+    'LBL_INBOUND_EMAIL' => 'Inkommande E-post',
+
+    'LBL_TRACKERS_ENABLED' => 'Trackers Enabled',
+    'LBL_TRACKER_LINKS_ENABLED' => 'Tracker Links Enabled',
+    'LBL_TRACKERS_ENABLED_FOOTNOTE' => 'When enabled, links will be replaced with tracker links that monitor clicks.',
+    'LBL_TRACKERS_DISABLED_FOOTNOTE' => 'When disabled, links will NOT be replaced with tracker links that monitor clicks.',
+    'LBL_TRACKERS_DISABLED_GLOBALLY_FOOTNOTE' => 'Trackers disabled globally. Email views and clicks will not be tracked.',
+    'LBL_HAS_TEST_DATA' => 'Has test data',
+);
