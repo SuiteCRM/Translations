d0c08b5599b7fbf2caeab0e4454d2f37720105a0
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-02-07 10:07:32 +0000

    New translations en_us.lang.php (Turkish)

--- translations/tr_TR/modules/AOK_KnowledgeBase/language/tr_TR.lang.php
+++ translations/tr_TR/modules/AOK_KnowledgeBase/language/tr_TR.lang.php
@@ -43,9 +43,9 @@ if (!defined('sugarEntry') || !sugarEntry) {
 }
 
 $mod_strings = array(
-    'LBL_ASSIGNED_TO_ID' => 'Atanmış kullanıcı kodu',
+    'LBL_ASSIGNED_TO_ID' => 'Atanmış kullanıcı kimliği',
     'LBL_ASSIGNED_TO_NAME' => 'Atanmış kişi',
-    'LBL_ID' => 'Kod',
+    'LBL_ID' => 'Kimlik',
     'LBL_DATE_ENTERED' => 'Eklenme tarihi',
     'LBL_DATE_MODIFIED' => 'Değiştirilme tarihi',
     'LBL_MODIFIED' => 'Değiştiren',
@@ -72,9 +72,9 @@ $mod_strings = array(
     'LBL_NEW_FORM_TITLE' => 'Yeni bilgi bankası',
     'LBL_STATUS' => 'Durum',
     'LBL_REVISION' => 'Sürüm',
-    'LBL_AUTHOR_USER_ID' => 'Sorumlu (ilişkili kullanıcı kodu)',
+    'LBL_AUTHOR_USER_ID' => 'Sorumlu (ilişkili kullanıcı kimliği)',
     'LBL_AUTHOR' => 'Sorumlu',
-    'LBL_APPROVER_USER_ID' => 'Onaylayan (ilişkili kullanıcı kodu)',
+    'LBL_APPROVER_USER_ID' => 'Onaylayan (ilişkili kullanıcı kimliği)',
     'LBL_APPROVER' => 'Onaylayan',
 );
 
