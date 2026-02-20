e31b84bc1397b0911b0fc4402ce3292520fefa49
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-02-20 17:43:44 +0000

    New translations en_us.lang.php (Hindi)

--- translations/hi_IN/modules/ExternalOAuthProvider/language/hi_IN.lang.php
+++ translations/hi_IN/modules/ExternalOAuthProvider/language/hi_IN.lang.php
@@ -44,9 +44,7 @@ if (!defined('sugarEntry') || !sugarEntry) {
 
 $mod_strings = [
 
-    'LBL_ASSIGNED_TO_ID' => 'पहचानकर्ता कुंजी:
-$मॉड_स्ट्रिंग्स [लेबल_असाइन_टू_आईडी\']
-$मॉड_स्ट्रिंग्स [लेबल_असाइन_टू_आईडी\']',
+    'LBL_ASSIGNED_TO_ID' => 'असाइन किया गया उपयोगकर्ता आईडी',
     'LBL_ASSIGNED_TO_NAME' => 'को सौंपना',
     'LBL_ID' => 'आईडी',
     'LBL_DATE_ENTERED' => 'बनाया गया दिनांक',
