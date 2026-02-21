4f85b32c0265791a8c6a5cd34c9814a98925d2a8
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-02-21 17:14:59 +0000

    New translations en_us.lang.php (Hindi)

--- translations/hi_IN/modules/SurveyQuestions/language/hi_IN.lang.php
+++ translations/hi_IN/modules/SurveyQuestions/language/hi_IN.lang.php
@@ -39,9 +39,7 @@
  */
 
 $mod_strings = array(
-    'LBL_ASSIGNED_TO_ID'                                                               => 'पहचानकर्ता कुंजी:
-$मॉड_स्ट्रिंग्स [लेबल_असाइन_टू_आईडी\']
-$मॉड_स्ट्रिंग्स [लेबल_असाइन_टू_आईडी\']',
+    'LBL_ASSIGNED_TO_ID'                                                               => 'असाइन किया गया उपयोगकर्ता आईडी',
     'LBL_ASSIGNED_TO_NAME'                                                             => 'को सौंपना',
     'LBL_SECURITYGROUPS'                                                               => 'सुरक्षा समूह',
     'LBL_SECURITYGROUPS_SUBPANEL_TITLE'                                                => 'सुरक्षा समूह',
