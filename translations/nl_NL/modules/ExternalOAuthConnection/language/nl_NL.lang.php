cabfeb0edafad15190615099a1c6695658d9762c
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-03-25 10:56:22 +0000

    New translations en_us.lang.php (Dutch)

--- translations/nl_NL/modules/ExternalOAuthConnection/language/nl_NL.lang.php
+++ translations/nl_NL/modules/ExternalOAuthConnection/language/nl_NL.lang.php
@@ -90,8 +90,8 @@ $mod_strings = [
     'LBL_EXTERNAL_OAUTH_PROVIDER_NAME' => 'Provider',
     'LBL_EXTERNAL_OAUTH_PROVIDER_ID' => 'Provider Id',
 
-    'LNK_LIST_CREATE_NEW_PERSONAL' => 'New Personal OAuth Connection',
-    'LNK_LIST_CREATE_NEW_GROUP' => 'New Group OAuth Connection',
+    'LNK_LIST_CREATE_NEW_PERSONAL' => 'Nieuwe persoonlijke OAuth verbinding',
+    'LNK_LIST_CREATE_NEW_GROUP' => 'Nieuwe Groep OAuth Verbinding',
     'LNK_LIST_INBOUND_EMAILS' => 'Inkomende Email Accounts',
     'LNK_LIST_OUTBOUND_EMAILS' => 'Uitgaande e-mailaccounts',
     'LNK_LIST_EXTERNAL_OAUTH_PROVIDERS' => 'Externe OAuth Providers',
