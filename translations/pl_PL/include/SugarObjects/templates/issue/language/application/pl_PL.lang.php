cc40adf4eea110877325a85e4d3096d6a18b7a6f
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-03-30 13:51:04 +0100

    New translations en_us.lang.php (Polish)

--- translations/pl_PL/include/SugarObjects/templates/issue/language/application/pl_PL.lang.php
+++ translations/pl_PL/include/SugarObjects/templates/issue/language/application/pl_PL.lang.php
@@ -66,8 +66,8 @@ $app_list_strings = array(
     $object_name . '_resolution_dom' => array(
         '' => '',
         'Accepted' => 'Zaakceptowano',
-        'Duplicate' => 'Skopiuj',
-        'Closed' => 'Zamknięty',
+        'Duplicate' => 'Duplikuj',
+        'Closed' => 'Zamknięte',
         'Out of Date' => 'Nieaktualny',
         'Invalid' => 'Niepoprawny',
     ),
