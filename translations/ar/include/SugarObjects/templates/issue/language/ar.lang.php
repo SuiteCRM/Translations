67ffadbeb1a07261a5831c7a16161954ab7c5cdb
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-03-30 11:08:16 +0100

    New translations en_us.lang.php (Arabic)

--- translations/ar/include/SugarObjects/templates/issue/language/ar.lang.php
+++ translations/ar/include/SugarObjects/templates/issue/language/ar.lang.php
@@ -44,25 +44,25 @@ if (!defined('sugarEntry') || !sugarEntry) {
 
 $mod_strings = array(
 
-    'LBL_NAME' => 'الاسم',
+    'LBL_NAME' => 'Name',
     'LBL_NUMBER' => 'الرقم:',
-    'LBL_STATUS' => 'الحالة:',
+    'LBL_STATUS' => 'Status:',
     'LBL_PRIORITY' => 'الأولوية:',
-    'LBL_DESCRIPTION' => 'الوصف:',
+    'LBL_DESCRIPTION' => 'Description:',
     'LBL_RESOLUTION' => 'الحل',
     'LBL_LAST_MODIFIED' => 'آخر تعديل',
     'LBL_ASSIGNED_TO_ID' => 'أُسند إلى:',
-    'LBL_ASSIGNED_TO_NAME' => 'المستخدم:',
+    'LBL_ASSIGNED_TO_NAME' => 'User:',
     'LBL_WORK_LOG' => 'سجل العمل:',
     'LBL_CREATED_BY' => 'أنشئ بواسطة:',
-    'LBL_DATE_ENTERED' => 'تاريخ الإنشاء:',
-    'LBL_DATE_MODIFIED' => 'تاريخ التعديل:',
+    'LBL_DATE_ENTERED' => 'Date Created:',
+    'LBL_DATE_MODIFIED' => 'Date Modified:',
     'LBL_MODIFIED_BY' => 'آخر تعديل بواسطة:',
     'LBL_ASSIGNED_USER' => 'المستخدم المُكلف:',
-    'LBL_TYPE' => 'النوع:',
+    'LBL_TYPE' => 'Type:',
     'LBL_SUBJECT' => 'الموضوع:',
 
-    'LBL_EDIT_BUTTON' => 'حرر',
-    'LBL_REMOVE' => 'إزالة',
+    'LBL_EDIT_BUTTON' => 'Edit',
+    'LBL_REMOVE' => 'Remove',
 
 );
