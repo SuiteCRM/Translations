fb333a60fedfd404b62760b89cbccda7b75be003
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-03-30 13:12:54 +0100

    New translations en_us.lang.php (Bulgarian)

--- translations/bg_BG/include/SugarObjects/templates/issue/language/application/bg_BG.lang.php
+++ translations/bg_BG/include/SugarObjects/templates/issue/language/application/bg_BG.lang.php
@@ -66,8 +66,8 @@ $app_list_strings = array(
     $object_name . '_resolution_dom' => array(
         '' => '',
         'Accepted' => 'Приет',
-        'Duplicate' => 'Дублирай',
-        'Closed' => 'Затворен',
+        'Duplicate' => 'Duplicate',
+        'Closed' => 'Closed',
         'Out of Date' => 'С изтекъл срок',
         'Invalid' => 'Невалиден',
     ),
