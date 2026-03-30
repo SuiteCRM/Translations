90386908fbd75a819c583458366e91e4b3f7f3a2
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-03-30 13:20:39 +0100

    New translations en_us.lang.php (Danish)

--- translations/da_DK/include/SugarObjects/implements/security_groups/language/da_DK.lang.php
+++ translations/da_DK/include/SugarObjects/implements/security_groups/language/da_DK.lang.php
@@ -44,5 +44,5 @@ if (!defined('sugarEntry') || !sugarEntry) {
 
 $mod_strings = array(
     'LBL_SECURITYGROUPS' => "Sikkerhedsgrupper",
-    'LBL_SECURITYGROUPS_SUBPANEL_TITLE' => "Sikkerhedsgrupper"
+    'LBL_SECURITYGROUPS_SUBPANEL_TITLE' => "Security Groups"
 );
