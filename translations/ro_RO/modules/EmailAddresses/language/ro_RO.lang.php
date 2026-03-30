dc1348f864a1dcef5d34425984c66758be9697d7
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-03-30 13:01:04 +0100

    New translations en_us.lang.php (Romanian)

--- translations/ro_RO/modules/EmailAddresses/language/ro_RO.lang.php
+++ translations/ro_RO/modules/EmailAddresses/language/ro_RO.lang.php
@@ -43,13 +43,13 @@ if (!defined('sugarEntry') || !sugarEntry) {
 }
 
 $mod_strings = array(
-    'LBL_EMAIL_ADDRESS_ID' => 'ID-ul',
-    'LBL_EMAIL_ADDRESS' => 'Email',
+    'LBL_EMAIL_ADDRESS_ID' => 'ID',
+    'LBL_EMAIL_ADDRESS' => 'Adresă Email',
     'LBL_EMAIL_ADDRESS_CAPS' => 'Adresă Mail Majuscule',
     'LBL_INVALID_EMAIL' => 'Email Invalid',
-    'LBL_OPT_OUT' => 'Dezabonat',
+    'LBL_OPT_OUT' => 'Renunțat',
     'LBL_CONFIRM_OPT_IN' => 'Confirmare Înscriere',
     'LBL_DATE_CREATE' => 'Dată Creat',
-    'LBL_DATE_MODIFIED' => 'Data modificării',
+    'LBL_DATE_MODIFIED' => 'Data modificare',
     'LBL_DELETED' => 'Ștergere',
 );
