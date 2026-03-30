51bde57c3101e9b2ae001b6e437fc0511c81d24a
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-03-30 13:08:02 +0100

    New translations en_us.lang.php (Afrikaans)

--- translations/af_ZA/modules/ACLRoles/language/af_ZA.lang.php
+++ translations/af_ZA/modules/ACLRoles/language/af_ZA.lang.php
@@ -43,22 +43,22 @@ if (!defined('sugarEntry') || !sugarEntry) {
 }
 
 $mod_strings = array(
-    'LBL_MODULE_NAME' => 'Rolle',
+    'LBL_MODULE_NAME' => 'Roles',
     'LBL_MODULE_TITLE' => 'Rolle: Tuis',
     'LBL_ROLE' => 'Role',
-    'LBL_NAME' => 'Naam',
+    'LBL_NAME' => 'Name',
     'LBL_DESCRIPTION' => 'Beskrywing',
     'LIST_ROLES' => 'List Roles',
-    'LBL_USERS_SUBPANEL_TITLE' => 'Gebruikers',
+    'LBL_USERS_SUBPANEL_TITLE' => 'Users',
     'LIST_ROLES_BY_USER' => 'List Roles By User',
-    'LBL_LIST_FORM_TITLE' => 'Rolle',
+    'LBL_LIST_FORM_TITLE' => 'Roles',
     'LBL_ROLES_SUBPANEL_TITLE' => 'User Roles',
-    'LBL_SEARCH_FORM_TITLE' => 'Soek',
+    'LBL_SEARCH_FORM_TITLE' => 'Search',
     'LBL_CREATE_ROLE' => 'Skep rol',
     'LBL_EDIT_VIEW_DIRECTIONS' => 'Double click on a cell to change value.',
     'LBL_ACCESS_DEFAULT' => 'Not Set',
     'LBL_ACTION_ADMIN' => 'Access Type',
-    'LBL_ALL' => 'Alle',
+    'LBL_ALL' => 'All',
     'LBL_DUPLICATE_OF' => 'Duplicate Of ',
 
     'LBL_SECURITYGROUPS' => 'Security Groups',
