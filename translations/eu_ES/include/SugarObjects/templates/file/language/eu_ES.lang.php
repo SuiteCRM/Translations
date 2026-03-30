bdfc5737b37c05a8b1c234c44515e7432a338545
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-03-30 13:28:01 +0100

    New translations en_us.lang.php (Basque)

--- translations/eu_ES/include/SugarObjects/templates/file/language/eu_ES.lang.php
+++ translations/eu_ES/include/SugarObjects/templates/file/language/eu_ES.lang.php
@@ -51,8 +51,8 @@ $mod_strings = array(
     'LBL_SEARCH_FORM_TITLE' => 'Dokumentuen bilaketa',
     //vardef labels
     'LBL_NAME' => 'Dokumentuaren izena',
-    'LBL_DESCRIPTION' => 'Deskribapena',
-    'LBL_ASSIGNED_TO' => 'Esleitua izan da',
+    'LBL_DESCRIPTION' => 'Description',
+    'LBL_ASSIGNED_TO' => 'Assigned to:',
     'LBL_CATEGORY' => 'Maila',
     'LBL_SUBCATEGORY' => 'Azpikategoria',
     'LBL_STATUS' => 'Egoera',
@@ -80,38 +80,38 @@ $mod_strings = array(
     'LBL_SUBCATEGORY_VALUE' => 'Azpikategoria',
     'LBL_DOC_STATUS' => 'Egoera',
     'LBL_DET_TEMPLATE_TYPE' => 'Dokumentu-mota',
-    'LBL_DOC_DESCRIPTION' => 'Deskribapena',
+    'LBL_DOC_DESCRIPTION' => 'Deskribapena:',
     'LBL_DOC_ACTIVE_DATE' => 'Argitaratze-data',
     'LBL_DOC_EXP_DATE' => 'Iraungitze-data',
 
     //document list view.
     'LBL_LIST_FORM_TITLE' => 'Dokumentuen zerrenda',
     'LBL_LIST_DOCUMENT' => 'Dokumentua',
-    'LBL_LIST_CATEGORY' => 'Maila',
-    'LBL_LIST_SUBCATEGORY' => 'Azpikategoria',
-    'LBL_LIST_REVISION' => 'Berrikuspena',
+    'LBL_LIST_CATEGORY' => 'Category',
+    'LBL_LIST_SUBCATEGORY' => 'Sub Category',
+    'LBL_LIST_REVISION' => 'Revision',
     'LBL_LIST_LAST_REV_CREATOR' => 'Nork argitaratua',
     'LBL_LIST_LAST_REV_DATE' => 'Berrikuspen-data',
     'LBL_LIST_VIEW_DOCUMENT' => 'Ikusi',
-    'LBL_LIST_ACTIVE_DATE' => 'Argitaratze-data',
-    'LBL_LIST_EXP_DATE' => 'Iraungitze-data',
-    'LBL_LIST_STATUS' => 'Egoera',
+    'LBL_LIST_ACTIVE_DATE' => 'Publish Date',
+    'LBL_LIST_EXP_DATE' => 'Expiration Date',
+    'LBL_LIST_STATUS' => 'Status',
 
     //document search form.
-    'LBL_SF_CATEGORY' => 'Maila',
-    'LBL_SF_SUBCATEGORY' => 'Azpikategoria',
+    'LBL_SF_CATEGORY' => 'Category:',
+    'LBL_SF_SUBCATEGORY' => 'Sub Category:',
 
     'DEF_CREATE_LOG' => 'Sortutako dokumentua',
 
     //error messages
-    'ERR_DOC_NAME' => 'Dokumentuaren izena',
-    'ERR_DOC_ACTIVE_DATE' => 'Argitaratze-data',
+    'ERR_DOC_NAME' => 'Document Name',
+    'ERR_DOC_ACTIVE_DATE' => 'Publish Date',
     'ERR_FILENAME' => 'Artxiboaren izena',
 
     //sub-panel vardefs.
-    'LBL_LIST_DOCUMENT_NAME' => 'Dokumentuaren izena',
+    'LBL_LIST_DOCUMENT_NAME' => 'Document Name',
 
     'LBL_EDIT_BUTTON' => 'Edit ',
-    'LBL_REMOVE' => 'Kendu',
+    'LBL_REMOVE' => 'Remove',
 
 );
