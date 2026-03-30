8ee05b1a6b3799905c01a201816d5a0fe65cfcf7
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-03-30 13:41:08 +0100

    New translations en_us.lang.php (Lithuanian)

--- translations/lt_LT/include/SugarObjects/implements/security_groups/language/lt_LT.lang.php
+++ translations/lt_LT/include/SugarObjects/implements/security_groups/language/lt_LT.lang.php
@@ -44,5 +44,5 @@ if (!defined('sugarEntry') || !sugarEntry) {
 
 $mod_strings = array(
     'LBL_SECURITYGROUPS' => "Apsaugotos grupės",
-    'LBL_SECURITYGROUPS_SUBPANEL_TITLE' => "Apsaugotos grupės"
+    'LBL_SECURITYGROUPS_SUBPANEL_TITLE' => "Security Groups"
 );
