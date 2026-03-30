70cedd9f8c47b7695d1acd78edf9b25d907d923c
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-03-30 10:59:38 +0100

    New translations en_us.lang.php (French)

--- translations/fr_FR/include/SugarObjects/templates/file/language/fr_FR.lang.php
+++ translations/fr_FR/include/SugarObjects/templates/file/language/fr_FR.lang.php
@@ -52,7 +52,7 @@ $mod_strings = array(
     //vardef labels
     'LBL_NAME' => 'Nom du document',
     'LBL_DESCRIPTION' => 'Description',
-    'LBL_ASSIGNED_TO' => 'Assigné à :',
+    'LBL_ASSIGNED_TO' => 'Assigned to:',
     'LBL_CATEGORY' => 'Catégorie',
     'LBL_SUBCATEGORY' => 'Sous Catégorie',
     'LBL_STATUS' => 'Statut',
@@ -80,38 +80,38 @@ $mod_strings = array(
     'LBL_SUBCATEGORY_VALUE' => 'Sous Catégorie :',
     'LBL_DOC_STATUS' => 'Statut :',
     'LBL_DET_TEMPLATE_TYPE' => 'Type de document :',
-    'LBL_DOC_DESCRIPTION' => 'Description :',
+    'LBL_DOC_DESCRIPTION' => 'Description:',
     'LBL_DOC_ACTIVE_DATE' => 'Date de publication :',
     'LBL_DOC_EXP_DATE' => 'Date d&#39;Expiration :',
 
     //document list view.
     'LBL_LIST_FORM_TITLE' => 'Liste des documents',
     'LBL_LIST_DOCUMENT' => 'Document',
-    'LBL_LIST_CATEGORY' => 'Catégorie',
-    'LBL_LIST_SUBCATEGORY' => 'Sous Catégorie',
-    'LBL_LIST_REVISION' => 'Version',
+    'LBL_LIST_CATEGORY' => 'Category',
+    'LBL_LIST_SUBCATEGORY' => 'Sub Category',
+    'LBL_LIST_REVISION' => 'Revision',
     'LBL_LIST_LAST_REV_CREATOR' => 'Publié par',
     'LBL_LIST_LAST_REV_DATE' => 'Date version',
     'LBL_LIST_VIEW_DOCUMENT' => 'Voir',
-    'LBL_LIST_ACTIVE_DATE' => 'Date de mise à disposition',
-    'LBL_LIST_EXP_DATE' => 'Date expiration',
-    'LBL_LIST_STATUS' => 'Statut',
+    'LBL_LIST_ACTIVE_DATE' => 'Publish Date',
+    'LBL_LIST_EXP_DATE' => 'Expiration Date',
+    'LBL_LIST_STATUS' => 'Status',
 
     //document search form.
-    'LBL_SF_CATEGORY' => 'Catégorie :',
-    'LBL_SF_SUBCATEGORY' => 'Sous Catégorie :',
+    'LBL_SF_CATEGORY' => 'Category:',
+    'LBL_SF_SUBCATEGORY' => 'Sub Category:',
 
     'DEF_CREATE_LOG' => 'Document créé',
 
     //error messages
-    'ERR_DOC_NAME' => 'Nom du document',
-    'ERR_DOC_ACTIVE_DATE' => 'Date de mise à disposition',
+    'ERR_DOC_NAME' => 'Document Name',
+    'ERR_DOC_ACTIVE_DATE' => 'Publish Date',
     'ERR_FILENAME' => 'Nom Fichier',
 
     //sub-panel vardefs.
-    'LBL_LIST_DOCUMENT_NAME' => 'Nom du document',
+    'LBL_LIST_DOCUMENT_NAME' => 'Document Name',
 
     'LBL_EDIT_BUTTON' => 'Modifier ',
-    'LBL_REMOVE' => 'Supprimer',
+    'LBL_REMOVE' => 'Remove',
 
 );
