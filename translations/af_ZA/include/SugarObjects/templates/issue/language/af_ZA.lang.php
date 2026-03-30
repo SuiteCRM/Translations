3cf6764491f474610469ba89810ce7e08747c9f8
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-03-30 11:05:37 +0100

    New translations en_us.lang.php (Afrikaans)

--- translations/af_ZA/include/SugarObjects/templates/issue/language/af_ZA.lang.php
+++ translations/af_ZA/include/SugarObjects/templates/issue/language/af_ZA.lang.php
@@ -44,7 +44,7 @@ if (!defined('sugarEntry') || !sugarEntry) {
 
 $mod_strings = array(
 
-    'LBL_NAME' => 'Naam',
+    'LBL_NAME' => 'Name',
     'LBL_NUMBER' => 'Number:',
     'LBL_STATUS' => 'Status:',
     'LBL_PRIORITY' => 'Priority:',
@@ -55,14 +55,14 @@ $mod_strings = array(
     'LBL_ASSIGNED_TO_NAME' => 'User:',
     'LBL_WORK_LOG' => 'Work Log:',
     'LBL_CREATED_BY' => 'Created by:',
-    'LBL_DATE_ENTERED' => 'Datum geskep:',
-    'LBL_DATE_MODIFIED' => 'Datum Gewysig:',
+    'LBL_DATE_ENTERED' => 'Date Created:',
+    'LBL_DATE_MODIFIED' => 'Date Modified:',
     'LBL_MODIFIED_BY' => 'Last Modified by:',
     'LBL_ASSIGNED_USER' => 'Assigned User:',
     'LBL_TYPE' => 'Type:',
     'LBL_SUBJECT' => 'Subject:',
 
-    'LBL_EDIT_BUTTON' => 'Wysig',
-    'LBL_REMOVE' => 'Verwyder',
+    'LBL_EDIT_BUTTON' => 'Edit',
+    'LBL_REMOVE' => 'Remove',
 
 );
