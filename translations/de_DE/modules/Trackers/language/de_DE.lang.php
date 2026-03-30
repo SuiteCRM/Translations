9da0dce1e0590e970bfba4d6d03f39ea6d4c9c57
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-03-30 11:23:41 +0100

    New translations en_us.lang.php (German)

--- translations/de_DE/modules/Trackers/language/de_DE.lang.php
+++ translations/de_DE/modules/Trackers/language/de_DE.lang.php
@@ -44,13 +44,13 @@ if (!defined('sugarEntry') || !sugarEntry) {
 
 $mod_strings = array(
     //Column header mapping
-    'action' => 'Aktion',
-    'date_modified' => 'Datum der letzten Aktion',
-    'item_id' => 'Id',
+    'action' => 'Action',
+    'date_modified' => 'Date of Last Action',
+    'item_id' => 'ID',
     'item_summary' => 'Name',
-    'module_name' => 'Modulname',
-    'users' => 'Benutzer',
+    'module_name' => 'Module Name',
+    'users' => 'Users',
 
     //Administration related labels
-    'LBL_MODULE_NAME' => 'Tracker',
+    'LBL_MODULE_NAME' => 'Trackers',
 );
