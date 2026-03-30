302f900a266e2a030e8d8e90fad39f63da41753a
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-03-30 11:04:11 +0100

    New translations en_us.lang.php (Spanish)

--- translations/es_ES/modules/EmailAddresses/language/es_ES.lang.php
+++ translations/es_ES/modules/EmailAddresses/language/es_ES.lang.php
@@ -44,12 +44,12 @@ if (!defined('sugarEntry') || !sugarEntry) {
 
 $mod_strings = array(
     'LBL_EMAIL_ADDRESS_ID' => 'ID',
-    'LBL_EMAIL_ADDRESS' => 'Dirección de Email',
+    'LBL_EMAIL_ADDRESS' => 'Email Address',
     'LBL_EMAIL_ADDRESS_CAPS' => 'Mayúsculas en Dirección de Email',
-    'LBL_INVALID_EMAIL' => 'Email no Válido',
-    'LBL_OPT_OUT' => 'Descartados',
+    'LBL_INVALID_EMAIL' => 'Invalid Email',
+    'LBL_OPT_OUT' => 'Opted Out',
     'LBL_CONFIRM_OPT_IN' => 'Confirmar Autorización',
     'LBL_DATE_CREATE' => 'Fecha de Creación',
-    'LBL_DATE_MODIFIED' => 'Fecha de Modificación',
-    'LBL_DELETED' => 'Eliminar',
+    'LBL_DATE_MODIFIED' => 'Date Modified',
+    'LBL_DELETED' => 'Delete',
 );
