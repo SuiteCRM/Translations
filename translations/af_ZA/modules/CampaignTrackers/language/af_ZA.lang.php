d297783189e11075dba3c9497150055b3b015e0b
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-03-30 11:06:33 +0100

    New translations en_us.lang.php (Afrikaans)

--- translations/af_ZA/modules/CampaignTrackers/language/af_ZA.lang.php
+++ translations/af_ZA/modules/CampaignTrackers/language/af_ZA.lang.php
@@ -52,8 +52,8 @@ $mod_strings = array(
     'LBL_DATE_MODIFIED' => 'Date Modified',
     'LBL_MODIFIED_USER_ID' => 'Modified User Id',
     'LBL_CREATED_BY' => 'Created By',
-    'LBL_DELETED' => 'Geskrap',
-    'LBL_CAMPAIGN' => 'Veldtog',
+    'LBL_DELETED' => 'Deleted',
+    'LBL_CAMPAIGN' => 'Campaign',
     'LBL_OPTOUT' => 'Opt-out',
 
     'LBL_MODULE_NAME' => 'Campaign Trackers',
@@ -61,12 +61,12 @@ $mod_strings = array(
     'LBL_EDIT_TRACKER_NAME' => 'Tracker Name:',
     'LBL_EDIT_TRACKER_URL' => 'Tracker URL:',
 
-    'LBL_SUBPANEL_TRACKER_NAME' => 'Naam',
+    'LBL_SUBPANEL_TRACKER_NAME' => 'Name',
     'LBL_SUBPANEL_TRACKER_URL' => 'URL',
     'LBL_SUBPANEL_TRACKER_KEY' => 'Key',
     'LBL_EDIT_MESSAGE_URL' => 'URL for Campaign Message:',
     'LBL_EDIT_TRACKER_KEY' => 'Tracker Key:',
     'LBL_EDIT_OPT_OUT' => 'Opt-out Link?',
-    'LNK_CAMPAIGN_LIST' => 'Veldtogte',
+    'LNK_CAMPAIGN_LIST' => 'Campaigns',
     'LBL_EDIT_LAYOUT' => 'Edit Layout' /*for 508 compliance fix*/,
 );
