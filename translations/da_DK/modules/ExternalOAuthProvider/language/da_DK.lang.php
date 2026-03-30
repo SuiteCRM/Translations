dd0ade33ac694fb76c4b1c81b9ba7f66c56994e1
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-03-30 13:21:49 +0100

    New translations en_us.lang.php (Danish)

--- translations/da_DK/modules/ExternalOAuthProvider/language/da_DK.lang.php
+++ translations/da_DK/modules/ExternalOAuthProvider/language/da_DK.lang.php
@@ -44,22 +44,22 @@ if (!defined('sugarEntry') || !sugarEntry) {
 
 $mod_strings = [
 
-    'LBL_ASSIGNED_TO_ID' => 'Tildelt brugerID',
-    'LBL_ASSIGNED_TO_NAME' => 'Tildelt til',
+    'LBL_ASSIGNED_TO_ID' => 'Assigned User Id',
+    'LBL_ASSIGNED_TO_NAME' => 'Assigned to',
     'LBL_ID' => 'ID',
-    'LBL_DATE_ENTERED' => 'Oprettelsesdato',
-    'LBL_DATE_MODIFIED' => 'Ændret dato',
-    'LBL_MODIFIED' => 'Ændret af',
-    'LBL_MODIFIED_NAME' => 'Ændret af navn',
-    'LBL_CREATED' => 'Oprettet af',
-    'LBL_DESCRIPTION' => 'Beskrivelse',
-    'LBL_DELETED' => 'Slet',
-    'LBL_NAME' => 'Navn',
-    'LBL_CREATED_USER' => 'Oprettet af bruger',
-    'LBL_MODIFIED_USER' => 'Ændret af bruger',
-    'LBL_LIST_NAME' => 'Navn',
-    'LBL_EDIT_BUTTON' => 'Rediger',
-    'LBL_REMOVE' => 'Fjern',
+    'LBL_DATE_ENTERED' => 'Date Created',
+    'LBL_DATE_MODIFIED' => 'Date Modified',
+    'LBL_MODIFIED' => 'Modified By',
+    'LBL_MODIFIED_NAME' => 'Modified By Name',
+    'LBL_CREATED' => 'Created By',
+    'LBL_DESCRIPTION' => 'Description',
+    'LBL_DELETED' => 'Deleted',
+    'LBL_NAME' => 'Name',
+    'LBL_CREATED_USER' => 'Created by User',
+    'LBL_MODIFIED_USER' => 'Modified by User',
+    'LBL_LIST_NAME' => 'Name',
+    'LBL_EDIT_BUTTON' => 'Edit',
+    'LBL_REMOVE' => 'Remove',
     'LBL_LIST_FORM_TITLE' => 'External OAuth Provider List',
     'LBL_MODULE_NAME' => 'External OAuth Providers',
     'LBL_MODULE_TITLE' => 'External OAuth Providers',
@@ -68,11 +68,11 @@ $mod_strings = [
 
     'LNK_LIST' => 'External OAuth Providers',
     'LBL_SEARCH_FORM_TITLE' => 'Search External OAuth Providers',
-    'LBL_HISTORY_SUBPANEL_TITLE' => 'Vis historik',
-    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Aktiviteter',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'View History',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Activities',
     'LBL_NEW_FORM_TITLE' => 'New External OAuth Provider',
 
-    'LBL_LIST_DELETE' => 'Slet',
+    'LBL_LIST_DELETE' => 'Delete',
     'LBL_TYPE' => 'Type',
     'LBL_CONNECTOR' => 'Connector',
     'LBL_REDIRECT_URI' => 'Redirect URI',
@@ -97,7 +97,7 @@ $mod_strings = [
 
     'LBL_EXTRA' => 'Extra configurations',
     'LBL_MAPPING' => 'Mapping configurations',
-    'LBL_OTHER' => 'Andet',
+    'LBL_OTHER' => 'Andre',
 
 
     'LNK_LIST_CREATE_NEW_PERSONAL' => 'New Personal Provider',
@@ -106,5 +106,5 @@ $mod_strings = [
     'LNK_LIST_OUTBOUND_EMAILS' => 'Outbound Email Accounts',
     'LNK_LIST_EXTERNAL_OAUTH_CONNECTION' => 'External OAuth Connections',
 
-    'LBL_OWNER' => 'Ejer',
+    'LBL_OWNER' => 'Owner',
 ];
