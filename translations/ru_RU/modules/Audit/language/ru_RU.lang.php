f405b601ba70c284e40aa1b463af512ffa746824
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-03-30 13:57:07 +0100

    New translations en_us.lang.php (Russian)

--- translations/ru_RU/modules/Audit/language/ru_RU.lang.php
+++ translations/ru_RU/modules/Audit/language/ru_RU.lang.php
@@ -49,5 +49,5 @@ $mod_strings = array(
     'LBL_CREATED_BY' => 'Изменено',
     'LBL_LIST_DATE' => 'Дата изменения',
     'LBL_AUDITED_FIELDS' => 'Поля, контролируемые в этом модуле: ',
-    'LBL_CHANGE_LOG' => 'Журнал изменений',
+    'LBL_CHANGE_LOG' => 'Журнал изменений:',
 );
