5c83a0a687c6b36a239f088f74bcb65c11f5cb2b
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-03-30 11:21:06 +0100

    New translations en_us.lang.php (Danish)

--- translations/da_DK/modules/Trackers/language/da_DK.lang.php
+++ translations/da_DK/modules/Trackers/language/da_DK.lang.php
@@ -44,13 +44,13 @@ if (!defined('sugarEntry') || !sugarEntry) {
 
 $mod_strings = array(
     //Column header mapping
-    'action' => 'Handling',
-    'date_modified' => 'Dato for seneste handling',
+    'action' => 'Action',
+    'date_modified' => 'Date of Last Action',
     'item_id' => 'ID',
-    'item_summary' => 'Navn',
-    'module_name' => 'Modulnavn',
-    'users' => 'Brugere',
+    'item_summary' => 'Name',
+    'module_name' => 'Module Name',
+    'users' => 'Users',
 
     //Administration related labels
-    'LBL_MODULE_NAME' => 'Tråd',
+    'LBL_MODULE_NAME' => 'Trackers',
 );
