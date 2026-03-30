df018e195636796a15ca5a5f51ad672c93fe0015
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-03-30 11:08:06 +0100

    New translations en_us.lang.php (Arabic)

--- translations/ar/include/SugarObjects/implements/security_groups/language/ar.lang.php
+++ translations/ar/include/SugarObjects/implements/security_groups/language/ar.lang.php
@@ -44,5 +44,5 @@ if (!defined('sugarEntry') || !sugarEntry) {
 
 $mod_strings = array(
     'LBL_SECURITYGROUPS' => "المجموعات الأمنية",
-    'LBL_SECURITYGROUPS_SUBPANEL_TITLE' => "المجموعات الأمنية"
+    'LBL_SECURITYGROUPS_SUBPANEL_TITLE' => "Security Groups"
 );
