117ce548e3b2f461cd1456358e915a6c42f13de0
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-03-30 11:05:35 +0100

    New translations en_us.lang.php (Afrikaans)

--- translations/af_ZA/include/SugarObjects/templates/issue/language/application/af_ZA.lang.php
+++ translations/af_ZA/include/SugarObjects/templates/issue/language/application/af_ZA.lang.php
@@ -66,8 +66,8 @@ $app_list_strings = array(
     $object_name . '_resolution_dom' => array(
         '' => '',
         'Accepted' => 'Aanvaar',
-        'Duplicate' => 'Duplikaat',
-        'Closed' => 'Gesluit',
+        'Duplicate' => 'Duplicate',
+        'Closed' => 'Closed',
         'Out of Date' => 'Verouderd',
         'Invalid' => 'Ongeldig',
     ),
