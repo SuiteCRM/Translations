3cadb419d07465f63a40fcc6300289d2edc022a3
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-03-30 13:15:13 +0100

    New translations en_us.lang.php (Bulgarian)

--- translations/bg_BG/modules/TemplateSectionLine/language/bg_BG.lang.php
+++ translations/bg_BG/modules/TemplateSectionLine/language/bg_BG.lang.php
@@ -43,12 +43,12 @@ if (!defined('sugarEntry') || !sugarEntry) {
 }
 
 $mod_strings = array(
-    'LBL_DESCRIPTION' => 'Описание',
-    'LBL_NAME' => 'Име',
-    'LBL_LIST_NAME' => 'Име',
-    'LBL_EDIT_BUTTON' => 'Редактиране',
-    'LBL_REMOVE' => 'Изтрий',
-    'LBL_GRP' => 'Група',
-    'LBL_ORD' => 'Поръчка',
+    'LBL_DESCRIPTION' => 'Description',
+    'LBL_NAME' => 'Name',
+    'LBL_LIST_NAME' => 'Name',
+    'LBL_EDIT_BUTTON' => 'Edit',
+    'LBL_REMOVE' => 'Remove',
+    'LBL_GRP' => 'Group',
+    'LBL_ORD' => 'Order',
     'LBL_THUMBNAIL' => 'Thumbnail',
 );
