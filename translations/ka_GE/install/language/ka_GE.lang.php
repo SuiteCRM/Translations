cefdd112ef9cfa7d1733383381e5d3b745f1ea83
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-03-30 13:36:56 +0100

    New translations en_us.lang.php (Georgian)

--- translations/ka_GE/install/language/ka_GE.lang.php
+++ translations/ka_GE/install/language/ka_GE.lang.php
@@ -43,7 +43,7 @@ if (!defined('sugarEntry') || !sugarEntry) {
 }
 
 $mod_strings = array(
-    'LBL_BASIC' => 'საბაზო',
+    'LBL_BASIC' => 'BASIC',
     'LBL_BASIC_SEARCH' => 'სწარფი ფილტრი',
     'LBL_ADVANCED_SEARCH' => 'Advanced Filter',
     'LBL_BASIC_TYPE' => 'Basic Type',
@@ -479,7 +479,7 @@ $mod_strings = array(
     'LBL_WELCOME_CHOOSE_LANGUAGE' => '<b>Choose your language</b>',
     'LBL_WELCOME_SETUP_WIZARD' => 'Setup Wizard',
     'LBL_WIZARD_TITLE' => 'SuiteCRM Setup Wizard: ',
-    'LBL_YES' => 'დიახ',
+    'LBL_YES' => 'Yes',
 
     'LBL_PATCHES_TITLE' => 'Install Latest Patches',
     'LBL_MODULE_TITLE' => 'Install Language Packs',
