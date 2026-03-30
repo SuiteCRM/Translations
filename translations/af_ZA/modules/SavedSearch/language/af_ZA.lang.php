dccc0b165875bec128028df042732bf00eca0f2f
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-03-30 11:07:38 +0100

    New translations en_us.lang.php (Afrikaans)

--- translations/af_ZA/modules/SavedSearch/language/af_ZA.lang.php
+++ translations/af_ZA/modules/SavedSearch/language/af_ZA.lang.php
@@ -49,14 +49,14 @@ $mod_strings = array(
     'LBL_DELETE_CONFIRM' => 'Are you sure you want to delete the selected Saved Filter?',
     'LBL_DELETE_BUTTON_TITLE' => 'Delete this Saved Filter',
     'LBL_SAVE_BUTTON_TITLE' => 'Save the current filter',
-    'LBL_LIST_NAME' => 'Naam',
+    'LBL_LIST_NAME' => 'Name',
     'LBL_LIST_MODULE' => 'Module',
     'LBL_ORDER_BY_COLUMNS' => 'Order by column:',
     'LBL_DIRECTION' => 'Direction:',
     'LBL_SAVE_SEARCH_AS' => 'Save filter as:',
     'LBL_ASCENDING' => 'Ascending',
     'LBL_DESCENDING' => 'Descending',
-    'LBL_MODIFY_CURRENT_FILTER' => 'Wysig huidige filter',
+    'LBL_MODIFY_CURRENT_FILTER' => 'Modify current filter',
 
     'LBL_CREATED_BY' => 'Created By',
 
