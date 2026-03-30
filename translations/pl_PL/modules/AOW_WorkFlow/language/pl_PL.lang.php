d1e85eda58a3771c18a875ec23b311cebbc46ef8
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-03-30 13:51:47 +0100

    New translations en_us.lang.php (Polish)

--- translations/pl_PL/modules/AOW_WorkFlow/language/pl_PL.lang.php
+++ translations/pl_PL/modules/AOW_WorkFlow/language/pl_PL.lang.php
@@ -44,10 +44,10 @@ if (!defined('sugarEntry') || !sugarEntry) {
 
 $mod_strings = array(
     'LBL_ASSIGNED_TO_ID' => 'ID przydzielonego użytkownika',
-    'LBL_ASSIGNED_TO_NAME' => 'Przypisano do',
+    'LBL_ASSIGNED_TO_NAME' => 'Przypisany Do',
     'LBL_ID' => 'ID',
     'LBL_DATE_ENTERED' => 'Data utworzenia',
-    'LBL_DATE_MODIFIED' => 'Data Modyfikacji:',
+    'LBL_DATE_MODIFIED' => 'Data Modyfikacji',
     'LBL_MODIFIED' => 'Zmodyfikowane przez',
     'LBL_MODIFIED_NAME' => 'Nazwa użytkownika modyfikującego',
     'LBL_CREATED' => 'Utworzone przez',
@@ -55,8 +55,8 @@ $mod_strings = array(
     'LBL_DELETED' => 'Usunięte',
     'LBL_NAME' => 'Nazwa',
     'LBL_CREATED_USER' => 'Utworzony przez Użytkownika',
-    'LBL_MODIFIED_USER' => 'Zmodyfikowany przez Użytkownika',
-    'LBL_LIST_NAME' => 'Nazwa',
+    'LBL_MODIFIED_USER' => 'Zmodyfikowany przez użytkownika',
+    'LBL_LIST_NAME' => 'Nazwa dokument',
     'LBL_LIST_FORM_TITLE' => 'Lista workflow',
     'LBL_MODULE_NAME' => 'Workflow',
     'LBL_MODULE_TITLE' => 'Workflow',
