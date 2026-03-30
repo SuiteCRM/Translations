f847ee0cb52de90ad3c9f2f9d3732f255cb72c16
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-03-30 11:11:46 +0100

    New translations en_us.lang.php (Bulgarian)

--- translations/bg_BG/modules/CalendarAccount/language/bg_BG.lang.php
+++ translations/bg_BG/modules/CalendarAccount/language/bg_BG.lang.php
@@ -31,39 +31,39 @@ if (!defined('sugarEntry') || !sugarEntry) {
 
 $mod_strings = [
     // System fields
-    'LBL_ASSIGNED_TO_ID' => 'Възложен на оператор с идентификационен номер',
-    'LBL_ASSIGNED_TO_NAME' => 'Отговорник',
-    'LBL_DATE_ENTERED' => 'Създадено на:',
-    'LBL_DATE_MODIFIED' => 'Модифицирано на',
-    'LBL_MODIFIED' => 'Модифицирано от',
-    'LBL_MODIFIED_NAME' => 'Модифицирано от',
-    'LBL_CREATED' => 'Създадено от',
-    'LBL_CREATED_USER' => 'Създадено от потребител',
-    'LBL_MODIFIED_USER' => 'Модифицирано от потребител',
-    'LBL_DESCRIPTION' => 'Описание',
-    'LBL_DELETED' => 'Изтрити',
+    'LBL_ASSIGNED_TO_ID' => 'Assigned User Id',
+    'LBL_ASSIGNED_TO_NAME' => 'Assigned to',
+    'LBL_DATE_ENTERED' => 'Date Created',
+    'LBL_DATE_MODIFIED' => 'Date Modified',
+    'LBL_MODIFIED' => 'Modified By',
+    'LBL_MODIFIED_NAME' => 'Modified By Name',
+    'LBL_CREATED' => 'Created By',
+    'LBL_CREATED_USER' => 'Created by User',
+    'LBL_MODIFIED_USER' => 'Modified by User',
+    'LBL_DESCRIPTION' => 'Description',
+    'LBL_DELETED' => 'Deleted',
 
     // Security groups
     'LBL_SECURITYGROUPS' => 'Security Groups',
     'LBL_SECURITYGROUPS_SUBPANEL_TITLE' => 'Security Groups',
 
     // List view labels
-    'LBL_LIST_NAME' => 'Име',
-    'LBL_LIST_DELETE' => 'Изтриване',
-    'LBL_EDIT_BUTTON' => 'Редактиране',
-    'LBL_REMOVE' => 'Изтрий',
-    'LBL_ASCENDING' => 'Във възходящ ред',
-    'LBL_DESCENDING' => 'В нисходящ ред',
+    'LBL_LIST_NAME' => 'Name',
+    'LBL_LIST_DELETE' => 'Delete',
+    'LBL_EDIT_BUTTON' => 'Edit',
+    'LBL_REMOVE' => 'Remove',
+    'LBL_ASCENDING' => 'Ascending',
+    'LBL_DESCENDING' => 'Descending',
 
     // Email opt-in labels
-    'LBL_OPT_IN' => 'Съгласие',
-    'LBL_OPT_IN_PENDING_EMAIL_NOT_SENT' => 'Поканата за очаквано потвърждение на съгласие не е изпратена',
-    'LBL_OPT_IN_PENDING_EMAIL_SENT' => 'Изпратена е покана за очаквано потвърждаване на съгласие',
-    'LBL_OPT_IN_CONFIRMED' => 'Съгласен',
+    'LBL_OPT_IN' => 'Opt In',
+    'LBL_OPT_IN_PENDING_EMAIL_NOT_SENT' => 'Pending Confirm opt in, Confirm opt in not sent',
+    'LBL_OPT_IN_PENDING_EMAIL_SENT' => 'Pending Confirm opt in, Confirm opt in sent',
+    'LBL_OPT_IN_CONFIRMED' => 'Opted in',
 
     // Subpanel titles
-    'LBL_HISTORY_SUBPANEL_TITLE' => 'История',
-    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Дейности',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'View History',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Activities',
     'LBL_CALENDARACCOUNT_SUBPANEL_TITLE' => 'Calendar Accounts',
     'LBL_MEETINGS_SUBPANEL_TITLE' => 'Synced Meetings',
 
@@ -91,9 +91,9 @@ $mod_strings = [
     'LNK_LIST' => 'View Calendar Accounts',
 
     // Standard fields
-    'LBL_NAME' => 'Име',
-    'LBL_ID' => 'Идентификационен номер',
-    'LBL_TYPE' => 'Тип',
+    'LBL_NAME' => 'Name',
+    'LBL_ID' => 'ID',
+    'LBL_TYPE' => 'Type',
     'LBL_SOURCE' => 'Calendar Source',
 
     // OAuth2 Fields
@@ -101,8 +101,8 @@ $mod_strings = [
     'LBL_CONNECT_OAUTH' => 'Connect OAuth',
 
     // Basic Auth Fields
-    'LBL_USERNAME' => 'Потребителско име',
-    'LBL_PASSWORD' => 'Парола:',
+    'LBL_USERNAME' => 'Username',
+    'LBL_PASSWORD' => 'Password',
     'LBL_SERVER_URL' => 'Server URL',
     'LBL_TEST_CONNECTION' => 'Test Connection',
 
@@ -162,12 +162,12 @@ $mod_strings = [
     'LBL_SELECT_CALENDAR_SOURCE_FIRST' => 'Please select a calendar source first.',
 
     // Connection test messages
-    'LBL_CALENDAR' => 'Календар',
+    'LBL_CALENDAR' => 'Calendar',
     'LBL_SUCCESSFUL' => 'Successful',
-    'LBL_FAILED' => 'Завършил неуспешно',
+    'LBL_FAILED' => 'Failed',
     'LBL_CONNECTION_TEST' => 'Connection Test',
-    'LBL_YES' => 'Да',
-    'LBL_NO' => 'Не',
+    'LBL_YES' => 'Yes',
+    'LBL_NO' => 'No',
     'LBL_FOUND_CALENDARS' => 'Found {count} calendars.',
     'LBL_CONNECTION_TEST_RESPONSE_ERROR' => 'An error occurred while processing the connection test response. Please check the logs for details.',
     'LBL_CONNECTION_TEST_HTTP_ERROR' => 'Connection test failed with HTTP status',
@@ -182,14 +182,14 @@ $mod_strings = [
 
     // Dialog labels
     'LBL_NOTIFICATION' => 'Notification',
-    'LBL_CONFIRM' => 'Потвърждавам',
+    'LBL_CONFIRM' => 'Confirm',
     'LBL_ARE_YOU_SURE' => 'Are you sure?',
 
     // Sync status enum translations
-    'LBL_SYNC_STATUS_IN_PROGRESS' => 'В процес на изпълнение',
-    'LBL_SYNC_STATUS_SUCCESS' => 'Успешно',
-    'LBL_SYNC_STATUS_WARNING' => 'Предупреждение',
-    'LBL_SYNC_STATUS_ERROR' => 'Грешка:',
+    'LBL_SYNC_STATUS_IN_PROGRESS' => 'In Progress',
+    'LBL_SYNC_STATUS_SUCCESS' => 'Success',
+    'LBL_SYNC_STATUS_WARNING' => 'Warning',
+    'LBL_SYNC_STATUS_ERROR' => 'Error',
 
     // Sync message enum translations
     'LBL_SYNC_MSG_SYNC_COMPLETE' => 'All meetings synced.',
@@ -202,5 +202,5 @@ $mod_strings = [
     'LBL_SYNC_MSG_CALENDAR_NOT_FOUND' => 'Calendar not found.',
 
     // Relationship labels
-    'LBL_MEETINGS' => 'Срещи',
+    'LBL_MEETINGS' => 'Meetings',
 ];
