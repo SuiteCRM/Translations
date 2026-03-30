580a998742d54a85a0c23592e62a4fd10c8ba477
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-03-30 13:30:49 +0100

    New translations en_us.lang.php (Finnish)

--- translations/fi_FI/include/SugarObjects/templates/issue/language/application/fi_FI.lang.php
+++ translations/fi_FI/include/SugarObjects/templates/issue/language/application/fi_FI.lang.php
@@ -66,8 +66,8 @@ $app_list_strings = array(
     $object_name . '_resolution_dom' => array(
         '' => '',
         'Accepted' => 'Hyväksytty',
-        'Duplicate' => 'Monista',
-        'Closed' => 'Suljettu',
+        'Duplicate' => 'Duplicate',
+        'Closed' => 'Closed',
         'Out of Date' => 'Vanhentunut',
         'Invalid' => 'Viallinen',
     ),
