a220b17197dba53701a19f885cc1da88a112a023
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-03-30 13:17:59 +0100

    New translations en_us.lang.php (Czech)

--- translations/cs_CZ/include/SugarObjects/implements/security_groups/language/cs_CZ.lang.php
+++ translations/cs_CZ/include/SugarObjects/implements/security_groups/language/cs_CZ.lang.php
@@ -44,5 +44,5 @@ if (!defined('sugarEntry') || !sugarEntry) {
 
 $mod_strings = array(
     'LBL_SECURITYGROUPS' => "Skupiny zabezpečení",
-    'LBL_SECURITYGROUPS_SUBPANEL_TITLE' => "Skupiny zabezpečení"
+    'LBL_SECURITYGROUPS_SUBPANEL_TITLE' => "Security Groups"
 );
