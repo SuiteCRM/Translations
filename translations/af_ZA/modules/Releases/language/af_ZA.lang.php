53dde5a06bb41903eee67b0d9b842abf5d71b082
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-03-30 11:07:35 +0100

    New translations en_us.lang.php (Afrikaans)

--- translations/af_ZA/modules/Releases/language/af_ZA.lang.php
+++ translations/af_ZA/modules/Releases/language/af_ZA.lang.php
@@ -43,7 +43,7 @@ if (!defined('sugarEntry') || !sugarEntry) {
 }
 
 $mod_strings = array(
-    'LBL_MODULE_NAME' => 'Vrystellings',
+    'LBL_MODULE_NAME' => 'Releases',
     'LBL_MODULE_TITLE' => 'Releases: Home',
     'LBL_SEARCH_FORM_TITLE' => 'Release Search',
     'LBL_LIST_FORM_TITLE' => 'Release List',
@@ -56,14 +56,14 @@ $mod_strings = array(
     'LBL_LIST_STATUS' => 'Status',
     'LBL_STATUS' => 'Status:',
     'LNK_NEW_RELEASE' => 'Release List',
-    'NTC_DELETE_CONFIRMATION' => 'Is jy seker jy wil die rekord skrap?',
+    'NTC_DELETE_CONFIRMATION' => 'Are you sure you want to delete this record?',
     'ERR_DELETE_RECORD' => 'You must specify a record number to delete the release.',
     'NTC_STATUS' => 'Set status to Inactive to remove this release from the Release dropdown lists',
     'NTC_LIST_ORDER' => 'Set the order this release will appear in the Release dropdown lists',
     'release_status_dom' =>
         array(
-            'Active' => 'Aktief',
-            'Inactive' => 'Onaktief',
+            'Active' => 'Active',
+            'Inactive' => 'Inactive',
         ),
     'LBL_EDITLAYOUT' => 'Edit Layout' /*for 508 compliance fix*/,
 );
