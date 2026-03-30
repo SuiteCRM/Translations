877f6e24a2113d434c1a44adffbf51d2b0f41c23
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-03-30 13:46:57 +0100

    New translations en_us.lang.php (Mongolian)

--- translations/mn_MN/modules/Calendar/language/mn_MN.lang.php
+++ translations/mn_MN/modules/Calendar/language/mn_MN.lang.php
@@ -113,7 +113,7 @@ $mod_strings = array(
     'LBL_ERROR_LOADING' => 'Ачааллахад алдаа гарлаа',
     'LBL_GOTO_DATE' => 'Огноо руу очих',
     'NOTICE_DURATION_TIME' => 'Үргэлжлэх хугацаа 0-с их байх ёстой',
-    'LBL_STYLE_BASIC' => 'Үндсэн', //No need to be translated in all caps. Translation used just in menu action items when using the SuiteP template
+    'LBL_STYLE_BASIC' => 'BASIC', //No need to be translated in all caps. Translation used just in menu action items when using the SuiteP template
     'LBL_STYLE_ADVANCED' => 'Ахисан', //No need to be translated in all caps. Translation used just in menu action items when using the SuiteP template
 
     'LBL_NO_USER' => 'Хуваарилагдсан талбарт тохирох илэрц олдохгүй байна',
