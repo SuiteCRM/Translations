8bb9f5bac28f96d47026030cdc7d7196ade8f989
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-03-30 11:20:51 +0100

    New translations en_us.lang.php (Danish)

--- translations/da_DK/modules/SchedulersJobs/language/da_DK.lang.php
+++ translations/da_DK/modules/SchedulersJobs/language/da_DK.lang.php
@@ -43,9 +43,9 @@ if (!defined('sugarEntry') || !sugarEntry) {
 }
 
 $mod_strings = array(
-    'LBL_NAME' => 'Jobnavn',
-    'LBL_EXECUTE_TIME' => 'Udfør tid',
-    'LBL_SCHEDULER_ID' => 'Planlægger',
+    'LBL_NAME' => 'Job Name',
+    'LBL_EXECUTE_TIME' => 'Execute Time',
+    'LBL_SCHEDULER_ID' => 'Scheduler',
     'LBL_STATUS' => 'Job Status',
     'LBL_RESOLUTION' => 'Result',
     'LBL_MESSAGE' => 'Messages',
