85b86965d9df7ad1a1cc791e8f16a067ea859ce5
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-03-30 11:05:14 +0100

    New translations en_us.lang.php (Spanish)

--- translations/es_ES/modules/TemplateSectionLine/language/es_ES.lang.php
+++ translations/es_ES/modules/TemplateSectionLine/language/es_ES.lang.php
@@ -43,12 +43,12 @@ if (!defined('sugarEntry') || !sugarEntry) {
 }
 
 $mod_strings = array(
-    'LBL_DESCRIPTION' => 'Descripción',
-    'LBL_NAME' => 'Nombre',
-    'LBL_LIST_NAME' => 'Nombre',
-    'LBL_EDIT_BUTTON' => 'Editar',
-    'LBL_REMOVE' => 'Quitar',
-    'LBL_GRP' => 'Grupo',
-    'LBL_ORD' => 'Orden',
+    'LBL_DESCRIPTION' => 'Description',
+    'LBL_NAME' => 'Name',
+    'LBL_LIST_NAME' => 'Name',
+    'LBL_EDIT_BUTTON' => 'Edit',
+    'LBL_REMOVE' => 'Remove',
+    'LBL_GRP' => 'Group',
+    'LBL_ORD' => 'Order',
     'LBL_THUMBNAIL' => 'Miniatura',
 );
