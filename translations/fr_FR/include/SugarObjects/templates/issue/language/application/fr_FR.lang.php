dd941f23f456dabb1421e0f55136fb781acafd5d
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-03-30 10:59:39 +0100

    New translations en_us.lang.php (French)

--- translations/fr_FR/include/SugarObjects/templates/issue/language/application/fr_FR.lang.php
+++ translations/fr_FR/include/SugarObjects/templates/issue/language/application/fr_FR.lang.php
@@ -66,8 +66,8 @@ $app_list_strings = array(
     $object_name . '_resolution_dom' => array(
         '' => '',
         'Accepted' => 'Accepté',
-        'Duplicate' => 'Dupliquer',
-        'Closed' => 'Fermé',
+        'Duplicate' => 'Duplicate',
+        'Closed' => 'Closed',
         'Out of Date' => 'Périmé',
         'Invalid' => 'Invalide',
     ),
