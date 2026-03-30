eac1bff0d271a94a70d747d40d5eb0a34fd4803c
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-03-30 10:56:25 +0100

    New translations en_us.lang.php (Romanian)

--- translations/ro_RO/include/SugarObjects/templates/issue/language/ro_RO.lang.php
+++ translations/ro_RO/include/SugarObjects/templates/issue/language/ro_RO.lang.php
@@ -44,25 +44,25 @@ if (!defined('sugarEntry') || !sugarEntry) {
 
 $mod_strings = array(
 
-    'LBL_NAME' => 'Nume',
+    'LBL_NAME' => 'Name',
     'LBL_NUMBER' => 'Număr:',
     'LBL_STATUS' => 'Status:',
     'LBL_PRIORITY' => 'Prioritate:',
-    'LBL_DESCRIPTION' => 'Descriere:',
+    'LBL_DESCRIPTION' => 'Description:',
     'LBL_RESOLUTION' => 'Soluție',
     'LBL_LAST_MODIFIED' => 'Data modificării',
     'LBL_ASSIGNED_TO_ID' => 'Atribuit lui:',
-    'LBL_ASSIGNED_TO_NAME' => 'Utilizator:',
+    'LBL_ASSIGNED_TO_NAME' => 'User:',
     'LBL_WORK_LOG' => 'Jurnal de activitate:',
     'LBL_CREATED_BY' => 'Creat de:',
-    'LBL_DATE_ENTERED' => 'Data înregistrării:',
-    'LBL_DATE_MODIFIED' => 'Dată Modificare:',
+    'LBL_DATE_ENTERED' => 'Date Created:',
+    'LBL_DATE_MODIFIED' => 'Date Modified:',
     'LBL_MODIFIED_BY' => 'Modificat de:',
     'LBL_ASSIGNED_USER' => 'Atribuit utilizatorului:',
-    'LBL_TYPE' => 'Tip:',
+    'LBL_TYPE' => 'Type:',
     'LBL_SUBJECT' => 'Subiect:',
 
-    'LBL_EDIT_BUTTON' => 'Editează',
-    'LBL_REMOVE' => 'Elimină',
+    'LBL_EDIT_BUTTON' => 'Edit',
+    'LBL_REMOVE' => 'Remove',
 
 );
