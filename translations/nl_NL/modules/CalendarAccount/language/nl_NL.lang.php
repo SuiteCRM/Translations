623ce5905870077518d5f6f997bc52c0495f8bf0
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-03-30 10:56:46 +0100

    New translations en_us.lang.php (Dutch)

--- translations/nl_NL/modules/CalendarAccount/language/nl_NL.lang.php
+++ translations/nl_NL/modules/CalendarAccount/language/nl_NL.lang.php
@@ -31,40 +31,40 @@ if (!defined('sugarEntry') || !sugarEntry) {
 
 $mod_strings = [
     // System fields
-    'LBL_ASSIGNED_TO_ID' => 'Toegewezen gebruiker-Id',
-    'LBL_ASSIGNED_TO_NAME' => 'Toegewezen aan',
-    'LBL_DATE_ENTERED' => 'Datum aangemaakt',
-    'LBL_DATE_MODIFIED' => 'Datum gewijzigd',
-    'LBL_MODIFIED' => 'Gewijzigd door',
-    'LBL_MODIFIED_NAME' => 'Gewijzigd door naam',
-    'LBL_CREATED' => 'Aangemaakt door',
-    'LBL_CREATED_USER' => 'Aangemaakt door gebruiker',
-    'LBL_MODIFIED_USER' => 'Gewijzigd door gebruiker',
-    'LBL_DESCRIPTION' => 'Omschrijving',
-    'LBL_DELETED' => 'Verwijderd',
+    'LBL_ASSIGNED_TO_ID' => 'Assigned User Id',
+    'LBL_ASSIGNED_TO_NAME' => 'Assigned to',
+    'LBL_DATE_ENTERED' => 'Date Created',
+    'LBL_DATE_MODIFIED' => 'Date Modified',
+    'LBL_MODIFIED' => 'Modified By',
+    'LBL_MODIFIED_NAME' => 'Modified By Name',
+    'LBL_CREATED' => 'Created By',
+    'LBL_CREATED_USER' => 'Created by User',
+    'LBL_MODIFIED_USER' => 'Modified by User',
+    'LBL_DESCRIPTION' => 'Description',
+    'LBL_DELETED' => 'Deleted',
 
     // Security groups
-    'LBL_SECURITYGROUPS' => 'Security Groepen',
-    'LBL_SECURITYGROUPS_SUBPANEL_TITLE' => 'Security Groepen',
+    'LBL_SECURITYGROUPS' => 'Security Groups',
+    'LBL_SECURITYGROUPS_SUBPANEL_TITLE' => 'Security Groups',
 
     // List view labels
-    'LBL_LIST_NAME' => 'Naam',
-    'LBL_LIST_DELETE' => 'Verwijderen',
-    'LBL_EDIT_BUTTON' => 'Wijzigen',
-    'LBL_REMOVE' => 'Verwijder',
-    'LBL_ASCENDING' => 'Oplopend',
-    'LBL_DESCENDING' => 'Aflopend',
+    'LBL_LIST_NAME' => 'Name',
+    'LBL_LIST_DELETE' => 'Delete',
+    'LBL_EDIT_BUTTON' => 'Edit',
+    'LBL_REMOVE' => 'Remove',
+    'LBL_ASCENDING' => 'Ascending',
+    'LBL_DESCENDING' => 'Descending',
 
     // Email opt-in labels
-    'LBL_OPT_IN' => 'Inschrijven',
-    'LBL_OPT_IN_PENDING_EMAIL_NOT_SENT' => 'Nog in afwachting van bevestigde opt-in, bevestigde opt-in niet verzonden',
-    'LBL_OPT_IN_PENDING_EMAIL_SENT' => 'In afwachting van bevestigen opt-in, opt-bevestigen in verzonden',
-    'LBL_OPT_IN_CONFIRMED' => 'Inschrijven',
+    'LBL_OPT_IN' => 'Opt In',
+    'LBL_OPT_IN_PENDING_EMAIL_NOT_SENT' => 'Pending Confirm opt in, Confirm opt in not sent',
+    'LBL_OPT_IN_PENDING_EMAIL_SENT' => 'Pending Confirm opt in, Confirm opt in sent',
+    'LBL_OPT_IN_CONFIRMED' => 'Opted in',
 
     // Subpanel titles
-    'LBL_HISTORY_SUBPANEL_TITLE' => 'Bekijk geschiedenis',
-    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Activiteiten',
-    'LBL_CALENDARACCOUNT_SUBPANEL_TITLE' => 'Kalender Accounts',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'View History',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Activities',
+    'LBL_CALENDARACCOUNT_SUBPANEL_TITLE' => 'Calendar Accounts',
     'LBL_MEETINGS_SUBPANEL_TITLE' => 'Gesynchroniseerde vergaderingen',
 
     // Import links
@@ -75,12 +75,12 @@ $mod_strings = [
     'LNK_LIST_CREATE_NEW_PERSONAL' => 'Persoonlijke kalender account aanmaken',
     'LNK_LIST_INBOUND_EMAILS' => 'Inkomende e-mails',
     'LNK_LIST_OUTBOUND_EMAILS' => 'Uitgaande e-mails',
-    'LNK_LIST_EXTERNAL_OAUTH_PROVIDERS' => 'Externe OAuth Providers',
-    'LNK_LIST_EXTERNAL_OAUTH_CONNECTIONS' => 'Externe OAuth verbindingen',
+    'LNK_LIST_EXTERNAL_OAUTH_PROVIDERS' => 'External OAuth Providers',
+    'LNK_LIST_EXTERNAL_OAUTH_CONNECTIONS' => 'External OAuth Connections',
 
     // Basic module labels
-    'LBL_MODULE_NAME' => 'Kalender Accounts',
-    'LBL_MODULE_TITLE' => 'Kalender Accounts',
+    'LBL_MODULE_NAME' => 'Calendar Accounts',
+    'LBL_MODULE_TITLE' => 'Calendar Accounts',
     'LBL_LIST_FORM_TITLE' => 'Kalender Accounts Lijst',
     'LBL_SEARCH_FORM_TITLE' => 'Kalender accounts zoeken',
     'LBL_NEW_FORM_TITLE' => 'Nieuwe Kalender Account',
@@ -91,8 +91,8 @@ $mod_strings = [
     'LNK_LIST' => 'Bekijk Kalender Accounts',
 
     // Standard fields
-    'LBL_NAME' => 'Naam',
-    'LBL_ID' => 'Id',
+    'LBL_NAME' => 'Name',
+    'LBL_ID' => 'ID',
     'LBL_TYPE' => 'Type',
     'LBL_SOURCE' => 'Kalender bron',
 
@@ -101,8 +101,8 @@ $mod_strings = [
     'LBL_CONNECT_OAUTH' => 'Verbind OAuth',
 
     // Basic Auth Fields
-    'LBL_USERNAME' => 'Gebruikersnaam',
-    'LBL_PASSWORD' => 'Wachtwoord',
+    'LBL_USERNAME' => 'Username',
+    'LBL_PASSWORD' => 'Password',
     'LBL_SERVER_URL' => 'Server URL',
     'LBL_TEST_CONNECTION' => 'Test verbinding',
 
@@ -114,7 +114,7 @@ $mod_strings = [
     // Calendar User Fields
     'LBL_CALENDAR_USER_ID' => 'Kalender Gebruikers ID',
     'LBL_CALENDAR_USER_NAME' => 'Kalender gebruiker',
-    'LBL_CALENDAR_USER' => 'Kalender gebruiker',
+    'LBL_CALENDAR_USER' => 'Calendar User',
 
     // Auth Actions
     'LBL_AUTH_ACTIONS' => 'Authenticatie acties',
@@ -126,7 +126,7 @@ $mod_strings = [
     'LBL_LAST_CONNECTION_STATUS' => 'Test succesvol',
     'LBL_LAST_CONNECTION_TEST' => 'Laatste Connectie Test',
     'LBL_LAST_SYNC_ATTEMPT_DATE' => 'Laatste synchronisatie poging',
-    'LBL_LAST_SYNC_ATTEMPT_STATUS' => 'Synchronisatie status',
+    'LBL_LAST_SYNC_ATTEMPT_STATUS' => 'Sync Status',
     'LBL_LAST_SYNC_ATTEMPT_MESSAGE' => 'Synchroniseer bericht',
     'LBL_LAST_SYNC_DATE' => 'Laatste synchronisatiedatum',
     'LBL_EXTERNAL_CALENDAR_ID' => 'Externe kalender ID',
@@ -162,12 +162,12 @@ $mod_strings = [
     'LBL_SELECT_CALENDAR_SOURCE_FIRST' => 'Selecteer eerst de bron van de kalender.',
 
     // Connection test messages
-    'LBL_CALENDAR' => 'Agenda',
-    'LBL_SUCCESSFUL' => 'Succesvolle',
-    'LBL_FAILED' => 'Mislukt',
+    'LBL_CALENDAR' => 'Calendar',
+    'LBL_SUCCESSFUL' => 'Successful',
+    'LBL_FAILED' => 'Failed',
     'LBL_CONNECTION_TEST' => 'Verbinding Test',
-    'LBL_YES' => 'Ja',
-    'LBL_NO' => 'Nee',
+    'LBL_YES' => 'Yes',
+    'LBL_NO' => 'No',
     'LBL_FOUND_CALENDARS' => 'Gevonden {count} kalenders.',
     'LBL_CONNECTION_TEST_RESPONSE_ERROR' => 'Er is een fout opgetreden tijdens het verwerken van de verbinding test reactie. Controleer de logs voor meer informatie.',
     'LBL_CONNECTION_TEST_HTTP_ERROR' => 'Verbindingstest mislukt met HTTP-status',
@@ -182,25 +182,25 @@ $mod_strings = [
 
     // Dialog labels
     'LBL_NOTIFICATION' => 'Notificatie',
-    'LBL_CONFIRM' => 'Bevestig',
+    'LBL_CONFIRM' => 'Confirm',
     'LBL_ARE_YOU_SURE' => 'Weet u het zeker?',
 
     // Sync status enum translations
-    'LBL_SYNC_STATUS_IN_PROGRESS' => 'In Bewerking',
-    'LBL_SYNC_STATUS_SUCCESS' => 'Succesvol',
-    'LBL_SYNC_STATUS_WARNING' => 'Waarschuwing',
-    'LBL_SYNC_STATUS_ERROR' => 'Foutmelding',
+    'LBL_SYNC_STATUS_IN_PROGRESS' => 'In Progress',
+    'LBL_SYNC_STATUS_SUCCESS' => 'Success',
+    'LBL_SYNC_STATUS_WARNING' => 'Warning',
+    'LBL_SYNC_STATUS_ERROR' => 'Error',
 
     // Sync message enum translations
-    'LBL_SYNC_MSG_SYNC_COMPLETE' => 'Alle vergaderingen gesynchroniseerd.',
-    'LBL_SYNC_MSG_UP_TO_DATE' => 'Geen nieuwe vergaderingen om te synchroniseren.',
-    'LBL_SYNC_MSG_MEETINGS_FAILED' => 'Sommige vergaderingen konden niet worden gesynchroniseerd. Controleer logs.',
-    'LBL_SYNC_MSG_SYNC_PARTIAL' => 'Sommige vergaderingen gesynchroniseerd. Meer in behandeling vanwege beperkingen.',
+    'LBL_SYNC_MSG_SYNC_COMPLETE' => 'All meetings synced.',
+    'LBL_SYNC_MSG_UP_TO_DATE' => 'No new meetings to sync.',
+    'LBL_SYNC_MSG_MEETINGS_FAILED' => 'Some meetings failed to sync. Check logs.',
+    'LBL_SYNC_MSG_SYNC_PARTIAL' => 'Some meetings synced. More pending due to limits.',
     'LBL_SYNC_MSG_SYNC_ERROR' => 'Synchronisatiefout. Controleer logs.',
     'LBL_SYNC_MSG_TOKEN_EXPIRED' => 'Authenticatietoken is verlopen.',
     'LBL_SYNC_MSG_CONNECTION_ERROR' => 'Verbindingsfout.',
     'LBL_SYNC_MSG_CALENDAR_NOT_FOUND' => 'Kalender niet gevonden.',
 
     // Relationship labels
-    'LBL_MEETINGS' => 'Afspraken',
+    'LBL_MEETINGS' => 'Meetings',
 ];
