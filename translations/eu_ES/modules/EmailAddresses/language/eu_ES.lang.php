dbe2ccce6aff5221146f1f501454a706635d6eae
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-03-30 11:27:50 +0100

    New translations en_us.lang.php (Basque)

--- translations/eu_ES/modules/EmailAddresses/language/eu_ES.lang.php
+++ translations/eu_ES/modules/EmailAddresses/language/eu_ES.lang.php
@@ -44,12 +44,12 @@ if (!defined('sugarEntry') || !sugarEntry) {
 
 $mod_strings = array(
     'LBL_EMAIL_ADDRESS_ID' => 'ID',
-    'LBL_EMAIL_ADDRESS' => 'Posta elektronikoa',
+    'LBL_EMAIL_ADDRESS' => 'Email Address',
     'LBL_EMAIL_ADDRESS_CAPS' => 'Email Address caps',
     'LBL_INVALID_EMAIL' => 'Invalid Email',
-    'LBL_OPT_OUT' => 'Baztertuak',
+    'LBL_OPT_OUT' => 'Opted Out',
     'LBL_CONFIRM_OPT_IN' => 'Confirm Opt In',
     'LBL_DATE_CREATE' => 'Date Create',
-    'LBL_DATE_MODIFIED' => 'Aldaketa-data',
-    'LBL_DELETED' => 'Ezabatu',
+    'LBL_DATE_MODIFIED' => 'Date Modified',
+    'LBL_DELETED' => 'Delete',
 );
