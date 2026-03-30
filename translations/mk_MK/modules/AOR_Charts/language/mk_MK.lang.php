de8db775176741dabec53eb53c2916d8182b6cae
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-03-30 13:44:15 +0100

    New translations en_us.lang.php (Macedonian)

--- translations/mk_MK/modules/AOR_Charts/language/mk_MK.lang.php
+++ translations/mk_MK/modules/AOR_Charts/language/mk_MK.lang.php
@@ -43,7 +43,7 @@ if (!defined('sugarEntry') || !sugarEntry) {
 }
 
 $mod_strings = array(
-    'LBL_ID' => 'ID',
+    'LBL_ID' => 'Број',
     'LBL_DATE_ENTERED' => 'Дата на креирање',
     'LBL_DATE_MODIFIED' => 'Дата на промена',
     'LBL_MODIFIED' => 'Променето од',
@@ -56,7 +56,7 @@ $mod_strings = array(
     'LBL_MODIFIED_USER' => 'Променето од корисник',
     'LBL_LIST_NAME' => 'Име',
     'LBL_EDIT_BUTTON' => 'Промени',
-    'LBL_REMOVE' => 'Отстрани',
+    'LBL_REMOVE' => 'Избриши',
     'LBL_ASSIGNED_TO_ID' => 'Доделено на Корисник ID',
     'LBL_ASSIGNED_TO_NAME' => 'Доделено на',
     'LBL_LIST_FORM_TITLE' => 'Charts List',
@@ -66,7 +66,7 @@ $mod_strings = array(
     'LNK_NEW_RECORD' => 'Create Charts',
     'LNK_LIST' => 'View Charts',
     'LBL_SEARCH_FORM_TITLE' => 'Search Charts',
-    'LBL_HISTORY_SUBPANEL_TITLE' => 'Види историја',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'View History',
     'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Активности',
     'LBL_AOR_CHARTS_SUBPANEL_TITLE' => 'Графикони',
     'LBL_NEW_FORM_TITLE' => 'New Charts',
