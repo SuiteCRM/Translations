c0a958e0fbd86cb9e717a161b2a8727ef5c2d737
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-03-30 11:10:18 +0100

    New translations en_us.lang.php (Arabic)

--- translations/ar/modules/SavedSearch/language/ar.lang.php
+++ translations/ar/modules/SavedSearch/language/ar.lang.php
@@ -49,15 +49,15 @@ $mod_strings = array(
     'LBL_DELETE_CONFIRM' => 'هل أنت متأكد من أنك تريد حذف الفرز المحفوظ المحدد؟',
     'LBL_DELETE_BUTTON_TITLE' => 'حذف هذا الفرز المحفوظ',
     'LBL_SAVE_BUTTON_TITLE' => 'حفظ الفرز الحالي',
-    'LBL_LIST_NAME' => 'الاسم',
-    'LBL_LIST_MODULE' => 'الوحدة',
+    'LBL_LIST_NAME' => 'Name',
+    'LBL_LIST_MODULE' => 'Module',
     'LBL_ORDER_BY_COLUMNS' => 'Order by column:',
-    'LBL_DIRECTION' => 'الاتجاه:',
+    'LBL_DIRECTION' => 'Direction:',
     'LBL_SAVE_SEARCH_AS' => 'حفظ الفرز كـ:',
-    'LBL_ASCENDING' => 'تصاعدي',
-    'LBL_DESCENDING' => 'تنازلي',
-    'LBL_MODIFY_CURRENT_FILTER' => 'تعديل الفلتر الحالي',
+    'LBL_ASCENDING' => 'Ascending',
+    'LBL_DESCENDING' => 'Descending',
+    'LBL_MODIFY_CURRENT_FILTER' => 'Modify current filter',
 
-    'LBL_CREATED_BY' => 'أنشئ بواسطة',
+    'LBL_CREATED_BY' => 'Created By',
 
 );
