9fa2a7698d85e33f734e30b23f8c50a8654d6d08
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-03-30 10:56:15 +0100

    New translations en_us.lang.php (Romanian)

--- translations/ro_RO/include/SugarObjects/implements/security_groups/language/ro_RO.lang.php
+++ translations/ro_RO/include/SugarObjects/implements/security_groups/language/ro_RO.lang.php
@@ -44,5 +44,5 @@ if (!defined('sugarEntry') || !sugarEntry) {
 
 $mod_strings = array(
     'LBL_SECURITYGROUPS' => "Grupuri Securitate",
-    'LBL_SECURITYGROUPS_SUBPANEL_TITLE' => "Grupuri Securitate"
+    'LBL_SECURITYGROUPS_SUBPANEL_TITLE' => "Security Groups"
 );
