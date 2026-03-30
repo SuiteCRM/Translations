f6b11fc2f6aa5da2d2c0ddbbde8f468ba4a41687
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-03-30 11:08:02 +0100

    New translations en_us.lang.php (Afrikaans)

--- translations/af_ZA/modules/jjwg_Markers/language/af_ZA.lang.php
+++ translations/af_ZA/modules/jjwg_Markers/language/af_ZA.lang.php
@@ -43,19 +43,19 @@ if (!defined('sugarEntry') || !sugarEntry) {
 }
 
 $mod_strings['LBL_ASSIGNED_TO_ID'] = 'Assigned User Id';
-$mod_strings['LBL_ASSIGNED_TO_NAME'] = 'Gebruiker';
+$mod_strings['LBL_ASSIGNED_TO_NAME'] = 'User';
 $mod_strings['LBL_ID'] = 'ID';
-$mod_strings['LBL_DATE_ENTERED'] = 'Datum geskep';
+$mod_strings['LBL_DATE_ENTERED'] = 'Date Created';
 $mod_strings['LBL_DATE_MODIFIED'] = 'Date Modified';
 $mod_strings['LBL_MODIFIED'] = 'Modified By';
-$mod_strings['LBL_MODIFIED_NAME'] = 'Gewysig deur Naam';
+$mod_strings['LBL_MODIFIED_NAME'] = 'Modified By Name';
 $mod_strings['LBL_CREATED'] = 'Created By';
-$mod_strings['LBL_DESCRIPTION'] = 'Beskrywing';
-$mod_strings['LBL_DELETED'] = 'Geskrap';
-$mod_strings['LBL_NAME'] = 'Naam';
-$mod_strings['LBL_CREATED_USER'] = 'Geskep deur Gebruiker';
-$mod_strings['LBL_MODIFIED_USER'] = 'Verander deur gebruiker';
-$mod_strings['LBL_LIST_NAME'] = 'Naam';
+$mod_strings['LBL_DESCRIPTION'] = 'Description';
+$mod_strings['LBL_DELETED'] = 'Deleted';
+$mod_strings['LBL_NAME'] = 'Name';
+$mod_strings['LBL_CREATED_USER'] = 'Created by User';
+$mod_strings['LBL_MODIFIED_USER'] = 'Modified by User';
+$mod_strings['LBL_LIST_NAME'] = 'Name';
 $mod_strings['LBL_LIST_FORM_TITLE'] = 'Markers List';
 $mod_strings['LBL_MODULE_NAME'] = 'Markers';
 $mod_strings['LBL_MODULE_TITLE'] = 'Markers';
@@ -65,7 +65,7 @@ $mod_strings['LNK_LIST'] = 'View Markers';
 $mod_strings['LNK_IMPORT_JJWG_MARKERS'] = 'Import Markers';
 $mod_strings['LBL_SEARCH_FORM_TITLE'] = 'Search Markers';
 $mod_strings['LBL_HISTORY_SUBPANEL_TITLE'] = 'View History';
-$mod_strings['LBL_ACTIVITIES_SUBPANEL_TITLE'] = 'Aktiwiteite';
+$mod_strings['LBL_ACTIVITIES_SUBPANEL_TITLE'] = 'Activities';
 $mod_strings['LBL_NEW_FORM_TITLE'] = 'New Markers';
 $mod_strings['LBL_CITY'] = 'City';
 $mod_strings['LBL_STATE'] = 'State';
