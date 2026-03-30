bd284b723363dee90dc2b68d16ec6eaaf938b724
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-03-30 11:22:52 +0100

    New translations en_us.lang.php (German)

--- translations/de_DE/modules/Groups/language/de_DE.lang.php
+++ translations/de_DE/modules/Groups/language/de_DE.lang.php
@@ -43,12 +43,12 @@ if (!defined('sugarEntry') || !sugarEntry) {
 }
 
 $mod_strings = array(
-    'LBL_MODULE_NAME' => 'Gruppen',
-    'LBL_GROUP_NAME' => 'Gruppenname:',
-    'LBL_DESCRIPTION' => 'Beschreibung:',
+    'LBL_MODULE_NAME' => 'Groups',
+    'LBL_GROUP_NAME' => 'Group Name:',
+    'LBL_DESCRIPTION' => 'Description:',
     'LBL_TEAM' => 'Team:',
     // ListView
-    'LBL_LIST_TITLE' => 'Gruppen',
+    'LBL_LIST_TITLE' => 'Groups',
     // Links
     'LNK_ALL_GROUPS' => 'Alle Gruppen',
     'LNK_NEW_GROUP' => 'Neue Gruppe',
