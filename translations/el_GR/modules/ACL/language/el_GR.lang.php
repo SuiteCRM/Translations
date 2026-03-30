84385098218b5e9667f725396b1da5cb9b3a4320
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-03-30 11:24:08 +0100

    New translations en_us.lang.php (Greek)

--- translations/el_GR/modules/ACL/language/el_GR.lang.php
+++ translations/el_GR/modules/ACL/language/el_GR.lang.php
@@ -43,15 +43,15 @@ if (!defined('sugarEntry') || !sugarEntry) {
 }
 
 $mod_strings = array(
-    'LBL_ROLE' => 'Ρόλος',
-    'LBL_NAME' => 'Όνομα',
-    'LBL_DESCRIPTION' => 'Περιγραφή',
+    'LBL_ROLE' => 'Role',
+    'LBL_NAME' => 'Name',
+    'LBL_DESCRIPTION' => 'Description',
     'LIST_ROLES' => 'Κατάλογος ρόλων',
-    'LBL_USERS_SUBPANEL_TITLE' => 'Χρήστες',
+    'LBL_USERS_SUBPANEL_TITLE' => 'Users',
     'LIST_ROLES_BY_USER' => 'Ρόλοι ανά χρήστη',
     'LBL_ROLES_SUBPANEL_TITLE' => 'Ρόλος χρήστη',
-    'LBL_SEARCH_FORM_TITLE' => 'Αναζήτηση',
-    'LBL_NO_ACCESS' => 'Δεν έχετε πρόσβαση σε αυτή την περιοχή. Παρακαλώ επικοινωνήστε με τον διαχειριστή του προγράμματος αν πιστεύετε πως πρέπει να έχετε πρόσβαση.',
+    'LBL_SEARCH_FORM_TITLE' => 'Search',
+    'LBL_NO_ACCESS' => 'You do not have access to this area. Contact your site administrator to obtain access.',
     'LBL_REDIRECT_TO_HOME' => 'Επαναδρομολόγηση σε Αρχική σε',
     'LBL_SECONDS' => 'δευτερόλεπτα',
     'LBL_ADDING' => 'Προσθέτοντας για',
