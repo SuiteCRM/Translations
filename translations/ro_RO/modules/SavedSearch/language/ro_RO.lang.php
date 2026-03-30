492769851a7a887f16f347ec085214d8c6100903
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-03-30 10:59:04 +0100

    New translations en_us.lang.php (Romanian)

--- translations/ro_RO/modules/SavedSearch/language/ro_RO.lang.php
+++ translations/ro_RO/modules/SavedSearch/language/ro_RO.lang.php
@@ -49,15 +49,15 @@ $mod_strings = array(
     'LBL_DELETE_CONFIRM' => 'Sunteți sigur că doriți să ștergeți filtrul salvat selectat?',
     'LBL_DELETE_BUTTON_TITLE' => 'Șterge acest filtru salvat',
     'LBL_SAVE_BUTTON_TITLE' => 'Salvează filtrul curent',
-    'LBL_LIST_NAME' => 'Nume',
-    'LBL_LIST_MODULE' => 'Modul',
+    'LBL_LIST_NAME' => 'Name',
+    'LBL_LIST_MODULE' => 'Module',
     'LBL_ORDER_BY_COLUMNS' => 'Ordinea pe coloane',
-    'LBL_DIRECTION' => 'Direcție:',
+    'LBL_DIRECTION' => 'Direction:',
     'LBL_SAVE_SEARCH_AS' => 'Salvează filtru ca:',
-    'LBL_ASCENDING' => 'Ascendent',
-    'LBL_DESCENDING' => 'Descendent',
-    'LBL_MODIFY_CURRENT_FILTER' => 'Modifică filtru curent',
+    'LBL_ASCENDING' => 'Ascending',
+    'LBL_DESCENDING' => 'Descending',
+    'LBL_MODIFY_CURRENT_FILTER' => 'Modify current filter',
 
-    'LBL_CREATED_BY' => 'Creat de',
+    'LBL_CREATED_BY' => 'Created By',
 
 );
