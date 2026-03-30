078c9be7d91d66fe787c1764a63c342e88e276d1
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-03-30 11:23:12 +0100

    New translations en_us.lang.php (German)

--- translations/de_DE/modules/OAuthTokens/language/de_DE.lang.php
+++ translations/de_DE/modules/OAuthTokens/language/de_DE.lang.php
@@ -46,8 +46,8 @@ $mod_strings = array(
     'LBL_OAUTH_REQUEST' => "Token anfordern",
     'LBL_OAUTH_AUTHORIZE' => "Token bestätigen",
     'LBL_OAUTH_CONSUMERREQ' => "Bestätigund des Token vom Verbraucher <b>%s</b>?",
-    'LBL_ASSIGNED_TO_NAME' => 'Benutzer',
-    'LBL_ID' => 'Id',
+    'LBL_ASSIGNED_TO_NAME' => 'User',
+    'LBL_ID' => 'ID',
     'LBL_STATUS' => 'Status',
     'LBL_TS' => 'Zeitstempel',
     'LBL_LIST_DELETE' => 'Token löschen',
@@ -56,7 +56,7 @@ $mod_strings = array(
 
     'LBL_TOKEN_TS' => 'Token TS',
     'LBL_CALLBACK_URL' => 'URL für Rückfragen',
-    'LBL_SECRET' => 'Schlüssel',
+    'LBL_SECRET' => 'Secret',
     'LBL_TSTATE' => 'TState',
-    'LBL_VERIFY' => 'Bestätigung',
+    'LBL_VERIFY' => 'Verify',
 );
