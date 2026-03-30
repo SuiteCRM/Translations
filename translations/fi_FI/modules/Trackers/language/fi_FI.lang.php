9ccefcf7f9ad621d3e67b50b0141737a10c01695
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-03-30 11:31:38 +0100

    New translations en_us.lang.php (Finnish)

--- translations/fi_FI/modules/Trackers/language/fi_FI.lang.php
+++ translations/fi_FI/modules/Trackers/language/fi_FI.lang.php
@@ -44,13 +44,13 @@ if (!defined('sugarEntry') || !sugarEntry) {
 
 $mod_strings = array(
     //Column header mapping
-    'action' => 'Toiminto',
-    'date_modified' => 'Viimeisen tapahtuman pvm',
-    'item_id' => 'Tunnus',
-    'item_summary' => 'Nimi',
-    'module_name' => 'Moduulin nimi',
-    'users' => 'Käyttäjät',
+    'action' => 'Action',
+    'date_modified' => 'Date of Last Action',
+    'item_id' => 'ID',
+    'item_summary' => 'Name',
+    'module_name' => 'Module Name',
+    'users' => 'Users',
 
     //Administration related labels
-    'LBL_MODULE_NAME' => 'Seurantakoodit',
+    'LBL_MODULE_NAME' => 'Trackers',
 );
