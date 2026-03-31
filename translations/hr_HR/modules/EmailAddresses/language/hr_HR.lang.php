f56e7bf7ff731105685d74ab37c83079db97f8d3
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-03-31 15:05:37 +0100

    New translations en_us.lang.php (Croatian)

--- translations/hr_HR/modules/EmailAddresses/language/hr_HR.lang.php
+++ translations/hr_HR/modules/EmailAddresses/language/hr_HR.lang.php
@@ -44,12 +44,12 @@ if (!defined('sugarEntry') || !sugarEntry) {
 
 $mod_strings = array(
     'LBL_EMAIL_ADDRESS_ID' => 'ID',
-    'LBL_EMAIL_ADDRESS' => 'Adresa e-pošte',
+    'LBL_EMAIL_ADDRESS' => 'Email Address',
     'LBL_EMAIL_ADDRESS_CAPS' => 'Email Address caps',
-    'LBL_INVALID_EMAIL' => 'Pogrešna e-pošta',
-    'LBL_OPT_OUT' => 'Odjavljeno',
+    'LBL_INVALID_EMAIL' => 'Invalid Email',
+    'LBL_OPT_OUT' => 'Opted Out',
     'LBL_CONFIRM_OPT_IN' => 'Confirm Opt In',
     'LBL_DATE_CREATE' => 'Datum kreiranja',
-    'LBL_DATE_MODIFIED' => 'Izmijenjeno:',
-    'LBL_DELETED' => 'Izbriši',
+    'LBL_DATE_MODIFIED' => 'Date Modified',
+    'LBL_DELETED' => 'Delete',
 );
