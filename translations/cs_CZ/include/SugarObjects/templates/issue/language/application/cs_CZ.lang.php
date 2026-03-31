bcfbecb9449dcbd8c7ba54579422fedd96e9f77a
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-03-31 11:58:22 +0100

    New translations en_us.lang.php (Czech)

--- translations/cs_CZ/include/SugarObjects/templates/issue/language/application/cs_CZ.lang.php
+++ translations/cs_CZ/include/SugarObjects/templates/issue/language/application/cs_CZ.lang.php
@@ -66,8 +66,8 @@ $app_list_strings = array(
     $object_name . '_resolution_dom' => array(
         '' => '',
         'Accepted' => 'Přijato',
-        'Duplicate' => 'Duplicate',
-        'Closed' => 'Closed',
+        'Duplicate' => 'Kopírovat',
+        'Closed' => 'Uzavřeno',
         'Out of Date' => 'Neaktuální',
         'Invalid' => 'Neplatné',
     ),
