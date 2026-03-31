3a6d93d6f2c922cd17233b4a5ae33db749b5828b
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-03-31 14:53:36 +0100

    New translations en_us.lang.php (Indonesian)

--- translations/id_ID/modules/Trackers/language/id_ID.lang.php
+++ translations/id_ID/modules/Trackers/language/id_ID.lang.php
@@ -44,12 +44,12 @@ if (!defined('sugarEntry') || !sugarEntry) {
 
 $mod_strings = array(
     //Column header mapping
-    'action' => 'Tindakan',
-    'date_modified' => 'Tanggal Terakhir Aktivitas',
+    'action' => 'Action',
+    'date_modified' => 'Date of Last Action',
     'item_id' => 'ID',
-    'item_summary' => 'Nama',
-    'module_name' => 'Nama Modul',
-    'users' => 'Pengguna',
+    'item_summary' => 'Nama ',
+    'module_name' => 'Module Name',
+    'users' => 'Users',
 
     //Administration related labels
     'LBL_MODULE_NAME' => 'Pemantau',
