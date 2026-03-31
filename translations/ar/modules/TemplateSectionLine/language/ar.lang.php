9d9460514e041b681018757ac69f195633abc87f
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-03-31 11:53:41 +0100

    New translations en_us.lang.php (Arabic)

--- translations/ar/modules/TemplateSectionLine/language/ar.lang.php
+++ translations/ar/modules/TemplateSectionLine/language/ar.lang.php
@@ -43,12 +43,12 @@ if (!defined('sugarEntry') || !sugarEntry) {
 }
 
 $mod_strings = array(
-    'LBL_DESCRIPTION' => 'Description',
-    'LBL_NAME' => 'Name',
-    'LBL_LIST_NAME' => 'Name',
-    'LBL_EDIT_BUTTON' => 'Edit',
+    'LBL_DESCRIPTION' => 'الوصف',
+    'LBL_NAME' => 'الاسم',
+    'LBL_LIST_NAME' => 'الاسم',
+    'LBL_EDIT_BUTTON' => 'حرر',
     'LBL_REMOVE' => 'Remove',
-    'LBL_GRP' => 'Group',
-    'LBL_ORD' => 'Order',
+    'LBL_GRP' => 'المجموعة',
+    'LBL_ORD' => 'ترتيب',
     'LBL_THUMBNAIL' => 'صورة مصغرة',
 );
