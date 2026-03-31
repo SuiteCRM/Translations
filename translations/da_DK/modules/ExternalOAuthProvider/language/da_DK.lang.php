e27ea606750b924a986cdba64ffdc6298edfa68b
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-03-31 12:02:28 +0100

    New translations en_us.lang.php (Danish)

--- translations/da_DK/modules/ExternalOAuthProvider/language/da_DK.lang.php
+++ translations/da_DK/modules/ExternalOAuthProvider/language/da_DK.lang.php
@@ -97,7 +97,7 @@ $mod_strings = [
 
     'LBL_EXTRA' => 'Extra configurations',
     'LBL_MAPPING' => 'Mapping configurations',
-    'LBL_OTHER' => 'Other',
+    'LBL_OTHER' => 'Andre',
 
 
     'LNK_LIST_CREATE_NEW_PERSONAL' => 'New Personal OAuth Provider',
