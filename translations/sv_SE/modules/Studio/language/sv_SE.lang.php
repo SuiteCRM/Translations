894c1f274b949afb31b067c6bfb027fdfe72f188
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-03-31 14:29:48 +0100

    New translations en_us.lang.php (Swedish)

--- translations/sv_SE/modules/Studio/language/sv_SE.lang.php
+++ translations/sv_SE/modules/Studio/language/sv_SE.lang.php
@@ -45,28 +45,28 @@ if (!defined('sugarEntry') || !sugarEntry) {
 $mod_strings = array(
     'LBL_EDIT_LAYOUT' => 'Redigera layout',
     'LBL_EDIT_FIELDS' => 'Redigera anpassade fält',
-    'LBL_SELECT_FILE' => 'Välj Fil',
+    'LBL_SELECT_FILE' => 'Select File',
     'LBL_MODULE_TITLE' => 'Studio',
-    'LBL_TOOLBOX' => 'Verktygsbox',
+    'LBL_TOOLBOX' => 'Toolbox',
     'LBL_SUITE_FIELDS_STAGE' => 'SuiteCRM Fields (click items to add to staging area)',
     'LBL_VIEW_SUITE_FIELDS' => 'Visa SuiteCRM-fält',
-    'LBL_FAILED_TO_SAVE' => 'Misslyckades med att spara',
+    'LBL_FAILED_TO_SAVE' => 'Failed To Save',
     'LBL_CONFIRM_UNSAVE' => 'Förändringar kommer inte sparas. Är du säker på att du vill fortsätta?',
-    'LBL_PUBLISHING' => 'Publisera ...',
-    'LBL_PUBLISHED' => 'Publiserad',
-    'LBL_FAILED_PUBLISHED' => 'Misslyckades med Publisering',
-    'LBL_DROP_HERE' => '[Släpp Här]',
+    'LBL_PUBLISHING' => 'Publishing...',
+    'LBL_PUBLISHED' => 'Published',
+    'LBL_FAILED_PUBLISHED' => 'Failed to Publish',
+    'LBL_DROP_HERE' => '[Drop Here]',
 
 //CUSTOM FIELDS
     'LBL_NAME' => 'Namn',
-    'LBL_LABEL' => 'Etikett',
-    'LBL_MASS_UPDATE' => 'Mass Uppdatering',
-    'LBL_DEFAULT_VALUE' => 'Standardvärde',
-    'LBL_REQUIRED' => 'Obligatorisk',
-    'LBL_DATA_TYPE' => 'Typ',
+    'LBL_LABEL' => 'Label',
+    'LBL_MASS_UPDATE' => 'Mass Update',
+    'LBL_DEFAULT_VALUE' => 'Default Value',
+    'LBL_REQUIRED' => 'Required',
+    'LBL_DATA_TYPE' => 'Type',
 
 
-    'LBL_HISTORY' => 'Historia',
+    'LBL_HISTORY' => 'History',
 
 //WIZARDS
 
@@ -79,67 +79,67 @@ $mod_strings = array(
     'LBL_SW_EDIT_GROUPTABS' => 'Konfigurera Flikgrupperingar',
     'LBL_SW_EDIT_PORTAL' => 'Ändra Portal',
     'LBL_SW_REPAIR_CUSTOMFIELDS' => 'Reparera Egna Fält',
-    'LBL_SW_MIGRATE_CUSTOMFIELDS' => 'Överför egna fält',
+    'LBL_SW_MIGRATE_CUSTOMFIELDS' => 'Migrate Custom Fields',
 
 // JS LABELS
-    'LBL_REBUILD_JAVASCRIPT_LANG_DESC_SHORT' => 'Bygg om javaskriptversioner av språkfiler',
-    'LBL_REBUILD_JAVASCRIPT_LANG_DESC' => 'Tar bort javaskript versioner på språkfiler, kommer att bygga om när det behövs.',
+    'LBL_REBUILD_JAVASCRIPT_LANG_DESC_SHORT' => 'Rebuilds javascript versions of language files',
+    'LBL_REBUILD_JAVASCRIPT_LANG_DESC' => 'Removing javascript versions of language files, will rebuild when needed.',
 
 
 //Manager Backups History
-    'LBL_MB_DELETE' => 'Radera',
+    'LBL_MB_DELETE' => 'Delete',
 
 //EDIT DROP DOWNS
     'LBL_ED_CREATE_DROPDOWN' => 'Skapa en Rullgardins meny',
     'LBL_DROPDOWN_NAME' => 'Rullgardins Namn:',
     'LBL_DROPDOWN_LANGUAGE' => 'Rullgardins Språk:',
-    'LBL_TABGROUP_LANGUAGE' => 'Språk:',
+    'LBL_TABGROUP_LANGUAGE' => 'Language:',
 
 //END WIZARDS
 
 //DROP DOWN EDITOR
     'LBL_DD_DISPALYVALUE' => 'Visa Värden',
     'LBL_DD_DATABASEVALUE' => 'Database Värden',
-    'LBL_DD_ALL' => 'Alla',
+    'LBL_DD_ALL' => 'All',
 
 //BUTTONS
-    'LBL_BTN_SAVE' => 'Spara',
-    'LBL_BTN_CANCEL' => 'Ångra',
-    'LBL_BTN_SAVEPUBLISH' => 'Spara & Publisera',
-    'LBL_BTN_HISTORY' => 'Historia',
-    'LBL_BTN_ADDROWS' => 'Lägg till Rader',
-    'LBL_BTN_UNDO' => 'Ångra',
-    'LBL_BTN_REDO' => 'Göra om Ångra',
-    'LBL_BTN_ADDCUSTOMFIELD' => 'Lägg till Egna Fält',
+    'LBL_BTN_SAVE' => 'Save',
+    'LBL_BTN_CANCEL' => 'Cancel',
+    'LBL_BTN_SAVEPUBLISH' => 'Save & Deploy',
+    'LBL_BTN_HISTORY' => 'History',
+    'LBL_BTN_ADDROWS' => 'Add Rows',
+    'LBL_BTN_UNDO' => 'Undo',
+    'LBL_BTN_REDO' => 'Redo',
+    'LBL_BTN_ADDCUSTOMFIELD' => 'Add Custom Field',
     'LBL_BTN_TABINDEX' => 'Ändra Flik Edit Tabbing Ordning',
 
 //TABS
     'LBL_MODULES' => 'Moduler',
-    'LBL_MODULE_NAME' => 'Administrering',
-    'LBL_CONFIGURE_GROUP_TABS' => 'Konfigurera gruppflikar',
+    'LBL_MODULE_NAME' => 'Administration',
+    'LBL_CONFIGURE_GROUP_TABS' => 'Configure Module Menu Filters',
     'LBL_GROUP_TAB_WELCOME' => 'Gruppfliks layouten nedan kommer att användas när en användare väljer att använda gruppflikar istället för vanliga modulflikar i Mitt Konto>Layout Val.',
     'LBL_RENAME_TAB_WELCOME' => 'Klicka på en flik Visa Värde i tabellen nedan för att döpa om fliken.',
     'LBL_DELETE_MODULE' => 'Ta bort&nbsp;modul<br />från&nbsp;filter',
     'LBL_TAB_GROUP_LANGUAGE_HELP' => 'Välj ett tillgängligt språk, ändra grupp labels och klicka på "Spara & distribuera" för att lägga till labels i valt språk.',
     'LBL_ADD_GROUP' => 'Lägg till filter',
     'LBL_NEW_GROUP' => 'Ny Grupp',
-    'LBL_RENAME_TABS' => 'Döp om flikar',
+    'LBL_RENAME_TABS' => 'Rename Modules',
 
 //ERRORS
-    'ERROR_INVALID_KEY_VALUE' => "Error: Ogiltigt nyckel värde: [&#39;]",
+    'ERROR_INVALID_KEY_VALUE' => "Error: Invalid Key Value: [']",
 
 //SUGAR PORTAL
-    'LBL_SAVE' => 'Spara' /*for 508 compliance fix*/,
-    'LBL_UNDO' => 'Ångra' /*for 508 compliance fix*/,
-    'LBL_REDO' => 'Göra om Ångra' /*for 508 compliance fix*/,
+    'LBL_SAVE' => 'Save' /*for 508 compliance fix*/,
+    'LBL_UNDO' => 'Undo' /*for 508 compliance fix*/,
+    'LBL_REDO' => 'Redo' /*for 508 compliance fix*/,
     'LBL_INLINE' => 'Inline' /*for 508 compliance fix*/,
-    'LBL_DELETE' => 'Radera' /*for 508 compliance fix*/,
-    'LBL_ADD_FIELD' => 'Lägg till Fält' /*for 508 compliance fix*/,
+    'LBL_DELETE' => 'Delete' /*for 508 compliance fix*/,
+    'LBL_ADD_FIELD' => 'Add Field' /*for 508 compliance fix*/,
     'LBL_MAXIMIZE' => 'Maximera' /*for 508 compliance fix*/,
     'LBL_MINIMIZE' => 'Minimera' /*for 508 compliance fix*/,
-    'LBL_PUBLISH' => 'publicera' /*for 508 compliance fix*/,
-    'LBL_ADDROWS' => 'Lägg till Rader' /*for 508 compliance fix*/,
-    'LBL_ADDFIELD' => 'Lägg till Fält' /*for 508 compliance fix*/,
+    'LBL_PUBLISH' => 'Publish' /*for 508 compliance fix*/,
+    'LBL_ADDROWS' => 'Add Rows' /*for 508 compliance fix*/,
+    'LBL_ADDFIELD' => 'Add Field' /*for 508 compliance fix*/,
     'LBL_EDIT' => 'Redigera' /*for 508 compliance fix*/,
 
     'LBL_LANGUAGE_TOOLTIP' => 'Välj språket du vill redigera.',
