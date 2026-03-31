d011a29402eadc732de84fa1b1082bd7bd9fd8fe
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-03-31 11:43:20 +0100

    New translations en_us.lang.php (Romanian)

--- translations/ro_RO/modules/TemplateSectionLine/language/ro_RO.lang.php
+++ translations/ro_RO/modules/TemplateSectionLine/language/ro_RO.lang.php
@@ -43,12 +43,12 @@ if (!defined('sugarEntry') || !sugarEntry) {
 }
 
 $mod_strings = array(
-    'LBL_DESCRIPTION' => 'Description',
-    'LBL_NAME' => 'Name',
-    'LBL_LIST_NAME' => 'Name',
-    'LBL_EDIT_BUTTON' => 'Edit',
+    'LBL_DESCRIPTION' => 'Descriere: ',
+    'LBL_NAME' => 'Nume: ',
+    'LBL_LIST_NAME' => 'Prenume',
+    'LBL_EDIT_BUTTON' => 'Editați',
     'LBL_REMOVE' => 'Remove',
-    'LBL_GRP' => 'Group',
-    'LBL_ORD' => 'Order',
+    'LBL_GRP' => 'Grup',
+    'LBL_ORD' => 'Comandă',
     'LBL_THUMBNAIL' => 'Miniatură',
 );
