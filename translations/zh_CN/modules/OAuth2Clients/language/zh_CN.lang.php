3e0001e6044dea6d8ff551f832ef59962330c4c3
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-03-31 14:38:34 +0100

    New translations en_us.lang.php (Chinese Simplified)

--- translations/zh_CN/modules/OAuth2Clients/language/zh_CN.lang.php
+++ translations/zh_CN/modules/OAuth2Clients/language/zh_CN.lang.php
@@ -64,7 +64,7 @@ $mod_strings = [
     'LBL_ACCESS_TOKEN_EXPIRES' => '访问令牌过期',
     'LBL_REFRESH_TOKEN_EXPIRES' => '刷新令牌过期',
 
-    'LNK_OAUTH2_TOKEN_LIST'=> '列出 oauth2 令牌',
+    'LNK_OAUTH2_TOKEN_LIST'=> '列出OAuth2令牌',
     'LNK_OAUTH2_CLIENT_LIST' => '列出Oauth2 客户端',
 
     'LNK_NEW_OAUTH2_CLIENT' => '创建OAuth2客户端',
