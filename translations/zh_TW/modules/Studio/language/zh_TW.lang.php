f9c581a3e0beb24b94a6e82250513d96d491e1a6
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-03-31 14:41:49 +0100

    New translations en_us.lang.php (Chinese Traditional)

--- translations/zh_TW/modules/Studio/language/zh_TW.lang.php
+++ translations/zh_TW/modules/Studio/language/zh_TW.lang.php
@@ -43,30 +43,30 @@ if (!defined('sugarEntry') || !sugarEntry) {
 }
 
 $mod_strings = array(
-    'LBL_EDIT_LAYOUT' => '編輯佈局',
+    'LBL_EDIT_LAYOUT' => 'Edit Layout',
     'LBL_EDIT_FIELDS' => '編輯自定義欄位',
     'LBL_SELECT_FILE' => '選擇文件',
-    'LBL_MODULE_TITLE' => '工作室',
-    'LBL_TOOLBOX' => '工具箱',
+    'LBL_MODULE_TITLE' => 'Studio',
+    'LBL_TOOLBOX' => 'Toolbox',
     'LBL_SUITE_FIELDS_STAGE' => 'SuiteCRM 欄位 (按一下項目將添加到臨時區域)',
     'LBL_VIEW_SUITE_FIELDS' => '查看 SuiteCRM 欄位',
-    'LBL_FAILED_TO_SAVE' => '保存失敗',
+    'LBL_FAILED_TO_SAVE' => 'Failed To Save',
     'LBL_CONFIRM_UNSAVE' => '任何改變將轉為未存狀態. 您確定您要繼續嗎?',
-    'LBL_PUBLISHING' => '公佈中...',
+    'LBL_PUBLISHING' => 'Publishing...',
     'LBL_PUBLISHED' => '已公佈',
-    'LBL_FAILED_PUBLISHED' => '公佈失敗',
-    'LBL_DROP_HERE' => '[拖到這裡]',
+    'LBL_FAILED_PUBLISHED' => '發佈失敗',
+    'LBL_DROP_HERE' => '[拖放到這裡]',
 
 //CUSTOM FIELDS
     'LBL_NAME' => '名稱',
-    'LBL_LABEL' => '標簽',
-    'LBL_MASS_UPDATE' => '批量更新',
-    'LBL_DEFAULT_VALUE' => '默認值',
-    'LBL_REQUIRED' => '必須',
+    'LBL_LABEL' => 'Label',
+    'LBL_MASS_UPDATE' => '主體更新',
+    'LBL_DEFAULT_VALUE' => 'Default Value',
+    'LBL_REQUIRED' => '需求',
     'LBL_DATA_TYPE' => '類型:',
 
 
-    'LBL_HISTORY' => '歷史',
+    'LBL_HISTORY' => '歷史記錄',
 
 //WIZARDS
 
@@ -79,15 +79,15 @@ $mod_strings = array(
     'LBL_SW_EDIT_GROUPTABS' => '設置頁籤群組',
     'LBL_SW_EDIT_PORTAL' => '編輯門戶網站',
     'LBL_SW_REPAIR_CUSTOMFIELDS' => '修復自訂欄位',
-    'LBL_SW_MIGRATE_CUSTOMFIELDS' => '移植自定義欄位',
+    'LBL_SW_MIGRATE_CUSTOMFIELDS' => 'Migrate Custom Fields',
 
 // JS LABELS
-    'LBL_REBUILD_JAVASCRIPT_LANG_DESC_SHORT' => '重建語言文件的Javascripts版本',
-    'LBL_REBUILD_JAVASCRIPT_LANG_DESC' => '移除語言文件的Javascripts版本，它會在需要的時候重建。',
+    'LBL_REBUILD_JAVASCRIPT_LANG_DESC_SHORT' => 'Rebuilds javascript versions of language files',
+    'LBL_REBUILD_JAVASCRIPT_LANG_DESC' => 'Removing javascript versions of language files, will rebuild when needed.',
 
 
 //Manager Backups History
-    'LBL_MB_DELETE' => '刪除',
+    'LBL_MB_DELETE' => 'Delete',
 
 //EDIT DROP DOWNS
     'LBL_ED_CREATE_DROPDOWN' => '新增下拉框',
@@ -110,37 +110,37 @@ $mod_strings = array(
     'LBL_BTN_ADDROWS' => '增加行',
     'LBL_BTN_UNDO' => '撤銷（Undo）',
     'LBL_BTN_REDO' => '重覆（Redo）',
-    'LBL_BTN_ADDCUSTOMFIELD' => '增加自定義欄位',
+    'LBL_BTN_ADDCUSTOMFIELD' => '添加客戶欄位',
     'LBL_BTN_TABINDEX' => '編輯頁籤順序',
 
 //TABS
-    'LBL_MODULES' => '模組',
+    'LBL_MODULES' => '模塊',
     'LBL_MODULE_NAME' => '系統管理',
-    'LBL_CONFIGURE_GROUP_TABS' => '設置模組功能表過濾',
+    'LBL_CONFIGURE_GROUP_TABS' => '設置組標簽',
     'LBL_GROUP_TAB_WELCOME' => '以下過濾器將顯示在使用者查看模組功能表時的更多清單裡面。可在過濾器間拖放模組。注意: 空的過濾器將不會被顯示在功能表裡面。',
     'LBL_RENAME_TAB_WELCOME' => '點擊任何標簽在表下方顯示值更改標簽名稱',
     'LBL_DELETE_MODULE' => '移除&nbsp;模組<br />從&nbsp;過濾條件',
     'LBL_TAB_GROUP_LANGUAGE_HELP' => '為其他語言設置標籤群組的標題。選擇一個語言，編輯標題，按一下儲存並佈署。',
     'LBL_ADD_GROUP' => '添加組',
     'LBL_NEW_GROUP' => '新建組',
-    'LBL_RENAME_TABS' => '重命名模組',
+    'LBL_RENAME_TABS' => 'Rename Modules',
 
 //ERRORS
-    'ERROR_INVALID_KEY_VALUE' => "錯誤: 無效的關鍵字值: [']",
+    'ERROR_INVALID_KEY_VALUE' => "錯誤: 無效的關鍵值: [&#39;]",
 
 //SUGAR PORTAL
     'LBL_SAVE' => '儲存' /*for 508 compliance fix*/,
-    'LBL_UNDO' => '撤銷（Undo）' /*for 508 compliance fix*/,
-    'LBL_REDO' => '重覆（Redo）' /*for 508 compliance fix*/,
+    'LBL_UNDO' => '取消' /*for 508 compliance fix*/,
+    'LBL_REDO' => '重做' /*for 508 compliance fix*/,
     'LBL_INLINE' => '內聯' /*for 508 compliance fix*/,
-    'LBL_DELETE' => '刪除' /*for 508 compliance fix*/,
+    'LBL_DELETE' => 'Delete' /*for 508 compliance fix*/,
     'LBL_ADD_FIELD' => '添加欄位' /*for 508 compliance fix*/,
     'LBL_MAXIMIZE' => '最大化' /*for 508 compliance fix*/,
     'LBL_MINIMIZE' => '最小化' /*for 508 compliance fix*/,
-    'LBL_PUBLISH' => '發佈' /*for 508 compliance fix*/,
+    'LBL_PUBLISH' => 'Publish' /*for 508 compliance fix*/,
     'LBL_ADDROWS' => '增加行' /*for 508 compliance fix*/,
     'LBL_ADDFIELD' => '添加欄位' /*for 508 compliance fix*/,
-    'LBL_EDIT' => '編輯' /*for 508 compliance fix*/,
+    'LBL_EDIT' => 'Edit' /*for 508 compliance fix*/,
 
     'LBL_LANGUAGE_TOOLTIP' => '選擇要編輯的語言。',
     'LBL_SINGULAR' => '單數標簽',
