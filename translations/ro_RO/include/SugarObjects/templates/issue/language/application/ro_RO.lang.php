0ed4c473bf38a4215b576e6fac9862d9c0cbd32c
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-03-31 11:40:58 +0100

    New translations en_us.lang.php (Romanian)

--- translations/ro_RO/include/SugarObjects/templates/issue/language/application/ro_RO.lang.php
+++ translations/ro_RO/include/SugarObjects/templates/issue/language/application/ro_RO.lang.php
@@ -66,8 +66,8 @@ $app_list_strings = array(
     $object_name . '_resolution_dom' => array(
         '' => '',
         'Accepted' => 'Acceptat',
-        'Duplicate' => 'Duplicate',
-        'Closed' => 'Closed',
+        'Duplicate' => 'Duplicare',
+        'Closed' => 'Închis',
         'Out of Date' => 'Depăşit',
         'Invalid' => 'Invalid',
     ),
