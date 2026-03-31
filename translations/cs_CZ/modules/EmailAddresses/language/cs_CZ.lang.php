3853efcb852d616f14ca0e417ab376dfed12a1af
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-03-31 11:59:32 +0100

    New translations en_us.lang.php (Czech)

--- translations/cs_CZ/modules/EmailAddresses/language/cs_CZ.lang.php
+++ translations/cs_CZ/modules/EmailAddresses/language/cs_CZ.lang.php
@@ -44,12 +44,12 @@ if (!defined('sugarEntry') || !sugarEntry) {
 
 $mod_strings = array(
     'LBL_EMAIL_ADDRESS_ID' => 'ID',
-    'LBL_EMAIL_ADDRESS' => 'Email Address',
+    'LBL_EMAIL_ADDRESS' => 'Emailová adresa',
     'LBL_EMAIL_ADDRESS_CAPS' => 'Emailová adresa velkými písmeny',
     'LBL_INVALID_EMAIL' => 'Invalid Email',
     'LBL_OPT_OUT' => 'Opted Out',
     'LBL_CONFIRM_OPT_IN' => 'Potvrdit Opt In',
     'LBL_DATE_CREATE' => 'Datum vytvoření',
-    'LBL_DATE_MODIFIED' => 'Date Modified',
-    'LBL_DELETED' => 'Delete',
+    'LBL_DATE_MODIFIED' => 'Datum změny',
+    'LBL_DELETED' => 'Smazat',
 );
