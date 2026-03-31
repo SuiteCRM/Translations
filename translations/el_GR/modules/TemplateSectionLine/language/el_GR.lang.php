70442737257d94438b8c61152832c4487d6bd058
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-03-31 12:08:37 +0100

    New translations en_us.lang.php (Greek)

--- translations/el_GR/modules/TemplateSectionLine/language/el_GR.lang.php
+++ translations/el_GR/modules/TemplateSectionLine/language/el_GR.lang.php
@@ -43,12 +43,12 @@ if (!defined('sugarEntry') || !sugarEntry) {
 }
 
 $mod_strings = array(
-    'LBL_DESCRIPTION' => 'Description',
-    'LBL_NAME' => 'Name',
-    'LBL_LIST_NAME' => 'Name',
-    'LBL_EDIT_BUTTON' => 'Edit',
+    'LBL_DESCRIPTION' => 'Περιγραφή',
+    'LBL_NAME' => 'Όνομα',
+    'LBL_LIST_NAME' => 'Όνομα',
+    'LBL_EDIT_BUTTON' => 'Επεξεργασία',
     'LBL_REMOVE' => 'Remove',
-    'LBL_GRP' => 'Group',
-    'LBL_ORD' => 'Order',
+    'LBL_GRP' => 'Γκρουπ',
+    'LBL_ORD' => 'Σειρά',
     'LBL_THUMBNAIL' => 'Thumbnail',
 );
