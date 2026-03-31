d86728cb4a6c7549291b251c3426433ed747e261
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-03-31 12:10:00 +0100

    New translations en_us.lang.php (Basque)

--- translations/eu_ES/modules/EAPM/language/eu_ES.lang.php
+++ translations/eu_ES/modules/EAPM/language/eu_ES.lang.php
@@ -43,14 +43,14 @@ if (!defined('sugarEntry') || !sugarEntry) {
 }
 
 $mod_strings = array(
-    'LBL_ASSIGNED_TO_ID' => 'Assigned User Id',
+    'LBL_ASSIGNED_TO_ID' => 'Esleitutako erabiltzailearen Id',
     'LBL_ASSIGNED_TO_NAME' => 'SuiteCRM User',
     'LBL_ID' => 'ID',
     'LBL_DATE_ENTERED' => 'Date Created',
     'LBL_DATE_MODIFIED' => 'Date Modified',
     'LBL_MODIFIED' => 'Modified By',
-    'LBL_MODIFIED_NAME' => 'Modified By Name',
-    'LBL_CREATED' => 'Created By',
+    'LBL_MODIFIED_NAME' => 'Nork aldatua',
+    'LBL_CREATED' => 'Sortzailea',
     'LBL_DESCRIPTION' => 'Description',
     'LBL_DELETED' => 'Deleted',
     'LBL_NAME' => 'App User Name',
@@ -64,7 +64,7 @@ $mod_strings = array(
     'LNK_NEW_RECORD' => 'Create External Account',
     'LNK_LIST' => 'View External Accounts',
     'LBL_SEARCH_FORM_TITLE' => 'Search External Source',
-    'LBL_HISTORY_SUBPANEL_TITLE' => 'View History',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Historia ikusi',
     'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Activities',
     'LBL_NEW_FORM_TITLE' => 'New External Account',
     'LBL_PASSWORD' => 'App Password',
