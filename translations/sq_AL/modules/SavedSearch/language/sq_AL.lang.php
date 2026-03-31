4313e176257a40fe01fc2f4ddd22b2fd6c1fe10e
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-03-31 14:26:44 +0100

    New translations en_us.lang.php (Albanian)

--- translations/sq_AL/modules/SavedSearch/language/sq_AL.lang.php
+++ translations/sq_AL/modules/SavedSearch/language/sq_AL.lang.php
@@ -49,16 +49,16 @@ $mod_strings = array(
     'LBL_DELETE_CONFIRM' => 'Are you sure you want to delete the selected Saved Filter?',
     'LBL_DELETE_BUTTON_TITLE' => 'Delete this Saved Filter',
     'LBL_SAVE_BUTTON_TITLE' => 'Save the current filter',
-    'LBL_LIST_NAME' => 'Emri',
-    'LBL_LIST_MODULE' => 'Modul',
+    'LBL_LIST_NAME' => 'Name',
+    'LBL_LIST_MODULE' => 'Module',
     'LBL_ORDER_BY_COLUMNS' => 'Order by column',
-    'LBL_DIRECTION' => 'Drejtimi',
+    'LBL_DIRECTION' => 'Direction',
     'LBL_SAVE_SEARCH_AS' => 'Save filter as:',
-    'LBL_ASCENDING' => 'Ngritje',
-    'LBL_DESCENDING' => 'Zbritje',
+    'LBL_ASCENDING' => 'Ascending',
+    'LBL_DESCENDING' => 'Descending',
     'LBL_MODIFY_CURRENT_FILTER' => 'Modify current filter',
 
-    'LBL_CREATED_BY' => 'Krijuar nga',
+    'LBL_CREATED_BY' => 'Created By',
     'LBL_QUICK_FILTER' => 'Quick Filter',
 
 
