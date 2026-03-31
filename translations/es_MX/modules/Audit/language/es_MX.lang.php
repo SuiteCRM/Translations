4465fe41ba0322e9c57f7816cb34a9abba160a1c
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-03-31 14:57:30 +0100

    New translations en_us.lang.php (Spanish, Mexico)

--- translations/es_MX/modules/Audit/language/es_MX.lang.php
+++ translations/es_MX/modules/Audit/language/es_MX.lang.php
@@ -43,11 +43,11 @@ if (!defined('sugarEntry') || !sugarEntry) {
 }
 
 $mod_strings = array(
-    'LBL_FIELD_NAME' => 'Campo',
+    'LBL_FIELD_NAME' => 'Field',
     'LBL_OLD_NAME' => 'Valor anterior',
     'LBL_NEW_VALUE' => 'Valor nuevo',
     'LBL_CREATED_BY' => 'Cambiado por',
     'LBL_LIST_DATE' => 'Fecha de cambio',
     'LBL_AUDITED_FIELDS' => 'Campos auditados en este módulo:',
-    'LBL_CHANGE_LOG' => 'Registro de cambios',
+    'LBL_CHANGE_LOG' => 'Change Log',
 );
