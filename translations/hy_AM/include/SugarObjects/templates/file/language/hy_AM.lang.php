a606a8d0a1c657de14fd06e38927d9040133fc1b
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-03-31 12:18:14 +0100

    New translations en_us.lang.php (Armenian)

--- translations/hy_AM/include/SugarObjects/templates/file/language/hy_AM.lang.php
+++ translations/hy_AM/include/SugarObjects/templates/file/language/hy_AM.lang.php
@@ -51,8 +51,8 @@ $mod_strings = array(
     'LBL_SEARCH_FORM_TITLE' => 'Փաստաթղթի որոնում',
     //vardef labels
     'LBL_NAME' => 'Փաստաթղթի անվանում',
-    'LBL_DESCRIPTION' => 'Նկարագրություն',
-    'LBL_ASSIGNED_TO' => 'Կատարող',
+    'LBL_DESCRIPTION' => 'Description',
+    'LBL_ASSIGNED_TO' => 'Assigned to:',
     'LBL_CATEGORY' => 'Կատեգորիա',
     'LBL_SUBCATEGORY' => 'Ենթակատեգորիա',
     'LBL_STATUS' => 'Կարգավիճակ',
@@ -80,38 +80,38 @@ $mod_strings = array(
     'LBL_SUBCATEGORY_VALUE' => 'Ենթակատեգորիա:',
     'LBL_DOC_STATUS' => 'Կարգավիճակ',
     'LBL_DET_TEMPLATE_TYPE' => 'Փաստաթղթի տեսակ:',
-    'LBL_DOC_DESCRIPTION' => 'Նկարագրություն.',
+    'LBL_DOC_DESCRIPTION' => 'Description:',
     'LBL_DOC_ACTIVE_DATE' => 'Հրապարակման ամսաթիվը',
     'LBL_DOC_EXP_DATE' => 'Վերջնաժամկետի օրը․',
 
     //document list view.
     'LBL_LIST_FORM_TITLE' => 'Փաստաթղթերի ցուցակ',
     'LBL_LIST_DOCUMENT' => 'Փաստաթուղթ',
-    'LBL_LIST_CATEGORY' => 'Կատեգորիա',
-    'LBL_LIST_SUBCATEGORY' => 'Ենթակատեգորիա',
-    'LBL_LIST_REVISION' => 'Տարբերակ',
+    'LBL_LIST_CATEGORY' => 'Category',
+    'LBL_LIST_SUBCATEGORY' => 'Sub Category',
+    'LBL_LIST_REVISION' => 'Revision',
     'LBL_LIST_LAST_REV_CREATOR' => 'Հրատարակված է',
     'LBL_LIST_LAST_REV_DATE' => 'Տարբերակի ամսաթիվը',
     'LBL_LIST_VIEW_DOCUMENT' => 'Դիտում',
-    'LBL_LIST_ACTIVE_DATE' => 'Հրապարակման ամսաթիվը',
-    'LBL_LIST_EXP_DATE' => 'Վերջնաժամկետի օրը',
-    'LBL_LIST_STATUS' => 'Կարգավիճակ',
+    'LBL_LIST_ACTIVE_DATE' => 'Publish Date',
+    'LBL_LIST_EXP_DATE' => 'Expiration Date',
+    'LBL_LIST_STATUS' => 'Status',
 
     //document search form.
-    'LBL_SF_CATEGORY' => 'Կատեգորիա:',
-    'LBL_SF_SUBCATEGORY' => 'Ենթակատեգորիա:',
+    'LBL_SF_CATEGORY' => 'Category:',
+    'LBL_SF_SUBCATEGORY' => 'Sub Category:',
 
     'DEF_CREATE_LOG' => 'Փաստաթուղթը ստեղծված է',
 
     //error messages
-    'ERR_DOC_NAME' => 'Փաստաթղթի անվանում',
-    'ERR_DOC_ACTIVE_DATE' => 'Հրապարակման ամսաթիվը',
+    'ERR_DOC_NAME' => 'Document Name',
+    'ERR_DOC_ACTIVE_DATE' => 'Publish Date',
     'ERR_FILENAME' => 'Ֆայլի անվանումը',
 
     //sub-panel vardefs.
-    'LBL_LIST_DOCUMENT_NAME' => 'Փաստաթղթի անվանում',
+    'LBL_LIST_DOCUMENT_NAME' => 'Document Name',
 
     'LBL_EDIT_BUTTON' => 'Խմբագրել ',
-    'LBL_REMOVE' => 'Հեռացնել',
+    'LBL_REMOVE' => 'Remove',
 
 );
