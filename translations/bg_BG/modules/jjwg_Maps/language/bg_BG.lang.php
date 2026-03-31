8256a4d453cf97600e1eb2d53aa80a68755fa7a2
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-03-31 11:55:59 +0100

    New translations en_us.lang.php (Bulgarian)

--- translations/bg_BG/modules/jjwg_Maps/language/bg_BG.lang.php
+++ translations/bg_BG/modules/jjwg_Maps/language/bg_BG.lang.php
@@ -82,7 +82,7 @@ $mod_strings['LBL_MODULE_HEADING'] = 'Module';
 
 $mod_strings['LBL_N/A'] = 'N/A';
 $mod_strings['LBL_ZERO_RESULTS'] = 'Zero Results';
-$mod_strings['LBL_OK'] = 'Ok';
+$mod_strings['LBL_OK'] = 'Ок';
 $mod_strings['LBL_INVALID_REQUEST'] = 'Invalid Request';
 $mod_strings['LBL_APPROXIMATE'] = 'Approximate';
 $mod_strings['LBL_EMPTY'] = 'Empty';
@@ -96,7 +96,7 @@ $mod_strings['LBL_EXPORT_ADDRESS_URL'] = 'Export URLs';
 $mod_strings['LBL_EXPORT_INSTRUCTIONS'] = 'Use the links below to export full addresses in need of geocodeing information. Then use an online or offline batch geocoding tool to geocode the addresses. When you are finished geocoding, import the addresses into the Address Cache module to be used with your maps. Note, the Address Cache module is optional. All geocoding information is stored in the representative module.';
 $mod_strings['LBL_ADDRESS_CACHE'] = 'Address Cache';
 $mod_strings['LBL_ADD_TO_TARGET_LIST'] = 'Add to Target List';
-$mod_strings['LBL_ADD_TO_TARGET_LIST_PROCESSING'] = 'Processing...';
+$mod_strings['LBL_ADD_TO_TARGET_LIST_PROCESSING'] = 'Обработва се...';
 
 
 $mod_strings['LBL_CONFIG_TITLE'] = 'Configuration Settings';
@@ -105,10 +105,10 @@ $mod_strings['LBL_BILLING_ADDRESS'] = 'Billing Address';
 $mod_strings['LBL_SHIPPING_ADDRESS'] = 'Shipping Address';
 $mod_strings['LBL_PRIMARY_ADDRESS'] = 'Primary Address';
 $mod_strings['LBL_ALTERNATIVE_ADDRESS'] = 'Alternative Address';
-$mod_strings['LBL_ADDRESS_FLEX_RELATE'] = 'Flex Relate';
+$mod_strings['LBL_ADDRESS_FLEX_RELATE'] = 'Свободно свъраване';
 $mod_strings['LBL_ADDRESS_ADDRESS'] = 'Address (Simple, Users)';
 $mod_strings['LBL_ADDRESS_CUSTOM'] = 'Custom (Custom Controller Logic)';
-$mod_strings['LBL_ENABLED'] = 'Enabled';
+$mod_strings['LBL_ENABLED'] = 'Активирано';
 $mod_strings['LBL_DISABLED'] = 'Disabled';
 $mod_strings['LBL_DEFAULT'] = 'Default:';
 $mod_strings['LBL_CONFIG_DEFAULT'] = 'Default:';
