8ec929c4d5f09a1bf19c94589dcddf099fa2d1f0
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-03-31 12:10:12 +0100

    New translations en_us.lang.php (Basque)

--- translations/eu_ES/modules/Favorites/language/eu_ES.lang.php
+++ translations/eu_ES/modules/Favorites/language/eu_ES.lang.php
@@ -43,13 +43,13 @@ if (!defined('sugarEntry') || !sugarEntry) {
 }
 
 $mod_strings = array(
-    'LBL_ASSIGNED_TO_ID' => 'Assigned User Id',
+    'LBL_ASSIGNED_TO_ID' => 'Esleitutako erabiltzailearen Id',
     'LBL_ASSIGNED_TO_NAME' => 'Assigned to',
     'LBL_ID' => 'ID',
     'LBL_DATE_ENTERED' => 'Date Created',
     'LBL_DATE_MODIFIED' => 'Date Modified',
     'LBL_MODIFIED' => 'Modified By',
-    'LBL_MODIFIED_NAME' => 'Modified By Name',
+    'LBL_MODIFIED_NAME' => 'Nork aldatua',
     'LBL_CREATED' => 'Created By',
     'LBL_DESCRIPTION' => 'Description',
     'LBL_DELETED' => 'Deleted',
@@ -66,7 +66,7 @@ $mod_strings = array(
     'LNK_NEW_RECORD' => 'Create Favorite',
     'LNK_LIST' => 'View Favorites',
     'LBL_SEARCH_FORM_TITLE' => 'Search Favorites',
-    'LBL_HISTORY_SUBPANEL_TITLE' => 'View History',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Historia ikusi',
     'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Activities',
     'LBL_NEW_FORM_TITLE' => 'New Favorite',
     'LBL_MARK_FAVORITE' => 'Mark as favorite',
