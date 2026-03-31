6aa7f18e878dc21c52c0c14c9eeace6230cb0fdc
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-03-31 14:17:32 +0100

    New translations en_us.lang.php (Russian)

--- translations/ru_RU/modules/SavedSearch/language/ru_RU.lang.php
+++ translations/ru_RU/modules/SavedSearch/language/ru_RU.lang.php
@@ -49,16 +49,16 @@ $mod_strings = array(
     'LBL_DELETE_CONFIRM' => 'Вы действительно хотите удалить выбранный фильтр?',
     'LBL_DELETE_BUTTON_TITLE' => 'Удалить эти условия фильтрации',
     'LBL_SAVE_BUTTON_TITLE' => 'Сохранить текущий фильтр',
-    'LBL_LIST_NAME' => 'Имя',
+    'LBL_LIST_NAME' => 'Название',
     'LBL_LIST_MODULE' => 'Модуль',
     'LBL_ORDER_BY_COLUMNS' => 'Сортировать по столбцу',
     'LBL_DIRECTION' => 'Направление',
     'LBL_SAVE_SEARCH_AS' => 'Сохранить фильтр как:',
     'LBL_ASCENDING' => 'Восходящая',
     'LBL_DESCENDING' => 'Нисходящая',
-    'LBL_MODIFY_CURRENT_FILTER' => 'Выбранный фильтр',
+    'LBL_MODIFY_CURRENT_FILTER' => 'Modify current filter',
 
-    'LBL_CREATED_BY' => 'Создано',
+    'LBL_CREATED_BY' => 'Кем создано',
     'LBL_QUICK_FILTER' => 'Быстрый фильтр',
 
 
