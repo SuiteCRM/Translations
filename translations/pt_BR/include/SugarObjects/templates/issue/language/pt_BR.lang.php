fd3f65d032edf7feb27c939e987e30ff07f07fe7
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-03-31 14:47:55 +0100

    New translations en_us.lang.php (Portuguese, Brazilian)

--- translations/pt_BR/include/SugarObjects/templates/issue/language/pt_BR.lang.php
+++ translations/pt_BR/include/SugarObjects/templates/issue/language/pt_BR.lang.php
@@ -44,7 +44,7 @@ if (!defined('sugarEntry') || !sugarEntry) {
 
 $mod_strings = array(
 
-    'LBL_NAME' => 'Nome',
+    'LBL_NAME' => 'Name',
     'LBL_NUMBER' => 'Número:',
     'LBL_STATUS' => 'Status:',
     'LBL_PRIORITY' => 'Prioridade:',
@@ -62,7 +62,7 @@ $mod_strings = array(
     'LBL_TYPE' => 'Tipo:',
     'LBL_SUBJECT' => 'Assunto:',
 
-    'LBL_EDIT_BUTTON' => 'Editar',
+    'LBL_EDIT_BUTTON' => 'Edit',
     'LBL_REMOVE' => 'Remover',
 
 );
