50e330b3439f95eb981de33c2b6ca5c8eaae97a6
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-03-31 12:09:09 +0100

    New translations en_us.lang.php (Basque)

--- translations/eu_ES/modules/AOK_KnowledgeBase/language/eu_ES.lang.php
+++ translations/eu_ES/modules/AOK_KnowledgeBase/language/eu_ES.lang.php
@@ -43,14 +43,14 @@ if (!defined('sugarEntry') || !sugarEntry) {
 }
 
 $mod_strings = array(
-    'LBL_ASSIGNED_TO_ID' => 'Assigned User Id',
+    'LBL_ASSIGNED_TO_ID' => 'Esleitutako erabiltzailearen Id',
     'LBL_ASSIGNED_TO_NAME' => 'Assigned to',
     'LBL_ID' => 'ID',
     'LBL_DATE_ENTERED' => 'Date Created',
     'LBL_DATE_MODIFIED' => 'Date Modified',
     'LBL_MODIFIED' => 'Modified By',
-    'LBL_MODIFIED_NAME' => 'Modified By Name',
-    'LBL_CREATED' => 'Created By',
+    'LBL_MODIFIED_NAME' => 'Nork aldatua',
+    'LBL_CREATED' => 'Sortzailea',
     'LBL_DESCRIPTION' => 'Body',
     'LBL_ADDITIONAL_INFO' => 'Resolution',
     'LBL_DELETED' => 'Deleted',
@@ -67,7 +67,7 @@ $mod_strings = array(
     'LNK_NEW_RECORD' => 'Create Knowledge Base',
     'LNK_LIST' => 'View Knowledge Base',
     'LBL_SEARCH_FORM_TITLE' => 'Search Knowledge Base',
-    'LBL_HISTORY_SUBPANEL_TITLE' => 'View History',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Historia ikusi',
     'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Activities',
     'LBL_NEW_FORM_TITLE' => 'New Knowledge Base',
     'LBL_STATUS' => 'Status',
