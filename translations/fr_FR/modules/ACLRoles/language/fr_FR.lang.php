e824387fac3d31c765e16b08a32cddb4caf7c6ce
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-03-31 11:43:49 +0100

    New translations en_us.lang.php (French)

--- translations/fr_FR/modules/ACLRoles/language/fr_FR.lang.php
+++ translations/fr_FR/modules/ACLRoles/language/fr_FR.lang.php
@@ -43,23 +43,23 @@ if (!defined('sugarEntry') || !sugarEntry) {
 }
 
 $mod_strings = array(
-    'LBL_MODULE_NAME' => 'Roles',
+    'LBL_MODULE_NAME' => 'Rôles',
     'LBL_MODULE_TITLE' => 'Rôles : Accueil',
     'LBL_ROLE' => 'Role',
-    'LBL_NAME' => 'Name',
+    'LBL_NAME' => 'Nom',
     'LBL_DESCRIPTION' => 'Description',
     'LIST_ROLES' => 'List Roles',
-    'LBL_USERS_SUBPANEL_TITLE' => 'Users',
+    'LBL_USERS_SUBPANEL_TITLE' => 'Utilisateurs',
     'LIST_ROLES_BY_USER' => 'List Roles By User',
-    'LBL_LIST_FORM_TITLE' => 'Roles',
+    'LBL_LIST_FORM_TITLE' => 'Rôles',
     'LBL_ROLES_SUBPANEL_TITLE' => 'User Roles',
     'LBL_SEARCH_FORM_TITLE' => 'Search',
     'LBL_CREATE_ROLE' => 'Créer un rôle',
     'LBL_EDIT_VIEW_DIRECTIONS' => 'Double-cliquez sur une cellule pour changer sa valeur.',
     'LBL_ACCESS_DEFAULT' => 'Not Set',
-    'LBL_ACTION_ADMIN' => 'Access Type',
-    'LBL_ALL' => 'All',
+    'LBL_ACTION_ADMIN' => 'Type d&#39;accès',
+    'LBL_ALL' => 'Tous',
     'LBL_DUPLICATE_OF' => 'Duplicata de ',
 
-    'LBL_SECURITYGROUPS' => 'Security Groups',
+    'LBL_SECURITYGROUPS' => 'Groupes de sécurité',
 );
