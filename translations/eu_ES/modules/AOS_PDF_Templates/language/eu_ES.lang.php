620ef7e21f7264982954ed83622c74224a03747d
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-03-31 12:09:23 +0100

    New translations en_us.lang.php (Basque)

--- translations/eu_ES/modules/AOS_PDF_Templates/language/eu_ES.lang.php
+++ translations/eu_ES/modules/AOS_PDF_Templates/language/eu_ES.lang.php
@@ -43,14 +43,14 @@ if (!defined('sugarEntry') || !sugarEntry) {
 }
 
 $mod_strings = array(
-    'LBL_ASSIGNED_TO_ID' => 'Assigned User Id',
+    'LBL_ASSIGNED_TO_ID' => 'Esleitutako erabiltzailearen Id',
     'LBL_ASSIGNED_TO_NAME' => 'Assigned to',
     'LBL_ID' => 'ID',
     'LBL_DATE_ENTERED' => 'Date Created',
     'LBL_DATE_MODIFIED' => 'Date Modified',
     'LBL_MODIFIED' => 'Modified By',
-    'LBL_MODIFIED_NAME' => 'Modified By Name',
-    'LBL_CREATED' => 'Created By',
+    'LBL_MODIFIED_NAME' => 'Nork aldatua',
+    'LBL_CREATED' => 'Sortzailea',
     'LBL_DESCRIPTION' => 'Body',
     'LBL_HEADER' => 'Header',
     'LBL_FOOTER' => 'Footer',
@@ -65,7 +65,7 @@ $mod_strings = array(
     'LNK_NEW_RECORD' => 'Create PDF Template',
     'LNK_LIST' => 'View PDF Templates',
     'LBL_SEARCH_FORM_TITLE' => 'Search PDF Templates',
-    'LBL_HISTORY_SUBPANEL_TITLE' => 'View History',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Historia ikusi',
     'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Activities',
     'LBL_NEW_FORM_TITLE' => 'New PDF Template',
     'LBL_TYPE' => 'Type',
