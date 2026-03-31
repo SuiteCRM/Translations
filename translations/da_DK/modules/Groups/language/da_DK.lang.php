364295f82c00f1b2e5e22cddd221563daac08f77
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-03-31 12:02:32 +0100

    New translations en_us.lang.php (Danish)

--- translations/da_DK/modules/Groups/language/da_DK.lang.php
+++ translations/da_DK/modules/Groups/language/da_DK.lang.php
@@ -45,7 +45,7 @@ if (!defined('sugarEntry') || !sugarEntry) {
 $mod_strings = array(
     'LBL_MODULE_NAME' => 'Groups',
     'LBL_GROUP_NAME' => 'Group Name:',
-    'LBL_DESCRIPTION' => 'Description:',
+    'LBL_DESCRIPTION' => 'Beskrivelse:',
     'LBL_TEAM' => 'Team:',
     // ListView
     'LBL_LIST_TITLE' => 'Groups',
