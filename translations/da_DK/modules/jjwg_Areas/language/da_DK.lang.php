476458fef53daa548077dafb66705fd2c5253f89
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-03-31 12:03:25 +0100

    New translations en_us.lang.php (Danish)

--- translations/da_DK/modules/jjwg_Areas/language/da_DK.lang.php
+++ translations/da_DK/modules/jjwg_Areas/language/da_DK.lang.php
@@ -43,7 +43,7 @@ if (!defined('sugarEntry') || !sugarEntry) {
 }
 
 $mod_strings['LBL_ASSIGNED_TO_ID'] = 'Assigned User Id';
-$mod_strings['LBL_ASSIGNED_TO_NAME'] = 'User';
+$mod_strings['LBL_ASSIGNED_TO_NAME'] = 'Bruger';
 $mod_strings['LBL_ID'] = 'ID';
 $mod_strings['LBL_DATE_ENTERED'] = 'Date Created';
 $mod_strings['LBL_DATE_MODIFIED'] = 'Date Modified';
@@ -70,7 +70,7 @@ $mod_strings['LBL_CITY'] = 'City';
 $mod_strings['LBL_STATE'] = 'State';
 $mod_strings['LBL_COUNTRY'] = 'Country';
 $mod_strings['LBL_COORDINATES'] = 'Coordinates';
-$mod_strings['LBL_LIST_ASSIGNED_USER'] = 'User';
+$mod_strings['LBL_LIST_ASSIGNED_USER'] = 'Bruger';
 $mod_strings['LBL_AREA_MAP'] = 'Area Map';
 
 $mod_strings['LBL_AREA_EDIT_TITLE'] = 'Area Creation Instructions:';
