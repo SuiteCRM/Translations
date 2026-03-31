b43e56c6340e7d0b31a6fbb53d57dd095984d363
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-03-31 12:03:03 +0100

    New translations en_us.lang.php (Danish)

--- translations/da_DK/modules/Roles/language/da_DK.lang.php
+++ translations/da_DK/modules/Roles/language/da_DK.lang.php
@@ -43,22 +43,22 @@ if (!defined('sugarEntry') || !sugarEntry) {
 }
 
 $mod_strings = array(
-    'LBL_ROLE' => 'Role:',
-    'LBL_LANGUAGE' => 'Language:',
+    'LBL_ROLE' => 'Rolle:',
+    'LBL_LANGUAGE' => 'Sprog:',
     'LBL_MODULE_NAME' => 'Roles',
     'LBL_MODULE_TITLE' => 'Roles: Home',
     'LBL_SEARCH_FORM_TITLE' => 'Rolle søgning',
     'LBL_LIST_FORM_TITLE' => 'Rolle liste',
     'LNK_NEW_ROLE' => 'Create Role',
     'LNK_ROLES' => 'Roles',
-    'LBL_NAME' => 'Name:',
-    'LBL_DESCRIPTION' => 'Description:',
+    'LBL_NAME' => 'Navn:',
+    'LBL_DESCRIPTION' => 'Beskrivelse:',
     'LBL_ALLOWED_MODULES' => 'Tilladte moduler: ',
     'LBL_DISALLOWED_MODULES' => 'Fravalgte moduler: ',
     'LBL_ASSIGN_MODULES' => 'Rediger moduler: ',
     'LBL_DEFAULT_SUBPANEL_TITLE' => 'Roles',
-    'LBL_USERS' => 'Users',
+    'LBL_USERS' => 'Brugere',
 
-    'LBL_USERS_SUBPANEL_TITLE' => 'Users',
+    'LBL_USERS_SUBPANEL_TITLE' => 'Brugere',
     'LBL_MODULES' => 'Modules',
 );
