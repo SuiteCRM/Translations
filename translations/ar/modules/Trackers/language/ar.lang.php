d21f5baf41c21a0a6a000e473a23d10cba4de9b8
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-03-31 11:53:42 +0100

    New translations en_us.lang.php (Arabic)

--- translations/ar/modules/Trackers/language/ar.lang.php
+++ translations/ar/modules/Trackers/language/ar.lang.php
@@ -44,13 +44,13 @@ if (!defined('sugarEntry') || !sugarEntry) {
 
 $mod_strings = array(
     //Column header mapping
-    'action' => 'Action',
-    'date_modified' => 'Date of Last Action',
-    'item_id' => 'ID',
-    'item_summary' => 'Name',
-    'module_name' => 'Module Name',
-    'users' => 'Users',
+    'action' => 'الإجراء',
+    'date_modified' => 'تاريخ آخر إجراء',
+    'item_id' => 'المُعرف',
+    'item_summary' => 'الاسم',
+    'module_name' => 'اسم الوحدة',
+    'users' => 'المستخدمون',
 
     //Administration related labels
-    'LBL_MODULE_NAME' => 'Trackers',
+    'LBL_MODULE_NAME' => 'المتعقبات',
 );
