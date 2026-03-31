ccc05bca1d6228ea273a87dc0f2248c3483099a5
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-03-31 15:20:16 +0100

    New translations en_us.lang.php (Tagalog)

--- translations/tl_PH/include/SugarObjects/implements/team_security/language/tl_PH.lang.php
+++ translations/tl_PH/include/SugarObjects/implements/team_security/language/tl_PH.lang.php
@@ -44,5 +44,5 @@ if (!defined('sugarEntry') || !sugarEntry) {
 
 $mod_strings = array(
     'LBL_TEAM' => 'Mga Pangkat',
-    'LBL_TEAMS' => 'Mga Pangkat',
+    'LBL_TEAMS' => 'Ang mga pangkat',
 );
