bedc923fa5b6f541ed1221a5bf491527afa6434d
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-03-31 12:24:20 +0100

    New translations en_us.lang.php (Japanese)

--- translations/ja_JP/modules/Audit/language/ja_JP.lang.php
+++ translations/ja_JP/modules/Audit/language/ja_JP.lang.php
@@ -43,11 +43,11 @@ if (!defined('sugarEntry') || !sugarEntry) {
 }
 
 $mod_strings = array(
-    'LBL_FIELD_NAME' => 'フィールド',
+    'LBL_FIELD_NAME' => 'Field',
     'LBL_OLD_NAME' => '以前の値',
     'LBL_NEW_VALUE' => '新しい値',
     'LBL_CREATED_BY' => '更新者',
     'LBL_LIST_DATE' => '最終更新日',
     'LBL_AUDITED_FIELDS' => 'このモジュールで監査されるフィールド: ',
-    'LBL_CHANGE_LOG' => 'ログ変更',
+    'LBL_CHANGE_LOG' => '更新履歴',
 );
