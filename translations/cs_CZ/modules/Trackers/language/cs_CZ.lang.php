773a3868874f03a6248061468d6474ec0de4b56b
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-03-31 12:00:55 +0100

    New translations en_us.lang.php (Czech)

--- translations/cs_CZ/modules/Trackers/language/cs_CZ.lang.php
+++ translations/cs_CZ/modules/Trackers/language/cs_CZ.lang.php
@@ -44,13 +44,13 @@ if (!defined('sugarEntry') || !sugarEntry) {
 
 $mod_strings = array(
     //Column header mapping
-    'action' => 'Action',
+    'action' => 'Akce',
     'date_modified' => 'Date of Last Action',
     'item_id' => 'ID',
-    'item_summary' => 'Name',
+    'item_summary' => 'Jméno',
     'module_name' => 'Module Name',
-    'users' => 'Users',
+    'users' => 'Uživatelé',
 
     //Administration related labels
-    'LBL_MODULE_NAME' => 'Trackers',
+    'LBL_MODULE_NAME' => 'Sledování',
 );
