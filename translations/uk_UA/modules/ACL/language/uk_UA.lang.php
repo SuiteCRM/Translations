c5eb389c557534ad84a9fc4184d260ddfbab52eb
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-04-01 13:32:56 +0100

    New translations en_us.lang.php (Ukrainian)

--- translations/uk_UA/modules/ACL/language/uk_UA.lang.php
+++ translations/uk_UA/modules/ACL/language/uk_UA.lang.php
@@ -43,15 +43,15 @@ if (!defined('sugarEntry') || !sugarEntry) {
 }
 
 $mod_strings = array(
-    'LBL_ROLE' => 'Роль',
+    'LBL_ROLE' => 'Role',
     'LBL_NAME' => 'Назва',
     'LBL_DESCRIPTION' => 'Опис',
     'LIST_ROLES' => 'Ролі',
-    'LBL_USERS_SUBPANEL_TITLE' => 'Користувачі',
+    'LBL_USERS_SUBPANEL_TITLE' => 'Users',
     'LIST_ROLES_BY_USER' => 'Список ролей по користувачам',
     'LBL_ROLES_SUBPANEL_TITLE' => 'Ролі користувача',
-    'LBL_SEARCH_FORM_TITLE' => 'Знайти',
-    'LBL_NO_ACCESS' => 'У Вас немає доступу до цієї сторінки. Зв&#039;яжіться з вашим системним адміністратором для отримання відповідних прав.',
+    'LBL_SEARCH_FORM_TITLE' => 'Search',
+    'LBL_NO_ACCESS' => 'You do not have access to this area. Contact your site administrator to obtain access.',
     'LBL_REDIRECT_TO_HOME' => 'Перенаправлення до-дому',
     'LBL_SECONDS' => 'секунд',
     'LBL_ADDING' => 'Добавлено для ',
