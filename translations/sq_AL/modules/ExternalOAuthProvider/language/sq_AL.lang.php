7dcb10312225e1990c901f1d6bef6b9aea4a6761
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-04-01 13:26:00 +0100

    New translations en_us.lang.php (Albanian)

--- translations/sq_AL/modules/ExternalOAuthProvider/language/sq_AL.lang.php
+++ translations/sq_AL/modules/ExternalOAuthProvider/language/sq_AL.lang.php
@@ -44,22 +44,22 @@ if (!defined('sugarEntry') || !sugarEntry) {
 
 $mod_strings = [
 
-    'LBL_ASSIGNED_TO_ID' => 'Id e përdoruesit të caktuar',
-    'LBL_ASSIGNED_TO_NAME' => 'Përdorues',
+    'LBL_ASSIGNED_TO_ID' => 'Assigned User Id',
+    'LBL_ASSIGNED_TO_NAME' => 'Assigned to',
     'LBL_ID' => 'ID',
-    'LBL_DATE_ENTERED' => 'Të dhënat e krijuara',
-    'LBL_DATE_MODIFIED' => 'Të dhënat e modifikuara',
-    'LBL_MODIFIED' => 'Modifikuar nga',
-    'LBL_MODIFIED_NAME' => 'Modifikuar nga emri',
-    'LBL_CREATED' => 'Krijuar nga',
-    'LBL_DESCRIPTION' => 'Përshkrim',
-    'LBL_DELETED' => 'E fshirë',
-    'LBL_NAME' => 'Emri',
-    'LBL_CREATED_USER' => 'Krijuar nga përdoruesi',
-    'LBL_MODIFIED_USER' => 'Modifikuar nga përdoruesi',
-    'LBL_LIST_NAME' => 'Emri',
-    'LBL_EDIT_BUTTON' => 'Ndrysho',
-    'LBL_REMOVE' => 'Largo',
+    'LBL_DATE_ENTERED' => 'Date Created',
+    'LBL_DATE_MODIFIED' => 'Date Modified',
+    'LBL_MODIFIED' => 'Modified By',
+    'LBL_MODIFIED_NAME' => 'Modified By Name',
+    'LBL_CREATED' => 'Created By',
+    'LBL_DESCRIPTION' => 'Description',
+    'LBL_DELETED' => 'Deleted',
+    'LBL_NAME' => 'Name',
+    'LBL_CREATED_USER' => 'Created by User',
+    'LBL_MODIFIED_USER' => 'Modified by User',
+    'LBL_LIST_NAME' => 'Name',
+    'LBL_EDIT_BUTTON' => 'Edit',
+    'LBL_REMOVE' => 'Remove',
     'LBL_LIST_FORM_TITLE' => 'External OAuth Provider List',
     'LBL_MODULE_NAME' => 'External OAuth Providers',
     'LBL_MODULE_TITLE' => 'External OAuth Providers',
@@ -69,11 +69,11 @@ $mod_strings = [
     'LNK_LIST' => 'External OAuth Providers',
     'LBL_SEARCH_FORM_TITLE' => 'Search External OAuth Providers',
     'LBL_HISTORY_SUBPANEL_TITLE' => 'View History',
-    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Aktivitetet',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Activities',
     'LBL_NEW_FORM_TITLE' => 'New External OAuth Provider',
 
-    'LBL_LIST_DELETE' => 'Fshi',
-    'LBL_TYPE' => 'Lloji',
+    'LBL_LIST_DELETE' => 'Delete',
+    'LBL_TYPE' => 'Type',
     'LBL_CONNECTOR' => 'Connector',
     'LBL_REDIRECT_URI' => 'Redirect URI',
     'LBL_REDIRECT_URI_TYPE' => 'Redirect URI Type',
@@ -97,7 +97,7 @@ $mod_strings = [
 
     'LBL_EXTRA' => 'Extra configurations',
     'LBL_MAPPING' => 'Mapping configurations',
-    'LBL_OTHER' => 'Tjetër',
+    'LBL_OTHER' => 'Other',
 
 
     'LNK_LIST_CREATE_NEW_PERSONAL' => 'New Personal OAuth Provider',
@@ -106,5 +106,5 @@ $mod_strings = [
     'LNK_LIST_OUTBOUND_EMAILS' => 'Outbound Email Accounts',
     'LNK_LIST_EXTERNAL_OAUTH_CONNECTION' => 'External OAuth Connections',
 
-    'LBL_OWNER' => 'Pronari',
+    'LBL_OWNER' => 'Owner',
 ];
