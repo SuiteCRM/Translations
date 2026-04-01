24d48638111881e9253a053c5c1e73d5a99139d3
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-04-01 14:01:18 +0100

    New translations en_us.lang.php (Croatian)

--- translations/hr_HR/include/SugarObjects/implements/security_groups/language/hr_HR.lang.php
+++ translations/hr_HR/include/SugarObjects/implements/security_groups/language/hr_HR.lang.php
@@ -44,5 +44,5 @@ if (!defined('sugarEntry') || !sugarEntry) {
 
 $mod_strings = array(
     'LBL_SECURITYGROUPS' => "Sigurnosne Grupe",
-    'LBL_SECURITYGROUPS_SUBPANEL_TITLE' => "Sigurnosne Grupe"
+    'LBL_SECURITYGROUPS_SUBPANEL_TITLE' => "Security Groups"
 );
