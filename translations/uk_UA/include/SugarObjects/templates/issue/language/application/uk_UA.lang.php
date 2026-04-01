c109ced8a6eeef5c1a452b00f257d5014bd65030
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-04-01 13:32:47 +0100

    New translations en_us.lang.php (Ukrainian)

--- translations/uk_UA/include/SugarObjects/templates/issue/language/application/uk_UA.lang.php
+++ translations/uk_UA/include/SugarObjects/templates/issue/language/application/uk_UA.lang.php
@@ -66,8 +66,8 @@ $app_list_strings = array(
     $object_name . '_resolution_dom' => array(
         '' => '',
         'Accepted' => 'Прийнято',
-        'Duplicate' => 'Дублювати',
-        'Closed' => 'Закрито',
+        'Duplicate' => 'Duplicate',
+        'Closed' => 'Closed',
         'Out of Date' => 'Застаріло',
         'Invalid' => 'Недійсне',
     ),
