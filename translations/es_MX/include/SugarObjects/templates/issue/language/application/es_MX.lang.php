698c2385d0060c000b8445ff6b850e5dcf536633
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-04-01 13:54:08 +0100

    New translations en_us.lang.php (Spanish, Mexico)

--- translations/es_MX/include/SugarObjects/templates/issue/language/application/es_MX.lang.php
+++ translations/es_MX/include/SugarObjects/templates/issue/language/application/es_MX.lang.php
@@ -66,8 +66,8 @@ $app_list_strings = array(
     $object_name . '_resolution_dom' => array(
         '' => '',
         'Accepted' => 'Aceptado',
-        'Duplicate' => 'Duplicar',
-        'Closed' => 'Cerrada',
+        'Duplicate' => 'Duplicate',
+        'Closed' => 'Closed',
         'Out of Date' => 'Expirada',
         'Invalid' => 'Inválido',
     ),
