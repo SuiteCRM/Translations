efb50dde0842983f2daf5fdf7c59f2c73dc1959b
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-04-01 13:37:34 +0100

    New translations en_us.lang.php (Chinese Simplified)

--- translations/zh_CN/modules/Roles/language/zh_CN.lang.php
+++ translations/zh_CN/modules/Roles/language/zh_CN.lang.php
@@ -44,21 +44,21 @@ if (!defined('sugarEntry') || !sugarEntry) {
 
 $mod_strings = array(
     'LBL_ROLE' => '角色',
-    'LBL_LANGUAGE' => '语言',
-    'LBL_MODULE_NAME' => '角色',
+    'LBL_LANGUAGE' => 'Language:',
+    'LBL_MODULE_NAME' => '查看角色',
     'LBL_MODULE_TITLE' => '角色',
     'LBL_SEARCH_FORM_TITLE' => '搜索',
     'LBL_LIST_FORM_TITLE' => '角色列表',
     'LNK_NEW_ROLE' => '创建角色',
-    'LNK_ROLES' => '角色',
-    'LBL_NAME' => '系统名称',
+    'LNK_ROLES' => '查看角色',
+    'LBL_NAME' => '营销活动名称',
     'LBL_DESCRIPTION' => '说明',
     'LBL_ALLOWED_MODULES' => '已允许的模块',
     'LBL_DISALLOWED_MODULES' => '不允许的模块',
     'LBL_ASSIGN_MODULES' => '编辑模块',
-    'LBL_DEFAULT_SUBPANEL_TITLE' => '角色',
-    'LBL_USERS' => '用户',
+    'LBL_DEFAULT_SUBPANEL_TITLE' => '查看角色',
+    'LBL_USERS' => '多个用户',
 
-    'LBL_USERS_SUBPANEL_TITLE' => '用户',
+    'LBL_USERS_SUBPANEL_TITLE' => '多个用户',
     'LBL_MODULES' => '模块',
 );
