ce09d1606a3f4817e1c500248e3985fd2fdd1e17
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-04-01 13:12:43 +0100

    New translations en_us.lang.php (Polish)

--- translations/pl_PL/modules/MergeRecords/language/pl_PL.lang.php
+++ translations/pl_PL/modules/MergeRecords/language/pl_PL.lang.php
@@ -49,7 +49,7 @@ $mod_strings = array(
     'LBL_LIST_FORM_TITLE' => 'Lista scaleń',
 
     'LBL_LBL_MERGE_RECORDS_STEP_1' => 'Znajdź rekordy, żeby je scalić',
-    'LBL_AVAIL_FIELDS' => 'Dostępne pola',
+    'LBL_AVAIL_FIELDS' => 'Available Fields',
     'LBL_FILTER_COND' => 'Konfiguracja filtru wyszukiwania',
     'LBL_MERGE_VALUE_OVER' => 'Scalanie wartości właściciela',
 
@@ -58,10 +58,10 @@ $mod_strings = array(
 
     'LBL_PERFORM_MERGE_BUTTON_TITLE' => 'Wykonaj scalanie',
     'LBL_PERFORM_MERGE_BUTTON_KEY' => 'P',
-    'LBL_PERFORM_MERGE_BUTTON_LABEL' => 'Wykonaj scalanie',
+    'LBL_PERFORM_MERGE_BUTTON_LABEL' => 'Perform Merge',
 
     'LBL_SAVE_MERGED_RECORD_BUTTON_TITLE' => 'Zapisz scalanie',
-    'LBL_SAVE_MERGED_RECORD_BUTTON_LABEL' => 'Zapisz scalanie',
+    'LBL_SAVE_MERGED_RECORD_BUTTON_LABEL' => 'Save Merge',
 
     'LBL_STEP2_FORM_TITLE' => 'Znalezione rekordy scal z',
     'LBL_SELECT_ERROR' => 'Musisz dokonać wyboru, za nim przejdziesz do scalania.',
