da2204775aef30487ca70a0006de2d82b2cda309
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-04-02 13:10:06 +0100

    New translations en_us.lang.php (Estonian)

--- translations/et_EE/include/SugarObjects/implements/security_groups/language/et_EE.lang.php
+++ translations/et_EE/include/SugarObjects/implements/security_groups/language/et_EE.lang.php
@@ -44,5 +44,5 @@ if (!defined('sugarEntry') || !sugarEntry) {
 
 $mod_strings = array(
     'LBL_SECURITYGROUPS' => "Turvagrupid",
-    'LBL_SECURITYGROUPS_SUBPANEL_TITLE' => "Turvagrupid"
+    'LBL_SECURITYGROUPS_SUBPANEL_TITLE' => "Security Groups"
 );
