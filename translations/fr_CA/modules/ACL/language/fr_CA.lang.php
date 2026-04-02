a3fc7b3950e624029f2bfb7206510ee75fe9c9b9
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-04-02 13:24:24 +0100

    New translations en_us.lang.php (French, Canada)

--- translations/fr_CA/modules/ACL/language/fr_CA.lang.php
+++ translations/fr_CA/modules/ACL/language/fr_CA.lang.php
@@ -43,15 +43,15 @@ if (!defined('sugarEntry') || !sugarEntry) {
 }
 
 $mod_strings = array(
-    'LBL_ROLE' => 'Rôle',
+    'LBL_ROLE' => 'Role',
     'LBL_NAME' => 'Nom',
     'LBL_DESCRIPTION' => 'Description:',
     'LIST_ROLES' => 'Liste des Rôles',
     'LBL_USERS_SUBPANEL_TITLE' => 'Utilisateurs',
     'LIST_ROLES_BY_USER' => 'Liste des Rôles par Utilisateur',
     'LBL_ROLES_SUBPANEL_TITLE' => 'Rôles des Utilisateurs',
-    'LBL_SEARCH_FORM_TITLE' => 'Rechercher',
-    'LBL_NO_ACCESS' => 'Vous n\'avez pas accès à cette zone. Veuillez contacter votre adminisatrateur si vous pensez que vous devriez avoir accès.',
+    'LBL_SEARCH_FORM_TITLE' => 'Recherche',
+    'LBL_NO_ACCESS' => 'Vous n\'avez pas accès à cette section. Veuillez contacter votre administrateur système pour obtenir un accès.',
     'LBL_REDIRECT_TO_HOME' => 'Redirigé vers la page d\'accueil dans',
     'LBL_SECONDS' => 'secondes',
     'LBL_ADDING' => 'Ajouter pour',
