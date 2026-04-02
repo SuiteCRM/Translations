cccadf41ae0d47f57bcb7d996a5b0391b32a4fa2
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-04-02 13:09:52 +0100

    New translations en_us.lang.php (Croatian)

--- translations/hr_HR/modules/TemplateSectionLine/language/hr_HR.lang.php
+++ translations/hr_HR/modules/TemplateSectionLine/language/hr_HR.lang.php
@@ -43,12 +43,12 @@ if (!defined('sugarEntry') || !sugarEntry) {
 }
 
 $mod_strings = array(
-    'LBL_DESCRIPTION' => 'Opis',
-    'LBL_NAME' => 'Ime',
-    'LBL_LIST_NAME' => 'Ime',
-    'LBL_EDIT_BUTTON' => 'Uredi',
-    'LBL_REMOVE' => 'Ukloni',
-    'LBL_GRP' => 'Grupa',
-    'LBL_ORD' => 'Redoslijed',
+    'LBL_DESCRIPTION' => 'Description',
+    'LBL_NAME' => 'Name',
+    'LBL_LIST_NAME' => 'Name',
+    'LBL_EDIT_BUTTON' => 'Edit',
+    'LBL_REMOVE' => 'Remove',
+    'LBL_GRP' => 'Group',
+    'LBL_ORD' => 'Order',
     'LBL_THUMBNAIL' => 'Thumbnail',
 );
