f0b23f6bed86375ccdb15c200df9c3050c7d4377
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-04-02 13:49:53 +0100

    New translations en_us.lang.php (Urdu (India))

--- translations/ur_IN/modules/Bugs/language/ur_IN.lang.php
+++ translations/ur_IN/modules/Bugs/language/ur_IN.lang.php
@@ -99,6 +99,6 @@ $mod_strings = array(
     'LBL_LIST_ASSIGNED_TO_NAME' => 'مقرر کردہ صارف',
     'LBL_ASSIGNED_TO_NAME' => 'Assigned to',
 
-    'LBL_BUG_INFORMATION' => 'جائزہ', //No need to be translated in all caps. Translation used just in menu action items when using the SuiteP template
+    'LBL_BUG_INFORMATION' => 'OVERVIEW', //No need to be translated in all caps. Translation used just in menu action items when using the SuiteP template
 
 );
