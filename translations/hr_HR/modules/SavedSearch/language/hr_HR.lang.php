fe0516ec435f518a683fb3fbe51ce32c0e79441b
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-04-02 13:09:36 +0100

    New translations en_us.lang.php (Croatian)

--- translations/hr_HR/modules/SavedSearch/language/hr_HR.lang.php
+++ translations/hr_HR/modules/SavedSearch/language/hr_HR.lang.php
@@ -49,15 +49,15 @@ $mod_strings = array(
     'LBL_DELETE_CONFIRM' => 'Are you sure you want to delete the selected Saved Filter?',
     'LBL_DELETE_BUTTON_TITLE' => 'Delete this Saved Filter',
     'LBL_SAVE_BUTTON_TITLE' => 'Save the current filter',
-    'LBL_LIST_NAME' => 'Ime',
-    'LBL_LIST_MODULE' => 'Modul',
+    'LBL_LIST_NAME' => 'Name',
+    'LBL_LIST_MODULE' => 'Module',
     'LBL_ORDER_BY_COLUMNS' => 'Order by column:',
-    'LBL_DIRECTION' => 'Smjer:',
+    'LBL_DIRECTION' => 'Direction:',
     'LBL_SAVE_SEARCH_AS' => 'Save filter as:',
-    'LBL_ASCENDING' => 'uzlazno',
-    'LBL_DESCENDING' => 'silazno',
-    'LBL_MODIFY_CURRENT_FILTER' => 'Izmjeni trenutni filter',
+    'LBL_ASCENDING' => 'Ascending',
+    'LBL_DESCENDING' => 'Descending',
+    'LBL_MODIFY_CURRENT_FILTER' => 'Modify current filter',
 
-    'LBL_CREATED_BY' => 'Kreirano od',
+    'LBL_CREATED_BY' => 'Created By',
 
 );
