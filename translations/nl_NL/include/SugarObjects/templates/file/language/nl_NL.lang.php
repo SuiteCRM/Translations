bb38537e8bd376375439f2096900d32841344a1d
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-04-08 13:38:04 +0100

    New translations en_us.lang.php (Dutch)

--- translations/nl_NL/include/SugarObjects/templates/file/language/nl_NL.lang.php
+++ translations/nl_NL/include/SugarObjects/templates/file/language/nl_NL.lang.php
@@ -51,8 +51,8 @@ $mod_strings = array(
     'LBL_SEARCH_FORM_TITLE' => 'Documenten zoeken',
     //vardef labels
     'LBL_NAME' => 'Document naam',
-    'LBL_DESCRIPTION' => 'Description',
-    'LBL_ASSIGNED_TO' => 'Assigned to:',
+    'LBL_DESCRIPTION' => 'Omschrijving',
+    'LBL_ASSIGNED_TO' => 'Toegewezen aan:',
     'LBL_CATEGORY' => 'Categorie',
     'LBL_SUBCATEGORY' => 'Sub-categorie',
     'LBL_STATUS' => 'Status',
@@ -87,31 +87,31 @@ $mod_strings = array(
     //document list view.
     'LBL_LIST_FORM_TITLE' => 'Document lijst',
     'LBL_LIST_DOCUMENT' => 'Document',
-    'LBL_LIST_CATEGORY' => 'Category',
-    'LBL_LIST_SUBCATEGORY' => 'Sub Category',
-    'LBL_LIST_REVISION' => 'Revision',
+    'LBL_LIST_CATEGORY' => 'Categorie',
+    'LBL_LIST_SUBCATEGORY' => 'Sub Categorie',
+    'LBL_LIST_REVISION' => 'Revisie',
     'LBL_LIST_LAST_REV_CREATOR' => 'Gepubliceerd door',
     'LBL_LIST_LAST_REV_DATE' => 'Revisie datum',
     'LBL_LIST_VIEW_DOCUMENT' => 'Bekijk',
-    'LBL_LIST_ACTIVE_DATE' => 'Publish Date',
-    'LBL_LIST_EXP_DATE' => 'Expiration Date',
+    'LBL_LIST_ACTIVE_DATE' => 'Publiceer datum',
+    'LBL_LIST_EXP_DATE' => 'Vervaldatum',
     'LBL_LIST_STATUS' => 'Status',
 
     //document search form.
-    'LBL_SF_CATEGORY' => 'Category:',
-    'LBL_SF_SUBCATEGORY' => 'Sub Category:',
+    'LBL_SF_CATEGORY' => 'Categorie:',
+    'LBL_SF_SUBCATEGORY' => 'Sub Categorie:',
 
     'DEF_CREATE_LOG' => 'Document aangemaakt',
 
     //error messages
-    'ERR_DOC_NAME' => 'Document Name',
-    'ERR_DOC_ACTIVE_DATE' => 'Publish Date',
+    'ERR_DOC_NAME' => 'Document naam',
+    'ERR_DOC_ACTIVE_DATE' => 'Publicatie datum',
     'ERR_FILENAME' => 'Bestandsnaam',
 
     //sub-panel vardefs.
-    'LBL_LIST_DOCUMENT_NAME' => 'Document Name',
+    'LBL_LIST_DOCUMENT_NAME' => 'Document naam',
 
     'LBL_EDIT_BUTTON' => 'Bewerken ',
-    'LBL_REMOVE' => 'Remove',
+    'LBL_REMOVE' => 'Verwijderen',
 
 );
