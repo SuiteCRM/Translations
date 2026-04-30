69e7588b3ffe30152e33eb7c498a4ac779fb9c9a
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-04-30 20:52:09 +0100

    New translations manifest.php (Dutch)
    [ci skip]

---
+++ translations/nl_NL/manifest.php
@@ -0,0 +1,25 @@
+<?php
+/******************************************
+ * SuiteCRM Translations
+ * @URL: https://crowdin.com/project/suitecrmtrans
+ * @author SuiteCRM Community via Crowdin
+ ******************************************/
+
+$manifest = array(
+    'name' => 'Dutch (Netherlands)',
+    'description' => 'Vertaling: crowdin.com/project/suitecrmtranslations',
+    'type' => 'langpack',
+    'is_uninstallable' => 'Yes',
+    'author' => 'SuiteCRM Community',
+    'version' => '8.9-1',
+    'published_date' => '16-01-2026',
+);
+$installdefs = array(
+    'id' => 'nl_NL',
+    'image_dir' => '<basepath>/images',
+    'copy' => array(
+        array('from' => '<basepath>/include', 'to' => 'include'),
+        array('from' => '<basepath>/modules', 'to' => 'modules'),
+        array('from' => '<basepath>/install', 'to' => 'install'),
+    )
+);
