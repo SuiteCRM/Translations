aad493e80953dcb2b233e91b4a91390f43173ec4
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-04-30 19:11:38 +0100

    New translations en_us.lang.php (Finnish)
    [ci skip]

---
+++ translations/fi_FI/modules/Contacts/language/fi_FI.lang.php
@@ -0,0 +1,229 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'ERR_DELETE_RECORD' => 'Poista kontakti antamalla tietueen numero.',
+    'LBL_ACCOUNT_ID' => 'Asiakastunnus:',
+    'LBL_ACCOUNT_NAME' => 'Account Name:',
+    'LBL_CAMPAIGN' => 'Campaign:',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Activities',
+    'LBL_ADDRESS_INFORMATION' => 'Address Information',
+    'LBL_ALT_ADDRESS_CITY' => 'Muu osoite Kaupunki',
+    'LBL_ALT_ADDRESS_COUNTRY' => 'Muu osoite Maa',
+    'LBL_ALT_ADDRESS_POSTALCODE' => 'Muu osoite Postinumero',
+    'LBL_ALT_ADDRESS_STATE' => 'Muu osoite Maakunta/lääni',
+    'LBL_ALT_ADDRESS_STREET_2' => 'Alternate Address Street 2:',
+    'LBL_ALT_ADDRESS_STREET_3' => 'Alternate Address Street 3:',
+    'LBL_ALT_ADDRESS_STREET' => 'Vaihtoehtoinen katuosoite:',
+    'LBL_ALTERNATE_ADDRESS' => 'Other Address:',
+    'LBL_ALT_ADDRESS' => 'Other Address:',
+    'LBL_ANY_ADDRESS' => 'Any Address:',
+    'LBL_ANY_EMAIL' => 'Any Email:',
+    'LBL_ANY_PHONE' => 'Any Phone:',
+    'LBL_ASSIGNED_TO_NAME' => 'Assigned to:',
+    'LBL_ASSIGNED_TO_ID' => 'Assigned User',
+    'LBL_ASSISTANT_PHONE' => 'Assistentin puhelin',
+    'LBL_ASSISTANT' => 'Avustaja:',
+    'LBL_BIRTHDATE' => 'Syntymäpäivä:',
+    'LBL_CITY' => 'City:',
+    'LBL_CAMPAIGN_ID' => 'Campaign ID',
+    'LBL_CONTACT_INFORMATION' => 'OVERVIEW', //No need to be translated in all caps. Translation used just in menu action items when using the SuiteP template
+    'LBL_CONTACT_NAME' => 'Contact Name:',
+    'LBL_CONTACT_OPP_FORM_TITLE' => 'Kontakti-Myyntimahdollisuus:',
+    'LBL_CONTACT_ROLE' => 'Role:',
+    'LBL_CONTACT' => 'Contact:',
+    'LBL_COUNTRY' => 'Country:',
+    'LBL_CREATED_ACCOUNT' => 'Luotu uusi asiakas',
+    'LBL_CREATED_CALL' => 'Luotu uusi puhelu',
+    'LBL_CREATED_CONTACT' => 'Luotu uusi kontakti',
+    'LBL_CREATED_MEETING' => 'Luotu uusi kokous',
+    'LBL_CREATED_OPPORTUNITY' => 'Luotu uusi myyntimahdollisuus',
+    'LBL_DATE_MODIFIED' => 'Date Modified:',
+    'LBL_DEFAULT_SUBPANEL_TITLE' => 'Contacts',
+    'LBL_DEPARTMENT' => 'Osasto:',
+    'LBL_DESCRIPTION' => 'Description:',
+    'LBL_DIRECT_REPORTS_SUBPANEL_TITLE' => 'Direct Reports',
+    'LBL_DO_NOT_CALL' => 'Ei saa soittaa:',
+    'LBL_DUPLICATE' => 'Mahdollinen duplikaatti kontakti',
+    'LBL_EMAIL_ADDRESS' => 'Email Address:',
+    'LBL_EMAIL_OPT_OUT' => 'Email Opt Out:',
+    'LBL_EXISTING_ACCOUNT' => 'Käytettiin olemassa olevaaa asiakasta',
+    'LBL_EXISTING_CONTACT' => 'Käytettiin olemassa olevaa kontaktia',
+    'LBL_EXISTING_OPPORTUNITY' => 'Käytettiin olemassa olevaa myyntimahdollisuutta',
+    'LBL_FAX_PHONE' => 'Fax:',
+    'LBL_FIRST_NAME' => 'Etunimi:',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'History',
+    'LBL_HOME_PHONE' => 'Koti:',
+    'LBL_ID' => 'Tunnus:',
+    'LBL_IMPORT_VCARD' => 'Import vCard',
+    'LBL_VCARD' => 'vCard',
+    'LBL_IMPORT_VCARDTEXT' => 'Luo uusi kontakti automaattisesti tuomalla vCard.',
+    'LBL_INVALID_EMAIL' => 'Invalid Email:',
+    'LBL_INVITEE' => 'Direct Reports',
+    'LBL_LAST_NAME' => 'Sukunimi:',
+    'LBL_LEAD_SOURCE' => 'Lead Source:',
+    'LBL_LIST_ACCEPT_STATUS' => 'Accept Status',
+    'LBL_LIST_ACCOUNT_NAME' => 'Account Name',
+    'LBL_LIST_CONTACT_NAME' => 'Contact Name',
+    'LBL_LIST_CONTACT_ROLE' => 'Role',
+    'LBL_LIST_EMAIL_ADDRESS' => 'Sähköposti',
+    'LBL_LIST_FIRST_NAME' => 'First Name',
+    'LBL_LIST_FORM_TITLE' => 'Contact List',
+    'LBL_LIST_LAST_NAME' => 'Last Name',
+    'LBL_LIST_NAME' => 'Nimi',
+    'LBL_LIST_PHONE' => 'Office Phone',
+    'LBL_LIST_TITLE' => 'Titteli',
+    'LBL_MOBILE_PHONE' => 'Gsm:',
+    'LBL_MODIFIED' => 'Muokkaaja: ',
+    'LBL_MODULE_NAME' => 'Contacts',
+    'LBL_MODULE_TITLE' => 'Kontaktit',
+    'LBL_NAME' => 'Name:',
+    'LBL_NEW_FORM_TITLE' => 'Uusi kontakti',
+    'LBL_NOTE_SUBJECT' => 'Muistion aihe:',
+    'LBL_OFFICE_PHONE' => 'Toimiston puhelin:',
+    'LBL_OPP_NAME' => 'Myyntimahdollisuuden nimi:',
+    'LBL_OPPORTUNITY_ROLE_ID' => 'Myyntimahdollisuus rooli ID:',
+    'LBL_OPPORTUNITY_ROLE' => 'Myyntimahdollisuuden rooli',
+    'LBL_OTHER_EMAIL_ADDRESS' => 'Other Email:',
+    'LBL_OTHER_PHONE' => 'Other Phone:',
+    'LBL_PHONE' => 'Phone:',
+    'LBL_PORTAL_APP' => 'Portaalisovellus',
+    'LBL_PORTAL_INFORMATION' => 'Portaalin tiedot',
+    'LBL_PORTAL_NAME' => 'Portaali nimi:',
+    'LBL_STREET' => 'Street',
+    'LBL_POSTAL_CODE' => 'Postal Code:',
+    'LBL_PRIMARY_ADDRESS_CITY' => 'Primary Address City:',
+    'LBL_PRIMARY_ADDRESS_COUNTRY' => 'Primary Address Country:',
+    'LBL_PRIMARY_ADDRESS_POSTALCODE' => 'Primary Address Postal Code:',
+    'LBL_PRIMARY_ADDRESS_STATE' => 'Primary Address State:',
+    'LBL_PRIMARY_ADDRESS_STREET_2' => 'Primary Address Street 2:',
+    'LBL_PRIMARY_ADDRESS_STREET_3' => 'Primary Address Street 3:',
+    'LBL_PRIMARY_ADDRESS_STREET' => 'Primary Address Street:',
+    'LBL_PRIMARY_ADDRESS' => 'Primary Address:',
+    'LBL_PRODUCTS_TITLE' => 'Products',
+    'LBL_REPORTS_TO_ID' => 'Raportoi hlölle ID:',
+    'LBL_REPORTS_TO' => 'Raportit:',
+    'LBL_RESOURCE_NAME' => 'Resurssi nimi',
+    'LBL_SALUTATION' => 'Tervehdys',
+    'LBL_SAVE_CONTACT' => 'Tallenna kontakti',
+    'LBL_SEARCH_FORM_TITLE' => 'Hae kontakti',
+    'LBL_SELECT_CHECKED_BUTTON_LABEL' => 'Valitse merkityt kontaktit',
+    'LBL_SELECT_CHECKED_BUTTON_TITLE' => 'Select Checked Contacts',
+    'LBL_STATE' => 'Alue tai osavaltio:',
+    'LBL_SYNC_CONTACT' => 'Synkronoi Outlook&reg;:',
+    'LBL_PROSPECT_LIST' => 'Prospect List',
+    'LBL_TITLE' => 'Titteli:',
+    'LNK_CONTACT_LIST' => 'Kontaktit',
+    'LNK_IMPORT_VCARD' => 'Luo kontakti vCard tiedostosta',
+    'LNK_NEW_ACCOUNT' => 'Create Account',
+    'LNK_NEW_APPOINTMENT' => 'Create Appointment',
+    'LNK_NEW_CALL' => 'Log Call',
+    'LNK_NEW_CASE' => 'Luo palvelupyyntö',
+    'LNK_NEW_CONTACT' => 'Create Contact',
+    'LNK_NEW_EMAIL' => 'Archive Email',
+    'LNK_NEW_MEETING' => 'Schedule Meeting',
+    'LNK_NEW_NOTE' => 'Create Note',
+    'LNK_NEW_OPPORTUNITY' => 'Create Opportunity',
+    'LNK_NEW_TASK' => 'Create Task',
+    'LNK_SELECT_ACCOUNT' => "Select Account",
+    'NTC_DELETE_CONFIRMATION' => 'Are you sure you want to delete this record?',
+    'NTC_SNOOZE_CONFIRMATION' => 'Oletko varma, että haluat torkuttaa?',
+    'NTC_OPPORTUNITY_REQUIRES_ACCOUNT' => 'Myyntimahdollisuuden luonti vaatii asiakkaan.\n Ole hyvä ja luo asiakas, tai valitse olemassa oleva.',
+    'NTC_REMOVE_CONFIRMATION' => 'Haluatko poistaa tämän kontaktin Palvelupyynnöstä?',
+
+    'LBL_LEADS_SUBPANEL_TITLE' => 'Leads',
+    'LBL_OPPORTUNITIES_SUBPANEL_TITLE' => 'Opportunities',
+    'LBL_DOCUMENTS_SUBPANEL_TITLE' => 'Documents',
+    'LBL_COPY_ADDRESS_CHECKED_PRIMARY' => 'Kopioi ensisijaiseen osoitteeseen',
+    'LBL_COPY_ADDRESS_CHECKED_ALT' => 'Kopioi muuhun osoitteeseen',
+
+    'LBL_CASES_SUBPANEL_TITLE' => 'Cases',
+    'LBL_BUGS_SUBPANEL_TITLE' => 'Bugs',
+    'LBL_PROJECTS_SUBPANEL_TITLE' => 'Projects',
+    'LBL_PROJECTS_RESOURCES' => 'Projektin resurssit',
+    'LBL_CAMPAIGNS' => 'Campaigns',
+    'LBL_CAMPAIGN_LIST_SUBPANEL_TITLE' => 'Campaigns',
+    'LBL_LIST_CITY' => 'City',
+    'LBL_LIST_STATE' => 'State',
+    'LBL_HOMEPAGE_TITLE' => 'Omat kontaktit',
+    'LBL_OPPORTUNITIES' => 'Opportunities',
+
+    'LBL_CONTACTS_SUBPANEL_TITLE' => 'Contacts',
+    'LBL_PROJECT_SUBPANEL_TITLE' => 'Projects',
+    'LNK_IMPORT_CONTACTS' => 'Tuo kontakteja',
+
+    // SNIP
+    'LBL_USER_SYNC' => 'Käyttäjän synkronointi',
+
+    'LBL_FP_EVENTS_CONTACTS_FROM_FP_EVENTS_TITLE' => 'Events',
+
+    'LBL_AOP_CASE_UPDATES' => 'Case Updates',
+    'LBL_CREATE_PORTAL_USER' => 'Luo käyttäjä portaaliin',
+    'LBL_ENABLE_PORTAL_USER' => 'Aktivoi portaalin käyttäjä',
+    'LBL_DISABLE_PORTAL_USER' => 'Passivoi portaalin käyttäjä',
+    'LBL_CREATE_PORTAL_USER_FAILED' => 'Käyttäjän luonti portaaliin ei onnistunut',
+    'LBL_ENABLE_PORTAL_USER_FAILED' => 'Käyttäjän aktivointi ei onnistunut',
+    'LBL_DISABLE_PORTAL_USER_FAILED' => 'Käyttäjän passivointi ei onnistunut',
+    'LBL_CREATE_PORTAL_USER_SUCCESS' => 'Luotu portaalin käyttäjä',
+    'LBL_ENABLE_PORTAL_USER_SUCCESS' => 'Aktivoitu portaalin käyttäjä',
+    'LBL_DISABLE_PORTAL_USER_SUCCESS' => 'Passivoitu portaalin käyttäjä',
+    'LBL_FAILED_TO_CONNECT_JOOMLA' => 'Failed to create portal user, not able to connect to Joomla',
+    'LBL_ERROR_CONTACT_ID_OR_EMAIL_EMPTY' => 'Contact ID or Email is empty',
+    'LBL_NO_JOOMLA_URL' => 'Portaalin URL:ää ei ole määritelty',
+    'LBL_PORTAL_USER_TYPE' => 'Portaalin käyttäjän tyyppi',
+    'LBL_PORTAL_ACCOUNT_DISABLED' => 'Tili passivoitu',
+    'LBL_JOOMLA_ACCOUNT_ID' => 'Joomla-tilin tunnus',
+    'LBL_AOP_DISABLED' => 'AOP is disabled, please enable via settings',
+
+    'LBL_AOS_CONTRACTS' => 'Contracts',
+    'LBL_AOS_INVOICES' => 'Invoices',
+    'LBL_AOS_QUOTES' => 'Quotes',
+    'LBL_PROJECT_CONTACTS_1_FROM_PROJECT_TITLE' => 'Projektin kontaktit projektin nimestä',
+    'LBL_LAST_MEETING' => 'Viimeinen vuorovaikutus oli kokous :',
+    'LBL_LAST_CALL' => 'Viimeinen vuorovaikutus oli puhelu :',
+    'LBL_LAST_EMAIL' => 'Viimeinen vuorovaikutus oli sähköposti :',
+    'LBL_NO_INTERACTION' => 'Sinulla ei ole vielä vuorovaikutusta tämän yhteystiedon kanssa.',
+
+    'LBL_LIST_INVITE_STATUS' => 'Invite Status',
+);
