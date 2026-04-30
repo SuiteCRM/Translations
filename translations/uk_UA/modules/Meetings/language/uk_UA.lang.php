d43d881f55b8f9b3c03c09d6f0bffa5b8dd3d344
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-04-30 19:52:24 +0100

    New translations en_us.lang.php (Ukrainian)
    [ci skip]

---
+++ translations/uk_UA/modules/Meetings/language/uk_UA.lang.php
@@ -0,0 +1,171 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ACCEPT_THIS' => 'Accept?',
+    'LBL_ADD_BUTTON' => 'Add',
+    'LBL_ADD_INVITEE' => 'Add Invitees',
+    'LBL_CONTACT_NAME' => 'Contact:',
+    'LBL_CONTACTS_SUBPANEL_TITLE' => 'Contacts',
+    'LBL_CREATED_BY' => 'Created by',
+    'LBL_DATE_END' => 'End Date',
+    'LBL_DATE_TIME' => 'Start Date & Time:',
+    'LBL_DATE' => 'Start Date:',
+    'LBL_DEFAULT_SUBPANEL_TITLE' => 'Meetings',
+    'LBL_DESCRIPTION' => 'Опис: ',
+    'LBL_DIRECTION' => 'Direction:',
+    'LBL_DURATION_HOURS' => 'Duration Hours:',
+    'LBL_DURATION_MINUTES' => 'Duration Minutes:',
+    'LBL_DURATION' => 'Duration:',
+    'LBL_EMAIL' => 'Розсилка електронною поштою',
+    'LBL_FIRST_NAME' => 'Ім\'я',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Notes',
+    'LBL_HOURS_ABBREV' => 'h',
+    'LBL_HOURS_MINS' => '(hours/minutes)',
+    'LBL_INVITEE' => 'Invitees',
+    'LBL_LAST_NAME' => 'Last Name',
+    'LBL_ASSIGNED_TO_NAME' => 'Відповідальна особа:',
+    'LBL_LIST_ASSIGNED_TO_NAME' => 'Відповідальна особа',
+    'LBL_LIST_CLOSE' => 'Close',
+    'LBL_LIST_CONTACT' => 'Контакт',
+    'LBL_LIST_DATE_MODIFIED' => 'Дата зміни',
+    'LBL_LIST_DATE' => 'Дата початку ',
+    'LBL_LIST_DIRECTION' => 'Direction',
+    'LBL_LIST_DUE_DATE' => 'Due Date',
+    'LBL_LIST_FORM_TITLE' => 'Список зустрічей',
+    'LBL_LIST_MY_MEETINGS' => 'Мої зустрічі',
+    'LBL_LIST_RELATED_TO' => 'Related to',
+    'LBL_LIST_STATUS' => 'Status',
+    'LBL_LIST_SUBJECT' => 'Subject',
+    'LBL_LEADS_SUBPANEL_TITLE' => 'Leads',
+    'LBL_LOCATION' => 'Location:',
+    'LBL_MINSS_ABBREV' => 'm',
+    'LBL_MODIFIED_BY' => 'Modified by',
+    'LBL_MODULE_NAME' => 'Meetings',
+    'LBL_MODULE_TITLE' => 'Зустрічі - Головна',
+    'LBL_NAME' => 'Назва',
+    'LBL_NEW_FORM_TITLE' => 'Create Appointment',
+    'LBL_OUTLOOK_ID' => 'Outlook ID',
+    'LBL_SEQUENCE' => 'Послідовність оновлення зустрічі',
+    'LBL_PHONE' => 'Phone Office:',
+    'LBL_REMINDER_TIME' => 'Reminder Time',
+    'LBL_EMAIL_REMINDER_SENT' => 'Нагадування надіслано електронним листом',
+    'LBL_REMINDER' => 'Нагадати:',
+    'LBL_REMINDER_POPUP' => 'Popup',
+    'LBL_REMINDER_EMAIL_ALL_INVITEES' => 'Email all invitees',
+    'LBL_EMAIL_REMINDER' => 'Email Reminder',
+    'LBL_EMAIL_REMINDER_TIME' => 'Email Reminder Time',
+    'LBL_REMOVE' => 'Remove',
+    'LBL_SCHEDULING_FORM_TITLE' => 'Scheduling',
+    'LBL_SEARCH_BUTTON' => 'Search',
+    'LBL_SEARCH_FORM_TITLE' => 'Знайти зустріч',
+    'LBL_SEND_BUTTON_LABEL' => 'Save & Send Invites',
+    'LBL_SEND_BUTTON_TITLE' => 'Save & Send Invites',
+    'LBL_STATUS' => 'Status:',
+    'LBL_TYPE' => 'Тип зустрічі',
+    'LBL_PASSWORD' => 'Пароль зустрічі',
+    'LBL_URL' => 'Розпочати зустріч/взяти участь',
+    'LBL_HOST_URL' => 'URL-адреса хоста',
+    'LBL_DISPLAYED_URL' => 'Відобразити URL',
+    'LBL_CREATOR' => 'Творець зустрічі',
+    'LBL_EXTERNALID' => 'ID зовнішнього додатку',
+    'LBL_SUBJECT' => 'Subject:',
+    'LBL_TIME' => 'Start Time:',
+    'LBL_USERS_SUBPANEL_TITLE' => 'Users',
+    'LBL_CALENDAR_ACCOUNTS' => 'Calendar Accounts',
+    'LBL_PARENT_TYPE' => 'Parent Type',
+    'LBL_PARENT_ID' => 'Parent ID',
+    'LNK_MEETING_LIST' => 'View Meetings',
+    'LNK_NEW_APPOINTMENT' => 'Create Appointment',
+    'LNK_NEW_MEETING' => 'Schedule Meeting',
+    'LNK_IMPORT_MEETINGS' => 'Import Meetings',
+
+    'LBL_CREATED_USER' => 'Created User',
+    'LBL_MODIFIED_USER' => 'Modified User',
+    'NOTICE_DURATION_TIME' => 'Duration time must be greater than 0',
+    'LBL_MEETING_INFORMATION' => 'ОГЛЯД', //No need to be translated in all caps. Translation used just in menu action items when using the SuiteP template
+    'LBL_LIST_JOIN_MEETING' => 'Взяти участь у зустрічі',
+    'LBL_ACCEPT_STATUS' => 'Accept Status',
+    'LBL_ACCEPT_LINK' => 'Посилання для прийняття',
+    // You are not invited to the meeting messages
+    'LBL_EXTNOT_MAIN' => 'Ви не можете взяти участь в цій зустрічі, оскільки Ви не запрошені.',
+    'LBL_EXTNOT_RECORD_LINK' => 'Огляд зустрічі',
+
+    //cannot start messages
+    'LBL_EXTNOSTART_MAIN' => 'Ви не можете почати цю зустріч, оскільки Ви не є Адміністратором або творцем зустрічі.',
+
+    // create invitee functionallity
+    'LBL_CREATE_INVITEE' => 'Додати запрошену особу',
+    'LBL_CREATE_CONTACT' => 'As Contact',  // Create invitee functionallity
+    'LBL_CREATE_LEAD' => 'As Lead',  // Create invitee functionallity
+    'LBL_CREATE_AND_ADD' => 'Create & Add',  // Create invitee functionallity
+    'LBL_CANCEL_CREATE_INVITEE' => 'Cancel',
+    'LBL_EMPTY_SEARCH_RESULT' => 'На жаль, нічого не знайдено. Додайте запрошену особу нижче.',
+    'LBL_NO_ACCESS' => 'У вас немає доступу для створення $module',  // Create invitee functionallity
+
+    'LBL_REPEAT_TYPE' => 'Тип повторення',
+    'LBL_REPEAT_INTERVAL' => 'Інтервал повторення',
+    'LBL_REPEAT_DOW' => 'Повторити на день тижня',
+    'LBL_REPEAT_UNTIL' => 'Repeat Until',
+    'LBL_REPEAT_COUNT' => 'Повторити скільки разів',
+    'LBL_REPEAT_PARENT_ID' => 'Батьківський ID повторення',
+    'LBL_RECURRING_SOURCE' => 'Повторюване джерело',
+
+    'LBL_SYNCED_RECURRING_MSG' => 'Ця зустріч була створена в іншій системі і була синхронізована з SuiteCRM. Щоб внести зміни, перейдіть на оригінальну зустріч в іншій системі. Зміни, внесені в іншій системі, можуть бути синхронізовані з цим записом.',
+    'LBL_RELATED_TO' => 'Відноситься до: ',
+
+    // for reminders
+    'LBL_REMINDERS' => 'Reminders',
+    'LBL_REMINDERS_ACTIONS' => 'Дії:',
+    'LBL_REMINDERS_POPUP' => 'Popup',
+    'LBL_REMINDERS_EMAIL' => 'Відіслати листа запрошеним особам',
+    'LBL_REMINDERS_WHEN' => 'When:',
+    'LBL_REMINDERS_REMOVE_REMINDER' => 'Remove reminder',
+    'LBL_REMINDERS_ADD_ALL_INVITEES' => 'Додати усіх запрошених',
+    'LBL_REMINDERS_ADD_REMINDER' => 'Add reminder',
+
+    // for google sync
+    'LBL_GSYNC_ID' => 'ID події Google',
+    'LBL_GSYNC_LASTSYNC' => 'Остання відмітка часу синхронізації Google',
+);
