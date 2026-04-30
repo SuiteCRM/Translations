ed308e4471a068c8e56aa3e732a1be7408cde073
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-04-30 20:12:19 +0100

    New translations en_us.lang.php (Swedish)
    [ci skip]

---
+++ translations/sv_SE/modules/Project/language/sv_SE.lang.php
@@ -0,0 +1,183 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_MODULE_NAME' => 'Project',
+    'LBL_MODULE_TITLE' => 'Projekt: Hem',
+    'LBL_SEARCH_FORM_TITLE' => 'Sök projekt',
+    'LBL_LIST_FORM_TITLE' => 'Projektlista',
+    'LBL_HISTORY_TITLE' => 'History',
+    'LBL_ID' => 'ID:',
+    'LBL_DATE_ENTERED' => 'Datum skapad:',
+    'LBL_DATE_MODIFIED' => 'Date Modified:',
+    'LBL_ASSIGNED_USER_ID' => 'Assigned To:',
+    'LBL_ASSIGNED_USER_NAME' => 'Projektledare:',
+    'LBL_MODIFIED_USER_ID' => 'Modifierad användar-ID:',
+    'LBL_CREATED_BY' => 'Skapad av:',
+    'LBL_NAME' => 'Name:',
+    'LBL_DESCRIPTION' => 'Description:',
+    'LBL_DELETED' => 'Raderad:',
+    'LBL_DATE' => 'Date:',
+    'LBL_DATE_START' => 'Start Date:',
+    'LBL_DATE_END' => 'End Date:',
+    'LBL_PRIORITY' => 'Priority:',
+    'LBL_LIST_NAME' => 'Namn',
+    'LBL_LIST_TOTAL_ESTIMATED_EFFORT' => 'Total uppskattad insats (tim)',
+    'LBL_LIST_TOTAL_ACTUAL_EFFORT' => 'Total faktisk insats (tim)',
+    'LBL_LIST_END_DATE' => 'End Date',
+    'LBL_PROJECT_SUBPANEL_TITLE' => 'Projects',
+    'LBL_PROJECT_TASK_SUBPANEL_TITLE' => 'Project Tasks',
+    'LBL_OPPORTUNITY_SUBPANEL_TITLE' => 'Opportunities',
+    'LBL_PROJECT_PREDECESSOR_NONE' => 'None',
+    'LBL_ALL_PROJECTS' => 'Alla projekt',
+    'LBL_ALL_USERS' => 'Alla användare',
+    'LBL_ALL_CONTACTS' => 'Alla kontakter',
+
+    // quick create label
+    'LBL_NEW_FORM_TITLE' => 'Nytt projekt',
+    'LNK_NEW_PROJECT' => 'Create Project',
+    'LNK_PROJECT_LIST' => 'Visa projektlista',
+    'LNK_NEW_PROJECT_TASK' => 'Skapa Projektuppgift',
+    'LNK_PROJECT_TASK_LIST' => 'Projektuppgifter',
+    'LBL_DEFAULT_SUBPANEL_TITLE' => 'Projects',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Activities',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'History',
+    'LBL_CONTACTS_SUBPANEL_TITLE' => 'Contacts',
+    'LBL_ACCOUNTS_SUBPANEL_TITLE' => 'Konton',
+    'LBL_OPPORTUNITIES_SUBPANEL_TITLE' => 'Opportunities',
+    'LBL_CASES_SUBPANEL_TITLE' => 'Ärenden',
+    'LBL_BUGS_SUBPANEL_TITLE' => 'Bugs',
+    'LBL_TASK_ID' => 'ID',
+    'LBL_TASK_NAME' => 'Task Name',
+    'LBL_DURATION' => 'Duration',
+    'LBL_ACTUAL_DURATION' => 'Actual Duration',
+    'LBL_START' => 'Starta',
+    'LBL_FINISH' => 'Finish',
+    'LBL_PREDECESSORS' => 'Predecessors',
+    'LBL_PERCENT_COMPLETE' => '% Complete',
+    'LBL_MORE' => 'Mer...',
+    'LBL_OPPORTUNITIES' => 'Opportunities',
+    'LBL_NEXT_WEEK' => 'Next',
+    'LBL_PROJECT_INFORMATION' => 'Projekt översikt',
+    'LBL_EDITLAYOUT' => 'Redigera layout' /*for 508 compliance fix*/,
+    'LBL_PROJECT_TASKS_SUBPANEL_TITLE' => 'Project Tasks',
+    'LBL_VIEW_GANTT_TITLE' => 'Visa Gantt',
+    'LBL_VIEW_GANTT_DURATION' => 'Duration',
+    'LBL_TASK_TITLE' => 'Redigera uppgift',
+    'LBL_DURATION_TITLE' => 'Redigera varaktighet',
+    'LBL_LAG' => 'Lagg',
+    'LBL_DAYS' => 'Days',
+    'LBL_HOURS' => 'Hours',
+    'LBL_MONTHS' => 'Months',
+    'LBL_SUBTASK' => 'Task',
+    'LBL_MILESTONE_FLAG' => 'Milstolpe',
+    'LBL_ADD_NEW_TASK' => 'Add New Task',
+    'LBL_DELETE_TASK' => 'Ta bort uppgift',
+    'LBL_EDIT_TASK_PROPERTIES' => 'Redigera uppgiftens egenskaper.',
+    'LBL_PARENT_TASK_ID' => 'Parent Task Id',
+    'LBL_RESOURCE_CHART' => 'Resurskalendern',
+    'LBL_RELATIONSHIP_TYPE' => 'Relationstyp',
+    'LBL_ASSIGNED_TO' => 'Project Manager',
+    'LBL_AM_PROJECTTEMPLATES_PROJECT_1_FROM_AM_PROJECTTEMPLATES_TITLE' => 'Projektmall',
+    'LBL_STATUS' => 'Status:',
+    'LBL_LIST_ASSIGNED_USER_ID' => 'Project Manager',
+    'LBL_TOOLTIP_PROJECT_NAME' => 'Project',
+    'LBL_TOOLTIP_TASK_NAME' => 'Task Name',
+    'LBL_TOOLTIP_TITLE' => 'Uppgifter på denna dag',
+    'LBL_TOOLTIP_TASK_DURATION' => 'Duration',
+    'LBL_RESOURCE_TYPE_TITLE_USER' => 'Resource is a User',
+    'LBL_RESOURCE_TYPE_TITLE_CONTACT' => 'Resurs är en kontakt',
+    'LBL_RESOURCE_CHART_PREVIOUS_MONTH' => 'Previous Month',
+    'LBL_RESOURCE_CHART_NEXT_MONTH' => 'Next Month',
+    'LBL_RESOURCE_CHART_WEEK' => 'Week',
+    'LBL_RESOURCE_CHART_DAY' => 'Day',
+    'LBL_RESOURCE_CHART_WARNING' => 'No resources have been assigned to a project.',
+    'LBL_PROJECT_DELETE_MSG' => 'Are you sure you want to delete this Project and its related Tasks?',
+    'LBL_LIST_MY_PROJECT' => 'Mina projekt',
+    'LBL_LIST_ASSIGNED_USER' => 'Project Manager',
+    'LBL_UNASSIGNED' => 'Ej tilldelad',
+    'LBL_PROJECT_USERS_1_FROM_USERS_TITLE' => 'Resurser',
+
+    'LBL_EMAIL' => 'Email',
+    'LBL_PHONE' => 'Phone Office:',
+    'LBL_ADD_BUTTON' => 'Add',
+    'LBL_ADD_INVITEE' => 'Lägg till resurs',
+    'LBL_FIRST_NAME' => 'Förnamn',
+    'LBL_LAST_NAME' => 'Efternamn',
+    'LBL_SEARCH_BUTTON' => 'Search',
+    'LBL_EMPTY_SEARCH_RESULT' => 'Tyvärr, inga resultat hittades. Skapa en inbjudan nedan.',
+    'LBL_CREATE_INVITEE' => 'Create a Resource',
+    'LBL_CREATE_CONTACT' => 'Som kontakt',
+    'LBL_CREATE_AND_ADD' => 'Create & Add',
+    'LBL_CANCEL_CREATE_INVITEE' => 'Cancel',
+    'LBL_NO_ACCESS' => 'Du har inte behörighet att skapa $module',
+    'LBL_SCHEDULING_FORM_TITLE' => 'Resurslista',
+    'LBL_REMOVE' => 'Remove',
+    'LBL_VIEW_DETAIL' => 'View Details',
+    'LBL_OVERRIDE_BUSINESS_HOURS' => 'Anse vara arbetsdagar',
+
+    'LBL_IMPORT_PROJECTS' => 'Importera projekt',
+
+    'LBL_PROJECTS_SEARCH' => 'Sök projekt',
+    'LBL_USERS_SEARCH' => 'Sök användare',
+    'LBL_CONTACTS_SEARCH' => 'Välj kontakter',
+    'LBL_RESOURCE_CHART_SEARCH_BUTTON' => 'Search',
+
+    'LBL_CHART_TYPE' => 'Type',
+    'LBL_CHART_WEEKLY' => 'Weekly',
+    'LBL_CHART_MONTHLY' => 'Monthly',
+    'LBL_CHART_QUARTERLY' => 'Quarterly',
+
+    'LBL_RESOURCE_CHART_MONTH' => 'Month',
+    'LBL_RESOURCE_CHART_QUARTER' => 'Kvartal',
+
+    'LBL_PROJECT_CONTACTS_1_FROM_CONTACTS_TITLE' => 'Project Contacts from Contacts Title',
+    'LBL_AM_PROJECTTEMPLATES_PROJECT_1_FROM_PROJECT_TITLE' => 'Projektmallar: Projekt från projekt titel',
+    'LBL_AOS_QUOTES_PROJECT' => 'Quotes: Project',
+    'LBL_TASKS_NOT_FOUND' => 'There are no Tasks to build the Gantt Table',
+
+
+    'LBL_ASCENDING' => 'Ascending',
+    'LBL_DESCENDING' => 'Descending',
+);
