323a90701817dc3535290f0f6d203cd367908140
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-04-30 20:52:39 +0100

    New translations manifest.php (Hindi)
    [ci skip]

---
+++ translations/hi_IN/manifest.php
@@ -0,0 +1,25 @@
+<?php
+/******************************************
+ * SuiteCRM Translations
+ * @URL: https://crowdin.com/project/suitecrmtrans
+ * @author SuiteCRM Community via Crowdin
+ ******************************************/
+
+$manifest = array(
+    'name' => 'Hindi (India)',
+    'description' => 'अनुवाद: crowdin.com/project/suitecrmtranslations',
+    'type' => 'लैंगपैक',
+    'is_uninstallable' => 'हाँ',
+    'author' => 'SuiteCRM समुदाय',
+    'version' => '8.9-1',
+    'published_date' => '2026-01-16',
+);
+$installdefs = array(
+    'id' => 'hi_IN',
+    'image_dir' => '<<basepath>>/इमेजिस',
+    'copy' => array(
+        array('from' => '<basepath>/शामिल करना', 'to' => 'शामिल करें'),
+        array('from' => '<basepath>/मॉड्यूल', 'to' => 'मॉड्यूल'),
+        array('from' => '<basepath>/स्थापित करना', 'to' => 'स्थापित करना'),
+    )
+);
