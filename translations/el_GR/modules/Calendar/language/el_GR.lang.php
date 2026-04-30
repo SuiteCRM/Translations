55f1952c975ba7a209831a3c1666296f6fbde323
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-04-30 18:50:53 +0100

    New translations en_us.lang.php (Greek)
    [ci skip]

---
+++ translations/el_GR/modules/Calendar/language/el_GR.lang.php
@@ -0,0 +1,225 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+
+    'LBL_SHAREDWEEK' => 'Shared Week',
+    'LBL_SHAREDMONTH' => 'Shared Month',
+
+    'LBL_MODULE_NAME' => 'Ημερολόγιο',
+    'LBL_MODULE_TITLE' => 'Ημερολόγιο',
+    'LNK_NEW_CALL' => 'Προγραμματισμός κλήσης',
+    'LNK_NEW_MEETING' => 'Προγραμματισμός Συνάντησης',
+    'LNK_NEW_TASK' => 'Δημιουργία Εργασίας',
+    'LNK_CALL_LIST' => 'Τηλεφωνικές Κλήσεις',
+    'LNK_MEETING_LIST' => 'Meetings',
+    'LNK_TASK_LIST' => 'Εργασίες',
+    'LNK_TASK' => 'Task',
+    'LNK_TASK_VIEW' => 'Προβολή Εργασιας',
+    'LNK_EVENT' => 'Event',
+    'LNK_EVENT_VIEW' => 'Προβολή Συμβάντος',
+    'LNK_VIEW_CALENDAR' => 'Today',
+    'LNK_IMPORT_CALLS' => 'Import Calls',
+    'LNK_IMPORT_MEETINGS' => 'Import Meetings',
+    'LNK_IMPORT_TASKS' => 'Import Tasks',
+    'LBL_MONTH' => 'Month',
+    'LBL_AGENDADAY' => 'Day',
+    'LBL_YEAR' => 'Έτος',
+
+    'LBL_AGENDAWEEK' => 'Εβδομάδα',
+    'LBL_PREVIOUS_MONTH' => 'Προηγούμενος μήνας',
+    'LBL_PREVIOUS_DAY' => 'Προηγούμενη ημέρα',
+    'LBL_PREVIOUS_YEAR' => 'Προηγούμενο έτος',
+    'LBL_PREVIOUS_WEEK' => 'Προηγούμενη εβδομάδα',
+    'LBL_NEXT_MONTH' => 'Επόμενο Μήνα',
+    'LBL_NEXT_DAY' => 'Επόμενη ημέρα',
+    'LBL_NEXT_YEAR' => 'Επόμενο Ετος',
+    'LBL_NEXT_WEEK' => 'Επόμενη εβδομάδα',
+    'LBL_AM' => 'Π.Μ',
+    'LBL_PM' => 'Μ.Μ',
+    'LBL_SCHEDULED' => 'Προγραμματισμένο',
+    'LBL_BUSY' => 'Απασχολημένο',
+    'LBL_CONFLICT' => 'Διένεξη',
+    'LBL_USER_CALENDARS' => 'Ημερολόγια χρήστη',
+    'LBL_SHARED' => 'Shared',
+    'LBL_PREVIOUS_SHARED' => 'Προηγούμενη',
+    'LBL_NEXT_SHARED' => 'Επόμενη',
+    'LBL_SHARED_CAL_TITLE' => 'Κοινοχρηστο ημερολογιο',
+    'LBL_USERS' => 'Χειριστές',
+    'LBL_REFRESH' => 'Ανανέωση',
+    'LBL_EDIT_USERLIST' => 'Λίστα Χειριστών',
+    'LBL_SELECT_USERS' => 'Επιλέξτε χρήστες για την εμφάνιση του ημερολογίου',
+    'LBL_FILTER_BY_TEAM' => 'Κατάλογος χρηστών ανά ομάδα:',
+    'LBL_ASSIGNED_TO_NAME' => 'Ανατέθηκε σε',
+    'LBL_DATE' => 'Ημερομηνία & Ώρα Εκκίνησης',
+    'LBL_CREATE_MEETING' => 'Προγραμματισμός Συνάντησης',
+    'LBL_CREATE_CALL' => 'Προγραμματισμός Κλήσης',
+    'LBL_HOURS_ABBREV' => 'ω',
+    'LBL_MINS_ABBREV' => 'λ',
+
+
+    'LBL_YES' => 'Ναί',
+    'LBL_NO' => 'κανένα',
+    'LBL_SETTINGS' => 'Settings',
+    'LBL_CREATE_NEW_RECORD' => 'Δημιουργία Δραστηριότητας',
+    'LBL_LOADING' => 'Φορτώνει...',
+    'LBL_SAVING' => 'Saving...',
+    'LBL_SENDING_INVITES' => 'Saving & Sending Invites...',
+    'LBL_CONFIRM_REMOVE' => 'Είστε βέβαιοι ότι θέλετε να καταργήσετε την εγγραφή;',
+    'LBL_CONFIRM_REMOVE_ALL_RECURRING' => 'Are you sure you want to remove all recurring records?',
+    'LBL_EDIT_RECORD' => 'Επεξεργασία Δραστηριότητας',
+    'LBL_ERROR_SAVING' => 'Error while saving',
+    'LBL_ERROR_LOADING' => 'Error while loading',
+    'LBL_GOTO_DATE' => 'Go to Date',
+    'NOTICE_DURATION_TIME' => 'Duration time must be greater than 0',
+    'LBL_STYLE_BASIC' => 'BASIC', //No need to be translated in all caps. Translation used just in menu action items when using the SuiteP template
+    'LBL_STYLE_ADVANCED' => 'Για προχωρημενους', //No need to be translated in all caps. Translation used just in menu action items when using the SuiteP template
+
+    'LBL_NO_USER' => 'No match for field: Assigned to',
+    'LBL_SUBJECT' => 'Subject',
+    'LBL_DURATION' => 'Duration',
+    'LBL_STATUS' => 'Κατάσταση',
+    'LBL_PRIORITY' => 'Priority',
+
+    'LBL_SETTINGS_TITLE' => 'Settings',
+    'LBL_SETTINGS_DISPLAY_TIMESLOTS' => 'Display time slots in Day and Week views:',
+    'LBL_SETTINGS_TIME_STARTS' => 'Start time:',
+    'LBL_SETTINGS_TIME_ENDS' => 'End time:',
+    'LBL_SETTINGS_CALLS_SHOW' => 'Εμφάνιση Κλήσεων',
+    'LBL_SETTINGS_TASKS_SHOW' => 'Show Tasks:',
+    'LBL_SETTINGS_COMPLETED_SHOW' => 'Show Completed Meetings, Calls and Tasks:',
+    'LBL_SETTINGS_DISPLAY_SHARED_CALENDAR_SEPARATE' => 'Shared Calendar Separate:',
+
+    'LBL_SAVE_BUTTON' => 'Save',
+    'LBL_DELETE_BUTTON' => 'Διαγραφή',
+    'LBL_APPLY_BUTTON' => 'Apply',
+    'LBL_SEND_INVITES' => 'Αποστολή προσκλήσεων',
+    'LBL_CANCEL_BUTTON' => 'Ακύρωση',
+    'LBL_CLOSE_BUTTON' => 'Close',
+
+    'LBL_GENERAL_TAB' => 'Details',
+    'LBL_PARTICIPANTS_TAB' => 'Invitees',
+    'LBL_REPEAT_TAB' => 'Recurrence',
+
+    'LBL_REPEAT_TYPE' => 'Επανάληψη',
+    'LBL_REPEAT_INTERVAL' => 'Every',
+    'LBL_REPEAT_END' => 'End',
+    'LBL_REPEAT_END_AFTER' => 'Μετά',
+    'LBL_REPEAT_OCCURRENCES' => 'recurrences',
+    'LBL_REPEAT_END_BY' => 'By',
+    'LBL_REPEAT_DOW' => 'Στις',
+    'LBL_REPEAT_UNTIL' => 'Επαναλάψη μέχρι',
+    'LBL_REPEAT_COUNT' => 'Number of recurrences',
+    'LBL_REPEAT_LIMIT_ERROR' => 'Your request was going to create more than $limit meetings.',
+
+    'LBL_EDIT_ALL_RECURRENCES' => 'Edit All Recurrences',
+    'LBL_REMOVE_ALL_RECURRENCES' => 'Delete All Recurrences',
+
+    'LBL_DATE_END_ERROR' => 'End date is before start date',
+    'ERR_YEAR_BETWEEN' => 'Sorry, calendar cannot handle the year you requested<br>Year must be between 1970 and 2037',
+    'ERR_NEIGHBOR_DATE' => 'get_neighbor_date_str: not defined for this view',
+    'LBL_NO_ITEMS_MOBILE' => 'Your calendar is clear for the week.',
+    'LBL_GENERAL_SETTINGS' => 'General Settings',
+    'LBL_COLOR_SETTINGS' => 'Colour Settings',
+    'LBL_MODULE' => 'Ενότητα',
+    'LBL_BODY' => 'Σώμα',
+    'LBL_BORDER' => 'Περιθώριο',
+    'LBL_TEXT' => 'Text',
+);
+
+
+$mod_list_strings = array(
+    'dom_cal_weekdays' =>
+        array(
+            '0' => "Sun",
+            '1' => "Δευτ",
+            '2' => "Τρ",
+            '3' => "Wed",
+            '4' => "Πέμ",
+            '5' => "Fri",
+            '6' => "Σάβ",
+        ),
+    'dom_cal_weekdays_long' =>
+        array(
+            '0' => "Sunday",
+            '1' => "Monday",
+            '2' => "Tuesday",
+            '3' => "Wednesday",
+            '4' => "Thursday",
+            '5' => "Friday",
+            '6' => "Σάββατο",
+        ),
+    'dom_cal_month' =>
+        array(
+            '0' => "",
+            '1' => "Jan",
+            '2' => "Feb",
+            '3' => "Mar",
+            '4' => "Apr",
+            '5' => "Μάιος",
+            '6' => "Jun",
+            '7' => "Ιούλ",
+            '8' => "Aug",
+            '9' => "Sep",
+            '10' => "Oct",
+            '11' => "Nov",
+            '12' => "Dec",
+        ),
+    'dom_cal_month_long' =>
+        array(
+            '0' => "",
+            '1' => "January",
+            '2' => "February",
+            '3' => "March",
+            '4' => "April",
+            '5' => "Μάιος",
+            '6' => "June",
+            '7' => "July",
+            '8' => "August",
+            '9' => "September",
+            '10' => "October",
+            '11' => "November",
+            '12' => "December",
+        ),
+);
