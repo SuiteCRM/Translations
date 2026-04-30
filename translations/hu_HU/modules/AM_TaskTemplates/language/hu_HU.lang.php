678241b2a2dc853a11eace809d8b786c8289024c
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-04-30 18:07:33 +0100

    New translations en_us.lang.php (Hungarian)
    [ci skip]

---
+++ translations/hu_HU/modules/AM_TaskTemplates/language/hu_HU.lang.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID' => 'Felelős felhasználó ID',
+    'LBL_ASSIGNED_TO_NAME' => 'Felelős',
+    'LBL_ID' => 'ID',
+    'LBL_DATE_ENTERED' => 'Létrehozva',
+    'LBL_DATE_MODIFIED' => 'Módosítás dátuma',
+    'LBL_MODIFIED' => 'Modified By',
+    'LBL_MODIFIED_ID' => 'Módosító Id',
+    'LBL_MODIFIED_NAME' => 'Modified By Name',
+    'LBL_CREATED' => 'Létrehozta',
+    'LBL_CREATED_ID' => 'Készítette: Id',
+    'LBL_DESCRIPTION' => 'Üzenet tartalma',
+    'LBL_DELETED' => 'Deleted',
+    'LBL_NAME' => 'Task Name',
+    'LBL_CREATED_USER' => 'Felhasználó által létrehozva',
+    'LBL_MODIFIED_USER' => 'Felhasználó által módosítva',
+    'LBL_LIST_NAME' => 'Elnevezés',
+    'LBL_EDIT_BUTTON' => 'Szerkesztés',
+    'LBL_REMOVE' => 'Eltávolítás',
+    'LBL_LIST_FORM_TITLE' => 'Projekt tevékenységlista sablonok',
+    'LBL_MODULE_NAME' => 'Project Task Templates',
+    'LBL_MODULE_TITLE' => 'Project Task Templates',
+    'LBL_HOMEPAGE_TITLE' => 'Saját Projekt Feladat Sablonjaim',
+    'LNK_NEW_RECORD' => 'Projekt feladat létrehozása sablonok',
+    'LNK_LIST' => 'Projekt feladat áttekintése sablonok',
+    'LNK_IMPORT_AM_TASKTEMPLATES' => 'Projekt feladat iportálása sablonok',
+    'LBL_SEARCH_FORM_TITLE' => 'Projekt feladat keresés sablonok',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Előzmény Megtekintése',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Activities',
+    'LBL_AM_TASKTEMPLATES_SUBPANEL_TITLE' => 'Project Task Templates',
+    'LBL_NEW_FORM_TITLE' => 'Új projekt feladat-sablonok',
+    'LBL_STATUS' => 'Status',
+    'LBL_PRIORITY' => 'Priority',
+    'LBL_PERCENT_COMPLETE' => 'Kész:',
+    'LBL_PREDECESSORS' => 'Előzmények',
+    'LBL_MILESTONE_FLAG' => 'Mérföldkő zászló',
+    'LBL_RELATIONSHIP_TYPE' => 'Összefüggés típusa',
+    'LBL_TASK_NUMBER' => 'Feladatszám',
+    'LBL_ORDER_NUMBER' => 'Rendelés ',
+    'LBL_ESTIMATED_EFFORT' => 'Becsült Ráfordítás (órák)',
+    'LBL_UTILIZATION' => 'Hasznosítás (%)',
+    'LBL_DURATION' => 'Időtartam (napok)',
+    'LBL_AM_TASKTEMPLATES_AM_PROJECTTEMPLATES_FROM_AM_PROJECTTEMPLATES_TITLE' => 'Project Templates',
+
+    'LBL_AM_TASKTEMPLATES_AM_PROJECTTEMPLATES_FROM_AM_TASKTEMPLATES_TITLE' => 'Feladat sablonok: A feladatnév sablonok Project-sablonok',
+);
