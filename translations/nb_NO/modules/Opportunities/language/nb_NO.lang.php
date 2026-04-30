c84fd55f92b4298b6808b9cdc9a3a85c7955148a
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-04-30 20:07:11 +0100

    New translations en_us.lang.php (Norwegian Bokmal)
    [ci skip]

---
+++ translations/nb_NO/modules/Opportunities/language/nb_NO.lang.php
@@ -0,0 +1,118 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_MODULE_NAME' => 'Opportunities',
+    'LBL_MODULE_TITLE' => 'Salgsmuligheter: Forside',
+    'LBL_SEARCH_FORM_TITLE' => 'Søk etter salgsmulighet',
+    'LBL_LIST_FORM_TITLE' => 'Liste over salgsmuligheter',
+    'LBL_OPPORTUNITY_NAME' => 'Opportunity Name:',
+    'LBL_OPPORTUNITY' => 'Salgsmulighet:',
+    'LBL_NAME' => 'Opportunity Name',
+    'LBL_INVITEE' => 'Contacts',
+    'LBL_CURRENCIES' => 'Currencies',
+    'LBL_LIST_OPPORTUNITY_NAME' => 'Name',
+    'LBL_LIST_ACCOUNT_NAME' => 'Account Name',
+    'LBL_LIST_AMOUNT' => 'Forcasted beløp',
+    'LBL_LIST_AMOUNT_USDOLLAR' => 'Amount',
+    'LBL_LIST_DATE_CLOSED' => 'Close',
+    'LBL_LIST_SALES_STAGE' => 'Sales Stage',
+    'LBL_ACCOUNT_ID' => 'Account ID',
+    'LBL_CURRENCY_NAME' => 'Currency Name',
+    'LBL_CURRENCY_SYMBOL' => 'Currency Symbol',
+    'LBL_DAYS_OPEN_FOR' => 'This Opportunity Was Open For',
+
+    'UPDATE' => 'Mulighet - valuta oppdatering',
+    'LBL_ACCOUNT_NAME' => 'Account Name:',
+    'LBL_AMOUNT' => 'Opportunity Amount:',
+    'LBL_AMOUNT_USDOLLAR' => 'Amount:',
+    'LBL_CURRENCY' => 'Currency:',
+    'LBL_DATE_CLOSED' => 'Expected Close Date:',
+    'LBL_TYPE' => 'Type:',
+    'LBL_CAMPAIGN' => 'Campaign:',
+    'LBL_NEXT_STEP' => 'Next Step:',
+    'LBL_LEAD_SOURCE' => 'Lead Source:',
+    'LBL_SALES_STAGE' => 'Sales Stage:',
+    'LBL_PROBABILITY' => 'Probability (%):',
+    'LBL_DESCRIPTION' => 'Beskrivelse:',
+    'LBL_DUPLICATE' => 'Mulig duplikat av en salgsmulighet',
+    'MSG_DUPLICATE' => 'Opprettelse av denne salgsmuligheten kan føre til to identiske salgsmuligheter. Du kan enten velge en salgsmulighet fra listen under, eller klikke på <b>Opprett ny salgsmulighet</b> for å fortsette.',
+    'LBL_NEW_FORM_TITLE' => 'Create Opportunity',
+    'LNK_NEW_OPPORTUNITY' => 'Create Opportunity',
+    'LNK_OPPORTUNITY_LIST' => 'Salgsmuligheter',
+    'ERR_DELETE_RECORD' => 'Oppgi referansenr for å slette det salgsmuligheten.',
+    'LBL_TOP_OPPORTUNITIES' => 'Største salgsmuligheter',
+    'OPPORTUNITY_REMOVE_PROJECT_CONFIRM' => 'Er du sikker på at du vil fjerne denne salgsmuligheten fra prosjektet?',
+    'LBL_DEFAULT_SUBPANEL_TITLE' => 'Opportunities',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Activities',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'History',
+
+    'LBL_LEADS_SUBPANEL_TITLE' => 'Leads',
+    'LBL_CONTACTS_SUBPANEL_TITLE' => 'Contacts',
+    'LBL_DOCUMENTS_SUBPANEL_TITLE' => 'Documents',
+    'LBL_PROJECTS_SUBPANEL_TITLE' => 'Projects',
+    'LBL_ASSIGNED_TO_NAME' => 'Assigned to:',
+    'LBL_LIST_ASSIGNED_TO_NAME' => 'Assigned User',
+    'LBL_MY_CLOSED_OPPORTUNITIES' => 'Mine lukkede salgsmuligheter',
+    'LBL_TOTAL_OPPORTUNITIES' => 'Total antall muligheter',
+    'LBL_CLOSED_WON_OPPORTUNITIES' => 'Muligheter avsluttet og vunnet',
+    'LBL_ASSIGNED_TO_ID' => 'Assigned User:',
+    'LBL_MODIFIED_NAME' => 'Modified by User Name',
+    'LBL_CREATED_USER' => 'Opprettet bruker',
+    'LBL_MODIFIED_USER' => 'Modified User',
+    'LBL_CAMPAIGN_OPPORTUNITY' => 'Campaigns',
+    'LBL_PROJECT_SUBPANEL_TITLE' => 'Projects',
+    'LNK_IMPORT_OPPORTUNITIES' => 'Importer salgsmuligheter',
+    'LBL_EDITLAYOUT' => 'Edit Layout'
+    /*for 508 compliance fix*/,
+
+    // SNIP
+
+    'LBL_AOS_CONTRACTS' => 'Contracts',
+    'LBL_AOS_QUOTES' => 'Quotes',
+
+    'LBL_SIZE_ANALYSIS' => 'Size Analysis',
+    'LBL_POSITION' => 'Position',
+    'LBL_OUT_OF' => 'Out of',
+);
