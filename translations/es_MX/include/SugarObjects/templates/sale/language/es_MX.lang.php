2fb53b1f98aa7dc689b4d39b5612df7e5fa96f8e
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-04-30 17:55:26 +0100

    New translations en_us.lang.php (Spanish, Mexico)
    [ci skip]

---
+++ translations/es_MX/include/SugarObjects/templates/sale/language/es_MX.lang.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_MODULE_NAME' => 'Venta',
+    'LBL_MODULE_TITLE' => 'Venta: Inicio',
+    'LBL_SEARCH_FORM_TITLE' => 'Ventas: Buscar',
+    'LBL_LIST_FORM_TITLE' => 'Ventas: Lista ',
+    'LBL_NAME' => 'Nombre',
+    'LBL_LIST_SALE_NAME' => 'Nombre',
+    'LBL_LIST_ACCOUNT_NAME' => 'Nombre de Cuenta',
+    'LBL_LIST_AMOUNT' => 'Cantidad',
+    'LBL_LIST_DATE_CLOSED' => 'Cerrar',
+    'LBL_LIST_SALE_STAGE' => 'Etapa de ventas',
+    'LBL_ACCOUNT_ID' => 'ID de la cuenta',
+    //DON'T CONVERT THESE THEY ARE MAPPINGS
+    'db_name' => 'LBL_NAME',
+    //END DON'T CONVERT
+    'LBL_ACCOUNT_NAME' => 'Nombre de la cuenta:',
+    'LBL_AMOUNT' => 'Cantidad en dólares EUA:',
+    'LBL_AMOUNT_USDOLLAR' => 'Cantidad en Dólares EEUU:',
+    'LBL_CURRENCY' => 'Moneda:',
+    'LBL_DATE_CLOSED' => 'Fecha de cierre:',
+    'LBL_TYPE' => 'Type:',
+    'LBL_CAMPAIGN' => 'Campaña:',
+    'LBL_LEADS_SUBPANEL_TITLE' => 'Clientes potenciales',
+    'LBL_PROJECTS_SUBPANEL_TITLE' => 'Proyectos',
+    'LBL_NEXT_STEP' => 'Siguiente paso:',
+    'LBL_LEAD_SOURCE' => 'Origen de clientes potenciales:',
+    'LBL_SALES_STAGE' => 'Etapa de ventas:',
+    'LBL_PROBABILITY' => 'Probabilidad (%):',
+    'LBL_DESCRIPTION' => 'Descripción:',
+    'LBL_DUPLICATE' => 'Posible venta duplicada',
+    'MSG_DUPLICATE' => 'El registro para la venta que va a crear podría ser un duplicado de otro registro de venta existente. Los registros de venta con nombres similares se listan a continuación.<br>Haga clic en Guardar para continuar con la creación de esta venta, o en Cancelar para volver al módulo sin crear la venta',
+    'LBL_NEW_FORM_TITLE' => 'Nueva venta',
+    'ERR_DELETE_RECORD' => 'Debe de especificar un número de registro para eliminar la venta',
+    'LBL_DEFAULT_SUBPANEL_TITLE' => 'Sale',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Activities',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Historial',
+
+    'LBL_CONTACTS_SUBPANEL_TITLE' => 'Contactos',
+    'LBL_ASSIGNED_TO_NAME' => 'User:',
+    'LBL_LIST_ASSIGNED_TO_NAME' => 'Usuario asignado',
+    'LBL_ASSIGNED_TO_ID' => 'Asignada a ID',
+    'LBL_MODIFIED_NAME' => 'Modificada por usuario',
+    'LBL_SALE_INFORMATION' => 'Información de la venta',
+    'LBL_CURRENCY_NAME' => 'Nombre de la divisa',
+    'LBL_CURRENCY_SYMBOL' => 'Símbolo de divisa',
+    'LBL_EDIT_BUTTON' => 'Editar',
+    'LBL_REMOVE' => 'Quitar',
+
+);
