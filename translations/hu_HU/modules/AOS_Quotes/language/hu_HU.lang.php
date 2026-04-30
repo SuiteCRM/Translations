0d76049f5f903107234609faab8261b814a8c4ae
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-04-30 18:34:08 +0100

    New translations en_us.lang.php (Hungarian)
    [ci skip]

---
+++ translations/hu_HU/modules/AOS_Quotes/language/hu_HU.lang.php
@@ -0,0 +1,207 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID' => 'Felelős felhasználó ID',
+    'LBL_ASSIGNED_TO_NAME' => 'Felelős',
+    'LBL_ID' => 'ID',
+    'LBL_DATE_ENTERED' => 'Létrehozva',
+    'LBL_DATE_MODIFIED' => 'Módosítás dátuma',
+    'LBL_MODIFIED' => 'Modified By',
+    'LBL_MODIFIED_NAME' => 'Modified By Name',
+    'LBL_CREATED' => 'Létrehozta',
+    'LBL_DESCRIPTION' => 'Üzenet tartalma',
+    'LBL_DELETED' => 'Deleted',
+    'LBL_NAME' => 'Megnevezés',
+    'LBL_CREATED_USER' => 'Felhasználó által létrehozva',
+    'LBL_MODIFIED_USER' => 'Felhasználó által módosítva',
+    'ERR_DELETE_RECORD' => 'A record number must be specified to delete the account.',
+    'LBL_ACCOUNT_NAME' => 'Megnevezés',
+    'LBL_ACCOUNT' => 'Company:',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Activities',
+    'LBL_ADDRESS_INFORMATION' => 'Cím információ',
+    'LBL_ANNUAL_REVENUE' => 'Éves bevétel:',
+    'LBL_ANY_ADDRESS' => 'Cím:',
+    'LBL_ANY_EMAIL' => 'Any Email:',
+    'LBL_ANY_PHONE' => 'Any Phone:',
+    'LBL_RATING' => 'Minősítés:',
+    'LBL_ASSIGNED_USER' => 'Felhasználó',
+    'LBL_BILLING_ADDRESS_CITY' => 'Számlázási cím, város:',
+    'LBL_BILLING_ADDRESS_COUNTRY' => 'Számlázási ország:',
+    'LBL_BILLING_ADDRESS_POSTALCODE' => 'Számlázási irányítószám:',
+    'LBL_BILLING_ADDRESS_STATE' => 'Számlázási megye/régió:',
+    'LBL_BILLING_ADDRESS_STREET_2' => 'Számlázási cím, utca 2',
+    'LBL_BILLING_ADDRESS_STREET_3' => 'Számlázási cím, utca 3',
+    'LBL_BILLING_ADDRESS_STREET_4' => 'Számlázási cím, utca 4',
+    'LBL_BILLING_ADDRESS_STREET' => 'Számlázási cím, utca:',
+    'LBL_BILLING_ADDRESS' => 'Számlázási cím:',
+    'LBL_ACCOUNT_INFORMATION' => 'Áttekintés',
+    'LBL_CITY' => 'Város:',
+    'LBL_CONTACTS_SUBPANEL_TITLE' => 'Kapcsolattartók:',
+    'LBL_COUNTRY' => 'Country:',
+    'LBL_DEFAULT_SUBPANEL_TITLE' => 'Ügyfelek',
+    'LBL_DUPLICATE' => 'Possible Duplicate Account',
+    'LBL_EMAIL' => 'Email:',
+    'LBL_EMPLOYEES' => 'Alkalmazottak:',
+    'LBL_FAX' => 'Fax:',
+    'LBL_INDUSTRY' => 'Industry:',
+    'LBL_LIST_ACCOUNT_NAME' => 'Ügyfélnév',
+    'LBL_LIST_CITY' => 'Város',
+    'LBL_LIST_EMAIL_ADDRESS' => 'Email:',
+    'LBL_LIST_PHONE' => 'Phone',
+    'LBL_LIST_STATE' => 'Megye/régió',
+    'LBL_MEMBER_OF' => 'Member of:',
+    'LBL_MEMBER_ORG_SUBPANEL_TITLE' => 'Member Organizations',
+    'LBL_OTHER_EMAIL_ADDRESS' => 'Egyéb eMail:',
+    'LBL_OTHER_PHONE' => 'Other Phone:',
+    'LBL_OWNERSHIP' => 'Ownership:',
+    'LBL_PARENT_ACCOUNT_ID' => 'Parent Account ID',
+    'LBL_PHONE_ALT' => 'Alternate Phone:',
+    'LBL_PHONE_FAX' => 'Phone Fax:',
+    'LBL_PHONE_OFFICE' => 'Phone Office:',
+    'LBL_PHONE' => 'Phone:',
+    'LBL_POSTAL_CODE' => 'Postal Code:',
+    'LBL_SAVE_ACCOUNT' => 'Save Account',
+    'LBL_SHIPPING_ADDRESS_CITY' => 'Shipping City:',
+    'LBL_SHIPPING_ADDRESS_COUNTRY' => 'Shipping Country:',
+    'LBL_SHIPPING_ADDRESS_POSTALCODE' => 'Shipping Postal Code:',
+    'LBL_SHIPPING_ADDRESS_STATE' => 'Shipping State:',
+    'LBL_SHIPPING_ADDRESS_STREET_2' => 'Shipping Street 2',
+    'LBL_SHIPPING_ADDRESS_STREET_3' => 'Shipping Street 3',
+    'LBL_SHIPPING_ADDRESS_STREET_4' => 'Shipping Street 4',
+    'LBL_SHIPPING_ADDRESS_STREET' => 'Shipping Street:',
+    'LBL_SHIPPING_ADDRESS' => 'Shipping Address:',
+    'LBL_STATE' => 'State/Region:',
+    'LBL_TICKER_SYMBOL' => 'Ticker Symbol:',
+    'LBL_TYPE' => 'Type:',
+    'LBL_WEBSITE' => 'Website:',
+    'LNK_ACCOUNT_LIST' => 'Ügyfelek',
+    'LNK_NEW_ACCOUNT' => 'Create Account',
+    'MSG_DUPLICATE' => 'Creating this account may potentially create a duplicate account. You may either select an account from the list below or you may click on Save to continue creating a new account with the previously entered data.',
+    'MSG_SHOW_DUPLICATES' => 'Creating this account may potentially create a duplicate account. You may either click on Save to continue creating this new account with the previously entered data or you may click Cancel.',
+    'NTC_DELETE_CONFIRMATION' => 'Are you sure you want to delete this record?',
+    'NTC_SNOOZE_CONFIRMATION' => 'Biztos beállítja a szüneteltetést?',
+    'LBL_LIST_FORM_TITLE' => 'Ajánlat lista',
+    'LBL_MODULE_NAME' => 'Quotes',
+    'LBL_MODULE_TITLE' => 'Ajánlat: Főoldal',
+    'LBL_HOMEPAGE_TITLE' => 'Ajánlataim',
+    'LNK_NEW_RECORD' => 'Ajánlat létrehozása',
+    'LNK_LIST' => 'Ajánlat nézet',
+    'LBL_SEARCH_FORM_TITLE' => 'Ajánlat keresése',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Előzmény Megtekintése',
+    'LBL_NEW_FORM_TITLE' => 'Ajánlat létrehozása',
+    'LBL_TERMS_C' => 'Terms',
+    'LBL_APPROVAL_ISSUE' => 'Approval Issues',
+    'LBL_APPROVAL_STATUS' => 'Approval Status',
+    'LBL_BILLING_ACCOUNT' => 'Ügyfél',
+    'LBL_BILLING_CONTACT' => 'Kapcsolattartó',
+    'LBL_EXPIRATION' => 'Valid Until',
+    'LBL_QUOTE_NUMBER' => 'Quote Number',
+    'LBL_OPPORTUNITY' => 'Opportunity',
+    'LBL_TEMPLATE_DDOWN_C' => 'Ajánlat sablon',
+    'LBL_STAGE' => 'Quote Stage',
+    'LBL_TERM' => 'Payment Terms',
+    'LBL_SUBTOTAL_AMOUNT' => 'Subtotal',
+    'LBL_DISCOUNT_AMOUNT' => 'Discount',
+    'LBL_TAX_AMOUNT' => 'Tax',
+    'LBL_SHIPPING_AMOUNT' => 'Shipping',
+    'LBL_TOTAL_AMT' => 'Összesen',
+    'VALUE' => 'Megnevezés',
+    'LBL_EMAIL_ADDRESSES' => 'Email Addresses',
+    'LBL_LINE_ITEMS' => 'Line Items',
+    'LBL_GRAND_TOTAL' => 'Grand Total',
+    'LBL_INVOICE_STATUS' => 'Invoice Status',
+    'LBL_PRODUCT_QUANITY' => 'Quantity',
+    'LBL_PRODUCT_NAME' => 'Product',
+    'LBL_PART_NUMBER' => 'Part Number',
+    'LBL_PRODUCT_NOTE' => 'Note',
+    'LBL_PRODUCT_DESCRIPTION' => 'Üzenet tartalma',
+    'LBL_LIST_PRICE' => 'List',
+    'LBL_DISCOUNT_AMT' => 'Discount',
+    'LBL_UNIT_PRICE' => 'Sale Price',
+    'LBL_TOTAL_PRICE' => 'Összesen',
+    'LBL_VAT' => 'Tax', // VAT
+    'LBL_VAT_AMT' => 'Tax Amount', // VAT
+    'LBL_ADD_PRODUCT_LINE' => 'Add Product Line',
+    'LBL_SERVICE_NAME' => 'Service',
+    'LBL_SERVICE_LIST_PRICE' => 'List',
+    'LBL_SERVICE_PRICE' => 'Sale Price',
+    'LBL_SERVICE_DISCOUNT' => 'Discount',
+    'LBL_ADD_SERVICE_LINE' => 'Add Service Line ',
+    'LBL_REMOVE_PRODUCT_LINE' => 'Eltávolítás',
+    'LBL_CONVERT_TO_INVOICE' => 'Számlává konvertál',
+    'LBL_PRINT_AS_PDF' => 'Print as PDF',
+    'LBL_EMAIL_QUOTE' => 'Email ajánlat',
+    'LBL_CREATE_CONTRACT' => 'Create Contract',
+    'LBL_LIST_NUM' => 'Num',
+    'LBL_PDF_NAME' => 'Quote',
+    'LBL_EMAIL_NAME' => 'Ajánlat részére',
+    'LBL_QUOTE_DATE' => 'Ajánlat dátuma',
+    'LBL_NO_TEMPLATE' => 'Hiba\nNem található sablon. Kérem, a PDF-sablonok modulban hozzon létre egyet!',
+    'LBL_SUBTOTAL_TAX_AMOUNT' => 'Subtotal + Tax',//pre shipping
+    'LBL_EMAIL_PDF' => 'Email PDF',
+    'LBL_ADD_GROUP' => 'Add Group',
+    'LBL_DELETE_GROUP' => 'Delete Group',
+    'LBL_GROUP_NAME' => 'Group Name',
+    'LBL_GROUP_TOTAL' => 'Group Total',
+    'LBL_SHIPPING_TAX' => 'Shipping Tax',
+    'LBL_SHIPPING_TAX_AMT' => 'Shipping Tax',
+    'LBL_IMPORT_LINE_ITEMS' => 'Import Line Items',
+    'LBL_CREATE_OPPORTUNITY' => 'Create Opportunity',
+    'LBL_SUBTOTAL_AMOUNT_USDOLLAR' => 'Subtotal (Default Currency)',
+    'LBL_DISCOUNT_AMOUNT_USDOLLAR' => 'Discount (Default Currency)',
+    'LBL_TAX_AMOUNT_USDOLLAR' => 'Tax (Default Currency)',
+    'LBL_SHIPPING_AMOUNT_USDOLLAR' => 'Shipping (Default Currency)',
+    'LBL_TOTAL_AMT_USDOLLAR' => 'Total (Default Currency)',
+    'LBL_SHIPPING_TAX_AMT_USDOLLAR' => 'Shipping Tax (Default Currency)',
+    'LBL_GRAND_TOTAL_USDOLLAR' => 'Grand Total (Default Currency)',
+    'LBL_QUOTE_TO' => 'Ajánlat részére',
+
+    'LBL_SUBTOTAL_TAX_AMOUNT_USDOLLAR' => 'Subtotal + Tax (Default Currency)',
+    'LBL_AOS_QUOTES_AOS_CONTRACTS' => 'Quotes: Contracts',
+    'LBL_AOS_QUOTES_AOS_INVOICES' => 'Quotes: Invoices',
+    'LBL_AOS_LINE_ITEM_GROUPS' => 'Line Item Groups',
+    'LBL_AOS_PRODUCT_QUOTES' => 'Termék idézetek',
+    'LBL_AOS_QUOTES_PROJECT' => 'Árajánlat: Projekt',
+);
