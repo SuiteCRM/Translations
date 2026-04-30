abda646add7e331560811c2839fef9703a73d019
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-04-30 18:41:27 +0100

    New translations en_us.lang.php (Chinese Simplified)
    [ci skip]

---
+++ translations/zh_CN/modules/Accounts/language/zh_CN.lang.php
@@ -0,0 +1,165 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    // DON'T CONVERT THESE THEY ARE MAPPINGS
+    'db_name' => 'LBL_LIST_ACCOUNT_NAME',
+    'db_website' => 'LBL_LIST_WEBSITE',
+    'db_billing_address_city' => 'LBL_LIST_CITY',
+    // END DON'T CONVERT
+    'LBL_DOCUMENTS_SUBPANEL_TITLE' => '文档',
+    // Dashlet Categories
+    'LBL_CHARTS' => '图表',
+    'LBL_DEFAULT' => '默认',
+    // END Dashlet Categories
+
+    'ERR_DELETE_RECORD' => '您必须指定记录编号才能删除',
+    'LBL_ACCOUNT_INFORMATION' => 'OVERVIEW', //No need to be translated in all caps. Translation used just in menu action items when using the SuiteP template
+    'LBL_ACCOUNT_NAME' => 'Account Name:',
+    'LBL_ACCOUNT' => '客户',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => '活动',
+    'LBL_ADDRESS_INFORMATION' => 'Address Information',
+    'LBL_ANNUAL_REVENUE' => 'Annual Revenue:',
+    'LBL_ANY_ADDRESS' => '任何地址',
+    'LBL_ANY_EMAIL' => '任何邮箱',
+    'LBL_ANY_PHONE' => '任何电话',
+    'LBL_ASSIGNED_TO_NAME' => '负责人',
+    'LBL_ASSIGNED_TO_ID' => 'Assigned User:',
+    'LBL_BILLING_ADDRESS_CITY' => 'Billing City:',
+    'LBL_BILLING_ADDRESS_COUNTRY' => '[账单地址]国家',
+    'LBL_BILLING_ADDRESS_POSTALCODE' => '[账单地址]邮编',
+    'LBL_BILLING_ADDRESS_STATE' => 'Billing State:',
+    'LBL_BILLING_ADDRESS_STREET_2' => 'Billing Street 2',
+    'LBL_BILLING_ADDRESS_STREET_3' => 'Billing Street 3',
+    'LBL_BILLING_ADDRESS_STREET_4' => 'Billing Street 4',
+    'LBL_BILLING_ADDRESS_STREET' => 'Billing Street:',
+    'LBL_BILLING_ADDRESS' => 'Billing Address:',
+    'LBL_BUGS_SUBPANEL_TITLE' => 'Bugs',
+    'LBL_CAMPAIGN_ID' => '营销活动ID',
+    'LBL_CASES_SUBPANEL_TITLE' => '客户反馈',
+    'LBL_CITY' => 'City:',
+    'LBL_CONTACTS_SUBPANEL_TITLE' => '联系人',
+    'LBL_COUNTRY' => 'Country:',
+    'LBL_DATE_ENTERED' => '创建日期',
+    'LBL_DATE_MODIFIED' => 'Date Modified:',
+    'LBL_DEFAULT_SUBPANEL_TITLE' => '客户',
+    'LBL_DESCRIPTION_INFORMATION' => 'Description Information',
+    'LBL_DESCRIPTION' => '说明',
+    'LBL_DUPLICATE' => 'Possible Duplicate Account',
+    'LBL_EMAIL' => 'Email Address:',
+    'LBL_EMAIL_OPT_OUT' => '退出邮件列表',
+    'LBL_EMAIL_ADDRESSES' => 'Email Addresses',
+    'LBL_EMPLOYEES' => 'Employees:',
+    'LBL_FAX' => 'Fax:',
+    'LBL_HISTORY_SUBPANEL_TITLE' => '历史记录',
+    'LBL_HOMEPAGE_TITLE' => '客户',
+    'LBL_INDUSTRY' => 'Industry:',
+    'LBL_INVALID_EMAIL' => '无效的邮箱',
+    'LBL_INVITEE' => '联系人',
+    'LBL_LEADS_SUBPANEL_TITLE' => '潜在客户',
+    'LBL_LIST_ACCOUNT_NAME' => '追踪器',
+    'LBL_LIST_CITY' => '城市',
+    'LBL_LIST_CONTACT_NAME' => '联系人',
+    'LBL_LIST_EMAIL_ADDRESS' => 'Email 地址',
+    'LBL_LIST_FORM_TITLE' => '客户列表',
+    'LBL_LIST_PHONE' => 'Phone',
+    'LBL_LIST_STATE' => '省份',
+    'LBL_MEMBER_OF' => 'Member of:',
+    'LBL_MEMBER_ORG_SUBPANEL_TITLE' => '子成员',
+    'LBL_MODULE_NAME' => '客户',
+    'LBL_MODULE_TITLE' => '客户',
+    'LBL_MODULE_ID' => '客户',
+    'LBL_NAME' => '营销活动名称',
+    'LBL_NEW_FORM_TITLE' => '新建',
+    'LBL_OPPORTUNITIES_SUBPANEL_TITLE' => '商业机会',
+    'LBL_OTHER_EMAIL_ADDRESS' => 'Other Email:',
+    'LBL_OTHER_PHONE' => 'Other Phone:',
+    'LBL_OWNERSHIP' => 'Ownership:',
+    'LBL_PARENT_ACCOUNT_ID' => 'Parent Account ID',
+    'LBL_PHONE_ALT' => 'Alternate Phone:',
+    'LBL_PHONE_FAX' => 'Phone Fax:',
+    'LBL_PHONE_OFFICE' => 'Office Phone:',
+    'LBL_PHONE' => 'Phone:',
+    'LBL_POSTAL_CODE' => 'Postal Code:',
+    'LBL_PRODUCTS_TITLE' => '产品',
+    'LBL_PROJECTS_SUBPANEL_TITLE' => '项目',
+    'LBL_PUSH_CONTACTS_BUTTON_LABEL' => '复制到联系人地址',
+    'LBL_PUSH_CONTACTS_BUTTON_TITLE' => '复制...',
+    'LBL_RATING' => '评分',
+    'LBL_SAVE_ACCOUNT' => 'Save Account',
+    'LBL_SEARCH_FORM_TITLE' => '搜索',
+    'LBL_SHIPPING_ADDRESS_CITY' => 'Shipping City:',
+    'LBL_SHIPPING_ADDRESS_COUNTRY' => 'Shipping Country:',
+    'LBL_SHIPPING_ADDRESS_POSTALCODE' => 'Shipping Postal Code:',
+    'LBL_SHIPPING_ADDRESS_STATE' => 'Shipping State:',
+    'LBL_SHIPPING_ADDRESS_STREET_2' => 'Shipping Street 2',
+    'LBL_SHIPPING_ADDRESS_STREET_3' => 'Shipping Street 3',
+    'LBL_SHIPPING_ADDRESS_STREET_4' => 'Shipping Street 4',
+    'LBL_SHIPPING_ADDRESS_STREET' => 'Shipping Street:',
+    'LBL_SHIPPING_ADDRESS' => 'Shipping Address:',
+    'LBL_SIC_CODE' => '标准行业代码',
+    'LBL_STATE' => '省份',
+    'LBL_TICKER_SYMBOL' => 'Ticker Symbol:',
+    'LBL_TYPE' => 'Type:',
+    'LBL_WEBSITE' => 'Website:',
+    'LBL_CAMPAIGNS' => '营销活动',
+    'LNK_ACCOUNT_LIST' => '查看客户',
+    'LNK_NEW_ACCOUNT' => 'Create Account',
+    'LNK_IMPORT_ACCOUNTS' => '导入客户',
+    'MSG_DUPLICATE' => 'The account record you are about to create might be a duplicate of an account record that already exists. Account records containing similar names are listed below.<br>Click Create Account to continue creating this new account, or select an existing account listed below.',
+    'MSG_SHOW_DUPLICATES' => '您将要创建的客户记录可能与已经存在的客户记录重复。下面列出了包含类似名称的客户记录。<br />点击保存来继续创建此新的客户，或者点击取消。',
+    'LBL_ASSIGNED_USER_NAME' => '负责人',
+    'LBL_PROSPECT_LIST' => '目标群体',
+    'LBL_ACCOUNTS_SUBPANEL_TITLE' => '客户',
+    'LBL_PROJECT_SUBPANEL_TITLE' => '项目',
+    //For export labels
+    'LBL_PARENT_ID' => '上级ID',
+    // SNIP
+    'LBL_PRODUCTS_SERVICES_PURCHASED_SUBPANEL_TITLE' => '购买的产品/服务',
+
+    'LBL_AOS_CONTRACTS' => '合同',
+    'LBL_AOS_INVOICES' => 'Invoices',
+    'LBL_AOS_QUOTES' => '报价',
+    'LBL_LIST_WEBSITE' => '网站',
+);
