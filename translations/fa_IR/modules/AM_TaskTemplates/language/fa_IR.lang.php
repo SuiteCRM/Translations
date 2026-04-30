9015b4bc72b7e8720a29226824da05ccf754f689
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-04-30 18:08:09 +0100

    New translations en_us.lang.php (Persian)
    [ci skip]

---
+++ translations/fa_IR/modules/AM_TaskTemplates/language/fa_IR.lang.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID' => 'Assigned User Id',
+    'LBL_ASSIGNED_TO_NAME' => 'اختصاص داده شده به',
+    'LBL_ID' => 'ID',
+    'LBL_DATE_ENTERED' => 'Date Created',
+    'LBL_DATE_MODIFIED' => 'Date Modified',
+    'LBL_MODIFIED' => 'Modified By',
+    'LBL_MODIFIED_ID' => 'شناسه ویرایش کننده',
+    'LBL_MODIFIED_NAME' => 'نام ویرایش کننده',
+    'LBL_CREATED' => 'Created By',
+    'LBL_CREATED_ID' => 'شناسه ایجاد کننده',
+    'LBL_DESCRIPTION' => 'Description',
+    'LBL_DELETED' => 'حذف شده',
+    'LBL_NAME' => 'Task Name',
+    'LBL_CREATED_USER' => 'Created by User',
+    'LBL_MODIFIED_USER' => 'کاربر ویرایش کننده',
+    'LBL_LIST_NAME' => 'نام',
+    'LBL_EDIT_BUTTON' => 'ویرایش',
+    'LBL_REMOVE' => 'حذف',
+    'LBL_LIST_FORM_TITLE' => 'لیست قالب‌های وظیفه پروژه',
+    'LBL_MODULE_NAME' => 'Project Task Templates',
+    'LBL_MODULE_TITLE' => 'Project Task Templates',
+    'LBL_HOMEPAGE_TITLE' => 'قالب‌های وظیفه پروژه من',
+    'LNK_NEW_RECORD' => 'ایجاد قالب‌های وظیفه پروژه',
+    'LNK_LIST' => 'مشاهده قالب‌های وظیفه پروژه',
+    'LNK_IMPORT_AM_TASKTEMPLATES' => 'درون‌ریزی قالب‌های وظیفه پروژه',
+    'LBL_SEARCH_FORM_TITLE' => 'جستجوی قالب‌های وظیفه پروژه',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'View History',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'فعالیت‌ها',
+    'LBL_AM_TASKTEMPLATES_SUBPANEL_TITLE' => 'Project Task Templates',
+    'LBL_NEW_FORM_TITLE' => 'قالب‌های وظیفه پروژه جدید',
+    'LBL_STATUS' => 'Status',
+    'LBL_PRIORITY' => 'Priority',
+    'LBL_PERCENT_COMPLETE' => 'درصد تکمیل',
+    'LBL_PREDECESSORS' => 'Predecessors',
+    'LBL_MILESTONE_FLAG' => 'پرچم نقطه عطف',
+    'LBL_RELATIONSHIP_TYPE' => 'نوع رابطه',
+    'LBL_TASK_NUMBER' => 'شماره وظیفه',
+    'LBL_ORDER_NUMBER' => 'ترتیب ',
+    'LBL_ESTIMATED_EFFORT' => 'تلاش پیش‌بینی شده (ساعت)',
+    'LBL_UTILIZATION' => 'استفاده (٪)',
+    'LBL_DURATION' => 'مدت (روز)',
+    'LBL_AM_TASKTEMPLATES_AM_PROJECTTEMPLATES_FROM_AM_PROJECTTEMPLATES_TITLE' => 'Project Templates',
+
+    'LBL_AM_TASKTEMPLATES_AM_PROJECTTEMPLATES_FROM_AM_TASKTEMPLATES_TITLE' => 'Task Templates: Project Templates from Task Templates Title',
+);
