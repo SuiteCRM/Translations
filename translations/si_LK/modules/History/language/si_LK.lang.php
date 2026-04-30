3750fcbca7a653cfaf73f366ee2e1371b7a89f49
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-04-30 19:41:22 +0100

    New translations en_us.lang.php (Sinhala)
    [ci skip]

---
+++ translations/si_LK/modules/History/language/si_LK.lang.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_MODULE_NAME' => 'History',
+    'LBL_MODULE_TITLE' => 'ඉතිහාසය: මුල් පිටුව',
+    'LBL_SEARCH_FORM_TITLE' => 'ඉතිහාසය සොයන්න',
+    'LBL_LIST_FORM_TITLE' => 'History',
+    'LBL_LIST_SUBJECT' => 'මාතෘකාව',
+    'LBL_LIST_CONTACT' => 'සබඳතාව',
+    'LBL_LIST_RELATED_TO' => 'Related to',
+    'LBL_LIST_DATE' => 'Date',
+    'LBL_LIST_CLOSE' => 'වසන්න',
+    'LBL_SUBJECT' => 'විෂය:',
+    'LBL_STATUS' => 'තත්ත්වය:',
+    'LBL_LOCATION' => 'Location:',
+    'LBL_DATE_TIME' => 'Start Date & Time:',
+    'LBL_DATE' => 'Start Date:',
+    'LBL_TIME' => 'Start Time:',
+    'LBL_DURATION' => 'Duration:',
+    'LBL_HOURS_MINS' => '(hours/minutes)',
+    'LBL_CONTACT_NAME' => 'Contact Name: ',
+    'LBL_DESCRIPTION' => 'Description:',
+    'LNK_NEW_CALL' => ' ලඝු-සටහන ඇමතුම්',
+    'LNK_NEW_MEETING' => 'Schedule Meeting',
+    'LNK_NEW_TASK' => ' කාර්ය සාධක නිර්මාණය',
+    'LNK_NEW_NOTE' => 'සටහන හෝ ඇමුණුම සාදන්න',
+    'LNK_NEW_EMAIL' => 'Archive Email',
+    'LNK_CALL_LIST' => 'Calls',
+    'LNK_MEETING_LIST' => 'Meetings',
+    'LNK_TASK_LIST' => ' කාර්යයන්',
+    'LNK_NOTE_LIST' => 'Notes',
+    'ERR_DELETE_RECORD' => 'වාර්තාගත ගිණුම මකා දැමීම නියම කළ යුතු ය.',
+    'LBL_INVITEE' => 'Invitees',
+    'LBL_LIST_DIRECTION' => 'Direction',
+    'LBL_DIRECTION' => 'Direction',
+    'LNK_NEW_APPOINTMENT' => 'New Appointment',
+    'LNK_VIEW_CALENDAR' => 'Today',
+    'LBL_OPEN_ACTIVITIES' => 'Open Activities',
+    'LBL_HISTORY' => 'History',
+    'LBL_NEW_TASK_BUTTON_TITLE' => ' කාර්ය සාධක නිර්මාණය',
+    'LBL_NEW_TASK_BUTTON_LABEL' => ' කාර්ය සාධක නිර්මාණය',
+    'LBL_SCHEDULE_MEETING_BUTTON_TITLE' => 'උපෙල්ඛනෙය් රැස්වීම',
+    'LBL_SCHEDULE_MEETING_BUTTON_LABEL' => 'උපෙල්ඛනෙය් රැස්වීම',
+    'LBL_SCHEDULE_CALL_BUTTON_LABEL' => ' ලඝු-සටහන ඇමතුම්',
+    'LBL_NEW_NOTE_BUTTON_TITLE' => 'සටහන හෝ ඇමුණුම සාදන්න',
+    'LBL_NEW_NOTE_BUTTON_LABEL' => 'සටහන හෝ ඇමුණුම සාදන්න',
+    'LBL_TRACK_EMAIL_BUTTON_TITLE' => 'Archive Email',
+    'LBL_TRACK_EMAIL_BUTTON_LABEL' => 'Archive Email',
+    'LBL_LIST_STATUS' => 'තත්ත්වය',
+    'LBL_LIST_DUE_DATE' => 'Due Date',
+    'LBL_LIST_LAST_MODIFIED' => 'Last Modified',
+    'LNK_IMPORT_NOTES' => 'Import Notes',
+    'LBL_ACCEPT_THIS' => 'Accept?',
+    'LBL_DEFAULT_SUBPANEL_TITLE' => 'History',
+
+    'LBL_CATEGORY' => 'ප්‍රවර්ගය',
+    'LBL_LIST_CATEGORY' => 'ප්‍රවර්ගය',
+);
