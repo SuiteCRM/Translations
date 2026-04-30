f588195baf57edfc80fe130432a69deeda0b9202
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-04-30 19:57:32 +0100

    New translations en_us.lang.php (German)
    [ci skip]

---
+++ translations/de_DE/modules/Notes/language/de_DE.lang.php
@@ -0,0 +1,111 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'ERR_DELETE_RECORD' => 'Zum Löschen der Firma muss eine Datensatznummer angegeben werden.',
+    'LBL_ACCOUNT_ID' => 'Firmen ID:',
+    'LBL_CASE_ID' => 'Fall ID:',
+    'LBL_CLOSE' => 'Close:',
+    'LBL_CONTACT_ID' => 'Kontakt ID:',
+    'LBL_CONTACT_NAME' => 'Contact:',
+    'LBL_DEFAULT_SUBPANEL_TITLE' => 'Notizen',
+    'LBL_DESCRIPTION' => 'Notiz',
+    'LBL_EMAIL_ADDRESS' => 'E-Mail:',
+    'LBL_EMAIL_ATTACHMENT' => 'Email Attachment',
+    'LBL_FILE_MIME_TYPE' => 'Mime Type',
+    'LBL_FILE_URL' => 'File URL',
+    'LBL_FILENAME' => 'Anlage:',
+    'LBL_LEAD_ID' => 'Interessent ID:',
+    'LBL_LIST_CONTACT_NAME' => 'Kontakt',
+    'LBL_LIST_DATE_MODIFIED' => 'Last Modified',
+    'LBL_LIST_FILENAME' => 'File',
+    'LBL_LIST_FILE' => 'File',
+    'LBL_LIST_FORM_TITLE' => 'Notizenliste',
+    'LBL_LIST_RELATED_TO' => 'Related To',
+    'LBL_LIST_SUBJECT' => 'Subject',
+    'LBL_LIST_STATUS' => 'Status',
+    'LBL_LIST_CONTACT' => 'Kontakt',
+    'LBL_MODULE_NAME' => 'Notizen',
+    'LBL_MODULE_TITLE' => 'Notizen: Startseite',
+    'LBL_NEW_FORM_TITLE' => 'Create Note or Add Attachment',
+    'LBL_NOTE_STATUS' => 'Notiz',
+    'LBL_NOTE_SUBJECT' => 'Subject:',
+    'LBL_NOTES_SUBPANEL_TITLE' => 'Attachments',
+    'LBL_NOTE' => 'Notiz:',
+    'LBL_OPPORTUNITY_ID' => 'Verkaufschance ID:',
+    'LBL_PARENT_ID' => 'Eltern ID:',
+    'LBL_PARENT_TYPE' => 'Parent Type',
+    'LBL_PHONE' => 'Telefon:',
+    'LBL_PORTAL_FLAG' => 'Im Portal anzeigen?',
+    'LBL_EMBED_FLAG' => 'In E-Mail einfügen?',
+    'LBL_PRODUCT_ID' => 'Produkt ID:',
+    'LBL_QUOTE_ID' => 'Angebot ID:',
+    'LBL_RELATED_TO' => 'Related To:',
+    'LBL_SEARCH_FORM_TITLE' => 'Notizen suchen',
+    'LBL_STATUS' => 'Status',
+    'LBL_SUBJECT' => 'Subject:',
+    'LNK_IMPORT_NOTES' => 'Import Notes',
+    'LNK_NEW_NOTE' => 'Create Note or Attachment',
+    'LNK_NOTE_LIST' => 'View Notes',
+    'LBL_MEMBER_OF' => 'Member of:',
+    'LBL_LIST_ASSIGNED_TO_NAME' => 'Zugewiesener Benutzer',
+    'LBL_REMOVING_ATTACHMENT' => 'Anhang wird entfernt...',
+    'ERR_REMOVING_ATTACHMENT' => 'Anhang konnte nicht entfernt werden...',
+    'LBL_CREATED_BY' => 'Erstellt von',
+    'LBL_MODIFIED_BY' => 'Geändert von',
+    'LBL_SEND_ANYWAYS' => 'This email has no subject. Send/save anyway?',
+    'LBL_NOTE_INFORMATION' => 'Überblick', //No need to be translated in all caps. Translation used just in menu action items when using the SuiteP template
+    'LBL_MY_NOTES_DASHLETNAME' => 'Meine Notizen',
+    'LBL_EDITLAYOUT' => 'Edit Layout' /*for 508 compliance fix*/,
+    //For export labels
+    'LBL_FIRST_NAME' => 'Vorname',
+    'LBL_LAST_NAME' => 'Last Name',
+    'LBL_DATE_ENTERED' => 'Erstellt am',
+    'LBL_DATE_MODIFIED' => 'Geändert am',
+    'LBL_DELETED' => 'Deleted',
+    'LBL_FILE_CONTENTS' => 'Dateiinhalt',
+
+    'LBL_ASCENDING' => 'Aufsteigend',
+    'LBL_DESCENDING' => 'Absteigend',
+);
