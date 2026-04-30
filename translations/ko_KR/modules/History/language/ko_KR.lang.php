25a09a4800b1cb23f79a357d9056c61b6ea0caf9
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-04-30 19:40:36 +0100

    New translations en_us.lang.php (Korean)
    [ci skip]

---
+++ translations/ko_KR/modules/History/language/ko_KR.lang.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_MODULE_NAME' => '연혁',
+    'LBL_MODULE_TITLE' => '이력: 홈',
+    'LBL_SEARCH_FORM_TITLE' => 'History Search',
+    'LBL_LIST_FORM_TITLE' => '연혁',
+    'LBL_LIST_SUBJECT' => 'Subject',
+    'LBL_LIST_CONTACT' => '연락처',
+    'LBL_LIST_RELATED_TO' => 'Related to',
+    'LBL_LIST_DATE' => 'Date',
+    'LBL_LIST_CLOSE' => '닫기',
+    'LBL_SUBJECT' => 'Subject:',
+    'LBL_STATUS' => 'Status:',
+    'LBL_LOCATION' => 'Location:',
+    'LBL_DATE_TIME' => 'Start Date & Time:',
+    'LBL_DATE' => 'Start Date:',
+    'LBL_TIME' => 'Start Time:',
+    'LBL_DURATION' => 'Duration:',
+    'LBL_HOURS_MINS' => '(hours/minutes)',
+    'LBL_CONTACT_NAME' => 'Contact Name: ',
+    'LBL_DESCRIPTION' => '상세설명:',
+    'LNK_NEW_CALL' => '콜스케쥴작성',
+    'LNK_NEW_MEETING' => 'Schedule Meeting',
+    'LNK_NEW_TASK' => '업무 작성',
+    'LNK_NEW_NOTE' => '노트작성',
+    'LNK_NEW_EMAIL' => '메일작성',
+    'LNK_CALL_LIST' => '콜',
+    'LNK_MEETING_LIST' => '미팅',
+    'LNK_TASK_LIST' => '업무',
+    'LNK_NOTE_LIST' => '노트:',
+    'ERR_DELETE_RECORD' => '거래처를 삭제하시려면 레코드 번호를 지정해야 합니다.',
+    'LBL_INVITEE' => 'Invitees',
+    'LBL_LIST_DIRECTION' => 'Direction',
+    'LBL_DIRECTION' => 'Direction',
+    'LNK_NEW_APPOINTMENT' => 'New Appointment',
+    'LNK_VIEW_CALENDAR' => 'Today',
+    'LBL_OPEN_ACTIVITIES' => 'Open Activities',
+    'LBL_HISTORY' => '연혁',
+    'LBL_NEW_TASK_BUTTON_TITLE' => '작업 생성',
+    'LBL_NEW_TASK_BUTTON_LABEL' => '작업 생성',
+    'LBL_SCHEDULE_MEETING_BUTTON_TITLE' => '미팅스케쥴작성',
+    'LBL_SCHEDULE_MEETING_BUTTON_LABEL' => '미팅스케쥴작성',
+    'LBL_SCHEDULE_CALL_BUTTON_LABEL' => '전화통화 예약',
+    'LBL_NEW_NOTE_BUTTON_TITLE' => '노트작성',
+    'LBL_NEW_NOTE_BUTTON_LABEL' => '노트작성',
+    'LBL_TRACK_EMAIL_BUTTON_TITLE' => '이메일 보관하기',
+    'LBL_TRACK_EMAIL_BUTTON_LABEL' => '이메일 보관하기',
+    'LBL_LIST_STATUS' => '상태',
+    'LBL_LIST_DUE_DATE' => 'Due Date',
+    'LBL_LIST_LAST_MODIFIED' => 'Last Modified',
+    'LNK_IMPORT_NOTES' => 'Import Notes',
+    'LBL_ACCEPT_THIS' => 'Accept?',
+    'LBL_DEFAULT_SUBPANEL_TITLE' => '연혁',
+
+    'LBL_CATEGORY' => 'Category',
+    'LBL_LIST_CATEGORY' => 'Category',
+);
