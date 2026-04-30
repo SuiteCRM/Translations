deb001c5b4370f3112361272a961c32fec978995
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-04-30 18:25:38 +0100

    New translations en_us.lang.php (Polish)
    [ci skip]

---
+++ translations/pl_PL/modules/AOS_Invoices/language/pl_PL.lang.php
@@ -0,0 +1,203 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID' => 'ID przydzielonego użytkownika',
+    'LBL_ASSIGNED_TO_NAME' => 'Przypisany Do',
+    'LBL_ID' => 'ID',
+    'LBL_DATE_ENTERED' => 'Data utworzenia',
+    'LBL_DATE_MODIFIED' => 'Data Modyfikacji',
+    'LBL_MODIFIED' => 'Zmodyfikowane przez',
+    'LBL_MODIFIED_NAME' => 'Nazwa użytkownika modyfikującego',
+    'LBL_CREATED' => 'Utworzone przez',
+    'LBL_DESCRIPTION' => 'Opis',
+    'LBL_DELETED' => 'Usunięte',
+    'LBL_NAME' => 'Tytuł',
+    'LBL_CREATED_USER' => 'Utworzony przez Użytkownika',
+    'LBL_MODIFIED_USER' => 'Zmodyfikowany przez użytkownika',
+    'ERR_DELETE_RECORD' => 'Aby usunąć Kontakt musisz określić numer wpisu.',
+    'LBL_ACCOUNT_NAME' => 'Tytuł',
+    'LBL_ACCOUNT' => 'Company:',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Działania',
+    'LBL_ADDRESS_INFORMATION' => 'Informacja adresowa',
+    'LBL_ANNUAL_REVENUE' => 'Annual Revenue:',
+    'LBL_ANY_ADDRESS' => 'Any Address:',
+    'LBL_ANY_EMAIL' => 'Dowolny e-mail:',
+    'LBL_ANY_PHONE' => 'Any Phone:',
+    'LBL_RATING' => 'Ocena',
+    'LBL_ASSIGNED_USER' => 'Przydzielony do',
+    'LBL_BILLING_ADDRESS_CITY' => 'Billing City:',
+    'LBL_BILLING_ADDRESS_COUNTRY' => 'Billing Country:',
+    'LBL_BILLING_ADDRESS_POSTALCODE' => 'Billing Postal Code:',
+    'LBL_BILLING_ADDRESS_STATE' => 'Billing State:',
+    'LBL_BILLING_ADDRESS_STREET_2' => 'Adres do Korespondencji - Ulica 2',
+    'LBL_BILLING_ADDRESS_STREET_3' => 'Adres do Korespondencji - Ulica 3',
+    'LBL_BILLING_ADDRESS_STREET_4' => 'Adres do Korespondencji - Ulica 4',
+    'LBL_BILLING_ADDRESS_STREET' => 'Adres do Korespondencji - Ulica:',
+    'LBL_BILLING_ADDRESS' => 'Adres do Korespondencji:',
+    'LBL_ACCOUNT_INFORMATION' => 'Przegląd',
+    'LBL_CITY' => 'Miasto:',
+    'LBL_CONTACTS_SUBPANEL_TITLE' => 'Kontakty',
+    'LBL_COUNTRY' => 'Kraj:',
+    'LBL_DEFAULT_SUBPANEL_TITLE' => 'Kontrahenci',
+    'LBL_DUPLICATE' => 'Możliwy duplikat kontrahenta',
+    'LBL_EMAIL' => 'Adres e-mail:',
+    'LBL_EMPLOYEES' => 'Employees:',
+    'LBL_FAX' => 'Faks:',
+    'LBL_INDUSTRY' => 'Industry:',
+    'LBL_LIST_ACCOUNT_NAME' => 'Nazwa kontrahenta',
+    'LBL_LIST_CITY' => 'City',
+    'LBL_LIST_EMAIL_ADDRESS' => 'E-mail',
+    'LBL_LIST_PHONE' => 'Telefon',
+    'LBL_LIST_STATE' => 'Woj.',
+    'LBL_MEMBER_OF' => 'Member of:',
+    'LBL_MEMBER_ORG_SUBPANEL_TITLE' => 'Członek organizacji',
+    'LBL_OTHER_EMAIL_ADDRESS' => 'Dodatkowy adres e-mail:',
+    'LBL_OTHER_PHONE' => 'Inny telefon:',
+    'LBL_OWNERSHIP' => 'Ownership:',
+    'LBL_PARENT_ACCOUNT_ID' => 'ID konta nadrzędnego',
+    'LBL_PHONE_ALT' => 'Alternatywny telefon:',
+    'LBL_PHONE_FAX' => 'Faks:',
+    'LBL_PHONE_OFFICE' => 'Telefon do biura:',
+    'LBL_PHONE' => 'Phone:',
+    'LBL_POSTAL_CODE' => 'Postal Code:',
+    'LBL_SAVE_ACCOUNT' => 'Zapisz kontrahenta',
+    'LBL_SHIPPING_ADDRESS_CITY' => 'Shipping City:',
+    'LBL_SHIPPING_ADDRESS_COUNTRY' => 'Shipping Country:',
+    'LBL_SHIPPING_ADDRESS_POSTALCODE' => 'Shipping Postal Code:',
+    'LBL_SHIPPING_ADDRESS_STATE' => 'Shipping State:',
+    'LBL_SHIPPING_ADDRESS_STREET_2' => 'Shipping Street 2',
+    'LBL_SHIPPING_ADDRESS_STREET_3' => 'Shipping Street 3',
+    'LBL_SHIPPING_ADDRESS_STREET_4' => 'Shipping Street 4',
+    'LBL_SHIPPING_ADDRESS_STREET' => 'Shipping Street:',
+    'LBL_SHIPPING_ADDRESS' => 'Shipping Address:',
+    'LBL_STATE' => 'Województwo:',
+    'LBL_TICKER_SYMBOL' => 'Ticker Symbol:',
+    'LBL_TYPE' => 'Typ:',
+    'LBL_WEBSITE' => 'Website:',
+    'LNK_ACCOUNT_LIST' => 'Kontrahenci',
+    'LNK_NEW_ACCOUNT' => 'Utwórz Kontrahenta',
+    'MSG_DUPLICATE' => 'Stworzenie tego wpisu może utworzyć duplikat. Możesz wybrać wpis z poniższej listy lub kliknąć Zapisz, aby kontynuować tworzenie nowego wpisu z wcześniej wprowadzonych danych.',
+    'MSG_SHOW_DUPLICATES' => 'Stworzenie tego wpisu może utworzyć duplikat. Możesz kliknąć Zapisz, aby kontynuować tworzenie nowego wpisu z wcześniej wprowadzonymi danymi lub kliknij Cofnij.',
+    'NTC_DELETE_CONFIRMATION' => 'Czy na pewno chcesz usunąć ten wpis?',
+    'LBL_LIST_FORM_TITLE' => 'Lista Faktur',
+    'LBL_MODULE_NAME' => 'Faktury',
+    'LBL_MODULE_TITLE' => 'Faktury: Strona Główna',
+    'LBL_HOMEPAGE_TITLE' => 'Moje Faktury',
+    'LNK_NEW_RECORD' => 'Stwórz Fakturę',
+    'LNK_LIST' => 'Zobacz Faktury',
+    'LBL_SEARCH_FORM_TITLE' => 'Wyszukaj Faktury',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Przeglądaj historię',
+    'LBL_NEW_FORM_TITLE' => 'Nowa Faktura',
+    'LBL_TERMS_C' => 'Warunki',
+    'LBL_APPROVAL_ISSUE' => 'Powód braku Zatwierdzenia',
+    'LBL_APPROVAL_STATUS' => 'Stan Zatwierdzenia',
+    'LBL_BILLING_ACCOUNT' => 'Konto',
+    'LBL_BILLING_CONTACT' => 'Kontakty',
+    'LBL_EXPIRATION' => 'Ważne do',
+    'LBL_INVOICE_NUMBER' => 'Numer Faktury',
+    'LBL_OPPORTUNITY' => 'Nazwa szansy sprzedaży',
+    'LBL_TEMPLATE_DDOWN_C' => 'Szablony Faktur',
+    'LBL_STAGE' => 'Status Oferty',
+    'LBL_TERM' => 'Termin Płatności',
+    'LBL_SUBTOTAL_AMOUNT' => 'Wartość netto (po rabacie)',
+    'LBL_DISCOUNT_AMOUNT' => 'Discount',
+    'LBL_TAX_AMOUNT' => 'Podatek',
+    'LBL_SHIPPING_AMOUNT' => 'Przesyłka',
+    'LBL_TOTAL_AMT' => 'Razem',
+    'VALUE' => 'Tytuł',
+    'LBL_EMAIL_ADDRESSES' => 'Adresy e-mail',
+    'LBL_LINE_ITEMS' => 'Pozycje',
+    'LBL_GRAND_TOTAL' => 'Suma',
+    'LBL_QUOTE_NUMBER' => 'Numer oferty',
+    'LBL_QUOTE_DATE' => 'Data oferty',
+    'LBL_INVOICE_DATE' => 'Data Faktury',
+    'LBL_DUE_DATE' => 'Data Wykonania',
+    'LBL_STATUS' => 'Status',
+    'LBL_INVOICE_STATUS' => 'Status Faktury',
+    'LBL_PRODUCT_QUANITY' => 'Ilość',
+    'LBL_PRODUCT_NAME' => 'Produkt',
+    'LBL_PART_NUMBER' => 'Numer części',
+    'LBL_PRODUCT_NOTE' => 'Notatka',
+    'LBL_PRODUCT_DESCRIPTION' => 'Opis',
+    'LBL_LIST_PRICE' => 'Lista',
+    'LBL_DISCOUNT_AMT' => 'Discount',
+    'LBL_UNIT_PRICE' => 'Cena jednostkowa netto',
+    'LBL_TOTAL_PRICE' => 'Wartość brutto',
+    'LBL_VAT' => 'Podatek', //VAT
+    'LBL_VAT_AMT' => 'Wartość VAT', //VAT
+    'LBL_ADD_PRODUCT_LINE' => 'Dodaj linię produktów',
+    'LBL_SERVICE_NAME' => 'Wsparcie',
+    'LBL_SERVICE_LIST_PRICE' => 'Lista',
+    'LBL_SERVICE_PRICE' => 'Cena sprzedaży',
+    'LBL_SERVICE_DISCOUNT' => 'Discount',
+    'LBL_ADD_SERVICE_LINE' => 'Dodaj linię usług ',
+    'LBL_REMOVE_PRODUCT_LINE' => 'Usuń',
+    'LBL_PRINT_AS_PDF' => 'Drukuj jako PDF',
+    'LBL_EMAIL_INVOICE' => 'Wyślij Fakturę',
+    'LBL_LIST_NUM' => 'Num.',
+    'LBL_PDF_NAME' => 'Faktura',
+    'LBL_EMAIL_NAME' => 'Faktura za',
+    'LBL_NO_TEMPLATE' => 'BŁĄD\nNie znaleziono szablonu. Jeśli nie utworzyłeś szablonu umowy, przejdź do Szablonów PDF i stwórz go',
+    'LBL_SUBTOTAL_TAX_AMOUNT' => 'Suma częściowa + podatek',//pre shipping
+    'LBL_EMAIL_PDF' => 'Email PDF',
+    'LBL_ADD_GROUP' => 'Add Group',
+    'LBL_DELETE_GROUP' => 'Delete Group',
+    'LBL_GROUP_NAME' => 'Group Name',
+    'LBL_GROUP_TOTAL' => 'Razem Grupa',
+    'LBL_SHIPPING_TAX' => 'Podatek od spedycji',
+    'LBL_SHIPPING_TAX_AMT' => 'VAT od przesyłki',
+    'LBL_IMPORT_LINE_ITEMS' => 'Importuj elementy',
+    'LBL_SUBTOTAL_AMOUNT_USDOLLAR' => 'Wartość netto (domyślna waluta)',
+    'LBL_DISCOUNT_AMOUNT_USDOLLAR' => 'Rabat (domyślna waluta)',
+    'LBL_TAX_AMOUNT_USDOLLAR' => 'Wartość VAT (domyślna waluta)',
+    'LBL_SHIPPING_AMOUNT_USDOLLAR' => 'Spedycja (domyślna waluta)',
+    'LBL_TOTAL_AMT_USDOLLAR' => 'Razem: wartość netto (domyślna waluta)',
+    'LBL_SHIPPING_TAX_AMT_USDOLLAR' => 'VAT od przesyłki (domyślna waluta)',
+    'LBL_GRAND_TOTAL_USDOLLAR' => 'Suma brutto (domyślna waluta)',
+    'LBL_INVOICE_TO' => 'Faktura dla',
+    'LBL_AOS_LINE_ITEM_GROUPS' => 'Grupy artykułów',
+    'LBL_AOS_PRODUCT_QUOTES' => 'Oferty produktowe',
+    'LBL_AOS_QUOTES_AOS_INVOICES' => 'Oferty : Faktury',
+);
