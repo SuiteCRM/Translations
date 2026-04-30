25095f065c21e7360ad4d9b71e444f01d13b52d1
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-04-30 20:38:16 +0100

    New translations en_us.lang.php (Japanese)
    [ci skip]

---
+++ translations/ja_JP/modules/Surveys/language/ja_JP.lang.php
@@ -0,0 +1,110 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID'                                     => 'アサインされたユーザーID',
+    'LBL_ADD_OPTION'                                         => 'Add new option',
+    'LBL_ASSIGNED_TO_NAME'                                   => '担当ユーザー名',
+    'LBL_SECURITYGROUPS'                                     => 'Security Groups',
+    'LBL_SECURITYGROUPS_SUBPANEL_TITLE'                      => 'Security Groups',
+    'LBL_ID'                                                 => 'ID',
+    'LBL_DATE_ENTERED'                                       => 'Date Created',
+    'LBL_DATE_MODIFIED'                                      => 'Date Modified',
+    'LBL_MODIFIED'                                           => 'Modified By',
+    'LBL_MODIFIED_ID'                                        => 'Modified By Id',
+    'LBL_MODIFIED_NAME'                                      => 'Modified By Name',
+    'LBL_CREATED'                                            => 'Created By',
+    'LBL_CREATED_ID'                                         => 'Created By Id',
+    'LBL_DESCRIPTION'                                        => 'Description',
+    'LBL_DELETED'                                            => 'Deleted',
+    'LBL_NAME'                                               => '名称',
+    'LBL_CREATED_USER'                                       => 'ユーザが作成',
+    'LBL_MODIFIED_USER'                                      => 'ユーザが更新',
+    'LBL_LIST_NAME'                                          => '名称',
+    'LBL_EDIT_BUTTON'                                        => 'Edit',
+    'LBL_REMOVE'                                             => 'Remove',
+    'LBL_LIST_FORM_TITLE'                                    => '調査一覧',
+    'LBL_MODULE_NAME'                                        => '調査',
+    'LBL_MODULE_TITLE'                                       => '調査',
+    'LBL_HOMEPAGE_TITLE'                                     => '私の調査',
+    'LNK_NEW_RECORD'                                         => '調査の作成',
+    'LNK_LIST'                                               => '調査の表示',
+    'LNK_IMPORT_SURVEYS'                                     => '調査のインポート',
+    'LBL_SEARCH_FORM_TITLE'                                  => '調査の検索',
+    'LBL_HISTORY_SUBPANEL_TITLE'                             => 'View History',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE'                          => 'Activities',
+    'LBL_SURVEYS_SUBPANEL_TITLE'                             => '調査',
+    'LBL_NEW_FORM_TITLE'                                     => '新しい調査',
+    'LBL_STATUS'                                             => 'Status',
+    'LBL_SURVEY_QUESTIONS_DISPLAY'                           => '質問',
+    'LBL_SURVEY_URL_DISPLAY'                                 => 'URL',
+    'LBL_HAPPINESS_QUESTION'                                 => '幸福の質問',
+    'LBL_CANT_EDIT_RESPONDED'                                => '回答のある調査質問が編集できません',
+    'LBL_VIEW_SURVEY_REPORTS'                                => '調査レポートの表示',
+    'LBL_CHECKED'                                            => 'チェック',
+    'LBL_UNCHECKED'                                          => '未チェック',
+    'LBL_RESPONSE_ANSWER'                                    => '回答',
+    'LBL_RESPONSE_CONTACT'                                   => '連絡先',
+    'LBL_RESPONSE_TIME'                                      => 'Date',
+    'LBL_UNKNOWN_CONTACT'                                    => 'Unknown',
+    'LBL_RESPONSE_COUNT'                                     => 'Count',
+    'LBL_SUBMIT_TEXT'                                        => 'テキストを送信',
+    'LBL_SATISFIED_TEXT'                                     => '満足したテキスト',
+    'LBL_NEITHER_TEXT'                                       => 'どちらかのテキスト',
+    'LBL_DISSATISFIED_TEXT'                                  => '不満足なテキスト',
+    'LBL_SURVEYS_SURVEYQUESTIONS_FROM_SURVEYQUESTIONS_TITLE' => '調査質問',
+    'LBL_SURVEYS_SURVEYRESPONSES_FROM_SURVEYRESPONSES_TITLE' => '調査の回答',
+    'LBL_SHOW_RESPONSES' => '回答の表示',
+    'LBL_HIDE_RESPONSES' => 'Hide responses',
+    'LNK_LIST_SURVEY_RESPONSES' => 'アンケート回答を表示',
+    'LBL_SURVEY_QUESTION' => '質問',
+    'LBL_SURVEY_TEXT' => 'Text',
+    'LBL_SURVEY_TYPE' => 'Type',
+    'LBL_SURVEY_ACTIONS' => 'Actions',
+    'LBL_SURVEY_NEW_QUESTION' => '新しい質問',
+    'LBL_SURVEY_RESPONSES' => '新しい質問',
+    'LBL_SURVEYS_SENT' => '回答を送信済み:',
+    'LBL_SURVEY_DISTINCT' => '送信済みの回答の内容:',
+    'LBL_DRAFT' => '草案',
+    'LBL_SUBMIT' => 'Submit',
+    'LBL_STARS' => 'スター',
+    'LBL_SURVEY_CLOSE_RESPONSE' => 'ご興味をお寄せいただきありがとうございます。この調査は終了しました。',
+    'LBL_SURVEY_THANKS' => 'Thanks for completing this survey.',
+);
