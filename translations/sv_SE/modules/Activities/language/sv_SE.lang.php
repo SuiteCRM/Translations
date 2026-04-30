a6a48aa5eeba952057e6d13b0f63eac39962498a
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-04-30 18:42:48 +0100

    New translations en_us.lang.php (Swedish)
    [ci skip]

---
+++ translations/sv_SE/modules/Activities/language/sv_SE.lang.php
@@ -0,0 +1,118 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_MODULE_NAME' => 'Activities',
+    'LBL_MODULE_TITLE' => 'Aktiviteter: Hem',
+    'LBL_SEARCH_FORM_TITLE' => 'Sök aktivitet',
+    'LBL_LIST_FORM_TITLE' => 'Lista aktiviteter',
+    'LBL_LIST_SUBJECT' => 'Subject',
+    'LBL_OVERVIEW' => 'OVERVIEW', //No need to be translated in all caps. Translation used just in menu action items when using the SuiteP template
+    'LBL_TASKS' => 'UPPGIFTER', //No need to be translated in all caps. Translation used just in menu action items when using the SuiteP template
+    'LBL_MEETINGS' => 'MÖTEN', //No need to be translated in all caps. Translation used just in menu action items when using the SuiteP template
+    'LBL_CALLS' => 'SAMTAL', //No need to be translated in all caps. Translation used just in menu action items when using the SuiteP template
+    'LBL_EMAILS' => 'E-POST', //No need to be translated in all caps. Translation used just in menu action items when using the SuiteP template
+    'LBL_NOTES' => 'NOTERINGAR', //No need to be translated in all caps. Translation used just in menu action items when using the SuiteP template
+    'LBL_PRINT' => 'UTSKRIFT', //No need to be translated in all caps. Translation used just in menu action items when using the SuiteP template
+    'LBL_MEETING_TYPE' => 'Meeting',
+    'LBL_CALL_TYPE' => 'Call',
+    'LBL_EMAIL_TYPE' => 'Email',
+    'LBL_NOTE_TYPE' => 'Note',
+    'LBL_DATA_TYPE_START' => 'Första:',
+    'LBL_DATA_TYPE_SENT' => 'Skickat:',
+    'LBL_DATA_TYPE_MODIFIED' => 'Ändrad',
+    'LBL_LIST_CONTACT' => 'Contact',
+    'LBL_LIST_RELATED_TO' => 'Related to',
+    'LBL_LIST_DATE' => 'Date',
+    'LBL_LIST_CLOSE' => 'Close',
+    'LBL_SUBJECT' => 'Subject:',
+    'LBL_STATUS' => 'Status:',
+    'LBL_LOCATION' => 'Plats:',
+    'LBL_DATE_TIME' => 'Startdatum & tid:',
+    'LBL_DATE' => 'Startdatum:',
+    'LBL_TIME' => 'Starttid:',
+    'LBL_DURATION' => 'Varaktighet:',
+    'LBL_HOURS_MINS' => '(timmar/minuter)',
+    'LBL_CONTACT_NAME' => 'Kontakt namn:',
+    'LBL_DESCRIPTION' => 'Description:',
+    'LNK_NEW_CALL' => 'Log Call',
+    'LNK_NEW_MEETING' => 'Schedule Meeting',
+    'LNK_NEW_TASK' => 'Create Task',
+    'LNK_NEW_NOTE' => 'Skapa anteckning eller bilaga',
+    'LNK_NEW_EMAIL' => 'Send Email',
+    'LNK_CALL_LIST' => 'Samtal',
+    'LNK_MEETING_LIST' => 'Möten',
+    'LNK_TASK_LIST' => 'Uppgifter',
+    'LNK_NOTE_LIST' => 'Anteckningar',
+    'LBL_DELETE_ACTIVITY' => 'Är du säker på att du vill radera denna aktivitet?',
+    'ERR_DELETE_RECORD' => 'En post måste specificeras för att ta bort företaget.',
+    'LBL_INVITEE' => 'Inbjudningar',
+    'LBL_LIST_DIRECTION' => 'Inriktning',
+    'LBL_DIRECTION' => 'Direction',
+    'LNK_NEW_APPOINTMENT' => 'Ny aktivitet',
+    'LNK_VIEW_CALENDAR' => 'Idag',
+    'LBL_OPEN_ACTIVITIES' => 'Öppna aktiviteter',
+    'LBL_HISTORY' => 'History',
+    'LBL_NEW_TASK_BUTTON_TITLE' => 'Create Task',
+    'LBL_NEW_TASK_BUTTON_LABEL' => 'Create Task',
+    'LBL_SCHEDULE_MEETING_BUTTON_TITLE' => 'Schedule Meeting',
+    'LBL_SCHEDULE_MEETING_BUTTON_LABEL' => 'Schedule Meeting',
+    'LBL_SCHEDULE_CALL_BUTTON_LABEL' => 'Log Call',
+    'LBL_NEW_NOTE_BUTTON_TITLE' => 'Skapa anteckning eller bilaga',
+    'LBL_NEW_NOTE_BUTTON_LABEL' => 'Create Note or Attachment',
+    'LBL_TRACK_EMAIL_BUTTON_TITLE' => 'Archive Email',
+    'LBL_TRACK_EMAIL_BUTTON_LABEL' => 'Archive Email',
+    'LBL_LIST_STATUS' => 'Status',
+    'LBL_LIST_DUE_DATE' => 'Due Date',
+    'LBL_LIST_LAST_MODIFIED' => 'Last Modified',
+    'LNK_IMPORT_CALLS' => 'Importera Samtal',
+    'LNK_IMPORT_MEETINGS' => 'Importera Möten',
+    'LNK_IMPORT_TASKS' => 'Importera Uppgifter',
+    'LNK_IMPORT_NOTES' => 'Importera anteckningar',
+    'LBL_ACCEPT_THIS' => 'Accepteras?',
+    'LBL_DEFAULT_SUBPANEL_TITLE' => 'Open Activities',
+    'LBL_LIST_ASSIGNED_TO_NAME' => 'Assigned User',
+
+    'LBL_ACCEPT' => 'Accept' /*for 508 compliance fix*/,
+);
