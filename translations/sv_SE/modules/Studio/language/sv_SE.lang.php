cea159085997221f9df2842606521089567baf86
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-04-30 20:30:11 +0100

    New translations en_us.lang.php (Swedish)
    [ci skip]

---
+++ translations/sv_SE/modules/Studio/language/sv_SE.lang.php
@@ -0,0 +1,150 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_EDIT_LAYOUT' => 'Redigera layout',
+    'LBL_EDIT_FIELDS' => 'Redigera anpassade fält',
+    'LBL_SELECT_FILE' => 'Select File',
+    'LBL_MODULE_TITLE' => 'Studio',
+    'LBL_TOOLBOX' => 'Toolbox',
+    'LBL_SUITE_FIELDS_STAGE' => 'SuiteCRM Fields (click items to add to staging area)',
+    'LBL_VIEW_SUITE_FIELDS' => 'Visa SuiteCRM-fält',
+    'LBL_FAILED_TO_SAVE' => 'Failed To Save',
+    'LBL_CONFIRM_UNSAVE' => 'Förändringar kommer inte sparas. Är du säker på att du vill fortsätta?',
+    'LBL_PUBLISHING' => 'Publishing...',
+    'LBL_PUBLISHED' => 'Published',
+    'LBL_FAILED_PUBLISHED' => 'Failed to Publish',
+    'LBL_DROP_HERE' => '[Drop Here]',
+
+//CUSTOM FIELDS
+    'LBL_NAME' => 'Namn',
+    'LBL_LABEL' => 'Label',
+    'LBL_MASS_UPDATE' => 'Mass Update',
+    'LBL_DEFAULT_VALUE' => 'Default Value',
+    'LBL_REQUIRED' => 'Required',
+    'LBL_DATA_TYPE' => 'Type',
+
+
+    'LBL_HISTORY' => 'History',
+
+//WIZARDS
+
+//STUDIO WIZARD
+    'LBL_SW_WELCOME' => '<h2>Välkommen till Studio!</h2><br> Wad skulle du vlija göra idag?<br><b> Vänligen välj från valen nedanför.</b>',
+    'LBL_SW_EDIT_MODULE' => 'Ändra en Modul',
+    'LBL_SW_EDIT_DROPDOWNS' => 'Ändra Rullgardins menyer',
+    'LBL_SW_EDIT_TABS' => 'Konfigurera Flikar',
+    'LBL_SW_RENAME_TABS' => 'Döpa om Flikar',
+    'LBL_SW_EDIT_GROUPTABS' => 'Konfigurera Flikgrupperingar',
+    'LBL_SW_EDIT_PORTAL' => 'Ändra Portal',
+    'LBL_SW_REPAIR_CUSTOMFIELDS' => 'Reparera Egna Fält',
+    'LBL_SW_MIGRATE_CUSTOMFIELDS' => 'Migrate Custom Fields',
+
+// JS LABELS
+    'LBL_REBUILD_JAVASCRIPT_LANG_DESC_SHORT' => 'Rebuilds javascript versions of language files',
+    'LBL_REBUILD_JAVASCRIPT_LANG_DESC' => 'Removing javascript versions of language files, will rebuild when needed.',
+
+
+//Manager Backups History
+    'LBL_MB_DELETE' => 'Delete',
+
+//EDIT DROP DOWNS
+    'LBL_ED_CREATE_DROPDOWN' => 'Skapa en Rullgardins meny',
+    'LBL_DROPDOWN_NAME' => 'Rullgardins Namn:',
+    'LBL_DROPDOWN_LANGUAGE' => 'Rullgardins Språk:',
+    'LBL_TABGROUP_LANGUAGE' => 'Language:',
+
+//END WIZARDS
+
+//DROP DOWN EDITOR
+    'LBL_DD_DISPALYVALUE' => 'Visa Värden',
+    'LBL_DD_DATABASEVALUE' => 'Database Värden',
+    'LBL_DD_ALL' => 'All',
+
+//BUTTONS
+    'LBL_BTN_SAVE' => 'Save',
+    'LBL_BTN_CANCEL' => 'Cancel',
+    'LBL_BTN_SAVEPUBLISH' => 'Save & Deploy',
+    'LBL_BTN_HISTORY' => 'History',
+    'LBL_BTN_ADDROWS' => 'Add Rows',
+    'LBL_BTN_UNDO' => 'Undo',
+    'LBL_BTN_REDO' => 'Redo',
+    'LBL_BTN_ADDCUSTOMFIELD' => 'Add Custom Field',
+    'LBL_BTN_TABINDEX' => 'Ändra Flik Edit Tabbing Ordning',
+
+//TABS
+    'LBL_MODULES' => 'Moduler',
+    'LBL_MODULE_NAME' => 'Administration',
+    'LBL_CONFIGURE_GROUP_TABS' => 'Configure Module Menu Filters',
+    'LBL_GROUP_TAB_WELCOME' => 'Gruppfliks layouten nedan kommer att användas när en användare väljer att använda gruppflikar istället för vanliga modulflikar i Mitt Konto>Layout Val.',
+    'LBL_RENAME_TAB_WELCOME' => 'Klicka på en flik Visa Värde i tabellen nedan för att döpa om fliken.',
+    'LBL_DELETE_MODULE' => 'Ta bort&nbsp;modul<br />från&nbsp;filter',
+    'LBL_TAB_GROUP_LANGUAGE_HELP' => 'Välj ett tillgängligt språk, ändra grupp labels och klicka på "Spara & distribuera" för att lägga till labels i valt språk.',
+    'LBL_ADD_GROUP' => 'Lägg till filter',
+    'LBL_NEW_GROUP' => 'Ny Grupp',
+    'LBL_RENAME_TABS' => 'Rename Modules',
+
+//ERRORS
+    'ERROR_INVALID_KEY_VALUE' => "Error: Invalid Key Value: [']",
+
+//SUGAR PORTAL
+    'LBL_SAVE' => 'Save' /*for 508 compliance fix*/,
+    'LBL_UNDO' => 'Undo' /*for 508 compliance fix*/,
+    'LBL_REDO' => 'Redo' /*for 508 compliance fix*/,
+    'LBL_INLINE' => 'Inline' /*for 508 compliance fix*/,
+    'LBL_DELETE' => 'Delete' /*for 508 compliance fix*/,
+    'LBL_ADD_FIELD' => 'Add Field' /*for 508 compliance fix*/,
+    'LBL_MAXIMIZE' => 'Maximera' /*for 508 compliance fix*/,
+    'LBL_MINIMIZE' => 'Minimera' /*for 508 compliance fix*/,
+    'LBL_PUBLISH' => 'Publish' /*for 508 compliance fix*/,
+    'LBL_ADDROWS' => 'Add Rows' /*for 508 compliance fix*/,
+    'LBL_ADDFIELD' => 'Add Field' /*for 508 compliance fix*/,
+    'LBL_EDIT' => 'Redigera' /*for 508 compliance fix*/,
+
+    'LBL_LANGUAGE_TOOLTIP' => 'Välj språket du vill redigera.',
+    'LBL_SINGULAR' => 'Singular Label',
+    'LBL_PLURAL' => 'Plural-etikett',
+    'LBL_RENAME_MOD_SAVE_HELP' => 'Click <b>Save</b> to apply the changes.'
+
+);
