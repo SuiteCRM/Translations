2e01415a36d7f45ff28ce8be07186aaeec13b538
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-04-30 19:26:17 +0100

    New translations en_us.lang.php (Korean)
    [ci skip]

---
+++ translations/ko_KR/modules/EmailTemplates/language/ko_KR.lang.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ADD_DOCUMENT' => '문서 추가',
+    'LBL_ADD_FILE' => '파일 추가',
+    'LBL_ATTACHMENTS' => 'Attachments',
+    'LBL_BODY' => '본문:',
+    'LBL_CLOSE' => '종료:',
+    'LBL_DESCRIPTION' => '상세설명:',
+    'LBL_EDIT_ALT_TEXT' => '일반 텍스트 편집',
+    'LBL_EMAIL_ATTACHMENT' => '이메일 첨부',
+    'LBL_HIDE_ALT_TEXT' => '일반 텍스트 숨기기',
+    'LBL_HTML_BODY' => 'HTML 본문',
+    'LBL_INSERT_VARIABLE' => '변수 삽입:',
+    'LBL_INSERT_URL_REF' => '참조 URL 삽입',
+    'LBL_INSERT_TRACKER_URL' => '트래커 URL 삽입:',
+    'LBL_INSERT' => 'Insert',
+    'LBL_LIST_DATE_MODIFIED' => 'Last Modified',
+    'LBL_LIST_DESCRIPTION' => '설명:',
+    'LBL_LIST_FORM_TITLE' => '이메일 템플릿 목록',
+    'LBL_LIST_NAME' => 'Name',
+    'LBL_MODULE_NAME' => '이메일 템플릿',
+    'LBL_MODULE_TITLE' => '이메일 템플릿: 홈',
+    'LBL_NAME' => '이름:',
+    'LBL_NEW_FORM_TITLE' => 'Create Email Template',
+    'LBL_PUBLISH' => '게시:',
+    'LBL_RELATED_TO' => 'Related To:',
+    'LBL_SEARCH_FORM_TITLE' => '이메일 템플릿 검색',
+    'LBL_SHOW_ALT_TEXT' => '일반 텍스트 보이기',
+    'LBL_SUBJECT' => 'Subject:',
+    'LBL_SUITE_DOCUMENT' => '자료 리스트',
+    'LBL_UPLOAD_FILE' => '파일 업로드',
+    'LBL_TEXT_BODY' => '텍스트 본문',
+    'LBL_USERS' => '사용자',
+
+    'LNK_EMAIL_TEMPLATE_LIST' => 'View Email Templates',
+    'LNK_IMPORT_NOTES' => 'Import Notes',
+    'LNK_NEW_EMAIL_TEMPLATE' => 'Create Email Template',
+    'LNK_NEW_EMAIL' => '메일작성',
+    'LNK_NEW_SEND_EMAIL' => '이메일 작성하기',
+    'LNK_SENT_EMAIL_LIST' => '송신한메일',
+    'LNK_VIEW_CALENDAR' => 'Today',
+    // for Inbox
+    'LBL_NEW' => '신규',
+    'LNK_MY_DRAFTS' => '내 초안',
+    'LNK_MY_INBOX' => '내 이메일',
+    'LBL_TEXT_ONLY' => '텍스트만 표시',
+    'LBL_SEND_AS_TEXT' => '텍스트 전송만 가능',
+    'LBL_ACCOUNT' => '거래처',
+    'LBL_FROM_NAME' => 'From Name',
+    'LBL_PLAIN_TEXT' => '일반 텍스트',
+    'LBL_CREATED_BY' => '작성자',
+    'LBL_PUBLISHED' => '게시됨',
+    'LNK_VIEW_MY_INBOX' => '내 이메일 보기',
+    'LBL_ASSIGNED_TO_ID' => '담당자',
+    'LBL_EDIT_LAYOUT' => '지면 배치 편집하기' /*for 508 compliance fix*/,
+    'LBL_SELECT' => 'Select' /*for 508 compliance fix*/,
+    'LBL_ID_FF_CLEAR' => 'Clear' /*for 508 compliance fix*/,
+    'LBL_TYPE' => '유형',
+    'LBL_WIDTH' => '기본너비',
+    'LNK_IMPORT_CAMPAIGNS' => '캠페인 가져오기',
+);
