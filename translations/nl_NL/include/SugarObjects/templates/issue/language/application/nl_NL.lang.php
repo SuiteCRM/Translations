d575a00f166ea725e67c0c81140ee8382e83fcae
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-04-30 14:19:21 +0100

    New translations en_us.lang.php (Dutch)
    [ci skip]

--- translations/nl_NL/include/SugarObjects/templates/issue/language/application/nl_NL.lang.php
+++ translations/nl_NL/include/SugarObjects/templates/issue/language/application/nl_NL.lang.php
@@ -66,8 +66,8 @@ $app_list_strings = array(
     $object_name . '_resolution_dom' => array(
         '' => '',
         'Accepted' => 'Accepteer',
-        'Duplicate' => 'Duplicate',
-        'Closed' => 'Closed',
+        'Duplicate' => 'Duplicaat',
+        'Closed' => 'Gesloten',
         'Out of Date' => 'Verlopen',
         'Invalid' => 'Ongeldig',
     ),
