9e0bb0fb408519b4ddcc07d506e988084d7632a2
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-04-30 18:50:57 +0100

    New translations en_us.lang.php (Hebrew)
    [ci skip]

---
+++ translations/he_IL/modules/Calendar/language/he_IL.lang.php
@@ -0,0 +1,225 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+
+    'LBL_SHAREDWEEK' => 'שיתוף שבוע',
+    'LBL_SHAREDMONTH' => 'שיתוף חודש',
+
+    'LBL_MODULE_NAME' => 'לוח שנה',
+    'LBL_MODULE_TITLE' => 'לוח שנה',
+    'LNK_NEW_CALL' => 'תזמון שיחה',
+    'LNK_NEW_MEETING' => 'לקבוע פגישה',
+    'LNK_NEW_TASK' => 'צור משימה',
+    'LNK_CALL_LIST' => 'Calls',
+    'LNK_MEETING_LIST' => 'Meetings',
+    'LNK_TASK_LIST' => 'משימות',
+    'LNK_TASK' => 'משימשות',
+    'LNK_TASK_VIEW' => 'תצוגת משימה',
+    'LNK_EVENT' => 'ארוע',
+    'LNK_EVENT_VIEW' => 'הצג אירוע',
+    'LNK_VIEW_CALENDAR' => 'Today',
+    'LNK_IMPORT_CALLS' => 'יבוא שיחות',
+    'LNK_IMPORT_MEETINGS' => 'יבוא פגישות',
+    'LNK_IMPORT_TASKS' => 'יבוא משימות',
+    'LBL_MONTH' => 'חודש',
+    'LBL_AGENDADAY' => 'יום',
+    'LBL_YEAR' => 'שנה',
+
+    'LBL_AGENDAWEEK' => 'שבוע',
+    'LBL_PREVIOUS_MONTH' => 'חודש קודם',
+    'LBL_PREVIOUS_DAY' => 'יום קודם',
+    'LBL_PREVIOUS_YEAR' => 'שנה קודמת',
+    'LBL_PREVIOUS_WEEK' => 'שבוע קודמת',
+    'LBL_NEXT_MONTH' => 'Next Month',
+    'LBL_NEXT_DAY' => 'יום הבא',
+    'LBL_NEXT_YEAR' => 'Next Year',
+    'LBL_NEXT_WEEK' => 'שבוע הבא',
+    'LBL_AM' => 'AM',
+    'LBL_PM' => 'PM',
+    'LBL_SCHEDULED' => 'מתוכנן',
+    'LBL_BUSY' => 'עסוק',
+    'LBL_CONFLICT' => 'מתנגש',
+    'LBL_USER_CALENDARS' => 'לוחות שנה של המשתמשים',
+    'LBL_SHARED' => 'Shared',
+    'LBL_PREVIOUS_SHARED' => 'Previous',
+    'LBL_NEXT_SHARED' => 'Next',
+    'LBL_SHARED_CAL_TITLE' => 'לוח שנה משותף',
+    'LBL_USERS' => 'משתמשים',
+    'LBL_REFRESH' => 'רענון',
+    'LBL_EDIT_USERLIST' => 'User List',
+    'LBL_SELECT_USERS' => 'בחר משתמשים לצפייה בלוח שנה',
+    'LBL_FILTER_BY_TEAM' => 'סינון רשימת משתמשים לפי צוות:',
+    'LBL_ASSIGNED_TO_NAME' => 'מוקצה למשתמש',
+    'LBL_DATE' => 'מועד התחלה',
+    'LBL_CREATE_MEETING' => 'לקבוע פגישה',
+    'LBL_CREATE_CALL' => 'יומן שיחה',
+    'LBL_HOURS_ABBREV' => 'ה',
+    'LBL_MINS_ABBREV' => 'מ',
+
+
+    'LBL_YES' => 'Yes',
+    'LBL_NO' => 'No',
+    'LBL_SETTINGS' => 'Settings',
+    'LBL_CREATE_NEW_RECORD' => 'צור פעילות',
+    'LBL_LOADING' => 'Loading...',
+    'LBL_SAVING' => 'Saving...',
+    'LBL_SENDING_INVITES' => 'שמור ושלח הזמנות',
+    'LBL_CONFIRM_REMOVE' => 'האם אתה בטוח שברצונך למחוק את הרשומה?',
+    'LBL_CONFIRM_REMOVE_ALL_RECURRING' => 'האם אתה בטוח שברצונך להסיר את כל המופעים החוזרים?',
+    'LBL_EDIT_RECORD' => 'עריכת פעילות',
+    'LBL_ERROR_SAVING' => 'ארעה שגיאה בעת שמירה',
+    'LBL_ERROR_LOADING' => 'ארעה שגיאה בעת טעינה',
+    'LBL_GOTO_DATE' => 'עבור לתאריך',
+    'NOTICE_DURATION_TIME' => 'משך זמן חייב להיות גדול מאפס',
+    'LBL_STYLE_BASIC' => 'BASIC', //No need to be translated in all caps. Translation used just in menu action items when using the SuiteP template
+    'LBL_STYLE_ADVANCED' => 'מתקדם', //No need to be translated in all caps. Translation used just in menu action items when using the SuiteP template
+
+    'LBL_NO_USER' => 'אין התאמה עבור שדה: הוקצה עבור',
+    'LBL_SUBJECT' => 'Subject',
+    'LBL_DURATION' => 'Duration',
+    'LBL_STATUS' => 'Status',
+    'LBL_PRIORITY' => 'עדיפות',
+
+    'LBL_SETTINGS_TITLE' => 'Settings',
+    'LBL_SETTINGS_DISPLAY_TIMESLOTS' => 'תצוגת משבצות הזמן בתצוגות יום ושבוע:',
+    'LBL_SETTINGS_TIME_STARTS' => 'שעת התחלה:',
+    'LBL_SETTINGS_TIME_ENDS' => 'מועד סיום:',
+    'LBL_SETTINGS_CALLS_SHOW' => 'הצג שיחות:',
+    'LBL_SETTINGS_TASKS_SHOW' => 'הצגת משימות:',
+    'LBL_SETTINGS_COMPLETED_SHOW' => 'הצג פגישות, שיחות ופעילויות שהושלמו:',
+    'LBL_SETTINGS_DISPLAY_SHARED_CALENDAR_SEPARATE' => 'Shared Calendar Separate:',
+
+    'LBL_SAVE_BUTTON' => 'שמור',
+    'LBL_DELETE_BUTTON' => 'Delete',
+    'LBL_APPLY_BUTTON' => 'החל',
+    'LBL_SEND_INVITES' => 'שלח הזמנות',
+    'LBL_CANCEL_BUTTON' => 'Cancel',
+    'LBL_CLOSE_BUTTON' => 'Close',
+
+    'LBL_GENERAL_TAB' => 'Details',
+    'LBL_PARTICIPANTS_TAB' => 'Invitees',
+    'LBL_REPEAT_TAB' => 'מופע חוזר',
+
+    'LBL_REPEAT_TYPE' => 'חזור שוב',
+    'LBL_REPEAT_INTERVAL' => 'כל',
+    'LBL_REPEAT_END' => 'End',
+    'LBL_REPEAT_END_AFTER' => 'After',
+    'LBL_REPEAT_OCCURRENCES' => 'מופעים חוזרים',
+    'LBL_REPEAT_END_BY' => 'על ידי',
+    'LBL_REPEAT_DOW' => 'פועל',
+    'LBL_REPEAT_UNTIL' => 'חזור עד ש',
+    'LBL_REPEAT_COUNT' => 'מספר מופעים חוזרים',
+    'LBL_REPEAT_LIMIT_ERROR' => 'הבקשה שלך תיצור יותר מאשר $limit פגישות.',
+
+    'LBL_EDIT_ALL_RECURRENCES' => 'עריכת כל המופעים החוזרים',
+    'LBL_REMOVE_ALL_RECURRENCES' => 'מחיקת כל המופעים החוזרים',
+
+    'LBL_DATE_END_ERROR' => 'תאריך סיום הוא מוקדם יותר מתאריך ההתחלה',
+    'ERR_YEAR_BETWEEN' => '. מצטערים, לוח שנה לא יכולה להתמודד השנה המבוקשת <br>השנה חייבת להיות בין 1970 ל- 2037',
+    'ERR_NEIGHBOR_DATE' => 'get_neighbor_date_str: אינה מוגדרת עבור תצוגה זו',
+    'LBL_NO_ITEMS_MOBILE' => 'את לוח השנה שלך פנוי השבוע.',
+    'LBL_GENERAL_SETTINGS' => 'הגדרות כלליות',
+    'LBL_COLOR_SETTINGS' => 'הגדרות צבע',
+    'LBL_MODULE' => 'Module',
+    'LBL_BODY' => 'Body',
+    'LBL_BORDER' => 'גבול',
+    'LBL_TEXT' => 'Text',
+);
+
+
+$mod_list_strings = array(
+    'dom_cal_weekdays' =>
+        array(
+            '0' => "ראשון",
+            '1' => "שני",
+            '2' => "שלישי",
+            '3' => "רביעי",
+            '4' => "חמישי",
+            '5' => "שישי",
+            '6' => "שבת",
+        ),
+    'dom_cal_weekdays_long' =>
+        array(
+            '0' => "יום ראשון",
+            '1' => "שני",
+            '2' => "שלישי",
+            '3' => "רביעי",
+            '4' => "חמישי",
+            '5' => "יום שישי",
+            '6' => "שבת",
+        ),
+    'dom_cal_month' =>
+        array(
+            '0' => "",
+            '1' => "Jan",
+            '2' => "Feb",
+            '3' => "Mar",
+            '4' => "Apr",
+            '5' => "May",
+            '6' => "Jun",
+            '7' => "Jul",
+            '8' => "Aug",
+            '9' => "Sep",
+            '10' => "Oct",
+            '11' => "Nov",
+            '12' => "Dec",
+        ),
+    'dom_cal_month_long' =>
+        array(
+            '0' => "",
+            '1' => "January",
+            '2' => "February",
+            '3' => "March",
+            '4' => "April",
+            '5' => "May",
+            '6' => "June",
+            '7' => "July",
+            '8' => "August",
+            '9' => "September",
+            '10' => "October",
+            '11' => "November",
+            '12' => "דצמבר",
+        ),
+);
