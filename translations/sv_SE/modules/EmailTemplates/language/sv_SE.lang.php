a9becf51275a4e4ad6d51bf609f11bdc3442be4d
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-04-30 19:26:32 +0100

    New translations en_us.lang.php (Swedish)
    [ci skip]

---
+++ translations/sv_SE/modules/EmailTemplates/language/sv_SE.lang.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ADD_DOCUMENT' => 'Lägg till ett dokument',
+    'LBL_ADD_FILE' => 'Lägg till en fil',
+    'LBL_ATTACHMENTS' => 'Attachments',
+    'LBL_BODY' => 'Body:',
+    'LBL_CLOSE' => 'Stäng:',
+    'LBL_DESCRIPTION' => 'Description:',
+    'LBL_EDIT_ALT_TEXT' => 'Redigera enkel text',
+    'LBL_EMAIL_ATTACHMENT' => 'Email Attachment',
+    'LBL_HIDE_ALT_TEXT' => 'Hide Plain Text',
+    'LBL_HTML_BODY' => 'HTML-body',
+    'LBL_INSERT_VARIABLE' => 'Infoga variabel:',
+    'LBL_INSERT_URL_REF' => 'Insert URL Reference',
+    'LBL_INSERT_TRACKER_URL' => 'Insert Tracker URL:',
+    'LBL_INSERT' => 'Insert',
+    'LBL_LIST_DATE_MODIFIED' => 'Last Modified',
+    'LBL_LIST_DESCRIPTION' => 'Description',
+    'LBL_LIST_FORM_TITLE' => 'Lista över E-postmallar',
+    'LBL_LIST_NAME' => 'Namn',
+    'LBL_MODULE_NAME' => 'Email Templates',
+    'LBL_MODULE_TITLE' => 'E-postmallar: Hem',
+    'LBL_NAME' => 'Name:',
+    'LBL_NEW_FORM_TITLE' => 'Create Email Template',
+    'LBL_PUBLISH' => 'Publicera:',
+    'LBL_RELATED_TO' => 'Related To:',
+    'LBL_SEARCH_FORM_TITLE' => 'Sökning av E-postmallar',
+    'LBL_SHOW_ALT_TEXT' => 'Visa enkel text',
+    'LBL_SUBJECT' => 'Subject:',
+    'LBL_SUITE_DOCUMENT' => 'Document',
+    'LBL_UPLOAD_FILE' => 'Ladda up fil',
+    'LBL_TEXT_BODY' => 'Textmeddelande',
+    'LBL_USERS' => 'Users',
+
+    'LNK_EMAIL_TEMPLATE_LIST' => 'View Email Templates',
+    'LNK_IMPORT_NOTES' => 'Import Notes',
+    'LNK_NEW_EMAIL_TEMPLATE' => 'Create Email Template',
+    'LNK_NEW_EMAIL' => 'Archive Email',
+    'LNK_NEW_SEND_EMAIL' => 'Compose Email',
+    'LNK_SENT_EMAIL_LIST' => 'Skickad E-post',
+    'LNK_VIEW_CALENDAR' => 'Today',
+    // for Inbox
+    'LBL_NEW' => 'New',
+    'LNK_MY_DRAFTS' => 'Mina utkast',
+    'LNK_MY_INBOX' => 'Min inbox',
+    'LBL_TEXT_ONLY' => 'Endast text',
+    'LBL_SEND_AS_TEXT' => 'Sänd endast text',
+    'LBL_ACCOUNT' => 'Account',
+    'LBL_FROM_NAME' => 'From Name',
+    'LBL_PLAIN_TEXT' => 'Brödtext',
+    'LBL_CREATED_BY' => 'Created By',
+    'LBL_PUBLISHED' => 'Publiserad',
+    'LNK_VIEW_MY_INBOX' => 'Visa Min Epost',
+    'LBL_ASSIGNED_TO_ID' => 'Assigned To',
+    'LBL_EDIT_LAYOUT' => 'Redigera layout' /*for 508 compliance fix*/,
+    'LBL_SELECT' => 'Select' /*for 508 compliance fix*/,
+    'LBL_ID_FF_CLEAR' => 'Clear' /*for 508 compliance fix*/,
+    'LBL_TYPE' => 'Type',
+    'LBL_WIDTH' => 'Width Default',
+    'LNK_IMPORT_CAMPAIGNS' => 'Importera kampanj',
+);
