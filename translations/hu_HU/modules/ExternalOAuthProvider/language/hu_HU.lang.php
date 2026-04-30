e1e4697e6ccc622f2970620cf9c4610b52f54ca0
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-04-30 19:32:00 +0100

    New translations en_us.lang.php (Hungarian)
    [ci skip]

---
+++ translations/hu_HU/modules/ExternalOAuthProvider/language/hu_HU.lang.php
@@ -0,0 +1,110 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2022 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = [
+
+    'LBL_ASSIGNED_TO_ID' => 'Felelős felhasználó ID',
+    'LBL_ASSIGNED_TO_NAME' => 'Felelős',
+    'LBL_ID' => 'ID',
+    'LBL_DATE_ENTERED' => 'Létrehozva',
+    'LBL_DATE_MODIFIED' => 'Módosítás dátuma',
+    'LBL_MODIFIED' => 'Modified By',
+    'LBL_MODIFIED_NAME' => 'Modified By Name',
+    'LBL_CREATED' => 'Létrehozta',
+    'LBL_DESCRIPTION' => 'Üzenet tartalma',
+    'LBL_DELETED' => 'Deleted',
+    'LBL_NAME' => 'Név',
+    'LBL_CREATED_USER' => 'Felhasználó által létrehozva',
+    'LBL_MODIFIED_USER' => 'Felhasználó által módosítva',
+    'LBL_LIST_NAME' => 'Elnevezés',
+    'LBL_EDIT_BUTTON' => 'Szerkesztés',
+    'LBL_REMOVE' => 'Eltávolítás',
+    'LBL_LIST_FORM_TITLE' => 'Külső OAuth szolgáltatók listája',
+    'LBL_MODULE_NAME' => 'External OAuth Providers',
+    'LBL_MODULE_TITLE' => 'External OAuth Providers',
+    'LBL_HOMEPAGE_TITLE' => 'Saját külső OAuth szolgáltatóim',
+    'LNK_NEW_RECORD' => 'Külső OAuth szolgáltató létrehozása',
+
+    'LNK_LIST' => 'External OAuth Providers',
+    'LBL_SEARCH_FORM_TITLE' => 'Külső OAuth szolgáltatók keresése',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'Előzmény Megtekintése',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Activities',
+    'LBL_NEW_FORM_TITLE' => 'Új külső OAuth szolgáltató',
+
+    'LBL_LIST_DELETE' => 'Törlés',
+    'LBL_TYPE' => 'Type',
+    'LBL_CONNECTOR' => 'Csatlakozó',
+    'LBL_REDIRECT_URI' => 'Átirányítási URL',
+    'LBL_REDIRECT_URI_TYPE' => 'Redirect URI Type',
+
+    'LBL_CLIENT_ID' => 'Client Id',
+    'LBL_CLIENT_SECRET' => 'Client Secret',
+    'LBL_SCOPE' => 'Terjedelem',
+    'LBL_URL_AUTHORIZE' => 'Engedélyezési URL',
+    'LBL_AUTHORIZE_URL_OPTIONS' => '"Engedélyezési URL beállításai',
+    'LBL_URL_ACCESS_TOKEN' => 'Hozzáférési token URL',
+    'LBL_EXTRA_PROVIDER_PARAMS' => 'További szolgáltató paraméterek',
+    'LBL_GET_TOKEN_REQUEST_GRANT' => 'Token kérés grant típusának lekérése',
+    'LBL_GET_TOKEN_REQUEST_OPTIONS' => 'Token kérés beállításainak lekérése',
+    'LBL_REFRESH_TOKEN_REQUEST_GRANT' => 'Frissítési token kérés grant típusa',
+    'LBL_REFRESH_TOKEN_REQUEST_OPTIONS' => 'Frissítési token kérés beállításai',
+
+    'LBL_ACCESS_TOKEN_MAPPING' => 'Hozzáférési token leképezése',
+    'LBL_EXPIRES_IN_MAPPING' => 'Lejár a következő időben leképezése',
+    'LBL_REFRESH_TOKEN_MAPPING' => 'Frissítési token leképezése',
+    'LBL_TOKEN_TYPE_MAPPING' => 'Token típusa leképezése',
+
+    'LBL_EXTRA' => 'További konfigurációk',
+    'LBL_MAPPING' => 'Leképezési konfigurációk',
+    'LBL_OTHER' => 'Other',
+
+
+    'LNK_LIST_CREATE_NEW_PERSONAL' => 'Új személyes szolgáltató',
+    'LNK_LIST_CREATE_NEW_GROUP' => 'Új csoportszolgáltató',
+    'LNK_LIST_INBOUND_EMAILS' => 'Beérkező email fiók',
+    'LNK_LIST_OUTBOUND_EMAILS' => 'Outbound Email Accounts',
+    'LNK_LIST_EXTERNAL_OAUTH_CONNECTION' => 'Külső OAuth kapcsolatok',
+
+    'LBL_OWNER' => 'Owner',
+];
