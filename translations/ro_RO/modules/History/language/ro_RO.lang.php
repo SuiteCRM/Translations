18f2454c92603a50f7b4abd83b4b6f27a5fcff05
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-04-30 19:40:09 +0100

    New translations en_us.lang.php (Romanian)
    [ci skip]

---
+++ translations/ro_RO/modules/History/language/ro_RO.lang.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_MODULE_NAME' => 'Istorie',
+    'LBL_MODULE_TITLE' => 'Istoric: Acasă',
+    'LBL_SEARCH_FORM_TITLE' => 'Căutare în istoric',
+    'LBL_LIST_FORM_TITLE' => 'Istorie',
+    'LBL_LIST_SUBJECT' => 'Subiect',
+    'LBL_LIST_CONTACT' => 'lista contact',
+    'LBL_LIST_RELATED_TO' => 'Asociat cu',
+    'LBL_LIST_DATE' => 'Data',
+    'LBL_LIST_CLOSE' => 'Închideți',
+    'LBL_SUBJECT' => 'Subiect',
+    'LBL_STATUS' => 'Status:',
+    'LBL_LOCATION' => 'Locație:',
+    'LBL_DATE_TIME' => 'Data și ora de început:',
+    'LBL_DATE' => 'Dată Începere:',
+    'LBL_TIME' => 'Ora Începere:',
+    'LBL_DURATION' => 'Durată:',
+    'LBL_HOURS_MINS' => 'ore/minute',
+    'LBL_CONTACT_NAME' => 'Contact Name: ',
+    'LBL_DESCRIPTION' => 'Descriere:',
+    'LNK_NEW_CALL' => 'Jurnal Apeluri',
+    'LNK_NEW_MEETING' => 'Schedule Meeting',
+    'LNK_NEW_TASK' => 'Creează sarcină',
+    'LNK_NEW_NOTE' => 'Nouă notă',
+    'LNK_NEW_EMAIL' => 'Arhivare e-mail',
+    'LNK_CALL_LIST' => 'Apeluri',
+    'LNK_MEETING_LIST' => 'Intalniri',
+    'LNK_TASK_LIST' => 'Sarcini',
+    'LNK_NOTE_LIST' => 'Note',
+    'ERR_DELETE_RECORD' => 'Un număr de înregistrare trebuie specificat pentru a șterge această organizație.',
+    'LBL_INVITEE' => 'Invitați',
+    'LBL_LIST_DIRECTION' => 'Direcție',
+    'LBL_DIRECTION' => 'Direcție',
+    'LNK_NEW_APPOINTMENT' => 'Programare Noua',
+    'LNK_VIEW_CALENDAR' => 'Azi',
+    'LBL_OPEN_ACTIVITIES' => 'Activități deschise',
+    'LBL_HISTORY' => 'Istorie',
+    'LBL_NEW_TASK_BUTTON_TITLE' => 'Creare sarcină',
+    'LBL_NEW_TASK_BUTTON_LABEL' => 'Creare sarcină',
+    'LBL_SCHEDULE_MEETING_BUTTON_TITLE' => 'Programare Întâlnire',
+    'LBL_SCHEDULE_MEETING_BUTTON_LABEL' => 'Programare Întâlnire',
+    'LBL_SCHEDULE_CALL_BUTTON_LABEL' => 'Nou apel',
+    'LBL_NEW_NOTE_BUTTON_TITLE' => 'Creează notă sau atașament',
+    'LBL_NEW_NOTE_BUTTON_LABEL' => 'Creează notă sau atașament',
+    'LBL_TRACK_EMAIL_BUTTON_TITLE' => 'Arhivează email',
+    'LBL_TRACK_EMAIL_BUTTON_LABEL' => 'Arhivează email',
+    'LBL_LIST_STATUS' => 'Statut',
+    'LBL_LIST_DUE_DATE' => 'Dată limită',
+    'LBL_LIST_LAST_MODIFIED' => 'Modificat la',
+    'LNK_IMPORT_NOTES' => 'Importă note',
+    'LBL_ACCEPT_THIS' => 'Acceptați?',
+    'LBL_DEFAULT_SUBPANEL_TITLE' => 'Istorie',
+
+    'LBL_CATEGORY' => 'Categorie',
+    'LBL_LIST_CATEGORY' => 'Categorie',
+);
