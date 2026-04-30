9b258cbb5b00046cb41f6bd6bacea3ba3f3dbb6b
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-04-30 18:31:05 +0100

    New translations en_us.lang.php (Finnish)
    [ci skip]

---
+++ translations/fi_FI/modules/AOS_Products/language/fi_FI.lang.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID' => 'Assigned User Id',
+    'LBL_ASSIGNED_TO_NAME' => 'Assigned to',
+    'LBL_ID' => 'Tunnus',
+    'LBL_DATE_ENTERED' => 'Date Created',
+    'LBL_DATE_MODIFIED' => 'Muokkauspvm',
+    'LBL_MODIFIED' => 'Modified By',
+    'LBL_MODIFIED_NAME' => 'Modified By Name',
+    'LBL_CREATED' => 'Created By',
+    'LBL_DESCRIPTION' => 'Description',
+    'LBL_DELETED' => 'Deleted',
+    'LBL_NAME' => 'Tuotteen nimi',
+    'LBL_CREATED_USER' => 'Created by User',
+    'LBL_MODIFIED_USER' => 'Modified by User',
+    'LBL_LIST_FORM_TITLE' => 'Tuotteet',
+    'LBL_MODULE_NAME' => 'Products',
+    'LBL_MODULE_TITLE' => 'Tuotteet',
+    'LBL_HOMEPAGE_TITLE' => 'Omat tuotteet',
+    'LNK_NEW_RECORD' => 'Luo tuote',
+    'LNK_LIST' => 'Tuotteet',
+    'LBL_SEARCH_FORM_TITLE' => 'Hae tuotteita',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'View History',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Activities',
+    'LBL_NEW_FORM_TITLE' => 'Uusi tuote',
+    'LBL_MAINCODE' => 'Tuotenumero',
+    'VALUE' => 'Product Name',
+    'LBL_PART_NUMBER' => 'Part Number',
+    'LBL_CATEGORY' => 'Category',
+    'LBL_TYPE' => 'Tuotteen tyyppi',
+    'LBL_COST' => 'Kustannus',
+    'LBL_PRICE' => 'Hinta',
+    'LBL_URL' => 'URL-osoite',
+    'LBL_CONTACT' => 'Contact',
+    'LBL_PRODUCT_IMAGE' => 'Tuotekuva',
+    'LBL_IMAGE_UPLOAD_FAIL' => 'VIRHE: ladattu tiedosto ylitti tiedostojen maksimikoon: maksimi tiedostokoko: ',
+    'LBL_AOS_PRODUCT_CATEGORYS_NAME' => 'Tuoteryhmä',
+    'LBL_AOS_PRODUCT_CATEGORY' => 'Tuoteryhmä ID',
+    'LBL_AOS_PRODUCT_CATEGORIES' => 'Product Categories',
+    'LBL_COST_USDOLLAR' => 'Kustannus (oletus valuutta)',
+    'LBL_PRICE_USDOLLAR' => 'Hinta (Oletusvaluutta)',
+    'LBL_FILE_URL' => 'Tiedoston URL',
+    'LBL_CUSTOMERS_PURCHASED_PRODUCTS_SUBPANEL_TITLE' => 'Ostot',
+    'LBL_PRODUCTS_PURCHASES' => 'Ostot',
+    'LBL_AOS_QUOTE_NAME' => 'Quote',
+    'LBL_ACCOUNT_NAME' => 'Asiakas',
+);
