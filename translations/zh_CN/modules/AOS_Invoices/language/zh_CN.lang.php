39778f103f6d11234eeb9ed7257deefdb9d9353a
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-04-30 18:25:50 +0100

    New translations en_us.lang.php (Chinese Simplified)
    [ci skip]

---
+++ translations/zh_CN/modules/AOS_Invoices/language/zh_CN.lang.php
@@ -0,0 +1,203 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = array(
+    'LBL_ASSIGNED_TO_ID' => '负责人ID',
+    'LBL_ASSIGNED_TO_NAME' => '分配给',
+    'LBL_ID' => 'ID',
+    'LBL_DATE_ENTERED' => '创建日期',
+    'LBL_DATE_MODIFIED' => '修改日期',
+    'LBL_MODIFIED' => '修改人',
+    'LBL_MODIFIED_NAME' => '修改人姓名',
+    'LBL_CREATED' => '创建人',
+    'LBL_DESCRIPTION' => '说明',
+    'LBL_DELETED' => '已删除',
+    'LBL_NAME' => '标题',
+    'LBL_CREATED_USER' => '创建人',
+    'LBL_MODIFIED_USER' => '修改人',
+    'ERR_DELETE_RECORD' => 'A record number must be specified to delete the account.',
+    'LBL_ACCOUNT_NAME' => '标题',
+    'LBL_ACCOUNT' => 'Company:',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => '活动',
+    'LBL_ADDRESS_INFORMATION' => 'Address Information',
+    'LBL_ANNUAL_REVENUE' => 'Annual Revenue:',
+    'LBL_ANY_ADDRESS' => '任何地址',
+    'LBL_ANY_EMAIL' => '任何邮箱',
+    'LBL_ANY_PHONE' => '任何电话',
+    'LBL_RATING' => '评分',
+    'LBL_ASSIGNED_USER' => '用户',
+    'LBL_BILLING_ADDRESS_CITY' => 'Billing City:',
+    'LBL_BILLING_ADDRESS_COUNTRY' => '[账单地址]国家',
+    'LBL_BILLING_ADDRESS_POSTALCODE' => '[账单地址]邮编',
+    'LBL_BILLING_ADDRESS_STATE' => 'Billing State:',
+    'LBL_BILLING_ADDRESS_STREET_2' => 'Billing Street 2',
+    'LBL_BILLING_ADDRESS_STREET_3' => 'Billing Street 3',
+    'LBL_BILLING_ADDRESS_STREET_4' => 'Billing Street 4',
+    'LBL_BILLING_ADDRESS_STREET' => 'Billing Street:',
+    'LBL_BILLING_ADDRESS' => 'Billing Address:',
+    'LBL_ACCOUNT_INFORMATION' => '客户信息',
+    'LBL_CITY' => 'City:',
+    'LBL_CONTACTS_SUBPANEL_TITLE' => '联系人',
+    'LBL_COUNTRY' => 'Country:',
+    'LBL_DEFAULT_SUBPANEL_TITLE' => '客户',
+    'LBL_DUPLICATE' => 'Possible Duplicate Account',
+    'LBL_EMAIL' => '电子邮件',
+    'LBL_EMPLOYEES' => 'Employees:',
+    'LBL_FAX' => 'Fax:',
+    'LBL_INDUSTRY' => 'Industry:',
+    'LBL_LIST_ACCOUNT_NAME' => 'Account Name',
+    'LBL_LIST_CITY' => '城市',
+    'LBL_LIST_EMAIL_ADDRESS' => 'Email 地址',
+    'LBL_LIST_PHONE' => 'Phone',
+    'LBL_LIST_STATE' => '省份',
+    'LBL_MEMBER_OF' => 'Member of:',
+    'LBL_MEMBER_ORG_SUBPANEL_TITLE' => '子成员',
+    'LBL_OTHER_EMAIL_ADDRESS' => 'Other Email:',
+    'LBL_OTHER_PHONE' => 'Other Phone:',
+    'LBL_OWNERSHIP' => 'Ownership:',
+    'LBL_PARENT_ACCOUNT_ID' => 'Parent Account ID',
+    'LBL_PHONE_ALT' => 'Alternate Phone:',
+    'LBL_PHONE_FAX' => 'Phone Fax:',
+    'LBL_PHONE_OFFICE' => 'Phone Office:',
+    'LBL_PHONE' => 'Phone:',
+    'LBL_POSTAL_CODE' => 'Postal Code:',
+    'LBL_SAVE_ACCOUNT' => 'Save Account',
+    'LBL_SHIPPING_ADDRESS_CITY' => 'Shipping City:',
+    'LBL_SHIPPING_ADDRESS_COUNTRY' => 'Shipping Country:',
+    'LBL_SHIPPING_ADDRESS_POSTALCODE' => 'Shipping Postal Code:',
+    'LBL_SHIPPING_ADDRESS_STATE' => 'Shipping State:',
+    'LBL_SHIPPING_ADDRESS_STREET_2' => 'Shipping Street 2',
+    'LBL_SHIPPING_ADDRESS_STREET_3' => 'Shipping Street 3',
+    'LBL_SHIPPING_ADDRESS_STREET_4' => 'Shipping Street 4',
+    'LBL_SHIPPING_ADDRESS_STREET' => 'Shipping Street:',
+    'LBL_SHIPPING_ADDRESS' => 'Shipping Address:',
+    'LBL_STATE' => '省份',
+    'LBL_TICKER_SYMBOL' => 'Ticker Symbol:',
+    'LBL_TYPE' => 'Type:',
+    'LBL_WEBSITE' => 'Website:',
+    'LNK_ACCOUNT_LIST' => '客户',
+    'LNK_NEW_ACCOUNT' => 'Create Account',
+    'MSG_DUPLICATE' => '您将要创建的客户记录可能与已经存在的客户记录重复。下面列出了包含类似名称的客户记录。点击创建客户来继续创建此新的客户，或者从下面列出的现有客户选择一个。',
+    'MSG_SHOW_DUPLICATES' => '您将要创建的客户可能与已经存在的客户重复。下面列出了包含类似名称的客户。<br />点击保存来继续创建此新的客户，或者点击取消。',
+    'NTC_DELETE_CONFIRMATION' => 'Are you sure you want to delete this record?',
+    'LBL_LIST_FORM_TITLE' => '发票列表',
+    'LBL_MODULE_NAME' => 'Invoices',
+    'LBL_MODULE_TITLE' => '发票',
+    'LBL_HOMEPAGE_TITLE' => '发票',
+    'LNK_NEW_RECORD' => '创建发票',
+    'LNK_LIST' => '查看发票',
+    'LBL_SEARCH_FORM_TITLE' => '搜索',
+    'LBL_HISTORY_SUBPANEL_TITLE' => '历史记录',
+    'LBL_NEW_FORM_TITLE' => '新建',
+    'LBL_TERMS_C' => '条款',
+    'LBL_APPROVAL_ISSUE' => '审批问题',
+    'LBL_APPROVAL_STATUS' => '审批状态',
+    'LBL_BILLING_ACCOUNT' => '客户',
+    'LBL_BILLING_CONTACT' => '联系人',
+    'LBL_EXPIRATION' => '有效期至',
+    'LBL_INVOICE_NUMBER' => '发票编号',
+    'LBL_OPPORTUNITY' => 'Opportunity Name',
+    'LBL_TEMPLATE_DDOWN_C' => '发票模板',
+    'LBL_STAGE' => '阶段',
+    'LBL_TERM' => '付款条款',
+    'LBL_SUBTOTAL_AMOUNT' => 'Subtotal',
+    'LBL_DISCOUNT_AMOUNT' => 'Discount',
+    'LBL_TAX_AMOUNT' => 'Tax',
+    'LBL_SHIPPING_AMOUNT' => '物流',
+    'LBL_TOTAL_AMT' => '总计',
+    'VALUE' => '标题',
+    'LBL_EMAIL_ADDRESSES' => 'Email 地址',
+    'LBL_LINE_ITEMS' => 'Line Items',
+    'LBL_GRAND_TOTAL' => 'Grand Total',
+    'LBL_QUOTE_NUMBER' => '报价编号',
+    'LBL_QUOTE_DATE' => '报价日期',
+    'LBL_INVOICE_DATE' => '开票日期',
+    'LBL_DUE_DATE' => '截止日期',
+    'LBL_STATUS' => '状态',
+    'LBL_INVOICE_STATUS' => '发票状态',
+    'LBL_PRODUCT_QUANITY' => 'Quantity',
+    'LBL_PRODUCT_NAME' => '产品',
+    'LBL_PART_NUMBER' => 'Part Number',
+    'LBL_PRODUCT_NOTE' => '备忘录',
+    'LBL_PRODUCT_DESCRIPTION' => '说明',
+    'LBL_LIST_PRICE' => '列表',
+    'LBL_DISCOUNT_AMT' => 'Discount',
+    'LBL_UNIT_PRICE' => 'Sale Price',
+    'LBL_TOTAL_PRICE' => '总计',
+    'LBL_VAT' => 'Tax', //VAT
+    'LBL_VAT_AMT' => 'Tax Amount', //VAT
+    'LBL_ADD_PRODUCT_LINE' => 'Add Product Line',
+    'LBL_SERVICE_NAME' => 'Service',
+    'LBL_SERVICE_LIST_PRICE' => '列表',
+    'LBL_SERVICE_PRICE' => 'Sale Price',
+    'LBL_SERVICE_DISCOUNT' => 'Discount',
+    'LBL_ADD_SERVICE_LINE' => 'Add Service Line ',
+    'LBL_REMOVE_PRODUCT_LINE' => '删除',
+    'LBL_PRINT_AS_PDF' => '查看 PDF 文件',
+    'LBL_EMAIL_INVOICE' => '邮件发送发票',
+    'LBL_LIST_NUM' => '编号',
+    'LBL_PDF_NAME' => 'Invoice',
+    'LBL_EMAIL_NAME' => '发票',
+    'LBL_NO_TEMPLATE' => '错误\n未找到模板。如果您没有创建发票模板，请到 PDF 模板模块创建一个发票模板。',
+    'LBL_SUBTOTAL_TAX_AMOUNT' => '小计+税',//pre shipping
+    'LBL_EMAIL_PDF' => 'Email PDF',
+    'LBL_ADD_GROUP' => 'Add Group',
+    'LBL_DELETE_GROUP' => 'Delete Group',
+    'LBL_GROUP_NAME' => 'Group Name',
+    'LBL_GROUP_TOTAL' => 'Group Total',
+    'LBL_SHIPPING_TAX' => 'Shipping Tax',
+    'LBL_SHIPPING_TAX_AMT' => 'Shipping Tax',
+    'LBL_IMPORT_LINE_ITEMS' => '导入条目',
+    'LBL_SUBTOTAL_AMOUNT_USDOLLAR' => 'Subtotal (Default Currency)',
+    'LBL_DISCOUNT_AMOUNT_USDOLLAR' => 'Discount (Default Currency)',
+    'LBL_TAX_AMOUNT_USDOLLAR' => 'Tax (Default Currency)',
+    'LBL_SHIPPING_AMOUNT_USDOLLAR' => 'Shipping (Default Currency)',
+    'LBL_TOTAL_AMT_USDOLLAR' => 'Total (Default Currency)',
+    'LBL_SHIPPING_TAX_AMT_USDOLLAR' => 'Shipping Tax (Default Currency)',
+    'LBL_GRAND_TOTAL_USDOLLAR' => 'Grand Total (Default Currency)',
+    'LBL_INVOICE_TO' => '发票给',
+    'LBL_AOS_LINE_ITEM_GROUPS' => '条目组',
+    'LBL_AOS_PRODUCT_QUOTES' => '产品报价',
+    'LBL_AOS_QUOTES_AOS_INVOICES' => '报价：发票',
+);
