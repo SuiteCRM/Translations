dee49a31c3748496538d8e1421e73c7c3c41af3c
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-05-01 21:03:39 +0100

    New translations en_us.lang.php (Turkish)
    [ci skip]

--- translations/tr_TR/modules/ExternalOAuthConnection/language/tr_TR.lang.php
+++ translations/tr_TR/modules/ExternalOAuthConnection/language/tr_TR.lang.php
@@ -45,7 +45,7 @@ if (!defined('sugarEntry') || !sugarEntry) {
 $mod_strings = [
 
     'LBL_ASSIGNED_TO_ID' => 'Atanmış kullanıcı kimliği',
-    'LBL_ASSIGNED_TO_NAME' => 'Atanmış kişi',
+    'LBL_ASSIGNED_TO_NAME' => 'Atanmış ilgili',
     'LBL_ID' => 'Kimlik',
     'LBL_DATE_ENTERED' => 'Eklenme tarihi',
     'LBL_DATE_MODIFIED' => 'Değiştirilme tarihi',
@@ -59,7 +59,7 @@ $mod_strings = [
     'LBL_MODIFIED_USER' => 'Değiştiren kullanıcı',
     'LBL_LIST_NAME' => 'Paket Adı:',
     'LBL_EDIT_BUTTON' => 'Değiştir',
-    'LBL_REMOVE' => 'Sil',
+    'LBL_REMOVE' => 'Kaldır',
     'LBL_LIST_FORM_TITLE' => 'Dış OAuth bağlantıları listesi',
     'LBL_MODULE_NAME' => 'Dış OAuth bağlantıları',
     'LBL_MODULE_TITLE' => 'Dış OAuth bağlantıları',
