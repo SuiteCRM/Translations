34366be10211aa08b5f87dcc5599f922ee83e71a
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-05-01 21:04:50 +0100

    New translations en_us.lang.php (Turkish)
    [ci skip]

--- translations/tr_TR/modules/AOD_IndexEvent/language/tr_TR.lang.php
+++ translations/tr_TR/modules/AOD_IndexEvent/language/tr_TR.lang.php
@@ -44,7 +44,7 @@ if (!defined('sugarEntry') || !sugarEntry) {
 
 $mod_strings = array(
     'LBL_ASSIGNED_TO_ID' => 'Atanmış kullanıcı kimliği',
-    'LBL_ASSIGNED_TO_NAME' => 'Atanmış kişi',
+    'LBL_ASSIGNED_TO_NAME' => 'Atanmış ilgili',
     'LBL_ID' => 'Kimlik',
     'LBL_DATE_ENTERED' => 'Eklenme tarihi',
     'LBL_DATE_MODIFIED' => 'Değiştirilme tarihi',
@@ -58,7 +58,7 @@ $mod_strings = array(
     'LBL_MODIFIED_USER' => 'Değiştiren kullanıcı',
     'LBL_LIST_NAME' => 'Paket Adı:',
     'LBL_EDIT_BUTTON' => 'Değiştir',
-    'LBL_REMOVE' => 'Sil',
+    'LBL_REMOVE' => 'Kaldır',
     'LBL_LIST_FORM_TITLE' => 'Dizin işlemleri listesi',
     'LBL_MODULE_NAME' => 'Dizin İşlemi',
     'LBL_MODULE_TITLE' => 'Dizin İşlemi',
@@ -70,8 +70,8 @@ $mod_strings = array(
     'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Etkinlikler',
     'LBL_NEW_FORM_TITLE' => 'Yeni dizin işlemi',
     'LBL_ERROR' => 'Hata:',
-    'LBL_SUCCESS' => 'Success',
-    'LBL_RECORD_MODULE' => 'Success',
+    'LBL_SUCCESS' => 'Başarılı',
+    'LBL_RECORD_MODULE' => 'Başarılı',
 
     'LBL_RECORD_ID' => 'Kayıt kimliği',
 );
