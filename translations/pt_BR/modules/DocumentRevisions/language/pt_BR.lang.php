01b1462869130010d3c38d48fe0629a88220569d
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-06-04 15:15:10 +0100

    New translations en_us.lang.php (Portuguese, Brazilian)
    [ci skip]

--- translations/pt_BR/modules/DocumentRevisions/language/pt_BR.lang.php
+++ translations/pt_BR/modules/DocumentRevisions/language/pt_BR.lang.php
@@ -66,7 +66,7 @@ $mod_strings = array(
 
     //document revisions.
     'LBL_REV_LIST_REVISION' => 'Revision',
-    'LBL_REV_LIST_ENTERED' => 'Criado em',
+    'LBL_REV_LIST_ENTERED' => 'Data de Criação',
     'LBL_REV_LIST_CREATED' => 'Created by',
     'LBL_REV_LIST_LOG' => 'Registro de Modificações',
     'LBL_REV_LIST_FILENAME' => 'File',
