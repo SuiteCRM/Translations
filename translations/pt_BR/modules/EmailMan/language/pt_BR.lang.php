abac6dd6f38781ce66e9e1dcefca85c76e9a760b
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-06-09 14:09:00 +0100

    New translations en_us.lang.php (Portuguese, Brazilian)
    [ci skip]

--- translations/pt_BR/modules/EmailMan/language/pt_BR.lang.php
+++ translations/pt_BR/modules/EmailMan/language/pt_BR.lang.php
@@ -44,7 +44,7 @@ if (!defined('sugarEntry') || !sugarEntry) {
 
 $mod_strings = array(
     'LBL_SEND_DATE_TIME' => 'Data de Envio',
-    'LBL_IN_QUEUE' => 'Em Análise',
+    'LBL_IN_QUEUE' => 'Em Andamento',
     'LBL_IN_QUEUE_DATE' => 'Data de Fila',
 
     'ERR_INT_ONLY_EMAIL_PER_RUN' => 'Apenas valores inteiros são aceitos para o número de e-mails enviados por lote',
@@ -66,7 +66,7 @@ $mod_strings = array(
     'LBL_LIST_CAMPAIGN' => 'Campanha',
     'LBL_LIST_FORM_TITLE' => 'Fila',
     'LBL_LIST_FROM_NAME' => 'Nome De',
-    'LBL_LIST_IN_QUEUE' => 'Em Análise',
+    'LBL_LIST_IN_QUEUE' => 'Em Andamento',
     'LBL_LIST_MESSAGE_NAME' => 'Mensagem de Marketing',
     'LBL_LIST_RECIPIENT_EMAIL' => 'Recipient Email',
     'LBL_LIST_RECIPIENT_NAME' => 'Recipient Name',
@@ -99,7 +99,7 @@ $mod_strings = array(
     'LBL_MODULE_TITLE' => 'Gestão da Fila de E-mails de Saída',
     'LBL_NOTIFICATION_ON_DESC' => 'Enviar notificações via e-mail quando determinados registros forem atribuídos.',
     'LBL_NOTIFY_FROMADDRESS' => 'Endereço "De":',
-    'LBL_NOTIFY_FROMNAME' => 'Nome De:',
+    'LBL_NOTIFY_FROMNAME' => 'Nome "De":',
     'LBL_NOTIFY_ON' => 'Notificações ligadas?',
     'LBL_NOTIFY_TITLE' => 'Opções de Notificações via E-mail',
     'LBL_OUTBOUND_EMAIL_TITLE' => 'Opções de E-mail de Saída',
