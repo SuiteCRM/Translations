d8cf6a218092411281c3518be8b546ad83714bb2
SuiteBot <34374233+SuiteBot@users.noreply.github.com>
2026-08-17 09:08:55 +0100

    New translations en_us.lang.php (Turkish)
    [ci skip]

--- translations/tr_TR/include/language/tr_TR.lang.php
+++ translations/tr_TR/include/language/tr_TR.lang.php
@@ -792,15 +792,15 @@ $app_list_strings = array(
         2 => 'Ögeleri doğrudan atarken yalnızca denetlemiş ögeler atamalısınız.',
     ),
     'dom_email_bool' => array(
-        'bool_true' => 'Yes',
+        'bool_true' => 'Evet',
         'bool_false' => 'Hayır',
     ),
     'dom_int_bool' => array(
-        1 => 'Yes',
+        1 => 'Evet',
         0 => 'Hayır',
     ),
     'dom_switch_bool' => array(
-        'on' => 'Yes',
+        'on' => 'Evet',
         'off' => 'Hayır',
         '' => 'Hayır',
     ),
